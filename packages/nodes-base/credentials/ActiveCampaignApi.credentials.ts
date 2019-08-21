3978b505d37a49cc9bcc4a41d536a4a37350aca6
Jan Oberhauser <jan.oberhauser@gmail.com>
2019-08-21 08:45:03 +0200

    :sparkles: Add ActiveCampaign-Node

---
+++ packages/nodes-base/credentials/ActiveCampaignApi.credentials.ts
@@ -0,0 +1,24 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+
+export class ActiveCampaignApi implements ICredentialType {
+	name = 'activeCampaignApi';
+	displayName = 'ActiveCampaign API';
+	properties = [
+		{
+			displayName: 'API URL',
+			name: 'apiUrl',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
---
+++ packages/nodes-base/nodes/ActiveCampaign/GenericFunctions.ts
@@ -0,0 +1,116 @@
+import {
+	IExecuteFunctions,
+	IHookFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+} from 'n8n-workflow';
+
+import { OptionsWithUri } from 'request';
+
+
+/**
+ * Make an API request to ActiveCampaign
+ *
+ * @param {IHookFunctions} this
+ * @param {string} method
+ * @param {string} url
+ * @param {object} body
+ * @returns {Promise<any>}
+ */
+export async function activeCampaignApiRequest(this: IHookFunctions | IExecuteFunctions, method: string, endpoint: string, body: IDataObject, query?: IDataObject, dataKey?: string): Promise<any> { // tslint:disable-line:no-any
+	const credentials = this.getCredentials('activeCampaignApi');
+	if (credentials === undefined) {
+		throw new Error('No credentials got returned!');
+	}
+
+	if (query === undefined) {
+		query = {};
+	}
+
+	const options: OptionsWithUri = {
+		headers: {
+			'Api-Token': credentials.apiKey,
+		},
+		method,
+		qs: query,
+		uri: `${credentials.apiUrl}${endpoint}`,
+		json: true
+	};
+
+	if (Object.keys(body).length !== 0) {
+		options.body = body;
+	}
+
+	try {
+		const responseData = await this.helpers.request(options);
+
+		if (responseData.success === false) {
+			throw new Error(`ActiveCampaign error response: ${responseData.error} (${responseData.error_info})`);
+		}
+
+		if (dataKey === undefined) {
+			return responseData;
+		} else {
+			return responseData[dataKey] as IDataObject;
+		}
+
+	} catch (error) {
+		if (error.statusCode === 403) {
+			// Return a clear error
+			throw new Error('The ActiveCampaign credentials are not valid!');
+		}
+
+		// If that data does not exist for some reason return the actual error
+		throw error;
+	}
+}
+
+
+
+/**
+ * Make an API request to paginated ActiveCampaign endpoint
+ * and return all results
+ *
+ * @export
+ * @param {(IHookFunctions | IExecuteFunctions)} this
+ * @param {string} method
+ * @param {string} endpoint
+ * @param {IDataObject} body
+ * @param {IDataObject} [query]
+ * @returns {Promise<any>}
+ */
+export async function activeCampaignApiRequestAllItems(this: IHookFunctions | IExecuteFunctions, method: string, endpoint: string, body: IDataObject, query?: IDataObject, dataKey?: string): Promise<any> { // tslint:disable-line:no-any
+
+	if (query === undefined) {
+		query = {};
+	}
+	query.limit = 100;
+	query.offset = 0;
+
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+
+	let itemsReceived = 0;
+	do {
+		responseData = await activeCampaignApiRequest.call(this, method, endpoint, body, query);
+
+		if (dataKey === undefined) {
+			returnData.push.apply(returnData, responseData);
+			itemsReceived += returnData.length;
+		} else {
+			returnData.push.apply(returnData, responseData[dataKey]);
+			itemsReceived += responseData[dataKey].length;
+		}
+
+		query.offset = itemsReceived;
+	} while (
+		responseData.meta !== undefined &&
+		responseData.meta.total !== undefined &&
+		responseData.meta.total > itemsReceived
+	);
+
+	return returnData;
+}
