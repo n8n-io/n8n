5aa2b6f73316ce063dff07ea796e4fe5487f96bf
Alexandre Nizoux <alexandre@nizoux.fr>
2019-10-26 13:42:22 +0200

    Double quotes to calm code autoreview
    Should now pass green on code review :)

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s /usr/share/zoneinfo/${GENERIC_TIMEZONE:-America/New_York} /etc/localtime
+ln -s "/usr/share/zoneinfo/${GENERIC_TIMEZONE:-America/New_York}" /etc/localtime
 
 exec "$@"
