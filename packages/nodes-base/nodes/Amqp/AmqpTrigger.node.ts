c31dc77b06f8b84d3b8323b2affb278851276736
Leo <leonard@linhome.ch>
2019-11-01 13:50:33 +0100

    added amqp sender

--- packages/nodes-base/nodes/Amqp/AmqpTrigger.node.ts
+++ packages/nodes-base/nodes/Amqp/AmqpTrigger.node.ts
@@ -7,44 +7,27 @@ import {
 } from 'n8n-workflow';
 
 
-export class AmqpListener implements INodeType {
+export class AmqpTrigger implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'AMQP Listener',
-		name: 'amqpListener',
+		displayName: 'AMQP Trigger',
+		name: 'amqpTrigger',
 		icon: 'file:amqp.png',
 		group: ['trigger'],
 		version: 1,
 		description: 'Listens to AMQP 1.0 Messages',
 		defaults: {
-			name: 'AMQP Listener',
+			name: 'AMQP Trigger',
 			color: '#00FF00',
 		},
 		inputs: [],
 		outputs: ['main'],
 		credentials: [{
 			name: 'amqp',
-			required: false,
+			required: true,
 		}],
 		properties: [
 			// Node properties which the user gets displayed and
 			// can change on the node.
-			{
-				displayName: 'Host',
-				name: 'hostname',
-				type: 'string',
-				default: 'localhost',
-				description: 'hostname of the amqp server',
-			},
-			{
-				displayName: 'Port',
-				name: 'port',
-				type: 'number',
-				typeOptions: {
-					minValue: 1,
-				},
-				default: 5672,
-				description: 'TCP Port to connect to',
-			},
 			{
 				displayName: 'Queue / Topic',
 				name: 'sink',
@@ -76,9 +59,10 @@ export class AmqpListener implements INodeType {
 	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
 
 		const credentials = this.getCredentials('amqp');
+		if (!credentials) {
+			throw new Error('Credentials are mandatory!');
+		}
 
-		const hostname = this.getNodeParameter('hostname', 'localhost') as string;
-		const port = this.getNodeParameter('port', 5672) as number;
 		const sink = this.getNodeParameter('sink', '') as string;
 		const clientname = this.getNodeParameter('clientname', '') as string;
 		const subscription = this.getNodeParameter('subscription', '') as string;
@@ -88,19 +72,18 @@ export class AmqpListener implements INodeType {
 		}
 		let durable: boolean = false;
 		if(subscription && clientname) {
-			console.log('durable subscription')
 			durable = true;
 		}
 
 		let container = require('rhea');
 		let connectOptions = {
-			host: hostname,
-			port: port,
+			host: credentials.hostname,
+			port: credentials.port,
 			reconnect: true,		// this id the default anyway
 			reconnect_limit: 50,	// try for max 50 times, based on a back-off algorithm
 			container_id: (durable ? clientname : null)
 		}
-		if (credentials) {
+		if (credentials.username || credentials.password) {
 			container.options.username = credentials.username;
 			container.options.password = credentials.password;
 		}
@@ -109,11 +92,8 @@ export class AmqpListener implements INodeType {
 		let self = this;
 
 		container.on('message', function (context: any) {
-			console.log('AMQP: received message id: ' + (context.message.message_id ? context.message.message_id : ''));
-			console.log(context.message.body);
 			if (context.message.message_id && context.message.message_id == lastMsgId) {
 				// ignore duplicate message check, don't think it's necessary, but it was in the rhea-lib example code
-				console.log('duplicate received: ' + context.message.message_id);
 				lastMsgId = context.message.message_id;
 				return;
 			}
@@ -141,25 +121,33 @@ export class AmqpListener implements INodeType {
 			}
 		}
 		connection.open_receiver(clientOptions);
-		console.log('AMQP: listener attached');
 
 
 		// The "closeFunction" function gets called by n8n whenever
 		// the workflow gets deactivated and can so clean up.
 		async function closeFunction() {
 			connection.close();
-			console.log('AMQP: listener closed');
 		}
 
 		// The "manualTriggerFunction" function gets called by n8n
 		// when a user is in the workflow editor and starts the
 		// workflow manually.
-		// does not make really sense for AMQP
+		// for AMQP it doesn't make much sense to wait here but
+		// for a new user who doesn't know how this works, it's better to wait and show a respective info message
 		async function manualTriggerFunction() {
-			console.log('AMQP: manual trigger clicked, this will make the node spinn, until a message is received on: ' + sink);
-/*			self.emit([self.helpers.returnJsonArray([{
-				error: '"manually triggered execution" stops the node right afterwards which unsubscribes the listener from the service bus. You need to activate the workflow to test.'
-			}])]); */
+
+			await new Promise( function( resolve ) {
+				let timeoutHandler = setTimeout(function() {
+					self.emit([self.helpers.returnJsonArray([{
+						error: 'Aborted, no message received within 30secs. This 30sec timeout is only set for "manually triggered execution". Active Workflows will listen indefinitely.'
+					}])]);
+					resolve(true);
+				}, 30000);
+				container.on('message', function (context: any) {
+					clearTimeout(timeoutHandler);
+					resolve(true);
+				});
+			});
 		}
 
 		return {
