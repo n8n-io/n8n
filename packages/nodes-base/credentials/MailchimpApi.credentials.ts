2e4cefac7fe4b6b9e64c44e9bd06f6e96ea175dd
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2019-11-12 15:48:45 -0500

    :rocket: node-setup

---
+++ packages/nodes-base/credentials/MailchimpApi.credentials.ts
@@ -0,0 +1,23 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class Mailchimp implements ICredentialType {
+	name = 'mailchimpApi';
+	displayName = 'Mailchimp API';
+	properties = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'Datacenter',
+			name: 'datacenter',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
