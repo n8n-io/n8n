451c87d8a99ebdde41b36d47a94699f91f52f0c0
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2019-11-14 18:44:07 -0500

    :sparkles: Done

--- packages/nodes-base/credentials/MailchimpApi.credentials.ts
+++ packages/nodes-base/credentials/MailchimpApi.credentials.ts
@@ -3,7 +3,7 @@ import {
 	NodePropertyTypes,
 } from 'n8n-workflow';
 
-export class Mailchimp implements ICredentialType {
+export class MailchimpApi implements ICredentialType {
 	name = 'mailchimpApi';
 	displayName = 'Mailchimp API';
 	properties = [
--- packages/nodes-base/nodes/Mailchimp/GenericFunctions.ts
+++ packages/nodes-base/nodes/Mailchimp/GenericFunctions.ts
@@ -41,3 +41,13 @@ export async function mailchimpApiRequest(this: IHookFunctions | IExecuteFunctio
 		throw error.response.body;
 	}
 }
+
+export function validateJSON(json: string | undefined): any { // tslint:disable-line:no-any
+	let result;
+	try {
+		result = JSON.parse(json!);
+	} catch (exception) {
+		result = '';
+	}
+	return result;
+}
