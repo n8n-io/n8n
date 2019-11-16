3cfcbead719c42a13d737acf2d22a0ce60830aa8
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2019-11-16 18:47:28 -0500

    :sparkles: Intercom node

--- packages/nodes-base/nodes/Intercom/GenericFunctions.ts
+++ packages/nodes-base/nodes/Intercom/GenericFunctions.ts
@@ -27,9 +27,6 @@ export async function intercomApiRequest(this: IHookFunctions | IExecuteFunction
 		json: true
 	};
 
-	console.log(options)
-
-
 	try {
 		return await this.helpers.request!(options);
 	} catch (error) {
@@ -42,4 +39,14 @@ export async function intercomApiRequest(this: IHookFunctions | IExecuteFunction
 		}
 		throw error.response.body;
 	}
+}
+
+export function validateJSON(json: string | undefined): any { // tslint:disable-line:no-any
+	let result;
+	try {
+		result = JSON.parse(json!);
+	} catch (exception) {
+		result = '';
+	}
+	return result;
 }
---
+++ packages/nodes-base/nodes/Intercom/LeadInterface.ts
@@ -0,0 +1,16 @@
+import { IDataObject } from "n8n-workflow";
+
+export interface ILeadCompany {
+	company_id?: string;
+}
+
+export interface ILead {
+	email: string;
+	phone?: string;
+	name?: string;
+	custom_attributes?: IDataObject;
+	companies?: ILeadCompany[];
+	last_request_at?: number;
+	unsubscribed_from_emails?: boolean;
+	update_last_request_at?: boolean;
+}
