9513a012aafd7eac156327618ed8d161c19b5377
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2019-12-01 16:47:53 -0500

    fixes done

--- packages/nodes-base/nodes/Jira/IssueDescription.ts
+++ packages/nodes-base/nodes/Jira/IssueDescription.ts
@@ -118,24 +118,6 @@ export const issueFields = [
 		description: 'Summary',
 	},
 	{
-		displayName: 'Parent Issue Key',
-		name: 'parentIssueKey',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'create',
-				],
-			},
-		},
-		default: '',
-		description: 'Parent Issue Key',
-	},
-	{
 		displayName: 'Additional Fields',
 		name: 'additionalFields',
 		type: 'collection',
@@ -152,6 +134,14 @@ export const issueFields = [
 			},
 		},
 		options: [
+			{
+				displayName: 'Parent Issue Key',
+				name: 'parentIssueKey',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'Parent Issue Key',
+			},
 			{
 				displayName: 'Labels',
 				name: 'labels',
@@ -226,65 +216,8 @@ export const issueFields = [
 		description: 'Issue Key',
 	},
 	{
-		displayName: 'Issue Type',
-		name: 'issueType',
-		type: 'options',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'update'
-				]
-			},
-		},
-		typeOptions: {
-			loadOptionsMethod: 'getIssueTypes',
-		},
-		default: '',
-		description: 'Issue Types',
-	},
-	{
-		displayName: 'Summary',
-		name: 'summary',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'update',
-				],
-			},
-		},
-		default: '',
-		description: 'Summary',
-	},
-	{
-		displayName: 'Parent Issue Key',
-		name: 'parentIssueKey',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'update',
-				],
-			},
-		},
-		default: '',
-		description: 'Parent Issue Key',
-	},
-	{
-		displayName: 'Additional Fields',
-		name: 'additionalFields',
+		displayName: 'Update Fields',
+		name: 'updateFields',
 		type: 'collection',
 		placeholder: 'Add Field',
 		default: {},
@@ -299,6 +232,33 @@ export const issueFields = [
 			},
 		},
 		options: [
+			{
+				displayName: 'Issue Type',
+				name: 'issueType',
+				type: 'options',
+				required: false,
+				typeOptions: {
+					loadOptionsMethod: 'getIssueTypes',
+				},
+				default: '',
+				description: 'Issue Types',
+			},
+			{
+				displayName: 'Summary',
+				name: 'summary',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'Summary',
+			},
+			{
+				displayName: 'Parent Issue Key',
+				name: 'parentIssueKey',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'Parent Issue Key',
+			},
 			{
 				displayName: 'Labels',
 				name: 'labels',
@@ -405,10 +365,11 @@ export const issueFields = [
 		description: 'Issue Key',
 	},
 	{
-		displayName: 'Fields',
-		name: 'fields',
-		type: 'string',
-		required: false,
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
 		displayOptions: {
 			show: {
 				resource: [
@@ -419,97 +380,72 @@ export const issueFields = [
 				],
 			},
 		},
-		default: '',
-		description: `A list of fields to return for the issue. This parameter accepts a comma-separated list. Use it to retrieve a subset of fields. Allowed values:
-		*all Returns all fields.
-		*navigable Returns navigable fields.
-		Any issue field, prefixed with a minus to exclude.`
-	},
-	{
-		displayName: 'Fields By Key',
-		name: 'fieldsByKey',
-		type: 'boolean',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'get',
-				],
+		options: [
+			{
+				displayName: 'Fields',
+				name: 'fields',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `A list of fields to return for the issue. This parameter accepts a comma-separated list.
+				Use it to retrieve a subset of fields. Allowed values:
+				*all Returns all fields.
+				*navigable Returns navigable fields.
+				Any issue field, prefixed with a minus to exclude.`
 			},
-		},
-		default: false,
-		description: `Indicates whether fields in fields are referenced by keys rather than IDs. This parameter is useful where fields have been added by a connect app and a field's key may differ from its ID.`,
-	},
-	{
-		displayName: 'Expand',
-		name: 'expand',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'get',
-				],
+			{
+				displayName: 'Fields By Key',
+				name: 'fieldsByKey',
+				type: 'boolean',
+				required: false,
+				default: false,
+				description: `Indicates whether fields in fields are referenced by keys rather than IDs.
+				This parameter is useful where fields have been added by a connect app and a field's key
+				may differ from its ID.`,
 			},
-		},
-		default: '',
-		description: `Use expand to include additional information about the issues in the response. This parameter accepts a comma-separated list. Expand options include:
-		renderedFields Returns field values rendered in HTML format.
-		names Returns the display name of each field.
-		schema Returns the schema describing a field type.
-		transitions Returns all possible transitions for the issue.
-		editmeta Returns information about how each field can be edited.
-		changelog Returns a list of recent updates to an issue, sorted by date, starting from the most recent.
-		versionedRepresentations Returns a JSON array for each version of a field's value, with the highest number representing the most recent version. Note: When included in the request, the fields parameter is ignored.`
-	},
-	{
-		displayName: 'Properties',
-		name: 'properties',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'get',
-				],
+			{
+				displayName: 'Expand',
+				name: 'expand',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `Use expand to include additional information about the issues in the response.
+				This parameter accepts a comma-separated list. Expand options include:
+				renderedFields Returns field values rendered in HTML format.
+				names Returns the display name of each field.
+				schema Returns the schema describing a field type.
+				transitions Returns all possible transitions for the issue.
+				editmeta Returns information about how each field can be edited.
+				changelog Returns a list of recent updates to an issue, sorted by date, starting from the most recent.
+				versionedRepresentations Returns a JSON array for each version of a field's value, with the highest number representing the most recent version. Note: When included in the request, the fields parameter is ignored.`
 			},
-		},
-		default: '',
-		description: `A list of issue properties to return for the issue. This parameter accepts a comma-separated list. Allowed values:
-		*all Returns all issue properties.
-		Any issue property key, prefixed with a minus to exclude.
-		Examples:
-		*all Returns all properties.
-		*all,-prop1 Returns all properties except prop1.
-		prop1,prop2 Returns prop1 and prop2 properties.
-		This parameter may be specified multiple times. For example, properties=prop1,prop2& properties=prop3.`
-	},
-	{
-		displayName: 'Update History',
-		name: 'updateHistory',
-		type: 'boolean',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'get',
-				],
+			{
+				displayName: 'Properties',
+				name: 'properties',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `A list of issue properties to return for the issue.
+				This parameter accepts a comma-separated list. Allowed values:
+				*all Returns all issue properties.
+				Any issue property key, prefixed with a minus to exclude.
+				Examples:
+				*all Returns all properties.
+				*all,-prop1 Returns all properties except prop1.
+				prop1,prop2 Returns prop1 and prop2 properties.
+				This parameter may be specified multiple times. For example, properties=prop1,prop2& properties=prop3.`
 			},
-		},
-		default: false,
-		description: `Whether the project in which the issue is created is added to the user's Recently viewed project list, as shown under Projects in Jira. This also populates the JQL issues search lastViewed field.`,
+			{
+				displayName: 'Update History',
+				name: 'updateHistory',
+				type: 'boolean',
+				required: false,
+				default: false,
+				description: `Whether the project in which the issue is created is added to the user's
+				Recently viewed project list, as shown under Projects in Jira. This also populates the
+				JQL issues search lastViewed field.`,
+			},
+		]
 	},
 
 /* -------------------------------------------------------------------------- */
@@ -574,7 +510,6 @@ export const issueFields = [
 		default: 50,
 		description: 'How many results to return.',
 	},
-
 /* -------------------------------------------------------------------------- */
 /*                                issue:notify                                */
 /* -------------------------------------------------------------------------- */
@@ -597,66 +532,6 @@ export const issueFields = [
 		description: 'Issue Key',
 	},
 	{
-		displayName: 'Subject',
-		name: 'subject',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'notify',
-				],
-			},
-		},
-		default: '',
-		description: 'The subject of the email notification for the issue. If this is not specified, then the subject is set to the issue key and summary.',
-	},
-	{
-		displayName: 'Text Body',
-		name: 'textBody',
-		type: 'string',
-		typeOptions: {
-			alwaysOpenEditWindow: true,
-		},
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'notify',
-				],
-			},
-		},
-		default: '',
-		description: 'The subject of the email notification for the issue. If this is not specified, then the subject is set to the issue key and summary.',
-	},
-	{
-		displayName: 'HTML Body',
-		name: 'htmlBody',
-		type: 'string',
-		typeOptions: {
-			alwaysOpenEditWindow: true,
-		},
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'notify',
-				],
-			},
-		},
-		default: '',
-		description: 'The HTML body of the email notification for the issue.',
-	},
-	{
 		displayName: 'JSON Parameters',
 		name: 'jsonParameters',
 		type: 'boolean',
@@ -672,6 +547,57 @@ export const issueFields = [
 				],
 			},
 		},
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: [
+					'issue',
+				],
+				operation: [
+					'notify',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Subject',
+				name: 'subject',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `The subject of the email notification for the issue. If this is not specified,
+				then the subject is set to the issue key and summary.`
+			},
+			{
+				displayName: 'Text Body',
+				name: 'textBody',
+				type: 'string',
+				typeOptions: {
+					alwaysOpenEditWindow: true,
+				},
+				required: false,
+				default: '',
+				description: `The subject of the email notification for the issue.
+				If this is not specified, then the subject is set to the issue key and summary.`
+			},
+			{
+				displayName: 'HTML Body',
+				name: 'htmlBody',
+				type: 'string',
+				typeOptions: {
+					alwaysOpenEditWindow: true,
+				},
+				required: false,
+				default: '',
+				description: 'The HTML body of the email notification for the issue.',
+			},
+		],
 	},
 	{
 		displayName: 'Notification Recipients',
@@ -878,10 +804,11 @@ export const issueFields = [
 		description: 'Issue Key',
 	},
 	{
-		displayName: 'Expand',
-		name: 'expand',
-		type: 'string',
-		required: false,
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
 		displayOptions: {
 			show: {
 				resource: [
@@ -892,43 +819,34 @@ export const issueFields = [
 				],
 			},
 		},
-		default: '',
-		description: 'Use expand to include additional information about transitions in the response. This parameter accepts transitions.fields, which returns information about the fields in the transition screen for each transition. Fields hidden from the screen are not returned. Use this information to populate the fields and update fields in Transition issue.',
-	},
-	{
-		displayName: 'Transition ID',
-		name: 'transitionId',
-		type: 'string',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'transitions',
-				],
+		options: [
+			{
+				displayName: 'Expand',
+				name: 'expand',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `Use expand to include additional information about transitions in the response.
+				 This parameter accepts transitions.fields, which returns information about the fields in the
+				 transition screen for each transition. Fields hidden from the screen are not returned. Use this
+				 information to populate the fields and update fields in Transition issue.`
 			},
-		},
-		default: '',
-		description: 'The ID of the transition.',
-	},
-	{
-		displayName: 'Skip Remote Only Condition',
-		name: 'skipRemoteOnlyCondition',
-		type: 'boolean',
-		required: false,
-		displayOptions: {
-			show: {
-				resource: [
-					'issue',
-				],
-				operation: [
-					'transitions',
-				],
+			{
+				displayName: 'Transition ID',
+				name: 'transitionId',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'The ID of the transition.',
 			},
-		},
-		default: false,
-		description: 'Indicates whether transitions with the condition Hide From User Condition are included in the response.',
+			{
+				displayName: 'Skip Remote Only Condition',
+				name: 'skipRemoteOnlyCondition',
+				type: 'boolean',
+				required: false,
+				default: false,
+				description: 'Indicates whether transitions with the condition Hide From User Condition are included in the response.',
+			},
+		],
 	},
 ] as INodeProperties[];
