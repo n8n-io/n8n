a53d111e5635d8e7ed666819abdc25e075aee8e3
quansenB <inaki.breinbauer@gmail.com>
2019-12-09 01:07:57 +0100

    add xml body parser for xml webhook support

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -7,6 +7,7 @@ import {
 	getConnectionManager,
 } from 'typeorm';
 import * as bodyParser from 'body-parser';
+require('body-parser-xml')(bodyParser);
 import * as history from 'connect-history-api-fallback';
 import * as requestPromise from 'request-promise-native';
 
@@ -231,6 +232,10 @@ class App {
 
 		// Support application/json type post data
 		this.app.use(bodyParser.json({ limit: "16mb" }));
+
+		// Support application/xml type post data
+		// @ts-ignore
+		this.app.use(bodyParser.xml({ limit: "16mb" }));
 
 		// Make sure that Vue history mode works properly
 		this.app.use(history({
