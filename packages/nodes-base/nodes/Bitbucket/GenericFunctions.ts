406f96dd8243366464e0143caa7f7eb185fff319
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-01-05 20:22:09 -0500

    :zap: removed console.log

--- packages/nodes-base/nodes/Bitbucket/GenericFunctions.ts
+++ packages/nodes-base/nodes/Bitbucket/GenericFunctions.ts
@@ -13,7 +13,6 @@ export async function bitbucketApiRequest(this: IHookFunctions | IExecuteFunctio
 		throw new Error('No credentials got returned!');
 	}
 	const userpass = `${credentials.username}:${credentials.appPassword}`
-	console.log()
 	let options: OptionsWithUri = {
 		method,
 		qs,
