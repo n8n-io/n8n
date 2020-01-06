dc0ffe887966d4831981e4f75c7a51ed1d7db1ff
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-01-05 21:32:22 -0500

    done

--- packages/nodes-base/nodes/Zendesk/GenericFunctions.ts
+++ packages/nodes-base/nodes/Zendesk/GenericFunctions.ts
@@ -18,47 +18,20 @@ export async function zendeskApiRequest(this: IHookFunctions | IExecuteFunctions
 		method,
 		qs,
 		body,
-		uri: uri ||`${credentials.domain}/api/v2${resource}`,
+		uri: uri ||`${credentials.domain}/api/v2${resource}.json`,
 		json: true
 	};
 	options = Object.assign({}, options, option);
 	if (Object.keys(options.body).length === 0) {
 		delete options.body;
 	}
-
 	try {
 		return await this.helpers.request!(options);
-	} catch (error) {
-		let errorMessage = error.message;
-		if (error.response.body) {
-			errorMessage = error.response.body.message || error.response.body.Message || error.message;
+	} catch (err) {
+		let errorMessage = '';
+		if (err.error && err.description) {
+			errorMessage = err.description;
 		}
-
 		throw new Error(errorMessage);
 	}
-}
-
-/**
- * Make an API request to paginated flow endpoint
- * and return all results
- */
-export async function zendeskApiRequestAllItems(this: IHookFunctions | IExecuteFunctions| ILoadOptionsFunctions, propertyName: string, method: string, resource: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-
-	const returnData: IDataObject[] = [];
-
-	let responseData;
-
-	let uri: string | undefined;
-
-	do {
-		responseData = await zendeskApiRequest.call(this, method, resource, body, query, uri);
-		query.continuation = responseData.pagination.continuation;
-		returnData.push.apply(returnData, responseData[propertyName]);
-	} while (
-		responseData.pagination !== undefined &&
-		responseData.pagination.has_more_items !== undefined &&
-		responseData.pagination.has_more_items !== false
-	);
-
-	return returnData;
 }
