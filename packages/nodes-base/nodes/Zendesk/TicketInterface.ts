28ae39e884f9fc37bf1707e9328b75aeb56cff69
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-01-07 14:56:42 -0500

    :sparkles: added zendesk node

---
+++ packages/nodes-base/nodes/Zendesk/TicketInterface.ts
@@ -0,0 +1,16 @@
+import { IDataObject } from "n8n-workflow";
+
+export interface ITicket {
+	subject?: string;
+	comment?: IComment;
+	type?: string;
+	group?: string;
+	external_id?: string;
+	tags?: string[];
+	status?: string;
+	recipient?: string;
+}
+
+export interface IComment {
+	body?: string;
+}
