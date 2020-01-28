befed78e45a4c0aa5c25cb46d43ecbbcd6b29239
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-01-28 13:51:49 -0500

    :sparkles: Uplead node

---
+++ packages/nodes-base/nodes/Uplead/GenericFunctions.ts
@@ -0,0 +1,32 @@
+import { OptionsWithUri } from 'request';
+import {
+	IExecuteFunctions,
+	IExecuteSingleFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+import { IDataObject } from 'n8n-workflow';
+
+export async function upleadApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, resource: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+	const credentials = this.getCredentials('upleadApi');
+	if (credentials === undefined) {
+		throw new Error('No credentials got returned!');
+	}
+	let options: OptionsWithUri = {
+		headers: { Authorization: credentials.apiKey },
+		method,
+		qs,
+		body,
+		uri: uri ||`https://api.uplead.com/v2${resource}`,
+		json: true
+	};
+	options = Object.assign({}, options, option);
+	if (Object.keys(options.body).length === 0) {
+		delete options.body;
+	}
+	try {
+		return await this.helpers.request!(options);
+	} catch (err) {
+		throw new Error(err);
+	}
+}
