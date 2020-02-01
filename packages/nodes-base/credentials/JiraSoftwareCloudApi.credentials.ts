6dc56e66857ef445e58ee71d48701831fdea4689
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-02-01 18:15:56 -0500

    :zap: Support for Jira server

--- packages/nodes-base/credentials/JiraSoftwareCloudApi.credentials.ts
+++ packages/nodes-base/credentials/JiraSoftwareCloudApi.credentials.ts
@@ -5,23 +5,77 @@ import {
 
 export class JiraSoftwareCloudApi implements ICredentialType {
 	name = 'jiraSoftwareCloudApi';
-	displayName = 'Jira SW Cloud API';
+	displayName = 'Jira SW API';
 	properties = [
+		{
+			displayName: 'Jira Version',
+			name: 'jiraVersion',
+			type: 'options' as NodePropertyTypes,
+			options: [
+				{
+					name: 'Cloud',
+					value: 'cloud',
+				},
+				{
+					name: 'Server (Self Hosted)',
+					value: 'server',
+				},
+			],
+			default: 'cloud',
+		},
 		{
 			displayName: 'Email',
 			name: 'email',
+			displayOptions: {
+				show: {
+					jiraVersion: [
+						'cloud',
+					],
+				},
+			},
 			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
 		{
 			displayName: 'API Token',
 			name: 'apiToken',
+			displayOptions: {
+				show: {
+					jiraVersion: [
+						'cloud',
+					],
+				},
+			},
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			displayOptions: {
+				show: {
+					jiraVersion: [
+						'server',
+					],
+				},
+			},
+			typeOptions: {
+				password: true,
+			},
 			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
 		{
 			displayName: 'Domain',
 			name: 'domain',
+			displayOptions: {
+				show: {
+					jiraVersion: [
+						'cloud',
+						'server',
+					],
+				},
+			},
 			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
--- packages/nodes-base/nodes/Jira/GenericFunctions.ts
+++ packages/nodes-base/nodes/Jira/GenericFunctions.ts
@@ -17,37 +17,7 @@ export async function jiraSoftwareCloudApiRequest(this: IHookFunctions | IExecut
 	if (credentials === undefined) {
 		throw new Error('No credentials got returned!');
 	}
-	const data = Buffer.from(`${credentials!.email}:${credentials!.apiToken}`).toString(BINARY_ENCODING);
-	const headerWithAuthentication = Object.assign({},
-		{ Authorization: `Basic ${data}`, Accept: 'application/json', 'Content-Type': 'application/json' });
-	const options: OptionsWithUri = {
-		headers: headerWithAuthentication,
-		method,
-		qs: query,
-		uri: uri || `${credentials.domain}/rest/api/2${endpoint}`,
-		body,
-		json: true
-	};
-
-	try {
-		return await this.helpers.request!(options);
-	} catch (error) {
-		const errorMessage =
-		 error.response.body.message || error.response.body.Message;
-
-		if (errorMessage !== undefined) {
-			throw errorMessage;
-		}
-		throw error.response.body;
-	}
-}
-
-export async function jiraSoftwareServerApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, endpoint: string, method: string, body: any = {}, query?: IDataObject, uri?: string): Promise<any> { // tslint:disable-line:no-any
-	const credentials = this.getCredentials('jiraSoftwareServerApi');
-	if (credentials === undefined) {
-		throw new Error('No credentials got returned!');
-	}
-	const data = Buffer.from(`${credentials!.username}:${credentials!.password}`).toString(BINARY_ENCODING);
+	const data = Buffer.from(`${credentials!.email}:${(credentials.jiraVersion === 'server') ? credentials.password : credentials.apiKey }`).toString(BINARY_ENCODING);
 	const headerWithAuthentication = Object.assign({},
 		{ Authorization: `Basic ${data}`, Accept: 'application/json', 'Content-Type': 'application/json' });
 	const options: OptionsWithUri = {
@@ -73,29 +43,6 @@ export async function jiraSoftwareServerApiRequest(this: IHookFunctions | IExecu
 }
 
 export async function jiraSoftwareCloudApiRequestAllItems(this: IHookFunctions | IExecuteFunctions, propertyName: string, endpoint: string, method: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-
-	const returnData: IDataObject[] = [];
-
-	let responseData;
-
-	query.maxResults = 100;
-
-	let uri: string | undefined;
-
-	do {
-		responseData = await jiraSoftwareCloudApiRequest.call(this, endpoint, method, body, query, uri);
-		uri = responseData.nextPage;
-		returnData.push.apply(returnData, responseData[propertyName]);
-	} while (
-		responseData.isLast !== false &&
-		responseData.nextPage !== undefined &&
-		responseData.nextPage !== null
-	);
-
-	return returnData;
-}
-
-export async function jiraSoftwareServerApiRequestAllItems(this: IHookFunctions | IExecuteFunctions, propertyName: string, endpoint: string, method: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
 
 	const returnData: IDataObject[] = [];
 
