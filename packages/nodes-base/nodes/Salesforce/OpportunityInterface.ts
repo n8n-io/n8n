2eaeb4f4c29b53f46b98987cb1d51a82c5927910
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-02-10 15:55:28 -0500

    :sparkles: salesforce node

---
+++ packages/nodes-base/nodes/Salesforce/OpportunityInterface.ts
@@ -0,0 +1,17 @@
+
+export interface IOpportunity {
+	Name?: string;
+	StageName?: string;
+	CloseDate?: string;
+	Type?: string;
+	Amount?: number;
+	OwnerId?: string;
+	NextStep?: string;
+	AccountId?: string;
+	CampaignId?: string;
+	LeadSource?: string;
+	Description?: string;
+	Probability?: number;
+	Pricebook2Id?:string;
+	ForecastCategoryName?: string;
+}
