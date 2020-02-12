fc8c7dacffba5b09e7a0fb31da1c1f50685c5416
d3no <d3no520@gmail.com>
2020-02-12 15:52:36 +0800

    Commit Mocean node

---
+++ packages/nodes-base/nodes/Mocean/MoceanApi.credentials.ts
@@ -0,0 +1,27 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+
+export class MoceanApi implements ICredentialType {
+	name = 'moceanApi';
+	displayName = 'Mocean Api';
+	properties = [
+		// The credentials to get from user and save encrypted.
+		// Properties can be defined exactly in the same way
+		// as node properties.
+		{
+			displayName: 'API Key',
+			name: 'mocean-api-key',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'API Secret',
+			name: 'mocean-api-secret',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
