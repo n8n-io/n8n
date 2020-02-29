c10df92b48e3fe931e577edbe9d424140c2ea03e
Ricardo Espinoza <ricardoespinoza105@gmail.com>
2020-02-28 19:55:14 -0500

    :sparkles: Drift node

---
+++ packages/nodes-base/credentials/DriftApi.credentials.ts
@@ -0,0 +1,18 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class DriftApi implements ICredentialType {
+	name = 'driftApi';
+	displayName = 'Drift API';
+	properties = [
+		{
+			displayName: 'Personal Access Token',
+			name: 'accessToken',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+			description: 'Visit your account details page, and grab the Access Token. See <a href="https://devdocs.drift.com/docs/quick-start">Drift auth</a>.'
+		},
+	];
+}
