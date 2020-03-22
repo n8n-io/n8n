1604a570a627383412204a6653a55a73d23681f9
ricardo <ricardoespinoza105@gmail.com>
2020-03-22 15:14:45 -0400

    :zap: Small improvements

--- packages/nodes-base/credentials/MicrosoftExcelOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/MicrosoftExcelOAuth2Api.credentials.ts
@@ -6,40 +6,16 @@ import {
 export class MicrosoftExcelOAuth2Api implements ICredentialType {
 	name = 'microsoftExcelOAuth2Api';
 	extends = [
-		'oAuth2Api',
+		'microsoftOAuth2Api',
 	];
 	displayName = 'Microsoft OAuth2 API';
 	properties = [
-		{
-			displayName: 'Authorization URL',
-			name: 'authUrl',
-			type: 'string' as NodePropertyTypes,
-			default: 'https://login.microsoftonline.com/{yourtenantid}/oauth2/v2.0/authorize',
-		},
-		{
-			displayName: 'Access Token URL',
-			name: 'accessTokenUrl',
-			type: 'string' as NodePropertyTypes,
-			default: 'https://login.microsoftonline.com/{yourtenantid}/oauth2/v2.0/token',
-		},
 		//https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent
 		{
 			displayName: 'Scope',
 			name: 'scope',
 			type: 'hidden' as NodePropertyTypes,
 			default: 'openid offline_access Files.ReadWrite',
-		},
-		{
-			displayName: 'Auth URI Query Parameters',
-			name: 'authQueryParameters',
-			type: 'hidden' as NodePropertyTypes,
-			default: 'response_mode=query',
-		},
-		{
-			displayName: 'Authentication',
-			name: 'authentication',
-			type: 'hidden' as NodePropertyTypes,
-			default: 'body',
 		},
 	];
 }
