96bf3e236c95763c2a33c9dac41803ab6b98a313
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-03-27 12:44:26 +0100

    :zap: Make it possible to use JotForm accounts from Europe

--- packages/nodes-base/credentials/JotFormApi.credentials.ts
+++ packages/nodes-base/credentials/JotFormApi.credentials.ts
@@ -13,5 +13,22 @@ export class JotFormApi implements ICredentialType {
 			type: 'string' as NodePropertyTypes,
 			default: '',
 		},
+		{
+			displayName: 'API Domain',
+			name: 'apiDomain',
+			type: 'options' as NodePropertyTypes,
+			options: [
+				{
+					name: 'api.jotform.com',
+					value: 'api.jotform.com',
+				},
+				{
+					name: 'eu-api.jotform.com',
+					value: 'eu-api.jotform.com',
+				},
+			],
+			default: 'api.jotform.com',
+			description: 'The API domain to use. Use "eu-api.jotform.com" if your account is in based in Europe.',
+		},
 	];
 }
