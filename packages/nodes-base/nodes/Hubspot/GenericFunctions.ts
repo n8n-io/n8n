202f21ac98c08d94bbcb2ff3c6dbce5f892144fa
ricardo <ricardoespinoza105@gmail.com>
2020-04-08 00:35:50 -0400

    :zap: Added ticket resource

--- packages/nodes-base/nodes/Hubspot/GenericFunctions.ts
+++ packages/nodes-base/nodes/Hubspot/GenericFunctions.ts
@@ -1,10 +1,12 @@
-import { OptionsWithUri } from 'request';
+import {
+	OptionsWithUri,
+ } from 'request';
 
 import {
 	IExecuteFunctions,
 	IHookFunctions,
 	ILoadOptionsFunctions,
-	IExecuteSingleFunctions
+	IExecuteSingleFunctions,
 } from 'n8n-core';
 
 import {
@@ -50,7 +52,7 @@ export async function hubspotApiRequestAllItems(this: IHookFunctions | IExecuteF
 
 	let responseData;
 
-	query.limit = 250;
+	query.limit = query.limit || 250;
 	query.count = 100;
 
 	do {
@@ -58,6 +60,9 @@ export async function hubspotApiRequestAllItems(this: IHookFunctions | IExecuteF
 		query.offset = responseData.offset;
 		query['vid-offset'] = responseData['vid-offset'];
 		returnData.push.apply(returnData, responseData[propertyName]);
+		if (query.limit && query.limit <= returnData.length) {
+			return returnData;
+		}
 	} while (
 		responseData['has-more'] !== undefined &&
 		responseData['has-more'] !== null &&
