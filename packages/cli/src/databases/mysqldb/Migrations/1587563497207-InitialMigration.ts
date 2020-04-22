ad96e73050989deee7d8de446b7659f87e057242
Rupenieks <ru@myos,co>
2020-04-22 15:52:15 +0200

    Initial migration files for each DB

---
+++ packages/cli/src/databases/mysqldb/Migrations/1587563497207-InitialMigration.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class InitialMigration1587563497207 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
