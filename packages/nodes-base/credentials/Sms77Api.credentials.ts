ae853ce602feb18ede10be7d1e97ba4d6fddaf19
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-05-02 13:09:16 +0200

    :zap: Small changes to Sms77 node

--- packages/nodes-base/credentials/Sms77Api.credentials.ts
+++ packages/nodes-base/credentials/Sms77Api.credentials.ts
@@ -3,9 +3,9 @@ import {
 	NodePropertyTypes,
 } from 'n8n-workflow';
 
-export class Sms77 implements ICredentialType {
-	name = 'Sms77';
-	displayName = 'Sms77';
+export class Sms77Api implements ICredentialType {
+	name = 'sms77Api';
+	displayName = 'Sms77 API';
 	properties = [
 		{
 			displayName: 'API Key',
