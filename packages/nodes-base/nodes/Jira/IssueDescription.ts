98ac9d93bce9ef041bea501b88f38ee3c107452c
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-05-04 19:35:02 +0200

    :zap: Fix loading of projects for hosted Jira

--- packages/nodes-base/nodes/Jira/IssueDescription.ts
+++ packages/nodes-base/nodes/Jira/IssueDescription.ts
@@ -81,6 +81,9 @@ export const issueFields = [
 		},
 		typeOptions: {
 			loadOptionsMethod: 'getProjects',
+			loadOptionsDependsOn: [
+				'jiraVersion',
+			],
 		},
 		description: 'Project',
 	},
