45f65dc7027496e4071b8280509025f988f02517
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-05-07 00:44:39 +0200

    :zap: Small improvements to Bannerbear-Node

--- packages/nodes-base/nodes/Bannerbear/ImageDescription.ts
+++ packages/nodes-base/nodes/Bannerbear/ImageDescription.ts
@@ -82,19 +82,38 @@ export const imageFields = [
 				description: 'Metadata that you need to store e.g. ID of a record in your DB',
 			},
 			{
+				displayName: 'Wait for Image',
+				name: 'waitForImage',
+				type: 'boolean',
+				default: false,
+				description: `Wait for the image to be proccesed before returning.<br />
+				If after three tries the images is not ready, an error will be thrown.<br />
+				Number of tries can be increased by setting "Wait Max Tries".`,
+			},
+			{
+				displayName: 'Wait Max Tries',
+				name: 'waitForImageMaxTries',
+				type: 'number',
+				typeOptions: {
+					minValue: 1,
+					maxValue: 10,
+				},
+				displayOptions: {
+					show: {
+						waitForImage: [
+							true,
+						],
+					},
+				},
+				default: 3,
+				description: `How often it should check if the image is available before it fails.`,
+			},
+			{
 				displayName: 'Webhook URL',
 				name: 'webhookUrl',
 				type: 'string',
 				default: '',
 				description: 'A url to POST the Image object to upon rendering completed',
-			},
-			{
-				displayName: 'Wait for Image',
-				name: 'waitForImage',
-				type: 'boolean',
-				default: false,
-				description: `Wait for the image to be proccesed before returning.</br>
-				If after two tries the images is not ready an error will be thrown`,
 			},
 		],
 	},
