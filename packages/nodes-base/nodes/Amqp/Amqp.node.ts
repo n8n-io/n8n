223fdde356cca259bc7ca2ff9eb316e28bcc36a4
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-05-12 16:26:51 +0200

    :boom: Fix one more node name

--- packages/nodes-base/nodes/Amqp/Amqp.node.ts
+++ packages/nodes-base/nodes/Amqp/Amqp.node.ts
@@ -7,10 +7,10 @@ import {
 	INodeTypeDescription,
 } from 'n8n-workflow';
 
-export class AmqpSender implements INodeType {
+export class Amqp implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'AMQP Sender',
-		name: 'amqpSender',
+		name: 'amqp',
 		icon: 'file:amqp.png',
 		group: ['transform'],
 		version: 1,
