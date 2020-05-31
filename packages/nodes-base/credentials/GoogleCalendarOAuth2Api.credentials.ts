4c0181b8dc071e52dc8782a8915f6f7e54c296fd
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-05-31 19:24:14 +0200

    :zap: Fix some minor issues with OAuth2

--- packages/nodes-base/credentials/GoogleCalendarOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/GoogleCalendarOAuth2Api.credentials.ts
@@ -8,12 +8,12 @@ const scopes = [
 	'https://www.googleapis.com/auth/calendar.events',
 ];
 
-export class GoogleOAuth2Api implements ICredentialType {
-	name = 'googleOAuth2Api';
+export class GoogleCalendarOAuth2Api implements ICredentialType {
+	name = 'googleCalendarOAuth2Api';
 	extends = [
-		'oAuth2Api',
+		'googleOAuth2Api',
 	];
-	displayName = 'Google OAuth2 API';
+	displayName = 'Google Calendar OAuth2 API';
 	properties = [
 		{
 			displayName: 'Scope',
