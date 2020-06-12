b519a178b41ca8849b0a40db11bff02cbe5fcd06
Iván Ovejero <ivov.src@gmail.com>
2020-06-12 17:23:36 -0300

    :sparkles: Hacker News node

---
+++ packages/nodes-base/nodes/HackerNews/GenericFunctions.ts
@@ -0,0 +1,69 @@
+import {
+	IExecuteFunctions,
+	IHookFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject, ILoadOptionsFunctions,
+} from 'n8n-workflow';
+
+import {
+	OptionsWithUri
+} from 'request';
+
+
+/**
+ * Make an API request to HackerNews
+ *
+ * @param {IHookFunctions} this
+ * @param {string} method
+ * @param {string} endpoint
+ * @param {IDataObject} qs
+ * @returns {Promise<any>}
+ */
+export async function hackerNewsApiRequest(this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions, method: string, endpoint: string, qs: IDataObject): Promise<any> { // tslint:disable-line:no-any
+	const options: OptionsWithUri = {
+		method: method,
+		qs,
+		uri: `http://hn.algolia.com/api/v1/${endpoint}`,
+		json: true,
+	};
+
+	return await this.helpers.request!(options);
+}
+
+
+/**
+ * Make an API request to HackerNews
+ * and return all results
+ *
+ * @export
+ * @param {(IHookFunctions | IExecuteFunctions)} this
+ * @param {string} method
+ * @param {string} endpoint
+ * @param {IDataObject} qs
+ * @returns {Promise<any>}
+ */
+export async function hackerNewsApiRequestAllItems(this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions, method: string, endpoint: string, qs: IDataObject): Promise<any> { // tslint:disable-line:no-any
+
+	qs.hitsPerPage = 100;
+
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+	let itemsReceived = 0;
+
+	do {
+		responseData = await hackerNewsApiRequest.call(this, method, endpoint, qs);
+		returnData.push.apply(returnData, responseData.hits);
+
+		if (returnData !== undefined) {
+			itemsReceived += returnData.length;
+		}
+
+	} while (
+		responseData.nbHits > itemsReceived
+	);
+
+	return returnData;
+}
