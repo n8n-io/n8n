f755fb3fdef8ce5f5afcb0524696f5cc76602d68
shraddha shaligram <sshaligr@usc.edu>
2020-06-22 11:51:15 -0700

    add meeting functionality

---
+++ packages/nodes-base/nodes/Zoom/MeetingDescription.ts
@@ -0,0 +1,751 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const meetingOperations = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Create',
+				value: 'create',
+				description: 'Create a meeting',
+			},
+			{
+				name: 'Delete',
+				value: 'delete',
+				description: 'Delete a meeting',
+			},
+			{
+				name: 'Get',
+				value: 'get',
+				description: 'Retrieve a meeting',
+			},
+			{
+				name: 'Get All',
+				value: 'getAll',
+				description: 'Retrieve all meetings',
+			},
+			{
+				name: 'Update',
+				value: 'update',
+				description: 'Update a meeting',
+			}
+		],
+		default: 'create',
+		description: 'The operation to perform.',
+	}
+] as INodeProperties[];
+
+export const meetingFields = [
+	/* -------------------------------------------------------------------------- */
+	/*                                 meeting:create                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Id',
+		name: 'userId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'create',
+
+
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'User ID.',
+	},
+	{
+		displayName: 'Additional settings',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+					'create',
+
+				],
+				resource: [
+					'meeting',
+				],
+			}
+		},
+		options: [
+			{
+				displayName: 'Meeting topic',
+				name: 'topic',
+				type: 'string',
+				default: '',
+				description: `Meeting topic.`,
+			},
+			{
+				displayName: 'Meeting type',
+				name: 'type',
+				type: 'options',
+				options: [
+					{
+						name: 'Instant Meeting',
+						value: 1,
+					},
+					{
+						name: 'Scheduled Meeting',
+						value: 2,
+					},
+					{
+						name: 'Recurring meeting with no fixed time',
+						value: 3,
+					},
+					{
+						name: 'Recurring meeting with no fixed time',
+						value: 8,
+					},
+
+				],
+				default: 2,
+				description: 'Meeting type.'
+			},
+			{
+				displayName: 'Start time',
+				name: 'startTime',
+				type: 'dateTime',
+				default: '',
+				description: 'Start time should be used only for scheduled or recurring meetings with fixed time',
+			},
+			{
+				displayName: 'Duration',
+				name: 'duration',
+				type: 'number',
+				default: '',
+				description: 'Duration.',
+			},
+			{
+				displayName: 'Timezone',
+				name: 'timeZone',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getTimezones',
+				},
+				default: '',
+				description: `Time zone used in the response. The default is the time zone of the calendar.`,
+			},
+			{
+				displayName: 'Schedule for',
+				name: 'scheduleFor',
+				type: 'string',
+				default: '',
+				description: 'Schedule meeting for someone else from your account, provide their email id.',
+			},
+			{
+				displayName: 'Password',
+				name: 'password',
+				type: 'string',
+				default: '',
+				description: 'Password to join the meeting with maximum 10 characters.',
+			},
+			{
+				displayName: 'Agenda',
+				name: 'agenda',
+				type: 'string',
+				default: '',
+				description: 'Meeting agenda.',
+			},
+			{
+				displayName: 'Host Meeting in China',
+				name: 'cn_meeting',
+				type: 'boolean',
+				default: false,
+				description: 'Host Meeting in China.',
+			},
+			{
+				displayName: 'Host Meeting in India',
+				name: 'in_meeting',
+				type: 'boolean',
+				default: false,
+				description: 'Host Meeting in India.',
+			},
+			{
+				displayName: 'Host Video',
+				name: 'host_video',
+				type: 'boolean',
+				default: false,
+				description: 'Start video when host joins the meeting.',
+			},
+			{
+				displayName: 'Participant Video',
+				name: 'participant_video',
+				type: 'boolean',
+				default: false,
+				description: 'Start video when participant joins the meeting.',
+			},
+			{
+				displayName: 'Join before Host',
+				name: 'join_before_host',
+				type: 'boolean',
+				default: false,
+				description: 'Allow participants to join the meeting before host starts it.',
+			},
+			{
+				displayName: 'Muting before entry',
+				name: 'mute_upon_entry',
+				type: 'boolean',
+				default: false,
+				description: 'Mute participants upon entry.',
+			},
+			{
+				displayName: 'Watermark',
+				name: 'watermark',
+				type: 'boolean',
+				default: false,
+				description: 'Adds watermark when viewing a shared screen.',
+			},
+			{
+				displayName: 'Alternative Hosts',
+				name: 'alternative_hosts',
+				type: 'string',
+				default: '',
+				description: 'Alternative hosts email ids.',
+			},
+			{
+				displayName: 'Auto recording',
+				name: 'auto_recording',
+				type: 'options',
+				options: [
+					{
+						name: 'Record on local',
+						value: 'local',
+					},
+					{
+						name: 'Record on cloud',
+						value: 'cloud',
+					},
+					{
+						name: 'Disabled',
+						value: 'none',
+					},
+
+
+				],
+				default: 'none',
+				description: 'Auto recording.',
+			},
+			{
+				displayName: 'Audio',
+				name: 'auto_recording',
+				type: 'options',
+				options: [
+					{
+						name: 'Both Telephony and VoiP',
+						value: 'both',
+					},
+					{
+						name: 'Telephony',
+						value: 'telephony',
+					},
+					{
+						name: 'VOIP',
+						value: 'voip',
+					},
+
+
+				],
+				default: 'both',
+				description: 'Determine how participants can join audio portion of the meeting.',
+			},
+			{
+				displayName: 'Registration type',
+				name: 'registration_type',
+				type: 'options',
+				options: [
+					{
+						name: 'Attendees register once and can attend any of the occurences',
+						value: 1,
+					},
+					{
+						name: 'Attendees need to register for every occurence',
+						value: 2,
+					},
+					{
+						name: 'Attendees register once and can choose one or more occurences to attend',
+						value: 3,
+					},
+
+
+				],
+				default: 1,
+				description: 'Registration type. Used for recurring meetings with fixed time only',
+			},
+
+
+		],
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 meeting:get                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Id',
+		name: 'userId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'get',
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'User ID.',
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 meeting:getAll                               */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Id',
+		name: 'userId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'User ID.',
+	},
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		default: false,
+		description: 'If all results should be returned or only up to a given limit.',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'meeting',
+				],
+				returnAll: [
+					false,
+				],
+			},
+		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 300
+		},
+		default: 30,
+		description: 'How many results to return.',
+	},
+	{
+		displayName: 'Additional settings',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+
+				],
+				resource: [
+					'meeting',
+				],
+			}
+		},
+		options: [
+			{
+				displayName: 'Type',
+				name: 'type',
+				type: 'options',
+				options: [
+					{
+						name: 'Scheduled',
+						value: 'scheduled',
+					},
+					{
+						name: 'Live',
+						value: 'live',
+					},
+					{
+						name: 'Upcoming',
+						value: 'upcoming',
+					},
+
+
+				],
+				default: 'live',
+				description: `Meeting type.`,
+			},
+		]
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 meeting:delete                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Meeting Id',
+		name: 'meetingId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'delete'
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'Meeting ID.',
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+					'delete',
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Occurence Id',
+				name: 'occurenceId',
+				type: 'string',
+				default: '',
+				description: 'Meeting occurence Id.',
+			},
+			{
+				displayName: 'Schedule a reminder',
+				name: 'scheduleForReminder',
+				type: 'boolean',
+				default: false,
+				description: 'Schedule a reminder via email',
+			},
+
+
+
+		],
+
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 meeting:update                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Meeting Id',
+		name: 'meetingId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'update',
+
+
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'Meeting ID.',
+	},
+	{
+		displayName: 'Occurence Id',
+		name: 'occurenceId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'update',
+
+
+				],
+				resource: [
+					'meeting',
+				],
+			},
+		},
+		description: 'Occurence ID.',
+	},
+	{
+		displayName: 'Additional settings',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+
+					'update',
+				],
+				resource: [
+					'meeting',
+				],
+			}
+		},
+		options: [
+			{
+				displayName: 'Meeting topic',
+				name: 'topic',
+				type: 'string',
+				default: '',
+				description: `Meeting topic.`,
+			},
+			{
+				displayName: 'Meeting type',
+				name: 'type',
+				type: 'options',
+				options: [
+					{
+						name: 'Instant Meeting',
+						value: 1,
+					},
+					{
+						name: 'Scheduled Meeting',
+						value: 2,
+					},
+					{
+						name: 'Recurring meeting with no fixed time',
+						value: 3,
+					},
+					{
+						name: 'Recurring meeting with no fixed time',
+						value: 8,
+					},
+
+				],
+				default: 2,
+				description: 'Meeting type.'
+			},
+			{
+				displayName: 'Start time',
+				name: 'startTime',
+				type: 'dateTime',
+				default: '',
+				description: 'Start time should be used only for scheduled or recurring meetings with fixed time',
+			},
+			{
+				displayName: 'Duration',
+				name: 'duration',
+				type: 'number',
+				default: '',
+				description: 'Duration.',
+			},
+			{
+				displayName: 'Timezone',
+				name: 'timeZone',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getTimezones',
+				},
+				default: '',
+				description: `Time zone used in the response. The default is the time zone of the calendar.`,
+			},
+			{
+				displayName: 'Schedule for',
+				name: 'scheduleFor',
+				type: 'string',
+				default: '',
+				description: 'Schedule meeting for someone else from your account, provide their email id.',
+			},
+			{
+				displayName: 'Password',
+				name: 'password',
+				type: 'string',
+				default: '',
+				description: 'Password to join the meeting with maximum 10 characters.',
+			},
+			{
+				displayName: 'Agenda',
+				name: 'agenda',
+				type: 'string',
+				default: '',
+				description: 'Meeting agenda.',
+			},
+			{
+				displayName: 'Host Meeting in China',
+				name: 'cn_meeting',
+				type: 'boolean',
+				default: false,
+				description: 'Host Meeting in China.',
+			},
+			{
+				displayName: 'Host Meeting in India',
+				name: 'in_meeting',
+				type: 'boolean',
+				default: false,
+				description: 'Host Meeting in India.',
+			},
+			{
+				displayName: 'Host Video',
+				name: 'host_video',
+				type: 'boolean',
+				default: false,
+				description: 'Start video when host joins the meeting.',
+			},
+			{
+				displayName: 'Participant Video',
+				name: 'participant_video',
+				type: 'boolean',
+				default: false,
+				description: 'Start video when participant joins the meeting.',
+			},
+			{
+				displayName: 'Join before Host',
+				name: 'join_before_host',
+				type: 'boolean',
+				default: false,
+				description: 'Allow participants to join the meeting before host starts it.',
+			},
+			{
+				displayName: 'Muting before entry',
+				name: 'mute_upon_entry',
+				type: 'boolean',
+				default: false,
+				description: 'Mute participants upon entry.',
+			},
+			{
+				displayName: 'Watermark',
+				name: 'watermark',
+				type: 'boolean',
+				default: false,
+				description: 'Adds watermark when viewing a shared screen.',
+			},
+			{
+				displayName: 'Alternative Hosts',
+				name: 'alternative_hosts',
+				type: 'string',
+				default: '',
+				description: 'Alternative hosts email ids.',
+			},
+			{
+				displayName: 'Auto recording',
+				name: 'auto_recording',
+				type: 'options',
+				options: [
+					{
+						name: 'Record on local',
+						value: 'local',
+					},
+					{
+						name: 'Record on cloud',
+						value: 'cloud',
+					},
+					{
+						name: 'Disabled',
+						value: 'none',
+					},
+
+
+				],
+				default: 'none',
+				description: 'Auto recording.',
+			},
+			{
+				displayName: 'Audio',
+				name: 'auto_recording',
+				type: 'options',
+				options: [
+					{
+						name: 'Both Telephony and VoiP',
+						value: 'both',
+					},
+					{
+						name: 'Telephony',
+						value: 'telephony',
+					},
+					{
+						name: 'VOIP',
+						value: 'voip',
+					},
+
+
+				],
+				default: 'both',
+				description: 'Determine how participants can join audio portion of the meeting.',
+			},
+			{
+				displayName: 'Registration type',
+				name: 'registration_type',
+				type: 'options',
+				options: [
+					{
+						name: 'Attendees register once and can attend any of the occurences',
+						value: 1,
+					},
+					{
+						name: 'Attendees need to register for every occurence',
+						value: 2,
+					},
+					{
+						name: 'Attendees register once and can choose one or more occurences to attend',
+						value: 3,
+					},
+
+
+				],
+				default: 1,
+				description: 'Registration type. Used for recurring meetings with fixed time only',
+			},
+
+
+		],
+	},
+
+] as INodeProperties[];
--- packages/nodes-base/nodes/Zoom/Zoom.node.ts
+++ packages/nodes-base/nodes/Zoom/Zoom.node.ts
@@ -3,7 +3,9 @@ import {
 	IDataObject,
 	INodeExecutionData,
 	INodeType,
+	ILoadOptionsFunctions,
 	INodeTypeDescription,
+	INodePropertyOptions,
 } from 'n8n-workflow';
 import {
 	zoomApiRequest,
@@ -14,7 +16,30 @@ import {
 import {
 	meetingOperations,
 	meetingFields,
-} from './ZoomOperations';
+} from './MeetingDescription';
+
+import {
+	meetingRegistrantOperations,
+	meetingRegistrantFields,
+
+} from './MeetingRegistrantDescription';
+
+import * as moment from 'moment-timezone';
+
+interface Settings {
+	host_video?: boolean;
+	participant_video?: boolean;
+	cn_meeting?: boolean;
+	in_meeting?: boolean;
+	join_before_host?: boolean;
+	mute_upon_entry?: boolean;
+	watermark?: boolean;
+	audio?: string;
+	alternative_hosts?: string;
+	auto_recording?: string;
+	registration_type?: number;
+
+}
 export class Zoom implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Zoom',
@@ -25,31 +50,11 @@ export class Zoom implements INodeType {
 		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
 		defaults: {
 			name: 'Zoom',
-			color: '#772244'
+			color: '#0B6CF9'
 		},
 		icon: 'file:zoom.png',
 		inputs: ['main'],
 		outputs: ['main'],
-		// credentials: [
-		// 	{
-		// 		name: 'zoomApi',
-		// 		required: true,
-		// 		displayOptions: {
-		// 			show: {
-		// 				authentication: ['accessToken']
-		// 			}
-		// 		}
-		// 	},
-		// 	{
-		// 		name: 'zoomOAuth2Api',
-		// 		required: true,
-		// 		displayOptions: {
-		// 			show: {
-		// 				authentication: ['oAuth2']
-		// 			}
-		// 		}
-		// 	}
-		// ],
 		credentials: [
 			{
 				name: 'zoomApi',
@@ -100,15 +105,40 @@ export class Zoom implements INodeType {
 					{
 						name: 'Meeting',
 						value: 'meeting'
+					},
+					{
+						name: 'Meeting Registrants',
+						value: 'meetingRegistrants'
 					}
 				],
 				default: 'meeting',
 				description: 'The resource to operate on.'
 			},
 			...meetingOperations,
-			...meetingFields
+			...meetingFields,
+			...meetingRegistrantOperations,
+			...meetingRegistrantFields,
 		]
 
+	};
+	methods = {
+		loadOptions: {
+			// Get all the timezones to display them to user so that he can select them easily
+			async getTimezones(
+				this: ILoadOptionsFunctions
+			): Promise<INodePropertyOptions[]> {
+				const returnData: INodePropertyOptions[] = [];
+				for (const timezone of moment.tz.names()) {
+					const timezoneName = timezone;
+					const timezoneId = timezone;
+					returnData.push({
+						name: timezoneName,
+						value: timezoneId
+					});
+				}
+				return returnData;
+			}
+		}
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
@@ -120,11 +150,13 @@ export class Zoom implements INodeType {
 		const resource = this.getNodeParameter('resource', 0) as string;
 		const operation = this.getNodeParameter('operation', 0) as string;
 		console.log(this.getCredentials('zoomOAuth2Api'));
+		let body: IDataObject = {};
 		for (let i = 0; i < length; i++) {
 			qs = {};
 			if (resource === 'meeting') {
 
 				if (operation === 'get') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meeting
 					const userId = this.getNodeParameter('userId', i) as string;
 
 					responseData = await zoomApiRequest.call(
@@ -134,6 +166,283 @@ export class Zoom implements INodeType {
 						{},
 						qs
 					);
+				}
+				if (operation === 'getAll') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetings
+					const userId = this.getNodeParameter('userId', i) as string;
+
+					responseData = await zoomApiRequest.call(
+						this,
+						'GET',
+						`/users/${userId}/meetings`,
+						{},
+						qs
+					);
+				}
+				if (operation === 'delete') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingdelete
+					const meetingId = this.getNodeParameter('meetingId', i) as string;
+					responseData = await zoomApiRequest.call(
+						this,
+						'DELETE',
+						`/meetings/${meetingId}`,
+						{},
+						qs
+					);
+					responseData = { success: true };
+				}
+				if (operation === 'create') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingcreate
+					const userId = this.getNodeParameter('userId', i) as string;
+					const additionalFields = this.getNodeParameter(
+						'additionalFields',
+						i
+					) as IDataObject;
+					const settings: Settings = {};
+					if (additionalFields.cn_meeting) {
+						settings.cn_meeting = additionalFields.cn_meeting as boolean;
+
+					}
+
+					if (additionalFields.in_meeting) {
+						settings.in_meeting = additionalFields.in_meeting as boolean;
+
+					}
+
+					if (additionalFields.join_before_host) {
+						settings.join_before_host = additionalFields.join_before_host as boolean;
+
+					}
+
+					if (additionalFields.mute_upon_entry) {
+						settings.mute_upon_entry = additionalFields.mute_upon_entry as boolean;
+
+					}
+
+					if (additionalFields.watermark) {
+						settings.watermark = additionalFields.watermark as boolean;
+
+					}
+
+					if (additionalFields.audio) {
+						settings.audio = additionalFields.audio as string;
+
+					}
+
+					if (additionalFields.alternative_hosts) {
+						settings.alternative_hosts = additionalFields.alternative_hosts as string;
+
+					}
+
+					if (additionalFields.participant_video) {
+						settings.participant_video = additionalFields.participant_video as boolean;
+
+					}
+
+					if (additionalFields.host_video) {
+						settings.host_video = additionalFields.host_video as boolean;
+
+					}
+
+					if (additionalFields.auto_recording) {
+						settings.auto_recording = additionalFields.auto_recording as string;
+
+					}
+
+					if (additionalFields.registration_type) {
+						settings.registration_type = additionalFields.registration_type as number;
+
+					}
+
+					body = {
+						settings,
+					};
+
+					if (additionalFields.topic) {
+						body.topic = additionalFields.topic as string;
+
+					}
+
+					if (additionalFields.type) {
+						body.type = additionalFields.type as string;
+
+					}
+
+					if (additionalFields.startTime) {
+						body.start_time = additionalFields.startTime as string;
+
+					}
+
+					if (additionalFields.duration) {
+						body.duration = additionalFields.duration as number;
+
+					}
+
+					if (additionalFields.scheduleFor) {
+						body.schedule_for = additionalFields.scheduleFor as string;
+
+					}
+
+					if (additionalFields.timeZone) {
+						body.timezone = additionalFields.timeZone as string;
+
+					}
+
+					if (additionalFields.password) {
+						body.password = additionalFields.password as string;
+
+					}
+
+					if (additionalFields.agenda) {
+						body.agenda = additionalFields.agenda as string;
+
+					}
+
+
+
+
+					responseData = await zoomApiRequest.call(
+						this,
+						'POST',
+						`/users/${userId}/meetings`,
+						body,
+						qs
+					);
+				}
+				if (operation === 'update') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingupdate
+					const meetingId = this.getNodeParameter('meetingId', i) as string;
+					qs.occurence_id = this.getNodeParameter('occurenceId', i) as string;
+					const additionalFields = this.getNodeParameter(
+						'additionalFields',
+						i
+					) as IDataObject;
+					const settings: Settings = {};
+					if (additionalFields.cn_meeting) {
+						settings.cn_meeting = additionalFields.cn_meeting as boolean;
+
+					}
+
+					if (additionalFields.in_meeting) {
+						settings.in_meeting = additionalFields.in_meeting as boolean;
+
+					}
+
+					if (additionalFields.join_before_host) {
+						settings.join_before_host = additionalFields.join_before_host as boolean;
+
+					}
+
+					if (additionalFields.mute_upon_entry) {
+						settings.mute_upon_entry = additionalFields.mute_upon_entry as boolean;
+
+					}
+
+					if (additionalFields.watermark) {
+						settings.watermark = additionalFields.watermark as boolean;
+
+					}
+
+					if (additionalFields.audio) {
+						settings.audio = additionalFields.audio as string;
+
+					}
+
+					if (additionalFields.alternative_hosts) {
+						settings.alternative_hosts = additionalFields.alternative_hosts as string;
+
+					}
+
+					if (additionalFields.participant_video) {
+						settings.participant_video = additionalFields.participant_video as boolean;
+
+					}
+
+					if (additionalFields.host_video) {
+						settings.host_video = additionalFields.host_video as boolean;
+
+					}
+
+					if (additionalFields.auto_recording) {
+						settings.auto_recording = additionalFields.auto_recording as string;
+
+					}
+
+					if (additionalFields.registration_type) {
+						settings.registration_type = additionalFields.registration_type as number;
+
+					}
+
+					body = {
+						settings,
+					};
+					if (additionalFields.topic) {
+						body.topic = additionalFields.topic as string;
+
+					}
+
+					if (additionalFields.type) {
+						body.type = additionalFields.type as string;
+
+					}
+
+					if (additionalFields.startTime) {
+						body.start_time = additionalFields.startTime as string;
+
+					}
+
+					if (additionalFields.duration) {
+						body.duration = additionalFields.duration as number;
+
+					}
+
+					if (additionalFields.scheduleFor) {
+						body.schedule_for = additionalFields.scheduleFor as string;
+
+					}
+
+					if (additionalFields.timeZone) {
+						body.timezone = additionalFields.timeZone as string;
+
+					}
+
+					if (additionalFields.password) {
+						body.password = additionalFields.password as string;
+
+					}
+
+					if (additionalFields.agenda) {
+						body.agenda = additionalFields.agenda as string;
+
+					}
+
+					responseData = await zoomApiRequest.call(
+						this,
+						'PATCH',
+						`/meetings/${meetingId}`,
+						body,
+						qs
+					);
+				}
+			}
+			if (resource === 'meetingRegistrant') {
+				if (operation === 'create') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrantcreate
+					const meetingId = this.getNodeParameter('meetingId', i) as string;
+					qs.occurence_id = this.getNodeParameter('occurenceId', i) as string;
+					responseData = await zoomApiRequest.call(
+						this,
+						'PATCH',
+						`/meetings/${meetingId}/registrants`,
+						body,
+						qs
+					);
+				}
+				if (operation === 'getAll') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrants
+				}
+				if (operation === 'update') {
+					//https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrantstatus
 				}
 			}
 		}
