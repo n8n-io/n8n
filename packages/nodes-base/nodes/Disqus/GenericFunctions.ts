c1918ff3c3a423a78cee50df6aad9ed2bf71e6f0
Rupenieks <ru@myos,co>
2020-06-22 11:56:12 +0200

    credentials, ui, generic functions 0auth2

--- packages/nodes-base/nodes/Disqus/GenericFunctions.ts
+++ packages/nodes-base/nodes/Disqus/GenericFunctions.ts
@@ -16,10 +16,27 @@ export async function disqusApiRequest(
 		option: IDataObject = {},
 	): Promise<any> { // tslint:disable-line:no-any
 
-	const credentials = await this.getCredentials('disqusApi') as IDataObject;
-	qs.api_key = credentials.accessToken;
-	if (credentials === undefined) {
-		throw new NodeOperationError(this.getNode(), 'No credentials got returned!');
+	const authenticationMethod = this.getNodeParameter('authentication', 0);
+
+	let options: OptionsWithUri = {
+		method,
+		body,
+		uri: `https://disqus.com/api/3.0/${uri}?`,
+		json: true,
+	};
+
+	if (authenticationMethod === 'accessToken') {
+		const credentials = await this.getCredentials('disqusApi') as IDataObject;
+		if (credentials === undefined) {
+			throw new NodeOperationError(this.getNode(), 'No credentials got returned!');
+		}
+		qs.api_key = credentials.accessToken;
+	}
+
+
+	options = Object.assign({}, options, option);
+	if (Object.keys(options.body).length === 0) {
+		delete options.body;
 	}
 
 	// Convert to query string into a format the API can read
@@ -34,19 +51,14 @@ export async function disqusApiRequest(
 		}
 	}
 
-	let options: OptionsWithUri = {
-		method,
-		body,
-		uri: `https://disqus.com/api/3.0/${uri}?${queryStringElements.join('&')}`,
-		json: true,
-	};
+	options.uri = options.uri + queryStringElements.join('&');
 
-	options = Object.assign({}, options, option);
-	if (Object.keys(options.body).length === 0) {
-		delete options.body;
-	}
 	try {
-		return await this.helpers.request!(options);
+		if (authenticationMethod === 'accessToken') {
+			return await await this.helpers.request!(options);
+		} else {
+			return await this.helpers.requestOAuth2!.call(this, 'disqusOAuth2Api', options);
+		}
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
 	}
