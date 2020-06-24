6c01eaae8e5b06956ed6e576362b4f019fc4a981
shraddha shaligram <sshaligr@usc.edu>
2020-06-24 11:25:07 -0700

    fix webinar naming conventions

--- packages/nodes-base/nodes/Zoom/Zoom.node.ts
+++ packages/nodes-base/nodes/Zoom/Zoom.node.ts
@@ -749,8 +749,8 @@ export class Zoom implements INodeType {
 						'additionalFields',
 						i
 					) as IDataObject;
-					if (additionalFields.occurrence_id) {
-						qs.occurrence_id = additionalFields.occurrence_id as string;
+					if (additionalFields.occurrenceId) {
+						qs.occurrence_id = additionalFields.occurrenceId as string;
 
 					}
 					const settings: Settings = {};
@@ -758,30 +758,34 @@ export class Zoom implements INodeType {
 						settings.audio = additionalFields.audio as string;
 
 					}
-					if (additionalFields.alternative_hosts) {
-						settings.alternative_hosts = additionalFields.alternative_hosts as string;
+					if (additionalFields.alternativeHosts) {
+						settings.alternative_hosts = additionalFields.alternativeHosts as string;
 
 					}
 
-					if (additionalFields.panelists_video) {
-						settings.panelists_video = additionalFields.panelists_video as boolean;
+					if (additionalFields.panelistsVideo) {
+						settings.panelists_video = additionalFields.panelistsVideo as boolean;
 
 					}
-					if (additionalFields.practice_session) {
-						settings.practice_session = additionalFields.practice_session as boolean;
+					if (additionalFields.hostVideo) {
+						settings.host_video = additionalFields.hostVideo as boolean;
 
 					}
-					if (additionalFields.auto_recording) {
-						settings.auto_recording = additionalFields.auto_recording as string;
+					if (additionalFields.practiceSession) {
+						settings.practice_session = additionalFields.practiceSession as boolean;
+
+					}
+					if (additionalFields.autoRecording) {
+						settings.auto_recording = additionalFields.autoRecording as string;
 
 					}
 
-					if (additionalFields.registration_type) {
-						settings.registration_type = additionalFields.registration_type as number;
+					if (additionalFields.registrationType) {
+						settings.registration_type = additionalFields.registrationType as number;
 
 					}
-					if (additionalFields.approval_type) {
-						settings.approval_type = additionalFields.approval_type as number;
+					if (additionalFields.approvalType) {
+						settings.approval_type = additionalFields.approvalType as number;
 
 					}
 
