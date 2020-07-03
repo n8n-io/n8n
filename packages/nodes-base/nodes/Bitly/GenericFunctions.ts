5c9882157ee84c5e90d4637abe58a17ba4440c99
Rupenieks <32895755+Rupenieks@users.noreply.github.com>
2020-07-03 11:22:39 +0200

    :construction: Setup
    Added OAuth2 credentials, UI, generic functions changes

--- packages/nodes-base/nodes/Bitly/GenericFunctions.ts
+++ packages/nodes-base/nodes/Bitly/GenericFunctions.ts
@@ -6,14 +6,12 @@ import {
 	ILoadOptionsFunctions,
 } from 'n8n-core';
 import { IDataObject } from 'n8n-workflow';
+import { attachmentFields } from '../Salesforce/AttachmentDescription';
 
 export async function bitlyApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, resource: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-	const credentials = this.getCredentials('bitlyApi');
-	if (credentials === undefined) {
-		throw new Error('No credentials got returned!');
-	}
+	const authenticationMethod = this.getNodeParameter('authentication', 0) as string;
 	let options: OptionsWithUri = {
-		headers: { Authorization: `Bearer ${credentials.accessToken}`},
+		headers: {},
 		method,
 		qs,
 		body,
@@ -24,10 +22,21 @@ export async function bitlyApiRequest(this: IHookFunctions | IExecuteFunctions |
 	if (Object.keys(options.body).length === 0) {
 		delete options.body;
 	}
-	try {
-		return await this.helpers.request!(options);
-	} catch (err) {
-		throw new Error(err);
+
+	try{
+		if (authenticationMethod === 'accessToken') {
+			const credentials = this.getCredentials('bitlyApi');
+			if (credentials === undefined) {
+				throw new Error('No credentials got returned!');
+			}
+			options.headers = { Authorization: `Bearer ${credentials.accessToken}`};
+
+			return await this.helpers.request!(options);
+		} else {
+			return await this.helpers.requestOAuth2!.call(this, 'bitlyOAuth2Api', options);
+		}
+	} catch(error) {
+		throw new Error(error);
 	}
 }
 
