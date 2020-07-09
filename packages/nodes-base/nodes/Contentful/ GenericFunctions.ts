517adc1ca0d9767bbe5eb7b99fd62b9cb22ac41e
Sven Schmidt <0xsven@gmail.com>
2020-07-09 12:10:22 +0200

    Handle preview api

--- packages/nodes-base/nodes/Contentful/ GenericFunctions.ts
+++ packages/nodes-base/nodes/Contentful/ GenericFunctions.ts
@@ -18,11 +18,22 @@ export const contentfulApiRequest = async (
 		throw new Error('No credentials got returned!');
 	}
 
-	let url = `https://cdn.contentful.com/spaces/${credentials.space_id}`;
+	const source = that.getNodeParameter('source', 0) as string;
+	const isPreview = source === 'preview_api';
+	let accessToken = credentials.access_token as string;
+	if (isPreview) {
+		accessToken = credentials.access_token_preview as string;
+		console.log('accessToken', accessToken);
+		if (!accessToken) {
+			throw new Error('No access token for preview API set in credentials!');
+		}
+	}
+
+	let url = `https://${isPreview ? 'preview' : 'cdn'}.contentful.com/spaces/${credentials.space_id}`;
 	if (environmentId) url = `${url}/environments/${environmentId}`;
 	if (endpoint) url = `${url}${endpoint}`;
 	qs = qs || {};
-	qs.access_token = credentials.access_token as string;
+	qs.access_token = accessToken;
 
 	const res = await that.helpers.request!({
 		url,
