a46a6fa00c53a10787c7122a321739fba3bd20d3
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-07-26 11:33:20 +0200

    :shirt: Fix lint issue

--- packages/cli/src/databases/sqlite/migrations/1594825041918-CreateIndexStoppedAt.ts
+++ packages/cli/src/databases/sqlite/migrations/1594825041918-CreateIndexStoppedAt.ts
@@ -1,20 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 import * as config from '../../../../config';
 
 export class CreateIndexStoppedAt1594825041918 implements MigrationInterface {
-    name = 'CreateIndexStoppedAt1594825041918'
+	name = 'CreateIndexStoppedAt1594825041918';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+	async up(queryRunner: QueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
 
-        await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1" ON "execution_entity" ("stoppedAt") `);
-    }
+		await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1" ON "execution_entity" ("stoppedAt") `);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+	async down(queryRunner: QueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
 
-        await queryRunner.query(`DROP INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1"`);
-    }
+		await queryRunner.query(`DROP INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1"`);
+	}
 
 }
