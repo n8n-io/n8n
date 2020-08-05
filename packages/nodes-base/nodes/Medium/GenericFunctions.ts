65ea8fa590ed27c58c87b9003f2a3399f26bb402
Shraddha Shaligram <sshaligr@usc.edu>
2020-08-05 00:29:29 -0700

    :sparkles: Add Medium Node (#788)
    * add medium node
    
    * minor fixes
    
    * fix publication post
    
    * fix according to feedback
    
    * additional fixes
    
    * fix license description
    
    * :zap: Small improvements
    
    * :zap: Improvements
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

---
+++ packages/nodes-base/nodes/Medium/GenericFunctions.ts
@@ -0,0 +1,54 @@
+import {
+	OptionsWithUri,
+} from 'request';
+
+import {
+	IExecuteFunctions,
+	IExecuteSingleFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+} from 'n8n-workflow';
+
+export async function mediumApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any = {}, query: IDataObject = {}, uri?: string): Promise<any> { // tslint:disable-line:no-any
+
+	const authenticationMethod = this.getNodeParameter('authentication', 0);
+
+	const options: OptionsWithUri = {
+		method,
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			'Accept-Charset': 'utf-8',
+		},
+		qs: query,
+		uri: uri || `https://api.medium.com/v1${endpoint}`,
+		body,
+		json: true,
+	};
+
+	try {
+		if (authenticationMethod === 'accessToken') {
+			const credentials = this.getCredentials('mediumApi');
+
+			if (credentials === undefined) {
+				throw new Error('No credentials got returned!');
+			}
+
+			options.headers!['Authorization'] = `Bearer ${credentials.accessToken}`;
+
+			return await this.helpers.request!(options);
+		}
+		else {
+			return await this.helpers.requestOAuth2!.call(this, 'mediumOAuth2Api', options);
+		}
+	} catch (error) {
+		if (error.statusCode === 401) {
+			throw new Error('The Medium credentials are not valid!');
+		}
+		throw error;
+	}
+}
