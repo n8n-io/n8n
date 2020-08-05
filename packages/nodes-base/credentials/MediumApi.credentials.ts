21a6dd2e8db97737a48fca08c1bfc6a9bba9c5bd
Shraddha Shaligram <sshaligr@usc.edu>
2020-08-05 00:29:29 -0700

    :sparkles: Add Medium Node (#788)
    * add medium node
    
    * minor fixes
    
    * fix publication post
    
    * fix according to feedback
    
    * additional fixes
    
    * fix license description
    
    * :zap: Small improvements
    
    * :zap: Improvements
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

---
+++ packages/nodes-base/credentials/MediumApi.credentials.ts
@@ -0,0 +1,17 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class MediumApi implements ICredentialType {
+	name = 'mediumApi';
+	displayName = 'Medium API';
+	properties = [
+		{
+			displayName: 'Access Token',
+			name: 'accessToken',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
