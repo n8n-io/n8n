df6eef28c25cb799e8251eef27205259a88a7453
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-08-30 11:06:46 +0200

    :bug: Fix bug that workflows did not get activated correctly

--- packages/cli/src/databases/mongodb/migrations/1592679094242-WebhookModel.ts
+++ packages/cli/src/databases/mongodb/migrations/1592679094242-WebhookModel.ts
@@ -2,20 +2,6 @@ import {
 	MigrationInterface,
 } from 'typeorm';
 
-import {
-	IWorkflowDb,
-	NodeTypes,
-	WebhookHelpers,
-} from '../../..';
-
-import {
-	Workflow,
-} from 'n8n-workflow/dist/src/Workflow';
-
-import {
-	IWebhookDb,
-} from '../../../Interfaces';
-
 import * as config from '../../../../config';
 
 import {
@@ -27,26 +13,6 @@ export class WebhookModel1592679094242 implements MigrationInterface {
 
 	async up(queryRunner: MongoQueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
-		const workflows = await queryRunner.cursor( `${tablePrefix}workflow_entity`, { active: true }).toArray() as IWorkflowDb[];
-		const data: IWebhookDb[] = [];
-		const nodeTypes = NodeTypes();
-		for (const workflow of workflows) {
-			const workflowInstance = new Workflow({ id: workflow.id as string, name: workflow.name, nodes: workflow.nodes, connections: workflow.connections, active: workflow.active, nodeTypes, staticData: workflow.staticData, settings: workflow.settings });
-			const webhooks = WebhookHelpers.getWorkflowWebhooksBasic(workflowInstance);
-			for (const webhook of webhooks) {
-				data.push({
-					workflowId: workflowInstance.id as string,
-					webhookPath: webhook.path,
-					method: webhook.httpMethod,
-					node: webhook.node,
-				 });
-			}
-		}
-
-		if (data.length !== 0) {
-			await queryRunner.manager.insertMany(`${tablePrefix}webhook_entity`, data);
-		}
-
 		await queryRunner.manager.createCollectionIndex(`${tablePrefix}webhook_entity`, ['webhookPath',  'method'], { unique: true, background: false });
 	}
 
