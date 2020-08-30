d8b9d37424f2c838fb0385ecbf8a95332ae49f91
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-08-30 11:06:46 +0200

    :bug: Fix bug that workflows did not get activated correctly

--- packages/cli/src/databases/postgresdb/migrations/1589476000887-WebhookModel.ts
+++ packages/cli/src/databases/postgresdb/migrations/1589476000887-WebhookModel.ts
@@ -3,20 +3,6 @@ import {
 	QueryRunner,
 } from 'typeorm';
 
-import {
-	IWorkflowDb,
-	NodeTypes,
-	WebhookHelpers,
-} from '../../..';
-
-import {
-	Workflow,
-} from 'n8n-workflow';
-
-import {
-	IWebhookDb,
-} from '../../../Interfaces';
-
 import * as config from '../../../../config';
 
 export class WebhookModel1589476000887 implements MigrationInterface {
@@ -31,30 +17,6 @@ export class WebhookModel1589476000887 implements MigrationInterface {
 		}
 
 		await queryRunner.query(`CREATE TABLE ${tablePrefix}webhook_entity ("workflowId" integer NOT NULL, "webhookPath" character varying NOT NULL, "method" character varying NOT NULL, "node" character varying NOT NULL, CONSTRAINT "PK_${tablePrefixIndex}b21ace2e13596ccd87dc9bf4ea6" PRIMARY KEY ("webhookPath", "method"))`, undefined);
-
-		const workflows = await queryRunner.query(`SELECT * FROM ${tablePrefix}workflow_entity WHERE active=true`) as IWorkflowDb[];
-		const data: IWebhookDb[] = [];
-		const nodeTypes = NodeTypes();
-		for (const workflow of workflows) {
-			const workflowInstance = new Workflow({ id: workflow.id as string, name: workflow.name, nodes: workflow.nodes, connections: workflow.connections, active: workflow.active, nodeTypes, staticData: workflow.staticData, settings: workflow.settings });
-			const webhooks = WebhookHelpers.getWorkflowWebhooksBasic(workflowInstance);
-			for (const webhook of webhooks) {
-				data.push({
-					workflowId: workflowInstance.id as string,
-					webhookPath: webhook.path,
-					method: webhook.httpMethod,
-					node: webhook.node,
-				 });
-			}
-		}
-
-		if (data.length !== 0) {
-			await queryRunner.manager.createQueryBuilder()
-			.insert()
-			.into(`${tablePrefix}webhook_entity`)
-			.values(data)
-			.execute();
-		}
 	}
 
 	async down(queryRunner: QueryRunner): Promise<void> {
