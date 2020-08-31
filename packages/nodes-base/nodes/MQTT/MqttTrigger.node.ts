4799eaf8f362449ace9a1b8f933b2efda295a803
ricardo <ricardoespinoza105@gmail.com>
2020-08-31 17:14:54 -0400

    :zap: Small improvements
    done

--- packages/nodes-base/nodes/MQTT/MqttTrigger.node.ts
+++ packages/nodes-base/nodes/MQTT/MqttTrigger.node.ts
@@ -1,8 +1,12 @@
-import { ITriggerFunctions } from 'n8n-core';
+import {
+	ITriggerFunctions,
+} from 'n8n-core';
+
 import {
 	INodeType,
 	INodeTypeDescription,
 	ITriggerResponse,
+	IDataObject,
 } from 'n8n-workflow';
 
 import * as mqtt from 'mqtt';
@@ -13,25 +17,25 @@ import {
 
 export class MqttTrigger implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Mqtt Trigger',
-    name: 'mqttTrigger',
-    icon: 'file:mqtt.png',
+		displayName: 'MQTT Trigger',
+		name: 'mqttTrigger',
+		icon: 'file:mqtt.png',
 		group: ['trigger'],
 		version: 1,
 		description: 'Listens to MQTT events',
 		defaults: {
-			name: 'Mqtt Trigger',
-			color: '#00FF00',
+			name: 'MQTT Trigger',
+			color: '#9b27af',
 		},
 		inputs: [],
 		outputs: ['main'],
-		credentials: [{
-			name: 'mqtt',
-			required: true,
-		}],
+		credentials: [
+			{
+				name: 'mqtt',
+				required: true,
+			},
+		],
 		properties: [
-			// Node properties which the user gets displayed and
-			// can change on the node.
 			{
 				displayName: 'Topics',
 				name: 'topics',
@@ -40,25 +44,51 @@ export class MqttTrigger implements INodeType {
 				description: `Topics to subscribe to, multiple can be defined with comma.<br/>
 				wildcard characters are supported (+ - for single level and # - for multi level)`,
 			},
-		]
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add Option',
+				default: {},
+				options: [
+					{
+						displayName: 'Only Message',
+						name: 'onlyMessage',
+						type: 'boolean',
+						default: false,
+						description: 'Returns only the message property.',
+					},
+					{
+						displayName: 'JSON Parse Message',
+						name: 'jsonParseMessage',
+						type: 'boolean',
+						default: false,
+						description: 'Try to parse the message to an object.',
+					},
+				],
+			},
+		],
 	};
-
 
 	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
 
 		const credentials = this.getCredentials('mqtt');
+
 		if (!credentials) {
 		  throw new Error('Credentials are mandatory!');
 		}
 
 		const topics = (this.getNodeParameter('topics') as string).split(',');
+
+		const options = this.getNodeParameter('options') as IDataObject;
+
 		if (!topics) {
 			throw new Error('Topics are mandatory!');
 		}
 
 		const protocol = credentials.protocol as string || 'mqtt';
-    const host = credentials.host as string || 'localhost';
-    const brokerUrl = `${protocol}://${host}`
+		const host = credentials.host as string;
+		const brokerUrl = `${protocol}://${host}`;
 		const port = credentials.port as number || 1883;
 
 		const clientOptions: IClientOptions = {
@@ -70,37 +100,59 @@ export class MqttTrigger implements INodeType {
 			clientOptions.password = credentials.password as string;
 		}
 
-		const container = mqtt.connect(`${brokerUrl}`, clientOptions);
+		const client = mqtt.connect(brokerUrl, clientOptions);
 
 		const self = this;
 
-    container.on('connect', () => {
-      
-      container.subscribe(topics, (err: Error) => {
-        console.log(`MQTT Node Connected : ${brokerUrl}`);
-				if (!err) {
-					console.log(`MQTT Node Subscription Success : ${topics}`);
-				} else {
-					console.log(`MQTT Node Subscription Fail : ${topics} (${err.toString()})`);
-				}
+		async function manualTriggerFunction() {
+			await new Promise(( resolve, reject ) => {
+				client.on('connect', () => {
+					client.subscribe(topics, (err, granted) => {
+						if (err) {
+							reject(err);
+						}
+						client.on('message', (topic: string, message: Buffer | string) => { // tslint:disable-line:no-any
+
+							let result: IDataObject = {};
+
+							message = message.toString() as string;
+
+							if (options.jsonParseBody) {
+								try {
+									message = JSON.parse(message.toString());
+								} catch (err) {}
+							}
+
+							result.message = message;
+							result.topic = topic;
+
+							if (options.onlyMessage) {
+								//@ts-ignore
+								result = message;
+							}
+
+							self.emit([self.helpers.returnJsonArray([result],
+							)]);
+							resolve(true);
+						});
+					});
+				});
+
+				client.on('error', (error) => {
+					reject(error);
+				});
 			});
-		});
-		
-		container.on('message', (topic: string, message: Buffer) => { // tslint:disable-line:no-any
-			self.emit([self.helpers.returnJsonArray([{ message: message.toString(), topic }])]);
-			console.log(`MQTT Node Message (${topic}) : ${message.toString()}`);
-		});
+		}
 
+		manualTriggerFunction();
 
-		// The "closeFunction" function gets called by n8n whenever
-		// the workflow gets deactivated and can so clean up.
 		async function closeFunction() {
-			container.end();
+			client.end();
 		}
 
 		return {
 			closeFunction,
+			manualTriggerFunction,
 		};
-
 	}
 }
