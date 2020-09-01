01f2e8870b64afe5718f7830549cc216afed7c69
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-09-01 17:50:51 +0200

    :zap: Update buffer code to not get deprecation message

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -298,7 +298,7 @@ class App {
 		this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
 			(req as ICustomRequest).parsedUrl = parseUrl(req);
 			// @ts-ignore
-			req.rawBody = new Buffer('', 'base64');
+			req.rawBody = Buffer.from('', 'base64');
 			next();
 		});
 
