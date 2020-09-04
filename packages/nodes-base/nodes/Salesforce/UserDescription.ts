8a5bd58e5fc841454a3bafdaf292f221f4d47ed6
YErii <yeriime@outlook.com>
2020-09-04 14:14:42 +0800

    sfdc user node getall

--- packages/nodes-base/nodes/Salesforce/UserDescription.ts
+++ packages/nodes-base/nodes/Salesforce/UserDescription.ts
@@ -39,5 +39,22 @@ export const userFields = [
 			}
 		},
 		description: 'Id of user that needs to be fetched'
+	},
+	/* -------------------------------------------------------------------------- */
+	/*                                 user:getAll                                */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource: ['user'],
+				operation: ['getAll']
+			}
+		},
+		default: false,
+		description:
+			'If all results should be returned or only up to a given limit.'
 	}
 ] as INodeProperties[];
