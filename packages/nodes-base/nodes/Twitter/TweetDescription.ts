7d888baf6eac7ff97bd83e56459b0351cb5ebdc0
ricardo <ricardoespinoza105@gmail.com>
2020-09-16 18:42:40 -0400

    :zap: Add retweet, reply and like operations

--- packages/nodes-base/nodes/Twitter/TweetDescription.ts
+++ packages/nodes-base/nodes/Twitter/TweetDescription.ts
@@ -18,12 +18,22 @@ export const tweetOperations = [
 			{
 				name: 'Create',
 				value: 'create',
-				description: 'Create a new tweet',
+				description: 'Create or reply a tweet',
 			},
 			{
 				name: 'Search',
 				value: 'search',
 				description: 'Search tweets',
+			},
+			{
+				name: 'Like',
+				value: 'like',
+				description: 'Like a tweet',
+			},
+			{
+				name: 'Retweet',
+				value: 'retweet',
+				description: 'Retweet a tweet',
 			},
 		],
 		default: 'create',
@@ -86,6 +96,13 @@ export const tweetFields = [
 				type: 'boolean',
 				default: false,
 				description: 'Whether or not to put a pin on the exact coordinates a Tweet has been sent from.',
+			},
+			{
+				displayName: 'In Reply to Tweet',
+				name: 'inReplyToStatusId',
+				type: 'string',
+				default: '',
+				description: 'The ID of an existing status that the update is in reply to.',
 			},
 			{
 				displayName: 'Location',
@@ -318,6 +335,100 @@ export const tweetFields = [
 				type: 'dateTime',
 				default: '',
 				description: 'Returns tweets created before the given date',
+			},
+		],
+	},
+/* -------------------------------------------------------------------------- */
+/*                                tweet:like                                  */
+/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Tweet ID',
+		name: 'tweetId',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				operation: [
+					'like',
+				],
+				resource: [
+					'tweet',
+				],
+			},
+		},
+		description: 'The ID of the tweet',
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+					'like',
+				],
+				resource: [
+					'tweet',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'include Entities',
+				name: 'includeEntities',
+				type: 'boolean',
+				default: false,
+				description: 'The entities node will be omitted when set to false',
+			},
+		],
+	},
+/* -------------------------------------------------------------------------- */
+/*                                tweet:retweet                               */
+/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Tweet ID',
+		name: 'tweetId',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				operation: [
+					'retweet',
+				],
+				resource: [
+					'tweet',
+				],
+			},
+		},
+		description: 'The ID of the tweet',
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				operation: [
+					'retweet',
+				],
+				resource: [
+					'tweet',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Trim User',
+				name: 'trimUser',
+				type: 'boolean',
+				default: false,
+				description: `When set to either true, each tweet returned in a timeline will include a user object including only the status authors numerical ID.`,
 			},
 		],
 	},
