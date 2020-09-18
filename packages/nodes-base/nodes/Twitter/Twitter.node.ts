78e20c5516d6a10f39f038d7560a781dee793a38
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-09-18 09:57:38 +0200

    :zap: Remove debug code

--- packages/nodes-base/nodes/Twitter/Twitter.node.ts
+++ packages/nodes-base/nodes/Twitter/Twitter.node.ts
@@ -230,7 +230,6 @@ export class Twitter implements INodeType {
 						}
 					}
 
-					console.log(body);
 					responseData = await twitterApiRequest.call(this, 'POST', '/statuses/update.json', body);
 				}
 				// https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
