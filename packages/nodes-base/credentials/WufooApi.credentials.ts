eeabe13e23a0b3a6a6149bc35536b52ced5de212
Rupenieks <32895755+Rupenieks@users.noreply.github.com>
2020-10-01 09:07:55 +0200

    :sparkles: Add Wufoo-Trigger node (#597)
    * Generic functions, credentials, interface, logo, triggernode
    
    * TSLint tabs rule, request error handling, webhook done
    
    On hold until Wufoo responds with sandbox account
    
    * Removed test logging, fixed various errors, cleaned up
    
    * Sorted imports, adjusted data display,  removed anys, tslint import sort rule
    
    * RAAAAAW data
    
    * :zap: Fix logo and improve formatting
    
    * :zap: Improvements
    
    * :zap: Minor improvements to WuFoo-Trigger
    
    Co-authored-by: Rupenieks <ru@myos,co>
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/credentials/WufooApi.credentials.ts
@@ -0,0 +1,23 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class WufooApi implements ICredentialType {
+	name = 'wufooApi';
+	displayName = 'Wufoo API';
+	properties = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'Subdomain',
+			name: 'subdomain',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+        }
+	];
+}
