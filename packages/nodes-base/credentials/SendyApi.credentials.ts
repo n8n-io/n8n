6098384a30e52e7fff14037fa8e129093947c997
Ricardo Espinoza <ricardo@n8n.io>
2020-10-13 04:48:20 -0400

    :sparkles: Add Sendy-Node (#1013)
    * :sparkles: Sendy-Node
    
    * :sparkles: Improvements
    
    * :zap: Small improvement

---
+++ packages/nodes-base/credentials/SendyApi.credentials.ts
@@ -0,0 +1,24 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class SendyApi implements ICredentialType {
+	name = 'sendyApi';
+	displayName = 'Sendy API';
+	properties = [
+		{
+			displayName: 'URL',
+			name: 'url',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+			placeholder: 'https://yourdomain.com',
+		},
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
