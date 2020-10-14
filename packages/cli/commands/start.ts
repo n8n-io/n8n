54bf06dabc0ee6910781b4ed351b6aaa79fc7060
Ben Hesseldieck <1849459+BHesseldieck@users.noreply.github.com>
2020-10-14 16:38:48 +0200

    :zap: Add hooks (#1053)
    * :zap: add 2 hooks
    
    * :zap: Fix issue that if hook errors n8n would not shut down
    
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/cli/commands/start.ts
+++ packages/cli/commands/start.ts
@@ -70,6 +70,13 @@ export class Start extends Command {
 	static async stopProcess() {
 		console.log(`\nStopping n8n...`);
 
+		const externalHooks = ExternalHooks();
+		try {
+			await externalHooks.run('n8n.stop', []);
+		} catch (error) {
+			console.error('There was an error in the "n8n.stop" hook.', error);
+		}
+
 		setTimeout(() => {
 			// In case that something goes wrong with shutdown we
 			// kill after max. 30 seconds no matter what
