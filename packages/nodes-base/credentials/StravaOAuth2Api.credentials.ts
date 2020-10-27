ca4c50c322727517519b7ace4b908b40c7c7bde4
Harshil Agrawal <ghagrawal17@gmail.com>
2020-10-27 15:48:24 +0000

    Add credential documentation URL for Strava (#1104)
    * Add credential documentation URL for Strava
    
    * 🔨 Fix the documentation URL

--- packages/nodes-base/credentials/StravaOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/StravaOAuth2Api.credentials.ts
@@ -9,6 +9,7 @@ export class StravaOAuth2Api implements ICredentialType {
 		'oAuth2Api',
 	];
 	displayName = 'Strava OAuth2 API';
+	documentationUrl = 'strava';
 	properties = [
 		{
 			displayName: 'Authorization URL',
