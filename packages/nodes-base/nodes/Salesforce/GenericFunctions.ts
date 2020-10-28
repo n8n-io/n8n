e3520822fb3eb446725d8cb951a1687a8e55beeb
Ricardo Espinoza <ricardo@n8n.io>
2020-10-28 18:07:35 -0400

    :zap: Feature/salesforce jwt bearer (#1082)
    * Salesforce - OAuth 2.0 JWT Bearer Flow for Server-to-Server Integration
    
    * :zap: Small improvements
    
    * :zap: Small fix
    
    Co-authored-by: Craig McElroy <craig@mcelroyfamily.com>

--- packages/nodes-base/nodes/Salesforce/GenericFunctions.ts
+++ packages/nodes-base/nodes/Salesforce/GenericFunctions.ts
@@ -13,19 +13,33 @@ import {
 	INodePropertyOptions,
 } from 'n8n-workflow';
 
+import * as moment from 'moment-timezone';
+
+import * as jwt from 'jsonwebtoken';
+
 export async function salesforceApiRequest(this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-	const credentials = this.getCredentials('salesforceOAuth2Api');
-	const subdomain = ((credentials!.accessTokenUrl as string).match(/https:\/\/(.+).salesforce\.com/) || [])[1];
-	const options: OptionsWithUri = {
-		method,
-		body: method === 'GET' ? undefined : body,
-		qs,
-		uri: `https://${subdomain}.salesforce.com/services/data/v39.0${uri || endpoint}`,
-		json: true,
-	};
+	const authenticationMethod = this.getNodeParameter('authentication', 0, 'oAuth2') as string;
+
 	try {
-		//@ts-ignore
-		return await this.helpers.requestOAuth2.call(this, 'salesforceOAuth2Api', options);
+		if (authenticationMethod === 'jwt') {
+			// https://help.salesforce.com/articleView?id=remoteaccess_oauth_jwt_flow.htm&type=5
+			const credentialsType = 'salesforceJwtApi';
+			const credentials = this.getCredentials(credentialsType);
+			const response = await getAccessToken.call(this, credentials as IDataObject);
+			const { instance_url, access_token } = response;
+			const options = getOptions.call(this, method, (uri || endpoint), body, qs, instance_url as string);
+			options.headers!.Authorization = `Bearer ${access_token}`;
+			//@ts-ignore
+			return await this.helpers.request(options);
+		} else {
+			// https://help.salesforce.com/articleView?id=remoteaccess_oauth_web_server_flow.htm&type=5
+			const credentialsType = 'salesforceOAuth2Api';
+			const credentials = this.getCredentials(credentialsType);
+			const subdomain = ((credentials!.accessTokenUrl as string).match(/https:\/\/(.+).salesforce\.com/) || [])[1];
+			const options = getOptions.call(this, method, (uri || endpoint), body, qs, `https://${subdomain}.salesforce.com`);
+			//@ts-ignore
+			return await this.helpers.requestOAuth2.call(this, credentialsType, options);
+		}
 	} catch (error) {
 		if (error.response && error.response.body && error.response.body[0] && error.response.body[0].message) {
 			// Try to return the error prettier
@@ -36,7 +50,6 @@ export async function salesforceApiRequest(this: IExecuteFunctions | IExecuteSin
 }
 
 export async function salesforceApiRequestAllItems(this: IExecuteFunctions | ILoadOptionsFunctions, propertyName: string, method: string, endpoint: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-
 	const returnData: IDataObject[] = [];
 
 	let responseData;
@@ -54,8 +67,6 @@ export async function salesforceApiRequestAllItems(this: IExecuteFunctions | ILo
 	return returnData;
 }
 
-
-
 /**
  * Sorts the given options alphabetically
  *
@@ -69,4 +80,57 @@ export function sortOptions(options: INodePropertyOptions[]): void {
 		if (a.name > b.name) { return 1; }
 		return 0;
 	});
+}
+
+function getOptions(this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any, qs: IDataObject, instanceUrl: string): OptionsWithUri {
+	const options: OptionsWithUri = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method,
+		body: method === 'GET' ? undefined : body,
+		qs,
+		uri: `${instanceUrl}/services/data/v39.0${endpoint}`,
+		json: true
+	};
+
+	//@ts-ignore
+	return options;
+}
+
+function getAccessToken(this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, credentials: IDataObject): Promise<IDataObject> {
+	const now = moment().unix();
+	const authUrl = credentials.environment === 'sandbox' ? 'https://test.salesforce.com' : 'https://login.salesforce.com';
+
+	const signature = jwt.sign(
+		{
+			'iss': credentials.clientId as string,
+			'sub': credentials.username as string,
+			'aud': authUrl,
+			'exp': now + 3 * 60,
+		},
+		credentials.privateKey as string,
+		{
+			algorithm: 'RS256',
+			header: {
+				'alg': 'RS256',
+			},
+		}
+	);
+
+	const options: OptionsWithUri = {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		method: 'POST',
+		form: {
+			grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+			assertion: signature,
+		},
+		uri: `${authUrl}/services/oauth2/token`,
+		json: true
+	};
+
+	//@ts-ignore
+	return this.helpers.request(options);
 }
