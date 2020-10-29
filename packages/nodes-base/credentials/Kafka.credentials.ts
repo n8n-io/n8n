b806ee3a5757f92b2f7383076337bff64dccb7c1
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-10-29 15:44:11 +0100

    :zap: Improvements to Kafka-Node

--- packages/nodes-base/credentials/Kafka.credentials.ts
+++ packages/nodes-base/credentials/Kafka.credentials.ts
@@ -13,18 +13,37 @@ export class Kafka implements ICredentialType {
 			name: 'clientId',
 			type: 'string' as NodePropertyTypes,
 			default: '',
+			placeholder: 'my-app',
 		},
 		{
 			displayName: 'Brokers',
 			name: 'brokers',
 			type: 'string' as NodePropertyTypes,
 			default: '',
+			placeholder: 'kafka1:9092,kafka2:9092',
 		},
 		{
 			displayName: 'SSL',
 			name: 'ssl',
 			type: 'boolean' as NodePropertyTypes,
 			default: true,
+		},
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+			description: 'Optional username if authenticated is required.',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string' as NodePropertyTypes,
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			description: 'Optional password if authenticated is required.',
 		},
 	];
 }
