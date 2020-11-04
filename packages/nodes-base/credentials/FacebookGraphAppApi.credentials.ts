d80d486e5936d7ae706918b19848842f84541f60
Jan Oberhauser <jan.oberhauser@gmail.com>
2020-11-04 12:26:09 +0100

    :zap: Small changes to Facebook Trigger Node

--- packages/nodes-base/credentials/FacebookGraphAppApi.credentials.ts
+++ packages/nodes-base/credentials/FacebookGraphAppApi.credentials.ts
@@ -3,15 +3,15 @@ import {
 	NodePropertyTypes,
 } from 'n8n-workflow';
 
-export class FacebookGraphSubscriptionApi implements ICredentialType {
-	name = 'facebookGraphSubscriptionApi';
-	displayName = 'Facebook Graph API';
+export class FacebookGraphAppApi implements ICredentialType {
+	name = 'facebookGraphAppApi';
+	displayName = 'Facebook Graph API (App)';
 	extends = [
 		'facebookGraphApi',
 	];
 	properties = [
 		{
-			displayName: 'APP Secret',
+			displayName: 'App Secret',
 			name: 'appSecret',
 			type: 'string' as NodePropertyTypes,
 			default: '',
