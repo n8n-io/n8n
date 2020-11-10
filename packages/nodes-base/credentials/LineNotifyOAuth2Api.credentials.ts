54368d64280f6fba52a8b60de6dcd917cc4bf02e
Ricardo Espinoza <ricardo@n8n.io>
2020-11-10 13:08:48 -0500

    :sparkles: Add Line-Node (#1137)
    * :sparkles: Line-Node
    
    * :zap: Improvements

---
+++ packages/nodes-base/credentials/LineNotifyOAuth2Api.credentials.ts
@@ -0,0 +1,47 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+export class LineNotifyOAuth2Api implements ICredentialType {
+	name = 'lineNotifyOAuth2Api';
+	extends = [
+		'oAuth2Api',
+	];
+	displayName = 'Line Notify OAuth2 API';
+	properties = [
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'https://notify-bot.line.me/oauth/authorize',
+			required: true,
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'https://notify-bot.line.me/oauth/token',
+			required: true,
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'notify',
+			required: true,
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'hidden' as NodePropertyTypes,
+			default: '',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'body',
+		},
+	];
+}
