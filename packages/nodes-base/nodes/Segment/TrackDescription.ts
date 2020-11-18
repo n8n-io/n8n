b0863e55b17f78e958f6ec53995c9f835bee275b
Ricardo Espinoza <ricardo@n8n.io>
2020-11-18 17:37:56 -0500

    :zap: Feature/segment extended (#1174)
    * :zap: Add custom traits
    
    * :zap: Improvements
    
    * :zap: Small changes
    
    * :zap: Improvements

--- packages/nodes-base/nodes/Segment/TrackDescription.ts
+++ packages/nodes-base/nodes/Segment/TrackDescription.ts
@@ -72,256 +72,6 @@ export const trackFields = [
 		required: true,
 	},
 	{
-		displayName: 'Traits',
-		name: 'traits',
-		placeholder: 'Add Trait',
-		type: 'fixedCollection',
-		typeOptions: {
-			multipleValues: false,
-		},
-		displayOptions: {
-			show: {
-				resource: [
-					'track',
-				],
-				operation: [
-					'event',
-				],
-			},
-		},
-		default: {},
-		options: [
-			{
-				name: 'traitsUi',
-				displayName: 'Trait',
-				values: [
-					{
-						displayName: 'Email',
-						name: 'email',
-						type: 'string',
-						default: '',
-						description: 'Email address of a user',
-					},
-					{
-						displayName: 'First Name',
-						name: 'firstname',
-						type: 'string',
-						default: '',
-						description: 'First name of a user',
-					},
-					{
-						displayName: 'Last Name',
-						name: 'lastname',
-						type: 'string',
-						default: '',
-						description: 'Last name of a user',
-					},
-					{
-						displayName: 'Gender',
-						name: 'gender',
-						type: 'string',
-						default: '',
-						description: 'Gender of a user',
-					},
-					{
-						displayName: 'Phone',
-						name: 'phone',
-						type: 'string',
-						default: '',
-						description: 'Phone number of a user',
-					},
-					{
-						displayName: 'Username',
-						name: 'username',
-						type: 'string',
-						default: '',
-						description: 'User’s username',
-					},
-					{
-						displayName: 'Website',
-						name: 'website',
-						type: 'string',
-						default: '',
-						description: 'Website of a user',
-					},
-					{
-						displayName: 'Age',
-						name: 'age',
-						type: 'number',
-						default: 1,
-						description: 'Age of a user',
-					},
-					{
-						displayName: 'Avatar',
-						name: 'avatar',
-						type: 'string',
-						default: '',
-						description: 'URL to an avatar image for the user',
-					},
-					{
-						displayName: 'Birthday',
-						name: 'birthday',
-						type: 'dateTime',
-						default: '',
-						description: 'User’s birthday',
-					},
-					{
-						displayName: 'Created At',
-						name: 'createdAt',
-						type: 'dateTime',
-						default: '',
-						description: 'Date the user’s account was first created at',
-					},
-					{
-						displayName: 'Description',
-						name: 'description',
-						type: 'string',
-						typeOptions: {
-							alwaysOpenEditWindow: true,
-						},
-						default: '',
-						description: 'Description of the user',
-					},
-					{
-						displayName: 'ID',
-						name: 'id',
-						type: 'string',
-						default: '',
-						description: 'Unique ID in your database for a user',
-					},
-					{
-						displayName: 'Company',
-						name: 'company',
-						placeholder: 'Add Company',
-						type: 'fixedCollection',
-						typeOptions: {
-							multipleValues: false,
-						},
-						default: {},
-						options: [
-							{
-								name: 'companyUi',
-								displayName: 'Company',
-								values: [
-									{
-										displayName: 'ID',
-										name: 'id',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Name',
-										name: 'name',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Industry',
-										name: 'industry',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Employee Count',
-										name: 'employeeCount',
-										type: 'number',
-										default: 1,
-									},
-									{
-										displayName: 'Plan',
-										name: 'plan',
-										type: 'string',
-										default: '',
-									},
-								],
-							},
-						],
-					},
-					{
-						displayName: 'Address',
-						name: 'address',
-						placeholder: 'Add Address',
-						type: 'fixedCollection',
-						typeOptions: {
-							multipleValues: false,
-						},
-						default: {},
-						options: [
-							{
-								name: 'addressUi',
-								displayName: 'Address',
-								values: [
-									{
-										displayName: 'Street',
-										name: 'street',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'City',
-										name: 'city',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'State',
-										name: 'state',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Postal Code',
-										name: 'postalCode',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Country',
-										name: 'country',
-										type: 'string',
-										default: '',
-									},
-								],
-							},
-						],
-					},
-					{
-						displayName: 'Custom Traits',
-						name: 'customTraitsUi',
-						placeholder: 'Add Custom Trait',
-						type: 'fixedCollection',
-						default: '',
-						typeOptions: {
-							multipleValues: true,
-						},
-						options: [
-							{
-								name: 'customTraitValues',
-								displayName: 'Custom Traits',
-								values: [
-									{
-										displayName: 'Key',
-										name: 'key',
-										type: 'string',
-										default: '',
-										description: '',
-									},
-									{
-										displayName: 'Value',
-										name: 'value',
-										type: 'string',
-										default: '',
-										description: '',
-									},
-								],
-							},
-						],
-					},
-				],
-			},
-		],
-	},
-	{
 		displayName: 'Context',
 		name: 'context',
 		placeholder: 'Add Context',
@@ -568,7 +318,7 @@ export const trackFields = [
 		placeholder: 'Add Properties',
 		type: 'fixedCollection',
 		typeOptions: {
-			multipleValues: false,
+			multipleValues: true,
 		},
 		displayOptions: {
 			show: {
@@ -584,31 +334,21 @@ export const trackFields = [
 		options: [
 			{
 				name: 'propertiesUi',
-				displayName: 'Properties',
+				displayName: 'Property',
 				values: [
 					{
-						displayName: 'Revenue',
-						name: 'revenue',
-						type: 'number',
-						typeOptions: {
-							numberPrecision: 2,
-						},
-						default: 1,
-						description: 'Amount of revenue an event resulted in. This should be a decimal value, so a shirt worth $19.99 would result in a revenue of 19.99.',
-					},
-					{
-						displayName: 'Currency',
-						name: 'currency',
+						displayName: 'Key',
+						name: 'key',
 						type: 'string',
 						default: '',
-						description: 'Currency of the revenue an event resulted in <p>This should be sent in the ISO 4127 format. If this is not set, we assume the revenue to be in US dollars.</p>',
+						description: '',
 					},
 					{
 						displayName: 'Value',
 						name: 'value',
-						type: 'number',
+						type: 'string',
 						default: '',
-						description: 'An abstract “value” to associate with an event. This is typically used in situations where the event doesn’t generate real-dollar revenue, but has an intrinsic value to a marketing team, like newsletter signups.',
+						description: '',
 					},
 				],
 			},
@@ -651,224 +391,6 @@ export const trackFields = [
 		description: 'Name of the page For example, most sites have a “Signup” page that can be useful to tag, so you can see users as they move through your funnel',
 	},
 	{
-		displayName: 'Traits',
-		name: 'traits',
-		placeholder: 'Add Trait',
-		type: 'fixedCollection',
-		typeOptions: {
-			multipleValues: false,
-		},
-		displayOptions: {
-			show: {
-				resource: [
-					'track',
-				],
-				operation: [
-					'page',
-				],
-			},
-		},
-		default: {},
-		options: [
-			{
-				name: 'traitsUi',
-				displayName: 'Trait',
-				values: [
-					{
-						displayName: 'Email',
-						name: 'email',
-						type: 'string',
-						default: '',
-						description: 'Email address of a user',
-					},
-					{
-						displayName: 'First Name',
-						name: 'firstname',
-						type: 'string',
-						default: '',
-						description: 'First name of a user',
-					},
-					{
-						displayName: 'Last Name',
-						name: 'lastname',
-						type: 'string',
-						default: '',
-						description: 'Last name of a user',
-					},
-					{
-						displayName: 'Gender',
-						name: 'gender',
-						type: 'string',
-						default: '',
-						description: 'Gender of a user',
-					},
-					{
-						displayName: 'Phone',
-						name: 'phone',
-						type: 'string',
-						default: '',
-						description: 'Phone number of a user',
-					},
-					{
-						displayName: 'Username',
-						name: 'username',
-						type: 'string',
-						default: '',
-						description: 'User’s username',
-					},
-					{
-						displayName: 'Website',
-						name: 'website',
-						type: 'string',
-						default: '',
-						description: 'Website of a user',
-					},
-					{
-						displayName: 'Age',
-						name: 'age',
-						type: 'number',
-						default: 1,
-						description: 'Age of a user',
-					},
-					{
-						displayName: 'Avatar',
-						name: 'avatar',
-						type: 'string',
-						default: '',
-						description: 'URL to an avatar image for the user',
-					},
-					{
-						displayName: 'Birthday',
-						name: 'birthday',
-						type: 'dateTime',
-						default: '',
-						description: 'User’s birthday',
-					},
-					{
-						displayName: 'Created At',
-						name: 'createdAt',
-						type: 'dateTime',
-						default: '',
-						description: 'Date the user’s account was first created at',
-					},
-					{
-						displayName: 'Description',
-						name: 'description',
-						type: 'string',
-						typeOptions: {
-							alwaysOpenEditWindow: true,
-						},
-						default: '',
-						description: 'Description of the user',
-					},
-					{
-						displayName: 'ID',
-						name: 'id',
-						type: 'string',
-						default: '',
-						description: 'Unique ID in your database for a user',
-					},
-					{
-						displayName: 'Company',
-						name: 'company',
-						placeholder: 'Add Company',
-						type: 'fixedCollection',
-						typeOptions: {
-							multipleValues: false,
-						},
-						default: {},
-						options: [
-							{
-								name: 'companyUi',
-								displayName: 'Company',
-								values: [
-									{
-										displayName: 'ID',
-										name: 'id',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Name',
-										name: 'name',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Industry',
-										name: 'industry',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Employee Count',
-										name: 'employeeCount',
-										type: 'number',
-										default: 1,
-									},
-									{
-										displayName: 'Plan',
-										name: 'plan',
-										type: 'string',
-										default: '',
-									},
-								],
-							},
-						],
-					},
-					{
-						displayName: 'Address',
-						name: 'address',
-						placeholder: 'Add Address',
-						type: 'fixedCollection',
-						typeOptions: {
-							multipleValues: false,
-						},
-						default: {},
-						options: [
-							{
-								name: 'addressUi',
-								displayName: 'Address',
-								values: [
-									{
-										displayName: 'Street',
-										name: 'street',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'City',
-										name: 'city',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'State',
-										name: 'state',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Postal Code',
-										name: 'postalCode',
-										type: 'string',
-										default: '',
-									},
-									{
-										displayName: 'Country',
-										name: 'country',
-										type: 'string',
-										default: '',
-									},
-								],
-							},
-						],
-					},
-				],
-			},
-		],
-	},
-	{
 		displayName: 'Context',
 		name: 'context',
 		placeholder: 'Add Context',
@@ -1115,7 +637,7 @@ export const trackFields = [
 		placeholder: 'Add Properties',
 		type: 'fixedCollection',
 		typeOptions: {
-			multipleValues: false,
+			multipleValues: true,
 		},
 		displayOptions: {
 			show: {
@@ -1131,56 +653,21 @@ export const trackFields = [
 		options: [
 			{
 				name: 'propertiesUi',
-				displayName: 'Properties',
+				displayName: 'Property',
 				values: [
 					{
-						displayName: 'Name',
-						name: 'name',
+						displayName: 'Key',
+						name: 'key',
 						type: 'string',
 						default: '',
-						description: 'Name of the page. This is reserved for future use.',
+						description: '',
 					},
 					{
-						displayName: 'Path',
-						name: 'path',
+						displayName: 'Value',
+						name: 'value',
 						type: 'string',
 						default: '',
-						description: 'Path portion of the URL of the page. Equivalent to canonical path which defaults to location.pathname from the DOM API.',
-					},
-					{
-						displayName: 'Referrer',
-						name: 'referrer',
-						type: 'string',
-						default: '',
-						description: 'Full URL of the previous page. Equivalent to document.referrer from the DOM API.',
-					},
-					{
-						displayName: 'Search',
-						name: 'search',
-						type: 'string',
-						default: '',
-						description: 'Query string portion of the URL of the page. Equivalent to location.search from the DOM API.',
-					},
-					{
-						displayName: 'Title',
-						name: 'title',
-						type: 'string',
-						default: '',
-						description: 'Title of the page. Equivalent to document.title from the DOM API.',
-					},
-					{
-						displayName: 'URL',
-						name: 'url',
-						type: 'string',
-						default: '',
-						description: 'Full URL of the page. First we look for the canonical url. If the canonical url is not provided, we use location.href from the DOM API.',
-					},
-					{
-						displayName: 'Keywords',
-						name: 'keywords',
-						type: 'string',
-						default: '',
-						description: 'A list/array of keywords describing the content of the page. The keywords would most likely be the same as, or similar to, the keywords you would find in an html meta tag for SEO purposes.',
+						description: '',
 					},
 				],
 			},
