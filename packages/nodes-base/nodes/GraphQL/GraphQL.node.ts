d9a7d5fa34fe47dc846b3d517a137eda666bd6c8
Henrik Feldt <henrik@haf.se>
2020-11-18 23:14:41 +0100

    :zap: Update GraphQL.node.ts (#1185)
    * Update GraphQL.node.ts
    
    Fix this
    
    * Update GraphQL.node.ts

--- packages/nodes-base/nodes/GraphQL/GraphQL.node.ts
+++ packages/nodes-base/nodes/GraphQL/GraphQL.node.ts
@@ -241,8 +241,8 @@ export class GraphQL implements INodeType {
 					if (typeof requestOptions.body.variables === 'string') {
 						try {
 							requestOptions.body.variables = JSON.parse(requestOptions.body.variables);
-						} catch {
-							requestOptions.body.variables = {};
+						} catch (e) {
+							throw new Error('Using variables failed:\n' + requestOptions.body.variables + '\n\nWith error message:\n' + e);
 						}
 					}
 					if (requestOptions.body.operationName === '') {
