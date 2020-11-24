5587461d14ed64ead2f56ac967c1cef7daf14164
Ronalds Upenieks <32895755+Rupenieks@users.noreply.github.com>
2020-11-24 14:15:47 +0100

    :zap: Add Harvest OAuth2 support (#697)
    * credentials, generic functions fit to oauth2, UI elements
    
    * :zap: Improvements to Harvest-Node
    
    * Update Harvest.node.ts
    
    * :zap: Add OAuth2 and move account id from credentials to node parameters
    
    * :zap: Minor improvements to Harvest-Node
    
    Co-authored-by: Rupenieks <ru@myos,co>
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/nodes-base/nodes/Harvest/InvoiceDescription.ts
+++ packages/nodes-base/nodes/Harvest/InvoiceDescription.ts
@@ -1,6 +1,6 @@
 import { INodeProperties } from 'n8n-workflow';
 
-const resource = [ 'invoice' ];
+const resource = ['invoice'];
 
 export const invoiceOperations = [
 	{
@@ -47,174 +47,174 @@ export const invoiceOperations = [
 
 export const invoiceFields = [
 
-/* -------------------------------------------------------------------------- */
-/*                                invoice:getAll                            */
-/* -------------------------------------------------------------------------- */
+	/* -------------------------------------------------------------------------- */
+	/*                                invoice:getAll                            */
+	/* -------------------------------------------------------------------------- */
 
-{
-	displayName: 'Return All',
-	name: 'returnAll',
-	type: 'boolean',
-	displayOptions: {
-		show: {
-			resource,
-			operation: [
-				'getAll',
-			],
-		},
-	},
-	default: false,
-	description: 'Returns a list of your invoices.',
-},
-{
-	displayName: 'Limit',
-	name: 'limit',
-	type: 'number',
-	displayOptions: {
-		show: {
-			resource,
-			operation: [
-				'getAll',
-			],
-			returnAll: [
-				false,
-			],
-		},
-	},
-	typeOptions: {
-		minValue: 1,
-		maxValue: 100,
-	},
-	default: 100,
-	description: 'How many results to return.',
-},
-{
-	displayName: 'Filters',
-	name: 'filters',
-	type: 'collection',
-	placeholder: 'Add Filter',
-	default: {},
-	displayOptions: {
-		show: {
-			resource,
-			operation: [
-				'getAll',
-			],
-		},
-	},
-	options: [
-		{
-			displayName: 'Client ID',
-			name: 'client_id',
-			type: 'string',
-			default: '',
-			description: 'Only return time entries belonging to the client with the given ID.',
-		},
-		{
-			displayName: 'Project ID',
-			name: 'project_id',
-			type: 'string',
-			default: '',
-			description: 'Only return time entries belonging to the client with the given ID.',
-		},
-		{
-			displayName: 'Updated Since',
-			name: 'updated_since',
-			type: 'dateTime',
-			default: '',
-			description: 'Only return time entries that have been updated since the given date and time.',
-		},
-		{
-			displayName: 'From',
-			name: 'from',
-			type: 'dateTime',
-			default: '',
-			description: 'Only return time entries with a spent_date on or after the given date.',
-		},
-		{
-			displayName: 'To',
-			name: 'to',
-			type: 'dateTime',
-			default: '',
-			description: 'Only return time entries with a spent_date on or before the given date.',
-		},
-		{
-			displayName: 'State',
-			name: 'state',
-			type: 'multiOptions',
-			options: [
-				{
-					name: 'draft',
-					value: 'draft',
-				},
-				{
-					name: 'open',
-					value: 'open',
-				},
-				{
-					name: 'paid',
-					value: 'paid',
-				},
-				{
-					name: 'closed',
-					value: 'closed',
-				},
-			],
-			default: [],
-			description: 'Only return invoices with a state matching the value provided. Options: draft, open, paid, or closed.',
-		},
-		{
-			displayName: 'Page',
-			name: 'page',
-			type: 'number',
-			typeOptions: {
-				minValue: 1,
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				resource,
+				operation: [
+					'getAll',
+				],
 			},
-			default: 1,
-			description: 'The page number to use in pagination. For instance, if you make a list request and receive 100 records, your subsequent call can include page=2 to retrieve the next page of the list. (Default: 1)',
 		},
-	],
-},
-
-/* -------------------------------------------------------------------------- */
-/*                                invoice:get                            */
-/* -------------------------------------------------------------------------- */
-{
-	displayName: 'Invoice Id',
-	name: 'id',
-	type: 'string',
-	default: '',
-	required: true,
-	displayOptions: {
-		show: {
-			operation: [
-				'get',
-			],
-			resource,
-		},
+		default: false,
+		description: 'Returns a list of your invoices.',
 	},
-	description: 'The ID of the invoice you are retrieving.',
-},
-
-/* -------------------------------------------------------------------------- */
-/*                                invoice:delete                            */
-/* -------------------------------------------------------------------------- */
-{
-	displayName: 'Invoice Id',
-	name: 'id',
-	type: 'string',
-	default: '',
-	required: true,
-	displayOptions: {
-		show: {
-			operation: [
-				'delete',
-			],
-			resource,
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		displayOptions: {
+			show: {
+				resource,
+				operation: [
+					'getAll',
+				],
+				returnAll: [
+					false,
+				],
+			},
 		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 100,
+		},
+		default: 100,
+		description: 'How many results to return.',
 	},
-	description: 'The ID of the invoice want to delete.',
-},
+	{
+		displayName: 'Filters',
+		name: 'filters',
+		type: 'collection',
+		placeholder: 'Add Filter',
+		default: {},
+		displayOptions: {
+			show: {
+				resource,
+				operation: [
+					'getAll',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Client ID',
+				name: 'client_id',
+				type: 'string',
+				default: '',
+				description: 'Only return time entries belonging to the client with the given ID.',
+			},
+			{
+				displayName: 'Project ID',
+				name: 'project_id',
+				type: 'string',
+				default: '',
+				description: 'Only return time entries belonging to the client with the given ID.',
+			},
+			{
+				displayName: 'Updated Since',
+				name: 'updated_since',
+				type: 'dateTime',
+				default: '',
+				description: 'Only return time entries that have been updated since the given date and time.',
+			},
+			{
+				displayName: 'From',
+				name: 'from',
+				type: 'dateTime',
+				default: '',
+				description: 'Only return time entries with a spent_date on or after the given date.',
+			},
+			{
+				displayName: 'To',
+				name: 'to',
+				type: 'dateTime',
+				default: '',
+				description: 'Only return time entries with a spent_date on or before the given date.',
+			},
+			{
+				displayName: 'State',
+				name: 'state',
+				type: 'multiOptions',
+				options: [
+					{
+						name: 'draft',
+						value: 'draft',
+					},
+					{
+						name: 'open',
+						value: 'open',
+					},
+					{
+						name: 'paid',
+						value: 'paid',
+					},
+					{
+						name: 'closed',
+						value: 'closed',
+					},
+				],
+				default: [],
+				description: 'Only return invoices with a state matching the value provided. Options: draft, open, paid, or closed.',
+			},
+			{
+				displayName: 'Page',
+				name: 'page',
+				type: 'number',
+				typeOptions: {
+					minValue: 1,
+				},
+				default: 1,
+				description: 'The page number to use in pagination. For instance, if you make a list request and receive 100 records, your subsequent call can include page=2 to retrieve the next page of the list. (Default: 1)',
+			},
+		],
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                invoice:get                            */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Invoice Id',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'get',
+				],
+				resource,
+			},
+		},
+		description: 'The ID of the invoice you are retrieving.',
+	},
+
+	/* -------------------------------------------------------------------------- */
+	/*                                invoice:delete                            */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Invoice Id',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: [
+					'delete',
+				],
+				resource,
+			},
+		},
+		description: 'The ID of the invoice want to delete.',
+	},
 
 	/* -------------------------------------------------------------------------- */
 	/*                                invoice:create                           */
@@ -344,7 +344,7 @@ export const invoiceFields = [
 		],
 	},
 
-		/* -------------------------------------------------------------------------- */
+	/* -------------------------------------------------------------------------- */
 	/*                                invoice:update                           */
 	/* -------------------------------------------------------------------------- */
 	{
