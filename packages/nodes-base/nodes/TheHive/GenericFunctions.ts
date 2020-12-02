5850d2acce7a8216084359ab01985bd3120d207d
Ricardo Espinoza <ricardo@n8n.io>
2020-12-02 05:24:25 -0500

    :sparkles: Add TheHive & Cortex nodes (#952)
    * :sparkles: TheHive & Cortex nodes
    
    * :hammer: Make changes mentioned in #887
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    * :zap: Add descriptions
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    Co-authored-by: MedAliMarz <servfrdali@yahoo.fr>

---
+++ packages/nodes-base/nodes/TheHive/GenericFunctions.ts
@@ -0,0 +1,123 @@
+import {
+	OptionsWithUri,
+} from 'request';
+
+import {
+	IExecuteFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+} from 'n8n-workflow';
+
+import * as moment from 'moment';
+
+export async function theHiveApiRequest(this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions, method: string, resource: string, body: any = {}, query: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+	const credentials = this.getCredentials('theHiveApi');
+
+	if (credentials === undefined) {
+		throw new Error('No credentials got returned!');
+	}
+
+	const headerWithAuthentication = Object.assign({}, { Authorization: `Bearer ${credentials.ApiKey}` });
+
+	let options: OptionsWithUri = {
+		headers: headerWithAuthentication,
+		method,
+		qs: query,
+		uri: uri || `${credentials.url}/api${resource}`,
+		body,
+		json: true,
+	};
+
+	if (Object.keys(option).length !== 0) {
+		options = Object.assign({},options, option);
+	}
+
+	if (Object.keys(body).length ===  0) {
+		delete options.body;
+	}
+
+	if (Object.keys(query).length ===  0) {
+		delete options.qs;
+	}
+	try {
+		return await this.helpers.request!(options);
+	} catch (error) {
+		if (error.error ) {
+			const errorMessage = `TheHive error response [${error.statusCode}]: ${error.error.message || error.error.type}`;
+			throw new Error(errorMessage);
+		} else throw error;
+	}
+}
+
+// Helpers functions
+export function mapResource(resource: string): string {
+	switch (resource) {
+		case 'alert':
+			return 'alert';
+		case 'case':
+			return 'case';
+		case 'observable':
+			return 'case_artifact';
+		case 'task':
+			return 'case_task';
+		case 'log':
+			return 'case_task_log';
+		default:
+			return '';
+	}
+}
+
+export function splitTags(tags: string): string[] {
+	return tags.split(',').filter(tag => tag !== ' ' && tag);
+}
+
+export function prepareOptional(optionals: IDataObject): IDataObject {
+	const response: IDataObject = {};
+	for (const key in optionals) {
+		if (optionals[key]!== undefined && optionals[key]!==null && optionals[key]!=='') {
+			if (moment(optionals[key] as string, moment.ISO_8601).isValid()) {
+				response[key] = Date.parse(optionals[key] as string);
+			} else if (key === 'artifacts') {
+				response[key] = JSON.parse(optionals[key] as string);
+			} else if (key === 'tags') {
+				response[key] = splitTags(optionals[key] as string);
+			} else {
+				response[key] = optionals[key];
+			}
+		}
+	}
+	return response;
+}
+
+export function prepareSortQuery(sort: string, body: { query: [IDataObject] }) {
+	if (sort) {
+		const field = sort.substring(1);
+		const value = sort.charAt(0) === '+' ? 'asc' : 'desc';
+		const sortOption: IDataObject = {};
+		sortOption[field] = value;
+		body.query.push(
+			{
+				'_name': 'sort',
+				'_fields': [
+					sortOption,
+				],
+			},
+		);
+	}
+}
+
+export function prepareRangeQuery(range: string, body: { 'query': Array<{}> }) {
+	if (range && range !== 'all') {
+		body['query'].push(
+			{
+				'_name': 'page',
+				'from': parseInt(range.split('-')[0], 10),
+				'to': parseInt(range.split('-')[1], 10)
+			}
+		);
+	}
+}
---
+++ packages/nodes-base/nodes/TheHive/descriptions/AlertDescription.ts
@@ -0,0 +1,839 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+import {
+	TLP,
+} from '../interfaces/AlertInterface';
+
+export const alertOperations = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		required: true,
+		typeOptions: {
+			loadOptionsMethod: 'loadAlertOptions',
+		},
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+			},
+		},
+		default: 'create',
+	},
+] as INodeProperties[];
+
+export const alertFields = [
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'alert',
+				],
+			},
+		},
+		default: false,
+		description: 'If all results should be returned or only up to a given limit.',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'alert',
+				],
+				returnAll: [
+					false,
+				],
+			},
+		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 500,
+		},
+		default: 100,
+		description: 'How many results to return.',
+	},
+	// required attributs
+	{
+		displayName: 'Alert ID',
+		name: 'id',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert'
+				],
+				operation: [
+					'promote',
+					'merge',
+					'update',
+					'executeResponder',
+					'get',
+				],
+			},
+		},
+		description: 'Title of the alert'
+	},
+	{
+		displayName: 'Case ID',
+		name: 'caseId',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'merge',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Title',
+		name: 'title',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Title of the alert',
+	},
+	{
+		displayName: 'Description',
+		name: 'description',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Description of the alert',
+	},
+	{
+		displayName: 'Severity',
+		name: 'severity',
+		type: 'options',
+		options:[
+			{
+				name: 'Low',
+				value: 1
+			},
+			{
+				name: 'Medium',
+				value: 2
+			},
+			{
+				name: 'High',
+				value: 3,
+			},
+		],
+		required: true,
+		default: 2,
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Severity of the alert. Default=Medium',
+	},
+	{
+		displayName: 'Date',
+		name: 'date',
+		type: 'dateTime',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Date and time when the alert was raised default=now'
+	},
+	{
+		displayName: 'Tags',
+		name: 'tags',
+		type: 'string',
+		required: true,
+		default: '',
+		placeholder:'tag,tag2,tag3...',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Case Tags'
+	},
+	{
+		displayName: 'TLP',
+		name: 'tlp',
+		type: 'options',
+		required: true,
+		default: 2,
+		options: [
+			{
+				name:'White',
+				value:TLP.white,
+			},
+			{
+				name:'Green',
+				value:TLP.green,
+			},
+			{
+				name:'Amber',
+				value:TLP.amber,
+			},{
+				name:'Red',
+				value:TLP.red,
+			}
+		],
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Traffict Light Protocol (TLP). Default=Amber'
+	},
+	{
+		displayName: 'Status',
+		name: 'status',
+		type: 'options',
+		required: true,
+		options:[
+			{
+				name: 'New',
+				value: 'New',
+			},
+			{
+				name: 'Updated',
+				value: 'Updated',
+			},
+			{
+				name: 'Ignored',
+				value: 'Ignored'
+			},
+			{
+				name: 'Imported',
+				value: 'Imported',
+			},
+		],
+		default: 'New',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Status of the alert',
+	},
+	{
+		displayName: 'Type',
+		name: 'type',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Type of the alert'
+	},
+	{
+		displayName: 'Source',
+		name: 'source',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Source of the alert'
+	},
+	{
+		displayName: 'SourceRef',
+		name: 'sourceRef',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'Source reference of the alert'
+	},
+	{
+		displayName: 'Follow',
+		name: 'follow',
+		type: 'boolean',
+		required: true,
+		default: true,
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		description: 'if true, the alert becomes active when updated default=true',
+	},
+	{
+		displayName: 'Artifacts',
+		name: 'artifactUi',
+		type: 'fixedCollection',
+		placeholder: 'Add Artifact',
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+		typeOptions: {
+			multipleValues: true,
+		},
+		options: [
+			{
+				displayName: 'Artifact',
+				name: 'artifactValues',
+				values: [
+					{
+						displayName: 'Data Type',
+						name: 'dataType',
+						type: 'options',
+						default: '',
+						options: [
+							{
+								name: 'IP',
+								value: 'ip',
+							},
+							{
+								name: 'Domain',
+								value: 'domain',
+							},
+							{
+								name: 'File',
+								value: 'file',
+							},
+						],
+						description: '',
+					},
+					{
+						displayName: 'Data',
+						name: 'data',
+						type: 'string',
+						displayOptions: {
+							hide: {
+								dataType: [
+									'file',
+								],
+							},
+						},
+						default: '',
+						description: '',
+					},
+					{
+						displayName: 'Binary Property',
+						name: 'binaryProperty',
+						type: 'string',
+						displayOptions: {
+							show: {
+								dataType: [
+									'file',
+								],
+							},
+						},
+						default: 'data',
+						description: '',
+					},
+					{
+						displayName: 'Message',
+						name: 'message',
+						type: 'string',
+						default: '',
+						description: '',
+					},
+					{
+						displayName: 'Case Tags',
+						name: 'tags',
+						type: 'string',
+						default: '',
+						description: '',
+					},
+				],
+			},
+		],
+		description: 'Artifact attributes'
+	},
+	// required for responder execution
+	{
+		displayName: 'Responder ID',
+		name: 'responder',
+		type: 'options',
+		required: true,
+		default: '',
+		typeOptions: {
+			loadOptionsDependsOn: [
+				'id',
+			],
+			loadOptionsMethod: 'loadResponders',
+		},
+		displayOptions:{
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'executeResponder',
+				],
+			},
+			hide: {
+				id: [
+					'',
+				],
+			},
+		},
+	},
+	// optional attributs (Create, Promote operations)
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		placeholder: 'Add Field',
+		type: 'collection',
+		required: false,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'create',
+					'promote',
+				],
+			},
+		},
+		options:[
+			{
+				displayName: 'Case Template',
+				name: 'caseTemplate',
+				type:'string',
+				default: '',
+				description: `Case template to use when a case is created from this alert`,
+			},
+		],
+	},
+	// optional attributs (Update operation)
+	{
+		displayName: 'Update Fields',
+		name: 'updateFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert',
+				],
+				operation: [
+					'update',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Artifacts',
+				name: 'artifactUi',
+				type: 'fixedCollection',
+				placeholder: 'Add Artifact',
+				default: '',
+				typeOptions: {
+					multipleValues: true,
+				},
+				options: [
+					{
+						displayName: 'Artifact',
+						name: 'artifactValues',
+						values: [
+							{
+								displayName: 'Data Type',
+								name: 'dataType',
+								type: 'options',
+								default: '',
+								options: [
+									{
+										name: 'IP',
+										value: 'ip',
+									},
+									{
+										name: 'Domain',
+										value: 'domain',
+									},
+									{
+										name: 'File',
+										value: 'file',
+									},
+								],
+								description: '',
+							},
+							{
+								displayName: 'Data',
+								name: 'data',
+								type: 'string',
+								displayOptions: {
+									hide: {
+										dataType: [
+											'file',
+										],
+									},
+								},
+								default: '',
+								description: '',
+							},
+							{
+								displayName: 'Binary Property',
+								name: 'binaryProperty',
+								type: 'string',
+								displayOptions: {
+									show: {
+										dataType: [
+											'file',
+										],
+									},
+								},
+								default: 'data',
+								description: '',
+							},
+							{
+								displayName: 'Message',
+								name: 'message',
+								type: 'string',
+								default: '',
+								description: '',
+							},
+							{
+								displayName: 'Case Tags',
+								name: 'tags',
+								type: 'string',
+								default: '',
+								description: '',
+							},
+						],
+					},
+				],
+			},
+			{
+				displayName: 'Case Template',
+				name: 'caseTemplate',
+				type: 'string',
+				required: false,
+				default: '',
+				description: `Case template to use when a case is created from this alert`,
+			},
+			{
+				displayName: 'Description',
+				name: 'description',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'Description of the alert',
+			},
+			{
+				displayName: 'Follow',
+				name: 'follow',
+				type: 'boolean',
+				default: true,
+				description: 'if true, the alert becomes active when updated default=true',
+			},
+			{
+				displayName: 'Severity',
+				name: ' severity',
+				type: 'options',
+				options:[
+					{
+						name: 'Low',
+						value: 1,
+					},
+					{
+						name: 'Medium',
+						value: 2,
+					},
+					{
+						name: 'High',
+						value: 3,
+					},
+				],
+				default: 2,
+				description: 'Severity of the alert. Default=Medium',
+			},
+			{
+				displayName: 'Status',
+				name: 'status',
+				type: 'options',
+				options:[
+					{
+						name: 'New',
+						value: 'New',
+					},
+					{
+						name:'Updated',
+						value:'Updated',
+					},
+					{
+						name: 'Ignored',
+						value:'Ignored',
+					},
+					{
+						name:'Imported',
+						value:'Imported',
+					},
+				],
+				default: 'New',
+			},
+			{
+				displayName: 'Case Tags',
+				name: 'tags',
+				type: 'string',
+				default: '',
+				placeholder:'tag,tag2,tag3...',
+			},
+			{
+				displayName: 'Title',
+				name: 'title',
+				type: 'string',
+				required: false,
+				default: '',
+				description: 'Title of the alert'
+			},
+			{
+				displayName: 'TLP',
+				name: 'tlp',
+				type: 'options',
+				required: false,
+				default: 2,
+				options: [
+					{
+						name: 'White',
+						value: TLP.white,
+					},
+					{
+						name: 'Green',
+						value: TLP.green,
+					},
+					{
+						name: 'Amber',
+						value: TLP.amber,
+					},
+					{
+						name: 'Red',
+						value: TLP.red,
+					},
+				],
+				description: 'Traffict Light Protocol (TLP). Default=Amber'
+			},
+		],
+	},
+	//Query attributs (Search operation)
+	{
+		displayName: 'Options',
+		name: 'options',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'alert',
+				],
+			},
+		},
+		type: 'collection',
+		placeholder: 'Add Option',
+		default: {},
+		options: [
+			{
+				displayName: 'Sort',
+				name: 'sort',
+				type: 'string',
+				placeholder: '±Attribut, exp +status',
+				default: '',
+			},
+		],
+	},
+	{
+		displayName: 'Filters',
+		name: 'filters',
+		placeholder: 'Add Filter',
+		default: {},
+		type: 'collection',
+		displayOptions: {
+			show: {
+				resource: [
+					'alert'
+				],
+				operation: [
+					'getAll',
+					'count',
+				],
+			},
+		},
+		options:[
+			{
+				displayName: 'Description',
+				name: 'description',
+				type: 'string',
+				default: '',
+				description: 'Description of the alert',
+			},
+			{
+				displayName: 'Follow',
+				name: 'follow',
+				type: 'boolean',
+				default: false,
+				description: 'if true, the alert becomes active when updated default=true',
+			},
+			{
+				displayName: 'Severity',
+				name: 'severity',
+				type: 'options',
+				options: [
+					{
+						name: 'Low',
+						value: 1
+					},
+					{
+						name: 'Medium',
+						value: 2
+					},
+					{
+						name: 'High',
+						value: 3
+					},
+				],
+				default: 2,
+				description: 'Severity of the alert. Default=Medium',
+			},
+			{
+				displayName: 'Tags',
+				name: 'tags',
+				type: 'string',
+				default: '',
+				placeholder: 'tag,tag2,tag3...',
+			},
+			{
+				displayName: 'Title',
+				name: 'title',
+				type: 'string',
+				default: '',
+			},
+			{
+				displayName: 'TLP',
+				name: 'tlp',
+				type: 'options',
+				default: 2,
+				options: [
+					{
+						name:'White',
+						value:TLP.white,
+					},
+					{
+						name:'Green',
+						value:TLP.green,
+					},
+					{
+						name:'Amber',
+						value:TLP.amber,
+					},
+					{
+						name:'Red',
+						value:TLP.red,
+					}
+				],
+				description: 'Traffict Light Protocol (TLP). Default=Amber'
+			},
+		],
+	}
+]  as INodeProperties[];
