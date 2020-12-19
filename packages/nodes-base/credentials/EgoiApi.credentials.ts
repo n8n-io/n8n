4db9cca92b89d621f679d61f9e9b69c075e03338
Ricardo Espinoza <ricardo@n8n.io>
2020-12-19 08:08:31 -0500

    :sparkles: Add Egoi node (#1227)
    * Add E-goi Node
    
    * :zap: Improvements to #1224
    
    * :zap: Small improvements
    
    * :zap: Minor improvements to Egoi-Node
    
    * :zap: Add 'simple' option to get and getAll operations.
    
    * :zap: Minor improvements to Egoi-Node
    
    Co-authored-by: fbobiano <fbobiano@e-goi.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/credentials/EgoiApi.credentials.ts
@@ -0,0 +1,21 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+
+export class EgoiApi implements ICredentialType {
+	name = 'egoiApi';
+	displayName = 'e-goi API';
+	properties = [
+		// The credentials to get from user and save encrypted.
+		// Properties can be defined exactly in the same way
+		// as node properties.
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string' as NodePropertyTypes,
+			default: '',
+		},
+	];
+}
