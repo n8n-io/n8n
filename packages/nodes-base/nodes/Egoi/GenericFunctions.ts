48830eda7734c9026fb01f819cca4661370a1ed0
Ricardo Espinoza <ricardo@n8n.io>
2020-12-19 08:08:31 -0500

    :sparkles: Add Egoi node (#1227)
    * Add E-goi Node
    
    * :zap: Improvements to #1224
    
    * :zap: Small improvements
    
    * :zap: Minor improvements to Egoi-Node
    
    * :zap: Add 'simple' option to get and getAll operations.
    
    * :zap: Minor improvements to Egoi-Node
    
    Co-authored-by: fbobiano <fbobiano@e-goi.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/nodes/Egoi/GenericFunctions.ts
@@ -0,0 +1,126 @@
+import {
+	OptionsWithUrl,
+} from 'request';
+
+import {
+	IExecuteFunctions,
+	IExecuteSingleFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+} from 'n8n-workflow';
+
+interface IContact {
+	tags: [],
+	base: IDataObject,
+	extra: IDataObject[],
+}
+
+const fieldCache: {
+	[key: string]: IDataObject[];
+} = {};
+
+
+export async function getFields(this: IExecuteFunctions, listId: string) {
+	if (fieldCache[listId]) {
+		return fieldCache[listId];
+	}
+	fieldCache[listId] = await egoiApiRequest.call(this, 'GET', `/lists/${listId}/fields`);
+	return fieldCache[listId]
+}
+
+
+export async function egoiApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any = {}, qs: IDataObject = {}, headers?: object): Promise<any> { // tslint:disable-line:no-any
+
+	const credentials = this.getCredentials('egoiApi') as IDataObject;
+
+	const options: OptionsWithUrl = {
+		headers: {
+			'accept': 'application/json',
+			'Apikey': `${credentials.apiKey}`,
+		},
+		method,
+		qs,
+		body,
+		url: `https://api.egoiapp.com${endpoint}`,
+		json: true,
+	};
+
+	if (Object.keys(body).length === 0) {
+		delete options.body;
+	}
+
+	try {
+
+		return await this.helpers.request!(options);
+
+	} catch (error) {
+		let errorMessage;
+
+		if (error.response && error.response.body) {
+
+			if (Array.isArray(error.response.body.errors)) {
+				const errors = error.response.body.errors;
+				errorMessage = errors.map((e: IDataObject) => e.detail);
+			} else {
+				errorMessage = error.response.body.detail;
+			}
+
+			throw new Error(`e-goi Error response [${error.statusCode}]: ${errorMessage}`);
+		}
+		throw error;
+	}
+}
+
+export async function egoiApiRequestAllItems(this: IExecuteFunctions | ILoadOptionsFunctions, propertyName: string, method: string, endpoint: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+
+	query.offset = 0;
+	query.count = 500;
+
+	do {
+		responseData = await egoiApiRequest.call(this, method, endpoint, body, query);
+		returnData.push.apply(returnData, responseData[propertyName]);
+		query.offset += query.count;
+	} while (
+		responseData[propertyName] && responseData[propertyName].length !== 0
+	);
+
+	return returnData;
+}
+
+
+export async function simplify(this: IExecuteFunctions, contacts: IContact[], listId: string) {
+	let fields = await getFields.call(this, listId);
+
+	fields = fields.filter((element: IDataObject) => element.type === 'extra');
+	const fieldsKeyValue: IDataObject = {};
+	for (const field of fields) {
+		fieldsKeyValue[field.field_id as string] = field.name;
+	}
+
+	const data: IDataObject[] = [];
+
+	for (const contact of contacts) {
+		const extras = contact.extra.reduce(
+			(acumulator: IDataObject, currentValue: IDataObject): any => {
+				const key = fieldsKeyValue[currentValue.field_id as string] as string
+				return { [key]: currentValue.value, ...acumulator }
+			},
+			{}
+		);
+		data.push({
+			...contact.base,
+			...extras,
+			tags: contact.tags,
+		})
+	}
+
+	return data;
+}
