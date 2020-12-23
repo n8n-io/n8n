c922163622831d4418c308764db7ecebfc1583a7
Jefiozie <jefiozie.bosch@gmail.com>
2020-12-23 08:51:07 +0100

    feat: add balance endpoint to messagebird

--- packages/nodes-base/nodes/MessageBird/GenericFunctions.ts
+++ packages/nodes-base/nodes/MessageBird/GenericFunctions.ts
@@ -43,8 +43,10 @@ export async function messageBirdApiRequest(
 		uri: `https://rest.messagebird.com${resource}`,
 		json: true,
 	};
-
 	try {
+		if (Object.keys(body).length === 0) {
+			delete options.body;
+		}
 		return await this.helpers.request(options);
 	} catch (error) {
 		if (error.statusCode === 401) {
