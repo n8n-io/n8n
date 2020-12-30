2ba4e91d5324fddaec4fae241f515ee138dd58ab
lublak <lublak.de@gmail.com>
2020-12-30 15:37:55 +0100

    :sparkles: n8n start trigger node

---
+++ packages/nodes-base/nodes/N8nStartTrigger.node.ts
@@ -0,0 +1,38 @@
+import { ITriggerFunctions } from 'n8n-core';
+import {
+	INodeType,
+	INodeTypeDescription,
+	ITriggerResponse,
+} from 'n8n-workflow';
+
+export class N8nStartTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'n8n Start Trigger',
+		name: 'n8nStartTrigger',
+		icon: 'fa:play-circle',
+		group: ['trigger'],
+		version: 1,
+		description: 'Starts the workflow when n8n starts',
+		defaults: {
+			name: 'n8n Start Trigger',
+			color: '#00e000',
+		},
+		inputs: [],
+		outputs: ['main'],
+		properties: []
+	};
+
+
+	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
+		const self = this;
+		async function manualTriggerFunction() {
+			self.emit([self.helpers.returnJsonArray([{}])]);
+		}
+		
+		manualTriggerFunction();
+
+		return {
+			manualTriggerFunction
+		};
+	}
+}
