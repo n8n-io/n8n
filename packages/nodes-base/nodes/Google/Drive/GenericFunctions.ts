098759ae1a7b64a63d2fe1892f5c8fa854e11c27
ricardo <ricardoespinoza105@gmail.com>
2021-01-10 19:36:42 -0500

    :zap: Add drive resource

--- packages/nodes-base/nodes/Google/Drive/GenericFunctions.ts
+++ packages/nodes-base/nodes/Google/Drive/GenericFunctions.ts
@@ -80,6 +80,7 @@ export async function googleApiRequestAllItems(this: IExecuteFunctions | ILoadOp
 
 	let responseData;
 	query.maxResults = 100;
+	query.pageSize = 100;
 
 	do {
 		responseData = await googleApiRequest.call(this, method, endpoint, body, query);
