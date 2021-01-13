d91b0f52cb6de8451f0686dfd3a69dee00dec158
Ben Hesseldieck <b.hesseldieck@gmail.com>
2021-01-13 20:54:59 +0100

    :construction: add webhookId to webhook entity, :wrench: refactor migrations

--- packages/cli/src/databases/sqlite/migrations/index.ts
+++ packages/cli/src/databases/sqlite/migrations/index.ts
@@ -1,3 +1,11 @@
-export * from './1588102412422-InitialMigration';
-export * from './1592445003908-WebhookModel';
-export * from './1594825041918-CreateIndexStoppedAt';
+import InitialMigration1588102412422 from './1588102412422-InitialMigration';
+import WebhookModel1592445003908 from './1592445003908-WebhookModel';
+import CreateIndexStoppedAt1594825041918 from './1594825041918-CreateIndexStoppedAt';
+import AddWebhookId1610570498276 from './1610570498276-AddWebhookId';
+
+export default [
+	InitialMigration1588102412422,
+	WebhookModel1592445003908,
+	CreateIndexStoppedAt1594825041918,
+	AddWebhookId1610570498276,
+]
