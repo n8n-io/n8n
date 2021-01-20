638aaa199f730434d5c59ea301d84a7cf4706544
Jan Oberhauser <jan.oberhauser@gmail.com>
2021-01-20 23:50:15 +0100

    :zap: Minor changes to CLI export/import

--- packages/cli/commands/export/credentials.ts
+++ packages/cli/commands/export/credentials.ts
@@ -9,23 +9,20 @@ import {
 import {
 	Db,
 	GenericHelpers,
-} from "../../src";
-
-import { stringify } from 'flatted';
+} from '../../src';
 
 import * as fs from 'fs';
 import * as path from 'path';
-import { getCredentialsDataWithParents } from '../../src/WorkflowHelpers';
 
 export class ExportCredentialsCommand extends Command {
 	static description = 'Export credentials';
-	
+
 	static examples = [
 		`$ n8n export:credentials --all`,
 		`$ n8n export:credentials --id=5 --output=file.json`,
 		`$ n8n export:credentials --all --output=backups/latest/`,
 	];
-	
+
 	static flags = {
 		help: flags.help({ char: 'h' }),
 		all: flags.boolean({
@@ -35,34 +32,36 @@ export class ExportCredentialsCommand extends Command {
 			description: 'The ID of the credential to export',
 		}),
 		output: flags.string({
-			description: 'Output file name or directory if using multiple files',
+			description: 'Output file name or directory if using separate files',
 		}),
-		multiple: flags.boolean({
+		pretty: flags.boolean({
+			description: 'Format the output in an easier to read fashion',
+		}),
+		separate: flags.boolean({
 			description: 'Exports one file per credential (useful for versioning). Must inform a directory via --output.',
 		}),
 	};
-	
+
 	async run() {
 		const { flags } = this.parse(ExportCredentialsCommand);
-		
+
 		if (!flags.all && !flags.id) {
 			GenericHelpers.logOutput(`Either option "--all" or "--id" have to be set!`);
 			return;
 		}
-		
+
 		if (flags.all && flags.id) {
 			GenericHelpers.logOutput(`You should either use "--all" or "--id" but never both!`);
 			return;
 		}
-		
-		
-		if (flags.multiple) {
+
+		if (flags.separate) {
 			try {
-				if(!flags.output) {
-					GenericHelpers.logOutput(`You must inform an output directory via --output when using --multiple`);
+				if (!flags.output) {
+					GenericHelpers.logOutput(`You must inform an output directory via --output when using --separate`);
 					return;
 				}
-				
+
 				if (fs.existsSync(flags.output)) {
 					if (!fs.lstatSync(flags.output).isDirectory()) {
 						GenericHelpers.logOutput(`The paramenter --output must be a directory`);
@@ -86,49 +85,41 @@ export class ExportCredentialsCommand extends Command {
 				}
 			}
 		}
-		
+
 		try {
 			await Db.init();
-			
+
 			const findQuery: IDataObject = {};
 			if (flags.id) {
 				findQuery.id = flags.id;
 			}
-			
+
 			const credentials = await Db.collections.Credentials!.find(findQuery);
 
 			if (credentials.length === 0) {
 				throw new Error('No credentials found with specified filters.');
 			}
-			
-			if (flags.multiple) {
+
+			if (flags.separate) {
 				let fileContents: string, i: number;
 				for (i = 0; i < credentials.length; i++) {
-					fileContents = stringify(credentials[i]);
+					fileContents = JSON.stringify(credentials[i], null, flags.pretty ? 2 : undefined);
 					const filename = (flags.output!.endsWith(path.sep) ? flags.output! : flags.output + path.sep) + credentials[i].id + ".json";
 					fs.writeFileSync(filename, fileContents);
 				}
 				console.log('Successfully exported', i, 'credentials.');
 			} else {
-				const fileContents = stringify(credentials);
+				const fileContents = JSON.stringify(credentials, null, flags.pretty ? 2 : undefined);
 				if (flags.output) {
 					fs.writeFileSync(flags.output!, fileContents);
 					console.log('Successfully exported', credentials.length, 'credentials.');
 				} else {
 					console.log(fileContents);
 				}
-				
 			}
-			
-			
-		} catch (e) {
-			console.error('\nGOT ERROR');
-			console.log('====================================');
-			console.error(e.message);
-			console.error(e.stack);
+		} catch (error) {
+			this.error(error.message);
 			this.exit(1);
 		}
-		
-		this.exit();
 	}
 }
--- packages/cli/commands/import/credentials.ts
+++ packages/cli/commands/import/credentials.ts
@@ -5,11 +5,7 @@ import {
 import {
 	Db,
 	GenericHelpers,
-} from "../../src";
-
-import { 
-	parse,
-} from 'flatted';
+} from '../../src';
 
 import * as fs from 'fs';
 import * as glob from 'glob-promise';
@@ -17,31 +13,31 @@ import * as path from 'path';
 
 export class ImportCredentialsCommand extends Command {
 	static description = 'Import credentials';
-	
+
 	static examples = [
 		`$ n8n import:credentials --input=file.json`,
-		`$ n8n import:credentials --multiple --input=backups/latest/`,
+		`$ n8n import:credentials --separate --input=backups/latest/`,
 	];
-	
+
 	static flags = {
 		help: flags.help({ char: 'h' }),
 		input: flags.string({
-			description: 'Input file name or directory if --multiple is used',
+			description: 'Input file name or directory if --separate is used',
 		}),
-		multiple: flags.boolean({
+		separate: flags.boolean({
 			description: 'Imports *.json files from directory provided by --input',
 		}),
 	};
-	
+
 	async run() {
 		const { flags } = this.parse(ImportCredentialsCommand);
-		
+
 		if (!flags.input) {
 			GenericHelpers.logOutput(`An input file or directory with --input must be provided`);
 			return;
 		}
 
-		if (flags.multiple) {
+		if (flags.separate) {
 			if (fs.existsSync(flags.input)) {
 				if (!fs.lstatSync(flags.input).isDirectory()) {
 					GenericHelpers.logOutput(`The paramenter --input must be a directory`);
@@ -49,36 +45,31 @@ export class ImportCredentialsCommand extends Command {
 				}
 			}
 		}
-		
+
 		try {
 			await Db.init();
 			let i;
-			if (flags.multiple) {
+			if (flags.separate) {
 				const files = await glob((flags.input.endsWith(path.sep) ? flags.input : flags.input + path.sep) + '*.json');
 				for (i = 0; i < files.length; i++) {
-					const credential = parse(fs.readFileSync(files[i], {encoding: 'utf8'}));
+					const credential = JSON.parse(fs.readFileSync(files[i], { encoding: 'utf8' }));
 					await Db.collections.Credentials!.save(credential);
 				}
 			} else {
-				const fileContents = parse(fs.readFileSync(flags.input, {encoding: 'utf8'}));
+				const fileContents = JSON.parse(fs.readFileSync(flags.input, { encoding: 'utf8' }));
 
 				if (!Array.isArray(fileContents)) {
 					throw new Error(`File does not seem to contain credentials.`);
 				}
-				
+
 				for (i = 0; i < fileContents.length; i++) {
 					await Db.collections.Credentials!.save(fileContents[i]);
 				}
 			}
 			console.log('Successfully imported', i, 'credentials.');
-		} catch (e) {
-			console.error('\nGOT ERROR');
-			console.log('====================================');
-			console.error(e.message);
-			console.error(e.stack);
+		} catch (error) {
+			this.error(error.message);
 			this.exit(1);
 		}
-		
-		this.exit();
 	}
 }
