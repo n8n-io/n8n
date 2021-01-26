7329f83b0a8664085cbc05109ed241ce9799471e
Ben Hesseldieck <b.hesseldieck@gmail.com>
2021-01-26 13:59:53 +0100

    :art: reset req.params

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -1693,7 +1693,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.activeWorkflowRunner.executeWebhook('HEAD', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
@@ -1735,7 +1735,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.activeWorkflowRunner.executeWebhook('GET', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
@@ -1757,7 +1757,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.activeWorkflowRunner.executeWebhook('POST', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
@@ -1779,7 +1779,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.testWebhooks.callTestWebhook('HEAD', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
@@ -1821,7 +1821,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.testWebhooks.callTestWebhook('GET', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
@@ -1843,7 +1843,7 @@ class App {
 
 			let response;
 			try {
-				delete req.params[0];
+				req.params = {};
 				response = await this.testWebhooks.callTestWebhook('POST', requestUrl, req, res);
 			} catch (error) {
 				ResponseHelper.sendErrorResponse(res, error);
