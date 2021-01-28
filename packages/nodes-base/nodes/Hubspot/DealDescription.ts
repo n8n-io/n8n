5900ee3415fea7ae13474afa89a46bea95a001fa
Tysmith17 <tylersmith1791@gmail.com>
2021-01-28 10:38:57 -0700

    add deal description to hubspot

--- packages/nodes-base/nodes/Hubspot/DealDescription.ts
+++ packages/nodes-base/nodes/Hubspot/DealDescription.ts
@@ -171,6 +171,12 @@ export const dealFields = [
 				],
 			},
 			{
+				displayName: 'Deal Description',
+				name: 'description',
+				type: 'string',
+				default: '',
+			},
+			{
 				displayName: 'Deal Name',
 				name: 'dealName',
 				type: 'string',
