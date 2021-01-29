ce7fb5f78fbbda24c0a65858b90b510f8df4e471
Iván Ovejero <ivov.src@gmail.com>
2021-01-29 12:22:30 -0300

    Remove returnAll and limit from subreddit:get

--- packages/nodes-base/nodes/Reddit/SubredditDescription.ts
+++ packages/nodes-base/nodes/Reddit/SubredditDescription.ts
@@ -83,47 +83,6 @@ export const subredditFields = [
 			},
 		},
 	},
-	{
-		displayName: 'Return All',
-		name: 'returnAll',
-		type: 'boolean',
-		default: false,
-		description: 'Return all results.',
-		displayOptions: {
-			show: {
-				resource: [
-					'subreddit',
-				],
-				operation: [
-					'get',
-				],
-			},
-		},
-	},
-	{
-		displayName: 'Limit',
-		name: 'limit',
-		type: 'number',
-		default: 5,
-		description: 'The number of results to return.',
-		typeOptions: {
-			minValue: 1,
-			maxValue: 100,
-		},
-		displayOptions: {
-			show: {
-				resource: [
-					'subreddit',
-				],
-				operation: [
-					'get',
-				],
-				returnAll: [
-					false,
-				],
-			},
-		},
-	},
 	// ----------------------------------
 	//        subreddit: getAll
 	// ----------------------------------
