0d7c859b25af75ef0ac52900f95812d452559afc
Iván Ovejero <ivov.src@gmail.com>
2021-02-04 05:37:03 -0300

    :sparkles: Add Reddit Node (#1345)
    * Set up initial scaffolding and auth
    
    * Add grant type to credentials
    
    * Add Account operations and OAuth2 request
    
    * Add post submission functionality
    
    * Refactor resources into resource descriptions
    
    * Refactor request for no auth URL
    
    * Refactor submission description for consistency
    
    * Add listing resource
    
    * Refactor My Account resource into details
    
    * Add request for all items
    
    * Add listings for specific subreddits
    
    * Fix minor linting details
    
    * Add subreddit resource
    
    * Add All-Reddit and Subreddit resource descriptions
    
    * Adjust display options for credentials
    
    * Add subreddit search functionality
    
    * Clean up auth parameter
    
    * Add user resource with GET endpoint
    
    * Add user description
    
    * Add submission search and commenting functionality
    
    * Clean up logging and comments
    
    * Fix minor details
    
    * Fix casing in properties
    
    * Add dividers to execute() method
    
    * Refactor per feedback
    
    * Remove unused description
    
    * Add punctuation to property descriptions
    
    * Fix resources indentation
    
    * Add resource dividers
    
    * Remove deprecated sidebar option
    
    * Make subreddit:get responses consistent
    
    * Remove returnAll and limit from subreddit:get
    
    * Flatten user:get response for about
    
    * Rename comment target property
    
    * Remove best property from post:getAll
    
    * Enrich subreddit search by keyword operation
    
    * Remove unneeded scopes
    
    * Add endpoint documentation
    
    * Add scaffolding for post comment
    
    * Add all operations for postComment resource
    
    * Add all operations for post resource
    
    * Refactor subreddit:getAll
    
    * Fix postComment:getAll
    
    * Flatten responses for profile:get
    
    * :zap: Improvements
    
    * Fix response traversal for postComment:add
    
    * Flatten response for postComment:reply
    
    * Fix subreddit:getAll with keyword search
    
    * Fix pagination to enforce limit
    
    * Wrap unauthenticated API call in try-catch block
    
    * Add 404 error for empty array responses
    
    * Revert "Fix pagination to enforce limit"
    
    This reverts commit 72548d952378a2f899517522b5ba4950a940c6e4.
    
    * Turn user:get (gilded) into listing
    
    * :zap: Small improvement
    
    * :zap: Improve Reddit-Node
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/nodes/Reddit/ProfileDescription.ts
@@ -0,0 +1,80 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const profileOperations = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'profile',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Get',
+				value: 'get',
+			},
+		],
+		default: 'get',
+		description: 'Operation to perform',
+	},
+] as INodeProperties[];
+
+
+export const profileFields = [
+	{
+		displayName: 'Details',
+		name: 'details',
+		type: 'options',
+		required: true,
+		default: 'identity',
+		description: 'Details of my account to retrieve.',
+		options: [
+			{
+				name: 'Identity',
+				value: 'identity',
+				description: 'Return the identity of the logged-in user',
+			},
+			{
+				name: 'Blocked Users',
+				value: 'blockedUsers',
+				description: 'Return the blocked users of the logged-in user',
+			},
+			{
+				name: 'Friends',
+				value: 'friends',
+				description: 'Return the friends of the logged-in user',
+			},
+			{
+				name: 'Karma',
+				value: 'karma',
+				description: 'Return the subreddit karma for the logged-in user',
+			},
+			{
+				name: 'Preferences',
+				value: 'prefs',
+				description: 'Return the settings preferences of the logged-in user',
+			},
+			{
+				name: 'Trophies',
+				value: 'trophies',
+				description: 'Return the trophies of the logged-in user',
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: [
+					'profile',
+				],
+				operation: [
+					'get',
+				],
+			},
+		},
+	},
+] as INodeProperties[];
