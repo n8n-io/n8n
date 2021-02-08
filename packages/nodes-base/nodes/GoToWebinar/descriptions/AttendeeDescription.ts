959a2f761b19ca68d8130e80b8bbc4a8a6859eea
Iván Ovejero <ivov.src@gmail.com>
2021-02-08 15:59:52 -0300

    Add attendee description

---
+++ packages/nodes-base/nodes/GoToWebinar/descriptions/AttendeeDescription.ts
@@ -0,0 +1,41 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const attendeeOperations = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		default: 'get',
+		description: 'Operation to perform',
+		options: [
+			{
+				name: 'Get',
+				value: 'get',
+			},
+			{
+				name: 'Get All',
+				value: 'getAll',
+			},
+			{
+				name: 'Get Details',
+				value: 'getAll',
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: [
+					'attendee',
+				],
+			},
+		},
+	},
+] as INodeProperties[];
+
+export const attendeeFields = [
+	// ----------------------------------
+	//         attendee: create
+	// ----------------------------------
+
+] as INodeProperties[];
