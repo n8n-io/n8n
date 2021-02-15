0a247b176609ed56e0e2284d79053230011d36e3
Iván Ovejero <ivov.src@gmail.com>
2021-02-15 17:35:05 -0300

    Apply cosmetic changes to trigger node

--- packages/nodes-base/nodes/Emelia/EmeliaTrigger.node.ts
+++ packages/nodes-base/nodes/Emelia/EmeliaTrigger.node.ts
@@ -19,175 +19,143 @@ interface Campaign {
 }
 
 export class EmeliaTrigger implements INodeType {
-	description: INodeTypeDescription;
-	methods: {
-		loadOptions: {
-			getCampaigns(
-				this: ILoadOptionsFunctions,
-			): Promise<INodePropertyOptions[]>
-		}
-	};
-	// @ts-ignore
-	webhookMethods: {
-		default: {
-			create(this: IHookFunctions): Promise<boolean>
-			delete(this: IHookFunctions): Promise<boolean>
-		}
-	};
-
-	constructor() {
-		this.description = {
-			displayName: 'Emelia Trigger',
-			name: 'emeliaTrigger',
-			icon: 'file:emelia.png',
-			group: ['trigger'],
-			version: 1,
-			description: 'Handle Emelia campaign activity events via webhooks',
-			defaults: {
-				name: 'Emelia Trigger',
-				color: '#e18063',
-			},
-			inputs: [],
-			outputs: ['main'],
-			credentials: [
-				{
-					name: 'emeliaApi',
-					required: true,
-				},
-			],
-			webhooks: [
-				{
-					name: 'default',
-					httpMethod: 'POST',
-					responseMode: 'onReceived',
-					path: 'webhook',
-				},
-			],
-			properties: [
-				{
-					displayName: 'Campaign',
-					name: 'campaignId',
-					type: 'options',
-					typeOptions: {
-						loadOptionsMethod: 'getCampaigns',
-					},
-					required: true,
-					default: '',
-				},
-				{
-					displayName: 'Events',
-					name: 'events',
-					type: 'multiOptions',
-					default: ['REPLIED'],
-					options: [
-						{
-							value: 'REPLIED',
-							name: 'Email replied',
-						},
-						{
-							value: 'OPENED',
-							name: 'Email opened',
-						},
-						{
-							value: 'CLICKED',
-							name: 'Link clicked',
-						},
-						{
-							value: 'SENT',
-							name: 'Email sent',
-						},
-						{
-							name: 'Email bounced',
-							value: 'BOUNCED',
-						},
-						{
-							value: 'UNSUBSCRIBED',
-							name: 'Unsubscribed contact',
-						},
-					],
-				},
-			],
+	description: INodeTypeDescription = {
+		displayName: 'Emelia Trigger',
+		name: 'emeliaTrigger',
+		icon: 'file:emelia.png',
+		group: ['trigger'],
+		version: 1,
+		description: 'Handle Emelia campaign activity events via webhooks',
+		defaults: {
+			name: 'Emelia Trigger',
+			color: '#e18063',
 		},
-
-		this.methods = {
-			loadOptions: {
-				async getCampaigns() {
-					const responseData = await emeliaGrapqlRequest.call(this, {
-						query:
-							'query GetCampaigns {\ncampaigns {\n_id\nname\n}\n}',
-						operationName: 'GetCampaigns',
-						variables: '{}',
-					});
-
-					return responseData.data.campaigns.map(
-						(campaign: Campaign) => ({
-							name: campaign.name,
-							value: campaign._id,
-						}),
-					);
-				},
+		inputs: [],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'emeliaApi',
+				required: true,
 			},
-		};
+		],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			},
+		],
+		properties: [
+			{
+				displayName: 'Campaign',
+				name: 'campaignId',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getCampaigns',
+				},
+				required: true,
+				default: '',
+			},
+			{
+				displayName: 'Events',
+				name: 'events',
+				type: 'multiOptions',
+				default: ['REPLIED'],
+				options: [
+					{
+						value: 'REPLIED',
+						name: 'Email replied',
+					},
+					{
+						value: 'OPENED',
+						name: 'Email opened',
+					},
+					{
+						value: 'CLICKED',
+						name: 'Link clicked',
+					},
+					{
+						value: 'SENT',
+						name: 'Email sent',
+					},
+					{
+						name: 'Email bounced',
+						value: 'BOUNCED',
+					},
+					{
+						value: 'UNSUBSCRIBED',
+						name: 'Unsubscribed contact',
+					},
+				],
+			},
+		],
+	};
 
-		this.webhookMethods = {
-			default: {
-				async create(this: IHookFunctions): Promise<boolean> {
-					const webhookUrl = this.getNodeWebhookUrl(
-						'default',
-					) as string;
-					const webhookData = this.getWorkflowStaticData('node');
-					const events = this.getNodeParameter('events', [
-						'REPLIED',
-					]) as string[];
-					const campaignId = this.getNodeParameter(
-						'campaignId',
-						'',
-					) as string;
+	methods = {
+		loadOptions: {
+			async getCampaigns(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+				const responseData = await emeliaGrapqlRequest.call(this, {
+					query: `
+					query GetCampaigns {
+						campaigns {
+							_id
+							name
+						}
+					}`,
+					operationName: 'GetCampaigns',
+					variables: '{}',
+				});
+
+				return responseData.data.campaigns.map(
+					(campaign: Campaign) => ({
+						name: campaign.name,
+						value: campaign._id,
+					}),
+				);
+			},
+		},
+	};
+
+	webhookMethods = {
+		default: {
+			async create(this: IHookFunctions): Promise<boolean> {
+				const webhookUrl = this.getNodeWebhookUrl('default') as string;
+				const webhookData = this.getWorkflowStaticData('node');
+				const events = this.getNodeParameter('events', ['REPLIED']) as string[];
+
+				const campaignId = this.getNodeParameter('campaignId', '') as string;
+				const body = {
+					hookUrl: webhookUrl,
+					events,
+					campaignId,
+				};
+
+				const { webhookId } = await emeliaApiRequest.call(this, 'POST', '/webhook/webhook', body);
+				webhookData.webhookId = webhookId;
+				return true;
+			},
+
+			async delete(this: IHookFunctions): Promise<boolean> {
+				const webhookData = this.getWorkflowStaticData('node');
+				const webhookUrl = this.getNodeWebhookUrl('default') as string;
+				const campaignId = this.getNodeParameter('campaignId', '') as string;
+
+				try {
 					const body = {
 						hookUrl: webhookUrl,
-						events,
 						campaignId,
 					};
-					const { webhookId } = await emeliaApiRequest.call(
-						this,
-						'POST',
-						'/webhook/webhook',
-						body,
-					);
-					webhookData.webhookId = webhookId;
-					return true;
-				},
-				async delete(this: IHookFunctions): Promise<boolean> {
-					const webhookData = this.getWorkflowStaticData('node');
-					const webhookUrl = this.getNodeWebhookUrl(
-						'default',
-					) as string;
-					const campaignId = this.getNodeParameter(
-						'campaignId',
-						'',
-					) as string;
+					await emeliaApiRequest.call(this, 'DELETE', '/webhook/webhook', body);
+				} catch (error) {
+					return false;
+				}
 
-					try {
-						const body = {
-							hookUrl: webhookUrl,
-							campaignId,
-						};
-
-						await emeliaApiRequest.call(
-							this,
-							'DELETE',
-							'/webhook/webhook',
-							body,
-						);
-					} catch (error) {
-						return false;
-					}
-					delete webhookData.webhookId;
-					return true;
-				},
+				delete webhookData.webhookId;
+				return true;
 			},
-		};
-	}
+		},
+	};
 
 	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
 		const req = this.getRequestObject();
