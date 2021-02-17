207529efdd89ef0105053f47b26b2254af87e8dd
Ben Hesseldieck <b.hesseldieck@gmail.com>
2021-02-17 15:28:26 +0100

    :construction: setup NodeErrorView

---
+++ packages/editor-ui/src/components/Error/NodeViewError.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="error-message">ERROR: {{error.message}}</div>
+    <pre><code>{{error.stack}}</code></pre>
+		<div>ABC IVAN</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: 'NodeErrorView',
+	props: [
+		'error',
+	],
+	data () {
+		return {
+			showFullError: false,
+		};
+	},
+	methods: {
+		revealError() {
+			this.showFullError = true;
+		},
+	},
+});
+</script>
+
+<style scoped>
+.error-message {
+	color: #ff0000;
+	font-weight: bold;
+}
+</style>
--- packages/editor-ui/src/components/RunData.vue
+++ packages/editor-ui/src/components/RunData.vue
@@ -81,8 +81,7 @@
 		<div class="data-display-content">
 			<span v-if="node && workflowRunData !== null && workflowRunData.hasOwnProperty(node.name)">
 				<div v-if="workflowRunData[node.name][runIndex].error" class="error-display">
-					<div class="error-message">ERROR: {{workflowRunData[node.name][runIndex].error.message}}</div>
-					<pre><code>{{workflowRunData[node.name][runIndex].error.stack}}</code></pre>
+					<NodeErrorView :error="workflowRunData[node.name][runIndex].error" />
 				</div>
 				<span v-else>
 					<div v-if="showData === false" class="to-much-data">
@@ -222,6 +221,7 @@ import {
 } from '@/constants';
 
 import BinaryDataDisplay from '@/components/BinaryDataDisplay.vue';
+import NodeErrorView from '@/components/Error/NodeViewError.vue';
 
 import { copyPaste } from '@/components/mixins/copyPaste';
 import { genericHelpers } from '@/components/mixins/genericHelpers';
@@ -243,6 +243,7 @@ export default mixins(
 		name: 'RunData',
 		components: {
 			BinaryDataDisplay,
+			NodeErrorView,
 			VueJsonPretty,
 		},
 		data () {
@@ -732,13 +733,6 @@ export default mixins(
 
 			.text {
 				margin-bottom: 1em;
-			}
-		}
-
-		.error-display {
-			.error-message {
-				color: #ff0000;
-				font-weight: bold;
 			}
 		}
 
