1254bf429968441c6f9fe93f16ba5f881bc40b93
Iván Ovejero <ivov.src@gmail.com>
2021-02-26 10:40:29 -0300

    :zap: Add generic functions stub

---
+++ packages/nodes-base/nodes/Wise/GenericFunctions.ts
@@ -0,0 +1,92 @@
+import {
+	IExecuteFunctions,
+	IHookFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+	ILoadOptionsFunctions,
+	INodeExecutionData,
+	INodePropertyOptions,
+} from 'n8n-workflow';
+
+import {
+	OptionsWithUri,
+} from 'request';
+
+/**
+ * Make an authenticated API request to Wise.
+ */
+export async function wiseApiRequest(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
+	method: string,
+	endpoint: string,
+	qs: IDataObject,
+	body: IDataObject,
+) {
+
+	const { apiToken, environment } = this.getCredentials('wiseApi') as {
+		apiToken: string,
+		environment: 'live' | 'test',
+	};
+
+	const rootUrl = environment === 'live'
+		? 'https://api.transferwise.com/v2'
+		: 'https://api.sandbox.transferwise.tech/v2';
+
+	const options: OptionsWithUri = {
+		headers: {
+			'user-agent': 'n8n',
+			'Authorization': `Bearer ${apiToken}`,
+		},
+		method,
+		uri: `${rootUrl}${endpoint}`,
+		qs,
+		body,
+		json: true,
+	};
+
+	if (!Object.keys(body).length) {
+		delete options.body;
+	}
+
+	if (!Object.keys(qs).length) {
+		delete options.qs;
+	}
+
+	try {
+		console.log(options);
+		return await this.helpers.request!(options);
+	} catch (error) {
+
+		// TODO
+
+		throw error;
+	}
+}
+
+/**
+ * Make an authenticated API request to Wise and return all results.
+ */
+export async function wiseApiRequestAllItems(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
+	method: string,
+	endpoint: string,
+	qs: IDataObject,
+	body: IDataObject,
+	resource: string,
+) {
+	// ...
+}
+
+/**
+ * Handle a Wise listing by returning all items or up to a limit.
+ */
+export async function handleListing(
+	this: IExecuteFunctions,
+	i: number,
+	endpoint: string,
+	resource: string,
+) {
+	// ...
+}
