731308124028457171a147e5e71ef85dfd05294d
Iván Ovejero <ivov.src@gmail.com>
2021-03-01 09:59:45 -0300

    :zap: Add credentials file

---
+++ packages/nodes-base/credentials/InstagramBasicDisplayOAuth2Api.credentials.ts
@@ -0,0 +1,51 @@
+import {
+	ICredentialType,
+	NodePropertyTypes,
+} from 'n8n-workflow';
+
+
+export class InstagramBasicDisplayOAuth2Api implements ICredentialType {
+	name = 'instagramBasicDisplayOAuth2Api';
+	extends = [
+		'oAuth2Api',
+	];
+	displayName = 'Instagram Basic Display OAuth2 API';
+	properties = [
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'https://api.instagram.com/oauth/authorize',
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'https://api.instagram.com/oauth/access_token',
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'user_profile,user_media',
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'response_type=code',
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'grant_type=authorization_code',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden' as NodePropertyTypes,
+			default: 'body',
+		},
+	];
+}
