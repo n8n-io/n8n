148e48247e8ae40f485340333ee39980fc1cc346
Iván Ovejero <ivov.src@gmail.com>
2021-03-10 20:00:32 -0300

    :zap: Add generic functions stub

--- packages/nodes-base/nodes/Discord/GenericFunctions.ts
+++ packages/nodes-base/nodes/Discord/GenericFunctions.ts
@@ -12,16 +12,12 @@ import {
 	IDataObject,
 } from 'n8n-workflow';
 
-import WebSocket = require('ws');
-
 export async function discordApiRequest(
 	this: IExecuteFunctions | ILoadOptionsFunctions | IHookFunctions,
 	method: string,
 	resource: string,
 	body: IDataObject = {},
 	qs: IDataObject = {},
-	uri?: string,
-	option: IDataObject = {},
 ) {
 
 	const { oauthTokenData } = this.getCredentials('discordOAuth2Api') as {
@@ -31,25 +27,22 @@ export async function discordApiRequest(
 	const options: OptionsWithUri = {
 		headers: {
 			'Content-Type': 'application/json',
+			'user-agent': 'n8n',
 			Authorization: `Bearer ${oauthTokenData.access_token}`,
 		},
 		method,
 		body,
 		qs,
-		uri: uri || `https://discord.com/api${resource}`,
+		uri: `https://discord.com/api${resource}`,
 		json: true,
 	};
 
-	if (Object.keys(option).length) {
-		Object.assign(options, option);
+	if (!Object.keys(body).length) {
+		delete options.body;
 	}
 
 	if (!Object.keys(qs).length) {
 		delete options.qs;
-	}
-
-	if (!Object.keys(body).length) {
-		delete options.body;
 	}
 
 	const oAuth2Options = {
@@ -57,24 +50,12 @@ export async function discordApiRequest(
 	};
 
 	try {
-		// console.log(options);
+		console.log(options);
 		return await this.helpers.requestOAuth2!.call(this, 'discordOAuth2Api', options, oAuth2Options);
-		// return await this.helpers.request!.call(this, options);
 	} catch (error) {
 
-		// TODO
+		// TODO: Prettify error
 
 		throw error;
 	}
-}
-
-export function setHeartbeatInterval(ws: WebSocket, data: string) {
-	const interval = JSON.parse(data).d.heartbeat_interval;
-
-	const payload = JSON.stringify({
-		op: 1, // opcode
-		d: 251, // last sequence number `s` received by client
-	});
-
-	return setInterval(() => ws.send(payload), interval);
 }
