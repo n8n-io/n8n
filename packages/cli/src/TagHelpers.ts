e293acb8b654f7bacc283956383033ec91fdb462
Iván Ovejero <ivov.src@gmail.com>
2021-04-14 18:00:43 +0200

    :hammer: Refactor per feedback

--- packages/cli/src/TagHelpers.ts
+++ packages/cli/src/TagHelpers.ts
@@ -1,97 +1,36 @@
 import {
-	FindOneOptions,
 	getConnection,
-	In,
 } from "typeorm";
 
 import {
 	Db,
 	ITagDb,
-	IWorkflowDb,
 	ResponseHelper,
 } from ".";
 
-
 // ----------------------------------
-//           validators
-// ----------------------------------
-
-/**
- * Validate whether a tag ID exists so that it can be used for a workflow create or tag update operation.
- */
-export async function validateId(id: string): Promise<void> | never {
-	const findQuery = { where: { id } } as FindOneOptions;
-	const tag = await Db.collections.Tag!.findOne(findQuery);
-
-	if (!tag) {
-		throw new ResponseHelper.ResponseError(`Tag with ID ${id} does not exist.`, undefined, 400);
-	}
-}
-
-/**
- * Validate whether a tag name has 1 to 24 characters.
- */
-export function validateLength(name: string): void | never {
-	if (name.length <= 0 || name.length > 24) {
-		throw new ResponseHelper.ResponseError('Tag name must be 1 to 24 characters long.', undefined, 400);
-	}
-}
-
-/**
- * Validate whether a tag name exists so that it cannot be used for a tag create or tag update operation.
- */
-export async function validateName(name: string): Promise<void> | never {
-	const findQuery = { where: { name } } as FindOneOptions;
-	const tag = await Db.collections.Tag!.findOne(findQuery);
-
-	if (tag) {
-		throw new ResponseHelper.ResponseError('Tag name already exists.', undefined, 400);
-	}
-}
-
-/**
- * Validate that a tag and a workflow are not related so that a link can be created.
- */
-export async function validateNoRelation(workflowId: number, tagId: number): Promise<void> | never {
-	const areRelated = await checkRelated(workflowId, tagId);
-
-	if (areRelated) {
-		throw new ResponseHelper.ResponseError(`Workflow ID ${workflowId} and tag ID ${tagId} are already related.`, undefined, 400);
-	}
-}
-
-/**
- * Validate that a tag and a workflow are related so that their link can be deleted.
- */
-export async function validateRelation(workflowId: number, tagId: number): Promise<void> | never {
-	const areRelated = await checkRelated(workflowId, tagId);
-
-	if (!areRelated) {
-		throw new ResponseHelper.ResponseError(`Workflow ID ${workflowId} and tag ID ${tagId} are not related.`, undefined, 400);
-	}
-}
-
-/**
- * Validate whether the request body for a create/update operation has a `name` property.
- */
-export function validateRequestBody({ name }: { name: string | undefined }): void | never {
-	if (!name) {
-		throw new ResponseHelper.ResponseError(`Property 'name' missing from request body.`, undefined, 400);
-	}
-}
-
-
-// ----------------------------------
-//             queries
+//              utils
 // ----------------------------------
 
 /**
- * Check if a workflow and a tag are related. Used only in validators.
+ * Type guard for string array.
  */
-async function checkRelated(
-	workflowId: number,
-	tagId: number
-): Promise<boolean> {
+function isStringArray(tags: unknown[]): tags is string[] {
+	return Array.isArray(tags) && !tags.some((value) => typeof value !== 'string');
+}
+
+/**
+ * Stringify the ID in every `ITagDb` in an array.
+ * Side effect: Remove `createdAt` and `updatedAt` for a slimmer response.
+ */
+export function stringifyId(tags: ITagDb[]) {
+	return tags.map(({ id, name }) => ({ id: id.toString(), name }));
+}
+
+/**
+ * Check if a workflow and a tag are related.
+ */
+async function checkRelated(workflowId: string, tagId: string): Promise<boolean> {
 	const result = await getConnection().createQueryBuilder()
 		.select()
 		.from('workflows_tags', 'workflows_tags')
@@ -102,8 +41,88 @@ async function checkRelated(
 }
 
 /**
- * Retrieve all existing tags, whether linked to a workflow or not,
- * including how many workflows each tag is linked to.
+ * Check whether a tag ID exists in the `tag_entity` table.
+ *
+ * Used for creating a workflow or updating a tag.
+ */
+export async function exists(id: string): Promise<void> | never {
+	const tag = await Db.collections.Tag!.findOne({ where: { id }});
+
+	if (!tag) {
+		throw new ResponseHelper.ResponseError(`Tag with ID ${id} does not exist.`, undefined, 400);
+	}
+}
+
+// ----------------------------------
+//           validators
+// ----------------------------------
+
+/**
+ * Validate whether every tag ID is a string array and exists in the `tag_entity` table.
+ *
+ * Used for creating a workflow or updating a tag.
+ */
+export async function validateTags(tags: unknown[]): Promise<void> | never {
+	if (!isStringArray(tags)) {
+		throw new ResponseHelper.ResponseError(`The tags property is not an array of strings.`, undefined, 400);
+	}
+
+	for (const tagId of tags) {
+		await exists(tagId);
+	}
+}
+
+/**
+ * Validate whether a tag name
+ * - is present in the request body,
+ * - is a string,
+ * - is 1 to 24 characters long, and
+ * - does not exist already.
+ *
+ * Used for creating or updating a tag.
+ */
+export async function validateName(name: unknown): Promise<void> | never {
+	if (name === undefined) {
+		throw new ResponseHelper.ResponseError(`Property 'name' missing from request body.`, undefined, 400);
+	}
+
+	if (typeof name !== 'string') {
+		throw new ResponseHelper.ResponseError(`Property 'name' must be a string.`, undefined, 400);
+	}
+
+	if (name.length <= 0 || name.length > 24) {
+		throw new ResponseHelper.ResponseError('Tag name must be 1 to 24 characters long.', undefined, 400);
+	}
+
+	const tag = await Db.collections.Tag!.findOne({ where: { name } });
+
+	if (tag) {
+		throw new ResponseHelper.ResponseError('Tag name already exists.', undefined, 400);
+	}
+}
+
+/**
+ * Validate that the provided tags are related to a workflow.
+ *
+ * Used for relating the tags and the workflow.
+ */
+export async function validateNotRelated(workflowId: string, tags: string[]): Promise<void> | never {
+	tags.forEach(async tagId => {
+		const areRelated = await checkRelated(workflowId, tagId);
+
+		if (areRelated) {
+			throw new ResponseHelper.ResponseError(`Workflow ID ${workflowId} and tag ID ${tagId} are already related.`, undefined, 400);
+		}
+	});
+}
+
+// ----------------------------------
+//             queries
+// ----------------------------------
+
+/**
+ * Retrieve all existing tags, whether related to a workflow or not,
+ * including how many workflows each tag is related to.
  */
 export async function getAllTagsWithUsageCount(): Promise<Array<{
 	id: number;
@@ -156,7 +175,7 @@ export async function createRelations(workflowId: string, tagIds: string[]) {
 /**
  * Remove all tags for a workflow during a tag update operation.
  */
-export async function deleteAllTagsForWorkflow(workflowId: string) {
+export async function removeRelations(workflowId: string) {
 	await getConnection().createQueryBuilder()
 		.delete()
 		.from('workflows_tags')
