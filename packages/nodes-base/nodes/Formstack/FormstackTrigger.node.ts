0099996ba79a87daf4680e09f26f4f1c56ae5450
Aniruddha Adhikary <aniruddha@adhikary.net>
2021-04-15 10:18:53 +0800

    fix formstack color

--- packages/nodes-base/nodes/Formstack/FormstackTrigger.node.ts
+++ packages/nodes-base/nodes/Formstack/FormstackTrigger.node.ts
@@ -24,7 +24,7 @@ export class FormstackTrigger implements INodeType {
 		description: 'Starts the workflow on a Formstack form submission.',
 		defaults: {
 			name: 'Formstack Trigger',
-			color: '#ebfaf2', //  21b573
+			color: '#21b573',
 		},
 		inputs: [],
 		outputs: ['main'],
