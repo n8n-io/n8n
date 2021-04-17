d22dd191124b77803e6420bf64b7ca9344fde11d
Ricardo Espinoza <ricardo@n8n.io>
2021-04-17 03:25:51 -0400

    :sparkles: Add Webflow Node (#1628)
    * :sparkles: Webflow Node
    
    * :books: Add endpoint documentation URLs
    
    * :zap: Simplify field name/type separator
    
    * :zap: Simplify API call function signature
    
    * :zap: Reposition query string object
    
    * :pencil2: Edit parameter descriptions
    
    * :zap: Improvements
    
    Co-authored-by: Iván Ovejero <ivov.src@gmail.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/nodes-base/nodes/Webflow/GenericFunctions.ts
+++ packages/nodes-base/nodes/Webflow/GenericFunctions.ts
@@ -4,17 +4,26 @@ import {
 
 import {
 	IExecuteFunctions,
-	IExecuteSingleFunctions,
 	IHookFunctions,
 	ILoadOptionsFunctions,
 	IWebhookFunctions,
 } from 'n8n-core';
 
 import {
-	IDataObject, NodeApiError, NodeOperationError,
- } from 'n8n-workflow';
+	IDataObject,
+	NodeApiError,
+	NodeOperationError,
+} from 'n8n-workflow';
 
-export async function webflowApiRequest(this: IHookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions | IWebhookFunctions, method: string, resource: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+export async function webflowApiRequest(
+	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | IWebhookFunctions,
+	method: string,
+	resource: string,
+	body: IDataObject = {},
+	qs: IDataObject = {},
+	uri?: string,
+	option: IDataObject = {},
+) {
 	const authenticationMethod = this.getNodeParameter('authentication', 0);
 
 	let options: OptionsWithUri = {
@@ -24,14 +33,18 @@ export async function webflowApiRequest(this: IHookFunctions | IExecuteFunctions
 		method,
 		qs,
 		body,
-		uri: uri ||`https://api.webflow.com${resource}`,
+		uri: uri || `https://api.webflow.com${resource}`,
 		json: true,
 	};
 	options = Object.assign({}, options, option);
+
+	if (Object.keys(options.qs).length === 0) {
+		delete options.qs;
+	}
+
 	if (Object.keys(options.body).length === 0) {
 		delete options.body;
 	}
-
 	try {
 		if (authenticationMethod === 'accessToken') {
 			const credentials = this.getCredentials('webflowApi');
@@ -49,3 +62,32 @@ export async function webflowApiRequest(this: IHookFunctions | IExecuteFunctions
 		throw new NodeApiError(this.getNode(), error);
 	}
 }
+
+export async function webflowApiRequestAllItems(
+	this: IExecuteFunctions | ILoadOptionsFunctions,
+	method: string,
+	endpoint: string,
+	body: IDataObject = {},
+	query: IDataObject = {},
+) {
+
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+
+	query.limit = 100;
+	query.offset = 0;
+
+	do {
+		responseData = await webflowApiRequest.call(this, method, endpoint, body, query);
+		if (responseData.offset !== undefined) {
+			query.offset += query.limit;
+		}
+		returnData.push.apply(returnData, responseData.items);
+	} while (
+		returnData.length < responseData.total
+	);
+
+	return returnData;
+}
+
