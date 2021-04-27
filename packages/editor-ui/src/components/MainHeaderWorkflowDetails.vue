66df00bc7c18d887ffdc01eabf0ec1150130a9e7
Mutasem <mutdmour@gmail.com>
2021-04-27 12:11:00 +0300

    clean up indentation

--- packages/editor-ui/src/components/MainHeaderWorkflowDetails.vue
+++ packages/editor-ui/src/components/MainHeaderWorkflowDetails.vue
@@ -1,73 +1,74 @@
 <template>
-    <div class="workflow-details">
-        <span>
-            <div>
-                <div class="workflow-title">
-                    WORKFLOW
-                </div>
-                <div class="primary-color clickable">
-                    <span v-if="currentWorkflow">
-                        <a @click="openRenameDialog">
-                            <font-awesome-icon icon="edit" />&nbsp;&nbsp;<WorkflowNameShort :name="workflowName"/><span v-if="isDirty">*</span>
-                        </a>
-                    </span>
-                    <span v-else>
-                        <a @click="openSaveDialog">
-                            <font-awesome-icon icon="edit" />&nbsp;&nbsp;Unsaved workflow
-                        </a>
-                    </span>
-                </div>
-            </div>
-        </span>
+	<div class="workflow-details">
+		<span>
+			<div>
+				<div class="workflow-title">WORKFLOW</div>
+				<div class="primary-color clickable">
+					<span v-if="currentWorkflow">
+						<a @click="openRenameDialog">
+							<font-awesome-icon icon="edit" />&nbsp;&nbsp;<WorkflowNameShort
+								:name="workflowName"
+							/><span v-if="isDirty">*</span>
+						</a>
+					</span>
+					<span v-else>
+						<a @click="openSaveDialog">
+							<font-awesome-icon icon="edit" />&nbsp;&nbsp;Unsaved workflow
+						</a>
+					</span>
+				</div>
+			</div>
+		</span>
 
-        <el-divider direction="vertical" v-if="currentWorkflowTagIds.length > 0"></el-divider>
+		<el-divider
+			direction="vertical"
+			v-if="currentWorkflowTagIds.length > 0"
+		></el-divider>
 
-        <TagContainer :tagIds="currentWorkflowTagIds" />
+		<TagContainer :tagIds="currentWorkflowTagIds" />
 
-        <span class="saving-workflow" v-if="isWorkflowSaving">
-            <font-awesome-icon icon="spinner" spin />
-            Saving...
-        </span>
-    </div>
+		<span class="saving-workflow" v-if="isWorkflowSaving">
+			<font-awesome-icon icon="spinner" spin />
+			Saving...
+		</span>
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'; 
-import { mapGetters } from 'vuex';
-import WorkflowNameShort from '@/components/WorkflowNameShort.vue';
-import TagContainer from '@/components/TagContainer.vue';
+import Vue from "vue";
+import { mapGetters } from "vuex";
+import WorkflowNameShort from "@/components/WorkflowNameShort.vue";
+import TagContainer from "@/components/TagContainer.vue";
 
 export default Vue.extend({
-	name: 'WorkflowDetails',
-    components: {
-        TagContainer,
-        WorkflowNameShort,
-    },
+	name: "WorkflowDetails",
+	components: {
+		TagContainer,
+		WorkflowNameShort,
+	},
 	computed: {
-        ...mapGetters('workflows', [
-		    'currentWorkflowTagIds',
-	    ]),
-        isWorkflowSaving (): boolean {
-			return this.$store.getters.isActionActive('workflowSaving');
+		...mapGetters("workflows", ["currentWorkflowTagIds"]),
+		isWorkflowSaving(): boolean {
+			return this.$store.getters.isActionActive("workflowSaving");
 		},
-        currentWorkflow (): string {
-            return this.$route.params.name;
-        },
-        workflowName (): string {
+		currentWorkflow(): string {
+			return this.$route.params.name;
+		},
+		workflowName(): string {
 			return this.$store.getters.workflowName;
 		},
-        isDirty () : boolean {
+		isDirty(): boolean {
 			return this.$store.getters.getStateIsDirty;
 		},
-    },
-    methods: {
-	    openSaveDialog() {
-			this.$store.commit('ui/openSaveAsDialog');
+	},
+	methods: {
+		openSaveDialog() {
+			this.$store.commit("ui/openSaveAsDialog");
 		},
 		openRenameDialog() {
-			this.$store.commit('ui/openRenameDialog');
-		}
-    }
+			this.$store.commit("ui/openRenameDialog");
+		},
+	},
 });
 </script>
 
@@ -87,23 +88,23 @@ export default Vue.extend({
 }
 
 .el-divider {
-    min-height: 30px;
+	min-height: 30px;
 }
 
 .saving-workflow {
-    display: inline-block;
-    padding: 0 15px;
-    color: $--color-primary;
-    background-color: $--color-primary-light;
-    line-height: 30px;
-    height: 30px;
-    border-radius: 15px;
+	display: inline-block;
+	padding: 0 15px;
+	color: $--color-primary;
+	background-color: $--color-primary-light;
+	line-height: 30px;
+	height: 30px;
+	border-radius: 15px;
 }
 
 .workflow-title {
-    font-size: 9px;
-    font-weight: 600;
-    letter-spacing: 0.75px;
-    color: #5A5E66;
+	font-size: 9px;
+	font-weight: 600;
+	letter-spacing: 0.75px;
+	color: #5a5e66;
 }
 </style>
