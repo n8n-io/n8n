9e09a746bf764be40ae5bb8f4d6a867f36ef2d2b
dali <servfrdali@yahoo.fr>
2021-04-28 18:06:08 +0200

    Refactor & update Line node

--- packages/nodes-base/nodes/Line/Line.node.ts
+++ packages/nodes-base/nodes/Line/Line.node.ts
@@ -78,67 +78,75 @@ export class Line implements INodeType {
 		const operation = this.getNodeParameter('operation', 0) as string;
 		for (let i = 0; i < length; i++) {
 
-			if (resource === 'notification') {
-				//https://notify-bot.line.me/doc/en/
-				if (operation === 'send') {
-					const message = this.getNodeParameter('message', i) as string;
+			try {
+				if (resource === 'notification') {
+					//https://notify-bot.line.me/doc/en/
+					if (operation === 'send') {
+						const message = this.getNodeParameter('message', i) as string;
 
-					const additionalFields = this.getNodeParameter('additionalFields', i) as IDataObject;
+						const additionalFields = this.getNodeParameter('additionalFields', i) as IDataObject;
 
-					const body: IDataObject = {
-						message,
-					};
+						const body: IDataObject = {
+							message,
+						};
 
-					Object.assign(body, additionalFields);
+						Object.assign(body, additionalFields);
 
-					if (body.hasOwnProperty('notificationDisabled')) {
-						body.notificationDisabled = (body.notificationDisabled) ? 'true' : 'false';
-					}
-
-					if (body.stickerUi) {
-						const sticker = (body.stickerUi as IDataObject).stickerValue as IDataObject;
-						if (sticker) {
-							body.stickerId = sticker.stickerId;
-							body.stickerPackageId = sticker.stickerPackageId;
+						if (body.hasOwnProperty('notificationDisabled')) {
+							body.notificationDisabled = (body.notificationDisabled) ? 'true' : 'false';
 						}
-						delete body.stickerUi;
-					}
 
-					if (body.imageUi) {
-						const image = (body.imageUi as IDataObject).imageValue as IDataObject;
-
-						if (image && image.binaryData === true) {
-							if (items[i].binary === undefined) {
-								throw new NodeOperationError(this.getNode(), 'No binary data exists on item!');
+						if (body.stickerUi) {
+							const sticker = (body.stickerUi as IDataObject).stickerValue as IDataObject;
+							if (sticker) {
+								body.stickerId = sticker.stickerId;
+								body.stickerPackageId = sticker.stickerPackageId;
 							}
-							//@ts-ignore
-							if (items[i].binary[image.binaryProperty] === undefined) {
-								throw new NodeOperationError(this.getNode(), `No binary data property "${image.binaryProperty}" does not exists on item!`);
-							}
-
-							const binaryData = (items[i].binary as IBinaryKeyData)[image.binaryProperty as string];
-
-							body.imageFile = {
-								value: Buffer.from(binaryData.data, BINARY_ENCODING),
-								options: {
-									filename: binaryData.fileName,
-								},
-							};
-						} else {
-							body.imageFullsize = image.imageFullsize;
-							body.imageThumbnail = image.imageThumbnail;
+							delete body.stickerUi;
 						}
-						delete body.imageUi;
+
+						if (body.imageUi) {
+							const image = (body.imageUi as IDataObject).imageValue as IDataObject;
+
+							if (image && image.binaryData === true) {
+								if (items[i].binary === undefined) {
+									throw new NodeOperationError(this.getNode(), 'No binary data exists on item!');
+								}
+								//@ts-ignore
+								if (items[i].binary[image.binaryProperty] === undefined) {
+									throw new NodeOperationError(this.getNode(), `No binary data property "${image.binaryProperty}" does not exists on item!`);
+								}
+
+								const binaryData = (items[i].binary as IBinaryKeyData)[image.binaryProperty as string];
+
+								body.imageFile = {
+									value: Buffer.from(binaryData.data, BINARY_ENCODING),
+									options: {
+										filename: binaryData.fileName,
+									},
+								};
+							} else {
+								body.imageFullsize = image.imageFullsize;
+								body.imageThumbnail = image.imageThumbnail;
+							}
+							delete body.imageUi;
+						}
+						responseData = await lineApiRequest.call(this, 'POST', '', {}, {}, 'https://notify-api.line.me/api/notify', { formData: body });
 					}
-					responseData = await lineApiRequest.call(this, 'POST', '', {}, {}, 'https://notify-api.line.me/api/notify', { formData: body });
 				}
-			}
-		}
-		if (Array.isArray(responseData)) {
-			returnData.push.apply(returnData, responseData as IDataObject[]);
+				if (Array.isArray(responseData)) {
+					returnData.push.apply(returnData, responseData as IDataObject[]);
 
-		} else if (responseData !== undefined) {
-			returnData.push(responseData as IDataObject);
+				} else if (responseData !== undefined) {
+					returnData.push(responseData as IDataObject);
+				}
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ error: error.message });
+					continue;
+				}
+				throw error;
+			}
 		}
 		return [this.helpers.returnJsonArray(returnData)];
 	}
