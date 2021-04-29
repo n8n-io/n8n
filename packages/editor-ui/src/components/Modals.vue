d5ceeef1bb3f168aad7c1d9d41cd50c4d99c0afe
Mutasem <mutdmour@gmail.com>
2021-04-29 14:56:56 +0300

    add modal root

--- packages/editor-ui/src/components/Modals.vue
+++ packages/editor-ui/src/components/Modals.vue
@@ -1,26 +1,41 @@
 <template>
 	<div>
-		<SaveWorkflowDialog
-			v-if="isOpen(SAVE_AS_MODAL_KEY)"
-			:isActive="isActive(SAVE_AS_MODAL_KEY)"
-			:modalName="SAVE_AS_MODAL_KEY"
-			title="Save new workflow"
-		/>
-		<SaveWorkflowDialog
-			v-if="isOpen(RENAME_MODAL_KEY)"
-			:isActive="isActive(RENAME_MODAL_KEY)"
-			:modalName="RENAME_MODAL_KEY"
-			:renameOnly="true"
-			title="Rename workflow"
-		/>
-		<TagsManager
-			v-if="isOpen(TAGS_MANAGER_MODAL_KEY)"	
-			:modalName="TAGS_MANAGER_MODAL_KEY"
-		/>
-		<WorkflowOpen
-			v-if="isOpen(WORKLOW_OPEN_MODAL_KEY)"
-			:modalName="WORKLOW_OPEN_MODAL_KEY"
-		/>
+		<ModalRoot :name="SAVE_AS_MODAL_KEY">
+			<template v-slot:default="{modalName, active}">
+				<SaveWorkflowDialog
+					:isActive="active"
+					:modalName="modalName"
+					title="Save new workflow"
+				/>
+			</template>
+		</ModalRoot>
+
+		<ModalRoot :name="RENAME_MODAL_KEY">
+			<template v-slot:default="{modalName, active}">
+				<SaveWorkflowDialog
+					:isActive="active"
+					:modalName="modalName"
+					:renameOnly="true"
+					title="Rename workflow"
+				/>
+			</template>
+		</ModalRoot>
+
+		<ModalRoot :name="TAGS_MANAGER_MODAL_KEY">
+			<template v-slot="{modalName}">
+				<TagsManager
+					:modalName="modalName"
+				/>
+			</template>
+		</ModalRoot>
+
+		<ModalRoot :name="WORKLOW_OPEN_MODAL_KEY">
+			<template v-slot="{modalName}">
+				<WorkflowOpen
+					:modalName="modalName"
+				/>
+			</template>
+		</ModalRoot>
 	</div>
 </template>
 
@@ -31,6 +46,7 @@ import { RENAME_MODAL_KEY, SAVE_AS_MODAL_KEY, TAGS_MANAGER_MODAL_KEY, WORKLOW_OP
 import TagsManager from "@/components/TagsManager.vue";
 import SaveWorkflowDialog from "@/components/SaveWorkflowDialog.vue";
 import WorkflowOpen from "@/components/WorkflowOpen.vue";
+import ModalRoot from "./ModalRoot.vue";
 
 export default Vue.extend({
 	name: "Modals",
@@ -38,6 +54,7 @@ export default Vue.extend({
 		TagsManager,
 		SaveWorkflowDialog,
 		WorkflowOpen,
+ModalRoot,
 	},
 	data: () => ({
 		RENAME_MODAL_KEY,
