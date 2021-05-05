6bd86bcc670a0754b55e50974822264e1b14d326
dali <servfrdali@yahoo.fr>
2021-05-05 09:27:33 +0200

    Refactor Discourse node

--- packages/nodes-base/nodes/Discourse/Discourse.node.ts
+++ packages/nodes-base/nodes/Discourse/Discourse.node.ts
@@ -490,6 +490,11 @@ export class Discourse implements INodeType {
 						);
 					}
 				}
+				if (Array.isArray(responseData)) {
+					returnData.push.apply(returnData, responseData as IDataObject[]);
+				} else if (responseData !== undefined) {
+					returnData.push(responseData as IDataObject);
+				}
 			} catch (error) {
 				if (this.continueOnFail()) {
 					returnData.push({ error: error.message });
@@ -497,11 +502,6 @@ export class Discourse implements INodeType {
 				}
 				throw error;
 			}
-		}
-		if (Array.isArray(responseData)) {
-			returnData.push.apply(returnData, responseData as IDataObject[]);
-		} else if (responseData !== undefined) {
-			returnData.push(responseData as IDataObject);
 		}
 		return [this.helpers.returnJsonArray(returnData)];
 	}
