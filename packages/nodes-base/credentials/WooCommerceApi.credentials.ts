c632f7982f5c3d40213c3ca8790a9f65bdad40e1
Ricardo Espinoza <ricardo@n8n.io>
2021-05-12 00:20:40 -0400

    :zap: Add parameter to include credentials in querystring (Woocommerce) (#1756)

--- packages/nodes-base/credentials/WooCommerceApi.credentials.ts
+++ packages/nodes-base/credentials/WooCommerceApi.credentials.ts
@@ -27,5 +27,14 @@ export class WooCommerceApi implements ICredentialType {
 			default: '',
 			placeholder: 'https://example.com',
 		},
+		{
+			displayName: 'Include Credentials in Query',
+			name: 'includeCredentialsInQuery',
+			type: 'boolean' as NodePropertyTypes,
+			default: false,
+			description: `Occasionally, some servers may not parse the Authorization header correctly</br>
+			(if you see a “Consumer key is missing” error when authenticating over SSL, you have a server issue).</br>
+			In this case, you may provide the consumer key/secret as query string parameters instead.`,
+		},
 	];
 }
