22ab7a307f2ec14dc6809d33d8c01a54b1b608d7
Iván Ovejero <ivov.src@gmail.com>
2021-05-14 11:12:12 +0200

    :zap: Adjust quotes in MySQL migration

--- packages/cli/src/databases/mysqldb/migrations/1620826335440-UniqueWorkflowNames.ts
+++ packages/cli/src/databases/mysqldb/migrations/1620826335440-UniqueWorkflowNames.ts
@@ -9,15 +9,15 @@ export class UniqueWorkflowNames1620826335440 implements MigrationInterface {
 
 			const workflowNames = await queryRunner.query(`
 				SELECT name
-				FROM "${tablePrefix}workflow_entity"
+				FROM ${tablePrefix}workflow_entity
 			`);
 
 			for (const { name } of workflowNames) {
 
 				const duplicates = await queryRunner.query(`
 					SELECT id, name
-					FROM "${tablePrefix}workflow_entity"
-					WHERE name = "${name}"
+					FROM ${tablePrefix}workflow_entity
+					WHERE name = '${name}'
 					ORDER BY createdAt ASC
 				`);
 
@@ -26,8 +26,8 @@ export class UniqueWorkflowNames1620826335440 implements MigrationInterface {
 					await Promise.all(duplicates.map(({ id, name }: { id: number; name: string; }, index: number) => {
 						if (index === 0) return Promise.resolve();
 						return queryRunner.query(`
-							UPDATE "${tablePrefix}workflow_entity"
-							SET name = "${name} ${index + 1}"
+							UPDATE ${tablePrefix}workflow_entity
+							SET name = '${name} ${index + 1}'
 							WHERE id = '${id}'
 						`);
 					}));
@@ -36,13 +36,13 @@ export class UniqueWorkflowNames1620826335440 implements MigrationInterface {
 
 			}
 
-			await queryRunner.query(`ALTER TABLE "${tablePrefix}workflow_entity" ADD UNIQUE INDEX "IDX_${tablePrefix}943d8f922be094eb507cb9a7f9" ('name')`);
+			await queryRunner.query('ALTER TABLE `' + tablePrefix + 'workflow_entity` ADD UNIQUE INDEX `IDX_' + tablePrefix + '943d8f922be094eb507cb9a7f9` (`name`)');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
 			const tablePrefix = config.get('database.tablePrefix');
 
-			await queryRunner.query(`ALTER TABLE "${tablePrefix}workflow_entity" DROP INDEX "IDX_${tablePrefix}943d8f922be094eb507cb9a7f9"`);
+			await queryRunner.query('ALTER TABLE `' + tablePrefix + 'workflow_entity` DROP INDEX `IDX_' + tablePrefix + '943d8f922be094eb507cb9a7f9`');
     }
 
 }
