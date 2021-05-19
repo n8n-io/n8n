87c3f72e1aec17aec5a8a833ad280ff498530d98
Iván Ovejero <ivov.src@gmail.com>
2021-05-19 11:26:49 +0200

    :zap: Adjust default dates migration for MySQL
    Remove change to data column in credentials_entity, already covered by Omar's migration. Also, fix quotes from table prefix addition.

--- packages/cli/src/databases/mysqldb/migrations/1620313300228-SetDefaultDates.ts
+++ packages/cli/src/databases/mysqldb/migrations/1620313300228-SetDefaultDates.ts
@@ -7,26 +7,24 @@ export class SetDefaultDates1620313300228 implements MigrationInterface {
 	async up(queryRunner: QueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
 
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "credentials_entity` CHANGE `createdAt` `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
 		await queryRunner.query("ALTER TABLE `" + tablePrefix + "credentials_entity` CHANGE `updatedAt` `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`execution_entity` DROP COLUMN `data`");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`execution_entity` ADD `data` text NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`tag_entity` CHANGE `createdAt` `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`tag_entity` CHANGE `updatedAt` `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `nodes` `nodes` json NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `createdAt` `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `updatedAt` `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "tag_entity` CHANGE `createdAt` `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "tag_entity` CHANGE `updatedAt` `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `nodes` `nodes` json NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `createdAt` `createdAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `updatedAt` `updatedAt` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)");
 	}
 
 	async down(queryRunner: QueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
 
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`workflow_entity` CHANGE `nodes` `nodes` json NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`tag_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`tag_entity` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`execution_entity` DROP COLUMN `data`");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`execution_entity` ADD `data` mediumtext NOT NULL");
-		await queryRunner.query("ALTER TABLE `" + tablePrefix + "`credentials_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "workflow_entity` CHANGE `nodes` `nodes` json NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "tag_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "tag_entity` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "credentials_entity` CHANGE `updatedAt` `updatedAt` datetime(0) NOT NULL");
+		await queryRunner.query("ALTER TABLE `" + tablePrefix + "credentials_entity` CHANGE `createdAt` `createdAt` datetime(0) NOT NULL");
 	}
 }
