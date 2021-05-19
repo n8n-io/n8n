0aa9a29d804b24c0382fa42b6c2183502dd384e0
Omar Ajoue <krynble@gmail.com>
2021-05-19 10:31:30 +0200

    Added colors and beautified execute all command output

--- packages/cli/commands/executeAll.ts
+++ packages/cli/commands/executeAll.ts
@@ -4,7 +4,7 @@ import {
 	UserSettings,
 } from 'n8n-core';
 import {
-	INode, IRun,
+	INode, IRun, Workflow,
 } from 'n8n-workflow';
 
 import {
@@ -28,7 +28,7 @@ import {
 	diff,
 	diffString
 } from 'json-diff';
-import { ObjectID } from 'typeorm';
+import { ObjectID, UpdateValuesMissingError } from 'typeorm';
 
 import { 
 	getLogger,
@@ -37,6 +37,38 @@ import {
 import {
 	LoggerProxy,
 } from 'n8n-workflow';
+
+const colorOutput = true;
+
+function updateProgress(executionProgress: IWorkflowExecutionProgress[]) {
+	process.stdout.clearLine(-1);
+    process.stdout.cursorTo(0);
+	const results = executionProgress.map(execution => {
+		let openColor = '', closeColor = '';
+		if (colorOutput) {
+			openColor = execution.status === 'success' ? "\x1b[32m" : (execution.status === 'error' ? "\x1b[31m" : "\x1b[0m");
+			closeColor = "\x1b[0m";
+		}
+		return openColor + execution.workflowId + closeColor;
+	});
+    process.stdout.write("Running batch; workflow IDs " + results.join(','));
+}
+
+function updateError(workflowId: string | number | Object, workflowExecutionsProgress: IWorkflowExecutionProgress[]) {
+	const workflowProgress = workflowExecutionsProgress.find(executionProgress => executionProgress.workflowId === workflowId);
+	if (workflowProgress !== undefined) {
+		workflowProgress.status = 'error';
+	}
+	updateProgress(workflowExecutionsProgress);
+}
+
+function updateSuccess(workflowId: string | number | Object, workflowExecutionsProgress: IWorkflowExecutionProgress[]) {
+	const workflowProgress = workflowExecutionsProgress.find(executionProgress => executionProgress.workflowId === workflowId);
+	if (workflowProgress !== undefined) {
+		workflowProgress.status = 'success';
+	}
+	updateProgress(workflowExecutionsProgress);
+}
 
 export class ExecuteAll extends Command {
 	static description = '\nExecutes all workflows once';
@@ -59,7 +91,7 @@ export class ExecuteAll extends Command {
 			description: 'Specifies workflow IDs to get executed, separated by a comma.',
 		}),
 		concurrency: flags.integer({
-			default: 5,
+			default: 0,
 			description: 'How many workflows can run in parallel.',
 		}),
 		output: flags.string({
@@ -78,6 +110,8 @@ export class ExecuteAll extends Command {
 
 		const logger = getLogger();
 		LoggerProxy.init(logger);
+
+		const executionTimeout = 3 * 60 * 1000;
 				
 
 		const { flags } = this.parse(ExecuteAll);
@@ -189,12 +223,9 @@ export class ExecuteAll extends Command {
 		// "requiredNodeTypes" are also defined in editor-ui/views/NodeView.vue
 		const requiredNodeTypes = ['n8n-nodes-base.start'];
 		let workflowsExecutionsPromises:Array<Promise<IRun | undefined>> = [];
+		let workflowExecutionsProgress:Array<IWorkflowExecutionProgress> = [];
 		for (let i = 0; i < allWorkflows.length; i++) {
 			const workflowData = allWorkflows[i];
-			
-			if (debug === true) {
-				this.log(`Starting execution of workflow ID ${workflowData.id}.`);
-			}
 			
 			const executionResult:IExecutionResult = {
 				workflowId: workflowData.id,
@@ -225,9 +256,25 @@ export class ExecuteAll extends Command {
 				result.executions.push(executionResult);
 				continue;
 			}
+
+			workflowExecutionsProgress.push({
+				workflowId: workflowData.id,
+				status: 'running',
+			});
 			
 			workflowsExecutionsPromises.push(
 				new Promise(async (resolve,reject) => {
+					let gotCancel = false;
+					
+					// Timeouts execution after 5 minutes.
+					const timeoutTimer = setTimeout(() => {
+						gotCancel = true;
+						executionResult.error = 'Workflow execution timed out.';
+						result.summary.failedExecutions++;
+						result.executions.push(executionResult);
+						result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
+						reject(new Error('Workflow execution timed out.'));
+					}, executionTimeout);
 					try {
 						const credentials = await WorkflowCredentials(workflowData!.nodes);
 
@@ -243,13 +290,17 @@ export class ExecuteAll extends Command {
 
 						const activeExecutions = ActiveExecutions.getInstance();
 						const data = await activeExecutions.getPostExecutePromise(executionId);
+						if (gotCancel) {
+							// The promise was rejected already so we simply ignore.
+							return;
+						}
 
 						if (data === undefined) {
-								executionResult.error = 'Workflow did not return any data.';
-								result.summary.failedExecutions++;
-								result.executions.push(executionResult);
-								result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
-
+							executionResult.error = 'Workflow did not return any data.';
+							result.summary.failedExecutions++;
+							result.executions.push(executionResult);
+							result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
+							updateError(workflowData.id, workflowExecutionsProgress);
 						}else{
 
 							workflowData.nodes.forEach(node => {
@@ -263,9 +314,13 @@ export class ExecuteAll extends Command {
 									data.data.resultData.error.hasOwnProperty('description') ? 
 									// @ts-ignore
 									data.data.resultData.error.description : data.data.resultData.error.message;
+								if (data.data.resultData.lastNodeExecuted !== undefined) {
+									executionResult.error += ` on node ${data.data.resultData.lastNodeExecuted}`;
+								}
 								result.summary.failedExecutions++;
 								result.executions.push(executionResult);
 								result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
+								updateError(workflowData.id, workflowExecutionsProgress);
 								if (debug === true) {
 									this.log(JSON.stringify(data, null, 2));
 									console.log(data.data.resultData.error);
@@ -277,6 +332,7 @@ export class ExecuteAll extends Command {
 								if (flags.compare === undefined){
 									result.summary.succeededExecution++;
 									result.executions.push(executionResult);
+									updateSuccess(workflowData.id, workflowExecutionsProgress);
 								} else {
 									const fileName = (flags.compare.endsWith(sep) ? flags.compare : flags.compare + sep) + `${workflowData.id}-snapshot.json`;
 									if (fs.existsSync(fileName) === true) {
@@ -293,6 +349,7 @@ export class ExecuteAll extends Command {
 											result.summary.failedExecutions++;
 											result.executions.push(executionResult);
 											result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
+											updateError(workflowData.id, workflowExecutionsProgress);
 											if (debug === true) {
 												// @ts-ignore
 												console.log('Detailed changes: ', diffString(JSON.parse(contents), data, undefined, {keysOnly: true}));
@@ -300,12 +357,14 @@ export class ExecuteAll extends Command {
 										}else{
 											result.summary.succeededExecution++;
 											result.executions.push(executionResult);
+											updateSuccess(workflowData.id, workflowExecutionsProgress);
 										}
 									} else {
 										executionResult.error = 'Snapshot for not found.';
 										result.summary.failedExecutions++;
 										result.executions.push(executionResult);
 										result.summary.errors.push({workflowId: workflowData.id, error: executionResult.error});
+										updateError(workflowData.id, workflowExecutionsProgress);
 									}
 								}
 								// Save snapshots only after comparing - this is to make sure we're updating
@@ -321,17 +380,22 @@ export class ExecuteAll extends Command {
 						executionResult.error = 'Workflow failed to execute.';
 						result.summary.exceptions++;
 						result.executions.push(executionResult);
+						updateError(workflowData.id, workflowExecutionsProgress);
 						if (debug === true) {
 							console.error(e.message);
 							console.error(e.stack);
 						}
 						reject(e);
 					}
+					clearTimeout(timeoutTimer);
 				})
 			);
-			if(i !== 0 && i % concurrency === 0){
+			if(concurrency === 0 || (i !== 0 && (i+1) % concurrency === 0)){
+				process.stdout.write('\n');
+				updateProgress(workflowExecutionsProgress);
 				await Promise.allSettled(workflowsExecutionsPromises);
 				workflowsExecutionsPromises = [];
+				workflowExecutionsProgress = [];
 			}
 
 		}
@@ -390,4 +454,9 @@ interface IExecutionResult{
 interface IExecutionError {
 	workflowId: string | number | ObjectID;
 	error: string;
+}
+
+interface IWorkflowExecutionProgress {
+	workflowId: string | number | ObjectID;
+	status: 'success' | 'error' | 'running';
 }
