4bab967cc99d61af7b4831e435f6b043c29e84a6
Jan Oberhauser <jan.oberhauser@gmail.com>
2021-05-25 19:19:57 -0500

    :zap: Improve SQLite migration to not lose execution data anymore

--- packages/cli/src/databases/sqlite/migrations/1621707690587-AddSleepColumn.ts
+++ packages/cli/src/databases/sqlite/migrations/1621707690587-AddSleepColumn.ts
@@ -7,18 +7,18 @@ export class AddSleepColumn1621707690587 implements MigrationInterface {
 	async up(queryRunner: QueryRunner): Promise<void> {
 		const tablePrefix = config.get('database.tablePrefix');
 
-		// IMPORTANT: Danger currently it will delete all past executions
-		// TODO: In the future we obviously have to copy the data (so first create temp, copy data, rename)
+		await queryRunner.query(`CREATE TABLE IF NOT EXISTS "${tablePrefix}temporary_execution_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "data" text NOT NULL, "finished" boolean NOT NULL, "mode" varchar NOT NULL, "retryOf" varchar, "retrySuccessId" varchar, "startedAt" datetime NOT NULL, "stoppedAt" datetime, "workflowData" text NOT NULL, "workflowId" varchar, "sleepTill" DATETIME)`, undefined);
+		await queryRunner.query(`INSERT INTO "${tablePrefix}temporary_execution_entity"("id", "data", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "workflowData", "workflowId") SELECT "id", "data", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "workflowData", "workflowId" FROM "${tablePrefix}execution_entity"`);
 		await queryRunner.query(`DROP TABLE "${tablePrefix}execution_entity"`);
-
-		await queryRunner.query(`CREATE TABLE IF NOT EXISTS "${tablePrefix}execution_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "data" text NOT NULL, "finished" boolean NOT NULL, "mode" varchar NOT NULL, "retryOf" varchar, "retrySuccessId" varchar, "startedAt" datetime NOT NULL, "stoppedAt" datetime, "workflowData" text NOT NULL, "workflowId" varchar, "sleepTill" DATETIME)`, undefined);
-		await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefix}c4d999a5e90784e8caccf5589d" ON "${tablePrefix}execution_entity" ("workflowId") `, undefined);
-		await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1" ON "${tablePrefix}execution_entity" ("stoppedAt") `);
-		await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}ca4a71b47f28ac6ea88293a8e2" ON "${tablePrefix}execution_entity" ("sleepTill") `);
+		await queryRunner.query(`ALTER TABLE "${tablePrefix}temporary_execution_entity" RENAME TO "${tablePrefix}execution_entity"`);
+		await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}cefb067df2402f6aed0638a6c1" ON "${tablePrefix}execution_entity" ("stoppedAt")`);
+		await queryRunner.query(`CREATE INDEX "IDX_${tablePrefix}ca4a71b47f28ac6ea88293a8e2" ON "${tablePrefix}execution_entity" ("sleepTill")`);
+		await queryRunner.query(`VACUUM;`);
 	}
 
 	async down(queryRunner: QueryRunner): Promise<void> {
 		// const tablePrefix = config.get('database.tablePrefix');
+		// TODO: Add rollback
 
 	}
 
