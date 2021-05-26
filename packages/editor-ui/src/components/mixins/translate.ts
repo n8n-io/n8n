ebc9133e3ed8ec974d3c713715686261a1f61b6c
Iván Ovejero <ivov.src@gmail.com>
2021-05-26 16:39:07 +0200

    :zap: Refactor translate mixin

--- packages/editor-ui/src/components/mixins/translate.ts
+++ packages/editor-ui/src/components/mixins/translate.ts
@@ -1,25 +1,31 @@
 import Vue from 'vue';
+import { TranslateResult } from 'vue-i18n';
 
 export const translate = Vue.extend({
+	computed: {
+		nodeType (): string {
+			return this.$store.getters.activeNode.type;
+		},
+	},
 	methods: {
-		translate({ key, fallback }: { key: string, fallback: string }) {
+		translate({ key, fallback }: { key: string, fallback: string }): TranslateResult {
 			return this.$te(key) ? this.$t(key) : fallback;
 		},
 		$translateDisplayName(displayName: string) {
 			return this.translate({
-				key: `${this.$store.getters.activeNode.type}.${displayName}`,
+				key: `${this.nodeType}.${displayName}`,
 				fallback: displayName,
 			});
 		},
 		$translateOptionName(optionsGroupName: string, optionName: string) {
 			return this.translate({
-				key: `${this.$store.getters.activeNode.type}.options.${optionsGroupName}.${optionName}`,
+				key: `${this.nodeType}.options.${optionsGroupName}.${optionName}`,
 				fallback: optionName,
 			});
 		},
 		$translateDescription(description: string) {
 			return this.translate({
-				key: `${this.$store.getters.activeNode.type}['${description}']`,
+				key: `${this.nodeType}['${description}']`,
 				fallback: description,
 			});
 		},
