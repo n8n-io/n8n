f19c850d85a25d04bf29ffab5ab31f6978b15722
Mutasem <mutdmour@gmail.com>
2021-05-27 15:14:55 +0300

    update duplicate message

--- packages/editor-ui/src/components/DuplicateWorkflowDialog.vue
+++ packages/editor-ui/src/components/DuplicateWorkflowDialog.vue
@@ -100,7 +100,7 @@ export default mixins(showMessage, workflowHelpers).extend({
 			if (!name) {
 				this.$showMessage({
 					title: "Name missing",
-					message: `Please enter a name, or press 'esc' to go back to the old one.`,
+					message: `Please enter a name.`,
 					type: "error",
 				});
 
