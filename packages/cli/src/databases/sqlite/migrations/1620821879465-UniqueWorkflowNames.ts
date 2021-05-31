65fae11f2424a433ee04df1403be7148dbcd9207
Iván Ovejero <ivov.src@gmail.com>
2021-05-31 20:12:55 +0200

    :bug: Fix escaping workflow name dedup (#1850)

--- packages/cli/src/databases/sqlite/migrations/1620821879465-UniqueWorkflowNames.ts
+++ packages/cli/src/databases/sqlite/migrations/1620821879465-UniqueWorkflowNames.ts
@@ -13,27 +13,27 @@ export class UniqueWorkflowNames1620821879465 implements MigrationInterface {
 			`);
 
 			for (const { name } of workflowNames) {
-
-				const duplicates = await queryRunner.query(`
+				const [duplicatesQuery, parameters] = queryRunner.connection.driver.escapeQueryWithParameters(`
 					SELECT id, name
 					FROM "${tablePrefix}workflow_entity"
-					WHERE name = "${name}"
+					WHERE name = :name
 					ORDER BY createdAt ASC
-				`);
+				`, { name }, {});
+
+				const duplicates = await queryRunner.query(duplicatesQuery, parameters);
 
 				if (duplicates.length > 1) {
-
 					await Promise.all(duplicates.map(({ id, name }: { id: number; name: string; }, index: number) => {
 						if (index === 0) return Promise.resolve();
-						return queryRunner.query(`
+						const [updateQuery, updateParams] = queryRunner.connection.driver.escapeQueryWithParameters(`
 							UPDATE "${tablePrefix}workflow_entity"
-							SET name = "${name} ${index + 1}"
+							SET name = :name
 							WHERE id = '${id}'
-						`);
+						`, { name: `${name} ${index + 1}`}, {});
+
+						return queryRunner.query(updateQuery, updateParams);
 					}));
-
 				}
-
 			}
 
 			await queryRunner.query(`CREATE UNIQUE INDEX "IDX_${tablePrefix}943d8f922be094eb507cb9a7f9" ON "${tablePrefix}workflow_entity" ("name") `);
