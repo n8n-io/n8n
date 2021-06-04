21c331c70a02322c92472eef2eb3df4ae18cfbf2
Iván Ovejero <ivov.src@gmail.com>
2021-06-04 11:22:55 +0200

    :zap: Initial pass at collecting base strings

--- packages/editor-ui/src/i18n/locales/de.ts
+++ packages/editor-ui/src/i18n/locales/de.ts
@@ -1,26 +1,270 @@
 export default {
 	de: {
+
+		nodeViewError: {
+			error: 'Deutsch',
+			details: 'Deutsch',
+			dataBelowMayContain: 'Deutsch',
+			stack: 'Deutsch',
+		},
+
+		noTagsView: {
+			readyToOrganizeYourWorkflows: 'Deutsch',
+			withWorkflowTagsYouReFree: 'Deutsch',
+			createATag: 'Deutsch',
+		},
+
+		tagsTable: {
+			areYouSureYouWantToDeleteThisTag: 'Deutsch',
+			createTag: 'Deutsch',
+			saveChanges: 'Deutsch',
+			deleteTag: 'Deutsch',
+		},
+
+		tagsTableHeader: {
+			addNew: 'Deutsch',
+		},
+
 		about: {
-			aboutN8n: "Über n8n",
-			sourceCode: "Quell Code",
-			license: "Lizenz",
-			close: "Schließen",
-			apacheWithCommons20Clause: "Apache 2.0 mit Commons-Klausel",
+			aboutN8n: 'Deutsch',
+			n8nVersion: 'Deutsch',
+			sourceCode: 'Deutsch',
+			license: 'Deutsch',
+			close: 'Deutsch',
+			apacheWithCommons20Clause: 'Deutsch',
 		},
-		nodeView: {
-			currentlyNoItemsExist: 'Derzeit sind keine Artikel vorhanden',
-			parameters: 'Parameter',
-			settings: 'Einstellungen',
+
+		binaryDataDisplay: {
+			backToList: 'Deutsch',
+			dataToDisplayDidNotGetFound: 'Deutsch',
 		},
-		runData: {
-			dataReturnedByTheNodeWillDisplayHere: 'Von diesem Knoten zurückgegebene Daten werden hier angezeigt.',
-			noData: 'Keine Daten',
-			items: 'Artikel',
+
+		collectionParameter: {
+			currentlyNoPropertiesExist: 'Deutsch',
 		},
-		"n8n-nodes-base": {
-			start: {
-				hello: "hallöchen",
+
+		credentialsEdit: {
+			nodeDocumentation: 'Deutsch',
+			needHelp: 'Deutsch',
+			openCredentialDocs: 'Deutsch',
+			credentialType: 'Deutsch',
+		},
+
+		credentialsInput: {
+			credentialsName: 'Deutsch',
+			credentialData: 'Deutsch',
+			enterAllProperties: 'Deutsch',
+			connected: 'Deutsch',
+			notConnected: 'Deutsch',
+			oAuth2CallbackUrl: 'Deutsch',
+			nodesWithAccess: 'Deutsch',
+			important: 'Deutsch',
+			addAtLeastOneNodeWhichHasAccessToTheCredentials: 'Deutsch',
+			save: 'Deutsch',
+			create: 'Deutsch',
+		},
+
+		credentialsList: {
+			yourSavedCredentials: 'Deutsch',
+			addNew: 'Deutsch',
+		},
+
+		dataDisplay: {
+			nodeDocumentation: 'Deutsch',
+			openDocumentationFor: 'Deutsch',
+		},
+
+		duplicateWorkflowDialog: {
+			enterWorkflowName: 'Deutsch', // search: placeholder="
+			save: 'Deutsch',
+			cancel: 'Deutsch',
+		},
+
+		executionsList: {
+			filters: 'Deutsch',
+			autoRefresh: 'Deutsch',
+			selected: 'Deutsch', // whitespace and colon
+			checkAll: 'Deutsch',
+			runningParens: 'Deutsch',
+			successRetry: 'Deutsch',
+			running: 'Deutsch',
+			success: 'Deutsch',
+			error: 'Deutsch',
+			unknown: 'Deutsch',
+			retryWithCurrentlySavedWorkflow: 'Deutsch',
+			retryWithOriginalworkflow: 'Deutsch',
+			loadMore: 'Deutsch',
+		},
+
+		expressionEdit: {
+			editExpression: 'Deutsch',
+			variableSelector: 'Deutsch',
+			expression: 'Deutsch',
+			result: 'Deutsch',
+		},
+
+		fixedCollectionParameter: {
+			currentlyNoItemsExist: 'Deutsch',
+		},
+
+		mainSideBar: {
+			workflows: 'Deutsch',
+			new: 'Deutsch',
+			open: 'Deutsch',
+			save: 'Deutsch',
+			duplicate: 'Deutsch',
+			delete: 'Deutsch',
+			download: 'Deutsch',
+			importFromUrl: 'Deutsch',
+			importFromFile: 'Deutsch',
+			settings: 'Deutsch',
+			credentials: 'Deutsch',
+			executions: 'Deutsch',
+			help: 'Deutsch',
+			aboutN8n: 'Deutsch',
+		},
+
+		multipleParameter: {
+			currentlyNoItemsExist: 'Deutsch',
+		},
+
+		nodeCreateList: {
+			noNodesMatchYourSearchCriteria: 'Deutsch',
+		},
+
+		nodeCreator: {
+			createNode: 'Deutsch',
+		},
+
+		nodeCredentials: {
+			credentials: 'Deutsch',
+		},
+
+		nodeSettings: {
+			theNodeIsNotValidAsItsTypeIsUnknown: 'Deutsch', // middle problem
+			thisNodeDoesNotHaveAnyParameters: 'Deutsch',
+			settings: {
+				notes: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				notesInFlow: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				color: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				alwaysOutputData: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				executeOnce: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				retryOnFail: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				maxTries: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				waitBetweenTries: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
+				continueOnFail: {
+					displayName: 'Deutsch',
+					description: 'Deutsch',
+				},
 			},
 		},
+
+		nodeWebhooks: {
+			webhookUrls: 'Deutsch',
+			displayUrlFor: 'Deutsch',
+		},
+
+		parameterInput: {
+			addExpression: 'Deutsch',
+			removeExpression: 'Deutsch',
+			resetValue: 'Deutsch',
+		},
+
+		pushConnectionTracker: {
+			cannotConnectToServer: 'Deutsch',
+			connectionLost: 'Deutsch',
+		},
+
+		runData: {
+			executeNode: 'Deutsch',
+			items: 'Deutsch',
+			startTime: 'Deutsch', // semicolon
+			executionTime: 'Deutsch', // semicolon
+			output: 'Deutsch', // pipe and colon
+			dataOfExecution: 'Deutsch', // pipe and colon
+			copyItemPath: 'Deutsch',
+			copyParameterPath: 'Deutsch',
+			copyValue: 'Deutsch',
+			nodeReturnedALargeAmountOfData: 'Deutsch',
+			theNodeContains: 'Deutsch', // middle problem
+			displayDataAnyway: 'Deutsch',
+			noTextDataFound: 'Deutsch',
+			entriesExistButThey: 'Deutsch',
+			noBinaryDataFound: 'Deutsch',
+			fileName: 'Deutsch', // mind the colon
+			fileExtension: 'Deutsch',
+			mimeType: 'Deutsch',
+			showBinaryData: 'Deutsch',
+			dataReturnedByTheNodeWillDisplayHere: 'Deutsch',
+			noData: 'Deutsch',
+		},
+
+		saveWorkflowButton: {
+			save: 'Deutsch',
+		},
+
+		tagsDropdown: {
+			typeToCreateATag: 'Deutsch',
+			noMatchingTagsExist: 'Deutsch',
+			noTagsExist: 'Deutsch',
+			manageTags: 'Deutsch',
+		},
+
+		variableSelectorItem: {
+			selectItem: 'Deutsch',
+			empty: 'Deutsch', // middle
+		},
+
+		workflowActivator: {
+			theWorkflowIsSetToBeActiveBut: 'Deutsch',
+		},
+
+		workflowSettings: {
+			errorWorkflow: 'Deutsch',
+			timezone: 'Deutsch',
+			saveDataErrorExecution: 'Deutsch', // colon
+			saveDataSuccessExecution: 'Deutsch',
+			saveManualExecutions: 'Deutsch',
+			saveExecutionProgress: 'Deutsch',
+			timeoutWorkflow: 'Deutsch',
+			editExpression: 'Deutsch',
+			timeoutAfter: 'Deutsch',
+			hours: 'Deutsch',
+			minutes: 'Deutsch',
+			seconds: 'Deutsch',
+			save: 'Deutsch',
+		},
+
+		nodeView: {
+			currentlyNoItemsExist: 'Deutsch',
+			parameters: 'Deutsch',
+			settings: 'Deutsch',
+		},
+
+		'n8n-nodes-base': {},
 	},
 };
--- packages/editor-ui/src/i18n/locales/en.ts
+++ packages/editor-ui/src/i18n/locales/en.ts
@@ -1,26 +1,271 @@
 export default {
 	en: {
+
+		nodeViewError: {
+			error: 'ERROR',
+			details: 'Details',
+			dataBelowMayContain: 'Data below may contain sensitive information. Proceed with caution when sharing.',
+			stack: 'Stack',
+		},
+
+		noTagsView: {
+			readyToOrganizeYourWorkflows: 'Ready to organize your workflows?',
+			withWorkflowTagsYouReFree: 'With workflow tags, you\'re free to create the perfect tagging system for your flows',
+			createATag: 'Create a tag',
+		},
+
+		tagsTable: {
+			areYouSureYouWantToDeleteThisTag: 'Are you sure you want to delete this tag?',
+			createTag: 'Create tag',
+			saveChanges: 'Save changes',
+			deleteTag: 'Delete tag',
+		},
+
+		tagsTableHeader: {
+			addNew: 'Add new',
+		},
+
 		about: {
 			aboutN8n: "About n8n",
+			n8nVersion: 'n8n Version',
 			sourceCode: "Source Code",
 			license: "License",
 			close: "Close",
 			apacheWithCommons20Clause: "Apache 2.0 with Commons Clause",
 		},
+
+		binaryDataDisplay: {
+			backToList: 'Back to list',
+			dataToDisplayDidNotGetFound: 'Data to display did not get found',
+		},
+
+		collectionParameter: {
+			currentlyNoPropertiesExist: 'Currently no properties exist',
+		},
+
+		credentialsEdit: {
+			nodeDocumentation: 'Node Documentation',
+			needHelp: 'Need help?',
+			openCredentialDocs: 'Open credential docs',
+			credentialType: 'Credential type:',
+		},
+
+		credentialsInput: {
+			credentialsName: 'Credentials Name:',
+			credentialData: 'Credential Data:',
+			enterAllProperties: 'Enter all required properties',
+			connected: 'Connected',
+			notConnected: 'Not connected',
+			oAuth2CallbackUrl: 'OAuth Callback URL',
+			nodesWithAccess: 'Nodes with access:',
+			important: 'Important',
+			addAtLeastOneNodeWhichHasAccessToTheCredentials: 'Add at least one node which has access to the credentials!',
+			save: 'Save',
+			create: 'Create',
+		},
+
+		credentialsList: {
+			yourSavedCredentials: 'Your saved credentials:',
+			addNew: 'Add New',
+		},
+
+		dataDisplay: {
+			nodeDocumentation: 'Node Documentation',
+			openDocumentationFor: 'Open documentation for',
+		},
+
+		duplicateWorkflowDialog: {
+			enterWorkflowName: 'Enter workflow name',
+			// search: placeholder="
+			save: 'Save',
+			cancel: 'Cancel',
+		},
+
+		executionsList: {
+			filters: 'Filters:',
+			autoRefresh: 'Auto refresh',
+			selected: 'Selected', // mind the whitespace+colon
+			checkAll: 'Check all',
+			runningParens: '(running)',
+			successRetry: 'Success retry',
+			running: 'Running',
+			success: 'Success',
+			error: 'Error',
+			unknown: 'Unknown',
+			retryWithCurrentlySavedWorkflow: 'Retry with currently saved workflow',
+			retryWithOriginalworkflow: 'Retry with original workflow',
+			loadMore: 'Load More',
+		},
+
+		expressionEdit: {
+			editExpression: 'Edit Expression',
+			variableSelector: 'Variable Selector',
+			expression: 'Expression',
+			result: 'Result',
+		},
+
+		fixedCollectionParameter: {
+			currentlyNoItemsExist: 'Currently no items exist',
+		},
+
+		mainSideBar: {
+			workflows: 'Workflows',
+			new: 'New',
+			open: 'Open',
+			save: 'Save',
+			duplicate: 'Duplicate',
+			delete: 'Delete',
+			download: 'Download',
+			importFromUrl: 'Import from URL',
+			importFromFile: 'Import from File',
+			settings: 'Settings',
+			credentials: 'Credentials',
+			executions: 'Executions',
+			help: 'Help',
+			aboutN8n: 'About n8n',
+		},
+
+		multipleParameter: {
+			currentlyNoItemsExist: 'Currently no items exist',
+		},
+
+		nodeCreateList: {
+			noNodesMatchYourSearchCriteria: '🙃 no nodes matching your search criteria',
+		},
+
+		nodeCreator: {
+			createNode: 'Create Node',
+		},
+
+		nodeCredentials: {
+			credentials: 'Credentials',
+		},
+
+		nodeSettings: {
+			theNodeIsNotValidAsItsTypeIsUnknown: 'The node is not valid as its type is unknown.', // middle problem
+			thisNodeDoesNotHaveAnyParameters: 'This node does not have any parameters.',
+			settings: {
+				notes: {
+					displayName: 'Notes',
+					description: 'Optional note to save with the node.',
+				},
+				notesInFlow: {
+					displayName: 'Display note in flow?',
+					description: 'If active, the note above will display in the flow as a subtitle.',
+				},
+				color: {
+					displayName: 'Node Color',
+					description: 'The color of the node in the flow.',
+				},
+				alwaysOutputData: {
+					displayName: 'Always Output Data',
+					description: 'If active, the node will return an empty item even if the <br />node returns no data during an initial execution. Be careful setting <br />this on IF-Nodes as it could cause an infinite loop.',
+				},
+				executeOnce: {
+					displayName: 'Execute Once',
+					description: 'If active, the node executes only once, with data<br /> from the first item it recieves.',
+				},
+				retryOnFail: {
+					displayName: 'Retry On Fail',
+					description: 'If active, the node tries to execute a failed attempt <br /> multiple times until it succeeds.',
+				},
+				maxTries: {
+					displayName: 'Max. Tries',
+					description: 'Number of times Retry On Fail should attempt to execute the node <br />before stopping and returning the execution as failed.',
+				},
+				waitBetweenTries: {
+					displayName: 'Wait Between Tries',
+					description: 'How long to wait between each attempt. Value in ms.',
+				},
+				continueOnFail: {
+					displayName: 'Continue On Fail',
+					description: 'If active, the workflow continues even if this node\'s <br />execution fails. When this occurs, the node passes along input data from<br />previous nodes - so your workflow should account for unexpected output data.',
+				},
+			},
+		},
+
+		nodeWebhooks: {
+			webhookUrls: 'Webhook URLs',
+			displayUrlFor: 'Display URL for:',
+		},
+
+		parameterInput: {
+			addExpression: 'Add Expression',
+			removeExpression: 'Remove Expression',
+			resetValue: 'Reset Value',
+		},
+
+		pushConnectionTracker: {
+			cannotConnectToServer: 'Cannot connect to server.<br />It is either down or you have a connection issue. <br />It should reconnect automatically once the issue is resolved.',
+			connectionLost: 'Connection lost',
+		},
+
+		runData: {
+			executeNode: 'Execute Node',
+			items: 'Items',
+			startTime: 'Start Time', // semicolon
+			executionTime: 'Execution Time', // semicolon
+			output: 'Output', // pipe and colon
+			dataOfExecution: 'Data of Execution', // pipe and colon
+			copyItemPath: 'Copy Item Path',
+			copyParameterPath: 'Copy Parameter Path',
+			copyValue: 'Copy Value',
+			nodeReturnedALargeAmountOfData: 'Node returned a large amount of data',
+			theNodeContains: 'The node contains {{parseInt(dataSize/1024).toLocaleString()}} KB of data.<br />Displaying it could cause problems!<br /><br />If you do decide to display it, avoid the JSON view!', // middle problem
+			displayDataAnyway: 'Display Data Anyway',
+			noTextDataFound: 'No text data found',
+			entriesExistButThey: 'Entries exist but they do not contain any JSON data.',
+			noBinaryDataFound: 'No binary data found',
+			fileName: 'File Name', // mind the colon
+			fileExtension: 'File Extension',
+			mimeType: 'Mime Type',
+			showBinaryData: 'Show Binary Data',
+			dataReturnedByTheNodeWillDisplayHere: 'Data returned by this node will display here.',
+			noData: 'No data',
+		},
+
+		saveWorkflowButton: {
+			save: 'Save',
+		},
+
+		tagsDropdown: {
+			typeToCreateATag: 'Type to create a tag',
+			noMatchingTagsExist: 'No matching tags exist',
+			noTagsExist: 'No tags exist',
+			manageTags: 'Manage tags',
+		},
+
+		variableSelectorItem: {
+			selectItem: 'Select Item',
+			empty: 'EMPTY', // middle
+		},
+
+		workflowActivator: {
+			theWorkflowIsSetToBeActiveBut: 'The workflow is set to be active but could not be started.<br />Click to display error message.</div>',
+		},
+
+		workflowSettings: {
+			errorWorkflow: 'Error Workflow',
+			timezone: 'Timezone',
+			saveDataErrorExecution: 'Save Data Error Execution', // colon
+			saveDataSuccessExecution: 'Save Data Success Execution',
+			saveManualExecutions: 'Save Manual Executions',
+			saveExecutionProgress: 'Save Execution Progress',
+			timeoutWorkflow: 'Timeout Workflow',
+			editExpression: 'Edit Expression',
+			timeoutAfter: 'Timeout After',
+			hours: 'hours',
+			minutes: 'minutes',
+			seconds: 'seconds',
+			save: 'Save',
+		},
+
 		nodeView: {
 			currentlyNoItemsExist: 'Currently no items exist',
 			parameters: 'Parameters',
 			settings: 'Settings',
 		},
-		runData: {
-			dataReturnedByTheNodeWillDisplayHere: 'Data returned by this node will display here.',
-			noData: 'No data',
-			items: 'Items',
-		},
-		"n8n-nodes-base": {
-			start: {
-				hello: "hi",
-			},
-		},
+
+		'n8n-nodes-base': {},
 	},
 };
