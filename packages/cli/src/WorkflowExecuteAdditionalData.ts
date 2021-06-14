3d4d6bd4da03eee572699c09e6ff035d1a8ab4c7
Omar Ajoue <krynble@gmail.com>
2021-06-14 15:13:03 +0200

    Adding more log messages to error workflows

--- packages/cli/src/WorkflowExecuteAdditionalData.ts
+++ packages/cli/src/WorkflowExecuteAdditionalData.ts
@@ -387,7 +387,12 @@ function hookFunctionsSave(parentProcessMode?: string): IWorkflowExecuteHooks {
 					}
 
 					// Leave log message before flatten as that operation increased memory usage a lot and the chance of a crash is highest here
-					Logger.debug(`Save execution data to database for execution ID ${this.executionId}`, { executionId: this.executionId, workflowId: this.workflowData.id });
+					Logger.debug(`Save execution data to database for execution ID ${this.executionId}`, { 
+						executionId: this.executionId, 
+						workflowId: this.workflowData.id, 
+						finished: fullExecutionData.finished,
+						stoppedAt: fullExecutionData.stoppedAt,
+					});
 
 					const executionData = ResponseHelper.flattenExecutionData(fullExecutionData);
 
@@ -404,6 +409,12 @@ function hookFunctionsSave(parentProcessMode?: string): IWorkflowExecuteHooks {
 						executeErrorWorkflow(this.workflowData, fullRunData, this.mode, this.executionId, this.retryOf);
 					}
 				} catch (error) {
+					Logger.error(`Failed saving execution data to DB on execution ID ${this.executionId}`, { 
+						executionId: this.executionId, 
+						workflowId: this.workflowData.id,
+						error,
+					});
+					
 					if (!isManualMode) {
 						executeErrorWorkflow(this.workflowData, fullRunData, this.mode, undefined, this.retryOf);
 					}
