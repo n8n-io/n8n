de856bc2023204b8b6fc809f1ed393a686c603c2
Iván Ovejero <ivov.src@gmail.com>
2021-06-29 17:44:26 +0200

    :zap: Fix workflow ID type on download

--- packages/editor-ui/src/Interface.ts
+++ packages/editor-ui/src/Interface.ts
@@ -202,7 +202,7 @@ export interface IVariableSelectorOption {
 
 // Simple version of n8n-workflow.Workflow
 export interface IWorkflowData {
-	id?: string;
+	id?: string | number;
 	name?: string;
 	active?: boolean;
 	nodes: INode[];
@@ -212,7 +212,7 @@ export interface IWorkflowData {
 }
 
 export interface IWorkflowDataUpdate {
-	id?: string;
+	id?: string | number;
 	name?: string;
 	nodes?: INode[];
 	connections?: IConnections;
