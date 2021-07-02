594e23f48e5077058bbdb6d47e890dcc9633476a
Iván Ovejero <ivov.src@gmail.com>
2021-07-02 11:17:10 +0200

    :zap: Simplify Mattermost types

--- packages/nodes-base/nodes/Mattermost/v1/actions/Interfaces.ts
+++ packages/nodes-base/nodes/Mattermost/v1/actions/Interfaces.ts
@@ -1,21 +1,24 @@
-type MattermostChannel = {
-	resource: 'channel';
-	operation: 'addUser' | 'create' | 'del' | 'members' | 'restore' | 'statistics';
+import {
+	AllEntities,
+	Entity,
+	PropertiesOf,
+} from 'n8n-workflow';
+
+type MattermostMap = {
+	channel: 'addUser' | 'create' | 'delete' | 'members' | 'restore' | 'statistics';
+	message: 'delete' | 'post' | 'postEphemeral';
+	reaction: 'create' | 'delete' | 'getAll';
+	user: 'create' | 'deactive' | 'getAll' | 'getById' | 'invite';
 };
 
-type MattermostMessage = {
-	resource: 'message';
-	operation: 'del' | 'post' | 'postEphemeral';
-};
+export type Mattermost = AllEntities<MattermostMap>;
 
-type MattermostReaction = {
-	resource: 'reaction';
-	operation: 'create' | 'del' | 'getAll';
-};
+export type MattermostChannel = Entity<MattermostMap, 'channel'>;
+export type MattermostMessage = Entity<MattermostMap, 'message'>;
+export type MattermostReaction = Entity<MattermostMap, 'reaction'>;
+export type MattermostUser = Entity<MattermostMap, 'user'>;
 
-type MattermostUser = {
-	resource: 'user';
-	operation: 'create' | 'deactive' | 'getAll' | 'getById' | 'invite';
-};
-
-type Mattermost = MattermostChannel | MattermostMessage | MattermostReaction | MattermostUser;
+export type ChannelProperties = PropertiesOf<MattermostChannel>;
+export type MessageProperties = PropertiesOf<MattermostMessage>;
+export type ReactionProperties = PropertiesOf<MattermostReaction>;
+export type UserProperties = PropertiesOf<MattermostUser>;
