a477323e2addd4e033d9f40c0a47c54467ec41a9
Iván Ovejero <ivov.src@gmail.com>
2021-07-02 13:44:59 +0200

    :zap: Fix API call function type

--- packages/nodes-base/nodes/Mattermost/v1/transport/index.ts
+++ packages/nodes-base/nodes/Mattermost/v1/transport/index.ts
@@ -21,7 +21,7 @@ export async function apiRequest(
 	this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions,
 	method: string,
 	endpoint: string,
-	body: IDataObject = {},
+	body: object = {},
 	qs: IDataObject = {},
 ) {
 	const credentials = this.getCredentials('mattermostApi');
