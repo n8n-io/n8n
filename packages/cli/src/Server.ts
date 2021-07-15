b372ebe62c1b2fd1708710437a3e504a8e66b6f5
Iván Ovejero <ivov.src@gmail.com>
2021-07-15 10:15:38 +0200

    :rewind: Revert "Reformat workflow deletion date to UTC"
    This reverts commit ca9628bc6d2b7d5786e45e8000086a3126b880ae.

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -119,8 +119,6 @@ import { TagEntity } from './databases/entities/TagEntity';
 import { WorkflowEntity } from './databases/entities/WorkflowEntity';
 import { WorkflowNameRequest } from './WorkflowHelpers';
 
-import { DateUtils } from 'typeorm/util/DateUtils';
-
 class App {
 
 	app: express.Application;
@@ -1787,11 +1785,8 @@ class App {
 			const deleteData = req.body as IExecutionDeleteFilter;
 
 			if (deleteData.deleteBefore !== undefined) {
-				// date reformatting needed - see https://github.com/typeorm/typeorm/issues/2286
-				const utcDeleteBefore = DateUtils.mixedDateToUtcDatetimeString(deleteData.deleteBefore);
-
 				const filters = {
-					startedAt: LessThanOrEqual(utcDeleteBefore),
+					startedAt: LessThanOrEqual(deleteData.deleteBefore),
 				};
 				if (deleteData.filters !== undefined) {
 					Object.assign(filters, deleteData.filters);
