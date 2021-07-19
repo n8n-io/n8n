ac95ff26bf365208f2955416f6080fe21f167630
Iván Ovejero <ivov.src@gmail.com>
2021-07-19 12:33:43 +0200

    :fire: Remove logging

--- packages/nodes-base/nodes/Google/Perspective/GenericFunctions.ts
+++ packages/nodes-base/nodes/Google/Perspective/GenericFunctions.ts
@@ -33,7 +33,6 @@ export async function googleApiRequest(
 	}
 
 	try {
-		console.log(options);
 		return await this.helpers.requestOAuth2.call(this, 'googlePerspectiveOAuth2Api', options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
