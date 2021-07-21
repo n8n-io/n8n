5fa588193c8b37f0e0d7b0d1e291bb8b68f1fd07
Iván Ovejero <ivov.src@gmail.com>
2021-07-21 16:07:30 +0200

    :pencil2: Add credentials instructions

--- packages/nodes-base/credentials/ClerkApi.credentials.ts
+++ packages/nodes-base/credentials/ClerkApi.credentials.ts
@@ -11,7 +11,7 @@ export class ClerkApi implements ICredentialType {
 			displayName: 'Webhook Secret',
 			name: 'webhookSecret',
 			type: 'string',
-			description: 'Secret to verify webhook signatures',
+			description: 'Secret to verify webhook signatures. Access your Clerk instance dashboard, click on Integrations, select Svix, add an endpoint, click on the new endpoint, and find the secret under "Signing Secret".',
 			placeholder: 'whsec_t6282haDhoSLqiamshd9X2deLWCmk3CA',
 			default: '',
 		},
