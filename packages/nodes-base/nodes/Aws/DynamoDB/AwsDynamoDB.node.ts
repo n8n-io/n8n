3a3806da8bc9c874aaa0fe0f478f27e52552c84e
Michael Hirschler <michael.vhirsch@gmail.com>
2021-07-23 11:35:51 +0200

    aws dynamodb: fixes using FilterExpression on Query

--- packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
+++ packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
@@ -312,15 +312,17 @@ export class AwsDynamoDB implements INodeType {
 						const {
 							indexName,
 							projectionExpression,
+							filterExpression,
 						} = this.getNodeParameter('options', i) as {
 							indexName: string;
 							projectionExpression: string;
+							filterExpression: string;
 						};
 
 						const expressionAttributeName = adjustExpressionAttributeName(eanUi);
 
 						if (Object.keys(expressionAttributeName).length) {
-							body.expressionAttributeNames = expressionAttributeName;
+							body.ExpressionAttributeNames = expressionAttributeName;
 						}
 
 						if (indexName) {
@@ -329,6 +331,10 @@ export class AwsDynamoDB implements INodeType {
 
 						if (projectionExpression && select !== 'COUNT') {
 							body.ProjectionExpression = projectionExpression;
+						}
+
+						if (filterExpression) {
+							body.FilterExpression = filterExpression;
 						}
 
 						if (select) {
