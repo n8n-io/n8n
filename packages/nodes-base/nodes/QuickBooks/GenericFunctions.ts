d9e2d45959a7140250719371185d6afb169c7630
Iván Ovejero <ivov.src@gmail.com>
2021-08-02 17:04:24 +0200

    :fire: Remove logging

--- packages/nodes-base/nodes/QuickBooks/GenericFunctions.ts
+++ packages/nodes-base/nodes/QuickBooks/GenericFunctions.ts
@@ -101,7 +101,6 @@ export async function quickBooksApiRequest(
 	}
 
 	try {
-		console.log(options);
 		return await this.helpers.requestOAuth2!.call(this, 'quickBooksOAuth2Api', options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
