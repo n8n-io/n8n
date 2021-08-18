314a7d9753d9fdce8fab961c6a6fd3f0ecd817e6
Mutasem <mutdmour@gmail.com>
2021-08-18 20:17:19 +0200

    fix type

--- packages/editor-ui/src/modules/credentials.ts
+++ packages/editor-ui/src/modules/credentials.ts
@@ -60,7 +60,7 @@ const module: Module<ICredentialsState, IRootState> = {
 			return Object.values(state.credentials)
 				.sort((a, b) => a.name.localeCompare(b.name));
 		},
-		allCredentialsByType(state: ICredentialsState, getters: any): {[type: string]: ICredentialsResponse[]} {
+		allCredentialsByType(state: ICredentialsState, getters: any): {[type: string]: ICredentialsResponse[]} { // tslint:disable-line:no-any
 			const credentials = getters.allCredentials as ICredentialsResponse[];
 			const types = getters.allCredentialTypes as ICredentialType[];
 
@@ -76,10 +76,10 @@ const module: Module<ICredentialsState, IRootState> = {
 		getCredentialById: (state: ICredentialsState) => {
 			return (id: string) => state.credentials[id];
 		},
-		getCredentialByName: (state: ICredentialsState, getters: any) => {
+		getCredentialByName: (state: ICredentialsState, getters: any) => { // tslint:disable-line:no-any
 			return (name: string) => getters.allCredentials.find((cred: ICredentialsResponse) => cred.name === name);
 		},
-		getCredentialsByType: (state: ICredentialsState, getters: any) => {
+		getCredentialsByType: (state: ICredentialsState, getters: any) => { // tslint:disable-line:no-any
 			return (credentialType: string): ICredentialsResponse[] => {
 				return getters.allCredentials.filter((credentialData: ICredentialsResponse) => credentialData.type === credentialType);
 			};
