66fd744b0aaab8a4a81bd88a6db6777cb7f88cbd
Omar Ajoue <krynble@gmail.com>
2021-08-19 15:26:38 +0200

    Adjustments suggested by @mutdmour

--- packages/editor-ui/src/components/DuplicateWorkflowDialog.vue
+++ packages/editor-ui/src/components/DuplicateWorkflowDialog.vue
@@ -109,7 +109,7 @@ export default mixins(showMessage, workflowHelpers).extend({
 
 			this.$data.isSaving = true;
 
-			const saved = await this.saveAsNewWorkflow({name, tags: this.currentTagIds, updateWebhookUrls: true});
+			const saved = await this.saveAsNewWorkflow({name, tags: this.currentTagIds, resetWebhookUrls: true});
 
 			if (saved) {
 				this.closeDialog();
