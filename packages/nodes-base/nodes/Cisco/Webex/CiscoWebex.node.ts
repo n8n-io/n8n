91f28130adf79b18732b1bb07890182845509d91
Ahsan Virani <ahsan.virani@gmail.com>
2021-08-20 18:08:40 +0200

    :sparkles: Initial changes for binary data management (#2105)
    * introduce binary data management
    
    * cr
    
    * add binary data changes to awsS3 node
    
    * add binary data changes to Box node
    
    * add binary data changes to CiscoWebex node
    
    * add binary data changes to HumaniticAi node
    
    * add binary data changes to Jira node
    
    * add binary data changes to Line node
    
    * add binary data changes to MicrosoftOneDrive node
    
    * add binary data changes to MicrosoftOutlook node
    
    * add binary data changes to Mindee node
    
    * add binary data changes to NocoDB node
    
    * add binary data changes to Pushbullet node
    
    * add binary data changes to Pushover node
    
    * add binary data changes to Raindrop node
    
    * add binary data changes to S3 node
    
    * add binary data changes to Salesforce node
    
    * add binary data changes to Ssh node
    
    * add binary data changes to TheHive node
    
    * add binary data changes to Twist node
    
    * add binary data changes to Twitter node
    
    * remove changes not needed right now
    
    * :bug: Fix issue with multiple runs
    
    * :bug: Revert fix and add support for multiple inputs
    
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/nodes-base/nodes/Cisco/Webex/CiscoWebex.node.ts
+++ packages/nodes-base/nodes/Cisco/Webex/CiscoWebex.node.ts
@@ -1,5 +1,4 @@
 import {
-	BINARY_ENCODING,
 	IExecuteFunctions,
 } from 'n8n-core';
 
@@ -174,10 +173,11 @@ export class CiscoWebex implements INodeType {
 								const binaryPropertyName = file.binaryPropertyName as string;
 
 								const binaryData = items[i].binary![binaryPropertyName] as IBinaryData;
+								const binaryDataBuffer = await this.helpers.getBinaryDataBuffer(i, binaryPropertyName);
 
 								const formData = {
 									files: {
-										value: Buffer.from(binaryData.data, BINARY_ENCODING),
+										value: binaryDataBuffer,
 										options: {
 											filename: binaryData.fileName,
 											contentType: binaryData.mimeType,
