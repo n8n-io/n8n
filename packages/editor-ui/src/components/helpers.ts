e8aa8e38045893849b1e83b3c63d3aea8dbc4d84
Iván Ovejero <ivov.src@gmail.com>
2021-08-20 16:46:10 +0200

    :hammer: Refactor string replacement

--- packages/editor-ui/src/components/helpers.ts
+++ packages/editor-ui/src/components/helpers.ts
@@ -1,5 +1,5 @@
 export function addTargetBlank(html: string) {
-	return html.includes('a href')
-		? html.replace(/a href/g, 'a target="_blank" href')
+	return html.includes('href=')
+		? html.replace(/href=/g, 'target="_blank" href=')
 		: html;
 }
