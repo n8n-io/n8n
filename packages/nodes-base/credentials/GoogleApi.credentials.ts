ff9dc78c063f7a9c867ec9b7833ea718d6b24d71
Iván Ovejero <ivov.src@gmail.com>
2021-08-26 16:33:01 +0200

    :pencil2: Update description and placeholder

--- packages/nodes-base/credentials/GoogleApi.credentials.ts
+++ packages/nodes-base/credentials/GoogleApi.credentials.ts
@@ -14,18 +14,18 @@ export class GoogleApi implements ICredentialType {
 			name: 'email',
 			type: 'string',
 			default: '',
-			description: 'The Google Service account similar to user-808@project.iam.gserviceaccount.com.',
-
+			placeholder: 'user-808@project.iam.gserviceaccount.com',
 		},
 		{
 			displayName: 'Private Key',
 			name: 'privateKey',
 			type: 'string',
 			default: '',
-			description: 'Use the multiline editor. Make sure there are exactly 3 lines.<br />-----BEGIN PRIVATE KEY-----<br />KEY IN A SINGLE LINE<br />-----END PRIVATE KEY-----',
+			placeholder: '-----BEGIN PRIVATE KEY-----\nXIYEvQIBADANBg<...>0IhA7TMoGYPQc=\n-----END PRIVATE KEY-----\n',
+			description: 'Enter the private key located in the JSON file downloaded from Google Cloud Console',
 		},
 		{
-			displayName: ' Impersonate a User',
+			displayName: 'Impersonate a User',
 			name: 'inpersonate',
 			type: 'boolean',
 			default: false,
