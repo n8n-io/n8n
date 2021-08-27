83be078023ffec10272d7b5f50796cce26c076b0
Iván Ovejero <ivov.src@gmail.com>
2021-08-27 16:15:49 +0200

    :fire: Remove no-plusplus exceptions

--- packages/node-dev/templates/execute/simple.ts
+++ packages/node-dev/templates/execute/simple.ts
@@ -37,7 +37,6 @@ export class ClassNameReplace implements INodeType {
 		// Itterates over all input items and add the key "myString" with the
 		// value the parameter "myString" resolves to.
 		// (This could be a different value for each item in case it contains an expression)
-		// eslint-disable-next-line no-plusplus
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			myString = this.getNodeParameter('myString', itemIndex, '') as string;
 			item = items[itemIndex];
