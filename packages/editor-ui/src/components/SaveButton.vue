4a204c1593cfd5b019045a06e9e93fd7e70fe5d5
Mutasem <mutdmour@gmail.com>
2021-09-03 18:00:27 +0200

    refactor save button

---
+++ packages/editor-ui/src/components/SaveButton.vue
@@ -0,0 +1,37 @@
+<template>
+	<span :class="$style.container">
+		<n8n-button v-if="!saved || isSaving" label="Save" :disabled="isSaving" @click="$emit('click')" />
+		<span :class="$style.saved" v-else>Saved</span>
+	</span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	name: "SaveButton",
+	props: {
+		saved: {
+			type: Boolean,
+		},
+		isSaving: {
+			type: Boolean,
+		},
+	},
+});
+</script>
+
+<style lang="scss" module>
+.container {
+	width: 65px;
+}
+
+.saved {
+	color: $--custom-font-very-light;
+	font-size: 12px;
+	font-weight: 600;
+	line-height: 12px;
+	text-align: center;
+	padding: var(--spacing-2xs) var(--spacing-xs);
+}
+</style>
