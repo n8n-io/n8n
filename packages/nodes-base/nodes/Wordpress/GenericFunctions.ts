21aa22a7d4c9efc75d5dfc54926ab6d15466e8b8
ricardo <ricardoespinoza105@gmail.com>
2021-09-04 15:53:55 -0400

    :zap: Small improvement to #2097

--- packages/nodes-base/nodes/Wordpress/GenericFunctions.ts
+++ packages/nodes-base/nodes/Wordpress/GenericFunctions.ts
@@ -51,7 +51,7 @@ export async function wordpressApiRequestAllItems(this: IExecuteFunctions | ILoa
 
 	let responseData;
 
-	query.per_page = 10;
+	query.per_page = 1;
 	query.page = 0;
 
 	do {
@@ -60,7 +60,7 @@ export async function wordpressApiRequestAllItems(this: IExecuteFunctions | ILoa
 		returnData.push.apply(returnData, responseData.body);
 	} while (
 		responseData.headers['x-wp-totalpages'] !== undefined &&
-		parseInt(responseData.headers['x-wp-totalpages'], 10) != query.page
+		parseInt(responseData.headers['x-wp-totalpages'], 10) !== query.page
 	);
 
 	return returnData;
