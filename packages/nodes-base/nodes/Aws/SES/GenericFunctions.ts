4aa61cf5c90954674a0910e411ebd7e1ac8249a2
quansenB <inaki.breinbauer@gmail.com>
2021-09-14 07:21:39 +0200

    revert ses change

--- packages/nodes-base/nodes/Aws/SES/GenericFunctions.ts
+++ packages/nodes-base/nodes/Aws/SES/GenericFunctions.ts
@@ -1,7 +1,18 @@
-import { URL } from 'url';
-import { sign } from 'aws4';
-import { OptionsWithUri } from 'request';
-import { parseString as parseXml } from 'xml2js';
+import {
+	URL,
+} from 'url';
+
+import {
+	sign,
+} from 'aws4';
+
+import {
+	OptionsWithUri,
+} from 'request';
+
+import {
+	parseString,
+} from 'xml2js';
 
 import {
 	IExecuteFunctions,
@@ -11,26 +22,12 @@ import {
 } from 'n8n-core';
 
 import {
-	ICredentialDataDecryptedObject, NodeApiError, NodeOperationError, IDataObject
+	IDataObject, NodeApiError, NodeOperationError,
 } from 'n8n-workflow';
 
 import {
 	get,
 } from 'lodash';
-
-function getEndpointForService(service: string, credentials: ICredentialDataDecryptedObject): string {
-	let endpoint;
-	if (service === 'lambda' && credentials.lambdaEndpoint) {
-		endpoint = credentials.lambdaEndpoint;
-	} else if (service === 'sns' && credentials.snsEndpoint) {
-		endpoint = credentials.snsEndpoint;
-	} else if (service === 'sqs' && credentials.sqsEndpoint) {
-		endpoint = credentials.sqsEndpoint;
-	} else {
-		endpoint = `https://${service}.${credentials.region}.amazonaws.com`;
-	}
-	return (endpoint as string).replace('{region}', credentials.region as string);
-}
 
 export async function awsApiRequest(this: IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions | IWebhookFunctions, service: string, method: string, path: string, body?: string, headers?: object): Promise<any> { // tslint:disable-line:no-any
 	const credentials = await this.getCredentials('aws');
@@ -38,19 +35,18 @@ export async function awsApiRequest(this: IHookFunctions | IExecuteFunctions | I
 		throw new NodeOperationError(this.getNode(), 'No credentials got returned!');
 	}
 
-	// Concatenate path and instantiate URL object so it parses correctly query strings
-	const endpoint = new URL(getEndpointForService(service, credentials) + path);
+	const endpoint = new URL(((credentials.sesEndpoint as string || '').replace('{region}', credentials.region as string) || `https://${service}.${credentials.region}.amazonaws.com`) + path);
 
 	// Sign AWS API request with the user credentials
+
 	const signOpts = { headers: headers || {}, host: endpoint.host, method, path, body };
 	sign(signOpts, { accessKeyId: `${credentials.accessKeyId}`.trim(), secretAccessKey: `${credentials.secretAccessKey}`.trim() });
-
 
 	const options: OptionsWithUri = {
 		headers: signOpts.headers,
 		method,
 		uri: endpoint.href,
-		body: signOpts.body,
+		body: signOpts.body as string,
 	};
 
 	try {
@@ -73,7 +69,7 @@ export async function awsApiRequestSOAP(this: IHookFunctions | IExecuteFunctions
 	const response = await awsApiRequest.call(this, service, method, path, body, headers);
 	try {
 		return await new Promise((resolve, reject) => {
-			parseXml(response, { explicitArray: false }, (err, data) => {
+			parseString(response, { explicitArray: false }, (err, data) => {
 				if (err) {
 					return reject(err);
 				}
