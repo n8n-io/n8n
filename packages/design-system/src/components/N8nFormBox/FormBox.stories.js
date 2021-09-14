b1c373d8ddaf8dd3c63ad5c4fa2c41b43a9077a1
Mutasem <mutdmour@gmail.com>
2021-09-14 14:26:48 +0200

    implement form components

---
+++ packages/design-system/src/components/N8nFormBox/FormBox.stories.js
@@ -0,0 +1,53 @@
+import N8nFormBox from './FormBox.vue';
+import { action } from '@storybook/addon-actions';
+
+export default {
+	title: 'Modules/FormBox',
+	component: N8nFormBox,
+	argTypes: {
+	},
+	parameters: {
+		backgrounds: { default: '--color-background-light' },
+	},
+};
+
+const methods = {
+	onAction: action('action'),
+};
+
+const Template = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: {
+		N8nFormBox,
+	},
+	template: '<n8n-form-box v-bind="$props" @action="onAction" />',
+	methods,
+});
+
+export const FormBox = Template.bind({});
+FormBox.args = {
+	title: 'Form title',
+	inputs: [
+		{
+			name: 'email',
+			label: 'Your Email',
+			type: 'email',
+			required: true,
+		},
+		{
+			name: 'password',
+			label: 'Your Password',
+			type: 'password',
+			required: true,
+		},
+		{
+			name: 'nickname',
+			label: 'Your Nickname',
+			placeholder: 'Monty',
+		},
+	],
+	buttonText: 'Action',
+	redirectText: 'Go somewhere',
+	redirectLink: 'https://n8n.io',
+};
+
