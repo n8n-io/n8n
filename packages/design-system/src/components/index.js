0fd99a9bf369040e8308c2a6f46331fac78f052b
Mutasem <mutdmour@gmail.com>
2021-09-14 14:29:24 +0200

    add to exports

--- packages/design-system/src/components/index.js
+++ packages/design-system/src/components/index.js
@@ -1,4 +1,6 @@
 import N8nButton from './N8nButton';
+import N8nFormBox from './N8nFormBox';
+import N8nFormInput from './N8nFormInput';
 import N8nIcon from './N8nIcon';
 import N8nIconButton from './N8nIconButton';
 import N8nInput from './N8nInput';
@@ -7,13 +9,15 @@ import N8nInputNumber from './N8nInputNumber';
 import N8nInputLabel from './N8nInputLabel';
 import N8nMenu from './N8nMenu';
 import N8nMenuItem from './N8nMenuItem';
+import N8nOption from './N8nOption';
 import N8nSelect from './N8nSelect';
 import N8nSpinner from './N8nSpinner';
 import N8nTooltip from './N8nTooltip';
-import N8nOption from './N8nOption';
 
 export {
 	N8nButton,
+	N8nFormBox,
+	N8nFormInput,
 	N8nIcon,
 	N8nIconButton,
 	N8nInfoTip,
@@ -22,8 +26,8 @@ export {
 	N8nInputNumber,
 	N8nMenu,
 	N8nMenuItem,
+	N8nOption,
 	N8nSelect,
 	N8nSpinner,
 	N8nTooltip,
-	N8nOption,
 };
