188a3704799c5a128a8bf905895a0f3850b17df7
Iván Ovejero <ivov.src@gmail.com>
2021-09-22 17:48:50 +0200

    :sparkles: Elastic Security node (#2206)
    * :sparkles: Create Elastic Security node
    
    * :hammer: Place Elastic nodes in Elastic dir
    
    * :zap: Improvements
    
    * :hammer: Split credentials
    
    * :art: Fix formatting
    
    * :zap: Tolerate trailing slash
    
    * :shirt: Fix lint
    
    * :shirt: Fix lint
    
    * :bug: Fix tags filter in case:getAll
    
    * :hammer: Refactor sort options in case:getAll
    
    * :pencil2: Reword param descriptions
    
    * :fire: Remove descriptions per feedback
    
    * :bug: Fix case:getStatus operation
    
    * :pencil2: Reword param and error message
    
    * :pencil2: Reword param descriptions
    
    * :hammer: Account for empty string in owner
    
    * :pencil2: Reword param description
    
    * :pencil2: Add more tooltip descriptions
    
    * :zap: Add cred test
    
    * :pencil2: Add param description
    
    * :pencil2: Add comment dividers
    
    * :zap: Improve UX for third-party service params
    
    * :hammer: Minor tweaks per feedback
    
    * :hammer: Make getStatus naming consistent
    
    * :zap: Fix operation
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>
    Co-authored-by: Mutasem <mutdmour@gmail.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/nodes/Elastic/ElasticSecurity/descriptions/ConnectorDescription.ts
@@ -0,0 +1,268 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const connectorOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		noDataExpression: true,
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Create',
+				value: 'create',
+				description: 'Create a connector',
+			},
+		],
+		default: 'create',
+	},
+];
+
+export const connectorFields: INodeProperties[] = [
+	// ----------------------------------------
+	//           connector: create
+	// ----------------------------------------
+	{
+		displayName: 'Connector Name',
+		name: 'name',
+		description: 'Connectors allow you to send Elastic Security cases into other systems (only ServiceNow, Jira, or IBM Resilient)',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Connector Type',
+		name: 'connectorType',
+		type: 'options',
+		required: true,
+		default: '.jira',
+		options: [
+			{
+				name: 'IBM Resilient',
+				value: '.resilient',
+			},
+			{
+				name: 'Jira',
+				value: '.jira',
+			},
+			{
+				name: 'ServiceNow ITSM',
+				value: '.servicenow',
+			},
+		],
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'API URL',
+		name: 'apiUrl',
+		type: 'string',
+		description: 'URL of the third-party instance',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Email',
+		name: 'email',
+		description: 'Jira-registered email',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.jira',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'API Token',
+		name: 'apiToken',
+		description: 'Jira API token',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.jira',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Project Key',
+		name: 'projectKey',
+		description: 'Jira Project Key',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.jira',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Username',
+		name: 'username',
+		description: 'ServiceNow ITSM username',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.servicenow',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Password',
+		name: 'password',
+		description: 'ServiceNow ITSM password',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.servicenow',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'API Key ID',
+		name: 'apiKeyId',
+		description: 'IBM Resilient API key ID',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.resilient',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'API Key Secret',
+		name: 'apiKeySecret',
+		description: 'IBM Resilient API key secret',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.resilient',
+				],
+			},
+		},
+	},
+	{
+		displayName: 'Organization ID',
+		name: 'orgId',
+		description: 'IBM Resilient organization ID',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: [
+					'connector',
+				],
+				operation: [
+					'create',
+				],
+				connectorType: [
+					'.resilient',
+				],
+			},
+		},
+	},
+];
