a4800be814407b57a1c9ac0a17aaa74265cd6308
Mutasem <mutdmour@gmail.com>
2021-09-27 15:31:47 +0200

    add avatar

---
+++ packages/design-system/src/components/N8nAvatar/Avatar.vue
@@ -0,0 +1,46 @@
+<template functional>
+	<component
+		:is="$options.components.Avatar"
+		:size="$options.methods.getSize(props.size)"
+		:name="props.firstName + ' ' + props.lastName"
+		variant="marble"
+		:colors="['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']"
+	/>
+</template>
+
+<script lang="ts">
+import Avatar from 'vue2-boring-avatars';
+
+const sizes: {[size: string]: number} = {
+	large: 48,
+	medium: 40,
+};
+
+export default {
+	name: 'n8n-avatar',
+	props: {
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+		},
+		size: {
+			type: String,
+			default: 'medium',
+		},
+	},
+	components: {
+		Avatar,
+	},
+	methods: {
+		getSize(size: string): number {
+			return sizes[size];
+		},
+	},
+};
+</script>
+
+<style lang="scss" module>
+</style>
