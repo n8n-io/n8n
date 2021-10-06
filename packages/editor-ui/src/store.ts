e2daa523d492eb11bc29519989e61168a46981c0
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2021-10-06 19:13:39 +0200

    :bug: Apply default save manual executions (#2286)
    * revert savemanualexecution changes
    
    * update toasts behavior to handle default case

--- packages/editor-ui/src/store.ts
+++ packages/editor-ui/src/store.ts
@@ -56,6 +56,7 @@ const state: IRootState = {
 	pushConnectionActive: true,
 	saveDataErrorExecution: 'all',
 	saveDataSuccessExecution: 'all',
+	saveManualExecutions: false,
 	timezone: 'America/New_York',
 	stateIsDirty: false,
 	executionTimeout: -1,
@@ -496,6 +497,9 @@ export const store = new Vuex.Store({
 		setSaveDataSuccessExecution (state, newValue: string) {
 			Vue.set(state, 'saveDataSuccessExecution', newValue);
 		},
+		setSaveManualExecutions (state, saveManualExecutions: boolean) {
+			Vue.set(state, 'saveManualExecutions', saveManualExecutions);
+		},
 		setTimezone (state, timezone: string) {
 			Vue.set(state, 'timezone', timezone);
 		},
@@ -654,8 +658,8 @@ export const store = new Vuex.Store({
 		saveDataSuccessExecution: (state): string => {
 			return state.saveDataSuccessExecution;
 		},
-		saveManualExecutions: (state, getters): boolean => {
-			return !!getters.workflowSettings.saveManualExecutions;
+		saveManualExecutions: (state): boolean => {
+			return state.saveManualExecutions;
 		},
 		timezone: (state): string => {
 			return state.timezone;
