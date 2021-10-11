5b48482ece92bcbdeab8659febe623f2bca007ae
quansenB <inaki.breinbauer@gmail.com>
2021-10-11 18:12:35 +0200

    finish mentions

---
+++ packages/nodes-base/nodes/Zammad/MentionsDescription.ts
@@ -0,0 +1,87 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const MentionsDescription = [
+	// ----------------------------------
+			//         Operation: mention
+			// ----------------------------------
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: ['mention'],
+						api: ['rest']
+					}
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create an entry.'
+					},
+					{
+						name: 'List',
+						value: 'list',
+						description: 'Get data of all entries.'
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						description: 'Delete an entry.'
+					},
+				],
+				default: 'create',
+				description: 'The operation to perform.'
+			},
+			// ----------------------------------
+			//         Fields: mention
+			// ----------------------------------
+			{
+				displayName: 'Mentionabe Type',
+				name: 'mentionable_type',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['create'],
+						resource: ['mention'],
+						api: ['rest'],
+					}
+				},
+				description: 'The type of the mentionable.'
+			},
+			{
+				displayName: 'Mentionable ID',
+				name: 'mentionable_id',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['create'],
+						resource: ['mention'],
+						api: ['rest'],
+					}
+				},
+				description: 'The id of the mention.'
+			},
+			{
+				displayName: 'Mention ID',
+				name: 'id',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['delete'],
+						resource: ['mention'],
+						api: ['rest'],
+					}
+				},
+				description: 'The id of the mention.'
+			},
+] as INodeProperties[];
--- packages/nodes-base/nodes/Zammad/Zammad.node.ts
+++ packages/nodes-base/nodes/Zammad/Zammad.node.ts
@@ -6,7 +6,8 @@ import {
 	INodeType,
 	INodeTypeDescription
 } from 'n8n-workflow';
-import { customFieldOperations } from '../ConvertKit/CustomFieldDescription';
+
+import { MentionsDescription } from './MentionsDescription';
 
 import { zammadApiRequest } from './GenericFunctions';
 
@@ -589,7 +590,7 @@ export class Zammad implements INodeType {
 						api: ['rest'],
 					}
 				},
-				description: 'The email address of the organization.'
+				description: 'The name of the organization.'
 			},
 			{
 				displayName: 'ID',
@@ -824,7 +825,7 @@ export class Zammad implements INodeType {
 						api: ['rest'],
 					}
 				},
-				description: 'The email address of the group.'
+				description: 'The name of the group.'
 			},
 			{
 				displayName: 'ID',
@@ -947,6 +948,247 @@ export class Zammad implements INodeType {
 					}
 				]
 			},
+			// ----------------------------------
+			//         Operation: ticket
+			// ----------------------------------
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: ['ticket'],
+						api: ['rest']
+					}
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create an entry.'
+					},
+					{
+						name: 'Show',
+						value: 'show',
+						description: 'Get data of an entry.'
+					},
+					{
+						name: 'Search',
+						value: 'search',
+						description: 'Get data of an entry.'
+					},
+					{
+						name: 'List',
+						value: 'list',
+						description: 'Get data of all entries.'
+					},
+					{
+						name: 'Update',
+						value: 'update',
+						description: 'Update an entry.'
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						description: 'Delete an entry.'
+					},
+				],
+				default: 'create',
+				description: 'The operation to perform.'
+			},
+			// ----------------------------------
+			//         Fields: ticket
+			// ----------------------------------
+			{
+				displayName: 'Group',
+				name: 'group',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['create', 'update'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				description: 'The group of the ticket.'
+			},
+			{
+				displayName: 'ID',
+				name: 'id',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['update', 'show', 'delete'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				description: 'The ID of the ticket.'
+			},
+			{
+				displayName: 'Additional Fields',
+				name: 'optionalFields',
+				type: 'collection',
+				displayOptions: {
+					show: {
+						operation: ['create', 'update'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				default: {},
+				description: 'Additional optional fields of the ticket.',
+				placeholder: 'Add Field',
+				options: [
+					{
+						displayName: 'Domain Assignment?',
+						name: 'domain_assignment',
+						type: 'boolean',
+						default: false,
+						description: "If the tickets domain assignment is active."
+					},
+					{
+						displayName: 'Domain',
+						name: 'domain',
+						type: 'string',
+						default: '',
+						description: "The domain of the ticket."
+					},
+					{
+						displayName: 'Shared?',
+						name: 'shared',
+						type: 'boolean',
+						default: false,
+						description: "If the ticket is shared."
+					},
+					{
+						displayName: 'Active?',
+						name: 'active',
+						type: 'boolean',
+						default: false,
+						description: 'If the ticket is active.'
+					},
+					{
+						displayName: 'Note',
+						name: 'note',
+						type: 'string',
+						default: '',
+						description: "The note of the ticket."
+					},
+				]
+			},
+			{
+				displayName: 'Custom User Fields',
+				name: 'customFields',
+				placeholder: 'Add Custom Field',
+				description: 'Adds a custom field to set the value of.',
+				type: 'fixedCollection',
+				typeOptions: {
+					multipleValues: true
+				},
+				displayOptions: {
+					show: {
+						operation: ['create', 'update'],
+						resource: ['ticket'],
+						api: ['rest']
+					}
+				},
+				default: {},
+				options: [
+					{
+						name: 'fields',
+						displayName: 'Field',
+						values: [
+							{
+								displayName: 'Field Name',
+								name: 'name',
+								type: 'string',
+								default: '',
+								description: 'Name of the field to set.'
+							},
+							{
+								displayName: 'Field Value',
+								name: 'value',
+								type: 'string',
+								default: '',
+								description: 'Value of the field to set.'
+							}
+						]
+					}
+				]
+			},
+			{
+				displayName: 'Query',
+				name: 'query',
+				type: 'string',
+				default: '',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				description: 'The query to search the tickets.'
+			},
+			{
+				displayName: 'Limit',
+				name: 'limit',
+				type: 'number',
+				default: 100,
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				description: 'The limit of how many tickets to get.'
+			},
+			{
+				displayName: 'Sort By',
+				name: 'sort_by',
+				type: 'string',
+				default: '',
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				description: 'How to sort the tickets.'
+			},
+			{
+				displayName: 'Order By',
+				name: 'order_by',
+				type: 'options',
+				displayOptions: {
+					show: {
+						operation: ['search'],
+						resource: ['ticket'],
+						api: ['rest'],
+					}
+				},
+				options: [
+					{
+						name: 'Ascending',
+						value: 'asc'
+					},
+					{
+						name: 'Descending',
+						value: 'desc'
+					},
+				],
+				default: [],
+				description: 'How to order the tickets.'
+			},
+			...MentionsDescription,
 		]
 	};
 
@@ -1372,12 +1614,126 @@ export class Zammad implements INodeType {
 							qs
 						);
 					}
+				} else if (resource === 'ticket') {
 					// ----------------------------------
-					//         group:search
+					//         ticket:create
+					// ----------------------------------
+					if (operation === 'create') {
+						requestMethod = 'POST';
+						endpoint = '/api/v1/tickets';
+						body = this.getNodeParameter('optionalFields', i) as IDataObject;
+						const customFields  = this.getNodeParameter(
+							'customFields',
+							i
+							) as any;
+						if(customFields && customFields.fields && customFields.fields.length !== 0){
+							customFields.fields.forEach!((field: any) => {
+								body[field['name']] = field['value'];
+							});
+						}
+						body.group = this.getNodeParameter(
+							'group',
+							i
+						) as string;
+
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         ticket:update
+					// ----------------------------------
+					else if (operation === 'update') {
+						requestMethod = 'PUT';
+						const ticketId = this.getNodeParameter('id', i) as string;
+						endpoint = '/api/v1/tickets/' + ticketId;
+						body = this.getNodeParameter('optionalFields', i) as IDataObject;
+						const customFields  = this.getNodeParameter(
+							'customFields',
+							i
+							) as any;
+						if(customFields && customFields.fields && customFields.fields.length !== 0){
+							customFields.fields.forEach!((field: any) => {
+								body[field['name']] = field['value'];
+							});
+						}
+						body.group = this.getNodeParameter(
+							'group',
+							i
+						) as string;
+
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         ticket:delete
+					// ----------------------------------
+					else if (operation === 'delete') {
+						requestMethod = 'DELETE';
+						const ticketId = this.getNodeParameter('id', i) as string;
+						endpoint = '/api/v1/tickets/' + ticketId;
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         ticket:show
+					// ----------------------------------
+					else if (operation === 'show') {
+						requestMethod = 'GET';
+						const ticketId = this.getNodeParameter('id', i) as string;
+						endpoint = '/api/v1/tickets/' + ticketId;
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         ticket:list
+					// ----------------------------------
+					else if (operation === 'list') {
+						requestMethod = 'GET';
+						endpoint = '/api/v1/tickets';
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         ticket:search
 					// ----------------------------------
 					else if (operation === 'search') {
 						requestMethod = 'GET';
-						endpoint = '/api/v1/groups/search';
+						endpoint = '/api/v1/tickets/search';
 						qs = {} as IDataObject;
 						qs.query = this.getNodeParameter('query', i) as string;
 						qs.limit = this.getNodeParameter('limit', i)?.toString() as string;
@@ -1389,6 +1745,66 @@ export class Zammad implements INodeType {
 						if(order_by.length !== 0){
 							qs.order_by = order_by;
 						}
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+				} else if (resource === 'mention') {
+					// ----------------------------------
+					//         mention:create
+					// ----------------------------------
+					if (operation === 'create') {
+						requestMethod = 'POST';
+						endpoint = '/api/v1/mentions';
+						body.mentionable_type = this.getNodeParameter(
+							'mentionable_type',
+							i
+						) as string;
+						body.mentionable_id = this.getNodeParameter(
+							'mentionable_id',
+							i
+						) as string;
+
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+
+					// ----------------------------------
+					//         mention:delete
+					// ----------------------------------
+					else if (operation === 'delete') {
+						requestMethod = 'DELETE';
+						const mentionId = this.getNodeParameter('id', i) as string;
+						endpoint = '/api/v1/mentions/' + mentionId;
+						qs = {} as IDataObject;
+
+						responseData = await zammadApiRequest.call(
+							this,
+							requestMethod,
+							endpoint,
+							body,
+							qs
+						);
+					}
+					// ----------------------------------
+					//         mention:list
+					// ----------------------------------
+					else if (operation === 'list') {
+						requestMethod = 'GET';
+						endpoint = '/api/v1/mentions';
+						qs = {} as IDataObject;
 
 						responseData = await zammadApiRequest.call(
 							this,
