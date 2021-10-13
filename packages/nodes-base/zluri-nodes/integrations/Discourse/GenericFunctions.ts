6155e94fad05831695f48cfc7af6711ad103c14a
ankit mishra <ankit.mishra@zluri.com>
2021-10-13 19:40:12 +0530

    added discourse

---
+++ packages/nodes-base/zluri-nodes/integrations/Discourse/GenericFunctions.ts
@@ -0,0 +1,57 @@
+import {
+	OptionsWithUri,
+} from 'request';
+
+import {
+	IExecuteFunctions,
+	IExecuteSingleFunctions,
+	ILoadOptionsFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject, NodeApiError,
+} from 'n8n-workflow';
+import { getZluriCredentials } from '../util';
+
+export async function discourseApiRequest(this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, path: string, body: any = {}, qs: IDataObject = {}, option = {}): Promise<any> { // tslint:disable-line:no-any
+
+	const credentials = await getZluriCredentials.call(this as IExecuteFunctions)
+
+	const options: OptionsWithUri = {
+		headers: {
+			'Api-Key': credentials.apiKey,
+			'Api-Username': credentials.username,
+		},
+		method,
+		body,
+		qs,
+		uri: `${credentials.baseUrl}${path}`,
+		json: true,
+	};
+
+	try {
+		if (Object.keys(body).length === 0) {
+			delete options.body;
+		}
+		//@ts-ignore
+		return await this.helpers.request.call(this, options);
+	} catch (error) {
+		throw new NodeApiError(this.getNode(), error);
+	}
+}
+
+export async function discourseApiRequestAllItems(this: IExecuteFunctions | ILoadOptionsFunctions, method: string, endpoint: string, body: any = {}, query: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
+
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+	query.page = 1;
+	do {
+		responseData = await discourseApiRequest.call(this, method, endpoint, body, query);
+		returnData.push.apply(returnData, responseData);
+		query.page++;
+	} while (
+		responseData.length !== 0
+	);
+	return returnData;
+}
