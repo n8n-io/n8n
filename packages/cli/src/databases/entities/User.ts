2bc0d07bb5eeabf826a29a57b66dcfabcbfe1008
Ben Hesseldieck <b.hesseldieck@gmail.com>
2021-11-03 18:39:26 +0100

    🚧 WIP DB entities

--- packages/cli/src/databases/entities/User.ts
+++ packages/cli/src/databases/entities/User.ts
@@ -1,33 +1,77 @@
 import {
 	BeforeUpdate,
 	Column,
+	ColumnOptions,
 	CreateDateColumn,
 	Entity,
 	Index,
 	PrimaryGeneratedColumn,
 	UpdateDateColumn,
 } from 'typeorm';
-import { getTimestampSyntax, resolveDataType } from '../utils';
+import { IsEmail, Length } from 'class-validator';
+import { IDataObject } from 'n8n-workflow';
+import config = require('../../../config');
+import { DatabaseType } from '../..';
+
+function resolveDataType(dataType: string) {
+	const dbType = config.get('database.type') as DatabaseType;
+
+	const typeMap: { [key in DatabaseType]: { [key: string]: string } } = {
+		sqlite: {
+			json: 'simple-json',
+		},
+		postgresdb: {
+			datetime: 'timestamptz',
+		},
+		mysqldb: {},
+		mariadb: {},
+	};
+
+	return typeMap[dbType][dataType] ?? dataType;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function getTimestampSyntax() {
+	const dbType = config.get('database.type') as DatabaseType;
+
+	const map: { [key in DatabaseType]: string } = {
+		sqlite: "STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')",
+		postgresdb: 'CURRENT_TIMESTAMP(3)',
+		mysqldb: 'CURRENT_TIMESTAMP(3)',
+		mariadb: 'CURRENT_TIMESTAMP(3)',
+	};
+
+	return map[dbType];
+}
 
 @Entity()
 export class User {
-	@PrimaryGeneratedColumn()
-	id: number;
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
 
-	@Column({ unique: true })
-	username: string;
-
-	@Column({ unique: true })
+	@Column({ unique: true, length: 254 })
+	@IsEmail()
 	email: string;
 
-	@Column()
+	@Column({ length: 32 })
+	@Length(1, 32, { message: 'First name must be 1 to 32 characters long.' })
 	firstName: string;
 
-	@Column()
+	@Column({ length: 32 })
+	@Length(1, 32, { message: 'Last name must be 1 to 32 characters long.' })
 	lastName: string;
 
 	@Column()
 	password: string;
+
+	@Column()
+	resetPasswordToken: string;
+
+	@Column({
+		type: resolveDataType('json') as ColumnOptions['type'],
+		nullable: true,
+	})
+	personalizationAnswers: IDataObject;
 
 	@CreateDateColumn({ precision: 3, default: () => getTimestampSyntax() })
 	createdAt: Date;
