d61d6685f4358a4885ca57017c0e1736158bdb59
Omar Ajoue <krynble@gmail.com>
2021-11-15 12:26:46 +0100

    Removing default headers for PUT and PATCH as well as already existing POST

--- packages/core/src/NodeExecuteFunctions.ts
+++ packages/core/src/NodeExecuteFunctions.ts
@@ -87,6 +87,8 @@ import {
 axios.defaults.timeout = 300000;
 // Prevent axios from adding x-form-www-urlencoded headers by default
 axios.defaults.headers.post = {};
+axios.defaults.headers.put = {};
+axios.defaults.headers.patch = {};
 axios.defaults.paramsSerializer = (params) => {
 	if (params instanceof URLSearchParams) {
 		return params.toString();
