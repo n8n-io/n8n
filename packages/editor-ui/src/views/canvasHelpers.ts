22e7d5ee62b30461054d056ece448f492fe717fc
Mutasem <mutdmour@gmail.com>
2021-11-16 12:03:28 +0100

    update comment

--- packages/editor-ui/src/views/canvasHelpers.ts
+++ packages/editor-ui/src/views/canvasHelpers.ts
@@ -701,7 +701,7 @@ export const moveBackInputLabelPosition = (targetEndpoint: Endpoint) => {
 
 export const addConnectionActionsOverlay = (connection: Connection, onDelete: Function, onAdd: Function) => {
 	if (getOverlay(connection, OVERLAY_CONNECTION_ACTIONS_ID)) {
-		return; // avoid free floating actions when dragging connection from one node to another
+		return; // avoid free floating actions when moving connection from one node to another
 	}
 	connection.addOverlay([
 		'Label',
