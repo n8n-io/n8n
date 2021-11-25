e012c962f1abff149817cd0bd49af5e7d102bdc1
Mutasem <mutdmour@gmail.com>
2021-11-25 17:21:37 +0100

    center plus

--- packages/editor-ui/src/plugins/PlusEndpointType.js
+++ packages/editor-ui/src/plugins/PlusEndpointType.js
@@ -97,6 +97,10 @@
 					font-size: 8px;
 				}
 
+				.plus-container svg {
+					width: 1em;
+				}
+
 				.plus-container svg path:nth-of-type(2) {
 					fill: #7D838F;
 				}
@@ -138,7 +142,7 @@
 			</div>
 
 			<div class="plus-container">
-				<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-plus fa-w-14 Icon__medium_ctPPJ">
+				<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-plus">
 					<path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" class=""></path>
 				</svg>
 				<div class="drop-hover-message">
