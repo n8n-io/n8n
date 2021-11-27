e484341bcf90afbb63bb924dc8dbd1630566f2d3
bearbobs <be10506.16@bitmesra.ac.in>
2021-11-27 16:48:06 +0530

    :tag: update get customer module

--- packages/nodes-base/nodes/SyncroMSP/v1/actions/customer/getCustomer/description.ts
+++ packages/nodes-base/nodes/SyncroMSP/v1/actions/customer/getCustomer/description.ts
@@ -20,38 +20,4 @@ export const customerGetCustomerDescription: CustomerProperties = [
 		default: '',
 		description: 'get specific customer by id',
 	},
-	{
-		displayName: 'Additional Fields',
-		name: 'additionalFields',
-		type: 'collection',
-		placeholder: 'Add Field',
-		displayOptions: {
-			show: {
-				resource: [
-					'customer',
-				],
-				operation: [
-					'getCustomer',
-				],
-			},
-		},
-		default: {},
-		options: [
-			{
-				displayName: 'Search Query',
-				name: 'query',
-				type: 'string',
-				default: '',
-				placeholder: 'John Doe',
-				description: 'Search query, it can be anything related to customer data like name etc.',
-			},
-			{
-				displayName: 'Page',
-				name: 'page',
-				type: 'number',
-				default: 1,
-				description: 'Returns provided page of results, each page contains 25 results',
-			},
-		],
-	},
 ];
--- packages/nodes-base/nodes/SyncroMSP/v1/actions/customer/getCustomer/execute.ts
+++ packages/nodes-base/nodes/SyncroMSP/v1/actions/customer/getCustomer/execute.ts
@@ -14,17 +14,11 @@ import {
 
 export async function getCustomer(this: IExecuteFunctions, index: number): Promise<INodeExecutionData[]> {
 	const id = this.getNodeParameter('id', index) as string;
-	const additionalFields = this.getNodeParameter('additionalFields', index) as IDataObject;
 
-	let qs = {} as IDataObject;
+	const qs = {} as IDataObject;
 	const requestMethod = 'GET';
-	const endpoint = 'customers';
+	const endpoint = `customers/${id}`;
 	const body = {} as IDataObject;
-
-	if (additionalFields) {
-		qs = additionalFields;
-	}
-	qs.id=id;
 
 	let responseData;
 	responseData = await apiRequest.call(this, requestMethod, endpoint, body, qs);
