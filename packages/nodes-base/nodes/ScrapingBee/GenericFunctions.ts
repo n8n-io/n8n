13cb2756df2cd73f63d9899d2536415c3808e2f4
Iván Ovejero <ivov.src@gmail.com>
2021-12-01 13:11:25 +0100

    :shirt: Fix lint

--- packages/nodes-base/nodes/ScrapingBee/GenericFunctions.ts
+++ packages/nodes-base/nodes/ScrapingBee/GenericFunctions.ts
@@ -1,67 +1,67 @@
 import {
-  OptionsWithUri,
+	OptionsWithUri,
 } from 'request';
 
 import {
-  IExecuteFunctions,
+	IExecuteFunctions,
 } from 'n8n-core';
 
 import {
-  IDataObject,
-  INodeExecutionData,
-  JsonObject,
-  NodeApiError,
+	IDataObject,
+	INodeExecutionData,
+	JsonObject,
+	NodeApiError,
 } from 'n8n-workflow';
 
 export async function scrapingBeeApiRequest(
-  this: IExecuteFunctions,
-  method: string,
-  headers: IDataObject = {},
-  qs: IDataObject = {},
-  option: IDataObject = {},
+	this: IExecuteFunctions,
+	method: string,
+	headers: IDataObject = {},
+	qs: IDataObject = {},
+	option: IDataObject = {},
 ) {
-  const { apiKey } = await this.getCredentials('scrapingBeeApi') as { apiKey: string };
+	const { apiKey } = await this.getCredentials('scrapingBeeApi') as { apiKey: string };
 
-  const options: OptionsWithUri = {
-    uri: 'https://app.scrapingbee.com/api/v1',
-    method,
-    headers: {
-      'User-Agent': 'n8n',
-      ...headers
-    },
-    qs: {
-      api_key: apiKey,
-      ...qs
-    },
-    json: true,
-  };
+	const options: OptionsWithUri = {
+		uri: 'https://app.scrapingbee.com/api/v1',
+		method,
+		headers: {
+			'User-Agent': 'n8n',
+			...headers,
+		},
+		qs: {
+			api_key: apiKey,
+			...qs,
+		},
+		json: true,
+	};
 
-  if (Object.keys(option)) {
+	if (Object.keys(option)) {
 		Object.assign(options, option);
 	}
 
-  try {
-    return await this.helpers.request(options);
-  } catch (error) {
-    if (error.error === 'Request Time-out' || error.error === 'net::ERR_TIMED_OUT') {
-      error.message = 'The scraping request has timed out';
-    }
+	try {
+		return await this.helpers.request(options);
+	} catch (error) {
+		if (error.error === 'Request Time-out' || error.error === 'net::ERR_TIMED_OUT') {
+			error.message = 'The scraping request has timed out';
+		}
 
-    if (error.error === 'net::ERR_EMPTY_RESPONSE') {
-      return {};
-    }
+		if (error.error === 'net::ERR_EMPTY_RESPONSE') {
+			return {};
+		}
 
-    throw new NodeApiError(this.getNode(), error as JsonObject);
-  }
+		throw new NodeApiError(this.getNode(), error as JsonObject);
+	}
 }
 
 export async function handleBinaryData(
 	this: IExecuteFunctions,
 	items: INodeExecutionData[],
 	i: number,
-  method: 'get' | 'post',
-  headers: IDataObject,
-  qs: IDataObject,
+	method: 'get' | 'post',
+	headers: IDataObject,
+	qs: IDataObject,
 ) {
 	const binaryProperty = this.getNodeParameter('binaryProperty', i) as string;
 	const data = await scrapingBeeApiRequest.call(this, method, headers, qs, { encoding: null });
@@ -70,7 +70,7 @@ export async function handleBinaryData(
 	items[i].binary![binaryProperty] = await this.helpers.prepareBinaryData(data);
 	items[i].binary![binaryProperty].fileName = 'screenshot';
 	items[i].binary![binaryProperty].fileExtension = 'png';
-  items[i].json = {};
+	items[i].json = {};
 
 	return items;
 }
--- packages/nodes-base/nodes/ScrapingBee/ScrapingBee.node.ts
+++ packages/nodes-base/nodes/ScrapingBee/ScrapingBee.node.ts
@@ -1,62 +1,62 @@
 import {
-  IExecuteFunctions,
+	IExecuteFunctions,
 } from 'n8n-core';
 
 import {
-  ICredentialsDecrypted,
-  ICredentialTestFunctions,
-  IDataObject,
-  INodeExecutionData,
-  INodeType,
-  INodeTypeDescription,
-  NodeCredentialTestResult,
+	ICredentialsDecrypted,
+	ICredentialTestFunctions,
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeCredentialTestResult,
 } from 'n8n-workflow';
 
 import {
-  handleBinaryData,
-  scrapingBeeApiRequest,
+	handleBinaryData,
+	scrapingBeeApiRequest,
 } from './GenericFunctions';
 
 import {
-  scrapeAdditionalFields,
-  screenshotAdditionalFields,
+	scrapeAdditionalFields,
+	screenshotAdditionalFields,
 } from './descriptions';
 
 import {
-  ScrapingBeeAdditionalOptions,
+	ScrapingBeeAdditionalOptions,
 } from './types';
 
 import {
-  OptionsWithUri,
+	OptionsWithUri,
 } from 'request';
 
 export class ScrapingBee implements INodeType {
-  description: INodeTypeDescription = {
-    displayName: 'ScrapingBee',
-    name: 'scrapingBee',
-    icon: 'file:scrapingBee.svg',
-    group: ['input'],
-    version: 1,
-    description: 'Consume the ScrapingBee API',
-    subtitle: '={{ $parameter["operation"] + ": " + $parameter["url"] }}',
-    defaults: {
-      name: 'ScrapingBee',
-      color: '#f26c23',
-    },
-    inputs: ['main'],
-    outputs: ['main'],
-    credentials: [
-      {
-        name: 'scrapingBeeApi',
-        required: true,
-        testedBy: 'scrapingBeeApiTest',
-      },
-    ],
-    properties: [
-      {
+	description: INodeTypeDescription = {
+		displayName: 'ScrapingBee',
+		name: 'scrapingBee',
+		icon: 'file:scrapingBee.svg',
+		group: ['input'],
+		version: 1,
+		description: 'Consume the ScrapingBee API',
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["url"] }}',
+		defaults: {
+			name: 'ScrapingBee',
+			color: '#f26c23',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'scrapingBeeApi',
+				required: true,
+				testedBy: 'scrapingBeeApiTest',
+			},
+		],
+		properties: [
+			{
 				displayName: 'Operation',
 				name: 'operation',
-        noDataExpression: true,
+				noDataExpression: true,
 				type: 'options',
 				options: [
 					{
@@ -71,49 +71,49 @@ export class ScrapingBee implements INodeType {
 				default: 'scrape',
 				description: 'Resource to consume',
 			},
-      {
-        displayName: 'Target URL',
-        name: 'url',
-        type: 'string',
-        placeholder: 'https://n8n.io',
-        default: '',
-        required: true,
-      },
-      scrapeAdditionalFields,
-      {
-        displayName: 'Binary Property',
-        name: 'binaryProperty',
-        type: 'string',
-        required: true,
-        default: 'data',
-        description: 'Name of the binary property to which to write to',
-        displayOptions: {
-          show: {
-            operation: [
-              'screenshot',
-            ],
-          },
-        },
-      },
-      screenshotAdditionalFields,
-    ],
-  };
+			{
+				displayName: 'Target URL',
+				name: 'url',
+				type: 'string',
+				placeholder: 'https://n8n.io',
+				default: '',
+				required: true,
+			},
+			scrapeAdditionalFields,
+			{
+				displayName: 'Binary Property',
+				name: 'binaryProperty',
+				type: 'string',
+				required: true,
+				default: 'data',
+				description: 'Name of the binary property to which to write to',
+				displayOptions: {
+					show: {
+						operation: [
+							'screenshot',
+						],
+					},
+				},
+			},
+			screenshotAdditionalFields,
+		],
+	};
 
-  methods = {
-    credentialTest: {
+	methods = {
+		credentialTest: {
 			async scrapingBeeApiTest(this: ICredentialTestFunctions, credential: ICredentialsDecrypted): Promise<NodeCredentialTestResult> {
 				const { apiKey } = credential.data as { apiKey: string };
 
 				const options: OptionsWithUri = {
 					method: 'get',
 					uri: 'https://app.scrapingbee.com/api/v1',
-          qs: {
-            api_key: apiKey,
-            url: 'https://en.wikipedia.org',
-          },
-          headers: {
-            'User-Agent': 'n8n',
-          },
+					qs: {
+						api_key: apiKey,
+						url: 'https://en.wikipedia.org',
+					},
+					headers: {
+						'User-Agent': 'n8n',
+					},
 					json: true,
 				};
 
@@ -131,79 +131,79 @@ export class ScrapingBee implements INodeType {
 				}
 			},
 		},
-  };
+	};
 
-  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-    const items = this.getInputData();
-    const returnData: INodeExecutionData[] = [];
-    let responseData;
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
+		let responseData;
 
-    for (let i = 0; i < items.length; i++) {
+		for (let i = 0; i < items.length; i++) {
 
-      try {
-        const qs: IDataObject & { url: string } = {
-          url: this.getNodeParameter('url', i) as string,
-        };
+			try {
+				const qs: IDataObject & { url: string } = {
+					url: this.getNodeParameter('url', i) as string,
+				};
 
-        if (qs.url.includes('google.com')) {
-          qs.custom_google = true;
-        }
+				if (qs.url.includes('google.com')) {
+					qs.custom_google = true;
+				}
 
-        const {
-          method = 'get',
-          block_targets,
-          extract_rules,
-          headers: rawHeaders,
-          screenshot_size,
-          ...rest
-        } = this.getNodeParameter('additionalFields', i) as ScrapingBeeAdditionalOptions;
+				const {
+					method = 'get',
+					block_targets,
+					extract_rules,
+					headers: rawHeaders,
+					screenshot_size,
+					...rest
+				} = this.getNodeParameter('additionalFields', i) as ScrapingBeeAdditionalOptions;
 
-        if (Object.keys(rest).length) {
-          Object.assign(qs, rest);
-        }
+				if (Object.keys(rest).length) {
+					Object.assign(qs, rest);
+				}
 
-        if (block_targets?.length) {
-          block_targets.forEach(target => {
-            qs[target] = true;
-          })
-        }
+				if (block_targets?.length) {
+					block_targets.forEach(target => {
+						qs[target] = true;
+					});
+				}
 
-        if (extract_rules) {
-          qs.extract_rules = JSON.stringify(JSON.parse(decodeURI(extract_rules)))
-        }
+				if (extract_rules) {
+					qs.extract_rules = JSON.stringify(JSON.parse(decodeURI(extract_rules)));
+				}
 
-        let headers = {};
-        if (rawHeaders?.headerValues.length) {
-          qs.forward_headers = true;
-          headers = rawHeaders.headerValues.reduce<Record<string, string>>((acc, cur) => {
-            acc[`Spb-${cur.key}`] = cur.value;
-            return acc;
-          }, {})
-        }
+				let headers = {};
+				if (rawHeaders?.headerValues.length) {
+					qs.forward_headers = true;
+					headers = rawHeaders.headerValues.reduce<Record<string, string>>((acc, cur) => {
+						acc[`Spb-${cur.key}`] = cur.value;
+						return acc;
+					}, {});
+				}
 
-        if (screenshot_size) {
-          qs.screenshot = true;
-          qs[screenshot_size] = true;
-        }
+				if (screenshot_size) {
+					qs.screenshot = true;
+					qs[screenshot_size] = true;
+				}
 
-        responseData = screenshot_size
-          ? await handleBinaryData.call(this, items, i, method, headers, qs)
-          : await scrapingBeeApiRequest.call(this, method, headers, qs)
+				responseData = screenshot_size
+					? await handleBinaryData.call(this, items, i, method, headers, qs)
+					: await scrapingBeeApiRequest.call(this, method, headers, qs);
 
-      } catch (error) {
-        if (this.continueOnFail()) {
-          // @ts-ignore
-          returnData.push({ error: error.message });
-          continue;
-        }
-        throw error;
-      }
+			} catch (error) {
+				if (this.continueOnFail()) {
+					// @ts-ignore
+					returnData.push({ error: error.message });
+					continue;
+				}
+				throw error;
+			}
 
-      Array.isArray(responseData)
+			Array.isArray(responseData)
 				? returnData.push(...responseData)
 				: returnData.push(responseData);
-    }
+		}
 
-    return [this.helpers.returnJsonArray(returnData)];
-  }
+		return [this.helpers.returnJsonArray(returnData)];
+	}
 }
