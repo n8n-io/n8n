f8749eb93a210302fa79e0faed542c6b7e29196a
Flash <flash@top20talent.com>
2021-12-06 15:37:04 +0800

    mapping the UI fileds to the API

--- packages/nodes-base/nodes/FriendGrid/FriendGrid.node.ts
+++ packages/nodes-base/nodes/FriendGrid/FriendGrid.node.ts
@@ -123,7 +123,47 @@ export class FriendGrid implements INodeType {
         ],
     };
 
-    async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-        return [[]];
-    }
+async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+	let responseData;
+	const resource = this.getNodeParameter('resource', 0) as string;
+	const operation = this.getNodeParameter('operation', 0) as string;
+	//Get credentials the user provided for this node
+	const credentials = await this.getCredentials('friendGridApi') as IDataObject;
+
+	if (resource === 'contact') {
+		if (operation === 'create') {
+			// get email input
+			const email = this.getNodeParameter('email', 0) as string;
+			// get additional fields input
+			const additionalFields = this.getNodeParameter('additionalFields', 0) as IDataObject;
+			const data: IDataObject = {
+				email,
+			};
+
+			Object.assign(data, additionalFields);
+
+			//Make http request according to <https://sendgrid.com/docs/api-reference/>
+			const options: OptionsWithUri = {
+				headers: {
+					'Accept': 'application/json',
+					'Authorization': `Bearer ${credentials.apiKey}`,
+				},
+				method: 'PUT',
+				body: {
+					contacts: [
+						data,
+					],
+				},
+				uri: `https://api.sendgrid.com/v3/marketing/contacts`,
+				json: true,
+			};
+
+			responseData = await this.helpers.request(options);
+		}
+	}
+
+	// Map data to n8n data
+	return [this.helpers.returnJsonArray(responseData)];
+}
+
 }
