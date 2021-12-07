3cd8ea076f5d5c6944be54568dbc084b1a013d88
Iván Ovejero <ivov.src@gmail.com>
2021-12-07 17:46:17 +0100

    :zap: Bring setup logic to plugin

--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -175,7 +175,7 @@ import {
 	loadLanguage,
 	addNodeTranslation,
 	addHeaders,
-} from '@/i18n';
+} from '@/plugins/i18n';
 
 import '../plugins/N8nCustomConnectorType';
 import '../plugins/PlusEndpointType';
