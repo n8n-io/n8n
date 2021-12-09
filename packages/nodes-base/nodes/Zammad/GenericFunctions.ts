191728e7a38207f9072fb39ec14794c426d8a659
quansenB <inaki.breinbauer@gmail.com>
2021-12-09 19:06:42 +0100

    fix ssl circumvent

--- packages/nodes-base/nodes/Zammad/GenericFunctions.ts
+++ packages/nodes-base/nodes/Zammad/GenericFunctions.ts
@@ -43,7 +43,7 @@ export async function zammadApiRequest(
 			method,
 			qs,
 			uri: `${credentials.zammadUrl}${endpoint}`,
-			rejectUnauthorized: credentials.allowUnauthorizedCerts as boolean,
+			rejectUnauthorized: !credentials.allowUnauthorizedCerts as boolean,
 			json: true,
 		};
 
@@ -90,7 +90,7 @@ export async function zammadApiRequest(
 			method,
 			qs,
 			uri: `${credentials.zammadUrl}${endpoint}`,
-			rejectUnauthorized: credentials.allowUnauthorizedCerts as boolean,
+			rejectUnauthorized: !credentials.allowUnauthorizedCerts as boolean,
 			json: true,
 		};
 
@@ -137,7 +137,7 @@ export async function zammadApiRequest(
 			method,
 			qs,
 			uri: `${credentials.zammadUrl}${endpoint}`,
-			rejectUnauthorized: credentials.allowUnauthorizedCerts as boolean,
+			rejectUnauthorized: !credentials.allowUnauthorizedCerts as boolean,
 			json: true,
 		};
 		if (method === 'DELETE' && Object.keys(body).length !== 0) {
