61cd511feec6903efb7d117b04a602a831fd6158
quansenB <inaki.breinbauer@gmail.com>
2021-12-09 18:32:51 +0100

    add logic to upload json

--- packages/nodes-base/nodes/Aws/Firehose/AwsFirehose.node.ts
+++ packages/nodes-base/nodes/Aws/Firehose/AwsFirehose.node.ts
@@ -1,6 +1,6 @@
 import { IExecuteFunctions } from 'n8n-core';
 
-import { IDataObject, INodeExecutionData, INodeType, INodeTypeDescription,
+import { GenericValue, IDataObject, INodeExecutionData, INodeType, INodeTypeDescription,
 	NodeOperationError, } from 'n8n-workflow';
 
 import { awsApiRequestREST } from './GenericFunctions';
@@ -20,6 +20,9 @@ type Files = {
 type File = {
 	[name: string]: string;
 };
+
+type GenericValueNotUndefined = string | object | number | boolean;
+
 
 export class AwsFirehose implements INodeType {
 	description: INodeTypeDescription = {
@@ -211,30 +214,69 @@ export class AwsFirehose implements INodeType {
 
 					item = items[i];
 
-					const recordsData = this.getNodeParameter('records', i) as Records;
-					if(recordsData){
-						for( let i= 0; i < recordsData.record.length; i++){
-							const data = recordsData.record[i].data as string;
-							const buffer = Buffer.from(data as string);
+					const recordsAsJson = this.getNodeParameter('jsonParametersRecords', i) as string;
+
+					if(recordsAsJson){
+						let recordsJson = this.getNodeParameter('recordsJson', i) as Records;
+						if(typeof recordsJson === 'string'){
+							recordsJson = JSON.parse(recordsJson)
+						}
+						if(!Array.isArray(recordsJson)){
+							throw new NodeOperationError(this.getNode(), 'You must provide an array or a JSON representation of an array. Entries of this array are flexible but need to be base64 encodable. They are encoded and sent to Firehose.');
+						}
+						recordsJson.forEach((record) => {
+							const buffer = Buffer.from(record);
 							records.push({Data: buffer.toString('base64')});
+						});
+					} else {
+						const recordsData = this.getNodeParameter('records', i) as Records;
+						if(recordsData  && recordsData.record){
+							for( let i= 0; i < recordsData.record.length; i++){
+								const data = recordsData.record[i].data as string;
+								const buffer = Buffer.from(data as string);
+								records.push({Data: buffer.toString('base64')});
+							}
 						}
 					}
+					const filesAsJson = this.getNodeParameter('jsonParametersFiles', i) as string;
 
-					const filesData = this.getNodeParameter('files', i) as Files;
-					if(filesData){
-						for( let i= 0; i < filesData.file.length; i++){
-							const dataPropertyName = filesData.file[i].dataPropertyName as string;
-
+					if(filesAsJson){
+						let filesJson = this.getNodeParameter('filesJson', i) as Files;
+						if(typeof filesJson === 'string'){
+							filesJson = JSON.parse(filesJson)
+						}
+						if(!Array.isArray(filesJson)){
+							throw new NodeOperationError(this.getNode(), 'You must provide an array or a JSON representation of an array. Entries of this array need to be property names of files stored in n8n. Every file will be base4 encoded and sent to Firehose.');
+						}
+						filesJson.forEach((file) => {
 							if (item.binary === undefined) {
 								throw new NodeOperationError(this.getNode(), 'No binary data set. So file can not be read!');
 							}
 
-							if (item.binary[dataPropertyName] === undefined) {
-								throw new NodeOperationError(this.getNode(), `The binary property "${dataPropertyName}" does not exist. So file can not be read!`);
+							if (item.binary[file] === undefined) {
+								throw new NodeOperationError(this.getNode(), `The binary property "${file}" does not exist. So file can not be read!`);
 							}
 
-							const buffer = await this.helpers.getBinaryDataBuffer(i, dataPropertyName);
+							const buffer = Buffer.from(file);
 							records.push({Data: buffer.toString('base64')});
+						});
+					} else {
+						const filesData = this.getNodeParameter('files', i) as Files;
+						if(filesData && filesData.file){
+							for( let i= 0; i < filesData.file.length; i++){
+								const dataPropertyName = filesData.file[i].dataPropertyName as string;
+
+								if (item.binary === undefined) {
+									throw new NodeOperationError(this.getNode(), 'No binary data set. So file can not be read!');
+								}
+
+								if (item.binary[dataPropertyName] === undefined) {
+									throw new NodeOperationError(this.getNode(), `The binary property "${dataPropertyName}" does not exist. So file can not be read!`);
+								}
+
+								const buffer = await this.helpers.getBinaryDataBuffer(i, dataPropertyName);
+								records.push({Data: buffer.toString('base64')});
+							}
 						}
 					}
 
