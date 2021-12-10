e42bdf80f19bc7792522929bf00304857ee30f08
pemontto <pemontto@gmail.com>
2021-12-10 17:24:19 +0000

    🐛 Fix confusing placeholder text

--- packages/nodes-base/nodes/ItemLists/ItemLists.node.ts
+++ packages/nodes-base/nodes/ItemLists/ItemLists.node.ts
@@ -312,7 +312,7 @@ export class ItemLists implements INodeType {
 				typeOptions: {
 					multipleValues: true,
 				},
-				placeholder: 'Add Field To Exclude',
+				placeholder: 'Add Field To Compare',
 				default: {},
 				displayOptions: {
 					show: {
