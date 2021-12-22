9feaaa091ca1d503046ea3c94813ad67a2408a1b
Flash <flash@top20talent.com>
2021-12-22 10:18:41 +0800

    extract client operations

---
+++ packages/nodes-base/nodes/Gllue/ClientDescription.ts
@@ -0,0 +1,66 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const clientOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'client',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Get All',
+				value: 'getAll',
+				description: 'Get all clients',
+			},
+		],
+		default: 'getAll',
+		description: 'The operation to perform.',
+	},
+];
+
+export const clientFields: INodeProperties[] = [
+	/* -------------------------------------------------------------------------- */
+	/*                                 client:getAll                             */
+	/* -------------------------------------------------------------------------- */
+	{
+		displayName: 'Filters',
+		name: 'filters',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: [
+					'client',
+				],
+				operation: [
+					'getAll',
+				],
+			},
+		},
+		options: [
+			{
+				displayName: 'Query',
+				name: 'query',
+				type: 'string',
+				default: '',
+				description: 'The query field accepts gql for searching for a client.',
+			},
+			{
+				displayName: 'Fields',
+				name: 'fields',
+				type: 'string',
+				default: 'id,name',
+				description: 'The fields need to return',
+			},
+		],
+	},
+];
--- packages/nodes-base/nodes/Gllue/Gllue.node.ts
+++ packages/nodes-base/nodes/Gllue/Gllue.node.ts
@@ -12,8 +12,10 @@ import {
 import {
 	OptionsWithUri,
 } from 'request';
+import {clientFields, clientOperations} from './ClientDescription';
 
 const helpers = require('./helpers');
+
 
 export class Gllue implements INodeType {
 	description: INodeTypeDescription = {
@@ -49,29 +51,10 @@ export class Gllue implements INodeType {
 				],
 				default: 'client',
 				required: true,
-				description: 'Resource to consume',
+				description: 'Resource to client',
 			},
-			{
-				displayName: 'Operation',
-				name: 'operation',
-				type: 'options',
-				displayOptions: {
-					show: {
-						resource: [
-							'client',
-						],
-					},
-				},
-				options: [
-					{
-						name: 'list',
-						value: 'list',
-						description: 'List clients',
-					},
-				],
-				default: 'list',
-				description: 'The operation to perform.',
-			},
+			...clientOperations,
+			...clientFields,
 		],
 	};
 
@@ -79,25 +62,26 @@ export class Gllue implements INodeType {
 		let responseData;
 		const resource = this.getNodeParameter('resource', 0) as string;
 		const operation = this.getNodeParameter('operation', 0) as string;
+
 		const credentials = await this.getCredentials('gllueApi') as IDataObject;
 
 		const timestamp = helpers.getCurrentTimeStamp();
 		const token = helpers.generateTokenWithAESKey(timestamp, credentials.apiUsername, credentials.apiAesKey);
 
 		if (resource === 'client') {
-			if (operation === 'list') {
+			if (operation === 'getAll') {
 				const options: OptionsWithUri = {
 					headers: {
 						'Accept': 'application/json',
 					},
 					method: 'GET',
-					uri: `${credentials.apiHost}/rest/client/simple_list_with_ids?paginate_by=25&ordering=-id&gql=&page=1&fields='id'&private_token=${token}`,
+					uri: `${credentials.apiHost}/rest/client/simple_list_with_ids?paginate_by=25&ordering=-id&gql=&page=1&fields=id,name&private_token=${token}`,
 					json: true,
 				};
-
+				console.log(`request with ${options.uri}`);
 				responseData = await this.helpers.request(options);
 			}
 		}
-		return [this.helpers.returnJsonArray(responseData.result.client)];
+		return [this.helpers.returnJsonArray(responseData)];
 	}
 }
