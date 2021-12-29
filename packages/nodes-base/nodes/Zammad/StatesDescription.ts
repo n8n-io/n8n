7c6d3354df3aa04f4b8af3af8fdf3a1f320ac740
quansenB <inaki.breinbauer@gmail.com>
2021-12-29 11:25:39 +0100

    add zammad

---
+++ packages/nodes-base/nodes/Zammad/StatesDescription.ts
@@ -0,0 +1,221 @@
+import { INodeProperties } from 'n8n-workflow';
+
+export const statesDescription = [
+	// ----------------------------------
+	//         Operation: state
+	// ----------------------------------
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		options: [
+			{
+				name: 'Create',
+				value: 'create',
+				description: 'Create an entry.',
+			},
+			{
+				name: 'Show',
+				value: 'show',
+				description: 'Get data of an entry.',
+			},
+			{
+				name: 'List',
+				value: 'list',
+				description: 'Get data of all entries.',
+			},
+			{
+				name: 'Update',
+				value: 'update',
+				description: 'Update an entry.',
+			},
+			{
+				name: 'Delete',
+				value: 'delete',
+				description: 'Delete an entry.',
+			},
+		],
+		default: 'create',
+		description: 'The operation to perform.',
+	},
+	// ----------------------------------
+	//         Fields: state
+	// ----------------------------------
+	{
+		displayName: 'Name',
+		name: 'name',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['create', 'update'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'The name of the state.',
+	},
+	{
+		displayName: 'State Type ID',
+		name: 'state_type_id',
+		type: 'number',
+		default: 0,
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['create', 'update'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'The state type ID of the state.',
+	},
+	{
+		displayName: 'State ID',
+		name: 'id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['update', 'show', 'delete'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'The ID of the state.',
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'optionalFields',
+		type: 'collection',
+		displayOptions: {
+			show: {
+				operation: ['create', 'update'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		default: {},
+		description: 'Additional optional fields of the state.',
+		placeholder: 'Add Field',
+		options: [
+			{
+				displayName: 'Next State ID',
+				name: 'next_state_id',
+				type: 'number',
+				default: 0,
+				description: 'If ID of the next state.',
+			},
+			{
+				displayName: 'Ignore Escalation?',
+				name: 'ignore_escalation',
+				type: 'boolean',
+				default: false,
+				description: 'If escalation should be ignored.',
+			},
+			{
+				displayName: 'Active?',
+				name: 'active',
+				type: 'boolean',
+				default: false,
+				description: 'If the state is active.',
+			},
+			{
+				displayName: 'Default create?',
+				name: 'default_create',
+				type: 'boolean',
+				default: false,
+				description: 'If state is default for create.',
+			},
+			{
+				displayName: 'Default Followup?',
+				name: 'default_follow_up',
+				type: 'boolean',
+				default: false,
+				description: 'If the state is default for follow up.',
+			},
+			{
+				displayName: 'Note',
+				name: 'note',
+				type: 'string',
+				default: '',
+				description: 'The note of the state.',
+			},
+		],
+	},
+	{
+		displayName: 'Query',
+		name: 'query',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['search'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'The query to search the states.',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		default: 100,
+		displayOptions: {
+			show: {
+				operation: ['search'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'The limit of how many states to get.',
+	},
+	{
+		displayName: 'Sort By',
+		name: 'sort_by',
+		type: 'string',
+		default: '',
+		displayOptions: {
+			show: {
+				operation: ['search'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		description: 'How to sort the states.',
+	},
+	{
+		displayName: 'Order By',
+		name: 'order_by',
+		type: 'options',
+		displayOptions: {
+			show: {
+				operation: ['search'],
+				resource: ['state'],
+				api: ['rest'],
+			},
+		},
+		options: [
+			{
+				name: 'Ascending',
+				value: 'asc',
+			},
+			{
+				name: 'Descending',
+				value: 'desc',
+			},
+		],
+		default: [],
+		description: 'How to order the states.',
+	},
+] as INodeProperties[];
---
+++ packages/nodes-base/nodes/Zammad/TagsDescription.ts
@@ -0,0 +1,105 @@
+import { INodeProperties } from 'n8n-workflow';
+
+export const tagsDescription = [
+	// ----------------------------------
+	//         Operation: tag
+	// ----------------------------------
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: ['tag'],
+				api: ['rest'],
+			},
+		},
+		options: [
+			{
+				name: 'Add',
+				value: 'add',
+				description: 'Add a tag to an object.',
+			},
+			{
+				name: 'List',
+				value: 'list',
+				description: 'Get data of all tags of an object.',
+			},
+			{
+				name: 'Search',
+				value: 'search',
+				description: 'Search tags.',
+			},
+			{
+				name: 'Remove',
+				value: 'remove',
+				description: 'Delete a tag from an object.',
+			},
+		],
+		default: 'add',
+		description: 'The operation to perform.',
+	},
+	// ----------------------------------
+	//         Fields: tag
+	// ----------------------------------
+	{
+		displayName: 'Tag Name',
+		name: 'item',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['add', 'remove'],
+				resource: ['tag'],
+				api: ['rest'],
+			},
+		},
+		description: 'The name of the tag.',
+	},
+	{
+		displayName: 'Object',
+		name: 'object',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['add', 'remove', 'list'],
+				resource: ['tag'],
+				api: ['rest'],
+			},
+		},
+		description: 'The object the tag is added to.',
+	},
+	{
+		displayName: 'Object ID',
+		name: 'o_id',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['add', 'remove', 'list'],
+				resource: ['tag'],
+				api: ['rest'],
+			},
+		},
+		description: 'The id of the object.',
+	},
+	{
+		displayName: 'Search Term',
+		name: 'term',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				operation: ['search'],
+				resource: ['tag'],
+				api: ['rest'],
+			},
+		},
+		description: 'The search term to search the tags by.',
+	},
+] as INodeProperties[];
