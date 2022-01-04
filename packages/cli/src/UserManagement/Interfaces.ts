5cf997bb44a465ce63a3dd10a4033908d26c1f49
Iván Ovejero <ivov.src@gmail.com>
2022-01-04 16:18:37 +0100

    :sparkles: Create request types

--- packages/cli/src/UserManagement/Interfaces.ts
+++ packages/cli/src/UserManagement/Interfaces.ts
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-cycle */
 import { Application } from 'express';
+import express = require('express');
 import { IDataObject } from 'n8n-workflow';
 import { JwtFromRequestFunction } from 'passport-jwt';
+import { User } from '../databases/entities/User';
 
 export interface JwtToken {
 	token: string;
@@ -20,9 +23,30 @@ export interface PublicUserData {
 	lastName?: string;
 	personalizationAnswers?: IDataObject | null;
 	password?: string;
+	passwordResetToken?: string;
 }
 
 export interface N8nApp {
 	app: Application;
 	restEndpoint: string;
 }
+
+export type UserRequest = express.Request & { user: User };
+
+export type UpdateUserSettingsRequest = RequestWithCustomBody<
+	Pick<PublicUserData, 'email' | 'firstName' | 'lastName'>
+> & {
+	user: User;
+};
+
+export type UpdateUserPasswordRequest = RequestWithCustomBody<Pick<PublicUserData, 'password'>> & {
+	user: User;
+};
+
+export type PersonalizationAnswersRequest = RequestWithCustomBody<
+	Pick<PublicUserData, 'personalizationAnswers'>
+> & {
+	user: User;
+};
+
+export type RequestWithCustomBody<T> = express.Request<{}, {}, T>;
