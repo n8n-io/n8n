f90b279e91778a66a779c5a6e5c9ba1c69a25244
Karl <karl@cranmoreonsulting.com>
2022-01-05 12:22:39 +0000

    - Flatten json being returned - Create type and interface to be used when flattening json - Return an empty array when the collection name is not found

--- packages/nodes-base/nodes/MongoDb/MongoDbTrigger.node.ts
+++ packages/nodes-base/nodes/MongoDb/MongoDbTrigger.node.ts
@@ -1,6 +1,12 @@
 import {INodeType, INodeTypeDescription, ITriggerResponse} from 'n8n-workflow';
 import {ITriggerFunctions} from 'n8n-core';
-import {ChangeStream, Collection, MongoClient, MongoError} from 'mongodb';
+import {
+	ChangeEvent,
+	ChangeStream,
+	Collection,
+	MongoClient,
+	MongoError
+} from 'mongodb';
 import {validateAndResolveMongoCredentials} from './mongo.node.utils';
 import {nodeDescription} from './mongoTrigger.node.options';
 
@@ -26,6 +32,7 @@ export class MongoDbTrigger implements INodeType {
 			.listCollections({name: collectionName})
 			.hasNext((error: MongoError, isFound: boolean) => {
 				if (isFound) {
+					// The collection name is found... Continue with change stream.
 					const collection: Collection = client.db(database).collection(collectionName);
 					changeStream = getFullDocument ?
 						collection.watch({fullDocument: 'updateLookup'}) :
@@ -33,14 +40,15 @@ export class MongoDbTrigger implements INodeType {
 
 					monitorListingsUsingEventEmitter();
 				}
+				// The collection name is not found... Return an empty array.
 				else {
-					self.emit([self.helpers.returnJsonArray({error: 'Collection does not exist.'})]);
+					return self.emit([]);
 				}
 		});
 
 		async function monitorListingsUsingEventEmitter() {
-			changeStream.on('change', (next) => {
-				changedObject = JSON.parse(JSON.stringify(next));
+			changeStream.on('change', (next: ChangeEvent) => {
+				changedObject = flattenJson(JSON.stringify(next));
 				self.emit([self.helpers.returnJsonArray(changedObject)]);
 			});
 		}
@@ -52,10 +60,37 @@ export class MongoDbTrigger implements INodeType {
 
 		async function manualTriggerFunction() {
 		}
-
 		return {
 			closeFunction,
 			manualTriggerFunction,
 		};
 	}
+}
+
+function flattenJson(jsonString: string): JSON {
+	const jsonToFlatten = JSON.parse(jsonString);
+	const result = {};
+	flatten(jsonToFlatten, result);
+	return result as JSON;
+
+	function flatten(property: IData, result: IData, parentName= '') {
+		const keys = Object.keys(property);
+
+		for (const key of keys) {
+			const value = property[key];
+
+			if (typeof(value) === 'object' && !Array.isArray(value)) {
+				flatten(value as IData, result, key);
+			}
+
+			else {
+				const keyName = parentName === '' ? key : `${parentName}.${key}`;
+				result[keyName] = value;
+			}
+		}
+	}
+}
+type GenericValue = string | object | number | boolean;
+interface IData {
+	[key: string]: GenericValue | IData | GenericValue[] | IData[];
 }
