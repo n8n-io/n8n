e991c6a53701a5a0e774c3c3cd42e0f7849aefc2
Anuj Kapoor <kapoor.anuj0@gmail.com>
2022-01-07 22:49:24 +0530

    :sparkles: Add Syncromsp node (#2477)
    * Init Node
    
    * Added get customer details api for syncomsp-node
    
    * Fixed formatting bug :bug:
    
    * Updated description for query params, and removed redundant condition in router
    
    * Changed default value for page parameter
    
    * :ship: Added tickets API
    
    * :ship: Added contacts API
    
    * :ship: Added RMM Alerts API
    
    * Add customer API
    
    * Updates post code review
    
    * Rename SyncroMspAPI.credentials.ts to SyncroMspApi.credentials.ts
    
    * added create and delete method to customers module
    
    * fix liniting issue
    
    * Added Update method to customer module
    
    * Code Review Changes
    
    * :ship: Added CRUD for contacts endpoint
    
    * :ship: Added CRUD for RMM Alert endpoint
    
    * Added options for status field
    
    * :bug: fix linting issues
    
    * Init Node
    
    * Added get customer details api for syncomsp-node
    
    * Fixed formatting bug :bug:
    
    * Updated description for query params, and removed redundant condition in router
    
    * Changed default value for page parameter
    
    * :ship: Added tickets API
    
    * :ship: Added contacts API
    
    * :ship: Added RMM Alerts API
    
    * Updates post code review
    
    * Add customer API
    
    * added create and delete method to customers module
    
    * fix liniting issue
    
    * Added Update method to customer module
    
    * Rename SyncroMspAPI.credentials.ts to SyncroMspApi.credentials.ts
    
    * Code Review Changes
    
    * :ship: Added CRUD for contacts endpoint
    
    * :ship: Added CRUD for RMM Alert endpoint
    
    * Added options for status field
    
    * :bug: fix linting issues
    
    * :ship: Added CRUD for ticket endpoint
    
    * :tag: update get customer module
    
    * :tag: update get customer module
    
    * Minor bug fixes
    
    * Changed response for ticket update
    
    * :shirt: Fix lint issue
    
    * Alphabetically ordered all options
    
    * :bug: Fixed build issue
    
    * :bug: Fixed Server.ts build issue, rebased from master
    
    * :zap: Fix node issues
    
    * :zap: Fix more issues
    
    * :zap: Fixed all operations with the standard convention
    
    * :shirt: Fix lint
    
    * Fix reviewed changes
    
    * update border color
    
    * :zap: minor fixes
    
    * minor fixes
    
    * Added fallback when port in use
    
    * :zap: Minor Fixes
    
    * :zap: Hide addtional paramerts when return all is active
    
    * :zap: Fix issues with Tickets
    
    * :zap: Fix issues with Rmm
    
    * :zap: Fix issues with Customer
    
    * :shirt: Fix lint
    
    * :zap: Fix issues with Contact
    
    * :shirt: Fixed formatting issue
    
    * :zap: Return 404 in ID not found
    
    * :shirt: Fixed formatting issue
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    * :zap: Add credentials verification
    
    * :zap: Improvements
    
    * :zap: Improvements
    
    * :zap: Additional improvements
    
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

---
+++ packages/nodes-base/nodes/SyncroMSP/v1/actions/versionDescription.ts
@@ -0,0 +1,61 @@
+import {
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import * as customer from './customer';
+import * as ticket from './ticket';
+import * as contact from './contact';
+import * as rmm from './rmm';
+
+export const versionDescription: INodeTypeDescription = {
+	displayName: 'SyncroMSP',
+	name: 'syncroMsp',
+	icon: 'file:syncromsp.png',
+	group: ['output'],
+	version: 1,
+	subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+	description: 'Gets data from SyncroMSP',
+	defaults: {
+		name: 'SyncroMSP',
+		color: '#08a4ab',
+	},
+	inputs: ['main'],
+	outputs: ['main'],
+	credentials: [
+		{
+			name: 'syncroMspApi',
+			required: true,
+			testedBy: 'syncroMspApiCredentialTest',
+		},
+	],
+	properties: [
+		{
+			displayName: 'Resource',
+			name: 'resource',
+			type: 'options',
+			options: [
+				{
+					name: 'Contact',
+					value: 'contact',
+				},
+				{
+					name: 'Customer',
+					value: 'customer',
+				},
+				{
+					name: 'RMM',
+					value: 'rmm',
+				},
+				{
+					name: 'Ticket',
+					value: 'ticket',
+				},
+			],
+			default: 'contact',
+		},
+		...customer.descriptions,
+		...ticket.descriptions,
+		...contact.descriptions,
+		...rmm.descriptions,
+	],
+};
