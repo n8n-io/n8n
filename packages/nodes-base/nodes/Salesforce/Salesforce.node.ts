7bb0a5eb089a42007e0a8ceb14477600ebfc6ace
Laurens <68639333+laurens-novyx@users.noreply.github.com>
2022-01-08 12:53:57 +0100

    :sparkles: Add field AccountNumber to Salesforce Node (#2649)

--- packages/nodes-base/nodes/Salesforce/Salesforce.node.ts
+++ packages/nodes-base/nodes/Salesforce/Salesforce.node.ts
@@ -1956,6 +1956,9 @@ export class Salesforce implements INodeType {
 						if (additionalFields.shippingCity !== undefined) {
 							body.ShippingCity = additionalFields.shippingCity as string;
 						}
+						if (additionalFields.accountNumber !== undefined) {
+							body.AccountNumber = additionalFields.accountNumber as string;
+						}
 						if (additionalFields.accountSource !== undefined) {
 							body.AccountSource = additionalFields.accountSource as string;
 						}
@@ -2063,6 +2066,9 @@ export class Salesforce implements INodeType {
 						}
 						if (updateFields.shippingCity !== undefined) {
 							body.ShippingCity = updateFields.shippingCity as string;
+						}
+						if (updateFields.accountNumber !== undefined) {
+							body.AccountNumber = updateFields.accountNumber as string;
 						}
 						if (updateFields.accountSource !== undefined) {
 							body.AccountSource = updateFields.accountSource as string;
