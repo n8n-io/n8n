97d5fd879a7edfc5c4067458f0eb69bf61585aaa
Jan Oberhauser <janober@users.noreply.github.com>
2022-01-09 16:57:36 +0100

    Revert ":bug: Fix VUE_APP_PUBLIC_PATH issue (#2648)" (#2655)
    This reverts commit 8d457b90b1859068e8b9d65e6f5157c619c0425e.

--- packages/editor-ui/vue.config.js
+++ packages/editor-ui/vue.config.js
@@ -37,5 +37,5 @@ module.exports = {
 			},
 		},
 	},
-	publicPath: process.env.VUE_APP_PUBLIC_PATH ? process.env.VUE_APP_PUBLIC_PATH : '/',
+	publicPath: process.env.VUE_APP_PUBLIC_PATH && process.env.VUE_APP_PUBLIC_PATH !== '/%BASE_PATH%/' ? process.env.VUE_APP_PUBLIC_PATH : '/',
 };
