4418b618227eeb5e251e403cad7f6b46d1b7fac9
serge@cgptalent.com <serge@cgpralent.com>
2022-01-19 17:43:56 +0800

    refactor getCandidateById, updateCandidateById into GllueCandidateService class

--- packages/nodes-base/nodes/Gllue/services.ts
+++ packages/nodes-base/nodes/Gllue/services.ts
@@ -1,42 +1,24 @@
-import {IDataObject, IExecuteFunctions} from 'n8n-workflow';
+import {IDataObject,} from 'n8n-workflow';
+import {IExecuteFunctions,} from 'n8n-core';
+import {Gllue, GllueCandidateCreateEndpoint, GllueCandidateListEndpoint, N8nRequest } from './GenericFunctions';
+import { CONSENT_STATUS_CONSENTED } from './constants';
+import { prepareGllueApiUpdateData } from './helpers';
 
-import {
-	generateTokenWithAESKey,
-	getCurrentTimeStamp,
-	getResponseByUri,
-	gllueUrlBuilder,
-	prepareGllueApiUpdateData,
-	UrlParams
-} from './helpers';
-import {CONSENT_STATUS_CONSENTED} from './constants';
+export class GllueCandidateService {
+	createEndpoint: GllueCandidateCreateEndpoint;
+	listEndpoint: GllueCandidateListEndpoint;
 
-export async function getGllueToken(this: IExecuteFunctions, credentials: IDataObject): Promise<string> {
-	const timestamp = getCurrentTimeStamp();
-	const token = generateTokenWithAESKey(timestamp, credentials.apiUsername as string, credentials.apiAesKey as string);
-	return token;
-}
+	constructor(apiHost: string, token: string, request: N8nRequest) {
+		this.createEndpoint = new GllueCandidateCreateEndpoint(apiHost, token, request);
+		this.listEndpoint = new GllueCandidateListEndpoint(apiHost, token, request);
+	}
 
-export async function getCandidateById(this: IExecuteFunctions, candidateId: string) : Promise<IDataObject> {
-	const credentials = await this.getCredentials('gllueApi') as IDataObject;
-	const token = await getGllueToken.call(this, credentials);
-	const urlParams = new UrlParams('id='+candidateId, '', token, 1);
-	const uriGenerated = gllueUrlBuilder(credentials.apiHost as string, 'candidate', 'list', urlParams);
-	const responseData = await getResponseByUri(uriGenerated, this.helpers.request);
-	console.log(responseData);
-	return responseData;
-}
+	async getCandidateById(candidateId: number, fields?: string) : Promise<IDataObject> {
+		return await this.listEndpoint.get(candidateId, fields);
+	}
 
-export async function updateCandidateById(this: IExecuteFunctions, candidateId: string, updateData: IDataObject) : Promise<IDataObject> {
-	const credentials = await this.getCredentials('gllueApi') as IDataObject;
-	const token = await getGllueToken.call(this, credentials);
-	const body = prepareGllueApiUpdateData(candidateId, updateData);
-	const urlParams : UrlParams = new UrlParams('','', token);
-	const uriGenerated = gllueUrlBuilder(credentials.apiHost as string, 'candidate', 'create', urlParams);
-	return getResponseByUri(uriGenerated, this.helpers.request,'POST', body);
-}
+	async createOrUpdate(data: IDataObject, candidateId ? : string) : Promise<IDataObject> {
+		return await this.createEndpoint.post(data);
+	}
 
-export function shouldUpdateConsentStatus(currentStatus: string|undefined, newStatus: string): boolean {
-	const newStatusIsNotNull = newStatus !== undefined && newStatus !== '';
-
-	return newStatusIsNotNull && newStatus !== currentStatus && currentStatus !== CONSENT_STATUS_CONSENTED;
 }
