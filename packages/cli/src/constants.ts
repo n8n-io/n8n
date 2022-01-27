568ff56ad994a15c34b004ff6cce339522f0a1d3
Iván Ovejero <ivov.src@gmail.com>
2022-01-27 18:00:56 +0100

    :pencil2: Adjust wording in missing encryption key message

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -2,5 +2,5 @@
 
 export const RESPONSE_ERROR_MESSAGES = {
 	NO_CREDENTIAL: 'Credential not found',
-	NO_ENCRYPTION_KEY: 'No encryption key found to decrypt the credentials',
+	NO_ENCRYPTION_KEY: 'Encryption key missing to decrypt credentials',
 };
