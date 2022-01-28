c99b7ef3ebb7509e1734a97f89304aaf13b6c5b6
Iván Ovejero <ivov.src@gmail.com>
2022-01-28 12:08:47 +0100

    :sparkles: Create utils

---
+++ packages/cli/test/utils.ts
@@ -0,0 +1,10 @@
+import express = require("express");
+
+/**
+ * Log all the routes mounted on the Express app, for debugging.
+ */
+export const logRoutes = (app: express.Application) => {
+	app._router.stack.forEach((r: { route?: { path?: string } }) => {
+		if (r?.route?.path) console.log(r.route.path);
+	});
+};
