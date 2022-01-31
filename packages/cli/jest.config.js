22da727831bd7ef6d1516ed77939a125c4fef5d3
Iván Ovejero <ivov.src@gmail.com>
2022-01-31 12:10:04 +0100

    :truck: Move jest config to own file

---
+++ packages/cli/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	verbose: true,
+	transform: {
+		"^.+\\.tsx?$": "ts-jest"
+	},
+	testURL: "http://localhost/",
+	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+	testPathIgnorePatterns: [
+		"/dist/",
+		"/node_modules/"
+	],
+	moduleFileExtensions: [
+		"ts",
+		"tsx",
+		"js",
+		"json"
+	]
+}
