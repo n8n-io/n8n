121459e9f2f84c7e5d212ed6ff6893d8c4a77290
Aël Gobrecht <ael.gobrecht@gmail.com>
2022-02-02 15:05:41 +0100

    Add node versioning(V1)

---
+++ packages/nodes-base/nodes/Google/Ads/v1/actions/campaign/getAll/description.ts
@@ -0,0 +1,65 @@
+import {
+	CampaignProperties,
+} from '../../Interfaces';
+
+export const reactionGetAllDescription: CampaignProperties = [
+	{
+		displayName: 'Post ID',
+		name: 'postId',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: [
+					'campaign',
+				],
+				operation: [
+					'getAll',
+				],
+			},
+		},
+		description: 'One or more (comma-separated) posts to retrieve reactions from.',
+	},
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'campaign',
+				],
+			},
+		},
+		default: true,
+		description: 'If all results should be returned or only up to a given limit.',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		displayOptions: {
+			show: {
+				operation: [
+					'getAll',
+				],
+				resource: [
+					'campaign',
+				],
+				returnAll: [
+					false,
+				],
+			},
+		},
+		typeOptions: {
+			minValue: 1,
+			maxValue: 100,
+		},
+		default: 100,
+		description: 'How many results to return.',
+	},
+];
---
+++ packages/nodes-base/nodes/Google/Ads/v1/actions/campaign/index.ts
@@ -0,0 +1,41 @@
+import * as get from './get';
+import * as getAll from './getAll';
+
+import { INodeProperties } from 'n8n-workflow';
+
+
+export {
+	get,
+	getAll,
+};
+
+export const descriptions: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+			show: {
+				resource: [
+					'campaign',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'Get All',
+				value: 'getAll',
+				description: 'Get all the campaigns',
+			},
+			{
+				name: 'Get',
+				value: 'get',
+				description: 'Get the reactions to one campaign via its ID',
+			},
+		],
+		default: 'getAll',
+		description: 'The operation to perform',
+	},
+	...get.description,
+	...getAll.description,
+];
