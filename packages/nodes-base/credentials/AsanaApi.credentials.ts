08bb69599f8cf2270e1a226377c2c45f6f327f5f
Jan Oberhauser <jan.oberhauser@gmail.com>
2022-02-05 22:33:36 +0100

    :zap: Remove test data

--- packages/nodes-base/credentials/AsanaApi.credentials.ts
+++ packages/nodes-base/credentials/AsanaApi.credentials.ts
@@ -1,11 +1,6 @@
 import {
-	// IAuthenticateBasicAuth,
-	// IAuthenticateBearer,
-	IAuthenticateHeaderAuth,
-	// IAuthenticateQueryAuth,
-	// ICredentialDataDecryptedObject,
+	IAuthenticateBearer,
 	ICredentialType,
-	// IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
 
@@ -22,51 +17,9 @@ export class AsanaApi implements ICredentialType {
 		},
 	];
 
-
-	// async authenticate(credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
-	// 	console.log('AsanaApi.authenticate');
-	// 	requestOptions.headers!['Authorization'] = `Bearer ${credentials.accessToken}`;
-	// 	return requestOptions;
-	// }
-
-
-	// authenticate = {
-	// 	type: 'bearer',
-	// 	properties: {
-	// 		tokenPropertyName: 'accessToken', // Should be by default accessToken, only has to be defined if different
-	// 	},
-	// } as IAuthenticateBearer;
-
-
 	authenticate = {
-		type: 'headerAuth',
-		properties: {
-			name: 'Authorization',
-			value: '=Bearer {{$credentials.accessToken}}',
-		},
-	} as IAuthenticateHeaderAuth;
+		type: 'bearer',
+		properties: {},
+	} as IAuthenticateBearer;
 
-	// authenticate = {
-	// 	type: 'basicAuth',
-	// 	properties: {
-	// 		// userPropertyName: 'user',
-	// 		// passwordPropertyName: 'password',
-	// 	},
-	// } as IAuthenticateBasicAuth;
-
-	// authenticate = {
-	// 	type: 'queryAuth',
-	// 	properties: {
-	// 		key: 'accessToken',
-	// 		value: '={{$credentials.accessToken}}',
-	// 	},
-	// } as IAuthenticateQueryAuth;
-
-	// test = {
-	// 	request: {
-	// 		baseURL: 'https://app.asana.com/api/1.0',
-	// 		url: '/users/me',
-	// 		method: 'GET',
-	// 	},
-	// } as ICredentialTestRequest;
 }
