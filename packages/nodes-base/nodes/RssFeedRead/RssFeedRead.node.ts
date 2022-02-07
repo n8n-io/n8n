c87be0bc28eae913865601ba7477812660b27eab
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2022-02-07 16:53:10 +0000

    :sparkles: Added support for multiple items and a limit on the amount of items to return

--- packages/nodes-base/nodes/RssFeedRead/RssFeedRead.node.ts
+++ packages/nodes-base/nodes/RssFeedRead/RssFeedRead.node.ts
@@ -33,60 +33,84 @@ export class RssFeedRead implements INodeType {
 				required: true,
 				description: 'URL of the RSS feed.',
 			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add Option',
+				default: {},
+				options: [
+					{
+						displayName: 'Limit',
+						name: 'limit',
+						type: 'number',
+						default: '5',
+						description: 'The amount of items to return',
+					},
+				],
+			},
 		],
 	};
 
 
+
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const length = items.length as unknown as number;
+		const returnData: IDataObject[] = [];
+		let itemCount;
+		for (let i = 0; i < length; i++) {
+			try{
+				const url = this.getNodeParameter('url', i) as string;
+				const options = this.getNodeParameter('options', i) as IDataObject;
 
-		try{
-
-			const url = this.getNodeParameter('url', 0) as string;
-
-			if (!url) {
-				throw new NodeOperationError(this.getNode(), 'The parameter "URL" has to be set!');
-			}
-
-			if (!validateURL(url)){
-				throw new NodeOperationError(this.getNode(), 'The provided "URL" is not valid!');
-			}
-
-			const parser = new Parser();
-
-			let feed: Parser.Output<IDataObject>;
-			try {
-				feed = await parser.parseURL(url);
-			} catch (error) {
-				if (error.code === 'ECONNREFUSED') {
-					throw new NodeOperationError(this.getNode(), `It was not possible to connect to the URL. Please make sure the URL "${url}" it is valid!`);
+				if (options.limit) {
+					itemCount = options.limit as number;
 				}
 
-				throw new NodeOperationError(this.getNode(), error);
+				if (!url) {
+					throw new NodeOperationError(this.getNode(), 'The parameter "URL" has to be set!');
+				}
+
+				if (!validateURL(url)){
+					throw new NodeOperationError(this.getNode(), 'The provided "URL" is not valid!');
+				}
+
+				const parser = new Parser();
+
+				let feed: Parser.Output<IDataObject>;
+				try {
+					feed = await parser.parseURL(url);
+				} catch (error) {
+					if (error.code === 'ECONNREFUSED') {
+						throw new NodeOperationError(this.getNode(), `It was not possible to connect to the URL. Please make sure the URL "${url}" it is valid!`);
+					}
+					throw new NodeOperationError(this.getNode(), error);
+				}
+
+				// For now we just take the items and ignore everything else
+				if (feed.items) {
+					if (itemCount) {
+						feed.items = feed.items.slice(0, itemCount);
+					}
+					feed.items.forEach((item) => {
+						// @ts-ignore
+						returnData.push(item);
+					});
+				}
+
+			} catch (error) {
+				if (this.continueOnFail()) {
+					return this.prepareOutputData([{json:{ error: error.message }}]);
+				}
+				throw error;
 			}
-
-
-			const returnData: IDataObject[] = [];
-
-			// For now we just take the items and ignore everything else
-			if (feed.items) {
-				feed.items.forEach((item) => {
-					// @ts-ignore
-					returnData.push(item);
-				});
-			}
-
-			return [this.helpers.returnJsonArray(returnData)];
-
-		} catch (error) {
-			if (this.continueOnFail()) {
-				return this.prepareOutputData([{json:{ error: error.message }}]);
-			}
-			throw error;
 		}
+		return [this.helpers.returnJsonArray(returnData)];
 	}
 }
 
-// Utility function
+// Utility functionß
 
 function validateURL (url: string) {
 	try {
