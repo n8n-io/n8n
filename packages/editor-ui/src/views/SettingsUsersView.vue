b9a3620184528c06bfffca288bc2a67e8eea64df
Mutasem <mutdmour@gmail.com>
2022-02-07 16:29:32 +0200

    update setting users page

--- packages/editor-ui/src/views/SettingsUsersView.vue
+++ packages/editor-ui/src/views/SettingsUsersView.vue
@@ -2,12 +2,12 @@
 	<SettingsView>
 		<div :class="$style.container">
 			<div>
-				<n8n-heading size="2xlarge">Users</n8n-heading>
+				<n8n-heading size="2xlarge">{{ $locale.baseText('USERS_LABEL') }}</n8n-heading>
 				<div :class="$style.buttonContainer" v-if="!showUMSetupWarning">
 						<n8n-tooltip :disabled="isSmtpSetup" placement="bottom">
-							<div slot="content">Set up SMTP to invite users. <a href="https://docs.n8n.io/reference/user-management#smtp" target="_blank">Instructions</a></div>
+							<div slot="content">{{$locale.baseText('SETUP_SMTP_TO_INVITE_USERS_MESSAGE')}} <a href="https://docs.n8n.io/reference/user-management#smtp" target="_blank">{{$locale.baseText('INSTRUCTIONS_LABEL')}}</a></div>
 							<div>
-								<n8n-button label="Invite" @click="onInvite" size="large" :disabled="!isSmtpSetup" />
+								<n8n-button :label="$locale.baseText('INVITE_LABEL')" @click="onInvite" size="large" :disabled="!isSmtpSetup" />
 							</div>
 						</n8n-tooltip>
 				</div>
@@ -15,9 +15,9 @@
 			<div v-if="showUMSetupWarning" :class="$style.setupInfoContainer">
 				<n8n-action-box
 					emoji="😿"
-					heading="You’re missing out on user management"
-					description="Set up an owner account in order to invite other users. Once set up, each user will need to use a password to access this instance."
-					buttonText="Set up my owner account"
+					:heading="$locale.baseText('USER_MANAGEMENT_MISSING_WARNING')"
+					:description="$locale.baseText('SET_UP_TO_INVITE_USERS_WARNING')"
+					:buttonText="$locale.baseText('SET_UP_MY_ACCOUNT_LABEL')"
 					@click="redirectToSetup"
 				/>
 			</div>
@@ -81,11 +81,11 @@ export default mixins(showMessage).extend({
 
 					this.$showToast({
 						type: 'success',
-						title: 'Invite resent',
-						message: `To ${user.email}`,
+						title: this.$locale.baseText('INVITE_RESENT_LABEL'),
+						message: this.$locale.baseText('EMAIL_SENT_TO_MESSAGE', { interpolate: { email: user.email } }),
 					});
 				} catch (e) {
-					this.$showError(e, 'Problem inviting user');
+					this.$showError(e, this.$locale.baseText('USER_INVITE_ERROR'));
 				}
 			}
 		},
