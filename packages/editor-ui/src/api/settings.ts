d9b1ccc93a9c8271ce411097ba22608ad1ce8bb0
Mutasem <mutdmour@gmail.com>
2022-02-08 09:10:20 +0200

    remove unnessary async

--- packages/editor-ui/src/api/settings.ts
+++ packages/editor-ui/src/api/settings.ts
@@ -2,19 +2,19 @@ import { IRestApiContext, IN8nPrompts, IN8nValueSurveyData, IN8nUISettings } fro
 import { makeRestApiRequest, get, post } from './helpers';
 import { TEMPLATES_BASE_URL } from '@/constants';
 
-export async function getSettings(context: IRestApiContext): Promise<IN8nUISettings> {
-	return await makeRestApiRequest(context, 'GET', '/settings');
+export function getSettings(context: IRestApiContext): Promise<IN8nUISettings> {
+	return makeRestApiRequest(context, 'GET', '/settings');
 }
 
-export async function getPromptsData(instanceId: string, userId: string): Promise<IN8nPrompts> {
-	return await get(TEMPLATES_BASE_URL, '/prompts', {}, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
+export function getPromptsData(instanceId: string, userId: string): Promise<IN8nPrompts> {
+	return get(TEMPLATES_BASE_URL, '/prompts', {}, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
 }
 
-export async function submitContactInfo(instanceId: string, userId: string, email: string): Promise<void> {
-	return await post(TEMPLATES_BASE_URL, '/prompt', { email }, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
+export function submitContactInfo(instanceId: string, userId: string, email: string): Promise<void> {
+	return post(TEMPLATES_BASE_URL, '/prompt', { email }, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
 }
 
-export async function submitValueSurvey(instanceId: string, userId: string, params: IN8nValueSurveyData): Promise<IN8nPrompts> {
-	return await post(TEMPLATES_BASE_URL, '/value-survey', params, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
+export function submitValueSurvey(instanceId: string, userId: string, params: IN8nValueSurveyData): Promise<IN8nPrompts> {
+	return post(TEMPLATES_BASE_URL, '/value-survey', params, {'n8n-instance-id': instanceId, 'n8n-user-id': userId});
 }
 
