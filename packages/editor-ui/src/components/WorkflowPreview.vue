c48357a001476fd55b4ef3e6b5be1f33fc81dc35
bearbobs <kapoor.anuj0@gmail.com>
2022-02-11 14:42:13 +0530

    :pencil: fixed formatting

--- packages/editor-ui/src/components/WorkflowPreview.vue
+++ packages/editor-ui/src/components/WorkflowPreview.vue
@@ -1,83 +1,86 @@
 <template>
-	<iframe :class="{'workflow_iframe': !this.nodeView,'workflow_iframe_node_view': this.nodeView}" id="preview_iframe" href="/workflows/demo"></iframe>
+	<iframe
+		:class="{ workflow_iframe: !this.nodeView, workflow_iframe_node_view: this.nodeView }"
+		id="preview_iframe"
+		href="/workflows/demo"
+	></iframe>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue';
+import Vue from 'vue';
 
-  export default Vue.extend({
-    props: ['workflow'],
-		methods: {
-			loadWorkflow(){
-				try {
-					const workflow = JSON.parse(this.workflow);
+export default Vue.extend({
+	props: ['workflow'],
+	methods: {
+		loadWorkflow() {
+			try {
+				const workflow = JSON.parse(this.workflow);
 
-					// Workflow Check
-					if (!workflow) {
-						throw new Error('Missing workflow');
-					}
-					if (!workflow.nodes || !Array.isArray(workflow.nodes)) {
-						throw new Error('Must have an array of nodes');
-					}
-					const iframe = document.getElementById('int_iframe') as HTMLIFrameElement;
-
-					// set workflow in canvas
-					iframe.contentWindow!.postMessage(JSON.stringify(
-						{
-							command: 'openWorkflow',
-							workflow,
-						}
-					), '*');
-				} catch (e) {
-					console.log('error invalid json');
+				// Workflow Check
+				if (!workflow) {
+					throw new Error('Missing workflow');
 				}
-			},
-			receiveMessage ({ data }: MessageEvent) {
-				try {
-					const json = JSON.parse(data);
-					if (json.command === 'n8nReady') {
-						this.loadWorkflow();
-					}
-					else if (json.command === 'openNDV') {
-						// expand iframe
-						this.nodeView = true;
-					}
-					else if (json.command === 'closeNDV') {
-						// close iframe
-						this.nodeView = false;
-					}
-				} catch (e) {
-					console.log('loading');
+				if (!workflow.nodes || !Array.isArray(workflow.nodes)) {
+					throw new Error('Must have an array of nodes');
 				}
+				const iframe = document.getElementById('int_iframe') as HTMLIFrameElement;
+
+				// set workflow in canvas
+				iframe.contentWindow!.postMessage(
+					JSON.stringify({
+						command: 'openWorkflow',
+						workflow,
+					}),
+					'*',
+				);
+			} catch (e) {
+				console.log('error invalid json');
 			}
 		},
-		data () {
-			return {
-				nodeView: false
+		receiveMessage({ data }: MessageEvent) {
+			try {
+				const json = JSON.parse(data);
+				if (json.command === 'n8nReady') {
+					this.loadWorkflow();
+				} else if (json.command === 'openNDV') {
+					// expand iframe
+					this.nodeView = true;
+				} else if (json.command === 'closeNDV') {
+					// close iframe
+					this.nodeView = false;
+				}
+			} catch (e) {
+				console.log('loading');
 			}
-  	},
-		mounted () {
-			window.addEventListener('message', this.receiveMessage)
 		},
-		beforeDestroy () {
-			window.removeEventListener('message', this.receiveMessage)
-		}
-  });
+	},
+	data() {
+		return {
+			nodeView: false,
+		};
+	},
+	mounted() {
+		window.addEventListener('message', this.receiveMessage);
+	},
+	beforeDestroy() {
+		window.removeEventListener('message', this.receiveMessage);
+	},
+});
 </script>
 
 <style lang="scss" module>
-	.workflow_iframe {
-		width: 100%;
-		height: 100%;
-		border: 0;
-		padding: 10px;
-	}
-	.workflow_iframe_node_view {
-		position:fixed;
-		top:0;
-		left:0;
-		height:100%;
-		width:100%;
-		z-index:9999999;
-	}
+.workflow_iframe {
+	width: 100%;
+	height: 100%;
+	border: 0;
+	padding: 10px;
+}
+.workflow_iframe_node_view {
+	position: fixed;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 100%;
+	z-index: 9999999;
+}
 </style>
