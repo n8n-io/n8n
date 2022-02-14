a121b08f36aea36f3b03b120e71529c1b4872bd4
ahsan-virani <ahsan.virani@gmail.com>
2022-02-14 20:42:06 +0100

    CR suggestions

--- packages/cli/src/databases/postgresdb/migrations/1644422880309-AddExecutionEntityIndexes.ts
+++ packages/cli/src/databases/postgresdb/migrations/1644422880309-AddExecutionEntityIndexes.ts
@@ -15,11 +15,14 @@ export class AddExecutionEntityIndexes1644422880309 implements MigrationInterfac
 			tablePrefix = schema + '.' + tablePrefix;
 		}
 
-        await queryRunner.query(`CREATE INDEX IF NOT EXISTS 'IDX_${tablePrefixPure}11738b290192b21e3dd6cbeae9' ON ${tablePrefix}execution_entity ('workflowId') `);
-        await queryRunner.query(`CREATE INDEX IF NOT EXISTS 'IDX_${tablePrefixPure}13f37c629fb4c12ced1af027b2' ON ${tablePrefix}execution_entity ('waitTill') `);
-        await queryRunner.query(`CREATE INDEX IF NOT EXISTS 'IDX_${tablePrefixPure}58154df94c686818c99fb754ce' ON ${tablePrefix}execution_entity ('id', 'workflowId', 'waitTill') `);
-        await queryRunner.query(`CREATE INDEX IF NOT EXISTS 'IDX_${tablePrefixPure}4f474ac92be81610439aaad61e' ON ${tablePrefix}execution_entity ('id', 'workflowId', 'finished') `);
-        await queryRunner.query(`CREATE INDEX IF NOT EXISTS 'IDX_${tablePrefixPure}d160d4771aba5a0d78943edbe3' ON ${tablePrefix}execution_entity ('id', 'workflowId') `);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}c4d999a5e90784e8caccf5589d"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}ca4a71b47f28ac6ea88293a8e2"`);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}33228da131bb1112247cf52a42" ON ${tablePrefix}execution_entity ("stoppedAt") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}58154df94c686818c99fb754ce" ON ${tablePrefix}execution_entity ("workflowId", "waitTill", "id") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}4f474ac92be81610439aaad61e" ON ${tablePrefix}execution_entity ("workflowId", "finished", "id") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}72ffaaab9f04c2c1f1ea86e662" ON ${tablePrefix}execution_entity ("finished", "id") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}85b981df7b444f905f8bf50747" ON ${tablePrefix}execution_entity ("waitTill", "id") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}d160d4771aba5a0d78943edbe3" ON ${tablePrefix}execution_entity ("workflowId", "id") `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -31,11 +34,14 @@ export class AddExecutionEntityIndexes1644422880309 implements MigrationInterfac
 			tablePrefix = schema + '.' + tablePrefix;
 		}
 
-        await queryRunner.query(`DROP INDEX '${schema}'.IDX_${tablePrefixPure}d160d4771aba5a0d78943edbe3'`);
-        await queryRunner.query(`DROP INDEX '${schema}'.'IDX_${tablePrefixPure}4f474ac92be81610439aaad61e'`);
-        await queryRunner.query(`DROP INDEX '${schema}'.'IDX_${tablePrefixPure}58154df94c686818c99fb754ce'`);
-        await queryRunner.query(`DROP INDEX '${schema}'.'IDX_${tablePrefixPure}13f37c629fb4c12ced1af027b2'`);
-        await queryRunner.query(`DROP INDEX '${schema}'.'IDX_${tablePrefixPure}11738b290192b21e3dd6cbeae9'`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}d160d4771aba5a0d78943edbe3"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}85b981df7b444f905f8bf50747"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}72ffaaab9f04c2c1f1ea86e662"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}4f474ac92be81610439aaad61e"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}58154df94c686818c99fb754ce"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "${schema}"."IDX_${tablePrefixPure}33228da131bb1112247cf52a42"`);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}ca4a71b47f28ac6ea88293a8e2" ON ${tablePrefix}execution_entity ("waitTill") `);
+        await queryRunner.query(`CREATE INDEX IF NOT EXISTS "IDX_${tablePrefixPure}c4d999a5e90784e8caccf5589d" ON ${tablePrefix}execution_entity ("workflowId") `);
     }
 
 }
