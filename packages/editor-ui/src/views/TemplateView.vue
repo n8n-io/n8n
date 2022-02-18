121b7b8ef983a24724c04e115fabfec75d110914
Mutasem <mutdmour@gmail.com>
2022-02-18 17:28:00 +0200

    update user inserted event

--- packages/editor-ui/src/views/TemplateView.vue
+++ packages/editor-ui/src/views/TemplateView.vue
@@ -87,6 +87,14 @@ export default mixins(workflowHelpers).extend({
 	},
 	methods: {
 		navigateTo(id: string, page: string, e: PointerEvent) {
+			if (page === 'WorkflowTemplate') {
+				this.$store.dispatch('templates/setTemplateSessionId', null);
+				this.$telemetry.track('User inserted workflow template', {
+					template_id: id,
+					source: 'workflow',
+				});
+			}
+
 			if (e.metaKey || e.ctrlKey) {
 				const route = this.$router.resolve({ name: page, params: { id } });
 				window.open(route.href, '_blank');
