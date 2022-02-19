08fc91629f9ccdcfd0c39ee4b14216db1f0d757c
David Sha <david.sha2002@gmail.com>
2022-02-19 16:20:06 +1100

    friendgrid tutorial finished

--- packages/nodes-base/nodes/Datagma/Datagma.node.ts
+++ packages/nodes-base/nodes/Datagma/Datagma.node.ts
@@ -15,24 +15,106 @@ import {
 
 export class Datagma implements INodeType {
 	description: INodeTypeDescription = {
-			displayName: 'Datagma',
-			name: 'datagma',
-			icon: 'file:datagma.svg',
-			group: ['transform'],
-			version: 1,
-			description: 'Consume Datagma API',
-			defaults: {
-					name: 'Datagma',
-					color: '#1A82e2',
+		displayName: 'Datagma',
+		name: 'datagma',
+		icon: 'file:datagma.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume Datagma API',
+		defaults: {
+				name: 'Datagma',
+				color: '#1A82e2',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name: 'Contact',
+						value: 'contact',
+					},
+				],
+				default: 'contact',
+				required: true,
+				description: 'Resource to consume',
 			},
-			inputs: ['main'],
-			outputs: ['main'],
-			credentials: [
-			],
-			properties: [
-					// Node properties which the user gets displayed and
-					// can change on the node.
-			],
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: [
+							'contact',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						description: 'Create a contact',
+					},
+				],
+				default: 'create',
+				description: 'The operation to perform.',
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						operation: [
+							'create',
+						],
+						resource: [
+							'contact',
+						],
+					},
+				},
+				default:'',
+				description:'Primary email for the contact',
+			},
+			{
+				displayName: 'Additional Fields',
+				name: 'additionalFields',
+				type: 'collection',
+				placeholder: 'Add Field',
+				default: {},
+				displayOptions: {
+					show: {
+						resource: [
+							'contact',
+						],
+						operation: [
+							'create',
+						],
+					},
+				},
+				options: [
+					{
+						displayName: 'First Name',
+						name: 'firstName',
+						type: 'string',
+						default: '',
+					},
+					{
+						displayName: 'Last Name',
+						name: 'lastName',
+						type: 'string',
+						default: '',
+					},
+				],
+			},
+		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
