3e577c6357a1eed53091489af18587f16264110f
ahsan-virani <ahsan.virani@gmail.com>
2022-02-21 17:27:25 +0100

    catch nodegraph errors

--- packages/workflow/src/TelemetryHelpers.ts
+++ packages/workflow/src/TelemetryHelpers.ts
@@ -11,6 +11,8 @@ import {
 	INodeType,
 } from '.';
 
+import { getInstance as getLoggerInstance } from './LoggerProxy';
+
 export function getNodeTypeForName(workflow: IWorkflowBase, nodeName: string): INode | undefined {
 	return workflow.nodes.find((node) => node.name === nodeName);
 }
@@ -26,51 +28,61 @@ export function generateNodesGraph(
 	};
 	const nodeNameAndIndex: INodeNameIndex = {};
 
-	workflow.nodes.forEach((node: INode, index: number) => {
-		nodesGraph.node_types.push(node.type);
-		const nodeItem: INodeGraphItem = {
-			type: node.type,
-			position: node.position,
+	try {
+		workflow.nodes.forEach((node: INode, index: number) => {
+			nodesGraph.node_types.push(node.type);
+			const nodeItem: INodeGraphItem = {
+				type: node.type,
+				position: node.position,
+			};
+
+			if (node.type === 'n8n-nodes-base.httpRequest') {
+				try {
+					nodeItem.domain = new URL(node.parameters.url as string).hostname;
+				} catch (e) {
+					nodeItem.domain = node.parameters.url as string;
+				}
+			} else {
+				const nodeType = nodeTypes.getByName(node.type) as INodeType;
+
+				if (nodeType.description?.properties) {
+					nodeType.description.properties.forEach((property) => {
+						if (
+							property.name === 'operation' ||
+							property.name === 'resource' ||
+							property.name === 'mode'
+						) {
+							nodeItem[property.name] = property.default ? property.default.toString() : undefined;
+						}
+					});
+				}
+
+				nodeItem.operation = node.parameters.operation?.toString() ?? nodeItem.operation;
+				nodeItem.resource = node.parameters.resource?.toString() ?? nodeItem.resource;
+				nodeItem.mode = node.parameters.mode?.toString() ?? nodeItem.mode;
+			}
+			nodesGraph.nodes[`${index}`] = nodeItem;
+			nodeNameAndIndex[node.name] = index.toString();
+		});
+
+		const getGraphConnectionItem = (startNode: string, connectionItem: IConnection) => {
+			return { start: nodeNameAndIndex[startNode], end: nodeNameAndIndex[connectionItem.node] };
 		};
 
-		if (node.type === 'n8n-nodes-base.httpRequest') {
-			try {
-				nodeItem.domain = new URL(node.parameters.url as string).hostname;
-			} catch (e) {
-				nodeItem.domain = node.parameters.url as string;
-			}
-		} else {
-			const nodeType = nodeTypes.getByName(node.type) as INodeType;
-			nodeType.description.properties.forEach((property) => {
-				if (
-					property.name === 'operation' ||
-					property.name === 'resource' ||
-					property.name === 'mode'
-				) {
-					nodeItem[property.name] = property.default ? property.default.toString() : undefined;
-				}
-			});
-
-			nodeItem.operation = node.parameters.operation?.toString() ?? nodeItem.operation;
-			nodeItem.resource = node.parameters.resource?.toString() ?? nodeItem.resource;
-			nodeItem.mode = node.parameters.mode?.toString() ?? nodeItem.mode;
-		}
-		nodesGraph.nodes[`${index}`] = nodeItem;
-		nodeNameAndIndex[node.name] = index.toString();
-	});
-
-	const getGraphConnectionItem = (startNode: string, connectionItem: IConnection) => {
-		return { start: nodeNameAndIndex[startNode], end: nodeNameAndIndex[connectionItem.node] };
-	};
-
-	Object.keys(workflow.connections).forEach((nodeName) => {
-		const connections = workflow.connections[nodeName];
-		connections.main.forEach((element) => {
-			element.forEach((element2) => {
-				nodesGraph.node_connections.push(getGraphConnectionItem(nodeName, element2));
+		Object.keys(workflow.connections).forEach((nodeName) => {
+			const connections = workflow.connections[nodeName];
+			connections.main.forEach((element) => {
+				element.forEach((element2) => {
+					nodesGraph.node_connections.push(getGraphConnectionItem(nodeName, element2));
+				});
 			});
 		});
-	});
+	} catch (e) {
+		const logger = getLoggerInstance();
+		logger.warn(`Failed to generate nodes graph for workflowId: ${workflow.id as string | number}`);
+		logger.debug((e as Error).message);
+		logger.debug((e as Error).stack ?? '');
+	}
 
 	return { nodeGraph: nodesGraph, nameIndices: nodeNameAndIndex };
 }
