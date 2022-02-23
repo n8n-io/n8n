8da5a9299fe72963751fb9de2c752b3958289c5c
Iván Ovejero <ivov.src@gmail.com>
2022-02-23 11:36:11 +0100

    :fire: Remove auth middleware test wrappers

--- packages/cli/test/integration/auth.middleware.test.ts
+++ packages/cli/test/integration/auth.middleware.test.ts
@@ -4,39 +4,29 @@ import { REST_PATH_SEGMENT } from './shared/constants';
 
 import * as utils from './shared/utils';
 
-describe('/me endpoints', () => {
-	let app: express.Application;
+let app: express.Application;
 
-	beforeAll(async () => {
-		app = utils.initTestServer({ applyAuth: true });
-		utils.initLogger();
-	});
+beforeAll(async () => {
+	app = utils.initTestServer({ applyAuth: true });
+	utils.initLogger();
+});
 
-	describe('Unauthorized requests', () => {
-		['GET /me', 'PATCH /me', 'PATCH /me/password', 'POST /me/survey'].forEach((route) => {
-			const [method, endpoint] = route.split(' ').map((i) => i.toLowerCase());
+describe('Unauthorized requests', () => {
+	['GET /me', 'PATCH /me', 'PATCH /me/password', 'POST /me/survey'].forEach((route) => {
+		const [method, endpoint] = route.split(' ').map((i) => i.toLowerCase());
 
-			test(`${route} should return 401 Unauthorized`, async () => {
-				const response = await request(app)[method](endpoint).use(utils.prefix(REST_PATH_SEGMENT));
+		test(`${route} should return 401 Unauthorized`, async () => {
+			const response = await request(app)[method](endpoint).use(utils.prefix(REST_PATH_SEGMENT));
 
-				expect(response.statusCode).toBe(401);
-			});
+			expect(response.statusCode).toBe(401);
 		});
 	});
 });
 
-describe('/owner endpoint', () => {
-	let app: express.Application;
+describe('Unauthorized requests', () => {
+	test(`POST /owner should return 401 Unauthorized`, async () => {
+		const response = await request(app).post('/owner').use(utils.prefix(REST_PATH_SEGMENT));
 
-	beforeAll(async () => {
-		app = utils.initTestServer({ applyAuth: true });
-	});
-
-	describe('Unauthorized requests', () => {
-		test(`POST /owner should return 401 Unauthorized`, async () => {
-			const response = await request(app).post('/owner').use(utils.prefix(REST_PATH_SEGMENT));
-
-			expect(response.statusCode).toBe(401);
-		});
+		expect(response.statusCode).toBe(401);
 	});
 });
