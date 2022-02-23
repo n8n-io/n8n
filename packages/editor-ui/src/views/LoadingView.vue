f502b3895f6a139c9421a820cc0b7012a116e8b7
schnapsterdog <olivertrajceski@yahoo.com>
2022-02-23 12:22:11 +0100

    N8N-3012 Make Loading Screen same color as Templates

--- packages/editor-ui/src/views/LoadingView.vue
+++ packages/editor-ui/src/views/LoadingView.vue
@@ -1,10 +1,19 @@
 <template>
-	<div :class="$style.spinner">
-		<n8n-spinner />
+  <div :class="$style.wrapper">
+		<div :class="$style.spinner">
+			<n8n-spinner />
+		</div>
 	</div>
 </template>
 
 <style lang="scss" module>
+.wrapper {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	background-color: var(--color-background-light);
+}
+
 .spinner {
 	margin-bottom: var(--spacing-l);
 	position: absolute;
