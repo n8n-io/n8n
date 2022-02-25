970516521bbedb5dafe780a1f0d791b0cc02fd32
Iván Ovejero <ivov.src@gmail.com>
2022-02-25 10:13:02 +0100

    🔥 Remove test description wrappers (#2874)
    * :fire: Remove /owner test wrappers
    
    * :fire: Remove auth middleware test wrappers
    
    * :fire: Remove auth endpoints test wrappers
    
    * :fire: Remove overlooked middleware wrappers
    
    * :fire: Remove me namespace test wrappers
    
    Co-authored-by: Ben Hesseldieck <b.hesseldieck@gmail.com>

--- packages/cli/test/integration/auth.endpoints.test.ts
+++ packages/cli/test/integration/auth.endpoints.test.ts
@@ -14,129 +14,125 @@ import { getGlobalOwnerRole } from './shared/utils';
 
 let globalOwnerRole: Role;
 
-describe('auth endpoints', () => {
-	describe('Owner requests', () => {
-		let app: express.Application;
+let app: express.Application;
 
-		beforeAll(async () => {
-			app = utils.initTestServer({ namespaces: ['auth'], applyAuth: true });
-			await utils.initTestDb();
-			await utils.truncate(['User']);
+beforeAll(async () => {
+	app = utils.initTestServer({ namespaces: ['auth'], applyAuth: true });
+	await utils.initTestDb();
+	await utils.truncate(['User']);
 
-			globalOwnerRole = await getGlobalOwnerRole();
-			utils.initLogger();
-		});
+	globalOwnerRole = await getGlobalOwnerRole();
+	utils.initLogger();
+});
 
-		beforeEach(async () => {
-			await utils.createUser({
-				id: uuid(),
-				email: TEST_USER.email,
-				firstName: TEST_USER.firstName,
-				lastName: TEST_USER.lastName,
-				password: hashSync(TEST_USER.password, genSaltSync(10)),
-				role: globalOwnerRole,
-			});
-
-			config.set('userManagement.hasOwner', true);
-
-			await Db.collections.Settings!.update(
-				{ key: 'userManagement.hasOwner' },
-				{ value: JSON.stringify(true) },
-			);
-		});
-
-		afterEach(async () => {
-			await utils.truncate(['User']);
-		});
-
-		afterAll(() => {
-			return getConnection().close();
-		});
-
-		test('POST /login should log user in', async () => {
-			const authlessAgent = await utils.createAgent(app);
-
-			const response = await authlessAgent.post('/login').send({
-				email: TEST_USER.email,
-				password: TEST_USER.password,
-			});
-
-			expect(response.statusCode).toBe(200);
-
-			const {
-				id,
-				email,
-				firstName,
-				lastName,
-				password,
-				personalizationAnswers,
-				globalRole,
-				resetPasswordToken,
-			} = response.body.data;
-
-			expect(validator.isUUID(id)).toBe(true);
-			expect(email).toBe(TEST_USER.email);
-			expect(firstName).toBe(TEST_USER.firstName);
-			expect(lastName).toBe(TEST_USER.lastName);
-			expect(password).toBeUndefined();
-			expect(personalizationAnswers).toBeNull();
-			expect(password).toBeUndefined();
-			expect(resetPasswordToken).toBeUndefined();
-			expect(globalRole).toBeDefined();
-			expect(globalRole.name).toBe('owner');
-			expect(globalRole.scope).toBe('global');
-
-			const authToken = utils.getAuthToken(response);
-			expect(authToken).toBeDefined();
-		});
-
-		test('GET /login should receive logged in user', async () => {
-			const owner = await Db.collections.User!.findOneOrFail();
-			const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
-
-			const response = await authOwnerAgent.get('/login');
-
-			expect(response.statusCode).toBe(200);
-
-			const {
-				id,
-				email,
-				firstName,
-				lastName,
-				password,
-				personalizationAnswers,
-				globalRole,
-				resetPasswordToken,
-			} = response.body.data;
-
-			expect(validator.isUUID(id)).toBe(true);
-			expect(email).toBe(TEST_USER.email);
-			expect(firstName).toBe(TEST_USER.firstName);
-			expect(lastName).toBe(TEST_USER.lastName);
-			expect(password).toBeUndefined();
-			expect(personalizationAnswers).toBeNull();
-			expect(password).toBeUndefined();
-			expect(resetPasswordToken).toBeUndefined();
-			expect(globalRole).toBeDefined();
-			expect(globalRole.name).toBe('owner');
-			expect(globalRole.scope).toBe('global');
-
-			expect(response.headers['set-cookie']).toBeUndefined();
-		});
-
-		test('POST /logout should log user out', async () => {
-			const owner = await Db.collections.User!.findOneOrFail();
-			const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
-
-			const response = await authOwnerAgent.post('/logout');
-
-			expect(response.statusCode).toBe(200);
-			expect(response.body).toEqual(LOGGED_OUT_RESPONSE_BODY);
-
-			const authToken = utils.getAuthToken(response);
-			expect(authToken).toBeUndefined();
-		});
+beforeEach(async () => {
+	await utils.createUser({
+		id: uuid(),
+		email: TEST_USER.email,
+		firstName: TEST_USER.firstName,
+		lastName: TEST_USER.lastName,
+		password: hashSync(TEST_USER.password, genSaltSync(10)),
+		role: globalOwnerRole,
 	});
+
+	config.set('userManagement.hasOwner', true);
+
+	await Db.collections.Settings!.update(
+		{ key: 'userManagement.hasOwner' },
+		{ value: JSON.stringify(true) },
+	);
+});
+
+afterEach(async () => {
+	await utils.truncate(['User']);
+});
+
+afterAll(() => {
+	return getConnection().close();
+});
+
+test('POST /login should log user in', async () => {
+	const authlessAgent = await utils.createAgent(app);
+
+	const response = await authlessAgent.post('/login').send({
+		email: TEST_USER.email,
+		password: TEST_USER.password,
+	});
+
+	expect(response.statusCode).toBe(200);
+
+	const {
+		id,
+		email,
+		firstName,
+		lastName,
+		password,
+		personalizationAnswers,
+		globalRole,
+		resetPasswordToken,
+	} = response.body.data;
+
+	expect(validator.isUUID(id)).toBe(true);
+	expect(email).toBe(TEST_USER.email);
+	expect(firstName).toBe(TEST_USER.firstName);
+	expect(lastName).toBe(TEST_USER.lastName);
+	expect(password).toBeUndefined();
+	expect(personalizationAnswers).toBeNull();
+	expect(password).toBeUndefined();
+	expect(resetPasswordToken).toBeUndefined();
+	expect(globalRole).toBeDefined();
+	expect(globalRole.name).toBe('owner');
+	expect(globalRole.scope).toBe('global');
+
+	const authToken = utils.getAuthToken(response);
+	expect(authToken).toBeDefined();
+});
+
+test('GET /login should receive logged in user', async () => {
+	const owner = await Db.collections.User!.findOneOrFail();
+	const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
+
+	const response = await authOwnerAgent.get('/login');
+
+	expect(response.statusCode).toBe(200);
+
+	const {
+		id,
+		email,
+		firstName,
+		lastName,
+		password,
+		personalizationAnswers,
+		globalRole,
+		resetPasswordToken,
+	} = response.body.data;
+
+	expect(validator.isUUID(id)).toBe(true);
+	expect(email).toBe(TEST_USER.email);
+	expect(firstName).toBe(TEST_USER.firstName);
+	expect(lastName).toBe(TEST_USER.lastName);
+	expect(password).toBeUndefined();
+	expect(personalizationAnswers).toBeNull();
+	expect(password).toBeUndefined();
+	expect(resetPasswordToken).toBeUndefined();
+	expect(globalRole).toBeDefined();
+	expect(globalRole.name).toBe('owner');
+	expect(globalRole.scope).toBe('global');
+
+	expect(response.headers['set-cookie']).toBeUndefined();
+});
+
+test('POST /logout should log user out', async () => {
+	const owner = await Db.collections.User!.findOneOrFail();
+	const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
+
+	const response = await authOwnerAgent.post('/logout');
+
+	expect(response.statusCode).toBe(200);
+	expect(response.body).toEqual(LOGGED_OUT_RESPONSE_BODY);
+
+	const authToken = utils.getAuthToken(response);
+	expect(authToken).toBeUndefined();
 });
 
 const TEST_USER = {
--- packages/cli/test/integration/owner.endpoints.test.ts
+++ packages/cli/test/integration/owner.endpoints.test.ts
@@ -12,81 +12,77 @@ import {
 	randomInvalidPassword,
 } from './shared/random';
 
-describe('/owner endpoints', () => {
-	describe('Shell requests', () => {
-		let app: express.Application;
+let app: express.Application;
 
-		beforeAll(async () => {
-			app = utils.initTestServer({ namespaces: ['owner'], applyAuth: true });
-			await utils.initTestDb();
+beforeAll(async () => {
+	app = utils.initTestServer({ namespaces: ['owner'], applyAuth: true });
+	await utils.initTestDb();
 
-			utils.initLogger();
-		});
+	utils.initLogger();
+});
 
-		beforeEach(async () => {
-			await utils.createOwnerShell();
-		});
+beforeEach(async () => {
+	await utils.createOwnerShell();
+});
 
-		afterEach(async () => {
-			await utils.truncate(['User']);
-		});
+afterEach(async () => {
+	await utils.truncate(['User']);
+});
 
-		afterAll(() => {
-			return getConnection().close();
-		});
+afterAll(() => {
+	return getConnection().close();
+});
 
-		test('POST /owner should create owner and enable hasOwner setting', async () => {
-			const owner = await Db.collections.User!.findOneOrFail();
-			const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
+test('POST /owner should create owner and enable hasOwner setting', async () => {
+	const owner = await Db.collections.User!.findOneOrFail();
+	const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
 
-			const response = await authOwnerAgent.post('/owner').send(TEST_USER);
+	const response = await authOwnerAgent.post('/owner').send(TEST_USER);
 
-			expect(response.statusCode).toBe(200);
+	expect(response.statusCode).toBe(200);
 
-			const {
-				id,
-				email,
-				firstName,
-				lastName,
-				personalizationAnswers,
-				globalRole,
-				password,
-				resetPasswordToken,
-			} = response.body.data;
+	const {
+		id,
+		email,
+		firstName,
+		lastName,
+		personalizationAnswers,
+		globalRole,
+		password,
+		resetPasswordToken,
+	} = response.body.data;
 
-			expect(validator.isUUID(id)).toBe(true);
-			expect(email).toBe(TEST_USER.email);
-			expect(firstName).toBe(TEST_USER.firstName);
-			expect(lastName).toBe(TEST_USER.lastName);
-			expect(personalizationAnswers).toBeNull();
-			expect(password).toBeUndefined();
-			expect(resetPasswordToken).toBeUndefined();
-			expect(globalRole.name).toBe('owner');
-			expect(globalRole.scope).toBe('global');
+	expect(validator.isUUID(id)).toBe(true);
+	expect(email).toBe(TEST_USER.email);
+	expect(firstName).toBe(TEST_USER.firstName);
+	expect(lastName).toBe(TEST_USER.lastName);
+	expect(personalizationAnswers).toBeNull();
+	expect(password).toBeUndefined();
+	expect(resetPasswordToken).toBeUndefined();
+	expect(globalRole.name).toBe('owner');
+	expect(globalRole.scope).toBe('global');
 
-			const storedOwner = await Db.collections.User!.findOneOrFail(id);
-			expect(storedOwner.password).not.toBe(TEST_USER.password);
-			expect(storedOwner.email).toBe(TEST_USER.email);
-			expect(storedOwner.firstName).toBe(TEST_USER.firstName);
-			expect(storedOwner.lastName).toBe(TEST_USER.lastName);
+	const storedOwner = await Db.collections.User!.findOneOrFail(id);
+	expect(storedOwner.password).not.toBe(TEST_USER.password);
+	expect(storedOwner.email).toBe(TEST_USER.email);
+	expect(storedOwner.firstName).toBe(TEST_USER.firstName);
+	expect(storedOwner.lastName).toBe(TEST_USER.lastName);
 
-			const hasOwnerConfig = config.get('userManagement.hasOwner');
-			expect(hasOwnerConfig).toBe(true);
+	const hasOwnerConfig = config.get('userManagement.hasOwner');
+	expect(hasOwnerConfig).toBe(true);
 
-			const hasOwnerSetting = await utils.getHasOwnerSetting();
-			expect(hasOwnerSetting).toBe(true);
-		});
+	const hasOwnerSetting = await utils.getHasOwnerSetting();
+	expect(hasOwnerSetting).toBe(true);
+});
 
-		test('POST /owner should fail with invalid inputs', async () => {
-			const owner = await Db.collections.User!.findOneOrFail();
-			const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
+test('POST /owner should fail with invalid inputs', async () => {
+	const owner = await Db.collections.User!.findOneOrFail();
+	const authOwnerAgent = await utils.createAgent(app, { auth: true, user: owner });
 
-			for (const invalidPayload of INVALID_POST_OWNER_PAYLOADS) {
-				const response = await authOwnerAgent.post('/owner').send(invalidPayload);
-				expect(response.statusCode).toBe(400);
-			}
-		});
-	});
+	for (const invalidPayload of INVALID_POST_OWNER_PAYLOADS) {
+		const response = await authOwnerAgent.post('/owner').send(invalidPayload);
+		expect(response.statusCode).toBe(400);
+	}
 });
 
 const TEST_USER = {
