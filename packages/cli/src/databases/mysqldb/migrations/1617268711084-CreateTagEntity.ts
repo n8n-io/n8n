3ede58f5128a44392e03d2f9098613809d8e8e90
Iván Ovejero <ivov.src@gmail.com>
2022-03-01 18:50:14 +0100

    :card_file_box: Fix tags migration

--- packages/cli/src/databases/mysqldb/migrations/1617268711084-CreateTagEntity.ts
+++ packages/cli/src/databases/mysqldb/migrations/1617268711084-CreateTagEntity.ts
@@ -9,6 +9,9 @@ export class CreateTagEntity1617268711084 implements MigrationInterface {
 
 			// create tags table + relationship with workflow entity
 
+			await queryRunner.query('SET @@GLOBAL.FOREIGN_KEY_CHECKS=1;');
+			await queryRunner.query('SET @@SESSION.FOREIGN_KEY_CHECKS=1;');
+
 			await queryRunner.query('CREATE TABLE `' + tablePrefix + 'tag_entity` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(24) NOT NULL, `createdAt` datetime NOT NULL, `updatedAt` datetime NOT NULL, UNIQUE INDEX `IDX_' + tablePrefix + '8f949d7a3a984759044054e89b` (`name`), PRIMARY KEY (`id`)) ENGINE=InnoDB');
 			await queryRunner.query('CREATE TABLE `' + tablePrefix + 'workflows_tags` (`workflowId` int NOT NULL, `tagId` int NOT NULL, INDEX `IDX_' + tablePrefix + '54b2f0343d6a2078fa13744386` (`workflowId`), INDEX `IDX_' + tablePrefix + '77505b341625b0b4768082e217` (`tagId`), PRIMARY KEY (`workflowId`, `tagId`)) ENGINE=InnoDB');
 			await queryRunner.query('ALTER TABLE `' + tablePrefix + 'workflows_tags` ADD CONSTRAINT `FK_' + tablePrefix + '54b2f0343d6a2078fa137443869` FOREIGN KEY (`workflowId`) REFERENCES `' + tablePrefix + 'workflow_entity`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION');
