386b9f18feb3443a631300649dc88f8575c47bcc
Mutasem <mutdmour@gmail.com>
2022-03-02 15:07:46 +0300

    fix list after ispending changes

--- packages/editor-ui/src/modules/users.ts
+++ packages/editor-ui/src/modules/users.ts
@@ -186,7 +186,7 @@ const module: Module<IUsersState, IRootState> = {
 		},
 		async inviteUsers(context: ActionContext<IUsersState, IRootState>, params: Array<{email: string}>): Promise<IInviteResponse[]> {
 			const users = await inviteUsers(context.rootGetters.getRestApiContext, params);
-			context.commit('addUsers', users.map(({user}) => user));
+			context.commit('addUsers', users.map(({user}) => ({isPending: true, ...user})));
 			return users;
 		},
 		async reinviteUser(context: ActionContext<IUsersState, IRootState>, params: {id: string}) {
