45c465effc9ae396c8bc09f442d42b8d562fc413
Iván Ovejero <ivov.src@gmail.com>
2022-03-03 18:12:08 +0100

    :fire: Remove `UM_` from SMTP env vars

--- packages/cli/config/index.ts
+++ packages/cli/config/index.ts
@@ -599,46 +599,46 @@ const config = convict({
 				doc: 'How to send emails',
 				format: ['', 'smtp'],
 				default: '',
-				env: 'N8N_UM_EMAIL_MODE',
+				env: 'N8N_EMAIL_MODE',
 			},
 			smtp: {
 				host: {
 					doc: 'SMTP server host',
 					format: String,
 					default: 'smtp.gmail.com',
-					env: 'N8N_UM_EMAIL_SMTP_HOST',
+					env: 'N8N_EMAIL_SMTP_HOST',
 				},
 				port: {
 					doc: 'SMTP Server port',
 					format: Number,
 					default: 465,
-					env: 'N8N_UM_EMAIL_SMTP_PORT',
+					env: 'N8N_EMAIL_SMTP_PORT',
 				},
 				secure: {
 					doc: 'Whether or not to use SSL',
 					format: Boolean,
 					default: true,
-					env: 'N8N_UM_EMAIL_SMTP_SSL',
+					env: 'N8N_EMAIL_SMTP_SSL',
 				},
 				auth: {
 					user: {
 						doc: 'SMTP Login username',
 						format: String,
 						default: 'youremail@gmail.com',
-						env: 'N8N_UM_EMAIL_SMTP_USER',
+						env: 'N8N_EMAIL_SMTP_USER',
 					},
 					pass: {
 						doc: 'SMTP Login password',
 						format: String,
 						default: 'my-super-password',
-						env: 'N8N_UM_EMAIL_SMTP_PASS',
+						env: 'N8N_EMAIL_SMTP_PASS',
 					},
 				},
 				sender: {
 					doc: 'How to display sender name',
 					format: String,
 					default: '"n8n rocks" <n8n@n8n.io>',
-					env: 'N8N_UM_EMAIL_SMTP_SENDER',
+					env: 'N8N_SMTP_SENDER',
 				},
 			},
 			templates: {
