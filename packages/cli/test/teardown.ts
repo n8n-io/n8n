320124990f00f34465071cea452f94da9c1d250a
Iván Ovejero <ivov.src@gmail.com>
2022-03-04 12:40:20 +0100

    :zap: Add no-leftover check to MySQL

--- packages/cli/test/teardown.ts
+++ packages/cli/test/teardown.ts
@@ -2,25 +2,26 @@ import { createConnection } from 'typeorm';
 import config = require('../config');
 import { exec } from 'child_process';
 import { DatabaseType } from '../src';
-import { getBootstrapPostgresOptions } from './integration/shared/testDb';
+import { getBootstrapMySqlOptions, getBootstrapPostgresOptions } from './integration/shared/testDb';
+import { BOOTSTRAP_MYSQL_CONNECTION_NAME } from './integration/shared/constants';
 
 export default async () => {
 	const dbType = config.get('database.type') as DatabaseType;
 
 	if (dbType === 'postgresdb') {
-		const bootstrap = await createConnection(getBootstrapPostgresOptions());
+		const bootstrapPostgres = await createConnection(getBootstrapPostgresOptions());
 
-		const results: { db_name: string }[] = await bootstrap.query(
+		const results: { db_name: string }[] = await bootstrapPostgres.query(
 			'SELECT datname as db_name FROM pg_database;',
 		);
 
 		const promises = results
-			.filter(({ db_name }) => db_name.startsWith('pg_') && db_name.endsWith('_n8n_test'))
-			.map(({ db_name }) => bootstrap.query(`DROP DATABASE ${db_name};`));
+			.filter(({ db_name: dbName }) => dbName.startsWith('pg_') && dbName.endsWith('_n8n_test'))
+			.map(({ db_name: dbName }) => bootstrapPostgres.query(`DROP DATABASE ${dbName};`));
 
 		await Promise.all(promises);
 
-		bootstrap.close();
+		bootstrapPostgres.close();
 	}
 
 	if (dbType === 'mysqldb') {
@@ -28,6 +29,20 @@ export default async () => {
 		const password = config.get('database.mysqldb.password');
 		const host = config.get('database.mysqldb.host');
 
-		exec(`echo "DROP DATABASE n8n_bs_mysql" | mysql -h ${host} -u ${user} -p${password}`);
+		const bootstrapMySql = await createConnection(getBootstrapMySqlOptions());
+
+		const results: { Database: string }[] = await bootstrapMySql.query('SHOW DATABASES;');
+
+		const promises = results
+			.filter(({ Database: dbName }) => dbName.startsWith('mysql_') && dbName.endsWith('_n8n_test'))
+			.map(({ Database: dbName }) => bootstrapMySql.query(`DROP DATABASE ${dbName};`));
+
+		await Promise.all(promises);
+
+		await bootstrapMySql.close();
+
+		exec(
+			`echo "DROP DATABASE ${BOOTSTRAP_MYSQL_CONNECTION_NAME}" | mysql -h ${host} -u ${user} -p${password}`,
+		);
 	}
 };
