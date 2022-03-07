b0c074a4177f68cf995bb778a10434b71df025cc
Ben Hesseldieck <b.hesseldieck@gmail.com>
2022-03-07 11:12:32 +0100

    :bug: remove cookie if cookie is not valid

--- packages/cli/src/UserManagement/routes/auth.ts
+++ packages/cli/src/UserManagement/routes/auth.ts
@@ -71,7 +71,7 @@ export function authenticationMethods(this: N8nApp): void {
 					user = await resolveJwt(cookieContents);
 					return sanitizeUser(user);
 				} catch (error) {
-					throw new Error('Invalid login information');
+					res.clearCookie(AUTH_COOKIE_NAME);
 				}
 			}
 
