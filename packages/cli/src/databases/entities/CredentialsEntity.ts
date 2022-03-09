16c1599e7d097ebc40a9e99f638029dbb341eb77
ricardo <ricardoespinoza105@gmail.com>
2022-03-09 12:37:50 -0500

    :shirt: Fix lint issue

--- packages/cli/src/databases/entities/CredentialsEntity.ts
+++ packages/cli/src/databases/entities/CredentialsEntity.ts
@@ -37,7 +37,7 @@ function getTimestampSyntax() {
 	const dbType = config.get('database.type') as DatabaseType;
 
 	const map: { [key in DatabaseType]: string } = {
-		sqlite: 'STRFTIME(\'%Y-%m-%d %H:%M:%f\', \'NOW\')',
+		sqlite: `STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')`,
 		postgresdb: 'CURRENT_TIMESTAMP(3)',
 		mysqldb: 'CURRENT_TIMESTAMP(3)',
 		mariadb: 'CURRENT_TIMESTAMP(3)',
