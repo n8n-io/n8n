aa1a84b5873b518d1d40830bb1817d055cfd6f06
Michael Kret <michael.k@radency.com>
2022-03-10 10:04:35 +0200

    :zap: fixed linter errors

--- packages/nodes-base/credentials/SlackOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/SlackOAuth2Api.credentials.ts
@@ -70,7 +70,7 @@ export class SlackOAuth2Api implements ICredentialType {
 			type: 'boolean',
 			default: false,
 			description: `Whether to use the bot's access token or user's access token`,
-			hint: "While using bot's access token you wouldn't be able to update user profile or do operations with stars resource!"
+			hint: `While using bot's access token you wouldn't be able to update user profile or do operations with stars resource!`,
 		},
 	];
 }
