4a5813ad52b61437b9801eeb1e6299974448e440
Ben Hesseldieck <b.hesseldieck@gmail.com>
2022-04-01 01:43:13 +0200

    🐘 add migration to lowercase email

---
+++ packages/cli/src/databases/mysqldb/migrations/1648740597343-LowerCaseEmail.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import config = require('../../../../config');
+
+export class LowerCaseEmail1648740597343 implements MigrationInterface {
+	name = 'LowerCaseEmail1648740597343';
+
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		const tablePrefix = config.get('database.tablePrefix');
+
+		await queryRunner.query(`
+			UPDATE ${tablePrefix}user
+			SET email = LOWER(email);
+		`);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {}
+}
