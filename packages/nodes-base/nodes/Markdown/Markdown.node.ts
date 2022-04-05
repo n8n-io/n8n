b66132e8901e8389ea9ce99bfca00bb117dd5701
Michael Kret <michael.k@radency.com>
2022-04-05 14:22:57 +0300

    :hammer: added options, added continue on fail, some clean up

--- packages/nodes-base/nodes/Markdown/Markdown.node.ts
+++ packages/nodes-base/nodes/Markdown/Markdown.node.ts
@@ -7,21 +7,14 @@ import {
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
+	JsonObject,
 } from 'n8n-workflow';
 
-//@ts-expect-error
-import * as showdown from 'showdown';
+import { Converter, Flavor } from 'showdown';
 
-//@ts-expect-error
-import * as jsdom from 'jsdom';
+import { JSDOM } from 'jsdom';
 
-const dom = new jsdom.JSDOM();
-
-const converter = new showdown.Converter();
-
-import {
-	set,
-} from 'lodash';
+import { set } from 'lodash';
 
 export class Markdown implements INodeType {
 	description: INodeTypeDescription = {
@@ -31,7 +24,7 @@ export class Markdown implements INodeType {
 		group: ['output'],
 		version: 1,
 		subtitle: '={{$parameter["mode"]==="markdownToHtml" ? "Markdown to HTML" : "HTML to Markdown"}}',
-		description: 'Convert data between Markdown and HTML.',
+		description: 'Convert data between Markdown and HTML',
 		defaults: {
 			name: 'Markdown',
 			color: '#000000',
@@ -57,7 +50,6 @@ export class Markdown implements INodeType {
 					},
 				],
 				default: 'htmlToMarkdown',
-				description: 'What to convert between.',
 			},
 			{
 				displayName: 'HTML',
@@ -72,7 +64,7 @@ export class Markdown implements INodeType {
 				},
 				default: '',
 				required: true,
-				description: 'The HTML to be converted',
+				description: 'The HTML to be converted to markdown',
 			},
 			{
 				displayName: 'Markdown',
@@ -87,7 +79,7 @@ export class Markdown implements INodeType {
 				},
 				default: '',
 				required: true,
-				description: 'The Markdown to be converted',
+				description: 'The Markdown to be converted to html',
 			},
 			{
 				displayName: 'Destination Key',
@@ -104,14 +96,23 @@ export class Markdown implements INodeType {
 				default: 'data',
 				required: true,
 				placeholder: '',
-				description: 'The field to put the output in. Specify nested fields<br />using dots, e.g."level1.level2.newKey"',
+				description: 'The field to put the output in. Specify nested fields<br />using dots, e.g."level1.level2.newKey".',
 			},
+
+			//============= HTML to Markdown Options ===============
 			{
 				displayName: 'Options',
 				name: 'options',
 				type: 'collection',
 				placeholder: 'Add Option',
 				default: {},
+				displayOptions: {
+					show: {
+						'mode': [
+							'htmlToMarkdown',
+						],
+					},
+				},
 				options: [
 					{
 						displayName: 'Markdown Flavor',
@@ -131,17 +132,237 @@ export class Markdown implements INodeType {
 							{
 								name: 'Original',
 								value: 'original',
-								description: `As first defined by John Gruber (<a href="https://daringfireball.net/projects/markdown/" target="_blank">more info</a>)`,
+								description: `As first defined by John Gruber (<a href="https://daringfireball.net/projects/markdown/" target='_blank'>more info</a>)`,
 							},
 						],
-						displayOptions: {
-							show: {
-								'/mode': [
-									'htmlToMarkdown',
-								],
-							},
-						},
 						default: 'vanilla',
+					},
+					{
+						displayName: 'Omit Trailing Newline',
+						name: 'omitExtraWLInCodeBlocks',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to omit the trailing newline in a code block',
+					},
+				],
+			},
+			//============= Markdown to HTML Options ===============
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add Option',
+				default: {},
+				displayOptions: {
+					show: {
+						'mode': [
+							'markdownToHtml',
+						],
+					},
+				},
+				options: [
+					{
+						displayName: 'Add Blank To Links',
+						name: 'openLinksInNewWindow',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to open all links in new windows (by adding the attribute target="_blank" to <a> tags)',
+					},
+					{
+						displayName: 'Automatic Linking To URLs',
+						name: 'simplifiedAutoLink',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable automatic linking to urls',
+					},
+					{
+						displayName: 'Backslash Escapes HTML Tags',
+						name: 'backslashEscapesHTMLTags',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to support for HTML Tag escaping ex: \<div>foo\</div',
+					},
+					{
+						displayName: 'Complete HTML Document',
+						name: 'completeHTMLDocument',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to output a complete html document, including <html>, <head> and <body> tags instead of an HTML fragment',
+					},
+					{
+						displayName: 'Customized Header ID',
+						name: 'customizedHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to use text in curly braces as header id',
+					},
+					{
+						displayName: 'Emoji Support',
+						name: 'emoji',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable emoji support. Ex: this is a :smile: emoji For more info on available emojis, see https://github.com/showdownjs/showdown/wiki/Emojis.',
+					},
+					{
+						displayName: 'Encode Emails',
+						name: 'encodeEmails',
+						type: 'boolean',
+						default: true,
+						description: 'Whether to enable e-mail addresses encoding through the use of Character Entities, transforming ASCII e-mail addresses into its equivalent decimal entities',
+					},
+					{
+						displayName: 'Exclude Trailing Punctuation From URLs',
+						name: 'excludeTrailingPunctuationFromURLs',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to exclude trailing punctuation from autolinking urls. Punctuation excluded: . ! ? ( ). Only applies if simplifiedAutoLink option is set to true.',
+					},
+					{
+						displayName: 'GitHub Code Blocks',
+						name: 'ghCodeBlocks',
+						type: 'boolean',
+						default: true,
+						description: 'Whether to enable support for GFM code block style',
+					},
+					{
+						displayName: 'GitHub Compatible Header IDs',
+						name: 'ghCompatibleHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to generate header ids compatible with github style (spaces are replaced with dashes and a bunch of non alphanumeric chars are removed)',
+					},
+					{
+						displayName: 'GitHub Mention Link',
+						name: 'ghMentionsLink',
+						type: 'string',
+						default: 'https://github.com/{u}',
+						description: 'Whether to change the link generated by @mentions. Showdown will replace {u} with the username. Only applies if ghMentions option is enabled.',
+					},
+					{
+						displayName: 'GitHub Mentions',
+						name: 'ghMentions',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable github @mentions, which link to the username mentioned',
+					},
+					{
+						displayName: 'GitHub Task Lists',
+						name: 'tasklists',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable support for GFM tasklists',
+					},
+					{
+						displayName: 'Header Level Start',
+						name: 'headerLevelStart',
+						type: 'number',
+						default: 1,
+						description: 'Whether to set the header starting level',
+					},
+					{
+						displayName: 'Mandatory Space Before Header',
+						name: 'requireSpaceBeforeHeadingText',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to make adding a space between # and the header text mandatory',
+					},
+					{
+						displayName: 'Middle Word Asterisks',
+						name: 'literalMidWordAsterisks',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to stop showdown from interpreting asterisks in the middle of words as <em> and <strong> and instead treat them as literal asterisks',
+					},
+					{
+						displayName: 'Middle Word Underscores',
+						name: 'literalMidWordUnderscores',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to stop showdown from interpreting underscores in the middle of words as <em> and <strong> and instead treat them as literal underscores',
+					},
+					{
+						displayName: 'No Header ID',
+						name: 'noHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to disable the automatic generation of header ids',
+					},
+					{
+						displayName: 'Parse Image Dimensions',
+						name: 'parseImgDimensions',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable support for setting image dimensions from within markdown syntax',
+					},
+					{
+						displayName: 'Prefix Header ID',
+						name: 'prefixHeaderId',
+						type: 'string',
+						default: 'section',
+						description: 'Add a prefix to the generated header ids',
+					},
+					{
+						displayName: 'Raw Header ID',
+						name: 'rawHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to remove only spaces, \' and " from generated header ids (including prefixes), replacing them with dashes (-)',
+					},
+					{
+						displayName: 'Raw Prefix Header ID',
+						name: 'rawPrefixHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to prevent showdown from modifying the prefix',
+					},
+					{
+						displayName: 'Simple Line Breaks',
+						name: 'simpleLineBreaks',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to parse line breaks as <br>, like GitHub does, without needing 2 spaces at the end of the line',
+					},
+					{
+						displayName: 'Smart Indentation Fix',
+						name: 'smartIndentationFix',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to try to smartly fix indentation problems related to es6 template strings in the midst of indented code',
+					},
+					{
+						displayName: 'Spaces Indented Sublists',
+						name: 'disableForced4SpacesIndentedSublists',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to disable the requirement of indenting sublists by 4 spaces for them to be nested, effectively reverting to the old behavior where 2 or 3 spaces were enough',
+					},
+					{
+						displayName: 'Split Adjacent Blockquotes',
+						name: 'splitAdjacentBlockquotes',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to split adjacent blockquote blocks',
+					},
+					{
+						displayName: 'Strikethrough',
+						name: 'strikethrough',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable support for strikethrough syntax',
+					},
+					{
+						displayName: 'Tables Header ID',
+						name: 'tablesHeaderId',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to add an ID property to table headers tags',
+					},
+					{
+						displayName: 'Tables Support',
+						name: 'tables',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to enable support for tables syntax',
 					},
 				],
 			},
@@ -150,32 +371,54 @@ export class Markdown implements INodeType {
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
-		const returnData: INodeExecutionData[] = [];
-		const length = items.length as unknown as number;
-		const newItem: INodeExecutionData = { json: {} };
+		const returnData: IDataObject[] = [];
+
+		const converter = new Converter();
+
 		const mode = this.getNodeParameter('mode', 0) as string;
 
+		const { length } = items;
 		for (let i = 0; i < length; i++) {
-			if (mode === 'htmlToMarkdown') {
-				const options = this.getNodeParameter('options', i) as IDataObject;
-				const destinationKey = this.getNodeParameter('destinationKey', i) as string;
-				converter.setFlavor(options.flavor as showdown.Flavor || 'vanilla');
-				const html = this.getNodeParameter('html', i) as string;
-				const md = converter.makeMarkdown(html, dom.window.document);
-				newItem.json = JSON.parse(JSON.stringify(items[i].json));
-				set(newItem.json, destinationKey, md);
-				returnData.push(newItem);
-			}
+			try {
+				if (mode === 'htmlToMarkdown') {
+					const options = this.getNodeParameter('options', i) as IDataObject;
+					const destinationKey = this.getNodeParameter('destinationKey', i) as string;
 
-			if (mode === 'markdownToHtml') {
-				const markdown = this.getNodeParameter('markdown', i) as string;
-				const html = converter.makeHtml(markdown);
-				const destinationKey = this.getNodeParameter('destinationKey', i) as string;
-				newItem.json = JSON.parse(JSON.stringify(items[i].json));
-				set(newItem.json, destinationKey, html);
-				returnData.push(newItem);
+					converter.setFlavor(options.flavor as Flavor || 'vanilla');
+					Object.keys(options).filter(key => key !== 'flavor').forEach( key => converter.setOption(key, options[key]));
+
+					const html = this.getNodeParameter('html', i) as string;
+					const dom = new JSDOM(html);
+
+					const markdownFromHTML = converter.makeMarkdown(dom.window.document.body.outerHTML, dom.window.document	);
+
+					const newItem = JSON.parse(JSON.stringify(items[i].json));
+					set(newItem, destinationKey, markdownFromHTML);
+
+					returnData.push(newItem);
+				}
+
+				if (mode === 'markdownToHtml') {
+					const markdown = this.getNodeParameter('markdown', i) as string;
+					const destinationKey = this.getNodeParameter('destinationKey', i) as string;
+					const options = this.getNodeParameter('options', i) as IDataObject;
+
+					Object.keys(options).forEach( key => converter.setOption(key, options[key]));
+					const htmlFromMarkdown = converter.makeHtml(markdown);
+
+					const newItem = JSON.parse(JSON.stringify(items[i].json));
+					set(newItem, destinationKey, htmlFromMarkdown);
+
+					returnData.push(newItem)
+				}
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ error: (error as JsonObject).message });
+					continue;
+				}
+				throw error;
 			}
 		}
-		return this.prepareOutputData(returnData);
+		return [this.helpers.returnJsonArray(returnData)];
 	}
 }
