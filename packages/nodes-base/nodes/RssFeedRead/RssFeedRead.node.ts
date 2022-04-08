f292f90d73af840ebfcb3eb88b56d4d6c0adf71e
Saurabh Kashyap <saurabhkashyap0001@gmail.com>
2022-04-08 12:46:35 +0530

    :fire:

--- packages/nodes-base/nodes/RssFeedRead/RssFeedRead.node.ts
+++ packages/nodes-base/nodes/RssFeedRead/RssFeedRead.node.ts
@@ -4,12 +4,12 @@ import {
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
-	NodeOperationError,
+	NodeOperationError
 } from 'n8n-workflow';
 
 import * as Parser from 'rss-parser';
 import { URL } from 'url';
-const isOnline = require('is-online');
+
 export class RssFeedRead implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'RSS Read',
@@ -36,19 +36,22 @@ export class RssFeedRead implements INodeType {
 		],
 	};
 
-
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		if (await isOnline()){
-		try{
-
+		try {
 			const url = this.getNodeParameter('url', 0) as string;
 
 			if (!url) {
-				throw new NodeOperationError(this.getNode(), 'The parameter "URL" has to be set!');
+				throw new NodeOperationError(
+					this.getNode(),
+					'The parameter "URL" has to be set!',
+				);
 			}
 
-			if (!validateURL(url)){
-				throw new NodeOperationError(this.getNode(), 'The provided "URL" is not valid!');
+			if (!validateURL(url)) {
+				throw new NodeOperationError(
+					this.getNode(),
+					'The provided "URL" is not valid!',
+				);
 			}
 
 			const parser = new Parser();
@@ -58,40 +61,41 @@ export class RssFeedRead implements INodeType {
 				feed = await parser.parseURL(url);
 			} catch (error) {
 				if (error.code === 'ECONNREFUSED') {
-					throw new NodeOperationError(this.getNode(), `It was not possible to connect to the URL. Please make sure the URL "${url}" it is valid!`);
+					throw new NodeOperationError(
+						this.getNode(),
+						`It was not possible to connect to the URL. Please make sure the URL "${url}" it is valid!`,
+					);
 				}
 
 				throw new NodeOperationError(this.getNode(), error);
 			}
 
-
 			const returnData: IDataObject[] = [];
 
 			// For now we just take the items and ignore everything else
 			if (feed.items) {
-				feed.items.forEach((item) => {
+				feed.items.forEach(item => {
 					// @ts-ignore
 					returnData.push(item);
 				});
 			}
 
 			return [this.helpers.returnJsonArray(returnData)];
-
 		} catch (error) {
 			if (this.continueOnFail()) {
-				return this.prepareOutputData([{json:{ error: error.message }}]);
+				return this.prepareOutputData([{ json: { error: error.message } }]);
 			}
 			throw error;
 		}
-	}
 	}
 }
 
 // Utility function
 
-function validateURL (url: string) {
+function validateURL(url: string) {
 	try {
 		const parseUrl = new URL(url);
+
 		return true;
 	} catch (err) {
 		return false;
