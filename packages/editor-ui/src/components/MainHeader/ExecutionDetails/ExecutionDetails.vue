54431d697890d4d730ef76c5d549ab108a7dca77
mrogers-5s <mrogers@weare5stones.com>
2022-04-19 10:52:04 -0500

    feat(ExecutionDetails.vue): Add retry button to the execution details page
    Add a button to the details page for an execution that allows users to quickly retry the workflow

--- packages/editor-ui/src/components/MainHeader/ExecutionDetails/ExecutionDetails.vue
+++ packages/editor-ui/src/components/MainHeader/ExecutionDetails/ExecutionDetails.vue
@@ -26,15 +26,24 @@
 			</span>
 			{{ $locale.baseText('executionDetails.of') }}
 			<span class="primary-color clickable" :title="$locale.baseText('executionDetails.openWorkflow')">
-				<ShortenName :name="workflowName">
+				<WorkflowNameShort :name="workflowName">
 					<template v-slot="{ shortenedName }">
 						<span @click="openWorkflow(workflowExecution.workflowId)">
 							"{{ shortenedName }}"
 						</span>
 					</template>
-				</ShortenName>
+				</WorkflowNameShort>
 			</span>
 			{{ $locale.baseText('executionDetails.workflow') }}
+		</span>
+		<span
+			class="retry-exec-button"
+			v-if="!executionFinished && !executionWaiting && !successfulRetry">
+			<n8n-button
+				:label="isRetrying ? 'Retrying' : 'Retry'"
+				:loading="isRetrying"
+				@click="retryExecution(executionId)"
+			/>
 		</span>
 		<ReadOnly class="read-only" />
 	</div>
@@ -42,32 +51,41 @@
 
 <script lang="ts">
 import mixins from "vue-typed-mixins";
-
 import { IExecutionResponse } from "../../../Interface";
-
+import { restApi } from '@/components/mixins/restApi';
+import { showMessage } from '@/components/mixins/showMessage';
 import { titleChange } from "@/components/mixins/titleChange";
-
-import ShortenName from "@/components/ShortenName.vue";
+import WorkflowNameShort from "@/components/WorkflowNameShort.vue";
 import ReadOnly from "@/components/MainHeader/ExecutionDetails/ReadOnly.vue";
-
-export default mixins(titleChange).extend({
+export default mixins(
+	titleChange,
+	restApi,
+	showMessage,
+).extend({
 	name: "ExecutionDetails",
 	components: {
-		ShortenName,
+		WorkflowNameShort,
 		ReadOnly,
+	},
+	data () {
+		return {
+			isRetrying: false,
+		};
 	},
 	computed: {
 		executionId(): string | undefined {
 			return this.$route.params.id;
 		},
+		successfulRetry(): boolean {
+			const fullExecution = this.$store.getters.getWorkflowExecution;
+			return !!fullExecution && !!fullExecution.retrySuccessId;
+		},
 		executionFinished(): boolean {
 			const fullExecution = this.$store.getters.getWorkflowExecution;
-
 			return !!fullExecution && fullExecution.finished;
 		},
 		executionWaiting(): boolean {
 			const fullExecution = this.$store.getters.getWorkflowExecution;
-
 			return !!fullExecution && !!fullExecution.waitTill;
 		},
 		workflowExecution(): IExecutionResponse | null {
@@ -86,6 +104,29 @@ export default mixins(titleChange).extend({
 				params: { name: workflowId },
 			});
 		},
+		async retryExecution (executionId: string) {
+			this.isRetrying = true;
+			try {
+				const retrySuccessful = await this.restApi().retryExecution(executionId, true);
+				if (retrySuccessful === true) {
+					this.$showMessage({
+						title: 'Retry successful',
+						message: 'The retry was successful!',
+						type: 'success',
+					});
+				} else {
+					this.$showMessage({
+						title: 'Retry unsuccessful',
+						message: 'The retry was not successful!',
+						type: 'error',
+					});
+				}
+				this.isRetrying = false;
+			} catch (error) {
+				this.$showError(error, 'Problem with retry', 'There was a problem with the retry:');
+				this.isRetrying = false;
+			}
+		},
 	},
 });
 </script>
@@ -94,7 +135,6 @@ export default mixins(titleChange).extend({
 * {
 	box-sizing: border-box;
 }
-
 .execution-icon {
  &.success {
 	color: var(--color-success);
@@ -103,21 +143,20 @@ export default mixins(titleChange).extend({
 	 color: var(--color-warning);
  }
 }
-
 .container {
 	width: 100%;
 	display: flex;
 }
-
+.retry-exec-button {
+	margin-right: 30px;
+}
 .title {
 	flex: 1;
 	text-align: center;
 }
-
 .read-only {
 	align-self: flex-end;
 }
-
 .el-tooltip.read-only div {
 	max-width: 400px;
 }
