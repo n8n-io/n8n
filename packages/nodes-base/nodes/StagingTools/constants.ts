e1c7ac976e0583232334d9bf40b4b938720ece3d
Flash Hu <idisblueflash@gmail.com>
2022-04-22 09:18:00 +0800

    quick-fix: clean QA emails

--- packages/nodes-base/nodes/StagingTools/constants.ts
+++ packages/nodes-base/nodes/StagingTools/constants.ts
@@ -1,12 +1,5 @@
 export const QA_EMAILS = [
 	'flash@cgptalent.com',
-	'idisblueflash@gmail.com',
-	'idisblueflash@icloud.com',
-	'denise@cgptalent.com',
-	'deniselin0114+migrationtestingg@gmail.com',
-	'deniselin0114+gcptesting@gmail.com',
-	'deniselin0114+aliyuntesting@gmail.com',
-	'deniselin0114@gmail.com',
 	'serge@cgptalent.com',
 ];
 
