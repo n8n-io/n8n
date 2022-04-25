b3bc98a430e0377bf3c191087c6d6d22f1ce5163
Mutasem <mutdmour@gmail.com>
2022-04-25 09:39:11 +0200

    update sticky story

--- packages/design-system/src/components/N8nSticky/Sticky.stories.js
+++ packages/design-system/src/components/N8nSticky/Sticky.stories.js
@@ -40,6 +40,9 @@ export default {
 
 const methods = {
 	onInput: action('input'),
+	onResize: action('resize'),
+	onResizeEnd: action('resizeend'),
+	onResizeStart: action('resizestart'),
 };
 
 const Template = (args, { argTypes }) => ({
@@ -48,18 +51,18 @@ const Template = (args, { argTypes }) => ({
 		N8nSticky,
 	},
 	template:
-		'<n8n-sticky v-bind="$props"  @blur="onBlur" @change="onChange" @focus="onFocus" @input="onInput" @unfocus="onUnfocus"></n8n-sticky>',
+		'<n8n-sticky v-bind="$props"  @resize="onResize" @resizeend="onResizeEnd" @resizeStart="onResizeStart" @input="onInput"></n8n-sticky>',
 	methods,
 });
 
 export const Sticky = Template.bind({});
 Sticky.args = {
-	content: `## I'm a note \n**Double click** to edit me. [Guide](https://docs.n8n.io/workflows/workflow-notes/)`,
 	height: 160,
+	width: 150,
+	content: `## I'm a note \n**Double click** to edit me. [Guide](https://docs.n8n.io/workflows/workflow-notes/)`,
 	defaultText: `## I'm a note \n**Double click** to edit me. [Guide](https://docs.n8n.io/workflows/workflow-notes/)`,
 	isEditable: false,
 	minHeight: 80,
 	minWidth: 150,
 	readOnly: false,
-	width: 240,
 };
