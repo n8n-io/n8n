02d7958ea48b6125c7f0c80a0ba052a965121a0e
ricardo <ricardoespinoza105@gmail.com>
2022-04-26 16:20:35 -0400

    :zap: Expose swagger ui in GET /{version}/docs

--- packages/cli/src/PublicApi/index.ts
+++ packages/cli/src/PublicApi/index.ts
@@ -8,15 +8,29 @@ import * as OpenApiValidator from 'express-openapi-validator';
 import { HttpError } from 'express-openapi-validator/dist/framework/types';
 import fs from 'fs/promises';
 import path from 'path';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
+import config from '../../config';
 import { authenticationHandler, specFormats } from './helpers';
 
 function createApiRouter(
 	version: string,
 	openApiSpecPath: string,
 	hanldersDirectory: string,
+	swaggerThemeCss: string,
 ): Router {
+	const n8nPath = config.getEnv('path');
+	const swaggerDocument = YAML.load(openApiSpecPath) as swaggerUi.JsonObject;
 	const apiController = express.Router();
-	apiController.use(`/${version}/spec`, express.static(openApiSpecPath));
+	apiController.use(
+		`/${version}/docs`,
+		swaggerUi.serveFiles(swaggerDocument),
+		swaggerUi.setup(swaggerDocument, {
+			customCss: swaggerThemeCss,
+			customSiteTitle: 'n8n Public API UI',
+			customfavIcon: `${n8nPath}favicon.ico`,
+		}),
+	);
 	apiController.use(`/${version}`, express.json());
 	apiController.use(
 		`/${version}`,
@@ -44,12 +58,14 @@ function createApiRouter(
 }
 
 export const loadPublicApiVersions = async (): Promise<express.Router[]> => {
-	const data = await fs.readdir(__dirname);
-	const versions = data.filter((folderName) => folderName.startsWith('v'));
+	const swaggerThemePath = path.join(__dirname, 'swaggerTheme.css');
+	const folders = await fs.readdir(__dirname);
+	const css = (await fs.readFile(swaggerThemePath)).toString();
+	const versions = folders.filter((folderName) => folderName.startsWith('v'));
 	const apiRouters: express.Router[] = [];
 	for (const version of versions) {
 		const openApiPath = path.join(__dirname, version, 'openapi.yml');
-		apiRouters.push(createApiRouter(version, openApiPath, __dirname));
+		apiRouters.push(createApiRouter(version, openApiPath, __dirname, css));
 	}
 	return apiRouters;
 };
