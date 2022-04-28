22b6e22bfa74e3882b5505c96906dbda45cf3e02
Mutasem <mutdmour@gmail.com>
2022-04-28 11:26:37 +0200

    fix build issue

--- packages/nodes-base/nodes/Ftp/Ftp.node.ts
+++ packages/nodes-base/nodes/Ftp/Ftp.node.ts
@@ -86,15 +86,19 @@ export class Ftp implements INodeType {
 				displayName: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
 				name: 'asas1',
 				type: 'notice',
-				truncate: true,
+				typeOptions: {
+					truncate: true,
+				},
 				default: '',
 			},
 			{
 				displayName: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
 				name: 'asas1',
 				type: 'notice',
-				truncate: true,
-				truncateAt: 50,
+				typeOptions: {
+					truncate: true,
+					truncateAt: 50,
+				},
 				default: '',
 			},
 			{
