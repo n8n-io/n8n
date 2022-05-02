ad74890c3286d0c3bddacd6f74b21c647940c10b
ricardo <ricardoespinoza105@gmail.com>
2022-05-02 16:41:14 -0400

    :zap: Add POST /workflows/:workflowId/desactivate

--- packages/cli/src/PublicApi/v1/handlers/workflows.ts
+++ packages/cli/src/PublicApi/v1/handlers/workflows.ts
@@ -4,7 +4,12 @@ import { WorkflowEntity } from '../../../databases/entities/WorkflowEntity';
 import { authorize, instanceOwnerSetup } from '../../middlewares';
 import { WorkflowRequest } from '../../publicApiRequest';
 import { isInstanceOwner } from '../../Services/user';
-import { getWorkflowById, getWorkflowAccess, activeWorkflow } from '../../Services/workflow';
+import {
+	getWorkflowById,
+	getWorkflowAccess,
+	activeWorkflow,
+	desactiveWorkflow,
+} from '../../Services/workflow';
 
 export = {
 	createWorkflow: [],
@@ -29,6 +34,7 @@ export = {
 				await workflowRunner.add(w.id.toString(), 'activate');
 				// change the status to active in the DB
 				await activeWorkflow(workflow);
+				workflow.active = true;
 			};
 
 			if (isInstanceOwner(req.user)) {
@@ -69,5 +75,62 @@ export = {
 			return res.status(404).json();
 		},
 	],
-	desactivateWorkflow: [],
+	desactivateWorkflow: [
+		instanceOwnerSetup,
+		authorize(['owner', 'member']),
+		async (req: WorkflowRequest.Activate, res: express.Response): Promise<express.Response> => {
+			const { workflowId } = req.params;
+
+			const workflow = await getWorkflowById(workflowId);
+			if (workflow === undefined) {
+				return res.status(404).json();
+			}
+
+			const workflowRunner = ActiveWorkflowRunner.getInstance();
+
+			const desactivateWorkflow = async (w: WorkflowEntity) => {
+				await workflowRunner.remove(w.id.toString());
+				// change the status to active in the DB
+				await desactiveWorkflow(workflow);
+				workflow.active = false;
+			};
+
+			if (isInstanceOwner(req.user)) {
+				if (workflow.active) {
+					try {
+						await desactivateWorkflow(workflow);
+						return res.json(workflow);
+					} catch (error) {
+						// todo
+						// remove the type assertion
+						const errorObject = error as unknown as { message: string };
+						return res.status(400).json({ error: errorObject.message });
+					}
+				}
+				// nothing to do as the wokflow is already desactive
+				return res.json(workflow);
+			}
+
+			const userHasAccessToWorkflow = await getWorkflowAccess(req.user, workflowId.toString());
+
+			if (userHasAccessToWorkflow) {
+				if (workflow.active) {
+					try {
+						await desactivateWorkflow(workflow);
+						return res.json(workflow);
+					} catch (error) {
+						// todo
+						// remove the type assertion
+						const errorObject = error as unknown as { message: string };
+						return res.status(400).json({ error: errorObject.message });
+					}
+				}
+				return res.json(workflow);
+			}
+			// member trying to access workflow that does not own
+			// 404 or 403?
+			// 403 will let him know about the existance of the workflow
+			return res.status(404).json();
+		},
+	],
 };
