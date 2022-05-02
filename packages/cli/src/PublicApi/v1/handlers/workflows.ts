0bc7ab6e2fba2adbcd912cb2d28fe980940cf299
ricardo <ricardoespinoza105@gmail.com>
2022-05-02 18:21:47 -0400

    :zap: Add POST /workflows

--- packages/cli/src/PublicApi/v1/handlers/workflows.ts
+++ packages/cli/src/PublicApi/v1/handlers/workflows.ts
@@ -1,8 +1,13 @@
 import express = require('express');
-import { ActiveWorkflowRunner } from '../../..';
+import { INode } from 'n8n-workflow';
+import { ActiveWorkflowRunner, Db } from '../../..';
+import { SharedWorkflow } from '../../../databases/entities/SharedWorkflow';
 import { WorkflowEntity } from '../../../databases/entities/WorkflowEntity';
+import { replaceInvalidCredentials } from '../../../WorkflowHelpers';
+// import { validateEntity } from '../../../GenericHelpers';
 import { authorize, instanceOwnerSetup } from '../../middlewares';
 import { WorkflowRequest } from '../../publicApiRequest';
+import { getWorkflowOwnerRole } from '../../Services/role';
 import { isInstanceOwner } from '../../Services/user';
 import {
 	getWorkflowById,
@@ -12,7 +17,52 @@ import {
 } from '../../Services/workflow';
 
 export = {
-	createWorkflow: [],
+	createWorkflow: [
+		instanceOwnerSetup,
+		authorize(['owner', 'member']),
+		async (req: WorkflowRequest.Create, res: express.Response): Promise<express.Response> => {
+			let workflow = req.body;
+
+			const startNode: INode = {
+				parameters: {},
+				name: 'Start',
+				type: 'n8n-nodes-base.start',
+				typeVersion: 1,
+				position: [240, 300],
+			};
+
+			workflow.active = false;
+
+			// add start node if nodes property empty or start node
+			// not found in the array
+			if (
+				!workflow.nodes.length ||
+				!workflow.nodes.find((node) => node.type === 'n8n-nodes-base.start')
+			) {
+				workflow.nodes.push(startNode);
+			}
+
+			const role = await getWorkflowOwnerRole();
+
+			// is this still needed?
+			await replaceInvalidCredentials(workflow);
+
+			await Db.transaction(async (transactionManager) => {
+				const newWorkflow = new WorkflowEntity();
+				Object.assign(newWorkflow, workflow);
+				workflow = await transactionManager.save<WorkflowEntity>(newWorkflow);
+				const newSharedWorkflow = new SharedWorkflow();
+				Object.assign(newSharedWorkflow, {
+					role,
+					user: req.user,
+					workflow,
+				});
+				await transactionManager.save<SharedWorkflow>(newSharedWorkflow);
+			});
+
+			return res.json(workflow);
+		},
+	],
 	deleteWorkflow: [],
 	getWorkflow: [],
 	getWorkflows: [],
