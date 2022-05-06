a2800a08307411cc99015c7ce69aae343f2fa417
ricardo <ricardoespinoza105@gmail.com>
2022-05-05 21:17:42 -0400

    :zap: Add delete, getAll and get to the workflow resource

--- packages/cli/src/PublicApi/Services/workflow.ts
+++ packages/cli/src/PublicApi/Services/workflow.ts
@@ -1,4 +1,5 @@
 import type { INode } from 'n8n-workflow';
+import { FindManyOptions } from 'typeorm';
 import { User } from '../../databases/entities/User';
 import { WorkflowEntity } from '../../databases/entities/WorkflowEntity';
 import { Db } from '../..';
@@ -16,14 +17,27 @@ export async function getSharedWorkflowIds(user: User): Promise<number[]> {
 
 export async function getSharedWorkflow(
 	user: User,
-	workflowId: string | undefined,
+	workflowId?: string | undefined,
 ): Promise<SharedWorkflow | undefined> {
-	const sharedWorkflows = await Db.collections.SharedWorkflow.findOne({
+	const sharedWorkflow = await Db.collections.SharedWorkflow.findOne({
 		where: {
 			...(!isInstanceOwner(user) && { user }),
-			workflow: { id: workflowId },
+			...(workflowId && { workflow: { id: workflowId } }),
 		},
 		relations: ['workflow'],
+	});
+	return sharedWorkflow;
+}
+
+export async function getSharedWorkflows(
+	user: User,
+	resolveWorkflows = false,
+): Promise<SharedWorkflow[]> {
+	const sharedWorkflows = await Db.collections.SharedWorkflow.find({
+		where: {
+			...(!isInstanceOwner(user) && { user }),
+		},
+		...(resolveWorkflows && { relations: ['workflow'] }),
 	});
 	return sharedWorkflows;
 }
@@ -43,6 +57,22 @@ export async function activeWorkflow(workflow: WorkflowEntity): Promise<void> {
 
 export async function desactiveWorkflow(workflow: WorkflowEntity): Promise<void> {
 	await Db.collections.Workflow.update(workflow.id, { active: false, updatedAt: new Date() });
+}
+
+export async function deleteWorkflow(workflow: WorkflowEntity): Promise<void> {
+	await Db.collections.Workflow.remove(workflow);
+}
+
+export async function getWorkflows(
+	options: FindManyOptions<WorkflowEntity>,
+): Promise<WorkflowEntity[]> {
+	const workflows = await Db.collections.Workflow.find(options);
+	return workflows;
+}
+
+export async function getWorkflowsCount(options: FindManyOptions<WorkflowEntity>): Promise<number> {
+	const count = await Db.collections.Workflow.count(options);
+	return count;
 }
 
 export async function updateWorkflow(
