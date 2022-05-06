12ef9624f79dcb0d75404c464b12c2a782246d05
agobrech <ael.gobrecht@gmail.com>
2022-05-06 10:45:45 +0200

    Jira new cred format

--- packages/nodes-base/credentials/JiraSoftwareCloudApi.credentials.ts
+++ packages/nodes-base/credentials/JiraSoftwareCloudApi.credentials.ts
@@ -1,5 +1,8 @@
 import {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
 	ICredentialType,
+	IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
 
@@ -28,4 +31,18 @@ export class JiraSoftwareCloudApi implements ICredentialType {
 			placeholder: 'https://example.atlassian.net',
 		},
 	];
+	async authenticate(credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
+		console.log('credentials', credentials.password);
+		const bearer = Buffer.from(`${credentials.email}:${credentials!.password || credentials!.apiToken}`).toString('base64');
+		requestOptions.headers!['Authorization'] = `Basic ${bearer}`;
+		console.log('### PASSED #### ');
+		console.log(`Bearer: ${bearer}`);
+		return requestOptions;
+	}
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.domain}}',
+			url: '/rest/api/2/project',
+		},
+	};
 }
