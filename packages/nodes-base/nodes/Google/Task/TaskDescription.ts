c5d7b3dd26ef40fb77df49b13c3855ef6fbe19dd
Iván Ovejero <ivov.src@gmail.com>
2022-05-06 14:20:30 +0200

    :shirt: Add exception for `<strong>` to render

--- packages/nodes-base/nodes/Google/Task/TaskDescription.ts
+++ packages/nodes-base/nodes/Google/Task/TaskDescription.ts
@@ -360,7 +360,8 @@ export const taskFields: INodeProperties[] = [
 				name: 'showCompleted',
 				type: 'boolean',
 				default: true,
-				description: 'Flag indicating whether completed tasks are returned in the result. &lt;strong&gt;Show Hidden&lt;/strong&gt; must also be True to show tasks completed in first party clients such as the web UI or Google\'s mobile apps.',
+				// eslint-disable-next-line n8n-nodes-base/node-param-description-unencoded-angle-brackets
+				description: 'Flag indicating whether completed tasks are returned in the result. <strong>Show Hidden</strong> must also be True to show tasks completed in first party clients such as the web UI or Google\'s mobile apps.',
 			},
 			{
 				displayName: 'Show Deleted',
