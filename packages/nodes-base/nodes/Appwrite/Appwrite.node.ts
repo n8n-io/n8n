7c5dff79eb389c2439b2269327ab675518a1d8b1
Saad Mujeeb <63791098+Saad-ISAA@users.noreply.github.com>
2022-05-10 12:42:39 +0500

    [modify] code optimize

--- packages/nodes-base/nodes/Appwrite/Appwrite.node.ts
+++ packages/nodes-base/nodes/Appwrite/Appwrite.node.ts
@@ -12,9 +12,13 @@ import {
 	INodeTypeDescription,
 } from 'n8n-workflow';
 
+import { appwriteApiRequest } from './GenericFunctions';
+
 import {
 	OptionsWithUri,
 } from 'request';
+
+import { documentFields, documentOperations } from './DocumentDescription';
 
 export class Appwrite implements INodeType {
 	description: INodeTypeDescription = {
@@ -23,6 +27,7 @@ export class Appwrite implements INodeType {
 			icon: 'file:Appwrite.svg',
 			group: ['transform'],
 			version: 1,
+			subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
 			description: 'Consume Appwrite API',
 			defaults: {
 					name: 'Appwrite',
@@ -52,156 +57,8 @@ export class Appwrite implements INodeType {
 					required: true,
 					description: 'Resource to consume',
 			},
-			{
-					displayName: 'Operation',
-					name: 'operation',
-					type: 'options',
-					displayOptions: {
-							show: {
-									resource: [
-											'document',
-									],
-							},
-					},
-					options: [
-							{
-									name: 'Create',
-									value: 'createDoc',
-									description: 'Create a document in collection',
-							},
-							{
-								name: 'Get',
-								value: 'getDoc',
-								description: 'Get a document in collection',
-							},
-							{
-								name: 'Get All',
-								value: 'getAllDocs',
-								description: 'Get all documents in collection',
-							},
-							{
-								name: 'Update',
-								value: 'updateDoc',
-								description: 'Get all documents in collection',
-							},
-							{
-								name: 'Delete',
-								value: 'deleteDoc',
-								description: 'Delete document in collection',
-							},
-					],
-					default: 'getAllDocs',
-					description: 'The operation to perform.',
-			},
-			{
-					displayName: 'Collection ID',
-					name: 'collectionId',
-					type: 'string',
-					required: true,
-					displayOptions: {
-							show: {
-									operation: [
-											'createDoc',
-											'getAllDocs',
-											'getDoc',
-											'updateDoc',
-											'deleteDoc',
-									],
-									resource: [
-											'document',
-									],
-							},
-					},
-					default:'',
-					description:'Collection to list/create documents in',
-			},
-			{
-				displayName: 'Document ID',
-				name: 'documentId',
-				type: 'string',
-				required: true,
-				displayOptions: {
-						show: {
-								operation: [
-										'getDoc',
-										'updateDoc',
-										'deleteDoc',
-								],
-								resource: [
-										'document',
-								],
-						},
-				},
-				default:'',
-				description:'Document ID to get from collection',
-			},
-			{
-				displayName: 'Body',
-				name: 'body',
-				type: 'json',
-				required: true,
-				displayOptions: {
-						show: {
-								operation: [
-										'createDoc',
-										'updateDoc',
-								],
-								resource: [
-										'document',
-								],
-						},
-				},
-				default:'{"attributeName1":"attribute-value1", "attributeName2":"attribute-value2"}',
-				description:'Body to create document with',
-			},
-			{
-				displayName: 'Options',
-				name: 'options',
-				type: 'collection',
-				placeholder: 'Add Field',
-				default: {},
-				displayOptions: {
-						show: {
-							operation: [
-								'createDoc',
-								'getAllDocs',
-							],
-							resource: [
-									'document',
-							],
-						},
-				},
-				options: [
-						{
-								displayName: 'limit',
-								name: 'limit',
-								type: 'number',
-								default: '',
-								description: 'Maximum number of documents to return in response. By default will return maximum 25 results. Maximum of 100 results allowed per request.'
-						},
-						{
-								displayName: 'offset',
-								name: 'offset',
-								type: 'number',
-								default: '',
-								description: 'Offset value. The default value is 0. Use this value to manage pagination. [learn more about pagination](https://appwrite.io/docs/pagination)'
-						},
-						{
-							displayName: 'cursor',
-							name: 'cursor',
-							type: 'string',
-							default: '',
-							description: 'ID of the document used as the starting point for the query, excluding the document itself. Should be used for efficient pagination when working with large sets of data. [learn more about pagination](https://appwrite.io/docs/pagination)',
-						},
-						{
-							displayName: 'cursorDirection',
-							name: 'cursorDirection',
-							type: 'string',
-							default: '',
-							description: 'Direction of the cursor.',
-						},
-				],
-			},
+			...documentOperations,
+			...documentFields
 		],
 	};
 
@@ -240,8 +97,6 @@ export class Appwrite implements INodeType {
 		let responseData;
 		const resource = this.getNodeParameter('resource', 0) as string;
 		const operation = this.getNodeParameter('operation', 0) as string;
-		//Get credentials the user provided for this node
-		const credentials = await this.getCredentials('appwriteApi') as IDataObject;
 
 		if (resource === 'document') {
 
@@ -250,25 +105,12 @@ export class Appwrite implements INodeType {
 				// get collectionID input
 				const collectionId = this.getNodeParameter('collectionId', 0) as string;
 
-				let options = {} as OptionsWithUri;
-
-				options = {
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'X-Appwrite-Project': `${credentials.projectId}`,
-						'X-Appwrite-Key': `${credentials.apiKey}`,
-					},
-					method: 'POST',
-					uri: `${credentials.url}/v1/database/collections/${collectionId}/documents`,
-					json: true,
-					body: {
-						documentId: "unique()",
-						data: this.getNodeParameter('body', 0) as IDataObject,
-					},
+				const body: IDataObject = {
+					documentId: "unique()",
+					data: this.getNodeParameter('body', 0) as IDataObject,
 				};
 
-				responseData = await this.helpers.request(options);
+				responseData = await appwriteApiRequest.call(this, 'POST', collectionId, '', body);
 			}
 
 			if (operation === 'getAllDocs') {
@@ -292,21 +134,7 @@ export class Appwrite implements INodeType {
 						qs.cursorDirection = optionalFields.cursorDirection;
 				}
 
-				let options = {} as OptionsWithUri;
-
-				options = {
-					headers: {
-						'Accept': 'application/json',
-						'X-Appwrite-Project': `${credentials.projectId}`,
-						'X-Appwrite-Key': `${credentials.apiKey}`,
-					},
-					method: 'GET',
-					uri: `${credentials.url}/v1/database/collections/${collectionId}/documents`,
-					json: true,
-					qs
-				};
-
-				responseData = await this.helpers.request(options);
+				responseData = await appwriteApiRequest.call(this, 'GET', collectionId, '', {}, qs);
 			}
 
 			if (operation === 'getDoc') {
@@ -316,20 +144,7 @@ export class Appwrite implements INodeType {
 				// get documentID input
 				const documentId = this.getNodeParameter('documentId', 0) as string;
 
-				let options = {} as OptionsWithUri;
-
-				options = {
-					headers: {
-						'Accept': 'application/json',
-						'X-Appwrite-Project': `${credentials.projectId}`,
-						'X-Appwrite-Key': `${credentials.apiKey}`,
-					},
-					method: 'GET',
-					uri: `${credentials.url}/v1/database/collections/${collectionId}/documents/${documentId}`,
-					json: true,
-				};
-
-				responseData = await this.helpers.request(options);
+				responseData = await appwriteApiRequest.call(this, 'GET', collectionId, documentId);
 			}
 
 			if (operation === 'updateDoc') {
@@ -339,24 +154,11 @@ export class Appwrite implements INodeType {
 				// get documentID input
 				const documentId = this.getNodeParameter('documentId', 0) as string;
 
-				let options = {} as OptionsWithUri;
-
-				options = {
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'X-Appwrite-Project': `${credentials.projectId}`,
-						'X-Appwrite-Key': `${credentials.apiKey}`,
-					},
-					method: 'PATCH',
-					uri: `${credentials.url}/v1/database/collections/${collectionId}/documents/${documentId}`,
-					json: true,
-					body: {
-						data: this.getNodeParameter('body', 0) as IDataObject,
-					},
+				const body: IDataObject = {
+					data: this.getNodeParameter('body', 0) as IDataObject,
 				};
 
-				responseData = await this.helpers.request(options);
+				responseData = await appwriteApiRequest.call(this, 'PATCH', collectionId, documentId, body);
 			}
 
 			if (operation === 'deleteDoc') {
@@ -366,21 +168,7 @@ export class Appwrite implements INodeType {
 				// get documentID input
 				const documentId = this.getNodeParameter('documentId', 0) as string;
 
-				let options = {} as OptionsWithUri;
-
-				options = {
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json',
-						'X-Appwrite-Project': `${credentials.projectId}`,
-						'X-Appwrite-Key': `${credentials.apiKey}`,
-					},
-					method: 'DELETE',
-					uri: `${credentials.url}/v1/database/collections/${collectionId}/documents/${documentId}`,
-					json: true,
-				};
-
-				responseData = await this.helpers.request(options);
+				responseData = await appwriteApiRequest.call(this, 'DELETE', collectionId, documentId);
 			}
 		}
 
---
+++ packages/nodes-base/nodes/Appwrite/DocumentDescription.ts
@@ -0,0 +1,159 @@
+import {
+	INodeProperties,
+} from 'n8n-workflow';
+
+export const documentOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		displayOptions: {
+				show: {
+						resource: [
+								'document',
+						],
+				},
+		},
+		options: [
+				{
+						name: 'Create',
+						value: 'createDoc',
+						description: 'Create a document in collection',
+				},
+				{
+					name: 'Get',
+					value: 'getDoc',
+					description: 'Get a document in collection',
+				},
+				{
+					name: 'Get All',
+					value: 'getAllDocs',
+					description: 'Get all documents in collection',
+				},
+				{
+					name: 'Update',
+					value: 'updateDoc',
+					description: 'Get all documents in collection',
+				},
+				{
+					name: 'Delete',
+					value: 'deleteDoc',
+					description: 'Delete document in collection',
+				},
+		],
+		default: 'getAllDocs',
+		description: 'The operation to perform.',
+	}
+];
+
+export const documentFields: INodeProperties[] = [
+	{
+		displayName: 'Collection ID',
+		name: 'collectionId',
+		type: 'string',
+		required: true,
+		displayOptions: {
+				show: {
+						operation: [
+								'createDoc',
+								'getAllDocs',
+								'getDoc',
+								'updateDoc',
+								'deleteDoc',
+						],
+						resource: [
+								'document',
+						],
+				},
+		},
+		default:'',
+		description:'Collection to list/create documents in',
+	},
+	{
+		displayName: 'Document ID',
+		name: 'documentId',
+		type: 'string',
+		required: true,
+		displayOptions: {
+				show: {
+						operation: [
+								'getDoc',
+								'updateDoc',
+								'deleteDoc',
+						],
+						resource: [
+								'document',
+						],
+				},
+		},
+		default:'',
+		description:'Document ID to get from collection',
+	},
+	{
+		displayName: 'Body',
+		name: 'body',
+		type: 'json',
+		required: true,
+		displayOptions: {
+				show: {
+						operation: [
+								'createDoc',
+								'updateDoc',
+						],
+						resource: [
+								'document',
+						],
+				},
+		},
+		default:'{"attributeName1":"attribute-value1", "attributeName2":"attribute-value2"}',
+		description:'Body to create document with',
+	},
+	{
+		displayName: 'Options',
+		name: 'options',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+				show: {
+					operation: [
+						'createDoc',
+						'getAllDocs',
+					],
+					resource: [
+							'document',
+					],
+				},
+		},
+		options: [
+				{
+						displayName: 'limit',
+						name: 'limit',
+						type: 'number',
+						default: '',
+						description: 'Maximum number of documents to return in response. By default will return maximum 25 results. Maximum of 100 results allowed per request.'
+				},
+				{
+						displayName: 'offset',
+						name: 'offset',
+						type: 'number',
+						default: '',
+						description: 'Offset value. The default value is 0. Use this value to manage pagination. [learn more about pagination](https://appwrite.io/docs/pagination)'
+				},
+				{
+					displayName: 'cursor',
+					name: 'cursor',
+					type: 'string',
+					default: '',
+					description: 'ID of the document used as the starting point for the query, excluding the document itself. Should be used for efficient pagination when working with large sets of data. [learn more about pagination](https://appwrite.io/docs/pagination)',
+				},
+				{
+					displayName: 'cursorDirection',
+					name: 'cursorDirection',
+					type: 'string',
+					default: '',
+					description: 'Direction of the cursor.',
+				},
+			],
+	}
+];
