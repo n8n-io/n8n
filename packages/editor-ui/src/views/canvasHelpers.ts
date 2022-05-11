2af41458a17155b4b54d903326bd4b8fb8e2518e
Milorad Filipović <miloradfilipovic19@gmail.com>
2022-05-11 12:52:16 +0200

    👌 Extracting quickstart note name to constant.

--- packages/editor-ui/src/views/canvasHelpers.ts
+++ packages/editor-ui/src/views/canvasHelpers.ts
@@ -1,5 +1,5 @@
 import { getStyleTokenValue, isNumber } from "@/components/helpers";
-import { NODE_OUTPUT_DEFAULT_KEY, START_NODE_TYPE, STICKY_NODE_TYPE } from "@/constants";
+import { NODE_OUTPUT_DEFAULT_KEY, START_NODE_TYPE, STICKY_NODE_TYPE, QUICKSTART_NOTE_NAME } from "@/constants";
 import { IBounds, INodeUi, IZoomConfig, XYPosition } from "@/Interface";
 import { Connection, Endpoint, Overlay, OverlaySpec, PaintStyle } from "jsplumb";
 import {
@@ -49,7 +49,7 @@ export const DEFAULT_START_NODE = {
 };
 
 export const WELCOME_STICKY_NODE = {
-	name: "QuickstartNote",
+	name: QUICKSTART_NOTE_NAME,
 	type: STICKY_NODE_TYPE,
 	typeVersion: 1,
 	position: [
