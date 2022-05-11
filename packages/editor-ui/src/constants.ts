6f30b2ed2beb299bf0c366952607d936f4f69c39
Milorad Filipović <miloradfilipovic19@gmail.com>
2022-05-11 12:52:16 +0200

    👌 Extracting quickstart note name to constant.

--- packages/editor-ui/src/constants.ts
+++ packages/editor-ui/src/constants.ts
@@ -12,6 +12,7 @@ export const MIN_WORKFLOW_NAME_LENGTH = 1;
 export const MAX_WORKFLOW_NAME_LENGTH = 128;
 export const DUPLICATE_POSTFFIX = ' copy';
 export const NODE_OUTPUT_DEFAULT_KEY = '_NODE_OUTPUT_DEFAULT_KEY_';
+export const QUICKSTART_NOTE_NAME = '_QUICKSTART_NOTE_';
 
 // tags
 export const MAX_TAG_NAME_LENGTH = 24;
