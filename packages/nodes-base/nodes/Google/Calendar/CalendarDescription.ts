112ab7704f0642f1df66a25ae208c3ed56fbf8ad
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2022-05-14 11:44:19 +0300

    fix(Google Calendar Node): Make it work with public calendars and clean up (#3283)

--- packages/nodes-base/nodes/Google/Calendar/CalendarDescription.ts
+++ packages/nodes-base/nodes/Google/Calendar/CalendarDescription.ts
@@ -7,6 +7,7 @@ export const calendarOperations: INodeProperties[] = [
 		displayName: 'Operation',
 		name: 'operation',
 		type: 'options',
+		noDataExpression: true,
 		displayOptions: {
 			show: {
 				resource: [
@@ -22,7 +23,6 @@ export const calendarOperations: INodeProperties[] = [
 			},
 		],
 		default: 'availability',
-		description: 'The operation to perform.',
 	},
 ];
 
