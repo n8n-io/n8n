64cb690ed0f07fd09cfc8ff5421b6b5522e995bf
Iván Ovejero <ivov.src@gmail.com>
2022-05-17 16:23:45 +0200

    :zap: Set one attribute per line

--- packages/editor-ui/src/components/ParameterOptions.vue
+++ packages/editor-ui/src/components/ParameterOptions.vue
@@ -1,14 +1,43 @@
 <template>
 	<div class="parameter-options" v-if="displayOptionsComputed">
-		<el-dropdown trigger="click" @command="optionSelected" size="mini">
+		<el-dropdown
+			trigger="click"
+			@command="optionSelected"
+			size="mini"
+		>
 			<span class="el-dropdown-link">
-				<font-awesome-icon icon="cogs" class="reset-icon clickable" :title="$locale.baseText('parameterInput.parameterOptions')"/>
+				<font-awesome-icon
+					icon="cogs"
+					class="reset-icon clickable"
+					:title="$locale.baseText('parameterInput.parameterOptions')"
+				/>
 			</span>
 			<el-dropdown-menu slot="dropdown">
-				<el-dropdown-item command="addExpression" v-if="parameter.noDataExpression !== true && !isValueExpression">{{ $locale.baseText('parameterInput.addExpression') }}</el-dropdown-item>
-				<el-dropdown-item command="removeExpression" v-if="parameter.noDataExpression !== true && isValueExpression">{{ $locale.baseText('parameterInput.removeExpression') }}</el-dropdown-item>
-				<el-dropdown-item command="refreshOptions" v-if="hasRemoteMethod">{{ $locale.baseText('parameterInput.refreshList') }}</el-dropdown-item>
-				<el-dropdown-item command="resetValue" :disabled="isDefault" divided>{{ $locale.baseText('parameterInput.resetValue') }}</el-dropdown-item>
+				<el-dropdown-item
+					v-if="parameter.noDataExpression !== true && !isValueExpression"
+					command="addExpression"
+				>
+					{{ $locale.baseText('parameterInput.addExpression') }}
+				</el-dropdown-item>
+				<el-dropdown-item
+					v-if="parameter.noDataExpression !== true && isValueExpression"
+					command="removeExpression"
+				>
+					{{ $locale.baseText('parameterInput.removeExpression') }}
+				</el-dropdown-item>
+				<el-dropdown-item
+					v-if="hasRemoteMethod"
+					command="refreshOptions"
+				>
+					{{ $locale.baseText('parameterInput.refreshList') }}
+				</el-dropdown-item>
+				<el-dropdown-item
+					command="resetValue"
+					:disabled="isDefault"
+					divided
+				>
+					{{ $locale.baseText('parameterInput.resetValue') }}
+				</el-dropdown-item>
 			</el-dropdown-menu>
 		</el-dropdown>
 	</div>
