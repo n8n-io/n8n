aa66010118b9ec19c28bb42da7f68a6a1e3141c7
Michael Kret <michael.k@radency.com>
2022-05-17 15:18:47 +0300

    :zap: test credentials fix

--- packages/nodes-base/credentials/TodoistApi.credentials.ts
+++ packages/nodes-base/credentials/TodoistApi.credentials.ts
@@ -1,7 +1,9 @@
 import {
 	IAuthenticateHeaderAuth,
+	ICredentialDataDecryptedObject,
 	ICredentialTestRequest,
 	ICredentialType,
+	IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
 
@@ -18,13 +20,15 @@ export class TodoistApi implements ICredentialType {
 			default: '',
 		},
 	];
-	authenticate: IAuthenticateHeaderAuth = {
-		type: 'headerAuth',
-		properties: {
-			name: 'Authorization',
-			value: '=Bearer {{$credentials.apiKey}}',
-		},
-	};
+	async authenticate(credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
+		requestOptions.headers = {
+			'authorization': `Bearer ${credentials.apiKey}`,
+		};
+		if (requestOptions.body && Object.keys(requestOptions.body).length === 0) {
+			delete requestOptions.body;
+		}
+		return requestOptions;
+	}
 	test: ICredentialTestRequest = {
 		request: {
 			baseURL: 'https://api.todoist.com/rest/v1',
