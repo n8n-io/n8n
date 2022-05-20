c1c43171681f4b2825fa6a1e36bf87d5bc5d58bf
agobrech <45268029+agobrech@users.noreply.github.com>
2022-05-20 13:52:53 +0200

    :shirt: Apply `node-param-type-options-missing-from-limit` (#3336)
    * Rule workig as intended
    
    * :pencil2: Uncomment rules
    
    Co-authored-by: Iván Ovejero <ivov.src@gmail.com>

--- packages/nodes-base/nodes/Bitwarden/descriptions/MemberDescription.ts
+++ packages/nodes-base/nodes/Bitwarden/descriptions/MemberDescription.ts
@@ -135,6 +135,9 @@ export const memberFields: INodeProperties[] = [
 		displayName: 'Limit',
 		name: 'limit',
 		type: 'number',
+		typeOptions: {
+			minValue: 1,
+		},
 		default: 10,
 		description: 'Max number of results to return',
 		displayOptions: {
