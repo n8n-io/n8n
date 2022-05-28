2ca1eaadaea2972eb603118fb3513ef2d4b8c99d
Michael Kret <michael.k@radency.com>
2022-05-28 14:26:05 +0300

    :hammer: trigger node

---
+++ packages/nodes-base/nodes/WebSocket/WebSocketTrigger.node.ts
@@ -0,0 +1,148 @@
+import {
+	ITriggerFunctions,
+} from 'n8n-core';
+
+import {
+	IDataObject,
+	INodeType,
+	INodeTypeDescription,
+	ITriggerResponse,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+import { WebSocket } from 'ws';
+
+export class WebSocketTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'WebSocket Trigger',
+		name: 'webSocketTrigger',
+		icon: 'file:websocket.svg',
+		group: ['trigger'],
+		version: 1,
+		description: 'Subscribe to redis channel',
+		defaults: {
+			name: 'WebSocket Trigger',
+		},
+		inputs: [],
+		outputs: ['main'],
+		credentials: [
+		],
+		properties: [
+			{
+				displayName: 'Address',
+				name: 'address',
+				type: 'string',
+				default: '',
+				required: true,
+			},
+			{
+				displayName: 'Output Field',
+				name: 'outputField',
+				type: 'string',
+				default: 'message',
+				required: true,
+			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add Option',
+				default: {},
+				options: [
+					{
+						displayName: 'Binary Type',
+						name: 'binaryType',
+						type: 'options',
+						default: 'nodebuffer',
+						options: [
+							{
+								name: 'Node Buffer',
+								value: 'nodebuffer',
+							},
+							{
+								name: 'Array Buffer',
+								value: 'arraybuffer',
+							},
+							{
+								name: 'Fragments',
+								value: 'fragments',
+							},
+						],
+					},
+					{
+						displayName: 'Message Encoding',
+						name: 'encoding',
+						type: 'string',
+						default: 'utf8',
+					},
+					{
+						displayName: 'Parse JSON',
+						name: 'toJson',
+						type: 'boolean',
+						default: false,
+					},
+				],
+			},
+		],
+	};
+
+	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
+		const address = this.getNodeParameter('address', 0) as string;
+		const outputField = this.getNodeParameter('outputField', 0) as string;
+		const options = this.getNodeParameter('options', 0) as IDataObject;
+		const encoding  = options?.encoding ? options.encoding as string : 'utf8';
+
+		const self = this;
+		const client = new WebSocket(address);
+
+		if (options.binaryType) {
+			client.binaryType = options.binaryType as BinaryType;
+		}
+
+
+		async function manualTriggerFunction() {
+			await new Promise((resolve, reject) => {
+
+				client.on('open', () =>{
+					console.log('connection open');
+				});
+
+				client.on('message', (data, isBinary) => {
+					let message = isBinary ? data : data.toString(encoding as BufferEncoding);
+					if (options.toJson) {
+						try {
+							message = JSON.parse(message as string);
+						} catch (error) {
+							console.log('Parse JSON failed, not a valid Json');
+						}
+					}
+					console.log('connection message', message);
+
+					self.emit([self.helpers.returnJsonArray({[outputField]: message})]);
+					resolve(true);
+				});
+
+				client.on('error', (error) => {
+					console.log('connection error');
+
+					reject(error);
+				});
+			});
+		}
+
+		if (this.getMode() === 'trigger') {
+			await manualTriggerFunction();
+		}
+
+		async function closeFunction() {
+			client.terminate();
+		}
+
+		return {
+			closeFunction,
+			manualTriggerFunction,
+		};
+	}
+}
+
+type BinaryType = 'nodebuffer' | 'arraybuffer' | 'fragments';
