f5f25a53b74d9b18d5a9670cdf9851864d1e17b8
cxgarcia <schlaubitzcristobal@gmail.com>
2022-05-28 11:41:43 +0200

    feat: media type and start of template components

--- packages/nodes-base/nodes/WhatsApp/MessagesDescription.ts
+++ packages/nodes-base/nodes/WhatsApp/MessagesDescription.ts
@@ -3,6 +3,17 @@ import {
 } from 'n8n-workflow';
 
 
+export const mediaTypes = [
+	"image",
+	"video",
+	"audio",
+	"sticker",
+	"document",
+]
+
+
+
+
 export const messageFields: INodeProperties[] = [
 	{
 		displayName: 'Phone number ID',
@@ -11,14 +22,14 @@ export const messageFields: INodeProperties[] = [
 		default: '',
 		placeholder: '',
 		required: true,
-		description: 'The ID of the business account\'s phone number from which the message will be sent from.',
+		description: 'The ID of the business account\'s phone number from which the message will be sent from',
 	},
 	{
 		displayName: 'Recipient\'s phone number',
-		name: 'to',
+		name: 'recipientPhoneNumber',
 		type: 'string',
 		default: '',
-		description: 'Phone number of the recipient of the message, starting with the country code without the leading +.',
+		description: 'Phone number of the recipient of the message, starting with the country code without the leading +',
 	},
 	{
 		displayName: 'Type',
@@ -34,19 +45,38 @@ export const messageFields: INodeProperties[] = [
 				name: 'Template',
 				value: 'template',
 			},
+			{
+				name: "Image",
+				value: "image",
+			},
+			{
+				name: "Document",
+				value: "document",
+			},
+			{
+				name: "Audio",
+				value: "audio",
+			},
+			{
+				name: "Video",
+				value: "video",
+			},
 		],
 		default: "text",
-		description: 'The type of the message.',
+		description: 'The type of the message',
 	},
 ];
 
 export const messageTypeFields: INodeProperties[] = [
+	// ----------------------------------
+	//         type: text
+	// ----------------------------------
 	{
-		displayName: "Body",
-		name: "body",
+		displayName: "Text Body",
+		name: "textBody",
 		type: "string",
 		default: "",
-		description: "The body of the message (max 4096 characters).",
+		description: "The body of the message (max 4096 characters)",
 		displayOptions: {
 			show: {
 				type: [
@@ -57,10 +87,10 @@ export const messageTypeFields: INodeProperties[] = [
 	},
 	{
 		displayName: "Preview URL",
-		name: "preview_url",
+		name: "previewUrl",
 		type: "boolean",
 		default: false,
-		description: "Allows for URL previews in text messages.",
+		description: 'Allows for URL previews in text messages',
 		displayOptions: {
 			show: {
 				type: [
@@ -69,9 +99,118 @@ export const messageTypeFields: INodeProperties[] = [
 			},
 		},
 	},
+	// ----------------------------------
+	//         type: media
+	// ----------------------------------
+	{
+		displayName: "Link or ID",
+		name: "mediaPath",
+		type: "options",
+		default: "useMediaLink",
+		description: "Use a link or an ID to upload a media file",
+		options: [
+			{
+				name: "Link",
+				value: "useMediaLink",
+				description: "When using a ink, WhatsApp will download the media, saving you the step of uploading media yourself"
+			},
+			{
+				name: "ID",
+				value: "useMediaId",
+				description: "You can use an ID if you have already uploaded the media to WhatsApp"
+			}
+		],
+		displayOptions: {
+			show: {
+				type: mediaTypes
+			},
+		},
+	},
+	{
+		displayName: "Link",
+		name: "mediaLink",
+		type: "string",
+		default: "",
+		description: 'Link of the media to be sent',
+		displayOptions: {
+			show: {
+				type: mediaTypes,
+				mediaPath: [
+					"useMediaLink"
+				]
+			},
+		},
+	},
+	{
+		displayName: "Provider Name",
+		name: "mediaLinkProvider",
+		type: "string",
+		default: "",
+		description: 'Provider name of the media to be sent',
+		displayOptions: {
+			show: {
+				type: mediaTypes,
+				mediaPath: [
+					"useMediaLink"
+				]
+			},
+		},
+	},
+	{
+		displayName: "ID",
+		name: "mediaId",
+		type: "string",
+		default: "",
+		description: 'ID of the media to be sent',
+		displayOptions: {
+			show: {
+				type: mediaTypes,
+				mediaPath: [
+					"useMediaId"
+				]
+			},
+		},
+	},
+	{
+		displayName: "Filename",
+		name: "mediaFilename",
+		type: "string",
+		default: "",
+		description: "The name of the file (required when using a file ID)",
+		displayOptions:{
+			show: {
+				type: [
+					"document"
+				],
+				mediaPath: [
+					"useMediaId"
+				]
+			},
+		},
+	},
+	{
+		displayName: "Media Caption",
+		name: "mediaCaption",
+		type: "string",
+		default: "",
+		description: "The caption of the media",
+		displayOptions:{
+			show: {
+				type: [
+					"image",
+					"video",
+					"document"
+				],
+			},
+		},
+	},
+
+	// ----------------------------------
+	//         type: template
+	// ----------------------------------
 	{
 		displayName: "Name",
-		name: "name",
+		name: "templateName",
 		default: "",
 		type: "string",
 		displayOptions: {
@@ -82,13 +221,14 @@ export const messageTypeFields: INodeProperties[] = [
 			},
 		},
 		required: true,
-		description: "Name of the template.",
+		description: 'Name of the template',
 	},
 	{
-		displayName: "Language",
-		name: "language",
+		//TODO: would be nice to change this to a searchable dropdown with all the possible language codes
+		displayName: "Language code",
+		name: "templateLanguageCode",
 		type: "string",
-		default: "en_US",
+		default: "en",
 		displayOptions: {
 			show: {
 				type: [
@@ -96,7 +236,108 @@ export const messageTypeFields: INodeProperties[] = [
 				],
 			},
 		},
-		description: "Specifies the language the template is rendered in."
+		description: 'The code of the language or locale to use. Accepts both language and language_locale formats (e.g., en and en_US).'
 
-	}
+	},
+	//TODO: dynamic template components
+	// {
+	// 	displayName: 'Template header',
+	// 	name: 'templateHeader',
+	// 	placeholder: 'Add Component',
+	// 	type: 'fixedCollection',
+	// 	typeOptions: {
+	// 		multipleValues: true,
+	// 	},
+	// 	displayOptions: {
+	// 		show: {
+	// 			type: [
+	// 				'template',
+	// 			],
+	// 		},
+	// 	},
+	// 	default: {},
+	// 	options: [
+	// 		{
+	// 			name: 'componentParameters',
+	// 			displayName: 'Component parameters',
+	// 			values: [
+	// 			{
+	// 				displayName: 'Parameters type',
+	// 				name: 'templateParametersType',
+	// 				type: 'options',
+	// 				options: [
+	// 					{
+	// 						name: 'Text',
+	// 						value: 'text',
+	// 					},
+	// 					{
+	// 						name: 'Image',
+	// 						value: 'image',
+	// 					},
+	// 					{
+	// 						name: 'Document',
+	// 						value: 'document',
+	// 					},
+	// 					{
+	// 						name: 'Video',
+	// 						value: 'video',
+	// 					},
+	// 				],
+	// 				default: 0,
+	// 				description: 'The type of the parameter',
+	// 			},
+	// 				// eslint-disable-next-line n8n-nodes-base/node-param-operation-without-no-data-expression
+	// 				{
+	// 					displayName: 'Text',
+	// 					name: 'componentText',
+	// 					type: 'string',
+	// 					default: '',
+	// 					description: 'Operation to decide where the the data should be mapped to',
+	// 				},
+	// 				{
+	// 					displayName: 'Value 2',
+	// 					name: 'value2',
+	// 					type: 'string',
+	// 					displayOptions: {
+	// 						hide: {
+	// 							operation: [
+	// 								'regex',
+	// 								'notRegex',
+	// 							],
+	// 						},
+	// 					},
+	// 					default: '',
+	// 					description: 'The value to compare with the first one',
+	// 				},
+	// 				{
+	// 					displayName: 'Regex',
+	// 					name: 'value2',
+	// 					type: 'string',
+	// 					displayOptions: {
+	// 						show: {
+	// 							operation: [
+	// 								'regex',
+	// 								'notRegex',
+	// 							],
+	// 						},
+	// 					},
+	// 					default: '',
+	// 					placeholder: '/text/i',
+	// 					description: 'The regex which has to match',
+	// 				},
+	// 				{
+	// 					displayName: 'Output',
+	// 					name: 'output',
+	// 					type: 'number',
+	// 					typeOptions: {
+	// 						minValue: 0,
+	// 						maxValue: 3,
+	// 					},
+	// 					default: 0,
+	// 					description: 'The index of output to which to send data to if rule matches',
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// },
 ]
