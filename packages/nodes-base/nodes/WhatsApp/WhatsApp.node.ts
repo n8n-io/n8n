5761ea9ff68ff8eeeb6e92354b301ff1c22f7a74
cxgarcia <schlaubitzcristobal@gmail.com>
2022-05-28 12:02:25 +0200

    fix: format

--- packages/nodes-base/nodes/WhatsApp/WhatsApp.node.ts
+++ packages/nodes-base/nodes/WhatsApp/WhatsApp.node.ts
@@ -1,49 +1,42 @@
-import {
-		IExecuteFunctions,
-} from 'n8n-core';
+import { IExecuteFunctions } from 'n8n-core';
+
+import { apiRequest } from './GenericFunctions';
 
 import {
-	apiRequest,
-} from './GenericFunctions';
-
-import {
-		IBinaryData,
-		IDataObject,
-		INodeExecutionData,
-		INodeType,
-		INodeTypeDescription,
-		NodeOperationError,
+	IBinaryData,
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	NodeOperationError,
 } from 'n8n-workflow';
 
-import {mediaTypes, messageFields, messageTypeFields} from './MessagesDescription';
-import {
-		OptionsWithUri,
-} from 'request';
+import { mediaTypes, messageFields, messageTypeFields } from './MessagesDescription';
+import { OptionsWithUri } from 'request';
 
 export class WhatsApp implements INodeType {
-		description: INodeTypeDescription = {
-				displayName: 'WhatsApp',
-				name: 'whatsApp',
-				icon: 'file:whatsapp.svg',
-				group: ['transform'],
-				version: 1,
-				// subtitle: '={{ $parameter["resource"] + ": " + $parameter["type"] }}',
-				subtitle: '',
-				description: 'Access WhatsApp API',
-				defaults: {
-						name: 'WhatsApp',
-				},
-				inputs: ['main'],
-				outputs: ['main'],
-				credentials: [
-					{
-						name: 'whatsAppApi',
-						required: true,
-					},
-				],
-				properties: [
-						{
-			    displayName: 'Resource',
+	description: INodeTypeDescription = {
+		displayName: 'WhatsApp',
+		name: 'whatsApp',
+		icon: 'file:whatsapp.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{ $parameter["resource"] + ": " + $parameter["type"] }}',
+		description: 'Access WhatsApp API',
+		defaults: {
+			name: 'WhatsApp',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'whatsAppApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
 				name: 'resource',
 				type: 'options',
 				noDataExpression: true,
@@ -55,125 +48,113 @@ export class WhatsApp implements INodeType {
 				],
 				default: 'messages',
 			},
-						...messageFields,
-						...messageTypeFields,
-				],
-		};
+			...messageFields,
+			...messageTypeFields,
+		],
+	};
 
-		async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-				const items = this.getInputData();
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
 		const returnData: IDataObject[] = [];
 
 		const resource = this.getNodeParameter('resource', 0) as string;
-				const phoneNumberId = this.getNodeParameter('phoneNumberId', 0);
+		const phoneNumberId = this.getNodeParameter('phoneNumberId', 0);
 		const messageType = this.getNodeParameter('type', 0) as string;
 
-
-				let body: IDataObject;
-		// For Query string
+		let body: IDataObject;
 		let qs: IDataObject;
-
 		let requestMethod: string;
 		let endpoint: string;
-		const returnAll = false;
+
 		let responseData;
-		// const type = this.getNodeParameter('type', 0) as string;
 
-				// console.log(type)
-				for (let i = 0; i < items.length; i++) {
-						try {
-								requestMethod = 'GET';
-								endpoint = '';
-								body = {};
-								qs = {};
+		for (let i = 0; i < items.length; i++) {
+			try {
+				requestMethod = 'GET';
+				endpoint = '';
+				body = {};
+				qs = {};
 
-								// ----------------------------------
-	            //         resource: messages
-	            // ----------------------------------
-								if (resource === 'messages') {
-										requestMethod = 'POST';
-										endpoint = `${phoneNumberId}/messages`;
-										body = { type: messageType,  to: this.getNodeParameter('recipientPhoneNumber', i), };
+				// ----------------------------------
+				//         resource: messages
+				// ----------------------------------
+				if (resource === 'messages') {
+					requestMethod = 'POST';
+					endpoint = `${phoneNumberId}/messages`;
+					body = { type: messageType, to: this.getNodeParameter('recipientPhoneNumber', i) };
 
-										// ----------------------------------
-	                //         type: text
-	                // ----------------------------------
-										if (messageType === 'text') {
-												const textBody = this.getNodeParameter('textBody', i) as string;
-												const previewUrl = this.getNodeParameter('previewUrl', i) as string;
+					// ----------------------------------
+					//         type: text
+					// ----------------------------------
+					if (messageType === 'text') {
+						const textBody = this.getNodeParameter('textBody', i) as string;
+						const previewUrl = this.getNodeParameter('previewUrl', i) as string;
 
-												body = {...body,
-														'text': {
-																body: textBody,
-																preview_url: previewUrl,
-														},
-												};
-												// ----------------------------------
-												//         type: template
-												// ----------------------------------
-										} else if (messageType === 'template') {
-												requestMethod = 'POST';
-												const templateName = this.getNodeParameter('templateName', i) as string;
-												const templateLanguageCode = this.getNodeParameter('templateLanguageCode', i) as string;
+						body = {
+							...body,
+							text: {
+								body: textBody,
+								preview_url: previewUrl,
+							},
+						};
+						// ----------------------------------
+						//         type: template
+						// ----------------------------------
+					} else if (messageType === 'template') {
+						requestMethod = 'POST';
+						const templateName = this.getNodeParameter('templateName', i) as string;
+						const templateLanguageCode = this.getNodeParameter('templateLanguageCode', i) as string;
 
-												body = {...body, template: {
-														name: templateName,
-														language: {
-																code: templateLanguageCode,
-														},
-												}};
-												// ----------------------------------
-												//         type: media
-												// ----------------------------------
-										} else if (mediaTypes.includes(messageType)) {
-												requestMethod = 'POST';
+						body = {
+							...body,
+							template: {
+								name: templateName,
+								language: {
+									code: templateLanguageCode,
+								},
+							},
+						};
+						// ----------------------------------
+						//         type: media
+						// ----------------------------------
+					} else if (mediaTypes.includes(messageType)) {
+						requestMethod = 'POST';
 
-												// some parameters are slightly different if using a link or an ID
-												const mediaPath = this.getNodeParameter('mediaPath', i) as string;
+						// some parameters are slightly different if using a link or an ID
+						const mediaPath = this.getNodeParameter('mediaPath', i) as string;
 
-												body = {...body, type: messageType};
+						body = { ...body, type: messageType };
 
-												if (mediaPath === 'useMediaLink') {
-														const link = this.getNodeParameter('mediaLink', i) as string;
-														const caption = this.getNodeParameter('mediaCaption', i) as string;
+						if (mediaPath === 'useMediaLink') {
+							const link = this.getNodeParameter('mediaLink', i) as string;
+							const caption = this.getNodeParameter('mediaCaption', i) as string;
 
+							body = { ...body, [messageType]: { link, caption } };
+						} else {
+							const mediaId = this.getNodeParameter('mediaId', i) as string;
+							const caption = this.getNodeParameter('mediaCaption', i) as string;
+							const filename = this.getNodeParameter('mediaFilename', i) as string;
 
-														body = {...body, [messageType]: { link, caption } };
-												} else {
-														const mediaId = this.getNodeParameter('mediaId', i) as string;
-														const caption = this.getNodeParameter('mediaCaption', i) as string;
-														const filename = this.getNodeParameter('mediaFilename', i) as string;
-
-														body = {...body, [messageType]: { id: mediaId, filename, caption } };
-
-												}
-
-										}
-								}
+							body = { ...body, [messageType]: { id: mediaId, filename, caption } };
+						}
+					}
+				}
 
 				responseData = await apiRequest.call(this, requestMethod, endpoint, body, qs);
-
-				if (returnAll === false && qs.limit) {
-					responseData = responseData.splice(0, qs.limit);
-				}
 
 				if (Array.isArray(responseData)) {
 					returnData.push.apply(returnData, responseData as IDataObject[]);
 				} else {
 					returnData.push(responseData as IDataObject);
 				}
-
-						} catch (error) {
-								if (this.continueOnFail()) {
-										returnData.push({ error: error.message });
-
-								}
-								throw error;
-						}
-
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ error: error.message });
 				}
-
-				return [this.helpers.returnJsonArray(returnData)];
+				throw error;
+			}
 		}
 
+		return [this.helpers.returnJsonArray(returnData)];
+	}
 }
