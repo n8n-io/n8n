b49d49365398f06376e53d86e6c8c5dc15f67e57
Iván Ovejero <ivov.src@gmail.com>
2022-05-30 11:33:17 +0200

    fix(core): Fix migrations on non-public Postgres schema (#3356)
    * :bug: Fix UM migration
    
    * :zap: Account for schema in `search_path`
    
    * :fire: Remove unneeded schema refs
    
    * :test_tube: Account for alt schema in DB testing
    
    * :zap: Add schema to `IncreaseTypeVarcharLimit`
    
    * :zap: Set `search_path` in every migration
    
    * :zap: Set `search_path` in down migrations

--- packages/cli/src/databases/postgresdb/migrations/1587669153312-InitialMigration.ts
+++ packages/cli/src/databases/postgresdb/migrations/1587669153312-InitialMigration.ts
@@ -14,6 +14,8 @@ export class InitialMigration1587669153312 implements MigrationInterface {
 			tablePrefix = schema + '.' + tablePrefix;
 		}
 
+		await queryRunner.query(`SET search_path TO ${schema};`);
+
 		await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${tablePrefix}credentials_entity ("id" SERIAL NOT NULL, "name" character varying(128) NOT NULL, "data" text NOT NULL, "type" character varying(32) NOT NULL, "nodesAccess" json NOT NULL, "createdAt" TIMESTAMP NOT NULL, "updatedAt" TIMESTAMP NOT NULL, CONSTRAINT PK_${tablePrefixIndex}814c3d3c36e8a27fa8edb761b0e PRIMARY KEY ("id"))`, undefined);
 		await queryRunner.query(`CREATE INDEX IF NOT EXISTS IDX_${tablePrefixIndex}07fde106c0b471d8cc80a64fc8 ON ${tablePrefix}credentials_entity (type) `, undefined);
 		await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${tablePrefix}execution_entity ("id" SERIAL NOT NULL, "data" text NOT NULL, "finished" boolean NOT NULL, "mode" character varying NOT NULL, "retryOf" character varying, "retrySuccessId" character varying, "startedAt" TIMESTAMP NOT NULL, "stoppedAt" TIMESTAMP NOT NULL, "workflowData" json NOT NULL, "workflowId" character varying, CONSTRAINT PK_${tablePrefixIndex}e3e63bbf986767844bbe1166d4e PRIMARY KEY ("id"))`, undefined);
@@ -28,6 +30,8 @@ export class InitialMigration1587669153312 implements MigrationInterface {
 		if (schema) {
 			tablePrefix = schema + '.' + tablePrefix;
 		}
+
+		await queryRunner.query(`SET search_path TO ${schema};`);
 
 		await queryRunner.query(`DROP TABLE ${tablePrefix}workflow_entity`, undefined);
 		await queryRunner.query(`DROP INDEX IDX_${tablePrefixIndex}c4d999a5e90784e8caccf5589d`, undefined);
--- packages/cli/src/databases/postgresdb/migrations/1589476000887-WebhookModel.ts
+++ packages/cli/src/databases/postgresdb/migrations/1589476000887-WebhookModel.ts
@@ -16,6 +16,8 @@ export class WebhookModel1589476000887 implements MigrationInterface {
 			tablePrefix = schema + '.' + tablePrefix;
 		}
 
+		await queryRunner.query(`SET search_path TO ${schema};`);
+
 		await queryRunner.query(`CREATE TABLE IF NOT EXISTS ${tablePrefix}webhook_entity ("workflowId" integer NOT NULL, "webhookPath" character varying NOT NULL, "method" character varying NOT NULL, "node" character varying NOT NULL, CONSTRAINT "PK_${tablePrefixIndex}b21ace2e13596ccd87dc9bf4ea6" PRIMARY KEY ("webhookPath", "method"))`, undefined);
 	}
 
@@ -25,6 +27,7 @@ export class WebhookModel1589476000887 implements MigrationInterface {
 		if (schema) {
 			tablePrefix = schema + '.' + tablePrefix;
 		}
+		await queryRunner.query(`SET search_path TO ${schema};`);
 		await queryRunner.query(`DROP TABLE ${tablePrefix}webhook_entity`, undefined);
 	}
 
