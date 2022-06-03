c03ab512ec48cdfb2401512c34aa98ddb8c59004
Iván Ovejero <ivov.src@gmail.com>
2022-06-03 12:46:24 +0200

    :shirt: Apply `node-param-description-miscased-url` (#3441)

--- .eslintrc.js
+++ .eslintrc.js
@@ -423,6 +423,7 @@ module.exports = {
 				'n8n-nodes-base/node-param-resource-with-plural-option': 'error',
 				'n8n-nodes-base/node-param-resource-without-no-data-expression': 'error',
 				'n8n-nodes-base/node-param-type-options-missing-from-limit': 'error',
+				'n8n-nodes-base/node-param-description-miscased-url': 'error',
 			},
 		},
 	],
