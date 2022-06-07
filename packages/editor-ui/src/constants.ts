4dde50c719a56b8db5854b65a1cfae20ac405f94
Milorad Filipovic <milorad@n8n.io>
2022-06-07 11:49:43 +0200

    💄 Community nodes installation modal updates: Moved links to constants and used them in translations, disabling inputs in loading state.

--- packages/editor-ui/src/constants.ts
+++ packages/editor-ui/src/constants.ts
@@ -51,6 +51,10 @@ export const N8N_IO_BASE_URL = `https://api.n8n.io/api/`;
 export const NPM_COMMUNITY_NODE_SEARCH_API_URL = `https://api.npms.io/v2/`;
 export const NPM_PACKAGE_DOCS_BASE_URL = `https://www.npmjs.com/package/`;
 export const NPM_KEYWORD_SEARCH_URL = `https://www.npmjs.com/search?q=n8n-community-node-package`;
+export const N8N_QUEUE_MODE_DOCS_URL = `https://docs.n8n.io/hosting/scaling/queue-mode/`;
+export const COMMUNITY_NODES_INSTALLATION_DOCS_URL = `https://docs.n8n.io/integrations/community-nodes/installation/`;
+export const COMMUNITY_NODES_RISKS_DOCS_URL = `https://docs.n8n.io/integrations/community-nodes/risks/`;
+export const COMMUNITY_NODES_BLOCKLIST_DOCS_URL = `https://docs.n8n.io/integrations/community-nodes/blocklist/`;
 
 // node types
 export const BAMBOO_HR_NODE_TYPE = 'n8n-nodes-base.bambooHr';
