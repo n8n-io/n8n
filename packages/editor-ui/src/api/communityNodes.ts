c98facd739caaea56b040168f31db1356da35267
Milorad Filipovic <milorad@n8n.io>
2022-06-08 11:05:57 +0200

    ✨ Implemented community packages install flow

--- packages/editor-ui/src/api/communityNodes.ts
+++ packages/editor-ui/src/api/communityNodes.ts
@@ -1,11 +1,12 @@
+import { IRestApiContext } from '@/Interface';
 import { PublicInstalledPackage } from 'n8n-workflow';
-import { get } from './helpers';
+import { get, post } from './helpers';
 
-const LOCAL_BASE_URL = 'http://localhost:5678';
-
-export async function getInstalledCommunityNodes(): Promise<PublicInstalledPackage[]> {
-	// TODO Use n8n API base URL instead of localhost once it's merged
-	const response = await get(LOCAL_BASE_URL, '/rest/nodes');
-
+export async function getInstalledCommunityNodes(context: IRestApiContext): Promise<PublicInstalledPackage[]> {
+	const response = await get(context.baseUrl, '/nodes');
 	return response.data || [];
+}
+
+export async function installNewPackage(context: IRestApiContext, name: string): Promise<PublicInstalledPackage> {
+	return await post(context.baseUrl, '/nodes', { name });
 }
