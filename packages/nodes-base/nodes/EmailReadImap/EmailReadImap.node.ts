53748aa60b6f558a504897e23544d6ac69424699
Michael Kret <michael.k@radency.com>
2022-06-10 11:11:24 +0300

    :zap: tlsOptions fix

--- packages/nodes-base/nodes/EmailReadImap/EmailReadImap.node.ts
+++ packages/nodes-base/nodes/EmailReadImap/EmailReadImap.node.ts
@@ -433,16 +433,18 @@ export class EmailReadImap implements INodeType {
 				},
 			};
 
+			const tlsOptions: IDataObject = {};
+
 			if (options.allowUnauthorizedCerts === true) {
-				config.imap.tlsOptions = {
-					rejectUnauthorized: false,
-				};
+				tlsOptions.rejectUnauthorized = false;
 			}
 
 			if (credentials.secure) {
-				config.imap.tlsOptions = {
-					servername: credentials.host as string,
-				};
+				tlsOptions.servername = credentials.host as string;
+			}
+
+			if (!_.isEmpty(tlsOptions)) {
+				config.imap.tlsOptions = tlsOptions;
 			}
 
 			// Connect to the IMAP server and open the mailbox
@@ -462,9 +464,8 @@ export class EmailReadImap implements INodeType {
 						}
 					} else {
 						Logger.error('Email Read Imap node encountered a connection error', { error });
+						this.emitError(error);
 					}
-
-					this.emitError(error);
 				});
 				return conn;
 			});
