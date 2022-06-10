5a234214ff59e3e673f7b1c45c37d6d0c0994a2a
Iván Ovejero <ivov.src@gmail.com>
2022-06-10 10:35:59 +0200

    :zap: Post-release refactorings

--- packages/cli/src/PublicApi/v1/handlers/executions/executions.service.ts
+++ packages/cli/src/PublicApi/v1/handlers/executions/executions.service.ts
@@ -1,23 +1,19 @@
 import { parse } from 'flatted';
 import { In, Not, ObjectLiteral, LessThan, IsNull } from 'typeorm';
+
 import { Db, IExecutionFlattedDb, IExecutionResponseApi } from '../../../..';
 import { ExecutionStatus } from '../../../types';
 
 function prepareExecutionData(
 	execution: IExecutionFlattedDb | undefined,
 ): IExecutionResponseApi | undefined {
-	if (execution === undefined) {
-		return undefined;
-	}
+	if (!execution) return undefined;
 
-	if (!execution.data) {
-		return execution;
-	}
+	if (!execution.data) return execution;
 
 	return {
 		...execution,
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		data: parse(execution.data),
+		data: parse(execution.data) as string,
 	};
 }
 
@@ -32,11 +28,12 @@ function getStatusCondition(status: ExecutionStatus): ObjectLiteral {
 		condition.stoppedAt = Not(IsNull());
 		condition.finished = false;
 	}
+
 	return condition;
 }
 
 function getExecutionSelectableProperties(includeData?: boolean): Array<keyof IExecutionFlattedDb> {
-	const returnData: Array<keyof IExecutionFlattedDb> = [
+	const selectFields: Array<keyof IExecutionFlattedDb> = [
 		'id',
 		'mode',
 		'retryOf',
@@ -47,10 +44,10 @@ function getExecutionSelectableProperties(includeData?: boolean): Array<keyof IE
 		'waitTill',
 		'finished',
 	];
-	if (includeData) {
-		returnData.push('data');
-	}
-	return returnData;
+
+	if (includeData) selectFields.push('data');
+
+	return selectFields;
 }
 
 export async function getExecutions(data: {
@@ -92,6 +89,7 @@ export async function getExecutionsCount(data: {
 		},
 		take: data.limit,
 	});
+
 	return executions;
 }
 
@@ -107,6 +105,7 @@ export async function getExecutionInWorkflows(
 			workflowId: In(workflows),
 		},
 	});
+
 	return prepareExecutionData(execution);
 }
 
