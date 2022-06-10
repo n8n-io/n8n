86e33fcf43ad5658bf76a4f974af29383c90840a
Bryce Sheehan <bryce.sheehan@gmail.com>
2022-06-10 12:55:58 +0800

    Reintroduced 'fix' block at :311 results in duplication

--- packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
+++ packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
@@ -320,11 +320,9 @@ export class AwsDynamoDB implements INodeType {
 						const {
 							indexName,
 							projectionExpression,
-							filterExpression,
 						} = this.getNodeParameter('options', i) as {
 							indexName: string;
 							projectionExpression: string;
-							filterExpression: string;
 						};
 
 						const expressionAttributeName = adjustExpressionAttributeName(eanUi);
@@ -345,10 +343,6 @@ export class AwsDynamoDB implements INodeType {
 
 						if (projectionExpression && select !== 'COUNT') {
 							body.ProjectionExpression = projectionExpression;
-						}
-
-						if (filterExpression) {
-							body.FilterExpression = filterExpression;
 						}
 
 						if (select) {
