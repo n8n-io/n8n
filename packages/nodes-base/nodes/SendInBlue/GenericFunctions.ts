0c78feffbf9ef2be43f1a19eb50f2667d048ab86
brianinoa <brian@n8n.io>
2022-06-11 02:56:55 +0200

    :zap: Implement override for body types

---
+++ packages/nodes-base/nodes/SendInBlue/GenericFunctions.ts
@@ -0,0 +1,32 @@
+import {
+	IExecuteSingleFunctions,
+	IHttpRequestOptions,
+	IN8nHttpFullResponse,
+	INodeExecutionData,
+	JsonObject,
+	NodeApiError,
+} from 'n8n-workflow';
+
+export enum OVERRIDE_MAP_TYPE {
+	'CATEGORY' = 'category',
+	'NORMAL' = 'normal',
+	'TRANSACTIONAL' = 'transactional',
+};
+
+enum OVERRIDE_MAP_VALUES {
+	'CATEGORY' = 'category',
+	'NORMAL' = 'boolean',
+	'TRANSACTIONAL' = 'id',
+};
+
+export const INTERCEPTORS = new Map<string, (body: JsonObject) => void>([
+	[OVERRIDE_MAP_TYPE.CATEGORY, (body: JsonObject) => {
+			body!.type = OVERRIDE_MAP_VALUES.CATEGORY;
+	}],
+	[OVERRIDE_MAP_TYPE.NORMAL, (body: JsonObject) => {
+		body!.type = OVERRIDE_MAP_VALUES.NORMAL;
+	}],
+	[OVERRIDE_MAP_TYPE.TRANSACTIONAL, (body: JsonObject) => {
+		body!.type = OVERRIDE_MAP_VALUES.TRANSACTIONAL;
+	}]
+]);
