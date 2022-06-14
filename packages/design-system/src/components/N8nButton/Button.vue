25161df9533d6a886be4fa79a06e8a652ee2e11c
Alex Grozav <alex@grozav.com>
2022-06-14 18:35:55 +0300

    fix: Fixed rebase conflicts.

--- packages/design-system/src/components/N8nButton/Button.vue
+++ packages/design-system/src/components/N8nButton/Button.vue
@@ -65,13 +65,6 @@ export default Vue.extend({
 		},
 		icon: {
 			type: [String, Array],
-<<<<<<< HEAD
-=======
-		},
-		round: {
-			type: Boolean,
-			default: false,
->>>>>>> 4e3546dbc (feat: Extracted code editor into separate form component.)
 		},
 		block: {
 			type: Boolean,
