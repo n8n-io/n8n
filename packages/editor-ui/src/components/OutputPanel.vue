65644adb1ab7806ed2898bf77974441178994ba0
Mutasem <mutdmour@gmail.com>
2022-06-15 14:13:43 +0200

    bring back waiting to execute for non triggers

--- packages/editor-ui/src/components/OutputPanel.vue
+++ packages/editor-ui/src/components/OutputPanel.vue
@@ -32,8 +32,9 @@
 			</div>
 		</template>
 
-		<template v-slot:node-not-run v-if="!workflowRunning">
-			<n8n-text v-if="isScheduleTrigger || !isTriggerNode">{{ $locale.baseText('ndv.output.runNodeHint') }}</n8n-text>
+		<template v-slot:node-not-run>
+			<n8n-text v-if="workflowRunning && !isTriggerNode">{{ $locale.baseText('ndv.output.waitingToRun') }}</n8n-text>
+			<n8n-text v-if="!workflowRunning && (isScheduleTrigger || !isTriggerNode)">{{ $locale.baseText('ndv.output.runNodeHint') }}</n8n-text>
 		</template>
 
 		<template v-slot:no-output-data>
