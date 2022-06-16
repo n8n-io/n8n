701b2ca31a54f63cb75d5092aaf2551446098fee
jon.lowe <jon.lowe@LP010>
2022-06-16 10:56:54 +0100

    Add npm modules required by scripts

---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -d /root/.n8n ] ; then
+  chmod o+rx /root
+  chown -R node /root/.n8n
+  ln -s /root/.n8n /home/node/
+fi
+
+chown -R node /home/node
+
+if [ "$#" -gt 0 ]; then
+  # Got started with arguments
+  exec su-exec node "$@"
+else
+  # Got started without arguments
+  exec su-exec node n8n
+fi
