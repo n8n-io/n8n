364da68791222cf82262f0353c442b6a7bd9d413
Iván Ovejero <ivov.src@gmail.com>
2022-06-17 11:38:48 +0200

    :fire: Remove exceptions for `node-param-placeholder-miscased-id`

--- packages/nodes-base/nodes/QuickBase/RecordDescription.ts
+++ packages/nodes-base/nodes/QuickBase/RecordDescription.ts
@@ -362,7 +362,6 @@ export const recordFields: INodeProperties[] = [
 		},
 		default: '',
 		required: true,
-		// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
 		placeholder: 'id,name,description',
 		description: 'Comma-separated list of the properties which should used as columns for the new rows',
 	},
@@ -488,7 +487,6 @@ export const recordFields: INodeProperties[] = [
 		},
 		default: '',
 		required: true,
-		// eslint-disable-next-line n8n-nodes-base/node-param-placeholder-miscased-id
 		placeholder: 'id,name,description',
 		description: 'Comma-separated list of the properties which should used as columns for the new rows',
 	},
