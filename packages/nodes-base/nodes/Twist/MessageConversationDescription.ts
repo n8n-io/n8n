14ec4c59cd02a312608b6f3eaf28e0930e8c2666
Iván Ovejero <ivan_ovejero@hotmail.com>
2022-06-17 18:07:15 +0200

    Update packages/nodes-base/nodes/Twist/MessageConversationDescription.ts

--- packages/nodes-base/nodes/Twist/MessageConversationDescription.ts
+++ packages/nodes-base/nodes/Twist/MessageConversationDescription.ts
@@ -227,7 +227,7 @@ export const messageConversationFields: INodeProperties[] = [
 				description: 'Name of the property that holds the binary data. Multiple can be defined separated by comma.',
 			},
 			{
-				displayName: 'Direct Mentions Name or ID',
+				displayName: 'Direct Mention Names or IDs',
 				name: 'direct_mentions',
 				type: 'multiOptions',
 				typeOptions: {
