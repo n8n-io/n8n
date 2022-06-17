e1c73de89f8e10c51063dff886bda3963df7ee6f
Marcus <marcus@n8n.io>
2022-06-17 10:50:37 +0200

    GenericFunctions.ts created highLevelApiPagination method

--- packages/nodes-base/nodes/HighLevel/GenericFunctions.ts
+++ packages/nodes-base/nodes/HighLevel/GenericFunctions.ts
@@ -1,3 +1,10 @@
+import {
+	IExecutePaginationFunctions,
+	IRequestOptionsFromParameters,
+	INodeExecutionData,
+	IDataObject
+} from "n8n-workflow";
+
 export function wait() {
 	return new Promise((resolve, reject) => {
 		setTimeout(() => {
@@ -5,3 +12,43 @@ export function wait() {
 		}, 1000);
 	});
 }
+
+export async function highLevelApiPagination(this: IExecutePaginationFunctions, requestData: IRequestOptionsFromParameters): Promise<INodeExecutionData[]> {
+
+	let rootProperty = '';
+	requestData.postReceive.forEach(pR => {
+		for (let i = 0; i < pR.actions.length; i++) {
+			const action: any = pR.actions[i];
+			if (action.type === 'rootProperty') {
+				rootProperty = action.properties.property
+				pR.actions.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	const responseData: INodeExecutionData[] = [];
+	const returnAll = this.getNodeParameter('returnAll');
+	let responseTotal = 0;
+
+	do {
+
+		console.log(requestData.options);
+
+		const pageResponseData: INodeExecutionData[] = await this.makeRoutingRequest(requestData);
+		const items = pageResponseData[0].json[rootProperty] as [];
+		items.forEach(item => responseData.push({ json: item }));
+
+		const meta = pageResponseData[0].json.meta as IDataObject;
+		const startAfterId = meta?.startAfterId as string;
+		const startAfter = meta?.startAfter as number;
+		requestData.options.qs = { startAfterId, startAfter };
+		responseTotal = meta?.total as number || 0;
+
+		// console.log(JSON.stringify(meta, null, 2));
+		// await wait();
+
+	} while (returnAll && responseTotal > responseData.length)
+
+	return responseData;
+};
--- packages/nodes-base/nodes/HighLevel/HighLevel.node.ts
+++ packages/nodes-base/nodes/HighLevel/HighLevel.node.ts
@@ -1,17 +1,13 @@
 import {
-	IDataObject,
-	IExecutePaginationFunctions,
-	INodeExecutionData,
 	INodeProperties,
 	INodeType,
 	INodeTypeDescription,
-	IRequestOptionsFromParameters,
 } from 'n8n-workflow';
 
 import { contactFields, contactOperations } from './description/ContactDescription';
 import { opportunityFields, opportunityOperations } from './description/OpportunityDescription';
 import { pipelineFields, pipelineOperations } from './description/PipelineDescription';
-import { wait } from './GenericFunctions';
+import { highLevelApiPagination, wait } from './GenericFunctions';
 
 
 const ressources: INodeProperties[] = [
@@ -72,45 +68,7 @@ export class HighLevel implements INodeType {
 			},
 		},
 		requestOperations: {
-			async pagination(this: IExecutePaginationFunctions, requestData: IRequestOptionsFromParameters): Promise<INodeExecutionData[]> {
-
-				let rootProperty = '';
-				requestData.postReceive.forEach(pR => {
-					for (let i = 0; i < pR.actions.length; i++) {
-						const action: any = pR.actions[i];
-						if (action.type === 'rootProperty') {
-							rootProperty = action.properties.property
-							pR.actions.splice(i, 1);
-							break;
-						}
-					}
-				});
-
-				const responseData: INodeExecutionData[] = [];
-				const returnAll = this.getNodeParameter('returnAll');
-				let responseTotal = 0;
-
-				do {
-
-					console.log(requestData.options);
-
-					const pageResponseData: INodeExecutionData[] = await this.makeRoutingRequest(requestData);
-					const items = pageResponseData[0].json[rootProperty] as [];
-					items.forEach(item => responseData.push({ json: item }));
-
-					const meta = pageResponseData[0].json.meta as IDataObject;
-					const startAfterId = meta?.startAfterId as string;
-					const startAfter = meta?.startAfter as number;
-					requestData.options.qs = { startAfterId, startAfter };
-					responseTotal = meta?.total as number || 0;
-
-					// console.log(JSON.stringify(meta, null, 2));
-					// await wait();
-
-				} while (returnAll && responseTotal > responseData.length)
-
-				return responseData;
-			},
+			pagination: highLevelApiPagination,
 		},
 		properties: [
 			...ressources,
