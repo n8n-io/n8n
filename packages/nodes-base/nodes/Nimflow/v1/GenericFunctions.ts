0f219ea7e5cbdfbb557a51b5d2798e923843376c
damiang <dgrosso@outlook.com>
2022-06-17 19:51:41 -0500

    Refactor dispatchAction and add node versioning

---
+++ packages/nodes-base/nodes/Nimflow/v1/GenericFunctions.ts
@@ -0,0 +1,11 @@
+import {
+	IDataObject,
+	IExecuteFunctions
+} from 'n8n-workflow'
+
+export function getNodeParameterAsObject(this: IExecuteFunctions, parameterName: string, index: number ): IDataObject {
+	const parameter = this.getNodeParameter(parameterName, index) as IDataObject | string;
+	if( typeof parameter === 'string')
+			return JSON.parse(parameter);
+	return parameter;
+}
