a2e188b87c8ac2c16d6f9c8940ea338ef3bfc398
Iván Ovejero <ivov.src@gmail.com>
2022-06-20 11:43:19 +0200

    :rewind: Undo `InfoTip` changes

--- packages/design-system/src/components/N8nInfoTip/InfoTip.vue
+++ packages/design-system/src/components/N8nInfoTip/InfoTip.vue
@@ -2,7 +2,7 @@
 	<div :class="{[$style[theme]]: true, [$style[type]]: true, [$style.bold]: bold}">
 		<n8n-tooltip :placement="tooltipPlacement" :popper-class="$style.tooltipPopper" :disabled="type !== 'tooltip'">
 			<span :class="$style.iconText">
-				<n8n-icon :icon="getIcon" />
+				<n8n-icon :icon="theme.startsWith('info') ? 'info-circle': 'exclamation-triangle'" />
 				<span v-if="type === 'note'"><slot></slot></span>
 			</span>
 			<span v-if="type === 'tooltip'" slot="content"><slot></slot></span>
@@ -25,7 +25,7 @@ export default {
 			type: String,
 			default: 'info',
 			validator: (value: string): boolean =>
-				['info', 'info-light', 'warning', 'danger', 'secondary'].includes(value),
+				['info', 'info-light', 'warning', 'danger'].includes(value),
 		},
 		type: {
 			type: String,
@@ -41,22 +41,7 @@ export default {
 			type: String,
 			default: 'top',
 		},
-		icon: {
-			type: String,
-			default: '',
-		},
 	},
-	computed: {
-		getIcon(): string {
-			if (this.icon) return this.icon;
-
-			if (['info', 'info-light', 'secondary'].includes(this.theme)) return 'info-circle';
-
-			if (['warning', 'danger'].includes(this.theme)) return 'exclamation-triangle';
-
-			throw new Error(`No icon found for theme: ${this.theme}`)
-		},
-	}
 };
 </script>
 
@@ -108,9 +93,5 @@ export default {
 
 .danger {
 	color: var(--color-danger);
-}
-
-.secondary {
-	color: var(--color-secondary);
 }
 </style>
