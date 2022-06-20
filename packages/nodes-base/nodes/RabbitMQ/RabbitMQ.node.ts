91b44c553d91f060165e0d4f507022fd85e5eb0e
Ben Hesseldieck <1849459+BHesseldieck@users.noreply.github.com>
2022-06-21 00:17:18 +0200

    :arrow_up: Upgrade multiple dependencies (#3547)
    * ⬆️ upgrade typeorm
    
    * ⬆️ upgrade @types/node
    
    * ⬆️ upgrade mssql
    
    * ⬆️ upgrade monaco-editor to 0.30.1
    
    * ⚡️ update package-lock
    
    Co-authored-by: Jan Oberhauser <janober@users.noreply.github.com>
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/nodes-base/nodes/RabbitMQ/RabbitMQ.node.ts
+++ packages/nodes-base/nodes/RabbitMQ/RabbitMQ.node.ts
@@ -333,6 +333,7 @@ export class RabbitMQ implements INodeType {
 				// @ts-ignore
 				const promisesResponses = await Promise.allSettled(queuePromises);
 
+				// @ts-ignore
 				promisesResponses.forEach((response: JsonObject) => {
 					if (response!.status !== 'fulfilled') {
 
@@ -398,6 +399,7 @@ export class RabbitMQ implements INodeType {
 				// @ts-ignore
 				const promisesResponses = await Promise.allSettled(exchangePromises);
 
+				// @ts-ignore
 				promisesResponses.forEach((response: JsonObject) => {
 					if (response!.status !== 'fulfilled') {
 
