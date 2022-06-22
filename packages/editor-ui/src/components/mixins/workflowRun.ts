86c4e4bbe69dc44f9a14cc7cadcd20a7d346407d
Iván Ovejero <ivov.src@gmail.com>
2022-06-22 09:42:34 +0200

    :shirt: Fix build

--- packages/editor-ui/src/components/mixins/workflowRun.ts
+++ packages/editor-ui/src/components/mixins/workflowRun.ts
@@ -153,6 +153,7 @@ export const workflowRun = mixins(
 				const workflowData = await this.getWorkflowDataToSave();
 
 				const pinData = Object.values(workflow.nodes).reduce<PinDataPayload>((acc, node) => {
+					// @ts-ignore
 					if (node.pinData) acc[node.name] = node.pinData;
 
 					return acc;
