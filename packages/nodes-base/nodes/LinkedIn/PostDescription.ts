04730704b7a5a75763e8cd4e968c396ba7f4a974
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2022-06-23 22:15:29 +0100

    fix(LinkedIn Node): Fix LinkedIn image preview (#3528)
    * Fixed issue with Preview not being generated when description and title is blank
    
    * Nodelinter fixes
    
    * Fixed lint issue... build should work
    
    Co-authored-by: Jan Oberhauser <janober@users.noreply.github.com>

--- packages/nodes-base/nodes/LinkedIn/PostDescription.ts
+++ packages/nodes-base/nodes/LinkedIn/PostDescription.ts
@@ -32,7 +32,7 @@ export const postFields: INodeProperties[] = [
 		displayName: 'Post As',
 		name: 'postAs',
 		type: 'options',
-		default: '',
+		default: 'person',
 		description: 'If to post on behalf of a user or an organization',
 		options: [
 			{
