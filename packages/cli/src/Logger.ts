a8f45169bc512edf07082cbb7a548994506b939b
Alex Grozav <alex@grozav.com>
2022-06-23 17:07:56 +0300

    feat: Migrated to npm release of riot-tmpl fork (#3581)
    * feat: Migrated to npm release of riot-tmpl fork.
    
    * fix: Updated imports to use @n8n_io/riot-tmpl
    
    * fix: Fixed Logger.ts types.
    
    Co-authored-by: Jan Oberhauser <janober@users.noreply.github.com>

--- packages/cli/src/Logger.ts
+++ packages/cli/src/Logger.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 import { inspect } from 'util';
 import winston from 'winston';
 
