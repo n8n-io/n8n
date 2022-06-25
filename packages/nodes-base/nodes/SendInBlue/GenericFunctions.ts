1853d636effb83920763840286888c8e2feb9c35
brianinoa <brian@n8n.io>
2022-06-25 17:51:39 -0400

    :shirt: fix lint issue

--- packages/nodes-base/nodes/SendInBlue/GenericFunctions.ts
+++ packages/nodes-base/nodes/SendInBlue/GenericFunctions.ts
@@ -2,13 +2,14 @@ import {
 	IDataObject,
 	IExecuteSingleFunctions,
 	IHttpRequestOptions,
-	JsonObject, NodeOperationError,
+	JsonObject,
+	NodeOperationError,
 } from 'n8n-workflow';
 
 export interface EmailAttachment {
-		content?: string;
-		name?: string;
-		url?: string;
+	content?: string;
+	name?: string;
+	url?: string;
 }
 
 export enum OVERRIDE_MAP_TYPE {
@@ -24,49 +25,78 @@ enum OVERRIDE_MAP_VALUES {
 }
 
 export const INTERCEPTORS = new Map<string, (body: JsonObject) => void>([
-	[OVERRIDE_MAP_TYPE.CATEGORY, (body: JsonObject) => {
+	[
+		OVERRIDE_MAP_TYPE.CATEGORY,
+		(body: JsonObject) => {
 			body!.type = OVERRIDE_MAP_VALUES.CATEGORY;
-	}],
-	[OVERRIDE_MAP_TYPE.NORMAL, (body: JsonObject) => {
-		body!.type = OVERRIDE_MAP_VALUES.NORMAL;
-	}],
-	[OVERRIDE_MAP_TYPE.TRANSACTIONAL, (body: JsonObject) => {
-		body!.type = OVERRIDE_MAP_VALUES.TRANSACTIONAL;
-	}],
+		},
+	],
+	[
+		OVERRIDE_MAP_TYPE.NORMAL,
+		(body: JsonObject) => {
+			body!.type = OVERRIDE_MAP_VALUES.NORMAL;
+		},
+	],
+	[
+		OVERRIDE_MAP_TYPE.TRANSACTIONAL,
+		(body: JsonObject) => {
+			body!.type = OVERRIDE_MAP_VALUES.TRANSACTIONAL;
+		},
+	],
 ]);
 
-function getFileName(itemIndex: number, mimeType: string, fileExt: string, fileName: string): string {
+function getFileName(
+	itemIndex: number,
+	mimeType: string,
+	fileExt: string,
+	fileName: string,
+): string {
 	let ext = fileExt;
-	if(fileExt === '') {
+	if (fileExt === '') {
 		ext = mimeType.split('/')[1];
 	}
 
 	let name = `${fileName}.${ext}`;
-	if(fileName === '') {
+	if (fileName === '') {
 		name = `file-${itemIndex}.${ext}`;
 	}
 	return name;
 }
 
-export async function validateAttachmentsData (this: IExecuteSingleFunctions, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
-	const dataPropertyList = this.getNodeParameter('additionalParameters.emailAttachments.attachment') as JsonObject[];
+export async function validateAttachmentsData(
+	this: IExecuteSingleFunctions,
+	requestOptions: IHttpRequestOptions,
+): Promise<IHttpRequestOptions> {
+	const dataPropertyList = this.getNodeParameter(
+		'additionalParameters.emailAttachments.attachment',
+	) as JsonObject[];
 	const { body } = requestOptions;
 
-	const { attachment = [] } = body as {attachment :Array<{content:string; name: string;}>;};
+	const { attachment = [] } = body as { attachment: Array<{ content: string; name: string }> };
 
 	try {
-		for(const [, attachmentDataName] of dataPropertyList.entries()) {
+		for (const [, attachmentDataName] of dataPropertyList.entries()) {
 			const { binaryPropertyName } = attachmentDataName;
 
 			const item = this.getInputData();
 
 			if (item.binary![binaryPropertyName as string] === undefined) {
-				throw new NodeOperationError(this.getNode(), `No binary data property “${binaryPropertyName}” exists on item!`);
+				throw new NodeOperationError(
+					this.getNode(),
+					`No binary data property “${binaryPropertyName}” exists on item!`,
+				);
 			}
 
-			const bufferFromIncomingData = await this.helpers.getBinaryDataBuffer(binaryPropertyName) as Buffer;
+			const bufferFromIncomingData = (await this.helpers.getBinaryDataBuffer(
+				binaryPropertyName,
+			)) as Buffer;
 
-			const {data:content, mimeType, fileName, fileExt} = await this.helpers.prepareBinaryData(bufferFromIncomingData);
+			const {
+				data: content,
+				mimeType,
+				fileName,
+				fileExt,
+			} = await this.helpers.prepareBinaryData(bufferFromIncomingData);
 
 			const itemIndex = this.getCurrentItemIndex();
 			const name = getFileName(itemIndex, mimeType, fileExt, fileName);
@@ -74,15 +104,10 @@ export async function validateAttachmentsData (this: IExecuteSingleFunctions, re
 			attachment.push({ content, name });
 		}
 
-
-		Object.assign(body as Object, { attachment });
+		Object.assign(body as {}, { attachment });
 
 		return requestOptions;
-	}
-	catch(err) {
-		throw new NodeOperationError(
-			this.getNode(),
-			`${err}`,
-		);
+	} catch (err) {
+		throw new NodeOperationError(this.getNode(), `${err}`);
 	}
 }
