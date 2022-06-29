9824f5e61181d4c8f9fb3c13540b9736afdfa1b8
agobrech <45268029+agobrech@users.noreply.github.com>
2022-06-29 09:04:36 +0200

    feat(Clockify Node): Add more resources and improvements (#3411)
    * ✨ Add more Clockify resources
    
    * Moved headers in credentials file and added testing
    
    * add address to additional fields
    
    * Add pagination to workflow
    
    * 🎨 Rename additional fields to filter
    
    * Remove non-required fields to additional fields
    
    * 🔥 Remove loading of client id
    
    * :zap: Improvements
    
    * :zap: Small change
    
    Co-authored-by: pemontto <pemontto@gmail.com>
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

--- packages/nodes-base/nodes/Clockify/GenericFunctions.ts
+++ packages/nodes-base/nodes/Clockify/GenericFunctions.ts
@@ -9,18 +9,15 @@ import {
 } from 'n8n-core';
 
 import {
-	IDataObject, NodeApiError, NodeOperationError,
+	IDataObject, NodeApiError,
 } from 'n8n-workflow';
 
 export async function clockifyApiRequest(this: ILoadOptionsFunctions | IPollFunctions | IExecuteFunctions, method: string, resource: string, body: any = {}, qs: IDataObject = {}, uri?: string, option: IDataObject = {}): Promise<any> { // tslint:disable-line:no-any
-
-	const credentials = await this.getCredentials('clockifyApi');
 	const BASE_URL = 'https://api.clockify.me/api/v1';
 
 	const options: OptionsWithUri = {
 		headers: {
 			'Content-Type': 'application/json',
-			'X-Api-Key': credentials.apiKey as string,
 		},
 		method,
 		qs,
@@ -31,7 +28,7 @@ export async function clockifyApiRequest(this: ILoadOptionsFunctions | IPollFunc
 	};
 
 	try {
-		return await this.helpers.request!(options);
+		return await this.helpers.requestWithAuthentication.call(this, 'clockifyApi', options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
 	}
