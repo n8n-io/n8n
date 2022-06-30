33ae496efdaa60a1812485225e8db394a03a4996
mohamed khrouf <mohamed@toolzgather.com>
2022-06-30 16:47:22 +0200

    contributing guidelines

--- packages/nodes-base/nodes/Toolzgather/Toolzgather.node.ts
+++ packages/nodes-base/nodes/Toolzgather/Toolzgather.node.ts
@@ -1,190 +1,193 @@
 /* eslint-disable n8n-nodes-base/node-param-operation-without-no-data-expression */
 import {
-    IExecuteFunctions,
+	IExecuteFunctions,
 } from 'n8n-core';
 
 import {
-    IDataObject,
-    INodeExecutionData,
-    INodeType,
-    INodeTypeDescription,
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
 } from 'n8n-workflow';
 
 import {
-    OptionsWithUri,
+	OptionsWithUri,
 } from 'request';
 
 export class Toolzgather implements INodeType {
 
-    description: INodeTypeDescription = {
-        displayName: 'Toolzgather',
-        name: 'toolzgather',
-        icon: 'file:toolzgather.svg',
-        group: ['transform'],
-        version: 1,
-        description: 'Consume Toolzgather API',
-        defaults: {
-            name: 'Toolzgather',
-            color: '#1A82e2',
-        },
-        inputs: ['main'],
-        outputs: ['main'],
-			credentials: [
-				{
-					name: 'toolzgatherApi',
-					required: true,
+	description: INodeTypeDescription = {
+		displayName: 'Toolzgather',
+		name: 'toolzgather',
+		icon: 'file:toolzgather.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume Toolzgather API.',
+		subtitle:'Toolzgather',
+		defaults: {
+			name: 'Toolzgather',
+			color: '#1A82e2',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'toolzgatherApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				noDataExpression:true,
+				displayName: 'Function',
+				name: 'resource',
+				type: 'options',
+				options: [
+					{
+						name:'Invite Users to Board',
+						value: 'autb',
+						description: 'Invites members and invited users to your workspace boards',
+					},
+					{
+						name: 'Remove Users From Board',
+						value: 'rufb',
+						description: 'Removes members and invited users from your workspace boards',
+					},
+					{
+						name: 'Remove Users From Workspace',
+						value: 'rufw',
+						description: 'Removes members and invited users from your workspace',
+					},
+				],
+				default: 'Select function',
+				required: true,
+				description: 'Functions to Use',
+			},
+			{
+				displayName: 'Workspace',
+				name: 'workspaceId.',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'rufw',
+						],
+					},
 				},
-        ],
-        properties: [
-					{
-						displayName: 'Function',
-						name: 'resource',
-						type: 'options',
-						options: [
-							{
-								name: 'invite users to boards',
-								value: 'autb',
-								description: 'invites members and invited users to your workspace boards ',
-							},
-							{
-								name:'remove users from boards',
-								value: 'rufb',
-								description: 'Removes members and invited users from your workspace boards',
-							},
-							{
-
-								name: 'remove users from workspace',
-								value: 'rufw',
-								description: 'Removes members and invited users from your workspace',
-							}
+				description: 'The workspace to delete users from',
+				default: '',
+			},
+			{
+				displayName: 'Boards',
+				name: 'BoardList',
+				type: 'number',
+				default:[],
+				required: true,
+				typeOptions: {
+					multipleValues: true,
+					multipleValueButtonText: 'Add BoardId',
+				},
+				displayOptions: {
+					show: {
+						resource: [
+							'autb',
+							'rufb',
 						],
-						default: 'Select function',
-						required: true,
-						description: 'Resource to consume',
 					},
-					{
-						displayName: 'Workspace',
-						name: 'workspaceId',
-						type: 'string',
-						required: true,
-						displayOptions: {
-							show: {
-								resource: [
-									'rufw',
-								],
-
-							},
-						},
-						description: 'the workspace to delete users from',
-						default: '',
-					},
-					{
-						displayName: 'Boards',
-						name: 'BoardList',
-						type: 'number',
-						default: [],
-						required: true,
-						typeOptions: {
-							multipleValues: true,
-							multipleValueButtonText: 'Add BoardId',
-						},
-						displayOptions: {
-							show: {
-								resource: [
-									'autb',
-									'rufb'
-								],
-
-							},
-						},
-						placeholder: 'BoardId',
-						description: 'BoardId of the selected boards',
-					},
-					{
-						displayName: 'Emails',
-						name: 'EmailList',
-						type: 'string',
-						default: [],
-						required: true,
-						typeOptions: {
-							multipleValues: true,
-							multipleValueButtonText: 'Add Email',
-						},
-						displayOptions: {
-							show: {
-								resource: [
-									'autb',
-									'rufb',
-									'rufw'
-								],
-
-							},
-						},
-						placeholder: 'info@example.com',
-						description: 'The email addresses of the recipients',
-					},
-					{
-						displayName: 'Role',
-						name: 'role',
-						type: 'options',
-						displayOptions: {
-							show: {
-								resource: [
-									'autb'
-								],
-							},
-						},
-						options: [
-							{
-								name: 'Editor',
-								value: 'Editor',
-
-							},
-							{
-								name: 'Guest',
-								value: 'Guest',
-
-							}
+				},
+				placeholder: 'BoardId',
+				description: 'BoardId of the selected boards',
+			},
+			{
+				displayName: 'Emails',
+				name: 'EmailList',
+				type: 'string',
+				default: [],
+				required: true,
+				typeOptions: {
+					multipleValues: true,
+					multipleValueButtonText: 'Add Email',
+				},
+				displayOptions: {
+					show: {
+						resource: [
+							'autb',
+							'rufb',
+							'rufw',
 						],
-						default: 'Guest',
-						description: 'invited users roles',
 					},
+				},
+				placeholder: 'info@example.com',
+				description: 'The email addresses of the recipients',
+			},
+			{
+				displayName: 'Role',
+				name: 'role',
+				type: 'options',
+				displayOptions: {
+					show: {
+						resource: [
+							'autb',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Editor',
+						value: 'Editor',
+					},
+					{
+						name: 'Guest',
+						value: 'Guest',
+
+					},
+				],
+				default: 'Guest',
+				description: 'Invited users roles',
+			},
 
 
-        ],
-    };
+		],
+	};
+
+
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		console.log(this.getInputData()[0].json)
+		let emails = JSON.stringify(this.getInputData()[0].json).match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)?.join('/n').split('/n');
+
+		console.log(emails)
 		let responseData;
 		const resource = this.getNodeParameter('resource', 0) as string;
 		if (resource === 'autb') {
-			const role = this.getNodeParameter('role', 0) as string
+			const role = this.getNodeParameter('role', 0) as string;
 			//Get credentials the user provided for this node
 			const credentials = await this.getCredentials('toolzgatherApi') as IDataObject;
-			const emails = this.getNodeParameter('EmailList', 0) as Array<string>;
-			const boards = this.getNodeParameter('BoardList', 0) as Array<number>
+			const emails = this.getNodeParameter('EmailList', 0) as string[];
+			const boards = this.getNodeParameter('BoardList', 0) as number[];
+       console.log(emails)
+			const options: OptionsWithUri = {
+				headers: {
+					'Accept': 'application/json',
+				},
+				method: 'POST',
+				body: {
+					token: credentials.apiKey,
+					boards: [...boards],
+					emails: [...emails],
+					role,
+				},
+				uri: `http://localhost:3000/api/add.users.boards`,
+				json: true,
+			};
 
-				const options: OptionsWithUri = {
-					headers: {
-						'Accept': 'application/json',
-					},
-					method: 'POST',
-					body: {
-						token:credentials.apiKey,
-						boards: [...boards],
-						emails: [...emails],
-						role:role,
-					},
-					uri: `http://localhost:3000/api/add.users.boards`,
-					json: true,
-				};
-
-				responseData = await this.helpers.request(options);
+			responseData = await this.helpers.request(options);
 
 		} else if (resource === 'rufb') {
 			const credentials = await this.getCredentials('toolzgatherApi') as IDataObject;
-			const emails = this.getNodeParameter('EmailList', 0) as Array<string>;
-			const boards = this.getNodeParameter('BoardList', 0) as Array<number>
+			const emails = this.getNodeParameter('EmailList', 0) as string[];
+			const boards = this.getNodeParameter('BoardList', 0) as number[];
 			const options: OptionsWithUri = {
 				headers: {
 					'Accept': 'application/json',
@@ -201,8 +204,8 @@ export class Toolzgather implements INodeType {
 			};
 			responseData = await this.helpers.request(options);
 		} else if (resource === 'rufw') {
-			const workspaceId = this.getNodeParameter('workspaceId', 0) as number
-			const emails = this.getNodeParameter('EmailList', 0) as Array<string>;
+			const workspaceId = this.getNodeParameter('workspaceId', 0) as number;
+			const emails = this.getNodeParameter('EmailList', 0) as string[];
 			const credentials = await this.getCredentials('toolzgatherApi') as IDataObject;
 			const options: OptionsWithUri = {
 				headers: {
@@ -211,7 +214,7 @@ export class Toolzgather implements INodeType {
 				method: 'POST',
 				body: {
 					token: credentials.apiKey,
-					workspaceId: workspaceId,
+					workspaceId,
 					emails: [...emails],
 
 				},
@@ -222,6 +225,7 @@ export class Toolzgather implements INodeType {
 			responseData = await this.helpers.request(options);
 		}
 		// Map data to n8n data
+		this.continueOnFail()
 		return [this.helpers.returnJsonArray(responseData)];
 	}
 }
