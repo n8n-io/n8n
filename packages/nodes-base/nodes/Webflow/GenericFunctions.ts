55f51cf55328d9eac498546cfcc20beea890a62a
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2022-07-04 11:44:26 +0300

    feat(Webflow Trigger Node): Reduce chance of webhook duplication and add credential test (#3594)
    * upstream merge
    
    * :zap: additional check for webhook, credentials update
    
    * :fire: Remove unnecessary condition
    
    * :zap: Change credential injection to generic type
    
    * :shirt: Fix linting issue
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

--- packages/nodes-base/nodes/Webflow/GenericFunctions.ts
+++ packages/nodes-base/nodes/Webflow/GenericFunctions.ts
@@ -12,7 +12,6 @@ import {
 import {
 	IDataObject,
 	NodeApiError,
-	NodeOperationError,
 } from 'n8n-workflow';
 
 export async function webflowApiRequest(
@@ -24,7 +23,17 @@ export async function webflowApiRequest(
 	uri?: string,
 	option: IDataObject = {},
 ) {
-	const authenticationMethod = this.getNodeParameter('authentication', 0);
+	const authenticationMethod = this.getNodeParameter('authentication', 0, 'accessToken');
+	let credentialsType = '';
+
+	if (authenticationMethod === 'accessToken') {
+		credentialsType = 'webflowApi';
+	}
+
+	if (authenticationMethod === 'oAuth2') {
+		credentialsType = 'webflowOAuth2Api';
+	}
+
 
 	let options: OptionsWithUri = {
 		headers: {
@@ -46,15 +55,7 @@ export async function webflowApiRequest(
 		delete options.body;
 	}
 	try {
-		if (authenticationMethod === 'accessToken') {
-			const credentials = await this.getCredentials('webflowApi');
-
-			options.headers!['authorization'] = `Bearer ${credentials.accessToken}`;
-
-			return await this.helpers.request!(options);
-		} else {
-			return await this.helpers.requestOAuth2!.call(this, 'webflowOAuth2Api', options);
-		}
+		return await this.helpers.requestWithAuthentication.call(this, credentialsType, options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
 	}
