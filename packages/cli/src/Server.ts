cb330999163f0347a6ac7bcc8e5268ac55545b65
Milorad Filipovic <milorad@n8n.io>
2022-07-07 15:31:36 +0200

    ✔️ Fixing linter error in server.ts

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -328,7 +328,7 @@ class App {
 				enabled: config.getEnv('templates.enabled'),
 				host: config.getEnv('templates.host'),
 			},
-			onboardingCallPromptEnabled: config.getEnv('onboardingCallPrompt.enabled');
+			onboardingCallPromptEnabled: config.getEnv('onboardingCallPrompt.enabled'),
 		};
 	}
 
