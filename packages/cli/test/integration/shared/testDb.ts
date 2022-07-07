0f84d6f8c305db5722394b03a7098f873d8e10f8
Iván Ovejero <ivov.src@gmail.com>
2022-07-07 10:37:37 +0200

    :fire: Remove leftover line

--- packages/cli/test/integration/shared/testDb.ts
+++ packages/cli/test/integration/shared/testDb.ts
@@ -222,7 +222,6 @@ export async function truncate(collections: Array<CollectionName>, testDbName: s
 		}
 
 		return await truncateMappingTables(dbType, collections, testDb);
-		// return Promise.resolve([])
 	}
 
 	/**
