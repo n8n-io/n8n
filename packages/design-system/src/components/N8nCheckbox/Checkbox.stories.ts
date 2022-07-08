89c0211a523bec7758537319894cd128a1d3d2bf
Milorad Filipovic <milorad@n8n.io>
2022-07-08 13:20:04 +0200

    ✨ Implementing N8nCheckbox Vue component

---
+++ packages/design-system/src/components/N8nCheckbox/Checkbox.stories.ts
@@ -0,0 +1,36 @@
+import N8nCheckbox from "./Checkbox.vue";
+import { StoryFn } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
+export default {
+	title: 'Atoms/Checkbox',
+	component: N8nCheckbox,
+	argTypes: {
+		checked: {
+			control: null,
+		},
+	},
+}
+
+const methods = {
+	onInput: action('input'),
+	onFocus: action('focus'),
+	onChange: action('change'),
+};
+
+const DefaultTemplate: StoryFn = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: {
+		N8nCheckbox,
+	},
+	template: '<n8n-checkbox v-bind="$props" @change="onChange" @focus="onFocus"></n8n-checkbox>',
+	methods,
+});
+
+export const Default = DefaultTemplate.bind({});
+Default.args = {
+	label: 'This is a default checkbox',
+	tooltipText: 'Checkbox tooltip',
+	checked: true,
+	disabled: false,
+}
---
+++ packages/design-system/src/components/N8nCheckbox/Checkbox.vue
@@ -0,0 +1,62 @@
+<template>
+	<el-checkbox
+		v-model="state"
+		v-bind="$props"
+		v-on="$listeners"
+		:disabled="disabled"
+	>
+		<n8n-input-label
+			:label="label"
+			:tooltipText="tooltipText"
+		></n8n-input-label>
+	</el-checkbox>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ElCheckbox from 'element-ui/lib/checkbox';
+import N8nInputLabel from '../N8nInputLabel';
+
+export default Vue.extend({
+	name: 'n8n-checkbox',
+	components: {
+		ElCheckbox,
+		N8nInputLabel,
+	},
+	props: {
+		checked: {
+			type: Boolean,
+			default: false,
+		},
+		label: {
+			type: String,
+			required: true,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		tooltipText: {
+			type: String,
+			required: false,
+		},
+	},
+	data() {
+		return {
+			state: false,
+		}
+	},
+	mounted() {
+        this.state = this.checked;
+    }
+});
+</script>
+
+<style lang="scss" module>
+.infoText {
+	margin-top: var(--spacing-2xs);
+	font-size: var(--font-size-2xs);
+	font-weight: var(--font-weight-regular);
+	color: var(--color-text-base);
+}
+</style>
