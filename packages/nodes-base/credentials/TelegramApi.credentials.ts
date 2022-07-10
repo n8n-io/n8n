7ed732035330d6b0f37426639a2a158abece4f7d
Taha Sönmez <35905778+tahasonmez@users.noreply.github.com>
2022-07-10 11:07:16 +0300

    fix(Telegram Node): Fix sending binaryData media (photo, document, video etc.) (#3408)
    * fixed send media (photo, document, video etc.) issues on Telegram Node
    
    * fixed send media (photo, document, video etc.) issues on Telegram Node
    
    * file name is optional now
    
    * :zap: lock file and linter fix
    
    * :zap: improvements
    
    * :zap: fixes
    
    * :zap: Improvements
    
    * :zap: Add placeholder to File Name
    
    * :zap: Add error message
    
    * :fire: Remove requestWithAuthentication
    
    * :zap: Fix typo
    
    * :shirt: Fix linting issues
    
    Co-authored-by: Michael Kret <michael.k@radency.com>
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

--- packages/nodes-base/credentials/TelegramApi.credentials.ts
+++ packages/nodes-base/credentials/TelegramApi.credentials.ts
@@ -1,4 +1,5 @@
 import {
+	ICredentialTestRequest,
 	ICredentialType,
 	INodeProperties,
 } from 'n8n-workflow';
@@ -17,4 +18,11 @@ export class TelegramApi implements ICredentialType {
 			description: 'Chat with the <a href="https://telegram.me/botfather">bot father</a> to obtain the access token',
 		},
 	];
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: `=https://api.telegram.org/bot{{$credentials?.accessToken}}`,
+			url: '/getMe',
+			method: 'GET',
+		},
+	};
 }
