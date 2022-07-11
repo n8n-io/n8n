3c863aeceb13b22a0cfc1ff43d9962e9bd3a2640
Alex Grozav <alex@grozav.com>
2022-07-11 18:24:29 +0300

    fix: Loading pin data when opening execution.

--- packages/editor-ui/src/components/RunData.vue
+++ packages/editor-ui/src/components/RunData.vue
@@ -7,7 +7,7 @@
 			:class="$style['pinned-data-callout']"
 		>
 			{{ $locale.baseText('runData.pindata.thisDataIsPinned') }}
-			<span class="ml-4xs">
+			<span class="ml-4xs" v-if="!isReadOnly">
 				<n8n-link
 					theme="secondary"
 					size="small"
@@ -45,7 +45,7 @@
 					@input="onDisplayModeChange"
 				/>
 				<n8n-icon-button
-					v-if="canPinData"
+					v-if="canPinData && !isReadOnly"
 					v-show="!editMode.enabled"
 					:title="$locale.baseText('runData.editOutput')"
 					:circle="false"
@@ -82,7 +82,7 @@
 						type="tertiary"
 						active
 						icon="thumbtack"
-						:disabled="editMode.enabled || (inputData.length === 0 && !hasPinData)"
+						:disabled="editMode.enabled || (inputData.length === 0 && !hasPinData) || isReadOnly"
 						@click="onTogglePinData({ source: 'pin-icon-click' })"
 					/>
 				</n8n-tooltip>
@@ -520,7 +520,6 @@ export default mixins(
 			canPinData (): boolean {
 				return this.paneType === 'output' &&
 					this.isPinDataNodeType &&
-					!this.isReadOnly &&
 					!(this.binaryData && this.binaryData.length > 0);
 			},
 			buttons(): Array<{label: string, value: string}> {
--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -539,12 +539,14 @@ export default mixins(
 				this.$store.commit('setWorkflowId', PLACEHOLDER_EMPTY_WORKFLOW_ID);
 
 				this.$store.commit('setWorkflowExecutionData', data);
+				this.$store.commit('setWorkflowPinData', data.workflowData.pinData);
 
 				await this.addNodes(JSON.parse(JSON.stringify(data.workflowData.nodes)), JSON.parse(JSON.stringify(data.workflowData.connections)));
 				this.$nextTick(() => {
 					this.zoomToFit();
 					this.$store.commit('setStateDirty', false);
 				});
+
 
 				this.$externalHooks().run('execution.open', { workflowId: data.workflowData.id, workflowName: data.workflowData.name, executionId });
 				this.$telemetry.track('User opened read-only execution', { workflow_id: data.workflowData.id, execution_mode: data.mode, execution_finished: data.finished });
