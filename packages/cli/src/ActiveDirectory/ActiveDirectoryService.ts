3fd4742bd058f68fc754284fc5cbe0f343f05033
ricardo <ricardoespinoza105@gmail.com>
2022-07-13 15:47:47 -0400

    aja

--- packages/cli/src/ActiveDirectory/ActiveDirectoryService.ts
+++ packages/cli/src/ActiveDirectory/ActiveDirectoryService.ts
@@ -27,8 +27,6 @@ export class ActiveDirectoryService {
 	private async bindAdmin(): Promise<void> {
 		await this.getClient();
 		if (this.client) {
-			console.log(this._config.binding.adminDn);
-			console.log(this._config.binding.adminPassword);
 			await this.client.bind(this._config.binding.adminDn, this._config.binding.adminPassword);
 		}
 	}
@@ -36,11 +34,9 @@ export class ActiveDirectoryService {
 	async searchWithAdminBinding(filter: string): Promise<Entry[]> {
 		await this.bindAdmin();
 		if (this.client) {
-			console.log(this._config.binding.baseDn);
 			const { searchEntries } = await this.client.search(this._config.binding.baseDn, {
 				filter,
 			});
-			console.log('aqui papa');
 			await this.client.unbind();
 			return searchEntries;
 		}
