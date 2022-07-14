59cdd5f16e391b1e8e76e542a7064d7d4ba04662
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2022-07-14 17:21:15 +0100

    Started implementing node version

---
+++ packages/nodes-base/nodes/Google/Sheet/v2/methods/loadOptions.ts
@@ -0,0 +1,35 @@
+import {
+	IDataObject,
+	ILoadOptionsFunctions,
+	INodePropertyOptions,
+	NodeOperationError,
+} from 'n8n-workflow';
+
+import {
+	GoogleSheet
+} from '../helper'
+
+export async function getSheets(this: ILoadOptionsFunctions): Promise<INodePropertyOptions[]> {
+	const spreadsheetId = this.getCurrentNodeParameter('sheetId') as string;
+
+	const sheet = new GoogleSheet(spreadsheetId, this);
+	const responseData = await sheet.spreadsheetGetSheets();
+
+	if (responseData === undefined) {
+		throw new NodeOperationError(this.getNode(), 'No data got returned');
+	}
+
+	const returnData: INodePropertyOptions[] = [];
+	for (const sheet of responseData.sheets!) {
+		if (sheet.properties!.sheetType !== 'GRID') {
+			continue;
+		}
+
+		returnData.push({
+			name: sheet.properties!.title as string,
+			value: sheet.properties!.sheetId as unknown as string,
+		});
+	}
+
+	return returnData;
+}
