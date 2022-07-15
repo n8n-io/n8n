c2720ce368541cbc5b2a431a1f8ee81f0f50c0ab
Iván Ovejero <ivov.src@gmail.com>
2022-07-15 13:18:26 +0200

    :shirt: Add lint exceptions

--- packages/workflow/src/NodeHelpers.ts
+++ packages/workflow/src/NodeHelpers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
