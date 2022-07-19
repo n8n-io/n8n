11525ca63bfe4ebc83198a4dd396a51a50411a21
Ricardo Espinoza <ricardo@n8n.io>
2022-07-19 04:09:06 -0400

    feat: Add support for preAuthentication and add Metabase credentials (#3399)
    * :zap: Add preAuthentication method to credentials
    
    * Improvements
    
    * :zap: Improvements
    
    * :zap: Add feedback
    
    * :fire: Remove comments
    
    * :zap: Add generic type to autheticate method
    
    * :zap: Fix typo
    
    * :zap: Remove console.log and fix indentation
    
    * :zap: Minor improvements
    
    * :zap: Expire credentials in every credential test run
    
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

---
+++ packages/nodes-base/credentials/MetabaseApi.credentials.ts
@@ -0,0 +1,76 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class MetabaseApi implements ICredentialType {
+	name = 'metabaseApi';
+	displayName = 'Metabase API';
+	documentationUrl = 'metabase';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Session Token',
+			name: 'sessionToken',
+			type: 'hidden',
+			typeOptions: {
+				expirable: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'URL',
+			name: 'url',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+	];
+
+	// method will only be called if "sessionToken" (the expirable property)
+	// is empty or is expired
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		// make reques to get session token
+		const url = credentials.url as string;
+		const { id } = (await this.helpers.httpRequest({
+			method: 'POST',
+			url: `${url.endsWith('/') ? url.slice(0, -1) : url}/api/session`,
+			body: {
+				username: credentials.username,
+				password: credentials.password,
+			},
+		})) as { id: string };
+		return { sessionToken: id };
+	}
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'X-Metabase-Session': '={{$credentials.sessionToken}}',
+			},
+		},
+	};
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.url}}',
+			url: '/api/user/current',
+		},
+	};
+}
