94bba72d2c1af5c2334a84baf2c349851e45cdf3
agobrech <ael.gobrecht@gmail.com>
2022-07-19 15:43:15 +0200

    Add cred injection, cred testing

--- packages/nodes-base/nodes/Phantombuster/GenericFunctions.ts
+++ packages/nodes-base/nodes/Phantombuster/GenericFunctions.ts
@@ -14,11 +14,8 @@ import {
 
 export async function phantombusterApiRequest(this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: string, path: string, body: any = {}, qs: IDataObject = {}, option = {}): Promise<any> { // tslint:disable-line:no-any
 
-	const credentials = await this.getCredentials('phantombusterApi');
-
 	const options: OptionsWithUri = {
 		headers: {
-			'X-Phantombuster-Key': credentials.apiKey,
 		},
 		method,
 		body,
@@ -31,7 +28,7 @@ export async function phantombusterApiRequest(this: IExecuteFunctions | IExecute
 			delete options.body;
 		}
 		//@ts-ignore
-		return await this.helpers.request.call(this, options);
+		return await this.helpers.requestWithAuthentication.call(this, 'phantombusterApi',options);
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error);
 	}
