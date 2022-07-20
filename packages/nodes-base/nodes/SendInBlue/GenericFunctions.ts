fbfb8eb57af9d16b0eb79779165e000af13470e1
brianinoa <brian@n8n.io>
2022-07-20 16:58:45 +0200

    :recycle: Add Webhook API functionality

--- packages/nodes-base/nodes/SendInBlue/GenericFunctions.ts
+++ packages/nodes-base/nodes/SendInBlue/GenericFunctions.ts
@@ -140,4 +140,85 @@ export namespace sendInBlueWebhookApi {
 
 	const credentialsName = 'sendinblueApi';
 	const baseURL = 'https://api.sendinblue.com/v3';
+	export const supportedAuthMap = new Map<string, (ref: IWebhookFunctions) => Promise<string>>([
+		[
+			'apiKey',
+			async (ref: IWebhookFunctions): Promise<string> => {
+				const credentials = await ref.getCredentials(credentialsName);
+				return credentials.sharedSecret as string;
+			},
+		],
+	]);
+
+	export const fetchWebhooks = async (ref: IHookFunctions, type: string): Promise<Webhooks> => {
+		const { apiKey } = await ref.getCredentials(credentialsName);
+		const endpoint = `${baseURL}/webhooks?type=${type}`;
+
+		const options: OptionsWithUri = {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Api-Key': apiKey,
+			},
+			uri: endpoint,
+		};
+
+		const webhooks = (await ref.helpers.requestWithAuthentication.call(
+			ref,
+			credentialsName,
+			options,
+		)) as string;
+
+		return JSON.parse(webhooks) as Webhooks;
+	};
+
+	export const createWebHook = async (
+		ref: IHookFunctions,
+		type: string,
+		events: string[],
+		url: string,
+	): Promise<WebhookId> => {
+		const { apiKey } = await ref.getCredentials(credentialsName);
+		const endpoint = `${baseURL}/webhooks`;
+
+		const options: OptionsWithUri = {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Api-Key': apiKey,
+			},
+			uri: endpoint,
+			body: {
+				events,
+				type,
+				url,
+			},
+		};
+
+		const webhookId = (await ref.helpers.requestWithAuthentication.call(
+			ref,
+			credentialsName,
+			options,
+		)) as WebhookId;
+
+		return webhookId;
+	};
+
+	export const deleteWebhook = async (ref: IHookFunctions, webhookId: string) => {
+		const endpoint = `${baseURL}/webhooks/${webhookId}`;
+		const { apiKey } = await ref.getCredentials(credentialsName);
+		const body = {};
+
+		const options: OptionsWithUri = {
+			method: 'DELETE',
+			headers: {
+				Accept: 'application/json',
+				'Api-Key': apiKey,
+			},
+			uri: endpoint,
+			body,
+		};
+
+		return await ref.helpers.requestWithAuthentication.call(ref, credentialsName, options);
+	};
 }
