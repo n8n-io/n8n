7d24f7fd09cc51fe6b425ff0465ef44cd419de2a
ricardo <ricardoespinoza105@gmail.com>
2022-07-21 11:48:55 -0400

    :shirt: Fix linting issue

--- packages/nodes-base/nodes/NocoDB/OperationDescription.ts
+++ packages/nodes-base/nodes/NocoDB/OperationDescription.ts
@@ -34,7 +34,7 @@ export const operationFields: INodeProperties[] = [
 			},
 		},
 		required: true,
-		description: 'The project to operate on. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/nodes/expressions.html#expressions">expression</a>.',
+		description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/nodes/expressions.html#expressions">expression</a>. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
 		typeOptions: {
 			loadOptionsMethod: 'getProjects',
 		},
@@ -52,7 +52,7 @@ export const operationFields: INodeProperties[] = [
 			},
 		},
 		required: true,
-		description: 'The table to operate on. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/nodes/expressions.html#expressions">expression</a>.',
+		description: 'The table to operate on. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/nodes/expressions.html#expressions">expression</a>. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
 		typeOptions: {
 			loadOptionsDependsOn: [
 				'projectId',
