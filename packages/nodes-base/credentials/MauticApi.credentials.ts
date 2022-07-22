863df0b5904aa95eb580249c6be09cc4e167bafb
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2022-07-22 17:52:34 +0100

    Fixes mautic credential issue

--- packages/nodes-base/credentials/MauticApi.credentials.ts
+++ packages/nodes-base/credentials/MauticApi.credentials.ts
@@ -1,4 +1,5 @@
 import {
+	IAuthenticateGeneric,
 	ICredentialDataDecryptedObject,
 	 ICredentialTestRequest,
 	 ICredentialType,
@@ -34,23 +35,16 @@ export class MauticApi implements ICredentialType {
 			default: '',
 		},
 	];
-	async authenticate (credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions> {
-		const {
-			url,
-			username,
-			password,
-		} = credentials as {
-			url: string,
-			username: string,
-			password: string,
-		};
-		const credentialUrl = url.endsWith('/') ? `${url}api/users/self` : `${url}/api/users/self`;
-		const base64Key = Buffer.from(`${username}:${password}`).toString('base64');
-		requestOptions.headers!['Authorization'] = `Basic ${base64Key}`;
-		requestOptions.url = credentialUrl ? credentialUrl : requestOptions.url;
 
-		return requestOptions;
-	}
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			auth: {
+				username: '={{$credentials.username}}',
+				password: '={{$credentials.password}}',
+			},
+		},
+	};
 
 	test: ICredentialTestRequest = {
 		request: {
