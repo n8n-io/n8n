a799360633c7e588fff32b45355f83fed3a73b97
Iván Ovejero <ivov.src@gmail.com>
2022-07-25 18:32:28 +0200

    :fire: Remove constant

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -29,6 +29,3 @@ export const NPM_COMMAND_TOKENS = {
 };
 
 export const NPM_PACKAGE_STATUS_GOOD = 'OK';
-
-// @TODO_PART_4: Replace with n8n account's key
-export const POSTHOG_API_KEY = 'phc_QLECpA3yiIJcyafUv0pe6EoxvdfIDZ5A12dGpGXH4jV';
