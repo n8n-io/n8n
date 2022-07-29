d874a7d3591cb48abe21bade4a73b193718184d1
Mutasem <mutdmour@gmail.com>
2022-07-29 11:21:24 +0200

    update menu

--- packages/design-system/src/components/N8nMenu/Menu.vue
+++ packages/design-system/src/components/N8nMenu/Menu.vue
@@ -1,14 +1,13 @@
-<template functional>
-	<component
-		:is="$options.components.ElMenu"
-		:defaultActive="props.defaultActive"
-		:collapse="props.collapse"
-		:router="props.router"
-		:class="$style[props.type + (props.light ? '-light' : '')]"
-		@select="(e) => listeners.select && listeners.select(e)"
+<template>
+	<el-menu
+		:defaultActive="defaultActive"
+		:collapse="collapse"
+		:router="router"
+		:class="$style[type + (light ? '-light' : '')]"
+		v-on="$listeners"
 	>
 		<slot></slot>
-	</component>
+	</el-menu>
 </template>
 
 <script lang="ts">
