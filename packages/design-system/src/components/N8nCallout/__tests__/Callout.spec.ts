d132f15f56107471a258fdb9487d74d01557d55e
Milorad Filipovic <milorad@n8n.io>
2022-07-29 13:01:17 +0200

    ✔️ Updating test snapshots and fixing lint warnings

--- packages/design-system/src/components/N8nCallout/__tests__/Callout.spec.ts
+++ packages/design-system/src/components/N8nCallout/__tests__/Callout.spec.ts
@@ -11,7 +11,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is an info callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -23,7 +23,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is a success callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -35,7 +35,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is a warning callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -47,7 +47,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is a danger callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -59,7 +59,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is a secondary callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -72,7 +72,7 @@ describe('components', () => {
 				stubs: [ 'n8n-icon', 'n8n-text' ],
 				slots: {
 					default: '<n8n-text size="small">This is a secondary callout.</n8n-text>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
@@ -87,7 +87,7 @@ describe('components', () => {
 					default: '<n8n-text size="small">This is a secondary callout.</n8n-text>',
 					actions: '<n8n-link size="small">Do something!</n8n-link>',
 					trailingContent: '<n8n-link theme="secondary" size="small" :bold="true" :underline="true" to="https://n8n.io">Learn more</n8n-link>',
-				}
+				},
 			});
 			expect(wrapper.html()).toMatchSnapshot();
 		});
