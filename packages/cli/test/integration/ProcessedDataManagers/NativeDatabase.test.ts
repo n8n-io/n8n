82a84a42d2038ec4391fe27c5e18bb1eb7c2c246
Jan Oberhauser <jan.oberhauser@gmail.com>
2022-07-30 14:56:04 +0200

    :heavy_check_mark: Add tests that different contexts are independent of each other

--- packages/cli/test/integration/ProcessedDataManagers/NativeDatabase.test.ts
+++ packages/cli/test/integration/ProcessedDataManagers/NativeDatabase.test.ts
@@ -62,9 +62,7 @@ test('ProcessedData: NativeDatabase should record and check data correctly', asy
 	};
 
 	let processedData: ICheckProcessedOutput;
-	// let response = await ProcessedDataManager.getInstance().checkProcessed(
 
-	// processedData = await ProcessedDataManager.getInstance().checkProcessedAndRecord(
 	processedData = await ProcessedDataManager.getInstance().checkProcessed(
 		['a', 'b'],
 		context,
@@ -110,5 +108,57 @@ test('ProcessedData: NativeDatabase should record and check data correctly', asy
 	);
 
 	// 'b' & 'd' got removed from the database so they should be new, 'a' & 'b' should still be known
+	expect(processedData).toEqual({ new: ['b', 'd'], processed: ['a', 'c'] });
+});
+
+test('ProcessedData: NativeDatabase different contexts should not interfere with each other', async () => {
+	const contextData: ICheckProcessedContextData = {
+		workflow,
+		node,
+	};
+
+	let processedData: ICheckProcessedOutput;
+
+	// Add data with context "node"
+	processedData = await ProcessedDataManager.getInstance().checkProcessedAndRecord(
+		['a', 'b'],
+		'node',
+		contextData,
+	);
+
+	// No data exists yet for context "node" so has to be new
+	expect(processedData).toEqual({ new: ['a', 'b'], processed: [] });
+
+	// Add data with context "workflow"
+	processedData = await ProcessedDataManager.getInstance().checkProcessedAndRecord(
+		['a', 'b', 'c'],
+		'workflow',
+		contextData,
+	);
+
+	// No data exists yet for context 'worklow' so has to be new
+	expect(processedData).toEqual({ new: ['a', 'b', 'c'], processed: [] });
+
+	await ProcessedDataManager.getInstance().removeProcessed(['a'], 'node', contextData);
+
+	processedData = await ProcessedDataManager.getInstance().checkProcessed(
+		['a', 'b', 'c'],
+		'node',
+		contextData,
+	);
+
+	// 'a' got removed for the context 'node' and 'c' never got saved, so only 'b' should be known
+	expect(processedData).toEqual({ new: ['a', 'c'], processed: ['b'] });
+
+	await ProcessedDataManager.getInstance().removeProcessed(['b'], 'workflow', contextData);
+
+	processedData = await ProcessedDataManager.getInstance().checkProcessed(
+		['a', 'b', 'c', 'd'],
+		'workflow',
+		contextData,
+	);
+
+	// 'b' got removed for the context 'workflow' and 'd' never got saved for that reason new
+	// 'a' and 'c' should should be known
 	expect(processedData).toEqual({ new: ['b', 'd'], processed: ['a', 'c'] });
 });
