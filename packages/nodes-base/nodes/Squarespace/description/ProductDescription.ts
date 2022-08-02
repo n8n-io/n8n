d85fae8a63dd99bf90c7663bb786ab5312163e98
Marcus <marcus@n8n.io>
2022-08-02 17:45:45 +0200

    create product operation required fields

--- packages/nodes-base/nodes/Squarespace/description/ProductDescription.ts
+++ packages/nodes-base/nodes/Squarespace/description/ProductDescription.ts
@@ -172,6 +172,22 @@ const createOperations: Array<INodeProperties> = [
 		default: '',
 	},
 	{
+		displayName: 'Name',
+		name: 'name',
+		type: 'string',
+		default: '',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['product'],
+				operation: ['create']
+			},
+		},
+		routing: {
+			send: { type: 'body', property: 'name' }
+		},
+	},
+	{
 		displayName: 'Variants',
 		name: 'variants',
 		placeholder: 'Add Variant',
@@ -201,11 +217,225 @@ const createOperations: Array<INodeProperties> = [
 						type: 'string',
 						default: '',
 						description: 'Merchant-defined code that identifies the product variant',
+						required: true,
 						routing: {
 							send: { type: 'body', property: '=variants[{{$index}}].sku' }
 						},
 					},
+					{
+						displayName: 'Base Price',
+						name: 'basePrice',
+						placeholder: 'Add Base Price',
+						type: 'fixedCollection',
+						default: null,
+						required: true,
+						typeOptions: {
+							multipleValues: false,
+						},
+						description: 'Amount per unit to charge for the variant',
+						options: [
+							{
+								name: 'basePriceValues',
+								displayName: 'Base Price',
+								values: [
+									{
+										displayName: 'Currency',
+										name: 'currency',
+										type: 'string',
+										default: '',
+										description: 'ISO 4217 currency code string like "USD" or "EUR"',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].pricing.basePrice.currency' }
+										},
+									},
+									{
+										displayName: 'Value',
+										name: 'value',
+										type: 'string',
+										default: '',
+										description: 'Amount has no currency markers and conforms to the selected. ISO currency\'s precision (e.g., JPY as 123; USD as 123.00 or 123). Amount must not be more than 1,000,000.',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].pricing.basePrice.value' }
+										},
+									},
+								],
+							},
+						],
+					},
+					{
+						displayName: 'On Sale',
+						name: 'onSale',
+						type: 'boolean',
+						default: false,
+						description: 'Whether the variant is sold according to its sale price',
+						routing: {
+							send: { type: 'body', property: '=variants[{{$index}}].pricing.onSale' }
+						},
+					},
+					{
+						displayName: 'Sale Price',
+						name: 'salePrice',
+						placeholder: 'Add Sale Price',
+						type: 'fixedCollection',
+						default: {},
+						required: true,
+						typeOptions: {
+							multipleValues: false,
+						},
+						description: 'Amount per unit charged when the variant is on sale',
+						options: [
+							{
+								name: 'salePriceValues',
+								displayName: 'Sale Price',
+								values: [
+									{
+										displayName: 'Currency',
+										name: 'currency',
+										type: 'string',
+										default: '',
+										description: 'ISO 4217 currency code string like "USD" or "EUR"',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].pricing.salePrice.currency' }
+										},
+									},
+									{
+										displayName: 'Value',
+										name: 'value',
+										type: 'string',
+										default: '',
+										description: 'Amount has no currency markers and conforms to the selected. ISO currency\'s precision (e.g., JPY as 123; USD as 123.00 or 123). Amount must not be more than 1,000,000.',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].pricing.salePrice.value' }
+										},
+									},
+								],
+							},
+						],
+					},
+					{
+						displayName: 'Stock',
+						name: 'stock',
+						placeholder: 'Add Stock',
+						type: 'fixedCollection',
+						default: {},
+						typeOptions: {
+							multipleValues: false,
+						},
+						description: 'If stock isn\'t provided, then the new variant uses the default values',
+						options: [
+							{
+								name: 'stockValues',
+								displayName: 'Stock',
+								values: [
+									{
+										displayName: 'Quantity',
+										name: 'quantity',
+										type: 'number',
+										default: '',
+										description: 'Number of units that can be purchased',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].stock.quantity' }
+										},
+									},
+									{
+										displayName: 'Unlimited',
+										name: 'unlimited',
+										type: 'boolean',
+										default: false,
+										description: 'Whether the quantity is unlimited or not',
+										required: true,
+										routing: {
+											send: { type: 'body', property: '=variants[{{$index}}].stock.unlimited' }
+										},
+									},
+								],
+							},
+						],
+					},
 				],
+			},
+		],
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['product'],
+				operation: ['create']
+			},
+		},
+		options: [
+			{
+				displayName: 'Description',
+				name: 'description',
+				type: 'string',
+				default: '',
+				description: 'Long-form product description represented in HTML',
+				routing: {
+					send: { type: 'body', property: 'description' }
+				},
+			},
+			{
+				displayName: 'Is Visible',
+				name: 'isVisible',
+				type: 'boolean',
+				default: false,
+				description: 'Whether the product is available for purchase',
+				routing: {
+					send: { type: 'body', property: 'isVisible' }
+				},
+			},
+			{
+				displayName: 'Tags',
+				name: 'tags',
+				type: 'string',
+				typeOptions: {
+					multipleValues: true,
+					multipleValueButtonText: 'Add Tag',
+				},
+				default: [],
+				routing: {
+					send: {
+						type: 'body',
+						property: 'tags',
+					}
+				},
+			},
+			{
+				displayName: 'URL Slug',
+				name: 'urlSlug',
+				type: 'string',
+				default: '',
+				description: 'Value can only consist of alphanumeric characters and non-contiguous hyphens',
+				routing: {
+					send: { type: 'body', property: 'urlSlug' }
+				},
+			},
+			{
+				displayName: 'Variant Attributes',
+				name: 'variantAttributes',
+				type: 'string',
+				typeOptions: {
+					multipleValues: true,
+					multipleValueButtonText: 'Add Variant Attribute',
+				},
+				description: 'List of attributes to distinguish variants of the product',
+				default: [],
+				routing: {
+					send: {
+						type: 'body',
+						property: 'variantAttributes',
+					}
+				},
 			},
 		],
 	},
