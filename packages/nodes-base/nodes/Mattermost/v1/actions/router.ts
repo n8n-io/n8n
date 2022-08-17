0488dc5b5bf6cbc37b784b05f25678463ceba418
Brian Inoa <briandaniloinoa@gmail.com>
2022-08-18 01:13:57 +0200

    :recycle: Mattermost node paired-item support

--- packages/nodes-base/nodes/Mattermost/v1/actions/router.ts
+++ packages/nodes-base/nodes/Mattermost/v1/actions/router.ts
@@ -3,6 +3,7 @@ import {
 } from 'n8n-core';
 
 import {
+	IDataObject,
 	INodeExecutionData,
 } from 'n8n-workflow';
 
@@ -15,6 +16,7 @@ import { Mattermost } from './Interfaces';
 export async function router(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 	const items = this.getInputData();
 	const operationResult: INodeExecutionData[] = [];
+	let responseData: IDataObject | IDataObject[] = [];
 
 	for (let i = 0; i < items.length; i++) {
 		const resource = this.getNodeParameter<Mattermost>('resource', i);
@@ -32,14 +34,20 @@ export async function router(this: IExecuteFunctions): Promise<INodeExecutionDat
 
 		try {
 			if (mattermost.resource === 'channel') {
-				operationResult.push(...await channel[mattermost.operation].execute.call(this, i));
+				responseData = await channel[mattermost.operation].execute.call(this, i);
 			} else if (mattermost.resource === 'message') {
-				operationResult.push(...await message[mattermost.operation].execute.call(this, i));
+				responseData = await message[mattermost.operation].execute.call(this, i);
 			} else if (mattermost.resource === 'reaction') {
-				operationResult.push(...await reaction[mattermost.operation].execute.call(this, i));
+				responseData = await reaction[mattermost.operation].execute.call(this, i);
 			} else if (mattermost.resource === 'user') {
-				operationResult.push(...await user[mattermost.operation].execute.call(this, i));
+				responseData = await user[mattermost.operation].execute.call(this, i);
 			}
+
+			const executionData = this.helpers.constructExecutionMetaData(
+				{item: i},
+				this.helpers.returnJsonArray(responseData)
+			);
+			operationResult.push(...executionData);
 		} catch (err) {
 			if (this.continueOnFail()) {
 				operationResult.push({json: this.getInputData(i)[0].json, error: err});
