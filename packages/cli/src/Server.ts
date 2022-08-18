b924746d73d1f5f5d77bdbb1a7cc968787c473d9
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-08-18 16:58:46 +0200

    pass config as an additional param to external hooks

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -2668,7 +2668,7 @@ export async function start(): Promise<void> {
 			console.log(`Locale: ${defaultLocale}`);
 		}
 
-		await app.externalHooks.run('n8n.ready', [app]);
+		await app.externalHooks.run('n8n.ready', [app, config]);
 		const cpus = os.cpus();
 		const binarDataConfig = config.getEnv('binaryDataManager');
 		const diagnosticInfo: IDiagnosticInfo = {
