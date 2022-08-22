e36b82bfd5603c8c16472533ca78f1ba3fc022cc
agobrech <ael.gobrecht@gmail.com>
2022-08-22 19:03:18 +0200

    ♻️ MicrosoftSql node paired-item support

--- packages/nodes-base/nodes/Microsoft/Sql/MicrosoftSql.node.ts
+++ packages/nodes-base/nodes/Microsoft/Sql/MicrosoftSql.node.ts
@@ -234,7 +234,9 @@ export class MicrosoftSql implements INodeType {
 		const pool = new mssql.ConnectionPool(config);
 		await pool.connect();
 
-		let returnItems = [];
+		const returnItems: INodeExecutionData[] = [];
+		let responseData: IDataObject | IDataObject[] = [];
+
 
 		const items = this.getInputData();
 		const operation = this.getNodeParameter('operation', 0) as string;
@@ -254,7 +256,7 @@ export class MicrosoftSql implements INodeType {
 						? flatten(queryResult.recordsets)
 						: queryResult.recordsets[0];
 
-				returnItems = this.helpers.returnJsonArray(result as IDataObject[]);
+				responseData = result;
 			} else if (operation === 'insert') {
 				// ----------------------------------
 				//         insert
@@ -281,7 +283,7 @@ export class MicrosoftSql implements INodeType {
 					},
 				);
 
-				returnItems = items;
+				responseData = items;
 			} else if (operation === 'update') {
 				// ----------------------------------
 				//         update
@@ -318,7 +320,7 @@ export class MicrosoftSql implements INodeType {
 					},
 				);
 
-				returnItems = items;
+				responseData = items;
 			} else if (operation === 'delete') {
 				// ----------------------------------
 				//         delete
@@ -368,9 +370,7 @@ export class MicrosoftSql implements INodeType {
 					0,
 				);
 
-				returnItems = this.helpers.returnJsonArray({
-					rowsDeleted,
-				} as IDataObject);
+				responseData = rowsDeleted;
 			} else {
 				await pool.close();
 				throw new NodeOperationError(
@@ -380,7 +380,7 @@ export class MicrosoftSql implements INodeType {
 			}
 		} catch (error) {
 			if (this.continueOnFail() === true) {
-				returnItems = items;
+				responseData = items;
 			} else {
 				await pool.close();
 				throw error;
@@ -389,7 +389,12 @@ export class MicrosoftSql implements INodeType {
 
 		// Close the connection
 		await pool.close();
+		const executionData = this.helpers.constructExecutionMetaData(
+			{ item: 0 },
+			this.helpers.returnJsonArray(responseData),
+		);
 
+		returnItems.push(...executionData);
 		return this.prepareOutputData(returnItems);
 	}
 }
