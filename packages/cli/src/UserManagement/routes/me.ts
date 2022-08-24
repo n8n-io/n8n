n8n-io/n8n

commit 7d74ddab29e05a81962fd80469248e8cee8bf9bf

feat(cli): notify external hooks about user profile and password changes (#3919)

packages/cli/src/UserManagement/routes/me.ts
@@ -48,6 +48,7 @@ export function meNamespace(this: N8nApp): void {
 					throw new ResponseHelper.ResponseError('Invalid email address', undefined, 400);
 				}
 
+				const { email: currentEmail } = req.user;
 				const newUser = new User();
 
 				Object.assign(newUser, req.user, req.body);
@@ -65,6 +66,7 @@ export function meNamespace(this: N8nApp): void {
 					user_id: req.user.id,
 					fields_changed: updatedkeys,
 				});
+				await this.externalHooks.run('user.profile.update', [ currentEmail, req.body ]);
 
 				return sanitizeUser(user);
 			},
@@ -109,6 +111,8 @@ export function meNamespace(this: N8nApp): void {
 				user_id: req.user.id,
 				fields_changed: ['password'],
 			});
+
+			await this.externalHooks.run('user.password.update', [ user.email, req.user.password ]);
 
 			return { success: true };
 		}),

commit f7055fec6afbe22987f083e75fbe1cbc10f8565b

fix(cli): fix the tests, that broke after #3919

packages/cli/src/UserManagement/routes/me.ts
@@ -66,7 +66,7 @@ export function meNamespace(this: N8nApp): void {
 					user_id: req.user.id,
 					fields_changed: updatedkeys,
 				});
-				await this.externalHooks.run('user.profile.update', [ currentEmail, req.body ]);
+				await this.externalHooks.run('user.profile.update', [currentEmail, req.body]);
 
 				return sanitizeUser(user);
 			},
@@ -112,7 +112,7 @@ export function meNamespace(this: N8nApp): void {
 				fields_changed: ['password'],
 			});
 
-			await this.externalHooks.run('user.password.update', [ user.email, req.user.password ]);
+			await this.externalHooks.run('user.password.update', [user.email, req.user.password]);
 
 			return { success: true };
 		}),

packages/cli/test/integration/shared/utils.ts
@@ -98,7 +98,7 @@ export async function initTestServer({
 
 	if (!endpointGroups) return testServer.app;
 
-	if (endpointGroups.includes('credentials')) {
+	if (endpointGroups.includes('credentials') || endpointGroups.includes('me')) {
 		testServer.externalHooks = ExternalHooks();
 	}
 