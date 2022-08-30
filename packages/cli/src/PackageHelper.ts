d8f8cafdb361616960bb4f9ce7f1122ff4c44e49
Omar Ajoue <krynble@gmail.com>
2022-08-30 09:50:07 +0200

    Minor fixes and refactors

--- packages/cli/src/PackageHelper.ts
+++
@@ -1,7 +0,0 @@
-import { Script } from 'vm';
-
-export const loadClassInIsolation = (filePath: string, className: string) => {
-	const script = new Script(`new (require('${filePath}').${className})()`);
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	return script.runInNewContext({ require });
-};
