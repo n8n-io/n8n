67ea912ec5c6334a790f817251678cd76a4487b5
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2022-09-01 09:40:35 +0200

    fix(docker): Revert docker `USER` and `WORKDIR` changes (#3992)
    Switching from `root` to another user needs a bit more testing, as we need to figure out how to auto-migrate all existing instances of n8n when people upgrade.

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+if [ -d /root/.n8n ] ; then
+  chmod o+rx /root
+  chown -R node /root/.n8n
+  ln -s /root/.n8n /home/node/
+fi
+
+chown -R node /home/node
+
 if [ "$#" -gt 0 ]; then
   # Got started with arguments
-  exec node "$@"
+  exec su-exec node "$@"
 else
   # Got started without arguments
-  exec n8n
+  exec su-exec n8n
 fi
