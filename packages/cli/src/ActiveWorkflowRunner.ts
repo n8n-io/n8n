43b9dd3d182b9cbf53492811f95a8abac48cb602
Jan Oberhauser <jan.oberhauser@gmail.com>
2022-09-05 13:30:27 +0200

    :zap: Retry indefinetly

--- packages/cli/src/ActiveWorkflowRunner.ts
+++ packages/cli/src/ActiveWorkflowRunner.ts
@@ -732,17 +732,31 @@ export class ActiveWorkflowRunner {
 					},
 				};
 
-				// Try to activate workflow again for 2 minutes (12x 10 seconds wait inbetween)
+				// Run Error Workflow if defined
+				const activationError = new WorkflowActivationError(
+					'There was a problem with the trigger, for that reason did the workflow had to be deactivated',
+					error,
+					node,
+				);
+				this.executeErrorWorkflow(activationError, workflowData, mode);
+
+				// Try to reactivate workflow again
 				let reactivationSuccessful = false;
-				for (let i = 1; i <= 12; i++) {
+				let timeoutTime = 1000;
+				do {
 					try {
+						// eslint-disable-next-line @typescript-eslint/no-loop-func
 						await new Promise((resolve) => {
 							setTimeout(() => {
 								resolve(undefined);
-							}, 10000);
+							}, timeoutTime);
 						});
 
-						Logger.info(`${i}/12 try to reactivate workflow "${workflowData.name}"`, {
+						if (timeoutTime < 180000) {
+							timeoutTime = Math.min(timeoutTime * 2, 180000);
+						}
+
+						Logger.info(`Try to reactivate workflow "${workflowData.name}"`, {
 							nodeName: node.name,
 							workflowId: workflowData.id.toString(),
 							workflowName: workflowData.name,
@@ -755,7 +769,6 @@ export class ActiveWorkflowRunner {
 							workflowName: workflowData.name,
 						});
 						reactivationSuccessful = true;
-						break;
 					} catch (error) {
 						Logger.info(
 							// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
@@ -767,26 +780,7 @@ export class ActiveWorkflowRunner {
 							},
 						);
 					}
-				}
-
-				if (!reactivationSuccessful) {
-					// All the tries to activate the workflow again failed
-					Logger.error(
-						`The workflow "${workflowData.name}" could not be reactivated and is now inactive. Error workflow will now run if one got defined.`,
-						{
-							nodeName: node.name,
-							workflowId: workflowData.id.toString(),
-							workflowName: workflowData.name,
-						},
-					);
-
-					const activationError = new WorkflowActivationError(
-						'There was a problem with the trigger, for that reason did the workflow had to be deactivated',
-						error,
-						node,
-					);
-					this.executeErrorWorkflow(activationError, workflowData, mode);
-				}
+				} while (!reactivationSuccessful);
 			};
 			return returnFunctions;
 		};
