67e49d577d287c3de91edb98d0953303e7035cb8
Iván Ovejero <ivov.src@gmail.com>
2022-09-07 11:19:37 +0200

    :zap: Pass `user_id` to manual exec props

--- packages/cli/src/InternalHooks.ts
+++ packages/cli/src/InternalHooks.ts
@@ -185,6 +185,7 @@ export class InternalHooksClass implements IInternalHooksClass {
 				}
 
 				const manualExecEventProperties: ITelemetryTrackProperties = {
+					user_id: userId,
 					workflow_id: workflow.id.toString(),
 					status: properties.success ? 'success' : 'failed',
 					error_message: properties.error_message as string,
