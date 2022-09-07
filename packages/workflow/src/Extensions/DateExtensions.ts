0eac8bf5c4ee7f4968a2f014df286b0d5c26cc6a
brianinoa <brian@n8n.io>
2022-09-07 12:48:37 +0200

    :zap: Add more extension methods

--- packages/workflow/src/Extensions/DateExtensions.ts
+++ packages/workflow/src/Extensions/DateExtensions.ts
@@ -18,7 +18,7 @@ export class DateExtensions extends BaseExtension<string> {
 			const [key, method] = c;
 			Object.assign(p, {
 				[key]: () => {
-					return method.call('', mainArg, extraArgs);
+					return method.call(this, mainArg, extraArgs);
 				},
 			});
 			return p;
@@ -26,7 +26,17 @@ export class DateExtensions extends BaseExtension<string> {
 	}
 
 	private initializeMethodMap(): void {
-		this.methodMapping = new Map<string, (value: string) => string | Date>([]);
+		this.methodMapping = new Map<
+			string,
+			(
+				value: string,
+				extraArgs?: string | number[] | string[] | boolean[] | undefined,
+			) => boolean | string | Date | number
+		>([
+			['isDst', this.isDst],
+			['isInLast', this.isInLast],
+			['plus', this.plus],
+		]);
 	}
 
 	private generateDurationObject(
@@ -51,23 +61,73 @@ export class DateExtensions extends BaseExtension<string> {
 		return durationObject;
 	}
 
+	begginingOf(): boolean {
+		return false;
+	}
+
+	endOfMonth(): boolean {
+		return false;
+	}
+
+	extract(): string {
+		return '';
+	}
+
+	format(): string {
+		return '';
+	}
+
+	isBetween(value: string, extraArgs?: any): boolean {
+		const comparisonDate = new Date(value);
+		const [firstDate, secondDate] = extraArgs as Date[];
+
+		if (firstDate > secondDate) {
+			return secondDate < comparisonDate && comparisonDate < firstDate;
+		}
+		return secondDate > comparisonDate && comparisonDate > firstDate;
+	}
+
 	isDst(value: string): boolean {
 		return DateTime.fromJSDate(new Date(value)).isInDST;
 	}
 
-	isInLast(value: string, extraArgs?: [number, DurationUnit]): boolean {
-		const [durationValue = 0, unit = 'minute'] = extraArgs ?? [];
+	isInLast(value: string, extraArgs?: any): boolean {
+		const [durationValue = 0, unit = 'minute'] = extraArgs as [number, DurationUnit];
 
 		const dateInThePast = DateTime.now().minus(this.generateDurationObject(durationValue, unit));
 		const thisDate = DateTime.fromJSDate(new Date(value));
 		return dateInThePast <= thisDate && thisDate <= DateTime.now();
 	}
 
-	plus(value: string, extraArgs?: [number, DurationUnit]): Date {
-		const [durationValue = 0, unit = 'minute'] = extraArgs ?? [];
+	isWeekend(): boolean {
+		return false;
+	}
+
+	minus(value: string, extraArgs?: any): Date {
+		const [durationValue = 0, unit = 'minute'] = extraArgs as [number, DurationUnit];
+
+		return DateTime.fromJSDate(new Date(value))
+			.minus(this.generateDurationObject(durationValue, unit))
+			.toJSDate();
+	}
+
+	plus(value: string, extraArgs?: any): Date {
+		const [durationValue = 0, unit = 'minute'] = extraArgs as [number, DurationUnit];
 
 		return DateTime.fromJSDate(new Date(value))
 			.plus(this.generateDurationObject(durationValue, unit))
 			.toJSDate();
+	}
+
+	toLocaleString(): string {
+		return '';
+	}
+
+	toTimeFromNow(): Date {
+		return new Date();
+	}
+
+	timeTo(): number {
+		return 1;
 	}
 }
