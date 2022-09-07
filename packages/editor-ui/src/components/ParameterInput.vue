772836abc7d81fce74547dd3644c45eaea9c0a75
Iván Ovejero <ivov.src@gmail.com>
2022-09-07 09:18:18 +0200

    fix(editor-ui): Disable editing in Function nodes in executions view (#4041)
    :bug: Disable editing in Function nodes in executions view

--- packages/editor-ui/src/components/ParameterInput.vue
+++ packages/editor-ui/src/components/ParameterInput.vue
@@ -37,6 +37,7 @@
 					:type="editorType"
 					:codeAutocomplete="codeAutocomplete"
 					:path="path"
+					:readonly="isReadOnly"
 					@closeDialog="closeCodeEditDialog"
 					@valueChanged="expressionUpdated"
 				></code-edit>
