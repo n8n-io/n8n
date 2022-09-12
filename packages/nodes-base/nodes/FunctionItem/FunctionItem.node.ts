de4ca3b0cc63b7a568810424fb3deeb34aa4b676
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2022-09-12 18:07:56 +0300

    test(UM tests): add missing UM tests n8n-3648 (#4077)
    * :zap: added test for users reinvite

--- packages/nodes-base/nodes/FunctionItem/FunctionItem.node.ts
+++ packages/nodes-base/nodes/FunctionItem/FunctionItem.node.ts
@@ -113,7 +113,7 @@ return item;`,
 						if (item?.binary && item?.index !== undefined && item?.index !== null) {
 							for (const binaryPropertyName of Object.keys(item.binary)) {
 								item.binary[binaryPropertyName].data = (
-									await this.helpers.getBinaryDataBuffer(item.index, binaryPropertyName)
+									await this.helpers.getBinaryDataBuffer(item.index as number, binaryPropertyName)
 								)?.toString('base64');
 							}
 						}
