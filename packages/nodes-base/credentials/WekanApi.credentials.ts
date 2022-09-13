e3d76cf0113845b78a1059c781d71a02f694c059
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2022-09-13 10:32:06 +0100

    fix(wekan Node): Fix authentication with new versions of Wekan

--- packages/nodes-base/credentials/WekanApi.credentials.ts
+++ packages/nodes-base/credentials/WekanApi.credentials.ts
@@ -1,4 +1,11 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	INodeProperties,
+} from 'n8n-workflow';
 
 export class WekanApi implements ICredentialType {
 	name = 'wekanApi';
@@ -15,6 +22,9 @@ export class WekanApi implements ICredentialType {
 			displayName: 'Password',
 			name: 'password',
 			type: 'string',
+			typeOptions: {
+				password: true,
+			},
 			default: '',
 		},
 		{
@@ -24,5 +34,43 @@ export class WekanApi implements ICredentialType {
 			default: '',
 			placeholder: 'https://wekan.yourdomain.com',
 		},
+		{
+			displayName: 'Session Token',
+			name: 'token',
+			type: 'hidden',
+			typeOptions: {
+				expirable: true,
+			},
+			default: '',
+		},
 	];
+
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		const url = credentials.url as string;
+		const { token } = (await this.helpers.httpRequest({
+			method: 'POST',
+			url: `${url.endsWith('/') ? url.slice(0, -1) : url}/users/login`,
+			body: {
+				username: credentials.username,
+				password: credentials.password,
+			},
+		})) as { token: string };
+		return { token };
+	}
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.token}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials.url.replace(new RegExp("/$"), "")}}',
+			url: '/api/user',
+		},
+	};
 }
