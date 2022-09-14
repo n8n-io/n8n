46f7adcb940cbabf3fc12c3b9cd1429902d6914a
Iván Ovejero <ivov.src@gmail.com>
2022-09-14 14:53:58 +0200

    :blue_book: Add type to mixin type

--- packages/editor-ui/src/components/CodeNodeEditor/types.ts
+++ packages/editor-ui/src/components/CodeNodeEditor/types.ts
@@ -1,10 +1,12 @@
 import type { EditorView } from '@codemirror/view';
 import type { I18nClass } from '@/plugins/i18n';
+import type { INodeUi } from '@/Interface';
 
 export type CodeNodeEditorMixin = Vue.VueConstructor<
 	Vue & {
 		$locale: I18nClass;
 		editor: EditorView | null;
 		mode: 'runOnceForAllItems' | 'runOnceForEachItem';
+		activeNode: INodeUi;
 	}
 >;
