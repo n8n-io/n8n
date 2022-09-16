4996d935d117c018c9d69a90f01b3cf022e82d21
yucheng19930906 <1030900948@qq.com>
2022-09-16 11:25:38 +0800

    Update

--- packages-0.193.5/nodes-base/nodes/utils/utilities.ts
+++ packages-0.193.5/nodes-base/nodes/utils/utilities.ts
@@ -14,9 +14,9 @@
  * chunk(['a', 'b', 'c', 'd'], 3)
  * // => [['a', 'b', 'c'], ['d']]
  */
+import {DEFAULT_DEV_HASURA_URL} from './constants';
 
-// tslint:disable-next-line:no-any
-export function chunk(array: any[], size = 1) {
+export function chunk(array: any[], size = 1) { // tslint:disable-line:no-any
 	const length = array == null ? 0 : array.length;
 	if (!length || size < 1) {
 		return [];
@@ -42,13 +42,10 @@ export function chunk(array: any[], size = 1) {
  * // => ['a', 'b', 'c', 'd']
  *
  */
-
-// tslint:disable-next-line:no-any
-export function flatten(nestedArray: any[][]) {
+export function flatten(nestedArray: any[][]) { // tslint:disable-line:no-any
 	const result = [];
 
-	// tslint:disable-next-line:no-any
-	(function loop(array: any[]) {
+	(function loop(array: any[]) { // tslint:disable-line:no-any
 		for (let i = 0; i < array.length; i++) {
 			if (Array.isArray(array[i])) {
 				loop(array[i]);
@@ -60,3 +57,23 @@ export function flatten(nestedArray: any[][]) {
 
 	return result;
 }
+
+export function getStageFromEnv(): string {
+	const stage = process.env.NODE_ENV as string;
+	return stage === undefined ? 'dev' : stage;
+}
+
+export function getHasuraUrl(): string {
+	const data = process.env.HASURA_URL as string;
+	if (data === undefined) {
+		return DEFAULT_DEV_HASURA_URL;
+	}
+	return data;
+}
+
+
+export function  getHasuraAdminSecret(): string {
+	const data = process.env.X_HASURA_ADMIN_SECRET as string;
+	return data === undefined ? '' : data;
+}
+
