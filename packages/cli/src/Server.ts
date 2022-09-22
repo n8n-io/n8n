323482324ca78d0e5f2a57b3bdbeaabf9d1a54e8
Iván Ovejero <ivov.src@gmail.com>
2022-09-22 12:34:32 +0200

    :zap: Fix comma from merge conflict resolution

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -326,7 +326,7 @@ class App {
 			allowedModules: {
 				builtIn: process.env.NODE_FUNCTION_ALLOW_BUILTIN,
 				external: process.env.NODE_FUNCTION_ALLOW_EXTERNAL,
-			};
+			},
 			enterprise: {
 				sharing: false,
 			},
