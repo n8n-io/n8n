bcf5a14b1e3d6f4b027b99ed3dd4e40beb0b955a
Shao Yu-Lung (Allen) <bestlong168@gmail.com>
2022-09-22 08:41:44 +0800

    fix(MSSQL Node): Support tdsVersion option

--- packages/nodes-base/nodes/Microsoft/Sql/MicrosoftSql.node.ts
+++ packages/nodes-base/nodes/Microsoft/Sql/MicrosoftSql.node.ts
@@ -271,6 +271,7 @@ export class MicrosoftSql implements INodeType {
 			options: {
 				encrypt: credentials.tls as boolean,
 				enableArithAbort: false,
+				tdsVersion: credentials.tdsVersion as string,
 			},
 		};
 
