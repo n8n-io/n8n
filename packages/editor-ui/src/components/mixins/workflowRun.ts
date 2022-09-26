6be8fc9aa65516a6984d6a3d75f29aecc6fb5ec2
Iván Ovejero <ivov.src@gmail.com>
2022-09-26 10:09:47 +0200

    :bug: Fix excess run in partial execution

--- packages/editor-ui/src/components/mixins/workflowRun.ts
+++ packages/editor-ui/src/components/mixins/workflowRun.ts
@@ -142,6 +142,8 @@ export const workflowRun = mixins(
 
 				const startNodes: string[] = [];
 
+				const workflowData = await this.getWorkflowDataToSave();
+
 				if (runData !== null && Object.keys(runData).length !== 0) {
 					newRunData = {};
 
@@ -164,7 +166,11 @@ export const workflowRun = mixins(
 								startNodes.push(parentNode);
 								break;
 							}
-							newRunData[parentNode] = runData[parentNode].slice(0, 1);
+
+							// send `runData` only if node is not pinned
+							if (workflowData.pinData && !workflowData.pinData[parentNode]) {
+								newRunData[parentNode] = runData[parentNode].slice(0, 1);
+							}
 						}
 					}
 
@@ -185,7 +191,6 @@ export const workflowRun = mixins(
 					await this.saveCurrentWorkflow();
 				}
 
-				const workflowData = await this.getWorkflowDataToSave();
 
 				const startRunData: IStartRunData = {
 					workflowData,
