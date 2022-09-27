7ca3b1a2f2cf03f637ae75a62b30aaa75294eae2
Mike Arvela <mike@n8n.io>
2022-09-27 12:05:51 +0300

    feat(n8nApi node): add core node for consuming the n8n API (#4076)
    * feat(n8n node): create n8n core node for consuming the n8n API
    
    Co-authored-by: Michael Kret <michael.k@radency.com>

---
+++ packages/nodes-base/nodes/N8n/ExecutionDescription.ts
@@ -0,0 +1,252 @@
+/* eslint-disable n8n-nodes-base/node-param-default-missing */
+import { getCursorPaginator } from './GenericFunctions';
+import { INodeProperties } from 'n8n-workflow';
+import { workflowIdLocator } from './WorkflowLocator';
+
+export const executionOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		default: 'getAll',
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+			},
+		},
+		options: [
+			{
+				name: 'Get',
+				value: 'get',
+				action: 'Get an execution',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '=/executions/{{ $parameter.executionId }}',
+					},
+				},
+			},
+			{
+				name: 'Get Many',
+				value: 'getAll',
+				action: 'Get many executions',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '/executions',
+					},
+					send: {
+						paginate: true,
+					},
+					operations: {
+						pagination: getCursorPaginator(),
+					},
+				},
+			},
+			{
+				name: 'Delete',
+				value: 'delete',
+				action: 'Delete an execution',
+				routing: {
+					request: {
+						method: 'DELETE',
+						url: '=/executions/{{ $parameter.executionId }}',
+					},
+				},
+			},
+		],
+	},
+];
+
+const deleteOperation: INodeProperties[] = [
+	{
+		displayName: 'Execution ID',
+		name: 'executionId',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['delete'],
+			},
+		},
+		default: '',
+	},
+];
+
+const getAllOperation: INodeProperties[] = [
+	{
+		displayName: 'Return All',
+		name: 'returnAll',
+		type: 'boolean',
+		default: false,
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['getAll'],
+			},
+		},
+		description: 'Whether to return all results or only up to a given limit',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'number',
+		default: 100,
+		typeOptions: {
+			minValue: 1,
+			maxValue: 250,
+		},
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['getAll'],
+				returnAll: [false],
+			},
+		},
+		routing: {
+			request: {
+				qs: {
+					limit: '={{ $value }}',
+				},
+			},
+		},
+		description: 'Max number of results to return',
+	},
+	{
+		displayName: 'Filters',
+		name: 'filters',
+		type: 'collection',
+		placeholder: 'Add Filter',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['getAll'],
+			},
+		},
+		options: [
+			{
+				// Use the common workflowIdLocator, but provide a custom routing
+				...workflowIdLocator,
+				routing: {
+					send: {
+						type: 'query',
+						property: 'workflowId',
+						value: '={{ $value || undefined }}',
+					},
+				},
+				description: 'Workflow to filter the executions by',
+			},
+			{
+				displayName: 'Status',
+				name: 'status',
+				type: 'options',
+				options: [
+					{
+						name: 'Error',
+						value: 'error',
+					},
+					{
+						name: 'Success',
+						value: 'success',
+					},
+					{
+						name: 'Waiting',
+						value: 'waiting',
+					},
+				],
+				default: 'success',
+				routing: {
+					send: {
+						type: 'query',
+						property: 'status',
+						value: '={{ $value }}',
+					},
+				},
+				description: 'Status to filter the executions by',
+			},
+		],
+	},
+	{
+		displayName: 'Options',
+		name: 'options',
+		type: 'collection',
+		default: {},
+		placeholder: 'Add Option',
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['getAll'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Include Execution Details',
+				name: 'activeWorkflows',
+				type: 'boolean',
+				default: false,
+				routing: {
+					send: {
+						type: 'query',
+						property: 'includeData',
+						value: '={{ $value }}',
+					},
+				},
+				description: 'Whether to include the detailed execution data',
+			},
+		],
+	},
+];
+
+const getOperation: INodeProperties[] = [
+	{
+		displayName: 'Execution ID',
+		name: 'executionId',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['get'],
+			},
+		},
+	},
+	{
+		displayName: 'Options',
+		name: 'options',
+		type: 'collection',
+		default: {},
+		placeholder: 'Add Option',
+		displayOptions: {
+			show: {
+				resource: ['execution'],
+				operation: ['get'],
+			},
+		},
+		options: [
+			{
+				displayName: 'Include Execution Details',
+				name: 'activeWorkflows',
+				type: 'boolean',
+				default: false,
+				routing: {
+					send: {
+						type: 'query',
+						property: 'includeData',
+						value: '={{ $value }}',
+					},
+				},
+				description: 'Whether to include the detailed execution data',
+			},
+		],
+	},
+];
+
+export const executionFields: INodeProperties[] = [
+	...deleteOperation,
+	...getAllOperation,
+	...getOperation,
+];
---
+++ packages/nodes-base/nodes/N8n/N8n.node.ts
@@ -0,0 +1,80 @@
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { credentialFields, credentialOperations } from './CredentialDescription';
+import { executionFields, executionOperations } from './ExecutionDescription';
+import { workflowFields, workflowOperations } from './WorkflowDescription';
+import { searchWorkflows } from './WorkflowLocator';
+
+/**
+ * The n8n node provides access to the n8n API.
+ *
+ * See: https://docs.n8n.io/api/api-reference/
+ */
+export class N8n implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'n8n',
+		name: 'n8n',
+		icon: 'file:n8n.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Consume n8n API',
+		defaults: {
+			name: 'n8n',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'n8nApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			returnFullResponse: true,
+			baseURL: '={{ $credentials.baseUrl.replace(new RegExp("/$"), "") }}',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Credential',
+						value: 'credential',
+					},
+					{
+						name: 'Execution',
+						value: 'execution',
+					},
+					{
+						name: 'Workflow',
+						value: 'workflow',
+					},
+				],
+				default: 'workflow',
+			},
+
+			...credentialOperations,
+			...credentialFields,
+
+			...executionOperations,
+			...executionFields,
+
+			...workflowOperations,
+			...workflowFields,
+		],
+	};
+
+	methods = {
+		listSearch: {
+			// Provide workflows search capability for the workflow resourceLocator
+			searchWorkflows,
+		},
+	};
+}
