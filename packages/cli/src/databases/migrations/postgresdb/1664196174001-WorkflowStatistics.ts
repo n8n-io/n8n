10deae812884624a0e160ccb1d36664512d5a70e
freyamade <freya@n8n.io>
2022-09-27 12:21:08 +0100

    :sparkles: - add postgres migration

---
+++ packages/cli/src/databases/migrations/postgresdb/1664196174001-WorkflowStatistics.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { logMigrationEnd, logMigrationStart } from '../../utils/migrationHelpers';
+import config from '../../../../config';
+
+export class WorkflowStatistics1664196174001 implements MigrationInterface {
+	name = 'WorkflowStatistics1664196174001';
+
+	async up(queryRunner: QueryRunner): Promise<void> {
+		logMigrationStart(this.name);
+
+		const tablePrefix = config.getEnv('database.tablePrefix');
+
+		await queryRunner.query(
+			`CREATE TABLE \`${tablePrefix}workflow_statistics\` (
+				"count" INTEGER DEFAULT 0,
+				"latestEvent" DATETIME NOT NULL,
+				"name" VARCHAR(128) NOT NULL,
+				"workflow" INTEGER,
+				PRIMARY KEY("workflow", "name"),
+				FOREIGN KEY("workflow") REFERENCES \`${tablePrefix}workflow_entity\`("id") ON DELETE CASCADE
+			)`,
+		);
+
+		// TODO - Prepop these keys / values
+
+		logMigrationEnd(this.name);
+	}
+
+	async down(queryRunner: QueryRunner): Promise<void> {
+		const tablePrefix = config.getEnv('database.tablePrefix');
+
+		await queryRunner.query(`DROP TABLE "${tablePrefix}workflow_entity"`);
+	}
+}
