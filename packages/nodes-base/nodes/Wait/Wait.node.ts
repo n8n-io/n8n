894548d04b0e1c00ff3c7600b283f3d5e91a7c7a
Mike Arvela <mike@n8n.io>
2022-09-29 13:33:16 +0300

    fix(nodes-base): fix and harmonize all primaryDocumentation links (#4191)
    * fix(nodes-base): fix and harmonize all primaryDocumentation links
    
    * feat(workflow, cli): expose documentation links to UI via node codex
    
    * fix(editor-ui): link to correct node and credential documentation URLs
    
    * config(nodes-base): update 'format' script to also format node descriptor json
    
    * chore: fix outdated links to node reference documentation

--- packages/nodes-base/nodes/Wait/Wait.node.ts
+++ packages/nodes-base/nodes/Wait/Wait.node.ts
@@ -212,7 +212,7 @@ export class Wait implements INodeType {
 			// ----------------------------------
 			{
 				displayName:
-					'The webhook URL will be generated at run time. It can be referenced with the <strong>$resumeWebhookUrl</strong> variable. Send it somewhere before getting to this node. <a href="https://docs.n8n.io/nodes/n8n-nodes-base.wait?utm_source=n8n_app&utm_medium=node_settings_modal-credential_link&utm_campaign=n8n-nodes-base.wait" target="_blank">More info</a>',
+					'The webhook URL will be generated at run time. It can be referenced with the <strong>$resumeWebhookUrl</strong> variable. Send it somewhere before getting to this node. <a href="https://docs.n8n.io/integrations/builtin/core-nodes/n8n-nodes-base.wait/?utm_source=n8n_app&utm_medium=node_settings_modal-credential_link&utm_campaign=n8n-nodes-base.wait" target="_blank">More info</a>',
 				name: 'webhookNotice',
 				type: 'notice',
 				displayOptions: {
