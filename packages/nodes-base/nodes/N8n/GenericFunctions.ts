f467c4499e069d607d528b5dece64718735897a3
Jonathan <jonathan.bennetts@gmail.com>
2022-10-01 14:47:09 +0100

    fix lint issues

--- packages/nodes-base/nodes/N8n/GenericFunctions.ts
+++ packages/nodes-base/nodes/N8n/GenericFunctions.ts
@@ -24,6 +24,7 @@ export async function apiRequest(
 	endpoint: string,
 	body: object,
 	query?: IDataObject,
+	// tslint:disable-next-line:no-any
 ): Promise<any> {
 	query = query || {};
 
@@ -59,6 +60,7 @@ export async function apiRequestAllItems(
 	endpoint: string,
 	body: object,
 	query?: IDataObject,
+	// tslint:disable-next-line:no-any
 ): Promise<any> {
 	query = query || {};
 	const returnData: IDataObject[] = [];
