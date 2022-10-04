459736c135e4772dbd4390952bdaffaa19bed19b
Csaba Tuncsik <csaba@n8n.io>
2022-10-04 14:53:19 +0200

    refactor(design-system): unbreaking wrapper around element ui tooltip

--- packages/design-system/src/components/N8nTooltip/Tooltip.vue
+++ packages/design-system/src/components/N8nTooltip/Tooltip.vue
@@ -1,7 +1,23 @@
+<template>
+	<el-tooltip v-bind="$attrs">
+		<template v-for="(_, scopedSlotName) in $scopedSlots" #[scopedSlotName]="slotData">
+			<slot :name="scopedSlotName" v-bind="slotData"/>
+		</template>
+		<template v-for="(_, slotName) in $slots" #[slotName]>
+			<slot :name="slotName"/>
+		</template>
+	</el-tooltip>
+</template>
+
 <script lang="ts">
+import Vue from "vue";
 import ElTooltip from 'element-ui/lib/tooltip';
 
-ElTooltip.name = 'n8n-tooltip'; // eslint-disable-line @typescript-eslint/no-unsafe-member-access
-
-export default ElTooltip;
+export default Vue.extend({
+	name: 'n8n-tooltip',
+	inheritAttrs: false,
+	components: {
+		ElTooltip, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+	},
+});
 </script>
