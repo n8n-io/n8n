d7b80394825104445458e311ffddf5cd3e5133ed
Iván Ovejero <ivov.src@gmail.com>
2022-10-05 13:55:04 +0200

    :zap: Extend `WorkflowOperationError`

--- packages/workflow/src/WorkflowErrors.ts
+++ packages/workflow/src/WorkflowErrors.ts
@@ -6,12 +6,22 @@ import { INode } from './Interfaces';
 export class WorkflowOperationError extends Error {
 	node: INode | undefined;
 
+	description = '';
+
+	cause: object;
+
 	timestamp: number;
 
-	constructor(message: string, node?: INode) {
+	constructor(message: string, description?: string, node?: INode) {
 		super(message);
 		this.name = this.constructor.name;
 		this.node = node;
 		this.timestamp = Date.now();
+		if (description) this.description = description;
+
+		this.cause = {
+			message,
+			stack: this.stack as string,
+		};
 	}
 }
