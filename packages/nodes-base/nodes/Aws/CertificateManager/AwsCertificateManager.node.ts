8b1e17520f20f70aa8e9dc228a9753a0460c72d4
ricardo <ricardoespinoza105@gmail.com>
2022-10-05 16:08:08 -0400

    :zap: Add codex and paired items

--- packages/nodes-base/nodes/Aws/CertificateManager/AwsCertificateManager.node.ts
+++ packages/nodes-base/nodes/Aws/CertificateManager/AwsCertificateManager.node.ts
@@ -55,98 +55,16 @@ export class AwsCertificateManager implements INodeType {
 		const resource = this.getNodeParameter('resource', 0) as string;
 		const operation = this.getNodeParameter('operation', 0) as string;
 		for (let i = 0; i < items.length; i++) {
-			if (resource === 'certificate') {
-				//https://docs.aws.amazon.com/acm/latest/APIReference/API_DeleteCertificate.html
-				if (operation === 'delete') {
-					const certificateArn = this.getNodeParameter('certificateArn', i) as string;
+			try {
+				if (resource === 'certificate') {
+					//https://docs.aws.amazon.com/acm/latest/APIReference/API_DeleteCertificate.html
+					if (operation === 'delete') {
+						const certificateArn = this.getNodeParameter('certificateArn', i) as string;
 
-					const body: IDataObject = {
-						CertificateArn: certificateArn,
-					};
+						const body: IDataObject = {
+							CertificateArn: certificateArn,
+						};
 
-					responseData = await awsApiRequestREST.call(
-						this,
-						`acm`,
-						'POST',
-						'',
-						JSON.stringify(body),
-						qs,
-						{
-							'X-Amz-Target': 'CertificateManager.DeleteCertificate',
-							'Content-Type': 'application/x-amz-json-1.1',
-						},
-					);
-
-					returnData.push({ success: true });
-				}
-
-				//https://docs.aws.amazon.com/acm/latest/APIReference/API_GetCertificate.html
-				if (operation === 'get') {
-					const certificateArn = this.getNodeParameter('certificateArn', i) as string;
-
-					const body: IDataObject = {
-						CertificateArn: certificateArn,
-					};
-
-					responseData = await awsApiRequestREST.call(
-						this,
-						`acm`,
-						'POST',
-						'',
-						JSON.stringify(body),
-						qs,
-						{
-							'X-Amz-Target': 'CertificateManager.GetCertificate',
-							'Content-Type': 'application/x-amz-json-1.1',
-						},
-					);
-
-					returnData.push(responseData);
-				}
-
-				//https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html
-				if (operation === 'getMany') {
-					const returnAll = this.getNodeParameter('returnAll', 0) as boolean;
-					const options = this.getNodeParameter('options', i) as IDataObject;
-
-					const body: { Includes: IDataObject; CertificateStatuses: string[]; MaxItems: number } = {
-						CertificateStatuses: [],
-						Includes: {},
-						MaxItems: 0,
-					};
-
-					if (options.certificateStatuses) {
-						body.CertificateStatuses = options.certificateStatuses as string[];
-					}
-
-					if (options.certificateStatuses) {
-						body.Includes['extendedKeyUsage'] = options.extendedKeyUsage as string[];
-					}
-
-					if (options.keyTypes) {
-						body.Includes['keyTypes'] = options.keyTypes as string[];
-					}
-
-					if (options.keyUsage) {
-						body.Includes['keyUsage'] = options.keyUsage as string[];
-					}
-
-					if (returnAll) {
-						responseData = await awsApiRequestAllItems.call(
-							this,
-							'CertificateSummaryList',
-							'acm',
-							'POST',
-							'',
-							'{}',
-							qs,
-							{
-								'X-Amz-Target': 'CertificateManager.ListCertificates',
-								'Content-Type': 'application/x-amz-json-1.1',
-							},
-						);
-					} else {
-						body.MaxItems = this.getNodeParameter('limit', 0) as number;
 						responseData = await awsApiRequestREST.call(
 							this,
 							`acm`,
@@ -155,68 +73,159 @@ export class AwsCertificateManager implements INodeType {
 							JSON.stringify(body),
 							qs,
 							{
-								'X-Amz-Target': 'CertificateManager.ListCertificates',
+								'X-Amz-Target': 'CertificateManager.DeleteCertificate',
 								'Content-Type': 'application/x-amz-json-1.1',
 							},
 						);
-						responseData = responseData.CertificateSummaryList;
+
+						responseData = { success: true };
 					}
 
-					returnData.push.apply(returnData, responseData);
-				}
+					//https://docs.aws.amazon.com/acm/latest/APIReference/API_GetCertificate.html
+					if (operation === 'get') {
+						const certificateArn = this.getNodeParameter('certificateArn', i) as string;
 
-				//https://docs.aws.amazon.com/acm/latest/APIReference/API_DescribeCertificate.html
-				if (operation === 'getMetadata') {
-					const certificateArn = this.getNodeParameter('certificateArn', i) as string;
+						const body: IDataObject = {
+							CertificateArn: certificateArn,
+						};
 
-					const body: IDataObject = {
-						CertificateArn: certificateArn,
-					};
+						responseData = await awsApiRequestREST.call(
+							this,
+							`acm`,
+							'POST',
+							'',
+							JSON.stringify(body),
+							qs,
+							{
+								'X-Amz-Target': 'CertificateManager.GetCertificate',
+								'Content-Type': 'application/x-amz-json-1.1',
+							},
+						);
+					}
 
-					responseData = await awsApiRequestREST.call(
-						this,
-						`acm`,
-						'POST',
-						'',
-						JSON.stringify(body),
-						qs,
-						{
-							'X-Amz-Target': 'CertificateManager.DescribeCertificate',
-							'Content-Type': 'application/x-amz-json-1.1',
-						},
+					//https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html
+					if (operation === 'getMany') {
+						const returnAll = this.getNodeParameter('returnAll', 0) as boolean;
+						const options = this.getNodeParameter('options', i) as IDataObject;
+
+						const body: { Includes: IDataObject; CertificateStatuses: string[]; MaxItems: number } = {
+							CertificateStatuses: [],
+							Includes: {},
+							MaxItems: 0,
+						};
+
+						if (options.certificateStatuses) {
+							body.CertificateStatuses = options.certificateStatuses as string[];
+						}
+
+						if (options.certificateStatuses) {
+							body.Includes['extendedKeyUsage'] = options.extendedKeyUsage as string[];
+						}
+
+						if (options.keyTypes) {
+							body.Includes['keyTypes'] = options.keyTypes as string[];
+						}
+
+						if (options.keyUsage) {
+							body.Includes['keyUsage'] = options.keyUsage as string[];
+						}
+
+						if (returnAll) {
+							responseData = await awsApiRequestAllItems.call(
+								this,
+								'CertificateSummaryList',
+								'acm',
+								'POST',
+								'',
+								'{}',
+								qs,
+								{
+									'X-Amz-Target': 'CertificateManager.ListCertificates',
+									'Content-Type': 'application/x-amz-json-1.1',
+								},
+							);
+						} else {
+							body.MaxItems = this.getNodeParameter('limit', 0) as number;
+							responseData = await awsApiRequestREST.call(
+								this,
+								`acm`,
+								'POST',
+								'',
+								JSON.stringify(body),
+								qs,
+								{
+									'X-Amz-Target': 'CertificateManager.ListCertificates',
+									'Content-Type': 'application/x-amz-json-1.1',
+								},
+							);
+							responseData = responseData.CertificateSummaryList;
+						}
+					}
+
+					//https://docs.aws.amazon.com/acm/latest/APIReference/API_DescribeCertificate.html
+					if (operation === 'getMetadata') {
+						const certificateArn = this.getNodeParameter('certificateArn', i) as string;
+
+						const body: IDataObject = {
+							CertificateArn: certificateArn,
+						};
+
+						responseData = await awsApiRequestREST.call(
+							this,
+							`acm`,
+							'POST',
+							'',
+							JSON.stringify(body),
+							qs,
+							{
+								'X-Amz-Target': 'CertificateManager.DescribeCertificate',
+								'Content-Type': 'application/x-amz-json-1.1',
+							},
+						);
+
+						responseData = responseData.Certificate;
+					}
+
+					//https://docs.aws.amazon.com/acm/latest/APIReference/API_RenewCertificate.html
+					if (operation === 'renew') {
+						const certificateArn = this.getNodeParameter('certificateArn', i) as string;
+
+						const body: IDataObject = {
+							CertificateArn: certificateArn,
+						};
+
+						responseData = await awsApiRequestREST.call(
+							this,
+							`acm`,
+							'POST',
+							'',
+							JSON.stringify(body),
+							qs,
+							{
+								'X-Amz-Target': 'CertificateManager.RenewCertificate',
+								'Content-Type': 'application/x-amz-json-1.1',
+							},
+						);
+
+						responseData = { success: true };
+					}
+
+					const executionData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray(responseData),
+						{ itemData: { item: i } },
 					);
 
-					responseData = responseData.Certificate;
-
-					returnData.push(responseData);
+					returnData.push(...executionData);
 				}
-
-				//https://docs.aws.amazon.com/acm/latest/APIReference/API_RenewCertificate.html
-				if (operation === 'renew') {
-					const certificateArn = this.getNodeParameter('certificateArn', i) as string;
-
-					const body: IDataObject = {
-						CertificateArn: certificateArn,
-					};
-
-					responseData = await awsApiRequestREST.call(
-						this,
-						`acm`,
-						'POST',
-						'',
-						JSON.stringify(body),
-						qs,
-						{
-							'X-Amz-Target': 'CertificateManager.RenewCertificate',
-							'Content-Type': 'application/x-amz-json-1.1',
-						},
-					);
-
-					returnData.push({ success: true });
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ json: { error: error.message } });
+					continue;
 				}
+				throw error;
 			}
 		}
 
-		return [this.helpers.returnJsonArray(returnData)];
+		return [returnData as INodeExecutionData[]];
 	}
 }
