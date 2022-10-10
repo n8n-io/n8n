e90775d1e19ea85323f1752057c0830192273ce5
Iván Ovejero <ivov.src@gmail.com>
2022-10-10 11:28:19 +0200

    :fire: Remove unneeded lint exception

--- packages/workflow/src/Workflow.ts
+++ packages/workflow/src/Workflow.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
