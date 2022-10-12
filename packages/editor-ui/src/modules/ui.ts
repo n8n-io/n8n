9765f6d5aed70423ded2a99638d5c4356e559f7b
Milorad FIlipović <milorad@n8n.io>
2022-10-12 18:18:06 +0200

    fix(editor): fix for menu collapse lag when loading a credentials page (#4329)
    🐛 Fixing a menu collapse bug on credentials view

--- packages/editor-ui/src/modules/ui.ts
+++ packages/editor-ui/src/modules/ui.ts
@@ -110,7 +110,7 @@ const module: Module<IUiState, IRootState> = {
 			},
 		},
 		modalStack: [],
-		sidebarMenuCollapsed: true,
+		sidebarMenuCollapsed: false,
 		isPageLoading: true,
 		currentView: '',
 		mainPanelDimensions: {},
