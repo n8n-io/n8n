f222ffd76f576ae70f9792fccee5c9b6e800d7ed
Iván Ovejero <ivov.src@gmail.com>
2022-10-12 10:48:38 +0200

    :bug: Prevent overlapping `runData` and `pinData`

--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -2311,6 +2311,10 @@ export default mixins(
 				});
 			},
 			onNodeRun({ name, data, waiting }: { name: string, data: ITaskData[] | null, waiting: boolean }) {
+				const pinData = this.$store.getters.pinData;
+
+				if (pinData && pinData[name]) return;
+
 				const sourceNodeName = name;
 				const sourceNode = this.$store.getters.getNodeByName(sourceNodeName);
 				const sourceId = sourceNode.id;
