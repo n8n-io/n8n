1efa85c72708932451d1a9bc5a0c994e099e5792
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-10-13 18:01:14 +0200

    fix(workflow): expression evaluation should respect `N8N_BLOCK_ENV_ACCESS_IN_NODE`

--- packages/workflow/src/Expression.ts
+++ packages/workflow/src/Expression.ts
@@ -104,7 +104,7 @@ export class Expression {
 			typeof process !== 'undefined'
 				? {
 						arch: process.arch,
-						env: process.env,
+						env: process.env.N8N_BLOCK_ENV_ACCESS_IN_NODE === 'true' ? {} : process.env,
 						platform: process.platform,
 						pid: process.pid,
 						ppid: process.ppid,
