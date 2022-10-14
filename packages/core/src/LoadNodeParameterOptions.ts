e6995852f5a26eef930acb3d1837fe0c4c327759
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-10-14 20:29:28 +0200

    fix(core): delete duplicate code between `LoadNode*` classes

--- packages/core/src/LoadNodeParameterOptions.ts
+++ packages/core/src/LoadNodeParameterOptions.ts
@@ -1,102 +1,21 @@
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
 import {
 	ILoadOptions,
-	INode,
-	INodeCredentials,
 	INodeExecutionData,
-	INodeParameters,
 	INodeProperties,
 	INodePropertyOptions,
 	INodeType,
-	INodeTypeNameVersion,
-	INodeTypes,
 	IRunExecutionData,
 	ITaskDataConnections,
 	IWorkflowExecuteAdditionalData,
 	RoutingNode,
-	Workflow,
 } from 'n8n-workflow';
 
-// eslint-disable-next-line import/no-cycle
 import { NodeExecuteFunctions } from '.';
+import { LoadNodeDetails } from './LoadNodeDetails';
 
 const TEMP_NODE_NAME = 'Temp-Node';
-const TEMP_WORKFLOW_NAME = 'Temp-Workflow';
 
-export class LoadNodeParameterOptions {
-	currentNodeParameters: INodeParameters;
-
-	path: string;
-
-	workflow: Workflow;
-
-	constructor(
-		nodeTypeNameAndVersion: INodeTypeNameVersion,
-		nodeTypes: INodeTypes,
-		path: string,
-		currentNodeParameters: INodeParameters,
-		credentials?: INodeCredentials,
-	) {
-		const nodeType = nodeTypes.getByNameAndVersion(
-			nodeTypeNameAndVersion.name,
-			nodeTypeNameAndVersion.version,
-		);
-		this.currentNodeParameters = currentNodeParameters;
-		this.path = path;
-		if (nodeType === undefined) {
-			throw new Error(
-				`The node-type "${nodeTypeNameAndVersion.name} v${nodeTypeNameAndVersion.version}"  is not known!`,
-			);
-		}
-
-		const nodeData: INode = {
-			parameters: currentNodeParameters,
-			id: 'uuid-1234',
-			name: TEMP_NODE_NAME,
-			type: nodeTypeNameAndVersion.name,
-			typeVersion: nodeTypeNameAndVersion.version,
-			position: [0, 0],
-		};
-		if (credentials) {
-			nodeData.credentials = credentials;
-		}
-
-		const workflowData = {
-			nodes: [nodeData],
-			connections: {},
-		};
-
-		this.workflow = new Workflow({
-			nodes: workflowData.nodes,
-			connections: workflowData.connections,
-			active: false,
-			nodeTypes,
-		});
-	}
-
-	/**
-	 * Returns data of a fake workflow
-	 *
-	 */
-	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-	getWorkflowData() {
-		return {
-			name: TEMP_WORKFLOW_NAME,
-			active: false,
-			connections: {},
-			nodes: Object.values(this.workflow.nodes),
-			createdAt: new Date(),
-			updatedAt: new Date(),
-		};
-	}
-
+export class LoadNodeParameterOptions extends LoadNodeDetails {
 	/**
 	 * Returns the available options via a predefined method
 	 *
@@ -106,29 +25,26 @@ export class LoadNodeParameterOptions {
 		methodName: string,
 		additionalData: IWorkflowExecuteAdditionalData,
 	): Promise<INodePropertyOptions[]> {
-		const node = this.workflow.getNode(TEMP_NODE_NAME);
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const node = this.workflow.getNode(TEMP_NODE_NAME)!;
 
-		const nodeType = this.workflow.nodeTypes.getByNameAndVersion(node!.type, node?.typeVersion);
+		const nodeType = this.workflow.nodeTypes.getByNameAndVersion(node.type, node.typeVersion);
+		const method = nodeType?.methods?.loadOptions?.[methodName];
 
-		if (
-			!nodeType ||
-			nodeType.methods === undefined ||
-			nodeType.methods.loadOptions === undefined ||
-			nodeType.methods.loadOptions[methodName] === undefined
-		) {
+		if (typeof method !== 'function') {
 			throw new Error(
-				`The node-type "${node!.type}" does not have the method "${methodName}" defined!`,
+				`The node-type "${node.type}" does not have the method "${methodName}" defined!`,
 			);
 		}
 
 		const thisArgs = NodeExecuteFunctions.getLoadOptionsFunctions(
 			this.workflow,
-			node!,
+			node,
 			this.path,
 			additionalData,
 		);
 
-		return nodeType.methods.loadOptions[methodName].call(thisArgs);
+		return method.call(thisArgs);
 	}
 
 	/**
