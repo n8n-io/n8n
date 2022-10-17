f7828c57b54fbf70e8517bfa018f2484e3a329a3
vcrwr <100850577+vcrwr@users.noreply.github.com>
2022-10-17 13:13:59 +0200

    feat(Hubspot Node): enable hubspot credentials for http predefined types (#3686)

--- packages/nodes-base/credentials/HubspotAppToken.credentials.ts
+++ packages/nodes-base/credentials/HubspotAppToken.credentials.ts
@@ -1,4 +1,4 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
+import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class HubspotAppToken implements ICredentialType {
 	name = 'hubspotAppToken';
@@ -12,4 +12,20 @@ export class HubspotAppToken implements ICredentialType {
 			default: '',
 		},
 	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '=Bearer {{$credentials.appToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.hubapi.com',
+			url: '/account-info/v3/details',
+		},
+	};
 }
