169e6831fd2a76f355cce22a59eba16bf8050b47
Iván Ovejero <ivov.src@gmail.com>
2022-10-18 13:47:32 +0200

    :sparkles: Create `SubworkflowOperationError`

--- packages/workflow/src/WorkflowErrors.ts
+++ packages/workflow/src/WorkflowErrors.ts
@@ -17,3 +17,20 @@ export class WorkflowOperationError extends Error {
 		this.timestamp = Date.now();
 	}
 }
+
+export class SubworkflowOperationError extends WorkflowOperationError {
+	description = '';
+
+	cause: { message: string; stack: string };
+
+	constructor(message: string, description: string) {
+		super(message);
+		this.name = this.constructor.name;
+		this.description = description;
+
+		this.cause = {
+			message,
+			stack: this.stack as string,
+		};
+	}
+}
