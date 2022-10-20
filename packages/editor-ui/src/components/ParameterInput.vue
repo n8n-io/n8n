990aae57bea541934c25947e2437b22849fa6527
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2022-10-20 15:45:58 +0200

    fix(editor): fix performance issues when opening node or editing code node with a lot of data (#4388)
    * debounce clicks
    
    * debounce correctly
    
    * debounce resize
    
    * if initialized avoid
    
    * set watcher fixes
    
    * add deboucne for setting values
    
    * increase debounce
    
    * reset workspace for memory issues
    
    * address comment
    
    * decrease debounce time
    
    * decrease debounce time
    
    * clean up
    
    * revert back to trailing
    
    * support dbl

--- packages/editor-ui/src/components/ParameterInput.vue
+++ packages/editor-ui/src/components/ParameterInput.vue
@@ -73,7 +73,7 @@
 					:mode="node.parameters.mode"
 					:jsCode="node.parameters.jsCode"
 					:isReadOnly="isReadOnly"
-					@valueChanged="valueChanged"
+					@valueChanged="valueChangedDebounced"
 				/>
 
 				<div v-else-if="isEditor === true" class="code-edit clickable ph-no-capture" @click="displayEditDialog()">
@@ -336,12 +336,14 @@ import { CUSTOM_API_CALL_KEY } from '@/constants';
 import { mapGetters } from 'vuex';
 import { CODE_NODE_TYPE } from '@/constants';
 import { PropType } from 'vue';
+import { debounceHelper } from './mixins/debounce';
 
 export default mixins(
 	externalHooks,
 	nodeHelpers,
 	showMessage,
 	workflowHelpers,
+	debounceHelper,
 )
 	.extend({
 		name: 'parameter-input',
@@ -921,6 +923,9 @@ export default mixins(
 				};
 
 				this.$emit('textInput', parameterData);
+			},
+			valueChangedDebounced (value: NodeParameterValueType | {} | Date) {
+				this.callDebounced('valueChanged', { debounceTime: 100 }, value);
 			},
 			valueChanged (value: NodeParameterValueType | {} | Date) {
 				if (this.parameter.name === 'nodeCredentialType') {
