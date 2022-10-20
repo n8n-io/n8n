122445a300a8b4e58ff711ccb1f7d6c426e3080a
Iván Ovejero <ivov.src@gmail.com>
2022-10-20 09:39:02 +0200

    :twisted_rightwards_arrows: Fix misresolved conflict

--- packages/workflow/src/utils.ts
+++ packages/workflow/src/utils.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import path from 'path';
+import { INodeType, Workflow } from '.';
+
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-argument */
 export const deepCopy = <T>(source: T): T => {
 	let clone: any;
@@ -30,3 +37,50 @@ export const deepCopy = <T>(source: T): T => {
 	return clone;
 };
 // eslint-enable
+
+// @TODO: De-duplicate, original in core
+
+type NodeModule = {
+	[className: string]: new () => {
+		methods: { [key: string]: { [key: string]: Function } };
+		webhookMethods: { [key: string]: { [key: string]: Function } };
+	};
+};
+
+function getVersionedNodeFilePath(sourcePath: string, version: number | number[]) {
+	if (Array.isArray(version)) return sourcePath;
+
+	const { dir, base } = path.parse(sourcePath);
+	const versionedNodeFilename = base.replace('.node.js', `V${version}.node.js`);
+
+	return path.resolve(dir, `v${version}`, versionedNodeFilename);
+}
+
+export function requireDistNode(nodeType: INodeType, workflow: Workflow) {
+	const sourcePath = workflow.nodeTypes.getSourcePath(nodeType.description.name);
+
+	const nodeFilePath =
+		nodeType.description.defaultVersion !== undefined
+			? getVersionedNodeFilePath(sourcePath, nodeType.description.version)
+			: sourcePath;
+
+	let _module;
+
+	try {
+		_module = require(nodeFilePath) as NodeModule;
+	} catch (_) {
+		throw new Error(`Failed to require node at ${sourcePath}`);
+	}
+
+	const _className = nodeFilePath.split('/').pop()?.split('.').shift();
+
+	if (!_className) {
+		throw new Error(`Failed to find class in node at ${nodeFilePath}`);
+	}
+
+	try {
+		return new _module[_className]();
+	} catch (error) {
+		throw new Error(`Failed to instantiate node at ${sourcePath}`);
+	}
+}
