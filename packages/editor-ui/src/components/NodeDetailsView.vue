356a42a18752eaf7b70ee2e0c41ef6f248cd10b8
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2022-10-20 15:45:58 +0200

    fix(editor): fix performance issues when opening node or editing code node with a lot of data (#4388)
    * debounce clicks
    
    * debounce correctly
    
    * debounce resize
    
    * if initialized avoid
    
    * set watcher fixes
    
    * add deboucne for setting values
    
    * increase debounce
    
    * reset workspace for memory issues
    
    * address comment
    
    * decrease debounce time
    
    * decrease debounce time
    
    * clean up
    
    * revert back to trailing
    
    * support dbl

--- packages/editor-ui/src/components/NodeDetailsView.vue
+++ packages/editor-ui/src/components/NodeDetailsView.vue
@@ -352,7 +352,9 @@ export default mixins(
 				this.avgOutputRowHeight = 0;
 				this.avgInputRowHeight = 0;
 
-				this.$store.commit('ui/setNDVSessionId');
+				setTimeout(() => {
+					this.$store.commit('ui/setNDVSessionId');
+				}, 0);
 				this.$externalHooks().run('dataDisplay.nodeTypeChanged', {
 					nodeSubtitle: this.getNodeSubtitle(node, this.activeNodeType, this.getCurrentWorkflow()),
 				});
@@ -397,10 +399,14 @@ export default mixins(
 			this.runInputIndex = -1;
 		},
 		inputNodeName(nodeName: string | undefined) {
-			this.$store.commit('ui/setInputNodeName', nodeName);
+			setTimeout(() => {
+				this.$store.commit('ui/setInputNodeName', nodeName);
+			}, 0);
 		},
 		inputRun() {
-			this.$store.commit('ui/setInputRunIndex', this.inputRun);
+			setTimeout(() => {
+				this.$store.commit('ui/setInputRunIndex', this.inputRun);
+			}, 0);
 		},
 	},
 	methods: {
