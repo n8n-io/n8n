28af82e75d7e491ce634185a586495b9c2ce9b8e
Iván Ovejero <ivov.src@gmail.com>
2022-10-24 12:16:37 +0200

    :zap: Consolidate utils tests

--- packages/workflow/test/utils.test.ts
+++ packages/workflow/test/utils.test.ts
@@ -1,19 +1,51 @@
-import { jsonParse } from '../src/utils';
+import { jsonParse, deepCopy } from '../src/utils';
 
-describe('utils', () => {
-	describe('jsonParse', () => {
-		it('optionally throws `errorMessage', () => {
-			expect(() => {
-				jsonParse('', { errorMessage: 'Invalid JSON' });
-			}).toThrow('Invalid JSON');
-		});
+describe('jsonParse', () => {
+	it('parses JSON', () => {
+		expect(jsonParse('[1, 2, 3]')).toEqual([1, 2, 3]);
+		expect(jsonParse('{ "a": 1 }')).toEqual({ a: 1 });
+	});
 
-		it('optionally returns a `fallbackValue`', () => {
-			expect(jsonParse('', { fallbackValue: { foo: 'bar' } })).toEqual({ foo: 'bar' });
-		});
+	it('optionally throws `errorMessage', () => {
+		expect(() => {
+			jsonParse('', { errorMessage: 'Invalid JSON' });
+		}).toThrow('Invalid JSON');
+	});
 
-		it('does not allow passing in both `errorMessage` as well as `fallbackValue`', () => {
-			// jsonParse('[1, 2, 3]', { errorMessage: 'This is an error', fallbackValue: '123' });
-		});
+	it('optionally returns a `fallbackValue`', () => {
+		expect(jsonParse('', { fallbackValue: { foo: 'bar' } })).toEqual({ foo: 'bar' });
+	});
+});
+
+describe('deepCopy', () => {
+	it('should deep copy an object', () => {
+		const object = {
+			deep: {
+				props: {
+					list: [{ a: 1 }, { b: 2 }, { c: 3 }],
+				},
+				arr: [1, 2, 3],
+			},
+			arr: [
+				{
+					prop: {
+						list: ['a', 'b', 'c'],
+					},
+				},
+			],
+			func: () => {},
+			date: new Date(),
+			undef: undefined,
+			nil: null,
+			bool: true,
+			num: 1,
+		};
+		const copy = deepCopy(object);
+		expect(copy).toEqual(object);
+		expect(copy).not.toBe(object);
+		expect(copy.arr).toEqual(object.arr);
+		expect(copy.arr).not.toBe(object.arr);
+		expect(copy.deep.props).toEqual(object.deep.props);
+		expect(copy.deep.props).not.toBe(object.deep.props);
 	});
 });
