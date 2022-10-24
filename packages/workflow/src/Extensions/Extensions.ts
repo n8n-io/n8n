f1446a05819eb65969374d4f2360bb2a1b6eac3c
Valya Bullions <valya@n8n.io>
2022-10-24 15:43:51 +0100

    refactor: remove commented out code

--- packages/workflow/src/Extensions/Extensions.ts
+++ packages/workflow/src/Extensions/Extensions.ts
@@ -1,30 +1,3 @@
-// import { Duration } from 'luxon';
-
-// export type ExtensionMethodHandler<K> = (
-// 	value: K,
-// 	args?: K | K[] | number[] | string[] | boolean[] | undefined,
-// ) => K | string | boolean | number | Date | Duration;
-
-// export abstract class BaseExtension<T> {
-// 	methodMapping: Map<string, ExtensionMethodHandler<T>>;
-
-// 	constructor() {
-// 		this.methodMapping = new Map<string, ExtensionMethodHandler<T>>();
-// 	}
-
-// 	hasMethod(methodName: string): boolean {
-// 		return this.methodMapping.has(methodName);
-// 	}
-
-// 	listMethods(): string[] {
-// 		return Array.from(this.methodMapping.keys());
-// 	}
-
-// 	methods() {
-// 		return Object.fromEntries(this.methodMapping.entries());
-// 	}
-// }
-
 export interface ExtensionMap {
 	typeName: string;
 	// eslint-disable-next-line @typescript-eslint/ban-types
