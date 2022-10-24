1ddcc418d5d53e4d409de7b854d7668539d4848f
Iván Ovejero <ivov.src@gmail.com>
2022-10-24 12:52:40 +0200

    :zap: Fix import with outdated name

--- packages/nodes-base/nodes/EmailReadImap/EmailReadImap.node.ts
+++ packages/nodes-base/nodes/EmailReadImap/EmailReadImap.node.ts
@@ -1,4 +1,4 @@
-import { INodeTypeBaseDescription, INodeVersionedType } from 'n8n-workflow';
+import { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow';
 import { NodeVersionedType } from '../../src/NodeVersionedType';
 import { EmailReadImapV1 } from './v1/EmailReadImapV1.node';
 import { EmailReadImapV2 } from './v2/EmailReadImapV2.node';
@@ -14,7 +14,7 @@ export class EmailReadImap extends NodeVersionedType {
 			defaultVersion: 2,
 		};
 
-		const nodeVersions: INodeVersionedType['nodeVersions'] = {
+		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
 			1: new EmailReadImapV1(baseDescription),
 			2: new EmailReadImapV2(baseDescription),
 		};
