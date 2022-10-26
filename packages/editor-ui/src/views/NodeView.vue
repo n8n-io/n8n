335abd7b76a95bff5626622c963b3bf5d560dad0
Iván Ovejero <ivov.src@gmail.com>
2022-10-26 09:40:17 +0200

    :zap: Set received hash

--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -718,6 +718,7 @@ export default mixins(
 				this.$store.commit('setWorkflowName', { newName: data.name, setStateDirty: false });
 				this.$store.commit('setWorkflowSettings', data.settings || {});
 				this.$store.commit('setWorkflowPinData', data.pinData || {});
+				this.$store.commit('setWorkflowHash', data.hash);
 
 				const tags = (data.tags || []) as ITag[];
 				this.$store.commit('tags/upsertTags', tags);
