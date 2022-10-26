ebf84ebe330ad20891d0ea670d7f6eed533e6260
Iván Ovejero <ivov.src@gmail.com>
2022-10-26 14:59:54 +0200

    feat(core, editor): introduce workflow caller policy (#4368)
    * :sparkles: Create env `N8N_WORKFLOW_CALLER_POLICY_DEFAULT_OPTION`
    
    * :shirt: Adjust BE settings interface
    
    * :shirt: Adjust FE settings interface
    
    * :zap: Send policy along with settings
    
    * :zap: Enforce policy
    
    * :sparkles: Create `SubworkflowOperationError`
    
    * :zap: Add policy to Vuex store
    
    * :zap: Add setting to FE
    
    * :zap: Trim caller IDs on BE
    
    * :zap: Hide new UI behind `isWorkflowSharingEnabled`
    
    * :pencil2: Copy updates
    
    * :shirt: Fix lint

--- packages/cli/src/WorkflowRunnerProcess.ts
+++ packages/cli/src/WorkflowRunnerProcess.ts
@@ -291,6 +291,7 @@ export class WorkflowRunnerProcess {
 				workflowData,
 				additionalData.userId,
 				options?.inputData,
+				options?.parentWorkflowId,
 			);
 			await sendToParentProcess('startExecution', { runData });
 			const executionId: string = await new Promise((resolve) => {
