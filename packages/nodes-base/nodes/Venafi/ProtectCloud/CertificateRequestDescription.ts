53bfb238a4980cc93a9f45342702a8c2981ce6f4
ricardo <ricardoespinoza105@gmail.com>
2022-10-28 18:14:46 -0400

    :zap: Make issuing template depending on application

--- packages/nodes-base/nodes/Venafi/ProtectCloud/CertificateRequestDescription.ts
+++ packages/nodes-base/nodes/Venafi/ProtectCloud/CertificateRequestDescription.ts
@@ -64,6 +64,9 @@ export const certificateRequestFields: INodeProperties[] = [
 			'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
 		typeOptions: {
 			loadOptionsMethod: 'getCertificateIssuingTemplates',
+			loadOptionsDependsOn: [
+				'applicationId',
+			],
 		},
 		displayOptions: {
 			show: {
