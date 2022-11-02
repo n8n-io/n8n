4cec044a3af3f409c524c0f33f2e6641c261d7a6
Michael Kret <michael.k@radency.com>
2022-11-02 20:02:18 +0200

    Revert ":zap: setup for trigger"
    This reverts commit 460ec79e22afd16f6bc2d8a1369f84e158915f91.

--- packages/nodes-base/nodes/Microsoft/Outlook/v2/transport/index.ts
+++ packages/nodes-base/nodes/Microsoft/Outlook/v2/transport/index.ts
@@ -1,19 +1,113 @@
-import { IExecuteFunctions, ILoadOptionsFunctions } from 'n8n-core';
-import { IDataObject, INodeExecutionData } from 'n8n-workflow';
+import { OptionsWithUri } from 'request';
+import { IExecuteFunctions, IExecuteSingleFunctions, ILoadOptionsFunctions } from 'n8n-core';
+import { IDataObject, INodeExecutionData, NodeApiError } from 'n8n-workflow';
 
-//declared in node root folder as also used by trigger node
-import { microsoftApiRequest, microsoftApiRequestAllItems } from '../../GenericFunctions';
-export { microsoftApiRequest, microsoftApiRequestAllItems };
+export async function microsoftApiRequest(
+	this: IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions,
+	method: string,
+	resource: string,
+	// tslint:disable-next-line:no-any
+	body: any = {},
+	qs: IDataObject = {},
+	uri?: string,
+	headers: IDataObject = {},
+	option: IDataObject = { json: true },
+	// tslint:disable-next-line:no-any
+): Promise<any> {
+	const credentials = await this.getCredentials('microsoftOutlookOAuth2Api');
+
+	let apiUrl = `https://graph.microsoft.com/v1.0/me${resource}`;
+	// If accessing shared mailbox
+	if (credentials.useShared && credentials.userPrincipalName) {
+		apiUrl = `https://graph.microsoft.com/v1.0/users/${credentials.userPrincipalName}${resource}`;
+	}
+
+	const options: OptionsWithUri = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method,
+		body,
+		qs,
+		uri: uri || apiUrl,
+	};
+	try {
+		Object.assign(options, option);
+
+		if (Object.keys(headers).length !== 0) {
+			options.headers = Object.assign({}, options.headers, headers);
+		}
+
+		if (Object.keys(body).length === 0) {
+			delete options.body;
+		}
+
+		return await this.helpers.requestWithAuthentication.call(
+			this,
+			'microsoftOutlookOAuth2Api',
+			options,
+		);
+	} catch (error) {
+		let message = (error.message as string) || '';
+		if (
+			message.toLowerCase().includes('bad request') ||
+			message.toLowerCase().includes('unknown error')
+		) {
+			message = error.description;
+		}
+
+		throw new NodeApiError(this.getNode(), error, {
+			message,
+			...error,
+		});
+	}
+}
+
+export async function microsoftApiRequestAllItems(
+	this: IExecuteFunctions | ILoadOptionsFunctions,
+	propertyName: string,
+	method: string,
+	endpoint: string,
+	// tslint:disable-next-line:no-any
+	body: any = {},
+	query: IDataObject = {},
+	headers: IDataObject = {},
+	// tslint:disable-next-line:no-any
+): Promise<any> {
+	const returnData: IDataObject[] = [];
+
+	let responseData;
+	let uri: string | undefined;
+	query['$top'] = 100;
+
+	do {
+		responseData = await microsoftApiRequest.call(
+			this,
+			method,
+			endpoint,
+			body,
+			query,
+			uri,
+			headers,
+		);
+		uri = responseData['@odata.nextLink'];
+		returnData.push.apply(returnData, responseData[propertyName]);
+	} while (responseData['@odata.nextLink'] !== undefined);
+
+	return returnData;
+}
 
 export async function microsoftApiRequestAllItemsSkip(
 	this: IExecuteFunctions | ILoadOptionsFunctions,
 	propertyName: string,
 	method: string,
 	endpoint: string,
-	body: IDataObject = {},
+	// tslint:disable-next-line:no-any
+	body: any = {},
 	query: IDataObject = {},
 	headers: IDataObject = {},
-) {
+	// tslint:disable-next-line:no-any
+): Promise<any> {
 	const returnData: IDataObject[] = [];
 
 	let responseData;
@@ -74,8 +168,8 @@ export async function downloadAttachments(
 				const data = Buffer.from(response.body as string, 'utf8');
 				element.binary![`${prefix}${index}`] = await this.helpers.prepareBinaryData(
 					data as unknown as Buffer,
-					attachment.name as string,
-					attachment.contentType as string,
+					attachment.name,
+					attachment.contentType,
 				);
 			}
 		}
