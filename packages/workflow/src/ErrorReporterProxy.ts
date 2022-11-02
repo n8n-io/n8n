51207f3cb2512dc0745b9b8d230d5b7b5310c38c
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-11-02 16:02:59 +0100

    check for NODE_ENV === 'production' instead

--- packages/workflow/src/ErrorReporterProxy.ts
+++ packages/workflow/src/ErrorReporterProxy.ts
@@ -12,17 +12,17 @@ class ErrorReporterProxy {
 	constructor(readonly reporter: IErrorReporter) {}
 }
 
-const isTesting = process.env.NODE_ENV === 'test';
+const isProduction = process.env.NODE_ENV === 'production';
 const consoleReporter: IErrorReporter = {
-	error: (error) => isTesting || console.error('ERROR', error.message, error.stack),
-	warn: (warning) => isTesting || console.warn('WARN', warning),
+	error: (error) => isProduction && console.error('ERROR', error.message, error.stack),
+	warn: (warning) => isProduction && console.warn('WARN', warning),
 };
 
 let instance: ErrorReporterProxy;
 export function getInstance(): IErrorReporter {
 	if (instance) return instance.reporter;
 
-	if (!isTesting) {
+	if (isProduction) {
 		console.warn('ErrorReporterProxy is not yet initialized');
 	}
 	return consoleReporter;
