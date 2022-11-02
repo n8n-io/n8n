d3e5d6470a7d6bc5a302fe98cc8c32f66b624e69
Csaba Tuncsik <csaba@n8n.io>
2022-11-02 22:36:10 +0100

    fix(editor): JSON view show string numbers and null properly

--- packages/editor-ui/src/components/RunDataJson.vue
+++ packages/editor-ui/src/components/RunDataJson.vue
@@ -62,7 +62,7 @@ import VueJsonPretty from 'vue-json-pretty';
 import { LOCAL_STORAGE_MAPPING_FLAG } from '@/constants';
 import { IDataObject, INodeExecutionData } from "n8n-workflow";
 import Draggable from '@/components/Draggable.vue';
-import { convertPath, executionDataToJson, isString, isStringNumber } from "@/components/helpers";
+import { convertPath, executionDataToJson, isString } from "@/components/helpers";
 import { INodeUi } from "@/Interface";
 import { shorten } from './helpers';
 import { externalHooks } from "@/components/mixins/externalHooks";
@@ -190,8 +190,8 @@ export default mixins(externalHooks).extend({
 				this.$telemetry.track('User dragged data for mapping', telemetryPayload);
 			}, 1000); // ensure dest data gets set if drop
 		},
-		getContent(value: string): string {
-			return isString(value) && !isStringNumber(value) ? `"${ value }"` : value;
+		getContent(value: unknown): string {
+			return isString(value) ? `"${ value }"` : JSON.stringify(value);
 		},
 	},
 });
