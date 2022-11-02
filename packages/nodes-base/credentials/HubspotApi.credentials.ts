04ffa2709509ca0c3ef33db9fc4a607adb103983
agobrech <ael.gobrecht@gmail.com>
2022-11-02 11:49:20 +0100

    ✨ add notice for credential depreciation

--- packages/nodes-base/credentials/HubspotApi.credentials.ts
+++ packages/nodes-base/credentials/HubspotApi.credentials.ts
@@ -6,6 +6,13 @@ export class HubspotApi implements ICredentialType {
 	documentationUrl = 'hubspot';
 	properties: INodeProperties[] = [
 		{
+			displayName:
+				'On 30 November, 2022 Hubspot will remove API key support. You will have to connect to HubSpot using a private app or Oauth authentication method. <a href="https://developers.hubspot.com/changelog/upcoming-api-key-sunset">More details</a>',
+			name: 'notice',
+			type: 'notice',
+			default: '',
+		},
+		{
 			displayName: 'API Key',
 			name: 'apiKey',
 			type: 'string',
