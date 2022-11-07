3e7f373523c8d747036242b7e3662e512045251a
Alex Grozav <alex@grozav.com>
2022-11-07 16:50:32 +0200

    fix: remove unused imports

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,5 @@
-const os = require("os");
-const { resolve } = require('path');
-const { copyFile } = require('fs/promises');
 const { defineConfig } = require("cypress");
 
-const userHomeDir = os.homedir();
-const n8nHomeFolder = resolve(userHomeDir, '.n8n');
-const n8nCypressDb = resolve(n8nHomeFolder, 'cypress.sqlite');
-const n8nCypressDbBackup = resolve(n8nHomeFolder, 'cypress.sqlite.bak');
 
 module.exports = defineConfig({
 	e2e: {
@@ -15,6 +8,5 @@ module.exports = defineConfig({
 		screenshotOnRunFailure: false,
 		experimentalSessionAndOrigin: true,
 		experimentalInteractiveRunEvents: true,
-		setupNodeEvents(on, config) {},
 	}
 });
--- cypress/e2e/0-smoke.cy.ts
+++ cypress/e2e/0-smoke.cy.ts
@@ -6,13 +6,11 @@ const password = DEFAULT_USER_PASSWORD;
 const firstName = randFirstName();
 const lastName = randLastName();
 
-describe('Sign up', () => {
+describe('Authentication flow', () => {
 	it('should sign user up', () => {
 		cy.signup(username, firstName, lastName, password);
 	});
-});
 
-describe('Sign in', () => {
 	it('should sign user in', () => {
 		cy.signin(username, password);
 	});
