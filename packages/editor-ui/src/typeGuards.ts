9c71722da9a717270160093c27323cb843820124
Milorad FIlipović <milorad@n8n.io>
2022-11-09 10:01:50 +0100

    refactor(editor): Finish pinia migration, remove all vuex dependancies (#4533)
    * ✨ Added pinia support. Migrated community nodes module.
    * ✨ Added ui pinia store, moved some data from root store to it, updated modals to work with pinia stores
    * ✨ Added ui pinia store and migrated a part of the root store
    * ✨ Migrated `settings` store to pinia
    * ✨ Removing vuex store refs from router
    * ✨ Migrated `users` module to pinia store
    * ⚡ Fixing errors after sync with master
    * ⚡ One more error after merge
    * ⚡ Created `workflows` pinia store. Moved large part of root store to it. Started updating references.
    * ✨ Finished migrating workflows store to pinia
    * ⚡ Renaming some getters and actions to make more sense
    * ✨ Finished migrating the root store to pinia
    * ✨ Migrated ndv store to pinia
    * ⚡ Renaming main panel dimensions getter so it doesn't clash with data prop name
    * ✔️ Fixing lint errors
    * ✨ Migrated `templates` store to pinia
    * ✨ Migrated the `nodeTypes`store
    * ⚡ Removed unused pieces of code and oold vuex modules
    * ✨ Adding vuex calls to pinia store, fixing wrong references
    * 💄 Removing leftover $store refs
    * ⚡ Added legacy getters and mutations to store to support webhooks
    * ⚡ Added missing front-end hooks, updated vuex state subscriptions to pinia
    * ✔️ Fixing linting errors
    * ⚡ Removing vue composition api plugin
    * ⚡ Fixing main sidebar state when loading node view
    * 🐛 Fixing an error when activating workflows
    * 🐛 Fixing isses with workflow settings and executions auto-refresh
    * 🐛 Removing duplicate listeners which cause import error
    * 🐛 Fixing route authentication
    * ⚡ Updating freshly pulled $store refs
    * ⚡ Adding deleted const
    * ⚡ Updating store references in ee features. Reseting NodeView credentials update flag when resetting workspace
    * ⚡ Adding return type to email submission modal
    * ⚡ Making NodeView only react to paste event when active
    * 🐛 Fixing signup view errors
    * ✨ Started migrating the `credentials` module to pinia
    * 👌 Addressing PR review comments
    * ✨ Migrated permissions module to pinia
    * ✨ Migrated `nodeCreator`, `tags` and `versions` modules to pinia
    * ✨ Implemented webhooks pinia store
    * ⚡ Removing all leftover vuex files and references
    * ✨ Removing final vuex refs
    * ⚡ Updating expected credentialId type
    * ⚡ Removing node credentials subscription code, reducing node click debounce timeout
    * 🐛 Fixing pushing nodes downstream when inserting new node
    * ✔️ Fixing a lint error in new type guard
    * ⚡ Updating helper reference
    * ✔️ Removing unnecessary awaits
    * ⚡ fix(editor): remove unnecessary imports from NDV
    * ⚡ Merging mapStores blocks in NodeView
    * ⚡ fix(editor): make sure JS Plumb not loaded earlier than needed
    * ⚡ Updating type guard nad credentials subscriptions
    * ⚡ Updating type guard so it doesn't use `any` type
    
    Co-authored-by: Csaba Tuncsik <csaba@n8n.io>

--- packages/editor-ui/src/typeGuards.ts
+++ packages/editor-ui/src/typeGuards.ts
@@ -1,4 +1,5 @@
 import { INodeParameterResourceLocator } from "n8n-workflow";
+import { ICredentialsResponse } from "./Interface";
 
 export function isResourceLocatorValue(value: unknown): value is INodeParameterResourceLocator {
 	return Boolean(typeof value === 'object' && value && 'mode' in value && 'value' in value);
@@ -6,4 +7,8 @@ export function isResourceLocatorValue(value: unknown): value is INodeParameterR
 
 export function isNotNull<T>(value: T | null): value is T {
 	return value !== null;
+}
+
+export function isValidCredentialResponse(value: unknown): value is ICredentialsResponse {
+	return typeof value === 'object' && value !== null && 'id' in value && 'createdAt' in value && 'updatedAt' in value;
 }
