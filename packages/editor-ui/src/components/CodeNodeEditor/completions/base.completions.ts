5e5c63984df1911c812e655aab9b6f06219a69ad
Iván Ovejero <ivov.src@gmail.com>
2022-11-09 11:28:32 +0100

    :zap: Add completions for legacy vars

--- packages/editor-ui/src/components/CodeNodeEditor/completions/base.completions.ts
+++ packages/editor-ui/src/components/CodeNodeEditor/completions/base.completions.ts
@@ -20,6 +20,25 @@ export const baseCompletions = (Vue as CodeNodeEditorMixin).extend({
 		),
 	},
 	methods: {
+		itemCompletions(context: CompletionContext): CompletionResult | null {
+			const preCursor = context.matchBefore(/i/);
+
+			if (!preCursor || (preCursor.from === preCursor.to && !context.explicit)) return null;
+
+			const options: Completion[] = [];
+
+			if (this.mode === 'runOnceForEachItem') {
+				options.push({ label: 'item', info: 'description....' });
+			} else if (this.mode === 'runOnceForAllItems') {
+				options.push({ label: 'items', info: 'description....' });
+			}
+
+			return {
+				from: preCursor.from,
+				options,
+			};
+		},
+
 		/**
 		 * - Complete `$` to `$execution $input $prevNode $runIndex $workflow $now $today
 		 * $jmespath $('nodeName')` in both modes.
