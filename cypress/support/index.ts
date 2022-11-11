50f7538779638574a4a7fe57a793ff04bfc641e9
OlegIvaniv <me@olegivaniv.com>
2022-11-11 09:07:14 +0100

    refactor(editor): Add Workflows view e2e tests (#4573)

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -5,6 +5,8 @@ declare global {
 	namespace Cypress {
 		interface Chainable {
 			getByTestId(selector: string, ...args: (Partial<Loggable & Timeoutable & Withinable & Shadow> | undefined)[]): Chainable<JQuery<HTMLElement>>
+			findChildByTestId(childTestId: string): Chainable<JQuery<HTMLElement>>
+			createFixtureWorkflow(fixtureKey: string, workflowName: string): void;
 			signin(email: string, password: string): void;
 			signup(email: string,  firstName: string, lastName: string, password: string): void;
 		}
