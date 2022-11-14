3d7c0f9689bd87b59531641109db054b019a2163
Iván Ovejero <ivov.src@gmail.com>
2022-11-14 10:19:20 +0100

    :zap: Wrap in try-catch

--- packages/editor-ui/src/components/CodeNodeEditor/linter.ts
+++ packages/editor-ui/src/components/CodeNodeEditor/linter.ts
@@ -30,10 +30,14 @@ export const linterExtension = (Vue as CodeNodeEditorMixin).extend({
 			} catch (syntaxError) {
 				let line;
 
-				const lineAtError = editorView.state.doc.line(syntaxError.lineNumber).text;
+				try {
+					const lineAtError = editorView.state.doc.line(syntaxError.lineNumber - 1).text;
 
-				// optional chaining operators currently unsupported by esprima-next
-				if (['?.', ']?'].some(operator => lineAtError.includes(operator))) return [];
+					// optional chaining operators currently unsupported by esprima-next
+					if (['?.', ']?'].some((operator) => lineAtError.includes(operator))) return [];
+				} catch (_) {
+					return [];
+				}
 
 				try {
 					line = editorView.state.doc.line(syntaxError.lineNumber);
@@ -46,7 +50,7 @@ export const linterExtension = (Vue as CodeNodeEditorMixin).extend({
 							message: this.$locale.baseText('codeNodeEditor.linter.bothModes.syntaxError'),
 						},
 					];
-				} catch (error) {
+				} catch (_) {
 					/**
 					 * For invalid (e.g. half-written) n8n syntax, esprima errors with an off-by-one line number for the final line. In future, we should add full linting for n8n syntax before parsing JS.
 					 */
