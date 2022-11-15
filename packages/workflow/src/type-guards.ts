e002c463c37730b270ca7794141d4c9ff6691d0b
Milorad Filipovic <milorad@n8n.io>
2022-11-15 16:45:04 +0100

    🐛 Adding a type guard to validate resource locator parameter values

--- packages/workflow/src/type-guards.ts
+++ packages/workflow/src/type-guards.ts
@@ -1,4 +1,4 @@
-import { INodeProperties, INodePropertyOptions, INodePropertyCollection } from './Interfaces';
+import { INodeProperties, INodePropertyOptions, INodePropertyCollection, INodeParameterResourceLocator } from './Interfaces';
 
 export const isINodeProperties = (
 	item: INodePropertyOptions | INodeProperties | INodePropertyCollection,
@@ -24,4 +24,17 @@ export const isINodePropertyCollectionList = (
 	items: INodeProperties['options'],
 ): items is INodePropertyCollection[] => {
 	return Array.isArray(items) && items.every(isINodePropertyCollection);
+};
+
+export const isValidResourceLocatorParameterValue = (
+	value: INodeParameterResourceLocator,
+): boolean => {
+	if (typeof value === 'object') {
+		if (typeof value.value === 'number') {
+			return true; // Accept all numbers
+		}
+		return !!value.value;
+	} else {
+		return !!value;
+	}
 };
