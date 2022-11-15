898c25fd7ef8a9eb983e3e6c66093510667ba010
Alex Grozav <alex@grozav.com>
2022-11-15 14:25:04 +0200

    feat: Add workflow sharing functionality and permissions (#4370)
    * feat(editor): extract credentials view into reusable layout components for workflows view
    
    * feat(editor): add workflow card and start work on empty state
    
    * feat: add hoverable card and finish workflows empty state
    
    * fix: undo workflows response interface changes
    
    * chore: fix linting issues.
    
    * fix: remove enterprise sharing env schema
    
    * fix(editor): fix workflows resource view when sharing is enabled
    
    * fix: change owner tag design and order
    
    * feat: add personalization survey on workflows page
    
    * fix: update component snapshots
    
    * feat: refactored workflow card to use workflow-activator properly
    
    * fix: fix workflow activator and proptypes
    
    * fix: hide owner tag for workflow card until sharing is available
    
    * fix: fixed ownedBy and sharedWith appearing for workflows list
    
    * feat: update tags component design
    
    * refactor: change resource filter select to n8n-user-select
    
    * fix: made telemetry messages reusable
    
    * chore: remove unused import
    
    * refactor: fix component name casing
    
    * refactor: use Vue.set to make workflow property reactive
    
    * feat: add support for clicking on tags for filtering
    
    * chore: fix tags linting issues
    
    * fix: fix resources list layout when title words are very long
    
    * refactor: add active and inactive status text to workflow activator
    
    * fix: fix credentials and workflows sorting when name contains leading whitespace
    
    * fix: remove wrongfully added style tag
    
    * feat: add translations and storybook examples for truncated tags
    
    * fix: remove enterprise sharing env from schema
    
    * refactor: fix workflows module and workflows field store naming conflict
    
    * feat: add workflow share button and open dummy modal
    
    * feat: add workflow sharing modal (in progress)
    
    * feat: add message when sharing disabled
    
    * feat: add sharing messages based on flags
    
    * feat: add workflow sharing api integration and readonly state handling
    
    * fix: change how foreign credentials are handled
    
    * refactor: migrate newly added workflow sharing store methods to pinia
    
    * fix: update foreign credentials handler and add executable prop to node-settings
    
    * fix: fix credentials display issue caused by addCredentials override
    
    * fix: fix various issues when sharing from empty state
    
    * fix: update node duplication credentials
    
    * fix: revert defautl values for sharing env
    
    * feat: hide share button behind feature flag
    
    * chore: add env variable for sharing feature (testing only)
    
    * fix: change enterprise-edition component casing

---
+++ packages/editor-ui/src/api/workflows.ee.ts
@@ -0,0 +1,13 @@
+import {
+	IRestApiContext,
+	IShareWorkflowsPayload,
+	IWorkflowsShareResponse,
+} from '@/Interface';
+import { makeRestApiRequest } from './helpers';
+import {
+	IDataObject,
+} from 'n8n-workflow';
+
+export async function setWorkflowSharedWith(context: IRestApiContext, id: string, data: IShareWorkflowsPayload): Promise<IWorkflowsShareResponse> {
+	return makeRestApiRequest(context, 'PUT', `/workflows/${id}/share`, data as unknown as IDataObject);
+}
--- packages/editor-ui/src/components/MainHeader/WorkflowDetails.vue
+++ packages/editor-ui/src/components/MainHeader/WorkflowDetails.vue
@@ -67,6 +67,15 @@
 				<span class="activator">
 					<WorkflowActivator :workflow-active="isWorkflowActive" :workflow-id="currentWorkflowId" />
 				</span>
+				<enterprise-edition :features="[EnterpriseEditionFeature.WorkflowSharing]">
+					<n8n-button
+						type="tertiary"
+						class="mr-2xs"
+						@click="onShareButtonClick"
+					>
+						{{ $locale.baseText('workflowDetails.share') }}
+					</n8n-button>
+				</enterprise-edition>
 				<SaveButton
 					type="secondary"
 					:saved="!this.isDirty && !this.isNewWorkflow"
@@ -87,10 +96,12 @@ import Vue from "vue";
 import mixins from "vue-typed-mixins";
 import {
 	DUPLICATE_MODAL_KEY,
+	EnterpriseEditionFeature,
 	MAX_WORKFLOW_NAME_LENGTH,
 	PLACEHOLDER_EMPTY_WORKFLOW_ID,
 	VIEWS, WORKFLOW_MENU_ACTIONS,
 	WORKFLOW_SETTINGS_MODAL_KEY,
+	WORKFLOW_SHARE_MODAL_KEY,
 } from "@/constants";
 
 import ShortenName from "@/components/ShortenName.vue";
@@ -143,6 +154,7 @@ export default mixins(workflowHelpers, titleChange).extend({
 			tagsEditBus: new Vue(),
 			MAX_WORKFLOW_NAME_LENGTH,
 			tagsSaving: false,
+			EnterpriseEditionFeature,
 		};
 	},
 	computed: {
@@ -227,6 +239,9 @@ export default mixins(workflowHelpers, titleChange).extend({
 			}
 			const saved = await this.saveCurrentWorkflow({ id: currentId, name: this.workflowName, tags: this.currentWorkflowTagIds });
 			if (saved) await this.settingsStore.fetchPromptsData();
+		},
+		onShareButtonClick() {
+			this.uiStore.openModal(WORKFLOW_SHARE_MODAL_KEY);
 		},
 		onTagsEditEnable() {
 			this.$data.appliedTagIds = this.currentWorkflowTagIds;
