dff1faaf4f095f83b147ede798792225d6db0b54
Omar Ajoue <krynble@gmail.com>
2022-11-16 14:13:36 +0100

    fix: Add missing content type to controllers (no-changelog) (#4617)
    * fix: add missing content type to controllers
    
    * refactor: use ResponseHelper instead of fixing content type
    
    * fix: remove res.send calls

--- packages/cli/src/workflows/workflows.controller.ee.ts
+++ packages/cli/src/workflows/workflows.controller.ee.ts
@@ -37,40 +37,44 @@ EEWorkflowController.use((req, res, next) => {
  * Grant or remove users' access to a workflow.
  */
 
-EEWorkflowController.put('/:workflowId/share', async (req: WorkflowRequest.Share, res) => {
-	const { workflowId } = req.params;
-	const { shareWithIds } = req.body;
+EEWorkflowController.put(
+	'/:workflowId/share',
+	ResponseHelper.send(async (req: WorkflowRequest.Share) => {
+		const { workflowId } = req.params;
+		const { shareWithIds } = req.body;
 
-	if (!Array.isArray(shareWithIds) || !shareWithIds.every((userId) => typeof userId === 'string')) {
-		return res.status(400).send('Bad Request');
-	}
-
-	const { ownsWorkflow, workflow } = await EEWorkflows.isOwned(req.user, workflowId);
-
-	if (!ownsWorkflow || !workflow) {
-		return res.status(403).send();
-	}
-
-	let newShareeIds: string[] = [];
-	await Db.transaction(async (trx) => {
-		// remove all sharings that are not supposed to exist anymore
-		await EEWorkflows.pruneSharings(trx, workflowId, [req.user.id, ...shareWithIds]);
-
-		const sharings = await EEWorkflows.getSharings(trx, workflowId);
-
-		// extract the new sharings that need to be added
-		newShareeIds = rightDiff(
-			[sharings, (sharing) => sharing.userId],
-			[shareWithIds, (shareeId) => shareeId],
-		);
-
-		if (newShareeIds.length) {
-			await EEWorkflows.share(trx, workflow, newShareeIds);
+		if (
+			!Array.isArray(shareWithIds) ||
+			!shareWithIds.every((userId) => typeof userId === 'string')
+		) {
+			throw new ResponseHelper.ResponseError('Bad request', undefined, 400);
 		}
-	});
 
-	return res.status(200).send();
-});
+		const { ownsWorkflow, workflow } = await EEWorkflows.isOwned(req.user, workflowId);
+
+		if (!ownsWorkflow || !workflow) {
+			throw new ResponseHelper.ResponseError('Forbidden', undefined, 403);
+		}
+
+		let newShareeIds: string[] = [];
+		await Db.transaction(async (trx) => {
+			// remove all sharings that are not supposed to exist anymore
+			await EEWorkflows.pruneSharings(trx, workflowId, [req.user.id, ...shareWithIds]);
+
+			const sharings = await EEWorkflows.getSharings(trx, workflowId);
+
+			// extract the new sharings that need to be added
+			newShareeIds = rightDiff(
+				[sharings, (sharing) => sharing.userId],
+				[shareWithIds, (shareeId) => shareeId],
+			);
+
+			if (newShareeIds.length) {
+				await EEWorkflows.share(trx, workflow, newShareeIds);
+			}
+		});
+	}),
+);
 
 EEWorkflowController.get(
 	'/:id(\\d+)',
