63f172d64ebd22addc8e7f7605d958a2eadde2d6
Alex Grozav <alex@grozav.com>
2022-11-22 11:37:26 +0200

    feat: Add credentials E2E test suite and page object (#4596)
    * fix: Fix inferred type of X cannot be named error after pnpm update
    
    * feat: Change page objects to expose actions and getters. Add credential creation suite

--- cypress/pages/workflows.ts
+++ cypress/pages/workflows.ts
@@ -2,7 +2,7 @@ import { BasePage } from "./base";
 
 export class WorkflowsPage extends BasePage {
 	url = '/workflows';
-	elements = {
+	getters = {
 		newWorkflowButtonCard: () => cy.getByTestId('new-workflow-card'),
 		newWorkflowTemplateCard: () => cy.getByTestId('new-workflow-template-card'),
 		searchBar: () => cy.getByTestId('resources-list-search'),
@@ -11,13 +11,13 @@ export class WorkflowsPage extends BasePage {
 		workflowCard: (workflowName: string) => cy.getByTestId(`workflow-card`)
 			.contains(workflowName)
 			.parents('[data-test-id="workflow-card"]'),
-		workflowTags: (workflowName: string) => this.elements.workflowCard(workflowName)
+		workflowTags: (workflowName: string) => this.getters.workflowCard(workflowName)
 			.findChildByTestId('workflow-card-tags'),
-		workflowActivator: (workflowName: string) => this.elements.workflowCard(workflowName)
+		workflowActivator: (workflowName: string) => this.getters.workflowCard(workflowName)
 			.findChildByTestId('workflow-card-activator'),
-		workflowActivatorStatus: (workflowName: string) => this.elements.workflowActivator(workflowName)
+		workflowActivatorStatus: (workflowName: string) => this.getters.workflowActivator(workflowName)
 			.findChildByTestId('workflow-activator-status'),
-		workflowCardActions: (workflowName: string) => this.elements.workflowCard(workflowName)
+		workflowCardActions: (workflowName: string) => this.getters.workflowCard(workflowName)
 			.findChildByTestId('workflow-card-actions'),
 		workflowDeleteButton: () => cy.getByTestId('action-toggle-dropdown').filter(':visible').contains('Delete')
 		// Not yet implemented
