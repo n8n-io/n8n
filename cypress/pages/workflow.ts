bf463fe8e2fb07bc389ad8f2b37cad86905474f0
Iván Ovejero <ivov.src@gmail.com>
2022-11-24 16:45:02 +0100

    :zap: Add getters

--- cypress/pages/workflow.ts
+++ cypress/pages/workflow.ts
@@ -1,11 +1,28 @@
-import { BasePage } from "./base";
+import { BasePage } from './base';
 
 export class WorkflowPage extends BasePage {
 	url = '/workflow/new';
 	getters = {
-		workflowNameInput: () => cy.getByTestId('workflow-name-input').then($el => cy.wrap($el.find('input'))),
+		workflowNameInput: () =>
+			cy.getByTestId('workflow-name-input').then(($el) => cy.wrap($el.find('input'))),
 		workflowImportInput: () => cy.getByTestId('workflow-import-input'),
 		workflowTags: () => cy.getByTestId('workflow-tags'),
 		saveButton: () => cy.getByTestId('save-button'),
+
+		nodeCreator: {
+			addNodeButton: () => cy.getByTestId('node-creator-add-node-button'),
+			searchBar: () => cy.getByTestId('node-creator-search-bar'),
+		},
+
+		canvas: {
+			addButton: () => cy.getByTestId('canvas-add-button'),
+			nodeBox: (nodeTypeName: string) => cy.getByTestId(`canvas-node-box-${nodeTypeName}`),
+		},
+
+		ndv: {
+			parameterInput: (parameterName: string) => cy.getByTestId(`parameter-input-${parameterName}`),
+			executeNodeButton: () => cy.contains('Execute node'),
+			outputPanel: () => cy.getByTestId('output-panel'),
+		},
 	};
 }
