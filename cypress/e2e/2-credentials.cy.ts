569ee856c06eccb2b9a926ad044a606bc6eb71a0
Alex Grozav <alex@grozav.com>
2022-11-24 16:48:51 +0200

    test(e2e): Update getters, add search and sort tests for credentials

--- cypress/e2e/2-credentials.cy.ts
+++ cypress/e2e/2-credentials.cy.ts
@@ -37,5 +37,48 @@ describe('Credentials', () => {
 
 		credentialsModal.actions.setName('My awesome Notion account');
 		credentialsModal.actions.save();
+		credentialsModal.actions.close();
+
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+	});
+
+	it('should create a new credential using Add Credential button', () => {
+		credentialsPage.getters.createCredentialButton().click();
+
+		credentialsModal.getters.newCredentialModal().should('be.visible');
+		credentialsModal.getters.newCredentialTypeSelect().should('be.visible');
+		credentialsModal.getters.newCredentialTypeOption('Airtable API').click();
+
+		credentialsModal.getters.newCredentialTypeButton().click();
+
+		credentialsModal.getters.connectionParameter('API Key').type('1234567890');
+
+		credentialsModal.actions.setName('Airtable Account');
+		credentialsModal.actions.save();
+		credentialsModal.actions.close();
+
+		credentialsPage.getters.credentialCards().should('have.length', 2);
+	});
+
+	it('should search credentials', () => {
+		// Search by name
+		credentialsPage.actions.search('Notion');
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+
+		// Search by Credential type
+		credentialsPage.actions.search('Airtable API');
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+
+		// No results
+		credentialsPage.actions.search('Google');
+		credentialsPage.getters.credentialCards().should('have.length', 0);
+		credentialsPage.getters.emptyList().should('be.visible');
+	});
+
+	it('should sort credentials', () => {
+		credentialsPage.actions.search('');
+		credentialsPage.actions.sortBy('nameDesc');
+		credentialsPage.getters.credentialCards().eq(0).should('contain.text', 'Notion');
+		credentialsPage.actions.sortBy('nameAsc');
 	});
 });
