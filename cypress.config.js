d08bf4b57de5ea35f823ce2350ca873b11b7f2ae
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2022-11-24 16:17:21 +0100

    ci: Setup cypress tasks for resetting DB, and setting up an owner

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,26 @@
+const fetch = require('node-fetch');
 const { defineConfig } = require('cypress');
+
+const baseUrl = 'http://localhost:5678';
 
 module.exports = defineConfig({
 	e2e: {
-		baseUrl: 'http://localhost:5678',
+		baseUrl,
 		video: false,
 		screenshotOnRunFailure: true,
 		experimentalSessionAndOrigin: true,
 		experimentalInteractiveRunEvents: true,
+
+		setupNodeEvents(on) {
+			on('task', {
+				'db:reset': () => fetch(baseUrl + '/e2e/db/reset', { method: 'POST' }),
+				'db:setup-owner': (payload) =>
+					fetch(baseUrl + '/e2e/db/setup-owner', {
+						method: 'POST',
+						body: JSON.stringify(payload),
+						headers: { 'Content-Type': 'application/json' },
+					}),
+			});
+		},
 	},
 });
