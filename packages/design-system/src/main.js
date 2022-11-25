aa71ed532ed5af4f5aa16e624ee756878119cca5
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2022-11-25 10:51:51 +0100

    refactor: Improve typings for element-ui imports (no-changelog) (#4678)
    * convert all stories to typescript
    
    * stricter typing for element-ui imports

--- packages/design-system/src/main.js
+++ packages/design-system/src/main.js
@@ -1,18 +1,4 @@
 import * as locale from './locale';
 import designSystemComponents from './plugins/n8nComponents';
-import elementUIComponents from './plugins/elementUIComponents';
 
-import Loading from 'element-ui/lib/loading';
-import MessageBox from 'element-ui/lib/message-box';
-import Message from 'element-ui/lib/message';
-import Notification from 'element-ui/lib/notification';
-
-export {
-	Loading,
-	MessageBox,
-	Message,
-	Notification,
-	locale,
-	designSystemComponents,
-	elementUIComponents,
-};
+export { locale, designSystemComponents };
