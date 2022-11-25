6139003580b0f7d15a9b26d87283a9442e12bfaf
Iván Ovejero <ivov.src@gmail.com>
2022-11-25 13:09:44 +0100

    test: Set up e2e tests for HTTP Request node (#4718)
    * :test_tube: Add test identifiers
    
    * :zap: Add getters
    
    * :test_tube: Add initial test
    
    * :zap: Refactor to use actions
    
    * :zap: Refactor per Oleg's advice
    
    * :bug: Fix overlapping input label IDs

--- packages/editor-ui/src/components/Node/NodeCreation.vue
+++ packages/editor-ui/src/components/Node/NodeCreation.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div v-if="!createNodeActive" :class="[$style.nodeButtonsWrapper, showStickyButton ? $style.noEvents : '']" @mouseenter="onCreateMenuHoverIn">
-			<div :class="$style.nodeCreatorButton">
+			<div :class="$style.nodeCreatorButton" data-test-id="node-creator-plus-button">
 				<n8n-icon-button size="xlarge" icon="plus" type="tertiary" :class="$style.nodeCreatorPlus" @click="openNodeCreator" :title="$locale.baseText('nodeView.addNode')"/>
 				<div :class="[$style.addStickyButton, showStickyButton ? $style.visibleButton : '']" @click="addStickyNote">
 					<n8n-icon-button size="medium" type="tertiary" :icon="['far', 'note-sticky']" :title="$locale.baseText('nodeView.addSticky')"/>
