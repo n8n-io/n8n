737604f0d2ed8047fe9a1d4d0b4a7d70bb2ac3b6
Alex Grozav <alex@grozav.com>
2022-11-28 12:11:39 +0200

    test: Update getters, add search and sort tests for credentials (no-changelog) (#4716)
    * test(e2e): Update getters, add search and sort tests for credentials
    
    * fix: Refactor sortOptions getter
    
    * fix: fix merge conflict
    
    * fix: removed double key
    
    * fix: Add db and session reset for every credentials suite run

--- cypress/e2e/2-credentials.cy.ts
+++ cypress/e2e/2-credentials.cy.ts
@@ -1,7 +1,6 @@
 import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD } from "../constants";
 import { randFirstName, randLastName } from "@ngneat/falso";
 import { CredentialsPage, CredentialsModal } from '../pages';
-// import { v4 as uuid } from 'uuid';
 
 const username = DEFAULT_USER_EMAIL;
 const password = DEFAULT_USER_PASSWORD;
@@ -11,6 +10,11 @@ const credentialsPage = new CredentialsPage();
 const credentialsModal = new CredentialsModal();
 
 describe('Credentials', () => {
+	before(() => {
+		cy.task('db:reset');
+		Cypress.session.clearAllSavedSessions();
+	});
+
 	beforeEach(() => {
 		cy.signup(username, firstName, lastName, password);
 
@@ -37,5 +41,48 @@ describe('Credentials', () => {
 
 		credentialsModal.actions.setName('My awesome Notion account');
 		credentialsModal.actions.save();
+		credentialsModal.actions.close();
+
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+	});
+
+	it('should create a new credential using Add Credential button', () => {
+		credentialsPage.getters.createCredentialButton().click();
+
+		credentialsModal.getters.newCredentialModal().should('be.visible');
+		credentialsModal.getters.newCredentialTypeSelect().should('be.visible');
+		credentialsModal.getters.newCredentialTypeOption('Airtable API').click();
+
+		credentialsModal.getters.newCredentialTypeButton().click();
+
+		credentialsModal.getters.connectionParameter('API Key').type('1234567890');
+
+		credentialsModal.actions.setName('Airtable Account');
+		credentialsModal.actions.save();
+		credentialsModal.actions.close();
+
+		credentialsPage.getters.credentialCards().should('have.length', 2);
+	});
+
+	it('should search credentials', () => {
+		// Search by name
+		credentialsPage.actions.search('Notion');
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+
+		// Search by Credential type
+		credentialsPage.actions.search('Airtable API');
+		credentialsPage.getters.credentialCards().should('have.length', 1);
+
+		// No results
+		credentialsPage.actions.search('Google');
+		credentialsPage.getters.credentialCards().should('have.length', 0);
+		credentialsPage.getters.emptyList().should('be.visible');
+	});
+
+	it('should sort credentials', () => {
+		credentialsPage.actions.search('');
+		credentialsPage.actions.sortBy('nameDesc');
+		credentialsPage.getters.credentialCards().eq(0).should('contain.text', 'Notion');
+		credentialsPage.actions.sortBy('nameAsc');
 	});
 });
