861f55db938ebc3ca0c47db39d52eea372003c3f
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2022-11-30 10:28:18 +0100

    fix: Lazy load nodes for credentials testing (#4760)

--- packages/workflow/test/Helpers.ts
+++ packages/workflow/test/Helpers.ts
@@ -673,12 +673,8 @@ class NodeTypesClass implements INodeTypes {
 		},
 	};
 
-	getAll(): INodeType[] {
-		return Object.values(this.nodeTypes).map((data) => NodeHelpers.getVersionedNodeType(data.type));
-	}
-
-	getByName(nodeType: string): INodeType | IVersionedNodeType | undefined {
-		return this.getByNameAndVersion(nodeType);
+	getByName(nodeType: string): INodeType | IVersionedNodeType {
+		return this.nodeTypes[nodeType].type;
 	}
 
 	getByNameAndVersion(nodeType: string, version?: number): INodeType {
