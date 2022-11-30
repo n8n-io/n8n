9fe23742f88fa5e739f57973a0381dbcb7e3fe21
Michael Kret <michael.k@radency.com>
2022-11-30 12:53:14 +0200

    :zap: append and concatenate aggregations

--- packages/nodes-base/nodes/ItemLists/summarize.operation.ts
+++ packages/nodes-base/nodes/ItemLists/summarize.operation.ts
@@ -27,6 +27,14 @@ export const description: INodeProperties[] = [
 						type: 'options',
 						options: [
 							{
+								name: 'Append',
+								value: 'append',
+							},
+							{
+								name: 'Concatenate',
+								value: 'concatenate',
+							},
+							{
 								name: 'Count',
 								value: 'count',
 							},
@@ -60,6 +68,68 @@ export const description: INodeProperties[] = [
 							'The name of an input field that you want to summarize. The field should contain numbers or dates.',
 						placeholder: 'e.g. cost',
 						hint: ' Enter the field name as text',
+					},
+					{
+						displayName: 'Include Empty Values',
+						name: 'includeEmpty',
+						type: 'boolean',
+						default: false,
+						displayOptions: {
+							show: {
+								aggregation: ['append'],
+							},
+						},
+					},
+					{
+						displayName: 'Separator',
+						name: 'separateBy',
+						type: 'options',
+						default: ',',
+						// eslint-disable-next-line n8n-nodes-base/node-param-options-type-unsorted-items
+						options: [
+							{
+								name: 'Comma',
+								value: ',',
+							},
+							{
+								name: 'Comma and Space',
+								value: ', ',
+							},
+							{
+								name: 'New Line',
+								value: '\n',
+							},
+							{
+								name: 'None',
+								value: '',
+							},
+							{
+								name: 'Space',
+								value: ' ',
+							},
+							{
+								name: 'Other',
+								value: 'other',
+							},
+						],
+						hint: 'What to insert between values',
+						displayOptions: {
+							show: {
+								aggregation: ['concatenate'],
+							},
+						},
+					},
+					{
+						displayName: 'Custom Separator',
+						name: 'customSeparator',
+						type: 'string',
+						default: '',
+						displayOptions: {
+							show: {
+								aggregation: ['concatenate'],
+								separateBy: ['other'],
+							},
+						},
 					},
 				],
 			},
@@ -100,66 +170,31 @@ export async function execute(
 		.map((field) => field.trim())
 		.filter((field) => field);
 
-	const fieldsToSummarize = this.getNodeParameter('fieldsToSummarize.values', 0, []) as Array<{
-		aggregation: string;
-		field: string;
-	}>;
+	const fieldsToSummarize = this.getNodeParameter(
+		'fieldsToSummarize.values',
+		0,
+		[],
+	) as Aggregations;
 
 	if (fieldsToSummarize.length === 0) {
 		throw new NodeOperationError(this.getNode(), 'Please select at least one field to summarize');
 	}
 
-	const summarizeField = (items: IDataObject[], field: string, aggregation: string) => {
-		if (aggregation === 'sum') {
-			return items.reduce((acc, item) => {
-				if (typeof item[field] === 'number') {
-					return acc + (item[field] as number);
-				} else {
-					throw new NodeOperationError(this.getNode(), `The field '${field}' is not a number`);
-				}
-			}, 0);
-		} else if (aggregation === 'count') {
-			return items.map((item) => item[field]).filter((item) => item).length;
-		} else if (aggregation === 'countUnique') {
-			return new Set(items.map((item) => item[field])).size;
-		} else if (aggregation === 'min') {
-			return Math.min(
-				...(items.map((item) => {
-					if (typeof item[field] === 'number') {
-						return item[field];
-					} else {
-						throw new NodeOperationError(this.getNode(), `The field '${field}' is not a number`);
-					}
-				}) as number[]),
-			);
-		} else if (aggregation === 'max') {
-			return Math.max(
-				...(items.map((item) => {
-					if (typeof item[field] === 'number') {
-						return item[field];
-					} else {
-						throw new NodeOperationError(this.getNode(), `The field '${field}' is not a number`);
-					}
-				}) as number[]),
-			);
-		}
-	};
+	checkAggregationsFieldType.call(this, newItems, fieldsToSummarize);
 
 	const getSummaries = (data: IDataObject[]) => {
-		type Aggregation = 'count' | 'countUnique' | 'max' | 'min' | 'sum';
-		const aggregationDisplayNames = {
+		const displayNames = {
+			append: 'List',
+			concatenate: 'Concatenation',
 			count: 'Count',
 			countUnique: 'Unique count',
-			max: 'Max',
-			min: 'Min',
+			max: 'Max value',
+			min: 'Min value',
 			sum: 'Sum',
 		};
-		return fieldsToSummarize.reduce((acc, { field, aggregation }) => {
-			acc[`${aggregationDisplayNames[aggregation as Aggregation]} of ${field}`] = summarizeField(
-				data,
-				field,
-				aggregation,
-			);
+		return fieldsToSummarize.reduce((acc, entry) => {
+			acc[`${displayNames[entry.aggregation as AggregationType]} of ${entry.field}`] =
+				summarizeField(data, entry);
 			return acc;
 		}, {} as IDataObject);
 	};
@@ -194,4 +229,80 @@ export async function execute(
 	const executionData = this.prepareOutputData(this.helpers.returnJsonArray(groupedDataAsObject));
 
 	return executionData;
+}
+
+//types
+type AggregationType = 'append' | 'concatenate' | 'count' | 'countUnique' | 'max' | 'min' | 'sum';
+type Aggregation = {
+	aggregation: AggregationType;
+	field: string;
+	includeEmpty?: boolean;
+	separateBy?: string;
+	customSeparator?: string;
+};
+type Aggregations = Aggregation[];
+
+//utils
+function checkAggregationsFieldType(
+	this: IExecuteFunctions,
+	items: IDataObject[],
+	aggregations: Aggregations,
+) {
+	const requireNumericField = ['sum', 'max', 'min'];
+
+	const numericFields = aggregations
+		.filter((entry) => requireNumericField.includes(entry.aggregation))
+		.map((entry) => entry.field);
+
+	for (const [index, item] of items.entries()) {
+		for (const field of numericFields) {
+			if (item[field] === undefined || typeof item[field] !== 'number') {
+				throw new NodeOperationError(
+					this.getNode(),
+					`The field '${field}' is not a number [item ${index}]`,
+				);
+			}
+		}
+	}
+}
+
+function summarizeField(items: IDataObject[], entry: Aggregation) {
+	const { aggregation, field } = entry;
+	if (aggregation === 'append') {
+		if (!entry.includeEmpty) {
+			items = items.filter((item) => item[field] || typeof item[field] === 'number');
+		}
+		return items.map((item) => item[field]);
+	} else if (aggregation === 'concatenate') {
+		const separateBy = entry.separateBy === 'other' ? entry.customSeparator : entry.separateBy;
+		return items
+			.map((item) => {
+				let value = item[field];
+				if (value !== null && typeof value === 'object') {
+					value = JSON.stringify(value);
+				}
+				return value;
+			})
+			.join(separateBy);
+	} else if (aggregation === 'sum') {
+		return items.reduce((acc, item) => {
+			return acc + (item[field] as number);
+		}, 0);
+	} else if (aggregation === 'count') {
+		return items.map((item) => item[field]).filter((item) => item).length;
+	} else if (aggregation === 'countUnique') {
+		return new Set(items.map((item) => item[field])).size;
+	} else if (aggregation === 'min') {
+		return Math.min(
+			...(items.map((item) => {
+				return item[field];
+			}) as number[]),
+		);
+	} else if (aggregation === 'max') {
+		return Math.max(
+			...(items.map((item) => {
+				return item[field];
+			}) as number[]),
+		);
+	}
 }
