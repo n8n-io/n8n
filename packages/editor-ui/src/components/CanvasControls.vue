5071ca339f31f7ff87a2fb0366f6500c61e9c475
OlegIvaniv <me@olegivaniv.com>
2022-12-01 18:12:25 +0100

    fix(editor): Remove unnecessary closing tag in CanvasControls (no-changelog) (#4792)
    fix(editor): Remove unnecessary closing tag in CanvasControls component (no-changelog)

--- packages/editor-ui/src/components/CanvasControls.vue
+++ packages/editor-ui/src/components/CanvasControls.vue
@@ -3,7 +3,6 @@
 		<n8n-icon-button @click="zoomToFit" type="tertiary" size="large" :title="$locale.baseText('nodeView.zoomToFit')"
 										 icon="expand"
 										 data-test-id="zoom-to-fit" />
-		/>
 		<n8n-icon-button @click="zoomIn" type="tertiary" size="large" :title="$locale.baseText('nodeView.zoomIn')"
 										 icon="search-plus" />
 		<n8n-icon-button @click="zoomOut" type="tertiary" size="large" :title="$locale.baseText('nodeView.zoomOut')"
