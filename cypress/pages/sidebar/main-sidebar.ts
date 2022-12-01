49ba8a0d3c61af808500f77bebe40399462e004f
Oleg Ivaniv <oleg@n8n.io>
2022-12-01 13:34:41 +0100

    Fix 3-default-owner spec

--- cypress/pages/sidebar/main-sidebar.ts
+++ cypress/pages/sidebar/main-sidebar.ts
@@ -2,14 +2,23 @@ import { BasePage } from "../base";
 
 export class MainSidebar extends BasePage {
 	getters = {
-		settings: () => cy.getByTestId('menu-item-settings', { timeout: 5000 }),
+		settings: () => cy.getByTestId('menu-item-settings'),
 		templates: () => cy.getByTestId('menu-item-templates'),
 		workflows: () => cy.getByTestId('menu-item-workflows'),
 		credentials: () => cy.getByTestId('menu-item-credentials'),
 		executions: () => cy.getByTestId('menu-item-executions'),
 	};
 	actions = {
-		goToSettings: () => this.getters.settings().click(),
-		goToCredentials: () => this.getters.credentials().click(),
+		goToSettings: () => {
+			// For some reason Cypress gets confused when clicking on the settings menu item
+			// and it pickups element that will get detached with the sidebar animation
+			// so we need to wait for it to happen first
+			cy.wait(500)
+			this.getters.settings().click();
+		},
+		goToCredentials: () => {
+			cy.wait(500)
+			this.getters.credentials().click()
+		},
 	};
 }
