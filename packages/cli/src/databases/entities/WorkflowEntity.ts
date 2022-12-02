350bf2ec9ad08ba3c987ab6afc89b3ce70b441cb
Omar Ajoue <krynble@gmail.com>
2022-12-02 09:10:39 +0100

    refactor: Remove unnecessary imports

--- packages/cli/src/databases/entities/WorkflowEntity.ts
+++ packages/cli/src/databases/entities/WorkflowEntity.ts
@@ -1,4 +1,3 @@
-import crypto from 'crypto';
 import { Length } from 'class-validator';
 
 import type {
@@ -11,9 +10,6 @@ import type {
 } from 'n8n-workflow';
 
 import {
-	AfterLoad,
-	AfterUpdate,
-	AfterInsert,
 	Column,
 	Entity,
 	Index,
@@ -29,7 +25,6 @@ import { SharedWorkflow } from './SharedWorkflow';
 import { objectRetriever, sqlite } from '../utils/transformers';
 import { AbstractEntity, jsonColumnType } from './AbstractEntity';
 import type { IWorkflowDb } from '@/Interfaces';
-import { alphabetizeKeys } from '@/utils';
 
 @Entity()
 export class WorkflowEntity extends AbstractEntity implements IWorkflowDb {
