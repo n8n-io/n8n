ac3a87c7880965721c7fef556480ebb0bef2cf20
Milorad Filipovic <milorad@n8n.io>
2022-12-02 15:54:26 +0100

    ✅ Adding workflow tags and copy/paste tests

--- cypress/e2e/7-workflow-actions.cy.ts
+++ cypress/e2e/7-workflow-actions.cy.ts
@@ -3,6 +3,8 @@ import { WorkflowPage as WorkflowPageClass } from '../pages/workflow';
 const NEW_WORKFLOW_NAME = 'Something else';
 const MANUAL_TRIGGER_NODE_NAME = 'Manual Trigger';
 const SCHEDULE_TRIGGER_NODE_NAME = 'Schedule Trigger';
+const CODE_NODE = 'Code'
+const TEST_WF_TAGS = ['Tag 1', 'Tag 2', 'Tag 3'];
 
 const WorkflowPage = new WorkflowPageClass();
 
@@ -52,6 +54,47 @@ describe('Workflow Actions', () => {
 		WorkflowPage.actions.renameWorkflow(NEW_WORKFLOW_NAME);
 		WorkflowPage.getters.isWorkflowSaved();
 		WorkflowPage.getters.workflowNameInputContainer().invoke('attr', 'title').should('eq', NEW_WORKFLOW_NAME);
+	});
+
+	it('should add tags', () => {
+		WorkflowPage.getters.newTagLink().click();
+		WorkflowPage.actions.addTags(TEST_WF_TAGS);
+		WorkflowPage.getters.isWorkflowSaved();
+		WorkflowPage.getters.workflowTagElements().should('have.length', TEST_WF_TAGS.length);
+	});
+
+	it('should add more tags', () => {
+		WorkflowPage.getters.newTagLink().click();
+		WorkflowPage.actions.addTags(TEST_WF_TAGS);
+		WorkflowPage.getters.workflowTagElements().first().click();
+		WorkflowPage.actions.addTags(['Another one']);
+		WorkflowPage.getters.workflowTagElements().should('have.length', TEST_WF_TAGS.length + 1);
+	});
+
+	it('should remove tags by clicking X in tag', () => {
+		WorkflowPage.getters.newTagLink().click();
+		WorkflowPage.actions.addTags(TEST_WF_TAGS);
+		WorkflowPage.getters.workflowTagElements().first().click();
+		WorkflowPage.getters.workflowTagsContainer().find('.el-tag__close').first().click();
+		cy.get('body').type('{enter}');
+		WorkflowPage.getters.workflowTagElements().should('have.length', TEST_WF_TAGS.length - 1);
+	});
+
+	it('should remove tags from dropdown', () => {
+		WorkflowPage.getters.newTagLink().click();
+		WorkflowPage.actions.addTags(TEST_WF_TAGS);
+		WorkflowPage.getters.workflowTagElements().first().click();
+		WorkflowPage.getters.workflowTagsDropdown().find('li').first().click();
+		cy.get('body').type('{enter}');
+		WorkflowPage.getters.workflowTagElements().should('have.length', TEST_WF_TAGS.length - 1);
+	});
+
+	it('should copy nodes', () => {
+		WorkflowPage.actions.addNodeToCanvas(SCHEDULE_TRIGGER_NODE_NAME);
+		WorkflowPage.actions.addNodeToCanvas(CODE_NODE);
+		cy.get('body').type('{meta}', { release: false }).type('a');
+		cy.get('body').type('{meta}', { release: false }).type('c');
+		WorkflowPage.getters.successToast().should('exist');
 	});
 
 });
