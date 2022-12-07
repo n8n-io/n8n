745dcca9f2d16c4c18f374a55daf82b0187b3512
Omar Ajoue <krynble@gmail.com>
2022-12-07 09:52:59 +0100

    refactor: Remove workflow statistics count due to error logs

--- packages/cli/src/events/index.ts
+++ packages/cli/src/events/index.ts
@@ -1,7 +1,10 @@
 import { eventEmitter } from 'n8n-core';
-import { nodeFetchedData, workflowExecutionCompleted } from './WorkflowStatistics';
+import { nodeFetchedData } from './WorkflowStatistics';
 
 // Check for undefined as during testing these functions end up undefined for some reason
-if (nodeFetchedData) eventEmitter.on(eventEmitter.types.nodeFetchedData, nodeFetchedData);
-if (workflowExecutionCompleted)
-	eventEmitter.on(eventEmitter.types.workflowExecutionCompleted, workflowExecutionCompleted);
+if (nodeFetchedData) {
+	eventEmitter.on(eventEmitter.types.nodeFetchedData, nodeFetchedData);
+}
+// TODO: Fix upsert issue and re-enable
+// if (workflowExecutionCompleted)
+// 	eventEmitter.on(eventEmitter.types.workflowExecutionCompleted, workflowExecutionCompleted);
