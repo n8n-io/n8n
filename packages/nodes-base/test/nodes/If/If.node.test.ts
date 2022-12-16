78315f65f3b843cda1c968f8e56ba5d7794764d6
Marcus <marcus@n8n.io>
2022-12-16 17:19:55 +0100

    🧪 If node test improvements

--- packages/nodes-base/test/nodes/If/If.node.test.ts
+++ packages/nodes-base/test/nodes/If/If.node.test.ts
@@ -1,11 +1,4 @@
-import {
-	createDeferredPromise,
-	IRun,
-	Workflow,
-	ILogger,
-	LoggerProxy,
-} from 'n8n-workflow';
-import { WorkflowExecute } from '../../../../core/dist';
+import { INodeType } from 'n8n-workflow';
 import * as Helpers from '../Helpers';
 import { WorkflowTestData } from '../types';
 import { readFileSync } from 'fs';
@@ -16,6 +9,7 @@ import { If } from '../../../nodes/If/If.node';
 import { NoOp } from '../../../nodes/NoOp/NoOp.node';
 import { Code } from '../../../nodes/Code/Code.node';
 
+import { executeWorkflow } from '../ExecuteWorkflow';
 
 describe('Node', () => {
 	describe('If', () => {
@@ -46,50 +40,17 @@ describe('Node', () => {
 			},
 		];
 
-		const fakeLogger = {
-			log: () => { },
-			debug: () => { },
-			verbose: () => { },
-			info: () => { },
-			warn: () => { },
-			error: () => { },
-		} as ILogger;
+		const nodes: INodeType[] = [new ManualTrigger(), new Code(), new Set(), new If(), new NoOp()];
+		const nodeTypes = Helpers.setup(nodes);
 
-		const executionMode = 'manual';
-		Helpers.NodeTypes().addNode('n8n-nodes-base.manualTrigger', new ManualTrigger());
-		Helpers.NodeTypes().addNode('n8n-nodes-base.code', new Code());
-		Helpers.NodeTypes().addNode('n8n-nodes-base.set', new Set());
-		Helpers.NodeTypes().addNode('n8n-nodes-base.if', new If());
-		Helpers.NodeTypes().addNode('n8n-nodes-base.noOp', new NoOp());
-
-		const nodeTypes = Helpers.NodeTypes();
-		LoggerProxy.init(fakeLogger);
 
 		for (const testData of tests) {
 			test(testData.description, async () => {
-				const workflowInstance = new Workflow({
-					id: 'test',
-					nodes: testData.input.workflowData.nodes,
-					connections: testData.input.workflowData.connections,
-					active: false,
+				const { executionData, result, nodeExecutionOrder } = await executeWorkflow(
+					testData,
 					nodeTypes,
-				});
-
-				const waitPromise = await createDeferredPromise<IRun>();
-				const nodeExecutionOrder: string[] = [];
-				const additionalData = Helpers.WorkflowExecuteAdditionalData(
-					waitPromise,
-					nodeExecutionOrder,
 				);
 
-				const workflowExecute = new WorkflowExecute(additionalData, executionMode);
-
-				const executionData = await workflowExecute.run(workflowInstance);
-				const result = await waitPromise.promise();
-
-				// Check if the data from WorkflowExecute is identical to data received
-				// by the webhooks
-				// Check if the output data of the nodes is correct
 				for (const nodeName of Object.keys(testData.output.nodeData)) {
 					if (result.data.resultData.runData[nodeName] === undefined) {
 						throw new Error(`Data for node "${nodeName}" is missing!`);
@@ -105,7 +66,6 @@ describe('Node', () => {
 					expect(resultData).toEqual(testData.output.nodeData[nodeName]);
 				}
 
-				// Check if other data has correct value
 				expect(result.finished).toEqual(true);
 			});
 		}
