ca3df9a1fae95ff6afcfb5c6d74dae2081390be9
Omar Ajoue <krynble@gmail.com>
2022-12-23 13:58:34 +0100

    perf: Improve workflows list performance (#5021)
    * spike: Improve workflow list performance
    
    * fix: Correcting override behavior
    
    * refactor: Remove unnecessary promise
    
    * remove duplicate code
    
    * remove the `async` that is breaking the listings page
    
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

--- packages/cli/src/workflows/workflows.controller.ts
+++ packages/cli/src/workflows/workflows.controller.ts
@@ -106,12 +106,7 @@ workflowsController.post(
 		await ExternalHooks().run('workflow.afterCreate', [savedWorkflow]);
 		void InternalHooksManager.getInstance().onWorkflowCreated(req.user.id, newWorkflow, false);
 
-		const { id, ...rest } = savedWorkflow;
-
-		return {
-			id: id.toString(),
-			...rest,
-		};
+		return WorkflowsService.entityToResponse(savedWorkflow);
 	}),
 );
 
@@ -121,7 +116,8 @@ workflowsController.post(
 workflowsController.get(
 	'/',
 	ResponseHelper.send(async (req: WorkflowRequest.GetAll) => {
-		return WorkflowsService.getMany(req.user, req.query.filter);
+		const workflows = await WorkflowsService.getMany(req.user, req.query.filter);
+		return workflows.map((workflow) => WorkflowsService.entityToResponse(workflow));
 	}),
 );
 
@@ -222,14 +218,7 @@ workflowsController.get(
 			);
 		}
 
-		const {
-			workflow: { id, ...rest },
-		} = shared;
-
-		return {
-			id: id.toString(),
-			...rest,
-		};
+		return WorkflowsService.entityToResponse(shared.workflow);
 	}),
 );
 
@@ -255,12 +244,7 @@ workflowsController.patch(
 			['owner'],
 		);
 
-		const { id, ...remainder } = updatedWorkflow;
-
-		return {
-			id: id.toString(),
-			...remainder,
-		};
+		return WorkflowsService.entityToResponse(updatedWorkflow);
 	}),
 );
 
