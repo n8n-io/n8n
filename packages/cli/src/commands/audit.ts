bd9b4db60b738a18e5cd5fce89ae3acebc5e1148
Iván Ovejero <ivov.src@gmail.com>
2023-01-04 17:20:04 +0100

    :zap: Add options to CLI command

--- packages/cli/src/commands/audit.ts
+++ packages/cli/src/commands/audit.ts
@@ -1,27 +1,96 @@
-import { BaseCommand } from './BaseCommand';
+import { LoggerProxy } from 'n8n-workflow';
+import { getLogger, Logger } from '@/Logger';
 import { audit } from '@/audit';
+import Command, { flags } from '@oclif/command';
+import { RISK_CATEGORIES } from '@/audit/constants';
+import { UserSettings } from 'n8n-core';
+import { CredentialTypes } from '@/CredentialTypes';
+import { NodeTypes } from '@/NodeTypes';
+import { LoadNodesAndCredentials } from '@/LoadNodesAndCredentials';
 import { InternalHooksManager } from '@/InternalHooksManager';
+import config from '@/config';
+import * as Db from '@/Db';
+import { inTest } from '@/constants';
+import type { Risk } from '@/audit/types';
 
-export class SecurityAuditCommand extends BaseCommand {
+export class SecurityAudit extends Command {
 	static description = 'Generate a security audit report for this n8n instance';
 
-	static examples = ['$ n8n audit'];
+	static examples = [
+		'$ n8n audit',
+		'$ n8n audit --categories=database,credentials',
+		'$ n8n audit --days-abandoned-workflow=10',
+	];
+
+	static flags = {
+		help: flags.help({ char: 'h' }),
+		categories: flags.string({
+			default: RISK_CATEGORIES.join(','),
+			description: 'Comma-separated list of categories to include in the report.',
+		}),
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		'days-abandoned-workflow': flags.integer({
+			default: config.getEnv('security.audit.daysAbandonedWorkflow'),
+			description: 'Number of days that must elapse for a workflow to be considered abandoned.',
+		}),
+	};
+
+	logger: Logger;
 
 	async run() {
-		this.initLogger();
+		await this.init();
 
-		await this.initInternalHooksManager();
+		const { flags: auditFlags } = this.parse(SecurityAudit);
 
-		const result = await audit();
+		const categories =
+			auditFlags.categories?.split(',').filter((i): i is Risk.Category => i !== '') ??
+			RISK_CATEGORIES;
+
+		if (auditFlags['days-abandoned-workflow']) {
+			config.set('security.audit.daysAbandonedWorkflow', auditFlags['days-abandoned-workflow']);
+		}
+
+		const result = await audit(categories);
 
 		process.stdout.write(JSON.stringify(result, null, 2));
 
 		void InternalHooksManager.getInstance().onAuditGeneratedViaCli();
 	}
 
+	async init() {
+		await Db.init();
+
+		this.initLogger();
+
+		await this.initInternalHooksManager();
+	}
+
+	initLogger() {
+		this.logger = getLogger();
+		LoggerProxy.init(this.logger);
+	}
+
+	async initInternalHooksManager(): Promise<void> {
+		const loadNodesAndCredentials = LoadNodesAndCredentials();
+		await loadNodesAndCredentials.init();
+
+		const nodeTypes = NodeTypes(loadNodesAndCredentials);
+		CredentialTypes(loadNodesAndCredentials);
+
+		const instanceId = await UserSettings.getInstanceId();
+		await InternalHooksManager.init(instanceId, nodeTypes);
+	}
+
 	async catch(error: Error) {
 		this.logger.error('Failed to generate security audit report');
 		this.logger.error(error.message);
+
 		this.exit(1);
+	}
+
+	async finally(): Promise<void> {
+		if (inTest) return;
+
+		this.exit();
 	}
 }
