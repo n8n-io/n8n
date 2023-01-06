n8n-io/n8n

commit 001fbbbdaafc9c4f6634deed63185414adc654c3

refactor(Read Binary File Node): Use node streams for to reduce memory usage (#5069)

packages/core/src/NodeExecuteFunctions.ts
@@ -91,6 +91,8 @@ import axios, {
 	Method,
 } from 'axios';
 import url, { URL, URLSearchParams } from 'url';
+import type { Readable } from 'stream';
+
 import { BinaryDataManager } from './BinaryDataManager';
 import type { IResponseError, IWorkflowSettings } from './Interfaces';
 import { extractValue } from './ExtractValue';
@@ -840,12 +842,12 @@ export async function getBinaryDataBuffer(
  *
  * @export
  * @param {IBinaryData} data
- * @param {Buffer} binaryData
+ * @param {Buffer | Readable} binaryData
  * @returns {Promise<IBinaryData>}
  */
 export async function setBinaryDataBuffer(
 	data: IBinaryData,
-	binaryData: Buffer,
+	binaryData: Buffer | Readable,
 	executionId: string,
 ): Promise<IBinaryData> {
 	return BinaryDataManager.getInstance().storeBinaryData(data, binaryData, executionId);
@@ -907,7 +909,7 @@ export async function copyBinaryFile(
  * base64 and adds metadata.
  */
 async function prepareBinaryData(
-	binaryData: Buffer,
+	binaryData: Buffer | Readable,
 	executionId: string,
 	filePath?: string,
 	mimeType?: string,
@@ -924,7 +926,8 @@ async function prepareBinaryData(
 			}
 		}
 
-		if (!mimeType) {
+		// TODO: detect filetype from streams
+		if (!mimeType && Buffer.isBuffer(binaryData)) {
 			// Use buffer to guess mime type
 			const fileTypeData = await FileType.fromBuffer(binaryData);
 			if (fileTypeData) {

packages/nodes-base/nodes/ReadBinaryFile/ReadBinaryFile.node.ts
@@ -6,7 +6,7 @@ import {
 	NodeOperationError,
 } from 'n8n-workflow';
 
-import { readFile as fsReadFile } from 'fs/promises';
+import { createReadStream } from 'fs';
 
 export class ReadBinaryFile implements INodeType {
 	description: INodeTypeDescription = {
@@ -58,7 +58,7 @@ export class ReadBinaryFile implements INodeType {
 
 				let data;
 				try {
-					data = await fsReadFile(filePath);
+					data = createReadStream(filePath);
 				} catch (error) {
 					if (error.code === 'ENOENT') {
 						throw new NodeOperationError(

packages/nodes-base/nodes/ReadBinaryFiles/ReadBinaryFiles.node.ts
@@ -1,8 +1,8 @@
 import { IExecuteFunctions } from 'n8n-core';
 import { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
 import glob from 'fast-glob';
-
-import { readFile as fsReadFile } from 'fs/promises';
+import { createReadStream } from 'fs';
+import type { Readable } from 'stream';
 
 export class ReadBinaryFiles implements INodeType {
 	description: INodeTypeDescription = {
@@ -47,9 +47,9 @@ export class ReadBinaryFiles implements INodeType {
 
 		const items: INodeExecutionData[] = [];
 		let item: INodeExecutionData;
-		let data: Buffer;
+		let data: Readable;
 		for (const filePath of files) {
-			data = await fsReadFile(filePath);
+			data = createReadStream(filePath);
 
 			item = {
 				binary: {

packages/workflow/src/Interfaces.ts
@@ -3,6 +3,7 @@
 import type * as express from 'express';
 import type * as FormData from 'form-data';
 import type { IncomingHttpHeaders } from 'http';
+import type { Readable } from 'stream';
 import type { URLSearchParams } from 'url';
 import type { OptionsWithUri, OptionsWithUrl } from 'request';
 import type { RequestPromiseOptions, RequestPromiseAPI } from 'request-promise-native';
@@ -633,7 +634,11 @@ export interface JsonHelperFunctions {
 }
 
 export interface BinaryHelperFunctions {
-	prepareBinaryData(binaryData: Buffer, filePath?: string, mimeType?: string): Promise<IBinaryData>;
+	prepareBinaryData(
+		binaryData: Buffer | Readable,
+		filePath?: string,
+		mimeType?: string,
+	): Promise<IBinaryData>;
 	setBinaryDataBuffer(data: IBinaryData, binaryData: Buffer): Promise<IBinaryData>;
 	copyBinaryFile(filePath: string, fileName: string, mimeType?: string): Promise<IBinaryData>;
 }

packages/core/src/Interfaces.ts
@@ -1,4 +1,5 @@
-import {
+import type { Readable } from 'stream';
+import type {
 	IPollResponse,
 	ITriggerResponse,
 	IWorkflowSettings as IWorkflowSettingsWorkflow,
@@ -67,7 +68,7 @@ export interface IBinaryDataManager {
 	copyBinaryFile(filePath: string, executionId: string): Promise<string>;
 	storeBinaryMetadata(identifier: string, metadata: BinaryMetadata): Promise<void>;
 	getBinaryMetadata(identifier: string): Promise<BinaryMetadata>;
-	storeBinaryData(binaryBuffer: Buffer, executionId: string): Promise<string>;
+	storeBinaryData(binaryData: Buffer | Readable, executionId: string): Promise<string>;
 	retrieveBinaryDataByIdentifier(identifier: string): Promise<Buffer>;
 	getBinaryPath(identifier: string): string;
 	markDataForDeletionByExecutionId(executionId: string): Promise<void>;

packages/core/src/BinaryDataManager/FileSystem.ts
@@ -2,6 +2,7 @@ import fs from 'fs/promises';
 import { jsonParse } from 'n8n-workflow';
 import path from 'path';
 import { v4 as uuid } from 'uuid';
+import type { Readable } from 'stream';
 
 import { BinaryMetadata, IBinaryDataConfig, IBinaryDataManager } from '../Interfaces';
 
@@ -66,10 +67,10 @@ export class BinaryDataFileSystem implements IBinaryDataManager {
 		return jsonParse(await fs.readFile(this.getMetadataPath(identifier), { encoding: 'utf-8' }));
 	}
 
-	async storeBinaryData(binaryBuffer: Buffer, executionId: string): Promise<string> {
+	async storeBinaryData(binaryData: Buffer | Readable, executionId: string): Promise<string> {
 		const binaryDataId = this.generateFileName(executionId);
 		await this.addBinaryIdToPersistMeta(executionId, binaryDataId);
-		await this.saveToLocalStorage(binaryBuffer, binaryDataId);
+		await this.saveToLocalStorage(binaryData, binaryDataId);
 		return binaryDataId;
 	}
 
@@ -234,8 +235,8 @@ export class BinaryDataFileSystem implements IBinaryDataManager {
 		await fs.cp(source, this.getBinaryPath(identifier));
 	}
 
-	private async saveToLocalStorage(data: Buffer, identifier: string) {
-		await fs.writeFile(this.getBinaryPath(identifier), data);
+	private async saveToLocalStorage(binaryData: Buffer | Readable, identifier: string) {
+		await fs.writeFile(this.getBinaryPath(identifier), binaryData);
 	}
 
 	private async retrieveFromLocalStorage(identifier: string): Promise<Buffer> {

packages/core/src/BinaryDataManager/index.ts
@@ -1,9 +1,11 @@
-import prettyBytes from 'pretty-bytes';
+import concatStream from 'concat-stream';
+import { readFile, stat } from 'fs/promises';
 import type { IBinaryData, INodeExecutionData } from 'n8n-workflow';
+import prettyBytes from 'pretty-bytes';
+import type { Readable } from 'stream';
 import { BINARY_ENCODING } from '../Constants';
 import type { BinaryMetadata, IBinaryDataConfig, IBinaryDataManager } from '../Interfaces';
 import { BinaryDataFileSystem } from './FileSystem';
-import { readFile, stat } from 'fs/promises';
 
 export class BinaryDataManager {
 	static instance: BinaryDataManager | undefined;
@@ -79,29 +81,35 @@ export class BinaryDataManager {
 
 	async storeBinaryData(
 		binaryData: IBinaryData,
-		binaryBuffer: Buffer,
+		input: Buffer | Readable,
 		executionId: string,
 	): Promise<IBinaryData> {
-		binaryData.fileSize = prettyBytes(binaryBuffer.length);
-
 		// If a manager handles this binary, return the binary data with its reference id.
 		const manager = this.managers[this.binaryDataMode];
 		if (manager) {
-			const identifier = await manager.storeBinaryData(binaryBuffer, executionId);
+			const identifier = await manager.storeBinaryData(input, executionId);
 			// Add data manager reference id.
 			binaryData.id = this.generateBinaryId(identifier);
 
 			// Prevent preserving data in memory if handled by a data manager.
 			binaryData.data = this.binaryDataMode;
 
+			const fileSize = await manager.getFileSize(identifier);
+			binaryData.fileSize = prettyBytes(fileSize);
+
 			await manager.storeBinaryMetadata(identifier, {
 				fileName: binaryData.fileName,
 				mimeType: binaryData.mimeType,
-				fileSize: binaryBuffer.length,
+				fileSize,
 			});
 		} else {
 			// Else fallback to storing this data in memory.
-			binaryData.data = binaryBuffer.toString(BINARY_ENCODING);
+			const buffer = await new Promise<Buffer>((resolve) => {
+				if (Buffer.isBuffer(input)) resolve(input);
+				else input.pipe(concatStream(resolve));
+			});
+			binaryData.data = buffer.toString(BINARY_ENCODING);
+			binaryData.fileSize = prettyBytes(buffer.length);
 		}
 
 		return binaryData;

pnpm-lock.yaml
@@ -331,6 +331,7 @@ importers:
 
   packages/core:
     specifiers:
+      '@types/concat-stream': ^2.0.0
       '@types/cron': ~1.7.1
       '@types/crypto-js': ^4.0.1
       '@types/express': ^4.17.6
@@ -340,6 +341,7 @@ importers:
       '@types/uuid': ^8.3.2
       axios: ^0.21.1
       client-oauth2: ^4.2.5
+      concat-stream: ^2.0.0
       cron: ~1.7.2
       crypto-js: ~4.1.1
       fast-glob: ^3.2.5
@@ -359,6 +361,7 @@ importers:
     dependencies:
       axios: 0.21.4
       client-oauth2: 4.3.3
+      concat-stream: 2.0.0
       cron: 1.7.2
       crypto-js: 4.1.1
       fast-glob: 3.2.12
@@ -376,6 +379,7 @@ importers:
       request-promise-native: 1.0.9_request@2.88.2
       uuid: 8.3.2
     devDependencies:
+      '@types/concat-stream': 2.0.0
       '@types/cron': 1.7.3
       '@types/crypto-js': 4.1.1
       '@types/express': 4.17.14
@@ -5552,6 +5556,12 @@ packages:
     resolution: {integrity: sha512-GuoIYzD70h+4JUqUabsm31FGqvpCYHGKcLtor7nQ/YvUyNX0o9SJZ9boFI5HjFfbOda5Oe/XOvNK6FES8Y/79w==}
     dependencies:
       '@types/express': 4.17.14
+    dev: true
+
+  /@types/concat-stream/2.0.0:
+    resolution: {integrity: sha512-t3YCerNM7NTVjLuICZo5gYAXYoDvpuuTceCcFQWcDQz26kxUR5uIWolxbIR5jRNIXpMqhOpW/b8imCR1LEmuJw==}
+    dependencies:
+      '@types/node': 16.11.65
     dev: true
 
   /@types/connect-history-api-fallback/1.3.5:

commit 7260614c315498f16ce6e10cec7bb5ec41a0342a

fix(Read Binary File Node): Do not crash the execution when the source file does not exist
This issue was introduced in https://github.com/n8n-io/n8n/pull/5069

packages/core/src/NodeExecuteFunctions.ts
@@ -67,6 +67,7 @@ import {
 	ITriggerFunctions,
 	IWebhookFunctions,
 	BinaryMetadata,
+	FileSystemHelperFunctions,
 } from 'n8n-workflow';
 
 import { Agent } from 'https';
@@ -93,6 +94,8 @@ import axios, {
 } from 'axios';
 import url, { URL, URLSearchParams } from 'url';
 import type { Readable } from 'stream';
+import { access as fsAccess } from 'fs/promises';
+import { createReadStream } from 'fs';
 
 import { BinaryDataManager } from './BinaryDataManager';
 import type { IResponseError, IWorkflowSettings } from './Interfaces';
@@ -1997,6 +2000,21 @@ const getRequestHelperFunctions = (
 	},
 });
 
+const getFileSystemHelperFunctions = (node: INode): FileSystemHelperFunctions => ({
+	async createReadStream(filePath) {
+		try {
+			await fsAccess(filePath);
+		} catch (error) {
+			throw error.code === 'ENOENT'
+				? new NodeOperationError(node, error, {
+						message: `The file "${String(filePath)}" could not be accessed.`,
+				  })
+				: error;
+		}
+		return createReadStream(filePath);
+	},
+});
+
 const getBinaryHelperFunctions = ({
 	executionId,
 }: IWorkflowExecuteAdditionalData): BinaryHelperFunctions => ({
@@ -2292,6 +2310,7 @@ export function getExecuteFunctions(
 			},
 			helpers: {
 				...getRequestHelperFunctions(workflow, node, additionalData),
+				...getFileSystemHelperFunctions(node),
 				...getBinaryHelperFunctions(additionalData),
 				getBinaryDataBuffer: async (itemIndex, propertyName, inputIndex = 0) =>
 					getBinaryDataBuffer(inputData, itemIndex, propertyName, inputIndex),

packages/nodes-base/nodes/ReadBinaryFile/ReadBinaryFile.node.ts
@@ -1,12 +1,5 @@
-import { IExecuteFunctions } from 'n8n-core';
-import {
-	INodeExecutionData,
-	INodeType,
-	INodeTypeDescription,
-	NodeOperationError,
-} from 'n8n-workflow';
-
-import { createReadStream } from 'fs';
+import type { IExecuteFunctions } from 'n8n-core';
+import type { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
 
 export class ReadBinaryFile implements INodeType {
 	description: INodeTypeDescription = {
@@ -53,23 +46,6 @@ export class ReadBinaryFile implements INodeType {
 		for (let itemIndex = 0; itemIndex < length; itemIndex++) {
 			try {
 				item = items[itemIndex];
-				const dataPropertyName = this.getNodeParameter('dataPropertyName', itemIndex) as string;
-				const filePath = this.getNodeParameter('filePath', itemIndex) as string;
-
-				let data;
-				try {
-					data = createReadStream(filePath);
-				} catch (error) {
-					if (error.code === 'ENOENT') {
-						throw new NodeOperationError(
-							this.getNode(),
-							`The file "${filePath}" could not be found.`,
-						);
-					}
-
-					throw error;
-				}
-
 				const newItem: INodeExecutionData = {
 					json: item.json,
 					binary: {},
@@ -85,7 +61,10 @@ export class ReadBinaryFile implements INodeType {
 					Object.assign(newItem.binary, item.binary);
 				}
 
-				newItem.binary![dataPropertyName] = await this.helpers.prepareBinaryData(data, filePath);
+				const filePath = this.getNodeParameter('filePath', itemIndex) as string;
+				const stream = await this.helpers.createReadStream(filePath);
+				const dataPropertyName = this.getNodeParameter('dataPropertyName', itemIndex) as string;
+				newItem.binary![dataPropertyName] = await this.helpers.prepareBinaryData(stream, filePath);
 				returnData.push(newItem);
 			} catch (error) {
 				if (this.continueOnFail()) {

packages/nodes-base/nodes/ReadBinaryFiles/ReadBinaryFiles.node.ts
@@ -1,8 +1,6 @@
-import { IExecuteFunctions } from 'n8n-core';
-import { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
+import type { IExecuteFunctions } from 'n8n-core';
+import type { INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
 import glob from 'fast-glob';
-import { createReadStream } from 'fs';
-import type { Readable } from 'stream';
 
 export class ReadBinaryFiles implements INodeType {
 	description: INodeTypeDescription = {
@@ -46,22 +44,17 @@ export class ReadBinaryFiles implements INodeType {
 		const files = await glob(fileSelector);
 
 		const items: INodeExecutionData[] = [];
-		let item: INodeExecutionData;
-		let data: Readable;
 		for (const filePath of files) {
-			data = createReadStream(filePath);
-
-			item = {
+			const stream = await this.helpers.createReadStream(filePath);
+			items.push({
 				binary: {
-					[dataPropertyName]: await this.helpers.prepareBinaryData(data, filePath),
+					[dataPropertyName]: await this.helpers.prepareBinaryData(stream, filePath),
 				},
 				json: {},
 				pairedItem: {
 					item: 0,
 				},
-			};
-
-			items.push(item);
+			});
 		}
 
 		return this.prepareOutputData(items);

packages/workflow/src/Interfaces.ts
@@ -15,6 +15,7 @@ import type { WorkflowActivationError } from './WorkflowActivationError';
 import type { WorkflowOperationError } from './WorkflowErrors';
 import type { NodeApiError, NodeOperationError } from './NodeErrors';
 import type { ExpressionError } from './ExpressionError';
+import { PathLike } from 'fs';
 
 export interface IAdditionalCredentialOptions {
 	oauth2?: IOAuth2Options;
@@ -640,6 +641,10 @@ export interface JsonHelperFunctions {
 	returnJsonArray(jsonData: IDataObject | IDataObject[]): INodeExecutionData[];
 }
 
+export interface FileSystemHelperFunctions {
+	createReadStream(path: PathLike): Promise<Readable>;
+}
+
 export interface BinaryHelperFunctions {
 	prepareBinaryData(
 		binaryData: Buffer | Readable,
@@ -725,6 +730,7 @@ export type IExecuteFunctions = ExecuteFunctions.GetNodeParameterFn &
 
 		helpers: RequestHelperFunctions &
 			BinaryHelperFunctions &
+			FileSystemHelperFunctions &
 			JsonHelperFunctions & {
 				normalizeItems(items: INodeExecutionData | INodeExecutionData[]): INodeExecutionData[];
 				constructExecutionMetaData(

packages/workflow/src/NodeErrors.ts
@@ -217,6 +217,7 @@ abstract class NodeError extends ExecutionBaseError {
 }
 
 interface NodeOperationErrorOptions {
+	message?: string;
 	description?: string;
 	runIndex?: number;
 	itemIndex?: number;
@@ -234,6 +235,9 @@ export class NodeOperationError extends NodeError {
 		}
 		super(node, error);
 
+		if (options.message) {
+			this.message = options.message;
+		}
 		this.description = options.description;
 		this.context.runIndex = options.runIndex;
 		this.context.itemIndex = options.itemIndex;