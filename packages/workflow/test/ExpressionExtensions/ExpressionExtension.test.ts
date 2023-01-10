b94b30e46e336e14ed84ae5ce45eede29eaea0d4
Valya <68596159+valya@users.noreply.github.com>
2023-01-10 13:06:12 +0000

    feat: Expression extension framework (#4372)
    * :zap: Introduce a framework for expression extension
    
    * :bulb: Add some inline comments
    
    * :zap: Introduce hash alias for encrypt
    
    * :zap: Introduce a manual granular level approach to shadowing/overrideing extensions
    
    * :fire: Cleanup comments
    
    * :zap: Introduce a basic method of extension for native functions
    
    * :zap: Add length to StringExtension
    
    * :zap: Add number type to extension return types
    
    * :zap: Temporarily introduce DateTime with extension
    
    * :zap: Cleanup comments
    
    * :zap: Organize imports
    
    * :recycle: Fix up some typings
    
    * :zap: Fix typings
    
    * :recycle: Remove unnecessary resolve of expression
    
    * :zap: Extensions Improvement
    
    * :recycle: Refactor EXPRESSION_EXTENSION_METHODS
    
    * :recycle: Refactor EXPRESSION_EXTENSION_METHODS
    
    * :recycle: Update extraArgs types
    
    * :recycle: Fix tests
    
    * :recycle: Fix bind type issue
    
    * :recycle: Fixing duration type issue
    
    * :recycle: Refactor to allow overrides on native methods
    
    * :recycle: Temporarily remove Date Extensions to pass tests
    
    * feat(dt-functions): introduce date expression extensions (#4045)
    
    * :tada: Add Date Extensions into the mix
    
    * :sparkles: Introduce additional date extension methods
    
    * :white_check_mark: Add Date Expression Extension tests
    
    * :wrench: Add ability to debug tests
    
    * :recycle: Refactor extension for native types
    
    * :fire: Move sayHi method to String Extension class
    
    * :recycle: Update scope when binding member methods
    
    * :white_check_mark: Add String Extension tests
    
    * feat(dt-functions): introduce array expression extensions (#4044)
    
    * :sparkles: Introduce Array Extensions
    
    * :white_check_mark: Add Array Expression tests
    
    * feat(dt-functions): introduce number expression extensions (#4046)
    
    * :tada: Introduce Number Extensions
    
    * :zap: Support more shared extensions
    
    * :zap: Improve handling of name collision
    
    * :white_check_mark: Update tests
    
    * Fixed up tests
    
    * :fire: Remove remove markdown
    
    * :recylce: Replace remove-markdown dependencies with implementation
    
    * :recycle: Replace remove-markdown dependencies with implementation
    
    * :white_check_mark: Update tests
    
    * :recycle: Fix scoping and cleanup
    
    * :recycle: Update comments and errors
    
    * :recycle: Fix linting errors
    
    * :heavy_minus_sign: Remove unused dependencies
    
    * fix: expression extension not working with multiple extensions
    
    * refactor: change extension transform to be more efficient
    
    * test: update most test to work with new extend function
    
    * fix: update and fix type error in config
    
    * refactor: replace babel with recast
    
    * feat: add hashing functions to string extension
    
    * fix: removed export
    
    * test: add extension parser and transform tests
    
    * fix: vite tests breaking
    
    * refactor: remove commented out code
    
    * fix: parse dates passed from $json in extend function
    
    * refactor: review feedback changes for date extensions
    
    * refactor: review feedback changes for number extensions
    
    * fix: date extension beginningOf test
    
    * fix: broken build from merge
    
    * fix: another merge issue
    
    * refactor: address review feedback (remove ignores)
    
    * feat: new extension functions and tests
    
    * feat: non-dot notation functions
    
    * test: most of the other tests
    
    * fix: toSentenceCase for node versions below 16.6
    
    * feat: add $if and $not expression extensions
    
    * Fix test to work on every timezone
    
    * lint: fix remaining lint issues
    
    Co-authored-by: Csaba Tuncsik <csaba@n8n.io>
    Co-authored-by: Omar Ajoue <krynble@gmail.com>

---
+++ packages/workflow/test/ExpressionExtensions/ExpressionExtension.test.ts
@@ -0,0 +1,162 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { extendTransform } from '@/Extensions';
+import { joinExpression, splitExpression } from '@/Extensions/ExpressionParser';
+import { evaluate } from './Helpers';
+
+describe('Expression Extension Transforms', () => {
+	describe('extend() transform', () => {
+		test('Basic transform with .isBlank', () => {
+			expect(extendTransform('"".isBlank()')!.code).toEqual('extend("", "isBlank", [])');
+		});
+
+		test('Chained transform with .sayHi.getOnlyFirstCharacters', () => {
+			expect(extendTransform('"".sayHi().getOnlyFirstCharacters(2)')!.code).toEqual(
+				'extend(extend("", "sayHi", []), "getOnlyFirstCharacters", [2])',
+			);
+		});
+
+		test('Chained transform with native functions .sayHi.trim.getOnlyFirstCharacters', () => {
+			expect(extendTransform('"aaa ".sayHi().trim().getOnlyFirstCharacters(2)')!.code).toEqual(
+				'extend(extend("aaa ", "sayHi", []).trim(), "getOnlyFirstCharacters", [2])',
+			);
+		});
+	});
+});
+
+describe('tmpl Expression Parser', () => {
+	describe('Compatible splitting', () => {
+		test('Lone expression', () => {
+			expect(splitExpression('{{ "" }}')).toEqual([
+				{ type: 'text', text: '' },
+				{ type: 'code', text: ' "" ', hasClosingBrackets: true },
+			]);
+		});
+
+		test('Multiple expression', () => {
+			expect(splitExpression('{{ "test".sayHi() }} you have ${{ (100).format() }}.')).toEqual([
+				{ type: 'text', text: '' },
+				{ type: 'code', text: ' "test".sayHi() ', hasClosingBrackets: true },
+				{ type: 'text', text: ' you have $' },
+				{ type: 'code', text: ' (100).format() ', hasClosingBrackets: true },
+				{ type: 'text', text: '.' },
+			]);
+		});
+
+		test('Unclosed expression', () => {
+			expect(splitExpression('{{ "test".sayHi() }} you have ${{ (100).format()')).toEqual([
+				{ type: 'text', text: '' },
+				{ type: 'code', text: ' "test".sayHi() ', hasClosingBrackets: true },
+				{ type: 'text', text: ' you have $' },
+				{ type: 'code', text: ' (100).format()', hasClosingBrackets: false },
+			]);
+		});
+
+		test('Escaped opening bracket', () => {
+			expect(splitExpression('test \\{{ no code }}')).toEqual([
+				{ type: 'text', text: 'test \\{{ no code }}' },
+			]);
+		});
+
+		test('Escaped closinging bracket', () => {
+			expect(splitExpression('test {{ code.test("\\}}") }}')).toEqual([
+				{ type: 'text', text: 'test ' },
+				{ type: 'code', text: ' code.test("}}") ', hasClosingBrackets: true },
+			]);
+		});
+	});
+
+	describe('Compatible joining', () => {
+		test('Lone expression', () => {
+			expect(joinExpression(splitExpression('{{ "" }}'))).toEqual('{{ "" }}');
+		});
+
+		test('Multiple expression', () => {
+			expect(
+				joinExpression(splitExpression('{{ "test".sayHi() }} you have ${{ (100).format() }}.')),
+			).toEqual('{{ "test".sayHi() }} you have ${{ (100).format() }}.');
+		});
+
+		test('Unclosed expression', () => {
+			expect(
+				joinExpression(splitExpression('{{ "test".sayHi() }} you have ${{ (100).format()')),
+			).toEqual('{{ "test".sayHi() }} you have ${{ (100).format()');
+		});
+
+		test('Escaped opening bracket', () => {
+			expect(joinExpression(splitExpression('test \\{{ no code }}'))).toEqual(
+				'test \\{{ no code }}',
+			);
+		});
+
+		test('Escaped closinging bracket', () => {
+			expect(joinExpression(splitExpression('test {{ code.test("\\}}") }}'))).toEqual(
+				'test {{ code.test("\\}}") }}',
+			);
+		});
+	});
+
+	describe('Non dot extensions', () => {
+		test('min', () => {
+			expect(evaluate('={{ min(1, 2, 3, 4, 5, 6) }}')).toEqual(1);
+			expect(evaluate('={{ min(1, NaN, 3, 4, 5, 6) }}')).toBeNaN();
+		});
+
+		test('max', () => {
+			expect(evaluate('={{ max(1, 2, 3, 4, 5, 6) }}')).toEqual(6);
+			expect(evaluate('={{ max(1, NaN, 3, 4, 5, 6) }}')).toBeNaN();
+		});
+
+		test('average', () => {
+			expect(evaluate('={{ average(1, 2, 3, 4, 5, 6) }}')).toEqual(3.5);
+			expect(evaluate('={{ average(1, NaN, 3, 4, 5, 6) }}')).toBeNaN();
+		});
+
+		test('numberList', () => {
+			expect(evaluate('={{ numberList(1, 10) }}')).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+			expect(evaluate('={{ numberList(1, -10) }}')).toEqual([
+				1, 0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10,
+			]);
+		});
+
+		test('zip', () => {
+			expect(evaluate('={{ zip(["test1", "test2", "test3"], [1, 2, 3]) }}')).toEqual({
+				test1: 1,
+				test2: 2,
+				test3: 3,
+			});
+		});
+
+		test('$if', () => {
+			expect(evaluate('={{ $if("a"==="a", 1, 2) }}')).toEqual(1);
+			expect(evaluate('={{ $if("a"==="b", 1, 2) }}')).toEqual(2);
+			expect(evaluate('={{ $if("a"==="a", 1) }}')).toEqual(1);
+			expect(evaluate('={{ $if("a"==="b", 1) }}')).toEqual(false);
+
+			// This will likely break when sandboxing is implemented but it works for now.
+			// If you're implementing sandboxing maybe provide a way to add functions to
+			// sandbox we can check instead?
+			const mockCallback = jest.fn(() => false);
+			// @ts-ignore
+			evaluate('={{ $if("a"==="a", true, $data["cb"]()) }}', [{ cb: mockCallback }]);
+			expect(mockCallback.mock.calls.length).toEqual(0);
+
+			// @ts-ignore
+			evaluate('={{ $if("a"==="b", true, $data["cb"]()) }}', [{ cb: mockCallback }]);
+			expect(mockCallback.mock.calls.length).toEqual(0);
+		});
+
+		test('$not', () => {
+			expect(evaluate('={{ $not(1) }}')).toEqual(false);
+			expect(evaluate('={{ $not(0) }}')).toEqual(true);
+			expect(evaluate('={{ $not(true) }}')).toEqual(false);
+			expect(evaluate('={{ $not(false) }}')).toEqual(true);
+			expect(evaluate('={{ $not(undefined) }}')).toEqual(true);
+			expect(evaluate('={{ $not(null) }}')).toEqual(true);
+			expect(evaluate('={{ $not("") }}')).toEqual(true);
+			expect(evaluate('={{ $not("a") }}')).toEqual(false);
+		});
+	});
+});
