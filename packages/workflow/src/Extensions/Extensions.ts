6a8d709417b2c4ae37be292b0d81c5db68ff56c7
Iván Ovejero <ivov.src@gmail.com>
2023-01-11 17:01:27 +0100

    :blue_book: Expand types

--- packages/workflow/src/Extensions/Extensions.ts
+++ packages/workflow/src/Extensions/Extensions.ts
@@ -1,5 +1,26 @@
-export interface ExtensionMap {
-	typeName: string;
-	// eslint-disable-next-line @typescript-eslint/ban-types
-	functions: Record<string, Function>;
-}
+type TypeName = 'String' | 'Number' | 'Array' | 'Object' | 'Date';
+
+export type ExtensionMap =
+	| NumberExtensions
+	| StringExtensions
+	| ObjectExtensions
+	| DateExtensions
+	| ArrayExtensions;
+
+type ExtensionFunctionMetadata = {
+	description?: string;
+};
+
+type MakeExtensions<N extends TypeName> = {
+	typeName: N;
+	functions: {
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		[key: string]: Function & ExtensionFunctionMetadata;
+	};
+};
+
+type NumberExtensions = MakeExtensions<'Number'>;
+type StringExtensions = MakeExtensions<'String'>;
+type ObjectExtensions = MakeExtensions<'Object'>;
+type DateExtensions = MakeExtensions<'Date'>;
+type ArrayExtensions = MakeExtensions<'Array'>;
