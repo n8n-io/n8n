74c4216963dee66d6bd5335d9a26be07532b14c9
Oleg Ivaniv <oleg@n8n.io>
2023-01-16 12:11:50 +0100

    Fix canvas e2e tests

--- cypress/e2e/12-canvas.cy.ts
+++ cypress/e2e/12-canvas.cy.ts
@@ -56,11 +56,11 @@ describe('Canvas Actions', () => {
 	it('should add note between two connected nodes', () => {
 		WorkflowPage.actions.addNodeToCanvas(SCHEDULE_TRIGGER_NODE_NAME);
 		WorkflowPage.actions.addNodeToCanvas(CODE_NODE_NAME);
-		WorkflowPage.getters.nodeConnections().first().trigger('mouseover', { force: true });
-		cy.get('.connection-actions .add').as('AddNodeConnectionButton')
-		cy.get('@AddNodeConnectionButton').invoke('show');
-		cy.get('@AddNodeConnectionButton').should('be.visible').click();
-		WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME);
+		WorkflowPage.getters.nodeConnections().first().realHover();
+		cy.get('.connection-actions .add').should('be.visible').click()
+
+		WorkflowPage.getters.nodeCreatorSearchBar().type(SET_NODE_NAME);
+		WorkflowPage.getters.nodeCreatorSearchBar().type('{enter}');
 		WorkflowPage.getters.canvasNodes().should('have.length', 3);
 		WorkflowPage.getters.nodeConnections().should('have.length', 2);
 		// And last node should be pushed to the right
@@ -198,7 +198,7 @@ describe('Canvas Actions', () => {
 	it('should delete connections by pressing the delete button', () => {
 		WorkflowPage.actions.addNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
 		WorkflowPage.actions.addNodeToCanvas(CODE_NODE_NAME);
-		WorkflowPage.getters.nodeConnections().first().trigger('mouseover', { force: true });
+		WorkflowPage.getters.nodeConnections().first().realHover();
 		cy.get('.connection-actions .delete').click();
 		WorkflowPage.getters.nodeConnections().should('have.length', 0);
 	});
