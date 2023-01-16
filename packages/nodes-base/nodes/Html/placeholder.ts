c0929236f2d35d904e49fd1b431eb6f78517659d
Iván Ovejero <ivov.src@gmail.com>
2023-01-16 09:23:33 +0100

    :pencil2: Adjust placeholder

--- packages/nodes-base/nodes/Html/placeholder.ts
+++ packages/nodes-base/nodes/Html/placeholder.ts
@@ -4,28 +4,35 @@ export const placeholder = `
 <html>
 <head>
   <meta charset="UTF-8" />
-  <title>Document</title>
+  <title>My HTML document</title>
 </head>
 <body>
   <div class="container">
-    <h1>This is a Heading</h1>
-    <p>This is a paragraph.</p>
-    <div>Some more content!</div>
+    <h1>This is an H1 heading</h1>
+    <h2>This is an H2 heading</h2>
+    <p>This is a paragraph</p>
   </div>
 </body>
 </html>
 
 <style>
 .container {
-  background-color: lightblue;
+  background-color: #ffffff;
   text-align: center;
   padding: 16px;
-  border-radius: 10px;
+  border-radius: 8px;
 }
 
 h1 {
-  color: purple;
+  color: #ff6d5a;
   font-size: 24px;
+  font-weight: bold;
+  padding: 8px;
+}
+
+h2 {
+  color: #909399;
+  font-size: 18px;
   font-weight: bold;
   padding: 8px;
 }
