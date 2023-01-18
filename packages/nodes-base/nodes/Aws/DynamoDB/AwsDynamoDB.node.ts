886a0a46a147f816566cf2fc286ab52d08a541ae
agobrech <ael.gobrecht@gmail.com>
2023-01-18 18:34:08 +0100

    Add pairedItem to Aws DynamoDB

--- packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
+++ packages/nodes-base/nodes/Aws/DynamoDB/AwsDynamoDB.node.ts
@@ -91,7 +91,7 @@ export class AwsDynamoDB implements INodeType {
 		const operation = this.getNodeParameter('operation', 0);
 
 		let responseData;
-		const returnData: IDataObject[] = [];
+		const returnData: INodeExecutionData[] = [];
 
 		for (let i = 0; i < items.length; i++) {
 			try {
@@ -391,14 +391,19 @@ export class AwsDynamoDB implements INodeType {
 							responseData = responseData.map(simplify);
 						}
 					}
-
-					Array.isArray(responseData)
-						? returnData.push(...responseData)
-						: returnData.push(responseData);
+					const executionData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray(responseData),
+						{ itemData: { item: i } },
+					);
+					returnData.push(...executionData);
 				}
 			} catch (error) {
 				if (this.continueOnFail()) {
-					returnData.push({ error: error.message });
+					const executionData = this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray({ error: error.message }),
+						{ itemData: { item: i } },
+					);
+					returnData.push(...executionData);
 					continue;
 				}
 
@@ -406,6 +411,6 @@ export class AwsDynamoDB implements INodeType {
 			}
 		}
 
-		return [this.helpers.returnJsonArray(returnData)];
+		return this.prepareOutputData(returnData);
 	}
 }
