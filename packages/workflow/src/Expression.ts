4ddf260afd5acf49b550ea67242c89e12161a3c5
Iván Ovejero <ivov.src@gmail.com>
2023-01-20 09:25:40 +0100

    :fire: Remove check

--- packages/workflow/src/Expression.ts
+++ packages/workflow/src/Expression.ts
@@ -309,19 +309,6 @@ export class Expression {
 			) {
 				throw new Error('invalid syntax');
 			}
-
-			if (
-				typeof process === 'undefined' &&
-				error instanceof Error &&
-				error.name === 'TypeError' &&
-				error.message.endsWith('is not a function')
-			) {
-				const match = error.message.match(/(?<msg>[^.]+is not a function)/);
-
-				if (!match?.groups?.msg) return null;
-
-				throw new Error(match.groups.msg);
-			}
 		}
 		return null;
 	}
