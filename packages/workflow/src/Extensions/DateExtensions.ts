e1e4235568df925a3b4c9d83da7b83368c7ddc17
Valya <68596159+valya@users.noreply.github.com>
2023-01-23 13:28:17 +0000

    fix: IsWeekend not checking if DateTime (#5221) (no-changelog)

--- packages/workflow/src/Extensions/DateExtensions.ts
+++ packages/workflow/src/Extensions/DateExtensions.ts
@@ -139,7 +139,10 @@ function isBetween(date: Date | DateTime, extraArgs: unknown[]): boolean {
 	return secondDate > date && date > firstDate;
 }
 
-function isDst(date: Date): boolean {
+function isDst(date: Date | DateTime): boolean {
+	if (isDateTime(date)) {
+		return date.isInDST;
+	}
 	return DateTime.fromJSDate(date).isInDST;
 }
 
@@ -154,10 +157,13 @@ function isInLast(date: Date | DateTime, extraArgs: unknown[]): boolean {
 	return dateInThePast <= thisDate && thisDate <= DateTime.now();
 }
 
-function isWeekend(date: Date): boolean {
+function isWeekend(date: Date | DateTime): boolean {
 	enum DAYS {
 		saturday = 6,
 		sunday = 7,
+	}
+	if (isDateTime(date)) {
+		return [DAYS.saturday, DAYS.sunday].includes(date.weekday);
 	}
 	return [DAYS.saturday, DAYS.sunday].includes(DateTime.fromJSDate(date).weekday);
 }
@@ -200,7 +206,7 @@ function toLocaleString(date: Date | DateTime, extraArgs: unknown[]): string {
 	return DateTime.fromJSDate(date).toLocaleString(dateFormat, { locale });
 }
 
-function toTimeFromNow(date: Date): string {
+function toTimeFromNow(date: Date | DateTime): string {
 	let diffObj: Duration;
 	if (isDateTime(date)) {
 		diffObj = date.diffNow();
