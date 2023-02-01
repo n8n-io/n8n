54e428b9bacbf9968981fd9a3f9528eaace8eb62
Iván Ovejero <ivov.src@gmail.com>
2023-02-01 09:32:51 +0100

    :test_tube: Unskip Pinia tests

--- packages/editor-ui/src/plugins/codemirror/completions/__tests__/completions.test.ts
+++ packages/editor-ui/src/plugins/codemirror/completions/__tests__/completions.test.ts
@@ -16,8 +16,6 @@ import { CompletionContext, CompletionSource, CompletionResult } from '@codemirr
 import { EditorState } from '@codemirror/state';
 import { n8nLang } from '@/plugins/codemirror/n8nLang';
 
-// @TODO: Unskip Pinia tests
-
 beforeEach(() => {
 	setActivePinia(createTestingPinia());
 	vi.spyOn(utils, 'receivesNoBinaryData').mockReturnValue(true); // hide $binary
@@ -61,7 +59,7 @@ describe('Top-level completions', () => {
 		expect(completions('{{ $| }}')).toHaveLength(dollarOptions().length);
 	});
 
-	test.skip('should return node selector completions for: {{ $(| }}', () => {
+	test('should return node selector completions for: {{ $(| }}', () => {
 		const initialState = { workflows: { workflow: { nodes: mockNodes } } };
 
 		setActivePinia(createTestingPinia({ initialState }));
@@ -177,7 +175,7 @@ describe('Resolution-based completions', () => {
 		});
 	});
 
-	describe.skip('references', () => {
+	describe('references', () => {
 		const resolveParameterSpy = vi.spyOn(workflowHelpers, 'resolveParameter');
 		const { $input, $ } = mockProxy;
 
