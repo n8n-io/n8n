91c261145ed8af9dc3e93eca51af56befca44fa7
Mutasem <mutdmour@gmail.com>
2023-02-02 17:50:12 +0300

    refactor: move tracking from hooks

--- packages/editor-ui/src/stores/posthog.ts
+++ packages/editor-ui/src/stores/posthog.ts
@@ -1,11 +1,20 @@
-import { ref, Ref } from 'vue';
+import { ref, Ref, watch } from 'vue';
 import { Telemetry } from '@/plugins/telemetry';
 import { ASSUMPTION_EXPERIMENT } from '@/constants';
 import { defineStore } from 'pinia';
+import { useUsersStore } from '@/stores/users';
+import { useRootStore } from '@/stores/n8nRootStore';
 
 export const usePostHog = defineStore('posthog', () => {
+	const usersStore = useUsersStore();
+	const rootStore = useRootStore();
+
 	const telemetry: Ref<Telemetry | null> = ref(null);
 	const featureFlags: Ref<Record<string, boolean | string>> = ref({});
+
+	const onLogout = () => {
+		window.posthog?.reset();
+	};
 
 	const getVariant = (experiment: string): string | boolean | undefined => {
 		return featureFlags.value[experiment];
@@ -15,8 +24,26 @@ export const usePostHog = defineStore('posthog', () => {
 		return getVariant(experiment) === variant;
 	};
 
+	const identify = () => {
+		try {
+			const instanceId = rootStore.instanceId;
+			const user = usersStore.currentUser;
+			const traits: Record<string, string> = { instance_id: instanceId };
+			if (user && user.createdAt instanceof Date) {
+				traits.joined_at = user.createdAt.toISOString();
+			} else if (user && typeof user.createdAt === 'string') {
+				traits.joined_at = user.createdAt;
+			}
+
+			// For PostHog, main ID _cannot_ be `undefined` as done for RudderStack.
+			const id = user ? `${instanceId}#${user.id}` : instanceId;
+			window.posthog?.identify(id, traits);
+		} catch (e) {}
+	};
+
 	const init = (tracking: Telemetry) => {
 		telemetry.value = tracking;
+		identify();
 
 		window.posthog?.onFeatureFlags((flags: string[], map: Record<string, string | boolean>) => {
 			featureFlags.value = map;
@@ -37,6 +64,16 @@ export const usePostHog = defineStore('posthog', () => {
 			variant: isDemo ? 'demo' : isVideo ? 'video' : 'control',
 		});
 	});
+
+	watch(
+		() => usersStore.currentUserId,
+		(userId, prevId) => {
+			if (!userId && prevId) {
+				onLogout();
+			}
+			identify();
+		},
+	);
 
 	return {
 		init,
