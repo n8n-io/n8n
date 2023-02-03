c6e5d1a3d3ec6aab218e90f7ccb7b69779ca7b4e
Jan Oberhauser <jan.oberhauser@gmail.com>
2023-02-02 22:33:11 -0600

    :zap: Suppress warnings

--- packages/nodes-base/nodes/Code/Code.node.ts
+++ packages/nodes-base/nodes/Code/Code.node.ts
@@ -9,6 +9,22 @@ import { getSandboxContextPython, SandboxPython } from './SandboxPython';
 import { standardizeOutput } from './utils';
 import type { CodeNodeMode } from './utils';
 
+// TODO: Find better way to suppress warnings
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+globalThis.Blob = require('node:buffer').Blob;
+// From: https://github.com/nodejs/node/issues/30810
+const { emitWarning } = process;
+process.emitWarning = (warning, ...args) => {
+	if (args[0] === 'ExperimentalWarning') {
+		return;
+	}
+	if (args[0] && typeof args[0] === 'object' && args[0].type === 'ExperimentalWarning') {
+		return;
+	}
+	return emitWarning(warning, ...(args as string[]));
+};
+
 export class Code implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Code',
