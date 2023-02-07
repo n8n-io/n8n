017fb6df85d0876ec9afb0152f18b6d27d4b9e2a
Marcus <marcus@n8n.io>
2023-02-07 17:34:08 +0100

    ⚡️IF boolean tests

--- packages/nodes-base/nodes/If/test/If.node.test.ts
+++ packages/nodes-base/nodes/If/test/If.node.test.ts
@@ -1,51 +1,5 @@
-import * as Helpers from '../../../test/nodes/Helpers';
-import type { WorkflowTestData } from '../../../test/nodes/types';
+import { testWorkflows, getWorkflowFilenames } from '../../../test/nodes/Helpers';
 
-import { executeWorkflow } from '../../../test/nodes/ExecuteWorkflow';
+const workflows = getWorkflowFilenames(__dirname);
 
-describe('Execute If Node', () => {
-	const tests: WorkflowTestData[] = [
-		{
-			description: 'should execute IF node true/false boolean',
-			input: {
-				workflowData: Helpers.readJsonFileSync('nodes/If/test/workflow.json'),
-			},
-			output: {
-				nodeData: {
-					'On True': [
-						[
-							{
-								value: true,
-							},
-						],
-					],
-					'On False': [
-						[
-							{
-								value: false,
-							},
-						],
-					],
-				},
-			},
-		},
-	];
-
-	const nodeTypes = Helpers.setup(tests);
-
-	for (const testData of tests) {
-		// eslint-disable-next-line @typescript-eslint/no-loop-func
-		test(testData.description, async () => {
-			// execute workflow
-			const { result } = await executeWorkflow(testData, nodeTypes);
-
-			// check if result node data matches expected test data
-			const resultNodeData = Helpers.getResultNodeData(result, testData);
-			resultNodeData.forEach(({ nodeName, resultData }) =>
-				expect(resultData).toEqual(testData.output.nodeData[nodeName]),
-			);
-
-			expect(result.finished).toEqual(true);
-		});
-	}
-});
+describe('Test ItemLists Node', () => testWorkflows(workflows));
