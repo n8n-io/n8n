b08d825019978c92c0ca4146b66fe3ec21b0b3fa
Marcus <marcus@n8n.io>
2023-02-07 16:53:33 +0100

    ⚡ migrate tests to pin data approach

--- packages/nodes-base/nodes/Code/test/Code.node.test.ts
+++ packages/nodes-base/nodes/Code/test/Code.node.test.ts
@@ -1,55 +1,5 @@
-import * as Helpers from '../../../test/nodes/Helpers';
-import type { WorkflowTestData } from '../../../test/nodes/types';
-import { executeWorkflow } from '../../../test/nodes/ExecuteWorkflow';
+import { testWorkflows, getWorkflowFilenames } from '../../../test/nodes/Helpers';
 
-describe('Execute Code Node', () => {
-	const tests: WorkflowTestData[] = [
-		{
-			description: 'run once for All and Each items',
-			input: {
-				workflowData: Helpers.readJsonFileSync('nodes/Code/test/Code.workflow.json'),
-			},
-			output: {
-				nodeData: {
-					'Run Once for All Items': [
-						[
-							{
-								sum: 3,
-							},
-						],
-					],
-					'Run Once for Each Item': [
-						[
-							{
-								value: 1,
-								myNewField: 1,
-							},
-							{
-								value: 2,
-								myNewField: 2,
-							},
-						],
-					],
-				},
-			},
-		},
-	];
+const workflows = getWorkflowFilenames(__dirname);
 
-	const nodeTypes = Helpers.setup(tests);
-
-	for (const testData of tests) {
-		// eslint-disable-next-line @typescript-eslint/no-loop-func
-		test(testData.description, async () => {
-			// execute workflow
-			const { result } = await executeWorkflow(testData, nodeTypes);
-
-			// check if result node data matches expected test data
-			const resultNodeData = Helpers.getResultNodeData(result, testData);
-			resultNodeData.forEach(({ nodeName, resultData }) =>
-				expect(resultData).toEqual(testData.output.nodeData[nodeName]),
-			);
-
-			expect(result.finished).toEqual(true);
-		});
-	}
-});
+describe('Test Code Node', () => testWorkflows(workflows));
