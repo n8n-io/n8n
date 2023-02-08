d7160b08b63a5d49c7fb968ae0612656b096058d
OlegIvaniv <me@olegivaniv.com>
2023-02-08 14:48:28 +0100

    test(editor): Add canvas dragging tests (no-changelog) (#5406)
    * WIP
    
    * Fix undo/redo tests and add canvas tests
    
    * Cleanup
    
    * Cleanup
    
    * Use drag and draganddrop commands
    
    * Fix hanging CI run
    
    * Undo redo spec

--- cypress/e2e/12-canvas.cy.ts
+++ cypress/e2e/12-canvas.cy.ts
@@ -1,8 +1,12 @@
 import {
 	MANUAL_TRIGGER_NODE_NAME,
+	MANUAL_TRIGGER_NODE_DISPLAY_NAME,
 	CODE_NODE_NAME,
 	SCHEDULE_TRIGGER_NODE_NAME,
 	SET_NODE_NAME,
+	SWITCH_NODE_NAME,
+	IF_NODE_NAME,
+	MERGE_NODE_NAME,
 	HTTP_REQUEST_NODE_NAME,
 } from './../constants';
 import { WorkflowPage as WorkflowPageClass } from '../pages/workflow';
@@ -33,11 +37,124 @@ describe('Canvas Actions', () => {
 		WorkflowPage.getters.executeWorkflowButton().should('be.visible');
 	});
 
+	it('should connect and disconnect a simple node', () => {
+		WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME);
+		WorkflowPage.getters.nodeViewBackground().click(600, 200, { force: true });
+		cy.get('.jtk-connector').should('have.length', 1);
+		WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME);
+
+		// Change connection from Set to Set1
+		cy.draganddrop(
+			WorkflowPage.getters.getEndpointSelector('input', SET_NODE_NAME),
+			WorkflowPage.getters.getEndpointSelector('input', `${SET_NODE_NAME}1`)
+		)
+
+		WorkflowPage.getters.canvasNodeInputEndpointByName(`${SET_NODE_NAME}1`).should('have.class', 'jtk-endpoint-connected');
+
+		cy.get('.jtk-connector').should('have.length', 1);
+		// Disconnect Set1
+		cy.drag(WorkflowPage.getters.getEndpointSelector('input', `${SET_NODE_NAME}1`), [-200, 100])
+		cy.get('.jtk-connector').should('have.length', 0);
+	});
+
 	it('should add first step', () => {
 		WorkflowPage.getters.canvasPlusButton().should('be.visible');
 		WorkflowPage.actions.addInitialNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
 		WorkflowPage.getters.canvasNodes().should('have.length', 1);
 	});
+
+	it('should add a node via plus endpoint drag', () => {
+		WorkflowPage.getters.canvasPlusButton().should('be.visible');
+		WorkflowPage.actions.addNodeToCanvas(SCHEDULE_TRIGGER_NODE_NAME, true);
+
+		cy.drag(WorkflowPage.getters.getEndpointSelector('plus', SCHEDULE_TRIGGER_NODE_NAME), [100, 100])
+
+		WorkflowPage.getters.nodeCreatorSearchBar().should('be.visible');
+		WorkflowPage.actions.addNodeToCanvas(IF_NODE_NAME, false);
+		WorkflowPage.getters.nodeViewBackground().click({ force: true });
+	});
+
+	it('should add switch node and test connections', () => {
+		WorkflowPage.actions.addNodeToCanvas(SWITCH_NODE_NAME, true);
+
+		// Switch has 4 output endpoints
+		for (let i = 0; i < 4; i++) {
+			WorkflowPage.getters.canvasNodePlusEndpointByName(SWITCH_NODE_NAME, i).click({ force: true })
+			WorkflowPage.getters.nodeCreatorSearchBar().should('be.visible');
+			WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME, false);
+			WorkflowPage.actions.zoomToFit();
+		}
+		WorkflowPage.actions.saveWorkflowOnButtonClick();
+		cy.reload()
+		cy.waitForLoad();
+		// Make sure all connections are there after reload
+		for (let i = 0; i < 4; i++) {
+			const setName = `${SET_NODE_NAME}${i > 0 ? i : ''}`;
+			WorkflowPage.getters.canvasNodeInputEndpointByName(setName).should('have.class', 'jtk-endpoint-connected');
+		}
+	});
+
+	it('should add merge node and test connections', () => {
+		WorkflowPage.actions.addNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
+		for (let i = 0; i < 2; i++) {
+			WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME, true);
+			WorkflowPage.getters.nodeViewBackground().click(600 + i * 100, 200, { force: true });
+		}
+		WorkflowPage.actions.zoomToFit();
+
+		WorkflowPage.actions.addNodeToCanvas(MERGE_NODE_NAME);
+		WorkflowPage.actions.zoomToFit();
+
+		// Connect manual to Set1
+		cy.draganddrop(
+			WorkflowPage.getters.getEndpointSelector('output', MANUAL_TRIGGER_NODE_DISPLAY_NAME),
+			WorkflowPage.getters.getEndpointSelector('input', `${SET_NODE_NAME}1`)
+		)
+
+		cy.get('.rect-input-endpoint.jtk-endpoint-connected').should('have.length', 2);
+
+		// Connect Set1 and Set2 to merge
+		cy.draganddrop(
+			WorkflowPage.getters.getEndpointSelector('plus', SET_NODE_NAME),
+			WorkflowPage.getters.getEndpointSelector('input', MERGE_NODE_NAME, 0)
+		)
+		cy.draganddrop(
+			WorkflowPage.getters.getEndpointSelector('plus', `${SET_NODE_NAME}1`),
+			WorkflowPage.getters.getEndpointSelector('input', MERGE_NODE_NAME, 1)
+		)
+
+		cy.get('.rect-input-endpoint.jtk-endpoint-connected').should('have.length', 4);
+
+		// Make sure all connections are there after save & reload
+		WorkflowPage.actions.saveWorkflowOnButtonClick();
+		cy.reload()
+		cy.waitForLoad();
+
+		cy.get('.rect-input-endpoint.jtk-endpoint-connected').should('have.length', 4);
+		WorkflowPage.actions.executeWorkflow();
+		// If the merged set nodes are connected and executed correctly, there should be 2 items in the output of merge node
+		cy.get('[data-label="2 items"]').should('be.visible');
+	});
+
+	it('should add nodes and check execution success', () => {
+		WorkflowPage.actions.addNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
+		for (let i = 0; i < 3; i++) {
+			WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME, true);
+		}
+		WorkflowPage.actions.zoomToFit();
+		WorkflowPage.actions.executeWorkflow();
+
+		cy.get('.jtk-connector.success').should('have.length', 3);
+		cy.get('.data-count').should('have.length', 4);
+		cy.get('.plus-draggable-endpoint').should('have.class', 'ep-success');
+
+		WorkflowPage.actions.addNodeToCanvas(SET_NODE_NAME);
+		WorkflowPage.actions.zoomToFit();
+
+		cy.get('.plus-draggable-endpoint').filter(':visible').should('not.have.class', 'ep-success');
+		cy.get('.jtk-connector.success').should('have.length', 3);
+		cy.get('.jtk-connector').should('have.length', 4);
+	})
 
 	it('should add a connected node using plus endpoint', () => {
 		WorkflowPage.actions.addNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
@@ -226,7 +343,7 @@ describe('Canvas Actions', () => {
 	it('should delete a connection by moving it away from endpoint', () => {
 		WorkflowPage.actions.addNodeToCanvas(MANUAL_TRIGGER_NODE_NAME);
 		WorkflowPage.actions.addNodeToCanvas(CODE_NODE_NAME);
-		cy.drag('.rect-input-endpoint.jtk-endpoint-connected', [0, -100]);
+		cy.drag(WorkflowPage.getters.getEndpointSelector('input', CODE_NODE_NAME), [0, -100]);
 		WorkflowPage.getters.nodeConnections().should('have.length', 0);
 	});
 
