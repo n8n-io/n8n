2b3afa0238206113d45e9892ef6a7ad1e2690c9d
Michael Kret <michael.k@radency.com>
2023-02-08 12:14:10 +0200

    :zap: get method test

--- packages/nodes-base/nodes/HttpRequest/test/node/HttpRequest.test.ts
+++ packages/nodes-base/nodes/HttpRequest/test/node/HttpRequest.test.ts
@@ -1,5 +1,66 @@
-import { testWorkflows, getWorkflowFilenames } from '../../../../test/nodes/Helpers';
+import {
+	setup,
+	equalityTest,
+	getWorkflowFilenames,
+	workflowToTests,
+} from '../../../../test/nodes/Helpers';
 
-const workflows = getWorkflowFilenames(__dirname);
+import nock from 'nock';
 
-describe('Test HTTP Request Node', () => testWorkflows(workflows));
+describe('Test HTTP Request Node', () => {
+	const workflows = getWorkflowFilenames(__dirname);
+	const tests = workflowToTests(workflows);
+
+	beforeEach(() => {
+		nock.disableNetConnect();
+
+		nock('https://dummyjson.com').get('/todos/1').reply(200, {
+			id: 1,
+			todo: 'Do something nice for someone I care about',
+			completed: true,
+			userId: 26,
+		});
+
+		nock('https://dummyjson.com')
+			.matchHeader('Authorization', 'Bearer 12345')
+			.get('/todos/3')
+			.reply(200, {
+				id: 3,
+				todo: 'Watch a classic movie',
+				completed: false,
+				userId: 4,
+			});
+
+		nock('https://dummyjson.com')
+			.get('/todos?limit=2&skip=10')
+			.reply(200, {
+				todos: [
+					{
+						id: 11,
+						todo: "Text a friend I haven't talked to in a long time",
+						completed: false,
+						userId: 39,
+					},
+					{
+						id: 12,
+						todo: 'Organize pantry',
+						completed: true,
+						userId: 39,
+					},
+				],
+				total: 150,
+				skip: 10,
+				limit: 2,
+			});
+	});
+
+	afterEach(() => {
+		nock.restore();
+	});
+
+	const nodeTypes = setup(tests);
+
+	for (const testData of tests) {
+		test(testData.description, async () => equalityTest(testData, nodeTypes));
+	}
+});
