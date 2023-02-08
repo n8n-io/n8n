478d8f3a39a39ab4a95e50684c335b95e424ed94
agobrech <ael.gobrecht@gmail.com>
2023-02-08 16:15:27 +0100

    Try new fix

--- packages/nodes-base/nodes/DateTime/DateTime.node.ts
+++ packages/nodes-base/nodes/DateTime/DateTime.node.ts
@@ -397,6 +397,7 @@ export class DateTime implements INodeType {
 		const returnData: INodeExecutionData[] = [];
 
 		const workflowTimezone = this.getTimezone();
+		console.log(workflowTimezone);
 		let item: INodeExecutionData;
 
 		for (let i = 0; i < length; i++) {
