c91feee62ac56013092b1ed036921f4018ec315d
Milorad Filipovic <milorad@n8n.io>
2023-02-09 14:15:20 +0100

    ✅ Added rest of the UM tests

--- cypress/e2e/18-user-management.cy.ts
+++ cypress/e2e/18-user-management.cy.ts
@@ -1,6 +1,6 @@
 import { MainSidebar } from './../pages/sidebar/main-sidebar';
 import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD } from '../constants';
-import { SettingsSidebar, WorkflowsPage } from '../pages';
+import { SettingsSidebar, SettingsUsersPage, WorkflowPage, WorkflowsPage } from '../pages';
 
 /**
  * User A - Instance owner
@@ -36,9 +36,8 @@ const users = [
 	},
 ];
 
-const mainSidebar = new MainSidebar();
-const workflowsPage = new WorkflowsPage();
-const settingsSidebar = new SettingsSidebar();
+const usersSettingsPage = new SettingsUsersPage();
+const workflowPage = new WorkflowPage();
 
 describe('User Management', () => {
 	before(() => {
@@ -58,12 +57,40 @@ describe('User Management', () => {
 	});
 
 	it('should prevent non-owners to access UM settings', () => {
-		cy.signin({ email: users[0].email, password: DEFAULT_USER_PASSWORD });
-		cy.visit(workflowsPage.url);
-		mainSidebar.actions.goToSettings();
-		settingsSidebar.getters.menuItem('Users').should('not.exist');
-		// Should be redirected to workflows page if trying to access UM url
-		cy.visit('/settings/users');
-		cy.url().should('match', new RegExp(workflowsPage.url));
+		usersSettingsPage.actions.loginAndVisit(users[0].email, users[0].password, false)
+	});
+
+	it('should allow instance owner to access UM settings', () => {
+		usersSettingsPage.actions.loginAndVisit(instanceOwner.email, instanceOwner.password, true);
+	});
+
+	it('should properly render UM settings page for instance owners', () => {
+		usersSettingsPage.actions.loginAndVisit(instanceOwner.email, instanceOwner.password, true);
+		// All items in user list should be there
+		usersSettingsPage.getters.userListItems().should('have.length', 3);
+		// List item for current user should have the `Owner` badge
+		usersSettingsPage.getters.userItem(instanceOwner.email).find('.n8n-badge:contains("Owner")').should('exist');
+		// Other users list items should contain action pop-up list
+		usersSettingsPage.getters.userActionsToggle(users[0].email).should('exist');
+		usersSettingsPage.getters.userActionsToggle(users[1].email).should('exist');
+	});
+
+	it('should delete user and their data', () => {
+		usersSettingsPage.actions.loginAndVisit(instanceOwner.email, instanceOwner.password, true);
+		usersSettingsPage.actions.opedDeleteDialog(users[0].email);
+		usersSettingsPage.getters.deleteDataRadioButton().realClick();
+		usersSettingsPage.getters.deleteDataInput().type('delete all data');
+		usersSettingsPage.getters.deleteUserButton().realClick();
+		workflowPage.getters.successToast().should('contain', 'User deleted');
+	});
+
+	it('should delete user and transfer their data', () => {
+		usersSettingsPage.actions.loginAndVisit(instanceOwner.email, instanceOwner.password, true);
+		usersSettingsPage.actions.opedDeleteDialog(users[1].email);
+		usersSettingsPage.getters.transferDataRadioButton().realClick();
+		usersSettingsPage.getters.userSelectDropDown().realClick();
+		usersSettingsPage.getters.userSelectOptions().first().realClick();
+		usersSettingsPage.getters.deleteUserButton().realClick();
+		workflowPage.getters.successToast().should('contain', 'User deleted');
 	});
 });
