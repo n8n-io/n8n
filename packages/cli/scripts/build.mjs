982ba13d26e915f38ecbad5c66211ce9931ce30c
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-02-09 21:21:32 +0100

    redo the cli build script

--- packages/cli/scripts/build.mjs
+++ packages/cli/scripts/build.mjs
@@ -7,30 +7,9 @@ const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, '..');
 
-const ROOT_DIR = path.resolve(__dirname, '..');
-const SPEC_FILENAME = 'openapi.yml';
-const SPEC_THEME_FILENAME = 'swaggerTheme.css';
-const EDITOR_UI_DIST_DIR = path.join(path.dirname(require.resolve('n8n-editor-ui')), 'dist');
-
-const userManagementEnabled = process.env.N8N_USER_MANAGEMENT_DISABLED !== 'true';
-const publicApiEnabled = process.env.N8N_PUBLIC_API_DISABLED !== 'true';
-const uiEnabled = process.env.N8N_DISABLE_UI !== 'true';
-
-if (userManagementEnabled) {
-	copyUserManagementEmailTemplates();
-}
-
-if (publicApiEnabled) {
-	copySwaggerTheme();
-	bundleOpenApiSpecs();
-}
-
-if (uiEnabled) {
-	copyUIAssets();
-}
-
-function copyUserManagementEmailTemplates(rootDir = ROOT_DIR) {
+function copyUserManagementEmailTemplates() {
 	const templates = {
 		source: path.resolve(rootDir, 'src', 'UserManagement', 'email', 'templates'),
 		destination: path.resolve(rootDir, 'dist', 'UserManagement', 'email'),
@@ -39,7 +18,8 @@ function copyUserManagementEmailTemplates(rootDir = ROOT_DIR) {
 	shell.cp('-r', templates.source, templates.destination);
 }
 
-function copySwaggerTheme(rootDir = ROOT_DIR, themeFilename = SPEC_THEME_FILENAME) {
+function copySwaggerTheme() {
+	const themeFilename = 'swaggerTheme.css';
 	const swaggerTheme = {
 		source: path.resolve(rootDir, 'src', 'PublicApi', themeFilename),
 		destination: path.resolve(rootDir, 'dist', 'PublicApi'),
@@ -48,14 +28,16 @@ function copySwaggerTheme(rootDir = ROOT_DIR, themeFilename = SPEC_THEME_FILENAM
 	shell.cp('-r', swaggerTheme.source, swaggerTheme.destination);
 }
 
-function bundleOpenApiSpecs(rootDir = ROOT_DIR, specFileName = SPEC_FILENAME) {
+function bundleOpenApiSpecs() {
+	const specFileName = 'openapi.yml';
 	const publicApiDir = path.resolve(rootDir, 'src', 'PublicApi');
 
 	shell
 		.find(publicApiDir)
-		.reduce((acc, cur) => {
-			return cur.endsWith(specFileName) ? [...acc, path.relative('./src', cur)] : acc;
-		}, [])
+		.reduce(
+			(acc, cur) => (cur.endsWith(specFileName) ? [...acc, path.relative('./src', cur)] : acc),
+			[],
+		)
 		.forEach((specPath) => {
 			const distSpecPath = path.resolve(rootDir, 'dist', specPath);
 			const command = `npm run swagger -- bundle src/${specPath} --type yaml --outfile ${distSpecPath}`;
@@ -63,6 +45,12 @@ function bundleOpenApiSpecs(rootDir = ROOT_DIR, specFileName = SPEC_FILENAME) {
 		});
 }
 
-function copyUIAssets(rootDir = ROOT_DIR) {
-	shell.cp('-r', EDITOR_UI_DIST_DIR, path.resolve(rootDir, 'dist', 'editor-ui'));
+function copyUIAssets() {
+	const uiDistDir = path.join(path.dirname(require.resolve('n8n-editor-ui')), 'dist');
+	shell.cp('-r', uiDistDir, path.resolve(rootDir, 'dist', 'editor-ui'));
 }
+
+copyUserManagementEmailTemplates();
+copySwaggerTheme();
+bundleOpenApiSpecs();
+copyUIAssets();
