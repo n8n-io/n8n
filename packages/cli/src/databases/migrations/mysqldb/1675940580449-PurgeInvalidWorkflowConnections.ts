3d5201ffcc382d89c25f2bc0a83f9e9f0341b82c
Oleg Ivaniv <oleg@n8n.io>
2023-02-09 18:03:22 +0100

    refactor(cli): JSON.parse connections and nodes if they come as string in purge invalid workflow connections (no-changelog)

--- packages/cli/src/databases/migrations/mysqldb/1675940580449-PurgeInvalidWorkflowConnections.ts
+++ packages/cli/src/databases/migrations/mysqldb/1675940580449-PurgeInvalidWorkflowConnections.ts
@@ -21,8 +21,9 @@ export class PurgeInvalidWorkflowConnections1675940580449 implements MigrationIn
 		const nodeTypes = NodeTypes();
 
 		workflows.forEach(async (workflow) => {
-			let connections: IConnections = workflow.connections;
-			const nodes: INode[] = workflow.nodes;
+			let connections: IConnections = typeof workflow.connections === 'string' ? JSON.parse(workflow.connections) : workflow.connections;
+			const nodes: INode[] = typeof workflow.nodes === 'string' ? JSON.parse(workflow.nodes) : workflow.nodes;
+
 
 			const nodesThatCannotReceiveInput: string[] = nodes.reduce((acc, node) => {
 				try {
