7247eecd8d9bc8f886b1795dd506f96fc5877ba8
Milorad Filipovic <milorad@n8n.io>
2023-02-14 16:00:01 +0100

    ⚡ Added native Number and Object methods to autocomplete

---
+++ packages/workflow/src/NativeMethods/Object.Methods.ts
@@ -0,0 +1,25 @@
+export const objectMethods = {
+	typeName: 'Object',
+	functions: {
+		keys: {
+			doc: {
+				name: 'keys',
+				description:
+					"Returns an array of a given object's own enumerable string-keyed property names.",
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys',
+				returnType: 'Array',
+			},
+		},
+		values: {
+			doc: {
+				name: 'values',
+				description:
+					"Returns an array of a given object's own enumerable string-keyed property values.",
+				docURL:
+					'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values',
+				returnType: 'Array',
+			},
+		},
+	},
+};
--- packages/workflow/src/NativeMethods/index.ts
+++ packages/workflow/src/NativeMethods/index.ts
@@ -1,6 +1,8 @@
 import { stringMethods } from './String.methods';
 import { arrayMethods } from './Array.methods';
+import { numberMethods } from './Number.methods';
+import { objectMethods } from './Object.Methods';
 
-const NATIVE_METHODS = [stringMethods, arrayMethods];
+const NATIVE_METHODS = [stringMethods, arrayMethods, numberMethods, objectMethods];
 
 export { NATIVE_METHODS as NativeMethods };
