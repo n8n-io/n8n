faf04ec34a430f64d37a77a46649d7e9e0d58b84
Csaba Tuncsik <csaba@n8n.io>
2023-02-14 12:06:16 +0100

    test(editor): Execution test rename and add stop test

--- cypress/e2e/19-execution.cy.ts
+++ cypress/e2e/19-execution.cy.ts
@@ -5,12 +5,9 @@ const workflowPage = new WorkflowPageClass();
 const workflowsPage = new WorkflowsPage();
 
 describe('Execution',() => {
-	before(() => {
+	beforeEach(() => {
 		cy.resetAll();
 		cy.skipSetup();
-	});
-
-	beforeEach(() => {
 		cy.visit('/');
 	});
 
@@ -47,6 +44,47 @@ describe('Execution',() => {
 		workflowPage.getters.canvasNodeByName('Manual').within(() => cy.get('.fa-check')).should('be.visible');
 		workflowPage.getters.canvasNodeByName('Wait').within(() => cy.get('.fa-check')).should('be.visible');
 		workflowPage.getters.canvasNodeByName('Set').within(() => cy.get('.fa-check')).should('be.visible');
+
+		// Check success toast (works because Cypress waits enough for the element to show after the http request node has finished)
+		workflowPage.getters.successToast().should('be.visible');
+	});
+
+	it('should test manual workflow stop', () => {
+		// Import workflow
+		workflowsPage.getters.newWorkflowButtonCard().click();
+		cy.createFixtureWorkflow('Manual_waiting_set.json', `Manual waiting set ${uuid()}`);
+
+		// Check workflow buttons
+		workflowPage.getters.executeWorkflowButton().should('be.visible');
+		workflowPage.getters.clearExecutionDataButton().should('not.exist');
+		workflowPage.getters.stopExecutionButton().should('not.exist');
+		workflowPage.getters.stopExecutionWaitingForWebhookButton().should('not.exist');
+
+		// Execute the workflow
+		workflowPage.getters.zoomToFitButton().click();
+		workflowPage.getters.executeWorkflowButton().click();
+
+		// Check workflow buttons
+		workflowPage.getters.executeWorkflowButton().get('.n8n-spinner').should('be.visible');
+		workflowPage.getters.clearExecutionDataButton().should('not.exist');
+		workflowPage.getters.stopExecutionButton().should('be.visible');
+		workflowPage.getters.stopExecutionWaitingForWebhookButton().should('not.exist');
+
+		// Check canvas nodes after 1st step (workflow passed the manual trigger node
+		workflowPage.getters.canvasNodeByName('Manual').within(() => cy.get('.fa-check')).should('be.visible');
+		workflowPage.getters.canvasNodeByName('Wait').within(() => cy.get('.fa-check').should('not.exist'));
+		workflowPage.getters.canvasNodeByName('Wait').within(() => cy.get('.fa-sync-alt')).should('be.visible');
+		workflowPage.getters.canvasNodeByName('Set').within(() => cy.get('.fa-check').should('not.exist'));
+
+
+		cy.wait(1000);
+		workflowPage.getters.stopExecutionButton().click();
+
+		// Check canvas nodes after workflow stopped
+		workflowPage.getters.canvasNodeByName('Manual').within(() => cy.get('.fa-check')).should('be.visible');
+		workflowPage.getters.canvasNodeByName('Wait').within(() => cy.get('.fa-check')).should('be.visible');
+		workflowPage.getters.canvasNodeByName('Wait').within(() => cy.get('.fa-sync-alt').should('not.visible'));
+		workflowPage.getters.canvasNodeByName('Set').within(() => cy.get('.fa-check').should('not.exist'));
 
 		// Check success toast (works because Cypress waits enough for the element to show after the http request node has finished)
 		workflowPage.getters.successToast().should('be.visible');
