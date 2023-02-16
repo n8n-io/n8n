1056fd9d3143411d735ce57d84710b965fd7a8e7
Michael Kret <michael.k@radency.com>
2023-02-16 13:18:02 +0200

    :zap: send operation clean up

--- packages/nodes-base/nodes/Discord/v2/actions/message/send.operation.ts
+++ packages/nodes-base/nodes/Discord/v2/actions/message/send.operation.ts
@@ -11,6 +11,95 @@ import { discordApiMultiPartRequest, discordApiRequest } from '../../transport';
 import { textChannelRLC, userRLC } from '../common.description';
 
 import FormData from 'form-data';
+import { isEmpty } from 'lodash';
+
+const embedFields: INodeProperties[] = [
+	{
+		displayName: 'Author',
+		name: 'author',
+		type: 'string',
+		default: '',
+		description: 'The name of the author',
+		placeholder: 'e.g. John Doe',
+	},
+	{
+		displayName: 'Color',
+		name: 'color',
+		// eslint-disable-next-line n8n-nodes-base/node-param-color-type-unused
+		type: 'string',
+		default: '',
+		description: 'Color code of the embed',
+		placeholder: 'e.g. 12123432',
+	},
+	{
+		displayName: 'Description',
+		name: 'description',
+		type: 'string',
+		default: '',
+		description: 'The description of embed',
+		placeholder: 'e.g. My description',
+		typeOptions: {
+			rows: 2,
+		},
+	},
+	{
+		displayName: 'Timestamp',
+		name: 'timestamp',
+		type: 'string',
+		default: '',
+		description: 'The time displayed at the bottom of the embed. Provide in ISO8601 format.',
+		placeholder: 'e.g. 2023-02-08 09:30:26',
+	},
+	{
+		displayName: 'Title',
+		name: 'title',
+		type: 'string',
+		default: '',
+		description: 'The title of embed',
+		placeholder: "e.g. Embed's title",
+	},
+	{
+		displayName: 'URL',
+		name: 'url',
+		type: 'string',
+		default: '',
+		description: 'The URL where you want to link the embed to',
+		placeholder: 'e.g. https://discord.com/',
+	},
+	{
+		displayName: 'URL Image',
+		name: 'image',
+		type: 'string',
+		default: '',
+		description: 'Source URL of image (only supports http(s) and attachments)',
+		placeholder: 'e.g. https://example.com/image.png',
+	},
+	{
+		displayName: 'URL Thumbnail',
+		name: 'thumbnail',
+		type: 'string',
+		default: '',
+		description: 'Source URL of thumbnail (only supports http(s) and attachments)',
+		placeholder: 'e.g. https://example.com/image.png',
+	},
+	{
+		displayName: 'URL Video',
+		name: 'video',
+		type: 'string',
+		default: '',
+		description: 'Source URL of video',
+		placeholder: 'e.g. https://example.com/video.mp4',
+	},
+];
+
+export const embedFieldsDescription = updateDisplayOptions(
+	{
+		show: {
+			inputMethod: ['fields'],
+		},
+	},
+	embedFields,
+);
 
 const properties: INodeProperties[] = [
 	{
@@ -116,11 +205,20 @@ const properties: INodeProperties[] = [
 				name: 'values',
 				values: [
 					{
-						displayName: 'Use JSON',
-						name: 'useJson',
-						type: 'boolean',
-						default: false,
-						description: 'Whether to use JSON as the value',
+						displayName: 'Input Method',
+						name: 'inputMethod',
+						type: 'options',
+						options: [
+							{
+								name: 'Enter Fields',
+								value: 'fields',
+							},
+							{
+								name: 'Raw JSON',
+								value: 'json',
+							},
+						],
+						default: 'json',
 					},
 					{
 						displayName: 'JSON',
@@ -132,132 +230,11 @@ const properties: INodeProperties[] = [
 						},
 						displayOptions: {
 							show: {
-								useJson: [true],
+								inputMethod: ['json'],
 							},
 						},
 					},
-					{
-						displayName: 'Author',
-						name: 'author',
-						type: 'string',
-						default: '',
-						description: 'The name of the author',
-						placeholder: 'e.g. John Doe',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'Color',
-						name: 'color',
-						// eslint-disable-next-line n8n-nodes-base/node-param-color-type-unused
-						type: 'string',
-						default: '',
-						description: 'Color code of the embed',
-						placeholder: 'e.g. 12123432',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'Description',
-						name: 'description',
-						type: 'string',
-						default: '',
-						description: 'The description of embed',
-						placeholder: 'e.g. My description',
-						typeOptions: {
-							rows: 2,
-						},
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'Timestamp',
-						name: 'timestamp',
-						type: 'string',
-						default: '',
-						description:
-							'The time displayed at the bottom of the embed. Provide in ISO8601 format.',
-						placeholder: 'e.g. 2023-02-08 09:30:26',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'Title',
-						name: 'title',
-						type: 'string',
-						default: '',
-						description: 'The title of embed',
-						placeholder: "e.g. Embed's title",
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'URL',
-						name: 'url',
-						type: 'string',
-						default: '',
-						description: 'The URL where you want to link the embed to',
-						placeholder: 'e.g. https://discord.com/',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'URL Image',
-						name: 'image',
-						type: 'string',
-						default: '',
-						description: 'Source URL of image (only supports http(s) and attachments)',
-						placeholder: 'e.g. https://example.com/image.png',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'URL Thumbnail',
-						name: 'thumbnail',
-						type: 'string',
-						default: '',
-						description: 'Source URL of thumbnail (only supports http(s) and attachments)',
-						placeholder: 'e.g. https://example.com/image.png',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
-					{
-						displayName: 'URL Video',
-						name: 'video',
-						type: 'string',
-						default: '',
-						description: 'Source URL of video',
-						placeholder: 'e.g. https://example.com/video.mp4',
-						displayOptions: {
-							show: {
-								useJson: [false],
-							},
-						},
-					},
+					...embedFieldsDescription,
 				],
 			},
 		],
@@ -283,6 +260,7 @@ const properties: INodeProperties[] = [
 						default: 'data',
 						description: 'The contents of the file being sent with the message',
 						placeholder: 'e.g. data',
+						hint: 'The name of the input field containing the binary file data to be sent',
 					},
 				],
 			},
@@ -299,6 +277,124 @@ const displayOptions = {
 
 export const description = updateDisplayOptions(displayOptions, properties);
 
+function prepareOptions(options: IDataObject, guildId: string) {
+	if (options.flags) {
+		if ((options.flags as string[]).length === 2) {
+			options.flags = (1 << 2) + (1 << 12);
+		} else if ((options.flags as string[]).includes('SUPPRESS_EMBEDS')) {
+			options.flags = 1 << 2;
+		} else if ((options.flags as string[]).includes('SUPPRESS_NOTIFICATIONS')) {
+			options.flags = 1 << 12;
+		}
+	}
+
+	if (options.message_reference) {
+		options.message_reference = {
+			message_id: options.message_reference,
+			guild_id: guildId,
+		};
+	}
+
+	return options;
+}
+
+function prepareEmbeds(this: IExecuteFunctions, embeds: IDataObject[]) {
+	return embeds
+		.map((embed) => {
+			if (embed.inputMethod === 'json') {
+				if (typeof embed.json === 'object') {
+					return embed.json;
+				}
+				try {
+					return jsonParse(embed.json as string);
+				} catch (error) {
+					throw new NodeOperationError(this.getNode(), 'Not a valid JSON', error);
+				}
+			}
+
+			const embedReturnData: IDataObject = {};
+
+			delete embed.inputMethod;
+
+			for (const key of Object.keys(embed)) {
+				if (embed[key] !== '') {
+					embedReturnData[key] = embed[key];
+				}
+			}
+
+			if (embedReturnData.author) {
+				embedReturnData.author = {
+					name: embedReturnData.author,
+				};
+			}
+			if (embedReturnData.video) {
+				embedReturnData.video = {
+					url: embedReturnData.video,
+					width: 1270,
+					height: 720,
+				};
+			}
+			if (embedReturnData.thumbnail) {
+				embedReturnData.thumbnail = {
+					url: embedReturnData.thumbnail,
+				};
+			}
+			if (embedReturnData.image) {
+				embedReturnData.image = {
+					url: embedReturnData.image,
+				};
+			}
+
+			return embedReturnData.filter;
+		})
+		.filter((embed) => !isEmpty(embed));
+}
+
+async function prepareMultiPartForm(
+	this: IExecuteFunctions,
+	items: INodeExecutionData[],
+	files: IDataObject[],
+	jsonPayload: IDataObject,
+	i: number,
+) {
+	const multiPartBody = new FormData();
+	const attachments: IDataObject[] = [];
+	const filesData: IDataObject[] = [];
+
+	for (const [index, file] of files.entries()) {
+		const binaryData = (items[i].binary as IBinaryKeyData)?.[file.inputFieldName as string];
+
+		if (!binaryData) {
+			throw new NodeOperationError(
+				this.getNode(),
+				`Input item [${i}] does not contain binary data on property ${file.inputFieldName}`,
+			);
+		}
+		attachments.push({
+			id: index,
+			filename: binaryData.fileName,
+		});
+		filesData.push({
+			data: await this.helpers.getBinaryDataBuffer(i, file.inputFieldName as string),
+			name: binaryData.fileName,
+			mime: binaryData.mimeType,
+		});
+	}
+
+	multiPartBody.append('payload_json', JSON.stringify({ ...jsonPayload, attachments }), {
+		contentType: 'application/json',
+	});
+
+	for (const [index, binaryData] of filesData.entries()) {
+		multiPartBody.append(`files[${index}]`, binaryData.data, {
+			contentType: binaryData.name as string,
+			filename: binaryData.mime as string,
+		});
+	}
+
+	return multiPartBody;
+}
+
 export async function execute(
 	this: IExecuteFunctions,
 	guildId: string,
@@ -308,29 +404,12 @@ export async function execute(
 
 	for (let i = 0; i < items.length; i++) {
 		const content = this.getNodeParameter('content', i) as string;
-		const options = this.getNodeParameter('options', i, {});
+		const options = prepareOptions(this.getNodeParameter('options', i, {}), guildId);
 
 		const embeds = (this.getNodeParameter('embeds', i, undefined) as IDataObject)
 			?.values as IDataObject[];
 		const files = (this.getNodeParameter('files', i, undefined) as IDataObject)
 			?.values as IDataObject[];
-
-		if (options.flags) {
-			if ((options.flags as string[]).length === 2) {
-				options.flags = (1 << 2) + (1 << 12);
-			} else if ((options.flags as string[]).includes('SUPPRESS_EMBEDS')) {
-				options.flags = 1 << 2;
-			} else if ((options.flags as string[]).includes('SUPPRESS_NOTIFICATIONS')) {
-				options.flags = 1 << 12;
-			}
-		}
-
-		if (options.message_reference) {
-			options.message_reference = {
-				message_id: options.message_reference,
-				guild_id: guildId,
-			};
-		}
 
 		const body: IDataObject = {
 			content,
@@ -338,51 +417,7 @@ export async function execute(
 		};
 
 		if (embeds) {
-			body.embeds = embeds.map((embed) => {
-				if (embed.useJson) {
-					if (typeof embed.json === 'object') {
-						embed.json = embed.json;
-					}
-					try {
-						return jsonParse(embed.json as string);
-					} catch (error) {
-						throw new NodeOperationError(this.getNode(), 'Not a valid JSON', error);
-					}
-				}
-
-				const embedReturnData: IDataObject = {};
-
-				for (const key of Object.keys(embed)) {
-					if (embed[key] !== '') {
-						embedReturnData[key] = embed[key];
-					}
-				}
-
-				if (embedReturnData.author) {
-					embedReturnData.author = {
-						name: embedReturnData.author,
-					};
-				}
-				if (embedReturnData.video) {
-					embedReturnData.video = {
-						url: embedReturnData.video,
-						width: 1270,
-						height: 720,
-					};
-				}
-				if (embedReturnData.thumbnail) {
-					embedReturnData.thumbnail = {
-						url: embedReturnData.thumbnail,
-					};
-				}
-				if (embedReturnData.image) {
-					embedReturnData.image = {
-						url: embedReturnData.image,
-					};
-				}
-
-				return embedReturnData;
-			});
+			body.embeds = prepareEmbeds.call(this, embeds);
 		}
 
 		try {
@@ -415,42 +450,7 @@ export async function execute(
 			let response: IDataObject[] = [];
 
 			if (files.length) {
-				const multiPartBody = new FormData();
-				const attachments: IDataObject[] = [];
-				const filesData: IDataObject[] = [];
-
-				for (const [index, file] of files.entries()) {
-					const binaryData = (items[i].binary as IBinaryKeyData)?.[file.inputFieldName as string];
-
-					if (!binaryData) {
-						throw new NodeOperationError(
-							this.getNode(),
-							`Input item [${i}] does not contain binary data on property ${file.inputFieldName}`,
-						);
-					}
-					attachments.push({
-						id: index,
-						filename: binaryData.fileName,
-					});
-					filesData.push({
-						data: await this.helpers.getBinaryDataBuffer(i, file.inputFieldName as string),
-						name: binaryData.fileName,
-						mime: binaryData.mimeType,
-					});
-				}
-
-				body.attachments = attachments;
-
-				multiPartBody.append('payload_json', JSON.stringify(body), {
-					contentType: 'application/json',
-				});
-
-				for (const [index, binaryData] of filesData.entries()) {
-					multiPartBody.append(`files[${index}]`, binaryData.data, {
-						contentType: binaryData.name as string,
-						filename: binaryData.mime as string,
-					});
-				}
+				const multiPartBody = await prepareMultiPartForm.call(this, items, files, body, i);
 
 				response = await discordApiMultiPartRequest.call(
 					this,
