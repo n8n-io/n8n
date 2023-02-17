31845a1696e566e5703f0e3268992849d4c94942
Michael Kret <michael.k@radency.com>
2023-02-17 12:08:26 +0200

    :zap: typo fix

--- packages/nodes-base/credentials/DiscordWebhookApi.credentials.ts
+++ packages/nodes-base/credentials/DiscordWebhookApi.credentials.ts
@@ -10,7 +10,7 @@ export class DiscordWebhookApi implements ICredentialType {
 	properties: INodeProperties[] = [
 		{
 			displayName:
-				'Follow instrucitons of how to add webhook to a channel on <a href="https://docs.n8n.io/integrations/builtin/credentials/discord/#creating-a-webhook-in-discord" target="_blank">this page</a>. <br/>Keep in my that only <strong>Webhook</strong> resource would be available.  <br/>To have access to other resources please use <strong>OAuth2</strong> authentication',
+				'Follow instrucitons of how to add webhook to a channel on <a href="https://docs.n8n.io/integrations/builtin/credentials/discord/#creating-a-webhook-in-discord" target="_blank">this page</a>. <br/>Keep in mind that only <strong>Webhook</strong> resource would be available when this authentication method is used.  <br/>To have access to other resources please use <strong>OAuth2</strong> authentication',
 			name: 'notice',
 			type: 'notice',
 			default: '',
