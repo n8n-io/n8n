eb9cbb4e6f86e49990f047f0bd381470cf1e7099
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-02-20 17:59:33 +0100

    add unit tests for TranslationController

--- packages/cli/src/controllers/translation.controller.ts
+++ packages/cli/src/controllers/translation.controller.ts
@@ -7,30 +7,34 @@ import { BadRequestError, InternalServerError } from '@/ResponseHelper';
 import { Config } from '@/config';
 import { NODES_BASE_DIR } from '@/constants';
 
-const credentialTranslationsDir = join(NODES_BASE_DIR, 'dist/credentials/translations');
-const nodeHeadersPath = join(NODES_BASE_DIR, 'dist/nodes/headers');
+export const CREDENTIAL_TRANSLATIONS_DIR = 'n8n-nodes-base/dist/credentials/translations';
+export const NODE_HEADERS_PATH = join(NODES_BASE_DIR, 'dist/nodes/headers');
+
+export declare namespace TranslationRequest {
+	export type Credential = Request<{}, {}, {}, { credentialType: string }>;
+}
 
 @RestController('/')
 export class TranslationController {
 	constructor(private config: Config, private credentialTypes: ICredentialTypes) {}
 
 	@Get('/credential-translation')
-	async getCredentialTranslation(req: Request & { query: { credentialType: string } }) {
+	async getCredentialTranslation(req: TranslationRequest.Credential) {
 		const { credentialType } = req.query;
 
 		if (!this.credentialTypes.recognizes(credentialType))
-			throw new BadRequestError(`Invalid Credential type ${credentialType}`);
+			throw new BadRequestError(`Invalid Credential type: "${credentialType}"`);
 
 		const defaultLocale = this.config.getEnv('defaultLocale');
 		const translationPath = join(
-			credentialTranslationsDir,
+			CREDENTIAL_TRANSLATIONS_DIR,
 			defaultLocale,
 			`${credentialType}.json`,
 		);
 
 		try {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-			return await import(translationPath);
+			return require(translationPath);
 		} catch (error) {
 			return null;
 		}
@@ -39,14 +43,14 @@ export class TranslationController {
 	@Get('/node-translation-headers')
 	async getNodeTranslationHeaders() {
 		try {
-			await access(`${nodeHeadersPath}.js`);
+			await access(`${NODE_HEADERS_PATH}.js`);
 		} catch (_) {
 			return; // no headers available
 		}
 
 		try {
 			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-			return await import(nodeHeadersPath);
+			return require(NODE_HEADERS_PATH);
 		} catch (error) {
 			throw new InternalServerError('Failed to load headers file');
 		}
