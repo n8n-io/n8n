fb07d77106bb4933758c63bbfb87f591bf4a27dd
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-02-21 11:21:04 +0100

    fix(core): Do not allow arbitrary path traversal in the credential-translation endpoint (#5522)

--- packages/cli/src/controllers/index.ts
+++ packages/cli/src/controllers/index.ts
@@ -2,4 +2,5 @@ export { AuthController } from './auth.controller';
 export { MeController } from './me.controller';
 export { OwnerController } from './owner.controller';
 export { PasswordResetController } from './passwordReset.controller';
+export { TranslationController } from './translation.controller';
 export { UsersController } from './users.controller';
