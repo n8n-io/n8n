49effe1edddc001b95cfbcf61c7aad85ee17ddaf
Marcus <marcus@n8n.io>
2023-02-21 15:39:14 +0100

    ⚡️disable chart types we don't want to support in P0

--- packages/nodes-base/nodes/QuickChart/constants.ts
+++ packages/nodes-base/nodes/QuickChart/constants.ts
@@ -1,18 +1,19 @@
 import type { INodePropertyOptions } from 'n8n-workflow';
 
+// Disable some charts that use different datasets for now
 export const CHART_TYPE_OPTIONS: INodePropertyOptions[] = [
 	{
 		name: 'Bar Chart',
 		value: 'bar',
 	},
-	{
-		name: 'Boxplot',
-		value: 'boxplot',
-	},
-	{
-		name: 'Bubble Chart',
-		value: 'bubble',
-	},
+	// {
+	// 	name: 'Boxplot',
+	// 	value: 'boxplot',
+	// },
+	// {
+	// 	name: 'Bubble Chart',
+	// 	value: 'bubble',
+	// },
 	{
 		name: 'Doughnut Chart',
 		value: 'doughnut',
@@ -29,26 +30,26 @@ export const CHART_TYPE_OPTIONS: INodePropertyOptions[] = [
 		name: 'Polar Chart',
 		value: 'polar',
 	},
-	{
-		name: 'Radar Chart',
-		value: 'radar',
-	},
-	{
-		name: 'Radial Gauge',
-		value: 'radialGauge',
-	},
-	{
-		name: 'Scatter Chart',
-		value: 'scatter',
-	},
-	{
-		name: 'Sparkline',
-		value: 'sparkline',
-	},
-	{
-		name: 'Violin Chart',
-		value: 'violin',
-	},
+	// {
+	// 	name: 'Radar Chart',
+	// 	value: 'radar',
+	// },
+	// {
+	// 	name: 'Radial Gauge',
+	// 	value: 'radialGauge',
+	// },
+	// {
+	// 	name: 'Scatter Chart',
+	// 	value: 'scatter',
+	// },
+	// {
+	// 	name: 'Sparkline',
+	// 	value: 'sparkline',
+	// },
+	// {
+	// 	name: 'Violin Chart',
+	// 	value: 'violin',
+	// },
 ];
 
 export const HORIZONTAL_CHARTS = ['bar', 'boxplot', 'violin'];
