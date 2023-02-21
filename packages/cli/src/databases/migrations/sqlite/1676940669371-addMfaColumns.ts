ec1f43aaf43c8bfa2d10dcf8fa3e54c4e75b1e70
ricardo <ricardoespinoza105@gmail.com>
2023-02-20 20:21:39 -0500

    :zap: simplify MFA migration

--- packages/cli/src/databases/migrations/sqlite/1676940669371-addMfaColumns.ts
+++ packages/cli/src/databases/migrations/sqlite/1676940669371-addMfaColumns.ts
@@ -10,69 +10,23 @@ export class AddMfaColumns1676940669371 implements MigrationInterface {
 
 		const tablePrefix = getTablePrefix();
 
-		await queryRunner.query('PRAGMA foreign_keys=OFF');
-
 		await queryRunner.query(
-			`CREATE TABLE "temporary_user" (
-				"id" varchar PRIMARY KEY NOT NULL,
-				"email" varchar(255),
-				"firstName" varchar(32),
-				"lastName" varchar(32),
-				"password" varchar,
-				"resetPasswordToken" varchar,
-				"resetPasswordTokenExpiration" integer DEFAULT NULL,
-				"mfaEnabled" boolean DEFAULT false,
-				"mfaSecret" varchar DEFAULT NULL,
-				"mfaRecoveryCodes" varchar DEFAULT NULL,
-				"personalizationAnswers" text,
-				"createdAt" datetime(3) NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
-				"updatedAt" datetime(3) NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
-				"globalRoleId" integer NOT NULL,
-				"settings" text,
-				"apiKey" varchar DEFAULT NULL,
-				CONSTRAINT "FK_${tablePrefix}f0609be844f9200ff4365b1bb3d" FOREIGN KEY ("globalRoleId") REFERENCES "${tablePrefix}role" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+			`ALTER TABLE "${tablePrefix}user" ADD COLUMN "mfaEnabled" boolean DEFAULT false`,
 		);
-
 		await queryRunner.query(
-			`INSERT INTO "temporary_user"(
-				"id",
-				"email",
-				"firstName",
-				"lastName",
-				"password",
-				"resetPasswordToken",
-				"resetPasswordTokenExpiration",
-				"personalizationAnswers",
-				"createdAt",
-				"updatedAt",
-				"globalRoleId",
-				"settings",
-				"apiKey"
-				) SELECT
-				"id",
-				"email",
-				"firstName",
-				"lastName",
-				"password",
-				"resetPasswordToken",
-				"resetPasswordTokenExpiration",
-				"personalizationAnswers",
-				"createdAt",
-				"updatedAt",
-				"globalRoleId",
-				"settings",
-				"apiKey"
-				FROM "${tablePrefix}user"`,
+			`ALTER TABLE "${tablePrefix}user" ADD COLUMN "mfaSecret" varchar DEFAULT NULL`,
 		);
-
-		await queryRunner.query(`DROP TABLE "${tablePrefix}user"`);
-
-		await queryRunner.query(`ALTER TABLE "temporary_user" RENAME TO "${tablePrefix}user"`);
-
-		await queryRunner.query('PRAGMA foreign_keys=ON');
+		await queryRunner.query(
+			`ALTER TABLE "${tablePrefix}user" ADD COLUMN "mfaRecoveryCodes" varchar DEFAULT NULL`,
+		);
 
 		logMigrationEnd(this.name);
 	}
 
-	async down(queryRunner: QueryRunner): Promise<void> {}
+	async down(queryRunner: QueryRunner): Promise<void> {
+		const tablePrefix = getTablePrefix();
+		await queryRunner.query(`ALTER TABLE "${tablePrefix}user" DROP COLUMN "mfaEnabled"`);
+		await queryRunner.query(`ALTER TABLE "${tablePrefix}user" DROP COLUMN "mfaSecret"`);
+		await queryRunner.query(`ALTER TABLE "${tablePrefix}user" DROP COLUMN "mfaRecoveryCodes"`);
+	}
 }
