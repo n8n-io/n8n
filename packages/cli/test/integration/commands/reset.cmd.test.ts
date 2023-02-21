c157fc4fe4fbfbf767b4ebaf8b3116bb639fe3f9
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-02-21 19:21:56 +0100

    refactor(core): Use an IoC container to manage singleton classes [Part-1] (no-changelog) (#5509)
    * add typedi
    
    * convert ActiveWorkflowRunner into an injectable service
    
    * convert ExternalHooks into an injectable service
    
    * convert InternalHooks into an injectable service
    
    * convert LoadNodesAndCredentials into an injectable service
    
    * convert NodeTypes and CredentialTypes into an injectable service
    
    * convert ActiveExecutions into an injectable service
    
    * convert WaitTracker into an injectable service
    
    * convert Push into an injectable service
    
    * convert ActiveWebhooks and  TestWebhooks into an injectable services
    
    * handle circular references, and log errors when a circular dependency is found

--- packages/cli/test/integration/commands/reset.cmd.test.ts
+++ packages/cli/test/integration/commands/reset.cmd.test.ts
@@ -2,10 +2,17 @@ import * as Db from '@/Db';
 import { Reset } from '@/commands/user-management/reset';
 import type { Role } from '@db/entities/Role';
 import * as testDb from '../shared/testDb';
+import { mockInstance } from '../shared/utils';
+import { InternalHooks } from '@/InternalHooks';
+import { LoadNodesAndCredentials } from '@/LoadNodesAndCredentials';
+import { NodeTypes } from '@/NodeTypes';
 
 let globalOwnerRole: Role;
 
 beforeAll(async () => {
+	mockInstance(InternalHooks);
+	mockInstance(LoadNodesAndCredentials);
+	mockInstance(NodeTypes);
 	await testDb.init();
 
 	globalOwnerRole = await testDb.getGlobalOwnerRole();
