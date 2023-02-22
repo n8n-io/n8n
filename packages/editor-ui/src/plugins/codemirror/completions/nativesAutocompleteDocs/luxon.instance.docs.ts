85955605b2f02339c9ff8c6efa6d3a5c2aea0bfa
Milorad Filipovic <milorad@n8n.io>
2023-02-22 09:51:04 +0100

    ⚡ Added correct doc info to Luxon instance properties

--- packages/editor-ui/src/plugins/codemirror/completions/nativesAutocompleteDocs/luxon.instance.docs.ts
+++ packages/editor-ui/src/plugins/codemirror/completions/nativesAutocompleteDocs/luxon.instance.docs.ts
@@ -8,222 +8,222 @@ export const luxonInstanceDocs: Required<NativeDoc> = {
 		day: {
 			doc: {
 				name: 'day',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeday',
 				returnType: 'number',
 			},
 		},
 		daysInMonth: {
 			doc: {
 				name: 'daysInMonth',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimedaysinmonth',
 				returnType: 'number',
 			},
 		},
 		daysInYear: {
 			doc: {
 				name: 'daysInYear',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimedaysinyear',
 				returnType: 'number',
 			},
 		},
 		hour: {
 			doc: {
 				name: 'hour',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimehour',
 				returnType: 'number',
 			},
 		},
 		locale: {
 			doc: {
 				name: 'locale',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimelocale',
+				returnType: 'string',
 			},
 		},
 		millisecond: {
 			doc: {
 				name: 'millisecond',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimemillisecond',
 				returnType: 'number',
 			},
 		},
 		minute: {
 			doc: {
 				name: 'minute',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeminute',
 				returnType: 'number',
 			},
 		},
 		month: {
 			doc: {
 				name: 'month',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimemonth',
 				returnType: 'number',
 			},
 		},
 		monthLong: {
 			doc: {
 				name: 'monthLong',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimemonthlong',
+				returnType: 'string',
 			},
 		},
 		monthShort: {
 			doc: {
 				name: 'monthShort',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimemonthshort',
+				returnType: 'string',
 			},
 		},
 		numberingSystem: {
 			doc: {
 				name: 'numberingSystem',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimenumberingsystem',
+				returnType: 'string',
 			},
 		},
 		offset: {
 			doc: {
 				name: 'offset',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeoffset',
 				returnType: 'number',
 			},
 		},
 		offsetNameLong: {
 			doc: {
 				name: 'offsetNameLong',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeoffsetnamelong',
+				returnType: 'string',
 			},
 		},
 		offsetNameShort: {
 			doc: {
 				name: 'offsetNameShort',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeoffsetnameshort',
+				returnType: 'string',
 			},
 		},
 		ordinal: {
 			doc: {
 				name: 'ordinal',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeordinal',
+				returnType: 'string',
 			},
 		},
 		outputCalendar: {
 			doc: {
 				name: 'outputCalendar',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeoutputcalendar',
+				returnType: 'string',
 			},
 		},
 		quarter: {
 			doc: {
 				name: 'quarter',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimequarter',
 				returnType: 'number',
 			},
 		},
 		second: {
 			doc: {
 				name: 'second',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimesecond',
 				returnType: 'number',
 			},
 		},
 		weekday: {
 			doc: {
 				name: 'weekday',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweekday',
 				returnType: 'number',
 			},
 		},
 		weekdayLong: {
 			doc: {
 				name: 'weekdayLong',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweekdaylong',
+				returnType: 'string',
 			},
 		},
 		weekdayShort: {
 			doc: {
 				name: 'weekdayShort',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweekdayshort',
+				returnType: 'string',
 			},
 		},
 		weekNumber: {
 			doc: {
 				name: 'weekNumber',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweeknumber',
 				returnType: 'number',
 			},
 		},
 		weeksInWeekYear: {
 			doc: {
 				name: 'weeksInWeekYear',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweeksinweekyear',
 				returnType: 'number',
 			},
 		},
 		weekYear: {
 			doc: {
 				name: 'weekYear',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeweekyear',
 				returnType: 'number',
 			},
 		},
 		year: {
 			doc: {
 				name: 'year',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeyear',
 				returnType: 'number',
 			},
 		},
 		zone: {
 			doc: {
 				name: 'zone',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimezone',
+				returnType: 'Zone',
 			},
 		},
 		zoneName: {
 			doc: {
 				name: 'zoneName',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimezonename',
+				returnType: 'string',
 			},
 		},
-	},
-	functions: {
 		isInDST: {
 			doc: {
 				name: 'isInDST',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeisindst',
+				returnType: 'boolean',
 			},
 		},
 		isInLeapYear: {
 			doc: {
 				name: 'isInLeapYear',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeisinleapyear',
+				returnType: 'boolean',
 			},
 		},
 		isOffsetFixed: {
 			doc: {
 				name: 'isOffsetFixed',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeisoffsetfixed',
+				returnType: 'boolean',
 			},
 		},
 		isValid: {
 			doc: {
 				name: 'isValid',
-				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetime',
-				returnType: 'number',
+				docURL: 'https://moment.github.io/luxon/api-docs/index.html#datetimeisvalid',
+				returnType: 'boolean',
 			},
 		},
+	},
+	functions: {
 		diff: {
 			doc: {
 				name: 'diff',
