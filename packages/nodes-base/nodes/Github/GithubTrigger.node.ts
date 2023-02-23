e0f894c2eb403eb377815f02e220ac9e8dbfff9a
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2023-02-23 08:59:26 +0000

    docs(Github Trigger Node): Added notice and more meaningful error to mention organization permissions for creating webhooks

--- packages/nodes-base/nodes/Github/GithubTrigger.node.ts
+++ packages/nodes-base/nodes/Github/GithubTrigger.node.ts
@@ -56,6 +56,13 @@ export class GithubTrigger implements INodeType {
 		],
 		properties: [
 			{
+				displayName:
+					'Only members with owner privileges for an organization or admin privileges for a repository can manage webhooks for an organization.',
+				name: 'notice',
+				type: 'notice',
+				default: '',
+			},
+			{
 				displayName: 'Authentication',
 				name: 'authentication',
 				type: 'options',
@@ -527,6 +534,13 @@ export class GithubTrigger implements INodeType {
 						throw new NodeOperationError(
 							this.getNode(),
 							'A webhook with the identical URL probably exists already. Please delete it manually on Github!',
+						);
+					}
+
+					if (error.cause.httpCode === '404') {
+						throw new NodeOperationError(
+							this.getNode(),
+							'Check that the repository exists and that you have permission to create webhooks for it',
 						);
 					}
 
