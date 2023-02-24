8d6fd00634600abcc55723bd72fdcd08c0866005
Omar Ajoue <krynble@gmail.com>
2023-02-24 14:53:49 +0100

    fix: Prevent executions from displaying as running forever (#5563)
    * fix: Prevent executions from displaying as running forever
    
    * fix: Correct migration query

--- packages/cli/src/databases/migrations/mysqldb/index.ts
+++ packages/cli/src/databases/migrations/mysqldb/index.ts
@@ -33,6 +33,7 @@ import { CreateLdapEntities1674509946020 } from './1674509946020-CreateLdapEntit
 import { PurgeInvalidWorkflowConnections1675940580449 } from './1675940580449-PurgeInvalidWorkflowConnections';
 import { AddStatusToExecutions1674138566000 } from './1674138566000-AddStatusToExecutions';
 import { MigrateExecutionStatus1676996103000 } from './1676996103000-MigrateExecutionStatus';
+import { UpdateRunningExecutionStatus1677236788851 } from './1677236788851-UpdateRunningExecutionStatus';
 
 export const mysqlMigrations = [
 	InitialMigration1588157391238,
@@ -70,4 +71,5 @@ export const mysqlMigrations = [
 	PurgeInvalidWorkflowConnections1675940580449,
 	AddStatusToExecutions1674138566000,
 	MigrateExecutionStatus1676996103000,
+	UpdateRunningExecutionStatus1677236788851,
 ];
