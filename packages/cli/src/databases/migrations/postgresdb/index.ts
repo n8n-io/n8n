055e50cafde91cadb27c793b5b53a76ffaa0a27b
Omar Ajoue <krynble@gmail.com>
2023-02-24 14:53:49 +0100

    fix: Prevent executions from displaying as running forever (#5563)
    * fix: Prevent executions from displaying as running forever
    
    * fix: Correct migration query

--- packages/cli/src/databases/migrations/postgresdb/index.ts
+++ packages/cli/src/databases/migrations/postgresdb/index.ts
@@ -31,6 +31,7 @@ import { CreateLdapEntities1674509946020 } from './1674509946020-CreateLdapEntit
 import { PurgeInvalidWorkflowConnections1675940580449 } from './1675940580449-PurgeInvalidWorkflowConnections';
 import { AddStatusToExecutions1674138566000 } from './1674138566000-AddStatusToExecutions';
 import { MigrateExecutionStatus1676996103000 } from './1676996103000-MigrateExecutionStatus';
+import { UpdateRunningExecutionStatus1677236854063 } from './1677236854063-UpdateRunningExecutionStatus';
 
 export const postgresMigrations = [
 	InitialMigration1587669153312,
@@ -66,4 +67,5 @@ export const postgresMigrations = [
 	PurgeInvalidWorkflowConnections1675940580449,
 	AddStatusToExecutions1674138566000,
 	MigrateExecutionStatus1676996103000,
+	UpdateRunningExecutionStatus1677236854063,
 ];
