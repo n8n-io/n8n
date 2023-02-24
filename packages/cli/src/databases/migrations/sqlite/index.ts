643a7f38169a009fe8472d1be35e94343fc02d6f
Omar Ajoue <krynble@gmail.com>
2023-02-24 12:12:49 +0100

    fix: Prevent executions from displaying as running forever

--- packages/cli/src/databases/migrations/sqlite/index.ts
+++ packages/cli/src/databases/migrations/sqlite/index.ts
@@ -30,6 +30,7 @@ import { CreateLdapEntities1674509946020 } from './1674509946020-CreateLdapEntit
 import { PurgeInvalidWorkflowConnections1675940580449 } from './1675940580449-PurgeInvalidWorkflowConnections';
 import { AddStatusToExecutions1674138566000 } from './1674138566000-AddStatusToExecutions';
 import { MigrateExecutionStatus1676996103000 } from './1676996103000-MigrateExecutionStatus';
+import { UpdateRunningExecutionStatus1677237073720 } from './1677237073720-UpdateRunningExecutionStatus';
 
 const sqliteMigrations = [
 	InitialMigration1588102412422,
@@ -64,6 +65,7 @@ const sqliteMigrations = [
 	PurgeInvalidWorkflowConnections1675940580449,
 	AddStatusToExecutions1674138566000,
 	MigrateExecutionStatus1676996103000,
+	UpdateRunningExecutionStatus1677237073720,
 ];
 
 export { sqliteMigrations };
