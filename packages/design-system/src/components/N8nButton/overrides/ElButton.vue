3cc5390dcdcc40d1d0783d58f1e42692baa6a6c6
Csaba Tuncsik <csaba@n8n.io>
2023-03-03 09:34:06 +0100

    fix(editor): Fix ElButton overrides

--- packages/design-system/src/components/N8nButton/overrides/ElButton.vue
+++ packages/design-system/src/components/N8nButton/overrides/ElButton.vue
@@ -10,6 +10,7 @@ import N8nButton from '../Button.vue';
 
 const classToTypeMap = {
 	'btn--cancel': 'secondary',
+	'el-picker-panel__link-btn': 'secondary',
 };
 
 export default Vue.extend({
@@ -18,16 +19,24 @@ export default Vue.extend({
 	},
 	computed: {
 		attrs() {
-			let type = 'primary';
+			let type = this.$attrs.type || 'primary';
+
+			// Element UI Button can have 'text' type which is not supported by n8n-button so render it as 'secondary'
+			if (type === 'text') {
+				type = 'secondary';
+			}
+
 			Object.entries(classToTypeMap).forEach(([className, mappedType]) => {
 				if (this.$refs.button && (this.$refs.button as Vue).$el.classList.contains(className)) {
 					type = mappedType;
 				}
 			});
 
+			delete this.$attrs.type;
+
 			return {
-				type,
 				...this.$attrs,
+				type,
 			};
 		},
 	},
