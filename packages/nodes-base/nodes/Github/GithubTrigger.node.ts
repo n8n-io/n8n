3c57062571161400221ffa85f1571278d39afcee
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-03-03 18:49:19 +0100

    refactor: Fix some typos (no-changelog) (#5616)

--- packages/nodes-base/nodes/Github/GithubTrigger.node.ts
+++ packages/nodes-base/nodes/Github/GithubTrigger.node.ts
@@ -564,7 +564,7 @@ export class GithubTrigger implements INodeType {
 					}
 
 					// Remove from the static workflow data so that it is clear
-					// that no webhooks are registred anymore
+					// that no webhooks are registered anymore
 					delete webhookData.webhookId;
 					delete webhookData.webhookEvents;
 				}
