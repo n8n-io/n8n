dc60b514fc5129bfe63becddd1445b1272c574fc
Michael Auerswald <michael.auerswald@gmail.com>
2023-03-06 17:45:25 +0100

    create SP through parameters instead of metadata

--- packages/cli/src/sso/saml/serviceProvider.ee.ts
+++ packages/cli/src/sso/saml/serviceProvider.ee.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { getInstanceBaseUrl } from '@/UserManagement/UserManagementHelper';
 import type { ServiceProviderInstance } from 'samlify';
 import { ServiceProvider } from 'samlify';
@@ -5,25 +6,27 @@ import { SamlUrls } from './constants';
 
 let serviceProviderInstance: ServiceProviderInstance | undefined;
 
-const metadata = `
-<EntityDescriptor
- xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
- xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
- xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
- entityID="${getInstanceBaseUrl() + SamlUrls.restMetadata}">
-    <SPSSODescriptor WantAssertionsSigned="true" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
-        <NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</NameIDFormat>
-        <AssertionConsumerService isDefault="true" index="0" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="${
-					getInstanceBaseUrl() + SamlUrls.restAcs
-				}"/>
-    </SPSSODescriptor>
-</EntityDescriptor>
-`;
-
+// TODO:SAML: make these configurable for the end user
 export function getServiceProviderInstance(): ServiceProviderInstance {
 	if (serviceProviderInstance === undefined) {
 		serviceProviderInstance = ServiceProvider({
-			metadata,
+			entityID: getInstanceBaseUrl() + SamlUrls.restMetadata,
+			authnRequestsSigned: false,
+			wantAssertionsSigned: true,
+			wantMessageSigned: true,
+			nameIDFormat: ['urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'],
+			assertionConsumerService: [
+				{
+					isDefault: true,
+					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
+				},
+				{
+					isDefault: false,
+					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-REDIRECT',
+					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
+				},
+			],
 		});
 	}
 
