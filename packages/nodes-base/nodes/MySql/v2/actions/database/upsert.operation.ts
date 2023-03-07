265f0bf963d87bf1a724ffc4de4818f9536bc19f
Michael Kret <michael.k@radency.com>
2023-03-07 15:48:33 +0200

    :zap: upsert operation

--- packages/nodes-base/nodes/MySql/v2/actions/database/upsert.operation.ts
+++ packages/nodes-base/nodes/MySql/v2/actions/database/upsert.operation.ts
@@ -1,7 +1,7 @@
 import type { IExecuteFunctions } from 'n8n-core';
-import type { INodeExecutionData, INodeProperties } from 'n8n-workflow';
+import type { IDataObject, INodeExecutionData, INodeProperties } from 'n8n-workflow';
 
-import type { QueryMode, QueryWithValues } from '../../helpers/interfaces';
+import type { QueryValues, QueryWithValues } from '../../helpers/interfaces';
 
 import { updateDisplayOptions } from '../../../../../utils/utilities';
 
@@ -9,7 +9,98 @@ import { runQueries } from '../../helpers/utils';
 
 import { optionsCollection, tableRLC } from '../common.descriptions';
 
-const properties: INodeProperties[] = [tableRLC, optionsCollection];
+const properties: INodeProperties[] = [
+	tableRLC,
+	{
+		displayName: 'Data Mode',
+		name: 'dataMode',
+		type: 'options',
+		options: [
+			{
+				name: 'Auto-Map Input Data to Columns',
+				value: 'autoMapInputData',
+				description: 'Use when node input properties match destination column names',
+			},
+			{
+				name: 'Map Each Column Below',
+				value: 'defineBelow',
+				description: 'Set the value for each destination column',
+			},
+		],
+		default: 'autoMapInputData',
+		description: 'Whether to insert the input data this node receives in the new row',
+	},
+	{
+		// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased, n8n-nodes-base/node-param-display-name-wrong-for-dynamic-options
+		displayName: 'Column to match on',
+		name: 'columnToMatchOn',
+		type: 'options',
+		required: true,
+		description:
+			'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
+		typeOptions: {
+			loadOptionsMethod: 'getColumns',
+			loadOptionsDependsOn: ['schema.value', 'table.value'],
+		},
+		default: '',
+		hint: "Used to find the correct row to update. Doesn't get changed.",
+	},
+	{
+		displayName: 'Value of Column to Match On',
+		name: 'valueToMatchOn',
+		type: 'string',
+		default: '',
+		displayOptions: {
+			show: {
+				dataMode: ['defineBelow'],
+			},
+		},
+	},
+	{
+		displayName: 'Values to Send',
+		name: 'valuesToSend',
+		placeholder: 'Add Value',
+		type: 'fixedCollection',
+		typeOptions: {
+			multipleValueButtonText: 'Add Value',
+			multipleValues: true,
+		},
+		displayOptions: {
+			show: {
+				dataMode: ['defineBelow'],
+			},
+		},
+		default: {},
+		options: [
+			{
+				displayName: 'Values',
+				name: 'values',
+				values: [
+					{
+						// eslint-disable-next-line n8n-nodes-base/node-param-display-name-wrong-for-dynamic-options
+						displayName: 'Column',
+						name: 'column',
+						type: 'options',
+						description:
+							'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
+						typeOptions: {
+							loadOptionsMethod: 'getColumnsWithoutColumnToMatchOn',
+							loadOptionsDependsOn: ['schema.value', 'table.value'],
+						},
+						default: [],
+					},
+					{
+						displayName: 'Value',
+						name: 'value',
+						type: 'string',
+						default: '',
+					},
+				],
+			},
+		],
+	},
+	optionsCollection,
+];
 
 const displayOptions = {
 	show: {
@@ -24,16 +115,60 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 	let returnData: INodeExecutionData[] = [];
 
 	const items = this.getInputData();
-	const _table = this.getNodeParameter('table', 0, '', { extractValue: true }) as string;
 
 	const nodeOptions = this.getNodeParameter('options', 0);
-	const queryBatching = (nodeOptions.queryBatching as QueryMode) || 'multiple';
 
 	const queries: QueryWithValues[] = [];
 
-	if (queryBatching === 'multiple') {
-	} else {
-		for (let i = 0; i < items.length; i++) {}
+	for (let i = 0; i < items.length; i++) {
+		const table = this.getNodeParameter('table', i, undefined, {
+			extractValue: true,
+		}) as string;
+
+		const columnToMatchOn = this.getNodeParameter('columnToMatchOn', i) as string;
+
+		const dataMode = this.getNodeParameter('dataMode', i) as string;
+
+		let item: IDataObject = {};
+
+		if (dataMode === 'autoMapInputData') {
+			item = items[i].json;
+		}
+
+		if (dataMode === 'defineBelow') {
+			const valuesToSend = (this.getNodeParameter('valuesToSend', i, []) as IDataObject)
+				.values as IDataObject[];
+
+			item = valuesToSend.reduce((acc, { column, value }) => {
+				acc[column as string] = value;
+				return acc;
+			}, {} as IDataObject);
+
+			item[columnToMatchOn] = this.getNodeParameter('valueToMatchOn', i) as string;
+		}
+
+		const onConflict = 'ON DUPLICATE KEY UPDATE';
+
+		const columns = Object.keys(item);
+		const escapedColumns = columns.map((column) => `\`${column}\``).join(', ');
+		const placeholder = `${columns.map(() => '?').join(',')}`;
+
+		const insertQuery = `INSERT INTO \`${table}\`(${escapedColumns}) VALUES(${placeholder})`;
+
+		const values = Object.values(item) as QueryValues;
+
+		const updateColumns = Object.keys(item).filter((column) => column !== columnToMatchOn);
+
+		const updates: string[] = [];
+
+		for (const column of updateColumns) {
+			updates.push(`\`${column}\` = ?`);
+			values.push(item[column] as string);
+		}
+
+		const query = `${insertQuery} ${onConflict} ${updates.join(', ')}`;
+
+		queries.push({ query, values });
 	}
 
 	returnData = await runQueries.call(this, queries, nodeOptions);
