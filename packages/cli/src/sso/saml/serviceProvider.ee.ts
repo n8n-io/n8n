3bf41bd7ee4fda55cf26964cf43d6f5ee95776ad
Michael Auerswald <michael.auerswald@gmail.com>
2023-03-07 13:07:34 +0100

    refactor SAML prefs and add SP configurations

--- packages/cli/src/sso/saml/serviceProvider.ee.ts
+++ packages/cli/src/sso/saml/serviceProvider.ee.ts
@@ -3,26 +3,27 @@ import { getInstanceBaseUrl } from '@/UserManagement/UserManagementHelper';
 import type { ServiceProviderInstance } from 'samlify';
 import { ServiceProvider } from 'samlify';
 import { SamlUrls } from './constants';
+import type { SamlPreferences } from './types/samlPreferences';
 
 let serviceProviderInstance: ServiceProviderInstance | undefined;
 
 // TODO:SAML: make these configurable for the end user
-export function getServiceProviderInstance(): ServiceProviderInstance {
+export function getServiceProviderInstance(prefs: SamlPreferences): ServiceProviderInstance {
 	if (serviceProviderInstance === undefined) {
 		serviceProviderInstance = ServiceProvider({
 			entityID: getInstanceBaseUrl() + SamlUrls.restMetadata,
-			authnRequestsSigned: false,
-			wantAssertionsSigned: true,
-			wantMessageSigned: true,
+			authnRequestsSigned: prefs.authnRequestsSigned,
+			wantAssertionsSigned: prefs.wantAssertionsSigned,
+			wantMessageSigned: prefs.wantMessageSigned,
 			nameIDFormat: ['urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'],
 			assertionConsumerService: [
 				{
-					isDefault: true,
+					isDefault: prefs.acsBinding === 'post',
 					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
 					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
 				},
 				{
-					isDefault: false,
+					isDefault: prefs.acsBinding === 'redirect',
 					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-REDIRECT',
 					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
 				},
