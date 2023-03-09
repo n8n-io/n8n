887e65c1ccf1e524709a2e7fb40b8577f1dc0ad7
Michael Auerswald <michael.auerswald@gmail.com>
2023-03-09 09:08:23 +0100

    feat(core): Refactor and add SAML preferences for service provider instance (#5637)
    * create SP through parameters instead of metadata
    
    * refactor SAML prefs and add SP configurations

--- packages/cli/src/sso/saml/serviceProvider.ee.ts
+++ packages/cli/src/sso/saml/serviceProvider.ee.ts
@@ -1,29 +1,33 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { getInstanceBaseUrl } from '@/UserManagement/UserManagementHelper';
 import type { ServiceProviderInstance } from 'samlify';
 import { ServiceProvider } from 'samlify';
 import { SamlUrls } from './constants';
+import type { SamlPreferences } from './types/samlPreferences';
 
 let serviceProviderInstance: ServiceProviderInstance | undefined;
 
-const metadata = `
-<EntityDescriptor
- xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata"
- xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
- xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
- entityID="${getInstanceBaseUrl() + SamlUrls.restMetadata}">
-    <SPSSODescriptor WantAssertionsSigned="true" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
-        <NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</NameIDFormat>
-        <AssertionConsumerService isDefault="true" index="0" Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="${
-					getInstanceBaseUrl() + SamlUrls.restAcs
-				}"/>
-    </SPSSODescriptor>
-</EntityDescriptor>
-`;
-
-export function getServiceProviderInstance(): ServiceProviderInstance {
+// TODO:SAML: make these configurable for the end user
+export function getServiceProviderInstance(prefs: SamlPreferences): ServiceProviderInstance {
 	if (serviceProviderInstance === undefined) {
 		serviceProviderInstance = ServiceProvider({
-			metadata,
+			entityID: getInstanceBaseUrl() + SamlUrls.restMetadata,
+			authnRequestsSigned: prefs.authnRequestsSigned,
+			wantAssertionsSigned: prefs.wantAssertionsSigned,
+			wantMessageSigned: prefs.wantMessageSigned,
+			nameIDFormat: ['urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'],
+			assertionConsumerService: [
+				{
+					isDefault: prefs.acsBinding === 'post',
+					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
+				},
+				{
+					isDefault: prefs.acsBinding === 'redirect',
+					Binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-REDIRECT',
+					Location: getInstanceBaseUrl() + SamlUrls.restAcs,
+				},
+			],
 		});
 	}
 
