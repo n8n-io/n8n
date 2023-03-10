01d2f185540a691d959d05708e0ee60f0514e1ae
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-03-10 19:05:51 +0100

    fix(core): Initialize License and LDAP in the correct order (#5673)

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -24,11 +24,11 @@ import * as GenericHelpers from '@/GenericHelpers';
 import * as Server from '@/Server';
 import { TestWebhooks } from '@/TestWebhooks';
 import { getAllInstalledPackages } from '@/CommunityNodes/packageModel';
-import { handleLdapInit } from '@/Ldap/helpers';
 import { EDITOR_UI_DIST_DIR, GENERATED_STATIC_DIR } from '@/constants';
 import { eventBus } from '@/eventbus';
 import { BaseCommand } from './BaseCommand';
 import { InternalHooks } from '@/InternalHooks';
+import { getLicense } from '@/License';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const open = require('open');
@@ -182,11 +182,26 @@ export class Start extends BaseCommand {
 		await Promise.all(files.map(compileFile));
 	}
 
+	async initLicense(): Promise<void> {
+		const license = getLicense();
+		await license.init(this.instanceId);
+
+		const activationKey = config.getEnv('license.activationKey');
+		if (activationKey) {
+			try {
+				await license.activate(activationKey);
+			} catch (e) {
+				LoggerProxy.error('Could not activate license', e as Error);
+			}
+		}
+	}
+
 	async init() {
 		await this.initCrashJournal();
 		await super.init();
 		this.logger.info('Initializing n8n process');
 
+		await this.initLicense();
 		await this.initBinaryManager();
 		await this.initExternalHooks();
 
@@ -325,8 +340,6 @@ export class Start extends BaseCommand {
 				'IMPORTANT! Do not share with anybody as it would give people access to your n8n instance!',
 			);
 		}
-
-		await handleLdapInit();
 
 		await Server.start();
 
