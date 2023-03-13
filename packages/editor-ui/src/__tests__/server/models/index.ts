95f10b1d2f9eaea7650cd78e9bf2cf5f5fd654d9
Alex Grozav <alex@grozav.com>
2023-03-13 11:05:08 +0200

    feat: Add Mirage.js server to enable editor-ui unit tests (#5671)
    feat: add Mirage.js server to enable editor-ui unit tests

---
+++ packages/editor-ui/src/__tests__/server/models/index.ts
@@ -0,0 +1,9 @@
+import { UserModel } from './user';
+import { CredentialModel } from './credential';
+import { CredentialTypeModel } from './credentialType';
+
+export const models = {
+	credential: CredentialModel,
+	credentialType: CredentialTypeModel,
+	user: UserModel,
+};
