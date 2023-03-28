d6c813777909d240fe6e0f1e371d3bda6e41af4d
Mutasem <mutdmour@gmail.com>
2023-03-28 13:41:17 +0200

    feat: update col

--- packages/cli/src/databases/migrations/sqlite/1679999714600-CreateFormsTables.ts
+++ packages/cli/src/databases/migrations/sqlite/1679999714600-CreateFormsTables.ts
@@ -13,7 +13,7 @@ export class CreateForms1679999714600 implements MigrationInterface {
 			`CREATE TABLE IF NOT EXISTS "${tablePrefix}forms" (
 				id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 				title TEXT NOT NULL,
-				form JSON NOT NULL,
+				schema JSON NOT NULL,
 			)`,
 		);
 
