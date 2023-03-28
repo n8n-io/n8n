45ed61c139b11c9dae6c835e38cffccfafcd9b70
Mutasem <mutdmour@gmail.com>
2023-03-28 15:24:00 +0200

    feat: comment back in migration

--- packages/cli/src/databases/migrations/sqlite/1679999714600-CreateFormsTables.ts
+++ packages/cli/src/databases/migrations/sqlite/1679999714600-CreateFormsTables.ts
@@ -8,25 +8,25 @@ export class CreateForms1679999714600 implements MigrationInterface {
 		logMigrationStart(this.name);
 		const tablePrefix = getTablePrefix();
 
-		// await queryRunner.query(
-		// 	`CREATE TABLE IF NOT EXISTS "${tablePrefix}forms" (
-		// 		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-		// 		title TEXT NOT NULL,
-		// 		schema JSON NOT NULL,
-		// 	)`,
-		// );
+		await queryRunner.query(
+			`CREATE TABLE IF NOT EXISTS "${tablePrefix}forms" (
+				id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+				title TEXT NOT NULL,
+				schema JSON NOT NULL,
+			)`,
+		);
 
-		// await queryRunner.query(
-		// 	`CREATE TABLE "${tablePrefix}form_workflow" (` +
-		// 		`"workflowId"	integer NOT NULL,` +
-		// 		`"formId"	integer NOT NULL,` +
-		// 		`"createdAt"	datetime(3) NOT NULL DEFAULT 'STRFTIME(''%Y-%m-%d %H:%M:%f'', ''NOW'')',` +
-		// 		`"updatedAt"	datetime(3) NOT NULL DEFAULT 'STRFTIME(''%Y-%m-%d %H:%M:%f'', ''NOW'')',` +
-		// 		`PRIMARY KEY("workflowId", "formId"), ` +
-		// 		`CONSTRAINT "FK_${tablePrefix}00000000000001234" FOREIGN KEY ("workflowId") REFERENCES "${tablePrefix}workflow_entity" ("id") ON DELETE CASCADE ON UPDATE NO ACTION, ` +
-		// 		`CONSTRAINT "FK_${tablePrefix}00000000000001234" FOREIGN KEY ("formId") REFERENCES "${tablePrefix}forms" ("id") ON DELETE CASCADE ON UPDATE NO ACTION ` +
-		// 		`);`,
-		// );
+		await queryRunner.query(
+			`CREATE TABLE "${tablePrefix}form_workflow" (` +
+				`"workflowId"	integer NOT NULL,` +
+				`"formId"	integer NOT NULL,` +
+				`"createdAt"	datetime(3) NOT NULL DEFAULT 'STRFTIME(''%Y-%m-%d %H:%M:%f'', ''NOW'')',` +
+				`"updatedAt"	datetime(3) NOT NULL DEFAULT 'STRFTIME(''%Y-%m-%d %H:%M:%f'', ''NOW'')',` +
+				`PRIMARY KEY("workflowId", "formId"), ` +
+				`CONSTRAINT "FK_${tablePrefix}00000000000001234" FOREIGN KEY ("workflowId") REFERENCES "${tablePrefix}workflow_entity" ("id") ON DELETE CASCADE ON UPDATE NO ACTION, ` +
+				`CONSTRAINT "FK_${tablePrefix}00000000000001234" FOREIGN KEY ("formId") REFERENCES "${tablePrefix}forms" ("id") ON DELETE CASCADE ON UPDATE NO ACTION ` +
+				`);`,
+		);
 
 		logMigrationEnd(this.name);
 	}
