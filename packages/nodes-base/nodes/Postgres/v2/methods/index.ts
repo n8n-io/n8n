0276068e6bc6f2ee18eb41403dfc0d94c1ac3126
Milorad Filipovic <milorad@n8n.io>
2023-03-29 16:12:27 +0200

    Removing unused import

--- packages/nodes-base/nodes/Postgres/v2/methods/index.ts
+++ packages/nodes-base/nodes/Postgres/v2/methods/index.ts
@@ -1,5 +1,3 @@
-import { resourceMapping } from '../../../Google/Sheet/v2/methods';
-
 export * as credentialTest from './credentialTest';
 export * as listSearch from './listSearch';
 export * as loadOptions from './loadOptions';
