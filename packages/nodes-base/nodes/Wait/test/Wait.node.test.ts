0c18e90e078d6bc1ef63f470ad404d957943ea23
Jon <jonathan.bennetts@gmail.com>
2023-04-18 11:28:05 +0100

    ci: Improve test for wait node (#5997)

--- packages/nodes-base/nodes/Wait/test/Wait.node.test.ts
+++ packages/nodes-base/nodes/Wait/test/Wait.node.test.ts
@@ -1,4 +1,19 @@
 import { testWorkflows, getWorkflowFilenames } from '../../../test/nodes/Helpers';
 const workflows = getWorkflowFilenames(__dirname);
 
-describe('Execute Wait Node', () => testWorkflows(workflows));
+describe('Execute Wait Node', () => {
+	let timer: NodeJS.Timer;
+	const { clearInterval, setInterval } = global;
+
+	beforeAll(() => {
+		timer = setInterval(() => jest.advanceTimersByTime(1000), 10);
+		jest.useFakeTimers();
+	});
+
+	afterAll(() => {
+		clearInterval(timer);
+		jest.useRealTimers();
+	});
+
+	testWorkflows(workflows);
+});
