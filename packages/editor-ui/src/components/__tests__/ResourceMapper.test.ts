df5bca67ceec6912fbe5a27f60f484cef99ba9a7
Milorad Filipovic <milorad@n8n.io>
2023-04-18 17:41:26 +0200

    ✅ Test refactoring

--- packages/editor-ui/src/components/__tests__/ResourceMapper.test.ts
+++ packages/editor-ui/src/components/__tests__/ResourceMapper.test.ts
@@ -1,5 +1,6 @@
 import { PiniaVuePlugin } from 'pinia';
 import { render } from '@testing-library/vue';
+import { merge } from 'lodash-es';
 import {
 	DEFAULT_SETUP,
 	MAPPING_COLUMNS_RESPONSE,
@@ -13,13 +14,15 @@ import ResourceMapper from '@/components/ResourceMapper/ResourceMapper.vue';
 let nodeTypeStore: ReturnType<typeof useNodeTypesStore>;
 
 const renderComponent = (renderOptions: Parameters<typeof render>[1] = {}) =>
-	render(ResourceMapper, renderOptions, (vue) => {
+	render(ResourceMapper, merge(DEFAULT_SETUP, renderOptions), (vue) => {
 		vue.use(PiniaVuePlugin);
 	});
 
 describe('ResourceMapper.vue', () => {
 	beforeEach(() => {
 		nodeTypeStore = useNodeTypesStore();
+		vi.spyOn(workflowHelpers, 'resolveParameter').mockReturnValue(NODE_PARAMETER_VALUES);
+		vi.spyOn(nodeTypeStore, 'getResourceMapperFields').mockResolvedValue(MAPPING_COLUMNS_RESPONSE);
 	});
 
 	afterEach(() => {
@@ -27,17 +30,15 @@ describe('ResourceMapper.vue', () => {
 	});
 
 	it('renders default configuration properly', async () => {
-		vi.spyOn(workflowHelpers, 'resolveParameter').mockReturnValue(NODE_PARAMETER_VALUES);
-		vi.spyOn(nodeTypeStore, 'getResourceMapperFields').mockResolvedValue(MAPPING_COLUMNS_RESPONSE);
-		const { container, getByTestId } = renderComponent(DEFAULT_SETUP);
+		const { getByTestId } = renderComponent();
 		await waitAllPromises();
 		expect(getByTestId('resource-mapper-container')).toBeInTheDocument();
 		expect(getByTestId('mapping-mode-select')).toBeInTheDocument();
 		expect(getByTestId('matching-column-select')).toBeInTheDocument();
 		expect(getByTestId('mapping-fields-container')).toBeInTheDocument();
+		// Should render one parameter input for each fetched column
 		expect(
-			container.querySelectorAll('[data-test-id="mapping-fields-container"] .parameter-input')
-				.length,
+			getByTestId('mapping-fields-container').querySelectorAll('.parameter-input').length,
 		).toBe(MAPPING_COLUMNS_RESPONSE.fields.length);
 	});
 });
