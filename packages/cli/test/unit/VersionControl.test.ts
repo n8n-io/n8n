71ed1f410c5a80f35ecaf913a5522b7788998695
Michael Auerswald <michael.auerswald@gmail.com>
2023-04-19 17:46:10 +0200

    feat(core): Add SSH key generation (#6006)
    * basic prefs and ssh key generation
    
    * review change
    
    * cleanup save
    
    * lint fix

---
+++ packages/cli/test/unit/VersionControl.test.ts
@@ -0,0 +1,11 @@
+import { generateSshKeyPair } from '../../src/environments/versionControl/versionControlHelper';
+
+describe('Version Control', () => {
+	it('should generate an SSH key pair', () => {
+		const keyPair = generateSshKeyPair();
+		expect(keyPair.privateKey).toBeTruthy();
+		expect(keyPair.privateKey).toContain('BEGIN OPENSSH PRIVATE KEY');
+		expect(keyPair.publicKey).toBeTruthy();
+		expect(keyPair.publicKey).toContain('ssh-ed25519');
+	});
+});
