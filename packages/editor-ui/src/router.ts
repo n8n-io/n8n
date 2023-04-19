d443f0481d7e69b50592ab7daa5b4fd88e0c90b3
Csaba Tuncsik <csaba@n8n.io>
2023-04-19 16:01:32 +0200

    feat(editor): Version control (WIP) (#6013)
    * feat(editor): Version control settings (with feature flag)
    
    * feat(editor): replace posthog feature flag with local storage key

--- packages/editor-ui/src/router.ts
+++ packages/editor-ui/src/router.ts
@@ -40,6 +40,7 @@ import SettingsUsageAndPlanVue from './views/SettingsUsageAndPlan.vue';
 import SettingsSso from './views/SettingsSso.vue';
 import SignoutView from '@/views/SignoutView.vue';
 import SamlOnboarding from '@/views/SamlOnboarding.vue';
+import SettingsVersionControl from './views/SettingsVersionControl.vue';
 
 Vue.use(Router);
 
@@ -568,6 +569,31 @@ export const routes = [
 								const settingsStore = useSettingsStore();
 								return settingsStore.isPublicApiEnabled === false;
 							},
+						},
+					},
+				},
+			},
+			{
+				path: 'version-control',
+				name: VIEWS.VERSION_CONTROL,
+				components: {
+					settingsView: SettingsVersionControl,
+				},
+				meta: {
+					telemetry: {
+						pageCategory: 'settings',
+						getProperties(route: Route) {
+							return {
+								feature: 'vc',
+							};
+						},
+					},
+					permissions: {
+						allow: {
+							role: [ROLE.Owner],
+						},
+						deny: {
+							shouldDeny: () => !window.localStorage.getItem('version-control'),
 						},
 					},
 				},
