3076db4ce235edcd890c94ec7ba617467ce8c8c9
Csaba Tuncsik <csaba@n8n.io>
2023-04-21 11:41:14 +0200

    feat(editor): Adding version control settings (WIP)

--- packages/editor-ui/src/stores/versionControl.ts
+++ packages/editor-ui/src/stores/versionControl.ts
@@ -1,16 +1,59 @@
-import { computed } from 'vue';
+import { computed, reactive } from 'vue';
 import { defineStore } from 'pinia';
+import { IDataObject } from 'n8n-workflow';
 import { EnterpriseEditionFeature } from '@/constants';
 import { useSettingsStore } from '@/stores/settings';
+import * as vcApi from '@/api/versionControl';
+import { useRootStore } from '@/stores/n8nRootStore';
 
 export const useVersionControlStore = defineStore('versionControl', () => {
+	const rootStore = useRootStore();
 	const settingsStore = useSettingsStore();
 
 	const isEnterpriseVersionControlEnabled = computed(() =>
 		settingsStore.isEnterpriseFeatureEnabled(EnterpriseEditionFeature.VersionControl),
 	);
 
+	const state = reactive({
+		branches: [] as string[],
+		currentBranch: '',
+		authorName: '',
+		authorEmail: '',
+		repositoryUrl: '',
+		sshKey: '',
+		commitMessage: 'commit message',
+	});
+
+	const initSsh = async (data: IDataObject) => {
+		state.sshKey = await vcApi.initSsh(rootStore.getRestApiContext, data);
+	};
+
+	const initRepository = async () => {
+		const { branches, currentBranch } = await vcApi.initRepository(rootStore.getRestApiContext);
+		state.branches = branches;
+		state.currentBranch = currentBranch;
+	};
+
+	const sync = async (data: { commitMessage: string }) => {
+		state.commitMessage = data.commitMessage;
+		return vcApi.sync(rootStore.getRestApiContext, { message: data.commitMessage });
+	};
+	const getConfig = async () => {
+		const { remoteRepository, name, email, currentBranch } = await vcApi.getConfig(
+			rootStore.getRestApiContext,
+		);
+		state.repositoryUrl = remoteRepository;
+		state.authorName = name;
+		state.authorEmail = email;
+		state.currentBranch = currentBranch;
+	};
+
 	return {
 		isEnterpriseVersionControlEnabled,
+		state,
+		initSsh,
+		initRepository,
+		sync,
+		getConfig,
 	};
 });
