8391d3982171650a9750ae3ac017aac46ab66473
BramKn <bram@knitco.nl>
2023-04-21 12:51:28 +0200

    fix description

--- packages/nodes-base/credentials/GotifyApi.credentials.ts
+++ packages/nodes-base/credentials/GotifyApi.credentials.ts
@@ -34,7 +34,7 @@ export class GotifyApi implements ICredentialType {
 			name: 'ignoreSSLIssues',
 			type: 'boolean',
 			default: false,
-			description: 'Ignore SSL Issues, to allow for connections to servers without SSL configured',
+			description: 'Whether to allow for connections to servers without SSL configured',
 		},
 	];
 }
