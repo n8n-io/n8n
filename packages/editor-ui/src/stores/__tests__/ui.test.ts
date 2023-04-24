afbf65450237befc67e9688155b62d57f83ce0ee
Iván Ovejero <ivov.src@gmail.com>
2023-04-24 10:42:03 +0200

    refactor(editor): Switch to plan change URL on cloud (no-changelog) (#6001)
    * :truck: Switch to plan change URL on cloud
    
    * :test_tube: Update URL in test

--- packages/editor-ui/src/stores/__tests__/ui.test.ts
+++ packages/editor-ui/src/stores/__tests__/ui.test.ts
@@ -30,7 +30,7 @@ describe('UI store', () => {
 			'production',
 			'https://n8n.io/pricing/?utm_source=n8n-internal&utm_medium=desktop&utm_campaign=utm-test-campaign',
 		],
-		['cloud', 'production', 'https://app.n8n.cloud/manage?edition=cloud'],
+		['cloud', 'production', 'https://app.n8n.cloud/account/change-plan'],
 	])(
 		'"upgradeLinkUrl" should generate the correct URL for "%s" deployment and "%s" license environment',
 		(type, environment, expectation) => {
