d6f6fdb12385da3fd0fc81f3db14f28488617e4e
Michael Auerswald <michael.auerswald@gmail.com>
2023-04-24 15:53:13 +0200

    cleanup

--- packages/cli/src/environments/versionControl/types/requests.ts
+++ packages/cli/src/environments/versionControl/types/requests.ts
@@ -2,6 +2,5 @@ import type { AuthenticatedRequest } from '@/requests';
 import type { VersionControlPreferences } from './versionControlPreferences';
 
 export declare namespace VersionControlRequest {
-	type GetPreferences = AuthenticatedRequest<{}, VersionControlPreferences, {}, {}>;
 	type UpdatePreferences = AuthenticatedRequest<{}, {}, Partial<VersionControlPreferences>, {}>;
 }
