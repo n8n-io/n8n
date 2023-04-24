a4df8a62d7485f2d4f211ba27818c34cfd08b7dc
Milorad Filipovic <milorad@n8n.io>
2023-04-24 13:45:41 +0200

    refactor(editor): Delete leftover `restApi` mixin file (no-changelog)

--- packages/editor-ui/src/stores/workflows.ts
+++ packages/editor-ui/src/stores/workflows.ts
@@ -375,7 +375,12 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, {
 			return workflows;
 		},
 
-		async fetchAndSetWorkflow(id: string): Promise<IWorkflowDb> {
+		// async fetchWorkflow(id: string): Promise<IWorkflowDb> {
+		// 	const rootStore = useRootStore();
+		// 	return makeRestApiRequest(rootStore.getRestApiContext, 'GET', `/workflows/${id}`);
+		// },
+
+		async fetchWorkflow(id: string): Promise<IWorkflowDb> {
 			const rootStore = useRootStore();
 			const workflow = await getWorkflow(rootStore.getRestApiContext, id);
 			this.addWorkflow(workflow);
@@ -494,7 +499,9 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, {
 			}, {});
 		},
 
-		deleteWorkflow(id: string): void {
+		async deleteWorkflow(id: string): Promise<void> {
+			const rootStore = useRootStore();
+			await makeRestApiRequest(rootStore.getRestApiContext, 'DELETE', `/workflows/${id}`);
 			const { [id]: deletedWorkflow, ...workflows } = this.workflowsById;
 			this.workflowsById = workflows;
 		},
@@ -1121,11 +1128,6 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, {
 			return response && unflattenExecutionData(response);
 		},
 
-		async fetchWorkflow(id: string): Promise<IWorkflowDb> {
-			const rootStore = useRootStore();
-			return makeRestApiRequest(rootStore.getRestApiContext, 'GET', `/workflows/${id}`);
-		},
-
 		// Creates a new workflow
 		async createNewWorkflow(sendData: IWorkflowDataUpdate): Promise<IWorkflowDb> {
 			const rootStore = useRootStore();
@@ -1135,12 +1137,6 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, {
 				'/workflows',
 				sendData as unknown as IDataObject,
 			);
-		},
-
-		// Deletes a workflow
-		async deleteWorkflowAPI(name: string): Promise<void> {
-			const rootStore = useRootStore();
-			return makeRestApiRequest(rootStore.getRestApiContext, 'DELETE', `/workflows/${name}`);
 		},
 
 		// Updates an existing workflow
