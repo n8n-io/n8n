2f71ccc6bdb82b078984871dd9958cef7bcde22a
Michael Kret <michael.k@radency.com>
2023-04-24 12:48:41 +0300

    :zap: finished createFromText operation

--- packages/nodes-base/nodes/Google/Drive/v2/actions/file/createFromText.operation.ts
+++ packages/nodes-base/nodes/Google/Drive/v2/actions/file/createFromText.operation.ts
@@ -1,9 +1,8 @@
 import type { IExecuteFunctions } from 'n8n-core';
 import type { IDataObject, INodeExecutionData, INodeProperties } from 'n8n-workflow';
-import { jsonParse } from 'n8n-workflow';
 
 import { updateDisplayOptions } from '../../../../../../utils/utilities';
-import { driveRLC, folderRLC } from '../common.descriptions';
+import { folderRLC } from '../common.descriptions';
 import { googleApiRequest } from '../../transport';
 
 const properties: INodeProperties[] = [
@@ -39,17 +38,19 @@ const properties: INodeProperties[] = [
 				type: 'boolean',
 				default: false,
 				description: 'Whether to create a Google Document (instead of the .txt default format)',
+				hint: 'Google Docs API has to be enabled in the <a href="https://console.developers.google.com/apis/library/docs.googleapis.com" target="_blank">Google API Console</a>.',
 			},
-			{
-				...driveRLC,
-				displayName: 'Drive',
-				required: false,
-				description:
-					'he Drive where you want to create the file in. By default, “My Drive“ is used.',
-			},
+			// {
+			// 	...driveRLC,
+			// 	displayName: 'Drive',
+			// 	required: false,
+			// 	description:
+			// 		'he Drive where you want to create the file in. By default, “My Drive“ is used.',
+			// },
 			{
 				...folderRLC,
 				displayName: 'Parent Folder',
+				name: 'parentFolderId',
 				required: false,
 				description:
 					'The Folder where you want to create the file in. By default, the root folder is used.',
@@ -68,50 +69,89 @@ const displayOptions = {
 export const description = updateDisplayOptions(displayOptions, properties);
 
 export async function execute(this: IExecuteFunctions, i: number): Promise<INodeExecutionData[]> {
-	const returnData: INodeExecutionData[] = [];
-
-	const content = Buffer.from(this.getNodeParameter('content', i, '') as string, 'utf8');
-	const contentLength = content.byteLength;
-
 	const name = (this.getNodeParameter('name', i) as string) || 'Untitled';
 
 	const options = this.getNodeParameter('options', i, {});
+	const parentFolderId = ((options?.parentFolderId as IDataObject)?.value as string) || 'root';
+	const convertToGoogleDocument = (options.convertToGoogleDocument as boolean) || false;
+	const mimeType = convertToGoogleDocument ? 'application/vnd.google-apps.document' : 'text/plain';
 
-	const parentFolderId = (options.parentFolderId as string) || 'root';
+	const bodyParameters = {
+		name,
+		parents: [parentFolderId],
+		mimeType,
+	};
 
-	const uploadResponse = await googleApiRequest.call(
-		this,
-		'POST',
-		'/upload/drive/v3/files',
-		content,
-		{
-			fields: {},
-			uploadType: 'media',
-		},
-		undefined,
-		{
-			headers: {
-				'Content-Type': 'text/plain',
-				'Content-Length': contentLength,
+	const boundary = 'XXXXXX';
+
+	let response;
+	if (convertToGoogleDocument) {
+		const document = await googleApiRequest.call(this, 'POST', '/drive/v3/files', bodyParameters);
+
+		const text = this.getNodeParameter('content', i, '') as string;
+
+		const body = {
+			requests: [
+				{
+					insertText: {
+						text,
+						endOfSegmentLocation: {
+							segmentId: '', //empty segment ID signifies the document's body
+						},
+					},
+				},
+			],
+		};
+
+		const updateResponse = await googleApiRequest.call(
+			this,
+			'POST',
+			'',
+			body,
+			undefined,
+			`https://docs.googleapis.com/v1/documents/${document.id}:batchUpdate`,
+		);
+
+		response = { id: updateResponse.documentId };
+	} else {
+		const content = Buffer.from(this.getNodeParameter('content', i, '') as string, 'utf8');
+		const contentLength = content.byteLength;
+
+		const body = `
+		\n--${boundary}\
+		\nContent-Type: application/json; charset=UTF-8\
+		\n\n${JSON.stringify(bodyParameters)}\
+		\n--${boundary}\
+		\nContent-Type: text/plain\
+		\nContent-Transfer-Encoding: base64\
+		\n\n${content}\
+		\n--${boundary}--`;
+
+		const responseData = await googleApiRequest.call(
+			this,
+			'POST',
+			'/upload/drive/v3/files',
+			body,
+			{
+				uploadType: 'multipart',
+				supportsAllDrives: true,
 			},
-			encoding: null,
-			json: false,
-		},
+			undefined,
+			{
+				headers: {
+					'Content-Type': `multipart/related; boundary=${boundary}`,
+					'Content-Length': contentLength,
+				},
+			},
+		);
+
+		response = { id: responseData.id };
+	}
+
+	const executionData = this.helpers.constructExecutionMetaData(
+		this.helpers.returnJsonArray(response as IDataObject),
+		{ itemData: { item: i } },
 	);
-	const uploadId = jsonParse<IDataObject>(uploadResponse as string).id;
 
-	const response = await googleApiRequest.call(
-		this,
-		'PATCH',
-		`/drive/v3/files/${uploadId}`,
-		{ name },
-		{
-			addParents: [parentFolderId],
-			supportsAllDrives: true,
-		},
-	);
-
-	console.log(response);
-
-	return returnData;
+	return executionData;
 }
