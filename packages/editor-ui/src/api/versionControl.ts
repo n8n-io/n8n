f914b73abb94567eaa81aa6588eb3081622d3f22
Csaba Tuncsik <csaba@n8n.io>
2023-04-25 14:37:16 +0200

    fix(editor): use rest api root path in version control

--- packages/editor-ui/src/api/versionControl.ts
+++ packages/editor-ui/src/api/versionControl.ts
@@ -1,23 +1,25 @@
-import { IRestApiContext } from '@/Interface';
+import type { IRestApiContext } from '@/Interface';
 import { makeRestApiRequest } from '@/utils';
-import { IDataObject } from 'n8n-workflow';
+import type { IDataObject } from 'n8n-workflow';
+
+const versionControlApiRoot = '/version-control';
 
 export const initSsh = (context: IRestApiContext, data: IDataObject): Promise<string> => {
-	return makeRestApiRequest(context, 'POST', '/environment/init-ssh', data);
+	return makeRestApiRequest(context, 'POST', `${versionControlApiRoot}/init-ssh`, data);
 };
 
 export const initRepository = (
 	context: IRestApiContext,
 ): Promise<{ branches: string[]; currentBranch: string }> => {
-	return makeRestApiRequest(context, 'POST', '/environment/init-repository');
+	return makeRestApiRequest(context, 'POST', `${versionControlApiRoot}/init-repository`);
 };
 
 export const sync = (context: IRestApiContext, data: IDataObject): Promise<void> => {
-	return makeRestApiRequest(context, 'POST', '/environment/push', data);
+	return makeRestApiRequest(context, 'POST', `${versionControlApiRoot}/push`, data);
 };
 
 export const getConfig = (
 	context: IRestApiContext,
 ): Promise<{ remoteRepository: string; name: string; email: string; currentBranch: string }> => {
-	return makeRestApiRequest(context, 'GET', '/environment/config');
+	return makeRestApiRequest(context, 'GET', `${versionControlApiRoot}/config`);
 };
