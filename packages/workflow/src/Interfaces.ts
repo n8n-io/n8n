ff0f02c3b49188c96b1b2f82d43a087aa05cf67b
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-04-25 14:57:21 +0000

    refactor(editor): Replace monaco-editor/prismjs with CodeMirror (#5983)
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>
    Co-authored-by: Milorad FIlipović <milorad@n8n.io>
    Co-authored-by: Alex Grozav <alex@grozav.com>

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -1019,7 +1019,8 @@ export type NodePropertyTypes =
 
 export type CodeAutocompleteTypes = 'function' | 'functionItem';
 
-export type EditorTypes = 'code' | 'codeNodeEditor' | 'htmlEditor' | 'json';
+export type EditorType = 'code' | 'codeNodeEditor' | 'htmlEditor' | 'json';
+export type CodeNodeEditorLanguage = 'javaScript' | 'json'; //| 'python' | 'sql';
 
 export interface ILoadOptions {
 	routing?: {
@@ -1032,7 +1033,8 @@ export interface ILoadOptions {
 export interface INodePropertyTypeOptions {
 	alwaysOpenEditWindow?: boolean; // Supported by: json
 	codeAutocomplete?: CodeAutocompleteTypes; // Supported by: string
-	editor?: EditorTypes; // Supported by: string
+	editor?: EditorType; // Supported by: string
+	editorLanguage?: CodeNodeEditorLanguage; // Supported by: string in combination with editor: codeNodeEditor
 	loadOptionsDependsOn?: string[]; // Supported by: options
 	loadOptionsMethod?: string; // Supported by: options
 	loadOptions?: ILoadOptions; // Supported by: options
