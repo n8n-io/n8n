c7e2b1001982bc9cbdba3bb204164d24f8ddd3f0
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-04-27 12:08:43 +0200

    fix(core): Skip auth for controllers/routes that don't use the `Authorized` decorator

--- packages/cli/src/decorators/registerController.ts
+++ packages/cli/src/decorators/registerController.ts
@@ -14,6 +14,7 @@ import type {
 	AuthRole,
 	AuthRoleMetadata,
 	Controller,
+	Method,
 	MiddlewareMetadata,
 	RouteMetadata,
 } from './types';
@@ -31,6 +32,11 @@ export const createAuthMiddleware =
 
 		res.status(403).json({ status: 'error', message: 'Unauthorized' });
 	};
+
+const authFreeRoutes: string[] = [];
+
+export const canSkipAuth = (method: string, path: string): boolean =>
+	authFreeRoutes.includes(`${method.toLowerCase()} ${path}`);
 
 export const registerController = (app: Application, config: Config, controller: object) => {
 	const controllerClass = controller.constructor;
@@ -69,6 +75,7 @@ export const registerController = (app: Application, config: Config, controller:
 					(controller as Controller)[handlerName](req, res),
 				),
 			);
+			if (!authRole || authRole === 'none') authFreeRoutes.push(`${method} ${prefix}${path}`);
 		});
 
 		app.use(prefix, router);
