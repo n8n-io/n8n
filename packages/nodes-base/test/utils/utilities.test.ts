bed3c1542a500b926ac95cfd47718d5db6767453
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2023-04-27 13:36:02 +0300

    fix(HTTP Request Node): Always lowercase headers

--- packages/nodes-base/test/utils/utilities.test.ts
+++ packages/nodes-base/test/utils/utilities.test.ts
@@ -1,4 +1,4 @@
-import { fuzzyCompare } from '../../utils/utilities';
+import { fuzzyCompare, keysToLowercase } from '../../utils/utilities';
 
 //most test cases for fuzzyCompare are done in Compare Datasets node tests
 describe('Test fuzzyCompare', () => {
@@ -27,5 +27,38 @@ describe('Test fuzzyCompare', () => {
 
 		expect(compareFunction(null, 0)).toEqual(false);
 		expect(compareFunction(null, '0')).toEqual(false);
+	});
+});
+
+describe('Test keysToLowercase', () => {
+	it('should convert keys to lowercase', () => {
+		const headers = {
+			'Content-Type': 'application/json',
+			'X-Test-Header': 'Test',
+			Accept: 'application/json',
+		};
+
+		const newHeaders = keysToLowercase(headers);
+
+		expect(newHeaders).toEqual({
+			'content-type': 'application/json',
+			'x-test-header': 'Test',
+			accept: 'application/json',
+		});
+	});
+	it('should return original value if it is not an object', () => {
+		const test1 = keysToLowercase(['hello']);
+		const test2 = keysToLowercase('test');
+		const test3 = keysToLowercase(1);
+		const test4 = keysToLowercase(true);
+		const test5 = keysToLowercase(null);
+		const test6 = keysToLowercase(undefined);
+
+		expect(test1).toEqual(['hello']);
+		expect(test2).toEqual('test');
+		expect(test3).toEqual(1);
+		expect(test4).toEqual(true);
+		expect(test5).toEqual(null);
+		expect(test6).toEqual(undefined);
 	});
 });
