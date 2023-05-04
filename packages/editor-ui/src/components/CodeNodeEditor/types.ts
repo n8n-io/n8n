35c8510ab6d607fe59056a4aa1d8d148e194d12c
Jan Oberhauser <janober@users.noreply.github.com>
2023-05-04 20:00:00 +0200

    feat(Code Node): Add Python support (#4295)

--- packages/editor-ui/src/components/CodeNodeEditor/types.ts
+++ packages/editor-ui/src/components/CodeNodeEditor/types.ts
@@ -1,19 +1,16 @@
 import type { EditorView } from '@codemirror/view';
 import type { I18nClass } from '@/plugins/i18n';
-import type { Workflow } from 'n8n-workflow';
+import type { Workflow, CodeExecutionMode, CodeNodeEditorLanguage } from 'n8n-workflow';
 import type { Node } from 'estree';
-import type { CODE_LANGUAGES, CODE_MODES } from './constants';
 
 export type CodeNodeEditorMixin = Vue.VueConstructor<
 	Vue & {
 		$locale: I18nClass;
 		editor: EditorView | null;
-		mode: 'runOnceForAllItems' | 'runOnceForEachItem';
+		mode: CodeExecutionMode;
+		language: CodeNodeEditorLanguage;
 		getCurrentWorkflow(): Workflow;
 	}
 >;
 
 export type RangeNode = Node & { range: [number, number] };
-
-export type CodeLanguage = (typeof CODE_LANGUAGES)[number];
-export type CodeMode = (typeof CODE_MODES)[number];
