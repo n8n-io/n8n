d12f8980ac072469cd921aabf2ba36cb09f23555
Csaba Tuncsik <csaba@n8n.io>
2023-05-05 22:28:27 +0200

    fix(editor): replace clearAllStickyNotifications

--- packages/editor-ui/src/components/RunData.vue
+++ packages/editor-ui/src/components/RunData.vue
@@ -508,12 +508,12 @@ import { nodeHelpers } from '@/mixins/nodeHelpers';
 import { pinData } from '@/mixins/pinData';
 import CodeNodeEditor from '@/components/CodeNodeEditor/CodeNodeEditor.vue';
 import { dataPinningEventBus } from '@/event-bus';
-import { clearJsonKey, executionDataToJson, stringSizeInBytes } from '@/utils';
-import { isEmpty } from '@/utils';
+import { clearJsonKey, executionDataToJson, stringSizeInBytes, isEmpty } from '@/utils';
 import { useWorkflowsStore } from '@/stores/workflows.store';
 import { mapStores } from 'pinia';
 import { useNDVStore } from '@/stores/ndv.store';
 import { useNodeTypesStore } from '@/stores/nodeTypes.store';
+import { useToast } from '@/composables';
 
 const RunDataTable = () => import('@/components/RunDataTable.vue');
 const RunDataJson = () => import('@/components/RunDataJson.vue');
@@ -584,6 +584,11 @@ export default mixins(externalHooks, genericHelpers, nodeHelpers, pinData).exten
 			type: Boolean,
 			default: false,
 		},
+	},
+	setup() {
+		return {
+			...useToast(),
+		};
 	},
 	data() {
 		return {
