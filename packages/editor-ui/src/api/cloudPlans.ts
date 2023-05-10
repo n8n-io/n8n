903557a116aec8b417437f670f1904a84be2922b
ricardo <ricardoespinoza105@gmail.com>
2023-05-10 15:39:06 -0400

    swap mockup date for endpoint

--- packages/editor-ui/src/api/cloudPlans.ts
+++ packages/editor-ui/src/api/cloudPlans.ts
@@ -1,29 +1,9 @@
 import type { Cloud, IRestApiContext } from '@/Interface';
+import { get } from '@/utils';
 
 export function getCurrentPlan(
 	context: IRestApiContext,
 	cloudUserId: string,
 ): Promise<Cloud.PlanData> {
-	return Promise.resolve({
-		planId: 43039,
-		monthlyExecutionsLimit: 200,
-		activeWorkflowsLimit: 10,
-		credentialsLimit: 100,
-		isActive: false,
-		displayName: 'Trial',
-		expirationDate: '2023-05-10T01:47:47Z',
-		metadata: {
-			version: 'v1',
-			group: 'trial',
-			slug: 'trial-1',
-			trial: {
-				length: 7,
-				gracePeriod: 3,
-			},
-		},
-		usage: {
-			executions: 100,
-			activeWorkflows: 10,
-		},
-	});
+	return get(context.baseUrl, `/user/${cloudUserId}/plan`);
 }
