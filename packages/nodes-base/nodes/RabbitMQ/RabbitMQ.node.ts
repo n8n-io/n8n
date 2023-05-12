976e3d7a74bc2d5fb5e5d0bc04bf24159681aefe
agobrech <ael.gobrecht@gmail.com>
2023-05-12 16:27:08 +0200

    Fix versioning

--- packages/nodes-base/nodes/RabbitMQ/RabbitMQ.node.ts
+++ packages/nodes-base/nodes/RabbitMQ/RabbitMQ.node.ts
@@ -23,7 +23,7 @@ export class RabbitMQ implements INodeType {
 		// eslint-disable-next-line n8n-nodes-base/node-class-description-icon-not-svg
 		icon: 'file:rabbitmq.png',
 		group: ['transform'],
-		version: 1.1,
+		version: [1, 1.1],
 		description: 'Sends messages to a RabbitMQ topic',
 		defaults: {
 			name: 'RabbitMQ',
