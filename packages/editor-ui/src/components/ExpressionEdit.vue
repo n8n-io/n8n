dd8f42abda9ab9bcb928addb3cacc70a352d71b5
Milorad Filipovic <milorad@n8n.io>
2023-05-12 10:01:04 +0200

    🔨 Removing old telemetry code, updating the current one based on the review feedback

--- packages/editor-ui/src/components/ExpressionEdit.vue
+++ packages/editor-ui/src/components/ExpressionEdit.vue
@@ -181,23 +181,7 @@ export default mixins(externalHooks, genericHelpers, debounceHelper).extend({
 					trackProperties.variable_type = 'Raw value';
 				}
 
-				if (splitVar[0].startsWith('$node')) {
-					const sourceNodeName = splitVar[0].split('"')[1];
-					trackProperties.node_type_source =
-						this.workflowsStore.getNodeByName(sourceNodeName)?.type;
-					const nodeConnections: Array<Array<{ node: string }>> =
-						this.workflowsStore.outgoingConnectionsByNodeName(sourceNodeName).main;
-					trackProperties.is_immediate_input =
-						nodeConnections &&
-						nodeConnections[0] &&
-						!!nodeConnections[0].find(({ node }) => node === this.ndvStore.activeNode?.name || '')
-							? true
-							: false;
-
-					if (splitVar[1].startsWith('parameter')) {
-						trackProperties.parameter_name_source = splitVar[1].split('"')[1];
-					}
-				} else if (splitVar[0].startsWith("$('")) {
+				if (splitVar[0].startsWith("$('")) {
 					const match = /\$\('(.*?)'\)/.exec(splitVar[0]);
 					if (match && match.length > 1) {
 						const sourceNodeName = match[1];
@@ -208,9 +192,7 @@ export default mixins(externalHooks, genericHelpers, debounceHelper).extend({
 						trackProperties.is_immediate_input =
 							nodeConnections &&
 							nodeConnections[0] &&
-							!!nodeConnections[0].find(({ node }) => node === this.ndvStore.activeNode?.name || '')
-								? true
-								: false;
+							nodeConnections[0].some(({ node }) => node === this.ndvStore.activeNode?.name || '');
 
 						if (splitVar[1].startsWith('parameter')) {
 							trackProperties.parameter_name_source = splitVar[1].split('"')[1];
