0c9ae471083a075c12ead1470bc8ddff37f4b84f
Michael Kret <michael.k@radency.com>
2023-05-16 09:04:36 +0300

    :zap: removed reusable connections

--- packages/nodes-base/nodes/Postgres/v2/transport/index.ts
+++ packages/nodes-base/nodes/Postgres/v2/transport/index.ts
@@ -11,7 +11,7 @@ import pgPromise from 'pg-promise';
 import { rm, writeFile } from 'fs/promises';
 import { file } from 'tmp-promise';
 
-import type { PgpClient, PgpDatabase } from '../helpers/interfaces';
+import type { PgpDatabase } from '../helpers/interfaces';
 
 async function createSshConnectConfig(credentials: IDataObject) {
 	if (credentials.sshAuthenticateWith === 'password') {
@@ -40,7 +40,7 @@ async function createSshConnectConfig(credentials: IDataObject) {
 	}
 }
 
-async function configurePostgres(
+export async function configurePostgres(
 	credentials: IDataObject,
 	options: IDataObject = {},
 	createdSshClient?: Client,
@@ -184,35 +184,35 @@ async function configurePostgres(
 	}
 }
 
-export const Connections = (function () {
-	let instance: { db: PgpDatabase; pgp: PgpClient; sshClient?: Client } | null = null;
+// export const Connections = (function () {
+// 	let instance: { db: PgpDatabase; pgp: PgpClient; sshClient?: Client } | null = null;
 
-	return {
-		async getInstance(
-			credentials: IDataObject = {},
-			options: IDataObject = {},
-			reload = false,
-			createdSshClient?: Client,
-			nulify = false,
-		) {
-			if (nulify) {
-				instance = null;
-				return instance;
-			}
+// 	return {
+// 		async getInstance(
+// 			credentials: IDataObject = {},
+// 			options: IDataObject = {},
+// 			reload = false,
+// 			createdSshClient?: Client,
+// 			nulify = false,
+// 		) {
+// 			if (nulify) {
+// 				instance = null;
+// 				return instance;
+// 			}
 
-			if (instance !== null && reload) {
-				if (instance.sshClient) {
-					instance.sshClient.end();
-				}
-				instance.pgp.end();
+// 			if (instance !== null && reload) {
+// 				if (instance.sshClient) {
+// 					instance.sshClient.end();
+// 				}
+// 				instance.pgp.end();
 
-				instance = null;
-			}
+// 				instance = null;
+// 			}
 
-			if (instance === null && Object.keys(credentials).length) {
-				instance = await configurePostgres(credentials, options, createdSshClient);
-			}
-			return instance;
-		},
-	};
-})();
+// 			if (instance === null && Object.keys(credentials).length) {
+// 				instance = await configurePostgres(credentials, options, createdSshClient);
+// 			}
+// 			return instance;
+// 		},
+// 	};
+// })();
