89fe940836ff6031a5484923331ff5bf51cce039
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-05-19 19:02:03 +0200

    fix(editor): Fix inverted checks on modal confirmation results

--- packages/editor-ui/src/mixins/workflowHelpers.ts
+++ packages/editor-ui/src/mixins/workflowHelpers.ts
@@ -766,7 +766,7 @@ export const workflowHelpers = defineComponent({
 						},
 					);
 
-					if (overwrite === MODAL_CONFIRM) {
+					if (overwrite !== MODAL_CONFIRM) {
 						return this.saveCurrentWorkflow({ id, name, tags }, redirect, true);
 					}
 
