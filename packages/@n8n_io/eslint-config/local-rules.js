75449869a0f38dea03f9547f5794a4c6605de469
Iván Ovejero <ivov.src@gmail.com>
2023-05-23 09:49:30 +0200

    Add rule `dangerously-use-html-string-missing`

--- packages/@n8n_io/eslint-config/local-rules.js
+++ packages/@n8n_io/eslint-config/local-rules.js
@@ -199,6 +199,103 @@ module.exports = {
 			};
 		},
 	},
+
+	'dangerously-use-html-string-missing': {
+		meta: {
+			type: 'error',
+			docs: {
+				description:
+					'Calls to the `showToast` and `showMessage` methods must include `dangerouslyUseHTMLString: true` when at least one of the values in `title` or `message` contains HTML.',
+				recommended: 'error',
+				/**
+				 * @note This rule does not yet cover cases where the result of calling
+				 * `this.$locale.someMethod()` is assigned to a variable that is then
+				 * assigned to `title or `message`, e.g. `message: errorMessage`.
+				 */
+			},
+		},
+		create(context) {
+			const LOCALE_MAP_FILEPATH = './src/plugins/i18n/locales/en.json';
+			const LOCALE_MAP = JSON.parse(require('fs').readFileSync(LOCALE_MAP_FILEPATH));
+			const LOCALE_NAMESPACE = '$locale';
+
+			const METHODS_POSSIBLY_REQUIRING_HTML = new Set(['showToast', 'showMessage']);
+			const PROPERTIES_POSSIBLY_CONTAINING_HTML = new Set(['title', 'message']);
+			const USE_HTML_PROPERTY = 'dangerouslyUseHTMLString';
+
+			const isMethodPossiblyRequiringRawHtml = (node) =>
+				node.callee.type === 'MemberExpression' &&
+				node.callee.object.type === 'ThisExpression' &&
+				node.callee.property.type === 'Identifier' &&
+				METHODS_POSSIBLY_REQUIRING_HTML.has(node.callee.property.name) &&
+				node.arguments.length === 1 &&
+				node.arguments.at(0).type === 'ObjectExpression';
+
+			const isPropertyWithLocaleStringAsValue = (property) =>
+				property.key.type === 'Identifier' &&
+				PROPERTIES_POSSIBLY_CONTAINING_HTML.has(property.key.name) &&
+				property.value.type === 'CallExpression' &&
+				property.value.callee.type === 'MemberExpression' &&
+				property.value.callee.object.type === 'MemberExpression' &&
+				property.value.callee.object.property.type === 'Identifier' &&
+				property.value.callee.object.property.name === LOCALE_NAMESPACE &&
+				property.value.arguments.length >= 1 &&
+				property.value.arguments.at(0).type === 'Literal' &&
+				typeof property.value.arguments.at(0).value === 'string';
+
+			const containsHtml = (str) => {
+				let insideTag = false;
+
+				for (let char of str) {
+					if (char === '<') {
+						insideTag = true;
+					} else if (char === '>') {
+						if (insideTag) return true;
+						insideTag = false;
+					}
+				}
+
+				return false;
+			};
+
+			return {
+				CallExpression(node) {
+					if (!isMethodPossiblyRequiringRawHtml(node)) return;
+
+					const arg = node.arguments.at(0);
+
+					const hasArgWitHtml = arg.properties
+						.reduce(
+							(acc, p) =>
+								isPropertyWithLocaleStringAsValue(p)
+									? [...acc, p.value.arguments.at(0).value]
+									: acc,
+							[],
+						)
+						.some((i) => containsHtml(LOCALE_MAP[i]));
+
+					if (!hasArgWitHtml) return;
+
+					const hasRawHtmlPropertyAsTrue = arg.properties.some(
+						(p) =>
+							p.key.type === 'Identifier' &&
+							p.key.name === USE_HTML_PROPERTY &&
+							p.value.type === 'Literal' &&
+							p.value.value === true,
+					);
+
+					if (hasRawHtmlPropertyAsTrue) return;
+
+					const methodName = node.callee.property.name;
+
+					context.report({
+						node,
+						message: `Set \`${USE_HTML_PROPERTY}: true\` in the argument to \`${methodName}\`. At least one of the values in \`title\` or \`message\` contains HTML.`,
+					});
+				},
+			};
+		},
+	},
 };
 
 const isJsonParseCall = (node) =>
