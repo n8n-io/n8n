846998932043d00a2e38ac6bdbd36f45d0781ce3
Michael Auerswald <michael.auerswald@gmail.com>
2023-05-30 12:52:02 +0200

    feat: Add manual login option and password reset link for SSO (#6328)
    * consolidate IUserSettings in workflow and add allowSSOManualLogin
    
    * add pw reset link to owners ui

--- packages/cli/src/user/user.service.ts
+++ packages/cli/src/user/user.service.ts
@@ -1,8 +1,10 @@
 import type { EntityManager, FindOptionsWhere } from 'typeorm';
 import { In } from 'typeorm';
+import { v4 as uuid } from 'uuid';
 import * as Db from '@/Db';
 import { User } from '@db/entities/User';
-import type { IUserSettings } from '@/Interfaces';
+import type { IUserSettings } from 'n8n-workflow';
+import { getInstanceBaseUrl } from '../UserManagement/UserManagementHelper';
 
 export class UserService {
 	static async get(where: FindOptionsWhere<User>): Promise<User | null> {
@@ -21,5 +23,18 @@ export class UserService {
 			where: { id },
 		});
 		return Db.collections.User.update(id, { settings: { ...currentSettings, ...userSettings } });
+	}
+
+	static async generatePasswordResetUrl(user: User): Promise<string> {
+		user.resetPasswordToken = uuid();
+		const { id, resetPasswordToken } = user;
+		const resetPasswordTokenExpiration = Math.floor(Date.now() / 1000) + 7200;
+		await Db.collections.User.update(id, { resetPasswordToken, resetPasswordTokenExpiration });
+
+		const baseUrl = getInstanceBaseUrl();
+		const url = new URL(`${baseUrl}/change-password`);
+		url.searchParams.append('userId', id);
+		url.searchParams.append('token', resetPasswordToken);
+		return url.toString();
 	}
 }
