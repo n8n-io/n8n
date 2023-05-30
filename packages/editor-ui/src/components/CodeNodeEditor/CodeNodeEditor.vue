97526f43a9c37c1a946ba24aba38554afd5e5339
Iván Ovejero <ivov.src@gmail.com>
2023-05-30 18:43:46 +0200

    fix: Show `Ask AI` only on Code Node (#6336)

--- packages/editor-ui/src/components/CodeNodeEditor/CodeNodeEditor.vue
+++ packages/editor-ui/src/components/CodeNodeEditor/CodeNodeEditor.vue
@@ -7,7 +7,7 @@
 	>
 		<div ref="codeNodeEditor" class="code-node-editor-input ph-no-capture"></div>
 		<n8n-button
-			v-if="isCloud && (isEditorHovered || isEditorFocused)"
+			v-if="aiButtonEnabled && (isEditorHovered || isEditorFocused)"
 			size="small"
 			type="tertiary"
 			:class="$style['ask-ai-button']"
@@ -38,7 +38,6 @@ import { workflowHelpers } from '@/mixins/workflowHelpers'; // for json field co
 import { ASK_AI_MODAL_KEY, CODE_NODE_TYPE } from '@/constants';
 import { codeNodeEditorEventBus } from '@/event-bus';
 import { useRootStore } from '@/stores/n8nRoot.store';
-import { useSettingsStore } from '@/stores/settings.store';
 
 import { readOnlyEditorExtensions, writableEditorExtensions } from './baseExtensions';
 import { CODE_PLACEHOLDERS } from './constants';
@@ -50,6 +49,10 @@ export default defineComponent({
 	name: 'code-node-editor',
 	mixins: [linterExtension, completerExtension, workflowHelpers],
 	props: {
+		aiButtonEnabled: {
+			type: Boolean,
+			default: false,
+		},
 		mode: {
 			type: String as PropType<CodeExecutionMode>,
 			validator: (value: CodeExecutionMode): boolean => CODE_EXECUTION_MODES.includes(value),
@@ -97,9 +100,6 @@ export default defineComponent({
 	},
 	computed: {
 		...mapStores(useRootStore),
-		isCloud() {
-			return useSettingsStore().deploymentType === 'cloud';
-		},
 		content(): string {
 			if (!this.editor) return '';
 
