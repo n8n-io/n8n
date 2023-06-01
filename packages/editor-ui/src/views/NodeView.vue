481ca36269177c074276dff73e67edaeaae53477
OlegIvaniv <me@olegivaniv.com>
2023-06-01 19:34:36 +0200

    fix(editor): Fix an issue with connections breaking during renaming (#6358)
    * fix(editor): Fix an issue where dragging during when rename modal active would break workflow connections
    
    * Use enum to check for confirm state of modal

--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -3044,6 +3044,8 @@ export default defineComponent({
 
 				const promptResponse = (await promptResponsePromise) as MessageBoxInputData;
 
+				if (promptResponse?.action !== MODAL_CONFIRM) return;
+
 				await this.renameNode(currentName, promptResponse.value, true);
 			} catch (e) {}
 		},
