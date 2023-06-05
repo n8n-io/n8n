69b3a586584171cd76bcb1b1ee05ad9e2923f515
Ricardo Espinoza <ricardo@n8n.io>
2023-06-05 13:47:06 -0400

    refactor(editor): Remove user activation modal (no-changelog) (#6361)
    * Remove user activation modal
    
    * remove export from index.ts
    
    * Update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

--- packages/editor-ui/src/components/Modals.vue
+++ packages/editor-ui/src/components/Modals.vue
@@ -91,10 +91,6 @@
 			<ImportCurlModal />
 		</ModalRoot>
 
-		<ModalRoot :name="USER_ACTIVATION_SURVEY_MODAL">
-			<WorkflowSuccessModal />
-		</ModalRoot>
-
 		<ModalRoot :name="COMMUNITY_PACKAGE_CONFIRM_MODAL_KEY">
 			<template #default="{ modalName, activeId, mode }">
 				<CommunityPackageManageConfirmModal
@@ -149,7 +145,6 @@ import {
 	IMPORT_CURL_MODAL_KEY,
 	LOG_STREAM_MODAL_KEY,
 	ASK_AI_MODAL_KEY,
-	USER_ACTIVATION_SURVEY_MODAL,
 	VERSION_CONTROL_PUSH_MODAL_KEY,
 } from '@/constants';
 
@@ -175,7 +170,6 @@ import ExecutionsModal from './ExecutionsModal.vue';
 import ActivationModal from './ActivationModal.vue';
 import ImportCurlModal from './ImportCurlModal.vue';
 import WorkflowShareModal from './WorkflowShareModal.ee.vue';
-import WorkflowSuccessModal from './UserActivationSurveyModal.vue';
 import EventDestinationSettingsModal from '@/components/SettingsLogStreaming/EventDestinationSettingsModal.ee.vue';
 import VersionControlPushModal from '@/components/VersionControlPushModal.ee.vue';
 
@@ -205,7 +199,6 @@ export default defineComponent({
 		WorkflowShareModal,
 		ImportCurlModal,
 		EventDestinationSettingsModal,
-		WorkflowSuccessModal,
 		VersionControlPushModal,
 	},
 	data: () => ({
@@ -231,7 +224,6 @@ export default defineComponent({
 		WORKFLOW_ACTIVE_MODAL_KEY,
 		IMPORT_CURL_MODAL_KEY,
 		LOG_STREAM_MODAL_KEY,
-		USER_ACTIVATION_SURVEY_MODAL,
 		VERSION_CONTROL_PUSH_MODAL_KEY,
 	}),
 });
--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -2023,7 +2023,6 @@ export interface IUserManagementSettings {
 
 export interface IUserSettings {
 	isOnboarded?: boolean;
-	showUserActivationSurvey?: boolean;
 	firstSuccessfulWorkflowId?: string;
 	userActivated?: boolean;
 	allowSSOManualLogin?: boolean;
@@ -2072,7 +2071,6 @@ export interface IN8nUISettings {
 		debug: boolean;
 	};
 	personalizationSurveyEnabled: boolean;
-	userActivationSurveyEnabled: boolean;
 	defaultLocale: string;
 	userManagement: IUserManagementSettings;
 	sso: {
