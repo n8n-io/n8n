dba3f44bc00de68113cc98db9afc6267f56ec04c
Csaba Tuncsik <csaba@n8n.io>
2023-06-06 11:23:53 +0200

    fix(editor): Adding branch color (#6380)
    * fix(editor): Adding branch color
    
    * fix(editor): backend refactor preferences update
    
    * fix(editor): frontend refactor preferences update

--- packages/cli/src/environments/versionControl/versionControlPreferences.service.ee.ts
+++ packages/cli/src/environments/versionControl/versionControlPreferences.service.ee.ts
@@ -114,19 +114,10 @@ export class VersionControlPreferencesService {
 		return this.versionControlPreferences;
 	}
 
-	setBranchReadOnly(branchReadOnly: boolean): void {
-		this._versionControlPreferences.branchReadOnly = branchReadOnly;
-	}
-
 	async validateVersionControlPreferences(
 		preferences: Partial<VersionControlPreferences>,
 		allowMissingProperties = true,
 	): Promise<ValidationError[]> {
-		if (this.isVersionControlConnected()) {
-			if (preferences.repositoryUrl !== this._versionControlPreferences.repositoryUrl) {
-				throw new Error('Cannot change repository while connected');
-			}
-		}
 		const preferencesObject = new VersionControlPreferences(preferences);
 		const validationResult = await validate(preferencesObject, {
 			forbidUnknownValues: false,
