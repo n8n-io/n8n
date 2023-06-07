f06f281caebc3218b28b22927837aed7a8080733
cyla <cyril@numberly.com>
2023-06-07 10:18:55 +0200

    support for more ssl options when connecting to kafka brokers

--- packages/nodes-base/credentials/Kafka.credentials.ts
+++ packages/nodes-base/credentials/Kafka.credentials.ts
@@ -30,6 +30,67 @@ export class Kafka implements ICredentialType {
 			default: true,
 		},
 		{
+			displayName: 'SSL CA',
+			name: 'sslCa',
+			type: 'string',
+			typeOptions: {
+				rows: 4,
+			},
+			displayOptions: {
+				show: {
+					ssl: [true],
+				},
+			},
+			default: '',
+			description: 'Cert chains in PEM format. One cert chain should be provided per private key. Each cert chain should consist of the PEM formatted certificate for a provided private key, followed by the PEM formatted intermediate certificates (if any), in order, and not including the root CA (the root CA must be pre-known to the peer, see ca). When providing multiple cert chains, they do not have to be in the same order as their private keys in key. If the intermediate certificates are not provided, the peer will not be able to validate the certificate, and the handshake will fail.', 
+			placeholder: '-----BEGIN CERTIFICATE-----\n\n-----END CERTIFICATE-----',
+		},
+		{
+			displayName: 'SSL Cert',
+			name: 'sslCert',
+			type: 'string',
+			typeOptions: {
+				rows: 4,
+			},
+			displayOptions: {
+				show: {
+					ssl: [true],
+				},
+			},
+			default: '',
+			description: 'Cert chains in PEM format. One cert chain should be provided per private key. Each cert chain should consist of the PEM formatted certificate for a provided private key, followed by the PEM formatted intermediate certificates (if any), in order, and not including the root CA (the root CA must be pre-known to the peer, see ca). When providing multiple cert chains, they do not have to be in the same order as their private keys in key. If the intermediate certificates are not provided, the peer will not be able to validate the certificate, and the handshake will fail.', 
+			placeholder: '-----BEGIN CERTIFICATE-----\n\n-----END CERTIFICATE-----',
+		},
+		{
+			displayName: 'SSL Key',
+			name: 'sslKey',
+			type: 'string',
+			typeOptions: {
+				rows: 4,
+			},
+			displayOptions: {
+				show: {
+					ssl: [true],
+				},
+			},
+			default: '',
+			description: 'Private keys in PEM format. PEM allows the option of private keys being encrypted. Encrypted keys will be decrypted with options.passphrase. Multiple keys using different algorithms can be provided either as an array of unencrypted key strings or buffers, or an array of objects in the form {pem: <string|buffer>[, passphrase: ]}. The object form can only occur in an array. object.passphrase is optional. Encrypted keys will be decrypted with object.passphrase if provided, or options.passphrase if it is not.', 
+			placeholder: '-----BEGIN PRIVATE KEY-----\n\n-----END PRIVATE KEY-----',
+		},
+		{
+			displayName: 'SSL Key Passphrase',
+			name: 'sslPassphrase',
+			type: 'string',
+			displayOptions: {
+				show: {
+					ssl: [true],
+				},
+			},
+			default: '',
+			description: 'Shared passphrase used for a single private key and/or a PFX.', 
+			placeholder: '',
+		},
+		{
 			displayName: 'Authentication',
 			name: 'authentication',
 			type: 'boolean',
