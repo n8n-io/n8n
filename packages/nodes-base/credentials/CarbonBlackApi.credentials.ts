fa20d817102bda839aeb8703677d151fde95648c
agobrech <ael.gobrecht@gmail.com>
2023-06-14 14:19:39 +0200

    Add Carbon black credentials

---
+++ packages/nodes-base/credentials/CarbonBlackApi.credentials.ts
@@ -0,0 +1,44 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class CarbonBlackApi implements ICredentialType {
+	name = 'carbonBlackApi';
+
+	displayName = 'Carbon Black API';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API URL',
+			name: 'apiUrl',
+			type: 'string',
+			default: 'https://defense.conferdeploy.net/',
+		},
+		{
+			displayName: 'Access Token',
+			name: 'accessToken',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'X-Auth-Token': '={{$credentials.accessToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials.apiUrl}}',
+			url: 'integrationServices/v3/auditlogs',
+		},
+	};
+}
