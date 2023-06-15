a3b7b70f3f8f74df14ea03823b51194b34ee02c8
agobrech <ael.gobrecht@gmail.com>
2023-06-15 11:58:55 +0200

    Add credentials Azure IAM OAuth2

---
+++ packages/nodes-base/credentials/AzureIAMOAuth2Api.credentials.ts
@@ -0,0 +1,20 @@
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class AzureIAMOAuth2Api implements ICredentialType {
+	name = 'azureIAMOAuth2Api';
+
+	displayName = 'Azure IAM OAuth2 API';
+
+	extends = ['microsoftOAuth2Api'];
+
+	documentationUrl = 'microsoft';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden',
+			default: 'user_impersonation',
+		},
+	];
+}
