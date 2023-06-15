002fe5fbf2a2da367ac962e60eadde60c2405ef1
agobrech <ael.gobrecht@gmail.com>
2023-06-15 10:55:44 +0200

    Add Virus Total credentials

---
+++ packages/nodes-base/credentials/VirusTotalApi.credentials.ts
@@ -0,0 +1,38 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class VirusTotalApi implements ICredentialType {
+	name = 'virusTotalApi';
+
+	displayName = 'Virus Total API';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Token',
+			name: 'accessToken',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-apikey': '={{$credentials.accessToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://www.virustotal.com/api/v3',
+			url: '/popular_threat_categories',
+		},
+	};
+}
