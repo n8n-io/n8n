1766cc4e08c3e3744b15082885df21954dd82ce7
agobrech <ael.gobrecht@gmail.com>
2023-06-15 11:04:23 +0200

    Add CiscoAMP credentials

---
+++ packages/nodes-base/credentials/CiscoAMPApi.credentials.ts
@@ -0,0 +1,45 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class CiscoAMPApi implements ICredentialType {
+	name = 'ciscoAMPApi';
+
+	displayName = 'Cisco AMP API';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Api Key',
+			name: 'accessToken',
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			auth: {
+				username: '={{$credentials.clientId}}',
+				password: '={{$credentials.accessToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.amp.cisco.com/v1',
+			url: '/version',
+		},
+	};
+}
