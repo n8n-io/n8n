72f0a8a0e183e8875a7e3fe6f41f8a0171634257
agobrech <ael.gobrecht@gmail.com>
2023-06-15 12:17:41 +0200

    Add ArcSight credentials

---
+++ packages/nodes-base/credentials/ArcSightOAuth2Api.credentials.ts
@@ -0,0 +1,100 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestHelper,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ArcSightOAuth2Api implements ICredentialType {
+	name = 'arcSigthOAuth2Api';
+
+	displayName = 'ArcSight OAuth2 API';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Session Token',
+			name: 'sessionToken',
+			type: 'hidden',
+			typeOptions: {
+				expirable: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'Hostname',
+			name: 'url',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+		{
+			displayName: 'Username',
+			name: 'username',
+			type: 'string',
+			default: '',
+		},
+		{
+			displayName: 'Password',
+			name: 'password',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+	];
+
+	async preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) {
+		// make reques to get session token
+		const url = credentials.url as string;
+		const token = await this.helpers.httpRequest({
+			method: 'POST',
+			url: `${url.endsWith('/') ? url.slice(0, -1) : url}/api/session`,
+			body: {
+				username: credentials.username,
+				password: credentials.password,
+				grant_type: 'password',
+			},
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: `Basic ${Buffer.from(
+					`${credentials.clientId}':'${credentials.clientSecret}`,
+				).toString('base64')}`,
+			},
+		});
+		console.log(token);
+		return { sessionToken: token };
+	}
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: 'Bearer ={{$credentials.sessionToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://={{$credentials?.url}}',
+			url: '/arcmc/rest-api-docs',
+		},
+	};
+}
