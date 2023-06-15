7d6178343f7f77d1bff90f9b50eb4a8437b35e35
agobrech <ael.gobrecht@gmail.com>
2023-06-15 11:55:36 +0200

    Add Microsoft Sentinel OAuth2

---
+++ packages/nodes-base/credentials/MicrosoftSentinelOAuth2Api.credentials.ts
@@ -0,0 +1,20 @@
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class MicrosoftSentinelOAuth2Api implements ICredentialType {
+	name = 'microsoftSentinelOAuth2Api';
+
+	displayName = 'Microsof Sentinel API';
+
+	extends = ['microsoftOAuth2Api'];
+
+	documentationUrl = 'microsoft';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden',
+			default: 'user_impersonation',
+		},
+	];
+}
