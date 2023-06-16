e8882b94c68a779839feccbabe5b7fcbf84c7f03
Alex Grozav <alex@grozav.com>
2023-06-16 11:56:21 +0300

    fix: prevent cypress from replacing global with globalThis in import path

--- packages/editor-ui/src/event-bus/index.ts
+++ packages/editor-ui/src/event-bus/index.ts
@@ -1,5 +1,5 @@
 export * from './code-node-editor';
 export * from './data-pinning';
-export * from './global-link-actions';
+export * from './link-actions';
 export * from './html-editor';
 export * from './node-view';
