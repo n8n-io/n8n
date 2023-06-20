fa1e73c2bd9ed27410ad1ea3760c0c1ee1d8d386
Marcus <marcus@n8n.io>
2023-06-20 13:55:41 +0200

    remove redundant codeVerifier

--- packages/cli/src/Server.ts
+++ packages/cli/src/Server.ts
@@ -187,8 +187,6 @@ export class Server extends AbstractServer {
 
 	loadNodesAndCredentials: LoadNodesAndCredentials;
 
-	codeVerifer: string;
-
 	nodeTypes: NodeTypes;
 
 	credentialTypes: ICredentialTypes;
