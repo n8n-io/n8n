409e0e202c3569c3cffe67040e09b8e1bf5fde99
agobrech <ael.gobrecht@gmail.com>
2023-06-20 12:46:59 +0200

    Add notice for TwitterOAuth2 api link

--- packages/nodes-base/credentials/TwitterOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/TwitterOAuth2Api.credentials.ts
@@ -27,6 +27,13 @@ export class TwitterOAuth2Api implements ICredentialType {
 
 	properties: INodeProperties[] = [
 		{
+			displayName:
+				'Some operations requires a Basic or a Pro API for more informations see <a href="https://developer.twitter.com/en/products/twitter-api" target="_blank">Twitter Api Doc</a>',
+			name: 'apiPermissioms',
+			type: 'notice',
+			default: '',
+		},
+		{
 			displayName: 'Grant Type',
 			name: 'grantType',
 			type: 'hidden',
