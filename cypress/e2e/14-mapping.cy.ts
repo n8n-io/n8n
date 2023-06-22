5a32f9334979d314119c41c5346bc26e01aa24a6
Ricardo Espinoza <ricardo@n8n.io>
2023-06-22 19:45:54 -0400

    fix: Show error when referencing node that exist but has not been executed (#6496)
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

--- cypress/e2e/14-mapping.cy.ts
+++ cypress/e2e/14-mapping.cy.ts
@@ -188,7 +188,11 @@ describe('Data mapping', () => {
 		ndv.getters
 			.inlineExpressionEditorInput()
 			.should('have.text', `{{ $('${SCHEDULE_TRIGGER_NODE_NAME}').item.json.input[0].count }}`);
-		ndv.getters.parameterExpressionPreview('value').should('not.exist');
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.invoke('text')
+			.invoke('replace', /\u00a0/g, ' ')
+			.should('equal', '[ERROR: no data, execute "Schedule Trigger" node first]');
 
 		ndv.actions.switchInputMode('Table');
 		ndv.actions.mapDataFromHeader(1, 'value');
