256e338aa5248e2ff3749ad3e30724788de7a056
Jon <jonathan.bennetts@gmail.com>
2023-06-23 11:29:24 +0100

    feat: Add missing input panels to some trigger nodes (#6518)

--- packages/nodes-base/nodes/Postgres/PostgresTrigger.node.ts
+++ packages/nodes-base/nodes/Postgres/PostgresTrigger.node.ts
@@ -21,8 +21,20 @@ export class PostgresTrigger implements INodeType {
 		group: ['trigger'],
 		version: 1,
 		description: 'Listens to Postgres messages',
+		eventTriggerDescription: '',
 		defaults: {
 			name: 'Postgres Trigger',
+		},
+		triggerPanel: {
+			header: '',
+			executionsHelp: {
+				inactive:
+					"<b>While building your workflow</b>, click the 'listen' button, then trigger a Postgres event. This will trigger an execution, which will show up in this editor.<br /> <br /><b>Once you're happy with your workflow</b>, <a data-key='activate'>activate</a> it. Then every time a change is detected, the workflow will execute. These executions will show up in the <a data-key='executions'>executions list</a>, but not in the editor.",
+				active:
+					"<b>While building your workflow</b>, click the 'listen' button, then trigger a Postgres event. This will trigger an execution, which will show up in this editor.<br /> <br /><b>Your workflow will also execute automatically</b>, since it's activated. Every time a change is detected, this node will trigger an execution. These executions will show up in the <a data-key='executions'>executions list</a>, but not in the editor.",
+			},
+			activationHint:
+				"Once you’ve finished building your workflow, <a data-key='activate'>activate</a> it to have it also listen continuously (you just won’t see those executions here).",
 		},
 		inputs: [],
 		outputs: ['main'],
