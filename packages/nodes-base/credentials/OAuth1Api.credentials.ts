23594168fa8ca589d1c4ec7b42f12f2c06f0e9f1
agobrech <ael.gobrecht@gmail.com>
2023-06-27 16:01:07 +0200

    Add notice to oauth1

--- packages/nodes-base/credentials/OAuth1Api.credentials.ts
+++ packages/nodes-base/credentials/OAuth1Api.credentials.ts
@@ -11,6 +11,13 @@ export class OAuth1Api implements ICredentialType {
 
 	properties: INodeProperties[] = [
 		{
+			displayName:
+				'Some operations requires a Basic or a Pro API for more informations see <a href="https://developer.twitter.com/en/products/twitter-api" target="_blank">Twitter Api Doc</a>',
+			name: 'apiPermissioms',
+			type: 'notice',
+			default: '',
+		},
+		{
 			displayName: 'Authorization URL',
 			name: 'authUrl',
 			type: 'string',
