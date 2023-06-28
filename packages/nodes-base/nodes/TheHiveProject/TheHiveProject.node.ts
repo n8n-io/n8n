7db641406010f31b868c3ecbfd00d9bdfae832c4
Michael Kret <michael.k@radency.com>
2023-06-28 07:40:23 +0300

    :zap: RLCs case, alert

--- packages/nodes-base/nodes/TheHiveProject/TheHiveProject.node.ts
+++ packages/nodes-base/nodes/TheHiveProject/TheHiveProject.node.ts
@@ -2,12 +2,12 @@ import type { IExecuteFunctions, INodeType, INodeTypeDescription } from 'n8n-wor
 
 import { versionDescription } from './actions/versionDescription';
 import { router } from './actions/router';
-import { loadOptions } from './methods';
+import { loadOptions, listSearch } from './methods';
 
 export class TheHiveProject implements INodeType {
 	description: INodeTypeDescription = versionDescription;
 
-	methods = { loadOptions };
+	methods = { loadOptions, listSearch };
 
 	async execute(this: IExecuteFunctions) {
 		return router.call(this);
--- packages/nodes-base/nodes/TheHiveProject/actions/common.description.ts
+++ packages/nodes-base/nodes/TheHiveProject/actions/common.description.ts
@@ -1,6 +1,112 @@
 import type { INodeProperties } from 'n8n-workflow';
 import { TLP } from '../helpers/interfaces';
 
+export const caseRLC: INodeProperties = {
+	displayName: 'Case',
+	name: 'caseId',
+	type: 'resourceLocator',
+	default: { mode: 'list', value: '' },
+	required: true,
+	modes: [
+		{
+			displayName: 'From List',
+			name: 'list',
+			type: 'list',
+			placeholder: 'Select a file...',
+			typeOptions: {
+				searchListMethod: 'caseSearch',
+				searchable: true,
+			},
+		},
+		{
+			displayName: 'Link',
+			name: 'url',
+			type: 'string',
+			extractValue: {
+				type: 'regex',
+				regex: 'https:\\/\\/.+\\/cases\\/(~[0-9]{1,})\\/details',
+			},
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: 'https:\\/\\/.+\\/cases\\/(~[0-9]{1,})\\/details',
+						errorMessage: 'Not a valid Case URL',
+					},
+				},
+			],
+		},
+		{
+			displayName: 'ID',
+			name: 'id',
+			type: 'string',
+			placeholder: 'e.g. ~123456789',
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: '(~[0-9]{1,})',
+						errorMessage: 'Not a valid Case ID',
+					},
+				},
+			],
+		},
+	],
+};
+
+export const alertRLC: INodeProperties = {
+	displayName: 'Alert',
+	name: 'alertId',
+	type: 'resourceLocator',
+	default: { mode: 'list', value: '' },
+	required: true,
+	modes: [
+		{
+			displayName: 'From List',
+			name: 'list',
+			type: 'list',
+			placeholder: 'Select a file...',
+			typeOptions: {
+				searchListMethod: 'alertSearch',
+				searchable: true,
+			},
+		},
+		{
+			displayName: 'Link',
+			name: 'url',
+			type: 'string',
+			extractValue: {
+				type: 'regex',
+				regex: 'https:\\/\\/.+\\/alerts\\/(~[0-9]{1,})\\/details',
+			},
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: 'https:\\/\\/.+\\/alerts\\/(~[0-9]{1,})\\/details',
+						errorMessage: 'Not a valid Alert URL',
+					},
+				},
+			],
+		},
+		{
+			displayName: 'ID',
+			name: 'id',
+			type: 'string',
+			placeholder: 'e.g. ~123456789',
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: '(~[0-9]{1,})',
+						errorMessage: 'Not a valid Alert ID',
+					},
+				},
+			],
+		},
+	],
+};
+
 export const returnAllAndLimit: INodeProperties[] = [
 	{
 		displayName: 'Return All',
