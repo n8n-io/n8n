26ccfa987ddd98a5779ce13efc04887f743d123c
Michael Kret <michael.k@radency.com>
2023-07-05 21:50:21 +0300

    :zap: fix

--- packages/nodes-base/nodes/Google/Drive/v2/actions/common.descriptions.ts
+++ packages/nodes-base/nodes/Google/Drive/v2/actions/common.descriptions.ts
@@ -27,14 +27,14 @@ export const fileRLC: INodeProperties = {
 			extractValue: {
 				type: 'regex',
 				regex:
-					'https:\\/\\/(?:drive|docs)\\.google\\.com\\/\\w+\\/d\\/([0-9a-zA-Z\\-_]+)(?:\\/.*|)',
+					'https:\\/\\/(?:drive|docs)\\.google\\.com(?:\\/.*|)\\/d\\/([0-9a-zA-Z\\-_]+)(?:\\/.*|)',
 			},
 			validation: [
 				{
 					type: 'regex',
 					properties: {
 						regex:
-							'https:\\/\\/(?:drive|docs)\\.google.com\\/\\w+\\/d\\/([0-9a-zA-Z\\-_]+)(?:\\/.*|)',
+							'https:\\/\\/(?:drive|docs)\\.google.com(?:\\/.*|)\\/d\\/([0-9a-zA-Z\\-_]+)(?:\\/.*|)',
 						errorMessage: 'Not a valid Google Drive File URL',
 					},
 				},
