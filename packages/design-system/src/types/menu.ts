84466e983336a3cfa8c20b30ead8d58abbf07cf0
Csaba Tuncsik <csaba@n8n.io>
2023-07-13 13:27:25 +0200

    fix(editor): Update design system menu item component (#6659)

--- packages/design-system/src/types/menu.ts
+++ packages/design-system/src/types/menu.ts
@@ -4,7 +4,11 @@ export type IMenuItem = {
 	id: string;
 	label: string;
 	icon?: string;
-	secondaryIcon?: { name: string; size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge' };
+	secondaryIcon?: {
+		name: string;
+		size?: 'xsmall' | 'small' | 'medium' | 'large' | 'xlarge';
+		tooltip?: Tooltip;
+	};
 	customIconSize?: 'medium' | 'small';
 	available?: boolean;
 	position?: 'top' | 'bottom';
@@ -16,7 +20,6 @@ export type IMenuItem = {
 	// For more specific matching, we can use paths
 	activateOnRoutePaths?: string[];
 	children?: IMenuItem[];
-	tooltip?: Tooltip & { bindTo?: 'menuItem' | 'secondaryIcon' };
 };
 
 export type ILinkMenuItemProperties = {
