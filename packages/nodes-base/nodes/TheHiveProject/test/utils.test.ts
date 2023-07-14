dea3a0ec2a9236a40d10fba9cecfe41cc7f7b4e3
Michael Kret <michael.k@radency.com>
2023-07-14 13:28:34 +0300

    :zap: utils tests

---
+++ packages/nodes-base/nodes/TheHiveProject/test/utils.test.ts
@@ -0,0 +1,108 @@
+import { splitAndTrim, fixFieldType, prepareInputItem } from '../helpers/utils';
+
+describe('Test TheHiveProject, splitAndTrim', () => {
+	it('should split and trim string, removing empty entries', () => {
+		const data = 'a, b,, c, d, e, f,,';
+
+		const result = splitAndTrim(data);
+
+		expect(result).toEqual(['a', 'b', 'c', 'd', 'e', 'f']);
+	});
+
+	it('should return unchanged array', () => {
+		const data = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+		const result = splitAndTrim(data);
+
+		expect(result).toEqual(data);
+	});
+});
+
+describe('Test TheHiveProject, fixFieldType', () => {
+	it('should split and trim tags', () => {
+		const data = {
+			tags: 'a, b,, c, d, e, f,,',
+			addTags: 'a, b,, c, d, e, f,,',
+			removeTags: 'a, b,, c, d, e, f,,',
+			notChanged: 'a, b,, c, d, e, f,,',
+		};
+
+		const result = fixFieldType(data);
+
+		expect(result).toEqual({
+			tags: ['a', 'b', 'c', 'd', 'e', 'f'],
+			addTags: ['a', 'b', 'c', 'd', 'e', 'f'],
+			removeTags: ['a', 'b', 'c', 'd', 'e', 'f'],
+			notChanged: 'a, b,, c, d, e, f,,',
+		});
+	});
+
+	it('should convert date strings to milis', () => {
+		const data = {
+			date: '2020-01-01T00:00:00.000Z',
+			lastSyncDate: '2020-01-01T00:00:00.000Z',
+			startDate: '2020-01-01T00:00:00.000Z',
+			endDate: '2020-01-01T00:00:00.000Z',
+			dueDate: '2020-01-01T00:00:00.000Z',
+			includeInTimeline: '2020-01-01T00:00:00.000Z',
+			sightedAt: '2020-01-01T00:00:00.000Z',
+			notChanged: '2020-01-01T00:00:00.000Z',
+		};
+
+		const result = fixFieldType(data);
+
+		expect(result).toEqual({
+			date: 1577836800000,
+			lastSyncDate: 1577836800000,
+			startDate: 1577836800000,
+			endDate: 1577836800000,
+			dueDate: 1577836800000,
+			includeInTimeline: 1577836800000,
+			sightedAt: 1577836800000,
+			notChanged: '2020-01-01T00:00:00.000Z',
+		});
+	});
+});
+
+describe('Test TheHiveProject, prepareInputItem', () => {
+	it('should return object with fields present in schema', () => {
+		const data = {
+			a: 1,
+			b: 2,
+			c: 3,
+			d: 4,
+			f: 5,
+			g: 6,
+		};
+
+		const schema = [
+			{
+				id: 'a',
+				required: true,
+			},
+			{
+				id: 'b',
+				required: true,
+			},
+			{
+				id: 'c',
+			},
+			{
+				id: 'd',
+				required: true,
+			},
+			{
+				id: 'e',
+			},
+		];
+
+		const result = prepareInputItem(data, schema, 0);
+
+		expect(result).toEqual({
+			a: 1,
+			b: 2,
+			c: 3,
+			d: 4,
+		});
+	});
+});
