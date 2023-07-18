00a0a0b38664064f0f75e9c55935f8ea38300a02
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-07-18 02:48:31 +0200

    delete duplicate code for sorting via code in ItemList node

--- packages/nodes-base/nodes/ItemLists/V3/helpers/utils.ts
+++ packages/nodes-base/nodes/ItemLists/V3/helpers/utils.ts
@@ -1,4 +1,11 @@
-import type { IDataObject, INode, INodeExecutionData } from 'n8n-workflow';
+import { NodeVM } from 'vm2';
+import {
+	NodeOperationError,
+	type IDataObject,
+	type IExecuteFunctions,
+	type INode,
+	type INodeExecutionData,
+} from 'n8n-workflow';
 
 import get from 'lodash/get';
 import isEqual from 'lodash/isEqual';
@@ -57,3 +64,26 @@ export const prepareFieldsArray = (fields: string | string[], fieldName = 'Field
 		`The \'${fieldName}\' parameter must be a string of fields separated by commas or an array of strings.`,
 	);
 };
+
+const returnRegExp = /\breturn\b/g;
+
+export function sortByCode(
+	this: IExecuteFunctions,
+	items: INodeExecutionData[],
+): INodeExecutionData[] {
+	const code = this.getNodeParameter('code', 0) as string;
+	if (!returnRegExp.test(code)) {
+		throw new NodeOperationError(
+			this.getNode(),
+			"Sort code doesn't return. Please add a 'return' statement to your code",
+		);
+	}
+
+	const mode = this.getMode();
+	const vm = new NodeVM({
+		console: mode === 'manual' ? 'redirect' : 'inherit',
+		sandbox: { items },
+	});
+
+	return vm.run(`module.exports = items.sort((a, b) => { ${code} })`);
+}
