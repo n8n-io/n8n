c3c845c20f5960d39afce739ce8f2515e07038e8
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2023-07-25 16:59:44 +0300

    fix(Telegram Trigger Node): Add guard to 'include' call on null or undefined (#6730)

--- packages/nodes-base/nodes/Telegram/TelegramTrigger.node.ts
+++ packages/nodes-base/nodes/Telegram/TelegramTrigger.node.ts
@@ -182,7 +182,7 @@ export class TelegramTrigger implements INodeType {
 
 				let allowedUpdates = this.getNodeParameter('updates') as string[];
 
-				if (allowedUpdates.includes('*')) {
+				if ((allowedUpdates || []).includes('*')) {
 					allowedUpdates = [];
 				}
 
