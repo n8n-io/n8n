ff36a175029cf7ffc5888c542a7a61f2acd6a16e
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-07-26 09:39:15 +0200

    fix(core): Fix RemoveResetPasswordColumns migration for sqlite (no-changelog) (#6739)

--- packages/cli/src/databases/migrations/common/1690000000030-RemoveResetPasswordColumns.ts
+++ packages/cli/src/databases/migrations/common/1690000000030-RemoveResetPasswordColumns.ts
@@ -3,27 +3,24 @@ import { TableColumn } from 'typeorm';
 
 export class RemoveResetPasswordColumns1690000000030 implements ReversibleMigration {
 	async up({ queryRunner, tablePrefix }: MigrationContext) {
-		await queryRunner.dropColumn(`${tablePrefix}user`, 'resetPasswordToken');
-		await queryRunner.dropColumn(`${tablePrefix}user`, 'resetPasswordTokenExpiration');
+		await queryRunner.dropColumns(`${tablePrefix}user`, [
+			'resetPasswordToken',
+			'resetPasswordTokenExpiration',
+		]);
 	}
 
 	async down({ queryRunner, tablePrefix }: MigrationContext) {
-		await queryRunner.addColumn(
-			`${tablePrefix}user`,
+		await queryRunner.addColumns(`${tablePrefix}user`, [
 			new TableColumn({
 				name: 'resetPasswordToken',
 				type: 'varchar',
 				isNullable: true,
 			}),
-		);
-
-		await queryRunner.addColumn(
-			`${tablePrefix}user`,
 			new TableColumn({
 				name: 'resetPasswordTokenExpiration',
 				type: 'int',
 				isNullable: true,
 			}),
-		);
+		]);
 	}
 }
