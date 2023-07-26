766d774d6d7ce551517ea8e5e7f738ecef9b4860
Oleg Ivaniv <me@olegivaniv.com>
2023-07-26 15:59:14 +0200

    fix(editor): Fix settings initialisation
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/editor-ui/src/App.vue
+++ packages/editor-ui/src/App.vue
@@ -101,13 +101,18 @@ export default defineComponent({
 	data() {
 		return {
 			postAuthenticateDone: false,
+			settingsInitialised: false,
 			loading: true,
 		};
 	},
 	methods: {
 		async initSettings(): Promise<void> {
+			// The settings should only be initialized once
+			if(this.settingsInitialised) return;
+
 			try {
 				await this.settingsStore.getSettings();
+				this.settingsInitialised = true;
 			} catch (e) {
 				this.showToast({
 					title: this.$locale.baseText('startupError'),
@@ -139,7 +144,6 @@ export default defineComponent({
 			}
 		},
 		async initialize(): Promise<void> {
-			await this.initSettings();
 			await Promise.all([this.loginWithCookie(), this.initTemplates()]);
 		},
 		trackPage(): void {
@@ -250,7 +254,7 @@ export default defineComponent({
 			this.postAuthenticateDone = true;
 		},
 	},
-	async mounted() {
+	async created() {
 		this.setTheme();
 		await this.initialize();
 		this.logHiringBanner();
@@ -264,7 +268,8 @@ export default defineComponent({
 		void this.postAuthenticate();
 
 		this.loading = false;
-
+	},
+	async mounted() {
 		this.trackPage();
 		void this.externalHooks.run('app.mount');
 
@@ -278,7 +283,8 @@ export default defineComponent({
 				void this.postAuthenticate();
 			}
 		},
-		$route(route) {
+		async $route(route) {
+			await this.initSettings();
 			this.authenticate();
 			this.redirectIfNecessary();
 
