920b53930af9ddac85dd37f55c21f26717a12327
Oleg Ivaniv <me@olegivaniv.com>
2023-07-28 11:26:50 +0200

    Use span as component wrapper instead of div
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/editor-ui/src/components/NodeExecuteButton.vue
+++ packages/editor-ui/src/components/NodeExecuteButton.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<span>
 		<n8n-tooltip placement="bottom" :disabled="!disabledHint">
 			<template #content>
 				<div>{{ disabledHint }}</div>
@@ -17,7 +17,7 @@
 				/>
 			</div>
 		</n8n-tooltip>
-	</div>
+	</span>
 </template>
 
 <script lang="ts">
