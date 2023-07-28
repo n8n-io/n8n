06908802477bc547ae009d3aaf160d0cefaac5b1
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-07-28 18:07:51 +0200

    refactor: move @n8n_io/eslint-config to @n8n/eslint-config (no-changelog)

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,10 +1,10 @@
-const sharedOptions = require('@n8n_io/eslint-config/shared');
+const sharedOptions = require('@n8n/eslint-config/shared');
 
 /**
  * @type {import('@types/eslint').ESLint.ConfigData}
  */
 module.exports = {
-	extends: ['@n8n_io/eslint-config/node'],
+	extends: ['@n8n/eslint-config/node'],
 
 	...sharedOptions(__dirname),
 
