b2d189f2b8ed86f53e5942b9c9d909c65ea61a76
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-07-31 09:51:50 +0200

    use raw queries instead for now

--- packages/cli/src/databases/migrations/common/1690787606731-AddMissingIndexOnExecutionData.ts
+++ packages/cli/src/databases/migrations/common/1690787606731-AddMissingIndexOnExecutionData.ts
@@ -1,18 +1,25 @@
 import type { ReversibleMigration, MigrationContext } from '@/databases/types';
 
 export class AddMissingIndexOnExecutionData1690787606731 implements ReversibleMigration {
-	async up({ queryRunner, tablePrefix }: MigrationContext) {
-		await queryRunner.createPrimaryKey(
-			`${tablePrefix}execution_data`,
-			['executionId'],
-			`PK_${tablePrefix}execution_data_executionId`,
-		);
+	async up({ queryRunner, tablePrefix, dbType }: MigrationContext) {
+		if (dbType === 'sqlite' || dbType === 'postgresdb') {
+			await queryRunner.query(
+				`CREATE UNIQUE INDEX "PK_${tablePrefix}execution_data_executionId" ON "${tablePrefix}execution_data"("executionId");`,
+			);
+		} else {
+			await queryRunner.query(
+				`CREATE UNIQUE INDEX \`PK_${tablePrefix}execution_data_executionId\` ON \`${tablePrefix}execution_data\`(\`executionId\`);`,
+			);
+		}
 	}
 
-	async down({ queryRunner, tablePrefix }: MigrationContext) {
-		await queryRunner.dropPrimaryKey(
-			`${tablePrefix}execution_data`,
-			`PK_${tablePrefix}execution_data_executionId`,
-		);
+	async down({ queryRunner, tablePrefix, dbType }: MigrationContext) {
+		if (dbType === 'sqlite' || dbType === 'postgresdb') {
+			await queryRunner.query(`DROP INDEX "PK_${tablePrefix}execution_data_executionId"`);
+		} else {
+			await queryRunner.query(
+				`DROP INDEX \`PK_${tablePrefix}execution_data_executionId\` ON \`${tablePrefix}execution_data\``,
+			);
+		}
 	}
 }
