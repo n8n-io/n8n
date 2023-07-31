b39877fcf4f470a401830a3059f1afbec326e321
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-07-31 12:35:42 +0200

    fix(core): Add missing primary key on the `execution_data` table on postgres (#6797)

--- packages/cli/src/databases/migrations/postgresdb/index.ts
+++ packages/cli/src/databases/migrations/postgresdb/index.ts
@@ -40,6 +40,7 @@ import { MigrateIntegerKeysToString1690000000000 } from './1690000000000-Migrate
 import { SeparateExecutionData1690000000020 } from './1690000000020-SeparateExecutionData';
 import { RemoveSkipOwnerSetup1681134145997 } from './1681134145997-RemoveSkipOwnerSetup';
 import { RemoveResetPasswordColumns1690000000030 } from '../common/1690000000030-RemoveResetPasswordColumns';
+import { AddMissingPrimaryKeyOnExecutionData1690787606731 } from './1690787606731-AddMissingPrimaryKeyOnExecutionData';
 
 export const postgresMigrations: Migration[] = [
 	InitialMigration1587669153312,
@@ -83,4 +84,5 @@ export const postgresMigrations: Migration[] = [
 	SeparateExecutionData1690000000020,
 	RemoveSkipOwnerSetup1681134145997,
 	RemoveResetPasswordColumns1690000000030,
+	AddMissingPrimaryKeyOnExecutionData1690787606731,
 ];
