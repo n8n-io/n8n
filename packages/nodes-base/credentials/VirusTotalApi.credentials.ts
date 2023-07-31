11567f946be411f081b2ba25c2ac60d5dcf56835
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2023-07-31 11:26:38 +0300

    feat(core): Credentials for popular SecOps services, Part 1 (#6775)

---
+++ packages/nodes-base/credentials/VirusTotalApi.credentials.ts
@@ -0,0 +1,41 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class VirusTotalApi implements ICredentialType {
+	name = 'virusTotalApi';
+
+	displayName = 'Virus Total API';
+
+	icon = 'file:icons/VirusTotal.svg';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Token',
+			name: 'accessToken',
+			type: 'string',
+			typeOptions: { password: true },
+			required: true,
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-apikey': '={{$credentials.accessToken}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://www.virustotal.com/api/v3',
+			url: '/popular_threat_categories',
+		},
+	};
+}
