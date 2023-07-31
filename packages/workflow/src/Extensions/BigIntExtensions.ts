58585f165bb58520eccc728fd90cfaf041f95cc0
Michael Auerswald <michael.auerswald@gmail.com>
2023-07-31 15:03:09 +0200

    refactor

--- packages/workflow/src/Extensions/BigIntExtensions.ts
+++
@@ -1,6 +0,0 @@
-// workaround to keep typescript happy
-// implementation is in packages/workflow/src/utils.ts
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface BigInt {
-	toJSON(): string;
-}
