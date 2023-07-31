3b2be755bc69f19a0fb84521a6d55f4716c0b282
Oleg Ivaniv <me@olegivaniv.com>
2023-07-31 11:01:54 +0200

    Do not chaing realHover after assertion
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- cypress/e2e/24-ndv-paired-item.cy.ts
+++ cypress/e2e/24-ndv-paired-item.cy.ts
@@ -153,9 +153,13 @@ describe('NDV', () => {
 			.inputTableRow(1)
 			.invoke('attr', 'data-test-id')
 			.should('equal', 'hovering-item');
-		ndv.getters.outputTableRow(1).should('have.text', '1111').realHover();
+		ndv.getters.outputTableRow(1).should('have.text', '1111');
+		ndv.getters.outputTableRow(1).realHover();
 
-		ndv.getters.outputTableRow(3).should('have.text', '4444').realHover();
+
+		ndv.getters.outputTableRow(3).should('have.text', '4444');
+		ndv.getters.outputTableRow(3).realHover();
+
 		ndv.getters
 			.inputTableRow(3)
 			.should('have.text', '4444')
@@ -167,7 +171,9 @@ describe('NDV', () => {
 		ndv.actions.changeOutputRunSelector('2 of 2 (6 items)');
 		cy.wait(50);
 
-		ndv.getters.inputTableRow(1).should('have.text', '1000').realHover();
+		ndv.getters.inputTableRow(1).should('have.text', '1000');
+		ndv.getters.inputTableRow(1).realHover();
+
 		ndv.getters
 			.outputTableRow(1)
 			.should('have.text', '1000')
@@ -177,7 +183,9 @@ describe('NDV', () => {
 			.invoke('attr', 'data-test-id')
 			.should('equal', 'hovering-item');
 
-		ndv.getters.outputTableRow(3).should('have.text', '2000').realHover();
+		ndv.getters.outputTableRow(3).should('have.text', '2000');
+		ndv.getters.outputTableRow(3).realHover();
+
 		ndv.getters
 			.inputTableRow(3)
 			.should('have.text', '2000')
@@ -254,11 +262,12 @@ describe('NDV', () => {
 			.inputTableRow(1)
 			.should('have.text', "This is an item, but it's empty.")
 			.realHover();
+
 		ndv.getters.outputHoveringItem().should('have.length', 6);
 		ndv.getters.parameterExpressionPreview('value').should('include.text', '1000');
 	});
 
-	it('can pair items between input and output across branches and runs', () => {
+	it.only ('can pair items between input and output across branches and runs', () => {
 		cy.fixture('Test_workflow_5.json').then((data) => {
 			cy.get('body').paste(JSON.stringify(data));
 		});
@@ -270,7 +279,9 @@ describe('NDV', () => {
 		ndv.actions.switchOutputMode('Table');
 
 		ndv.actions.switchOutputBranch('False Branch (2 items)');
-		ndv.getters.outputTableRow(1).should('have.text', '8888').realHover();
+		ndv.getters.outputTableRow(1).should('have.text', '8888');
+		ndv.getters.outputTableRow(1).realHover();
+
 		ndv.getters
 			.inputTableRow(5)
 			.should('have.text', '8888')
@@ -280,7 +291,8 @@ describe('NDV', () => {
 			.invoke('attr', 'data-test-id')
 			.should('equal', 'hovering-item');
 
-		ndv.getters.outputTableRow(2).should('have.text', '9999').realHover();
+		ndv.getters.outputTableRow(2).should('have.text', '9999');
+		ndv.getters.outputTableRow(2).realHover();
 
 		ndv.getters
 			.inputTableRow(6)
@@ -297,20 +309,27 @@ describe('NDV', () => {
 
 		ndv.actions.switchInputBranch('True Branch');
 		ndv.actions.changeOutputRunSelector('1 of 2 (2 items)');
-		ndv.getters.outputTableRow(1).should('have.text', '8888').realHover();
+		ndv.getters.outputTableRow(1).should('have.text', '8888')
+		ndv.getters.outputTableRow(1).realHover();
+		cy.wait(100);
 		ndv.getters.inputHoveringItem().should('not.exist');
 
-		ndv.getters.inputTableRow(1).should('have.text', '1111').realHover();
+		ndv.getters.inputTableRow(1).should('have.text', '1111')
+		ndv.getters.inputTableRow(1).realHover();
+		cy.wait(100);
 		ndv.getters.outputHoveringItem().should('not.exist');
 
 		ndv.actions.switchInputBranch('False Branch');
-		ndv.getters.inputTableRow(1).should('have.text', '8888').realHover();
+		ndv.getters.inputTableRow(1).should('have.text', '8888');
+		ndv.getters.inputTableRow(1).realHover();
 
 		ndv.actions.changeOutputRunSelector('2 of 2 (4 items)');
-		ndv.getters.outputTableRow(1).should('have.text', '1111').realHover();
+		ndv.getters.outputTableRow(1).should('have.text', '1111');
+		ndv.getters.outputTableRow(1).realHover();
 
 		ndv.actions.changeOutputRunSelector('1 of 2 (2 items)');
-		ndv.getters.inputTableRow(1).should('have.text', '8888').realHover();
+		ndv.getters.inputTableRow(1).should('have.text', '8888');
+		ndv.getters.inputTableRow(1).realHover();
 		ndv.getters.outputHoveringItem().should('have.text', '8888');
 		// todo there's a bug here need to fix ADO-534
 		// ndv.getters.outputHoveringItem().should('not.exist');
