6251a527006f55fd4bda1de64df9cd871cde7431
Alex Grozav <alex@grozav.com>
2023-08-02 14:17:14 +0300

    fix: Fix saving workflow sharing (no-changelog) (#6828)
    fix: fix saving workflow sharing

--- packages/editor-ui/src/components/WorkflowShareModal.ee.vue
+++ packages/editor-ui/src/components/WorkflowShareModal.ee.vue
@@ -479,7 +479,9 @@ export default defineComponent({
 	},
 	watch: {
 		workflow(workflow) {
-			this.sharedWith = workflow.sharedWith;
+			if (workflow.sharedWith) {
+				this.sharedWith = workflow.sharedWith;
+			}
 		},
 	},
 });
