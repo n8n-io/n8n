5844abee6f6fa41409732d58b5eecc0abb09e1a3
Iván Ovejero <ivov.src@gmail.com>
2023-08-04 11:52:45 +0200

    perf(core): Cache webhooks (#6825)
    * refactor: Initial setup
    
    * Refactor for clarity
    
    * Comments to clarify
    
    * More replacements
    
    * Simplify with `fullPath`
    
    * Fix tests
    
    * Implement remaining methods
    
    * chore: Fix misresolved conflicts
    
    * Simplify syntax
    
    * Reduce diff
    
    * Minor cleanup
    
    * Fix lint
    
    * Inject dependency
    
    * Improve typings
    
    * Remove unused method
    
    * Restore method
    
    * Add comment
    
    * Rename in test
    
    * Restore comments
    
    * Clean up dynamic webhook handling
    
    * Clean up tests
    
    * Remove redundant `cache` prefix
    
    * fix: Correct `uniquePath` for dynamic webhooks

--- packages/cli/test/unit/ActiveWorkflowRunner.test.ts
+++ packages/cli/test/unit/ActiveWorkflowRunner.test.ts
@@ -24,7 +24,7 @@ import { mockInstance } from '../integration/shared/utils/';
 import { Push } from '@/push';
 import { ActiveExecutions } from '@/ActiveExecutions';
 import { NodeTypes } from '@/NodeTypes';
-import type { WebhookRepository } from '@/databases/repositories';
+import { WebhookService } from '@/services/webhook.service';
 import { VariablesService } from '../../src/environments/variables/variables.service';
 
 /**
@@ -141,7 +141,7 @@ const workflowExecuteAdditionalDataExecuteErrorWorkflowSpy = jest.spyOn(
 describe('ActiveWorkflowRunner', () => {
 	let externalHooks: ExternalHooks;
 	let activeWorkflowRunner: ActiveWorkflowRunner;
-	let webhookRepository = mock<WebhookRepository>();
+	const webhookService = mockInstance(WebhookService);
 
 	beforeAll(async () => {
 		LoggerProxy.init(getLogger());
@@ -167,7 +167,7 @@ describe('ActiveWorkflowRunner', () => {
 			new ActiveExecutions(),
 			externalHooks,
 			Container.get(NodeTypes),
-			webhookRepository,
+			webhookService,
 		);
 	});
 
@@ -182,7 +182,7 @@ describe('ActiveWorkflowRunner', () => {
 		await activeWorkflowRunner.init();
 		expect(await activeWorkflowRunner.getActiveWorkflows()).toHaveLength(0);
 		expect(mocked(Db.collections.Workflow.find)).toHaveBeenCalled();
-		expect(mocked(webhookRepository.clear)).toHaveBeenCalled();
+		expect(webhookService.deleteInstanceWebhooks).toHaveBeenCalled();
 		expect(externalHooks.run).toHaveBeenCalledTimes(1);
 	});
 
@@ -193,7 +193,7 @@ describe('ActiveWorkflowRunner', () => {
 			databaseActiveWorkflowsCount,
 		);
 		expect(mocked(Db.collections.Workflow.find)).toHaveBeenCalled();
-		expect(mocked(webhookRepository.clear)).toHaveBeenCalled();
+		expect(webhookService.deleteInstanceWebhooks).toHaveBeenCalled();
 		expect(externalHooks.run).toHaveBeenCalled();
 	});
 
