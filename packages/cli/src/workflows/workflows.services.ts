1c3a7aff156271c375e29331401bbe60015900a8
Iván Ovejero <ivov.src@gmail.com>
2023-08-04 16:40:51 +0200

    Fix lint

--- packages/cli/src/workflows/workflows.services.ts
+++ packages/cli/src/workflows/workflows.services.ts
@@ -140,7 +140,7 @@ export class WorkflowsService {
 
 		if (options?.filter) {
 			try {
-				filter = await WorkflowRepository.toQueryFilter(options.filter);
+				filter = WorkflowRepository.toQueryFilter(options.filter);
 			} catch (error) {
 				WorkflowsService.handleQueryParamError('filter', options.filter, error);
 			}
