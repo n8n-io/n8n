5eef4663b0b8911caaf6a9b066950f58be45b90a
Csaba Tuncsik <csaba@n8n.io>
2023-08-07 13:21:13 +0200

    test: add tooltip design system component test

--- packages/editor-ui/src/components/ParameterInputFull.vue
+++ packages/editor-ui/src/components/ParameterInputFull.vue
@@ -169,7 +169,7 @@ export default defineComponent({
 					'data-test-id': 'dismiss-mapping-tooltip',
 				},
 				listeners: {
-					click: mappingTooltipDismissHandler,
+					onClick: mappingTooltipDismissHandler,
 				},
 			},
 		];
@@ -329,7 +329,7 @@ export default defineComponent({
 			}, 200);
 		},
 		onMappingTooltipDismissed() {
-			this.ndvStore.disableMappingHint();
+			this.ndvStore.disableMappingHint(false);
 		},
 	},
 	watch: {
