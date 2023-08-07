08982ede4c3dd2b38b1a0ce91adf8198f0af1776
Csaba Tuncsik <csaba@n8n.io>
2023-08-07 20:16:01 +0200

    fix(editor): Do not show mapping discoverability tooltip after dismiss (#6862)
    * fix(editor): Do not show mapping discoverability tooltip after dismiss
    
    * test: add tooltip design system component test
    
    * fix(editor): no need to dismiss mapping tooltip multiple times

--- cypress/e2e/14-mapping.cy.ts
+++ cypress/e2e/14-mapping.cy.ts
@@ -268,7 +268,6 @@ describe('Data mapping', () => {
 
 		ndv.actions.typeIntoParameterInput('value', 'fun');
 		ndv.actions.clearParameterInput('value'); // keep focus on param
-		ndv.actions.dismissMappingTooltip();
 		cy.wait(300);
 
 		ndv.getters.inputDataContainer().should('exist').find('span').contains('count').realMouseDown();
