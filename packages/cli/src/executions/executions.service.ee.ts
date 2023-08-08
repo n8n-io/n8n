fd47f528524351385a564a8d9b40b2748d5b88a6
Iván Ovejero <ivov.src@gmail.com>
2023-08-08 11:45:46 +0200

    fix(core): Add sharing data to workflows in EE executions

--- packages/cli/src/executions/executions.service.ee.ts
+++ packages/cli/src/executions/executions.service.ee.ts
@@ -1,6 +1,10 @@
 import type { User } from '@db/entities/User';
 import { getSharedWorkflowIds } from '@/WorkflowHelpers';
 import { ExecutionsService } from './executions.service';
+import type { ExecutionRequest } from '@/requests';
+import type { IExecutionResponse, IExecutionFlattedResponse } from '@/Interfaces';
+import { WorkflowEntity } from '@/databases/entities/WorkflowEntity';
+import { EEWorkflowsService as EEWorkflows } from '../workflows/workflows.services.ee';
 
 export class EEExecutionsService extends ExecutionsService {
 	/**
@@ -9,5 +13,22 @@ export class EEExecutionsService extends ExecutionsService {
 	static async getWorkflowIdsForUser(user: User): Promise<string[]> {
 		// Get all workflows
 		return getSharedWorkflowIds(user);
+	}
+
+	static async getExecution(
+		req: ExecutionRequest.Get,
+	): Promise<IExecutionResponse | IExecutionFlattedResponse | undefined> {
+		const execution = await super.getExecution(req);
+
+		if (!execution) return;
+
+		const workflow = Object.assign(new WorkflowEntity(), execution.workflowData);
+
+		EEWorkflows.addOwnerAndSharings(workflow);
+		await EEWorkflows.addCredentialsToWorkflow(workflow, req.user);
+
+		execution.workflowData = workflow;
+
+		return execution;
 	}
 }
