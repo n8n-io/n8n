2459b0ad48bf13c7e9b4053c7c258d98b0f25e33
Jon <jonathan.bennetts@gmail.com>
2023-08-09 12:30:53 +0100

    fix: Fix issue with key based credentials not being read correctly (#6824)

--- packages/nodes-base/utils/utilities.ts
+++ packages/nodes-base/utils/utilities.ts
@@ -218,6 +218,36 @@ export const keysToLowercase = <T>(headers: T) => {
 };
 
 /**
+ * Formats a private key by removing unnecessary whitespace and adding line breaks.
+ * @param privateKey - The private key to format.
+ * @returns The formatted private key.
+ */
+export function formatPrivateKey(privateKey: string): string {
+	if (/\n/.test(privateKey)) {
+		return privateKey;
+	}
+	let formattedPrivateKey = '';
+	const parts = privateKey.split('-----').filter((item) => item !== '');
+	parts.forEach((part) => {
+		const regex = /(PRIVATE KEY|CERTIFICATE)/;
+		if (regex.test(part)) {
+			formattedPrivateKey += `-----${part}-----`;
+		} else {
+			const passRegex = /Proc-Type|DEK-Info/;
+			if (passRegex.test(part)) {
+				part = part.replace(/:\s+/g, ':');
+				formattedPrivateKey += part.replace(/\\n/g, '\n');
+				formattedPrivateKey += part.replace(/\s+/g, '\n');
+			} else {
+				formattedPrivateKey += part.replace(/\\n/g, '\n');
+				formattedPrivateKey += part.replace(/\s+/g, '\n');
+			}
+		}
+	});
+	return formattedPrivateKey;
+}
+
+/**
  * @TECH_DEBT Explore replacing with handlebars
  */
 export function getResolvables(expression: string) {
