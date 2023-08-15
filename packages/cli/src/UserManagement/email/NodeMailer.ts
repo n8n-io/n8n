6e20e24af7de764da40d6e3c1e5f297f318a8d3a
Xavier Calland <xavier.calland@gmail.com>
2023-08-15 10:55:58 +0200

    feat: Add support for not requiring SMTP auth with user management (#3742)

--- packages/cli/src/UserManagement/email/NodeMailer.ts
+++ packages/cli/src/UserManagement/email/NodeMailer.ts
@@ -4,20 +4,29 @@ import { createTransport } from 'nodemailer';
 import { ErrorReporterProxy as ErrorReporter, LoggerProxy as Logger } from 'n8n-workflow';
 import config from '@/config';
 import type { MailData, SendEmailResult } from './Interfaces';
+import type SMTPConnection from 'nodemailer/lib/smtp-connection';
 
 export class NodeMailer {
 	private transport?: Transporter;
 
 	async init(): Promise<void> {
-		this.transport = createTransport({
+		const transportConfig: SMTPConnection.Options = {
 			host: config.getEnv('userManagement.emails.smtp.host'),
 			port: config.getEnv('userManagement.emails.smtp.port'),
 			secure: config.getEnv('userManagement.emails.smtp.secure'),
-			auth: {
+		};
+
+		if (
+			config.getEnv('userManagement.emails.smtp.auth.user') &&
+			config.getEnv('userManagement.emails.smtp.auth.pass')
+		) {
+			transportConfig.auth = {
 				user: config.getEnv('userManagement.emails.smtp.auth.user'),
 				pass: config.getEnv('userManagement.emails.smtp.auth.pass'),
-			},
-		});
+			};
+		}
+
+		this.transport = createTransport(transportConfig);
 	}
 
 	async verifyConnection(): Promise<void> {
