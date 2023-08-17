fcbf2c827c350042108cbe4bdd018d93fd32438d
Jan Oberhauser <jan.oberhauser@gmail.com>
2023-08-17 21:06:52 +0200

    :zap: Add Xata Memory support

---
+++ packages/@n8n/nodes-langchain/nodes/LangChainMemoryXata/LangChainMemoryXata.node.ts
@@ -0,0 +1,80 @@
+/* eslint-disable n8n-nodes-base/node-dirname-against-convention */
+import { NodeOperationError } from 'n8n-workflow';
+import type { IExecuteFunctions, INodeType, INodeTypeDescription, SupplyData } from 'n8n-workflow';
+import { BufferMemory } from 'langchain/memory';
+import { XataChatMessageHistory } from 'langchain/stores/message/xata';
+import { BaseClient } from '@xata.io/client';
+import { logWrapper } from '../../utils/logWrapper';
+
+export class LangChainMemoryXata implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'LangChain - Xata',
+		name: 'LangChainMemoryXata',
+		icon: 'file:xata.png',
+		group: ['transform'],
+		version: 1,
+		description: 'Xata Memory',
+		defaults: {
+			name: 'LangChain - Xata',
+			color: '#303030',
+		},
+		// eslint-disable-next-line n8n-nodes-base/node-class-description-inputs-wrong-regular-node
+		inputs: [],
+		// eslint-disable-next-line n8n-nodes-base/node-class-description-outputs-wrong
+		outputs: ['memory'],
+		outputNames: ['Memory'],
+		credentials: [
+			{
+				name: 'xataApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Session ID',
+				name: 'sessionId',
+				type: 'string',
+				required: true,
+				default: '',
+			},
+		],
+	};
+
+	async supplyData(this: IExecuteFunctions): Promise<SupplyData> {
+		const credentials = await this.getCredentials('xataApi');
+
+		const itemIndex = 0;
+
+		const xataClient = new BaseClient({
+			apiKey: credentials.apiKey as string,
+			branch: (credentials.branch as string) || 'main',
+			databaseURL: credentials.databaseEndpoint as string,
+		});
+
+		// TODO: Should it get executed once per item or not?
+		const sessionId = this.getNodeParameter('sessionId', itemIndex) as string;
+
+		const table = (credentials.databaseEndpoint as string).match(
+			/https:\/\/[^.]+\.[^.]+\.xata\.sh\/db\/([^\/:]+)/,
+		);
+
+		if (table === null) {
+			throw new NodeOperationError(
+				this.getNode(),
+				'It was not possible to extract the table from the Database Endpoint.',
+			);
+		}
+
+		const memory = new BufferMemory({
+			chatHistory: new XataChatMessageHistory({
+				table: table[1],
+				sessionId,
+				client: xataClient,
+				apiKey: credentials.apiKey as string,
+			}),
+		});
+		return {
+			response: logWrapper(memory, this),
+		};
+	}
+}
--- packages/@n8n/nodes-langchain/utils/logWrapper.ts
+++ packages/@n8n/nodes-langchain/utils/logWrapper.ts
@@ -1,15 +1,18 @@
 import { IExecuteFunctions } from 'n8n-workflow';
 
 import { Tool } from 'langchain/tools';
-import { BaseMessage, ChatResult } from 'langchain/dist/schema';
+import { BaseMessage, ChatResult, InputValues } from 'langchain/dist/schema';
 import { BaseChatModel } from 'langchain/dist/chat_models/base';
 import { CallbackManagerForLLMRun } from 'langchain/dist/callbacks';
+import { BaseChatMemory } from 'langchain/memory';
+import { MemoryVariables, OutputValues } from 'langchain/dist/memory/base';
 
 export function logWrapper(
-	originalInstance: Tool | BaseChatModel,
+	originalInstance: Tool | BaseChatMemory | BaseChatModel,
 	executeFunctions: IExecuteFunctions,
 ) {
 	return new Proxy(originalInstance, {
+		// For Tool
 		get: (target, prop) => {
 			if (prop === '_call') {
 				return async (query: string): Promise<string> => {
@@ -19,6 +22,32 @@ export function logWrapper(
 					executeFunctions.addOutputData('tool', [[{ json: { response } }]]);
 					return response;
 				};
+
+				// For BaseChatMemory
+			} else if (prop === 'loadMemoryVariables') {
+				return async (values: InputValues): Promise<MemoryVariables> => {
+					console.log('loadMemoryVariables....1');
+					executeFunctions.addInputData('memory', [
+						[{ json: { action: 'loadMemoryVariables', values } }],
+					]);
+					// @ts-ignore
+					const response = await target[prop](values);
+					executeFunctions.addOutputData('memory', [
+						[{ json: { action: 'loadMemoryVariables', response } }],
+					]);
+					return response;
+				};
+			} else if (prop === 'saveContext') {
+				return async (inputValues: InputValues, outputValues: OutputValues): Promise<void> => {
+					executeFunctions.addInputData('memory', [
+						[{ json: { action: 'saveContext', inputValues, outputValues } }],
+					]);
+					// @ts-ignore
+					await target[prop](inputValues, outputValues);
+					executeFunctions.addOutputData('memory', [[{ json: { action: 'saveContext' } }]]);
+				};
+
+				// For BaseChatModel
 			} else if (prop === '_generate') {
 				return async (
 					messages: BaseMessage[],
