d1218d7c8ff57e649ba6eae25283ccb023217130
Oleg Ivaniv <me@olegivaniv.com>
2023-08-23 23:06:17 +0200

    Comment out execute of pinecone vs
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/@n8n/nodes-langchain/nodes/vector_store/VectorStorePinecone/VectorStorePinecone.node.ts
+++ packages/@n8n/nodes-langchain/nodes/vector_store/VectorStorePinecone/VectorStorePinecone.node.ts
@@ -1,4 +1,4 @@
-import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription, SupplyData } from 'n8n-workflow';
+import { IExecuteFunctions, INodeType, INodeTypeDescription, SupplyData } from 'n8n-workflow';
 import { Document } from 'langchain/document';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { PineconeClient } from '@pinecone-database/pinecone'
@@ -144,56 +144,56 @@ export class VectorStorePinecone implements INodeType {
 		}
 	}
 
-	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		// @ts-ignore
-		let response: VectorStoreRetriever | PineconeStore;
-		let vectorStore: PineconeStore;
+	// async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+	// 	// @ts-ignore
+	// 	let response: VectorStoreRetriever | PineconeStore;
+	// 	let vectorStore: PineconeStore;
 
-		const namespace = this.getNodeParameter('pineconeNamespace', 0) as string;
-		const index = this.getNodeParameter('pineconeIndex', 0) as string;
-		const mode = this.getNodeParameter('mode', 0) as 'load' | 'insert';
-		const topK = this.getNodeParameter('topK', 0) as number;
-		const outputType = this.getNodeParameter('outputType', 0) as 'retriever' | 'vectorStore';
+	// 	const namespace = this.getNodeParameter('pineconeNamespace', 0) as string;
+	// 	const index = this.getNodeParameter('pineconeIndex', 0) as string;
+	// 	const mode = this.getNodeParameter('mode', 0) as 'load' | 'insert';
+	// 	const topK = this.getNodeParameter('topK', 0) as number;
+	// 	const outputType = this.getNodeParameter('outputType', 0) as 'retriever' | 'vectorStore';
 
-		const documentsNodes = await this.getInputConnectionData('document', 0) || [];
-		const documents = documentsNodes.flatMap((node) => node.response as Document);
-		const embeddingNodes = await this.getInputConnectionData('embedding', 0);
-		const embeddings = (embeddingNodes || [])[0]?.response as Embeddings;
-		const credentials = await this.getCredentials('pineconeApi');
+	// 	const documentsNodes = await this.getInputConnectionData('document', 0) || [];
+	// 	const documents = documentsNodes.flatMap((node) => node.response as Document);
+	// 	const embeddingNodes = await this.getInputConnectionData('embedding', 0);
+	// 	const embeddings = (embeddingNodes || [])[0]?.response as Embeddings;
+	// 	const credentials = await this.getCredentials('pineconeApi');
 
-		const client = new PineconeClient()
-		await client.init({
-				apiKey: credentials.apiKey as string,
-				environment: credentials.environment as string,
-		})
+	// 	const client = new PineconeClient()
+	// 	await client.init({
+	// 			apiKey: credentials.apiKey as string,
+	// 			environment: credentials.environment as string,
+	// 	})
 
-		const pineconeIndex = client.Index(index);
-		console.log('Initiated Pinecone Client', pineconeIndex)
-		if (mode === 'insert') {
-			const flattenedDocs = flatten(documents)
-			console.log("🚀 ~ file: VectorStorePinecone.node.ts:172 ~ VectorStorePinecone ~ execute ~ flattenedDocs:", flattenedDocs)
+	// 	const pineconeIndex = client.Index(index);
+	// 	console.log('Initiated Pinecone Client', pineconeIndex)
+	// 	if (mode === 'insert') {
+	// 		const flattenedDocs = flatten(documents)
+	// 		console.log("🚀 ~ file: VectorStorePinecone.node.ts:172 ~ VectorStorePinecone ~ execute ~ flattenedDocs:", flattenedDocs)
 
-			vectorStore = await PineconeStore.fromDocuments(documents, embeddings, {
-				namespace: namespace || undefined,
-				pineconeIndex: pineconeIndex
-			})
-		} else {
-			console.log('Before fromExistingIndex', 'namespace', namespace, 'embeddings', embeddings)
-			vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-				namespace: namespace || undefined,
-				pineconeIndex: pineconeIndex,
-			})
-			// console.log('After constructing vectorStore', vectorStore)
-		}
+	// 		vectorStore = await PineconeStore.fromDocuments(documents, embeddings, {
+	// 			namespace: namespace || undefined,
+	// 			pineconeIndex: pineconeIndex
+	// 		})
+	// 	} else {
+	// 		console.log('Before fromExistingIndex', 'namespace', namespace, 'embeddings', embeddings)
+	// 		vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+	// 			namespace: namespace || undefined,
+	// 			pineconeIndex: pineconeIndex,
+	// 		})
+	// 		// console.log('After constructing vectorStore', vectorStore)
+	// 	}
 
-		if (outputType === 'retriever') {
-			response = vectorStore.asRetriever(topK);
-		} else {
-			response = vectorStore;
-		}
+	// 	if (outputType === 'retriever') {
+	// 		response = vectorStore.asRetriever(topK);
+	// 	} else {
+	// 		response = vectorStore;
+	// 	}
 
-		const items = this.getInputData(0);
-		// Only pass it through?
-		return this.prepareOutputData(items);
-	}
+	// 	const items = this.getInputData(0);
+	// 	// Only pass it through?
+	// 	return this.prepareOutputData(items);
+	// }
 }
