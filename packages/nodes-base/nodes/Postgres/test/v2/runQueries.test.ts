176ccd62bc1d6f28958c0fc894ee647f1e3a5f6e
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2023-08-25 18:38:09 +0300

    fix(Postgres Node): Empty return data fix for Postgres and MySQL (#7016)

--- packages/nodes-base/nodes/Postgres/test/v2/runQueries.test.ts
+++ packages/nodes-base/nodes/Postgres/test/v2/runQueries.test.ts
@@ -44,7 +44,9 @@ describe('Test PostgresV2, runQueries', () => {
 		const thisArg = mock<IExecuteFunctions>();
 		const runQueries = configureQueryRunner.call(thisArg, node, false, pgp, db);
 
-		const result = await runQueries([{ query: 'SELECT * FROM table', values: [] }], [], {});
+		const result = await runQueries([{ query: 'SELECT * FROM table', values: [] }], [], {
+			nodeVersion: 2.2,
+		});
 
 		expect(result).toBeDefined();
 		expect(result).toHaveLength(1);
