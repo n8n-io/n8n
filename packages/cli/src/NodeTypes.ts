82a04bc61c2cb7ece8ee6b229f05a0711757d5b9
Iván Ovejero <ivov.src@gmail.com>
2023-08-29 10:46:24 +0200

    refactor: Move node i18n logic to `NodeTypes` (no-changelog) (#7035)

--- packages/cli/src/NodeTypes.ts
+++ packages/cli/src/NodeTypes.ts
@@ -10,6 +10,9 @@ import { NodeHelpers } from 'n8n-workflow';
 import { Service } from 'typedi';
 import { RESPONSE_ERROR_MESSAGES } from './constants';
 import { LoadNodesAndCredentials } from './LoadNodesAndCredentials';
+import { join, dirname } from 'path';
+import { readdir } from 'fs/promises';
+import type { Dirent } from 'fs';
 
 @Service()
 export class NodeTypes implements INodeTypes {
@@ -77,5 +80,47 @@ export class NodeTypes implements INodeTypes {
 
 	private get knownNodes() {
 		return this.nodesAndCredentials.known.nodes;
+	}
+
+	async getNodeTranslationPath({
+		nodeSourcePath,
+		longNodeType,
+		locale,
+	}: {
+		nodeSourcePath: string;
+		longNodeType: string;
+		locale: string;
+	}) {
+		const nodeDir = dirname(nodeSourcePath);
+		const maxVersion = await this.getMaxVersion(nodeDir);
+		const nodeType = longNodeType.replace('n8n-nodes-base.', '');
+
+		return maxVersion
+			? join(nodeDir, `v${maxVersion}`, 'translations', locale, `${nodeType}.json`)
+			: join(nodeDir, 'translations', locale, `${nodeType}.json`);
+	}
+
+	private async getMaxVersion(dir: string) {
+		const entries = await readdir(dir, { withFileTypes: true });
+
+		const dirnames = entries.reduce<string[]>((acc, cur) => {
+			if (this.isVersionedDirname(cur)) acc.push(cur.name);
+			return acc;
+		}, []);
+
+		if (!dirnames.length) return null;
+
+		return Math.max(...dirnames.map((d) => parseInt(d.charAt(1), 10)));
+	}
+
+	private isVersionedDirname(dirent: Dirent) {
+		if (!dirent.isDirectory()) return false;
+
+		const ALLOWED_VERSIONED_DIRNAME_LENGTH = [2, 3]; // e.g. v1, v10
+
+		return (
+			ALLOWED_VERSIONED_DIRNAME_LENGTH.includes(dirent.name.length) &&
+			dirent.name.toLowerCase().startsWith('v')
+		);
 	}
 }
