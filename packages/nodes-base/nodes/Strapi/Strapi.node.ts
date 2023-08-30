1840c790be15393692c7f42c5f681990200eabf0
Elias Meire <elias@meire.dev>
2023-08-30 14:54:14 +0200

    Add token credentials for Strapi Node

--- packages/nodes-base/nodes/Strapi/Strapi.node.ts
+++ packages/nodes-base/nodes/Strapi/Strapi.node.ts
@@ -9,6 +9,7 @@ import type {
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
+	CredentialInformation,
 } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 
@@ -41,9 +42,39 @@ export class Strapi implements INodeType {
 				name: 'strapiApi',
 				required: true,
 				testedBy: 'strapiApiTest',
+				displayOptions: {
+					show: {
+						authentication: ['password'],
+					},
+				},
+			},
+			{
+				name: 'strapiTokenApi',
+				required: true,
+				displayOptions: {
+					show: {
+						authentication: ['token'],
+					},
+				},
 			},
 		],
 		properties: [
+			{
+				displayName: 'Authentication',
+				name: 'authentication',
+				type: 'options',
+				options: [
+					{
+						name: 'Username & Password',
+						value: 'password',
+					},
+					{
+						name: 'API Token',
+						value: 'token',
+					},
+				],
+				default: 'password',
+			},
 			{
 				displayName: 'Resource',
 				name: 'resource',
@@ -112,10 +143,18 @@ export class Strapi implements INodeType {
 		const resource = this.getNodeParameter('resource', 0);
 		const operation = this.getNodeParameter('operation', 0);
 
-		const { apiVersion } = await this.getCredentials('strapiApi');
-		const { jwt } = await getToken.call(this);
+		const authenticationMethod = this.getNodeParameter('authentication', 0);
 
-		headers.Authorization = `Bearer ${jwt}`;
+		let apiVersion: CredentialInformation;
+
+		if (authenticationMethod === 'password') {
+			const { jwt } = await getToken.call(this);
+			apiVersion = (await this.getCredentials('strapiApi')).apiVersion;
+			headers.Authorization = `Bearer ${jwt}`;
+		} else {
+			apiVersion = (await this.getCredentials('strapiTokenApi')).apiVersion;
+		}
+
 		for (let i = 0; i < length; i++) {
 			try {
 				if (resource === 'entry') {
