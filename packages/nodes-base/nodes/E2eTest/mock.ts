f5a1aa4c23457046cbc0b6939b3e15a3b2a2780d
Mutasem <mutdmour@gmail.com>
2023-08-31 15:26:40 +0200

    refactor: use mock generator

--- packages/nodes-base/nodes/E2eTest/mock.ts
+++ packages/nodes-base/nodes/E2eTest/mock.ts
@@ -1,3 +1,4 @@
+import { array, name, uuid } from 'minifaker';
 import type {
 	INodeExecutionData,
 	INodeListSearchResult,
@@ -52,155 +53,11 @@ export const resourceMapperFields: ResourceMapperFields = {
 	],
 };
 
-export const searchOptions: INodeListSearchResult['results'] = [
-	{
-		name: 'Ester Spier',
-		value: 'a4071e98-7d40-41fb-8911-ce3e7bf94fb2',
-		url: 'https://example.com/user/a4071e98-7d40-41fb-8911-ce3e7bf94fb2',
-	},
-	{
-		name: 'Gram De Witt',
-		value: '9050951d-f2ca-40d4-a22f-57ce5e6dc047',
-		url: 'https://example.com/user/9050951d-f2ca-40d4-a22f-57ce5e6dc047',
-	},
-	{
-		name: 'Valerye Gailor',
-		value: '7285135b-d2fe-4e63-826f-35d9007052b9',
-		url: 'https://example.com/user/7285135b-d2fe-4e63-826f-35d9007052b9',
-	},
-	{
-		name: 'Benedicto Ryott',
-		value: '780edf09-4ed0-4bbd-b628-3afcd960a2aa',
-		url: 'https://example.com/user/780edf09-4ed0-4bbd-b628-3afcd960a2aa',
-	},
-	{
-		name: 'Kinsley Popple',
-		value: '41ef124a-d107-471b-8156-4cbbfa562c4f',
-		url: 'https://example.com/user/41ef124a-d107-471b-8156-4cbbfa562c4f',
-	},
-	{
-		name: 'Alla Wile',
-		value: 'ba72c104-f0a4-460a-88e3-ecf8a6565c9f',
-		url: 'https://example.com/user/ba72c104-f0a4-460a-88e3-ecf8a6565c9f',
-	},
-	{
-		name: 'Barnabe Kentish',
-		value: '60e51650-78ba-4130-9c8f-807c5650fd71',
-		url: 'https://example.com/user/60e51650-78ba-4130-9c8f-807c5650fd71',
-	},
-	{
-		name: 'Friedrick Westlake',
-		value: '4f13f640-a9b3-4f33-84a8-5417f8df0fb3',
-		url: 'https://example.com/user/4f13f640-a9b3-4f33-84a8-5417f8df0fb3',
-	},
-	{
-		name: 'Niels Jarvis',
-		value: '87bec55a-751a-4f2d-8168-be34012415b9',
-		url: 'https://example.com/user/87bec55a-751a-4f2d-8168-be34012415b9',
-	},
-	{
-		name: 'Berkly Christopher',
-		value: 'b96c2a37-841e-41c8-bb9e-5623d4abf556',
-		url: 'https://example.com/user/b96c2a37-841e-41c8-bb9e-5623d4abf556',
-	},
-	{
-		name: 'Harrietta Matoshin',
-		value: '66feb8b7-9252-4417-87fc-e99ad7124636',
-		url: 'https://example.com/user/66feb8b7-9252-4417-87fc-e99ad7124636',
-	},
-	{
-		name: 'Hatty Sturm',
-		value: '265ffc4b-10d1-43fe-ba93-260f9fe4620c',
-		url: 'https://example.com/user/265ffc4b-10d1-43fe-ba93-260f9fe4620c',
-	},
-	{
-		name: 'Denny Squibe',
-		value: '67164f38-a377-4c82-94c6-a9d3fc3aac76',
-		url: 'https://example.com/user/67164f38-a377-4c82-94c6-a9d3fc3aac76',
-	},
-	{
-		name: 'Carolan Dreus',
-		value: '7985f97b-d2a6-4838-87b9-c4379be9f946',
-		url: 'https://example.com/user/7985f97b-d2a6-4838-87b9-c4379be9f946',
-	},
-	{
-		name: 'Isa Prophet',
-		value: 'fd9153ec-4d5c-409e-9ae9-e2641ee934f5',
-		url: 'https://example.com/user/fd9153ec-4d5c-409e-9ae9-e2641ee934f5',
-	},
-	{
-		name: 'Otes Arnaldi',
-		value: 'e7af33b2-6728-474a-abae-8fc193a89aa2',
-		url: 'https://example.com/user/e7af33b2-6728-474a-abae-8fc193a89aa2',
-	},
-	{
-		name: 'Florian Dearden',
-		value: '1796f976-e89f-4ffe-a6e5-9aada761f21a',
-		url: 'https://example.com/user/1796f976-e89f-4ffe-a6e5-9aada761f21a',
-	},
-	{
-		name: 'Adey Barthelet',
-		value: '4099dc21-2ebf-4f90-9174-aafce4ba5e23',
-		url: 'https://example.com/user/4099dc21-2ebf-4f90-9174-aafce4ba5e23',
-	},
-	{
-		name: 'Elston Shillito',
-		value: 'd76f15e4-4f16-4c02-93a2-95fb957494c8',
-		url: 'https://example.com/user/d76f15e4-4f16-4c02-93a2-95fb957494c8',
-	},
-	{
-		name: 'Westleigh Blamire',
-		value: '70e4e1af-ccb2-4d93-8f34-d200e2c2519e',
-		url: 'https://example.com/user/70e4e1af-ccb2-4d93-8f34-d200e2c2519e',
-	},
-	{
-		name: 'Humfrid Cockburn',
-		value: 'a3cbbd2d-20c0-4eb7-acc9-d1a98b58939e',
-		url: 'https://example.com/user/a3cbbd2d-20c0-4eb7-acc9-d1a98b58939e',
-	},
-	{
-		name: 'Fifi Ricardin',
-		value: '15ca77a5-9f54-4fa1-a622-0c3e969e5535',
-		url: 'https://example.com/user/15ca77a5-9f54-4fa1-a622-0c3e969e5535',
-	},
-	{
-		name: 'Nanine Nicholls',
-		value: 'a33e39f0-d91d-4a97-b371-2089e638382e',
-		url: 'https://example.com/user/a33e39f0-d91d-4a97-b371-2089e638382e',
-	},
-	{
-		name: 'Vi Sanger',
-		value: 'b803a32d-2b70-4705-aca6-9caf3fc8436c',
-		url: 'https://example.com/user/b803a32d-2b70-4705-aca6-9caf3fc8436c',
-	},
-	{
-		name: 'Haywood Doughartie',
-		value: '80394c64-7a7e-4e52-9b0a-f19c34a97f9f',
-		url: 'https://example.com/user/80394c64-7a7e-4e52-9b0a-f19c34a97f9f',
-	},
-	{
-		name: 'Stavros Polhill',
-		value: 'e3fc36d3-9cf3-466a-bc32-50ea37e26e9c',
-		url: 'https://example.com/user/e3fc36d3-9cf3-466a-bc32-50ea37e26e9c',
-	},
-	{
-		name: 'Aube Volet',
-		value: '2a4ab982-e728-43b5-a491-0a09c503aec2',
-		url: 'https://example.com/user/2a4ab982-e728-43b5-a491-0a09c503aec2',
-	},
-	{
-		name: 'Burton Segge',
-		value: '53290379-35df-4dc2-885e-5529faec737c',
-		url: 'https://example.com/user/53290379-35df-4dc2-885e-5529faec737c',
-	},
-	{
-		name: 'Kennedy Chiddy',
-		value: '57e23d3c-8ffc-456b-84c1-7ed1536911b6',
-		url: 'https://example.com/user/57e23d3c-8ffc-456b-84c1-7ed1536911b6',
-	},
-	{
-		name: 'Odessa Praundl',
-		value: '8d602f94-50f6-40c8-ac94-07304b1a7b72',
-		url: 'https://example.com/user/8d602f94-50f6-40c8-ac94-07304b1a7b72',
-	},
-];
+export const searchOptions: INodeListSearchResult['results'] = array(100, () => {
+	const value = uuid.v4();
+	return {
+		name: name(),
+		value,
+		url: 'https://example.com/user/' + value,
+	};
+});
