8c10cbd5626245f530320f4551f6132adeef11a5
Oleg Ivaniv <me@olegivaniv.com>
2023-08-31 20:25:10 +0200

    WIP: Basic version of structured output
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/@n8n/nodes-langchain/nodes/chains/ChainStructuredOutput/ChainStructuredOutput.node.ts
+++ packages/@n8n/nodes-langchain/nodes/chains/ChainStructuredOutput/ChainStructuredOutput.node.ts
@@ -3,17 +3,13 @@ import {
 	type INodeExecutionData,
 	type INodeType,
 	type INodeTypeDescription,
+	NodeOperationError,
 } from 'n8n-workflow';
 
-// import { loadSummarizationChain } from 'langchain/chains';
-// import type { BaseLanguageModel } from 'langchain/dist/base_language';
 import { getAndValidateSupplyInput } from '../../../utils/getAndValidateSupplyInput';
-import { N8nJsonLoader } from '../../document_loaders/DocumentJSONInputLoader/DocumentJSONInputLoader.node';
-import { Document } from 'langchain/document';
-import { N8nBinaryLoader } from '../../document_loaders/DocumentBinaryInputLoader/DocumentBinaryInputLoader.node';
-import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from 'langchain/dist/prompts';
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from 'langchain/prompts';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
-import { JsonOutputFunctionsParser } from 'langchain/dist/output_parsers/openai_functions';
+import { JsonOutputFunctionsParser } from 'langchain/output_parsers';
 
 function getPromptTemplate(prompt: string) {
 	return new ChatPromptTemplate({
@@ -34,7 +30,7 @@ export class ChainStructuredOutput implements INodeType {
 		version: 1,
 		description: 'Chain to run to output structured data following a schema',
 		defaults: {
-			name: 'Summarization Chain',
+			name: 'Structured Output Chain',
 			color: '#432032',
 		},
 		codex: {
@@ -44,8 +40,8 @@ export class ChainStructuredOutput implements INodeType {
 			},
 		},
 		// eslint-disable-next-line n8n-nodes-base/node-class-description-inputs-wrong-regular-node
-		inputs: ['main', 'document', 'languageModel'],
-		inputNames: ['', 'Document', 'Language Model'],
+		inputs: ['main', 'languageModel'],
+		inputNames: ['', 'Language Model'],
 		outputs: ['main', 'chain'],
 		outputNames: ['', 'Chain'],
 		credentials: [],
@@ -58,29 +54,20 @@ export class ChainStructuredOutput implements INodeType {
 				default: '',
 			},
 			{
-				displayName: 'Mode',
-				name: 'mode',
-				type: 'options',
-				noDataExpression: true,
-				options: [
-					{
-						name: 'Run Once for All Items',
-						value: 'runOnceForAllItems',
-						description: 'Run this chain only once, no matter how many input items there are',
-					},
-					{
-						name: 'Run Once for Each Item',
-						value: 'runOnceForEachItem',
-						description: 'Run this chain as many times as there are input items',
-					},
-				],
-				default: 'runOnceForAllItems',
-			},
-			{
 				displayName: 'Prompt',
 				name: 'prompt',
 				type: 'string',
 				default: '',
+				required: true,
+			},
+			{
+				displayName: 'Input Text',
+				name: 'inputText',
+				type: 'string',
+				default: '',
+				typeOptions: {
+					rows: 4,
+				},
 				required: true,
 			},
 			{
@@ -95,11 +82,7 @@ export class ChainStructuredOutput implements INodeType {
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		this.logger.verbose('Executing Vector Store QA Chain');
-		const runMode = this.getNodeParameter('mode', 0) as string;
-		const prompt = this.getNodeParameter('prompt', 0) as string;
-		const schema = this.getNodeParameter('prompt', 0) as string;
-
+		this.logger.verbose('Executing Structured Output Chain');
 		const model = (await getAndValidateSupplyInput(
 			this,
 			'languageModel',
@@ -107,45 +90,40 @@ export class ChainStructuredOutput implements INodeType {
 		)) as ChatOpenAI;
 
 
-		// Binding "function_call" below makes the model always call the specified function.
-		// If you want to allow the model to call functions selectively, omit it.
-		const functionCallingModel = model.bind({
-			functions: [
-				{
-					name: "output_formatter",
-					description: "Should always be used to properly format output",
-					// @ts-ignore
-					parameters: schema,
-				},
-			],
-			function_call: { name: "output_formatter" },
-		});
 		const outputParser = new JsonOutputFunctionsParser();
 
 
 		const items = this.getInputData();
-		if (runMode === 'runOnceForAllItems') {
-			const chain = prompt.pipe(functionCallingModel).pipe(outputParser);
+		const returnData: INodeExecutionData[] = [];
 
-			const response = await chain.call({
-				input_documents: processedDocuments,
+		for (let i = 0; i < items.length; i++) {
+			const prompt = this.getNodeParameter('prompt', i) as string;
+			const inputText = this.getNodeParameter('inputText', i) as string;
+			const schema = this.getNodeParameter('jsonSchema', i) as string;
+
+
+			try {
+				JSON.parse(schema);
+			} catch (error) {
+				throw new NodeOperationError(this.getNode(), 'Error whine parsing JSON Schema.');
+			}
+			const functionCallingModel = model.bind({
+				functions: [
+					{
+						name: "output_formatter",
+						description: "Should always be used to properly format output",
+						parameters: JSON.parse(schema),
+					},
+				],
+				function_call: { name: "output_formatter" },
 			});
 
-			return this.prepareOutputData([{ json: { response } }]);
-		}
+			const chain = getPromptTemplate(prompt)
+				.pipe(functionCallingModel)
+				.pipe(outputParser);
 
-		// Run for each item
-		const returnData: INodeExecutionData[] = [];
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-			let processedDocuments: Document[];
-			if (documentInput instanceof N8nJsonLoader || documentInput instanceof N8nBinaryLoader) {
-				processedDocuments = await documentInput.process([items[itemIndex]]);
-			} else {
-				processedDocuments = documentInput;
-			}
-
-			const response = await chain.call({
-				input_documents: processedDocuments,
+			const response = await chain.invoke({
+				inputText,
 			});
 
 			returnData.push({ json: { response } });
