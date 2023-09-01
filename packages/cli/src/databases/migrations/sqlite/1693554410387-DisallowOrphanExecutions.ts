5db211195561e0a47b27714ca7627df0cb806a6b
Iván Ovejero <ivov.src@gmail.com>
2023-09-01 16:57:10 +0200

    Add indices

--- packages/cli/src/databases/migrations/sqlite/1693554410387-DisallowOrphanExecutions.ts
+++ packages/cli/src/databases/migrations/sqlite/1693554410387-DisallowOrphanExecutions.ts
@@ -7,7 +7,11 @@ export class DisallowOrphanExecutions1693554410387 implements ReversibleMigratio
 	 * Ensure all executions point to a workflow. Recreate table because sqlite
 	 * does not support modifying column.
 	 */
-	async up({ escape, schemaBuilder: { createTable, column }, runQuery }: MigrationContext) {
+	async up({
+		escape,
+		schemaBuilder: { createTable, column, createIndex, dropIndex },
+		runQuery,
+	}: MigrationContext) {
 		const executionEntity = escape.tableName('execution_entity');
 		const workflowId = escape.columnName('workflowId');
 
@@ -32,6 +36,29 @@ export class DisallowOrphanExecutions1693554410387 implements ReversibleMigratio
 				onDelete: 'CASCADE',
 			});
 
+		const isUnique = false;
+
+		await dropIndex('execution_entity', ['waitTill'], 'idx_execution_entity_wait_till');
+		await dropIndex('execution_entity', ['stoppedAt'], 'idx_execution_entity_stopped_at');
+		await dropIndex('execution_entity', ['waitTill', 'id'], 'IDX_b94b45ce2c73ce46c54f20b5f9');
+		await dropIndex(
+			'execution_entity',
+			['status', 'workflowId'],
+			'IDX_8b6f3f9ae234f137d707b98f3bf43584',
+		);
+		await dropIndex('execution_entity', ['workflowId', 'id'], 'IDX_81fc04c8a17de15835713505e4');
+
+		await createIndex('temp', ['waitTill'], isUnique, 'idx_execution_entity_wait_till');
+		await createIndex('temp', ['stoppedAt'], isUnique, 'idx_execution_entity_stopped_at');
+		await createIndex('temp', ['waitTill', 'id'], isUnique, 'IDX_b94b45ce2c73ce46c54f20b5f9');
+		await createIndex(
+			'temp',
+			['status', 'workflowId'],
+			isUnique,
+			'IDX_8b6f3f9ae234f137d707b98f3bf43584',
+		);
+		await createIndex('temp', ['workflowId', 'id'], isUnique, 'IDX_81fc04c8a17de15835713505e4');
+
 		await runQuery(
 			`INSERT INTO "temp" ("id", "workflowId", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "waitTill", "status") SELECT "id", "workflowId", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "waitTill", "status" FROM ${executionEntity};`,
 		);
@@ -44,7 +71,11 @@ export class DisallowOrphanExecutions1693554410387 implements ReversibleMigratio
 	/**
 	 * Reversal excludes restoring deleted rows.
 	 */
-	async down({ escape, schemaBuilder: { createTable, column }, runQuery }: MigrationContext) {
+	async down({
+		escape,
+		schemaBuilder: { createTable, column, createIndex, dropIndex },
+		runQuery,
+	}: MigrationContext) {
 		const executionEntity = escape.tableName('execution_entity');
 
 		await createTable('temp')
@@ -65,6 +96,29 @@ export class DisallowOrphanExecutions1693554410387 implements ReversibleMigratio
 				columnName: 'id',
 				onDelete: 'CASCADE',
 			});
+
+		await dropIndex('execution_entity', ['waitTill'], 'idx_execution_entity_wait_till');
+		await dropIndex('execution_entity', ['stoppedAt'], 'idx_execution_entity_stopped_at');
+		await dropIndex('execution_entity', ['waitTill', 'id'], 'IDX_b94b45ce2c73ce46c54f20b5f9');
+		await dropIndex(
+			'execution_entity',
+			['status', 'workflowId'],
+			'IDX_8b6f3f9ae234f137d707b98f3bf43584',
+		);
+		await dropIndex('execution_entity', ['workflowId', 'id'], 'IDX_81fc04c8a17de15835713505e4');
+
+		const isUnique = false;
+
+		await createIndex('temp', ['waitTill'], isUnique, 'idx_execution_entity_wait_till');
+		await createIndex('temp', ['stoppedAt'], isUnique, 'idx_execution_entity_stopped_at');
+		await createIndex('temp', ['waitTill', 'id'], isUnique, 'IDX_b94b45ce2c73ce46c54f20b5f9');
+		await createIndex(
+			'temp',
+			['status', 'workflowId'],
+			isUnique,
+			'IDX_8b6f3f9ae234f137d707b98f3bf43584',
+		);
+		await createIndex('temp', ['workflowId', 'id'], isUnique, 'IDX_81fc04c8a17de15835713505e4');
 
 		await runQuery(
 			`INSERT INTO "temp" ("id", "workflowId", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "waitTill", "status") SELECT "id", "workflowId", "finished", "mode", "retryOf", "retrySuccessId", "startedAt", "stoppedAt", "waitTill", "status" FROM ${executionEntity};`,
