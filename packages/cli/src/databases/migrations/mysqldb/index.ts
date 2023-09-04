413e0bccb4d0144e5bdcf9221762e3f2b4428194
Iván Ovejero <ivov.src@gmail.com>
2023-09-04 14:34:03 +0200

    feat(core): Migration for soft deletions for executions (#7088)
    Based on https://github.com/n8n-io/n8n/pull/7065
    
    ---------
    
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

--- packages/cli/src/databases/migrations/mysqldb/index.ts
+++ packages/cli/src/databases/migrations/mysqldb/index.ts
@@ -46,6 +46,7 @@ import { RemoveResetPasswordColumns1690000000030 } from '../common/1690000000030
 import { CreateWorkflowNameIndex1691088862123 } from '../common/1691088862123-CreateWorkflowNameIndex';
 import { AddMfaColumns1690000000030 } from './../common/1690000000040-AddMfaColumns';
 import { CreateWorkflowHistoryTable1692967111175 } from '../common/1692967111175-CreateWorkflowHistoryTable';
+import { ExecutionSoftDelete1693491613982 } from '../common/1693491613982-ExecutionSoftDelete';
 
 export const mysqlMigrations: Migration[] = [
 	InitialMigration1588157391238,
@@ -95,4 +96,5 @@ export const mysqlMigrations: Migration[] = [
 	CreateWorkflowNameIndex1691088862123,
 	AddMfaColumns1690000000030,
 	CreateWorkflowHistoryTable1692967111175,
+	ExecutionSoftDelete1693491613982,
 ];
