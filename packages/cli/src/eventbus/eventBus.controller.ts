b00e1f1c8cab5c9d26b09f8badbd009e01614c05
Michael Auerswald <michael.auerswald@gmail.com>
2023-09-05 10:39:10 +0200

    cleanup

--- packages/cli/src/eventbus/eventBus.controller.ts
+++ packages/cli/src/eventbus/eventBus.controller.ts
@@ -22,33 +22,11 @@ import { RestController, Get, Post, Authorized } from '@/decorators';
 // TypeGuards
 // ----------------------------------------
 
-// const isWithIdString = (candidate: unknown): candidate is { id: string } => {
-// 	const o = candidate as { id: string };
-// 	if (!o) return false;
-// 	return o.id !== undefined;
-// };
-
 const isWithQueryString = (candidate: unknown): candidate is { query: string } => {
 	const o = candidate as { query: string };
 	if (!o) return false;
 	return o.query !== undefined;
 };
-
-// const isMessageEventBusDestinationWebhookOptions = (
-// 	candidate: unknown,
-// ): candidate is MessageEventBusDestinationWebhookOptions => {
-// 	const o = candidate as MessageEventBusDestinationWebhookOptions;
-// 	if (!o) return false;
-// 	return o.url !== undefined;
-// };
-
-// const isMessageEventBusDestinationOptions = (
-// 	candidate: unknown,
-// ): candidate is MessageEventBusDestinationOptions => {
-// 	const o = candidate as MessageEventBusDestinationOptions;
-// 	if (!o) return false;
-// 	return o.__type !== undefined;
-// };
 
 // ----------------------------------------
 // Controller
@@ -141,81 +119,6 @@ export class EventBusController {
 		}
 		return msg;
 	}
-
-	// // ----------------------------------------
-	// // Destinations
-	// // ----------------------------------------
-
-	// @Get('/destination', { middlewares: [logStreamingLicensedMiddleware] })
-	// async getDestination(req: express.Request): Promise<MessageEventBusDestinationOptions[]> {
-	// 	if (isWithIdString(req.query)) {
-	// 		return eventBus.findDestination(req.query.id);
-	// 	} else {
-	// 		return eventBus.findDestination();
-	// 	}
-	// }
-
-	// @Authorized(['global', 'owner'])
-	// @Post('/destination', { middlewares: [logStreamingLicensedMiddleware] })
-	// async postDestination(req: AuthenticatedRequest): Promise<any> {
-	// 	let result: MessageEventBusDestination | undefined;
-	// 	if (isMessageEventBusDestinationOptions(req.body)) {
-	// 		switch (req.body.__type) {
-	// 			case MessageEventBusDestinationTypeNames.sentry:
-	// 				if (isMessageEventBusDestinationSentryOptions(req.body)) {
-	// 					result = await eventBus.addDestination(
-	// 						new MessageEventBusDestinationSentry(eventBus, req.body),
-	// 					);
-	// 				}
-	// 				break;
-	// 			case MessageEventBusDestinationTypeNames.webhook:
-	// 				if (isMessageEventBusDestinationWebhookOptions(req.body)) {
-	// 					result = await eventBus.addDestination(
-	// 						new MessageEventBusDestinationWebhook(eventBus, req.body),
-	// 					);
-	// 				}
-	// 				break;
-	// 			case MessageEventBusDestinationTypeNames.syslog:
-	// 				if (isMessageEventBusDestinationSyslogOptions(req.body)) {
-	// 					result = await eventBus.addDestination(
-	// 						new MessageEventBusDestinationSyslog(eventBus, req.body),
-	// 					);
-	// 				}
-	// 				break;
-	// 			default:
-	// 				throw new BadRequestError(
-	// 					`Body is missing ${req.body.__type} options or type ${req.body.__type} is unknown`,
-	// 				);
-	// 		}
-	// 		if (result) {
-	// 			await result.saveToDb();
-	// 			return {
-	// 				...result.serialize(),
-	// 				eventBusInstance: undefined,
-	// 			};
-	// 		}
-	// 		throw new BadRequestError('There was an error adding the destination');
-	// 	}
-	// 	throw new BadRequestError('Body is not configuring MessageEventBusDestinationOptions');
-	// }
-
-	// @Get('/testmessage', { middlewares: [logStreamingLicensedMiddleware] })
-	// async sendTestMessage(req: express.Request): Promise<boolean> {
-	// 	if (isWithIdString(req.query)) {
-	// 		return eventBus.testDestination(req.query.id);
-	// 	}
-	// 	return false;
-	// }
-
-	// @Authorized(['global', 'owner'])
-	// @Delete('/destination', { middlewares: [logStreamingLicensedMiddleware] })
-	// async deleteDestination(req: AuthenticatedRequest): Promise<DeleteResult | undefined> {
-	// 	if (isWithIdString(req.query)) {
-	// 		return eventBus.removeDestination(req.query.id);
-	// 	} else {
-	// 		throw new BadRequestError('Query is missing id');
-	// 	}
-	// }
 
 	// ----------------------------------------
 	// Utilities
