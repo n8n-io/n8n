115b017f516c3e102ff9e5fb04b40534c65df5ce
Oleg Ivaniv <me@olegivaniv.com>
2023-09-05 18:02:29 +0200

    :zap: Restrict ChainStructuredOuput to ChatOpenAi only
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/@n8n/nodes-langchain/nodes/chains/ChainStructuredOutput/ChainStructuredOutput.node.ts
+++ packages/@n8n/nodes-langchain/nodes/chains/ChainStructuredOutput/ChainStructuredOutput.node.ts
@@ -51,6 +51,9 @@ export class ChainStructuredOutput implements INodeType {
 				displayName: 'Language Model',
 				maxConnections: 1,
 				type: 'languageModel',
+				filter: {
+					nodes: ['@n8n/nodes-langchain.lmChatOpenAi'],
+				},
 				required: true,
 			},
 		],
