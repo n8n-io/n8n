032d68dc65308b353610564587dbe0eecdce05b3
Oleg Ivaniv <me@olegivaniv.com>
2023-09-07 17:54:10 +0200

    :bug: Do not wrap images and add option to simplify chat output
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/@n8n/nodes-langchain/nodes/memory/MemoryChatRetriever/MemoryChatRetriever.node.ts
+++ packages/@n8n/nodes-langchain/nodes/memory/MemoryChatRetriever/MemoryChatRetriever.node.ts
@@ -1,12 +1,20 @@
 /* eslint-disable n8n-nodes-base/node-dirname-against-convention */
 import type {
+	IDataObject,
 	IExecuteFunctions,
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
 } from 'n8n-workflow';
 import type { BaseChatMemory } from 'langchain/memory';
+import type { SerializedConstructor } from 'langchain/dist/load/serializable';
 
+function simplifyMessage(message: SerializedConstructor) {
+	return {
+		sender: message.id[message.id.length - 1],
+		text: message.kwargs.content,
+	};
+}
 export class MemoryChatRetriever implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Chat Messages Retriever',
@@ -36,16 +44,34 @@ export class MemoryChatRetriever implements INodeType {
 		],
 		// eslint-disable-next-line n8n-nodes-base/node-class-description-outputs-wrong
 		outputs: ['main'],
-		properties: [],
+		properties: [
+			{
+				displayName: 'Simplify Output',
+				name: 'simplifyOutput',
+				type: 'boolean',
+				description: 'Whether to simplify the output to only include the sender and the text',
+				default: true,
+			},
+		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		this.logger.verbose('Executing Chat Memory Retriever');
 
 		const memory = (await this.getInputConnectionData('memory', 0)) as BaseChatMemory | undefined;
-		const messages = await memory?.chatHistory.getMessages();
-		const serializedMessages = messages?.map((message) => message.toJSON());
+		const simplifyOutput = this.getNodeParameter('simplifyOutput', 0) as boolean;
 
-		return this.prepareOutputData([{ json: { messages: serializedMessages } }]);
+		const messages = await memory?.chatHistory.getMessages();
+
+		const serializedMessages =
+			messages?.map((message) => {
+				const serializedMessage = message.toJSON();
+				if (simplifyOutput)
+					return { json: simplifyMessage(serializedMessage as SerializedConstructor) };
+
+				return { json: serializedMessage as unknown as IDataObject };
+			}) ?? [];
+
+		return this.prepareOutputData(serializedMessages);
 	}
 }
