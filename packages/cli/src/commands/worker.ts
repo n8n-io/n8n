005a3629b74e83f0afac1f58e604a5999119f08a
Omri Attoun <omriattoun@gmail.com>
2023-09-12 10:44:12 -0700

    Increased concurrency

--- packages/cli/src/commands/worker.ts
+++ packages/cli/src/commands/worker.ts
@@ -32,7 +32,7 @@ export class Worker extends BaseCommand {
 	static flags = {
 		help: flags.help({ char: 'h' }),
 		concurrency: flags.integer({
-			default: 10,
+			default: 50,
 			description: 'How many jobs can run in parallel.',
 		}),
 	};
