c73f18c2e2dbac85c9ba4b90dc609c1a8dcd4ab2
Iván Ovejero <ivov.src@gmail.com>
2023-09-14 17:09:37 +0200

    Add type guard

---
+++ packages/core/src/binaryData/utils.ts
@@ -0,0 +1,15 @@
+import type { BinaryData } from './types';
+
+export class InvalidModeError extends Error {}
+
+/**
+ * Modes for storing binary data:
+ * - `default` (in memory)
+ * - `filesystem` (on disk)
+ * - `object` (S3)
+ */
+export const BINARY_DATA_MODES = ['default', 'filesystem', 'object'] as const;
+
+export function areValidModes(modes: string[]): modes is BinaryData.Mode[] {
+	return modes.every((m) => BINARY_DATA_MODES.includes(m as BinaryData.Mode));
+}
