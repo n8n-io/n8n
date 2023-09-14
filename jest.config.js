12be06cc3728802c90e6992e05326dd032977dd8
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-09-14 19:43:05 +0200

    ci: Use @swc/jest for some of the packages (no-changelog)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,27 @@
-const { compilerOptions } = require('./tsconfig.json');
+const tsConfig = require('get-tsconfig').getTsconfig();
+const compilerOptions = tsConfig.config.compilerOptions;
+const { baseUrl, paths } = compilerOptions;
+const useSwc = !tsConfig.path.includes('packages/cli');
 
+/** @type {import('@swc/types').Config} */
+const swcJestOptions = {
+	jsc: {
+		parser: {
+			syntax: 'typescript',
+			tsx: false,
+			decorators: true,
+		},
+		transform: {
+			legacyDecorator: true,
+			decoratorMetadata: true,
+		},
+		target: 'es2020',
+	},
+};
+
+// TODO: remove ts-jest and use swc in `cli` once swc supports emitting decorator metadata
+// https://github.com/swc-project/swc/issues/6824
+/** @type {import('ts-jest').TsJestTransformerOptions} */
 const tsJestOptions = {
 	isolatedModules: true,
 	tsconfig: {
@@ -9,8 +31,6 @@ const tsJestOptions = {
 	},
 };
 
-const { baseUrl, paths } = require('get-tsconfig').getTsconfig().config?.compilerOptions;
-
 /** @type {import('jest').Config} */
 const config = {
 	verbose: true,
@@ -18,7 +38,7 @@ const config = {
 	testRegex: '\\.(test|spec)\\.(js|ts)$',
 	testPathIgnorePatterns: ['/dist/', '/node_modules/'],
 	transform: {
-		'^.+\\.ts$': ['ts-jest', tsJestOptions],
+		'^.+\\.ts$': useSwc ? ['@swc/jest', swcJestOptions] : ['ts-jest', tsJestOptions],
 	},
 	// This resolve the path mappings from the tsconfig relative to each jest.config.js
 	moduleNameMapper: Object.entries(paths || {}).reduce((acc, [path, [mapping]]) => {
