f1209b1517e530589627f07fef2bf6aa466d647b
Val <68596159+valya@users.noreply.github.com>
2023-09-14 14:14:46 +0100

    fix: Expression evaluator not being set for node subtitles

--- packages/editor-ui/src/mixins/nodeHelpers.ts
+++ packages/editor-ui/src/mixins/nodeHelpers.ts
@@ -19,7 +19,7 @@ import type {
 	INodePropertyOptions,
 	IDataObject,
 } from 'n8n-workflow';
-import { NodeHelpers } from 'n8n-workflow';
+import { NodeHelpers, ExpressionEvaluatorProxy } from 'n8n-workflow';
 
 import type {
 	ICredentialsResponse,
@@ -531,6 +531,9 @@ export const nodeHelpers = defineComponent({
 
 			if (nodeType !== null && nodeType.subtitle !== undefined) {
 				try {
+					ExpressionEvaluatorProxy.setEvaluator(
+						useSettingsStore().settings.expressions?.evaluator ?? 'tmpl',
+					);
 					return workflow.expression.getSimpleParameterValue(
 						data as INode,
 						nodeType.subtitle,
