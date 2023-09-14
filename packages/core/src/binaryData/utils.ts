8fde8be2b3afc0757f8ef896a0727644a9044250
Iván Ovejero <ivov.src@gmail.com>
2023-09-14 19:38:14 +0200

    Better error

--- packages/core/src/binaryData/utils.ts
+++ packages/core/src/binaryData/utils.ts
@@ -1,7 +1,5 @@
 import type { BinaryData } from './types';
 
-export class InvalidBinaryModeError extends Error {}
-
 /**
  * Modes for storing binary data:
  * - `default` (in memory)
@@ -12,4 +10,13 @@ export const BINARY_DATA_MODES = ['default', 'filesystem', 'object'] as const;
 
 export function areValidModes(modes: string[]): modes is BinaryData.Mode[] {
 	return modes.every((m) => BINARY_DATA_MODES.includes(m as BinaryData.Mode));
+}
+
+export class InvalidBinaryModeError extends Error {
+	constructor() {
+		const validModes = BINARY_DATA_MODES.join(', ');
+		super(
+			`Invalid binary data mode. Set N8N_AVAILABLE_BINARY_DATA_MODES using only valid modes: ${validModes}`,
+		);
+	}
 }
