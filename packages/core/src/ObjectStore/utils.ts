e22f0bf6147de97c6667fa33c73e00ad1fc3c6c5
Iván Ovejero <ivov.src@gmail.com>
2023-09-19 11:27:31 +0200

    Remove unused

--- packages/core/src/ObjectStore/utils.ts
+++ packages/core/src/ObjectStore/utils.ts
@@ -3,17 +3,3 @@ import { Stream } from 'node:stream';
 export function isStream(maybeStream: unknown): maybeStream is Stream {
 	return maybeStream instanceof Stream;
 }
-
-// @TODO: Add more info to errors
-
-export class DownloadTypeError extends TypeError {
-	constructor(expectedType: 'stream' | 'buffer', actualType: string) {
-		super(`Expected ${expectedType} but received ${actualType} from external storage download.`);
-	}
-}
-
-export class RequestToObjectStorageFailed extends Error {
-	constructor() {
-		super('Request to external object storage failed');
-	}
-}
