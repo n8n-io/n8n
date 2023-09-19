d5c893586068959d3c7b588cbe3252c3cc0ee2ac
Iván Ovejero <ivov.src@gmail.com>
2023-09-19 11:35:10 +0200

    Cleanup

--- packages/core/src/ObjectStore/ObjectStore.service.ee.ts
+++ packages/core/src/ObjectStore/ObjectStore.service.ee.ts
@@ -1,67 +1,125 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 import axios from 'axios';
 import { Service } from 'typedi';
 import { sign } from 'aws4';
-import { isStream, DownloadTypeError, RequestToObjectStorageFailed } from './utils';
-import type { AxiosPromise, Method } from 'axios';
+import { isStream } from './utils';
+import { ObjectStorageError } from './errors';
+import { createHash } from 'node:crypto';
+import type { AxiosRequestConfig, Method, ResponseType } from 'axios';
 import type { Request as Aws4Options, Credentials as Aws4Credentials } from 'aws4';
-
-/**
- * `/workflows/{workflowId}/executions/{executionId}/binary_data/{fileId}`
- */
 
 @Service()
 export class ObjectStoreService {
+	private credentials: Aws4Credentials;
+
 	constructor(
 		private bucket: { region: string; name: string },
-		private credentials: { accountId: string; secretKey: string },
+		credentials: { accountId: string; secretKey: string },
 	) {
-		// @TODO: Confirm connection
+		this.credentials = {
+			accessKeyId: credentials.accountId,
+			secretAccessKey: credentials.secretKey,
+		};
 	}
 
-	async getStream(objectPath: string) {
-		const result = await this.request('GET', objectPath, { mode: 'stream' });
+	/**
+	 * Confirm that the configured bucket exists and that the caller has permission to access it.
+	 *
+	 * @doc https://docs.aws.amazon.com/AmazonS3/latest/API/API_HeadBucket.html
+	 */
+	async checkConnection() {
+		const host = `s3.${this.bucket.region}.amazonaws.com`;
 
-		if (isStream(result)) return result;
-
-		throw new DownloadTypeError('stream', typeof result);
+		return this.request('HEAD', host, this.bucket.name); // @TODO: Not working with Axios
 	}
 
-	async getBuffer(objectPath: string) {
-		const result = await this.request('GET', objectPath, { mode: 'buffer' });
+	/**
+	 * Upload an object to the configured bucket.
+	 *
+	 * @doc https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutObject.html
+	 */
+	async put(filename: string, buffer: Buffer) {
+		const host = `${this.bucket.name}.s3.${this.bucket.region}.amazonaws.com`;
 
-		if (Buffer.isBuffer(result)) return result;
+		const headers = {
+			'Content-Length': buffer.length,
+			'Content-Type': 'image/jpg', // @TODO: Derive
+			'Content-MD5': createHash('md5').update(buffer).digest('base64'),
+		};
 
-		throw new DownloadTypeError('buffer', typeof result);
+		return this.request('PUT', host, `/${filename}`, { body: buffer, headers });
+	}
+
+	/**
+	 * Download an object as a stream from the configured bucket.
+	 *
+	 * @doc https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetObject.html
+	 */
+	async getStream(path: string) {
+		const host = `${this.bucket.name}.s3.${this.bucket.region}.amazonaws.com`;
+
+		const { data } = await this.request('GET', host, path, { responseType: 'stream' });
+
+		if (isStream(data)) return data;
+
+		throw new ObjectStorageError.TypeMismatch('stream', typeof data);
+	}
+
+	/**
+	 * Download an object as a buffer from the configured bucket.
+	 *
+	 * @doc https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetObject.html
+	 */
+	async getBuffer(path: string) {
+		const host = `${this.bucket.name}.s3.${this.bucket.region}.amazonaws.com`;
+
+		const { data } = await this.request('GET', host, path, { responseType: 'arraybuffer' });
+
+		if (Buffer.isBuffer(data)) return data;
+
+		throw new ObjectStorageError.TypeMismatch('buffer', typeof data);
 	}
 
 	private async request(
 		method: Method,
-		objectPath: string,
-		{ mode }: { mode: 'stream' | 'buffer' },
+		host: string,
+		path: string,
+		{
+			headers,
+			responseType,
+		}: {
+			headers?: Record<string, string | number>;
+			body?: Buffer;
+			responseType?: ResponseType;
+		} = {},
 	) {
-		// @TODO Decouple host from AWS
-		const host = `${this.bucket.name}.s3.${this.bucket.region}.amazonaws.com`;
+		const slashPath = path.startsWith('/') ? path : `/${path}`;
 
-		const options: Aws4Options = {
+		const optionsToSign: Aws4Options = {
 			host,
-			path: `/${objectPath}`,
+			path: slashPath,
+			...headers,
 		};
 
-		const credentials: Aws4Credentials = {
-			accessKeyId: this.credentials.accountId,
-			secretAccessKey: this.credentials.secretKey,
-		};
+		const signedOptions = sign(optionsToSign, this.credentials);
 
-		const signed = sign(options, credentials);
-
-		const response: Awaited<AxiosPromise<unknown>> = await axios(`https://${host}/${objectPath}`, {
+		const config: AxiosRequestConfig = {
 			method,
-			headers: signed.headers,
-			responseType: mode === 'buffer' ? 'arraybuffer' : 'stream',
-		});
+			url: `https://${host}${slashPath}`,
+			headers: signedOptions.headers,
+		};
 
-		if (response.status !== 200) throw new RequestToObjectStorageFailed();
+		if (responseType) config.responseType = responseType;
 
-		return response.data;
+		console.log('Axios request config', config); // @TODO: Remove
+
+		try {
+			return await axios.request<unknown>(config);
+		} catch (error) {
+			if (error instanceof Error) console.log(error.message); // @TODO: Remove
+			// throw new ObjectStorageError.RequestFailed(config); // @TODO: Restore
+			throw error;
+		}
 	}
 }
