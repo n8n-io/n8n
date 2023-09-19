c5b77d0c842b552d324f4f87f4e8ac78ee4f7dc2
Mutasem <mutdmour@gmail.com>
2023-09-19 19:05:52 +0200

    test: enable

--- cypress/e2e/29-templates.cy.ts
+++ cypress/e2e/29-templates.cy.ts
@@ -5,12 +5,12 @@ const templatesPage = new TemplatesPage();
 const WorkflowPage = new WorkflowPageClass();
 
 describe('Templates', () => {
-	// it('can open onboarding flow', () => {
-	// 	templatesPage.actions.openOnboardingFlow();
+	it('can open onboarding flow', () => {
+		templatesPage.actions.openOnboardingFlow();
 
-	// 	WorkflowPage.getters.canvasNodes().should('have.length', 4);
-	// 	WorkflowPage.getters.stickies().should('have.length', 2);
-	// });
+		WorkflowPage.getters.canvasNodes().should('have.length', 4);
+		WorkflowPage.getters.stickies().should('have.length', 2);
+	});
 
 	it('can import template', () => {
 		templatesPage.actions.openTemplateImportFlow();
