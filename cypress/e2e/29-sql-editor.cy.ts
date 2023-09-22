929370599662b441b79f9ef40c0ce5e58b9b43b2
Michael Kret <michael.k@radency.com>
2023-09-22 13:47:21 +0300

    :zap: removed similar tests

--- cypress/e2e/29-sql-editor.cy.ts
+++ cypress/e2e/29-sql-editor.cy.ts
@@ -8,22 +8,6 @@ describe('SQL editors', () => {
 		workflowPage.actions.visit();
 	});
 
-	it('should preserve changes when opening-closing MySQL node', () => {
-		workflowPage.actions.addInitialNodeToCanvas('Manual');
-		workflowPage.actions.addNodeToCanvas('MySQL');
-		workflowPage.actions.openNode('MySQL');
-		ndv.getters.parameterInput('operation').click();
-		cy.get('div').contains('Execute SQL').click();
-		cy.get('div.cm-activeLine').type('SELECT * FROM `testTable`');
-		ndv.actions.close();
-		workflowPage.actions.openNode('MySQL');
-		cy.get('div.cm-activeLine').type('{end} LIMIT 10');
-		ndv.actions.close();
-		workflowPage.actions.openNode('MySQL');
-
-		cy.get('div.cm-activeLine').contains('SELECT * FROM `testTable` LIMIT 10');
-	});
-
 	it('should preserve changes when opening-closing Postgres node', () => {
 		workflowPage.actions.addInitialNodeToCanvas('Manual');
 		workflowPage.actions.addNodeToCanvas('Postgres');
@@ -36,22 +20,6 @@ describe('SQL editors', () => {
 		cy.get('div.cm-activeLine').type('{end} LIMIT 10');
 		ndv.actions.close();
 		workflowPage.actions.openNode('Postgres');
-
-		cy.get('div.cm-activeLine').contains('SELECT * FROM `testTable` LIMIT 10');
-	});
-
-	it('should preserve changes when opening-closing Google BigQuery node', () => {
-		workflowPage.actions.addInitialNodeToCanvas('Manual');
-		workflowPage.actions.addNodeToCanvas('Google BigQuery');
-		workflowPage.actions.openNode('Google BigQuery');
-		ndv.getters.parameterInput('operation').click();
-		cy.get('div').contains('Execute Query').click();
-		cy.get('div.cm-activeLine').type('SELECT * FROM `testTable`');
-		ndv.actions.close();
-		workflowPage.actions.openNode('Google BigQuery');
-		cy.get('div.cm-activeLine').type('{end} LIMIT 10');
-		ndv.actions.close();
-		workflowPage.actions.openNode('Google BigQuery');
 
 		cy.get('div.cm-activeLine').contains('SELECT * FROM `testTable` LIMIT 10');
 	});
