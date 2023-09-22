5ba2f9d86c165b890f7b10dac250c8a0d2c89037
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-09-22 12:18:48 +0200

    fix(editor): Update WF history API paths

--- packages/editor-ui/src/api/workflowHistory.ts
+++ packages/editor-ui/src/api/workflowHistory.ts
@@ -2,15 +2,21 @@ import type { IRestApiContext } from '@/Interface';
 import { makeRestApiRequest } from '@/utils';
 import type { WorkflowHistory, WorkflowVersion } from '@/types/workflowHistory';
 
+const workflowHistoryApiRoot = '/workflow-history';
+
 export const getWorkflowHistory = async (
 	context: IRestApiContext,
 	workflowId: string,
 ): Promise<WorkflowHistory[]> =>
-	makeRestApiRequest(context, 'POST', `/workflow/${workflowId}/history`);
+	makeRestApiRequest(context, 'POST', `${workflowHistoryApiRoot}/workflow/${workflowId}`);
 
 export const getWorkflowVersion = async (
 	context: IRestApiContext,
 	workflowId: string,
 	versionId: string,
 ): Promise<WorkflowVersion> =>
-	makeRestApiRequest(context, 'POST', `/workflow/${workflowId}/history/${versionId}`);
+	makeRestApiRequest(
+		context,
+		'POST',
+		`${workflowHistoryApiRoot}/workflow/${workflowId}/version/${versionId}`,
+	);
