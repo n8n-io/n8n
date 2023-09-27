a2f67ddc378fe0495e1ae5c5a76bfc59347ffbbd
Iván Ovejero <ivov.src@gmail.com>
2023-09-27 17:35:39 +0200

    Cleanup

--- packages/core/src/ObjectStore/errors.ts
+++ packages/core/src/ObjectStore/errors.ts
@@ -3,8 +3,8 @@ import { AxiosRequestConfig } from 'axios';
 export namespace ObjectStore {
 	export class RequestFailedError extends Error {
 		constructor(error: unknown, config: AxiosRequestConfig) {
-			super(`Request to external object storage failed. Config: ${JSON.stringify(config)}`);
-			this.cause = { error };
+			super('Request to external object storage failed.');
+			this.cause = { error, details: config };
 		}
 	}
 }
