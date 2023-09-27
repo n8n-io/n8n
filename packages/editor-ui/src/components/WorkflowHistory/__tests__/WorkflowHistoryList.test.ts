6dff94952792417785fad45f83bf9de142c89a51
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-09-27 17:15:24 +0200

    test(editor): Test workflow history list component

---
+++ packages/editor-ui/src/components/WorkflowHistory/__tests__/WorkflowHistoryList.test.ts
@@ -0,0 +1,68 @@
+import { vi } from 'vitest';
+import { createPinia, setActivePinia } from 'pinia';
+import { faker } from '@faker-js/faker';
+import { createComponentRenderer } from '@/__tests__/render';
+import WorkflowHistoryList from '@/components/WorkflowHistory/WorkflowHistoryList.vue';
+import type { WorkflowHistory, WorkflowHistoryActionTypes } from '@/types/workflowHistory';
+
+vi.stubGlobal(
+	'IntersectionObserver',
+	vi.fn(() => ({
+		disconnect: vi.fn(),
+		observe: vi.fn(),
+		takeRecords: vi.fn(),
+		unobserve: vi.fn(),
+	})),
+);
+
+const workflowHistoryDataFactory: () => WorkflowHistory = () => ({
+	versionId: faker.string.nanoid(),
+	createdAt: faker.date.past().toDateString(),
+	authors: Array.from({ length: Math.floor(Math.random() * 5) + 1 }, faker.person.fullName).join(
+		', ',
+	),
+});
+
+const actionTypes: WorkflowHistoryActionTypes = ['restore', 'clone', 'open', 'download'];
+
+const renderComponent = createComponentRenderer(WorkflowHistoryList);
+
+let pinia: ReturnType<typeof createPinia>;
+
+describe('WorkflowHistoryList', () => {
+	beforeEach(() => {
+		pinia = createPinia();
+		setActivePinia(pinia);
+	});
+
+	it('should render empty workflow history list', () => {
+		const { getByText } = renderComponent({
+			pinia,
+			props: {
+				items: [],
+				actionTypes,
+				activeItem: null,
+				requestNumberOfItems: 20,
+			},
+		});
+
+		expect(getByText(/No versions yet/)).toBeInTheDocument();
+	});
+
+	it('should render workflow history list', () => {
+		const numberOfItems = Math.floor(Math.random() * 20) + 1;
+		const items = Array.from({ length: numberOfItems }, workflowHistoryDataFactory);
+
+		const { container } = renderComponent({
+			pinia,
+			props: {
+				items,
+				actionTypes,
+				activeItem: null,
+				requestNumberOfItems: 20,
+			},
+		});
+
+		expect(container.querySelectorAll('li')).toHaveLength(numberOfItems);
+	});
+});
