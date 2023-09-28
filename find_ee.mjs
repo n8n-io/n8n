128fbd179119964c120125b78282e6d2a4dc673a
Marcus <marcus@n8n.io>
2023-09-28 14:03:45 +0200

    delete unused file that was commited by accident

--- find_ee.mjs
+++
@@ -1,9 +0,0 @@
-import { glob, globSync, globStream, globStreamSync, Glob } from 'glob';
-
-let tsFiles = await glob('**/*.ee.ts', { ignore: 'node_modules/**' });
-
-tsFiles = tsFiles.map((f) => f.replace(/\\/g, '/'));
-
-const tsFileNames = tsFiles.map((f) => f.split('/').reverse()[0]);
-
-console.log({ tsFiles, tsFileNames });
