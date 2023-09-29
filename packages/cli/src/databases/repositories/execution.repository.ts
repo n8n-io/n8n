d45a3fa70f38a1f61cb50e2a36e0830e7979375f
Iván Ovejero <ivov.src@gmail.com>
2023-09-29 16:54:09 +0200

    Undo pruning test change

--- packages/cli/src/databases/repositories/execution.repository.ts
+++ packages/cli/src/databases/repositories/execution.repository.ts
@@ -79,7 +79,7 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 
 	private rates: Record<string, number> = {
 		softDeletion: 1 * TIME.HOUR,
-		hardDeletion: 10 * TIME.SECOND,
+		hardDeletion: 15 * TIME.MINUTE,
 	};
 
 	private isMainInstance = config.get('generic.instanceType') === 'main';
