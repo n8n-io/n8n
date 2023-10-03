101255d18661d18cbc236ea3ac20c8401fa357e4
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-10-03 20:49:04 +0200

    ci: Make builds release-channel aware. Add support for scheduled beta builds (#7323)
    ADO-1121
    
    ---------
    
    Co-authored-by: Jan Oberhauser <jan.oberhauser@gmail.com>

--- packages/editor-ui/src/components/Logo.vue
+++ packages/editor-ui/src/components/Logo.vue
@@ -5,19 +5,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { mapStores } from 'pinia';
-import { useRootStore, useSettingsStore } from '@/stores';
+import { useRootStore, useUIStore } from '@/stores';
 
 export default defineComponent({
 	computed: {
-		...mapStores(useRootStore, useSettingsStore),
+		...mapStores(useRootStore, useUIStore),
 		basePath(): string {
 			return this.rootStore.baseUrl;
 		},
 		logoPath(): string {
-			return (
-				this.basePath +
-				(this.settingsStore.settings.isBetaRelease ? 'n8n-beta-logo.svg' : 'n8n-logo-expanded.svg')
-			);
+			return this.basePath + this.uiStore.logo;
 		},
 	},
 });
