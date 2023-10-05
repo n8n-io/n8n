490c06c506601252ac7b6bf146c89fdf744be9b2
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-10-05 12:56:56 +0200

    debug long running queries in postgres tests

--- packages/cli/test/teardown.ts
+++ packages/cli/test/teardown.ts
@@ -17,6 +17,17 @@ export default async () => {
 		.filter(({ Database: dbName }) => dbName.startsWith(testDbPrefix))
 		.map(({ Database: dbName }) => dbName);
 
+	if (dbType === 'postgres') {
+		const runningQueries = await connection.query(
+			`SELECT query FROM pg_stat_activity WHERE datname in (${databases
+				.map((d) => `'${d}'`)
+				.join(',')}) AND pid <> pg_backend_pid();`,
+		);
+		if (runningQueries.length) {
+			console.log('Long running queries detected', runningQueries);
+		}
+	}
+
 	const promises = databases.map(async (dbName) => connection.query(`DROP DATABASE ${dbName};`));
 	await Promise.all(promises);
 	await connection.destroy();
