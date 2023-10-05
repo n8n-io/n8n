67ef8c613cbb05d6431057c2068c597a12ba7929
Iván Ovejero <ivov.src@gmail.com>
2023-10-05 15:25:17 +0200

    feat(core): Integrate object store as binary data manager (#7253)
    Depends on: #7225 | Story:
    [PAY-848](https://linear.app/n8n/issue/PAY-848)
    
    This PR integrates the object store service as a new binary data manager
    for Enterprise.

--- packages/core/src/BinaryData/errors.ts
+++ packages/core/src/BinaryData/errors.ts
@@ -1,12 +1,10 @@
 import { BINARY_DATA_MODES } from './utils';
 
-export class InvalidBinaryDataMode extends Error {
-	constructor() {
-		super(`Invalid binary data mode. Valid modes: ${BINARY_DATA_MODES.join(', ')}`);
-	}
+export class InvalidBinaryDataModeError extends Error {
+	message = `Invalid binary data mode. Valid modes: ${BINARY_DATA_MODES.join(', ')}`;
 }
 
-export class UnknownBinaryDataManager extends Error {
+export class UnknownBinaryDataManagerError extends Error {
 	constructor(mode: string) {
 		super(`No binary data manager found for: ${mode}`);
 	}
---
+++ packages/core/test/utils.ts
@@ -0,0 +1,22 @@
+import { Container } from 'typedi';
+import { mock } from 'jest-mock-extended';
+import { Duplex } from 'stream';
+
+import type { DeepPartial } from 'ts-essentials';
+
+export const mockInstance = <T>(
+	constructor: new (...args: unknown[]) => T,
+	data: DeepPartial<T> | undefined = undefined,
+) => {
+	const instance = mock<T>(data);
+	Container.set(constructor, instance);
+	return instance;
+};
+
+export function toStream(buffer: Buffer) {
+	const duplexStream = new Duplex();
+	duplexStream.push(buffer);
+	duplexStream.push(null);
+
+	return duplexStream;
+}
