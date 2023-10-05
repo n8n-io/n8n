de4043390640e21c0f1e5fd59b0dfb48f8149183
Csaba Tuncsik <csaba@n8n.io>
2023-10-05 14:29:43 +0200

    fix(editor): Simplifying types

--- packages/editor-ui/src/stores/__tests__/utils/workflowHistoryTestUtils.ts
+++ packages/editor-ui/src/stores/__tests__/utils/workflowHistoryTestUtils.ts
@@ -4,6 +4,7 @@ import type { WorkflowHistory, WorkflowVersion } from '@/types/workflowHistory';
 export const workflowHistoryDataFactory: () => WorkflowHistory = () => ({
 	versionId: faker.string.nanoid(),
 	createdAt: faker.date.past().toDateString(),
+	updatedAt: faker.date.past().toDateString(),
 	authors: Array.from({ length: faker.number.int({ min: 2, max: 5 }) }, faker.person.fullName).join(
 		', ',
 	),
@@ -11,7 +12,6 @@ export const workflowHistoryDataFactory: () => WorkflowHistory = () => ({
 
 export const workflowVersionDataFactory: () => WorkflowVersion = () => ({
 	...workflowHistoryDataFactory(),
-	updatedAt: faker.date.past().toDateString(),
 	workflowId: faker.string.nanoid(),
 	connections: {},
 	nodes: [],
--- packages/editor-ui/src/types/workflowHistory.ts
+++ packages/editor-ui/src/types/workflowHistory.ts
@@ -4,16 +4,13 @@ export type WorkflowHistory = {
 	versionId: string;
 	authors: string;
 	createdAt: string;
+	updatedAt: string;
 };
 
 export type WorkflowVersionId = WorkflowHistory['versionId'];
 
-export type WorkflowVersion = WorkflowHistory & {
-	updatedAt: string;
-	nodes: IWorkflowDb['nodes'];
-	connections: IWorkflowDb['connections'];
-	workflowId: IWorkflowDb['id'];
-};
+export type WorkflowVersion = WorkflowHistory &
+	Pick<IWorkflowDb, 'nodes' | 'connections'> & { workflowId: IWorkflowDb['id'] };
 
 export type WorkflowHistoryActionTypes = ['restore', 'clone', 'open', 'download'];
 
