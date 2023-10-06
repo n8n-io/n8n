d923e044f01bf8b9d8dd4ae3a6ca551026b51b14
Michael Auerswald <michael.auerswald@gmail.com>
2023-10-06 13:58:11 +0200

    refactor(core): Have one orchestration service per instance type (#7303)
    webhook instances will not listen to either worker or event log messages
    on the Redis pub/sub channel

--- packages/cli/src/services/orchestration/helpers.ts
+++ packages/cli/src/services/orchestration/helpers.ts
@@ -2,6 +2,10 @@ import { LoggerProxy, jsonParse } from 'n8n-workflow';
 import type { RedisServiceCommandObject } from '../redis/RedisServiceCommands';
 import { COMMAND_REDIS_CHANNEL } from '../redis/RedisServiceHelper';
 
+export interface RedisServiceCommandLastReceived {
+	[date: string]: Date;
+}
+
 export function messageToRedisServiceCommandObject(messageString: string) {
 	if (!messageString) return;
 	let message: RedisServiceCommandObject;
@@ -14,4 +18,16 @@ export function messageToRedisServiceCommandObject(messageString: string) {
 		return;
 	}
 	return message;
+}
+
+const lastReceived: RedisServiceCommandLastReceived = {};
+
+export function debounceMessageReceiver(message: RedisServiceCommandObject, timeout: number = 100) {
+	const now = new Date();
+	const lastReceivedDate = lastReceived[message.command];
+	if (lastReceivedDate && now.getTime() - lastReceivedDate.getTime() < timeout) {
+		return false;
+	}
+	lastReceived[message.command] = now;
+	return true;
 }
