dc388139d85fca8d34ae6ad93c93aac40d42f155
Michael Auerswald <michael.auerswald@gmail.com>
2023-10-06 17:52:27 +0200

    feat(core): Add Job Summary to Worker response (#7360)

--- packages/cli/src/services/orchestration/helpers.ts
+++ packages/cli/src/services/orchestration/helpers.ts
@@ -1,6 +1,7 @@
 import { LoggerProxy, jsonParse } from 'n8n-workflow';
 import type { RedisServiceCommandObject } from '../redis/RedisServiceCommands';
 import { COMMAND_REDIS_CHANNEL } from '../redis/RedisServiceHelper';
+import * as os from 'os';
 
 export interface RedisServiceCommandLastReceived {
 	[date: string]: Date;
@@ -30,4 +31,10 @@ export function debounceMessageReceiver(message: RedisServiceCommandObject, time
 	}
 	lastReceived[message.command] = now;
 	return true;
+}
+
+export function getOsCpuString(): string {
+	const cpus = os.cpus();
+	if (cpus.length === 0) return 'no CPU info';
+	return `${cpus.length}x ${cpus[0].model} - speed: ${cpus[0].speed}`;
 }
