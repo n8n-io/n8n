fbd7ad2c1c01f2151aa10ba16782135b385108c7
Iván Ovejero <ivov.src@gmail.com>
2023-10-10 10:06:06 +0200

    feat(core): Switch binary filesystem mode to nested path structure (#7307)
    Depends on #7253 | Story:
    [PAY-863](https://linear.app/n8n/issue/PAY-863/switch-binary-filesystem-mode-to-nested-path-structure)
    
    This PR introduces `filesystem-v2` to store binary data in the
    filesystem in the same format as `s3`.

--- packages/core/src/BinaryData/utils.ts
+++ packages/core/src/BinaryData/utils.ts
@@ -3,23 +3,19 @@ import type { Readable } from 'node:stream';
 import type { BinaryData } from './types';
 import concatStream from 'concat-stream';
 
-/**
- * Modes for storing binary data:
- * - `default` (in memory)
- * - `filesystem` (on disk)
- * - `s3` (S3-compatible storage)
- */
-export const BINARY_DATA_MODES = ['default', 'filesystem', 's3'] as const;
+export const CONFIG_MODES = ['default', 'filesystem', 's3'] as const;
 
-export function areValidModes(modes: string[]): modes is BinaryData.Mode[] {
-	return modes.every((m) => BINARY_DATA_MODES.includes(m as BinaryData.Mode));
+const STORED_MODES = ['filesystem', 'filesystem-v2', 's3'] as const;
+
+export function areConfigModes(modes: string[]): modes is BinaryData.ConfigMode[] {
+	return modes.every((m) => CONFIG_MODES.includes(m as BinaryData.ConfigMode));
 }
 
-export function isValidNonDefaultMode(mode: string): mode is BinaryData.NonDefaultMode {
-	return BINARY_DATA_MODES.filter((m) => m !== 'default').includes(mode as BinaryData.Mode);
+export function isStoredMode(mode: string): mode is BinaryData.StoredMode {
+	return STORED_MODES.includes(mode as BinaryData.StoredMode);
 }
 
-export async function ensureDirExists(dir: string) {
+export async function assertDir(dir: string) {
 	try {
 		await fs.access(dir);
 	} catch {
