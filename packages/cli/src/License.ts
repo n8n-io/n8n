dc8ece29096649614ddd5f85bc6ca85f42ceb543
Cornelius Suermann <cornelius.suermann@gmail.com>
2023-10-21 12:45:01 +0200

    collect usage metrics on license renewal

--- packages/cli/src/License.ts
+++ packages/cli/src/License.ts
@@ -12,6 +12,7 @@ import {
 	UNLIMITED_LICENSE_QUOTA,
 } from './constants';
 import Container, { Service } from 'typedi';
+import { WorkflowRepository } from './databases/repositories';
 import type { BooleanLicenseFeature, N8nInstanceType, NumericLicenseFeature } from './Interfaces';
 import type { RedisServicePubSubPublisher } from './services/redis/RedisServicePubSubPublisher';
 import { RedisService } from './services/redis.service';
@@ -65,6 +66,7 @@ export class License {
 				loadCertStr: async () => this.loadCertStr(),
 				saveCertStr,
 				deviceFingerprint: () => this.instanceSettings.instanceId,
+				collectUsageMetrics: async () => this.collectUsageMetrics(),
 				onFeatureChange,
 			});
 
@@ -74,6 +76,15 @@ export class License {
 				this.logger.error('Could not initialize license manager sdk', e);
 			}
 		}
+	}
+
+	async collectUsageMetrics() {
+		return [
+			{
+				name: 'activeWorkflows',
+				value: await Container.get(WorkflowRepository).count({ where: { active: true } }),
+			},
+		];
 	}
 
 	async loadCertStr(): Promise<TLicenseBlock> {
