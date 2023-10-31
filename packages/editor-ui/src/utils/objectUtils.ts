71ea0ff82a1476912fdc3de3c1ab557fce0110fe
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-10-31 20:44:13 +0100

    fix(editor): Update types and add tests

--- packages/editor-ui/src/utils/objectUtils.ts
+++ packages/editor-ui/src/utils/objectUtils.ts
@@ -1,12 +1,18 @@
-export function isObjectOrArray(maybeObject: unknown): maybeObject is { [key: string]: string } {
-	return typeof maybeObject === 'object' && maybeObject !== null;
+type ObjectOrArray = Record<string, unknown> | unknown[];
+
+export function isDateObject(maybeDate: unknown): maybeDate is Date {
+	return maybeDate instanceof Date;
 }
 
-export function isObject(maybeObject: unknown): maybeObject is { [key: string]: string } {
+export function isObjectOrArray(maybeObject: unknown): maybeObject is ObjectOrArray {
+	return typeof maybeObject === 'object' && maybeObject !== null && !isDateObject(maybeObject);
+}
+
+export function isObject(maybeObject: unknown): maybeObject is ObjectOrArray {
 	return isObjectOrArray(maybeObject) && !Array.isArray(maybeObject);
 }
 
-export const searchInObject = (obj: unknown, searchString: string): boolean =>
+export const searchInObject = (obj: ObjectOrArray, searchString: string): boolean =>
 	(Array.isArray(obj) ? obj : Object.entries(obj)).some((entry) =>
 		isObjectOrArray(entry)
 			? searchInObject(entry, searchString)
