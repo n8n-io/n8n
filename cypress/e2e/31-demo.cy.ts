1b2b0a4410585fe6e49f693780ee1533ac78173b
Mutasem <mutdmour@gmail.com>
2023-11-02 14:05:18 +0100

    test: refactor

--- cypress/e2e/31-demo.cy.ts
+++ cypress/e2e/31-demo.cy.ts
@@ -1,24 +1,23 @@
 import workflow from '../fixtures/Manual_wait_set.json';
-import { DemoPage } from '../pages/demo';
+import { importWorkflow, vistDemoPage } from '../pages/demo';
 import { WorkflowPage } from '../pages/workflow';
 
-const demoPage = new DemoPage();
 const workflowPage = new WorkflowPage();
 
 describe('Demo', () => {
 	it('can import template', () => {
-		demoPage.actions.visit();
-		demoPage.actions.importWorkflow(workflow);
+		vistDemoPage();
+		importWorkflow(workflow);
 		workflowPage.getters.canvasNodes().should('have.length', 3);
 	});
 
 	it('can override theme to dark', () => {
-		demoPage.actions.visit('dark');
+		vistDemoPage('dark');
 		cy.get('body').should('have.attr', 'data-theme', 'dark');
 	});
 
 	it('can override theme to light', () => {
-		demoPage.actions.visit('light');
+		vistDemoPage('light');
 		cy.get('body').should('have.attr', 'data-theme', 'light');
 	});
 });
