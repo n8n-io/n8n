9094dd33c852152e91635621ff18472241dc6f0e
Alex Grozav <alex@grozav.com>
2023-11-03 14:14:51 +0200

    feat(editor): Update storybook to 7.5.2 (no-changelog) (#7601)
    Github issue / Community forum post (link here to close automatically):

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,17 +1,21 @@
+import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/vue3-vite';
+
+function getAbsolutePath(value: string): string {
+	return dirname(require.resolve(join(value, 'package.json'))).toString();
+}
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
 	addons: [
-		'@storybook/addon-styling',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-a11y',
-		'storybook-dark-mode',
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-a11y'),
+		getAbsolutePath('storybook-dark-mode'),
 	],
 	staticDirs: ['../public'],
 	framework: {
-		name: '@storybook/vue3-vite',
+		name: getAbsolutePath('@storybook/vue3-vite') as '@storybook/vue3-vite',
 		options: {},
 	},
 	core: {
