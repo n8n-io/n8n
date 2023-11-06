aad757175ff5feadf2939439369017d26278f4c2
Mutasem <mutdmour@gmail.com>
2023-11-06 12:56:02 +0100

    refactor: fix types

--- packages/editor-ui/src/composables/useStorage.ts
+++ packages/editor-ui/src/composables/useStorage.ts
@@ -1,6 +1,7 @@
 import { useStorage as useStorageComposable } from '@vueuse/core';
+import type { Ref } from 'vue';
 
-export function useStorage(key: string) {
+export function useStorage(key: string): Ref<string | null> {
 	const data = useStorageComposable(key, null, window.localStorage, { writeDefaults: false });
 
 	if (data.value === 'undefined') {
