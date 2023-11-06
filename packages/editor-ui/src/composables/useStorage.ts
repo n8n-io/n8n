1f31593053e58bff01d4b12a6f2f607170ff9513
Mutasem <mutdmour@gmail.com>
2023-11-06 12:54:57 +0100

    refactor: use useStorage composable

--- packages/editor-ui/src/composables/useStorage.ts
+++ packages/editor-ui/src/composables/useStorage.ts
@@ -1,33 +1,11 @@
-import { ref, watchEffect } from 'vue';
+import { useStorage as useStorageComposable } from '@vueuse/core';
 
 export function useStorage(key: string) {
-	const data = ref<null | string>(null);
+	const data = useStorageComposable(key, null, window.localStorage, { writeDefaults: false });
 
-	function updateLocalStorage(value: string | null) {
-		try {
-			if (value === null) {
-				localStorage.removeItem(key);
-			} else {
-				localStorage.setItem(key, value);
-			}
-		} catch (e) {
-			console.log(`Failed to write to localStorage for key: ${key}`);
-		}
+	if (data.value === 'undefined') {
+		data.value = null;
 	}
-
-	// initialize
-	try {
-		const storedValue = localStorage.getItem(key);
-		// bug in 1.15.1 where it returns 'undefined' as a string
-		if (storedValue !== null && storedValue !== 'undefined') {
-			data.value = storedValue;
-		}
-	} catch (e) {
-		console.log(`Failed to read from localStorage for key ${key}`);
-	}
-
-	// Watch the ref and update localStorage whenever it changes
-	watchEffect(() => updateLocalStorage(data.value));
 
 	return data;
 }
