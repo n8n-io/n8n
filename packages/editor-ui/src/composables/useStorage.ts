8cebf1ba500e8ee9d63d1f6745200b711ae9c8a2
Mutasem <mutdmour@gmail.com>
2023-11-06 12:58:03 +0100

    fix: add comment

--- packages/editor-ui/src/composables/useStorage.ts
+++ packages/editor-ui/src/composables/useStorage.ts
@@ -4,6 +4,7 @@ import type { Ref } from 'vue';
 export function useStorage(key: string): Ref<string | null> {
 	const data = useStorageComposable(key, null, window.localStorage, { writeDefaults: false });
 
+	// bug in 1.15.1
 	if (data.value === 'undefined') {
 		data.value = null;
 	}
