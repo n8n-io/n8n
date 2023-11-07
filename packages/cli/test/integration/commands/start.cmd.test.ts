5841bada5d1d478e49d946ee210cf178ca82379f
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2023-11-07 15:58:28 +0100

    ci(core): Load config schema after process.env has been overwritten (no-changelog) (#7550)

--- packages/cli/test/integration/commands/start.cmd.test.ts
+++ packages/cli/test/integration/commands/start.cmd.test.ts
@@ -36,6 +36,7 @@ afterEach(() => {
 
 test('should not init license if instance is follower in multi-main scenario', async () => {
 	config.set('executions.mode', 'queue');
+	config.set('endpoints.disableUi', true);
 	config.set('leaderSelection.enabled', true);
 
 	jest.spyOn(MultiMainInstancePublisher.prototype, 'isFollower', 'get').mockReturnValue(true);
