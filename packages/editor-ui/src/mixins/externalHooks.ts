a538a8933daab926a50839357e4b61f4d29f7be7
Alex Grozav <alex@grozav.com>
2023-11-08 16:09:02 +0200

    fix: clarify extension hooks vs existing hooks

--- packages/editor-ui/src/mixins/externalHooks.ts
+++ packages/editor-ui/src/mixins/externalHooks.ts
@@ -4,23 +4,24 @@ import type { ExternalHooks, ExternalHooksGenericContext } from '@/types';
 import { defineComponent } from 'vue';
 import { runExternalHook } from '@/utils';
 
-export function extendExternalHooks(hooks: PartialDeep<ExternalHooks>) {
+export function extendExternalHooks(hooksExtension: PartialDeep<ExternalHooks>) {
 	if (typeof window.n8nExternalHooks === 'undefined') {
 		window.n8nExternalHooks = {};
 	}
 
-	for (const resource of Object.keys(hooks) as Array<keyof ExternalHooks>) {
+	for (const resource of Object.keys(hooksExtension) as Array<keyof ExternalHooks>) {
 		if (typeof window.n8nExternalHooks[resource] === 'undefined') {
 			window.n8nExternalHooks[resource] = {};
 		}
 
-		const context = hooks[resource] as ExternalHooksGenericContext;
-		for (const operator of Object.keys(context)) {
+		const extensionContext = hooksExtension[resource] as ExternalHooksGenericContext;
+		const context = window.n8nExternalHooks[resource] as ExternalHooksGenericContext;
+		for (const operator of Object.keys(extensionContext)) {
 			if (typeof context[operator] === 'undefined') {
 				context[operator] = [];
 			}
 
-			context[operator].push(...context[operator]);
+			context[operator].push(...extensionContext[operator]);
 		}
 	}
 }
