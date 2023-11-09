2426a4a6623953e88d8d3fd4ad6615d86dde5733
ricardo <ricardoespinoza105@gmail.com>
2023-11-08 19:23:21 -0500

    Add tests to POST /users

--- packages/cli/test/integration/users.controller.test.ts
+++ packages/cli/test/integration/users.controller.test.ts
@@ -1,12 +1,24 @@
-import { createMember, createOwner } from './shared/db/users';
+import { randomEmail, randomString } from './shared/random';
 import * as testDb from './shared/testDb';
 import { setupTestServer } from './shared/utils/';
-import type { User } from '@/databases/entities/User';
 import type { PublicUser } from '@/Interfaces';
+import config from '@/config';
+import * as samlHelpers from '@/sso/saml/samlHelpers';
+import { UserManagementMailer } from '@/UserManagement/email';
+import * as utils from './shared/utils/';
+import { ExternalHooks } from '@/ExternalHooks';
+import * as Db from '@/Db';
+import type { User } from '@/databases/entities/User';
+import { InternalHooks } from '@/InternalHooks';
+import { createOwner, createMember } from './shared/db/users';
 
 const { any } = expect;
 
 const testServer = setupTestServer({ endpointGroups: ['users'] });
+
+const externalHooks = utils.mockInstance(ExternalHooks);
+const mailer = utils.mockInstance(UserManagementMailer, { isEmailSetUp: true });
+const internalHooks = utils.mockInstance(InternalHooks);
 
 let owner: User;
 let member: User;
@@ -15,6 +27,7 @@ beforeEach(async () => {
 	await testDb.truncate(['User']);
 	owner = await createOwner();
 	member = await createMember();
+	externalHooks.run.mockReset();
 });
 
 const validatePublicUser = (user: PublicUser) => {
@@ -245,5 +258,121 @@ describe('GET /users', () => {
 
 			expect(response.body).toEqual({ data: [{ firstName: any(String) }] });
 		});
+	});
+});
+
+describe('POST /users', () => {
+	test('should error due to invalid input', async () => {
+		const INVALID_INPUTS = [[{}], { email: randomString(1, 10) }];
+
+		for (const [_, invalidInput] of INVALID_INPUTS.entries()) {
+			const response = await testServer
+				.authAgentFor(owner)
+				.post('/users')
+				.send(invalidInput)
+				.expect(400);
+
+			expect(response.body?.code).toBeDefined();
+			expect(response.body?.message).toBeDefined();
+			expect(response.body?.code).toBe(400);
+		}
+	});
+
+	test('should fail due to SAML license enabled', async () => {
+		jest.spyOn(samlHelpers, 'isSamlLicensedAndEnabled').mockImplementation(() => true);
+
+		const response = await testServer
+			.authAgentFor(owner)
+			.post('/users')
+			.send([{ email: randomEmail() }])
+			.expect(400);
+
+		expect(response.body?.code).toBeDefined();
+		expect(response.body?.message).toBeDefined();
+		expect(response.body?.code).toBe(400);
+	});
+
+	test('should fail due to license max users restriction', async () => {
+		jest.spyOn(samlHelpers, 'isSamlLicensedAndEnabled').mockImplementation(() => true);
+
+		const response = await testServer
+			.authAgentFor(owner)
+			.post('/users')
+			.send([{ email: randomEmail() }])
+			.expect(400);
+
+		expect(response.body?.code).toBeDefined();
+		expect(response.body?.message).toBeDefined();
+		expect(response.body?.code).toBe(400);
+	});
+
+	test('should fail due to not owner setup', async () => {
+		config.set('userManagement.isInstanceOwnerSetUp', false);
+
+		const response = await testServer
+			.authAgentFor(owner)
+			.post('/users')
+			.send([{ email: randomEmail() }])
+			.expect(400);
+
+		expect(response.body?.code).toBeDefined();
+		expect(response.body?.message).toBeDefined();
+		expect(response.body?.code).toBe(400);
+	});
+
+	//TODO - check that members cannot  use the invitation endpoint
+
+	test.only('should invite member and "send" email', async () => {
+		const email = randomEmail();
+
+		jest.spyOn(mailer, 'invite').mockImplementation(async (data) => {
+			return {
+				emailSent: true,
+			};
+		});
+
+		const response = await testServer
+			.authAgentFor(owner)
+			.post('/users')
+			.send([{ email }])
+			.expect(200);
+
+		const user = await Db.collections.User.findOneByOrFail({ email });
+
+		expect(internalHooks.onUserTransactionalEmail).toHaveBeenCalledTimes(1);
+		const [onUserTransactionalEmailParameter] =
+			internalHooks.onUserTransactionalEmail.mock.calls[0];
+		expect(onUserTransactionalEmailParameter.user_id).toBe(user.id);
+		expect(onUserTransactionalEmailParameter.message_type).toBe('New user invite');
+		expect(onUserTransactionalEmailParameter.public_api).toBe(false);
+
+		expect(internalHooks.onUserInvite).toHaveBeenCalledTimes(1);
+		const [onUserInviteParameter] = internalHooks.onUserInvite.mock.calls[0];
+		expect(onUserInviteParameter.user.id).toBe(owner.id);
+		expect(onUserInviteParameter.target_user_id[0]).toBe(user.id);
+		expect(onUserInviteParameter.public_api).toBe(false);
+		expect(onUserInviteParameter.email_sent).toBe(true);
+
+		expect(externalHooks.run).toHaveBeenCalledTimes(1);
+		const [hookName, hookData] = externalHooks.run.mock.calls[0];
+		expect(hookName).toBe('user.invited');
+		expect(hookData[0][0]).toBe(email);
+
+		expect(user.isOwner).toBe(false);
+		expect(user.isPending).toBe(true);
+		expect(user.firstName).toBe(null);
+		expect(user.lastName).toBe(null);
+		expect(user.password).toBe(null);
+
+		expect(response.body?.data).toBeDefined();
+		expect(response.body?.data).toBeInstanceOf(Array);
+		expect(response.body?.data[0]?.user?.id).toBe(user.id);
+		expect(response.body?.data[0]?.user?.email).toBe(user.email);
+		expect(response.body?.data[0]?.user?.emailSent).toBe(true);
+
+		const inviteUrl = new URL(response.body?.data[0]?.user?.inviteAcceptUrl);
+
+		expect(inviteUrl.searchParams.get('inviterId')).toBe(owner.id);
+		expect(inviteUrl.searchParams.get('inviteeId')).toBe(user.id);
 	});
 });
