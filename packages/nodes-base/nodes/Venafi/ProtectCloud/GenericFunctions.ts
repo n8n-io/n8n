a08fca51d928b7bfb7c0081287a38274048892bb
Ricardo Espinoza <ricardo@n8n.io>
2023-11-11 10:35:56 -0500

    feat(Venafi TLS Protect Cloud Node): Add region parameter to Venafi protect cloud (#7689)
    Github issue / Community forum post (link here to close automatically):

--- packages/nodes-base/nodes/Venafi/ProtectCloud/GenericFunctions.ts
+++ packages/nodes-base/nodes/Venafi/ProtectCloud/GenericFunctions.ts
@@ -23,6 +23,9 @@ export async function venafiApiRequest(
 	option: IDataObject = {},
 ): Promise<any> {
 	const operation = this.getNodeParameter('operation', 0);
+	const credentials = await this.getCredentials('venafiTlsProtectCloudApi');
+
+	const region = credentials.region ?? 'cloud';
 
 	const options: OptionsWithUri = {
 		headers: {
@@ -32,7 +35,7 @@ export async function venafiApiRequest(
 		method,
 		body,
 		qs,
-		uri: `https://api.venafi.cloud${resource}`,
+		uri: `https://api.venafi.${region}${resource}`,
 		json: true,
 	};
 
