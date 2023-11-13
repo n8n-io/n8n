f6fb0e7ffd0f42acef96d11d804ddc22baca6a85
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-11-13 11:51:32 +0100

    fix: Update vitest workspaces

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,3 @@
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace(['packages/**/vite.config.ts']);
+export default defineWorkspace(['packages/design-system', 'packages/editor-ui']);
