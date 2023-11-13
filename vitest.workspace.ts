91c5a446809bf7df9de44e3e8b22b7f4b2e13596
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-11-13 12:15:53 +0100

    fix: Use glob pattern in vitest workspaces

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,3 @@
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace(['packages/design-system', 'packages/editor-ui']);
+export default defineWorkspace(['packages/{design-system,editor-ui}']);
