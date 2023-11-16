0dd3bdcf069257640969f90a839f91dbab68ce55
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-11-16 22:32:12 +0100

    fix: Turning off more rules

--- packages/@n8n_io/eslint-config/frontend.js
+++ packages/@n8n_io/eslint-config/frontend.js
@@ -41,6 +41,7 @@ module.exports = {
 		'@typescript-eslint/no-unsafe-call': 'off',
 		'@typescript-eslint/no-unsafe-assignment': 'off',
 		'@typescript-eslint/no-unsafe-argument': 'off',
+		'@typescript-eslint/no-unsafe-return': 'off',
 		'@typescript-eslint/restrict-template-expressions': 'off',
 		'@typescript-eslint/unbound-method': 'off',
 
