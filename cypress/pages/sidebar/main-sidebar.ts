b2e5653f46a61a5e3110c95da3c0fc599f3d53f2
ricardo <ricardoespinoza105@gmail.com>
2023-11-16 16:47:43 -0500

    Add small e2e test to assert "Admin Panel" button shows on cloud deployment

--- cypress/pages/sidebar/main-sidebar.ts
+++ cypress/pages/sidebar/main-sidebar.ts
@@ -12,6 +12,7 @@ export class MainSidebar extends BasePage {
 		workflows: () => this.getters.menuItem('Workflows'),
 		credentials: () => this.getters.menuItem('Credentials'),
 		executions: () => this.getters.menuItem('Executions'),
+		adminPanel: () => this.getters.menuItem('Admin Panel'),
 		userMenu: () => cy.get('div[class="action-dropdown-container"]'),
 		logo: () => cy.getByTestId('n8n-logo'),
 	};
