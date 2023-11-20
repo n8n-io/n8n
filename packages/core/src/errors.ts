062cb861d608346c46c7d4b593e63d800e45a064
Iván Ovejero <ivov.src@gmail.com>
2023-11-20 09:59:56 +0100

    refactor(core): Consolidate path-related errors in Sentry (no-changelog)

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -1,6 +1,6 @@
 export class FileNotFoundError extends Error {
 	constructor(readonly filePath: string) {
-		super(`File not found: ${filePath}`);
+		super('File not found', { cause: { filePath } });
 	}
 }
 
@@ -8,6 +8,6 @@ export class BinaryFileNotFoundError extends FileNotFoundError {}
 
 export class InvalidPathError extends Error {
 	constructor(readonly filePath: string) {
-		super(`Invalid path detected: ${filePath}`);
+		super('Invalid path detected', { cause: { filePath } });
 	}
 }
