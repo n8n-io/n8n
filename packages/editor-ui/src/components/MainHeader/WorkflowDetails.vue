a6843e4a2466b0f44cb1fb74e32c8d070423ee40
Michael Auerswald <michael.auerswald@gmail.com>
2023-11-23 13:05:22 +0100

    remove extra if-else

--- packages/editor-ui/src/components/MainHeader/WorkflowDetails.vue
+++ packages/editor-ui/src/components/MainHeader/WorkflowDetails.vue
@@ -587,21 +587,17 @@ export default defineComponent({
 							data: { eventBus: this.eventBus, status },
 						});
 					} catch (error) {
-						if ('message' in error) {
-							// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-							switch (error.message) {
-								case 'source_control_not_connected':
-									this.showError(
-										{ ...error, message: '' },
-										this.$locale.baseText('settings.sourceControl.error.not.connected.title'),
-										this.$locale.baseText('settings.sourceControl.error.not.connected.message'),
-									);
-									break;
-								default:
-									this.showError(error, this.$locale.baseText('error'));
-							}
-						} else {
-							this.showError(error, this.$locale.baseText('error'));
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+						switch (error.message) {
+							case 'source_control_not_connected':
+								this.showError(
+									{ ...error, message: '' },
+									this.$locale.baseText('settings.sourceControl.error.not.connected.title'),
+									this.$locale.baseText('settings.sourceControl.error.not.connected.message'),
+								);
+								break;
+							default:
+								this.showError(error, this.$locale.baseText('error'));
 						}
 					} finally {
 						this.stopLoading();
