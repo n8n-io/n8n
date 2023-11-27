5247ef16b5134b033f516af8aedbc74975be80dd
Val <68596159+valya@users.noreply.github.com>
2023-11-27 12:27:04 +0000

    use licensed decorator on variables controller

--- packages/cli/src/environments/variables/variables.controller.ee.ts
+++ packages/cli/src/environments/variables/variables.controller.ee.ts
@@ -2,7 +2,7 @@ import { Container, Service } from 'typedi';
 
 import * as ResponseHelper from '@/ResponseHelper';
 import { VariablesRequest } from '@/requests';
-import { Authorized, Delete, Get, Patch, Post, RestController } from '@/decorators';
+import { Authorized, Delete, Get, Licensed, Patch, Post, RestController } from '@/decorators';
 import {
 	VariablesService,
 	VariablesLicenseError,
@@ -10,31 +10,20 @@ import {
 } from './variables.service.ee';
 import { isVariablesEnabled } from './enviromentHelpers';
 import { Logger } from '@/Logger';
-import type { RequestHandler } from 'express';
-
-const variablesLicensedMiddleware: RequestHandler = (req, res, next) => {
-	if (isVariablesEnabled()) {
-		next();
-	} else {
-		res.status(403).json({ status: 'error', message: 'Unauthorized' });
-	}
-};
 
 @Service()
 @Authorized()
 @RestController('/variables')
 export class VariablesController {
-	constructor(
-		private variablesService: VariablesService,
-		private logger: Logger,
-	) {}
+	constructor(private variablesService: VariablesService, private logger: Logger) {}
 
 	@Get('/')
 	async getVariables() {
 		return Container.get(VariablesService).getAllCached();
 	}
 
-	@Post('/', { middlewares: [variablesLicensedMiddleware] })
+	@Post('/')
+	@Licensed('feat:variables')
 	async createVariable(req: VariablesRequest.Create) {
 		if (req.user.globalRole.name !== 'owner') {
 			this.logger.info('Attempt to update a variable blocked due to lack of permissions', {
@@ -66,7 +55,8 @@ export class VariablesController {
 		return variable;
 	}
 
-	@Patch('/:id', { middlewares: [variablesLicensedMiddleware] })
+	@Patch('/:id')
+	@Licensed('feat:variables')
 	async updateVariable(req: VariablesRequest.Update) {
 		const id = req.params.id;
 		if (req.user.globalRole.name !== 'owner') {
