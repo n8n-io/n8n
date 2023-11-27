1f2be47d8dffabd501140d65f7bf9ec60144d1d7
Csaba Tuncsik <csaba@n8n.io>
2023-11-27 09:11:52 +0100

    build: Upgrade lint related packages (#7790)

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -24,7 +24,7 @@ import { eventBus } from '@/eventbus';
 import { BaseCommand } from './BaseCommand';
 import { InternalHooks } from '@/InternalHooks';
 import { License, FeatureNotLicensedError } from '@/License';
-import { IConfig } from '@oclif/config';
+import type { IConfig } from '@oclif/config';
 import { SingleMainSetup } from '@/services/orchestration/main/SingleMainSetup';
 import { OrchestrationHandlerMainService } from '@/services/orchestration/main/orchestration.handler.main.service';
 import { PruningService } from '@/services/pruning.service';
