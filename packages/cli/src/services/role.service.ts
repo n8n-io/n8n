2356fb0f0c247271ffa00d1cf25460e06212f1c4
Iván Ovejero <ivov.src@gmail.com>
2023-11-28 14:16:47 +0100

    feat(core): Set up endpoint for all existing roles with license flag (#7834)
    https://linear.app/n8n/issue/PAY-1034/create-endpoint-to-list-all-existing-roles

--- packages/cli/src/services/role.service.ts
+++ packages/cli/src/services/role.service.ts
@@ -56,6 +56,10 @@ export class RoleService {
 		{ scope: 'workflow', name: 'editor' },
 	];
 
+	listRoles() {
+		return this.roles;
+	}
+
 	private isValid(scope: RoleScopes, name: RoleNames) {
 		return this.roles.some((r) => r.scope === scope && r.name === name);
 	}
