e82c3339d01ed24ee8c9fdbdba27055940890ef1
Omar Ajoue <krynble@gmail.com>
2023-12-06 11:40:27 +0000

    refactor: Add node information when available for error workflow

--- packages/cli/src/WorkflowHelpers.ts
+++ packages/cli/src/WorkflowHelpers.ts
@@ -173,9 +173,13 @@ export async function executeErrorWorkflow(
 		});
 
 		try {
+			const failedNode = workflowErrorData.execution?.lastNodeExecuted
+				? workflowInstance.getNode(workflowErrorData.execution?.lastNodeExecuted)
+				: undefined;
 			await PermissionChecker.checkSubworkflowExecutePolicy(
 				workflowInstance,
 				workflowErrorData.workflow.id!,
+				failedNode ?? undefined,
 			);
 		} catch (error) {
 			const initialNode = workflowInstance.getStartNode();
