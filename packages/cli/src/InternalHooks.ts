1f2e13f1e0e49f12c9213d247f1c38d32308a047
Iván Ovejero <ivov.src@gmail.com>
2023-12-08 16:09:56 +0100

    refactor(core): Add telemetry for RBAC roles

--- packages/cli/src/InternalHooks.ts
+++ packages/cli/src/InternalHooks.ts
@@ -498,6 +498,7 @@ export class InternalHooks implements IInternalHooksClass {
 		target_user_id: string[];
 		public_api: boolean;
 		email_sent: boolean;
+		invitee_role: string;
 	}): Promise<void> {
 		void Promise.all([
 			eventBus.sendAuditEvent({
@@ -507,13 +508,17 @@ export class InternalHooks implements IInternalHooksClass {
 					targetUserId: userInviteData.target_user_id,
 				},
 			}),
-			this.telemetry.track('User invited new user', {
-				user_id: userInviteData.user.id,
-				target_user_id: userInviteData.target_user_id,
-				public_api: userInviteData.public_api,
-				email_sent: userInviteData.email_sent,
-			}),
+			this.telemetry.track('User invited new user', userInviteData),
 		]);
+	}
+
+	async onUserRoleChange(userRoleChangeData: {
+		user: User;
+		target_user_id: string;
+		public_api: boolean;
+		target_user_new_role: string;
+	}) {
+		void this.telemetry.track('User changed role', userRoleChangeData);
 	}
 
 	async onUserReinvite(userReinviteData: {
