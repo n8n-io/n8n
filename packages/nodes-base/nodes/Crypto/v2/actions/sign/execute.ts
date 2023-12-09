6644526bbab80acb29c293b39e944909f57c39f5
Hrishikesh <h8hjoshi@gmail.com>
2023-12-09 12:33:54 +1100

    Format key retrieved from credentials before for signing

--- packages/nodes-base/nodes/Crypto/v2/actions/sign/execute.ts
+++ packages/nodes-base/nodes/Crypto/v2/actions/sign/execute.ts
@@ -6,10 +6,11 @@ export async function execute(this: IExecuteFunctions, index: number) {
 	const algorithm = this.getNodeParameter('algorithm', index) as string;
 	const encoding = this.getNodeParameter('encoding', index) as BinaryToTextEncoding;
 	const privateKey = (await this.getCredentials('crypto', index)).privateKey as string;
+	const formattedKey = privateKey.replace(/\\n/g, '\n');
 	const value = this.getNodeParameter('value', index, '') as string;
 
 	const sign = createSign(algorithm);
 	sign.write(value);
 	sign.end();
-	return sign.sign(privateKey, encoding);
+	return sign.sign(formattedKey, encoding);
 }
