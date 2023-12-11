c378f60a25b0046aef133c244c7dd450a21c88db
Iván Ovejero <ivov.src@gmail.com>
2023-12-11 18:23:42 +0100

    refactor(core): Introduce password utility (no-changelog) (#7979)
    ## Summary
    Provide details about your pull request and what it adds, fixes, or
    changes. Photos and videos are recommended.
    Continue breaking down `UserManagementHelper.ts`
    ...
    
    #### How to test the change:
    1. ...
    
    
    ## Issues fixed
    Include links to Github issue or Community forum post or **Linear
    ticket**:
    > Important in order to close automatically and provide context to
    reviewers
    
    ...
    
    
    ## Review / Merge checklist
    - [ ] PR title and summary are descriptive. **Remember, the title
    automatically goes into the changelog. Use `(no-changelog)` otherwise.**
    ([conventions](https://github.com/n8n-io/n8n/blob/master/.github/pull_request_title_conventions.md))
    - [ ] [Docs updated](https://github.com/n8n-io/n8n-docs) or follow-up
    ticket created.
    - [ ] Tests included.
    > A bug is not considered fixed, unless a test is added to prevent it
    from happening again. A feature is not complete without tests.
      >
    > *(internal)* You can use Slack commands to trigger [e2e
    tests](https://www.notion.so/n8n/How-to-use-Test-Instances-d65f49dfc51f441ea44367fb6f67eb0a?pvs=4#a39f9e5ba64a48b58a71d81c837e8227)
    or [deploy test
    instance](https://www.notion.so/n8n/How-to-use-Test-Instances-d65f49dfc51f441ea44367fb6f67eb0a?pvs=4#f6a177d32bde4b57ae2da0b8e454bfce)
    or [deploy early access version on
    Cloud](https://www.notion.so/n8n/Cloudbot-3dbe779836004972b7057bc989526998?pvs=4#fef2d36ab02247e1a0f65a74f6fb534e).

--- packages/cli/src/sso/saml/samlHelpers.ts
+++ packages/cli/src/sso/saml/samlHelpers.ts
@@ -3,7 +3,7 @@ import config from '@/config';
 import { AuthIdentity } from '@db/entities/AuthIdentity';
 import { User } from '@db/entities/User';
 import { License } from '@/License';
-import { hashPassword } from '@/UserManagement/UserManagementHelper';
+import { PasswordUtility } from '@/services/password.utility';
 import type { SamlPreferences } from './types/samlPreferences';
 import type { SamlUserAttributes } from './types/samlUserAttributes';
 import type { FlowResult } from 'samlify/types/src/flow';
@@ -106,7 +106,7 @@ export async function createUserFromSamlAttributes(attributes: SamlUserAttribute
 	user.lastName = attributes.lastName;
 	user.globalRole = await Container.get(RoleService).findGlobalMemberRole();
 	// generates a password that is not used or known to the user
-	user.password = await hashPassword(generatePassword());
+	user.password = await Container.get(PasswordUtility).hash(generatePassword());
 	authIdentity.providerId = attributes.userPrincipalName;
 	authIdentity.providerType = 'saml';
 	authIdentity.user = user;
