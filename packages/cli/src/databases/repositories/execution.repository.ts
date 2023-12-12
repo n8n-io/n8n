06c4c6ee0e8dfe73e29edfe2279f35254f7ec26a
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-12-12 11:14:04 +0100

    refactor(core): Don't use DB transactions on ExecutionRepository.createNewExecution (no-changelog)

--- packages/cli/src/databases/repositories/execution.repository.ts
+++ packages/cli/src/databases/repositories/execution.repository.ts
@@ -25,7 +25,7 @@ import type {
 import config from '@/config';
 import type { IGetExecutionsQueryFilter } from '@/executions/executions.service';
 import { isAdvancedExecutionFiltersEnabled } from '@/executions/executionHelpers';
-import type { ExecutionData } from '../entities/ExecutionData';
+import { ExecutionData } from '../entities/ExecutionData';
 import { ExecutionEntity } from '../entities/ExecutionEntity';
 import { ExecutionMetadata } from '../entities/ExecutionMetadata';
 import { ExecutionDataRepository } from './executionData.repository';
@@ -213,17 +213,30 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 		return rest;
 	}
 
-	async createNewExecution(execution: ExecutionPayload) {
+	async createNewExecution(execution: ExecutionPayload): Promise<string> {
 		const { data, workflowData, ...rest } = execution;
+		const { identifiers: inserted } = await this.manager
+			.createQueryBuilder()
+			.insert()
+			.into(ExecutionEntity)
+			.values([rest])
+			.execute();
 
-		const newExecution = await this.save(rest);
-		await this.executionDataRepository.save({
-			execution: newExecution,
-			workflowData,
-			data: stringify(data),
-		});
-
-		return newExecution;
+		const { id: executionId } = inserted[0] as { id: string };
+		const { connections, nodes, name } = workflowData ?? {};
+		await this.manager
+			.createQueryBuilder()
+			.insert()
+			.into(ExecutionData)
+			.values([
+				{
+					executionId,
+					workflowData: { connections, nodes, name },
+					data: stringify(data),
+				},
+			])
+			.execute();
+		return executionId;
 	}
 
 	async markAsCrashed(executionIds: string[]) {
