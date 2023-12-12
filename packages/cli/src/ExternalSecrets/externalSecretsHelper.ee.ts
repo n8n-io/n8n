7c0f760790fc0476e090511ef6adbe939da86b6e
Jon <jonathan.bennetts@gmail.com>
2023-12-12 14:22:14 +0000

    feat: Add config option for external secret update interval (#7995)
    ## Summary
    Adds `N8N_EXTERNAL_SECRETS_UPDATE_INTERVAL` to allow enterprise users to
    tweak the update internal for importing new secrets.
    
    If using a config file the value is:
    
    ```
    "externalSecrets": {
      "updateInterval": 300
    }
    ```
    
    #### How to test the change:
    1. Run as normal and check that the secret is updated every 5 minutes
    2. Set `N8N_EXTERNAL_SECRETS_UPDATE_INTERVAL` to 10
    3. Check the secret is reloaded after 10 seconds
    
    
    ## Review / Merge checklist
    - [x] PR title and summary are descriptive. **Remember, the title
    automatically goes into the changelog. Use `(no-changelog)` otherwise.**
    ([conventions](https://github.com/n8n-io/n8n/blob/master/.github/pull_request_title_conventions.md))
    - [x] [Docs updated](https://github.com/n8n-io/n8n-docs) or follow-up
    ticket created.

--- packages/cli/src/ExternalSecrets/externalSecretsHelper.ee.ts
+++ packages/cli/src/ExternalSecrets/externalSecretsHelper.ee.ts
@@ -1,5 +1,8 @@
 import { License } from '@/License';
+import config from '@/config';
 import Container from 'typedi';
+
+export const updateIntervalTime = () => config.getEnv('externalSecrets.updateInterval') * 1000;
 
 export function isExternalSecretsEnabled() {
 	const license = Container.get(License);
