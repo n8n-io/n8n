67c495ea350b8c5940d22c491960f6830e99fbdd
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-12-13 16:44:34 +0100

    test: Remove '.only'

--- cypress/e2e/17-sharing.cy.ts
+++ cypress/e2e/17-sharing.cy.ts
@@ -131,7 +131,7 @@ describe('Sharing', { disableAutoLogin: true }, () => {
 		credentialsModal.getters.testSuccessTag().should('be.visible');
 	});
 
-	it.only('should work for admin role on credentials created by others (also can share it with themselves)', () => {
+	it('should work for admin role on credentials created by others (also can share it with themselves)', () => {
 		cy.signin(INSTANCE_MEMBERS[0]);
 
 		cy.visit(credentialsPage.url);
