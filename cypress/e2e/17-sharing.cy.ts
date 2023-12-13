bb2124b9c78b400f83f5b9d15ac4ca2d5765a2e7
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2023-12-13 16:41:52 +0100

    test: Extend e2e test for credential sharing as admin

--- cypress/e2e/17-sharing.cy.ts
+++ cypress/e2e/17-sharing.cy.ts
@@ -150,6 +150,9 @@ describe('Sharing', { disableAutoLogin: true }, () => {
 		credentialsModal.getters.testSuccessTag().should('be.visible');
 		cy.get('input').should('not.have.length');
 		credentialsModal.actions.changeTab('Sharing');
+		cy.contains(
+			'You can view this credential because you have permission to read and share',
+		).should('be.visible');
 
 		credentialsModal.getters.usersSelect().click();
 		cy.getByTestId('user-email')
