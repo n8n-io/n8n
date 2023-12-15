1b92abbb071462fc0880e3728d3ccb8665c0a723
Omar Ajoue <krynble@gmail.com>
2023-12-15 15:49:25 +0000

    refactor: Hardcode opening personalization modal

--- packages/editor-ui/src/stores/ui.store.ts
+++ packages/editor-ui/src/stores/ui.store.ts
@@ -114,7 +114,7 @@ export const useUIStore = defineStore(STORES.UI, {
 				open: false,
 			},
 			[PERSONALIZATION_MODAL_KEY]: {
-				open: false,
+				open: true,
 			},
 			[INVITE_USER_MODAL_KEY]: {
 				open: false,
