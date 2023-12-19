1bf92bf463c641b426d9a2bce4bc04469684fb8b
Milorad FIlipović <milorad@n8n.io>
2023-12-19 15:10:03 +0100

    feat(editor): Add lead enrichment suggestions to workflow list (#8042)
    ## Summary
    We want to show lead enrichment template suggestions to cloud users that
    agreed to this. This PR introduces the front-end part of this feature
    - Handoff document
    - Figma Hi-fi
    - [How to
    test](https://linear.app/n8n/issue/ADO-1549/[n8n-fe]-update-workflows-list-page-to-show-fake-door-templates#comment-b6644c99)
    
    Tests are being worked on in a separate PR
    
    ## Related tickets and issues
    Fixes ADO-1546
    Fixes ADO-1549
    Fixes ADO-1604
    
    ## Review / Merge checklist
    - [ ] PR title and summary are descriptive. **Remember, the title
    automatically goes into the changelog. Use `(no-changelog)` otherwise.**
    ([conventions](https://github.com/n8n-io/n8n/blob/master/.github/pull_request_title_conventions.md))
    - [ ] [Docs updated](https://github.com/n8n-io/n8n-docs) or follow-up
    ticket created.
    - [ ] Tests included.
    > A bug is not considered fixed, unless a test is added to prevent it
    from happening again.
       > A feature is not complete without tests.
    
    ---------
    
    Co-authored-by: ricardo <ricardoespinoza105@gmail.com>

--- packages/editor-ui/src/components/WorkflowPreview.vue
+++ packages/editor-ui/src/components/WorkflowPreview.vue
@@ -38,12 +38,14 @@ const props = withDefaults(
 		executionMode?: string;
 		loaderType?: 'image' | 'spinner';
 		canOpenNDV?: boolean;
+		hideNodeIssues?: boolean;
 	}>(),
 	{
 		loading: false,
 		mode: 'workflow',
 		loaderType: 'image',
 		canOpenNDV: true,
+		hideNodeIssues: false,
 	},
 );
 
@@ -85,6 +87,7 @@ const loadWorkflow = () => {
 				command: 'openWorkflow',
 				workflow: props.workflow,
 				canOpenNDV: props.canOpenNDV,
+				hideNodeIssues: props.hideNodeIssues,
 			}),
 			'*',
 		);
