180746c00e8ee1def857dde4bc385f677e546531
Alex Grozav <alex@grozav.com>
2023-12-20 12:23:59 +0200

    fix: intercept and mock versions response

--- cypress/e2e/36-versions.cy.ts
+++ cypress/e2e/36-versions.cy.ts
@@ -3,6 +3,7 @@ import { WorkflowsPage } from '../pages/workflows';
 import {
 	closeVersionUpdatesPanel,
 	getVersionCard,
+	getVersionUpdatesPanelOpenButton,
 	openVersionUpdatesPanel,
 } from '../composables/versions';
 
@@ -26,13 +27,40 @@ describe('Versions', () => {
 				}
 			});
 		}).as('settings');
+
+		cy.intercept('GET', 'https://api.n8n.io/api/versions/1.0.0', [
+			{
+				name: '1.3.1',
+				createdAt: '2023-08-18T11:53:12.857Z',
+				hasSecurityIssue: null,
+				hasSecurityFix: null,
+				securityIssueFixVersion: null,
+				hasBreakingChange: null,
+				documentationUrl: 'https://docs.n8n.io/release-notes/#n8n131',
+				nodes: [],
+				description: 'Includes <strong>bug fixes</strong>',
+			},
+			{
+				name: '1.0.5',
+				createdAt: '2023-07-24T10:54:56.097Z',
+				hasSecurityIssue: false,
+				hasSecurityFix: null,
+				securityIssueFixVersion: null,
+				hasBreakingChange: true,
+				documentationUrl: 'https://docs.n8n.io/release-notes/#n8n104',
+				nodes: [],
+				description: 'Includes <strong>core functionality</strong> and <strong>bug fixes</strong>',
+			},
+		]);
+
 		cy.signin(INSTANCE_OWNER);
 
 		cy.visit(workflowsPage.url);
 		cy.wait('@settings');
 
+		getVersionUpdatesPanelOpenButton().should('contain', '2 updates');
 		openVersionUpdatesPanel();
-		getVersionCard().should('have.length.greaterThan', 10);
+		getVersionCard().should('have.length', 2);
 		closeVersionUpdatesPanel();
 	});
 });
