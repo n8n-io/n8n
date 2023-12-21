fc5ee8fe8d1eed272ee4c56c3a4c47883aef4932
Iván Ovejero <ivov.src@gmail.com>
2023-12-21 18:27:07 +0100

    refactor(core): Move typeorm operators from `EnterpriseWorkflowService` to `WorkflowRepository` (no-changelog)

--- packages/cli/src/databases/repositories/workflow.repository.ts
+++ packages/cli/src/databases/repositories/workflow.repository.ts
@@ -1,7 +1,17 @@
 import { Service } from 'typedi';
-import { DataSource, Repository, type UpdateResult, type FindOptionsWhere } from 'typeorm';
+import {
+	DataSource,
+	Repository,
+	type UpdateResult,
+	type FindOptionsWhere,
+	type DeleteResult,
+	type EntityManager,
+	In,
+	Not,
+} from 'typeorm';
 import config from '@/config';
 import { WorkflowEntity } from '../entities/WorkflowEntity';
+import { SharedWorkflow } from '../entities/SharedWorkflow';
 
 @Service()
 export class WorkflowRepository extends Repository<WorkflowEntity> {
@@ -35,6 +45,29 @@ export class WorkflowRepository extends Repository<WorkflowEntity> {
 			active: true,
 		});
 		return totalTriggerCount ?? 0;
+	}
+
+	async getSharings(
+		transaction: EntityManager,
+		workflowId: string,
+		relations = ['shared'],
+	): Promise<SharedWorkflow[]> {
+		const workflow = await transaction.findOne(WorkflowEntity, {
+			where: { id: workflowId },
+			relations,
+		});
+		return workflow?.shared ?? [];
+	}
+
+	async pruneSharings(
+		transaction: EntityManager,
+		workflowId: string,
+		userIds: string[],
+	): Promise<DeleteResult> {
+		return transaction.delete(SharedWorkflow, {
+			workflowId,
+			userId: Not(In(userIds)),
+		});
 	}
 
 	async updateWorkflowTriggerCount(id: string, triggerCount: number): Promise<UpdateResult> {
