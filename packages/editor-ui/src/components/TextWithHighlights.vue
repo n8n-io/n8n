54eb1b030cfdad958ea5373f67ea0fb7888e0e24
Mutasem Aldmour <mutasem@n8n.io>
2023-12-21 15:52:01 +0100

    fix: fix json mode santizing content for search

---
+++ packages/editor-ui/src/components/TextWithHighlights.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue';
+import { GenericValue } from 'n8n-workflow';
+import { useI18n } from '@/composables/useI18n';
+import { isString } from '@/utils/typeGuards';
+
+const i18n = useI18n();
+
+const props = defineProps({
+    content: {
+        type: [Object, String, Number] as PropType<GenericValue>,
+    },
+    search: {
+        type: String,
+    },
+});
+
+const splitTextBySearch = (text = '', search = ''): {tag: 'span' | 'mark', content: string}[] => {
+	if (!search) {
+		return [
+			{
+				tag: 'span',
+				content: text,
+			}
+		]
+	}
+
+	const pattern = new RegExp(`(${search})`, 'g');
+	const splitText = text.split(new RegExp(pattern, 'gi'));
+
+	return splitText.map((t) => ({ tag: pattern.test(t)? 'mark' : 'span', content: t }));
+}
+
+</script>
+
+<template>
+    <span v-if="props.search && typeof props.content === 'string'">
+        <template v-for="part in splitTextBySearch(props.content, props.search)">
+            <mark v-if="part.tag === 'mark'">{{ part.content }}</mark>
+            <span v-else>{{ part.content }}</span>
+        </template>
+    </span>
+    <span v-else>{{ props.content }}</span>
+</template>
