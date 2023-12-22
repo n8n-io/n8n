40a2e96106506bac368e308f3999855da945ccfb
Iván Ovejero <ivov.src@gmail.com>
2023-12-22 13:35:23 +0100

    refactor(core): Move some `typeorm` operators to repositories (no-changelog) (#8139)
    Moving some persistence logic to repositories to reduce circular
    dependencies.

--- packages/cli/src/databases/repositories/tag.repository.ts
+++ packages/cli/src/databases/repositories/tag.repository.ts
@@ -1,10 +1,17 @@
 import { Service } from 'typedi';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, In, Repository } from 'typeorm';
 import { TagEntity } from '../entities/TagEntity';
 
 @Service()
 export class TagRepository extends Repository<TagEntity> {
 	constructor(dataSource: DataSource) {
 		super(TagEntity, dataSource.manager);
+	}
+
+	async findMany(tagIds: string[]) {
+		return this.find({
+			select: ['id', 'name'],
+			where: { id: In(tagIds) },
+		});
 	}
 }
