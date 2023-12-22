dfc54a9549ffc3f5eb25b90c107856b6c3eccc51
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2023-12-22 16:58:56 +0100

    create a unified Class type

--- packages/cli/test/shared/mocking.ts
+++ packages/cli/test/shared/mocking.ts
@@ -1,11 +1,9 @@
 import { Container } from 'typedi';
 import { mock } from 'jest-mock-extended';
 import type { DeepPartial } from 'ts-essentials';
+import type { Class } from 'n8n-core';
 
-export const mockInstance = <T>(
-	ctor: new (...args: unknown[]) => T,
-	data: DeepPartial<T> | undefined = undefined,
-) => {
+export const mockInstance = <T>(ctor: Class<T>, data: DeepPartial<T> | undefined = undefined) => {
 	const instance = mock<T>(data);
 	Container.set(ctor, instance);
 	return instance;
