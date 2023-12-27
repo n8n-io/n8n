e48e7d8c20be76335c8dfdcf35ca30790380a269
Iván Ovejero <ivov.src@gmail.com>
2023-12-27 17:48:58 +0100

    Add quick test

---
+++ packages/cli/test/unit/repositories/execution.repository.test.ts
@@ -0,0 +1,35 @@
+import { mock } from 'jest-mock-extended';
+import Container from 'typedi';
+import { EntityManager, DataSource } from 'typeorm';
+import { mockInstance } from '../../shared/mocking';
+import { ExecutionRepository } from '@/databases/repositories/execution.repository';
+
+describe('ExecutionRepository', () => {
+	const entityManager = mockInstance(EntityManager);
+	const dataSource = mockInstance(DataSource, { manager: entityManager });
+	dataSource.getMetadata.mockReturnValue(mock());
+	Object.assign(entityManager, { connection: dataSource });
+
+	const executionRepository = Container.get(ExecutionRepository);
+
+	describe('getWaitingExecutions()', () => {
+		test('should', async () => {
+			jest.spyOn(executionRepository, 'findMultipleExecutions').mockResolvedValueOnce([]);
+
+			await executionRepository.getWaitingExecutions();
+
+			const expectedQuery = expect.objectContaining({
+				order: { waitTill: 'ASC' },
+				select: ['id', 'waitTill'],
+				where: expect.objectContaining({
+					waitTill: expect.objectContaining({
+						_type: 'lessThanOrEqual',
+						_value: expect.any(String),
+					}),
+				}),
+			});
+
+			expect(executionRepository.findMultipleExecutions).toHaveBeenCalledWith(expectedQuery);
+		});
+	});
+});
