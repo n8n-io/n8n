fb273912b50e4edfd278b233e81dc6da3e04432f
ricardo <ricardoespinoza105@gmail.com>
2023-12-27 08:11:10 -0500

    update copy

--- packages/nodes-base/nodes/ManualTrigger/ManualTrigger.node.ts
+++ packages/nodes-base/nodes/ManualTrigger/ManualTrigger.node.ts
@@ -25,7 +25,7 @@ export class ManualTrigger implements INodeType {
 		properties: [
 			{
 				displayName:
-					'This node is where a manual workflow execution starts. To make one, go back to the canvas and click ‘execute workflow’',
+					'This node is where a manual workflow execution starts. To make one, go back to the canvas and click test workflow’',
 				name: 'notice',
 				type: 'notice',
 				default: '',
