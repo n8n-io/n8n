30356628c3705150d8fe49c21012a24d5686b7aa
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-01-05 16:13:47 +0200

    fix: Hide setup button if workflow has no credentials

--- packages/editor-ui/src/components/SetupWorkflowCredentialsButton/SetupWorkflowCredentialsButton.vue
+++ packages/editor-ui/src/components/SetupWorkflowCredentialsButton/SetupWorkflowCredentialsButton.vue
@@ -1,12 +1,15 @@
 <script lang="ts" setup>
 import { useI18n } from '@/composables/useI18n';
 import { SETUP_CREDENTIALS_MODAL_KEY, TEMPLATE_CREDENTIAL_SETUP_EXPERIMENT } from '@/constants';
+import { useNodeTypesStore } from '@/stores/nodeTypes.store';
 import { usePostHog } from '@/stores/posthog.store';
 import { useUIStore } from '@/stores/ui.store';
 import { useWorkflowsStore } from '@/stores/workflows.store';
+import { doesNodeHaveCredentialsToFill } from '@/utils/nodes/nodeTransforms';
 import { computed, onBeforeUnmount } from 'vue';
 
 const workflowsStore = useWorkflowsStore();
+const nodeTypesStore = useNodeTypesStore();
 const uiStore = useUIStore();
 const posthogStore = usePostHog();
 const i18n = useI18n();
@@ -14,8 +17,12 @@ const i18n = useI18n();
 const showButton = computed(() => {
 	const isFeatureEnabled = posthogStore.isFeatureEnabled(TEMPLATE_CREDENTIAL_SETUP_EXPERIMENT);
 	const isCreatedFromTemplate = !!workflowsStore.workflow?.meta?.templateId;
+	if (!isFeatureEnabled || !isCreatedFromTemplate) {
+		return false;
+	}
 
-	return isFeatureEnabled && isCreatedFromTemplate;
+	const nodes = workflowsStore.workflow?.nodes ?? [];
+	return nodes.some((node) => doesNodeHaveCredentialsToFill(nodeTypesStore, node));
 });
 
 const handleClick = () => {
