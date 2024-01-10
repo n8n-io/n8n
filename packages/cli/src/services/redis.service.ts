994a8576ac1c1c8e59e86b84b4dd66810e94341c
Iván Ovejero <ivov.src@gmail.com>
2024-01-10 17:08:32 +0100

    refactor(core): Eliminate dead Redis code (no-changelog)

--- packages/cli/src/services/redis.service.ts
+++ packages/cli/src/services/redis.service.ts
@@ -1,10 +1,6 @@
 import { Service } from 'typedi';
 import { RedisServicePubSubSubscriber } from './redis/RedisServicePubSubSubscriber';
 import { RedisServicePubSubPublisher } from './redis/RedisServicePubSubPublisher';
-import { RedisServiceListReceiver } from './redis/RedisServiceListReceiver';
-import { RedisServiceListSender } from './redis/RedisServiceListSender';
-import { RedisServiceStreamConsumer } from './redis/RedisServiceStreamConsumer';
-import { RedisServiceStreamProducer } from './redis/RedisServiceStreamProducer';
 
 /*
  * This is a convenience service that provides access to all the Redis clients.
@@ -14,10 +10,6 @@ export class RedisService {
 	constructor(
 		private redisServicePubSubSubscriber: RedisServicePubSubSubscriber,
 		private redisServicePubSubPublisher: RedisServicePubSubPublisher,
-		private redisServiceListReceiver: RedisServiceListReceiver,
-		private redisServiceListSender: RedisServiceListSender,
-		private redisServiceStreamConsumer: RedisServiceStreamConsumer,
-		private redisServiceStreamProducer: RedisServiceStreamProducer,
 	) {}
 
 	async getPubSubSubscriber() {
@@ -28,25 +20,5 @@ export class RedisService {
 	async getPubSubPublisher() {
 		await this.redisServicePubSubPublisher.init();
 		return this.redisServicePubSubPublisher;
-	}
-
-	async getListSender() {
-		await this.redisServiceListSender.init();
-		return this.redisServiceListSender;
-	}
-
-	async getListReceiver() {
-		await this.redisServiceListReceiver.init();
-		return this.redisServiceListReceiver;
-	}
-
-	async getStreamProducer() {
-		await this.redisServiceStreamProducer.init();
-		return this.redisServiceStreamProducer;
-	}
-
-	async getStreamConsumer() {
-		await this.redisServiceStreamConsumer.init();
-		return this.redisServiceStreamConsumer;
 	}
 }
