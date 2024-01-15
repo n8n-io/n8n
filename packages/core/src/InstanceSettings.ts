ef83c96b82775e142231f3ce4888d8af71db4f9f
Iván Ovejero <ivov.src@gmail.com>
2024-01-15 15:15:24 +0100

    Debugging

--- packages/core/src/InstanceSettings.ts
+++ packages/core/src/InstanceSettings.ts
@@ -75,12 +75,12 @@ export class InstanceSettings {
 
 			const { encryptionKey, tunnelSubdomain } = settings;
 
-			console.log('[file] encryptionKey', encryptionKey);
-			console.log('[env] N8N_ENCRYPTION_KEY', process.env.N8N_ENCRYPTION_KEY);
+			// console.log('[file] encryptionKey', encryptionKey);
+			// console.log('[env] N8N_ENCRYPTION_KEY', process.env.N8N_ENCRYPTION_KEY);
 
 			if (process.env.N8N_ENCRYPTION_KEY && encryptionKey !== process.env.N8N_ENCRYPTION_KEY) {
 				throw new ApplicationError(
-					`The encryption key in the settings file ${this.settingsFile} does not match the one in the N8N_ENCRYPTION_KEY. If specifying this environment variable, please make sure it matches the value in the settings file.`,
+					`The encryption key in the settings file ${this.settingsFile} does not match the one in the N8N_ENCRYPTION_KEY ${process.env.N8N_ENCRYPTION_KEY}. If specifying this environment variable, please make sure it matches the value in the settings file.`,
 				);
 			}
 
