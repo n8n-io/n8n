7e503c294e9c8903c09e5c76339b11b5e3d11078
Iván Ovejero <ivov.src@gmail.com>
2024-01-15 10:40:27 +0100

    refactor(core): Suggestions for PAY-1147 (#8330)

--- packages/cli/test/unit/WaitTracker.test.ts
+++ packages/cli/test/unit/WaitTracker.test.ts
@@ -1,114 +1,83 @@
 import { WaitTracker } from '@/WaitTracker';
 import { mock } from 'jest-mock-extended';
-import { mockInstance } from '../shared/mocking';
-import { OwnershipService } from '@/services/ownership.service';
-import { User } from '@/databases/entities/User';
-import { ExecutionRepository } from '@/databases/repositories/execution.repository';
+import type { ExecutionRepository } from '@/databases/repositories/execution.repository';
 import type { IExecutionResponse } from '@/Interfaces';
-import { Logger } from '@/Logger';
-import Container from 'typedi';
-import { Push } from '@/push';
-import { ActiveExecutions } from '@/ActiveExecutions';
 
 jest.useFakeTimers();
 
-Container.set(Logger, mockInstance(Logger));
-Container.set(Push, mockInstance(Push));
-Container.set(ActiveExecutions, mockInstance(ActiveExecutions));
-
-const workflowOwner = new User();
-workflowOwner.id = '123';
-
-let executionsToReturn: IExecutionResponse[] = [];
-let executionToReturn: IExecutionResponse | undefined;
-
-let startExecutionSpy: jest.SpyInstance;
-
-const executionRepositoryFindSingleExecutionMock = jest.fn(async () => executionToReturn);
-const executionRepositoryGetWaitingExecutionsMock = jest.fn(async () => executionsToReturn);
-const executionRepositoryUpdateExistingExecutionMock = jest.fn();
-
-const ownershipServiceGetWorkflowOwnerCachedMock = jest.fn(async () => {
-	console.log('potato');
-	return workflowOwner;
-});
-
-const fakeExecution: IExecutionResponse = {
-	id: '123',
-	data: { resultData: { runData: {} } },
-	workflowData: {
-		id: '456',
-		name: 'Test workflow',
-		active: true,
-		createdAt: new Date(),
-		updatedAt: new Date(),
-		nodes: [],
-		connections: {},
-	},
-	mode: 'trigger',
-	startedAt: new Date(),
-	finished: false,
-	status: 'new',
-	waitTill: new Date(new Date().getTime() + 1000),
-};
-
 describe('WaitTracker', () => {
-	let waitTracker: WaitTracker;
+	const executionRepository = mock<ExecutionRepository>();
 
-	beforeEach(() => {
-		waitTracker = new WaitTracker(
-			mock(),
-			mockInstance(ExecutionRepository, {
-				// @ts-expect-error
-				// We won't be implementing all overrides here
-				findSingleExecution: executionRepositoryFindSingleExecutionMock,
-				getWaitingExecutions: executionRepositoryGetWaitingExecutionsMock,
-				updateExistingExecution: executionRepositoryUpdateExistingExecutionMock,
-			}),
-			mockInstance(OwnershipService),
-		);
-		startExecutionSpy = jest.spyOn(waitTracker, 'startExecution');
+	const execution = mock<IExecutionResponse>({
+		id: '123',
+		waitTill: new Date(Date.now() + 1000),
 	});
 
 	afterEach(() => {
-		waitTracker.shutdown();
+		jest.clearAllMocks();
 	});
 
-	describe('getWaitingExecutions', () => {
-		it('Should reach out to the database when constructed', async () => {
-			expect(executionRepositoryGetWaitingExecutionsMock).toHaveBeenCalledTimes(1);
+	describe('constructor()', () => {
+		it('should query DB for waiting executions', async () => {
+			executionRepository.getWaitingExecutions.mockResolvedValue([execution]);
+
+			new WaitTracker(mock(), executionRepository, mock());
+
+			expect(executionRepository.getWaitingExecutions).toHaveBeenCalledTimes(1);
 		});
 
-		it('Should do nothing when there are no executions to start', async () => {
-			await waitTracker.getWaitingExecutions();
+		it('if no executions to start, should do nothing', () => {
+			executionRepository.getWaitingExecutions.mockResolvedValue([]);
 
-			expect(executionRepositoryFindSingleExecutionMock).not.toHaveBeenCalled();
+			new WaitTracker(mock(), executionRepository, mock());
+
+			expect(executionRepository.findSingleExecution).not.toHaveBeenCalled();
 		});
 
-		it('Should not call start execution when there is an execution to start but not enough time pass', async () => {
-			executionsToReturn = [fakeExecution];
+		describe('if execution to start', () => {
+			it('if not enough time passed, should not start execution', async () => {
+				executionRepository.getWaitingExecutions.mockResolvedValue([execution]);
+				const waitTracker = new WaitTracker(mock(), executionRepository, mock());
 
-			await waitTracker.getWaitingExecutions();
-			jest.advanceTimersByTime(100);
-			expect(startExecutionSpy).not.toHaveBeenCalled();
-		});
+				executionRepository.getWaitingExecutions.mockResolvedValue([execution]);
+				await waitTracker.getWaitingExecutions();
 
-		it('Should call start execution when there is an execution to start and enough time elapsed', async () => {
-			executionsToReturn = [fakeExecution];
+				const startExecutionSpy = jest.spyOn(waitTracker, 'startExecution');
 
-			await waitTracker.getWaitingExecutions();
-			jest.advanceTimersByTime(20000);
-			expect(startExecutionSpy).toHaveBeenCalledWith('123');
+				jest.advanceTimersByTime(100);
+
+				expect(startExecutionSpy).not.toHaveBeenCalled();
+			});
+
+			it('if enough time passed, should start execution', async () => {
+				executionRepository.getWaitingExecutions.mockResolvedValue([]);
+				const waitTracker = new WaitTracker(mock(), executionRepository, mock());
+
+				executionRepository.getWaitingExecutions.mockResolvedValue([execution]);
+				await waitTracker.getWaitingExecutions();
+
+				const startExecutionSpy = jest.spyOn(waitTracker, 'startExecution');
+
+				jest.advanceTimersByTime(2_000);
+
+				expect(startExecutionSpy).toHaveBeenCalledWith(execution.id);
+			});
 		});
 	});
 
-	describe('startExecution', () => {
-		it('Should fetch execution when called', async () => {
-			executionRepositoryFindSingleExecutionMock.mockClear();
-			executionRepositoryFindSingleExecutionMock.mockResolvedValue(fakeExecution);
-			waitTracker.startExecution('123');
+	describe('startExecution()', () => {
+		it('should query for execution to start', async () => {
+			executionRepository.getWaitingExecutions.mockResolvedValue([]);
+			const waitTracker = new WaitTracker(mock(), executionRepository, mock());
+
+			executionRepository.findSingleExecution.mockResolvedValue(execution);
+			waitTracker.startExecution(execution.id);
 			jest.advanceTimersByTime(5);
-			expect(executionRepositoryFindSingleExecutionMock).toHaveBeenCalled();
+
+			expect(executionRepository.findSingleExecution).toHaveBeenCalledWith(execution.id, {
+				includeData: true,
+				unflattenData: true,
+			});
 		});
 	});
 });
