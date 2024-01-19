7543c9abe2074d7f9da5f77b3d914bef8936d318
Danny Martini <danny@n8n.io>
2024-01-19 15:07:44 +0100

    clean up test
    1. use Time constant for more readable time conversion
    2. use jest-mock-extended to create users and requests
    3. use type guards and jest.fail to remove unecessary assertions

--- packages/cli/test/unit/auth/jwt.test.ts
+++ packages/cli/test/unit/auth/jwt.test.ts
@@ -1,55 +1,45 @@
 import { Container } from 'typedi';
-import { v4 as uuid } from 'uuid';
+import { mock } from 'jest-mock-extended';
 
 import config from '@/config';
 import { JwtService } from '@/services/jwt.service';
 import { License } from '@/License';
-import { User } from '@db/entities/User';
+import { Time } from '@/constants';
 import { issueJWT } from '@/auth/jwt';
 
 import { mockInstance } from '../../shared/mocking';
 
-// TODO: Why do I need to mock the License but not for example the JwtService 🤷
-// A: because of the server not being initialized
+import type { User } from '@db/entities/User';
+
 mockInstance(License);
 
-// TODO: is the the right level of testing?
-// Should I mock more or less?
-// Should I write an integration test that checks that a user is actually logged out?
 describe('jwt.issueJWT', () => {
 	const jwtService = Container.get(JwtService);
 
 	describe('when not setting userManagement.jwtSessionDuration', () => {
 		it('should default to expire in 168 hours', () => {
-			const defaultInSeconds = 7 * 24 * 60 * 60;
-			// TODO: is this the right way to create a mock user?
-			const mockUser = Object.assign(new User(), {
-				id: uuid(),
-				password: 'passwordHash',
-				mfaEnabled: false,
-				mfaSecret: 'test',
-				mfaRecoveryCodes: ['test'],
-				updatedAt: new Date(),
-				authIdentities: [],
-			});
+			const defaultInSeconds = 7 * Time.days.toSeconds;
+			const mockUser = mock<User>({ password: 'passwordHash' });
 			const { token, expiresInSeconds } = issueJWT(mockUser);
 
 			expect(expiresInSeconds).toBe(defaultInSeconds);
 			const decodedToken = jwtService.verify(token);
-			expect(decodedToken.exp).toBeDefined();
-			expect(decodedToken.iat).toBeDefined();
-			expect(decodedToken.exp! - decodedToken.iat!).toBe(defaultInSeconds);
+			if (decodedToken.exp === undefined || decodedToken.iat === undefined) {
+				fail('Expected exp and iat to be defined');
+			}
+
+			expect(decodedToken.exp - decodedToken.iat).toBe(defaultInSeconds);
 		});
 	});
 
 	describe('when setting userManagement.jwtSessionDuration', () => {
 		let oldDuration: number;
-		let testDuration = 1;
+		let testDurationHours = 1;
 
 		beforeEach(() => {
 			mockInstance(License);
 			oldDuration = config.get('userManagement.jwtSessionDurationHours');
-			config.set('userManagement.jwtSessionDurationHours', testDuration);
+			config.set('userManagement.jwtSessionDurationHours', testDurationHours);
 		});
 
 		afterEach(() => {
@@ -57,22 +47,15 @@ describe('jwt.issueJWT', () => {
 		});
 
 		it('should apply it to tokens', () => {
-			const mockUser = Object.assign(new User(), {
-				id: uuid(),
-				password: 'passwordHash',
-				mfaEnabled: false,
-				mfaSecret: 'test',
-				mfaRecoveryCodes: ['test'],
-				updatedAt: new Date(),
-				authIdentities: [],
-			});
+			const mockUser = mock<User>({ password: 'passwordHash' });
 			const { token, expiresInSeconds } = issueJWT(mockUser);
 
-			expect(expiresInSeconds).toBe(testDuration * 60 * 60);
+			expect(expiresInSeconds).toBe(testDurationHours * Time.hours.toSeconds);
 			const decodedToken = jwtService.verify(token);
-			expect(decodedToken.exp).toBeDefined();
-			expect(decodedToken.iat).toBeDefined();
-			expect(decodedToken.exp! - decodedToken.iat!).toBe(testDuration * 60 * 60);
+			if (decodedToken.exp === undefined || decodedToken.iat === undefined) {
+				fail('Expected exp and iat to be defined on decodedToken');
+			}
+			expect(decodedToken.exp - decodedToken.iat).toBe(testDurationHours * Time.hours.toSeconds);
 		});
 	});
 });
