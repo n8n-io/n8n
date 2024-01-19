4e9bd04e62f51b8d50cd823ca33844286165a603
Danny Martini <danny@n8n.io>
2024-01-19 15:07:45 +0100

    make the refresh timeout configurable

--- packages/cli/test/unit/middleware/auth.test.ts
+++ packages/cli/test/unit/middleware/auth.test.ts
@@ -1,42 +1,37 @@
-import { v4 as uuid } from 'uuid';
+import { mock } from 'jest-mock-extended';
 
 import config from '@/config';
-import { AUTH_COOKIE_NAME } from '@/constants';
+import { AUTH_COOKIE_NAME, Time } from '@/constants';
 import { License } from '@/License';
-import { User } from '@/databases/entities/User';
 import { issueJWT } from '@/auth/jwt';
 import { refreshExpiringCookie } from '@/middlewares';
 
 import { mockInstance } from '../../shared/mocking';
 
 import type { AuthenticatedRequest } from '@/requests';
-import type { Response } from 'express';
+import type { NextFunction, Response } from 'express';
+import type { User } from '@/databases/entities/User';
 
 mockInstance(License);
+
+jest.useFakeTimers();
 
 describe('refreshExpiringCookie', () => {
 	const oldDuration: number = config.get('userManagement.jwtSessionDurationHours');
 	let mockUser: User;
 
 	beforeEach(() => {
-		mockUser = Object.assign(new User(), {
-			id: uuid(),
-			password: 'passwordHash',
-			mfaEnabled: false,
-			mfaSecret: 'test',
-			mfaRecoveryCodes: ['test'],
-			updatedAt: new Date(),
-			authIdentities: [],
-		});
+		mockUser = mock<User>({ password: 'passwordHash' });
 	});
 
 	afterEach(() => {
 		config.set('userManagement.jwtSessionDuration', oldDuration);
+		config.set('userManagement.jwtRefreshTimeoutHours', 0);
 	});
 
 	it('does not do anything if the user is not authorized', async () => {
-		const req = {} as AuthenticatedRequest;
-		const res = { cookie: jest.fn() } as unknown as Response;
+		const req = mock<AuthenticatedRequest>();
+		const res = mock<Response>({ cookie: jest.fn() });
 		const next = jest.fn();
 
 		await refreshExpiringCookie(req, res, next);
@@ -45,27 +40,117 @@ describe('refreshExpiringCookie', () => {
 		expect(res.cookie).not.toHaveBeenCalled();
 	});
 
-	it('does not do anything if the cookie is still valid', async () => {
-		config.set('userManagement.jwtSessionDurationHours', 73);
-		const { token } = issueJWT(mockUser);
+	// TODO: structure tests
+	describe('with N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS=-1', () => {
+		it('does not refresh the cookie, ever', async () => {
+			config.set('userManagement.jwtSessionDurationHours', 1);
+			config.set('userManagement.jwtRefreshTimeoutHours', -1);
+			const { token } = issueJWT(mockUser);
 
-		const req = { cookies: { [AUTH_COOKIE_NAME]: token }, user: mockUser } as AuthenticatedRequest;
-		const res = { cookie: jest.fn() } as unknown as Response;
-		const next = jest.fn();
-		await refreshExpiringCookie(req, res, next);
-		expect(next).toHaveBeenCalledTimes(1);
-		expect(res.cookie).not.toHaveBeenCalled();
+			jest.advanceTimersByTime(1000 * 60 * 55); /* 55 minutes */
+
+			const req = mock<AuthenticatedRequest>({
+				cookies: { [AUTH_COOKIE_NAME]: token },
+				user: mockUser,
+			});
+			const res = mock<Response>({ cookie: jest.fn() });
+			const next = jest.fn();
+			await refreshExpiringCookie(req, res, next);
+
+			expect(next).toHaveBeenCalledTimes(1);
+			expect(res.cookie).not.toHaveBeenCalled();
+		});
 	});
 
-	it('refreshes the cookie if it has less than 3 days to live', async () => {
-		config.set('userManagement.jwtSessionDurationHours', 71);
-		const { token } = issueJWT(mockUser);
-		const req = { cookies: { [AUTH_COOKIE_NAME]: token }, user: mockUser } as AuthenticatedRequest;
-		const res = { cookie: jest.fn() } as unknown as Response;
-		const next = jest.fn();
+	describe('with N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS=0', () => {
+		let token: string;
+		let req: AuthenticatedRequest;
+		let res: Response;
+		let next: NextFunction;
 
-		await refreshExpiringCookie(req, res, next);
-		expect(next).toHaveBeenCalledTimes(1);
-		expect(res.cookie).toHaveBeenCalledTimes(1);
+		beforeEach(() => {
+			// ARRANGE
+			config.set('userManagement.jwtSessionDurationHours', 1);
+			config.set('userManagement.jwtRefreshTimeoutHours', 0);
+			token = issueJWT(mockUser).token;
+
+			req = mock<AuthenticatedRequest>({
+				cookies: { [AUTH_COOKIE_NAME]: token },
+				user: mockUser,
+			});
+			res = mock<Response>({ cookie: jest.fn() });
+			next = jest.fn();
+		});
+
+		it('does not refresh the cookie when more than 1/4th of time is left', async () => {
+			// ARRANGE
+			jest.advanceTimersByTime(44 * Time.minutes.toMilliseconds); /* 44 minutes */
+
+			// ACT
+			await refreshExpiringCookie(req, res, next);
+
+			// ASSERT
+			expect(next).toHaveBeenCalledTimes(1);
+			expect(res.cookie).not.toHaveBeenCalled();
+		});
+
+		it('refreshes the cookie when 1/4th of time is left', async () => {
+			// ARRANGE
+			jest.advanceTimersByTime(46 * Time.minutes.toMilliseconds); /* 46 minutes */
+
+			// ACT
+			await refreshExpiringCookie(req, res, next);
+
+			// ASSERT
+			expect(next).toHaveBeenCalledTimes(1);
+			expect(res.cookie).toHaveBeenCalledTimes(1);
+		});
+	});
+
+	describe('with N8N_USER_MANAGEMENT_JWT_REFRESH_TIMEOUT_HOURS=50', () => {
+		let token: string;
+		let req: AuthenticatedRequest;
+		let res: Response;
+		let next: NextFunction;
+
+		// ARRANGE
+		beforeEach(() => {
+			config.set('userManagement.jwtSessionDurationHours', 51);
+			config.set('userManagement.jwtRefreshTimeoutHours', 50);
+
+			token = issueJWT(mockUser).token;
+			req = mock<AuthenticatedRequest>({
+				cookies: { [AUTH_COOKIE_NAME]: token },
+				user: mockUser,
+			});
+			res = mock<Response>({ cookie: jest.fn() });
+			next = jest.fn();
+		});
+
+		it('does not do anything if the cookie is still valid', async () => {
+			// ARRANGE
+			// cookie has 50.5 hours to live: 51 - 0.5
+			jest.advanceTimersByTime(30 * Time.minutes.toMilliseconds);
+
+			// ACT
+			await refreshExpiringCookie(req, res, next);
+
+			// ASSERT
+			expect(next).toHaveBeenCalledTimes(1);
+			expect(res.cookie).not.toHaveBeenCalled();
+		});
+
+		it('refreshes the cookie if it has less than 50 hours to live', async () => {
+			// ARRANGE
+			// cookie has 49.5 hours to live: 51 - 1.5
+			jest.advanceTimersByTime(1.5 * Time.hours.toMilliseconds);
+
+			// ACT
+			await refreshExpiringCookie(req, res, next);
+
+			// ASSERT
+			expect(next).toHaveBeenCalledTimes(1);
+			expect(res.cookie).toHaveBeenCalledTimes(1);
+		});
 	});
 });
