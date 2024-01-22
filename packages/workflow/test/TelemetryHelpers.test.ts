7cd39a6c20447802651abb794718b29d338f66d7
Giulio Andreini <g.andreini@gmail.com>
2024-01-22 09:20:02 +0100

    docs(editor): All buttons to sentence case (no-changelog) (#8399)

--- packages/workflow/test/TelemetryHelpers.test.ts
+++ packages/workflow/test/TelemetryHelpers.test.ts
@@ -665,7 +665,7 @@ describe('generateNodesGraph', () => {
 				{
 					parameters: {},
 					id: 'fe69383c-e418-4f98-9c0e-924deafa7f93',
-					name: 'When clicking "Test Workflow"',
+					name: 'When clicking "Test workflow"',
 					type: 'n8n-nodes-base.manualTrigger',
 					typeVersion: 1,
 					position: [540, 220],
@@ -690,7 +690,7 @@ describe('generateNodesGraph', () => {
 				},
 			],
 			connections: {
-				'When clicking "Test Workflow"': {
+				'When clicking "Test workflow"': {
 					main: [
 						[
 							{
@@ -756,7 +756,7 @@ describe('generateNodesGraph', () => {
 				is_pinned: false,
 			},
 			nameIndices: {
-				'When clicking "Test Workflow"': '0',
+				'When clicking "Test workflow"': '0',
 				Chain: '1',
 				Model: '2',
 			},
