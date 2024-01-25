277170e89327c88b17b2b454cf1255669f54bdcc
Danny Martini <danny@n8n.io>
2024-01-25 21:16:18 +0100

    make sure that the help for commands and topics still shows up

--- packages/cli/src/help.ts
+++ packages/cli/src/help.ts
@@ -1,17 +1,11 @@
-import type { Command } from '@oclif/core';
-import { HelpBase } from '@oclif/core';
+import { Help } from '@oclif/core';
 
 // oclif expects a default export
 // eslint-disable-next-line import/no-default-export
-export default class CustomHelp extends HelpBase {
-	async showHelp(_args: string[]) {
+export default class CustomHelp extends Help {
+	async showRootHelp() {
 		console.log(
 			'You can find up to date information about the CLI here:\nhttps://docs.n8n.io/hosting/cli-commands/',
 		);
-	}
-
-	async showCommandHelp(_command: Command.Loadable) {
-		// This is not used because we have a multi-command CLI, but it needs to
-		// be defined to satisfy the abstract base class.
 	}
 }
