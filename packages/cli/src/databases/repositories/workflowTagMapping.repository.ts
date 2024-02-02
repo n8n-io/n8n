cc50a03463eef3d732c4a93ad34289844bb06a7c
Iván Ovejero <ivov.src@gmail.com>
2024-02-02 12:36:55 +0100

    fix(core): Fix workflow tagging failure due to unique constraint check (#8505)

--- packages/cli/src/databases/repositories/workflowTagMapping.repository.ts
+++ packages/cli/src/databases/repositories/workflowTagMapping.repository.ts
@@ -7,4 +7,14 @@ export class WorkflowTagMappingRepository extends Repository<WorkflowTagMapping>
 	constructor(dataSource: DataSource) {
 		super(WorkflowTagMapping, dataSource.manager);
 	}
+
+	async overwriteTaggings(workflowId: string, tagIds: string[]) {
+		return await this.manager.transaction(async () => {
+			await this.delete({ workflowId });
+
+			const taggings = tagIds.map((tagId) => this.create({ workflowId, tagId }));
+
+			return await this.insert(taggings);
+		});
+	}
 }
