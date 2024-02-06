96f4c11fc89e3e3a1d58b318bf3eb3a414d35246
Nurudeen Soremekun <nurudeen.soremekun@gmail.com>
2024-02-06 14:29:09 +0100

    Update docker-entrypoint.sh to use n8n worker

--- docker/images/n8n-custom/docker-entrypoint.sh
+++ docker/images/n8n-custom/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 if [ "$#" -gt 0 ]; then
-  if [ "$1" = "worker" ]; then
-    n8n worker
-	else
-  	# Got started with arguments
-  	node "$@"
-	fi
+	n8n worker
+ 	#  if [ "$1" = "worker" ]; then
+ 	#    n8n worker
+		# else
+ 	#  	# Got started with arguments
+ 	#  	node "$@"
+		# fi
 else
 	# Run n8n without any additional arguments
 	n8n
