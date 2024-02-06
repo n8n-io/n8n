f0fb3042c329f5da5feed20c18ddffe5b186e748
Nurudeen Soremekun <nurudeen.soremekun@gmail.com>
2024-02-06 14:19:42 +0100

    Update docker-entrypoint.sh

--- docker/images/n8n-custom/docker-entrypoint.sh
+++ docker/images/n8n-custom/docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 if [ "$#" -gt 0 ]; then
-  # Got started with arguments
-  node "$@"
-else
-  # Got started without arguments
   if [ "$1" = "worker" ]; then
-    n8n
-    # Run n8n with worker argument
-    /bin/sh -c "sleep 5; n8n worker"
-  else
-    # Run n8n without any additional arguments
-    n8n
-  fi
+    n8n worker
+	else
+  	# Got started with arguments
+  	node "$@"
+	fi
+else
+	# Run n8n without any additional arguments
+	n8n
 fi
