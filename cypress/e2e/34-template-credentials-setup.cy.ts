0d88551faf49ce7ea0231d583f2b093ce338263d
Milorad Filipovic <milorad@n8n.io>
2024-02-07 12:35:35 +0100

    ✔️ Removing credentials setup tests that use deleted pages

--- cypress/e2e/34-template-credentials-setup.cy.ts
+++ cypress/e2e/34-template-credentials-setup.cy.ts
@@ -36,28 +36,6 @@ describe('Template credentials setup', () => {
 		});
 	});
 
-	it('can be opened from template workflow page', () => {
-		templateWorkflowPage.actions.visit(testTemplate.id);
-		templateCredentialsSetupPage.enableTemplateCredentialSetupFeatureFlag();
-		templateWorkflowPage.getters.useTemplateButton().should('be.visible');
-		templateCredentialsSetupPage.enableTemplateCredentialSetupFeatureFlag();
-		templateWorkflowPage.actions.clickUseThisWorkflowButton();
-
-		templateCredentialsSetupPage.getters
-			.title(`Set up 'Promote new Shopify products on Twitter and Telegram' template`)
-			.should('be.visible');
-	});
-
-	it('can be opened from template collection page', () => {
-		visitTemplateCollectionPage(testData.ecommerceStarterPack);
-		templateCredentialsSetupPage.enableTemplateCredentialSetupFeatureFlag();
-		clickUseWorkflowButtonByTitle('Promote new Shopify products on Twitter and Telegram');
-
-		templateCredentialsSetupPage.getters
-			.title(`Set up 'Promote new Shopify products on Twitter and Telegram' template`)
-			.should('be.visible');
-	});
-
 	it('has all the elements on page', () => {
 		templateCredentialsSetupPage.visitTemplateCredentialSetupPage(testTemplate.id);
 
