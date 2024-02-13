98c93958621bf34954140c1730e6b86a00a8313f
Danny Martini <danny@n8n.io>
2024-02-13 12:42:31 +0100

    use existing helper function instead

--- packages/nodes-base/nodes/Merge/v2/MergeV2.node.ts
+++ packages/nodes-base/nodes/Merge/v2/MergeV2.node.ts
@@ -29,6 +29,7 @@ import {
 } from './GenericFunctions';
 
 import { optionsDescription } from './OptionsDescription';
+import { preparePairedItemDataArray } from '@utils/utilities';
 
 const versionDescription: INodeTypeDescription = {
 	displayName: 'Merge',
@@ -604,31 +605,10 @@ export class MergeV2 implements INodeType {
 					returnData.push.apply(returnData, this.getInputData(1));
 				}
 				if (output === 'empty') {
-					// TODO: was necessary to satisfies types.
-					// I'm wondering if there is already a function that does exactly that? That turns
-					// Array<IPairedItemData | IPairedItemData[] | number | undefined> -> IPairedItemData[]
-					// or
-					// IPairedItemData | IPairedItemData[] | number | undefined -> IPairedItemData | undefined
-					function numberToPairedItem(
-						possiblyPairedItems: Array<INodeExecutionData['pairedItem']>,
-					): IPairedItemData[] {
-						return possiblyPairedItems.reduce((pairedItems: IPairedItemData[], pairedItem) => {
-							if (typeof pairedItem === 'number') {
-								pairedItems.push({ item: pairedItem });
-							} else if (Array.isArray(pairedItem)) {
-								pairedItems.push(...pairedItem);
-							} else if (pairedItem) {
-								pairedItems.push(pairedItem);
-							}
-
-							return pairedItems;
-						}, [] as IPairedItemData[]);
-					}
-
-					const pairedItem = numberToPairedItem([
+					const pairedItem = [
 						...this.getInputData(0).map((inputData) => inputData.pairedItem),
 						...this.getInputData(1).map((inputData) => inputData.pairedItem),
-					]);
+					].flatMap(preparePairedItemDataArray);
 
 					returnData.push({
 						json: {},
