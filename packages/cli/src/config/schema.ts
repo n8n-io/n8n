1b2c11e4d6c2acd206fb9a2013d4b67190820745
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-02-14 15:12:35 +0100

    Update packages/cli/src/config/schema.ts
    Co-authored-by: Omar Ajoue <krynble@gmail.com>

--- packages/cli/src/config/schema.ts
+++ packages/cli/src/config/schema.ts
@@ -236,7 +236,7 @@ export const schema = {
 	executions: {
 		// TODO: remove this and all usage of `executions.process` when we're sure that nobody has this in their config file anymore.
 		process: {
-			doc: 'Own mode has been removed and is only here for backwards compatibility of config files. N8n will use main mode for executions unless `executions.mode` is set to `queue`.',
+			doc: 'Deprecated key, that will be removed in the future. Please remove it from your configuration and environment variables to prevent issues in the future.',
 			format: String,
 			default: '',
 			env: 'EXECUTIONS_PROCESS',
