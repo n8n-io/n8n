078ea5e0321f229a1c1e6dbc98c3595ec10b887c
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-02-15 11:29:44 +0100

    fix: Fix project redirection

--- packages/editor-ui/src/features/projects/views/Projects.vue
+++ packages/editor-ui/src/features/projects/views/Projects.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPathAsRegexPattern } from '@/utils/routeUtils';
 import { oldRoutesToProjectMap } from '@/features/projects/projects-constants';
@@ -7,16 +7,19 @@ import { oldRoutesToProjectMap } from '@/features/projects/projects-constants';
 const router = useRouter();
 const route = useRoute();
 
+const redirectionSuccess = ref(false);
+
 onBeforeMount(async () => {
 	const oldRoutePatterns = Object.keys(oldRoutesToProjectMap).map(getPathAsRegexPattern);
 	if (oldRoutePatterns.some((pattern) => pattern.test(route.path))) {
 		// Get the project id from backend
 		const projectId = 'home';
-		await router.push({ path: `/projects/${projectId}/${route.path}` });
+		await router.replace({ path: `/projects/${projectId}/${route.path}` });
+		redirectionSucccess.value = true;
 	}
 });
 </script>
 
 <template>
-	<div>Projects</div>
+	<router-view v-if="redirectionSuccess" />
 </template>
