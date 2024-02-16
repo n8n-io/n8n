526d1bb26bafb09ff7c3e80fc81f6c0c213573c4
Elias Meire <elias@meire.dev>
2024-02-16 14:32:43 +0100

    fix(editor): Filter component: do not clear expression when changing operator (#8635)

--- packages/editor-ui/src/utils/expressions.ts
+++ packages/editor-ui/src/utils/expressions.ts
@@ -1,6 +1,9 @@
 import { ExpressionParser } from 'n8n-workflow';
 
-export const isExpression = (expr: string) => expr.startsWith('=');
+export const isExpression = (expr: unknown) => {
+	if (typeof expr !== 'string') return false;
+	return expr.startsWith('=');
+};
 
 export const isTestableExpression = (expr: string) => {
 	return ExpressionParser.splitExpression(expr).every((c) => {
