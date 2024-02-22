46afbdb29df8ef0b7b51ed393871c1e51830c17b
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-02-22 16:17:11 +0100

    fix(editor): Fix projects nav

--- packages/editor-ui/src/features/projects/components/ProjectNavigation.vue
+++ packages/editor-ui/src/features/projects/components/ProjectNavigation.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
+import { ref, computed } from 'vue';
+import { useRoute } from 'vue-router';
 import type { IMenuItem } from 'n8n-design-system/types';
-import { ref } from 'vue';
+import { VIEWS } from '@/constants';
 
 type Props = {
 	collapsed: boolean;
@@ -8,33 +10,55 @@ type Props = {
 
 const props = defineProps<Props>();
 
+const route = useRoute();
+
 const home = ref<IMenuItem>({
 	id: 'home',
 	label: 'Home',
 	icon: 'home',
 	route: {
-		to: '/home/workflows',
+		to: { name: VIEWS.HOMEPAGE },
 	},
 });
-
 const addProject = ref<IMenuItem>({
 	id: 'addProject',
 	label: 'Add project',
 	icon: 'plus',
-	route: {
-		to: '/projects/abc123/settings',
-	},
 });
+
+const activeTab = computed(() =>
+	route.name === VIEWS.HOMEPAGE ||
+	route.name === VIEWS.WORKFLOWS ||
+	route.name === VIEWS.CREDENTIALS
+		? 'home'
+		: undefined,
+);
+
+const homeClicked = () => {};
+const addProjectClicked = () => {
+	console.log('Add project clicked');
+};
 </script>
 
 <template>
 	<div :class="$style.projects">
 		<ElMenu :collapse="props.collapsed" class="home">
-			<n8n-menu-item :item="home" :compact="props.collapsed" />
+			<n8n-menu-item
+				:item="home"
+				:compact="props.collapsed"
+				:handle-select="homeClicked"
+				:active-tab="activeTab"
+				mode="tabs"
+			/>
 		</ElMenu>
 		<hr class="mt-m mb-m" />
 		<ElMenu :collapse="props.collapsed" class="pl-xs pr-xs">
-			<n8n-menu-item :item="addProject" :compact="props.collapsed" />
+			<n8n-menu-item
+				:item="addProject"
+				:compact="props.collapsed"
+				:handle-select="addProjectClicked"
+				mode="tabs"
+			/>
 		</ElMenu>
 		<hr class="mt-m mb-m" />
 	</div>
