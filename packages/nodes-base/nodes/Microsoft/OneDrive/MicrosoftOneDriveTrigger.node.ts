200b11dfb991ce658a29ceb74a1f9f58cc84c989
BramKn <bram@knitco.nl>
2024-02-26 17:00:12 +0100

    v1

---
+++ packages/nodes-base/nodes/Microsoft/OneDrive/MicrosoftOneDriveTrigger.node.ts
@@ -0,0 +1,148 @@
+import type {
+	IPollFunctions,
+	IDataObject,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+
+import { DateTime } from 'luxon';
+import { triggerDescription } from './TriggerDescription';
+import { getPath, microsoftApiRequest, microsoftApiRequestAllItemsDelta } from './GenericFunctions';
+
+export class MicrosoftOneDriveTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Microsoft OneDrive Trigger',
+		name: 'microsoftOneDriveTrigger',
+		icon: 'file:oneDrive.svg',
+		group: ['trigger'],
+		version: 1,
+		description: 'Trigger for Microsoft OneDrive API.',
+		subtitle: '={{"Microsoft OneDrive Trigger"}}',
+		defaults: {
+			name: 'Microsoft OneDrive Trigger',
+		},
+		credentials: [
+			{
+				name: 'microsoftOneDriveOAuth2Api',
+				required: true,
+			},
+		],
+		polling: true,
+		inputs: [],
+		outputs: ['main'],
+		properties: [...triggerDescription],
+	};
+
+	methods = {
+		loadOptions: {},
+	};
+
+	async poll(this: IPollFunctions): Promise<INodeExecutionData[][] | null> {
+		const workflowData = this.getWorkflowStaticData('node');
+		let responseData: IDataObject[];
+
+		const lastLink: string =
+			(workflowData.LastLink as string) ||
+			'https://graph.microsoft.com/v1.0/me/drive/root/delta?token=latest';
+
+		const now = DateTime.now().toUTC();
+		const start = DateTime.fromISO(workflowData.lastTimeChecked as string) || now;
+		const end = now;
+		const event = this.getNodeParameter('event', 'fileCreated') as string;
+		const watch = this.getNodeParameter('watch', 'anyFile') as string;
+
+		let eventType = 'created';
+		let eventResource = 'file';
+		if (event.includes('Updated')) {
+			eventType = 'updated';
+		}
+		if (event.includes('folder')) {
+			eventResource = 'folder';
+		}
+		try {
+			if (this.getMode() === 'manual') {
+				responseData = (
+					await microsoftApiRequest.call(
+						this,
+						'GET',
+						'',
+						{},
+						{},
+						'https://graph.microsoft.com/v1.0/me/drive/root/delta',
+					)
+				).value as IDataObject[];
+			} else {
+				const response: IDataObject = (await microsoftApiRequestAllItemsDelta.call(
+					this,
+					lastLink,
+					start,
+					eventType,
+				)) as IDataObject;
+				responseData = response.returnData as IDataObject[];
+				workflowData.LastLink = response.deltaLink;
+			}
+			workflowData.lastTimeChecked = end.toISO();
+			if (watch === 'selectedFile') {
+				const fileId = this.getNodeParameter('fileId.value', '', {
+					extractValue: true,
+				}) as string;
+				if (fileId) {
+					responseData = responseData.filter((item: IDataObject) => item.id === fileId);
+				}
+			}
+			if (watch === 'selectedFolder') {
+				const folderId = (
+					this.getNodeParameter('folderId.value', '', {
+						extractValue: true,
+					}) as string
+				).replace('%21', '!');
+				if (folderId) {
+					if (eventResource === 'folder') {
+						responseData = responseData.filter((item: IDataObject) => item.id === folderId);
+					} else {
+						responseData = responseData.filter(
+							(item: IDataObject) => (item.parentReference as IDataObject).id === folderId,
+						);
+					}
+				}
+			}
+			if (watch === 'selectedFolderChild') {
+				const folderId = (
+					this.getNodeParameter('folderId.value', '', {
+						extractValue: true,
+					}) as string
+				).replace('%21', '!');
+				const folderPath = await getPath.call(this, folderId);
+				responseData = responseData.filter((item: IDataObject) =>
+					((item.parentReference as IDataObject).path as string).startsWith(folderPath),
+				);
+			}
+			responseData = responseData.filter((item: IDataObject) => item[eventResource]);
+			if (!responseData?.length) {
+				return null;
+			}
+			if (this.getMode() === 'manual') {
+				return [this.helpers.returnJsonArray(responseData[0])];
+			} else {
+				return [this.helpers.returnJsonArray(responseData)];
+			}
+		} catch (error) {
+			if (this.getMode() === 'manual' || !workflowData.lastTimeChecked) {
+				throw error;
+			}
+			const workflow = this.getWorkflow();
+			const node = this.getNode();
+			this.logger.error(
+				`There was a problem in '${node.name}' node in workflow '${workflow.id}': '${error.description}'`,
+				{
+					node: node.name,
+					workflowId: workflow.id,
+					error,
+				},
+			);
+		}
+
+		return null;
+	}
+}
---
+++ packages/nodes-base/nodes/Microsoft/OneDrive/TriggerDescription.ts
@@ -0,0 +1,222 @@
+import type { INodeProperties } from 'n8n-workflow';
+import { MICROSOFT_DRIVE_FILE_URL_REGEX, MICROSOFT_DRIVE_FOLDER_URL_REGEX } from './constants';
+
+export const fileRLC: INodeProperties = {
+	displayName: 'File',
+	name: 'fileId',
+	type: 'resourceLocator',
+	default: { mode: 'id', value: '' },
+	required: true,
+	modes: [
+		{
+			displayName: 'Link',
+			name: 'url',
+			type: 'string',
+			placeholder:
+				'e.g. https://onedrive.live.com/edit.aspx?resid=170B5C65E30736A3!257&cid=170b5c65e30736a3&CT=1708697995542&OR=ItemsView',
+			extractValue: {
+				type: 'regex',
+				regex: MICROSOFT_DRIVE_FILE_URL_REGEX,
+			},
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: MICROSOFT_DRIVE_FILE_URL_REGEX,
+						errorMessage: 'Not a valid Microsoft Drive File URL',
+					},
+				},
+			],
+		},
+		{
+			displayName: 'ID',
+			name: 'id',
+			type: 'string',
+			placeholder: 'e.g. 170B5C65E30736A3!257',
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: '[a-zA-Z0-9\\!%21]{5,}',
+						errorMessage: 'Not a valid Microsoft Drive File ID',
+					},
+				},
+			],
+			url: '=https://onedrive.live.com/...',
+		},
+	],
+	description: 'The file to operate on',
+};
+
+export const folderRLC: INodeProperties = {
+	displayName: 'Folder',
+	name: 'folderId',
+	type: 'resourceLocator',
+	default: { mode: 'id', value: '', cachedResultName: '' },
+	required: true,
+	modes: [
+		{
+			displayName: 'Link',
+			name: 'url',
+			type: 'string',
+			placeholder:
+				'e.g. https://onedrive.live.com/?WT.mc_id=PROD%5FOL%2DWeb%5FInApp%5FLeftNav%5FFreeOfficeBarOD&ocid=PROD%5FOL%2DWeb%5FInApp%5FLeftNav%5FFreeOfficeBarOD&id=170B5C65E30736A3%21136&cid=170B5C65E30736A3',
+			extractValue: {
+				type: 'regex',
+				regex: MICROSOFT_DRIVE_FOLDER_URL_REGEX,
+			},
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: MICROSOFT_DRIVE_FOLDER_URL_REGEX,
+						errorMessage: 'Not a valid Microsoft Drive Folder URL',
+					},
+				},
+			],
+		},
+		{
+			displayName: 'ID',
+			name: 'id',
+			type: 'string',
+			placeholder: 'e.g. 170B5C65E30736A3%21136',
+			validation: [
+				{
+					type: 'regex',
+					properties: {
+						regex: '[a-zA-Z0-9\\!%21]{5,}',
+						errorMessage: 'Not a valid Microsoft Drive Folder ID',
+					},
+				},
+			],
+			url: '=https://onedrive.live.com/...',
+		},
+	],
+	description: 'The folder to operate on',
+};
+
+export const triggerDescription: INodeProperties[] = [
+	{
+		displayName: 'Event',
+		name: 'event',
+		type: 'options',
+		default: 'fileCreated',
+		options: [
+			{
+				name: 'File Created',
+				value: 'fileCreated',
+			},
+			{
+				name: 'File Updated',
+				value: 'fileUpdated',
+			},
+			{
+				name: 'Folder Created',
+				value: 'folderCreated',
+			},
+			{
+				name: 'Folder Updated',
+				value: 'folderUpdated',
+			},
+		],
+	},
+	{
+		displayName: 'Watch',
+		name: 'watch',
+		description: 'How to select which file/folder to watch',
+		type: 'options',
+		default: 'anyFile',
+		displayOptions: {
+			show: {
+				event: ['fileCreated'],
+			},
+		},
+		options: [
+			{
+				name: 'Any File',
+				value: 'anyFile',
+			},
+			{
+				name: 'Selected Folder',
+				value: 'selectedFolder',
+			},
+			{
+				name: 'Any Child of a Selected Folder',
+				value: 'selectedFolderChild',
+			},
+		],
+	},
+	{
+		displayName: 'Watch',
+		name: 'watch',
+		description: 'How to select which file/folder to watch',
+		type: 'options',
+		default: 'anyFile',
+		displayOptions: {
+			show: {
+				event: ['fileUpdated'],
+			},
+		},
+		options: [
+			{
+				name: 'Any File',
+				value: 'anyFile',
+			},
+			{
+				name: 'Selected Folder',
+				value: 'selectedFolder',
+			},
+			{
+				name: 'Selected File',
+				value: 'selectedFile',
+			},
+			{
+				name: 'Any Child of a Selected Folder',
+				value: 'selectedFolderChild',
+			},
+		],
+	},
+	{
+		displayName: 'Watch',
+		name: 'watch',
+		description: 'How to select which file/folder to watch',
+		type: 'options',
+		default: 'anyFile',
+		displayOptions: {
+			show: {
+				event: ['folderCreated', 'folderUpdated'],
+			},
+		},
+		options: [
+			{
+				name: 'Any Folder',
+				value: 'anyFolder',
+			},
+			{
+				name: 'Selected Folder',
+				value: 'selectedFolder',
+			},
+			{
+				name: 'Any Child of a Selected Folder',
+				value: 'selectedFolderChild',
+			},
+		],
+	},
+	{
+		...fileRLC,
+		displayOptions: {
+			show: {
+				event: ['fileUpdated'],
+				watch: ['selectedFile'],
+			},
+		},
+	},
+	{
+		...folderRLC,
+		displayOptions: {
+			show: {
+				watch: ['selectedFolder', 'selectedFolderChild'],
+			},
+		},
+	},
+];
