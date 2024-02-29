93640c8e33aeede7c93645c81869e687f55606a0
Danny Martini <danny@n8n.io>
2024-02-29 12:01:52 +0100

    extract WorkflowSharingService tests from controller test file into it's own file

---
+++ packages/cli/test/integration/workflows/workflowSharing.service.test.ts
@@ -0,0 +1,50 @@
+import Container from 'typedi';
+
+import type { User } from '@db/entities/User';
+import { WorkflowSharingService } from '@/workflows/workflowSharing.service';
+
+import * as testDb from '../shared/testDb';
+import { createUser } from '../shared/db/users';
+import { createWorkflow, shareWorkflowWithUsers } from '../shared/db/workflows';
+
+let owner: User;
+let member: User;
+let anotherMember: User;
+
+beforeAll(async () => {
+	await testDb.init();
+	owner = await createUser({ role: 'global:owner' });
+	member = await createUser({ role: 'global:member' });
+	anotherMember = await createUser({ role: 'global:member' });
+});
+
+beforeEach(async () => {
+	await testDb.truncate(['Workflow', 'SharedWorkflow', 'WorkflowHistory']);
+});
+
+describe('getSharedWorkflowIds', () => {
+	it('should show all workflows to owners', async () => {
+		owner.role = 'global:owner';
+		const workflow1 = await createWorkflow({}, member);
+		const workflow2 = await createWorkflow({}, anotherMember);
+		const sharedWorkflowIds =
+			await Container.get(WorkflowSharingService).getSharedWorkflowIds(owner);
+		expect(sharedWorkflowIds).toHaveLength(2);
+		expect(sharedWorkflowIds).toContain(workflow1.id);
+		expect(sharedWorkflowIds).toContain(workflow2.id);
+	});
+
+	it('should show shared workflows to users', async () => {
+		member.role = 'global:member';
+		const workflow1 = await createWorkflow({}, anotherMember);
+		const workflow2 = await createWorkflow({}, anotherMember);
+		const workflow3 = await createWorkflow({}, anotherMember);
+		await shareWorkflowWithUsers(workflow1, [member]);
+		await shareWorkflowWithUsers(workflow3, [member]);
+		const sharedWorkflowIds =
+			await Container.get(WorkflowSharingService).getSharedWorkflowIds(member);
+		expect(sharedWorkflowIds).toHaveLength(2);
+		expect(sharedWorkflowIds).toContain(workflow1.id);
+		expect(sharedWorkflowIds).toContain(workflow3.id);
+	});
+});
