0e507d397f26258258f677db7fcb5ae56fa2bcbd
Danny Martini <danny@n8n.io>
2024-02-29 12:03:14 +0100

    restructure test

--- packages/cli/test/integration/workflows/workflowSharing.service.test.ts
+++ packages/cli/test/integration/workflows/workflowSharing.service.test.ts
@@ -10,41 +10,43 @@ import { createWorkflow, shareWorkflowWithUsers } from '../shared/db/workflows';
 let owner: User;
 let member: User;
 let anotherMember: User;
+let workflowSharingService: WorkflowSharingService;
 
 beforeAll(async () => {
 	await testDb.init();
 	owner = await createUser({ role: 'global:owner' });
 	member = await createUser({ role: 'global:member' });
 	anotherMember = await createUser({ role: 'global:member' });
+	workflowSharingService = Container.get(WorkflowSharingService);
 });
 
 beforeEach(async () => {
 	await testDb.truncate(['Workflow', 'SharedWorkflow', 'WorkflowHistory']);
 });
 
-describe('getSharedWorkflowIds', () => {
-	it('should show all workflows to owners', async () => {
-		owner.role = 'global:owner';
-		const workflow1 = await createWorkflow({}, member);
-		const workflow2 = await createWorkflow({}, anotherMember);
-		const sharedWorkflowIds =
-			await Container.get(WorkflowSharingService).getSharedWorkflowIds(owner);
-		expect(sharedWorkflowIds).toHaveLength(2);
-		expect(sharedWorkflowIds).toContain(workflow1.id);
-		expect(sharedWorkflowIds).toContain(workflow2.id);
-	});
+describe('WorkflowSharingService', () => {
+	describe('getSharedWorkflowIds', () => {
+		it('should show all workflows to owners', async () => {
+			owner.role = 'global:owner';
+			const workflow1 = await createWorkflow({}, member);
+			const workflow2 = await createWorkflow({}, anotherMember);
+			const sharedWorkflowIds = await workflowSharingService.getSharedWorkflowIds(owner);
+			expect(sharedWorkflowIds).toHaveLength(2);
+			expect(sharedWorkflowIds).toContain(workflow1.id);
+			expect(sharedWorkflowIds).toContain(workflow2.id);
+		});
 
-	it('should show shared workflows to users', async () => {
-		member.role = 'global:member';
-		const workflow1 = await createWorkflow({}, anotherMember);
-		const workflow2 = await createWorkflow({}, anotherMember);
-		const workflow3 = await createWorkflow({}, anotherMember);
-		await shareWorkflowWithUsers(workflow1, [member]);
-		await shareWorkflowWithUsers(workflow3, [member]);
-		const sharedWorkflowIds =
-			await Container.get(WorkflowSharingService).getSharedWorkflowIds(member);
-		expect(sharedWorkflowIds).toHaveLength(2);
-		expect(sharedWorkflowIds).toContain(workflow1.id);
-		expect(sharedWorkflowIds).toContain(workflow3.id);
+		it('should show shared workflows to users', async () => {
+			member.role = 'global:member';
+			const workflow1 = await createWorkflow({}, anotherMember);
+			const workflow2 = await createWorkflow({}, anotherMember);
+			const workflow3 = await createWorkflow({}, anotherMember);
+			await shareWorkflowWithUsers(workflow1, [member]);
+			await shareWorkflowWithUsers(workflow3, [member]);
+			const sharedWorkflowIds = await workflowSharingService.getSharedWorkflowIds(member);
+			expect(sharedWorkflowIds).toHaveLength(2);
+			expect(sharedWorkflowIds).toContain(workflow1.id);
+			expect(sharedWorkflowIds).toContain(workflow3.id);
+		});
 	});
 });
