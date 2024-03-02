4653356ace56d02108eec281ebb2ef8e490cd8e7
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-03-02 19:37:10 +0100

    refactor(editor): Continue porting components over to composition API (no-changelog)

--- packages/design-system/src/types/index.ts
+++ packages/design-system/src/types/index.ts
@@ -4,5 +4,6 @@ export * from './form';
 export * from './i18n';
 export * from './menu';
 export * from './router';
+export * from './select';
 export * from './user';
 export * from './keyboardshortcut';
