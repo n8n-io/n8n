47e1785336e59ee4bf11769bd43e5ed53e987f09
Marcus <marcus@n8n.io>
2024-03-05 15:12:42 +0100

    fix errorsToTrack check

--- packages/editor-ui/src/components/RunData.vue
+++ packages/editor-ui/src/components/RunData.vue
@@ -744,7 +744,7 @@ export default defineComponent({
 			const error = this.workflowRunData?.[this.node.name]?.[this.runIndex]?.error;
 			const errorsToTrack = ['unknown error'];
 
-			if (error && errorsToTrack.map((e) => error.message.toLowerCase().includes(e))) {
+			if (error && errorsToTrack.some((e) => error.message.toLowerCase().includes(e))) {
 				this.$telemetry.track(
 					`User encountered an error: "${error.message}"`,
 					{
