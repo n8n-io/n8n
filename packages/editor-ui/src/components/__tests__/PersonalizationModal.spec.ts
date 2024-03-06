50ef1a0a02052cf4f555d48d401d215f801b1559
Omar Ajoue <krynble@gmail.com>
2024-03-06 10:29:47 +0000

    feat: Track self serve trial (no-changelog) (#8801)

--- packages/editor-ui/src/components/__tests__/PersonalizationModal.spec.ts
+++ packages/editor-ui/src/components/__tests__/PersonalizationModal.spec.ts
@@ -1,31 +1,58 @@
 import PersonalizationModal from '@/components/PersonalizationModal.vue';
 import { createTestingPinia } from '@pinia/testing';
-import { PERSONALIZATION_MODAL_KEY, STORES } from '@/constants';
+import userEvent from '@testing-library/user-event';
+import { PERSONALIZATION_MODAL_KEY, STORES, VIEWS } from '@/constants';
 import { retry } from '@/__tests__/utils';
 import { createComponentRenderer } from '@/__tests__/render';
 import { fireEvent } from '@testing-library/vue';
+import { useUsersStore } from '@/stores/users.store';
+import { useUsageStore } from '@/stores/usage.store';
+
+const pinia = createTestingPinia({
+	initialState: {
+		[STORES.UI]: {
+			modals: {
+				[PERSONALIZATION_MODAL_KEY]: { open: true },
+			},
+		},
+		[STORES.SETTINGS]: {
+			settings: {
+				templates: {
+					host: '',
+				},
+			},
+		},
+		[STORES.USERS]: {
+			users: {
+				123: {
+					email: 'john@doe.com',
+					firstName: 'John',
+					lastName: 'Doe',
+					isDefaultUser: false,
+					isPendingUser: false,
+					hasRecoveryCodesLeft: true,
+					isOwner: true,
+					mfaEnabled: false,
+				},
+			},
+			currentUserId: '123',
+		},
+	},
+});
 
 const renderComponent = createComponentRenderer(PersonalizationModal, {
 	props: {
 		teleported: false,
 		appendToBody: false,
 	},
-	pinia: createTestingPinia({
-		initialState: {
-			[STORES.UI]: {
-				modals: {
-					[PERSONALIZATION_MODAL_KEY]: { open: true },
-				},
-			},
-			[STORES.SETTINGS]: {
-				settings: {
-					templates: {
-						host: '',
-					},
-				},
+	pinia,
+	global: {
+		mocks: {
+			$route: {
+				name: VIEWS.NEW_WORKFLOW,
 			},
 		},
-	}),
+	},
 });
 
 describe('PersonalizationModal.vue', () => {
@@ -60,5 +87,51 @@ describe('PersonalizationModal.vue', () => {
 				expect(wrapper.container.querySelector('[name^="automationGoal"]')).toBeInTheDocument();
 			});
 		}
+	});
+
+	it('should display self serve trial option when company size is larger than 500', async () => {
+		const wrapper = renderComponent();
+
+		await retry(() =>
+			expect(wrapper.container.querySelector('.modal-content')).toBeInTheDocument(),
+		);
+
+		const select = wrapper.container.querySelectorAll('.n8n-select')[3]!;
+		await fireEvent.click(select);
+
+		const item = select.querySelectorAll('.el-select-dropdown__item')[3];
+		await fireEvent.click(item);
+
+		await retry(() => {
+			expect(wrapper.container.querySelector('.card')).not.toBeNull();
+		});
+	});
+
+	it('should display send telemetry when requesting enterprise trial', async () => {
+		const usersStore = useUsersStore(pinia);
+		vi.spyOn(usersStore, 'submitPersonalizationSurvey').mockResolvedValue();
+
+		const usageStore = useUsageStore(pinia);
+		const spyLicenseTrial = vi.spyOn(usageStore, 'requestEnterpriseLicenseTrial');
+
+		const wrapper = renderComponent();
+
+		await retry(() =>
+			expect(wrapper.container.querySelector('.modal-content')).toBeInTheDocument(),
+		);
+
+		const select = wrapper.container.querySelectorAll('.n8n-select')[3]!;
+		await fireEvent.click(select);
+
+		const item = select.querySelectorAll('.el-select-dropdown__item')[3];
+		await fireEvent.click(item);
+
+		const agreeCheckbox = wrapper.container.querySelector('.n8n-checkbox')!;
+		await fireEvent.click(agreeCheckbox);
+
+		const submitButton = wrapper.getByRole('button')!;
+		await userEvent.click(submitButton);
+
+		await retry(() => expect(spyLicenseTrial).toHaveBeenCalled());
 	});
 });
