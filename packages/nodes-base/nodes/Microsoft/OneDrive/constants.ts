f74f237f4f452ab5eb94c8278dbc24a8d0fcc3cd
BramKn <bram@knitco.nl>
2024-03-08 08:44:16 +0100

    fixes

--- packages/nodes-base/nodes/Microsoft/OneDrive/constants.ts
+++ packages/nodes-base/nodes/Microsoft/OneDrive/constants.ts
@@ -1,5 +1,4 @@
 export const MICROSOFT_DRIVE_FILE_URL_REGEX =
 	'https:\\/\\/onedrive\\.live\\.com(?:\\/.*?resid=)(.+)(?:&cid.*OR=ItemsView)';
 
-export const MICROSOFT_DRIVE_FOLDER_URL_REGEX =
-	'https:\\/\\/onedrive\\.live\\.com(?:\\/.*&id=)(.+)(?:&cid.*)';
+export const MICROSOFT_DRIVE_FOLDER_URL_REGEX = 'https://onedrive.live.com(?:/.*id=)(.+)(?:&cid.*)';
