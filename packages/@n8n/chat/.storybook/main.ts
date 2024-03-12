89a95bca50e1a6f7f8041842e7fe87c9051bdd0a
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-03-12 16:37:55 +0100

    extract storybook dependencies into a meta-package

--- packages/@n8n/chat/.storybook/main.ts
+++ packages/@n8n/chat/.storybook/main.ts
@@ -1,33 +1,4 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import { sharedConfig } from '@n8n/storybook/main';
 
-import { join, dirname } from 'path';
-
-/**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
- */
-function getAbsolutePath(value: string): any {
-	return dirname(require.resolve(join(value, 'package.json')));
-}
-
-const config: StorybookConfig = {
-	stories: ['../src/**/*.stories.ts'],
-	addons: [
-		'@storybook/addon-a11y',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-links',
-	].map((path) => getAbsolutePath(path)),
-	framework: {
-		name: getAbsolutePath('@storybook/vue3-vite') as '@storybook/vue3-vite',
-		options: {},
-	},
-	core: {
-		disableTelemetry: true,
-	},
-	docs: {
-		autodocs: true,
-	},
-};
-
+const config = { ...sharedConfig };
 export default config;
--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,29 +1,4 @@
-import { dirname, join } from 'path';
-import type { StorybookConfig } from '@storybook/vue3-vite';
+import { sharedConfig } from '@n8n/storybook/main';
 
-function getAbsolutePath(value: string): string {
-	return dirname(require.resolve(join(value, 'package.json'))).toString();
-}
-
-const config: StorybookConfig = {
-	stories: ['../src/**/*.stories.ts'],
-	addons: [
-		'@storybook/addon-a11y',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-links',
-	].map((path) => getAbsolutePath(path)),
-	staticDirs: ['../public'],
-	framework: {
-		name: getAbsolutePath('@storybook/vue3-vite') as '@storybook/vue3-vite',
-		options: {},
-	},
-	core: {
-		disableTelemetry: true,
-	},
-	docs: {
-		autodocs: true,
-	},
-};
-
+const config = { ...sharedConfig, staticDirs: ['../public'] };
 export default config;
