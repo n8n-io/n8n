fbdda1e06aa591605b683b32968fd18ef152df7b
Danny Martini <danny@n8n.io>
2024-03-13 09:52:07 +0100

    update `POST /workflows/run`

--- packages/cli/src/workflows/workflows.controller.ts
+++ packages/cli/src/workflows/workflows.controller.ts
@@ -349,6 +349,7 @@ export class WorkflowsController {
 		return true;
 	}
 
+	// NOTE: updated
 	@Post('/run')
 	async runManually(req: WorkflowRequest.ManualRun) {
 		if (this.license.isSharingEnabled()) {
