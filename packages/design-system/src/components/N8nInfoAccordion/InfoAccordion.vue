n8n-io/n8n

commit 1fed37243694d06fff74d665712599dec573692b

refactor(editor): Port more components over to composition API (no-changelog) (#8794)

packages/design-system/src/components/N8nInfoAccordion/InfoAccordion.vue
@@ -38,75 +38,53 @@
 	</div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { onMounted } from 'vue';
 import N8nText from '../N8nText';
 import N8nIcon from '../N8nIcon';
-import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
-import type { EventBus } from '../../utils';
-import { createEventBus } from '../../utils';
+import type { IconColor } from '@/types/icon';
+import { createEventBus, type EventBus } from '../../utils';
 
-export interface IAccordionItem {
+interface IAccordionItem {
 	id: string;
 	label: string;
 	icon: string;
-	iconColor?: string;
+	iconColor?: IconColor;
 	tooltip?: string;
 }
 
-export default defineComponent({
-	name: 'N8nInfoAccordion',
-	components: {
-		N8nText,
-		N8nIcon,
-	},
-	props: {
-		title: {
-			type: String,
-		},
-		description: {
-			type: String,
-		},
-		items: {
-			type: Array as PropType<IAccordionItem[]>,
-			default: () => [],
-		},
-		initiallyExpanded: {
-			type: Boolean,
-			default: false,
-		},
-		headerIcon: {
-			type: Object as PropType<{ icon: string; color: string }>,
-			required: false,
-		},
-		eventBus: {
-			type: Object as PropType<EventBus>,
-			default: () => createEventBus(),
-		},
-	},
-	data() {
-		return {
-			expanded: false,
-		};
-	},
-	mounted() {
-		this.eventBus.on('expand', () => {
-			this.expanded = true;
-		});
-		this.expanded = this.initiallyExpanded;
-	},
-	methods: {
-		toggle() {
-			this.expanded = !this.expanded;
-		},
-		onClick(e: MouseEvent) {
-			this.$emit('click:body', e);
-		},
-		onTooltipClick(item: string, event: MouseEvent) {
-			this.$emit('tooltipClick', item, event);
-		},
-	},
+interface InfoAccordionProps {
+	title?: string;
+	description?: string;
+	items?: IAccordionItem[];
+	initiallyExpanded?: boolean;
+	headerIcon?: { icon: string; color: IconColor };
+	eventBus?: EventBus;
+}
+
+defineOptions({ name: 'N8nInfoAccordion' });
+const props = withDefaults(defineProps<InfoAccordionProps>(), {
+	items: () => [],
+	initiallyExpanded: false,
+	eventBus: () => createEventBus(),
 });
+const $emit = defineEmits(['click:body', 'tooltipClick']);
+
+let expanded = false;
+onMounted(() => {
+	props.eventBus.on('expand', () => {
+		expanded = true;
+	});
+	expanded = props.initiallyExpanded;
+});
+
+const toggle = () => {
+	expanded = !expanded;
+};
+
+const onClick = (e: MouseEvent) => $emit('click:body', e);
+
+const onTooltipClick = (item: string, event: MouseEvent) => $emit('tooltipClick', item, event);
 </script>
 
 <style lang="scss" module>

packages/design-system/src/components/N8nTags/Tags.vue
@@ -13,68 +13,55 @@
 			size="small"
 			@click.stop.prevent="onExpand"
 		>
-			{{ t('tags.showMore', `${hiddenTagsLength}`) }}
+			{{ t('tags.showMore', [`${hiddenTagsLength}`]) }}
 		</N8nLink>
 	</div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { computed } from 'vue';
 import N8nTag from '../N8nTag';
 import N8nLink from '../N8nLink';
-import Locale from '../../mixins/locale';
-import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
+import { useI18n } from '../../composables/useI18n';
 
 export interface ITag {
 	id: string;
 	name: string;
 }
 
-export default defineComponent({
-	name: 'N8nTags',
-	components: {
-		N8nTag,
-		N8nLink,
-	},
-	mixins: [Locale],
-	props: {
-		tags: {
-			type: Array as PropType<ITag[]>,
-			default: () => [],
-		},
-		truncate: {
-			type: Boolean,
-			default: false,
-		},
-		truncateAt: {
-			type: Number,
-			default: 3,
-		},
-	},
-	data() {
-		return {
-			showAll: false,
-		};
-	},
-	computed: {
-		visibleTags(): ITag[] {
-			if (this.truncate && !this.showAll && this.tags.length > this.truncateAt) {
-				return this.tags.slice(0, this.truncateAt);
-			}
+interface TagsProp {
+	tags?: ITag[];
+	truncate?: boolean;
+	truncateAt?: number;
+}
 
-			return this.tags;
-		},
-		hiddenTagsLength(): number {
-			return this.tags.length - this.truncateAt;
-		},
-	},
-	methods: {
-		onExpand() {
-			this.showAll = true;
-			this.$emit('expand', true);
-		},
-	},
+defineOptions({ name: 'N8nTags' });
+const props = withDefaults(defineProps<TagsProp>(), {
+	tags: () => [],
+	truncate: false,
+	truncateAt: 3,
 });
+
+const $emit = defineEmits(['expand', 'click:tag']);
+
+const { t } = useI18n();
+
+let showAll = false;
+
+const visibleTags = computed((): ITag[] => {
+	const { tags, truncate, truncateAt } = props;
+	if (truncate && !showAll && tags.length > truncateAt) {
+		return tags.slice(0, truncateAt);
+	}
+	return tags;
+});
+
+const hiddenTagsLength = computed((): number => props.tags.length - props.truncateAt);
+
+const onExpand = () => {
+	showAll = true;
+	$emit('expand', true);
+};
 </script>
 
 <style lang="scss" module>

packages/design-system/src/components/N8nActionBox/ActionBox.vue
@@ -35,48 +35,27 @@
 	</div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import N8nButton from '../N8nButton';
 import N8nHeading from '../N8nHeading';
 import N8nText from '../N8nText';
-import N8nCallout from '../N8nCallout';
-import { defineComponent } from 'vue';
+import N8nCallout, { type CalloutTheme } from '../N8nCallout';
+import type { ButtonType } from '@/types/button';
 
-export default defineComponent({
-	name: 'N8nActionBox',
-	components: {
-		N8nButton,
-		N8nHeading,
-		N8nText,
-		N8nCallout,
-	},
-	props: {
-		emoji: {
-			type: String,
-		},
-		heading: {
-			type: String,
-		},
-		buttonText: {
-			type: String,
-		},
-		buttonType: {
-			type: String,
-		},
-		description: {
-			type: String,
-		},
-		calloutText: {
-			type: String,
-		},
-		calloutTheme: {
-			type: String,
-			default: 'info',
-		},
-		calloutIcon: {
-			type: String,
-		},
-	},
+interface ActionBoxProps {
+	emoji: string;
+	heading: string;
+	buttonText: string;
+	buttonType: ButtonType;
+	description: string;
+	calloutText: string;
+	calloutTheme: CalloutTheme;
+	calloutIcon: string;
+}
+
+defineOptions({ name: 'N8nActionBox' });
+withDefaults(defineProps<ActionBoxProps>(), {
+	calloutTheme: 'info',
 });
 </script>
 

packages/design-system/src/components/N8nActionBox/__tests__/__snapshots__/ActionBox.spec.ts.snap
@@ -9,7 +9,7 @@ exports[`N8NActionBox > should render correctly 1`] = `
   <div class="description">
     <n8n-text-stub color="text-base" bold="false" size="medium" compact="false" tag="span"></n8n-text-stub>
   </div>
-  <n8n-button-stub label="Do something" type="primary" size="large" loading="false" disabled="false" outline="false" text="false" block="false" active="false" square="false" element="button"></n8n-button-stub>
+  <n8n-button-stub block="false" element="button" label="Do something" square="false" active="false" disabled="false" loading="false" outline="false" size="large" text="false" type="primary"></n8n-button-stub>
   <!--v-if-->
 </div>"
 `;

packages/design-system/src/components/N8nActionDropdown/ActionDropdown.vue
@@ -50,15 +50,21 @@
 	</div>
 </template>
 
-<script lang="ts">
-import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
-import { ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus';
+<script lang="ts" setup>
+// This component is visually similar to the ActionToggle component
+// but it offers more options when it comes to dropdown items styling
+// (supports icons, separators, custom styling and all options provided
+// by Element UI dropdown component).
+// It can be used in different parts of editor UI while ActionToggle
+// is designed to be used in card components.
+import { ref, useCssModule, useAttrs } from 'vue';
+import { ElDropdown, ElDropdownMenu, ElDropdownItem, type Placement } from 'element-plus';
 import N8nIcon from '../N8nIcon';
 import { N8nKeyboardShortcut } from '../N8nKeyboardShortcut';
 import type { KeyboardShortcut } from '../../types';
+import type { IconSize } from '@/types/icon';
 
-export interface IActionDropdownItem {
+interface IActionDropdownItem {
 	id: string;
 	label: string;
 	icon?: string;
@@ -68,92 +74,56 @@ export interface IActionDropdownItem {
 	customClass?: string;
 }
 
-// This component is visually similar to the ActionToggle component
-// but it offers more options when it comes to dropdown items styling
-// (supports icons, separators, custom styling and all options provided
-// by Element UI dropdown component).
-// It can be used in different parts of editor UI while ActionToggle
-// is designed to be used in card components.
-export default defineComponent({
-	name: 'N8nActionDropdown',
-	components: {
-		ElDropdown,
-		ElDropdownMenu,
-		ElDropdownItem,
-		N8nIcon,
-		N8nKeyboardShortcut,
-	},
-	props: {
-		items: {
-			type: Array as PropType<IActionDropdownItem[]>,
-			required: true,
-		},
-		placement: {
-			type: String,
-			default: 'bottom',
-			validator: (value: string): boolean =>
-				['top', 'top-end', 'top-start', 'bottom', 'bottom-end', 'bottom-start'].includes(value),
-		},
-		activatorIcon: {
-			type: String,
-			default: 'ellipsis-h',
-		},
-		activatorSize: {
-			type: String,
-			default: 'medium',
-		},
-		iconSize: {
-			type: String,
-			default: 'medium',
-			validator: (value: string): boolean => ['small', 'medium', 'large'].includes(value),
-		},
-		trigger: {
-			type: String,
-			default: 'click',
-			validator: (value: string): boolean => ['click', 'hover'].includes(value),
-		},
-		hideArrow: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	data() {
-		const testIdPrefix = this.$attrs['data-test-id'];
-		return { testIdPrefix };
-	},
-	methods: {
-		getItemClasses(item: IActionDropdownItem): Record<string, boolean> {
-			return {
-				[this.$style.itemContainer]: true,
-				[this.$style.disabled]: item.disabled,
-				[this.$style.hasCustomStyling]: item.customClass !== undefined,
-				...(item.customClass !== undefined ? { [item.customClass]: true } : {}),
-			};
-		},
-		onSelect(action: string): void {
-			this.$emit('select', action);
-		},
-		onVisibleChange(open: boolean): void {
-			this.$emit('visibleChange', open);
-		},
-		onButtonBlur(event: FocusEvent): void {
-			const elementDropdown = this.$refs.elementDropdown as InstanceType<typeof ElDropdown>;
+const TRIGGER = ['click', 'hover'] as const;
 
-			// Hide dropdown when clicking outside of current document
-			if (elementDropdown?.handleClose && event.relatedTarget === null) {
-				elementDropdown.handleClose();
-			}
-		},
-		open() {
-			const elementDropdown = this.$refs.elementDropdown as InstanceType<typeof ElDropdown>;
-			elementDropdown.handleOpen();
-		},
-		close() {
-			const elementDropdown = this.$refs.elementDropdown as InstanceType<typeof ElDropdown>;
-			elementDropdown.handleClose();
-		},
-	},
+interface ActionDropdownProps {
+	items: IActionDropdownItem[];
+	placement?: Placement;
+	activatorIcon?: string;
+	activatorSize?: IconSize;
+	iconSize?: IconSize;
+	trigger?: (typeof TRIGGER)[number];
+	hideArrow?: boolean;
+}
+
+withDefaults(defineProps<ActionDropdownProps>(), {
+	placement: 'bottom',
+	activatorIcon: 'ellipsis-h',
+	activatorSize: 'medium',
+	iconSize: 'medium',
+	trigger: 'click',
+	hideArrow: false,
 });
+
+const $attrs = useAttrs();
+const testIdPrefix = $attrs['data-test-id'];
+
+const $style = useCssModule();
+const getItemClasses = (item: IActionDropdownItem): Record<string, boolean> => {
+	return {
+		[$style.itemContainer]: true,
+		[$style.disabled]: !!item.disabled,
+		[$style.hasCustomStyling]: item.customClass !== undefined,
+		...(item.customClass !== undefined ? { [item.customClass]: true } : {}),
+	};
+};
+
+const $emit = defineEmits(['select', 'visibleChange']);
+const elementDropdown = ref<InstanceType<typeof ElDropdown>>();
+
+const onSelect = (action: string) => $emit('select', action);
+const onVisibleChange = (open: boolean) => $emit('visibleChange', open);
+
+const onButtonBlur = (event: FocusEvent) => {
+	// Hide dropdown when clicking outside of current document
+	if (elementDropdown.value?.handleClose && event.relatedTarget === null) {
+		elementDropdown.value.handleClose();
+	}
+};
+
+const open = () => elementDropdown.value?.handleOpen();
+const close = () => elementDropdown.value?.handleClose();
+defineExpose({ open, close });
 </script>
 
 <style lang="scss" module>

packages/design-system/src/components/N8nActionToggle/ActionToggle.vue
@@ -41,60 +41,36 @@
 	</span>
 </template>
 
-<script lang="ts">
-import type { PropType } from 'vue';
-import { defineComponent } from 'vue';
-import { ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus';
-import N8nIcon from '../N8nIcon';
+<script lang="ts" setup>
+import { ElDropdown, ElDropdownMenu, ElDropdownItem, type Placement } from 'element-plus';
 import type { UserAction } from '@/types';
+import N8nIcon from '../N8nIcon';
+import type { IconOrientation, IconSize } from '@/types/icon';
 
-export default defineComponent({
-	name: 'N8nActionToggle',
-	components: {
-		ElDropdown,
-		ElDropdownMenu,
-		ElDropdownItem,
-		N8nIcon,
-	},
-	props: {
-		actions: {
-			type: Array as PropType<UserAction[]>,
-			default: () => [],
-		},
-		placement: {
-			type: String,
-			default: 'bottom',
-			validator: (value: string): boolean =>
-				['top', 'top-end', 'top-start', 'bottom', 'bottom-end', 'bottom-start'].includes(value),
-		},
-		size: {
-			type: String,
-			default: 'medium',
-			validator: (value: string): boolean => ['mini', 'small', 'medium'].includes(value),
-		},
-		iconSize: {
-			type: String,
-		},
-		theme: {
-			type: String,
-			default: 'default',
-			validator: (value: string): boolean => ['default', 'dark'].includes(value),
-		},
-		iconOrientation: {
-			type: String,
-			default: 'vertical',
-			validator: (value: string): boolean => ['horizontal', 'vertical'].includes(value),
-		},
-	},
-	methods: {
-		onCommand(value: string) {
-			this.$emit('action', value);
-		},
-		onVisibleChange(value: boolean) {
-			this.$emit('visible-change', value);
-		},
-	},
+const SIZE = ['mini', 'small', 'medium'] as const;
+const THEME = ['default', 'dark'] as const;
+
+interface ActionToggleProps {
+	actions?: UserAction[];
+	placement?: Placement;
+	size?: (typeof SIZE)[number];
+	iconSize?: IconSize;
+	theme?: (typeof THEME)[number];
+	iconOrientation?: IconOrientation;
+}
+
+defineOptions({ name: 'N8nActionToggle' });
+withDefaults(defineProps<ActionToggleProps>(), {
+	actions: () => [],
+	placement: 'bottom',
+	size: 'medium',
+	theme: 'default',
+	iconOrientation: 'vertical',
 });
+
+const $emit = defineEmits(['action', 'visible-change']);
+const onCommand = (value: string) => $emit('action', value);
+const onVisibleChange = (value: boolean) => $emit('visible-change', value);
 </script>
 
 <style lang="scss" module>

packages/design-system/src/components/N8nAlert/Alert.vue
@@ -57,20 +57,20 @@ const icon = computed(() => {
 	}
 });
 
-const style = useCssModule();
+const $style = useCssModule();
 const alertBoxClassNames = computed(() => {
-	const classNames = ['n8n-alert', style.alert];
+	const classNames = ['n8n-alert', $style.alert];
 	if (props.type) {
-		classNames.push(style[props.type]);
+		classNames.push($style[props.type]);
 	}
 	if (props.effect) {
-		classNames.push(style[props.effect]);
+		classNames.push($style[props.effect]);
 	}
 	if (props.center) {
-		classNames.push(style.center);
+		classNames.push($style.center);
 	}
 	if (props.background) {
-		classNames.push(style.background);
+		classNames.push($style.background);
 	}
 	return classNames;
 });

packages/design-system/src/components/N8nAvatar/Avatar.vue
@@ -12,63 +12,48 @@
 	</span>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { computed } from 'vue';
 import Avatar from 'vue-boring-avatars';
+
+interface AvatarProps {
+	firstName: string;
+	lastName: string;
+	size: string;
+	colors: string[];
+}
+
+defineOptions({ name: 'N8nAvatar' });
+const props = withDefaults(defineProps<AvatarProps>(), {
+	firstName: '',
+	lastName: '',
+	size: 'medium',
+	colors: () => [
+		'--color-primary',
+		'--color-secondary',
+		'--color-avatar-accent-1',
+		'--color-avatar-accent-2',
+		'--color-primary-tint-1',
+	],
+});
+
+const initials = computed(
+	() =>
+		(props.firstName ? props.firstName.charAt(0) : '') +
+		(props.lastName ? props.lastName.charAt(0) : ''),
+);
+
+const getColors = (colors: string[]): string[] => {
+	const style = getComputedStyle(document.body);
+	return colors.map((color: string) => style.getPropertyValue(color));
+};
 
 const sizes: { [size: string]: number } = {
 	small: 28,
 	large: 48,
 	medium: 40,
 };
-
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-	name: 'N8nAvatar',
-	components: {
-		Avatar,
-	},
-	props: {
-		firstName: {
-			type: String,
-			default: '',
-		},
-		lastName: {
-			type: String,
-			default: '',
-		},
-		size: {
-			type: String,
-			default: 'medium',
-		},
-		colors: {
-			default: () => [
-				'--color-primary',
-				'--color-secondary',
-				'--color-avatar-accent-1',
-				'--color-avatar-accent-2',
-				'--color-primary-tint-1',
-			],
-		},
-	},
-	computed: {
-		initials() {
-			return (
-				(this.firstName ? this.firstName.charAt(0) : '') +
-				(this.lastName ? this.lastName.charAt(0) : '')
-			);
-		},
-	},
-	methods: {
-		getColors(colors: string[]): string[] {
-			const style = getComputedStyle(document.body);
-			return colors.map((color: string) => style.getPropertyValue(color));
-		},
-		getSize(size: string): number {
-			return sizes[size];
-		},
-	},
-});
+const getSize = (size: string): number => sizes[size];
 </script>
 
 <style lang="scss" module>

commit 83e76d04d385d1fb6632e218312740869e440c09

fix the reactive properties that broke in #8794

packages/design-system/src/components/N8nInfoAccordion/InfoAccordion.vue
@@ -39,7 +39,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import N8nText from '../N8nText';
 import N8nIcon from '../N8nIcon';
 import type { IconColor } from '@/types/icon';
@@ -70,16 +70,16 @@ const props = withDefaults(defineProps<InfoAccordionProps>(), {
 });
 const $emit = defineEmits(['click:body', 'tooltipClick']);
 
-let expanded = false;
+let expanded = ref(false);
 onMounted(() => {
 	props.eventBus.on('expand', () => {
-		expanded = true;
+		expanded.value = true;
 	});
-	expanded = props.initiallyExpanded;
+	expanded.value = props.initiallyExpanded;
 });
 
 const toggle = () => {
-	expanded = !expanded;
+	expanded.value = !expanded.value;
 };
 
 const onClick = (e: MouseEvent) => $emit('click:body', e);

packages/design-system/src/components/N8nTags/Tags.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import N8nTag from '../N8nTag';
 import N8nLink from '../N8nLink';
 import { useI18n } from '../../composables/useI18n';
@@ -46,11 +46,11 @@ const $emit = defineEmits(['expand', 'click:tag']);
 
 const { t } = useI18n();
 
-let showAll = false;
+let showAll = ref(false);
 
 const visibleTags = computed((): ITag[] => {
 	const { tags, truncate, truncateAt } = props;
-	if (truncate && !showAll && tags.length > truncateAt) {
+	if (truncate && !showAll.value && tags.length > truncateAt) {
 		return tags.slice(0, truncateAt);
 	}
 	return tags;
@@ -59,7 +59,7 @@ const visibleTags = computed((): ITag[] => {
 const hiddenTagsLength = computed((): number => props.tags.length - props.truncateAt);
 
 const onExpand = () => {
-	showAll = true;
+	showAll.value = true;
 	$emit('expand', true);
 };
 </script>

packages/design-system/src/components/N8nUserSelect/UserSelect.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import N8nUserInfo from '../N8nUserInfo';
 import N8nSelect from '../N8nSelect';
 import N8nOption from '../N8nOption';
@@ -59,7 +59,7 @@ const $emit = defineEmits(['blur', 'focus']);
 
 const { t } = useI18n();
 
-let filter: string = '';
+let filter = ref('');
 
 const filteredUsers = computed(() =>
 	props.users.filter((user) => {
@@ -72,13 +72,13 @@ const filteredUsers = computed(() =>
 		}
 
 		if (user.fullName) {
-			const match = user.fullName.toLowerCase().includes(filter.toLowerCase());
+			const match = user.fullName.toLowerCase().includes(filter.value.toLowerCase());
 			if (match) {
 				return true;
 			}
 		}
 
-		return user.email.includes(filter);
+		return user.email.includes(filter.value);
 	}),
 );
 
@@ -100,7 +100,7 @@ const sortedUsers = computed(() =>
 );
 
 const setFilter = (value: string) => {
-	filter = value;
+	filter.value = value;
 };
 
 const onBlur = () => $emit('blur');