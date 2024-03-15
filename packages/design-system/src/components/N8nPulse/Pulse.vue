6c693e1afd93862fb23bbf62cf4fcfd3f14301b0
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-03-15 12:43:08 +0100

    refactor(editor): Continue porting components over to composition API (no-changelog) (#8893)

--- packages/design-system/src/components/N8nPulse/Pulse.vue
+++ packages/design-system/src/components/N8nPulse/Pulse.vue
@@ -8,12 +8,8 @@
 	</div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-	name: 'N8nPulse',
-});
+<script lang="ts" setup>
+defineOptions({ name: 'N8nPulse' });
 </script>
 
 <style lang="scss" module>
