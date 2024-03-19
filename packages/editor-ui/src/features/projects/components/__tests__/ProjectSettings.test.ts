0dcc22a448ffbb66405f0e964bafec501f6795f6
Csaba Tuncsik <csaba@n8n.io>
2024-03-19 16:48:30 +0100

    fix: Fix project settings unit test

--- packages/editor-ui/src/features/projects/components/__tests__/ProjectSettings.test.ts
+++ packages/editor-ui/src/features/projects/components/__tests__/ProjectSettings.test.ts
@@ -1,11 +1,11 @@
-import { setActivePinia } from 'pinia';
-import { createTestingPinia } from '@pinia/testing';
+import { createPinia, setActivePinia } from 'pinia';
 import userEvent from '@testing-library/user-event';
 import { createComponentRenderer } from '@/__tests__/render';
 import { useRoute, useRouter } from 'vue-router';
 import ProjectSettings from '@/features/projects/components/ProjectSettings.vue';
 import { useProjectsStore } from '@/features/projects/projects.store';
 import { VIEWS } from '@/constants';
+import { useUsersStore } from '@/stores/users.store';
 
 vi.mock('vue-router', () => {
 	const params = {};
@@ -26,14 +26,18 @@ const renderComponent = createComponentRenderer(ProjectSettings);
 let router: ReturnType<typeof useRouter>;
 let route: ReturnType<typeof useRoute>;
 let projectsStore: ReturnType<typeof useProjectsStore>;
+let usersStore: ReturnType<typeof useUsersStore>;
 
 describe('ProjectSettings', () => {
 	beforeEach(() => {
-		const pinia = createTestingPinia();
+		const pinia = createPinia();
 		setActivePinia(pinia);
 		route = useRoute();
 		router = useRouter();
 		projectsStore = useProjectsStore();
+		usersStore = useUsersStore();
+
+		vi.spyOn(usersStore, 'fetchUsers').mockImplementation(async () => await Promise.resolve());
 	});
 
 	it('should show confirmation modal before deleting project', async () => {
@@ -51,8 +55,9 @@ describe('ProjectSettings', () => {
 		await userEvent.click(deleteButton);
 		expect(deleteProjectSpy).not.toHaveBeenCalled();
 		let modal = getByRole('dialog');
-		expect(modal).toBeInTheDocument();
+		expect(modal).toBeVisible();
 		const cancelButton = modal.querySelector('.btn--cancel');
+		expect(cancelButton).toBeVisible();
 
 		if (cancelButton) {
 			await userEvent.click(cancelButton);
@@ -62,12 +67,14 @@ describe('ProjectSettings', () => {
 
 		await userEvent.click(deleteButton);
 		modal = getByRole('dialog');
-		expect(modal).toBeInTheDocument();
+		expect(modal).toBeVisible();
 		const confirmButton = modal.querySelector('.btn--confirm');
+		expect(confirmButton).toBeVisible();
 
 		if (confirmButton) {
 			await userEvent.click(confirmButton);
-			expect(deleteProjectSpy).toHaveBeenCalled();
+			expect(queryByRole('dialog')).not.toBeInTheDocument();
+			expect(deleteProjectSpy).toHaveBeenCalledWith('123');
 			expect(router.push).toHaveBeenCalledWith({ name: VIEWS.HOMEPAGE });
 		}
 	});
