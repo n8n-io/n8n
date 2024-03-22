666867a236bce519dbd1a8f9162d4ced1b80d567
Milorad FIlipović <milorad@n8n.io>
2024-03-22 11:33:39 +0100

    feat: Fetch user cloud role and pass it on in website links (#8942)

--- cypress/e2e/29-templates.cy.ts
+++ cypress/e2e/29-templates.cy.ts
@@ -28,9 +28,11 @@ describe('Workflow templates', () => {
 		// Link should contain instance address and n8n version
 		mainSidebar.getters.templates().parent('a').then(($a) => {
 			const href = $a.attr('href');
-			// Link should have current instance address and n8n version
-			expect(href).to.include(`utm_instance=${window.location.origin}`);
-			expect(href).to.match(/utm_n8n_version=[0-9]+\.[0-9]+\.[0-9]+/);
+			const params = new URLSearchParams(href);
+			// Link should have all mandatory parameters expected on the website
+			expect(decodeURIComponent(`${params.get('utm_instance')}`)).to.include(window.location.origin);
+			expect(params.get('utm_n8n_version')).to.match(/[0-9]+\.[0-9]+\.[0-9]+/);
+			expect(params.get('utm_awc')).to.match(/[0-9]+/);
 		});
 		mainSidebar.getters.templates().parent('a').should('have.attr', 'target', '_blank');
 	});
