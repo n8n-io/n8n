656002d697b6477036aaf8ade46944e453287dae
ricardo <ricardoespinoza105@gmail.com>
2024-03-24 12:50:10 -0400

    fix description

--- packages/cli/src/services/hooks.service.ts
+++ packages/cli/src/services/hooks.service.ts
@@ -3,7 +3,7 @@ import { UserService } from '@/services/user.service';
 import type { AssignableRole, User } from '@/databases/entities/User';
 
 /**
- * Exposes function to be used by the cloud BE hooks.
+ * Exposes functionality to be used by the cloud BE hooks.
  * DO NOT DELETE any of the methods without making sure this is not used in cloud BE hooks.
  */
 @Service()
