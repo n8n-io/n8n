7c4ff273d4a7a7bde3022516da6209aae7b1f9bc
ricardo <ricardoespinoza105@gmail.com>
2024-03-24 12:39:33 -0400

    fix: Introduce `HooksService`

---
+++ packages/cli/src/services/hooks.service.ts
@@ -0,0 +1,20 @@
+import { Service } from 'typedi';
+import { UserService } from '@/services/user.service';
+import type { AssignableRole, User } from '@/databases/entities/User';
+
+/**
+ * Exposes function to be used by the cloud BE hooks.
+ * DO NOT DELETE any of the methods without making sure this is not used in cloud BE hooks.
+ */
+@Service()
+export class HooksService {
+	constructor(private userService: UserService) {}
+
+	/**
+	 * Invites users to the instance.
+	 * This method is used in the cloud BE hooks, to invite members during sign-up
+	 */
+	async inviteUsers(owner: User, attributes: Array<{ email: string; role: AssignableRole }>) {
+		return await this.userService.inviteUsers(owner, attributes);
+	}
+}
