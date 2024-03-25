09a9a41aa39d187fc32610ccc8f4ff0f14887516
Iván Ovejero <ivov.src@gmail.com>
2024-03-25 17:59:42 +0100

    refactor(core): Remove dead utils (no-changelog) (#8947)

--- packages/core/src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { DateTime } from 'luxon';
-
-export function toUtcDate(datetime: Date, tz: string) {
-	return DateTime.fromISO(datetime.toISOString().slice(0, -1), { zone: tz }).toUTC().toJSDate();
-}
