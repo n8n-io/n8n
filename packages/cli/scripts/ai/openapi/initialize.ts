639ed814ea2c26151130bce31f6ae1ff348f7e3c
Alex Grozav <alex@grozav.com>
2024-03-26 15:02:22 +0200

    chore: remove openapi scripts

--- packages/cli/scripts/ai/openapi/initialize.ts
+++
@@ -1,6 +0,0 @@
-import childProcess from 'child_process';
-import path from 'path';
-
-const targetPath = path.join(__dirname, 'openapi-directory');
-
-childProcess.execSync(`git clone git@github.com:APIs-guru/openapi-directory.git ${targetPath}`);
