2d07ee376348a5359fe117857eed113f90555dc2
Iván Ovejero <ivov.src@gmail.com>
2024-03-26 09:21:14 +0100

    Write migration

--- packages/cli/src/databases/migrations/mysqldb/index.ts
+++ packages/cli/src/databases/migrations/mysqldb/index.ts
@@ -53,6 +53,7 @@ import { ModifyWorkflowHistoryNodesAndConnections1695829275184 } from '../common
 import { AddGlobalAdminRole1700571993961 } from '../common/1700571993961-AddGlobalAdminRole';
 import { DropRoleMapping1705429061930 } from '../common/1705429061930-DropRoleMapping';
 import { RemoveFailedExecutionStatus1711018413374 } from '../common/1711018413374-RemoveFailedExecutionStatus';
+import { MoveSshKeysToDatabase1711390882123 } from '../common/1711390882123-MoveSshKeysToDatabase';
 
 export const mysqlMigrations: Migration[] = [
 	InitialMigration1588157391238,
@@ -109,4 +110,5 @@ export const mysqlMigrations: Migration[] = [
 	AddGlobalAdminRole1700571993961,
 	DropRoleMapping1705429061930,
 	RemoveFailedExecutionStatus1711018413374,
+	MoveSshKeysToDatabase1711390882123,
 ];
