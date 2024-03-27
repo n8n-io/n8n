5607c85e6031a3a28d582a0db153d4e8564062d4
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-03-27 11:48:59 +0100

    fix(editor): Add resource scope checking to permissions

--- packages/editor-ui/src/permissions.ts
+++ packages/editor-ui/src/permissions.ts
@@ -7,6 +7,7 @@
 import type { IUser, ICredentialsResponse, IWorkflowDb } from '@/Interface';
 import { hasPermission } from './rbac/permissions';
 import { isUserGlobalOwner } from './utils/userUtils';
+import type { CredentialScope, WorkflowScope } from '@n8n/permissions';
 
 /**
  * Old permissions implementation
@@ -62,43 +63,35 @@ export const parsePermissionsTable = (
  */
 
 export const getCredentialPermissions = (user: IUser | null, credential: ICredentialsResponse) => {
-	const table: IPermissionsTable = [
-		{
-			name: 'read',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'credential:read' } }),
-		},
-		{
-			name: 'save',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'credential:create' } }),
-		},
-		{
-			name: 'update',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'credential:update' } }),
-		},
-		{
-			name: 'share',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'credential:share' } }),
-		},
-		{
-			name: 'delete',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'credential:delete' } }),
-		},
+	const credentialScopes: CredentialScope[] = [
+		'credential:create',
+		'credential:read',
+		'credential:update',
+		'credential:delete',
+		'credential:list',
+		'credential:share',
 	];
-
+	const table: IPermissionsTable = credentialScopes.map((scope) => ({
+		name: scope.split(':').pop() ?? '',
+		test: () =>
+			hasPermission(['rbac'], { rbac: { scope } }) || !!credential.scopes?.includes(scope),
+	}));
 	return parsePermissionsTable(user, table);
 };
 
 export const getWorkflowPermissions = (user: IUser | null, workflow: IWorkflowDb) => {
-	const table: IPermissionsTable = [
-		{
-			name: 'updateSharing',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'workflow:share' } }),
-		},
-		{
-			name: 'delete',
-			test: () => hasPermission(['rbac'], { rbac: { scope: 'workflow:delete' } }),
-		},
+	const workflowScopes: WorkflowScope[] = [
+		'workflow:create',
+		'workflow:read',
+		'workflow:update',
+		'workflow:delete',
+		'workflow:list',
+		'workflow:share',
 	];
+	const table: IPermissionsTable = workflowScopes.map((scope) => ({
+		name: scope.split(':').pop() ?? '',
+		test: () => hasPermission(['rbac'], { rbac: { scope } }) || !!workflow.scopes?.includes(scope),
+	}));
 
 	return parsePermissionsTable(user, table);
 };
