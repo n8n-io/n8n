97658e6d6466c0f2841e07acec5e96b5c91160cf
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2024-03-27 17:16:02 +0200

    feat: Telemetry include basic llm optional promps, trigger on save workflow event (#8981)

--- packages/workflow/src/TelemetryHelpers.ts
+++ packages/workflow/src/TelemetryHelpers.ts
@@ -13,6 +13,7 @@ import type {
 import { ApplicationError } from './errors/application.error';
 import {
 	AGENT_LANGCHAIN_NODE_TYPE,
+	CHAIN_LLM_LANGCHAIN_NODE_TYPE,
 	CHAIN_SUMMARIZATION_LANGCHAIN_NODE_TYPE,
 	HTTP_REQUEST_NODE_TYPE,
 	LANGCHAIN_CUSTOM_TOOLS,
@@ -168,7 +169,7 @@ export function generateNodesGraph(
 		}
 
 		if (node.type === AGENT_LANGCHAIN_NODE_TYPE) {
-			nodeItem.agent = (node.parameters.agent as string) || 'conversationalAgent';
+			nodeItem.agent = (node.parameters.agent as string) ?? 'conversationalAgent';
 		} else if (node.type === HTTP_REQUEST_NODE_TYPE && node.typeVersion === 1) {
 			try {
 				nodeItem.domain = new URL(node.parameters.url as string).hostname;
@@ -228,7 +229,7 @@ export function generateNodesGraph(
 		if (options?.isCloudDeployment === true) {
 			if (node.type === OPENAI_LANGCHAIN_NODE_TYPE) {
 				nodeItem.prompts =
-					(((node.parameters?.messages as IDataObject) || {}).values as IDataObject[]) || [];
+					(((node.parameters?.messages as IDataObject) ?? {}).values as IDataObject[]) ?? [];
 			}
 
 			if (node.type === AGENT_LANGCHAIN_NODE_TYPE) {
@@ -265,15 +266,20 @@ export function generateNodesGraph(
 
 			if (node.type === CHAIN_SUMMARIZATION_LANGCHAIN_NODE_TYPE) {
 				nodeItem.prompts = (
-					(((node.parameters?.options as IDataObject) || {})
-						.summarizationMethodAndPrompts as IDataObject) || {}
+					(((node.parameters?.options as IDataObject) ?? {})
+						.summarizationMethodAndPrompts as IDataObject) ?? {}
 				).values as IDataObject;
 			}
 
 			if (LANGCHAIN_CUSTOM_TOOLS.includes(node.type)) {
 				nodeItem.prompts = {
-					description: (node.parameters?.description as string) || '',
+					description: (node.parameters?.description as string) ?? '',
 				};
+			}
+
+			if (node.type === CHAIN_LLM_LANGCHAIN_NODE_TYPE) {
+				nodeItem.prompts =
+					(((node.parameters?.messages as IDataObject) ?? {}).messageValues as IDataObject[]) ?? [];
 			}
 		}
 
