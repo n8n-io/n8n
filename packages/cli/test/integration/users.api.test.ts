bcd0898727930e4ab4a20ce5c9f51d4285de1d48
Danny Martini <danny@n8n.io>
2024-03-27 22:23:00 +0100

    add more tests for other failure cases

--- packages/cli/test/integration/users.api.test.ts
+++ packages/cli/test/integration/users.api.test.ts
@@ -542,6 +542,20 @@ describe('DELETE /users/:id', () => {
 		expect(user).toBeDefined();
 	});
 
+	test('should fail to delete a user that does not exist', async () => {
+		await ownerAgent.delete('/users/foobar').query({ transferId: '' }).expect(404);
+	});
+
+	test('should fail to transfer to a project that does not exist', async () => {
+		const member = await createMember();
+
+		await ownerAgent.delete(`/users/${member.id}`).query({ transferId: 'foobar' }).expect(404);
+
+		const user = await Container.get(UserRepository).findOneBy({ id: member.id });
+
+		expect(user).toBeDefined();
+	});
+
 	test('should fail to delete if user to delete is transferee', async () => {
 		const member = await createMember();
 		const personalProject = await getPersonalProject(member);
