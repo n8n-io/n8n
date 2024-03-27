d0d8d44fbc8939adcfafaab2757f15af87b4c76f
Ricardo Espinoza <ricardo@n8n.io>
2024-03-27 18:40:08 -0400

    improve function documentation

--- packages/cli/src/services/hooks.service.ts
+++ packages/cli/src/services/hooks.service.ts
@@ -4,7 +4,7 @@ import type { AssignableRole, User } from '@/databases/entities/User';
 
 /**
  * Exposes functionality to be used by the cloud BE hooks.
- * DO NOT DELETE any of the methods without making sure this is not used in cloud BE hooks.
+ * DO NOT DELETE or RENAME any of the methods without making sure this is not used in cloud BE hooks.
  */
 @Service()
 export class HooksService {
