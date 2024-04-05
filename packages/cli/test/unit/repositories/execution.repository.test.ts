46bafcd4991810ab0330c860ace2a82d507e79ca
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-04-05 13:47:49 +0200

    ci: Delete some duplicate code in cli tests (no-changelog) (#9049)

--- packages/cli/test/unit/repositories/execution.repository.test.ts
+++ packages/cli/test/unit/repositories/execution.repository.test.ts
@@ -1,20 +1,14 @@
-import { mock } from 'jest-mock-extended';
 import Container from 'typedi';
-import type { EntityMetadata } from '@n8n/typeorm';
-import { EntityManager, DataSource, Not, LessThanOrEqual } from '@n8n/typeorm';
+import { Not, LessThanOrEqual } from '@n8n/typeorm';
 
 import config from '@/config';
 import { ExecutionEntity } from '@db/entities/ExecutionEntity';
 import { ExecutionRepository } from '@db/repositories/execution.repository';
 
-import { mockInstance } from '../../shared/mocking';
+import { mockEntityManager } from '../../shared/mocking';
 
 describe('ExecutionRepository', () => {
-	const entityManager = mockInstance(EntityManager);
-	const dataSource = mockInstance(DataSource, { manager: entityManager });
-	dataSource.getMetadata.mockReturnValue(mock<EntityMetadata>({ target: ExecutionEntity }));
-	Object.assign(entityManager, { connection: dataSource });
-
+	const entityManager = mockEntityManager(ExecutionEntity);
 	const executionRepository = Container.get(ExecutionRepository);
 	const mockDate = new Date('2023-12-28 12:34:56.789Z');
 
