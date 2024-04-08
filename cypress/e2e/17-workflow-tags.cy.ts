ea81d4879ae81b6bc2c7fd4ba8698e5acd5bb567
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-04-08 11:04:59 +0200

    fix(editor): Fix E2E tests

--- cypress/e2e/17-workflow-tags.cy.ts
+++ cypress/e2e/17-workflow-tags.cy.ts
@@ -79,6 +79,7 @@ describe('Workflow tags', () => {
 		wf.getters.nthTagPill(1).click();
 		wf.getters.tagsDropdown().find('.el-tag__close').first().click();
 		cy.get('body').click(0, 0);
+		wf.getters.workflowTags().click();
 		wf.getters.tagPills().should('have.length', TEST_TAGS.length - 1);
 	});
 
@@ -88,6 +89,7 @@ describe('Workflow tags', () => {
 		wf.getters.nthTagPill(1).click();
 		wf.getters.tagsInDropdown().filter('.selected').first().click();
 		cy.get('body').click(0, 0);
+		wf.getters.workflowTags().click();
 		wf.getters.tagPills().should('have.length', TEST_TAGS.length - 1);
 	});
 });
--- cypress/pages/workflow.ts
+++ cypress/pages/workflow.ts
@@ -145,7 +145,7 @@ export class WorkflowPage extends BasePage {
 		},
 		addInitialNodeToCanvas: (
 			nodeDisplayName: string,
-			opts?: { keepNdvOpen?: boolean; action?: string, isTrigger?: boolean},
+			opts?: { keepNdvOpen?: boolean; action?: string; isTrigger?: boolean },
 		) => {
 			this.getters.canvasPlusButton().click();
 			this.getters.nodeCreatorSearchBar().type(nodeDisplayName);
@@ -318,7 +318,6 @@ export class WorkflowPage extends BasePage {
 				this.getters.workflowTagsInput().type(tag);
 				this.getters.workflowTagsInput().type('{enter}');
 			});
-			cy.realPress('Tab');
 			// For a brief moment the Element UI tag component shows the tags as(+X) string
 			// so we need to wait for it to disappear
 			this.getters.workflowTagsContainer().should('not.contain', `+${tags.length}`);
