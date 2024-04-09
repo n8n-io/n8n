4fb67a3013a726e04014685e9a60f43108d493b7
Csaba Tuncsik <csaba@n8n.io>
2024-04-09 15:28:13 +0200

    fix(editor): Fix projects navigation element layout

--- packages/editor-ui/src/features/projects/components/ProjectNavigation.vue
+++ packages/editor-ui/src/features/projects/components/ProjectNavigation.vue
@@ -118,7 +118,7 @@ const displayProjects = computed(() => {
 			/>
 		</ElMenu>
 		<hr class="mt-m mb-m" />
-		<ElMenu :collapse="props.collapsed" :class="$style.projectItems">
+		<ElMenu v-if="displayProjects.length" :collapse="props.collapsed" :class="$style.projectItems">
 			<N8nMenuItem
 				v-for="project in displayProjects"
 				:key="project.id"
@@ -130,7 +130,7 @@ const displayProjects = computed(() => {
 				data-test-id="project-menu-item"
 			/>
 		</ElMenu>
-		<ElMenu v-if="canCreateProjects" :collapse="props.collapsed" class="pt-s pl-xs pr-xs">
+		<ElMenu v-if="canCreateProjects" :collapse="props.collapsed" class="pl-xs pr-xs">
 			<N8nMenuItem
 				:item="addProject"
 				:compact="props.collapsed"
@@ -149,12 +149,12 @@ const displayProjects = computed(() => {
 	grid-auto-rows: auto;
 	width: 100%;
 	overflow: hidden;
-	min-height: 300px;
+	align-items: start;
 }
 
 .projectItems {
 	height: 100%;
-	padding: 0 var(--spacing-xs);
+	padding: 0 var(--spacing-xs) var(--spacing-s);
 	overflow: auto;
 }
 </style>
