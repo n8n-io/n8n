ee990833349f799084e0784266c5d1b137bd0890
Alex Grozav <alex@grozav.com>
2024-04-12 10:41:35 +0300

    chore: remove deprecated JSPlumb migration

--- packages/editor-ui/src/components/canvas/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-import type { CanvasPlugin } from '@/types';
-
-export const canvasPan: CanvasPlugin = (ctx) => {
-	ctx.instance.bind('pan', (e) => {
-		console.log('pan', e);
-	});
-};
-
-export const canvasZoom: CanvasPlugin = () => {};
