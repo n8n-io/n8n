e671b7596972217d362d38756a584de42fd79708
Elias Meire <elias@meire.dev>
2024-04-17 09:45:38 +0200

    fix(HTTP Request Node): Tolerate header name being empty (#9138)

--- packages/nodes-base/nodes/HttpRequest/V3/HttpRequestV3.node.ts
+++ packages/nodes-base/nodes/HttpRequest/V3/HttpRequestV3.node.ts
@@ -1527,7 +1527,10 @@ export class HttpRequestV3 implements INodeType {
 			if (sendHeaders && headerParameters) {
 				let additionalHeaders: IDataObject = {};
 				if (specifyHeaders === 'keypair') {
-					additionalHeaders = await reduceAsync(headerParameters, parametersToKeyValue);
+					additionalHeaders = await reduceAsync(
+						headerParameters.filter((header) => header.name),
+						parametersToKeyValue,
+					);
 				} else if (specifyHeaders === 'json') {
 					// body is specified using JSON
 					try {
