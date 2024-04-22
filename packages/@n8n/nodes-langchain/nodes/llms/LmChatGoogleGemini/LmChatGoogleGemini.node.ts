26775caf17f586b7ed1a823c685a83be61e96029
Oleg Ivaniv <me@olegivaniv.com>
2024-04-22 10:29:44 +0200

    fix(Google Gemini Chat Model Node): Ping @google/generative-ai version (no-changelog)
    Signed-off-by: Oleg Ivaniv <me@olegivaniv.com>

--- packages/@n8n/nodes-langchain/nodes/llms/LmChatGoogleGemini/LmChatGoogleGemini.node.ts
+++ packages/@n8n/nodes-langchain/nodes/llms/LmChatGoogleGemini/LmChatGoogleGemini.node.ts
@@ -218,7 +218,7 @@ export class LmChatGoogleGemini implements INodeType {
 
 		const model = new ChatGoogleGenerativeAI({
 			apiKey: credentials.apiKey as string,
-			modelName,
+			model: modelName,
 			topK: options.topK,
 			topP: options.topP,
 			temperature: options.temperature,
