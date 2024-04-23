4157807633816cc33f14826edc2ddc0dbeac02d7
Iván Ovejero <ivov.src@gmail.com>
2024-04-23 17:47:47 +0200

    Simplify paths

--- packages/cli/src/benchmark/tasks/webhook.tasks.ts
+++ packages/cli/src/benchmark/tasks/webhook.tasks.ts
@@ -1,34 +1,25 @@
 import axios from 'axios';
-import { task, beforeEach } from '../main.js';
+import { task } from '../main.js';
 
-const client = axios.create({
-	baseURL: 'http://localhost:5678/',
-});
-
-beforeEach(async () => {
-	await client.post('/rest/login', {
-		email: 'test@test.com',
-		password: 'password',
-	});
-});
+const client = axios.create({ baseURL: 'http://localhost:5678/' });
 
 task(
 	'1.1. Production workflow with authless webhook node using "Respond immediately" mode',
 	async () => {
-		await client.get('/webhook/d58b0160-2370-417b-bc0e-f3050b0c7adf');
+		await client.get('/webhook/1.1');
 	},
 );
 
 task(
 	'1.2. Production workflow with authless webhook node using "When last node finishes" mode',
 	async () => {
-		await client.get('/webhook/a669d108-7de3-4e72-b332-919a11b2328b');
+		await client.get('/webhook/1.2');
 	},
 );
 
 task(
 	'1.3. Production workflow with authless webhook node using "Using \'Respond to Webhook\' node" mode',
 	async () => {
-		await client.get('/webhook/c143e038-b0bd-46ca-9708-33b868499c61');
+		await client.get('/webhook/1.3');
 	},
 );
