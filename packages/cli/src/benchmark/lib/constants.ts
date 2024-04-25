133a2b1e448a0c41eabf18b7b19f2f6c81ab54da
Iván Ovejero <ivov.src@gmail.com>
2024-04-25 11:25:51 +0200

    Try 127.0.0.1

--- packages/cli/src/benchmark/lib/constants.ts
+++ packages/cli/src/benchmark/lib/constants.ts
@@ -1,1 +1,6 @@
-export const BACKEND_BASE_URL = 'http://localhost:5678';
+// localhost refuses connections on GH Actions runners
+export const BACKEND_BASE_URL = 'http://127.0.0.1:5678';
+
+// https://stackoverflow.com/a/72785693
+
+// https://stackoverflow.com/a/75881555
