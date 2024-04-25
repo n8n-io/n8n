bae62c82cd32820d325ef7cd29485e960cc5172e
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2024-04-25 16:30:59 +0300

    fix(MySQL Node): Query to statements splitting fix (#9207)

--- packages/nodes-base/nodes/MySql/MySql.node.ts
+++ packages/nodes-base/nodes/MySql/MySql.node.ts
@@ -11,7 +11,7 @@ export class MySql extends VersionedNodeType {
 			name: 'mySql',
 			icon: 'file:mysql.svg',
 			group: ['input'],
-			defaultVersion: 2.3,
+			defaultVersion: 2.4,
 			description: 'Get, add and update data in MySQL',
 			parameterPane: 'wide',
 		};
@@ -22,6 +22,7 @@ export class MySql extends VersionedNodeType {
 			2.1: new MySqlV2(baseDescription),
 			2.2: new MySqlV2(baseDescription),
 			2.3: new MySqlV2(baseDescription),
+			2.4: new MySqlV2(baseDescription),
 		};
 
 		super(nodeVersions, baseDescription);
