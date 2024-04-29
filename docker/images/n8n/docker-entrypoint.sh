cec3c620f95ca94c9a7869eb81d222869c5cdc36
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-04-29 15:36:54 +0100

    Add c_rehash to generate cert links for openssl

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -3,6 +3,7 @@ if [ -d /opt/custom-certificates ]; then
   echo "Trusting custom certificates from /opt/custom-certificates."
   export NODE_OPTIONS=--use-openssl-ca $NODE_OPTIONS
   export SSL_CERT_DIR=/opt/custom-certificates
+  c_rehash /opt/custom-certificates
 fi
 
 if [ "$#" -gt 0 ]; then
