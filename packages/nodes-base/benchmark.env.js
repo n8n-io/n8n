00db7b6967be6b33124a252908ab3c845d89d7fe
Iván Ovejero <ivov.src@gmail.com>
2024-05-07 13:10:55 +0200

    Restore plugin and add logs

--- packages/nodes-base/benchmark.env.js
+++ packages/nodes-base/benchmark.env.js
@@ -1,5 +1,6 @@
 const NodeEnvironment = require('jest-environment-node').TestEnvironment;
 const { Bench } = require('tinybench');
+const { withCodSpeed } = require('@codspeed/tinybench-plugin');
 
 class BenchmarkEnvironment extends NodeEnvironment {
 	constructor(config, context) {
@@ -10,10 +11,12 @@ class BenchmarkEnvironment extends NodeEnvironment {
 	}
 
 	async setup() {
+		console.log('[benchmarking] env setup');
 		await super.setup();
 	}
 
 	async teardown() {
+		console.log('[benchmarking] env teardown');
 		await this.benchmark();
 
 		await super.teardown();
@@ -24,16 +27,20 @@ class BenchmarkEnvironment extends NodeEnvironment {
 	}
 
 	setupBenchmark() {
-		this.bench = new Bench();
+		console.log('[benchmarking] this.bench setup');
+		this.bench = withCodSpeed(new Bench());
 	}
 
 	async benchmark() {
+		console.log('[benchmarking] bench warmup');
 		await this.bench.warmup();
+		console.log('[benchmarking] bench run');
 		await this.bench.run();
 	}
 
 	async handleTestEvent(event) {
 		if (event.name === 'add_test') {
+			console.log('[benchmarking] add test', event.testName);
 			this.bench.add(event.testName, event.fn);
 		}
 	}
