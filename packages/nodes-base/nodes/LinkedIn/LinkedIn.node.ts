5cfdf8b032d4872c2b6a8fd33bec38e665dcb3b0
Florin Lungu <1656976+floryn90@users.noreply.github.com>
2024-05-08 12:23:08 +0300

    feat(LinkedIn Node): Upgrade LinkedIn API version (#9307)

--- packages/nodes-base/nodes/LinkedIn/LinkedIn.node.ts
+++ packages/nodes-base/nodes/LinkedIn/LinkedIn.node.ts
@@ -172,11 +172,22 @@ export class LinkedIn implements INodeType {
 							);
 
 							const binaryPropertyName = this.getNodeParameter('binaryPropertyName', i);
-							this.helpers.assertBinaryData(i, binaryPropertyName);
+							const imageMetadata = this.helpers.assertBinaryData(i, binaryPropertyName);
 
 							const buffer = await this.helpers.getBinaryDataBuffer(i, binaryPropertyName);
 							const { uploadUrl, image } = registerObject.value;
-							await linkedInApiRequest.call(this, 'POST', uploadUrl as string, buffer, true);
+
+							const headers = {};
+							Object.assign(headers, { 'Content-Type': imageMetadata.mimeType });
+
+							await linkedInApiRequest.call(
+								this,
+								'POST',
+								uploadUrl as string,
+								buffer,
+								true,
+								headers,
+							);
 
 							const imageBody = {
 								content: {
@@ -225,11 +236,22 @@ export class LinkedIn implements INodeType {
 								);
 
 								const binaryPropertyName = additionalFields.thumbnailBinaryPropertyName as string;
-								this.helpers.assertBinaryData(i, binaryPropertyName);
+								const imageMetadata = this.helpers.assertBinaryData(i, binaryPropertyName);
 
 								const buffer = await this.helpers.getBinaryDataBuffer(i, binaryPropertyName);
 								const { uploadUrl, image } = registerObject.value;
-								await linkedInApiRequest.call(this, 'POST', uploadUrl as string, buffer, true);
+
+								const headers = {};
+								Object.assign(headers, { 'Content-Type': imageMetadata.mimeType });
+
+								await linkedInApiRequest.call(
+									this,
+									'POST',
+									uploadUrl as string,
+									buffer,
+									true,
+									headers,
+								);
 								Object.assign(articleBody.content.article, { thumbnail: image });
 							}
 
