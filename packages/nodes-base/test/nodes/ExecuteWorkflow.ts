4ccef058a6a5e753a6745f6509569564d8b67272
Iván Ovejero <ivov.src@gmail.com>
2024-05-10 19:12:14 +0200

    perf: Reduce variance in `nodes-base` benchmarks (#9363)

--- packages/nodes-base/test/nodes/ExecuteWorkflow.ts
+++ packages/nodes-base/test/nodes/ExecuteWorkflow.ts
@@ -1,11 +1,18 @@
 import nock from 'nock';
 import { WorkflowExecute } from 'n8n-core';
-import type { INodeTypes, IRun, IRunExecutionData } from 'n8n-workflow';
+import type {
+	IDeferredPromise,
+	INodeTypes,
+	IRun,
+	IRunExecutionData,
+	IWorkflowExecuteAdditionalData,
+	WorkflowExecuteMode,
+} from 'n8n-workflow';
 import { createDeferredPromise, Workflow } from 'n8n-workflow';
 import * as Helpers from './Helpers';
 import type { WorkflowTestData } from './types';
 
-export async function executeWorkflow(testData: WorkflowTestData, nodeTypes: INodeTypes) {
+export async function setupExecution(testData: WorkflowTestData, nodeTypes: INodeTypes) {
 	if (testData.nock) {
 		const { baseUrl, mocks } = testData.nock;
 		const agent = nock(baseUrl);
@@ -30,7 +37,24 @@ export async function executeWorkflow(testData: WorkflowTestData, nodeTypes: INo
 		testData,
 	);
 
-	let executionData: IRun;
+	return {
+		waitPromise,
+		additionalData,
+		executionMode,
+		testData,
+		workflowInstance,
+		nodeExecutionOrder,
+	};
+}
+
+export async function performExecution(
+	waitPromise: IDeferredPromise<IRun>,
+	additionalData: IWorkflowExecuteAdditionalData,
+	executionMode: WorkflowExecuteMode,
+	testData: WorkflowTestData,
+	workflowInstance: Workflow,
+	nodeExecutionOrder: string[],
+) {
 	const runExecutionData: IRunExecutionData = {
 		resultData: {
 			runData: {},
@@ -50,9 +74,24 @@ export async function executeWorkflow(testData: WorkflowTestData, nodeTypes: INo
 			],
 		},
 	};
-	const workflowExecute = new WorkflowExecute(additionalData, executionMode, runExecutionData);
-	executionData = await workflowExecute.processRunExecutionData(workflowInstance);
 
+	const workflowExecute = new WorkflowExecute(additionalData, executionMode, runExecutionData);
+	const finalExecutionData = await workflowExecute.processRunExecutionData(workflowInstance);
 	const result = await waitPromise.promise();
-	return { executionData, result, nodeExecutionOrder };
+
+	return { executionData: finalExecutionData, result, nodeExecutionOrder };
+}
+
+export async function executeWorkflow(testData: WorkflowTestData, nodeTypes: INodeTypes) {
+	const { waitPromise, additionalData, executionMode, workflowInstance, nodeExecutionOrder } =
+		await setupExecution(testData, nodeTypes);
+
+	return await performExecution(
+		waitPromise,
+		additionalData,
+		executionMode,
+		testData,
+		workflowInstance,
+		nodeExecutionOrder,
+	);
 }
