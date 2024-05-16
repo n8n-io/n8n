f062390f92f2320e450d5a0ae2e742b144aba611
Omar Ajoue <krynble@gmail.com>
2024-05-16 13:51:51 +0100

    fix: Credential testing for sharees

--- packages/cli/src/credentials/credentials.controller.ts
+++ packages/cli/src/credentials/credentials.controller.ts
@@ -113,8 +113,30 @@ export class CredentialsController {
 		}
 
 		const mergedCredentials = deepCopy(credentials);
+		const decryptedData = this.credentialsService.decrypt(storedCredential);
+
+		// Let's check if it's a credential within a personal project
+		// That the current user does not own.
+		// In this scenario, we need to override the payload with the db information
+		storedCredential.shared.forEach((sharedCredentials) => {
+			if (sharedCredentials.role === 'credential:owner') {
+				if (sharedCredentials.project.type === 'personal') {
+					// Find the owner of this personal project
+					sharedCredentials.project.projectRelations.forEach((projectRelation) => {
+						if (
+							projectRelation.role === 'project:personalOwner' &&
+							projectRelation.user.id !== req.user.id
+						) {
+							// If we realize that the current user does not own this credential
+							// We replace the payload with the stored decrypted data
+							mergedCredentials.data = decryptedData;
+						}
+					});
+				}
+			}
+		});
+
 		if (mergedCredentials.data && storedCredential) {
-			const decryptedData = this.credentialsService.decrypt(storedCredential);
 			mergedCredentials.data = this.credentialsService.unredact(
 				mergedCredentials.data,
 				decryptedData,
