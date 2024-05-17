7f265fc7fd09dd1b699fe0b2b9b83e6f834a8d65
Elias Meire <elias@meire.dev>
2024-05-17 11:47:02 +0200

    Remove unused import

--- packages/editor-ui/src/plugins/codemirror/completions/bracketAccess.completions.ts
+++ packages/editor-ui/src/plugins/codemirror/completions/bracketAccess.completions.ts
@@ -1,6 +1,5 @@
 import { resolveParameter } from '@/composables/useWorkflowHelpers';
 import { prefixMatch, longestCommonPrefix } from './utils';
-import type { IDataObject } from 'n8n-workflow';
 import type { Completion, CompletionContext, CompletionResult } from '@codemirror/autocomplete';
 import type { Resolved } from './types';
 import { escapeMappingString } from '@/utils/mappingUtils';
