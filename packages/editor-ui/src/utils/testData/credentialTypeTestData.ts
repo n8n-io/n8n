a80d93e5616bf9bf5d77d8f04c863f752d9bebb2
Daniil Zobov <ddzobov@gmail.com>
2024-05-20 15:47:01 +0300

    feat(Telegram Node): Add support for local bot api server (#8437)
    Co-authored-by: Jonathan Bennetts <jonathan.bennetts@gmail.com>

--- packages/editor-ui/src/utils/testData/credentialTypeTestData.ts
+++ packages/editor-ui/src/utils/testData/credentialTypeTestData.ts
@@ -26,10 +26,17 @@ export const credentialTypeTelegram = {
 			description:
 				'Chat with the <a href="https://telegram.me/botfather">bot father</a> to obtain the access token',
 		},
+		{
+			displayName: 'Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: 'https://api.telegram.org',
+			description: 'Base URL for Telegram Bot API',
+		},
 	],
 	test: {
 		request: {
-			baseURL: '=https://api.telegram.org/bot{{$credentials.accessToken}}',
+			baseURL: '={{$credentials.baseUrl}}/bot{{$credentials.accessToken}}',
 			url: '/getMe',
 		},
 	},
