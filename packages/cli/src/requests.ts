b585777c79eaae4661fdab3dd33cfc200cc030ed
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-05-23 19:08:01 +0200

    refactor(core): Use consistent CSRF state validation across oAuth controllers (#9104)
    Co-authored-by: Danny Martini <danny@n8n.io>

--- packages/cli/src/requests.ts
+++ packages/cli/src/requests.ts
@@ -377,7 +377,7 @@ export declare namespace OAuthRequest {
 			{},
 			{},
 			{},
-			{ oauth_verifier: string; oauth_token: string; cid: string }
+			{ oauth_verifier: string; oauth_token: string; state: string }
 		> & {
 			user?: User;
 		};
