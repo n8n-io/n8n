9128dab8e0e953f07000317b207f48b2649c17a7
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-05-28 11:51:40 +0200

    fix(editor): Show execution error also if there is no error stack

--- packages/editor-ui/src/views/NodeView.vue
+++ packages/editor-ui/src/views/NodeView.vue
@@ -1329,8 +1329,8 @@ export default defineComponent({
 				}
 
 				if (
-					(!nodeErrorFound && data.data.resultData.error.stack) ||
-					data.data.resultData.error.message
+					!nodeErrorFound &&
+					(data.data.resultData.error.stack || data.data.resultData.error.message)
 				) {
 					// Display some more information for now in console to make debugging easier
 					console.error(`Execution ${executionId} error:`);
