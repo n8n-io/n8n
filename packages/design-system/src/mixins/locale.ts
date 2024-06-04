fad053ca9dda3101a88025ab5cde7f0bae1a7dcb
Iván Ovejero <ivov.src@gmail.com>
2024-06-04 09:29:16 +0200

    refactor: Enforce `strict` mode for design system

--- packages/design-system/src/mixins/locale.ts
+++ packages/design-system/src/mixins/locale.ts
@@ -3,7 +3,7 @@ import { t } from '../locale';
 export default {
 	methods: {
 		t(path: string, ...args: string[]) {
-			return t.call(this, path, ...args);
+			return t.call(this, path, args);
 		},
 	},
 };
