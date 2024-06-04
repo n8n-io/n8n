019bcabe8a89aa40805ade18bca7b58aa394fc01
Danny Martini <danny@n8n.io>
2024-06-04 14:12:41 +0200

    remove todo

--- packages/cli/test/integration/workflows/workflows.controller.test.ts
+++ packages/cli/test/integration/workflows/workflows.controller.test.ts
@@ -261,7 +261,6 @@ describe('POST /workflows', () => {
 		//
 		const tag = await createTag({ name: 'A' });
 		const workflow = makeWorkflow();
-		// TODO: use helpers instead
 		const project = await projectRepository.save(
 			projectRepository.create({
 				name: 'Team Project',
