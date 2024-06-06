cc6f6c2084d95969c81c6ffd3c7305e5d4affbcb
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2024-06-06 17:14:55 +0200

    fix: Reset pagination when output size changes (#9652)

--- packages/editor-ui/src/components/RunData.vue
+++ packages/editor-ui/src/components/RunData.vue
@@ -1094,6 +1094,9 @@ export default defineComponent({
 		jsonData(data: IDataObject[], prevData: IDataObject[]) {
 			if (isEqual(data, prevData)) return;
 			this.refreshDataSize();
+			if (this.dataCount) {
+				this.resetCurrentPageIfTooFar();
+			}
 			this.showPinDataDiscoveryTooltip(data);
 		},
 		binaryData(newData: IBinaryKeyData[], prevData: IBinaryKeyData[]) {
@@ -1411,12 +1414,16 @@ export default defineComponent({
 				items_total: this.dataCount,
 			});
 		},
-		onPageSizeChange(pageSize: number) {
-			this.pageSize = pageSize;
+		resetCurrentPageIfTooFar() {
 			const maxPage = Math.ceil(this.dataCount / this.pageSize);
 			if (maxPage < this.currentPage) {
 				this.currentPage = maxPage;
 			}
+		},
+		onPageSizeChange(pageSize: number) {
+			this.pageSize = pageSize;
+
+			this.resetCurrentPageIfTooFar();
 
 			this.$telemetry.track('User changed ndv page size', {
 				node_type: this.activeNode?.type,
