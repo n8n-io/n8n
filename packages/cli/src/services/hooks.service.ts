d793d48e71b7c7de81ccb58eb2c4f5d8f70af7bf
Ricardo Espinoza <ricardo@n8n.io>
2024-06-05 21:01:21 -0400

    Add more methods used in the hooks to the service

--- packages/cli/src/services/hooks.service.ts
+++ packages/cli/src/services/hooks.service.ts
@@ -1,6 +1,12 @@
 import { Service } from 'typedi';
 import { UserService } from '@/services/user.service';
 import type { AssignableRole, User } from '@/databases/entities/User';
+import { AuthService } from '@/auth/auth.service';
+import type { Response } from 'express';
+import { UserRepository } from '@/databases/repositories/user.repository';
+import { SettingsRepository } from '@/databases/repositories/settings.repository';
+import type { Settings } from '@oclif/core';
+import type { QueryDeepPartialEntity } from '@n8n/typeorm/query-builder/QueryPartialEntity';
 
 /**
  * Exposes functionality to be used by the cloud BE hooks.
@@ -8,7 +14,12 @@ import type { AssignableRole, User } from '@/databases/entities/User';
  */
 @Service()
 export class HooksService {
-	constructor(private userService: UserService) {}
+	constructor(
+		private userService: UserService,
+		private authService: AuthService,
+		private userRepository: UserRepository,
+		private settingsRepository: SettingsRepository,
+	) {}
 
 	/**
 	 * Invites users to the instance.
@@ -16,5 +27,25 @@ export class HooksService {
 	 */
 	async inviteUsers(owner: User, attributes: Array<{ email: string; role: AssignableRole }>) {
 		return await this.userService.inviteUsers(owner, attributes);
+	}
+
+	issueCookie(res: Response, user: User) {
+		return this.authService.issueCookie(res, user);
+	}
+
+	async findInstanceOwner() {
+		return await this.userRepository.findOne({ where: { role: 'global:owner' } });
+	}
+
+	async findUserById(id: string) {
+		return await this.userRepository.findOne({ where: { id } });
+	}
+
+	async saveUser(user: User) {
+		return await this.userRepository.save(user);
+	}
+
+	async updateSettings(key: keyof Settings, value: QueryDeepPartialEntity<Settings>) {
+		return await this.settingsRepository.update({ key }, { value: JSON.stringify(value) });
 	}
 }
