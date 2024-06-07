52b4ad0452bfc031aad6d4fbe127121c3f7fa429
Mutasem Aldmour <mutasem@n8n.io>
2024-06-07 16:03:27 +0200

    add tests

--- packages/editor-ui/src/api/npsSurvey.ts
+++ packages/editor-ui/src/api/npsSurvey.ts
@@ -3,5 +3,5 @@ import { makeRestApiRequest } from '@/utils/apiUtils';
 import type { NpsSurveyState } from 'n8n-workflow';
 
 export async function updateNpsSurveyState(context: IRestApiContext, state: NpsSurveyState) {
-	await makeRestApiRequest(context, 'POST', '/user-settings/nps-survey', state);
+	await makeRestApiRequest(context, 'PATCH', '/user-settings/nps-survey', state);
 }
