88f8f5edefefcdf64b43fed6feaa09889c9464ba
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-06-11 08:32:30 +0200

    minor refactors

--- cypress/e2e/27-cloud.cy.ts
+++ cypress/e2e/27-cloud.cy.ts
@@ -11,7 +11,7 @@ const mainSidebar = new MainSidebar();
 const bannerStack = new BannerStack();
 const workflowPage = new WorkflowPage();
 
-describe('Cloud', { disableAutoLogin: true }, () => {
+describe('Cloud', () => {
 	before(() => {
 		const now = new Date();
 		const fiveDaysFromNow = new Date(now.getTime() + 5 * 24 * 60 * 60 * 1000);
@@ -19,16 +19,12 @@ describe('Cloud', { disableAutoLogin: true }, () => {
 	});
 
 	beforeEach(() => {
-		cy.intercept('GET', '/rest/admin/cloud-plan', {
-			body: planData,
-		}).as('getPlanData');
-		cy.intercept('GET', '/rest/cloud/proxy/user/me', {}).as('getCloudUserInfo');
-
 		cy.overrideSettings({
 			deployment: { type: 'cloud' },
 			n8nMetadata: { userId: '1' },
 		});
-
+		cy.intercept('GET', '/rest/admin/cloud-plan', planData).as('getPlanData');
+		cy.intercept('GET', '/rest/cloud/proxy/user/me', {}).as('getCloudUserInfo');
 		cy.intercept('GET', new RegExp('/rest/projects*')).as('projects');
 		cy.intercept('GET', new RegExp('/rest/roles')).as('roles');
 	});
@@ -42,8 +38,6 @@ describe('Cloud', { disableAutoLogin: true }, () => {
 
 	describe('BannerStack', () => {
 		it('should render trial banner for opt-in cloud user', () => {
-			cy.signinAsOwner();
-
 			visitWorkflowPage();
 
 			bannerStack.getters.banner().should('be.visible');
@@ -56,8 +50,6 @@ describe('Cloud', { disableAutoLogin: true }, () => {
 
 	describe('Admin Home', () => {
 		it('Should show admin button', () => {
-			cy.signinAsOwner();
-
 			visitWorkflowPage();
 
 			mainSidebar.getters.adminPanel().should('be.visible');
@@ -66,8 +58,6 @@ describe('Cloud', { disableAutoLogin: true }, () => {
 
 	describe('Public API', () => {
 		it('Should show upgrade CTA for Public API if user is trialing', () => {
-			cy.signinAsOwner();
-
 			visitPublicApiPage();
 			cy.wait(['@loadSettings', '@projects', '@roles', '@getPlanData']);
 
