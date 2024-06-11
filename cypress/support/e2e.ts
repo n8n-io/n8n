314c7a1ed031bb5de4206cc912718787c7191f91
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-06-11 04:57:33 +0200

    bring back templates e2e tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -19,12 +19,14 @@ beforeEach(() => {
 		win.localStorage.setItem('N8N_THEME', 'light');
 	});
 
-	cy.intercept('GET', '/rest/settings', (req) =>
+	cy.intercept('GET', '/rest/settings', (req) => {
+		// Disable cache
+		delete req.headers['if-none-match'];
 		req.on('response', (res) => {
 			const defaultSettings = res.body.data;
 			res.send({ data: merge(cloneDeep(defaultSettings), settings) });
-		}),
-	).as('loadSettings');
+		});
+	}).as('loadSettings');
 	cy.intercept('GET', '/types/nodes.json').as('loadNodeTypes');
 
 	// Always intercept the request to test credentials and return a success
