d00193b255f6dbd8caaa3a5bef942c92fd44e060
Csaba Tuncsik <csaba@n8n.io>
2024-06-11 17:01:58 +0200

    fix(editor): Fix type error in unit test

--- packages/editor-ui/src/views/CredentialsView.test.ts
+++ packages/editor-ui/src/views/CredentialsView.test.ts
@@ -7,7 +7,7 @@ import CredentialsView from '@/views/CredentialsView.vue';
 import ResourcesListLayout from '@/components/layouts/ResourcesListLayout.vue';
 
 vi.mock('@/components/layouts/ResourcesListLayout.vue', async (importOriginal) => {
-	const original: InstanceType<typeof ResourcesListLayout> = await importOriginal();
+	const original = await importOriginal<ResourcesListLayout>();
 	return {
 		default: {
 			...original.default,
