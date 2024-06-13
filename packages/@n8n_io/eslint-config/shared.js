73a87bf2bb98b614566a5bfb19d66d293567ea40
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-06-13 14:43:57 +0200

    use `typescript-eslint-parser-for-extra-files`  as the eslint parser for the frontend packages

--- packages/@n8n_io/eslint-config/shared.js
+++ packages/@n8n_io/eslint-config/shared.js
@@ -2,40 +2,41 @@
  * @type {(dir: string, mode: 'frontend' | undefined) => import('@types/eslint').ESLint.ConfigData}
  */
 module.exports = (tsconfigRootDir, mode) => {
-	const isFrontend = mode === 'frontend';
-	const parser = isFrontend ? 'vue-eslint-parser' : '@typescript-eslint/parser';
-	const extraParserOptions = isFrontend
-		? {
-				extraFileExtensions: ['.vue'],
-				parser: {
-					ts: '@typescript-eslint/parser',
-					js: '@typescript-eslint/parser',
-					vue: 'vue-eslint-parser',
-					template: 'vue-eslint-parser',
-				},
-		  }
-		: {};
-
-	const settings = {
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts'],
-		},
-
-		'import/resolver': {
-			typescript: {
-				tsconfigRootDir,
-				project: './tsconfig.json',
+	const parserOptions = {
+		tsconfigRootDir,
+		project: './tsconfig.json',
+	};
+	const config = {
+		parser: '@typescript-eslint/parser',
+		parserOptions,
+		settings: {
+			'import/parsers': {
+				'@typescript-eslint/parser': ['.ts'],
+			},
+			'import/resolver': {
+				typescript: parserOptions,
 			},
 		},
 	};
 
-	return {
-		parser,
-		parserOptions: {
-			tsconfigRootDir,
-			project: ['./tsconfig.json'],
-			...extraParserOptions,
-		},
-		settings,
-	};
+	if (mode === 'frontend') {
+		config.parser = 'vue-eslint-parser';
+		config.parserOptions.extraFileExtensions = ['.vue'];
+		config.overrides = [
+			{
+				files: ['*.ts'],
+				parser: 'typescript-eslint-parser-for-extra-files',
+				parserOptions,
+			},
+			{
+				files: ['*.vue'],
+				parserOptions: {
+					parser: 'typescript-eslint-parser-for-extra-files',
+					...parserOptions,
+				},
+			},
+		];
+	}
+
+	return config;
 };
