558fbe2a7cdb11be81e9cf16aef5e90d587d58f2
Michael Kret <michael.k@radency.com>
2024-06-17 09:02:32 +0300

    pairedItem for combineBySql

--- packages/nodes-base/nodes/Merge/v3/actions/mode/combineBySql.ts
+++ packages/nodes-base/nodes/Merge/v3/actions/mode/combineBySql.ts
@@ -3,6 +3,7 @@ import type {
 	IExecuteFunctions,
 	INodeExecutionData,
 	INodeProperties,
+	IPairedItemData,
 } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 
@@ -16,13 +17,19 @@ import type { Database } from 'alasql';
 export const properties: INodeProperties[] = [
 	numberInputsProperty,
 	{
+		displayName:
+			'Input data available as tables with coresponding number(e.g. input1, input2), example:<br> <code>SELECT * FROM input1 LEFT JOIN input2 ON input1.name = input2.ID;</code>',
+		name: 'queryTooltip',
+		type: 'notice',
+		default: '',
+	},
+	{
 		displayName: 'Query',
 		name: 'query',
 		type: 'string',
 		default: '',
 		noDataExpression: true,
 		required: true,
-		hint: 'Input data available as tables with coresponding number, e.g. input1, input2, ...',
 		typeOptions: {
 			rows: 5,
 			editor: 'sqlEditor',
@@ -48,6 +55,7 @@ export const description = updateDisplayOptions(displayOptions, properties);
 export async function execute(this: IExecuteFunctions): Promise<INodeExecutionData[]> {
 	const nodeId = this.getNode().id;
 	const returnData: INodeExecutionData[] = [];
+	const pairedItem: IPairedItemData[] = [];
 
 	const inputs = getMergeNodeInputs(this);
 
@@ -56,6 +64,40 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 	try {
 		for (let i = 0; i < inputs.length; i++) {
 			const inputData = this.getInputData(i) ?? [];
+
+			inputData.forEach((item, index) => {
+				if (item.pairedItem === undefined) {
+					item.pairedItem = index;
+				}
+
+				if (typeof item.pairedItem === 'number') {
+					pairedItem.push({
+						item: item.pairedItem,
+						input: i,
+					});
+					return;
+				}
+
+				if (Array.isArray(item.pairedItem)) {
+					const pairedItems = item.pairedItem
+						.filter((p) => p !== undefined)
+						.map((p) => (typeof p === 'number' ? { item: p } : p))
+						.map((p) => {
+							return {
+								item: p.item,
+								input: i,
+							};
+						});
+					pairedItem.push(...pairedItems);
+					return;
+				}
+
+				pairedItem.push({
+					item: item.pairedItem.item,
+					input: i,
+				});
+			});
+
 			db.exec(`CREATE TABLE input${i + 1}`);
 			db.tables[`input${i + 1}`].data = inputData.map((entry) => entry.json);
 		}
@@ -78,22 +120,36 @@ export async function execute(this: IExecuteFunctions): Promise<INodeExecutionDa
 		const result: IDataObject[] = db.exec(query);
 
 		if (!simplify) {
-			returnData.push(
-				...result.map((item, index) => ({
-					json: { statementIndex: index, data: item },
-				})),
-			);
+			const isSingleStatment =
+				query
+					.replace(/\n/g, '')
+					.split(';')
+					.filter((q) => q).length === 1;
+
+			if (isSingleStatment) {
+				returnData.push({
+					json: { statementIndex: 0, data: result },
+					pairedItem,
+				});
+			} else {
+				returnData.push(
+					...result.map((item, index) => ({
+						json: { statementIndex: index, data: item },
+						pairedItem,
+					})),
+				);
+			}
 		} else {
 			for (const item of result) {
 				if (Array.isArray(item)) {
-					returnData.push(...item.map((json) => ({ json })));
+					returnData.push(...item.map((json) => ({ json, pairedItem })));
 				} else if (typeof item === 'object') {
-					returnData.push({ json: item });
+					returnData.push({ json: item, pairedItem });
 				}
 			}
 
 			if (!returnData.length) {
-				returnData.push({ json: { success: true } });
+				returnData.push({ json: { success: true }, pairedItem });
 			}
 		}
 	} catch (error) {
