d98073dede4237e0301936605bd08fbccbc754f8
Mutasem Aldmour <mutasem@n8n.io>
2024-06-18 17:39:48 +0200

    test: add e2e tests

---
+++ cypress/e2e/2111-ado-support-pinned-data-in-expressions.cy.ts
@@ -0,0 +1,62 @@
+import { WorkflowPage, NDV } from '../pages';
+
+const workflowPage = new WorkflowPage();
+const ndv = new NDV();
+
+describe('ADO-2111 expressions should support pinned data', () => {
+	beforeEach(() => {
+		workflowPage.actions.visit();
+	});
+
+	it('supports pinned data in expressions unexecuted and executed parent nodes', () => {
+		cy.createFixtureWorkflow('Test_workflow_pinned_data_in_expressions.json', 'Expressions');
+
+		// test previous node unexecuted
+		workflowPage.actions.openNode('NotPinnedSet1');
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(0)
+			.should('include.text', 'Joe\nJoe\nJoan\nJoan\nJoe\nJoan\n\nJoe\nJoan\n\nJoe');
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(1)
+			.should('contain.text', '0,0\nJoe\n\nJoe\n\nJoe\n\nJoe\nJoe');
+
+		// test can resolve correctly based on item
+		ndv.actions.switchInputMode('Table');
+
+		ndv.getters.inputTableRow(2).realHover();
+		cy.wait(50);
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(0)
+			.should('include.text', 'Joe\nJoe\nJoan\nJoan\nJoe\nJoan\n\nJoe\nJoan\n\nJoe');
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(1)
+			.should('contain.text', '0,1\nJoan\n\nJoan\n\nJoan\n\nJoan\nJoan');
+
+		// test previous node executed
+		ndv.actions.execute();
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(0)
+			.should('include.text', 'Joe\nJoe\nJoan\nJoan\nJoe\nJoan\n\nJoe\nJoan\n\nJoe');
+
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(1)
+			.should('contain.text', '0,0\nJoe\n\nJoe\n\nJoe\n\nJoe\nJoe');
+
+		ndv.getters.inputTableRow(2).realHover();
+		cy.wait(50);
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(0)
+			.should('include.text', 'Joe\nJoe\nJoan\nJoan\nJoe\nJoan\n\nJoe\nJoan\n\nJoe');
+		ndv.getters
+			.parameterExpressionPreview('value')
+			.eq(1)
+			.should('contain.text', '0,1\nJoan\n\nJoan\n\nJoan\n\nJoan\nJoan');
+	});
+});
