cd89317223c2a6d65c5fa70a0b53ca4752eeed20
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-06-19 11:10:26 +0200

    update docs

--- packages/workflow/src/utils.ts
+++ packages/workflow/src/utils.ts
@@ -187,7 +187,7 @@ export function randomInt(min: number, max: number): number;
  * Generates a random integer within a specified range.
  *
  * @param {number} min - The lower bound of the range. If `max` is not provided, this value is used as the upper bound and the lower bound is set to 0.
- * @param {number} [max] - The upper bound of the range.
+ * @param {number} [max] - The upper bound of the range, not inclusive.
  * @returns {number} A random integer within the specified range.
  */
 export function randomInt(min: number, max?: number): number {
