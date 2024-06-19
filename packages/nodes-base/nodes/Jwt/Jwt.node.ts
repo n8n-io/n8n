1f1f265c26b98423b8b9fe5fc4a38b3882927bee
ekadin-mtc <105028623+ekadin-mtc@users.noreply.github.com>
2024-06-18 23:31:14 -0400

    Update Jwt.node.ts to include option to set Key Id

--- packages/nodes-base/nodes/Jwt/Jwt.node.ts
+++ packages/nodes-base/nodes/Jwt/Jwt.node.ts
@@ -261,6 +261,19 @@ export class Jwt implements INodeType {
 						},
 					},
 					{
+            displayName: 'Key ID',
+            name: 'kid',
+            type: 'string',
+            placeholder: 'e.g. 123456',
+						default: '',
+						description: 'The kid (key ID) claim is an optional header claim, used to specify the key for validating the signature.',
+            displayOptions: {
+							show: {
+								'/operation': ['sign'],
+							},
+						},
+          },
+					{
 						displayName: 'Override Algorithm',
 						name: 'algorithm',
 						type: 'options',
@@ -349,6 +362,7 @@ export class Jwt implements INodeType {
 				ignoreExpiration?: boolean;
 				ignoreNotBefore?: boolean;
 				clockTolerance?: number;
+				kid?: string;
 			};
 
 			try {
@@ -375,6 +389,16 @@ export class Jwt implements INodeType {
 						secretOrPrivateKey = formatPrivateKey(credentials.privateKey);
 					}
 
+					if (options.kid) {
+					const token = jwt.sign(payload, secretOrPrivateKey, {
+						algorithm: options.algorithm ?? credentials.algorithm,
+						keyid: options.kid,
+					});
+					returnData.push({
+						json: { token },
+						pairedItem: itemIndex,
+					});
+				} else {
 					const token = jwt.sign(payload, secretOrPrivateKey, {
 						algorithm: options.algorithm ?? credentials.algorithm,
 					});
