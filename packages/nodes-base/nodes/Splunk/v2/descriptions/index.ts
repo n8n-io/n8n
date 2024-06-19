8e0ee1f20e20fb527f347b791e82cdf750f52a8e
Michael Kret <michael.k@radency.com>
2024-06-19 12:12:23 +0300

    setup wip

---
+++ packages/nodes-base/nodes/Splunk/v2/descriptions/index.ts
@@ -0,0 +1,5 @@
+export * from './FiredAlertDescription';
+export * from './SearchConfigurationDescription';
+export * from './SearchJobDescription';
+export * from './SearchResultDescription';
+export * from './UserDescription';
