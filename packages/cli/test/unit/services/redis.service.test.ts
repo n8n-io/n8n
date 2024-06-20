bfb1056f60ec3e2df0eab5508b712f421ff9caa2
Iván Ovejero <ivov.src@gmail.com>
2024-06-20 09:42:28 +0200

    Simplify tests

--- packages/cli/test/unit/services/redis.service.test.ts
+++ packages/cli/test/unit/services/redis.service.test.ts
@@ -4,21 +4,6 @@ import config from '@/config';
 import { RedisService } from '@/services/redis.service';
 import { mockInstance } from '../../shared/mocking';
 
-jest.mock('ioredis', () => {
-	const Redis = require('ioredis-mock');
-	if (typeof Redis === 'object') {
-		// the first mock is an ioredis shim because ioredis-mock depends on it
-		// https://github.com/stipsan/ioredis-mock/blob/master/src/index.js#L101-L111
-		return {
-			Command: { _transformer: { argument: {}, reply: {} } },
-		};
-	}
-	// second mock for our code
-	return function (...args: any) {
-		return new Redis(args);
-	};
-});
-
 mockInstance(Logger);
 const redisService = Container.get(RedisService);
 
