130e4720b57293378b8019a04edd0287de72b25a
Michael Kret <michael.k@radency.com>
2024-06-21 09:59:46 +0300

    create report operation

--- packages/nodes-base/nodes/Splunk/v2/actions/node.type.ts
+++ packages/nodes-base/nodes/Splunk/v2/actions/node.type.ts
@@ -2,7 +2,7 @@ import type { AllEntities } from 'n8n-workflow';
 
 type NodeMap = {
 	alert: 'getReport';
-	report: 'deleteReport' | 'get' | 'getAll';
+	report: 'create' | 'deleteReport' | 'get' | 'getAll';
 	search: 'create' | 'deleteJob' | 'get' | 'getAll' | 'getResult';
 	user: 'create' | 'deleteUser' | 'get' | 'getAll' | 'update';
 };
--- packages/nodes-base/nodes/Splunk/v2/actions/report/index.ts
+++ packages/nodes-base/nodes/Splunk/v2/actions/report/index.ts
@@ -1,10 +1,11 @@
 import type { INodeProperties } from 'n8n-workflow';
 
+import * as create from './create.operation';
 import * as deleteReport from './deleteReport.operation';
 import * as get from './get.operation';
 import * as getAll from './getAll.operation';
 
-export { deleteReport, get, getAll };
+export { create, deleteReport, get, getAll };
 
 export const description: INodeProperties[] = [
 	{
@@ -19,27 +20,34 @@ export const description: INodeProperties[] = [
 		},
 		options: [
 			{
+				name: 'Create From Search',
+				value: 'create',
+				description: 'Create a search report from a search job',
+				action: 'Create a search report',
+			},
+			{
 				name: 'Delete',
 				value: 'deleteReport',
-				description: 'Delete a search configuration',
-				action: 'Delete a search configuration',
+				description: 'Delete a search report',
+				action: 'Delete a search report',
 			},
 			{
 				name: 'Get',
 				value: 'get',
-				description: 'Retrieve a search configuration',
-				action: 'Get a search configuration',
+				description: 'Retrieve a search report',
+				action: 'Get a search report',
 			},
 			{
 				name: 'Get Many',
 				value: 'getAll',
-				description: 'Retrieve many search configurations',
-				action: 'Get many search configurations',
+				description: 'Retrieve many search reports',
+				action: 'Get many search reports',
 			},
 		],
 		default: 'getAll',
 	},
 
+	...create.description,
 	...deleteReport.description,
 	...get.description,
 	...getAll.description,
