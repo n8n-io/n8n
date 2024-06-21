62defc5540ae930fc856f9ccf0527a3186f21441
BramKn <bram@knitco.nl>
2024-06-21 14:23:34 +0200

    Elastic

---
+++ packages/nodes-base/credentials/ElasticApi.credentials.ts
@@ -0,0 +1,60 @@
+import type {
+	ICredentialDataDecryptedObject,
+	ICredentialTestRequest,
+	ICredentialType,
+	IHttpRequestOptions,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class ElasticApi implements ICredentialType {
+	name = 'elasticApi';
+
+	displayName = 'Elastic API';
+
+	documentationUrl = 'elastic';
+
+	icon = { light: 'file:icons/Elastic.svg', dark: 'file:icons/Elastic.svg' } as const;
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Base URL',
+			name: 'baseUrl',
+			type: 'string',
+			default: '',
+			placeholder: 'e.g. https://mydeployment.kb.us-central1.gcp.cloud.es.io:9243',
+			description: "Referred to as Kibana 'endpoint' in the Elastic deployment dashboard",
+			required: true,
+		},
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			required: true,
+			type: 'string',
+			typeOptions: { password: true },
+			default: '',
+		},
+	];
+
+	async authenticate(
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> {
+		requestOptions.headers = {
+			Authorization: 'ApiKey ' + credentials.apiKey,
+		};
+
+		if (requestOptions.method === 'GET') {
+			delete requestOptions.body;
+		}
+
+		return requestOptions;
+	}
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials.baseUrl}}',
+			url: '/api/endpoint/metadata',
+			method: 'GET',
+		},
+	};
+}
