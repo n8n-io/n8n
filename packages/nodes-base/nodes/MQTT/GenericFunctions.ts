c4994764930f455341cf266608a2a7520d5bf00a
Elias Meire <elias@meire.dev>
2024-06-24 18:07:58 +0200

    Improve credential type, remove non-null assertion

--- packages/nodes-base/nodes/MQTT/GenericFunctions.ts
+++ packages/nodes-base/nodes/MQTT/GenericFunctions.ts
@@ -2,7 +2,7 @@ import { connectAsync, type IClientOptions, type MqttClient } from 'mqtt';
 import { randomString } from 'n8n-workflow';
 import { formatPrivateKey } from '@utils/utilities';
 
-export interface MqttCredential {
+interface BaseMqttCredential {
 	protocol: 'mqtt' | 'mqtts' | 'ws';
 	host: string;
 	port: number;
@@ -10,13 +10,21 @@ export interface MqttCredential {
 	password: string;
 	clean: boolean;
 	clientId: string;
-	ssl: boolean;
 	passwordless?: boolean;
-	ca?: string;
-	rejectUnauthorized?: boolean;
-	cert?: string;
-	key?: string;
 }
+
+type NonSslMqttCredential = BaseMqttCredential & {
+	ssl: false;
+};
+
+type SslMqttCredential = BaseMqttCredential & {
+	ssl: true;
+	ca: string;
+	cert: string;
+	key: string;
+	rejectUnauthorized?: boolean;
+};
+export type MqttCredential = NonSslMqttCredential | SslMqttCredential;
 
 export const createClient = async (credentials: MqttCredential): Promise<MqttClient> => {
 	const { protocol, host, port, clean, clientId, username, password } = credentials;
@@ -36,8 +44,8 @@ export const createClient = async (credentials: MqttCredential): Promise<MqttCli
 
 	if (credentials.ssl) {
 		clientOptions.ca = formatPrivateKey(credentials.ca!);
-		clientOptions.cert = formatPrivateKey(credentials.cert!);
-		clientOptions.key = formatPrivateKey(credentials.key!);
+		clientOptions.cert = formatPrivateKey(credentials.cert);
+		clientOptions.key = formatPrivateKey(credentials.key);
 		clientOptions.rejectUnauthorized = credentials.rejectUnauthorized;
 	}
 
