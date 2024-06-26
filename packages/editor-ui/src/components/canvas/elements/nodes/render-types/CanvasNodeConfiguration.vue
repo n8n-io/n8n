e995309789c886f9203290e6973149ad96102fb2
Alex Grozav <alex@grozav.com>
2024-06-26 16:56:58 +0300

    feat(editor): Add node enable/disable functionality in new canvas (no-changelog) (#9872)

--- packages/editor-ui/src/components/canvas/elements/nodes/render-types/CanvasNodeConfiguration.vue
+++ packages/editor-ui/src/components/canvas/elements/nodes/render-types/CanvasNodeConfiguration.vue
@@ -1,17 +1,22 @@
 <script lang="ts" setup>
 import { computed, inject, useCssModule } from 'vue';
 import { CanvasNodeKey } from '@/constants';
+import { useI18n } from '@/composables/useI18n';
 
 const node = inject(CanvasNodeKey);
 
 const $style = useCssModule();
+const i18n = useI18n();
 
 const label = computed(() => node?.label.value ?? '');
+
+const isDisabled = computed(() => node?.data.value.disabled ?? false);
 
 const classes = computed(() => {
 	return {
 		[$style.node]: true,
 		[$style.selected]: node?.selected.value,
+		[$style.disabled]: isDisabled.value,
 	};
 });
 </script>
@@ -19,24 +24,26 @@ const classes = computed(() => {
 <template>
 	<div :class="classes" data-test-id="canvas-node-configuration">
 		<slot />
-		<div v-if="label" :class="$style.label">{{ label }}</div>
+		<div v-if="label" :class="$style.label">
+			{{ label }}
+			<div v-if="isDisabled">({{ i18n.baseText('node.disabled') }})</div>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" module>
 .node {
-	width: 75px;
-	height: 75px;
+	--canvas-node--width: 75px;
+	--canvas-node--height: 75px;
+
+	width: var(--canvas-node--width);
+	height: var(--canvas-node--height);
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	background: var(--canvas-node--background, var(--node-type-supplemental-background));
 	border: 2px solid var(--canvas-node--border-color, var(--color-foreground-dark));
 	border-radius: 50%;
-}
-
-.selected {
-	box-shadow: 0 0 0 4px var(--color-canvas-selected);
 }
 
 .label {
@@ -47,5 +54,13 @@ const classes = computed(() => {
 	width: 100%;
 	min-width: 200px;
 	margin-top: var(--spacing-2xs);
+}
+
+.selected {
+	box-shadow: 0 0 0 4px var(--color-canvas-selected);
+}
+
+.disabled {
+	border-color: var(--color-canvas-node-disabled-border, var(--color-foreground-base));
 }
 </style>
