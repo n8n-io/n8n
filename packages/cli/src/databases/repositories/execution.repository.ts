536b29c03ce94f0c827bc6bd7bf333078d075ab7
Iván Ovejero <ivov.src@gmail.com>
2024-07-02 15:11:30 +0200

    fix(core): Throw on adding execution without execution data (#9903)

--- packages/cli/src/databases/repositories/execution.repository.ts
+++ packages/cli/src/databases/repositories/execution.repository.ts
@@ -42,6 +42,8 @@ import { ExecutionDataRepository } from './executionData.repository';
 import { Logger } from '@/Logger';
 import type { ExecutionSummaries } from '@/executions/execution.types';
 import { PostgresLiveRowsRetrievalError } from '@/errors/postgres-live-rows-retrieval.error';
+import { separate } from '@/utils';
+import { ErrorReporterProxy as ErrorReporter } from 'n8n-workflow';
 
 export interface IGetExecutionsQueryFilter {
 	id?: FindOperator<string> | string;
@@ -155,7 +157,9 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 		const executions = await this.find(queryParams);
 
 		if (options?.includeData && options?.unflattenData) {
-			return executions.map((execution) => {
+			const [valid, invalid] = separate(executions, (e) => e.executionData !== null);
+			this.reportInvalidExecutions(invalid);
+			return valid.map((execution) => {
 				const { executionData, metadata, ...rest } = execution;
 				return {
 					...rest,
@@ -165,7 +169,9 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 				} as IExecutionResponse;
 			});
 		} else if (options?.includeData) {
-			return executions.map((execution) => {
+			const [valid, invalid] = separate(executions, (e) => e.executionData !== null);
+			this.reportInvalidExecutions(invalid);
+			return valid.map((execution) => {
 				const { executionData, metadata, ...rest } = execution;
 				return {
 					...rest,
@@ -180,6 +186,16 @@ export class ExecutionRepository extends Repository<ExecutionEntity> {
 			const { executionData, ...rest } = execution;
 			return rest;
 		});
+	}
+
+	reportInvalidExecutions(executions: ExecutionEntity[]) {
+		if (executions.length === 0) return;
+
+		ErrorReporter.error(
+			new ApplicationError('Found executions without executionData', {
+				extra: { executionIds: executions.map(({ id }) => id) },
+			}),
+		);
 	}
 
 	async findSingleExecution(
