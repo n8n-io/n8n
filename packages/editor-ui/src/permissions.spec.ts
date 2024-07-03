72bbff20ad7d0a66dd01d08e4bb9525a667396e8
Csaba Tuncsik <csaba@n8n.io>
2024-07-03 21:02:01 +0200

    test: Unit test getResourcePermissions

--- packages/editor-ui/src/permissions.spec.ts
+++ packages/editor-ui/src/permissions.spec.ts
@@ -3,6 +3,7 @@ import {
 	getProjectPermissions,
 	getCredentialPermissions,
 	getWorkflowPermissions,
+	getResourcePermissions,
 } from '@/permissions';
 import type { ICredentialsResponse, IUser, IWorkflowDb } from '@/Interface';
 import type { Project } from '@/types/projects.types';
@@ -115,6 +116,80 @@ describe('permissions', () => {
 			share: true,
 			execute: true,
 			move: true,
+		});
+	});
+
+	it('getResourcePermissions', () => {
+		expect(
+			getResourcePermissions([
+				'credential:create',
+				'credential:delete',
+				'credential:list',
+				'credential:move',
+				'credential:read',
+				'credential:share',
+				'credential:update',
+				'eventBusDestination:list',
+				'eventBusDestination:test',
+				'project:list',
+				'project:read',
+				'tag:create',
+				'tag:list',
+				'tag:read',
+				'tag:update',
+				'user:list',
+				'variable:list',
+				'variable:read',
+				'workflow:create',
+				'workflow:delete',
+				'workflow:execute',
+				'workflow:list',
+				'workflow:move',
+				'workflow:read',
+				'workflow:share',
+				'workflow:update',
+			]),
+		).toEqual({
+			credential: {
+				create: true,
+				delete: true,
+				list: true,
+				move: true,
+				read: true,
+				share: true,
+				update: true,
+			},
+			eventBusDestination: {
+				list: true,
+				test: true,
+			},
+			project: {
+				list: true,
+				read: true,
+			},
+			tag: {
+				create: true,
+				list: true,
+				read: true,
+				update: true,
+			},
+			user: {
+				list: true,
+			},
+			variable: {
+				list: true,
+				read: true,
+			},
+			workflow: {
+				create: true,
+				delete: true,
+				execute: true,
+				list: true,
+				move: true,
+				read: true,
+				share: true,
+				update: true,
+			},
 		});
 	});
 });
