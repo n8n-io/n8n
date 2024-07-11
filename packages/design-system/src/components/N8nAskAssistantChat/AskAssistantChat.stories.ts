271b628aa03c01bc2f39f4058594701eeeb7d972
Mutasem Aldmour <mutasem@n8n.io>
2024-07-11 12:54:08 +0200

    feat: add code section

--- packages/design-system/src/components/N8nAskAssistantChat/AskAssistantChat.stories.ts
+++ packages/design-system/src/components/N8nAskAssistantChat/AskAssistantChat.stories.ts
@@ -38,8 +38,25 @@ const messages: AssistantMessage[] = [
 		type: 'code-diff',
 		role: 'assistant',
 		description: 'Short solution description here that can spill over to two lines',
-		codeDiff:
-			'--- original.js\n+++ modified.js\n- cons a = 1\n+ const a = 1\n\n+for (const item of items) {\n+  item.json.myNewField = 1;\n+}\n\n+return items;',
+		// codeDiff:
+		// '--- original.js\n+++ modified.js\n- cons a = 1\n+ const a = 1\n\n+for (const item of items) {\n+  item.json.myNewField = 1;\n+}\n\n+return items;',
+		codeDiff: `@@ -1,7 +1,6 @@
+-The Way that can be told of is not the eternal Way;
+-The name that can be named is not the eternal name.
+ The Nameless is the origin of Heaven and Earth;
+-The Named is the mother of all things.
++The named is the mother of all things.
++
+ Therefore let there always be non-being,
+   so we may see their subtlety,
+ And let there always be being,
+@@ -9,3 +8,6 @@
+ The two are the same,
+ But after they are produced,
+   they have different names.
++They both may be called deep and profound.
++Deeper and more profound,
++The door of all subtleties!`,
 	},
 	{
 		type: 'quick-replies',
--- packages/design-system/src/components/N8nAskAssistantChat/CodeDiff.vue
+++ packages/design-system/src/components/N8nAskAssistantChat/CodeDiff.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import parseDiff from 'parse-diff';
+import { computed } from 'vue';
+
 interface Props {
 	title: string;
 	content: string;
@@ -10,6 +13,28 @@ const props = withDefaults(defineProps<Props>(), {
 	replacing: false,
 	replaced: false,
 });
+
+const diffs = computed(() => {
+	const parsed = parseDiff(props.content);
+
+	const file = parsed[0];
+
+	return file.chunks.reduce((accu: parseDiff.Change[], chunk) => {
+		const changes = chunk.changes.map((change) => {
+			let content = change.content;
+			if (change.type === 'add' && content.startsWith('+')) {
+				content = content.replace('+', '');
+			} else if (change.type === 'del' && content.startsWith('-')) {
+				content = content.replace('-', '');
+			}
+			return {
+				...change,
+				content,
+			};
+		});
+		return [...accu, ...changes];
+	}, []);
+});
 </script>
 
 <template>
@@ -17,10 +42,20 @@ const props = withDefaults(defineProps<Props>(), {
 		<div :class="$style.title">
 			{{ props.title }}
 		</div>
-		<div>
-			{{ props.content }}
+		<div :class="$style.diffSection">
+			<div v-for="(diff, i) in diffs" :key="i" :class="$style.diff">
+				<div :class="$style.lineNumber">
+					{{ diff.type === 'normal' ? diff.ln1 : diff.ln }}
+				</div>
+				<div :class="[$style[diff.type], $style.diffContent]">
+					<span v-if="diff.type === 'add'">&nbsp;+&nbsp;</span>
+					<span v-else-if="diff.type === 'del'">&nbsp;-&nbsp;</span>
+					<span v-else>&nbsp;&nbsp;&nbsp;</span>
+					<span>{{ diff.content }}</span>
+				</div>
+			</div>
 		</div>
-		<div>
+		<div :class="$style.actions">
 			<n8n-button type="primary" size="mini">Replace my code</n8n-button>
 			<!-- <button>Undo</button> -->
 		</div>
@@ -38,5 +73,46 @@ const props = withDefaults(defineProps<Props>(), {
 	padding: 8px;
 	font-weight: 600;
 	font-size: 12px;
+}
+
+.lineNumber {
+	font-size: 10px;
+	min-width: 18px;
+	max-width: 18px;
+	text-align: center;
+	background-color: var(--color-foreground-light);
+	border-right: var(--border-base);
+}
+
+.diffSection {
+	overflow-x: scroll;
+	border-top: var(--border-base);
+	border-bottom: var(--border-base);
+}
+
+.diff {
+	display: flex;
+	font-size: 10px;
+	line-height: 18px; /* 100% */
+}
+
+.diffContent {
+	// flex-grow: 1;
+	width: 100%;
+	text-wrap: nowrap;
+}
+
+.add {
+	color: var(--color-success);
+	background-color: var(--color-success-tint-2);
+}
+
+.del {
+	color: var(--color-danger);
+	background-color: var(--color-danger-tint-2);
+}
+
+.actions {
+	padding: 8px;
 }
 </style>
