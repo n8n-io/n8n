995792363880ccdf48c4fcbc5edec6b02e8b8e84
Mutasem Aldmour <mutasem@n8n.io>
2024-07-12 12:28:04 +0200

    fix: hide button if open

--- packages/editor-ui/src/components/AskAssistant/AskAssistantFloatingButton.vue
+++ packages/editor-ui/src/components/AskAssistant/AskAssistantFloatingButton.vue
@@ -2,16 +2,16 @@
 import { useAssistantStore } from '@/stores/assistant.store';
 import { computed } from 'vue';
 
-const { openChat, chatEnabled, chatWindowOpen } = useAssistantStore();
+const assistantStore = useAssistantStore();
 
 const shouldShow = computed(() => {
-	return chatEnabled && !chatWindowOpen;
+	return assistantStore.chatEnabled && !assistantStore.chatWindowOpen;
 });
 </script>
 
 <template>
 	<div v-if="shouldShow" :class="$style.container">
-		<n8n-a-i-assistant-button @click="openChat" />
+		<n8n-a-i-assistant-button @click="assistantStore.openChat" />
 	</div>
 </template>
 
