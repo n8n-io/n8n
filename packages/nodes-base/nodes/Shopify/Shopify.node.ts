58291b48097dbac3ec9c5f5f5897a9b728416baa
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-07-15 15:52:52 +0100

    improve notice

--- packages/nodes-base/nodes/Shopify/Shopify.node.ts
+++ packages/nodes-base/nodes/Shopify/Shopify.node.ts
@@ -64,7 +64,7 @@ export class Shopify implements INodeType {
 		],
 		properties: [
 			{
-				displayName: '2024-07',
+				displayName: 'Shopify API Version: 2024-07',
 				type: 'notice',
 				name: 'apiVersion',
 				default: '',
