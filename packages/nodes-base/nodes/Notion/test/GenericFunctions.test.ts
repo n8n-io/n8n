ce199454e589f2d45e09ed7bac9b28bbb819020c
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-07-15 11:18:56 +0100

    Add test

--- packages/nodes-base/nodes/Notion/test/GenericFunctions.test.ts
+++ packages/nodes-base/nodes/Notion/test/GenericFunctions.test.ts
@@ -1,4 +1,4 @@
-import { formatBlocks } from '../shared/GenericFunctions';
+import { extractPageId, formatBlocks } from '../shared/GenericFunctions';
 
 describe('Test NotionV2, formatBlocks', () => {
 	it('should format to_do block', () => {
@@ -29,5 +29,41 @@ describe('Test NotionV2, formatBlocks', () => {
 				},
 			},
 		]);
+	});
+});
+
+describe('Test Notion', () => {
+	describe('extractPageId', () => {
+		const baseUrl = 'https://www.notion.so/fake-instance';
+		test('should return the part after "p="', () => {
+			const page = `${baseUrl}?p=4eb10d5001254b7faaa831d72d9445aa`;
+			const result = extractPageId(page);
+			expect(result).toBe('4eb10d5001254b7faaa831d72d9445aa');
+		});
+		test('should return the last part after splitting by "-" when URL contains "-" and "https"', () => {
+			const page = `${baseUrl}/some-page-4eb10d5001254b7faaa831d72d9445aa`;
+			const result = extractPageId(page);
+			expect(result).toBe('4eb10d5001254b7faaa831d72d9445aa');
+		});
+		test('should return the last part after splitting by "-" when URL contains "-" and "https" and ignore extra url param', () => {
+			const page = `${baseUrl}/some-page-4eb10d5001254b7faaa831d72d9445aa?pvs=4`;
+			const result = extractPageId(page);
+			expect(result).toBe('4eb10d5001254b7faaa831d72d9445aa');
+		});
+		test('should return just the ID', () => {
+			const page = `${baseUrl}/4eb10d5001254b7faaa831d72d9445aa`;
+			const result = extractPageId(page);
+			expect(result).toBe('4eb10d5001254b7faaa831d72d9445aa');
+		});
+		test('should return the input as is when no conditions are met', () => {
+			const page = `${baseUrl}/some-page`;
+			const result = extractPageId(page);
+			expect(result).toBe(page);
+		});
+		test('should return the input as is when input is an empty string', () => {
+			const page = '';
+			const result = extractPageId(page);
+			expect(result).toBe(page);
+		});
 	});
 });
