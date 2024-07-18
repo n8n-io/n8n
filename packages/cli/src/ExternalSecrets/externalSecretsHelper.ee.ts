812090d0b8631e21a99e3211ffea1083b7cccbff
Iván Ovejero <ivov.src@gmail.com>
2024-07-18 09:56:30 +0200

    refactor(core): Port external secrets config (no-changelog)

--- packages/cli/src/ExternalSecrets/externalSecretsHelper.ee.ts
+++ packages/cli/src/ExternalSecrets/externalSecretsHelper.ee.ts
@@ -1,9 +1,9 @@
 import { License } from '@/License';
-import config from '@/config';
+import { ExternalSecretsConfig } from '@n8n/config/src/configs/external-secrets';
 import Container from 'typedi';
 
-export const updateIntervalTime = () => config.getEnv('externalSecrets.updateInterval') * 1000;
-export const preferGet = () => config.getEnv('externalSecrets.preferGet');
+export const updateIntervalTime = () => Container.get(ExternalSecretsConfig).updateInterval * 1000;
+export const preferGet = () => Container.get(ExternalSecretsConfig).preferGet;
 
 export function isExternalSecretsEnabled() {
 	const license = Container.get(License);
