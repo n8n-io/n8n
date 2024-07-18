f876f9ec8b3a8c5e6bb16878d9341555a392c8ea
Iván Ovejero <ivov.src@gmail.com>
2024-07-18 15:00:24 +0200

    refactor(core): Decouple source control telemetry from internal hooks (no-changelog) (#10095)

--- packages/cli/src/environments/sourceControl/sourceControlHelper.ee.ts
+++ packages/cli/src/environments/sourceControl/sourceControlHelper.ee.ts
@@ -121,58 +121,43 @@ function filterSourceControlledFilesUniqueIds(files: SourceControlledFile[]) {
 	);
 }
 
-export function getTrackingInformationFromPullResult(result: SourceControlledFile[]): {
-	cred_conflicts: number;
-	workflow_conflicts: number;
-	workflow_updates: number;
-} {
+export function getTrackingInformationFromPullResult(result: SourceControlledFile[]) {
 	const uniques = filterSourceControlledFilesUniqueIds(result);
 	return {
-		cred_conflicts: uniques.filter(
+		credConflicts: uniques.filter(
 			(file) =>
 				file.type === 'credential' && file.status === 'modified' && file.location === 'local',
 		).length,
-		workflow_conflicts: uniques.filter(
+		workflowConflicts: uniques.filter(
 			(file) => file.type === 'workflow' && file.status === 'modified' && file.location === 'local',
 		).length,
-		workflow_updates: uniques.filter((file) => file.type === 'workflow').length,
+		workflowUpdates: uniques.filter((file) => file.type === 'workflow').length,
 	};
 }
 
-export function getTrackingInformationFromPrePushResult(result: SourceControlledFile[]): {
-	workflows_eligible: number;
-	workflows_eligible_with_conflicts: number;
-	creds_eligible: number;
-	creds_eligible_with_conflicts: number;
-	variables_eligible: number;
-} {
+export function getTrackingInformationFromPrePushResult(result: SourceControlledFile[]) {
 	const uniques = filterSourceControlledFilesUniqueIds(result);
 	return {
-		workflows_eligible: uniques.filter((file) => file.type === 'workflow').length,
-		workflows_eligible_with_conflicts: uniques.filter(
+		workflowsEligible: uniques.filter((file) => file.type === 'workflow').length,
+		workflowsEligibleWithConflicts: uniques.filter(
 			(file) => file.type === 'workflow' && file.conflict,
 		).length,
-		creds_eligible: uniques.filter((file) => file.type === 'credential').length,
-		creds_eligible_with_conflicts: uniques.filter(
+		credsEligible: uniques.filter((file) => file.type === 'credential').length,
+		credsEligibleWithConflicts: uniques.filter(
 			(file) => file.type === 'credential' && file.conflict,
 		).length,
-		variables_eligible: uniques.filter((file) => file.type === 'variables').length,
+		variablesEligible: uniques.filter((file) => file.type === 'variables').length,
 	};
 }
 
-export function getTrackingInformationFromPostPushResult(result: SourceControlledFile[]): {
-	workflows_eligible: number;
-	workflows_pushed: number;
-	creds_pushed: number;
-	variables_pushed: number;
-} {
+export function getTrackingInformationFromPostPushResult(result: SourceControlledFile[]) {
 	const uniques = filterSourceControlledFilesUniqueIds(result);
 	return {
-		workflows_pushed: uniques.filter((file) => file.pushed && file.type === 'workflow').length ?? 0,
-		workflows_eligible: uniques.filter((file) => file.type === 'workflow').length ?? 0,
-		creds_pushed:
+		workflowsPushed: uniques.filter((file) => file.pushed && file.type === 'workflow').length ?? 0,
+		workflowsEligible: uniques.filter((file) => file.type === 'workflow').length ?? 0,
+		credsPushed:
 			uniques.filter((file) => file.pushed && file.file.startsWith('credential_stubs')).length ?? 0,
-		variables_pushed:
+		variablesPushed:
 			uniques.filter((file) => file.pushed && file.file.startsWith('variable_stubs')).length ?? 0,
 	};
 }
