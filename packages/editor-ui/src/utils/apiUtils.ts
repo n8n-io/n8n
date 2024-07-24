77b4d076df7cbcf5b064f5bdf7b74ae81a2ca89f
Mutasem Aldmour <mutasem@n8n.io>
2024-07-24 12:15:25 +0200

    fix: bug with browserid

--- packages/editor-ui/src/utils/apiUtils.ts
+++ packages/editor-ui/src/utils/apiUtils.ts
@@ -1,8 +1,15 @@
 import type { AxiosRequestConfig, Method, RawAxiosRequestHeaders } from 'axios';
 import axios from 'axios';
-import { ApplicationError, type GenericValue, type IDataObject } from 'n8n-workflow';
+import { ApplicationError, jsonParse, type GenericValue, type IDataObject } from 'n8n-workflow';
 import type { IExecutionFlattedResponse, IExecutionResponse, IRestApiContext } from '@/Interface';
 import { parse } from 'flatted';
+
+const debugLog = [];
+
+// @ts-ignore
+window.debugChat = () => {
+	console.log(debugLog);
+};
 
 const BROWSER_ID_STORAGE_KEY = 'n8n-browserId';
 let browserId = localStorage.getItem(BROWSER_ID_STORAGE_KEY);
@@ -191,3 +198,94 @@ export function unflattenExecutionData(fullExecutionData: IExecutionFlattedRespo
 
 	return returnData;
 }
+
+export const postFetch = async (
+	context: IRestApiContext,
+	apiEndpoint: string,
+	payload: object,
+): Promise<object> => {
+	const headers: Record<string, string> = {
+		'Content-Type': 'application/json',
+	};
+	if (browserId) {
+		headers['browser-id'] = browserId;
+	}
+	const request = {
+		headers,
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify(payload),
+	};
+	debugLog.push({ request: { ...request, endpoint: `${context.baseUrl}${apiEndpoint}` } });
+	const response = await fetch(`${context.baseUrl}${apiEndpoint}`);
+
+	debugLog.push({ response });
+	if (response.ok && response.body) {
+		return await response.json();
+	} else {
+		throw new Error(response.statusText);
+	}
+};
+
+export const streamRequest = async (
+	context: IRestApiContext,
+	apiEndpoint: string,
+	payload: object,
+	onChunk?: (chunk: object) => void,
+	onDone?: () => void,
+	onError?: (e: Error) => void,
+): Promise<void> => {
+	const headers: Record<string, string> = {
+		'Content-Type': 'application/json',
+	};
+	if (browserId) {
+		headers['browser-id'] = browserId;
+	}
+	const request: RequestInit = {
+		headers,
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify(payload),
+	};
+	debugLog.push({ request: { ...request, endpoint: `${context.baseUrl}${apiEndpoint}` } });
+	const response = await fetch(`${context.baseUrl}${apiEndpoint}`, request);
+
+	debugLog.push({ response });
+	if (response.ok && response.body) {
+		// Handle the streaming response
+		const reader = response.body.getReader();
+		const decoder = new TextDecoder('utf-8');
+
+		async function readStream() {
+			const { done, value } = await reader.read();
+			if (done) {
+				debugLog.push({ done });
+				onDone?.();
+				return;
+			}
+
+			const chunk = decoder.decode(value);
+			debugLog.push({ chunk });
+			const splitChunks = chunk.split('\n');
+
+			for (const splitChunk of splitChunks) {
+				if (splitChunk && onChunk) {
+					try {
+						onChunk(jsonParse(splitChunk, { errorMessage: 'Invalid json chunk in stream' }));
+					} catch (e: unknown) {
+						if (e instanceof Error) {
+							console.log(`${e.message}: ${splitChunk}`);
+							onError?.(e);
+						}
+					}
+				}
+			}
+			await readStream();
+		}
+
+		// Start reading the stream
+		await readStream();
+	} else if (onError) {
+		onError(new Error(response.statusText));
+	}
+};
