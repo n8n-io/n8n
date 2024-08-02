d50c4d42d269a657703d897fbafa16965a77c1ce
Iván Ovejero <ivov.src@gmail.com>
2024-08-02 14:16:17 +0200

    feat(core): Allow filtering executions and users by project in Public API  (#10250)

--- packages/cli/src/PublicApi/v1/handlers/users/users.handler.ee.ts
+++ packages/cli/src/PublicApi/v1/handlers/users/users.handler.ee.ts
@@ -12,6 +12,7 @@ import {
 } from '../../shared/middlewares/global.middleware';
 import type { UserRequest } from '@/requests';
 import { InternalHooks } from '@/InternalHooks';
+import { ProjectRelationRepository } from '@/databases/repositories/projectRelation.repository';
 import type { Response } from 'express';
 import { InvitationController } from '@/controllers/invitation.controller';
 import { UsersController } from '@/controllers/users.controller';
@@ -51,12 +52,17 @@ export = {
 		validCursor,
 		globalScope(['user:list', 'user:read']),
 		async (req: UserRequest.Get, res: express.Response) => {
-			const { offset = 0, limit = 100, includeRole = false } = req.query;
+			const { offset = 0, limit = 100, includeRole = false, projectId } = req.query;
+
+			const _in = projectId
+				? await Container.get(ProjectRelationRepository).findUserIdsByProjectId(projectId)
+				: undefined;
 
 			const [users, count] = await getAllUsersAndCount({
 				includeRole,
 				limit,
 				offset,
+				in: _in,
 			});
 
 			const telemetryData = {
