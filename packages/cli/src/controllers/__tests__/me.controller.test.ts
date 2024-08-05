7d94c6dbceb0ddc82455d0cc67faf4770b9c2067
Iván Ovejero <ivov.src@gmail.com>
2024-08-05 12:07:42 +0200

    refactor(core): Decouple user events from internal hooks (no-changelog) (#10292)

--- packages/cli/src/controllers/__tests__/me.controller.test.ts
+++ packages/cli/src/controllers/__tests__/me.controller.test.ts
@@ -13,6 +13,7 @@ import { InternalHooks } from '@/InternalHooks';
 import { License } from '@/License';
 import { BadRequestError } from '@/errors/response-errors/bad-request.error';
 import { UserRepository } from '@/databases/repositories/user.repository';
+import { EventService } from '@/events/event.service';
 import { badPasswords } from '@test/testData';
 import { mockInstance } from '@test/mocking';
 
@@ -20,7 +21,8 @@ const browserId = 'test-browser-id';
 
 describe('MeController', () => {
 	const externalHooks = mockInstance(ExternalHooks);
-	const internalHooks = mockInstance(InternalHooks);
+	mockInstance(InternalHooks);
+	const eventService = mockInstance(EventService);
 	const userService = mockInstance(UserService);
 	const userRepository = mockInstance(UserRepository);
 	mockInstance(License).isWithinUsersLimit.mockReturnValue(true);
@@ -202,9 +204,9 @@ describe('MeController', () => {
 				req.user.password,
 			]);
 
-			expect(internalHooks.onUserUpdate).toHaveBeenCalledWith({
+			expect(eventService.emit).toHaveBeenCalledWith('user-updated', {
 				user: req.user,
-				fields_changed: ['password'],
+				fieldsChanged: ['password'],
 			});
 		});
 	});
