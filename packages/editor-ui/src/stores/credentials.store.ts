de88997674e7a66d66cf39d818e6d12a91f3f974
Elias Meire <elias@meire.dev>
2024-08-12 14:30:26 +0200

    Hide credential-only nodes with supported nodes (instead of deleting)

--- packages/editor-ui/src/stores/credentials.store.ts
+++ packages/editor-ui/src/stores/credentials.store.ts
@@ -200,7 +200,9 @@ export const useCredentialsStore = defineStore(STORES.CREDENTIALS, () => {
 	});
 
 	const httpOnlyCredentialTypes = computed(() => {
-		return allCredentialTypes.value.filter((credentialType) => credentialType.httpRequestNode);
+		return allCredentialTypes.value.filter(
+			(credentialType) => credentialType.httpRequestNode && !credentialType.httpRequestNode.hidden,
+		);
 	});
 
 	// #endregion
