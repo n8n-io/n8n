117e2d968fcc535f32c583ac8f2dc8a84e8cd6bd
Csaba Tuncsik <csaba@n8n.io>
2024-08-13 15:45:28 +0200

    fix(editor): Connect up new project viewer role to the FE (#9913)

--- packages/editor-ui/src/components/executions/workflow/WorkflowExecutionsCard.test.ts
+++ packages/editor-ui/src/components/executions/workflow/WorkflowExecutionsCard.test.ts
@@ -2,6 +2,13 @@ import { createComponentRenderer } from '@/__tests__/render';
 import WorkflowExecutionsCard from '@/components/executions/workflow/WorkflowExecutionsCard.vue';
 import { createPinia, setActivePinia } from 'pinia';
 
+vi.mock('vue-router', () => ({
+	useRoute: () => ({
+		params: {},
+	}),
+	RouterLink: vi.fn(),
+}));
+
 const renderComponent = createComponentRenderer(WorkflowExecutionsCard, {
 	global: {
 		stubs: {
@@ -25,53 +32,102 @@ describe('WorkflowExecutionsCard', () => {
 	test.each([
 		[
 			{
-				id: '1',
-				mode: 'manual',
-				status: 'success',
-				retryOf: null,
-				retrySuccessId: null,
+				execution: {
+					id: '1',
+					mode: 'manual',
+					status: 'success',
+					retryOf: null,
+					retrySuccessId: null,
+				},
+				workflowPermissions: {
+					execute: true,
+				},
 			},
+			false,
 			false,
 		],
 		[
 			{
-				id: '2',
-				mode: 'manual',
-				status: 'error',
-				retryOf: null,
-				retrySuccessId: null,
+				execution: {
+					id: '2',
+					mode: 'manual',
+					status: 'error',
+					retryOf: null,
+					retrySuccessId: null,
+				},
+				workflowPermissions: {
+					execute: true,
+				},
 			},
 			true,
-		],
-		[
-			{
-				id: '3',
-				mode: 'manual',
-				status: 'error',
-				retryOf: '2',
-				retrySuccessId: null,
-			},
-			true,
-		],
-		[
-			{
-				id: '4',
-				mode: 'manual',
-				status: 'error',
-				retryOf: null,
-				retrySuccessId: '3',
-			},
 			false,
 		],
-	])('with execution %j retry button visibility is %s', (execution, shouldRenderRetryBtn) => {
-		const { queryByTestId } = renderComponent({
-			props: {
-				execution,
+		[
+			{
+				execution: {
+					id: '3',
+					mode: 'manual',
+					status: 'error',
+					retryOf: null,
+					retrySuccessId: '3',
+				},
+				workflowPermissions: {
+					execute: true,
+				},
 			},
-		});
+			false,
+			false,
+		],
+		[
+			{
+				execution: {
+					id: '4',
+					mode: 'manual',
+					status: 'success',
+					retryOf: '4',
+					retrySuccessId: null,
+				},
+				workflowPermissions: {
+					execute: true,
+				},
+			},
+			false,
+			false,
+		],
+		[
+			{
+				execution: {
+					id: '2',
+					mode: 'manual',
+					status: 'error',
+					retryOf: null,
+					retrySuccessId: null,
+				},
+				workflowPermissions: {},
+			},
+			true,
+			true,
+		],
+	])(
+		'with execution %j retry button visibility is %s and if visible is disabled %s',
+		(props, shouldRenderRetryBtn, disabled) => {
+			const { queryByTestId } = renderComponent({
+				props,
+			});
 
-		expect(!!queryByTestId('retry-execution-button') && shouldRenderRetryBtn).toBe(
-			shouldRenderRetryBtn,
-		);
-	});
+			const retryButton = queryByTestId('retry-execution-button');
+
+			if (shouldRenderRetryBtn) {
+				expect(retryButton).toBeVisible();
+
+				if (disabled) {
+					expect(retryButton?.querySelector('.is-disabled')).toBeVisible();
+				} else {
+					expect(retryButton?.querySelector('.is-disabled')).toBe(null);
+				}
+			} else {
+				expect(retryButton).toBe(null);
+			}
+		},
+	);
 });
