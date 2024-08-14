12604f2983d78f7a76bb3338233f2dfa8f93aba2
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-14 10:52:08 +0300

    refactor(editor): Type node view event bus
    Builds on top of #10367

--- packages/editor-ui/src/event-bus/node-view.ts
+++ packages/editor-ui/src/event-bus/node-view.ts
@@ -1,3 +1,30 @@
+import type { EventBus } from 'n8n-design-system/utils';
 import { createEventBus } from 'n8n-design-system/utils';
+import type { IDataObject } from 'n8n-workflow';
 
-export const nodeViewEventBus = createEventBus();
+export type CallbackFn = () => void;
+
+export interface NodeViewEventBusEvents {
+	/** Command to create a new workflow */
+	newWorkflow: never;
+
+	/** Command to open the chat */
+	openChat: never;
+
+	/** Command to save the current workflow */
+	saveWorkflow: CallbackFn;
+
+	/** Command to import a workflow from given data */
+	importWorkflowData: IDataObject;
+
+	/** Command to import a workflow from given URL */
+	importWorkflowUrl: IDataObject;
+
+	'runWorkflowButton:mouseenter': never;
+
+	'runWorkflowButton:mouseleave': never;
+}
+
+export type NodeViewEventBus = EventBus<NodeViewEventBusEvents>;
+
+export const nodeViewEventBus = createEventBus<NodeViewEventBusEvents>();
