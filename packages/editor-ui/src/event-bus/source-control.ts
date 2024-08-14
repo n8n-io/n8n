c9344c0642f89b276097f0363b55220e8a4e2b61
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-14 10:40:57 +0300

    refactor(editor): Type source control event bus
    Builds on top of #10367

--- packages/editor-ui/src/event-bus/source-control.ts
+++ packages/editor-ui/src/event-bus/source-control.ts
@@ -1,3 +1,10 @@
+import type { EventBus } from 'n8n-design-system/utils';
 import { createEventBus } from 'n8n-design-system/utils';
 
-export const sourceControlEventBus = createEventBus();
+export interface SourceControlEventBusEvents {
+	pull: never;
+}
+
+export type SourceControlEventBus = EventBus<SourceControlEventBusEvents>;
+
+export const sourceControlEventBus = createEventBus<SourceControlEventBusEvents>();
