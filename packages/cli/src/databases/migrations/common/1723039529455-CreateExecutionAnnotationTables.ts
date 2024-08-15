fa712642bc1006f44301de21abc38e9d533fe045
Eugene Molodkin <eugene@n8n.io>
2024-08-15 16:33:07 +0200

    wip: add unique annotation tag constraint + index

--- packages/cli/src/databases/migrations/common/1723039529455-CreateExecutionAnnotationTables.ts
+++ packages/cli/src/databases/migrations/common/1723039529455-CreateExecutionAnnotationTables.ts
@@ -5,7 +5,7 @@ const annotationTagsTableName = 'annotation_tag_entity';
 const annotationTagMappingsTableName = 'execution_annotation_tags';
 
 export class CreateAnnotationTables1723039529455 implements ReversibleMigration {
-	async up({ schemaBuilder: { createTable, column }, tablePrefix }: MigrationContext) {
+	async up({ schemaBuilder: { createTable, column } }: MigrationContext) {
 		await createTable(annotationsTableName)
 			.withColumns(
 				column('id').int.notNull.primary.autoGenerate,
@@ -20,10 +20,9 @@ export class CreateAnnotationTables1723039529455 implements ReversibleMigration 
 				onDelete: 'CASCADE',
 			}).withTimestamps;
 
-		await createTable(annotationTagsTableName).withColumns(
-			column('id').varchar(24).primary.notNull,
-			column('name').varchar(24).notNull,
-		).withTimestamps;
+		await createTable(annotationTagsTableName)
+			.withColumns(column('id').varchar(24).primary.notNull, column('name').varchar(24).notNull)
+			.withIndexOn('name', true).withTimestamps;
 
 		await createTable(annotationTagMappingsTableName)
 			.withColumns(column('annotationId').int.notNull, column('tagId').varchar(24).notNull)
@@ -42,7 +41,6 @@ export class CreateAnnotationTables1723039529455 implements ReversibleMigration 
 	}
 
 	async down({ schemaBuilder: { dropTable } }: MigrationContext) {
-		console.log('DOWN DOWN DOWN');
 		await dropTable(annotationTagMappingsTableName);
 		await dropTable(annotationTagsTableName);
 		await dropTable(annotationsTableName);
