1f2721df8ed8546beda1e0ac78af224c26034bb6
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-08-15 12:15:26 +0200

    fix(core): Use explicit types in configs to ensure valid decorator metadata

--- packages/@n8n/config/src/configs/user-management.config.ts
+++ packages/@n8n/config/src/configs/user-management.config.ts
@@ -4,26 +4,26 @@ import { Config, Env, Nested } from '../decorators';
 class SmtpAuth {
 	/** SMTP login username */
 	@Env('N8N_SMTP_USER')
-	user = '';
+	user: string = '';
 
 	/** SMTP login password */
 	@Env('N8N_SMTP_PASS')
-	pass = '';
+	pass: string = '';
 
 	/** SMTP OAuth Service Client */
 	@Env('N8N_SMTP_OAUTH_SERVICE_CLIENT')
-	serviceClient = '';
+	serviceClient: string = '';
 
 	/** SMTP OAuth Private Key */
 	@Env('N8N_SMTP_OAUTH_PRIVATE_KEY')
-	privateKey = '';
+	privateKey: string = '';
 }
 
 @Config
 class SmtpConfig {
 	/** SMTP server host */
 	@Env('N8N_SMTP_HOST')
-	host = '';
+	host: string = '';
 
 	/** SMTP server port */
 	@Env('N8N_SMTP_PORT')
@@ -39,7 +39,7 @@ class SmtpConfig {
 
 	/** How to display sender name */
 	@Env('N8N_SMTP_SENDER')
-	sender = '';
+	sender: string = '';
 
 	@Nested
 	auth: SmtpAuth;
@@ -49,19 +49,19 @@ class SmtpConfig {
 export class TemplateConfig {
 	/** Overrides default HTML template for inviting new people (use full path) */
 	@Env('N8N_UM_EMAIL_TEMPLATES_INVITE')
-	invite = '';
+	invite: string = '';
 
 	/** Overrides default HTML template for resetting password (use full path) */
 	@Env('N8N_UM_EMAIL_TEMPLATES_PWRESET')
-	passwordReset = '';
+	passwordReset: string = '';
 
 	/** Overrides default HTML template for notifying that a workflow was shared (use full path) */
 	@Env('N8N_UM_EMAIL_TEMPLATES_WORKFLOW_SHARED')
-	workflowShared = '';
+	workflowShared: string = '';
 
 	/** Overrides default HTML template for notifying that credentials were shared (use full path) */
 	@Env('N8N_UM_EMAIL_TEMPLATES_CREDENTIALS_SHARED')
-	credentialsShared = '';
+	credentialsShared: string = '';
 }
 
 @Config
