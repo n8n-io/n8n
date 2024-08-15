2d874dc032f592dc679751018c3edb00772cc943
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-08-15 13:58:20 +0200

    fix(core): Use explicit types in configs to ensure valid decorator metadata (#10433)

--- packages/@n8n/config/src/configs/endpoints.config.ts
+++ packages/@n8n/config/src/configs/endpoints.config.ts
@@ -4,51 +4,51 @@ import { Config, Env, Nested } from '../decorators';
 class PrometheusMetricsConfig {
 	/** Whether to enable the `/metrics` endpoint to expose Prometheus metrics. */
 	@Env('N8N_METRICS')
-	enable = false;
+	enable: boolean = false;
 
 	/** Prefix for Prometheus metric names. */
 	@Env('N8N_METRICS_PREFIX')
-	prefix = 'n8n_';
+	prefix: string = 'n8n_';
 
 	/** Whether to expose system and Node.js metrics. See: https://www.npmjs.com/package/prom-client */
 	@Env('N8N_METRICS_INCLUDE_DEFAULT_METRICS')
-	includeDefaultMetrics = true;
+	includeDefaultMetrics: boolean = true;
 
 	/** Whether to include a label for workflow ID on workflow metrics. */
 	@Env('N8N_METRICS_INCLUDE_WORKFLOW_ID_LABEL')
-	includeWorkflowIdLabel = false;
+	includeWorkflowIdLabel: boolean = false;
 
 	/** Whether to include a label for node type on node metrics. */
 	@Env('N8N_METRICS_INCLUDE_NODE_TYPE_LABEL')
-	includeNodeTypeLabel = false;
+	includeNodeTypeLabel: boolean = false;
 
 	/** Whether to include a label for credential type on credential metrics. */
 	@Env('N8N_METRICS_INCLUDE_CREDENTIAL_TYPE_LABEL')
-	includeCredentialTypeLabel = false;
+	includeCredentialTypeLabel: boolean = false;
 
 	/** Whether to expose metrics for API endpoints. See: https://www.npmjs.com/package/express-prom-bundle */
 	@Env('N8N_METRICS_INCLUDE_API_ENDPOINTS')
-	includeApiEndpoints = false;
+	includeApiEndpoints: boolean = false;
 
 	/** Whether to include a label for the path of API endpoint calls. */
 	@Env('N8N_METRICS_INCLUDE_API_PATH_LABEL')
-	includeApiPathLabel = false;
+	includeApiPathLabel: boolean = false;
 
 	/** Whether to include a label for the HTTP method of API endpoint calls. */
 	@Env('N8N_METRICS_INCLUDE_API_METHOD_LABEL')
-	includeApiMethodLabel = false;
+	includeApiMethodLabel: boolean = false;
 
 	/** Whether to include a label for the status code of API endpoint calls. */
 	@Env('N8N_METRICS_INCLUDE_API_STATUS_CODE_LABEL')
-	includeApiStatusCodeLabel = false;
+	includeApiStatusCodeLabel: boolean = false;
 
 	/** Whether to include metrics for cache hits and misses. */
 	@Env('N8N_METRICS_INCLUDE_CACHE_METRICS')
-	includeCacheMetrics = false;
+	includeCacheMetrics: boolean = false;
 
 	/** Whether to include metrics derived from n8n's internal events */
 	@Env('N8N_METRICS_INCLUDE_MESSAGE_EVENT_BUS_METRICS')
-	includeMessageEventBusMetrics = false;
+	includeMessageEventBusMetrics: boolean = false;
 }
 
 @Config
@@ -62,41 +62,41 @@ export class EndpointsConfig {
 
 	/** Path segment for REST API endpoints. */
 	@Env('N8N_ENDPOINT_REST')
-	rest = 'rest';
+	rest: string = 'rest';
 
 	/** Path segment for form endpoints. */
 	@Env('N8N_ENDPOINT_FORM')
-	form = 'form';
+	form: string = 'form';
 
 	/** Path segment for test form endpoints. */
 	@Env('N8N_ENDPOINT_FORM_TEST')
-	formTest = 'form-test';
+	formTest: string = 'form-test';
 
 	/** Path segment for waiting form endpoints. */
 	@Env('N8N_ENDPOINT_FORM_WAIT')
-	formWaiting = 'form-waiting';
+	formWaiting: string = 'form-waiting';
 
 	/** Path segment for webhook endpoints. */
 	@Env('N8N_ENDPOINT_WEBHOOK')
-	webhook = 'webhook';
+	webhook: string = 'webhook';
 
 	/** Path segment for test webhook endpoints. */
 	@Env('N8N_ENDPOINT_WEBHOOK_TEST')
-	webhookTest = 'webhook-test';
+	webhookTest: string = 'webhook-test';
 
 	/** Path segment for waiting webhook endpoints. */
 	@Env('N8N_ENDPOINT_WEBHOOK_WAIT')
-	webhookWaiting = 'webhook-waiting';
+	webhookWaiting: string = 'webhook-waiting';
 
 	/** Whether to disable n8n's UI (frontend). */
 	@Env('N8N_DISABLE_UI')
-	disableUi = false;
+	disableUi: boolean = false;
 
 	/** Whether to disable production webhooks on the main process, when using webhook-specific processes. */
 	@Env('N8N_DISABLE_PRODUCTION_MAIN_PROCESS')
-	disableProductionWebhooksOnMainProcess = false;
+	disableProductionWebhooksOnMainProcess: boolean = false;
 
 	/** Colon-delimited list of additional endpoints to not open the UI on. */
 	@Env('N8N_ADDITIONAL_NON_UI_ROUTES')
-	additionalNonUIRoutes = '';
+	additionalNonUIRoutes: string = '';
 }
