90db188ed8269e03ed8fdc07e4ce84b1ae40c7b7
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-20 14:45:08 +0300

    revert: Revert n8n-workflow package usage

--- packages/@n8n/benchmark/src/n8nApiClient/n8nApiClient.types.ts
+++ packages/@n8n/benchmark/src/n8nApiClient/n8nApiClient.types.ts
@@ -1,5 +1,8 @@
-import type { IWorkflowBase } from 'n8n-workflow';
 /**
  * n8n workflow. This is a simplified version of the actual workflow object.
  */
-export type Workflow = Pick<IWorkflowBase, 'id' | 'name' | 'nodes' | 'connections'>;
+export type Workflow = {
+	id: string;
+	name: string;
+	tags?: string[];
+};
