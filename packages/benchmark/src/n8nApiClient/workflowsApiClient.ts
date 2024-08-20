e5b629e42f2142aaece045f152dfaa2807559ce6
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-20 14:33:44 +0300

    refactor: Repeat api endpoint

--- packages/benchmark/src/n8nApiClient/workflowsApiClient.ts
+++ packages/benchmark/src/n8nApiClient/workflowsApiClient.ts
@@ -2,37 +2,30 @@ import { Workflow } from '@/n8nApiClient/n8nApiClient.types';
 import { AuthenticatedN8nApiClient } from './authenticatedN8nApiClient';
 
 export class WorkflowApiClient {
-	private readonly apiEndpoint = '/workflows';
-
 	constructor(private readonly apiClient: AuthenticatedN8nApiClient) {}
 
 	async getAllWorkflows(): Promise<Workflow[]> {
-		const response = await this.apiClient.get<{ count: number; data: Workflow[] }>(
-			this.apiEndpoint,
-		);
+		const response = await this.apiClient.get<{ count: number; data: Workflow[] }>('/workflows');
 
 		return response.data.data;
 	}
 
 	async createWorkflow(workflow: unknown): Promise<Workflow> {
-		const response = await this.apiClient.post<{ data: Workflow }>(this.apiEndpoint, workflow);
+		const response = await this.apiClient.post<{ data: Workflow }>('/workflows', workflow);
 
 		return response.data.data;
 	}
 
 	async activateWorkflow(workflow: Workflow): Promise<Workflow> {
-		const response = await this.apiClient.patch<{ data: Workflow }>(
-			`${this.apiEndpoint}/${workflow.id}`,
-			{
-				...workflow,
-				active: true,
-			},
-		);
+		const response = await this.apiClient.patch<{ data: Workflow }>(`/workflows/${workflow.id}`, {
+			...workflow,
+			active: true,
+		});
 
 		return response.data.data;
 	}
 
 	async deleteWorkflow(workflowId: Workflow['id']): Promise<void> {
-		await this.apiClient.delete(`${this.apiEndpoint}/${workflowId}`);
+		await this.apiClient.delete(`/workflows/${workflowId}`);
 	}
 }
