7f8cb116d39d2b0b36260de641158d0961aaff76
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-21 17:19:04 +0300

    refactor(editor): Add types to codeNodeEditorEventBus (no-changelog)

--- packages/editor-ui/src/event-bus/code-node-editor.ts
+++ packages/editor-ui/src/event-bus/code-node-editor.ts
@@ -1,3 +1,13 @@
 import { createEventBus } from 'n8n-design-system/utils';
 
-export const codeNodeEditorEventBus = createEventBus();
+export type ErrorLineNumberEvent = number | 'final';
+
+export interface CodeNodeEditorEventBusEvents {
+	/** Event that a diff have been applied to the code node editor */
+	codeDiffApplied: never;
+
+	/** Command to highlight a specific line in the code node editor */
+	'error-line-number': ErrorLineNumberEvent;
+}
+
+export const codeNodeEditorEventBus = createEventBus<CodeNodeEditorEventBusEvents>();
