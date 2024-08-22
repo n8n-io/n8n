590271e8ddebb35523dcc750179a08645f60efdb
Shireen Missi <shireen@n8n.io>
2024-08-22 11:59:34 +0100

    Add node version to Remove Duplicate node

--- packages/nodes-base/nodes/Transform/RemoveDuplicates/v1/RemoveDuplicatesV1.node.ts
+++ packages/nodes-base/nodes/Transform/RemoveDuplicates/v1/RemoveDuplicatesV1.node.ts
@@ -1,138 +1,123 @@
-import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow';
-import { VersionedNodeType } from 'n8n-workflow';
-
-import { RemoveDuplicatesV1 } from './v1/RemoveDuplicatesV1.node';
-// import { HighLevelV2 } from './v2/HighLevelV2.node';
-export class RemoveDuplicates extends VersionedNodeType {
-	constructor() {
-		const baseDescription: INodeTypeBaseDescription = {
-			displayName: 'Remove Duplicates',
-			name: 'removeDuplicates',
-			icon: 'file:removeDuplicates.svg',
-			group: ['transform'],
-			defaultVersion: 1,
-			description: 'Delete items with matching field values',
-		};
-
-		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
-			1: new RemoveDuplicatesV1(baseDescription),
-			1.1: new RemoveDuplicatesV1(baseDescription),
-			// 2: new HighLevelV2(baseDescription),
-		};
-
-		super(nodeVersions, baseDescription);
+/* eslint-disable n8n-nodes-base/node-filename-against-convention */
 import get from 'lodash/get';
 import isEqual from 'lodash/isEqual';
 import lt from 'lodash/lt';
 import pick from 'lodash/pick';
-import {
-	NodeOperationError,
-	NodeConnectionType,
-	type IExecuteFunctions,
-	type INodeExecutionData,
-	type INodeType,
-	type INodeTypeDescription,
+import { NodeOperationError } from 'n8n-workflow';
+import type {
+	INodeTypeBaseDescription,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
 } from 'n8n-workflow';
-import { prepareFieldsArray } from '../utils/utils';
-import { validateInputData } from './utils';
+import { prepareFieldsArray } from '../../utils/utils';
+import { validateInputData } from '../utils';
 import { compareItems, flattenKeys } from '@utils/utilities';
-
-export class RemoveDuplicates implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Remove Duplicates',
-		name: 'removeDuplicates',
-		icon: 'file:removeDuplicates.svg',
-		group: ['transform'],
-		subtitle: '',
-		version: [1, 1.1],
-		description: 'Delete items with matching field values',
-		defaults: {
-			name: 'Remove Duplicates',
+const versionDescription: INodeTypeDescription = {
+	displayName: 'Remove Duplicates',
+	name: 'removeDuplicates',
+	icon: 'file:removeDuplicates.svg',
+	group: ['transform'],
+	subtitle: '',
+	version: [1, 1.1],
+	description: 'Delete items with matching field values',
+	defaults: {
+		name: 'Remove Duplicates',
+	},
+	inputs: ['main'],
+	outputs: ['main'],
+	properties: [
+		{
+			displayName: 'Compare',
+			name: 'compare',
+			type: 'options',
+			options: [
+				{
+					name: 'All Fields',
+					value: 'allFields',
+				},
+				{
+					name: 'All Fields Except',
+					value: 'allFieldsExcept',
+				},
+				{
+					name: 'Selected Fields',
+					value: 'selectedFields',
+				},
+			],
+			default: 'allFields',
+			description: 'The fields of the input items to compare to see if they are the same',
 		},
-		inputs: [NodeConnectionType.Main],
-		outputs: [NodeConnectionType.Main],
-		properties: [
-			{
-				displayName: 'Compare',
-				name: 'compare',
-				type: 'options',
-				options: [
-					{
-						name: 'All Fields',
-						value: 'allFields',
-					},
-					{
-						name: 'All Fields Except',
-						value: 'allFieldsExcept',
-					},
-					{
-						name: 'Selected Fields',
-						value: 'selectedFields',
-					},
-				],
-				default: 'allFields',
-				description: 'The fields of the input items to compare to see if they are the same',
-			},
-			{
-				displayName: 'Fields To Exclude',
-				name: 'fieldsToExclude',
-				type: 'string',
-				placeholder: 'e.g. email, name',
-				requiresDataPath: 'multiple',
-				description: 'Fields in the input to exclude from the comparison',
-				default: '',
-				displayOptions: {
-					show: {
-						compare: ['allFieldsExcept'],
-					},
+		{
+			displayName: 'Fields To Exclude',
+			name: 'fieldsToExclude',
+			type: 'string',
+			placeholder: 'e.g. email, name',
+			requiresDataPath: 'multiple',
+			description: 'Fields in the input to exclude from the comparison',
+			default: '',
+			displayOptions: {
+				show: {
+					compare: ['allFieldsExcept'],
 				},
 			},
-			{
-				displayName: 'Fields To Compare',
-				name: 'fieldsToCompare',
-				type: 'string',
-				placeholder: 'e.g. email, name',
-				requiresDataPath: 'multiple',
-				description: 'Fields in the input to add to the comparison',
-				default: '',
-				displayOptions: {
-					show: {
-						compare: ['selectedFields'],
-					},
+		},
+		{
+			displayName: 'Fields To Compare',
+			name: 'fieldsToCompare',
+			type: 'string',
+			placeholder: 'e.g. email, name',
+			requiresDataPath: 'multiple',
+			description: 'Fields in the input to add to the comparison',
+			default: '',
+			displayOptions: {
+				show: {
+					compare: ['selectedFields'],
 				},
 			},
-			{
-				displayName: 'Options',
-				name: 'options',
-				type: 'collection',
-				placeholder: 'Add Field',
-				default: {},
-				displayOptions: {
-					show: {
-						compare: ['allFieldsExcept', 'selectedFields'],
-					},
+		},
+		{
+			displayName: 'Options',
+			name: 'options',
+			type: 'collection',
+			placeholder: 'Add Field',
+			default: {},
+			displayOptions: {
+				show: {
+					compare: ['allFieldsExcept', 'selectedFields'],
 				},
-				options: [
-					{
-						displayName: 'Disable Dot Notation',
-						name: 'disableDotNotation',
-						type: 'boolean',
-						default: false,
-						description:
-							'Whether to disallow referencing child fields using `parent.child` in the field name',
-					},
-					{
-						displayName: 'Remove Other Fields',
-						name: 'removeOtherFields',
-						type: 'boolean',
-						default: false,
-						description:
-							'Whether to remove any fields that are not being compared. If disabled, will keep the values from the first of the duplicates.',
-					},
-				],
 			},
-		],
-	};
+			options: [
+				{
+					displayName: 'Disable Dot Notation',
+					name: 'disableDotNotation',
+					type: 'boolean',
+					default: false,
+					description:
+						'Whether to disallow referencing child fields using `parent.child` in the field name',
+				},
+				{
+					displayName: 'Remove Other Fields',
+					name: 'removeOtherFields',
+					type: 'boolean',
+					default: false,
+					description:
+						'Whether to remove any fields that are not being compared. If disabled, will keep the values from the first of the duplicates.',
+				},
+			],
+		},
+	],
+};
+export class RemoveDuplicatesV1 implements INodeType {
+	description: INodeTypeDescription;
+
+	constructor(baseDescription: INodeTypeBaseDescription) {
+		this.description = {
+			...baseDescription,
+			...versionDescription,
+		};
+	}
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
