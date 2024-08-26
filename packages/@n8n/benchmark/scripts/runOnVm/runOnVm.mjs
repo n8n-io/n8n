82c986a8a2bd672c030ce7475b22c8c9080cf6ba
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-26 11:38:38 +0300

    feat(benchmark): Add benchmarking of pooled sqlite

--- packages/@n8n/benchmark/scripts/runOnVm/runOnVm.mjs
+++ packages/@n8n/benchmark/scripts/runOnVm/runOnVm.mjs
@@ -2,23 +2,19 @@
 /**
  * This script runs the benchmarks using a given docker compose setup
  */
+// @ts-check
+import path from 'path';
+import { $, argv, fs } from 'zx';
 
-import { $ } from 'zx';
-
-const [n8nSetupToUse] = argv._;
-
-if (!n8nSetupToUse) {
-	printUsage();
-	process.exit(1);
-}
-
-function printUsage() {
-	console.log('Usage: zx runOnVm.mjs <envName>');
-	console.log('   eg: zx runOnVm.mjs sqlite');
-}
+const paths = {
+	n8nSetupsDir: path.join(__dirname, 'n8nSetups'),
+};
 
 async function main() {
-	const composeFilePath = path.join(__dirname, 'n8nSetups', n8nSetupToUse);
+	const [n8nSetupToUse] = argv._;
+	validateN8nSetup(n8nSetupToUse);
+
+	const composeFilePath = path.join(paths.n8nSetupsDir, n8nSetupToUse);
 	const n8nTag = argv.n8nDockerTag || process.env.N8N_DOCKER_TAG || 'latest';
 	const benchmarkTag = argv.benchmarkDockerTag || process.env.BENCHMARK_DOCKER_TAG || 'latest';
 	const k6ApiToken = argv.k6ApiToken || process.env.K6_API_TOKEN || undefined;
@@ -30,6 +26,7 @@ async function main() {
 			N8N_VERSION: n8nTag,
 			BENCHMARK_VERSION: benchmarkTag,
 			K6_API_TOKEN: k6ApiToken,
+			N8N_BENCHMARK_SCENARIO_NAME_PREFIX: n8nSetupToUse,
 		},
 	});
 
@@ -50,6 +47,30 @@ async function main() {
 async function dumpN8nInstanceLogs($$) {
 	console.error('n8n instance logs:');
 	await $$`docker-compose logs n8n`;
+}
+
+function printUsage() {
+	const availableSetups = getAllN8nSetups();
+	console.log('Usage: zx runOnVm.mjs <n8n setup to use>');
+	console.log(`   eg: zx runOnVm.mjs ${availableSetups[0]}`);
+	console.log('');
+	console.log('Available setups:');
+	console.log(availableSetups.join(', '));
+}
+
+/**
+ * @returns {string[]}
+ */
+function getAllN8nSetups() {
+	return fs.readdirSync(paths.n8nSetupsDir);
+}
+
+function validateN8nSetup(givenSetup) {
+	const availableSetups = getAllN8nSetups();
+	if (!availableSetups.includes(givenSetup)) {
+		printUsage();
+		process.exit(1);
+	}
 }
 
 main();
