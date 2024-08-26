b957f710d091ef603ca17a98e0d04d4183d33d6b
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-08-26 19:27:06 +0200

    refactor(core): Delete InternalHooks (no-changelog)

--- packages/cli/test/integration/shared/utils/testCommand.ts
+++ packages/cli/test/integration/shared/utils/testCommand.ts
@@ -6,9 +6,6 @@ import type { BaseCommand } from '@/commands/base-command';
 import * as testDb from '../testDb';
 import { TelemetryEventRelay } from '@/events/telemetry-event-relay';
 import { mockInstance } from '@test/mocking';
-import { InternalHooks } from '@/internal-hooks';
-
-mockInstance(InternalHooks);
 
 export const setupTestCommand = <T extends BaseCommand>(Command: Class<T>) => {
 	const config = mock<Config>();
