84b3d07bce1d954d14f528288742e0d0388a39d8
Valentina Lilova <valentina.lilova98@gmail.com>
2024-08-26 16:05:58 +0200

    Remove unneeded preAuthentication

--- packages/nodes-base/credentials/GoogleMyBusinessOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/GoogleMyBusinessOAuth2Api.credentials.ts
@@ -1,11 +1,8 @@
 import {
-	ApplicationError,
 	type IAuthenticateGeneric,
+	type ICredentialTestRequest,
 	type ICredentialType,
-	type IDataObject,
-	type IHttpRequestHelper,
 	type IHttpRequestMethods,
-	type IHttpRequestOptions,
 	type INodeProperties,
 } from 'n8n-workflow';
 
@@ -29,45 +26,6 @@ export class GoogleMyBusinessOAuth2Api implements ICredentialType {
 		},
 	];
 
-	// ToDo: Test the authentication
-	// This function runs before the authentication happens
-	preAuthentication = async function (
-		this: IHttpRequestHelper,
-		credentials: IDataObject,
-	): Promise<IDataObject> {
-		// Perform the OAuth2 flow to obtain an access token
-		const tokenRequestOptions: IHttpRequestOptions = {
-			method: 'POST',
-			url: 'https://oauth2.googleapis.com/token',
-			body: {
-				client_id: credentials.clientId,
-				client_secret: credentials.clientSecret,
-				grant_type: 'authorization_code',
-				code: credentials.code,
-				redirect_uri: credentials.redirectUri,
-			},
-			json: true,
-		};
-
-		try {
-			// Send the request to get the access token
-			const response: IDataObject = (await this.helpers?.httpRequest(
-				tokenRequestOptions,
-			)) as IDataObject;
-
-			// Return the acquired tokens and other necessary data
-			return {
-				...credentials,
-				accessToken: response.access_token,
-				refreshToken: response.refresh_token,
-				expiresIn: response.expires_in,
-			};
-		} catch (error) {
-			// Handle any errors that occur during the request
-			throw new ApplicationError(`Failed to obtain access token: ${(error as Error).message}`);
-		}
-	};
-
 	// This allows the credential to be used by other parts of n8n
 	// stating how this credential is injected as part of the request
 	// An example is the Http Request node that can make generic calls
@@ -84,7 +42,7 @@ export class GoogleMyBusinessOAuth2Api implements ICredentialType {
 	// The test block is used to test the credential
 	// It uses a deprecated method to avoid the need of account and a location input
 	// ToDo: Test the test function
-	test = {
+	test: ICredentialTestRequest = {
 		request: {
 			method: 'GET' as IHttpRequestMethods,
 			url: 'https://mybusiness.googleapis.com/v4/categories',
@@ -92,9 +50,9 @@ export class GoogleMyBusinessOAuth2Api implements ICredentialType {
 				Authorization: 'Bearer {{$credentials.accessToken}}',
 			},
 			qs: {
-				pageSize: 1, // Test with a small page size to minimize data returned
+				pageSize: 1,
 			},
 			json: true,
-		} as IHttpRequestOptions,
+		},
 	};
 }
