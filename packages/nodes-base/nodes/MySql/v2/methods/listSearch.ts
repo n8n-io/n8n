374a0974cd71c742f2bcc7d8b9a0e4ac3133cfb7
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-08-27 12:27:04 +0200

    refactor(core): Allow typings on getCredentials (no-changelog)

--- packages/nodes-base/nodes/MySql/v2/methods/listSearch.ts
+++ packages/nodes-base/nodes/MySql/v2/methods/listSearch.ts
@@ -3,7 +3,7 @@ import { createPool } from '../transport';
 import type { MysqlNodeCredentials } from '../helpers/interfaces';
 
 export async function searchTables(this: ILoadOptionsFunctions): Promise<INodeListSearchResult> {
-	const credentials = (await this.getCredentials('mySql')) as MysqlNodeCredentials;
+	const credentials = await this.getCredentials<MysqlNodeCredentials>('mySql');
 
 	const nodeOptions = this.getNodeParameter('options', 0) as IDataObject;
 
