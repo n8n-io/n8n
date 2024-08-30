3e8672897935c5917b0052fa494e2eebe3a16de0
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-08-30 13:36:32 +0300

    ci: Fixes to benchmarks in cloud
    - Run destroy benchmark env workflow after nightly benchmark
    - Destroy env before running nightly benchmark
    - Use correct VM size
    - Wait until the data disk becomes available
    - Fix n8n container permissions in the docker volume
    - Combine two ssh commands into one for less overhead

--- packages/@n8n/benchmark/infra/vars.tf
+++ packages/@n8n/benchmark/infra/vars.tf
@@ -15,8 +15,8 @@ variable "host_size_family" {
 
 variable "vm_size" {
   description = "VM Size"
-  # 2 vCPUs, 8 GiB memory
-  default = "Standard_DC2s_v2"
+  # 8 vCPUs, 32 GiB memory
+  default = "Standard_DC8_v2"
 }
 
 variable "number_of_vms" {
