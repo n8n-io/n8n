b028a865392e2b83dcff3014d805cfd39ada660b
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-09-02 12:47:43 +0200

    fix(core): Flush responses for ai streaming endpoints

--- packages/cli/src/controllers/ai-assistant.controller.ts
+++ packages/cli/src/controllers/ai-assistant.controller.ts
@@ -1,28 +1,42 @@
 import { Post, RestController } from '@/decorators';
 import { AiAssistantService } from '@/services/ai-assistant.service';
 import { AiAssistantRequest } from '@/requests';
-import { Response } from 'express';
+import type { Response } from 'express';
 import type { AiAssistantSDK } from '@n8n_io/ai-assistant-sdk';
-import { Readable, promises } from 'node:stream';
+import { Readable, promises, Writable } from 'node:stream';
 import { InternalServerError } from 'express-openapi-validator/dist/openapi.validator';
 import { strict as assert } from 'node:assert';
 import { ErrorReporterProxy } from 'n8n-workflow';
+
+type FlushableResponse = Response & { flush: () => void };
+
+class OutputStream extends Writable {
+	constructor(private readonly res: FlushableResponse) {
+		super();
+		this.once('finish', () => this.res.end());
+		res.header('Content-type', 'application/json-lines').flush();
+	}
+
+	// eslint-disable-next-line id-denylist
+	override _write(chunk: any, encoding: BufferEncoding, cb: (error?: Error | null) => void) {
+		this.res.write(chunk, encoding);
+		this.res.flush();
+		cb();
+	}
+}
 
 @RestController('/ai-assistant')
 export class AiAssistantController {
 	constructor(private readonly aiAssistantService: AiAssistantService) {}
 
 	@Post('/chat', { rateLimit: { limit: 100 } })
-	async chat(req: AiAssistantRequest.Chat, res: Response) {
+	async chat(req: AiAssistantRequest.Chat, res: FlushableResponse) {
 		try {
-			const stream = await this.aiAssistantService.chat(req.body, req.user);
-
-			if (stream.body) {
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-				await promises.pipeline(Readable.fromWeb(stream.body), res);
+			const aiResponse = await this.aiAssistantService.chat(req.body, req.user);
+			if (aiResponse.body) {
+				await promises.pipeline(Readable.fromWeb(aiResponse.body), new OutputStream(res));
 			}
 		} catch (e) {
-			// todo add sentry reporting
 			assert(e instanceof Error);
 			ErrorReporterProxy.error(e);
 			throw new InternalServerError({ message: `Something went wrong: ${e.message}` });
