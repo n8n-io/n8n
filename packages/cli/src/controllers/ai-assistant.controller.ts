91f9c8009065283cc52962628eeaed8987006663
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-09-02 15:59:15 +0200

    switch to web streams

--- packages/cli/src/controllers/ai-assistant.controller.ts
+++ packages/cli/src/controllers/ai-assistant.controller.ts
@@ -3,27 +3,12 @@ import { AiAssistantService } from '@/services/ai-assistant.service';
 import { AiAssistantRequest } from '@/requests';
 import type { Response } from 'express';
 import type { AiAssistantSDK } from '@n8n_io/ai-assistant-sdk';
-import { Readable, promises, Writable } from 'node:stream';
+import { WritableStream } from 'node:stream/web';
 import { InternalServerError } from 'express-openapi-validator/dist/openapi.validator';
 import { strict as assert } from 'node:assert';
 import { ErrorReporterProxy } from 'n8n-workflow';
 
 type FlushableResponse = Response & { flush: () => void };
-
-class OutputStream extends Writable {
-	constructor(private readonly res: FlushableResponse) {
-		super();
-		this.once('finish', () => this.res.end());
-		res.header('Content-type', 'application/json-lines').flush();
-	}
-
-	// eslint-disable-next-line id-denylist
-	override _write(chunk: any, encoding: BufferEncoding, cb: (error?: Error | null) => void) {
-		this.res.write(chunk, encoding);
-		this.res.flush();
-		cb();
-	}
-}
 
 @RestController('/ai-assistant')
 export class AiAssistantController {
@@ -34,7 +19,16 @@ export class AiAssistantController {
 		try {
 			const aiResponse = await this.aiAssistantService.chat(req.body, req.user);
 			if (aiResponse.body) {
-				await promises.pipeline(Readable.fromWeb(aiResponse.body), new OutputStream(res));
+				res.header('Content-type', 'application/json-lines').flush();
+				await aiResponse.body.pipeTo(
+					new WritableStream({
+						write(chunk) {
+							res.write(chunk);
+							res.flush();
+						},
+					}),
+				);
+				res.end();
 			}
 		} catch (e) {
 			assert(e instanceof Error);
