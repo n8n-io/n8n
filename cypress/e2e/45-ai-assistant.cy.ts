e559530be99c874cd481eebd0558432b1dda9fb8
Milorad FIlipović <milorad@n8n.io>
2024-09-03 10:43:24 +0200

    feat(editor): AI assistant UX improvements (no-changelog) (#10568)

--- cypress/e2e/45-ai-assistant.cy.ts
+++ cypress/e2e/45-ai-assistant.cy.ts
@@ -130,14 +130,15 @@ describe('AI Assistant::enabled', () => {
 		ndv.getters.nodeExecuteButton().click();
 		aiAssistant.getters.nodeErrorViewAssistantButton().click();
 		cy.wait('@chatRequest');
-		aiAssistant.getters.quickReplies().should('have.length', 2);
-		aiAssistant.getters.quickReplies().eq(0).click();
+		aiAssistant.getters.quickReplyButtons().should('have.length', 2);
+		aiAssistant.getters.quickReplyButtons().eq(0).click();
 		cy.wait('@chatRequest');
 		aiAssistant.getters.chatMessagesUser().should('have.length', 1);
 		aiAssistant.getters.chatMessagesUser().eq(0).should('contain.text', "Sure, let's do it");
 	});
 
-	it('should send message to assistant when node is executed', () => {
+	it('should send message to assistant when node is executed only once', () => {
+		const TOTAL_REQUEST_COUNT = 1;
 		cy.intercept('POST', '/rest/ai-assistant/chat', {
 			statusCode: 200,
 			fixture: 'aiAssistant/simple_message_response.json',
@@ -148,10 +149,46 @@ describe('AI Assistant::enabled', () => {
 		aiAssistant.getters.nodeErrorViewAssistantButton().click();
 		cy.wait('@chatRequest');
 		aiAssistant.getters.chatMessagesAssistant().should('have.length', 1);
-		// Executing the same node should sende a new message to the assistant automatically
+		cy.get('@chatRequest.all').then((interceptions) => {
+			expect(interceptions).to.have.length(TOTAL_REQUEST_COUNT);
+		});
+		// Executing the same node should not send a new message if users haven't responded to quick replies
+		ndv.getters.nodeExecuteButton().click();
+		cy.get('@chatRequest.all').then((interceptions) => {
+			expect(interceptions).to.have.length(TOTAL_REQUEST_COUNT);
+		});
+		aiAssistant.getters.chatMessagesAssistant().should('have.length', 2);
+	});
+
+	it('should show quick replies when node is executed after new suggestion', () => {
+		cy.intercept('POST', '/rest/ai-assistant/chat', (req) => {
+			req.reply((res) => {
+				if (['init-error-helper', 'message'].includes(req.body.payload.type)) {
+					res.send({ statusCode: 200, fixture: 'aiAssistant/simple_message_response.json' });
+				} else if (req.body.payload.type === 'event') {
+					res.send({ statusCode: 200, fixture: 'aiAssistant/node_execution_error_response.json' });
+				} else {
+					res.send({ statusCode: 500 });
+				}
+			});
+		}).as('chatRequest');
+		cy.createFixtureWorkflow('aiAssistant/test_workflow.json');
+		wf.actions.openNode('Edit Fields');
+		ndv.getters.nodeExecuteButton().click();
+		aiAssistant.getters.nodeErrorViewAssistantButton().click();
+		cy.wait('@chatRequest');
+		aiAssistant.getters.chatMessagesAssistant().should('have.length', 1);
 		ndv.getters.nodeExecuteButton().click();
 		cy.wait('@chatRequest');
-		aiAssistant.getters.chatMessagesAssistant().should('have.length', 2);
+		// Respond 'Yes' to the quick reply (request new suggestion)
+		aiAssistant.getters.quickReplies().contains('Yes').click();
+		cy.wait('@chatRequest');
+		// No quick replies at this point
+		aiAssistant.getters.quickReplies().should('not.exist');
+		ndv.getters.nodeExecuteButton().click();
+		// But after executing the node again, quick replies should be shown
+		aiAssistant.getters.chatMessagesAssistant().should('have.length', 4);
+		aiAssistant.getters.quickReplies().should('have.length', 2);
 	});
 
 	it('should warn before starting a new session', () => {
