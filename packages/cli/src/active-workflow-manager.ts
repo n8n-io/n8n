7a22f1facd95e85be8762e359a9d2c9d8029ed30
Iván Ovejero <ivov.src@gmail.com>
2024-09-03 15:52:01 +0200

    Remove log

--- packages/cli/src/active-workflow-manager.ts
+++ packages/cli/src/active-workflow-manager.ts
@@ -227,7 +227,6 @@ export class ActiveWorkflowManager {
 	 * deregister those webhooks from external services.
 	 */
 	async clearWebhooks(workflowId: string) {
-		console.log('========== clearWebhooks');
 		const workflowData = await this.workflowRepository.findOne({
 			where: { id: workflowId },
 		});
