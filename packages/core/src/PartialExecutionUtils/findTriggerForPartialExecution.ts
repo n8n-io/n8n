78f95f7cb3dc83ba38c47d0fa14ee7f5f2689177
Danny Martini <danny@n8n.io>
2024-09-04 11:53:21 +0200

    update comment and clean dead code

--- packages/core/src/PartialExecutionUtils/findTriggerForPartialExecution.ts
+++ packages/core/src/PartialExecutionUtils/findTriggerForPartialExecution.ts
@@ -1,8 +1,6 @@
 import type { INode, Workflow } from 'n8n-workflow';
 
 function findAllParentTriggers(workflow: Workflow, destinationNode: string) {
-	// Traverse from the destination node back until we found all trigger nodes.
-	// Do this recursively, because why not.
 	const parentNodes = workflow
 		.getParentNodes(destinationNode)
 		.map((name) => {
@@ -33,7 +31,8 @@ export function findTriggerForPartialExecution(
 		(trigger) => !trigger.disabled,
 	);
 	const pinnedTriggers = parentTriggers
-		// TODO: add the other filters here from `findAllPinnedActivators`
+		// TODO: add the other filters here from `findAllPinnedActivators`, see
+		// copy below.
 		.filter((trigger) => workflow.pinData?.[trigger.name])
 		.sort((n) => (n.type.endsWith('webhook') ? -1 : 1));
 
@@ -54,51 +53,4 @@ export function findTriggerForPartialExecution(
 //				node.type !== 'n8n-nodes-base.respondToWebhook',
 //		)
 //		.sort((a) => (a.type.endsWith('webhook') ? -1 : 1));
-//}
-
-// TODO: deduplicate this with
-// packages/cli/src/workflows/workflowExecution.service.ts
-//function selectPinnedActivatorStarter(
-//	workflow: Workflow,
-//	startNodes?: string[],
-//	pinData?: IPinData,
-//) {
-//	if (!pinData || !startNodes) return null;
-//
-//	const allPinnedActivators = findAllPinnedActivators(workflow, pinData);
-//
-//	if (allPinnedActivators.length === 0) return null;
-//
-//	const [firstPinnedActivator] = allPinnedActivators;
-//
-//	// full manual execution
-//
-//	if (startNodes?.length === 0) return firstPinnedActivator ?? null;
-//
-//	// partial manual execution
-//
-//	/**
-//	 * If the partial manual execution has 2+ start nodes, we search only the zeroth
-//	 * start node's parents for a pinned activator. If we had 2+ start nodes without
-//	 * a common ancestor and so if we end up finding multiple pinned activators, we
-//	 * would still need to return one to comply with existing usage.
-//	 */
-//	const [firstStartNodeName] = startNodes;
-//
-//	const parentNodeNames =
-//		//new Workflow({
-//		//		nodes: workflow.nodes,
-//		//		connections: workflow.connections,
-//		//		active: workflow.active,
-//		//		nodeTypes: this.nodeTypes,
-//		//	}).
-//		workflow.getParentNodes(firstStartNodeName);
-//
-//	if (parentNodeNames.length > 0) {
-//		const parentNodeName = parentNodeNames.find((p) => p === firstPinnedActivator.name);
-//
-//		return allPinnedActivators.find((pa) => pa.name === parentNodeName) ?? null;
-//	}
-//
-//	return allPinnedActivators.find((pa) => pa.name === firstStartNodeName) ?? null;
 //}
