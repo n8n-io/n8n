22ea05ad018701ba90a6f0285b2acb4fb22b53d0
Valentina Lilova <valentina.lilova98@gmail.com>
2024-09-04 08:29:50 +0200

    Remove unused properties

--- packages/nodes-base/credentials/GoogleMyBusinessOAuth2Api.credentials.ts
+++ packages/nodes-base/credentials/GoogleMyBusinessOAuth2Api.credentials.ts
@@ -1,10 +1,4 @@
-import {
-	type IAuthenticateGeneric,
-	type ICredentialTestRequest,
-	type ICredentialType,
-	type IHttpRequestMethods,
-	type INodeProperties,
-} from 'n8n-workflow';
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 const scopes = ['https://www.googleapis.com/auth/business.manage'];
 
@@ -24,35 +18,12 @@ export class GoogleMyBusinessOAuth2Api implements ICredentialType {
 			type: 'hidden',
 			default: scopes.join(' '),
 		},
+		{
+			displayName:
+				'Make sure that you have fulfilled the prerequisites and requested access to Google My Business API. <a href="https://developers.google.com/my-business/content/prereqs" target="_blank">More info</a>. Make sure that you have enabled the following APIs & Services in the Google Cloud Console: Google My Business API, Google My Business Management API. <a href="https://docs.n8n.io/integrations/builtin/credentials/google/oauth-generic/#scopes" target="_blank">More info</a>.',
+			name: 'notice',
+			type: 'notice',
+			default: '',
+		},
 	];
-
-	// This allows the credential to be used by other parts of n8n
-	// stating how this credential is injected as part of the request
-	// An example is the Http Request node that can make generic calls
-	// reusing this credential
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				Authorization: 'Bearer {{$credentials.accessToken}}',
-			},
-		},
-	};
-
-	// The test block is used to test the credential
-	// It uses a deprecated method to avoid the need of account and a location input
-	// ToDo: Test the test function
-	test: ICredentialTestRequest = {
-		request: {
-			method: 'GET' as IHttpRequestMethods,
-			url: 'https://mybusiness.googleapis.com/v4/categories',
-			headers: {
-				Authorization: 'Bearer {{$credentials.accessToken}}',
-			},
-			qs: {
-				pageSize: 1,
-			},
-			json: true,
-		},
-	};
 }
