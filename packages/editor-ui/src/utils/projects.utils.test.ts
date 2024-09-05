e318a6323ad5300e9520f3b7ed226b82b4ffc33e
Csaba Tuncsik <csaba@n8n.io>
2024-09-05 09:17:46 +0200

    refactor(editor): Refactor project utils `splitName` (no-changelog) (#10669)

--- packages/editor-ui/src/utils/projects.utils.test.ts
+++ packages/editor-ui/src/utils/projects.utils.test.ts
@@ -5,54 +5,71 @@ describe('splitName', () => {
 		[
 			'First Last <email@domain.com>',
 			{
-				firstName: 'First',
-				lastName: 'Last',
+				name: 'First Last',
 				email: 'email@domain.com',
 			},
 		],
 		[
 			'First Last Third <email@domain.com>',
 			{
-				firstName: 'First Last',
-				lastName: 'Third',
+				name: 'First Last Third',
 				email: 'email@domain.com',
 			},
 		],
 		[
 			'First Last Third Fourth <email@domain.com>',
 			{
-				firstName: 'First Last Third',
-				lastName: 'Fourth',
+				name: 'First Last Third Fourth',
+				email: 'email@domain.com',
+			},
+		],
+		[
+			' First Last Third Fourth <email@domain.com>',
+			{
+				name: 'First Last Third Fourth',
 				email: 'email@domain.com',
 			},
 		],
 		[
 			'<email@domain.com>',
 			{
-				firstName: undefined,
-				lastName: undefined,
+				name: undefined,
 				email: 'email@domain.com',
 			},
 		],
 		[
 			' <email@domain.com>',
 			{
-				firstName: '',
-				lastName: '',
+				name: undefined,
 				email: 'email@domain.com',
 			},
 		],
 		[
 			'My project',
 			{
-				firstName: 'My',
-				lastName: 'project',
+				name: 'My project',
+				email: undefined,
+			},
+		],
+		[
+			' My project ',
+			{
+				name: 'My project',
+				email: undefined,
 			},
 		],
 		[
 			'MyProject',
 			{
-				firstName: 'MyProject',
+				name: 'MyProject',
+				email: undefined,
+			},
+		],
+		[
+			undefined,
+			{
+				name: undefined,
+				email: undefined,
 			},
 		],
 	])('should split a name in the format "First Last <email@domain.com>"', (input, result) => {
