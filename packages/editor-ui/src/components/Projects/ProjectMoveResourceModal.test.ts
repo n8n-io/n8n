a2a8c3b7efe5e5f7140cb42c4422e958ba6056f7
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-09-10 17:38:56 +0200

    test(editor): Add resource moving component tests

--- packages/editor-ui/src/components/Projects/ProjectMoveResourceModal.test.ts
+++ packages/editor-ui/src/components/Projects/ProjectMoveResourceModal.test.ts
@@ -1,8 +1,10 @@
-import { createPinia, setActivePinia } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
 import { createComponentRenderer } from '@/__tests__/render';
 import { PROJECT_MOVE_RESOURCE_MODAL } from '@/constants';
 import ProjectMoveResourceModal from '@/components/Projects/ProjectMoveResourceModal.vue';
 import { useTelemetry } from '@/composables/useTelemetry';
+import { mockedStore } from '@/__tests__/utils';
+import { useProjectsStore } from '@/stores/projects.store';
 
 const renderComponent = createComponentRenderer(ProjectMoveResourceModal, {
 	global: {
@@ -19,25 +21,40 @@ let telemetry: ReturnType<typeof useTelemetry>;
 
 describe('ProjectMoveResourceModal', () => {
 	beforeEach(() => {
-		setActivePinia(createPinia());
 		telemetry = useTelemetry();
 	});
 
 	it('should send telemetry when mounted', async () => {
+		const pinia = createTestingPinia();
 		const telemetryTrackSpy = vi.spyOn(telemetry, 'track');
+
+		const projectsStore = mockedStore(useProjectsStore);
+		projectsStore.projects = [
+			{
+				id: '1',
+				name: 'My Project',
+				type: 'personal',
+				role: 'project:personalOwner',
+				createdAt: '2021-01-01T00:00:00.000Z',
+				updatedAt: '2021-01-01T00:00:00.000Z',
+			},
+		];
 
 		const props = {
 			modalName: PROJECT_MOVE_RESOURCE_MODAL,
 			data: {
 				resourceType: 'workflow',
+				resourceTypeLabel: 'Workflow',
 				resource: {
 					id: '1',
+					homeProject: {
+						id: '2',
+						name: 'My Project',
+					},
 				},
-				projectId: '1',
-				projectName: 'My Project',
 			},
 		};
-		renderComponent({ props });
+		renderComponent({ props, pinia });
 		expect(telemetryTrackSpy).toHaveBeenCalledWith(
 			'User clicked to move a workflow',
 			expect.objectContaining({ workflow_id: '1' }),
