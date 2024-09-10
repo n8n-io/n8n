02e94f1afe05c7bb49c4b4ea4939d4fae5c4b571
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-09-10 18:15:21 +0300

    fix(benchmark): Make benchmark checks more robust (#10761)

--- packages/@n8n/benchmark/scenarios/jsCodeNodeOnceForEach/jsCodeNodeOnceForEach.script.js
+++ packages/@n8n/benchmark/scenarios/jsCodeNodeOnceForEach/jsCodeNodeOnceForEach.script.js
@@ -7,6 +7,16 @@ export default function () {
 	const res = http.post(`${apiBaseUrl}/webhook/code-node-benchmark`, {});
 	check(res, {
 		'is status 200': (r) => r.status === 200,
-		'has items in response': (r) => JSON.parse(r.body).length === 5,
+		'has items in response': (r) => {
+			if (r.status !== 200) return false;
+
+			try {
+				const body = JSON.parse(r.body);
+				return Array.isArray(body) ? body.length === 5 : false;
+			} catch (error) {
+				console.error('Error parsing response body: ', error);
+				return false;
+			}
+		},
 	});
 }
