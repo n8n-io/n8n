14da392a49304cc532495160d2fb6a3a0777925e
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-09-13 16:22:14 +0200

    fix(editor): Fix unit test

--- packages/editor-ui/src/__tests__/setup.ts
+++ packages/editor-ui/src/__tests__/setup.ts
@@ -1,5 +1,9 @@
 import '@testing-library/jest-dom';
 import { configure } from '@testing-library/vue';
+import { config } from '@vue/test-utils';
+import { N8nPlugin } from 'n8n-design-system';
+
+config.global.plugins = [N8nPlugin];
 
 configure({ testIdAttribute: 'data-test-id' });
 
