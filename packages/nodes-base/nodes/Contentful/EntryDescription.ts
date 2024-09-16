e39ad2efa6dce5f77baddbe4ff19a3d00026474a
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-09-16 12:19:40 +0100

    fix(Contentful Node): Add missing additional fields to entry > get

--- packages/nodes-base/nodes/Contentful/EntryDescription.ts
+++ packages/nodes-base/nodes/Contentful/EntryDescription.ts
@@ -177,6 +177,28 @@ export const fields: INodeProperties[] = [
 		],
 	},
 	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		displayOptions: {
+			show: {
+				resource: [resource.value],
+				operation: ['get'],
+			},
+		},
+		options: [
+			{
+				displayName: 'RAW Data',
+				name: 'rawData',
+				type: 'boolean',
+				default: false,
+				description: 'Whether the data should be returned RAW instead of parsed',
+			},
+		],
+	},
+	{
 		displayName: 'Entry ID',
 		name: 'entryId',
 		type: 'string',
