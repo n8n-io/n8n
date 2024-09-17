bb0b1037475cf50d13314bb56de5f1f173f8e139
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-09-17 11:05:33 +0200

    fix(editor): Fix type issue

--- packages/design-system/src/utils/string.ts
+++ packages/design-system/src/utils/string.ts
@@ -1,2 +1,2 @@
-export const truncate = (text: string, length?: number = 30): string =>
+export const truncate = (text: string, length = 30): string =>
 	text.length > length ? text.slice(0, length).trim() + '...' : text;
