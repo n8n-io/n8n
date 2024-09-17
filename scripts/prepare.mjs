d0b3bce5b771af38bfc134ac6818066a588a5ec3
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-09-17 16:30:25 +0300

    Detect in prepare script if we are in docker

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
+import fs from 'node:fs';
 
-if (process.env.CI) {
-	process.exit(0)
+function isDockerContainer() {
+	try {
+		return fs.existsSync('/.dockerenv');
+	} catch {
+		return false;
+	}
+}
+
+function isDockerBuildkit() {
+	try {
+		const cgroupPath = '/proc/1/cgroup';
+
+		if (!fs.existsSync(cgroupPath)) return false;
+
+		const cgroupContent = fs.readFileSync(cgroupPath, 'utf8');
+		return cgroupContent.includes('docker') || cgroupContent.includes('0::/');
+	} catch {
+		return;
+	}
+}
+
+function isDocker() {
+	return isDockerContainer() || isDockerBuildkit();
+}
+
+if (process.env.CI || isDocker()) {
+	process.exit(0);
 }
 
 execSync('./node_modules/.bin/lefthook install', { stdio: 'inherit' });
