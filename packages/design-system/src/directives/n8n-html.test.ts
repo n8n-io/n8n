83574a70e25957d3439ba8547bd3a3761f14cffa
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-09-17 11:51:25 +0200

    fix(editor): Update unit test wording

--- packages/design-system/src/directives/n8n-html.test.ts
+++ packages/design-system/src/directives/n8n-html.test.ts
@@ -30,7 +30,7 @@ describe('Directive n8n-html', () => {
 	it('should not touch safe html', async () => {
 		const { html } = render(TestComponent, {
 			props: {
-				html: '<span>text</span><a href="https://malicious.com">malicious</a><img alt="Ok" src="./images/logo.svg" />',
+				html: '<span>text</span><a href="https://safe.com">safe</a><img alt="Ok" src="./images/logo.svg" />',
 			},
 			global: {
 				directives: {
