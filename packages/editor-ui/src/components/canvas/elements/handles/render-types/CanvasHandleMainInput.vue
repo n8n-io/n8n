5fe33d4cdc1d2eab30f7eadd86ea76f2e1f768dd
r00gm <raul00gm@gmail.com>
2024-09-18 10:33:31 +0200

    feat: Hook handle to event logic

--- packages/editor-ui/src/components/canvas/elements/handles/render-types/CanvasHandleMainInput.vue
+++ packages/editor-ui/src/components/canvas/elements/handles/render-types/CanvasHandleMainInput.vue
@@ -1,14 +1,32 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useVueFlow } from '@vue-flow/core';
 import { useCanvasNodeHandle } from '@/composables/useCanvasNodeHandle';
 
-const { label } = useCanvasNodeHandle();
+const { label, handleString } = useCanvasNodeHandle();
+
+const { onEdgeMouseEnter, onEdgeMouseLeave } = useVueFlow();
+
+const sourceEdgeIsHovered = ref(false);
+onEdgeMouseEnter(({ edge }) => {
+	if (handleString.value !== edge.targetHandle) return;
+	sourceEdgeIsHovered.value = true;
+});
+
+onEdgeMouseLeave(({ edge }) => {
+	if (handleString.value !== edge.targetHandle) return;
+	sourceEdgeIsHovered.value = false;
+});
 
 const handleClasses = 'target';
 </script>
 <template>
 	<div :class="['canvas-node-handle-main-input', $style.handle]">
 		<div :class="[$style.label]">{{ label }}</div>
-		<CanvasHandleRectangle :handle-classes="handleClasses" />
+		<CanvasHandleRectangle
+			:handle-classes="handleClasses"
+			:source-edge-hovered="sourceEdgeIsHovered"
+		/>
 	</div>
 </template>
 
