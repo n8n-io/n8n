22801645acb9536ea46b2f0092bd167103afbe09
r00gm <raul00gm@gmail.com>
2024-09-18 10:33:03 +0200

    feat: Add hovered prop

--- packages/editor-ui/src/components/canvas/elements/handles/render-types/parts/CanvasHandleRectangle.vue
+++ packages/editor-ui/src/components/canvas/elements/handles/render-types/parts/CanvasHandleRectangle.vue
@@ -2,6 +2,7 @@
 withDefaults(
 	defineProps<{
 		handleClasses?: string;
+		sourceEdgeHovered: boolean;
 	}>(),
 	{
 		handleClasses: undefined,
@@ -10,7 +11,13 @@ withDefaults(
 </script>
 
 <template>
-	<div :class="[$style.rectangle, handleClasses]" />
+	<div
+		:class="{
+			[$style.rectangle]: true,
+			handleClasses,
+			[$style['rectangle--hovered']]: sourceEdgeHovered,
+		}"
+	/>
 </template>
 
 <style lang="scss" module>
@@ -18,9 +25,14 @@ withDefaults(
 	width: var(--handle--indicator--width);
 	height: var(--handle--indicator--height);
 	background: var(--color-foreground-xdark);
+	transition: background 0.3s ease;
 
 	&:hover {
 		background: var(--color-primary);
 	}
+}
+
+.rectangle--hovered {
+	background: var(--color-primary);
 }
 </style>
