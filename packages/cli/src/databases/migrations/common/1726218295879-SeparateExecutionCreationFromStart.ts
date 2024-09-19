99188b498535c02cc80d8de4bcedbd094c2efcc0
Iván Ovejero <ivov.src@gmail.com>
2024-09-19 10:16:55 +0200

    Copy over `startedAt` to `createdAt`

--- packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
+++ packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
@@ -1,11 +1,21 @@
 import type { MigrationContext, ReversibleMigration } from '@/databases/types';
 
 export class SeparateExecutionCreationFromStart1726218295879 implements ReversibleMigration {
-	async up({ schemaBuilder: { addColumns, column, dropNotNull } }: MigrationContext) {
+	async up({
+		schemaBuilder: { addColumns, column, dropNotNull },
+		runQuery,
+		escape,
+	}: MigrationContext) {
 		await addColumns('execution_entity', [
 			column('createdAt').notNull.timestamp().default('NOW()'),
 		]);
+
 		await dropNotNull('execution_entity', 'startedAt');
+
+		const executionEntity = escape.tableName('execution_entity');
+
+		// inaccurate for pre-migration rows but prevents `createdAt` from being nullable
+		await runQuery(`UPDATE ${executionEntity} SET createdAt = startedAt;`);
 	}
 
 	async down({ schemaBuilder: { dropColumns, addNotNull } }: MigrationContext) {
