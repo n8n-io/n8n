8fc5bd735336f68c8790f6b4793420c2f3417617
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-09-19 08:42:53 +0100

    Add test

--- packages/nodes-base/nodes/Brandfetch/test/GenericFunctions.test.ts
+++ packages/nodes-base/nodes/Brandfetch/test/GenericFunctions.test.ts
@@ -1,74 +1,48 @@
-import nock from 'nock';
+import type {
+	IExecuteFunctions,
+	IHookFunctions,
+	ILoadOptionsFunctions,
+	IHttpRequestMethods,
+	INode,
+} from 'n8n-workflow';
 
-import { IExecuteFunctions, IHookFunctions, ILoadOptionsFunctions, NodeApiError } from 'n8n-workflow';
-import { brandfetchApiRequest } from '../GenericFunctions'; // Adjust the import path as necessary
+import { brandfetchApiRequest } from '../GenericFunctions';
 
-describe('brandfetchApiRequest', () => {
-	const mockThis = {
-		helpers: {
-			requestWithAuthentication: jest.fn(),
-		},
-		getNodeParameter: jest.fn(),
-		getNode: jest.fn(),
-	} as unknown as IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions;
+export const node: INode = {
+	id: 'c4a5ca75-18c7-4cc8-bf7d-5d57bb7d84da',
+	name: 'Brandfetch',
+	type: 'n8n-nodes-base.Brandfetch',
+	typeVersion: 1,
+	position: [0, 0],
+	parameters: {
+		operation: 'font',
+		domain: 'n8n.io',
+	},
+};
 
-	beforeEach(() => {
-		jest.clearAllMocks();
-	});
+describe('Brandfetch', () => {
+	describe('brandfetchApiRequest', () => {
+		const mockThis = {
+			helpers: {
+				requestWithAuthentication: jest.fn().mockResolvedValue({ statusCode: 200 }),
+			},
+			getNode() {
+				return node;
+			},
+			getNodeParameter: jest.fn(),
+		} as unknown as IHookFunctions | IExecuteFunctions | ILoadOptionsFunctions;
 
-	it('should make a successful API request', async () => {
-		const method = 'GET';
-		const resource = '/test';
-		const body = {};
-		const qs = {};
-		const uri = 'https://api.brandfetch.io/v2/test';
-		const option = {};
+		it('should make an authenticated API request to Brandfetch', async () => {
+			const method: IHttpRequestMethods = 'GET';
+			const resource = '/brands/n8n.io';
 
-		const mockResponse = { statusCode: 200, data: 'test data' };
+			await brandfetchApiRequest.call(mockThis, method, resource);
 
-		nock('https://api.brandfetch.io').get('/v2/test').reply(200, mockResponse);
-
-		mockThis.helpers.requestWithAuthentication.mockResolvedValue(mockResponse);
-		mockThis.getNodeParameter.mockReturnValue('logo');
-
-		const response = await brandfetchApiRequest.call(
-			mockThis,
-			method,
-			resource,
-			body,
-			qs,
-			uri,
-			option,
-		);
-
-		expect(response).toEqual(mockResponse);
-		expect(mockThis.helpers.requestWithAuthentication).toHaveBeenCalledWith(
-			'brandfetchApi',
-			expect.any(Object),
-		);
-	});
-
-	it('should throw an error for non-200 status codes', async () => {
-		const method = 'GET';
-		const resource = '/brands/n8n.io';
-		const body = {};
-		const qs = {};
-		const uri = 'https://api.brandfetch.io/v2/brands/n8n.io';
-		const option = {};
-
-		const mockErrorResponse = { statusCode: 400, message: 'Bad Request' };
-
-		nock('https://api.brandfetch.io').get('/v2/brands/n8n.io').reply(400, mockErrorResponse);
-
-		mockThis.helpers.requestWithAuthentication.mockResolvedValue(mockErrorResponse);
-		mockThis.getNodeParameter.mockReturnValue('logo');
-
-		await expect(
-			brandfetchApiRequest.call(mockThis, method, resource, body, qs, uri, option),
-		).rejects.toThrow(NodeApiError);
-		expect(mockThis.helpers.requestWithAuthentication).toHaveBeenCalledWith(
-			'brandfetchApi',
-			expect.any(Object),
-		);
+			expect(mockThis.helpers.requestWithAuthentication).toHaveBeenCalledWith('brandfetchApi', {
+				method: 'GET',
+				url: 'https://api.brandfetch.io/v2/brands/n8n.io',
+				json: true,
+			});
+		});
 	});
 });
