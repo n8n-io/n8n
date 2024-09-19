ea731a948556ece2932a8d65c22f727dee0f9694
Iván Ovejero <ivov.src@gmail.com>
2024-09-19 12:02:31 +0200

    Escape column names

--- packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
+++ packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
@@ -13,9 +13,11 @@ export class SeparateExecutionCreationFromStart1726218295879 implements Reversib
 		await dropNotNull('execution_entity', 'startedAt');
 
 		const executionEntity = escape.tableName('execution_entity');
+		const createdAt = escape.columnName('createdAt');
+		const startedAt = escape.columnName('startedAt');
 
 		// inaccurate for pre-migration rows but prevents `createdAt` from being nullable
-		await runQuery(`UPDATE ${executionEntity} SET createdAt = startedAt;`);
+		await runQuery(`UPDATE ${executionEntity} SET ${createdAt} = ${startedAt};`);
 	}
 
 	async down({ schemaBuilder: { dropColumns, addNotNull } }: MigrationContext) {
