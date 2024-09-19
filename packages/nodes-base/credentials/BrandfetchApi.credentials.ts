a0fcc6955d409700ab33992c3c933429b4991a56
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2024-09-19 09:07:51 +0100

    lint fix

--- packages/nodes-base/credentials/BrandfetchApi.credentials.ts
+++ packages/nodes-base/credentials/BrandfetchApi.credentials.ts
@@ -1,4 +1,9 @@
-import type { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
 
 export class BrandfetchApi implements ICredentialType {
 	name = 'brandfetchApi';
@@ -21,7 +26,7 @@ export class BrandfetchApi implements ICredentialType {
 		type: 'generic',
 		properties: {
 			headers: {
-				'Authorization': '=Bearer {{$credentials.apiKey}}',
+				Authorization: '=Bearer {{$credentials.apiKey}}',
 			},
 		},
 	};
