e8756dc76cc1a6f71059de65677535dadad70f2f
Milorad FIlipović <milorad@n8n.io>
2024-09-19 05:38:45 +0200

    refactor(editor): Migrate `AuthView` and associated components to composition api (#10713)

--- packages/editor-ui/src/views/ForgotMyPasswordView.vue
+++ packages/editor-ui/src/views/ForgotMyPasswordView.vue
@@ -1,108 +1,102 @@
-<script lang="ts">
+<script setup lang="ts">
 import AuthView from './AuthView.vue';
-import { useToast } from '@/composables/useToast';
-
-import { defineComponent } from 'vue';
 import type { IFormBoxConfig } from '@/Interface';
-import { mapStores } from 'pinia';
+import { useI18n } from '@/composables/useI18n';
+import { useToast } from '@/composables/useToast';
 import { useSettingsStore } from '@/stores/settings.store';
 import { useUsersStore } from '@/stores/users.store';
+import { computed, ref } from 'vue';
 
-export default defineComponent({
-	name: 'ForgotMyPasswordView',
-	components: {
-		AuthView,
-	},
-	setup() {
-		return {
-			...useToast(),
-		};
-	},
-	data() {
-		return {
-			loading: false,
-		};
-	},
-	computed: {
-		...mapStores(useSettingsStore, useUsersStore),
-		formConfig(): IFormBoxConfig {
-			const EMAIL_INPUTS: IFormBoxConfig['inputs'] = [
-				{
-					name: 'email',
-					properties: {
-						label: this.$locale.baseText('auth.email'),
-						type: 'email',
-						required: true,
-						validationRules: [{ name: 'VALID_EMAIL' }],
-						autocomplete: 'email',
-						capitalize: true,
-					},
-				},
-			];
+const settingsStore = useSettingsStore();
+const usersStore = useUsersStore();
 
-			const NO_SMTP_INPUTS: IFormBoxConfig['inputs'] = [
-				{
-					name: 'no-smtp-warning',
-					properties: {
-						label: this.$locale.baseText('forgotPassword.noSMTPToSendEmailWarning'),
-						type: 'info',
-					},
-				},
-			];
+const toast = useToast();
+const locale = useI18n();
 
-			const DEFAULT_FORM_CONFIG = {
-				title: this.$locale.baseText('forgotPassword.recoverPassword'),
-				redirectText: this.$locale.baseText('forgotPassword.returnToSignIn'),
-				redirectLink: '/signin',
-			};
+const loading = ref(false);
 
-			if (this.settingsStore.isSmtpSetup) {
-				return {
-					...DEFAULT_FORM_CONFIG,
-					buttonText: this.$locale.baseText('forgotPassword.getRecoveryLink'),
-					inputs: EMAIL_INPUTS,
-				};
-			}
-			return {
-				...DEFAULT_FORM_CONFIG,
-				inputs: NO_SMTP_INPUTS,
-			};
+const formConfig = computed(() => {
+	const EMAIL_INPUTS: IFormBoxConfig['inputs'] = [
+		{
+			name: 'email',
+			properties: {
+				label: locale.baseText('auth.email'),
+				type: 'email',
+				required: true,
+				validationRules: [{ name: 'VALID_EMAIL' }],
+				autocomplete: 'email',
+				capitalize: true,
+			},
 		},
-	},
-	methods: {
-		async onSubmit(values: { email: string }) {
-			try {
-				this.loading = true;
-				await this.usersStore.sendForgotPasswordEmail(values);
+	];
 
-				this.showMessage({
-					type: 'success',
-					title: this.$locale.baseText('forgotPassword.recoveryEmailSent'),
-					message: this.$locale.baseText('forgotPassword.emailSentIfExists', {
-						interpolate: { email: values.email },
-					}),
-				});
-			} catch (error) {
-				let message = this.$locale.baseText('forgotPassword.smtpErrorContactAdministrator');
-				if (error.httpStatusCode) {
-					const { httpStatusCode: status } = error;
-					if (status === 429) {
-						message = this.$locale.baseText('forgotPassword.tooManyRequests');
-					} else if (error.httpStatusCode === 422) {
-						message = this.$locale.baseText(error.message);
-					}
-
-					this.showMessage({
-						type: 'error',
-						title: this.$locale.baseText('forgotPassword.sendingEmailError'),
-						message,
-					});
-				}
-			}
-			this.loading = false;
+	const NO_SMTP_INPUTS: IFormBoxConfig['inputs'] = [
+		{
+			name: 'no-smtp-warning',
+			properties: {
+				label: locale.baseText('forgotPassword.noSMTPToSendEmailWarning'),
+				type: 'info',
+			},
 		},
-	},
+	];
+
+	const DEFAULT_FORM_CONFIG = {
+		title: locale.baseText('forgotPassword.recoverPassword'),
+		redirectText: locale.baseText('forgotPassword.returnToSignIn'),
+		redirectLink: '/signin',
+	};
+
+	if (settingsStore.isSmtpSetup) {
+		return {
+			...DEFAULT_FORM_CONFIG,
+			buttonText: locale.baseText('forgotPassword.getRecoveryLink'),
+			inputs: EMAIL_INPUTS,
+		};
+	}
+	return {
+		...DEFAULT_FORM_CONFIG,
+		inputs: NO_SMTP_INPUTS,
+	};
 });
+
+const isFormWithEmail = (values: { [key: string]: string }): values is { email: string } => {
+	return 'email' in values;
+};
+
+const onSubmit = async (values: { [key: string]: string }) => {
+	if (!isFormWithEmail(values)) {
+		return;
+	}
+	try {
+		loading.value = true;
+		await usersStore.sendForgotPasswordEmail(values);
+
+		toast.showMessage({
+			type: 'success',
+			title: locale.baseText('forgotPassword.recoveryEmailSent'),
+			message: locale.baseText('forgotPassword.emailSentIfExists', {
+				interpolate: { email: values.email },
+			}),
+		});
+	} catch (error) {
+		let message = locale.baseText('forgotPassword.smtpErrorContactAdministrator');
+		if (error.httpStatusCode) {
+			const { httpStatusCode: status } = error;
+			if (status === 429) {
+				message = locale.baseText('forgotPassword.tooManyRequests');
+			} else if (error.httpStatusCode === 422) {
+				message = locale.baseText(error.message);
+			}
+
+			toast.showMessage({
+				type: 'error',
+				title: locale.baseText('forgotPassword.sendingEmailError'),
+				message,
+			});
+		}
+	}
+	loading.value = false;
+};
 </script>
 
 <template>
