bb5ffbd60e12ddca806142ff71bd4c5264b6a9b5
Ricardo Espinoza <ricardo@n8n.io>
2024-09-22 22:18:39 -0400

    fix typing issue

--- packages/editor-ui/src/components/TemplatesInfoCarousel.vue
+++ packages/editor-ui/src/components/TemplatesInfoCarousel.vue
@@ -24,7 +24,7 @@ const props = withDefaults(
 );
 
 const emit = defineEmits<{
-	openCollection: [payload: { event: MouseEvent; id: string }];
+	openCollection: [payload: { event: MouseEvent; id: number }];
 }>();
 
 const carouselScrollPosition = ref(0);
@@ -45,7 +45,7 @@ const updateCarouselScroll = () => {
 };
 
 const onCardClick = (event: MouseEvent, id: number) => {
-	emit('openCollection', { event, id: id.toString() });
+	emit('openCollection', { event, id });
 };
 
 const scrollLeft = () => {
