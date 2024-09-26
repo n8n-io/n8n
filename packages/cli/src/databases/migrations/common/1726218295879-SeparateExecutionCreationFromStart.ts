6a62d475c5a39309f9d802240db7019d8f17e2aa
Iván Ovejero <ivov.src@gmail.com>
2024-09-26 11:33:52 +0200

    Batch updates

--- packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
+++ packages/cli/src/databases/migrations/common/1726218295879-SeparateExecutionCreationFromStart.ts
@@ -5,6 +5,7 @@ export class SeparateExecutionCreationFromStart1726218295879 implements Reversib
 		schemaBuilder: { addColumns, column, dropNotNull },
 		runQuery,
 		escape,
+		runInBatches,
 	}: MigrationContext) {
 		await addColumns('execution_entity', [
 			column('createdAt').notNull.timestamp().default('NOW()'),
@@ -15,9 +16,22 @@ export class SeparateExecutionCreationFromStart1726218295879 implements Reversib
 		const executionEntity = escape.tableName('execution_entity');
 		const createdAt = escape.columnName('createdAt');
 		const startedAt = escape.columnName('startedAt');
+		const id = escape.columnName('id');
 
-		// inaccurate for pre-migration rows but prevents `createdAt` from being nullable
-		await runQuery(`UPDATE ${executionEntity} SET ${createdAt} = ${startedAt};`);
+		const BATCH_SIZE = 500;
+
+		await runInBatches(
+			`SELECT ${id} FROM ${executionEntity}`,
+			async (batch: Array<{ id: string }>) => {
+				const ids = batch.map((row) => row.id).join(',');
+				await runQuery(`
+          UPDATE ${executionEntity}
+          SET ${createdAt} = ${startedAt}
+          WHERE ${id} IN (${ids});
+        `);
+			},
+			BATCH_SIZE,
+		);
 	}
 
 	async down({ schemaBuilder: { dropColumns, addNotNull } }: MigrationContext) {
