efc5d859eb384c40826180786ea024b3ebcf82b6
Eugene <eugene@n8n.io>
2024-09-26 13:15:08 +0200

    chore: Correct naming of annotation-related files (#10946)

--- packages/cli/test/integration/shared/test-db.ts
+++ packages/cli/test/integration/shared/test-db.ts
@@ -87,9 +87,18 @@ const repositories = [
  */
 export async function truncate(names: Array<(typeof repositories)[number]>) {
 	for (const name of names) {
-		const RepositoryClass: Class<Repository<object>> =
-			// eslint-disable-next-line n8n-local-rules/no-dynamic-import-template
-			(await import(`@/databases/repositories/${kebabCase(name)}.repository`))[`${name}Repository`];
+		let RepositoryClass: Class<Repository<object>>;
+
+		try {
+			RepositoryClass = (await import(`@/databases/repositories/${kebabCase(name)}.repository`))[
+				`${name}Repository`
+			];
+		} catch (e) {
+			RepositoryClass = (await import(`@/databases/repositories/${kebabCase(name)}.repository.ee`))[
+				`${name}Repository`
+			];
+		}
+
 		await Container.get(RepositoryClass).delete({});
 	}
 }
