04058f0b251f379edf7a2deeef1f8cd1ad772965
Marcus <marcus@n8n.io>
2024-10-01 16:12:05 +0200

    fix lefthook path

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process';
+import path from 'path';
+
 
 // Skip lefthook install in CI or Docker build
 if (process.env.CI || process.env.DOCKER_BUILD) {
 	process.exit(0);
 }
 
-execSync('./node_modules/.bin/lefthook install', { stdio: 'inherit' });
+const lefthook = path.resolve('node_modules', '.bin', 'lefthook');
+
+execSync(`${lefthook} install`, { stdio: 'inherit' });
