74fa259b371c8d41ca2a8b0162d62cab353a76f0
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-10-02 15:16:02 +0300

    feat: Make task runners work with n8n from npm (no-changelog) (#11015)

--- packages/@n8n/task-runner-node-js/src/start.ts
+++ packages/@n8n/task-runner-node-js/src/start.ts
@@ -1,5 +1,4 @@
 import * as a from 'node:assert/strict';
-
 import { JsTaskRunner } from './code';
 import { authenticate } from './authenticator';
 
@@ -7,28 +6,39 @@ let _runner: JsTaskRunner;
 
 type Config = {
 	n8nUri: string;
-	authToken: string;
+	authToken?: string;
+	grantToken?: string;
 };
 
 function readAndParseConfig(): Config {
 	const authToken = process.env.N8N_RUNNERS_AUTH_TOKEN;
-	a.ok(authToken, 'Missing task runner auth token. Use N8N_RUNNERS_AUTH_TOKEN to configure it');
+	const grantToken = process.env.N8N_RUNNERS_GRANT_TOKEN;
+	if (!authToken && !grantToken) {
+		throw new Error(
+			'Missing task runner authentication. Use either N8N_RUNNERS_AUTH_TOKEN or N8N_RUNNERS_GRANT_TOKEN to configure it',
+		);
+	}
 
 	return {
 		n8nUri: process.env.N8N_RUNNERS_N8N_URI ?? 'localhost:5678',
 		authToken,
+		grantToken,
 	};
 }
 
 void (async function start() {
 	const config = readAndParseConfig();
 
-	const grantToken = await authenticate({
-		authToken: config.authToken,
-		n8nUri: config.n8nUri,
-	});
+	let grantToken = config.grantToken;
+	if (!grantToken) {
+		a.ok(config.authToken);
+
+		grantToken = await authenticate({
+			authToken: config.authToken,
+			n8nUri: config.n8nUri,
+		});
+	}
 
 	const wsUrl = `ws://${config.n8nUri}/rest/runners/_ws`;
-
 	_runner = new JsTaskRunner('javascript', wsUrl, grantToken, 5);
 })();
