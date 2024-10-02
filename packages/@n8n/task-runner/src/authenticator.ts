5f9e9f3b231477b2e35a4cdd81908022fd2090f7
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-10-02 16:14:57 +0200

    ci: `@n8n/task-runner` package setup (no-changelog) (#11067)

--- packages/@n8n/task-runner/src/authenticator.ts
+++ packages/@n8n/task-runner/src/authenticator.ts
@@ -1,3 +1,4 @@
+import { ApplicationError } from 'n8n-workflow';
 import * as a from 'node:assert/strict';
 
 export type AuthOpts = {
@@ -23,7 +24,9 @@ export async function authenticate(opts: AuthOpts) {
 		});
 
 		if (!response.ok) {
-			throw new Error(`Invalid response status ${response.status}: ${await response.text()}`);
+			throw new ApplicationError(
+				`Invalid response status ${response.status}: ${await response.text()}`,
+			);
 		}
 
 		const { data } = (await response.json()) as { data: { token: string } };
@@ -34,9 +37,11 @@ export async function authenticate(opts: AuthOpts) {
 	} catch (e) {
 		console.error(e);
 		const error = e as Error;
-
-		throw new Error(`Could not connect to n8n message broker ${opts.n8nUri}: ${error.message}`, {
-			cause: error,
-		});
+		throw new ApplicationError(
+			`Could not connect to n8n message broker ${opts.n8nUri}: ${error.message}`,
+			{
+				cause: error,
+			},
+		);
 	}
 }
