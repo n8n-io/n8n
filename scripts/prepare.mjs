0f372f62598564d1598d7bfafe468e656aafe63f
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-10-02 15:58:13 +0300

    chore: Fix dev setup on windows machines (no-changelog)

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process';
+import path from 'node:path';
 
 // Skip lefthook install in CI or Docker build
 if (process.env.CI || process.env.DOCKER_BUILD) {
 	process.exit(0);
 }
 
-execSync('./node_modules/.bin/lefthook install', { stdio: 'inherit' });
+const isWindows = process.platform === 'win32';
+const lefthookExecutable = isWindows ? 'lefthook.cmd' : 'lefthook';
+const executablePath = path.resolve(path.join('./node_modules/.bin/', lefthookExecutable));
+execSync(`${executablePath} install`, { stdio: 'inherit' });
