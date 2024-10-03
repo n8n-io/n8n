84d1120c0bba34aafb05e9f4399d5f86342ae966
Alex Grozav <alex@grozav.com>
2024-10-03 16:06:58 +0300

    fix: fix condition for allowing keyboard shortcuts and pasting

--- packages/editor-ui/src/views/NodeView.v2.vue
+++ packages/editor-ui/src/views/NodeView.v2.vue
@@ -242,7 +242,7 @@ const fallbackNodes = computed<INodeUi[]>(() =>
 );
 
 const keyBindingsEnabled = computed(() => {
-	return !ndvStore.activeNode;
+	return !ndvStore.activeNode && uiStore.activeModals.length === 0;
 });
 
 /**
@@ -551,11 +551,11 @@ async function onCopyNodes(ids: string[]) {
 }
 
 async function onClipboardPaste(plainTextData: string): Promise<void> {
-	if (getNodeViewTab(route) !== MAIN_HEADER_TABS.WORKFLOW) {
-		return;
-	}
-
-	if (!checkIfEditingIsAllowed()) {
+	if (
+		getNodeViewTab(route) !== MAIN_HEADER_TABS.WORKFLOW ||
+		!keyBindingsEnabled.value ||
+		!checkIfEditingIsAllowed()
+	) {
 		return;
 	}
 
