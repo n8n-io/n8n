b37112ceb39f13a694d963e4b67eeeb5c7e70e15
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-10-06 21:03:48 +0300

    refactor: Use single request to execute `runOnceForEachItem` in task runner

--- packages/nodes-base/nodes/Code/JsTaskRunnerSandbox.ts
+++ packages/nodes-base/nodes/Code/JsTaskRunnerSandbox.ts
@@ -1,3 +1,4 @@
+import { ok } from 'node:assert/strict';
 import {
 	ensureError,
 	type CodeExecutionMode,
@@ -11,34 +12,17 @@ import {
 	mapItemsNotDefinedErrorIfNeededForRunForAll,
 	validateNoDisallowedMethodsInRunForEach,
 } from './JsCodeValidator';
-import { Sandbox } from './Sandbox';
-import { ValidationError } from './ValidationError';
-
-function isArrayOfArrays(
-	data: INodeExecutionData | INodeExecutionData[] | INodeExecutionData[][],
-): data is INodeExecutionData[][] {
-	return Array.isArray(data) && data.every((item) => Array.isArray(item));
-}
-
-const TEXT_KEYS = {
-	object: {
-		singular: 'object',
-		plural: 'objects',
-	},
-};
 
 /**
  * JS Code execution sandbox that executes the JS code using task runner.
  */
-export class JsTaskRunnerSandbox extends Sandbox {
+export class JsTaskRunnerSandbox {
 	constructor(
 		private readonly jsCode: string,
 		private readonly nodeMode: CodeExecutionMode,
 		private readonly workflowMode: WorkflowExecuteMode,
 		private readonly executeFunctions: IExecuteFunctions,
-	) {
-		super(TEXT_KEYS, executeFunctions.helpers);
-	}
+	) {}
 
 	async runCode<T = unknown>(): Promise<T> {
 		const itemIndex = 0;
@@ -60,62 +44,17 @@ export class JsTaskRunnerSandbox extends Sandbox {
 		}
 	}
 
-	async runCodeAllItems(options?: {
-		multiOutput?: boolean;
-	}): Promise<INodeExecutionData[] | INodeExecutionData[][]> {
+	async runCodeAllItems(): Promise<INodeExecutionData[]> {
 		const itemIndex = 0;
 
-		const executionResult: INodeExecutionData | INodeExecutionData[] | INodeExecutionData[][] =
-			await this.executeFunctions
-				.startJob<INodeExecutionData | INodeExecutionData[] | INodeExecutionData[][]>(
-					'javascript',
-					{
-						code: this.jsCode,
-						nodeMode: this.nodeMode,
-						workflowMode: this.workflowMode,
-					},
-					itemIndex,
-				)
-				.catch((e) => {
-					const error = ensureError(e);
-					// anticipate user expecting `items` to pre-exist as in Function Item node
-					mapItemsNotDefinedErrorIfNeededForRunForAll(this.jsCode, error);
-
-					throw new ExecutionError(error);
-				});
-
-		if (executionResult === null) return [];
-
-		if (options?.multiOutput === true) {
-			// Check if executionResult is an array of arrays
-			if (!isArrayOfArrays(executionResult)) {
-				throw new ValidationError({
-					message: "The code doesn't return an array of arrays",
-					description:
-						'Please return an array of arrays. One array for the different outputs and one for the different items that get returned.',
-				});
-			}
-
-			return executionResult.map((data) => {
-				return this.validateRunCodeAllItems(data);
-			});
-		}
-
-		return this.validateRunCodeAllItems(
-			executionResult as INodeExecutionData | INodeExecutionData[],
-		);
-	}
-
-	async runCodeEachItem(itemIndex: number): Promise<INodeExecutionData | undefined> {
-		validateNoDisallowedMethodsInRunForEach(this.jsCode, itemIndex);
-
-		const executionResult: INodeExecutionData = await this.executeFunctions
-			.startJob<INodeExecutionData>(
+		return await this.executeFunctions
+			.startJob<INodeExecutionData[]>(
 				'javascript',
 				{
 					code: this.jsCode,
 					nodeMode: this.nodeMode,
 					workflowMode: this.workflowMode,
+					continueOnFail: this.executeFunctions.continueOnFail(),
 				},
 				itemIndex,
 			)
@@ -126,9 +65,29 @@ export class JsTaskRunnerSandbox extends Sandbox {
 
 				throw new ExecutionError(error);
 			});
+	}
 
-		if (executionResult === null) return undefined;
+	async runCodeForEachItem(): Promise<INodeExecutionData[]> {
+		validateNoDisallowedMethodsInRunForEach(this.jsCode, 0);
+		const itemIndex = 0;
 
-		return this.validateRunCodeEachItem(executionResult, itemIndex);
+		return await this.executeFunctions
+			.startJob<INodeExecutionData[]>(
+				'javascript',
+				{
+					code: this.jsCode,
+					nodeMode: this.nodeMode,
+					workflowMode: this.workflowMode,
+					continueOnFail: this.executeFunctions.continueOnFail(),
+				},
+				itemIndex,
+			)
+			.catch((e) => {
+				const error = ensureError(e);
+				// anticipate user expecting `items` to pre-exist as in Function Item node
+				mapItemsNotDefinedErrorIfNeededForRunForAll(this.jsCode, error);
+
+				throw new ExecutionError(error);
+			});
 	}
 }
