30b13fc6137a3a8f270059444bdb8db4601ed2b4
r00gm <raul00gm@gmail.com>
2024-10-07 13:43:12 +0200

    feat: route navigation

--- packages/editor-ui/src/components/CredentialCard.vue
+++ packages/editor-ui/src/components/CredentialCard.vue
@@ -21,6 +21,10 @@ const CREDENTIAL_LIST_ITEM_ACTIONS = {
 	MOVE: 'move',
 };
 
+const emit = defineEmits<{
+	click: [credentialId: string];
+}>();
+
 const props = withDefaults(
 	defineProps<{
 		data: ICredentialsResponse;
@@ -83,7 +87,7 @@ const formattedCreatedAtDate = computed(() => {
 });
 
 function onClick() {
-	uiStore.openExistingCredential(props.data.id);
+	emit('click', props.data.id);
 }
 
 async function onAction(action: string) {
