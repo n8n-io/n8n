2ff965a3a455ee2bcaa9b63e3cf2284d34e4048c
Danny Martini <danny@n8n.io>
2024-10-09 16:24:05 +0200

    handle cycles

--- packages/core/src/WorkflowExecute.ts
+++ packages/core/src/WorkflowExecute.ts
@@ -51,12 +51,12 @@ import PCancelable from 'p-cancelable';
 import * as NodeExecuteFunctions from './NodeExecuteFunctions';
 import {
 	DirectedGraph,
-	findCycles,
 	findStartNodes,
 	findSubgraph,
 	findTriggerForPartialExecution,
 	cleanRunData,
 	recreateNodeExecutionStack,
+	handleCycles,
 } from './PartialExecutionUtils';
 
 export class WorkflowExecute {
@@ -352,15 +352,11 @@ export class WorkflowExecute {
 		const filteredNodes = subgraph.getNodes();
 
 		// 3. Find the Start Nodes
-		const startNodes = findStartNodes({ graph: subgraph, trigger, destination, runData });
+		let startNodes = findStartNodes({ graph: subgraph, trigger, destination, runData });
 
 		// 4. Detect Cycles
-		const cycles = findCycles(workflow);
-
 		// 5. Handle Cycles
-		if (cycles.length) {
-			// TODO: handle
-		}
+		startNodes = handleCycles(graph, startNodes, trigger);
 
 		// 6. Clean Run Data
 		const newRunData: IRunData = cleanRunData(runData, graph, startNodes);
