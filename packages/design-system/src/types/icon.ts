36b561a17325bf70d6c88682bc5c334b84fd72d5
r00gm <raul00gm@gmail.com>
2024-10-10 13:32:51 +0200

    fix: typing

--- packages/design-system/src/types/icon.ts
+++ packages/design-system/src/types/icon.ts
@@ -1,8 +1,9 @@
+import type { TextColor } from 'n8n-design-system/types/text';
+
 const ICON_SIZE = ['xsmall', 'small', 'medium', 'large'] as const;
 export type IconSize = (typeof ICON_SIZE)[number];
 
-const ICON_COLOR = ['primary', 'danger', 'success', 'warning', 'text-base'] as const;
-export type IconColor = (typeof ICON_COLOR)[number];
+export type IconColor = TextColor;
 
 const ICON_ORIENTATION = ['horizontal', 'vertical'] as const;
 export type IconOrientation = (typeof ICON_ORIENTATION)[number];
