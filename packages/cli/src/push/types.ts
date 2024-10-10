04eb5de142f5002cee7317450d050099978105bb
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-10-10 16:51:50 +0200

    refactor(core): Remove sse-channel (no-changelog) (#11207)

--- packages/cli/src/push/types.ts
+++ packages/cli/src/push/types.ts
@@ -11,7 +11,15 @@ export type PushRequest = AuthenticatedRequest<{}, {}, {}, { pushRef: string }>;
 export type SSEPushRequest = PushRequest & { ws: undefined };
 export type WebSocketPushRequest = PushRequest & { ws: WebSocket };
 
-export type PushResponse = Response & { req: PushRequest };
+export type PushResponse = Response & {
+	req: PushRequest;
+	/**
+	 * `flush()` is defined in the compression middleware.
+	 * This is necessary because the compression middleware sometimes waits
+	 * for a certain amount of data before sending the data to the client
+	 */
+	flush: () => void;
+};
 
 export interface OnPushMessage {
 	pushRef: string;
