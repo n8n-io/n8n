f7118498b8c4506acc1a10974a40dcc8494706c9
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2024-10-18 16:23:42 +0300

    refactor: Remove auth token usage from task runners

--- packages/@n8n/task-runner/src/start.ts
+++ packages/@n8n/task-runner/src/start.ts
@@ -1,7 +1,5 @@
 import { ApplicationError, ensureError } from 'n8n-workflow';
-import * as a from 'node:assert/strict';
 
-import { authenticate } from './authenticator';
 import { JsTaskRunner } from './js-task-runner/js-task-runner';
 
 let runner: JsTaskRunner | undefined;
@@ -9,22 +7,17 @@ let isShuttingDown = false;
 
 type Config = {
 	n8nUri: string;
-	authToken?: string;
-	grantToken?: string;
+	grantToken: string;
 };
 
 function readAndParseConfig(): Config {
-	const authToken = process.env.N8N_RUNNERS_AUTH_TOKEN;
 	const grantToken = process.env.N8N_RUNNERS_GRANT_TOKEN;
-	if (!authToken && !grantToken) {
-		throw new ApplicationError(
-			'Missing task runner authentication. Use either N8N_RUNNERS_AUTH_TOKEN or N8N_RUNNERS_GRANT_TOKEN to configure it',
-		);
+	if (!grantToken) {
+		throw new ApplicationError('Missing N8N_RUNNERS_GRANT_TOKEN environment variable');
 	}
 
 	return {
 		n8nUri: process.env.N8N_RUNNERS_N8N_URI ?? '127.0.0.1:5679',
-		authToken,
 		grantToken,
 	};
 }
@@ -55,20 +48,10 @@ function createSignalHandler(signal: string) {
 void (async function start() {
 	const config = readAndParseConfig();
 
-	let grantToken = config.grantToken;
-	if (!grantToken) {
-		a.ok(config.authToken);
-
-		grantToken = await authenticate({
-			authToken: config.authToken,
-			n8nUri: config.n8nUri,
-		});
-	}
-
 	const wsUrl = `ws://${config.n8nUri}/runners/_ws`;
 	runner = new JsTaskRunner({
 		wsUrl,
-		grantToken,
+		grantToken: config.grantToken,
 		maxConcurrency: 5,
 		allowedBuiltInModules: process.env.NODE_FUNCTION_ALLOW_BUILTIN,
 		allowedExternalModules: process.env.NODE_FUNCTION_ALLOW_EXTERNAL,
