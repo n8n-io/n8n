9027f17accb327408679a6a69399fa29e0f3ce16
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2024-10-18 12:08:21 +0200

    fix(editor): Fix type error

--- packages/editor-ui/src/views/SettingsUsageAndPlan.test.ts
+++ packages/editor-ui/src/views/SettingsUsageAndPlan.test.ts
@@ -7,7 +7,7 @@ import SettingsUsageAndPlan from '@/views/SettingsUsageAndPlan.vue';
 import { useUIStore } from '@/stores/ui.store';
 import { COMMUNITY_PLUS_ENROLLMENT_MODAL } from '@/constants';
 import { useUsersStore } from '@/stores/users.store';
-import { IUser } from '@/Interface';
+import type { IUser } from '@/Interface';
 
 vi.mock('vue-router', () => {
 	return {
