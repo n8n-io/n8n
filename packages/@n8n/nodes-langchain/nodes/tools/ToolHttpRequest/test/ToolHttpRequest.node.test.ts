a59a4d7064e5c9b5d4747e70ab70f0cf1ffe3ffd
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-10-22 15:39:19 +0200

    simplify test

--- packages/@n8n/nodes-langchain/nodes/tools/ToolHttpRequest/test/ToolHttpRequest.node.test.ts
+++ packages/@n8n/nodes-langchain/nodes/tools/ToolHttpRequest/test/ToolHttpRequest.node.test.ts
@@ -1,75 +1,37 @@
-import type { IExecuteFunctions } from 'n8n-workflow';
+import { mock } from 'jest-mock-extended';
+import type { IExecuteFunctions, INode } from 'n8n-workflow';
 import { jsonParse } from 'n8n-workflow';
 
 import type { N8nTool } from '../../../../utils/N8nTool';
 import { ToolHttpRequest } from '../ToolHttpRequest.node';
 
 describe('ToolHttpRequest', () => {
-	let httpTool: ToolHttpRequest;
-	let executeFunctions: IExecuteFunctions;
+	const httpTool = new ToolHttpRequest();
+	const helpers = mock<IExecuteFunctions['helpers']>();
+	const executeFunctions = mock<IExecuteFunctions>({ helpers });
 
 	describe('Binary response', () => {
 		beforeEach(() => {
-			httpTool = new ToolHttpRequest();
-			executeFunctions = {
-				addInputData: jest.fn().mockResolvedValue({ index: 0 }),
-				addOutputData: jest.fn(),
-				getInputData: jest.fn(),
-				getNodeParameter: jest.fn(),
-				getNode: jest.fn(() => {
-					return {
-						type: 'n8n-nodes-base.httpRequest',
-						name: 'HTTP Request',
-						typeVersion: 1.1,
-					};
+			jest.resetAllMocks();
+			executeFunctions.getNode.mockReturnValue(
+				mock<INode>({
+					type: 'n8n-nodes-base.httpRequest',
+					name: 'HTTP Request',
+					typeVersion: 1.1,
 				}),
-				getCredentials: jest.fn(),
-				helpers: {
-					httpRequest: jest.fn(),
-					httpRequestWithAuthentication: jest.fn(),
-					request: jest.fn(),
-					requestOAuth1: jest.fn(
-						async () =>
-							await Promise.resolve({
-								statusCode: 200,
-								headers: { 'content-type': 'application/json' },
-								body: Buffer.from(JSON.stringify({ success: true })),
-							}),
-					),
-					requestOAuth2: jest.fn(
-						async () =>
-							await Promise.resolve({
-								statusCode: 200,
-								headers: { 'content-type': 'application/json' },
-								body: Buffer.from(JSON.stringify({ success: true })),
-							}),
-					),
-					requestWithAuthentication: jest.fn(),
-					requestWithAuthenticationPaginated: jest.fn(),
-					assertBinaryData: jest.fn(),
-					getBinaryStream: jest.fn(),
-					getBinaryMetadata: jest.fn(),
-					binaryToString: jest.fn((buffer: Buffer) => {
-						return buffer.toString();
-					}),
-					prepareBinaryData: jest.fn(),
-				},
-				getContext: jest.fn(),
-				sendMessageToUI: jest.fn(),
-				continueOnFail: jest.fn(),
-				getMode: jest.fn(),
-			} as unknown as IExecuteFunctions;
+			);
+			executeFunctions.addInputData.mockReturnValue({ index: 0 });
 		});
 
 		it('should return the error when receiving a binary response', async () => {
-			(executeFunctions.helpers.httpRequest as jest.Mock).mockResolvedValue({
+			helpers.httpRequest.mockResolvedValue({
 				body: Buffer.from(''),
 				headers: {
 					'content-type': 'image/jpeg',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -87,20 +49,20 @@ describe('ToolHttpRequest', () => {
 			const { response } = await httpTool.supplyData.call(executeFunctions, 0);
 
 			const res = await (response as N8nTool).invoke({});
-			expect(executeFunctions.helpers.httpRequest as jest.Mock).toHaveBeenCalled();
+			expect(helpers.httpRequest).toHaveBeenCalled();
 			expect(res).toContain('error');
 			expect(res).toContain('Binary data is not supported');
 		});
 
 		it('should return the response text when receiving a text response', async () => {
-			(executeFunctions.helpers.httpRequest as jest.Mock).mockResolvedValue({
+			helpers.httpRequest.mockResolvedValue({
 				body: 'Hello World',
 				headers: {
 					'content-type': 'text/plain',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -118,19 +80,19 @@ describe('ToolHttpRequest', () => {
 			const { response } = await httpTool.supplyData.call(executeFunctions, 0);
 
 			const res = await (response as N8nTool).invoke({});
-			expect(executeFunctions.helpers.httpRequest as jest.Mock).toHaveBeenCalled();
+			expect(helpers.httpRequest).toHaveBeenCalled();
 			expect(res).toEqual('Hello World');
 		});
 
 		it('should return the response text when receiving a text response with a charset', async () => {
-			(executeFunctions.helpers.httpRequest as jest.Mock).mockResolvedValue({
+			helpers.httpRequest.mockResolvedValue({
 				body: 'こんにちは世界',
 				headers: {
 					'content-type': 'text/plain; charset=iso-2022-jp',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -148,21 +110,21 @@ describe('ToolHttpRequest', () => {
 			const { response } = await httpTool.supplyData.call(executeFunctions, 0);
 
 			const res = await (response as N8nTool).invoke({});
-			expect(executeFunctions.helpers.httpRequest as jest.Mock).toHaveBeenCalled();
+			expect(helpers.httpRequest).toHaveBeenCalled();
 			expect(res).toEqual('こんにちは世界');
 		});
 
 		it('should return the response object when receiving a JSON response', async () => {
 			const mockJson = { hello: 'world' };
 
-			(executeFunctions.helpers.httpRequest as jest.Mock).mockResolvedValue({
+			helpers.httpRequest.mockResolvedValue({
 				body: JSON.stringify(mockJson),
 				headers: {
 					'content-type': 'application/json',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -180,19 +142,19 @@ describe('ToolHttpRequest', () => {
 			const { response } = await httpTool.supplyData.call(executeFunctions, 0);
 
 			const res = await (response as N8nTool).invoke({});
-			expect(executeFunctions.helpers.httpRequest as jest.Mock).toHaveBeenCalled();
+			expect(helpers.httpRequest).toHaveBeenCalled();
 			expect(jsonParse(res)).toEqual(mockJson);
 		});
 
 		it('should handle authentication with predefined credentials', async () => {
-			(executeFunctions.helpers.httpRequestWithAuthentication as jest.Mock).mockResolvedValue({
+			helpers.httpRequestWithAuthentication.mockResolvedValue({
 				body: 'Hello World',
 				headers: {
 					'content-type': 'text/plain',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -217,9 +179,7 @@ describe('ToolHttpRequest', () => {
 
 			expect(res).toEqual('Hello World');
 
-			expect(
-				executeFunctions.helpers.httpRequestWithAuthentication as jest.Mock,
-			).toHaveBeenCalledWith(
+			expect(helpers.httpRequestWithAuthentication).toHaveBeenCalledWith(
 				'linearApi',
 				expect.objectContaining({
 					returnFullResponse: true,
@@ -229,14 +189,14 @@ describe('ToolHttpRequest', () => {
 		});
 
 		it('should handle authentication with generic credentials', async () => {
-			(executeFunctions.helpers.httpRequest as jest.Mock).mockResolvedValue({
+			helpers.httpRequest.mockResolvedValue({
 				body: 'Hello World',
 				headers: {
 					'content-type': 'text/plain',
 				},
 			});
 
-			(executeFunctions.getNodeParameter as jest.Mock).mockImplementation((paramName: string) => {
+			executeFunctions.getNodeParameter.mockImplementation((paramName: string) => {
 				switch (paramName) {
 					case 'method':
 						return 'GET';
@@ -255,7 +215,7 @@ describe('ToolHttpRequest', () => {
 				}
 			});
 
-			(executeFunctions.getCredentials as jest.Mock).mockResolvedValue({
+			executeFunctions.getCredentials.mockResolvedValue({
 				user: 'username',
 				password: 'password',
 			});
@@ -266,7 +226,7 @@ describe('ToolHttpRequest', () => {
 
 			expect(res).toEqual('Hello World');
 
-			expect(executeFunctions.helpers.httpRequest as jest.Mock).toHaveBeenCalledWith(
+			expect(helpers.httpRequest).toHaveBeenCalledWith(
 				expect.objectContaining({
 					returnFullResponse: true,
 					auth: expect.objectContaining({
