10ca6ee76181d3a331db3cf2ee56598622c86e4c
Shireen Missi <shireen@n8n.io>
2024-10-23 12:40:30 +0100

    Fix mysql migration and use raw queries

--- packages/cli/src/databases/migrations/common/1729607673464-UpdateProcessedDataValueColumnToText.ts
+++ packages/cli/src/databases/migrations/common/1729607673464-UpdateProcessedDataValueColumnToText.ts
@@ -2,41 +2,35 @@ import type { MigrationContext, ReversibleMigration } from '@/databases/types';
 
 const processedDataTableName = 'processed_data';
 export class UpdateProcessedDataValueColumnToText1729607673464 implements ReversibleMigration {
-	async up({
-		schemaBuilder: { addNotNull, addColumns, dropColumns, column },
-		runQuery,
-		tablePrefix,
-	}: MigrationContext) {
+	async up({ schemaBuilder: { addNotNull }, isMysql, runQuery, tablePrefix }: MigrationContext) {
 		const prefixedTableName = `${tablePrefix}${processedDataTableName}`;
+		await runQuery(`ALTER TABLE ${prefixedTableName} ADD COLUMN value_temp TEXT;`);
+		await runQuery(`UPDATE ${prefixedTableName} SET value_temp = value;`);
+		await runQuery(`ALTER TABLE ${prefixedTableName} DROP COLUMN value;`);
 
-		await addColumns(processedDataTableName, [column('value_temp').text]);
-
-		await runQuery(`
-		UPDATE ${prefixedTableName}
-		SET value_temp = value;
-	`);
-
-		await dropColumns(processedDataTableName, ['value']);
-		await runQuery(`ALTER TABLE ${prefixedTableName} RENAME COLUMN value_temp TO value`);
-		await addNotNull(processedDataTableName, 'value');
+		if (isMysql) {
+			await runQuery(
+				`ALTER TABLE ${processedDataTableName} CHANGE value_temp value TEXT NOT NULL;`,
+			);
+		} else {
+			await runQuery(`ALTER TABLE ${prefixedTableName} RENAME COLUMN value_temp TO value`);
+			await addNotNull(processedDataTableName, 'value');
+		}
 	}
 
-	async down({
-		schemaBuilder: { addNotNull, addColumns, dropColumns, column },
-		runQuery,
-		tablePrefix,
-	}: MigrationContext) {
+	async down({ schemaBuilder: { addNotNull }, isMysql, runQuery, tablePrefix }: MigrationContext) {
 		const prefixedTableName = `${tablePrefix}${processedDataTableName}`;
+		await runQuery(`ALTER TABLE ${prefixedTableName} ADD COLUMN value_temp VARCHAR(255);`);
+		await runQuery(`UPDATE ${prefixedTableName} SET value_temp = value;`);
+		await runQuery(`ALTER TABLE ${prefixedTableName} DROP COLUMN value;`);
 
-		await addColumns(processedDataTableName, [column('value_temp').varchar(255)]);
-
-		await runQuery(`
-		UPDATE ${prefixedTableName}
-		SET value_temp = value;
-	`);
-
-		await dropColumns(processedDataTableName, ['value']);
-		await runQuery(`ALTER TABLE ${prefixedTableName} RENAME COLUMN value_temp TO value`);
-		await addNotNull(processedDataTableName, 'value');
+		if (isMysql) {
+			await runQuery(
+				`ALTER TABLE ${processedDataTableName} CHANGE value_temp value VARCHAR(255) NOT NULL;`,
+			);
+		} else {
+			await runQuery(`ALTER TABLE ${prefixedTableName} RENAME COLUMN value_temp TO value`);
+			await addNotNull(processedDataTableName, 'value');
+		}
 	}
 }
