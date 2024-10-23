6fcb8ea0c682ef24079dbaebfed9ea5656256f60
Alex Grozav <alex@grozav.com>
2024-10-23 17:08:27 +0300

    feat(editor): Improve new canvas discovery design by specifying ALPHA version (no-changelog) (#11373)

--- packages/design-system/src/types/action-dropdown.ts
+++ packages/design-system/src/types/action-dropdown.ts
@@ -4,6 +4,7 @@ export interface ActionDropdownItem {
 	id: string;
 	label: string;
 	badge?: string;
+	badgeProps?: Record<string, unknown>;
 	icon?: string;
 	divided?: boolean;
 	disabled?: boolean;
