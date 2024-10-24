39adb5979e6fcbb87d27fbfdf356704bc9a30b5a
Matthias Stallmann <feelgood.interface@gmail.com>
2024-10-24 15:45:51 +0200

    deep merge generic pagination request options

--- packages/workflow/src/RoutingNode.ts
+++ packages/workflow/src/RoutingNode.ts
@@ -688,7 +688,7 @@ export class RoutingNode {
 						// Make the HTTP request
 						tempResponseData = await this.rawRoutingRequest(
 							executeSingleFunctions,
-							{ ...requestData, options: { ...requestData.options, ...paginateRequestData } },
+							{ ...requestData, options: merge({}, requestData.options, paginateRequestData) },
 							credentialType,
 							credentialsDecrypted,
 						);
