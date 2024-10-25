233d1c77ca7eb6074863af2f774cfd6edb43f8ec
Charlie Kolb <charlie@n8n.io>
2024-10-25 12:01:15 +0200

    remove comment

--- cypress/e2e/2270-ADO-opening-webhook-ndv-marks-workflow-as-unsaved.cy.ts
+++ cypress/e2e/2270-ADO-opening-webhook-ndv-marks-workflow-as-unsaved.cy.ts
@@ -5,7 +5,6 @@ const ndv = new NDV();
 
 describe('ADO-2270 Save button resets on webhook node open', () => {
 	it('should not reset the save button if webhook node is opened and closed', () => {
-		// setup, load workflow with debughelper node with random seed
 		workflowPage.actions.visit();
 		workflowPage.actions.addInitialNodeToCanvas('Webhook');
 		workflowPage.getters.saveButton().click();
