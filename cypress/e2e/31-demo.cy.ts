9f56222ba8f3f89f86902e787c6856b5b51730f4
Mutasem Aldmour <mutasem@n8n.io>
2024-10-31 16:15:19 +0100

    test: add e2e

--- cypress/e2e/31-demo.cy.ts
+++ cypress/e2e/31-demo.cy.ts
@@ -1,9 +1,9 @@
-import workflow from '../fixtures/Manual_wait_set.json';
+import { getOutputTableRow } from '../composables/ndv';
+import { getCanvasNodes, openNode } from '../composables/workflow';
+import SIMPLE_WORKFLOW from '../fixtures/Manual_wait_set.json';
+import WORKFLOW_WITH_PINNED from '../fixtures/Webhook_set_pinned.json';
 import { importWorkflow, visitDemoPage } from '../pages/demo';
 import { errorToast } from '../pages/notifications';
-import { WorkflowPage } from '../pages/workflow';
-
-const workflowPage = new WorkflowPage();
 
 describe('Demo', () => {
 	beforeEach(() => {
@@ -14,8 +14,17 @@ describe('Demo', () => {
 	it('can import template', () => {
 		visitDemoPage();
 		errorToast().should('not.exist');
-		importWorkflow(workflow);
-		workflowPage.getters.canvasNodes().should('have.length', 3);
+		importWorkflow(SIMPLE_WORKFLOW);
+		getCanvasNodes().should('have.length', 3);
+	});
+
+	it('can import workflow with pin data', () => {
+		visitDemoPage();
+		importWorkflow(WORKFLOW_WITH_PINNED);
+		getCanvasNodes().should('have.length', 2);
+		openNode('Webhook');
+		getOutputTableRow(0).should('include.text', 'headers');
+		getOutputTableRow(1).should('include.text', 'dragons');
 	});
 
 	it('can override theme to dark', () => {
