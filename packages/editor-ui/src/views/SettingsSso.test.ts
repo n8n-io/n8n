7e58fc4fec468aca0b45d5bfe6150e1af632acbc
Alex Grozav <alex@grozav.com>
2024-11-01 16:12:26 +0200

    refactor: Move test files alongside tested files

--- packages/editor-ui/src/views/SettingsSso.test.ts
+++ packages/editor-ui/src/views/SettingsSso.test.ts
@@ -1,11 +1,16 @@
 import { createTestingPinia } from '@pinia/testing';
-import { createComponentRenderer } from '@/__tests__/render';
-import SettingsSso from './SettingsSso.vue';
-import { useSSOStore } from '@/stores/sso.store';
 import { useUIStore } from '@/stores/ui.store';
 import { within, waitFor } from '@testing-library/vue';
+import { mockedStore, retry } from '@/__tests__/utils';
+import { createPinia, setActivePinia } from 'pinia';
+import SettingsSso from '@/views/SettingsSso.vue';
+import { setupServer } from '@/__tests__/server';
+import { useSettingsStore } from '@/stores/settings.store';
 import userEvent from '@testing-library/user-event';
-import { mockedStore } from '@/__tests__/utils';
+import { useSSOStore } from '@/stores/sso.store';
+import { createComponentRenderer } from '@/__tests__/render';
+import { EnterpriseEditionFeature } from '@/constants';
+import { nextTick } from 'vue';
 
 const renderView = createComponentRenderer(SettingsSso);
 
@@ -199,5 +204,83 @@ describe('SettingsSso View', () => {
 			await userEvent.click(toggle);
 			expect(toggle.textContent).toContain('Deactivated');
 		});
+	});
+});
+
+let pinia: ReturnType<typeof createPinia>;
+let ssoStore: ReturnType<typeof useSSOStore>;
+let settingsStore: ReturnType<typeof useSettingsStore>;
+let server: ReturnType<typeof setupServer>;
+
+const renderComponent = createComponentRenderer(SettingsSso);
+
+describe('SettingsSso', () => {
+	beforeAll(() => {
+		server = setupServer();
+	});
+
+	beforeEach(async () => {
+		window.open = vi.fn();
+
+		pinia = createPinia();
+		setActivePinia(pinia);
+
+		ssoStore = useSSOStore();
+		settingsStore = useSettingsStore();
+
+		await settingsStore.getSettings();
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
+	afterAll(() => {
+		server.shutdown();
+	});
+
+	it('should render paywall state when there is no license', () => {
+		const { getByTestId, queryByTestId, queryByRole } = renderComponent({
+			pinia,
+		});
+
+		expect(queryByRole('checkbox')).not.toBeInTheDocument();
+		expect(queryByTestId('sso-content-licensed')).not.toBeInTheDocument();
+		expect(getByTestId('sso-content-unlicensed')).toBeInTheDocument();
+	});
+
+	it('should render licensed content', async () => {
+		settingsStore.settings.enterprise[EnterpriseEditionFeature.Saml] = true;
+		await nextTick();
+
+		const { getByTestId, queryByTestId, getByRole } = renderComponent({
+			pinia,
+		});
+
+		expect(getByRole('switch')).toBeInTheDocument();
+		expect(getByTestId('sso-content-licensed')).toBeInTheDocument();
+		expect(queryByTestId('sso-content-unlicensed')).not.toBeInTheDocument();
+	});
+
+	it('should enable activation checkbox and test button if data is already saved', async () => {
+		await ssoStore.getSamlConfig();
+		settingsStore.settings.enterprise[EnterpriseEditionFeature.Saml] = true;
+		await nextTick();
+
+		const { container, getByTestId, getByRole } = renderComponent({
+			pinia,
+		});
+
+		const xmlRadioButton = getByTestId('radio-button-xml');
+		await userEvent.click(xmlRadioButton);
+
+		await retry(() =>
+			expect(container.querySelector('textarea[name="metadata"]')).toHaveValue(
+				'<?xml version="1.0"?>',
+			),
+		);
+
+		expect(getByRole('switch')).toBeEnabled();
+		expect(getByTestId('sso-test')).toBeEnabled();
 	});
 });
