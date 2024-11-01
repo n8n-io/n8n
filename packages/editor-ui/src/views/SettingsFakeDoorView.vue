c6d9e020dd87ac2ea849086d929bd819c31fb851
Mutasem Aldmour <mutasem@n8n.io>
2024-11-01 13:31:44 +0100

    remove scss

--- packages/editor-ui/src/views/SettingsFakeDoorView.vue
+++ packages/editor-ui/src/views/SettingsFakeDoorView.vue
@@ -9,15 +9,3 @@ defineProps<Props>();
 <template>
 	<FeatureComingSoon :feature-id="featureId" show-title />
 </template>
-
-<style lang="scss" module>
-.header {
-	display: flex;
-	align-items: center;
-	white-space: nowrap;
-
-	*:first-child {
-		flex-grow: 1;
-	}
-}
-</style>
