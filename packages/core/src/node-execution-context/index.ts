09704d2085fd4a8a8b62ce74d57816e044d733a1
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-11-04 11:43:19 +0100

    refactor(core): Extract webhook context out of NodeExecutionFunctions (no-changelog) (#11455)

--- packages/core/src/node-execution-context/index.ts
+++ packages/core/src/node-execution-context/index.ts
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/no-cycle
 export { PollContext } from './poll-context';
 export { TriggerContext } from './trigger-context';
+export { WebhookContext } from './webhook-context';
