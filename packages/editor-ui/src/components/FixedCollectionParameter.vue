69d900774f7eb83c7fdb27c49ea27f47009999de
Shireen Missi <shireen@n8n.io>
2024-11-05 10:15:07 +0000

    migrate FixedCollectionParameter to composition API

--- packages/editor-ui/src/components/FixedCollectionParameter.vue
+++ packages/editor-ui/src/components/FixedCollectionParameter.vue
@@ -1,6 +1,4 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-import type { PropType } from 'vue';
+<script lang="ts" setup>
 import type { IUpdateInformation } from '@/Interface';
 
 import type { INodeParameters, INodeProperties, INodePropertyCollection } from 'n8n-workflow';
@@ -8,213 +6,196 @@ import { deepCopy, isINodePropertyCollectionList } from 'n8n-workflow';
 
 import { get } from 'lodash-es';
 
-export default defineComponent({
-	name: 'FixedCollectionParameter',
-	props: {
-		nodeValues: {
-			type: Object as PropType<INodeParameters>,
-			required: true,
-		},
-		parameter: {
-			type: Object as PropType<INodeProperties>,
-			required: true,
-		},
-		path: {
-			type: String,
-			required: true,
-		},
-		values: {
-			type: Object as PropType<Record<string, INodeParameters[]>>,
-			default: () => ({}),
-		},
-		isReadOnly: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	data() {
-		return {
-			selectedOption: undefined,
-			mutableValues: {} as Record<string, INodeParameters[]>,
-		};
-	},
-	computed: {
-		getPlaceholderText(): string {
-			const placeholder = this.$locale.nodeText().placeholder(this.parameter, this.path);
-			return placeholder ? placeholder : this.$locale.baseText('fixedCollectionParameter.choose');
-		},
-		getProperties(): INodePropertyCollection[] {
-			const returnProperties = [];
-			let tempProperties;
-			for (const name of this.propertyNames) {
-				tempProperties = this.getOptionProperties(name);
-				if (tempProperties !== undefined) {
-					returnProperties.push(tempProperties);
-				}
-			}
-			return returnProperties;
-		},
-		multipleValues(): boolean {
-			return !!this.parameter.typeOptions?.multipleValues;
-		},
-		parameterOptions(): INodePropertyCollection[] {
-			if (this.multipleValues && isINodePropertyCollectionList(this.parameter.options)) {
-				return this.parameter.options;
-			}
+import { computed, ref, watch, onBeforeMount } from 'vue';
+import { useI18n } from '@/composables/useI18n';
 
-			return (this.parameter.options as INodePropertyCollection[]).filter((option) => {
-				return !this.propertyNames.includes(option.name);
-			});
-		},
-		propertyNames(): string[] {
-			return Object.keys(this.mutableValues || {});
-		},
-		sortable(): boolean {
-			return !!this.parameter.typeOptions?.sortable;
-		},
-	},
-	watch: {
-		values: {
-			handler(newValues: Record<string, INodeParameters[]>) {
-				this.mutableValues = deepCopy(newValues);
-			},
-			deep: true,
-		},
-	},
-	created() {
-		this.mutableValues = deepCopy(this.values);
-	},
-	methods: {
-		deleteOption(optionName: string, index?: number) {
-			const currentOptionsOfSameType = this.mutableValues[optionName];
-			if (!currentOptionsOfSameType || currentOptionsOfSameType.length > 1) {
-				// it's not the only option of this type, so just remove it.
-				this.$emit('valueChanged', {
-					name: this.getPropertyPath(optionName, index),
-					value: undefined,
-				});
-			} else {
-				// it's the only option, so remove the whole type
-				this.$emit('valueChanged', {
-					name: this.getPropertyPath(optionName),
-					value: undefined,
-				});
-			}
-		},
-		getPropertyPath(name: string, index?: number) {
-			return `${this.path}.${name}` + (index !== undefined ? `[${index}]` : '');
-		},
-		getOptionProperties(optionName: string): INodePropertyCollection | undefined {
-			if (isINodePropertyCollectionList(this.parameter.options)) {
-				for (const option of this.parameter.options) {
-					if (option.name === optionName) {
-						return option;
-					}
-				}
-			}
-			return undefined;
-		},
-		moveOptionDown(optionName: string, index: number) {
-			if (Array.isArray(this.mutableValues[optionName])) {
-				this.mutableValues[optionName].splice(
-					index + 1,
-					0,
-					this.mutableValues[optionName].splice(index, 1)[0],
-				);
-			}
+const locale = useI18n();
 
-			const parameterData = {
-				name: this.getPropertyPath(optionName),
-				value: this.mutableValues[optionName],
-				type: 'optionsOrderChanged',
-			};
+const props = defineProps<{
+	nodeValues: INodeParameters;
+	parameter: INodeProperties;
+	path: string;
+	values: Record<string, INodeParameters[]>;
+	isReadOnly: boolean | undefined;
+}>();
 
-			this.$emit('valueChanged', parameterData);
-		},
-		moveOptionUp(optionName: string, index: number) {
-			if (Array.isArray(this.mutableValues[optionName])) {
-				this.mutableValues?.[optionName].splice(
-					index - 1,
-					0,
-					this.mutableValues[optionName].splice(index, 1)[0],
-				);
-			}
+const emit = defineEmits<{
+	valueChanged: [value?: any];
+}>();
 
-			const parameterData = {
-				name: this.getPropertyPath(optionName),
-				value: this.mutableValues[optionName],
-				type: 'optionsOrderChanged',
-			};
-
-			this.$emit('valueChanged', parameterData);
-		},
-		optionSelected(optionName: string) {
-			const option = this.getOptionProperties(optionName);
-			if (option === undefined) {
-				return;
-			}
-			const name = `${this.path}.${option.name}`;
-
-			const newParameterValue: INodeParameters = {};
-
-			for (const optionParameter of option.values) {
-				if (
-					optionParameter.type === 'fixedCollection' &&
-					optionParameter.typeOptions !== undefined &&
-					optionParameter.typeOptions.multipleValues === true
-				) {
-					newParameterValue[optionParameter.name] = {};
-				} else if (
-					optionParameter.typeOptions !== undefined &&
-					optionParameter.typeOptions.multipleValues === true
-				) {
-					// Multiple values are allowed so append option to array
-					const multiValue = get(this.nodeValues, [this.path, optionParameter.name], []);
-
-					if (Array.isArray(optionParameter.default)) {
-						multiValue.push(...deepCopy(optionParameter.default));
-					} else if (
-						optionParameter.default !== '' &&
-						typeof optionParameter.default !== 'object'
-					) {
-						multiValue.push(deepCopy(optionParameter.default));
-					}
-
-					newParameterValue[optionParameter.name] = multiValue;
-				} else {
-					// Add a new option
-					newParameterValue[optionParameter.name] = deepCopy(optionParameter.default);
-				}
-			}
-
-			let newValue;
-			if (this.multipleValues) {
-				newValue = get(this.nodeValues, name, []) as INodeParameters[];
-
-				newValue.push(newParameterValue);
-			} else {
-				newValue = newParameterValue;
-			}
-
-			const parameterData = {
-				name,
-				value: newValue,
-			};
-
-			this.$emit('valueChanged', parameterData);
-			this.selectedOption = undefined;
-		},
-		valueChanged(parameterData: IUpdateInformation) {
-			this.$emit('valueChanged', parameterData);
-		},
-	},
+const getPlaceholderText = computed(() => {
+	const placeholder = locale.nodeText().placeholder(props.parameter, props.path);
+	return placeholder ? placeholder : locale.baseText('fixedCollectionParameter.choose');
 });
+const mutableValues = ref({} as Record<string, INodeParameters[]>);
+const selectedOption = ref<string | null | undefined>(null);
+const propertyNames = computed(() => {
+	return Object.keys(mutableValues.value || {});
+});
+const getProperties = computed(() => {
+	const returnProperties = [];
+	let tempProperties;
+	for (const name of propertyNames.value) {
+		tempProperties = getOptionProperties(name);
+		if (tempProperties !== undefined) {
+			returnProperties.push(tempProperties);
+		}
+	}
+	return returnProperties;
+});
+const multipleValues = computed(() => {
+	return !!props.parameter.typeOptions?.multipleValues;
+});
+const parameterOptions = computed(() => {
+	if (multipleValues && isINodePropertyCollectionList(props.parameter.options)) {
+		return props.parameter.options;
+	}
+
+	return (props.parameter.options as INodePropertyCollection[]).filter((option) => {
+		return !propertyNames.value.includes(option.name);
+	});
+});
+
+const sortable = computed(() => {
+	return !!props.parameter.typeOptions?.sortable;
+});
+
+watch(
+	() => props.values,
+	(newValues: Record<string, INodeParameters[]>) => {
+		mutableValues.value = deepCopy(newValues);
+	},
+	{ deep: true },
+);
+onBeforeMount(() => {
+	mutableValues.value = deepCopy(props.values);
+});
+const deleteOption = (optionName: string, index?: number) => {
+	const currentOptionsOfSameType = mutableValues.value[optionName];
+	if (!currentOptionsOfSameType || currentOptionsOfSameType.length > 1) {
+		// it's not the only option of this type, so just remove it.
+		emit('valueChanged', {
+			name: getPropertyPath(optionName, index),
+			value: undefined,
+		});
+	} else {
+		// it's the only option, so remove the whole type
+		emit('valueChanged', {
+			name: getPropertyPath(optionName),
+			value: undefined,
+		});
+	}
+};
+const getPropertyPath = (name: string, index?: number) => {
+	return `${props.path}.${name}` + (index !== undefined ? `[${index}]` : '');
+};
+const getOptionProperties = (optionName: string) => {
+	if (isINodePropertyCollectionList(props.parameter.options)) {
+		for (const option of props.parameter.options) {
+			if (option.name === optionName) {
+				return option;
+			}
+		}
+	}
+	return undefined;
+};
+const moveOptionDown = (optionName: string, index: number) => {
+	if (Array.isArray(mutableValues.value[optionName])) {
+		mutableValues.value[optionName].splice(
+			index + 1,
+			0,
+			mutableValues.value[optionName].splice(index, 1)[0],
+		);
+	}
+
+	const parameterData = {
+		name: getPropertyPath(optionName),
+		value: mutableValues.value[optionName],
+		type: 'optionsOrderChanged',
+	};
+
+	emit('valueChanged', parameterData);
+};
+const moveOptionUp = (optionName: string, index: number) => {
+	if (Array.isArray(mutableValues.value[optionName])) {
+		mutableValues.value?.[optionName].splice(
+			index - 1,
+			0,
+			mutableValues.value[optionName].splice(index, 1)[0],
+		);
+	}
+
+	const parameterData = {
+		name: getPropertyPath(optionName),
+		value: mutableValues.value[optionName],
+		type: 'optionsOrderChanged',
+	};
+
+	emit('valueChanged', parameterData);
+};
+const optionSelected = (optionName: string) => {
+	const option = getOptionProperties(optionName);
+	if (option === undefined) {
+		return;
+	}
+	const name = `${props.path}.${option.name}`;
+
+	const newParameterValue: INodeParameters = {};
+
+	for (const optionParameter of option.values) {
+		if (
+			optionParameter.type === 'fixedCollection' &&
+			optionParameter.typeOptions !== undefined &&
+			optionParameter.typeOptions.multipleValues === true
+		) {
+			newParameterValue[optionParameter.name] = {};
+		} else if (
+			optionParameter.typeOptions !== undefined &&
+			optionParameter.typeOptions.multipleValues === true
+		) {
+			// Multiple values are allowed so append option to array
+			const multiValue = get(props.nodeValues, [props.path, optionParameter.name], []);
+
+			if (Array.isArray(optionParameter.default)) {
+				multiValue.push(...deepCopy(optionParameter.default));
+			} else if (optionParameter.default !== '' && typeof optionParameter.default !== 'object') {
+				multiValue.push(deepCopy(optionParameter.default));
+			}
+
+			newParameterValue[optionParameter.name] = multiValue;
+		} else {
+			// Add a new option
+			newParameterValue[optionParameter.name] = deepCopy(optionParameter.default);
+		}
+	}
+
+	let newValue;
+	if (multipleValues) {
+		newValue = get(props.nodeValues, name, []) as INodeParameters[];
+
+		newValue.push(newParameterValue);
+	} else {
+		newValue = newParameterValue;
+	}
+
+	const parameterData = {
+		name,
+		value: newValue,
+	};
+
+	emit('valueChanged', parameterData);
+	selectedOption.value = undefined;
+};
+
+const valueChanged = (parameterData: IUpdateInformation) => {
+	emit('valueChanged', parameterData);
+};
 </script>
 
 <template>
-	<div
+	<LazyFixedCollectionParameter
 		class="fixed-collection-parameter"
-		:data-test-id="`fixed-collection-${parameter.name}`"
+		:data-test-id="`fixed-collection-${props.parameter?.name}`"
 		@keydown.stop
 	>
 		<div v-if="getProperties.length === 0" class="no-items-exist">
@@ -293,7 +274,7 @@ export default defineComponent({
 							text
 							size="mini"
 							icon="trash"
-							:title="$locale.baseText('fixedCollectionParameter.deleteItem')"
+							:title="locale.baseText('fixedCollectionParameter.deleteItem')"
 							@click="deleteOption(property.name)"
 						></n8n-icon-button>
 					</div>
@@ -336,7 +317,7 @@ export default defineComponent({
 				</n8n-select>
 			</div>
 		</div>
-	</div>
+	</LazyFixedCollectionParameter>
 </template>
 
 <style scoped lang="scss">
