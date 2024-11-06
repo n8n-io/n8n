0374b82c8d27efd36853017c45fa4255dea0ecac
Ivan Atanasov <ivan.atanasov@n8n.io>
2024-11-06 15:28:32 +0100

    add tests to node error view

--- packages/editor-ui/src/components/Error/NodeErrorView.test.ts
+++ packages/editor-ui/src/components/Error/NodeErrorView.test.ts
@@ -3,7 +3,7 @@ import { createComponentRenderer } from '@/__tests__/render';
 import NodeErrorView from '@/components/Error/NodeErrorView.vue';
 
 import { createTestingPinia } from '@pinia/testing';
-import { type INode } from 'n8n-workflow';
+import type { NodeError } from 'n8n-workflow';
 import { useAssistantStore } from '@/stores/assistant.store';
 import { useNodeTypesStore } from '@/stores/nodeTypes.store';
 import { mockedStore } from '@/__tests__/utils';
@@ -14,28 +14,53 @@ let aiAssistantStore: ReturnType<typeof mockedStore<typeof useAssistantStore>>;
 let nodeTypeStore: ReturnType<typeof mockedStore<typeof useNodeTypesStore>>;
 
 describe('NodeErrorView.vue', () => {
-	let mockNode: INode;
+	let error: NodeError;
 
 	beforeEach(() => {
 		createTestingPinia();
 
 		aiAssistantStore = mockedStore(useAssistantStore);
 		nodeTypeStore = mockedStore(useNodeTypesStore);
+		//@ts-expect-error
+		error = {
+			name: 'NodeOperationError',
+			message: 'Test error message',
+			description: 'Test error description',
+			context: {
+				descriptionKey: 'noInputConnection',
+				nodeCause: 'Test node cause',
+				runIndex: '1',
+				itemIndex: '2',
+				parameter: 'testParameter',
+				data: { key: 'value' },
+				causeDetailed: 'Detailed cause',
+			},
+			node: {
+				parameters: {
+					mode: 'runOnceForAllItems',
+					language: 'javaScript',
+					jsCode: 'cons error = 9;',
+					notice: '',
+				},
+				id: 'd1ce5dc9-f9ae-4ac6-84e5-0696ba175dd9',
+				name: 'Code',
+				type: 'n8n-nodes-base.code',
+				typeVersion: 2,
+				position: [940, 240],
+			},
+			messages: ['Test message 1', 'Test message 2'],
+			timestamp: Date.now(),
+			stack: 'Test stack trace',
+			findProperty: () => 'Test error data',
+			addToMessages: () => {},
+			setDescriptiveErrorMessage: () => [
+				'Test error data',
+				['Test raw message 1', 'Test raw message 2'],
+			],
+			lineNumber: 1,
+		};
 	});
 	afterEach(() => {
-		mockNode = {
-			parameters: {
-				mode: 'runOnceForAllItems',
-				language: 'javaScript',
-				jsCode: 'cons error = 9;',
-				notice: '',
-			},
-			id: 'd1ce5dc9-f9ae-4ac6-84e5-0696ba175dd9',
-			name: 'Code',
-			type: 'n8n-nodes-base.code',
-			typeVersion: 2,
-			position: [940, 240],
-		};
 		vi.clearAllMocks();
 	});
 
@@ -43,7 +68,7 @@ describe('NodeErrorView.vue', () => {
 		const { getByTestId } = renderComponent({
 			props: {
 				error: {
-					node: mockNode,
+					node: error.node,
 					messages: ['Unexpected identifier [line 1]'],
 				},
 			},
@@ -58,7 +83,7 @@ describe('NodeErrorView.vue', () => {
 		const { getByTestId } = renderComponent({
 			props: {
 				error: {
-					node: mockNode,
+					node: error.node,
 					message: 'Unexpected identifier [line 1]',
 				},
 			},
@@ -83,7 +108,7 @@ describe('NodeErrorView.vue', () => {
 			props: {
 				error: {
 					node: {
-						...mockNode,
+						...error.node,
 						type: 'n8n-nodes-base.function',
 						typeVersion: 1,
 					},
@@ -94,5 +119,28 @@ describe('NodeErrorView.vue', () => {
 		const aiAssistantButton = queryByTestId('ask-assistant-button');
 
 		expect(aiAssistantButton).toBeNull();
+	});
+
+	it('renders error message', () => {
+		const { getByTestId } = renderComponent({
+			props: { error },
+		});
+		expect(getByTestId('node-error-message').textContent).toContain('Test error message');
+	});
+
+	it('renders error description', () => {
+		const { getByTestId } = renderComponent({
+			props: { error },
+		});
+		expect(getByTestId('node-error-description').innerHTML).toContain(
+			'This node has no input data. Please make sure this node is connected to another node.',
+		);
+	});
+
+	it('renders stack trace', () => {
+		const { getByText } = renderComponent({
+			props: { error },
+		});
+		expect(getByText('Test stack trace')).toBeTruthy();
 	});
 });
