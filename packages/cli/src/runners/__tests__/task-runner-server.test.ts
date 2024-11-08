726639ac33fecafb3c2a5278c8999d0ab06507a7
Iván Ovejero <ivov.src@gmail.com>
2024-11-08 09:19:53 +0100

    Split test cases

--- packages/cli/src/runners/__tests__/task-runner-server.test.ts
+++ packages/cli/src/runners/__tests__/task-runner-server.test.ts
@@ -35,8 +35,30 @@ describe('TaskRunnerServer', () => {
 
 			response.writeHead(401);
 			expect(ws.close).toHaveBeenCalledWith(); // no args
+		});
 
-			jest.clearAllMocks();
+		it('should not close WebSocket when response status code is 200', () => {
+			const ws = mock<WebSocket>();
+			const request = mock<TaskRunnerServerInitRequest>({
+				url: '/runners/_ws',
+				ws,
+			});
+
+			const server = new TaskRunnerServer(
+				mock(),
+				mock<GlobalConfig>({ taskRunners: { path: '/runners' } }),
+				mock<TaskRunnerAuthController>(),
+				mock(),
+			);
+
+			// @ts-expect-error Private property
+			server.handleUpgradeRequest(request, mock(), Buffer.from(''));
+
+			const response = new ServerResponse(request);
+			response.writeHead = (statusCode) => {
+				if (statusCode > 200) ws.close();
+				return response;
+			};
 
 			response.writeHead(200);
 			expect(ws.close).not.toHaveBeenCalled();
