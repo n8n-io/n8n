ce44c19157cf76344b730346e8b53c4a7f3ecd14
Ricardo Espinoza <ricardo@n8n.io>
2024-11-10 15:54:13 -0500

    add tests

--- packages/editor-ui/src/components/canvas/elements/nodes/render-types/CanvasNodeStickyNote.test.ts
+++ packages/editor-ui/src/components/canvas/elements/nodes/render-types/CanvasNodeStickyNote.test.ts
@@ -3,6 +3,7 @@ import { createComponentRenderer } from '@/__tests__/render';
 import { createCanvasNodeProvide } from '@/__tests__/data';
 import { createTestingPinia } from '@pinia/testing';
 import { setActivePinia } from 'pinia';
+import { fireEvent } from '@testing-library/vue';
 
 const renderComponent = createComponentRenderer(CanvasNodeStickyNote);
 
@@ -41,5 +42,30 @@ describe('CanvasNodeStickyNote', () => {
 		const resizeControls = container.querySelectorAll('.vue-flow__resize-control');
 
 		expect(resizeControls).toHaveLength(0);
+	});
+
+	it('should disable sticky options when in edit mode', async () => {
+		const { container } = renderComponent({
+			global: {
+				provide: {
+					...createCanvasNodeProvide({
+						id: 'sticky',
+						readOnly: false,
+					}),
+				},
+			},
+		});
+
+		const stickyTextarea = container.querySelector('.sticky-textarea');
+
+		if (!stickyTextarea) return;
+
+		await fireEvent.dblClick(stickyTextarea);
+
+		const stickyOptions = container.querySelector('.sticky-options');
+
+		if (!stickyOptions) return;
+
+		expect(getComputedStyle(stickyOptions).display).toBe('none');
 	});
 });
