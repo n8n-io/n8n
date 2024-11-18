d4c87724ae8ac87d3a98aad1bec0baf07e71b7db
Ivan Atanasov <ivan.atanasov@n8n.io>
2024-11-18 16:33:25 +0100

    Add documentation link to insufficient quota message

--- packages/editor-ui/src/components/NodeExecutionErrorMessage.vue
+++ packages/editor-ui/src/components/NodeExecutionErrorMessage.vue
@@ -1,16 +1,25 @@
 <script lang="ts" setup>
 import { useI18n } from '@/composables/useI18n';
+import { sanitizeHtml } from '@/utils/htmlUtils';
+
+import { computed } from 'vue';
+
 const i18n = useI18n();
 
-defineProps<{
+const props = defineProps<{
 	nodeName: string;
 	errorMessage?: string;
 }>();
+
+const sanitizedErrorMessage = computed(() => {
+	return props.errorMessage ? sanitizeHtml(props.errorMessage) : '';
+});
 </script>
 
 <template>
 	<div>
-		{{ errorMessage }}<br />
+		<span v-html="sanitizedErrorMessage"></span>
+		<br />
 		<a data-action="openNodeDetail" :data-action-parameter-node="nodeName">{{
 			i18n.baseText('node.executionError.openNode')
 		}}</a>
