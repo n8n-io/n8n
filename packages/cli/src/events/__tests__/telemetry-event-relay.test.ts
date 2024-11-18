3024144def96444a01d8f2507b0cb8a28304f7d0
Iván Ovejero <ivov.src@gmail.com>
2024-11-18 14:08:45 +0100

    refactor(core): Port diagnostics config (#11761)

--- packages/cli/src/events/__tests__/telemetry-event-relay.test.ts
+++ packages/cli/src/events/__tests__/telemetry-event-relay.test.ts
@@ -2,7 +2,6 @@ import type { GlobalConfig } from '@n8n/config';
 import { mock } from 'jest-mock-extended';
 import type { IWorkflowBase } from 'n8n-workflow';
 
-import config from '@/config';
 import { N8N_VERSION } from '@/constants';
 import type { WorkflowEntity } from '@/databases/entities/workflow-entity';
 import type { ProjectRelationRepository } from '@/databases/repositories/project-relation.repository';
@@ -66,7 +65,7 @@ describe('TelemetryEventRelay', () => {
 	});
 
 	beforeEach(() => {
-		config.set('diagnostics.enabled', true);
+		globalConfig.diagnostics.enabled = true;
 	});
 
 	afterEach(() => {
@@ -75,7 +74,7 @@ describe('TelemetryEventRelay', () => {
 
 	describe('init', () => {
 		it('with diagnostics enabled, should init telemetry and register listeners', async () => {
-			config.set('diagnostics.enabled', true);
+			globalConfig.diagnostics.enabled = true;
 			const telemetryEventRelay = new TelemetryEventRelay(
 				eventService,
 				telemetry,
@@ -96,7 +95,7 @@ describe('TelemetryEventRelay', () => {
 		});
 
 		it('with diagnostics disabled, should neither init telemetry nor register listeners', async () => {
-			config.set('diagnostics.enabled', false);
+			globalConfig.diagnostics.enabled = false;
 			const telemetryEventRelay = new TelemetryEventRelay(
 				eventService,
 				telemetry,
