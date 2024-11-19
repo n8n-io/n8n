1987363f7941285c51fda849a4ac92832368b25a
Ivan Atanasov <ivan.atanasov@n8n.io>
2024-11-19 16:28:02 +0100

    fix(editor): Add documentation link to insufficient quota message (#11777)

--- packages/@n8n/nodes-langchain/nodes/vendors/OpenAi/helpers/error-handling.ts
+++ packages/@n8n/nodes-langchain/nodes/vendors/OpenAi/helpers/error-handling.ts
@@ -1,8 +1,9 @@
-import { OpenAIError } from 'openai/error';
 import { RateLimitError } from 'openai';
+import { OpenAIError } from 'openai/error';
 
 const errorMap: Record<string, string> = {
-	insufficient_quota: 'OpenAI: Insufficient quota',
+	insufficient_quota:
+		'Insufficient quota detected. <a href="https://docs.n8n.io/integrations/builtin/app-nodes/n8n-nodes-langchain.openai/common-issues/#insufficient-quota" target="_blank">Learn more</a> about resolving this issue',
 	rate_limit_exceeded: 'OpenAI: Rate limit reached',
 };
 
@@ -23,7 +24,10 @@ export const openAiFailedAttemptHandler = (error: any) => {
 			const customErrorMessage = getCustomErrorMessage(errorCode);
 
 			if (customErrorMessage) {
-				error.message = customErrorMessage;
+				if (error.error) {
+					(error.error as { message: string }).message = customErrorMessage;
+					error.message = customErrorMessage;
+				}
 			}
 		}
 
