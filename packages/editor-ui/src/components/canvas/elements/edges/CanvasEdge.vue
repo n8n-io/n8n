1e70e71f80f7e100105a27c93b2c72a2bc0c00cf
Alex Grozav <alex@grozav.com>
2024-11-21 16:42:07 +0200

    fix: remove unused marker

--- packages/editor-ui/src/components/canvas/elements/edges/CanvasEdge.vue
+++ packages/editor-ui/src/components/canvas/elements/edges/CanvasEdge.vue
@@ -131,20 +131,6 @@ function onDelete() {
 		:interaction-width="40"
 	/>
 
-	<svg>
-		<marker
-			id="canvas-edge-arrow"
-			viewBox="0 0 10 10"
-			refX="9"
-			refY="5"
-			markerWidth="6"
-			markerHeight="6"
-			orient="auto-start-reverse"
-		>
-			<path d="M 0 0 L 10 5 L 0 10 z" fill="currentColor" />
-		</marker>
-	</svg>
-
 	<EdgeLabelRenderer>
 		<div
 			data-test-id="edge-label-wrapper"
