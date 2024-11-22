fbaa17951fd6a598578e17b47039bf6307b37e48
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2024-11-22 15:04:21 +0100

    refactor(core): Extract SupplyDataContext out of NodeExecutionFunctions (no-changelog) (#11834)

--- packages/core/src/node-execution-context/index.ts
+++ packages/core/src/node-execution-context/index.ts
@@ -3,5 +3,6 @@ export { ExecuteSingleContext } from './execute-single-context';
 export { HookContext } from './hook-context';
 export { LoadOptionsContext } from './load-options-context';
 export { PollContext } from './poll-context';
+export { SupplyDataContext } from './supply-data-context';
 export { TriggerContext } from './trigger-context';
 export { WebhookContext } from './webhook-context';
