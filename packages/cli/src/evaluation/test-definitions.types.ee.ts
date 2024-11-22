1c74958c5a0b09e3da76139f8ecc80cc80bc26f1
Eugene Molodkin <eugene@n8n.io>
2024-11-22 16:31:12 +0100

    wip: test metrics CRUD controller

--- packages/cli/src/evaluation/test-definitions.types.ee.ts
+++ packages/cli/src/evaluation/test-definitions.types.ee.ts
@@ -31,3 +31,33 @@ export declare namespace TestDefinitionsRequest {
 
 	type Delete = AuthenticatedRequest<RouteParams.TestId>;
 }
+
+// ----------------------------------
+//             /test-definitions/:testDefinitionId/metrics
+// ----------------------------------
+
+export declare namespace TestMetricsRequest {
+	namespace RouteParams {
+		type TestDefinitionId = {
+			testDefinitionId: string;
+		};
+
+		type TestMetricId = {
+			id: string;
+		};
+	}
+
+	type GetOne = AuthenticatedRequest<RouteParams.TestDefinitionId & RouteParams.TestMetricId>;
+
+	type GetMany = AuthenticatedRequest<RouteParams.TestDefinitionId>;
+
+	type Create = AuthenticatedRequest<RouteParams.TestDefinitionId, {}, { name: string }>;
+
+	type Patch = AuthenticatedRequest<
+		RouteParams.TestDefinitionId & RouteParams.TestMetricId,
+		{},
+		{ name: string }
+	>;
+
+	type Delete = AuthenticatedRequest<RouteParams.TestDefinitionId & RouteParams.TestMetricId>;
+}
