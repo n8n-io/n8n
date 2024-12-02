8c99aef3577cce1d98a29a75612ea3e4ed99c655
Thomas B <9094255+Ten0@users.noreply.github.com>
2024-12-02 14:56:14 +0100

    docs: Update Skip SSL to mention it is insecure (no-changelog) (#11935)

--- packages/workflow/src/NodeHelpers.ts
+++ packages/workflow/src/NodeHelpers.ts
@@ -320,7 +320,7 @@ const declarativeNodeOptionParameters: INodeProperties = {
 			],
 		},
 		{
-			displayName: 'Ignore SSL Issues',
+			displayName: 'Ignore SSL Issues (Insecure)',
 			name: 'allowUnauthorizedCerts',
 			type: 'boolean',
 			noDataExpression: true,
