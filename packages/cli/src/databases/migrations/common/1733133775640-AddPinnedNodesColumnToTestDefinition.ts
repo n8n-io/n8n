80d0ce1e1ae861a12ed9ad419adb04c6027b91f5
Eugene Molodkin <eugene@n8n.io>
2024-12-02 14:53:07 +0100

    wip: Fix migration for postgres

--- packages/cli/src/databases/migrations/common/1733133775640-AddPinnedNodesColumnToTestDefinition.ts
+++ packages/cli/src/databases/migrations/common/1733133775640-AddPinnedNodesColumnToTestDefinition.ts
@@ -1,12 +1,14 @@
 import type { MigrationContext, ReversibleMigration } from '@/databases/types';
 
 export class AddPinnedNodesColumnToTestDefinition1733133775640 implements ReversibleMigration {
-	async up({ queryRunner, tablePrefix }: MigrationContext) {
+	async up({ queryRunner, tablePrefix, escape }: MigrationContext) {
 		// We have to use raw query migration instead of schemaBuilder helpers,
 		// because the typeorm schema builder implements addColumns by a table recreate for sqlite
 		// which causes weird issues with the migration
+		const pinnedNodesColumnName = escape.columnName('pinnedNodes');
+
 		await queryRunner.query(
-			`ALTER TABLE ${tablePrefix}test_definition ADD COLUMN \`pinnedNodes\` JSON DEFAULT '[]' NOT NULL`,
+			`ALTER TABLE ${tablePrefix}test_definition ADD COLUMN ${pinnedNodesColumnName} JSON DEFAULT '[]' NOT NULL`,
 		);
 	}
 
