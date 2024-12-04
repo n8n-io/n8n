439eb72e0334dad4eb45ea208c21e11e6d12cef8
Mutasem Aldmour <4711238+mutdmour@users.noreply.github.com>
2024-12-04 13:44:25 +0100

    fix: Load workflows with unconnected Switch outputs (#12020)

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -364,7 +364,8 @@ export interface ICredentialDataDecryptedObject {
 
 // First array index: The output/input-index (if node has multiple inputs/outputs of the same type)
 // Second array index: The different connections (if one node is connected to multiple nodes)
-export type NodeInputConnections = IConnection[][];
+// Any index can be null, for example in a switch node with multiple indexes some of which are not connected
+export type NodeInputConnections = Array<IConnection[] | null>;
 
 export interface INodeConnection {
 	sourceIndex: number;
