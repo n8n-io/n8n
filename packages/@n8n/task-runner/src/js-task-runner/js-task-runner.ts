2f3c9a33a373d6024fa7287f90bd2a3a43559cac
Iván Ovejero <ivov.src@gmail.com>
2024-12-11 19:48:36 +0100

    fix(core): Fix `$getWorkflowStaticData` on task runners (#12153)
    Co-authored-by: Tomi Turtiainen <10324676+tomi@users.noreply.github.com>

--- packages/@n8n/task-runner/src/js-task-runner/js-task-runner.ts
+++ packages/@n8n/task-runner/src/js-task-runner/js-task-runner.ts
@@ -1,5 +1,5 @@
 import { getAdditionalKeys } from 'n8n-core';
-import { WorkflowDataProxy, Workflow } from 'n8n-workflow';
+import { WorkflowDataProxy, Workflow, ObservableObject } from 'n8n-workflow';
 import type {
 	CodeExecutionMode,
 	IWorkflowExecuteAdditionalData,
@@ -138,6 +138,8 @@ export class JsTaskRunner extends TaskRunner {
 			},
 		};
 
+		workflow.staticData = ObservableObject.create(workflow.staticData);
+
 		const result =
 			settings.nodeMode === 'runOnceForAllItems'
 				? await this.runForAllItems(task.taskId, settings, data, workflow, customConsole, signal)
@@ -146,6 +148,7 @@ export class JsTaskRunner extends TaskRunner {
 		return {
 			result,
 			customData: data.runExecutionData.resultData.metadata,
+			staticData: workflow.staticData.__dataChanged ? workflow.staticData : undefined,
 		};
 	}
 
@@ -200,7 +203,7 @@ export class JsTaskRunner extends TaskRunner {
 			module: {},
 			console: customConsole,
 			items: inputItems,
-
+			$getWorkflowStaticData: (type: 'global' | 'node') => workflow.getStaticData(type, data.node),
 			...this.getNativeVariables(),
 			...dataProxy,
 			...this.buildRpcCallObject(taskId),
@@ -273,7 +276,8 @@ export class JsTaskRunner extends TaskRunner {
 				module: {},
 				console: customConsole,
 				item,
-
+				$getWorkflowStaticData: (type: 'global' | 'node') =>
+					workflow.getStaticData(type, data.node),
 				...this.getNativeVariables(),
 				...dataProxy,
 				...this.buildRpcCallObject(taskId),
