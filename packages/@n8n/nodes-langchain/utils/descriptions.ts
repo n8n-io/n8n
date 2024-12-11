f8a3754c22595d38db48e953637ab4e6c4117e06
Charlie Kolb <charlie@n8n.io>
2024-12-11 13:56:53 +0100

    Remove lint exceptions

--- packages/@n8n/nodes-langchain/utils/descriptions.ts
+++ packages/@n8n/nodes-langchain/utils/descriptions.ts
@@ -71,14 +71,12 @@ export const promptTypeOptions: INodeProperties = {
 	type: 'options',
 	options: [
 		{
-			// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
 			name: 'Connected Chat Trigger Node',
 			value: 'auto',
 			description:
 				'Looks for an input field called chatInput that is coming from a directly connected Chat Trigger',
 		},
 		{
-			// eslint-disable-next-line n8n-nodes-base/node-param-display-name-miscased
 			name: 'Define below',
 			value: 'define',
 			description: 'Use an expression to reference data in previous nodes or enter static text',
