26e7f55388db19e3c4cfdd074e50e77bbe886be9
Eugene <eugene@n8n.io>
2024-12-13 12:40:04 +0100

    chore: Add node mocking logic to the Test Runner (no-changelog) (#12009)

--- packages/cli/src/evaluation/test-runner/__tests__/create-pin-data.ee.test.ts
+++ packages/cli/src/evaluation/test-runner/__tests__/create-pin-data.ee.test.ts
@@ -13,12 +13,44 @@ const executionDataJson = JSON.parse(
 
 describe('createPinData', () => {
 	test('should create pin data from past execution data', () => {
-		const pinData = createPinData(wfUnderTestJson, executionDataJson);
+		const mockedNodes = ['When clicking ‘Test workflow’'].map((name) => ({ name }));
+
+		const pinData = createPinData(wfUnderTestJson, mockedNodes, executionDataJson);
 
 		expect(pinData).toEqual(
 			expect.objectContaining({
 				'When clicking ‘Test workflow’': expect.anything(),
 			}),
 		);
+	});
+
+	test('should not create pin data for non-existing mocked nodes', () => {
+		const mockedNodes = ['Non-existing node'].map((name) => ({ name }));
+
+		const pinData = createPinData(wfUnderTestJson, mockedNodes, executionDataJson);
+
+		expect(pinData).toEqual({});
+	});
+
+	test('should create pin data for all mocked nodes', () => {
+		const mockedNodes = ['When clicking ‘Test workflow’', 'Edit Fields', 'Code'].map((name) => ({
+			name,
+		}));
+
+		const pinData = createPinData(wfUnderTestJson, mockedNodes, executionDataJson);
+
+		expect(pinData).toEqual(
+			expect.objectContaining({
+				'When clicking ‘Test workflow’': expect.anything(),
+				'Edit Fields': expect.anything(),
+				Code: expect.anything(),
+			}),
+		);
+	});
+
+	test('should return empty object if no mocked nodes are provided', () => {
+		const pinData = createPinData(wfUnderTestJson, [], executionDataJson);
+
+		expect(pinData).toEqual({});
 	});
 });
