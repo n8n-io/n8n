dde46db54fa344540a738f64ecd01ee759d3ea20
Milorad FIlipović <milorad@n8n.io>
2024-12-16 16:00:30 +0100

    ✅ Using save workflow request to detect template id

--- cypress/e2e/29-templates.cy.ts
+++ cypress/e2e/29-templates.cy.ts
@@ -141,6 +141,7 @@ describe('Workflow templates', () => {
 		});
 
 		it('should save template id with the workflow', () => {
+			cy.intercept('POST', '/rest/workflows').as('saveWorkflow');
 			templatesPage.actions.importTemplate();
 
 			cy.visit(templatesPage.url);
@@ -158,10 +159,8 @@ describe('Workflow templates', () => {
 			workflowPage.actions.hitSelectAll();
 			workflowPage.actions.hitCopy();
 
-			cy.grantBrowserPermissions('clipboardReadWrite', 'clipboardSanitizedWrite');
-			// Check workflow JSON by copying it to clipboard
-			cy.readClipboard().then((workflowJSON) => {
-				expect(workflowJSON).to.contain('"templateId": "1"');
+			cy.wait('@saveWorkflow').then((interception) => {
+				expect(interception.request.body.meta.templateId).to.equal('1');
 			});
 		});
 
