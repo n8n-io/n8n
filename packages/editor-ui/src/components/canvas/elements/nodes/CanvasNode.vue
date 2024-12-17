e3888e807e0a3b5d52965cdda81782365b58cfa7
Alex Grozav <alex@grozav.com>
2024-12-17 16:57:40 +0200

    fix(editor): Remove invalid connections after node handles change (#12247)

--- packages/editor-ui/src/components/canvas/elements/nodes/CanvasNode.vue
+++ packages/editor-ui/src/components/canvas/elements/nodes/CanvasNode.vue
@@ -31,6 +31,7 @@ import { useCanvas } from '@/composables/useCanvas';
 import { createCanvasConnectionHandleString } from '@/utils/canvasUtilsV2';
 import type { EventBus } from 'n8n-design-system';
 import { createEventBus } from 'n8n-design-system';
+import { isEqual } from 'lodash-es';
 
 type Props = NodeProps<CanvasNodeData> & {
 	readOnly?: boolean;
@@ -47,6 +48,8 @@ const emit = defineEmits<{
 	activate: [id: string];
 	'open:contextmenu': [id: string, event: MouseEvent, source: 'node-button' | 'node-right-click'];
 	update: [id: string, parameters: Record<string, unknown>];
+	'update:inputs': [id: string];
+	'update:outputs': [id: string];
 	move: [id: string, position: XYPosition];
 }>();
 
@@ -264,6 +267,18 @@ watch(
 		emit('select', props.id, value);
 	},
 );
+
+watch(inputs, (newValue, oldValue) => {
+	if (!isEqual(newValue, oldValue)) {
+		emit('update:inputs', props.id);
+	}
+});
+
+watch(outputs, (newValue, oldValue) => {
+	if (!isEqual(newValue, oldValue)) {
+		emit('update:outputs', props.id);
+	}
+});
 
 onMounted(() => {
 	props.eventBus?.on('nodes:action', emitCanvasNodeEvent);
