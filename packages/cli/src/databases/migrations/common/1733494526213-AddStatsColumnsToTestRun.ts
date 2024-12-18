58cb56e2876e959041f3e9741ae1efffcb150c38
Eugene Molodkin <eugene@n8n.io>
2024-12-18 12:41:29 +0100

    wip: Rewrite migrations to raw SQL, add column constraints

--- packages/cli/src/databases/migrations/common/1733494526213-AddStatsColumnsToTestRun.ts
+++ packages/cli/src/databases/migrations/common/1733494526213-AddStatsColumnsToTestRun.ts
@@ -1,11 +1,27 @@
 import type { MigrationContext, ReversibleMigration } from '@/databases/types';
 
 export class AddStatsColumnsToTestRun1733494526213 implements ReversibleMigration {
-	async up({ schemaBuilder: { addColumns, column } }: MigrationContext) {
-		await addColumns('test_run', [column('total').int, column('passed').int, column('failed').int]);
+	async up({ escape, runQuery }: MigrationContext) {
+		const tableName = escape.tableName('test_run');
+		const columnNames = ['total', 'passed', 'failed'].map((name) => escape.columnName(name));
+
+		// Values can be NULL only if the test run is new, otherwise they must be non-negative integers
+		for (const name of columnNames) {
+			await runQuery(`ALTER TABLE ${tableName} ADD COLUMN ${name} INT CHECK(
+				CASE
+					WHEN status = 'new' THEN ${name} IS NULL
+					ELSE ${name} >= 0
+				END
+			)`);
+		}
 	}
 
-	async down({ schemaBuilder: { dropColumns } }: MigrationContext) {
-		await dropColumns('test_run', ['total', 'passed', 'failed']);
+	async down({ escape, runQuery }: MigrationContext) {
+		const tableName = escape.tableName('test_run');
+		const columnNames = ['total', 'passed', 'failed'].map((name) => escape.columnName(name));
+
+		for (const name of columnNames) {
+			await runQuery(`ALTER TABLE ${tableName} DROP COLUMN ${name}`);
+		}
 	}
 }
