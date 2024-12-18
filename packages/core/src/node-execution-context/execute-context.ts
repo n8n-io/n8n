92af245d1aab5bfad8618fda69b2405f5206875d
Dana <152518854+dana-gill@users.noreply.github.com>
2024-12-18 16:59:12 +0100

    fix(Extract from File Node): Detect file encoding  (#12081)
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

--- packages/core/src/node-execution-context/execute-context.ts
+++ packages/core/src/node-execution-context/execute-context.ts
@@ -37,6 +37,7 @@ import {
 	getSSHTunnelFunctions,
 	getFileSystemHelperFunctions,
 	getCheckProcessedHelperFunctions,
+	detectBinaryEncoding,
 } from '@/NodeExecuteFunctions';
 
 import { BaseExecuteContext } from './base-execute-context';
@@ -96,6 +97,7 @@ export class ExecuteContext extends BaseExecuteContext implements IExecuteFuncti
 				assertBinaryData(inputData, node, itemIndex, propertyName, 0),
 			getBinaryDataBuffer: async (itemIndex, propertyName) =>
 				await getBinaryDataBuffer(inputData, itemIndex, propertyName, 0),
+			detectBinaryEncoding: (buffer: Buffer) => detectBinaryEncoding(buffer),
 		};
 
 		this.nodeHelpers = {
