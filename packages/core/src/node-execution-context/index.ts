96581dfe9dc50253b5829a042112829a03b4fe91
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-12-19 18:56:27 +0100

    move getInputConnectionData out of NodeExecuteFunctions. add tests

--- packages/core/src/node-execution-context/index.ts
+++ packages/core/src/node-execution-context/index.ts
@@ -4,6 +4,7 @@ export { ExecuteSingleContext } from './execute-single-context';
 export { HookContext } from './hook-context';
 export { LoadOptionsContext } from './load-options-context';
 export { PollContext } from './poll-context';
+// eslint-disable-next-line import/no-cycle
 export { SupplyDataContext } from './supply-data-context';
 export { TriggerContext } from './trigger-context';
 export { WebhookContext } from './webhook-context';
