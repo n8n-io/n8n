c70aae9b54d94ce0ecbbd76056c5aa91e833a63c
Charlie Kolb <charlie@n8n.io>
2024-12-19 09:07:45 +0100

    Update test

--- packages/nodes-base/nodes/ExecuteWorkflow/ExecuteWorkflowTrigger/ExecuteWorkflowTrigger.node.test.ts
+++ packages/nodes-base/nodes/ExecuteWorkflow/ExecuteWorkflowTrigger/ExecuteWorkflowTrigger.node.test.ts
@@ -3,13 +3,15 @@ import type { FieldValueOption, IExecuteFunctions, INode, INodeExecutionData } f
 
 import { ExecuteWorkflowTrigger } from './ExecuteWorkflowTrigger.node';
 import { getFieldEntries } from '../GenericFunctions';
+import { WORKFLOW_INPUTS } from '../constants';
+import { json } from 'generate-schema';
 
 jest.mock('../GenericFunctions');
 
 describe('ExecuteWorkflowTrigger', () => {
 	const mockInputData: INodeExecutionData[] = [
-		{ json: { item: 0, foo: 'bar' } },
-		{ json: { item: 1, foo: 'quz' } },
+		{ json: { item: 0, foo: 'bar' }, index: 0 },
+		{ json: { item: 1, foo: 'quz' }, index: 1 },
 	];
 	const mockNode = mock<INode>({ typeVersion: 1 });
 	const executeFns = mock<IExecuteFunctions>({
@@ -18,24 +20,32 @@ describe('ExecuteWorkflowTrigger', () => {
 		getNodeParameter: jest.fn(),
 	});
 
-	it('should return its input data on V1', async () => {
+	it('should return its input data on V1 or V1.1 passthrough', async () => {
+		// User selection in V1.1, or fallback return value in V1 with dropdown not displayed
 		executeFns.getNodeParameter.mockReturnValueOnce('passthrough');
 		const result = await new ExecuteWorkflowTrigger().execute.call(executeFns);
 
 		expect(result).toEqual([mockInputData]);
 	});
 
-	it('should return transformed input data based on newParams when input source is not passthrough', async () => {
-		executeFns.getNodeParameter.mockReturnValueOnce('usingFieldsBelow');
+	it('should filter out parent input in `Using Fields below` mode', async () => {
+		executeFns.getNodeParameter.mockReturnValueOnce(WORKFLOW_INPUTS);
 		const mockNewParams = [
 			{ name: 'value1', type: 'string' },
 			{ name: 'value2', type: 'number' },
+			{ name: 'foo', type: 'string' },
 		] as FieldValueOption[];
 		const getFieldEntriesMock = (getFieldEntries as jest.Mock).mockReturnValue(mockNewParams);
 
 		const result = await new ExecuteWorkflowTrigger().execute.call(executeFns);
+		const expected = [
+			[
+				{ index: 0, json: { value1: null, value2: null, foo: mockInputData[0].json.foo } },
+				{ index: 1, json: { value1: null, value2: null, foo: mockInputData[1].json.foo } },
+			],
+		];
 
-		expect(result).toEqual([mockInputData]);
+		expect(result).toEqual(expected);
 		expect(getFieldEntriesMock).toHaveBeenCalledWith(executeFns);
 	});
 });
