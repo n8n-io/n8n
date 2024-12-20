efe823ed06bbcaca67482508be050a1e25983684
Charlie Kolb <charlie@n8n.io>
2024-12-20 12:14:19 +0100

    remove unused function

--- packages/editor-ui/src/stores/workflows.store.ts
+++ packages/editor-ui/src/stores/workflows.store.ts
@@ -495,14 +495,6 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, () => {
 		return workflowData;
 	}
 
-	async function getOrFetchWorkflow(id: string): Promise<IWorkflowDb> {
-		if (workflowsById.value[id]) {
-			return workflowsById.value[id];
-		}
-
-		return await fetchWorkflow(id);
-	}
-
 	async function getNewWorkflowData(name?: string, projectId?: string): Promise<INewWorkflowData> {
 		let workflowData = {
 			name: '',
@@ -1764,6 +1756,5 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, () => {
 		setNodes,
 		setConnections,
 		markExecutionAsStopped,
-		getOrFetchWorkflow,
 	};
 });
