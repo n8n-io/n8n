9ed7354294b9486bc799fe683f0c16876c9b031d
Charlie Kolb <charlie@n8n.io>
2024-12-20 12:14:03 +0100

    Udpdate subworkflow example

--- packages/editor-ui/src/constants.workflows.ts
+++ packages/editor-ui/src/constants.workflows.ts
@@ -176,7 +176,7 @@ export const SAMPLE_SUBWORKFLOW_WORKFLOW: WorkflowDataWithTemplateId = {
 		{
 			id: 'c055762a-8fe7-4141-a639-df2372f30060',
 			typeVersion: 1.1,
-			name: 'Execute Workflow Trigger',
+			name: 'Workflow Input Trigger',
 			type: 'n8n-nodes-base.executeWorkflowTrigger',
 			position: [260, 340],
 			parameters: {},
