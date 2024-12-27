733d7edf3bf98c98e6043c8d0b3880bd8b47c087
Danny Martini <danny@n8n.io>
2024-12-27 10:46:51 +0100

    add docstring

--- packages/@n8n/api-types/src/dto/credentials/credentials-get-one-request.dto.ts
+++ packages/@n8n/api-types/src/dto/credentials/credentials-get-one-request.dto.ts
@@ -3,5 +3,11 @@ import { Z } from 'zod-class';
 import { booleanLiteral } from 'dto/common';
 
 export class CredentialsGetOneRequestQuery extends Z.class({
+	/**
+	 * Adds the decrypted `data` field to each credential.
+	 *
+	 * It only does this for credentials for which the user has the
+	 * `credential:update` scope.
+	 */
 	includeData: booleanLiteral.optional(),
 }) {}
