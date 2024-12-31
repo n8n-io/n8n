e7e649c7cc780aed102e4cd445fb0340b1ec4e77
Danny Martini <danny@n8n.io>
2024-12-31 13:04:37 +0100

    feat(core): Add includeData parameter to `GET /credentials` (#12220)
    Co-authored-by: r00gm <raul00gm@gmail.com>

--- packages/@n8n/api-types/src/dto/index.ts
+++ packages/@n8n/api-types/src/dto/index.ts
@@ -29,3 +29,5 @@ export { UserUpdateRequestDto } from './user/user-update-request.dto';
 export { CommunityRegisteredRequestDto } from './license/community-registered-request.dto';
 
 export { VariableListRequestDto } from './variables/variables-list-request.dto';
+export { CredentialsGetOneRequestQuery } from './credentials/credentials-get-one-request.dto';
+export { CredentialsGetManyRequestQuery } from './credentials/credentials-get-many-request.dto';
