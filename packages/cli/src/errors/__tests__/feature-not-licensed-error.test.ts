049fa3417868f01a8f01030fcbee66333ac70de6
Iván Ovejero <ivov.src@gmail.com>
2024-12-31 13:54:59 +0100

    Remove test for `stack` property

--- packages/cli/src/errors/__tests__/feature-not-licensed-error.test.ts
+++
@@ -1,8 +0,0 @@
-import { FeatureNotLicensedError } from '../feature-not-licensed.error';
-
-describe('FeatureNotLicensedError', () => {
-	it('should have a stack property', () => {
-		const error = new FeatureNotLicensedError('feat:multipleMainInstances');
-		expect(error.stack).toBe('');
-	});
-});
