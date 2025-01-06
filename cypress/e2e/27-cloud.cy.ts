b37897c2d3130918fe748acba6b17b32fb110fa2
Ricardo Espinoza <ricardo@n8n.io>
2025-01-06 18:06:27 -0500

    Update easy AI workflow setup instructions when user is in free AI credits experiment

--- cypress/e2e/27-cloud.cy.ts
+++ cypress/e2e/27-cloud.cy.ts
@@ -5,11 +5,15 @@ import {
 	WorkflowPage,
 	visitPublicApiPage,
 	getPublicApiUpgradeCTA,
+	WorkflowsPage,
 } from '../pages';
+
+const NUMBER_OF_AI_CREDITS = 100;
 
 const mainSidebar = new MainSidebar();
 const bannerStack = new BannerStack();
 const workflowPage = new WorkflowPage();
+const workflowsPage = new WorkflowsPage();
 
 describe('Cloud', () => {
 	before(() => {
@@ -22,6 +26,10 @@ describe('Cloud', () => {
 		cy.overrideSettings({
 			deployment: { type: 'cloud' },
 			n8nMetadata: { userId: '1' },
+			aiCredits: {
+				enabled: true,
+				credits: NUMBER_OF_AI_CREDITS,
+			},
 		});
 		cy.intercept('GET', '/rest/admin/cloud-plan', planData).as('getPlanData');
 		cy.intercept('GET', '/rest/cloud/proxy/user/me', {}).as('getCloudUserInfo');
@@ -62,6 +70,96 @@ describe('Cloud', () => {
 			cy.wait(['@loadSettings', '@projects', '@roles', '@getPlanData']);
 
 			getPublicApiUpgradeCTA().should('be.visible');
+		});
+	});
+
+	describe('Easy AI workflow experiment', () => {
+		it('should not show option to take you to the easy AI workflow if user already onboarded on it', () => {
+			window.localStorage.setItem(
+				'N8N_EXPERIMENT_OVERRIDES',
+				JSON.stringify({ '026_easy_ai_workflow': 'variant' }),
+			);
+
+			/*
+				Ideally this should intercept the GET /login endpoint but the GET /users
+				overrides the settings of that endpoint.
+			*/
+			cy.intercept('GET', '/rest/users', (req) => {
+				req.on('response', (res) => {
+					res.body.data = res.body.data.map((user: any) => ({
+						...user,
+						settings: {
+							easyAIWorkflowOnboarded: true,
+						},
+					}));
+				});
+			}).as('loadUsers');
+
+			cy.visit(workflowsPage.url);
+
+			cy.wait('@loadUsers');
+
+			cy.getByTestId('easy-ai-workflow-card').should('not.exist');
+		});
+
+		it('should not show option to take you to the easy AI workflow if experiment is control', () => {
+			window.localStorage.setItem(
+				'N8N_EXPERIMENT_OVERRIDES',
+				JSON.stringify({ '026_easy_ai_workflow': 'control' }),
+			);
+
+			cy.visit(workflowsPage.url);
+
+			cy.getByTestId('easy-ai-workflow-card').should('not.exist');
+		});
+
+		it('should show option to take you to the easy AI workflow if experiment is variant', () => {
+			window.localStorage.setItem(
+				'N8N_EXPERIMENT_OVERRIDES',
+				JSON.stringify({ '026_easy_ai_workflow': 'variant' }),
+			);
+
+			cy.visit(workflowsPage.url);
+
+			cy.getByTestId('easy-ai-workflow-card').should('to.exist');
+		});
+
+		it('should show default instructions if free AI credits experiment is control', () => {
+			window.localStorage.setItem(
+				'N8N_EXPERIMENT_OVERRIDES',
+				JSON.stringify({ '027_free_openai_calls': 'control', '026_easy_ai_workflow': 'variant' }),
+			);
+
+			cy.visit(workflowsPage.url);
+
+			cy.getByTestId('easy-ai-workflow-card').click();
+
+			workflowPage.getters
+				.stickies()
+				.eq(0)
+				.should(($el) => {
+					expect($el).contains.text('Set up your OpenAI credentials in the OpenAI Model node');
+				});
+		});
+
+		it('should show updated instructions if free AI credits experiment is variant', () => {
+			window.localStorage.setItem(
+				'N8N_EXPERIMENT_OVERRIDES',
+				JSON.stringify({ '027_free_openai_calls': 'variant', '026_easy_ai_workflow': 'variant' }),
+			);
+
+			cy.visit(workflowsPage.url);
+
+			cy.getByTestId('easy-ai-workflow-card').click();
+
+			workflowPage.getters
+				.stickies()
+				.eq(0)
+				.should(($el) => {
+					expect($el).contains.text(
+						`Claim your free ${NUMBER_OF_AI_CREDITS} OpenAI calls in the OpenAI model node`,
+					);
+				});
 		});
 	});
 });
