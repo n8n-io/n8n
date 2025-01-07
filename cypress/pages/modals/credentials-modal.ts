573dbe84a084a51aff4ed594f60ce9be622dcd1c
Eugene Molodkin <eugene@n8n.io>
2025-01-07 10:26:18 +0100

    wip: fix broken e2e test

--- cypress/pages/modals/credentials-modal.ts
+++ cypress/pages/modals/credentials-modal.ts
@@ -60,6 +60,7 @@ export class CredentialsModal extends BasePage {
 			this.getters.credentialInputs().should('have.length.greaterThan', 0);
 			this.getters
 				.credentialInputs()
+				.find(':not([role=combobox])')
 				.find('input[type=text], input[type=password]')
 				.each(($el) => {
 					cy.wrap($el).type('test');
