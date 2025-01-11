5b21d15babcbf8ea0c78c17578f7108b2bf29c26
Alex Grozav <alex@grozav.com>
2025-01-11 21:58:12 +0200

    fix: fix openSelectiveNodeCreator custom action on new canvas

--- packages/editor-ui/src/views/NodeView.v2.vue
+++ packages/editor-ui/src/views/NodeView.v2.vue
@@ -31,6 +31,7 @@ import type {
 	IWorkflowDb,
 	IWorkflowTemplate,
 	NodeCreatorOpenSource,
+	NodeFilterType,
 	ToggleNodeCreatorOptions,
 	WorkflowDataWithTemplateId,
 	XYPosition,
@@ -1554,12 +1555,15 @@ function registerCustomActions() {
 	registerCustomAction({
 		key: 'openSelectiveNodeCreator',
 		action: ({
+			creatorview: creatorView,
 			connectiontype: connectionType,
 			node,
 		}: {
+			creatorview: NodeFilterType;
 			connectiontype: NodeConnectionType;
 			node: string;
 		}) => {
+			nodeCreatorStore.openSelectiveNodeCreator({ node, connectionType, creatorView });
 			void onOpenSelectiveNodeCreator(node, connectionType);
 		},
 	});
