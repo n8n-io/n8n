ade8362797ec1893097bb6c0b0d6f55e5c4c0c1c
r00gm <raul00gm@gmail.com>
2025-01-13 10:57:10 +0100

    fix: type

--- packages/editor-ui/src/utils/sourceControlUtils.ts
+++ packages/editor-ui/src/utils/sourceControlUtils.ts
@@ -1,9 +1,8 @@
 import { useI18n } from '@/composables/useI18n';
-import {
-	SOURCE_CONTROL_FILE_STATUS,
-	type SourceControlledFileStatus,
-} from '@/types/sourceControl.types';
+import { type SourceControlledFile, SOURCE_CONTROL_FILE_STATUS } from '@n8n/api-types';
 import type { BaseTextKey } from '@/plugins/i18n';
+
+type SourceControlledFileStatus = SourceControlledFile['status'];
 
 export const getStatusText = (status: SourceControlledFileStatus) =>
 	useI18n().baseText(`settings.sourceControl.status.${status}` as BaseTextKey);
@@ -12,28 +11,28 @@ export const getStatusTheme = (status: SourceControlledFileStatus) => {
 	const statusToBadgeThemeMap: Partial<
 		Record<SourceControlledFileStatus, 'success' | 'danger' | 'warning'>
 	> = {
-		[SOURCE_CONTROL_FILE_STATUS.CREATED]: 'success',
-		[SOURCE_CONTROL_FILE_STATUS.DELETED]: 'danger',
-		[SOURCE_CONTROL_FILE_STATUS.MODIFIED]: 'warning',
+		[SOURCE_CONTROL_FILE_STATUS.created]: 'success',
+		[SOURCE_CONTROL_FILE_STATUS.deleted]: 'danger',
+		[SOURCE_CONTROL_FILE_STATUS.modified]: 'warning',
 	} as const;
 	return statusToBadgeThemeMap[status];
 };
 
 type StatusPriority = Partial<Record<SourceControlledFileStatus, number>>;
 const pullStatusPriority: StatusPriority = {
-	[SOURCE_CONTROL_FILE_STATUS.MODIFIED]: 2,
-	[SOURCE_CONTROL_FILE_STATUS.CREATED]: 1,
-	[SOURCE_CONTROL_FILE_STATUS.DELETED]: 3,
+	[SOURCE_CONTROL_FILE_STATUS.modified]: 2,
+	[SOURCE_CONTROL_FILE_STATUS.created]: 1,
+	[SOURCE_CONTROL_FILE_STATUS.deleted]: 3,
 } as const;
 
 export const getPullPriorityByStatus = (status: SourceControlledFileStatus) =>
 	pullStatusPriority[status] ?? 0;
 
 const pushStatusPriority: StatusPriority = {
-	[SOURCE_CONTROL_FILE_STATUS.MODIFIED]: 1,
-	[SOURCE_CONTROL_FILE_STATUS.RENAMED]: 2,
-	[SOURCE_CONTROL_FILE_STATUS.CREATED]: 3,
-	[SOURCE_CONTROL_FILE_STATUS.DELETED]: 4,
+	[SOURCE_CONTROL_FILE_STATUS.modified]: 1,
+	[SOURCE_CONTROL_FILE_STATUS.renamed]: 2,
+	[SOURCE_CONTROL_FILE_STATUS.created]: 3,
+	[SOURCE_CONTROL_FILE_STATUS.deleted]: 4,
 } as const;
 
 export const getPushPriorityByStatus = (status: SourceControlledFileStatus) =>
