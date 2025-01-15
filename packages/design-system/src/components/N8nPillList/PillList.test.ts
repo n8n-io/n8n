5f116b2f51501ad8915e5fcd68431ebc66bd8138
Charlie Kolb <charlie@n8n.io>
2025-01-15 17:44:19 +0100

    add test to assert sort order

--- packages/design-system/src/components/N8nPillList/PillList.test.ts
+++ packages/design-system/src/components/N8nPillList/PillList.test.ts
@@ -37,20 +37,31 @@ describe('N8nPillList', () => {
 		const wrapper = render(N8nPillList, {
 			props: {
 				modelValue: {
+					propC: false,
 					propA: 'propA value',
 				},
 				inputs: [
 					{ name: 'propA', initialValue: '' },
 					{ name: 'propB', initialValue: 3 },
 					{ name: 'propC', initialValue: true },
+					{ name: 'propD', initialValue: true },
 				],
 			},
 		});
 
 		expect(wrapper.queryByTestId('pill-list-pill-propA')).not.toBeInTheDocument();
+		expect(wrapper.queryByTestId('pill-list-pill-propC')).not.toBeInTheDocument();
 		expect(wrapper.getByTestId('pill-list-slot-propA')).toBeInTheDocument();
 		expect(wrapper.getByTestId('pill-list-pill-propB')).toBeInTheDocument();
-		expect(wrapper.getByTestId('pill-list-pill-propC')).toBeInTheDocument();
+		expect(wrapper.getByTestId('pill-list-slot-propC')).toBeInTheDocument();
+		expect(wrapper.getByTestId('pill-list-pill-propD')).toBeInTheDocument();
+
+		// This asserts order - specifically that propA appears before propC
+		expect(
+			wrapper
+				.getByTestId('pill-list-slot-propA')
+				.compareDocumentPosition(wrapper.getByTestId('pill-list-slot-propC')),
+		).toEqual(2);
 
 		expect(wrapper.html()).toMatchSnapshot();
 	});
