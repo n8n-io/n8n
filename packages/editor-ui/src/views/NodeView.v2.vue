361f8a350383f564dea4abcd8cc3076fa7fccf1e
jeanpaul <jeanpaul@users.noreply.github.com>
2025-01-15 17:08:31 +0100

    fix(editor): Show run workflow button when chat trigger has pinned data (#12616)

--- packages/editor-ui/src/views/NodeView.v2.vue
+++ packages/editor-ui/src/views/NodeView.v2.vue
@@ -1075,7 +1075,9 @@ const isExecutionDisabled = computed(() => {
 	return !containsTriggerNodes.value || allTriggerNodesDisabled.value;
 });
 
-const isRunWorkflowButtonVisible = computed(() => !isOnlyChatTriggerNodeActive.value);
+const isRunWorkflowButtonVisible = computed(
+	() => !isOnlyChatTriggerNodeActive.value || chatTriggerNodePinnedData.value,
+);
 const isStopExecutionButtonVisible = computed(
 	() => isWorkflowRunning.value && !isExecutionWaitingForWebhook.value,
 );
