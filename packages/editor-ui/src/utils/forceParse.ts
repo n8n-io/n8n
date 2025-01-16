02d953db34ec4e44977a8ca908628b62cca82fde
Alex Grozav <alex@grozav.com>
2025-01-16 19:36:08 +0200

    fix(editor): Fix Code node bug erasing and overwriting code when switching between nodes (#12637)
    Co-authored-by: Elias Meire <elias@meire.dev>

--- packages/editor-ui/src/utils/forceParse.ts
+++ packages/editor-ui/src/utils/forceParse.ts
@@ -1,4 +1,7 @@
+import { Annotation } from '@codemirror/state';
 import type { EditorView } from '@codemirror/view';
+
+export const ignoreUpdateAnnotation = Annotation.define<boolean>();
 
 /**
  * Simulate user action to force parser to catch up during scroll.
@@ -6,9 +9,11 @@ import type { EditorView } from '@codemirror/view';
 export function forceParse(view: EditorView) {
 	view.dispatch({
 		changes: { from: view.viewport.to, insert: '_' },
+		annotations: [ignoreUpdateAnnotation.of(true)],
 	});
 
 	view.dispatch({
 		changes: { from: view.viewport.to - 1, to: view.viewport.to, insert: '' },
+		annotations: [ignoreUpdateAnnotation.of(true)],
 	});
 }
