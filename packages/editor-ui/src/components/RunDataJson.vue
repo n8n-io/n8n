a1b80b497cbe9776b0808d4369807db72494eab7
Marc Littlemore <MarcL@users.noreply.github.com>
2025-01-17 15:38:07 +0000

    fix(editor): Correct missing whitespace in JSON output (#12677)

--- packages/editor-ui/src/components/RunDataJson.vue
+++ packages/editor-ui/src/components/RunDataJson.vue
@@ -258,6 +258,7 @@ const getListItemName = (path: string) => {
 	> span {
 		padding: 0 var(--spacing-5xs) 0 var(--spacing-5xs);
 		margin-left: var(--spacing-5xs);
+		white-space: pre-wrap;
 	}
 }
 
