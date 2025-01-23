db331d13e105b9fc6af941d8bd5eff1c5f8c3ed6
Michael Kret <michael.k@radency.com>
2025-01-23 09:51:17 +0200

    test

--- packages/nodes-base/nodes/Telegram/GenericFunctions.ts
+++ packages/nodes-base/nodes/Telegram/GenericFunctions.ts
@@ -257,9 +257,9 @@ export function getSecretToken(this: IHookFunctions | IWebhookFunctions) {
 
 export function createSendAndWaitMessageBody(context: IExecuteFunctions) {
 	const chat_id = context.getNodeParameter('chatId', 0) as string;
-	let text = context.getNodeParameter('message', 0) as string;
 
 	const config = getSendAndWaitConfig(context);
+	let text = config.message;
 
 	const instanceId = context.getInstanceId();
 	const attributionText = 'This message was sent automatically with ';
