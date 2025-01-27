c94aa3c7e1f056b2a14a5d419c82989d3d13c7a5
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2025-01-27 18:17:51 +0100

    chore: Upgrade nock and delete nock related boilerplate code (no-changelog) (#12869)

--- packages/nodes-base/nodes/Notion/test/node/v2/user/getAll.test.ts
+++ packages/nodes-base/nodes/Notion/test/node/v2/user/getAll.test.ts
@@ -1,5 +1,4 @@
 import type { IHttpRequestMethods } from 'n8n-workflow';
-import nock from 'nock';
 
 import { equalityTest, setup, workflowToTests } from '@test/nodes/Helpers';
 
@@ -52,16 +51,6 @@ jest.mock('../../../../shared/GenericFunctions', () => {
 describe('Test NotionV2, user => getAll', () => {
 	const workflows = ['nodes/Notion/test/node/v2/user/getAll.workflow.json'];
 	const tests = workflowToTests(workflows);
-
-	beforeAll(() => {
-		nock.disableNetConnect();
-	});
-
-	afterAll(() => {
-		nock.restore();
-		jest.unmock('../../../../shared/GenericFunctions');
-	});
-
 	const nodeTypes = setup(tests);
 
 	for (const testData of tests) {
