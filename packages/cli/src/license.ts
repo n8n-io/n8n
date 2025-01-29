0d7c19b6dbb65b62bc1c4c3c558534783b67ec83
Csaba Tuncsik <csaba@n8n.io>
2025-01-29 15:12:30 +0100

    fix(cli): Remove fake license settings

--- packages/cli/src/license.ts
+++ packages/cli/src/license.ts
@@ -223,7 +223,6 @@ export class License {
 	}
 
 	isFeatureEnabled(feature: BooleanLicenseFeature) {
-		return true;
 		return this.manager?.hasFeatureEnabled(feature) ?? false;
 	}
 
@@ -378,7 +377,6 @@ export class License {
 	}
 
 	getTeamProjectLimit() {
-		return UNLIMITED_LICENSE_QUOTA;
 		return this.getFeatureValue(LICENSE_QUOTAS.TEAM_PROJECT_LIMIT) ?? 0;
 	}
 
