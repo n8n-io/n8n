8ae101121e871e502cb9cea0b1631c97daa65683
Ricardo Espinoza <ricardo@n8n.io>
2025-01-31 07:16:18 -0500

    improve expiresAt typing

--- packages/@n8n/api-types/src/api-keys.ts
+++ packages/@n8n/api-types/src/api-keys.ts
@@ -1,11 +1,14 @@
+/** Unix timestamp. Seconds since epoch */
+export type UnixTimestamp = number | null;
+
 export type ApiKey = {
 	id: string;
 	label: string;
 	apiKey: string;
 	createdAt: string;
 	updatedAt: string;
-	/** Expiration date in unix timestamp. Null if API key never expire */
-	expiresAt: number | null;
+	/** Null if API key never expires */
+	expiresAt: UnixTimestamp | null;
 };
 
 export type ApiKeyWithRawValue = ApiKey & { rawApiKey: string };
