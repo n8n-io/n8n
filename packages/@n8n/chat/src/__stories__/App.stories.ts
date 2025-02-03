03bc27677c2fa8e51e6cbd86197c1c68ac172071
Michael Kret <michael.k@radency.com>
2025-02-03 09:38:16 +0200

    fix - fail on text response from respond to webhook node

--- packages/@n8n/chat/src/__stories__/App.stories.ts
+++ packages/@n8n/chat/src/__stories__/App.stories.ts
@@ -5,7 +5,7 @@ import { onMounted } from 'vue';
 import { createChat } from '@n8n/chat/index';
 import type { ChatOptions } from '@n8n/chat/types';
 
-const webhookUrl = 'http://localhost:5678/webhook/40a1e365-17e7-43db-aa4d-ec5a4194c15f/chat';
+const webhookUrl = 'http://localhost:5678/webhook/ad712f8b-3546-4d08-b049-e0d035334a4c/chat';
 
 const meta = {
 	title: 'Chat',
@@ -46,7 +46,7 @@ export const Windowed: Story = {
 export const WorkflowChat: Story = {
 	name: 'Workflow Chat',
 	args: {
-		webhookUrl: 'http://localhost:5678/webhook/40a1e365-17e7-43db-aa4d-ec5a4194c15f/chat',
+		webhookUrl: 'http://localhost:5678/webhook/ad712f8b-3546-4d08-b049-e0d035334a4c/chat',
 		mode: 'fullscreen',
 		allowedFilesMimeTypes: 'image/*,text/*,audio/*, application/pdf',
 		allowFileUploads: true,
