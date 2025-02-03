fafebcc6f5e4b2e2564574e030883bb2eb00d4a0
Danny Martini <danny@n8n.io>
2025-02-03 11:28:02 +0100

    Update packages/@n8n/config/src/configs/partial-executions.config.ts
    Co-authored-by: Iván Ovejero <ivov.src@gmail.com>

--- packages/@n8n/config/src/configs/partial-executions.config.ts
+++ packages/@n8n/config/src/configs/partial-executions.config.ts
@@ -2,7 +2,7 @@ import { Config, Env } from '../decorators';
 
 @Config
 export class PartialExecutionsConfig {
-	/** Set this to 2 to enable the new partial execution logic by default. */
+	/** Partial execution logic version to use by default. */
 	@Env('PARTIAL_EXECUTION_VERSION_DEFAULT')
 	version: 1 | 2 = 1;
 
