25edde9ecfc470f5a145733665ca3bdf68248c3c
Elias Meire <elias@meire.dev>
2025-02-04 12:31:59 +0100

    fix(core): Do not enable strict type validation by default for resource mapper (#13037)

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -2719,8 +2719,8 @@ export type ResourceMapperValue = {
 	value: { [key: string]: string | number | boolean | null } | null;
 	matchingColumns: string[];
 	schema: ResourceMapperField[];
-	attemptToConvertTypes: boolean;
-	convertFieldsToString: boolean;
+	attemptToConvertTypes?: boolean;
+	convertFieldsToString?: boolean;
 };
 
 export type FilterOperatorType =
