884a7e23f84258756d8dcdd2dfe933bdedf61adc
Danny Martini <danny@n8n.io>
2025-02-04 12:16:00 +0100

    fix(editor): Actually enforce the version and don't break for old values in local storage (#13025)
    Co-authored-by: Csaba Tuncsik <csaba@n8n.io>

--- packages/editor-ui/src/stores/workflows.store.ts
+++ packages/editor-ui/src/stores/workflows.store.ts
@@ -124,8 +124,7 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, () => {
 	const nodeHelpers = useNodeHelpers();
 	const usersStore = useUsersStore();
 
-	const version = settingsStore.partialExecutionVersion;
-
+	const version = computed(() => settingsStore.partialExecutionVersion);
 	const workflow = ref<IWorkflowDb>(createEmptyWorkflow());
 	const usedCredentials = ref<Record<string, IUsedCredential>>({});
 
@@ -1470,7 +1469,7 @@ export const useWorkflowsStore = defineStore(STORES.WORKFLOWS, () => {
 			return await makeRestApiRequest(
 				rootStore.restApiContext,
 				'POST',
-				`/workflows/${startRunData.workflowData.id}/run?partialExecutionVersion=${version}`,
+				`/workflows/${startRunData.workflowData.id}/run?partialExecutionVersion=${version.value}`,
 				startRunData as unknown as IDataObject,
 			);
 		} catch (error) {
