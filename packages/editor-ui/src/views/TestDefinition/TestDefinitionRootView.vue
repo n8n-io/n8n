f1e5ab3e0455b32d29106af8dcbe759e5afcd29c
r00gm <raul00gm@gmail.com>
2025-02-07 12:21:05 +0100

    chore: initialize workflow state

--- packages/editor-ui/src/views/TestDefinition/TestDefinitionRootView.vue
+++ packages/editor-ui/src/views/TestDefinition/TestDefinitionRootView.vue
@@ -2,27 +2,30 @@
 import { useWorkflowHelpers } from '@/composables/useWorkflowHelpers';
 import { useWorkflowsStore } from '@/stores/workflows.store';
 import { useRouter } from 'vue-router';
-import { onMounted } from 'vue';
+import { useAsyncState } from '@vueuse/core';
+
+const props = defineProps<{
+	name: string;
+}>();
+
 const router = useRouter();
 const workflowHelpers = useWorkflowHelpers({ router });
 const workflowStore = useWorkflowsStore();
 
-async function initWorkflow() {
-	const workflowId = router.currentRoute.value.params.name as string;
+const { isReady } = useAsyncState(async () => {
+	const workflowId = props.name;
 	const isAlreadyInitialized = workflowStore.workflow.id === workflowId;
 
 	if (isAlreadyInitialized) return;
 
 	const workflow = await workflowStore.fetchWorkflow(workflowId);
-	void workflowHelpers.initState(workflow);
-}
-
-onMounted(initWorkflow);
+	workflowHelpers.initState(workflow);
+}, undefined);
 </script>
 
 <template>
 	<div :class="$style.evaluationsView">
-		<router-view />
+		<router-view v-if="isReady" />
 	</div>
 </template>
 
@@ -30,7 +33,5 @@ onMounted(initWorkflow);
 .evaluationsView {
 	width: 100%;
 	height: 100%;
-	margin: auto;
-	padding: var(--spacing-xl) var(--spacing-l);
 }
 </style>
