d41ca832dc6e96440004cc69366ff4e034b5eed9
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2025-02-07 18:16:37 +0100

    refactor(core): Move ExecutionLifecycleHooks to core (#13042)

--- packages/core/src/execution-engine/index.ts
+++ packages/core/src/execution-engine/index.ts
@@ -5,3 +5,4 @@ export * from './node-execution-context';
 export * from './partial-execution-utils';
 export * from './node-execution-context/utils/execution-metadata';
 export * from './workflow-execute';
+export { ExecutionLifecycleHooks } from './execution-lifecycle-hooks';
