2f6dcc2d6074192ea13b90fc9a652571553f9ec6
r00gm <raul00gm@gmail.com>
2025-02-07 12:22:16 +0100

    feat: remove watchers and header section

--- packages/editor-ui/src/views/TestDefinition/TestDefinitionEditView.vue
+++ packages/editor-ui/src/views/TestDefinition/TestDefinitionEditView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted, watch, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { computed, watch, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { NODE_PINNING_MODAL_KEY, VIEWS } from '@/constants';
 import { useToast } from '@/composables/useToast';
 import { useI18n } from '@/composables/useI18n';
@@ -8,156 +8,85 @@ import { useAnnotationTagsStore } from '@/stores/tags.store';
 import { useDebounce } from '@/composables/useDebounce';
 import { useTestDefinitionForm } from '@/components/TestDefinition/composables/useTestDefinitionForm';
 
-import HeaderSection from '@/components/TestDefinition/EditDefinition/sections/HeaderSection.vue';
+import { N8nIconButton, N8nText, N8nButton } from 'n8n-design-system';
 import RunsSection from '@/components/TestDefinition/EditDefinition/sections/RunsSection.vue';
 import type { TestMetricRecord, TestRunRecord } from '@/api/testDefinition.ee';
 import { useUIStore } from '@/stores/ui.store';
 import { useTestDefinitionStore } from '@/stores/testDefinition.store.ee';
 import ConfigSection from '@/components/TestDefinition/EditDefinition/sections/ConfigSection.vue';
-import { useTelemetry } from '@/composables/useTelemetry';
-import { useRootStore } from '@/stores/root.store';
 import { useExecutionsStore } from '@/stores/executions.store';
 import { useWorkflowsStore } from '@/stores/workflows.store';
 import type { IPinData } from 'n8n-workflow';
+import InlineNameEdit from '@/components/InlineNameEdit.vue';
+import { useDocumentVisibility } from '@vueuse/core';
 
 const props = defineProps<{
-	testId?: string;
+	testId: string;
+	name: string;
 }>();
 
 const router = useRouter();
-const route = useRoute();
 const locale = useI18n();
 const { debounce } = useDebounce();
 const toast = useToast();
 const testDefinitionStore = useTestDefinitionStore();
 const tagsStore = useAnnotationTagsStore();
 const uiStore = useUIStore();
-const telemetry = useTelemetry();
 const executionsStore = useExecutionsStore();
 const workflowStore = useWorkflowsStore();
+
+const visibility = useDocumentVisibility();
+watch(visibility, async () => {
+	if (visibility.value !== 'visible') return;
+
+	await tagsStore.fetchAll({ force: true, withUsageCount: true });
+	await getExamplePinnedDataForTags();
+	testDefinitionStore.updateRunFieldIssues(props.testId);
+});
 
 const {
 	state,
 	isSaving,
 	cancelEditing,
 	loadTestData,
-	createTest,
 	updateTest,
 	startEditing,
 	saveChanges,
-	handleKeydown,
 	deleteMetric,
 	updateMetrics,
 } = useTestDefinitionForm();
 
 const isLoading = computed(() => tagsStore.isLoading);
 const tagsById = computed(() => tagsStore.tagsById);
-const testId = computed(() => props.testId ?? (route.params.testId as string));
-const currentWorkflowId = computed(() => route.params.name as string);
+const currentWorkflowId = computed(() => props.name);
 const appliedTheme = computed(() => uiStore.appliedTheme);
 const workflowName = computed(() => workflowStore.workflow.name);
 const hasRuns = computed(() => runs.value.length > 0);
-const fieldsIssues = computed(() => testDefinitionStore.getFieldIssues(testId.value) ?? []);
+const fieldsIssues = computed(() => testDefinitionStore.getFieldIssues(props.testId) ?? []);
 
 const showConfig = ref(true);
 const selectedMetric = ref<string>('');
 const examplePinnedData = ref<IPinData>({});
 
-onMounted(async () => {
-	if (!testDefinitionStore.isFeatureEnabled) {
-		toast.showMessage({
-			title: locale.baseText('testDefinition.notImplemented'),
-			type: 'warning',
-		});
+void loadTestData(props.testId, props.name);
 
-		void router.push({
-			name: VIEWS.WORKFLOW,
-			params: { name: router.currentRoute.value.params.name },
-		});
-		return; // Add early return to prevent loading if feature is disabled
-	}
-	if (testId.value) {
-		await loadTestData(testId.value, currentWorkflowId.value);
-	} else {
-		// 1. Create a tag from the test name
-		await onSaveTest();
-		const tag = generateTagFromName(state.value.name.value);
-
-		const testTag = await createTag(tag);
-		state.value.tags.value = [testTag.id];
-		await onSaveTest();
-	}
-
-	document.addEventListener('visibilitychange', async () => {
-		// Refresh the tags & example pinned data when tab is activated
-		if (document.visibilityState === 'visible') {
-			await tagsStore.fetchAll({ force: true, withUsageCount: true });
-			await getExamplePinnedDataForTags();
-			testDefinitionStore.updateRunFieldIssues(testId.value);
-		}
-	});
-});
-
-function generateTagFromName(name: string): string {
-	let tag = name.toLowerCase().replace(/\s+/g, '_');
-	if (tag.length > 18) {
-		const start = tag.slice(0, 10);
-		const end = tag.slice(-8);
-		tag = `${start}..${end}`;
-	}
-	return tag;
-}
-
-async function onSaveTest() {
+const handleUpdateTest = async () => {
 	try {
-		let savedTest;
-		if (testId.value) {
-			savedTest = await updateTest(testId.value);
-		} else {
-			savedTest = await createTest(currentWorkflowId.value);
-		}
-		if (savedTest && route.name === VIEWS.NEW_TEST_DEFINITION) {
-			await router.replace({
-				name: VIEWS.TEST_DEFINITION_EDIT,
-				params: { testId: savedTest.id },
-			});
-			testDefinitionStore.updateRunFieldIssues(savedTest.id);
-
-			telemetry.track(
-				'User created test',
-				{
-					test_id: savedTest.id,
-					workflow_id: currentWorkflowId.value,
-					session_id: useRootStore().pushRef,
-				},
-				{
-					withPostHog: true,
-				},
-			);
-		}
+		await updateTest(props.testId);
 	} catch (e: unknown) {
 		toast.showError(e, locale.baseText('testDefinition.edit.testSaveFailed'));
 	}
-}
+};
+
+const handleUpdateTestDebounced = debounce(handleUpdateTest, { debounceTime: 400, trailing: true });
+const handleUpdateMetricsDebounced = debounce(updateMetrics, { debounceTime: 400, trailing: true });
 
 function getFieldIssues(key: string) {
 	return fieldsIssues.value.filter((issue) => issue.field === key);
 }
 
-async function onDeleteMetric(deletedMetric: Partial<TestMetricRecord>) {
-	if (deletedMetric.id) {
-		await deleteMetric(deletedMetric.id, testId.value);
-	}
-}
-
-async function createTag(tagName: string) {
-	try {
-		const newTag = await tagsStore.create(tagName, { incrementExisting: true });
-		return newTag;
-	} catch (error) {
-		toast.showError(error, 'Error', error.message);
-		throw error;
-	}
+async function onDeleteMetric(deletedMetric: TestMetricRecord) {
+	await deleteMetric(deletedMetric.id, props.testId);
 }
 
 async function openPinningModal() {
@@ -165,15 +94,15 @@ async function openPinningModal() {
 }
 
 async function runTest() {
-	await testDefinitionStore.startTestRun(testId.value);
-	await testDefinitionStore.fetchTestRuns(testId.value);
+	await testDefinitionStore.startTestRun(props.testId);
+	await testDefinitionStore.fetchTestRuns(props.testId);
 }
 
 async function openExecutionsViewForTag() {
 	const executionsRoute = router.resolve({
 		name: VIEWS.WORKFLOW_EXECUTIONS,
 		params: { name: currentWorkflowId.value },
-		query: { tag: state.value.tags.value[0], testId: testId.value },
+		query: { tag: state.value.tags.value[0], testId: props.testId },
 	});
 
 	window.open(executionsRoute.href, '_blank');
@@ -181,7 +110,7 @@ async function openExecutionsViewForTag() {
 
 const runs = computed(() =>
 	Object.values(testDefinitionStore.testRunsById ?? {}).filter(
-		(run) => run.testDefinitionId === testId.value,
+		(run) => run.testDefinitionId === props.testId,
 	),
 );
 
@@ -191,13 +120,9 @@ const isRunTestEnabled = computed(() => fieldsIssues.value.length === 0 && !isRu
 async function onDeleteRuns(toDelete: TestRunRecord[]) {
 	await Promise.all(
 		toDelete.map(async (run) => {
-			await testDefinitionStore.deleteTestRun({ testDefinitionId: testId.value, runId: run.id });
+			await testDefinitionStore.deleteTestRun({ testDefinitionId: props.testId, runId: run.id });
 		}),
 	);
-}
-
-function toggleConfig() {
-	showConfig.value = !showConfig.value;
 }
 
 async function renameTag(newName: string) {
@@ -227,66 +152,84 @@ async function getExamplePinnedDataForTags() {
 	}
 }
 
-// Debounced watchers for auto-saving
-watch(
-	() => state.value.metrics,
-	debounce(async () => await updateMetrics(testId.value), { debounceTime: 400 }),
-	{ deep: true },
-);
 watch(() => state.value.tags, getExamplePinnedDataForTags);
-watch(
-	() => state.value.name,
-	async (newName) => {
-		if (newName && !state.value.name.isEditing) {
-			const tag = generateTagFromName(newName.value);
-			await renameTag(tag);
-		}
-	},
-	{ deep: true },
-);
 
-watch(
-	() => [
-		state.value.description,
-		state.value.name,
-		state.value.tags,
-		state.value.evaluationWorkflow,
-		state.value.mockedNodes,
-	],
-	debounce(onSaveTest, { debounceTime: 400 }),
-	{ deep: true },
-);
+const updateName = (value: string) => {
+	state.value.name.value = value;
+	void handleUpdateTestDebounced();
+};
+
+const updateDescription = (value: string) => {
+	state.value.description.value = value;
+	void handleUpdateTestDebounced();
+};
 </script>
 
 <template>
-	<div :class="[$style.container, { [$style.noRuns]: !hasRuns }]">
-		<HeaderSection
-			v-model:name="state.name"
-			v-model:description="state.description"
-			v-model:tags="state.tags"
-			:show-config-toggle="hasRuns"
-			:is-saving="isSaving"
-			:get-field-issues="getFieldIssues"
-			:start-editing="startEditing"
-			:save-changes="saveChanges"
-			:handle-keydown="handleKeydown"
-			:on-save-test="onSaveTest"
-			:run-test="runTest"
-			:has-runs="hasRuns"
-			:show-config="showConfig"
-			:toggle-config="toggleConfig"
-			:run-test-enabled="isRunTestEnabled"
-		>
-			<template #runTestTooltip>
-				<template v-if="fieldsIssues.length > 0">
-					<div>{{ locale.baseText('testDefinition.completeConfig') }}</div>
-					<div v-for="issue in fieldsIssues" :key="issue.field">- {{ issue.message }}</div>
-				</template>
-				<template v-if="isRunning">
-					{{ locale.baseText('testDefinition.testIsRunning') }}
-				</template>
-			</template>
-		</HeaderSection>
+	<div v-if="!isLoading" :class="[$style.container, { [$style.noRuns]: !hasRuns }]">
+		<div :class="$style.header">
+			<div style="display: flex; align-items: center">
+				<N8nIconButton
+					icon="arrow-left"
+					type="tertiary"
+					:class="$style.arrowBack"
+					@click="router.push({ name: VIEWS.TEST_DEFINITION, params: { testId } })"
+				></N8nIconButton>
+				<InlineNameEdit
+					:model-value="state.name.value"
+					max-height="none"
+					type="Test name"
+					@update:model-value="updateName"
+				>
+					<N8nText bold size="xlarge" color="text-dark">{{ state.name.value }}</N8nText>
+				</InlineNameEdit>
+			</div>
+			<div style="display: flex; align-items: center; gap: 10px">
+				<N8nText v-if="hasRuns" color="text-light" size="small">
+					{{
+						isSaving
+							? locale.baseText('testDefinition.edit.saving')
+							: locale.baseText('testDefinition.edit.saved')
+					}}
+				</N8nText>
+				<N8nTooltip :disabled="isRunTestEnabled" :placement="'left'">
+					<N8nButton
+						:disabled="!isRunTestEnabled"
+						:class="$style.runTestButton"
+						size="small"
+						data-test-id="run-test-button"
+						:label="locale.baseText('testDefinition.runTest')"
+						type="primary"
+						@click="runTest"
+					/>
+					<template #content>
+						<template v-if="fieldsIssues.length > 0">
+							<div>{{ locale.baseText('testDefinition.completeConfig') }}</div>
+							<div v-for="issue in fieldsIssues" :key="issue.field">- {{ issue.message }}</div>
+						</template>
+						<template v-if="isRunning">
+							{{ locale.baseText('testDefinition.testIsRunning') }}
+						</template>
+					</template>
+				</N8nTooltip>
+			</div>
+		</div>
+		<div :class="$style.description">
+			<InlineNameEdit
+				:model-value="state.description.value"
+				placeholder="Add a description..."
+				:required="false"
+				:autosize="{ minRows: 1, maxRows: 3 }"
+				input-type="textarea"
+				:maxlength="260"
+				max-height="none"
+				type="Test description"
+				:class="$style.editDescription"
+				@update:model-value="updateDescription"
+			>
+				<N8nText size="small" color="text-base">{{ state.description.value }}</N8nText>
+			</InlineNameEdit>
+		</div>
 
 		<div :class="$style.content">
 			<RunsSection
@@ -311,9 +254,12 @@ watch(
 				:start-editing="startEditing"
 				:save-changes="saveChanges"
 				:has-runs="hasRuns"
-				@rename-tag="renameTag"
 				:example-pinned-data="examplePinnedData"
 				:sample-workflow-name="workflowName"
+				@rename-tag="renameTag"
+				@update:metrics="() => handleUpdateMetricsDebounced(testId)"
+				@update:evaluation-workflow="handleUpdateTestDebounced"
+				@update:mocked-nodes="handleUpdateTestDebounced"
 				@open-pinning-modal="openPinningModal"
 				@delete-metric="onDeleteMetric"
 				@open-executions-view-for-tag="openExecutionsViewForTag"
@@ -323,30 +269,35 @@ watch(
 </template>
 
 <style module lang="scss">
-.container {
-	--evaluation-edit-panel-width: 24rem;
-	--metrics-chart-height: 10rem;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-
-	@media (min-height: 56rem) {
-		--metrics-chart-height: 16rem;
-	}
-
-	@include mixins.breakpoint('lg-and-up') {
-		--evaluation-edit-panel-width: 30rem;
-	}
-}
-
 .content {
 	display: flex;
-	overflow-y: hidden;
-	position: relative;
+	justify-content: center;
+	padding: 0 var(--spacing-l);
+	gap: var(--spacing-m);
+}
 
-	.noRuns & {
-		justify-content: center;
-		overflow-y: auto;
-	}
+.header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: var(--spacing-m) var(--spacing-l);
+	padding-left: 27px;
+	padding-bottom: 8px;
+	position: sticky;
+	top: 0;
+	left: 0;
+	background-color: var(--color-background-light);
+	z-index: 2;
+}
+
+.description {
+	max-width: 600px;
+	margin-left: 58px;
+	margin-bottom: 10px;
+}
+
+.arrowBack {
+	--button-hover-background-color: transparent;
+	border: 0;
 }
 </style>
