27da5a0525c26930cb6f90fa27a0b41a2d8701fe
Michael Kret <88898367+michael-radency@users.noreply.github.com>
2025-02-12 20:05:27 +0100

    feat(WebSocket Trigger Node): New node
    Co-authored-by: कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>

---
+++ packages/nodes-base/nodes/WebSocket/WebSocketTrigger.node.ts
@@ -0,0 +1,94 @@
+import type {
+	INodeType,
+	INodeTypeDescription,
+	IDataObject,
+	ITriggerFunctions,
+	ITriggerResponse,
+} from 'n8n-workflow';
+import { jsonParse, NodeConnectionType } from 'n8n-workflow';
+import { type RawData, WebSocket } from 'ws';
+
+type Options = {
+	jsonParseBody: boolean;
+};
+
+export class WebSocketTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'WebSocket Trigger',
+		name: 'webSocketTrigger',
+		description: 'Starts the workflow when a webhook message is recieved',
+		icon: {
+			light: 'file:websocket.svg',
+			dark: 'file:websocket-dark.svg',
+		},
+		iconColor: 'black',
+		group: ['trigger'],
+		version: 1,
+		defaults: {
+			name: 'WebSocket Trigger',
+		},
+		inputs: [],
+		outputs: [NodeConnectionType.Main],
+		properties: [
+			{
+				displayName: 'URL',
+				name: 'url',
+				type: 'string',
+				default: '',
+				required: true,
+			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add Option',
+				default: {},
+				options: [
+					{
+						displayName: 'JSON Parse Body',
+						name: 'jsonParseBody',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to try to parse the message to an object',
+					},
+				],
+			},
+		],
+	};
+
+	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
+		const url = this.getNodeParameter('url', 0) as string;
+		const options = this.getNodeParameter('options') as Options;
+
+		const processMessage = (data: RawData) => {
+			const messageStr = data.toString('utf-8');
+			return options.jsonParseBody ? jsonParse<IDataObject>(messageStr) : messageStr;
+		};
+
+		const ws = new WebSocket(url);
+		const manualTriggerFunction = async () =>
+			await new Promise<void>(async (resolve) => {
+				ws.once('message', (data: RawData) => {
+					const message = processMessage(data);
+					this.emit([this.helpers.returnJsonArray({ message })]);
+					resolve();
+				});
+			});
+
+		if (this.getMode() === 'trigger') {
+			ws.on('message', (data: RawData) => {
+				const message = processMessage(data);
+				this.emit([this.helpers.returnJsonArray({ message })]);
+			});
+		}
+
+		const closeFunction = async () => {
+			ws.close();
+		};
+
+		return {
+			closeFunction,
+			manualTriggerFunction,
+		};
+	}
+}
