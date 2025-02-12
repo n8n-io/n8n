a0eb96b6e38338dec550185f428d100c3aaddacb
Jan Oberhauser <janoberhauser@Jans-MacBook-Pro.local>
2025-02-12 17:59:38 +0100

    Remove no longer needed change

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -2012,10 +2012,6 @@ export type IWorkflowDataProxyAdditionalKeys = IDataObject & {
 	$secrets?: IDataObject;
 	$pageCount?: number;
 
-	$environment?: {
-		deployment: string;
-	};
-
 	/** @deprecated */
 	$executionId?: string;
 	/** @deprecated */
