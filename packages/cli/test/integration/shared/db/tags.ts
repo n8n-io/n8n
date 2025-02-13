f001edb2a2e56ad938e4a9764215162c71117461
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-02-13 10:54:11 +0200

    refactor(core): Use IWorkflowBase over WorkflowEntity in most places (#13225)

--- packages/cli/test/integration/shared/db/tags.ts
+++ packages/cli/test/integration/shared/db/tags.ts
@@ -1,14 +1,14 @@
 import { Container } from '@n8n/di';
+import type { IWorkflowBase } from 'n8n-workflow';
 
 import type { TagEntity } from '@/databases/entities/tag-entity';
-import type { WorkflowEntity } from '@/databases/entities/workflow-entity';
 import { TagRepository } from '@/databases/repositories/tag.repository';
 import { WorkflowTagMappingRepository } from '@/databases/repositories/workflow-tag-mapping.repository';
 import { generateNanoId } from '@/databases/utils/generators';
 
 import { randomName } from '../random';
 
-export async function createTag(attributes: Partial<TagEntity> = {}, workflow?: WorkflowEntity) {
+export async function createTag(attributes: Partial<TagEntity> = {}, workflow?: IWorkflowBase) {
 	const { name } = attributes;
 
 	const tag = await Container.get(TagRepository).save({
