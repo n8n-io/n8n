cdea04e8fb58b6981166bc90f8cd5cb839312c22
Ricardo Espinoza <ricardo@n8n.io>
2025-02-13 10:01:58 -0500

    add missing import

--- packages/cli/test/integration/shared/db/tags.ts
+++ packages/cli/test/integration/shared/db/tags.ts
@@ -2,6 +2,7 @@ import { Container } from '@n8n/di';
 import type { IWorkflowBase } from 'n8n-workflow';
 
 import type { TagEntity } from '@/databases/entities/tag-entity';
+import type { WorkflowEntity } from '@/databases/entities/workflow-entity';
 import { TagRepository } from '@/databases/repositories/tag.repository';
 import { WorkflowTagMappingRepository } from '@/databases/repositories/workflow-tag-mapping.repository';
 import { generateNanoId } from '@/databases/utils/generators';
