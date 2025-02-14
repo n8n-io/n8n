4a77617b4881d9bacb1e4001d15ca2d21f04f9e5
Milorad Filipovic <milorad@n8n.io>
2025-02-14 11:29:57 +0100

    ✨ Implemented async hidden items loading

--- packages/design-system/src/components/N8nBreadcrumbs/Breadcrumbs.stories.ts
+++ packages/design-system/src/components/N8nBreadcrumbs/Breadcrumbs.stories.ts
@@ -1,6 +1,8 @@
 import type { StoryFn } from '@storybook/vue3';
 
-import Breadcrumbs, { type PathItem } from './Breadcrumbs.vue';
+import AsyncLoadingDemo from './AsyncLoadingDemo.vue';
+import Breadcrumbs from './Breadcrumbs.vue';
+import { type PathItem } from './Breadcrumbs.vue';
 
 export default {
 	title: 'Atoms/Breadcrumbs',
@@ -9,20 +11,32 @@ export default {
 };
 
 const items: PathItem[] = [
-	{ id: '1', label: 'Home', href: '/', type: 'item' },
-	{ id: '2', label: 'Folder1', type: 'item' },
-	{ id: '2', label: '', type: 'ellipsis' },
-	{ id: '3', label: 'Current', href: '/folder2', type: 'item' },
+	{ id: '1', label: 'Folder 1', href: '/folder1' },
+	{ id: '2', label: 'Folder 2', href: '/folder2' },
+	{ id: '3', label: 'Folder 3', href: '/folder3' },
+	{ id: '4', label: 'Current' },
 ];
 
-const template: StoryFn = (args, { argTypes }) => ({
+const defaultTemplate: StoryFn = (args, { argTypes }) => ({
 	setup: () => ({ args }),
 	components: { Breadcrumbs },
 	props: Object.keys(argTypes),
 	template: '<Breadcrumbs v-bind="args" />',
 });
 
-export const Default = template.bind({});
+export const Default = defaultTemplate.bind({});
 Default.args = {
 	items,
+};
+
+const asyncLoadingTemplate: StoryFn = (args, { argTypes }) => ({
+	setup: () => ({ args }),
+	components: { AsyncLoadingDemo },
+	props: Object.keys(argTypes),
+	template: '<AsyncLoadingDemo v-bind="args" />',
+});
+
+export const AsyncLoading = asyncLoadingTemplate.bind({});
+AsyncLoading.args = {
+	items: items.slice(2),
 };
--- packages/design-system/src/components/N8nBreadcrumbs/Breadcrumbs.vue
+++ packages/design-system/src/components/N8nBreadcrumbs/Breadcrumbs.vue
@@ -1,53 +1,70 @@
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
-
-import type { Icon } from '../N8nIconPicker/IconPicker.vue';
+import { computed } from 'vue';
 
 export type PathItem = {
 	id: string;
 	label: string;
-	type: 'item' | 'ellipsis';
 	href?: string;
-	icon?: Icon;
 };
 
 type Props = {
 	items: PathItem[];
-	asyncPathHandler?: () => Promise<PathItem[]>;
-	visibleLevels?: number;
-	showRoot?: boolean;
+	hasHiddenItems?: boolean;
+	hiddenItemsTooltip: string;
 	theme: 'small' | 'medium';
 	showBorder?: boolean;
 };
 
+const emit = defineEmits<{
+	beforeTooltipOpen: [];
+	tooltipOpened: [];
+	tooltipClosed: [];
+}>();
+
 defineOptions({ name: 'N8nBreadcrumbs' });
 
 const props = withDefaults(defineProps<Props>(), {
-	visibleLevels: 3,
 	theme: 'medium',
-	showRoot: true,
+	hasHiddenItems: false,
+	hiddenItemsTooltip: '',
 	showBorder: true,
-	asyncPathHandler: undefined,
 });
-
-const fullPath = ref<PathItem[]>([]);
 
 const tooltipText = computed(() => {
-	return props.items.map((item) => item.label).join(' / ');
+	return props.hiddenItemsTooltip;
 });
+
+const handleBeforeTooltipShow = () => {
+	emit('beforeTooltipOpen');
+};
+
+const handleTooltipShow = () => {
+	emit('tooltipOpened');
+};
+
+const handleTooltipClose = () => {
+	emit('tooltipClosed');
+};
 </script>
 <template>
 	<div :class="$style.container">
 		<ul :class="$style.list">
+			<li v-if="props.hasHiddenItems" :class="$style.ellipsis" aria-hidden="true">
+				<n8n-tooltip
+					:content="tooltipText"
+					:popper-class="$style.tooltip"
+					@show="handleTooltipShow"
+					@before-show="handleBeforeTooltipShow"
+					@hide="handleTooltipClose"
+				>
+					<span>...</span>
+				</n8n-tooltip>
+			</li>
+			<li v-if="props.hasHiddenItems" class="separator" aria-hidden="true">/</li>
 			<template v-for="(item, index) in items" :key="item.id">
-				<li v-if="item.type === 'item'">
+				<li>
 					<n8n-link v-if="item.href" :href="item.href" theme="text">{{ item.label }}</n8n-link>
 					<n8n-text v-else>{{ item.label }}</n8n-text>
-				</li>
-				<li v-else-if="item.type === 'ellipsis'" :class="$style.ellipsis">
-					<n8nTooltip :content="tooltipText">
-						<n8n-text>...</n8n-text>
-					</n8nTooltip>
 				</li>
 				<li v-if="index !== items.length - 1" class="separator" aria-hidden="true">/</li>
 			</template>
@@ -64,15 +81,27 @@ const tooltipText = computed(() => {
 	list-style: none;
 	padding: 0;
 	margin: 0;
-	gap: var(--spacing-4xs);
 
 	li {
 		color: var(--color-text-base);
 		padding: var(--spacing-5xs);
 	}
+}
 
-	.ellipsis {
-		cursor: pointer;
+.ellipsis {
+	cursor: pointer;
+}
+
+.hiddenItems {
+	display: flex;
+	align-items: center;
+}
+
+.tooltip {
+	& > div {
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-3xs);
 	}
 }
 </style>
