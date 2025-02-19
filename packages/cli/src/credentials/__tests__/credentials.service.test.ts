6ef9ae18627b25767c76f11266e7791334123cf8
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-02-19 17:50:42 +0200

    refactor: Add dto for `POST /credentials` endpoint (#13306)

--- packages/cli/src/credentials/__tests__/credentials.service.test.ts
+++ packages/cli/src/credentials/__tests__/credentials.service.test.ts
@@ -1,5 +1,4 @@
 import { mock } from 'jest-mock-extended';
-import { nanoId, date } from 'minifaker';
 import { CREDENTIAL_ERRORS, CredentialDataError, Credentials, type ErrorReporter } from 'n8n-core';
 import { CREDENTIAL_EMPTY_VALUE, type ICredentialType } from 'n8n-workflow';
 
@@ -7,11 +6,7 @@ import { CREDENTIAL_BLANKING_VALUE } from '@/constants';
 import type { CredentialTypes } from '@/credential-types';
 import { CredentialsService } from '@/credentials/credentials.service';
 import type { CredentialsEntity } from '@/databases/entities/credentials-entity';
-import type { AuthenticatedRequest } from '@/requests';
-
-import { createNewCredentialsPayload, credentialScopes } from './credentials.test-data';
-
-let req = { user: { id: '123' } } as AuthenticatedRequest;
+import type { CredentialsRepository } from '@/databases/repositories/credentials.repository';
 
 describe('CredentialsService', () => {
 	const credType = mock<ICredentialType>({
@@ -34,8 +29,9 @@ describe('CredentialsService', () => {
 
 	const errorReporter = mock<ErrorReporter>();
 	const credentialTypes = mock<CredentialTypes>();
+	const credentialsRepository = mock<CredentialsRepository>();
 	const service = new CredentialsService(
-		mock(),
+		credentialsRepository,
 		mock(),
 		mock(),
 		mock(),
@@ -77,69 +73,6 @@ describe('CredentialsService', () => {
 				accessToken: CREDENTIAL_EMPTY_VALUE,
 				oauthTokenData: CREDENTIAL_BLANKING_VALUE,
 				csrfSecret: CREDENTIAL_BLANKING_VALUE,
-			});
-		});
-	});
-
-	describe('createCredential', () => {
-		it('it should create new credentials and return with scopes', async () => {
-			// Arrange
-
-			const encryptedData = 'encryptedData';
-
-			const newCredentialPayloadData = createNewCredentialsPayload();
-
-			const newCredential = mock<CredentialsEntity>({
-				name: newCredentialPayloadData.name,
-				data: JSON.stringify(newCredentialPayloadData.data),
-				type: newCredentialPayloadData.type,
-			});
-
-			const encryptedDataResponse = {
-				name: newCredentialPayloadData.name,
-				type: newCredentialPayloadData.type,
-				updatedAt: date(),
-				data: encryptedData,
-			};
-
-			const saveCredentialsResponse = {
-				id: nanoId.nanoid(),
-				name: newCredentialPayloadData.name,
-				type: newCredentialPayloadData.type,
-				updatedAt: encryptedDataResponse.updatedAt,
-				createdAt: date(),
-				data: encryptedDataResponse.data,
-				isManaged: false,
-				shared: undefined,
-			};
-
-			service.prepareCreateData = jest.fn().mockReturnValue(newCredential);
-			service.createEncryptedData = jest.fn().mockImplementation(() => encryptedDataResponse);
-			service.save = jest.fn().mockResolvedValue(saveCredentialsResponse);
-			service.getCredentialScopes = jest.fn().mockReturnValue(credentialScopes);
-
-			// Act
-
-			const createdCredential = await service.createCredential(newCredentialPayloadData, req.user);
-
-			// Assert
-
-			expect(service.prepareCreateData).toHaveBeenCalledWith(newCredentialPayloadData);
-			expect(service.createEncryptedData).toHaveBeenCalledWith(null, newCredential);
-			expect(service.save).toHaveBeenCalledWith(
-				newCredential,
-				encryptedDataResponse,
-				req.user,
-				newCredentialPayloadData.projectId,
-			);
-			expect(service.getCredentialScopes).toHaveBeenCalledWith(
-				req.user,
-				saveCredentialsResponse.id,
-			);
-
-			expect(createdCredential).toEqual({
-				...saveCredentialsResponse,
-				scopes: credentialScopes,
 			});
 		});
 	});
