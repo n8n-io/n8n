1b951b1260ee4a8a6e4ec5c411f781c44af1ce3b
Cesar Sanchez <cesar.sanchezibr@gmail.com>
2025-02-18 19:04:24 -0500

    feat(Airtop Node): Fix ci pipeline

--- packages/nodes-base/nodes/Airtop/Airtop.node.ts
+++ packages/nodes-base/nodes/Airtop/Airtop.node.ts
@@ -6,61 +6,53 @@ import { router } from './actions/router';
 import * as session from './actions/session/Session.resource';
 import * as window from './actions/window/Window.resource';
 
-const nodeDescription: INodeTypeDescription = {
-	displayName: 'Airtop',
-	name: 'airtop',
-	icon: 'file:airtop.svg',
-	group: ['transform'],
-	version: 1,
-	subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
-	description: 'Scrape and control any site with Airtop',
-	defaults: {
-		name: 'Airtop',
-	},
-	inputs: [NodeConnectionType.Main],
-	outputs: [NodeConnectionType.Main],
-	credentials: [
-		{
-			name: 'airtopApi',
-			required: true,
-		},
-	],
-	properties: [
-		{
-			displayName: 'Resource',
-			name: 'resource',
-			type: 'options',
-			noDataExpression: true,
-			options: [
-				{
-					name: 'Session',
-					value: 'session',
-				},
-				{
-					name: 'Window',
-					value: 'window',
-				},
-				{
-					name: 'Extraction',
-					value: 'extraction',
-				},
-			],
-			default: 'session',
-		},
-		...session.description,
-		...window.description,
-		...extraction.description,
-	],
-};
-
 export class Airtop implements INodeType {
-	description: INodeTypeDescription;
-
-	constructor() {
-		this.description = {
-			...nodeDescription,
-		};
-	}
+	description: INodeTypeDescription = {
+		displayName: 'Airtop',
+		name: 'airtop',
+		icon: 'file:airtop.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+		description: 'Scrape and control any site with Airtop',
+		defaults: {
+			name: 'Airtop',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'airtopApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Session',
+						value: 'session',
+					},
+					{
+						name: 'Window',
+						value: 'window',
+					},
+					{
+						name: 'Extraction',
+						value: 'extraction',
+					},
+				],
+				default: 'session',
+			},
+			...session.description,
+			...window.description,
+			...extraction.description,
+		],
+	};
 
 	async execute(this: IExecuteFunctions) {
 		return await router.call(this);
