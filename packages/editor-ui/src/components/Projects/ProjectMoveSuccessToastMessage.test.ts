7bd83d7d330b6f01b5798461f2218254a9964d87
Csaba Tuncsik <csaba@n8n.io>
2025-02-21 11:05:37 +0100

    fix(editor): Optionally share credentials used by the workflow when moving the workflow between projects (#12524)
    Co-authored-by: Danny Martini <danny@n8n.io>

--- packages/editor-ui/src/components/Projects/ProjectMoveSuccessToastMessage.test.ts
+++ packages/editor-ui/src/components/Projects/ProjectMoveSuccessToastMessage.test.ts
@@ -18,43 +18,59 @@ describe('ProjectMoveSuccessToastMessage', () => {
 	it('should show credentials message if the resource is a workflow', async () => {
 		const props = {
 			routeName: VIEWS.PROJECTS_WORKFLOWS,
-			resource: {
-				id: '1',
-				name: 'My Workflow',
-				homeProject: {
-					id: '2',
-					name: 'My Project',
-				},
-			},
 			resourceType: ResourceType.Workflow,
-			resourceTypeLabel: 'Workflow',
 			targetProject: {
 				id: '2',
 				name: 'My Project',
 			},
+			isShareCredentialsChecked: false,
+			areAllUsedCredentialsShareable: false,
 		};
 		const { getByText } = renderComponent({ props });
-		expect(getByText(/Please double check any credentials/)).toBeInTheDocument();
+		expect(getByText(/The workflow's credentials were not shared/)).toBeInTheDocument();
+	});
+
+	it('should show all credentials shared message if the resource is a workflow', async () => {
+		const props = {
+			routeName: VIEWS.PROJECTS_WORKFLOWS,
+			resourceType: ResourceType.Workflow,
+			targetProject: {
+				id: '2',
+				name: 'My Project',
+			},
+			isShareCredentialsChecked: true,
+			areAllUsedCredentialsShareable: true,
+		};
+		const { getByText } = renderComponent({ props });
+		expect(getByText(/The workflow's credentials were shared/)).toBeInTheDocument();
+	});
+
+	it('should show not all credentials shared message if the resource is a workflow', async () => {
+		const props = {
+			routeName: VIEWS.PROJECTS_WORKFLOWS,
+			resourceType: ResourceType.Workflow,
+			targetProject: {
+				id: '2',
+				name: 'My Project',
+			},
+			isShareCredentialsChecked: true,
+			areAllUsedCredentialsShareable: false,
+		};
+		const { getByText } = renderComponent({ props });
+		expect(getByText(/Due to missing permissions/)).toBeInTheDocument();
 	});
 
 	it('should show link if the target project type is team project', async () => {
 		const props = {
 			routeName: VIEWS.PROJECTS_WORKFLOWS,
-			resource: {
-				id: '1',
-				name: 'My Workflow',
-				homeProject: {
-					id: '2',
-					name: 'My Project',
-				},
-			},
 			resourceType: ResourceType.Workflow,
-			resourceTypeLabel: 'workflow',
 			targetProject: {
 				id: '2',
 				name: 'Team Project',
 				type: ProjectTypes.Team,
 			},
+			isShareCredentialsChecked: false,
+			areAllUsedCredentialsShareable: false,
 		};
 		const { getByRole } = renderComponent({ props });
 		expect(getByRole('link')).toBeInTheDocument();
@@ -63,25 +79,17 @@ describe('ProjectMoveSuccessToastMessage', () => {
 	it('should show only general if the resource is credential and moved to a personal project', async () => {
 		const props = {
 			routeName: VIEWS.PROJECTS_WORKFLOWS,
-			resource: {
-				id: '1',
-				name: 'Notion API',
-				homeProject: {
-					id: '2',
-					name: 'My Project',
-				},
-			},
 			resourceType: ResourceType.Credential,
-			resourceTypeLabel: 'credential',
 			targetProject: {
 				id: '2',
 				name: 'Personal Project',
 				type: ProjectTypes.Personal,
 			},
+			isShareCredentialsChecked: false,
+			areAllUsedCredentialsShareable: false,
 		};
-		const { getByText, queryByText, queryByRole } = renderComponent({ props });
-		expect(getByText(/credential was moved to /)).toBeInTheDocument();
-		expect(queryByText(/Please double check any credentials/)).not.toBeInTheDocument();
+		const { queryByText, queryByRole } = renderComponent({ props });
+		expect(queryByText(/The workflow's credentials were not shared/)).not.toBeInTheDocument();
 		expect(queryByRole('link')).not.toBeInTheDocument();
 	});
 });
