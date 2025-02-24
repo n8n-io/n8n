43108d1a9834e7cd4d92797fef2d4772f4a714c5
r00gm <raul00gm@gmail.com>
2025-02-24 11:48:48 +0100

    fix: class name to match already existing test

--- packages/editor-ui/src/components/TestDefinition/EditDefinition/EvaluationStep.vue
+++ packages/editor-ui/src/components/TestDefinition/EditDefinition/EvaluationStep.vue
@@ -32,7 +32,8 @@ const hasIssues = computed(() => props.issues.length > 0);
 
 const containerClass = computed(() => {
 	return {
-		[$style.hasIssues]: hasIssues.value,
+		[$style.evaluationStep]: true,
+		[$style['has-issues']]: true,
 	};
 });
 
@@ -67,68 +68,66 @@ const popperModifiers = [
 </script>
 
 <template>
-	<div :class="containerClass">
-		<div :class="[$style.evaluationStep]" data-test-id="evaluation-step">
-			<div :class="$style.content">
-				<N8nTooltip
-					placement="right"
-					:disabled="!externalTooltip"
-					:show-arrow="false"
-					:popper-class="$style.evaluationTooltip"
-					:popper-options="{ modifiers: popperModifiers }"
-					:content="tooltip"
-				>
-					<div :class="$style.header" @click="toggleExpand">
-						<div :class="$style.label">
-							<N8nText bold>
-								<slot v-if="$slots.title" name="title" />
-								<template v-else>{{ title }}</template>
-							</N8nText>
-							<N8nInfoTip
-								v-if="!externalTooltip"
-								:class="$style.infoTip"
-								:bold="true"
-								type="tooltip"
-								theme="info"
-								tooltip-placement="top"
-								:enterable="false"
-							>
-								{{ tooltip }}
-							</N8nInfoTip>
-						</div>
-						<div :class="$style.actions">
-							<N8nInfoTip
-								v-if="renderIssues"
-								:bold="true"
-								type="tooltip"
-								theme="warning"
-								tooltip-placement="top"
-								:enterable="false"
-							>
-								{{ issuesList }}
-							</N8nInfoTip>
-							<N8nText
-								v-if="$slots.cardContent"
-								data-test-id="evaluation-step-collapse-button"
-								size="xsmall"
-								:color="hasIssues ? 'primary' : 'text-base'"
-								bold
-							>
-								{{
-									isExpanded
-										? locale.baseText('testDefinition.edit.step.collapse')
-										: locale.baseText('testDefinition.edit.step.configure')
-								}}
-								<font-awesome-icon :icon="isExpanded ? 'angle-up' : 'angle-down'" size="lg" />
-							</N8nText>
-						</div>
+	<div :class="containerClass" data-test-id="evaluation-step">
+		<div :class="$style.content">
+			<N8nTooltip
+				placement="right"
+				:disabled="!externalTooltip"
+				:show-arrow="false"
+				:popper-class="$style.evaluationTooltip"
+				:popper-options="{ modifiers: popperModifiers }"
+				:content="tooltip"
+			>
+				<div :class="$style.header" @click="toggleExpand">
+					<div :class="$style.label">
+						<N8nText bold>
+							<slot v-if="$slots.title" name="title" />
+							<template v-else>{{ title }}</template>
+						</N8nText>
+						<N8nInfoTip
+							v-if="!externalTooltip"
+							:class="$style.infoTip"
+							:bold="true"
+							type="tooltip"
+							theme="info"
+							tooltip-placement="top"
+							:enterable="false"
+						>
+							{{ tooltip }}
+						</N8nInfoTip>
 					</div>
-				</N8nTooltip>
-				<div v-if="$slots.cardContent && isExpanded" :class="$style.cardContentWrapper">
-					<div :class="$style.cardContent" data-test-id="evaluation-step-content">
-						<N8nText v-if="description" size="small" color="text-light">{{ description }}</N8nText>
-						<slot name="cardContent" />
+					<div :class="$style.actions">
+						<N8nInfoTip
+							v-if="renderIssues"
+							:bold="true"
+							type="tooltip"
+							theme="warning"
+							tooltip-placement="top"
+							:enterable="false"
+						>
+							{{ issuesList }}
+						</N8nInfoTip>
+						<N8nText
+							v-if="$slots.cardContent"
+							data-test-id="evaluation-step-collapse-button"
+							size="xsmall"
+							:color="hasIssues ? 'primary' : 'text-base'"
+							bold
+						>
+							{{
+								isExpanded
+									? locale.baseText('testDefinition.edit.step.collapse')
+									: locale.baseText('testDefinition.edit.step.configure')
+							}}
+							<font-awesome-icon :icon="isExpanded ? 'angle-up' : 'angle-down'" size="lg" />
+						</N8nText>
 					</div>
+				</div>
+			</N8nTooltip>
+			<div v-if="$slots.cardContent && isExpanded" :class="$style.cardContentWrapper">
+				<div :class="$style.cardContent" data-test-id="evaluation-step-content">
+					<N8nText v-if="description" size="small" color="text-light">{{ description }}</N8nText>
+					<slot name="cardContent" />
 				</div>
 			</div>
 		</div>
@@ -211,5 +210,12 @@ const popperModifiers = [
 
 .cardContentWrapper {
 	border-top: var(--border-base);
+}
+
+.has-issues {
+	/**
+		* This comment is needed or the css module
+		* will interpret as undefined
+	 */
 }
 </style>
