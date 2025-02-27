0d7b002dfcf30426b61c87b616c6954bcf2d4252
Matthias Stallmann <feelgood.interface@gmail.com>
2025-02-27 12:16:13 +0100

    add shared key tests

--- packages/nodes-base/nodes/Microsoft/Storage/test/credentials/sharedKey.test.ts
+++ packages/nodes-base/nodes/Microsoft/Storage/test/credentials/sharedKey.test.ts
@@ -3,6 +3,9 @@ import nock from 'nock';
 
 import { CredentialsHelper, equalityTest, setup, workflowToTests } from '@test/nodes/Helpers';
 
+import { AzureStorageSharedKeyApi } from '../../../../../credentials/AzureStorageSharedKeyApi.credentials';
+import { FAKE_CREDENTIALS_DATA } from '../../../../../test/nodes/FakeCredentialsMap';
+
 describe('Azure Storage Node', () => {
 	const workflows = ['nodes/Microsoft/Storage/test/workflows/credentials_sharedKey.workflow.json'];
 	const workflowTests = workflowToTests(workflows);
@@ -82,5 +85,74 @@ describe('Azure Storage Node', () => {
 			};
 			test(workflow.description, async () => await equalityTest(workflow, nodeTypes));
 		}
+	});
+
+	describe('authenticate', () => {
+		const azureStorageSharedKeyApi = new AzureStorageSharedKeyApi();
+
+		it('should remove undefined query parameters and headers', async () => {
+			const credentials: ICredentialDataDecryptedObject = {
+				account: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.account,
+				key: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.key,
+			};
+			const requestOptions: IHttpRequestOptions = {
+				url: `${FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.baseUrl}/mycontainer`,
+				qs: { restype: 'container', query1: undefined },
+				headers: {
+					'Content-Length': undefined,
+				},
+				method: 'GET',
+			};
+
+			const result = await azureStorageSharedKeyApi.authenticate(credentials, requestOptions);
+
+			expect(result.qs).toEqual({ restype: 'container' });
+			expect(result.headers).not.toHaveProperty('Content-Length');
+		});
+
+		it('should default method to GET if not provided', async () => {
+			const credentials: ICredentialDataDecryptedObject = {
+				account: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.account,
+				key: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.key,
+			};
+			const requestOptions: IHttpRequestOptions = {
+				url: `${FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.baseUrl}/mycontainer`,
+				qs: { restype: 'container' },
+				headers: {
+					'Content-Length': undefined,
+				},
+			};
+
+			const result = await azureStorageSharedKeyApi.authenticate(credentials, requestOptions);
+			expect(result.method).toBe('GET');
+		});
+
+		it('should generate a valid authorization header', async () => {
+			const credentials: ICredentialDataDecryptedObject = {
+				account: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.account,
+				key: FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.key,
+			};
+			const requestOptions: IHttpRequestOptions = {
+				url: `${FAKE_CREDENTIALS_DATA.azureStorageSharedKeyApi.baseUrl}/mycontainer/myblob`,
+				qs: { param1: 'value1' },
+				headers: {
+					'x-ms-date': 'Thu, 27 Feb 2025 11:05:49 GMT',
+					'x-ms-version': '2021-12-02',
+					'x-ms-blob-content-language': 'en-EN',
+					'x-ms-blob-content-type': 'image/jpeg',
+					'x-ms-expiry-option': 'Absolute',
+					'x-ms-blob-type': 'BlockBlob',
+					'x-ms-blob-content-disposition': 'attachment; filename="image.jpg"',
+					'x-ms-meta-key1': 'value1',
+					'x-ms-tags': 'tag1=value1',
+				},
+				method: 'PUT',
+			};
+			const result = await azureStorageSharedKeyApi.authenticate(credentials, requestOptions);
+
+			expect(result.headers?.authorization).toBe(
+				'SharedKey devstoreaccount1:6sSQ3N4yNFQynBs/iLptIRPS5DQeaFBocW+dyYbAdOI=',
+			);
+		});
 	});
 });
