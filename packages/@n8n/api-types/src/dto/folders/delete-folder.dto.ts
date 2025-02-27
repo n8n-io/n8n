01963f38b86c04da82ce1c43ba9f427fff9691d3
Ricardo Espinoza <ricardo@n8n.io>
2025-02-27 11:29:26 -0500

    fix schema path

--- packages/@n8n/api-types/src/dto/folders/delete-folder.dto.ts
+++ packages/@n8n/api-types/src/dto/folders/delete-folder.dto.ts
@@ -1,6 +1,6 @@
 import { Z } from 'zod-class';
 
-import { folderId } from 'schemas/folder.schema';
+import { folderId } from '../../schemas/folder.schema';
 
 export class DeleteFolderDto extends Z.class({
 	transferToFolderId: folderId.optional(),
