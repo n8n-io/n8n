2f2ee86bb038cb8c5a72aa7aa1ad407ed01a5566
Dana Lee <dana@n8n.io>
2025-02-28 13:40:05 +0100

    Add tests for updating rows with special characters

--- packages/nodes-base/nodes/Google/Sheet/test/v2/node/update.test.ts
+++ packages/nodes-base/nodes/Google/Sheet/test/v2/node/update.test.ts
@@ -106,4 +106,102 @@ describe('Google Sheet - Update', () => {
 			'USER_ENTERED',
 		);
 	});
+
+	it('should update rows by column values with special character', async () => {
+		mockExecuteFunctions.getInputData.mockReturnValueOnce([
+			{
+				json: {
+					row_number: 3,
+					name: '** δ$% " []',
+					text: 'δ$% " []',
+				},
+				pairedItem: {
+					item: 0,
+					input: undefined,
+				},
+			},
+		]);
+
+		mockExecuteFunctions.getNodeParameter.mockImplementation((parameterName: string) => {
+			const params: { [key: string]: string | object } = {
+				options: {},
+				'options.cellFormat': 'USER_ENTERED',
+				'columns.matchingColumns': ['row_number'],
+				'columns.value': {
+					'ha []': 'kayyyy$',
+					macarena: 'baile',
+					'Real.1': 't&c',
+					'21 "': 'Σ',
+				},
+				dataMode: 'autoMapInputData',
+			};
+			return params[parameterName];
+		});
+
+		mockGoogleSheet.getData.mockResolvedValueOnce([
+			['Real.1', '21 "', 'dfd', 'ha []', 'macarena'],
+			['aye.2', '"book"', 'ee', 'dd', 'dance'],
+			['t&c', 'Σ', 'baz', 'kayyyy$', 'baile'],
+			['fudge.2', '9080', 'live', 'dog', 'brazil'],
+		]);
+
+		mockGoogleSheet.getColumnValues.mockResolvedValueOnce([]);
+
+		mockGoogleSheet.prepareDataForUpdatingByRowNumber.mockReturnValueOnce({
+			updateData: [
+				{
+					range: 'Sheet1!B3',
+					values: [['** δ$% " []']],
+				},
+				{
+					range: 'Sheet1!C3',
+					values: [['δ$% " []']],
+				},
+			],
+		});
+
+		await execute.call(mockExecuteFunctions, mockGoogleSheet, 'Sheet1');
+
+		expect(mockGoogleSheet.getData).toHaveBeenCalledWith('Sheet1', 'FORMATTED_VALUE');
+
+		expect(mockGoogleSheet.getColumnValues).toHaveBeenCalledWith({
+			range: 'Sheet1!A:Z',
+			keyIndex: -1,
+			dataStartRowIndex: 1,
+			valueRenderMode: 'UNFORMATTED_VALUE',
+			sheetData: [
+				['Real.1', '21 "', 'dfd', 'ha []', 'macarena'],
+				['aye.2', '"book"', 'ee', 'dd', 'dance'],
+				['t&c', 'Σ', 'baz', 'kayyyy$', 'baile'],
+				['fudge.2', '9080', 'live', 'dog', 'brazil'],
+			],
+		});
+
+		expect(mockGoogleSheet.prepareDataForUpdatingByRowNumber).toHaveBeenCalledWith(
+			[
+				{
+					'21 "': 'Σ',
+					'Real.1': 't&c',
+					'ha []': 'kayyyy$',
+					macarena: 'baile',
+				},
+			],
+			'Sheet1!A:Z',
+			[['Real.1', '21 "', 'dfd', 'ha []', 'macarena']],
+		);
+
+		expect(mockGoogleSheet.batchUpdate).toHaveBeenCalledWith(
+			[
+				{
+					range: 'Sheet1!B3',
+					values: [['** δ$% " []']],
+				},
+				{
+					range: 'Sheet1!C3',
+					values: [['δ$% " []']],
+				},
+			],
+			'USER_ENTERED',
+		);
+	});
 });
