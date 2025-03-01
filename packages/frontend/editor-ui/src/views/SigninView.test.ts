0b5f49fd378814c0b88a31002097a92e9a32f0f8
Ricardo Espinoza <ricardo@n8n.io>
2025-03-01 02:13:52 +0100

    update name in FE tets

--- packages/frontend/editor-ui/src/views/SigninView.test.ts
+++ packages/frontend/editor-ui/src/views/SigninView.test.ts
@@ -85,7 +85,7 @@ describe('SigninView', () => {
 		await userEvent.click(submitButton);
 
 		expect(usersStore.loginWithCreds).toHaveBeenCalledWith({
-			email: 'test@n8n.io',
+			emailOrLdapUsername: 'test@n8n.io',
 			password: 'password',
 			mfaCode: undefined,
 			mfaRecoveryCode: undefined,
