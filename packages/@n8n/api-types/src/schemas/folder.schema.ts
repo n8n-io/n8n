6c182d8936f6d66d88256eeea3976f3fcd810e91
Ricardo Espinoza <ricardo@n8n.io>
2025-03-12 13:53:45 +0100

    feat(core): Update PATCH `/projects/:projectId/folders/:folderId` endpoint to allow moving folder (no-changelog) (#13574)

--- packages/@n8n/api-types/src/schemas/folder.schema.ts
+++ packages/@n8n/api-types/src/schemas/folder.schema.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod';
 
 export const folderNameSchema = z.string().trim().min(1).max(128);
-export const folderId = z.string().max(36);
+export const folderIdSchema = z.string().max(36);
