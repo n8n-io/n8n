325e6c0a727e6cf3ede744c790bc12c60f7cc306
autologie <suguru@n8n.io>
2025-03-12 12:35:47 +0100

    fix formatting

--- packages/frontend/editor-ui/src/plugins/icons/index.ts
+++ packages/frontend/editor-ui/src/plugins/icons/index.ts
@@ -184,7 +184,7 @@ import {
 	statusCanceled,
 	statusNew,
 	statusUnknown,
-  faPopOut,
+	faPopOut,
 } from './custom';
 import { faStickyNote } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -377,7 +377,7 @@ export const FontAwesomePlugin: Plugin = {
 		addIcon(statusCanceled);
 		addIcon(statusNew);
 		addIcon(statusUnknown);
-    
+
 		addIcon(faPopOut);
 
 		app.component('FontAwesomeIcon', FontAwesomeIcon);
