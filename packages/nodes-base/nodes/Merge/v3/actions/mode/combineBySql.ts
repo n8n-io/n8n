5284e6910172098e10d482b7f84b06f478b85408
Michael Kret <michael.k@radency.com>
2025-03-12 13:19:55 +0200

    tests

--- packages/nodes-base/nodes/Merge/v3/actions/mode/combineBySql.ts
+++ packages/nodes-base/nodes/Merge/v3/actions/mode/combineBySql.ts
@@ -15,6 +15,7 @@ import { NodeOperationError } from 'n8n-workflow';
 import { getResolvables, updateDisplayOptions } from '@utils/utilities';
 
 import { numberInputsProperty } from '../../helpers/descriptions';
+import { modifySelectQuery, rowToExecutionData } from '../../helpers/utils';
 
 export const properties: INodeProperties[] = [
 	numberInputsProperty,
@@ -56,47 +57,7 @@ const prepareError = (node: INode, error: Error) => {
 	});
 };
 
-const rowToExecutionData = (data: IDataObject): INodeExecutionData => {
-	const keys = Object.keys(data);
-	const pairedItem: IPairedItemData[] = [];
-	const json: IDataObject = {};
-
-	for (const key of keys) {
-		if (key.startsWith('pairedItem')) {
-			if (data[key] === undefined) continue;
-			pairedItem.push(data[key] as IPairedItemData);
-		} else {
-			json[key] = data[key];
-		}
-	}
-
-	return { json, pairedItem };
-};
-
-function modifySelectQuery(userQuery: string, inputLength: number): string {
-	const selectMatch = userQuery.match(/SELECT\s+(.+?)\s+FROM/i);
-	if (!selectMatch) return userQuery;
-
-	let selectedColumns = selectMatch[1].trim();
-
-	if (selectedColumns === '*') {
-		return userQuery;
-	}
-
-	const pairedItemColumns = [];
-
-	for (let i = 1; i <= inputLength; i++) {
-		if (userQuery.includes(`input${i}`)) {
-			pairedItemColumns.push(`input${i}.pairedItem AS pairedItem${i}`);
-		}
-	}
-
-	selectedColumns += ', ' + pairedItemColumns.join(', ');
-
-	return userQuery.replace(selectMatch[0], `SELECT ${selectedColumns} FROM`);
-}
-
-export async function executeSelectWithMapedPairedItems(
+async function executeSelectWithMapedPairedItems(
 	node: INode,
 	inputsData: INodeExecutionData[][],
 	query: string,
