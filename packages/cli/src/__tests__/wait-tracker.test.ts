21d019cb2fcd2711e87124ba164b33900f08c397
Jaakko Husso <jaakko@n8n.io>
2025-03-12 13:54:50 +0200

    test(core): Unit test for waiting sub-workflow resuming parent

--- packages/cli/src/__tests__/wait-tracker.test.ts
+++ packages/cli/src/__tests__/wait-tracker.test.ts
@@ -1,7 +1,7 @@
 import { mock } from 'jest-mock-extended';
 import type { InstanceSettings } from 'n8n-core';
-import type { IRun, IWorkflowBase } from 'n8n-workflow';
-import { createDeferredPromise } from 'n8n-workflow';
+import type { IRun, IWorkflowBase, INode } from 'n8n-workflow';
+import { createDeferredPromise, WAIT_INDEFINITELY } from 'n8n-workflow';
 
 import type { ActiveExecutions } from '@/active-executions';
 import type { Project } from '@/databases/entities/project';
@@ -149,10 +149,11 @@ describe('WaitTracker', () => {
 			);
 		});
 
-		it('should also resume parent execution once sub-workflow finishes', async () => {
+		it('should also resume parent execution once waiting sub-workflow finishes with no data', async () => {
 			const parentExecution = mock<IExecutionResponse>({
 				id: 'parent_execution_id',
 				finished: false,
+				status: 'waiting',
 			});
 			parentExecution.workflowData = mock<IWorkflowBase>({ id: 'parent_workflow_id' });
 			execution.data.parentExecution = {
@@ -189,8 +190,254 @@ describe('WaitTracker', () => {
 				execution.id,
 			);
 
-			postExecutePromise.resolve(mock<IRun>());
+			const subworkflowResults = mock<IRun>({
+				data: {
+					resultData: {
+						runData: {},
+					},
+				},
+			});
+
+			postExecutePromise.resolve(subworkflowResults);
 			await jest.advanceTimersByTimeAsync(100);
+
+			expect(workflowRunner.run).toHaveBeenCalledTimes(2);
+			expect(workflowRunner.run).toHaveBeenNthCalledWith(
+				2,
+				{
+					executionMode: parentExecution.mode,
+					executionData: parentExecution.data,
+					workflowData: parentExecution.workflowData,
+					projectId: project.id,
+					pushRef: parentExecution.data.pushRef,
+				},
+				false,
+				false,
+				parentExecution.id,
+			);
+		});
+
+		it('should resume parent execution with sub-workflow results once waiting sub-workflow finishes with data', async () => {
+			const parentExecution: IExecutionResponse = {
+				id: 'parent_execution_id',
+				finished: false,
+				status: 'waiting',
+				waitTill: WAIT_INDEFINITELY,
+				workflowData: mock<IWorkflowBase>({ id: 'parent_workflow_id' }),
+				customData: {},
+				annotation: {
+					tags: [],
+				},
+				createdAt: new Date(),
+				startedAt: new Date(),
+				mode: 'manual',
+				workflowId: 'parent_workflow_id',
+				data: {
+					resultData: {
+						runData: {
+							'Execute Sub Workflow': [
+								{
+									hints: [],
+									startTime: new Date().getTime(),
+									executionTime: 20,
+									source: [
+										{
+											previousNode: 'Previous Node In Parent Workflow',
+										},
+									],
+									metadata: {
+										subExecution: {
+											executionId: '123',
+											workflowId: 'some random id',
+										},
+										subExecutionsCount: 1,
+									},
+									executionStatus: 'waiting',
+									data: {
+										main: [
+											[
+												{
+													json: {
+														data: 'Input sub workflow wait node received before waiting',
+													},
+													pairedItem: {
+														item: 0,
+													},
+												},
+											],
+										],
+									},
+								},
+							],
+						},
+						lastNodeExecuted: 'Execute Sub Workflow',
+					},
+					executionData: {
+						contextData: mock(),
+						nodeExecutionStack: [
+							{
+								node: mock<INode>({ name: 'Execute Sub Workflow' }),
+								data: {
+									main: [
+										[
+											{
+												json: {
+													data: 'Input received before sub-workflow execution',
+												},
+												pairedItem: {
+													item: 0,
+												},
+											},
+										],
+									],
+								},
+								source: {
+									main: [
+										{
+											previousNode: 'Previous Node In Parent Workflow',
+										},
+									],
+								},
+							},
+						],
+						metadata: {},
+						waitingExecution: {},
+						waitingExecutionSource: {},
+					},
+					parentExecution: undefined,
+				},
+			};
+
+			execution.data.parentExecution = {
+				executionId: parentExecution.id,
+				workflowId: parentExecution.workflowData.id,
+			};
+
+			executionRepository.findSingleExecution
+				.calledWith(parentExecution.id)
+				.mockResolvedValue(parentExecution);
+			executionRepository.updateExistingExecution.mockResolvedValue();
+
+			const postExecutePromise = createDeferredPromise<IRun | undefined>();
+			activeExecutions.getPostExecutePromise
+				.calledWith(execution.id)
+				.mockReturnValue(postExecutePromise.promise);
+
+			await waitTracker.startExecution(execution.id);
+
+			expect(executionRepository.findSingleExecution).toHaveBeenNthCalledWith(1, execution.id, {
+				includeData: true,
+				unflattenData: true,
+			});
+
+			expect(workflowRunner.run).toHaveBeenCalledTimes(1);
+			expect(workflowRunner.run).toHaveBeenNthCalledWith(
+				1,
+				{
+					executionMode: execution.mode,
+					executionData: execution.data,
+					workflowData: execution.workflowData,
+					projectId: project.id,
+					pushRef: execution.data.pushRef,
+				},
+				false,
+				false,
+				execution.id,
+			);
+
+			const subworkflowResults: IRun = {
+				mode: 'manual',
+				startedAt: new Date(),
+				status: 'running',
+				data: {
+					resultData: {
+						runData: {
+							'Test sub-workflow node': [
+								{
+									hints: [],
+									startTime: new Date().getTime(),
+									executionTime: 0,
+									source: [
+										{
+											previousNode: 'Wait on Timer',
+										},
+									],
+									executionStatus: 'success',
+									data: {
+										main: [
+											[
+												{
+													json: {
+														data: 'Test sub-workflow result',
+													},
+													pairedItem: {
+														item: 0,
+													},
+												},
+											],
+										],
+									},
+								},
+							],
+						},
+						lastNodeExecuted: 'Test sub-workflow node',
+					},
+				},
+			};
+
+			postExecutePromise.resolve(subworkflowResults);
+			await jest.advanceTimersByTimeAsync(100);
+
+			expect(executionRepository.updateExistingExecution).toHaveBeenCalledTimes(1);
+			expect(executionRepository.updateExistingExecution).toHaveBeenNthCalledWith(
+				1,
+				parentExecution.id,
+				{
+					data: {
+						...parentExecution.data,
+						executionData: {
+							...parentExecution.data.executionData,
+							nodeExecutionStack: [
+								{
+									...parentExecution.data.executionData!.nodeExecutionStack[0],
+									data: {
+										main: [
+											[
+												{
+													json: {
+														data: 'Test sub-workflow result',
+													},
+													pairedItem: {
+														item: 0,
+													},
+												},
+											],
+										],
+									},
+								},
+							],
+						},
+					},
+				},
+			);
+
+			expect(executionRepository.findSingleExecution).toHaveBeenCalledTimes(3);
+			expect(executionRepository.findSingleExecution).toHaveBeenNthCalledWith(
+				2,
+				parentExecution.id,
+				{
+					includeData: true,
+					unflattenData: true,
+				},
+			);
+			expect(executionRepository.findSingleExecution).toHaveBeenNthCalledWith(
+				3,
+				parentExecution.id,
+				{
+					includeData: true,
+					unflattenData: true,
+				},
+			);
 
 			expect(workflowRunner.run).toHaveBeenCalledTimes(2);
 			expect(workflowRunner.run).toHaveBeenNthCalledWith(
