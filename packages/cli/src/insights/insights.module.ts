4cdcc8c46b767b7f20d2556ca0c7ee351f7345f5
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-13 12:14:44 +0100

    create insights module and register lifecycle hooks

---
+++ packages/cli/src/insights/insights.module.ts
@@ -0,0 +1,25 @@
+import type { ExecutionLifecycleHooks } from 'n8n-core';
+import { Logger } from 'n8n-core';
+
+import type { BaseN8nModule } from '@/decorators/module';
+import { N8nModule } from '@/decorators/module';
+
+import { InsightsService } from './insights.service';
+
+@N8nModule()
+export class InsightsModule implements BaseN8nModule {
+	constructor(
+		private readonly logger: Logger,
+		private readonly insightsService: InsightsService,
+	) {
+		this.logger = this.logger.scoped('insights');
+	}
+
+	registerLifecycleHooks(hooks: ExecutionLifecycleHooks) {
+		const insightsService = this.insightsService;
+
+		hooks.addHandler('workflowExecuteAfter', async function (fullRunData) {
+			await insightsService.workflowExecuteAfterHandler(this, fullRunData);
+		});
+	}
+}
