94f00e566d15efef3802d0ab2a7a049e6862983b
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-17 10:54:29 +0100

    Fix summary value displays according to expected api values (#13981)
    Co-authored-by: Csaba Tuncsik <csaba@n8n.io>

--- packages/frontend/editor-ui/src/features/insights/insights.utils.ts
+++ packages/frontend/editor-ui/src/features/insights/insights.utils.ts
@@ -1,16 +1,22 @@
-import type { InsightsSummary } from '@n8n/api-types';
+import type { InsightsSummary, InsightsSummaryType } from '@n8n/api-types';
 import type { InsightsSummaryDisplay } from '@/features/insights/insights.types';
 import {
 	INSIGHTS_SUMMARY_ORDER,
 	INSIGHTS_UNIT_MAPPING,
 } from '@/features/insights/insights.constants';
 
+const transformInsightsValues: Partial<Record<InsightsSummaryType, (value: number) => number>> = {
+	timeSaved: (value: number): number => value / 3600, // we want to show saved time in hours
+	averageRunTime: (value: number): number => Math.round((value / 1000) * 100) / 100, // we want to show average run time in seconds with 2 decimal places
+	failureRate: (value: number): number => value * 100, // we want to show failure rate in percentage
+};
+
 export const transformInsightsSummary = (data: InsightsSummary | null): InsightsSummaryDisplay =>
 	data
 		? INSIGHTS_SUMMARY_ORDER.map((key) => ({
 				id: key,
-				value: key === 'timeSaved' ? data[key].value / 3600 : data[key].value, // we want to show saved time in hours
-				deviation: data[key].deviation,
+				value: transformInsightsValues[key]?.(data[key].value) ?? data[key].value,
+				deviation: transformInsightsValues[key]?.(data[key].deviation) ?? data[key].deviation,
 				unit: INSIGHTS_UNIT_MAPPING[key],
 			}))
 		: [];
