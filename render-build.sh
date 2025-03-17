3334c86f0f5bfecb0eb5fc6013ca61659b8fa8e6
norieuxui <163439236+norieuxui@users.noreply.github.com>
2025-03-17 14:46:14 +0700

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Cập nhật gói và cài đặt Chromium
+apt-get update && apt-get install -y \
+  chromium-browser \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libdrm2 \
+  libgbm1 \
+  libgtk-3-0 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils
+
+# Cài đặt Puppeteer
+npm install puppeteer
