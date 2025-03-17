128e2dfa0c05b049ec3ae2d8634a32fd209b5df5
Jon <jonathan.bennetts@gmail.com>
2025-03-17 14:40:58 +0000

    feat(SeaTable Node): Update node with new options (#11431)

---
+++ packages/nodes-base/nodes/SeaTable/v1/SeaTable.node.ts
@@ -0,0 +1,41 @@
+import { NodeConnectionType, type INodeTypeDescription } from 'n8n-workflow';
+
+import { rowFields, rowOperations } from './RowDescription';
+
+export const versionDescription: INodeTypeDescription = {
+	displayName: 'SeaTable',
+	name: 'seaTable',
+	icon: 'file:seaTable.svg',
+	group: ['input'],
+	version: 1,
+	subtitle: '={{$parameter["resource"] + ": " + $parameter["operation"]}}',
+	description: 'Consume the SeaTable API',
+	defaults: {
+		name: 'SeaTable',
+	},
+	inputs: [NodeConnectionType.Main],
+	outputs: [NodeConnectionType.Main],
+	credentials: [
+		{
+			name: 'seaTableApi',
+			required: true,
+		},
+	],
+	properties: [
+		{
+			displayName: 'Resource',
+			name: 'resource',
+			type: 'options',
+			noDataExpression: true,
+			options: [
+				{
+					name: 'Row',
+					value: 'row',
+				},
+			],
+			default: 'row',
+		},
+		...rowOperations,
+		...rowFields,
+	],
+};
