0fc6697a89e06999e11637f65732514adf95603e
Alex Grozav <alex@grozav.com>
2025-03-18 14:15:08 +0200

    fix: remove compiled files

--- packages/@n8n/utils/src/random/randomInt.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function randomInt(max: number): number;
-export declare function randomInt(min: number, max: number): number;
--- packages/@n8n/utils/src/random/randomString.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function randomString(length: number): string;
-export declare function randomString(minLength: number, maxLength: number): string;
