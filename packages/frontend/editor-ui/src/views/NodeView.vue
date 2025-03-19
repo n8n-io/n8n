e55235840637c1b302804c43c3f66c1f2de946e2
Csaba Tuncsik <csaba@n8n.io>
2025-03-19 06:02:58 +0100

    fix(editor): Fix routing between workflow editing and new workflow pages (#14031)

--- packages/frontend/editor-ui/src/views/NodeView.vue
+++ packages/frontend/editor-ui/src/views/NodeView.vue
@@ -1612,8 +1612,10 @@ function showAddFirstStepIfEnabled() {
 watch(
 	() => route.name,
 	async (newRouteName, oldRouteName) => {
-		// it's navigating from and existing workflow to a new workflow
-		const force = newRouteName === VIEWS.NEW_WORKFLOW && oldRouteName === VIEWS.WORKFLOW;
+		// When navigating from an existing workflow to a new workflow or the other way around we should load the new workflow
+		const force =
+			(newRouteName === VIEWS.NEW_WORKFLOW && oldRouteName === VIEWS.WORKFLOW) ||
+			(newRouteName === VIEWS.WORKFLOW && oldRouteName === VIEWS.NEW_WORKFLOW);
 		await initializeRoute(force);
 	},
 );
