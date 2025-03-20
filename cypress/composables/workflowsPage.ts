78699e3da56a8123e0231406f160e7296c86ba6b
Charlie Kolb <charlie@n8n.io>
2025-03-20 12:07:44 +0100

    Add e2e test

--- cypress/composables/workflowsPage.ts
+++ cypress/composables/workflowsPage.ts
@@ -6,6 +6,8 @@ export function getWorkflowsPageUrl() {
 	return '/home/workflows';
 }
 
+export const getCreateWorkflowButton = () => cy.getByTestId('add-resource-workflow');
+
 /**
  * Actions
  */
--- cypress/e2e/44-routing.cy.ts
+++ cypress/e2e/44-routing.cy.ts
@@ -1,4 +1,14 @@
-import { getSaveChangesModal } from '../composables/modals/save-changes-modal';
+import {
+	getCancelSaveChangesButton,
+	getCloseSaveChangesButton,
+	getSaveChangesModal,
+} from '../composables/modals/save-changes-modal';
+import { getHomeButton } from '../composables/projects';
+import {
+	getCreateWorkflowButton,
+	getWorkflowsPageUrl,
+	visitWorkflowsPage,
+} from '../composables/workflowsPage';
 import { EDIT_FIELDS_SET_NODE_NAME } from '../constants';
 import { WorkflowPage as WorkflowPageClass } from '../pages/workflow';
 import { WorkflowsPage as WorkflowsPageClass } from '../pages/workflows';
@@ -8,7 +18,7 @@ const WorkflowPage = new WorkflowPageClass();
 
 describe('Workflows', () => {
 	beforeEach(() => {
-		cy.visit(WorkflowsPage.url);
+		visitWorkflowsPage();
 	});
 
 	it('should ask to save unsaved changes before leaving route', () => {
@@ -19,8 +29,27 @@ describe('Workflows', () => {
 
 		WorkflowPage.actions.addNodeToCanvas(EDIT_FIELDS_SET_NODE_NAME);
 
-		cy.getByTestId('project-home-menu-item').click();
+		getHomeButton().click();
 
 		getSaveChangesModal().should('be.visible');
+	});
+
+	it('should correct route after cancelling saveChangesModal', () => {
+		getCreateWorkflowButton().click();
+
+		cy.createFixtureWorkflow('Test_workflow_1.json', 'Empty State Card Workflow');
+		WorkflowPage.actions.addNodeToCanvas(EDIT_FIELDS_SET_NODE_NAME);
+
+		// Here we go back via browser rather than the home button
+		// As this already updates the route
+		cy.go(-1);
+
+		cy.url().should('include', getWorkflowsPageUrl());
+
+		getSaveChangesModal().should('be.visible');
+		getCloseSaveChangesButton().click();
+
+		// Confirm the url is back to the workflow
+		cy.url().should('include', '/workflow/');
 	});
 });
