8fd1fce778f29d90b5630f148152bcbcf18ba43d
Ricardo Espinoza <ricardo@n8n.io>
2025-03-20 12:10:38 -0400

    refactor(core): Remove env variable to enable folders (no-changelog) (#14076)

--- cypress/e2e/49-folders.cy.ts
+++ cypress/e2e/49-folders.cy.ts
@@ -51,6 +51,7 @@ describe('Folders', () => {
 	before(() => {
 		cy.resetDatabase();
 		cy.enableFeature('sharing');
+		cy.enableFeature('folders');
 		cy.enableFeature('advancedPermissions');
 		cy.enableFeature('projectRole:admin');
 		cy.enableFeature('projectRole:editor');
