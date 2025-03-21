d9bb707690ca6e3482aec6ebb5789ffff3338cc1
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-21 10:12:42 +0100

    remove utils function

--- packages/cli/src/modules/insights/entities/insights-by-period.ts
+++ packages/cli/src/modules/insights/entities/insights-by-period.ts
@@ -60,38 +60,4 @@ export class InsightsByPeriod extends BaseEntity {
 
 	@Column({ type: datetimeColumnType })
 	periodStart: Date;
-
-	static fromRaw(value: unknown) {
-		const schema = z.object({
-			id: z.number(),
-			metaId: z.number(),
-			type: z.number(),
-			//.refine((v) => {
-			//	if (isValidType(v, NumberToType)) {
-			//		return v;
-			//	} else {
-			//		throw new Error();
-			//	}
-			//})
-			//.transform((v) => NumberToType[v]),
-			value: z.number(),
-			periodUnit: z.number(),
-			//.refine((v) => {
-			//	if (isValidType(v, NumberToPeriodUnit)) {
-			//		return v;
-			//	}
-			//}),
-			periodStart: z.number(),
-		});
-		const rawEvent = schema.parse(value);
-		const event = new InsightsByPeriod();
-		event.id = rawEvent.id;
-		event.metaId = rawEvent.metaId;
-		event.type = NumberToType[rawEvent.type as TypeUnitNumbers];
-		event.value = rawEvent.value;
-		event.periodUnit = NumberToPeriodUnit[rawEvent.periodUnit as PeriodUnitNumbers];
-		event.periodStart = new Date(rawEvent.periodStart * 1000);
-
-		return event;
-	}
 }
