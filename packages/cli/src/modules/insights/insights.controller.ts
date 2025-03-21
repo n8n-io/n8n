e486afec3f6d07e2218166ee4de23cf7bb6204c7
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-21 15:25:00 +0100

    feat(API): Only allow instance owners and admins to see overview (#14063)

--- packages/cli/src/modules/insights/insights.controller.ts
+++ packages/cli/src/modules/insights/insights.controller.ts
@@ -2,7 +2,7 @@
 
 import type { InsightsSummary } from '@n8n/api-types';
 
-import { Get, RestController } from '@/decorators';
+import { Get, GlobalScope, RestController } from '@/decorators';
 
 import { InsightsService } from './insights.service';
 
@@ -11,6 +11,7 @@ export class InsightsController {
 	constructor(private readonly insightsService: InsightsService) {}
 
 	@Get('/summary')
+	@GlobalScope('insights:list')
 	async getInsightsSummary(): Promise<InsightsSummary> {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 		return await this.insightsService.getInsightsSummary();
