8ec02cb221a7e02531c3a4b72ec9dc77484275cb
Csaba Tuncsik <csaba@n8n.io>
2025-03-21 20:22:28 +0100

    feat(editor): Insights summary banner (#13424)
    Co-authored-by: Guillaume Jacquart <jacquart.guillaume@gmail.com>

---
+++ packages/frontend/editor-ui/src/features/insights/components/InsightsSummary.test.ts
@@ -0,0 +1,56 @@
+import InsightsSummary from '@/features/insights/components/InsightsSummary.vue';
+import { createComponentRenderer } from '@/__tests__/render';
+import type { InsightsSummaryDisplay } from '@/features/insights/insights.types';
+
+const renderComponent = createComponentRenderer(InsightsSummary);
+
+describe('InsightsSummary', () => {
+	it('should render without error', () => {
+		expect(() =>
+			renderComponent({
+				props: {
+					summary: [],
+				},
+			}),
+		).not.toThrow();
+	});
+
+	test.each<InsightsSummaryDisplay[]>([
+		[[]],
+		[
+			[
+				{ id: 'total', value: 525, deviation: 85, unit: '' },
+				{ id: 'failed', value: 14, deviation: 3, unit: '' },
+				{ id: 'failureRate', value: 1.9, deviation: -0.8, unit: '%' },
+				{ id: 'timeSaved', value: 55.55555555555556, deviation: -5.164722222222222, unit: 'h' },
+				{ id: 'averageRunTime', value: 2.5, deviation: -0.5, unit: 's' },
+			],
+		],
+		[
+			[
+				{ id: 'total', value: 525, deviation: 85, unit: '' },
+				{ id: 'failed', value: 14, deviation: 3, unit: '' },
+				{ id: 'failureRate', value: 1.9, deviation: -0.8, unit: '%' },
+				{ id: 'timeSaved', value: 0, deviation: 0, unit: 'h' },
+				{ id: 'averageRunTime', value: 2.5, deviation: -0.5, unit: 's' },
+			],
+		],
+		[
+			[
+				{ id: 'total', value: 525, deviation: -2, unit: '' },
+				{ id: 'failed', value: 14, deviation: -3, unit: '' },
+				{ id: 'failureRate', value: 1.9, deviation: 0.8, unit: '%' },
+				{ id: 'timeSaved', value: 55.55555555555556, deviation: 0, unit: 'h' },
+				{ id: 'averageRunTime', value: 2.5, deviation: 0.5, unit: 's' },
+			],
+		],
+	])('should render the summary correctly', (summary) => {
+		const { html } = renderComponent({
+			props: {
+				summary,
+			},
+		});
+
+		expect(html()).toMatchSnapshot();
+	});
+});
