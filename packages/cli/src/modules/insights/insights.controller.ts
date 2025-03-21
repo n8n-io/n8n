11599b6aae1b4c279ec32e5d9aded7d0ca6668f1
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-21 13:51:17 +0100

    remove comments

--- packages/cli/src/modules/insights/insights.controller.ts
+++ packages/cli/src/modules/insights/insights.controller.ts
@@ -1,4 +1,3 @@
-// TODO: use type once rebased on master
 //import type { InsightsSummary } from '@n8n/api-types';
 
 import type { InsightsSummary } from '@n8n/api-types';
@@ -11,7 +10,6 @@ import { InsightsService } from './insights.service';
 export class InsightsController {
 	constructor(private readonly insightsService: InsightsService) {}
 
-	// TODO: api test for this
 	@Get('/summary')
 	async getInsightsSummary(): Promise<InsightsSummary> {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
