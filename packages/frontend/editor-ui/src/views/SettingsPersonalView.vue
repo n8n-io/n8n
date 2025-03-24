0c89fe01ee8392af461f75ece8365ac6961b4c4b
Alex Grozav <alex@grozav.com>
2025-03-24 17:20:40 +0200

    fix: add correct type for translations item

--- packages/frontend/editor-ui/src/views/SettingsPersonalView.vue
+++ packages/frontend/editor-ui/src/views/SettingsPersonalView.vue
@@ -16,6 +16,7 @@ import { useSettingsStore } from '@/stores/settings.store';
 import { createFormEventBus } from '@n8n/design-system/utils';
 import type { MfaModalEvents } from '@/event-bus/mfa';
 import { promptMfaCodeBus } from '@/event-bus/mfa';
+import type { BaseTextKey } from '@/plugins/i18n';
 
 type UserBasicDetailsForm = {
 	firstName: string;
@@ -36,7 +37,7 @@ const formInputs = ref<null | IFormInputs>(null);
 const formBus = createFormEventBus();
 const readyToSubmit = ref(false);
 const currentSelectedTheme = ref(useUIStore().theme);
-const themeOptions = ref<Array<{ name: ThemeOption; label: string }>>([
+const themeOptions = ref<Array<{ name: ThemeOption; label: BaseTextKey }>>([
 	{
 		name: 'system',
 		label: 'settings.personal.theme.systemDefault',
