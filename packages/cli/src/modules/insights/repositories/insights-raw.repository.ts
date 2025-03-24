19f5b036150ad768f937a670c71a547525c97024
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-03-24 09:59:32 +0100

    feat(API): Implement compaction logic for insights (#14062)
    Co-authored-by: Danny Martini <danny@n8n.io>

--- packages/cli/src/modules/insights/repositories/insights-raw.repository.ts
+++ packages/cli/src/modules/insights/repositories/insights-raw.repository.ts
@@ -8,4 +8,18 @@ export class InsightsRawRepository extends Repository<InsightsRaw> {
 	constructor(dataSource: DataSource) {
 		super(InsightsRaw, dataSource.manager);
 	}
+
+	getRawInsightsBatchQuery(compactionBatchSize: number) {
+		// Build the query to gather raw insights data for the batch
+		const batchQuery = this.createQueryBuilder()
+			.select(
+				['id', 'metaId', 'type', 'value'].map((fieldName) =>
+					this.manager.connection.driver.escape(fieldName),
+				),
+			)
+			.addSelect('timestamp', 'periodStart')
+			.orderBy('timestamp', 'ASC')
+			.limit(compactionBatchSize);
+		return batchQuery;
+	}
 }
