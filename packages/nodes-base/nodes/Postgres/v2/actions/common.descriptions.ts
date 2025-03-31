09f832884e8109a678ff62aad66935e0c6cb5ca9
Michael Kret <michael.k@radency.com>
2025-03-31 11:58:07 +0300

    batching insert warning

--- packages/nodes-base/nodes/Postgres/v2/actions/common.descriptions.ts
+++ packages/nodes-base/nodes/Postgres/v2/actions/common.descriptions.ts
@@ -52,7 +52,7 @@ export const optionsCollection: INodeProperties = {
 					description: 'A single query for all incoming items',
 				},
 				{
-					name: 'Independently',
+					name: 'Indpendent',
 					value: 'independently',
 					description: 'Execute one query per incoming item of the run',
 				},
