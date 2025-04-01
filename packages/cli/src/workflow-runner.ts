a3d15fc70eac9b8387909cdcf7dc6b404132df57
Nobi <nobi@apollo.local>
2025-04-01 17:55:31 +0700

    fix(cli): Fix shouldEnqueue in execution cli mode

--- packages/cli/src/workflow-runner.ts
+++ packages/cli/src/workflow-runner.ts
@@ -156,9 +156,10 @@ export class WorkflowRunner {
 
 		// @TODO: Reduce to true branch once feature is stable
 		const shouldEnqueue =
-			process.env.OFFLOAD_MANUAL_EXECUTIONS_TO_WORKERS === 'true'
+			data.executionMode !== 'cli' &&
+			(process.env.OFFLOAD_MANUAL_EXECUTIONS_TO_WORKERS === 'true'
 				? this.executionsMode === 'queue'
-				: this.executionsMode === 'queue' && data.executionMode !== 'manual';
+				: this.executionsMode === 'queue' && data.executionMode !== 'manual');
 
 		if (shouldEnqueue) {
 			await this.enqueueExecution(executionId, data, loadStaticData, realtime);
