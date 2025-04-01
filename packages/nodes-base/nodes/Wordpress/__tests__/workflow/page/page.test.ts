fd8bd31bb397c93bfc58b08109d23ac90904d35c
Jonathan Bennetts <jonathan.bennetts@gmail.com>
2025-04-01 15:19:47 +0100

    Fixes

--- packages/nodes-base/nodes/Wordpress/__tests__/workflow/page/page.test.ts
+++ packages/nodes-base/nodes/Wordpress/__tests__/workflow/page/page.test.ts
@@ -1,46 +1,35 @@
 import nock from 'nock';
 
-import {
-	setup,
-	equalityTest,
-	workflowToTests,
-	getWorkflowFilenames,
-} from '../../../../../test/nodes/Helpers';
+import { getWorkflowFilenames, testWorkflows } from '@test/nodes/Helpers';
+
 import { pageCreate, pageGet, pageGetMany, pageUpdate } from '../apiResponses';
 
 describe('Wordpress > Page Workflows', () => {
-	describe('Run workflow', () => {
-		const workflows = getWorkflowFilenames(__dirname);
-		const tests = workflowToTests(workflows);
+	const workflows = getWorkflowFilenames(__dirname);
 
-		beforeAll(() => {
-			const mock = nock('https://myblog.com');
-			mock
-				.post('/wp-json/wp/v2/pages', {
-					title: 'A new page',
-					content: 'Some content',
-					status: 'draft',
-					comment_status: 'closed',
-					ping_status: 'closed',
-					menu_order: 1,
-				})
-				.reply(200, pageCreate);
-			mock.get('/wp-json/wp/v2/pages/2').reply(200, pageGet);
-			mock.get('/wp-json/wp/v2/pages').query({ per_page: 1 }).reply(200, pageGetMany);
-			mock
-				.post('/wp-json/wp/v2/pages/2', {
-					id: 2,
-					title: 'New Title',
-					content: 'Updated Content',
-					slug: 'new-slug',
-				})
-				.reply(200, pageUpdate);
-		});
-
-		const nodeTypes = setup(tests);
-
-		for (const testData of tests) {
-			test(testData.description, async () => await equalityTest(testData, nodeTypes));
-		}
+	beforeAll(() => {
+		const mock = nock('https://myblog.com');
+		mock
+			.post('/wp-json/wp/v2/pages', {
+				title: 'A new page',
+				content: 'Some content',
+				status: 'draft',
+				comment_status: 'closed',
+				ping_status: 'closed',
+				menu_order: 1,
+			})
+			.reply(200, pageCreate);
+		mock.get('/wp-json/wp/v2/pages/2').reply(200, pageGet);
+		mock.get('/wp-json/wp/v2/pages').query({ per_page: 1 }).reply(200, pageGetMany);
+		mock
+			.post('/wp-json/wp/v2/pages/2', {
+				id: 2,
+				title: 'New Title',
+				content: 'Updated Content',
+				slug: 'new-slug',
+			})
+			.reply(200, pageUpdate);
 	});
+
+	testWorkflows(workflows);
 });
