39ca08780ca082e336bbc2c125d4c187fdab6e0b
feelgood-interface <feelgood.interface@gmail.com>
2025-04-07 10:52:06 +0200

    add tests

--- packages/nodes-base/nodes/Microsoft/SharePoint/test/methods/listSearch.test.ts
+++ packages/nodes-base/nodes/Microsoft/SharePoint/test/methods/listSearch.test.ts
@@ -1,5 +1,7 @@
 /* eslint-disable n8n-local-rules/no-plain-errors */
 /* eslint-disable n8n-nodes-base/node-param-display-name-miscased */
+import type { MockProxy } from 'jest-mock-extended';
+import { mock } from 'jest-mock-extended';
 import type { ILoadOptionsFunctions, INodeParameterResourceLocator } from 'n8n-workflow';
 
 import { FAKE_CREDENTIALS_DATA } from '@test/nodes/FakeCredentialsMap';
@@ -8,6 +10,24 @@ import { MicrosoftSharePoint } from '../../MicrosoftSharePoint.node';
 
 describe('Microsoft SharePoint Node', () => {
 	describe('List search', () => {
+		let loadOptionsFunctions: MockProxy<ILoadOptionsFunctions>;
+		let mockRequestWithAuthentication: jest.Mock;
+		let node: MicrosoftSharePoint;
+
+		beforeEach(() => {
+			loadOptionsFunctions = mock<ILoadOptionsFunctions>();
+			mockRequestWithAuthentication = jest.fn();
+			loadOptionsFunctions.helpers.httpRequestWithAuthentication = mockRequestWithAuthentication;
+			loadOptionsFunctions.getCredentials.mockResolvedValue(
+				FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api,
+			);
+			node = new MicrosoftSharePoint();
+		});
+
+		afterEach(() => {
+			jest.resetAllMocks();
+		});
+
 		it('should list search files', async () => {
 			const mockResponse = {
 				'@odata.nextLink':
@@ -21,7 +41,7 @@ describe('Microsoft SharePoint Node', () => {
 					{
 						'@odata.etag': '"{10F06786-5AAF-434A-8A4E-3ED44DE4A987},6"',
 						id: '01SPEVVYEGM7YBBL22JJBYUTR62RG6JKMH',
-						name: 'image1.jpg',
+						name: 'file2.jpg',
 						file: {
 							hashes: {
 								quickXorHash: 'uFgaWYgAVo55sg4DIR9BTMzlmH8=',
@@ -46,44 +66,65 @@ describe('Microsoft SharePoint Node', () => {
 					},
 				],
 			};
-			const mockRequestWithAuthentication = jest.fn().mockReturnValue(mockResponse);
-			const mockGetNodeParameter = jest.fn((parameterName, _fallbackValue, _options) => {
-				if (parameterName === 'site') {
-					return {
-						value: 'site1',
-					} as INodeParameterResourceLocator;
-				}
-				if (parameterName === 'folder') {
-					return {
-						value: 'folder1',
-					} as INodeParameterResourceLocator;
-				}
-				throw new Error('Unknown parameter');
-			});
-			const mockGetCredentials = jest.fn(async (type: string, _itemIndex?: number) => {
-				if (type === 'microsoftSharePointOAuth2Api') {
-					return FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api;
-				}
-				throw new Error('Unknown credentials');
-			});
-			const mockContext = {
-				getCredentials: mockGetCredentials,
-				getNodeParameter: mockGetNodeParameter,
-				helpers: {
-					requestWithAuthentication: mockRequestWithAuthentication,
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('folder');
+
+			const listSearchResult = await node.methods.listSearch.getFiles.call(
+				loadOptionsFunctions,
+				'file',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				qs: {
+					$filter: "name eq 'file'",
 				},
-			} as unknown as ILoadOptionsFunctions;
-			const node = new MicrosoftSharePoint();
-
-			const listSearchResult = await node.methods.listSearch.getFiles.call(mockContext);
-
+			});
 			expect(listSearchResult).toEqual({
 				results: [
 					{ name: 'file1.txt', value: '01SPEVVYHAKLCA556QZ5HIPV5XHD4GKMJ3' },
-					{ name: 'image1.jpg', value: '01SPEVVYEGM7YBBL22JJBYUTR62RG6JKMH' },
+					{ name: 'file2.jpg', value: '01SPEVVYEGM7YBBL22JJBYUTR62RG6JKMH' },
 				],
 				paginationToken:
 					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+		});
+
+		it('should list search files with pagination', async () => {
+			const mockResponse = {
+				value: [
+					{
+						'@odata.etag': '"{0EC452E0-D0F7-4ECF-87D7-B738F865313B},1"',
+						id: '01SPEVVYHAKLCA556QZ5HIPV5XHD4GKMJ3',
+						name: 'file1.txt',
+						file: {
+							hashes: {
+								quickXorHash: 'AAAAAAAAAAAAAAAAAAAAAAAAAAA=',
+							},
+							irmEffectivelyEnabled: false,
+							irmEnabled: false,
+							mimeType: 'text/plain',
+						},
+					},
+				],
+			};
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('folder');
+
+			const listSearchResult = await node.methods.listSearch.getFiles.call(
+				loadOptionsFunctions,
+				undefined,
+				'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				url: 'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+			expect(listSearchResult).toEqual({
+				results: [{ name: 'file1.txt', value: '01SPEVVYHAKLCA556QZ5HIPV5XHD4GKMJ3' }],
 			});
 		});
 
@@ -117,39 +158,70 @@ describe('Microsoft SharePoint Node', () => {
 					},
 				],
 			};
-			const mockRequestWithAuthentication = jest.fn().mockReturnValue(mockResponse);
-			const mockGetNodeParameter = jest.fn((parameterName, _fallbackValue, _options) => {
-				if (parameterName === 'site') {
-					return {
-						value: 'site1',
-					} as INodeParameterResourceLocator;
-				}
-				throw new Error('Unknown parameter');
-			});
-			const mockGetCredentials = jest.fn(async (type: string, _itemIndex?: number) => {
-				if (type === 'microsoftSharePointOAuth2Api') {
-					return FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api;
-				}
-				throw new Error('Unknown credentials');
-			});
-			const mockContext = {
-				getCredentials: mockGetCredentials,
-				getNodeParameter: mockGetNodeParameter,
-				helpers: {
-					requestWithAuthentication: mockRequestWithAuthentication,
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+
+			const listSearchResult = await node.methods.listSearch.getFolders.call(
+				loadOptionsFunctions,
+				'folder',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				qs: {
+					$filter: "name eq 'folder'",
 				},
-			} as unknown as ILoadOptionsFunctions;
-			const node = new MicrosoftSharePoint();
-
-			const listSearchResult = await node.methods.listSearch.getFolders.call(mockContext);
-
+			});
 			expect(listSearchResult).toEqual({
 				results: [
-					{ name: 'folder1', value: '01SPEVVYBKV2ZKHGJASRA2HC7MOGBMUMAA' },
-					{ name: 'folder2', value: '01SPEVVYDNI2NFECGXK5ELO4JE6RTVI2RK' },
+					{
+						name: 'folder1',
+						value: '01SPEVVYBKV2ZKHGJASRA2HC7MOGBMUMAA',
+					},
+					{
+						name: 'folder2',
+						value: '01SPEVVYDNI2NFECGXK5ELO4JE6RTVI2RK',
+					},
 				],
 				paginationToken:
 					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+		});
+
+		it('should list search folders with pagination', async () => {
+			const mockResponse = {
+				value: [
+					{
+						'@odata.etag': '"{A3B2AE2A-2099-4194-A38B-EC7182CA3000},1"',
+						id: '01SPEVVYBKV2ZKHGJASRA2HC7MOGBMUMAA',
+						name: 'folder1',
+						folder: {
+							childCount: 7,
+							view: {},
+						},
+					},
+				],
+			};
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+
+			const listSearchResult = await node.methods.listSearch.getFolders.call(
+				loadOptionsFunctions,
+				undefined,
+				'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				url: 'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/items?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+			expect(listSearchResult).toEqual({
+				results: [
+					{
+						name: 'folder1',
+						value: '01SPEVVYBKV2ZKHGJASRA2HC7MOGBMUMAA',
+					},
+				],
 			});
 		});
 
@@ -159,6 +231,15 @@ describe('Microsoft SharePoint Node', () => {
 					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/listItems?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
 				value: [
 					{
+						'@odata.etag': '"98af0386-66bd-4524-a653-80be05e0f14d,2"',
+						id: '2',
+						'fields@odata.navigationLink': 'fields',
+						fields: {
+							'@odata.etag': '"98af0386-66bd-4524-a653-80be05e0f14d,2"',
+							Title: 'Title 2',
+						},
+					},
+					{
 						'@odata.etag': '"0ea4148a-f8e5-4f2f-a815-2e2be693b164,4"',
 						id: '1',
 						'fields@odata.navigationLink': 'fields',
@@ -167,54 +248,73 @@ describe('Microsoft SharePoint Node', () => {
 							Title: 'Title 1',
 						},
 					},
+				],
+			};
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('list');
+
+			const listSearchResult = await node.methods.listSearch.getItems.call(
+				loadOptionsFunctions,
+				'Title',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				qs: {
+					$filter: "fields/Title eq 'Title'",
+				},
+			});
+			expect(listSearchResult).toEqual({
+				results: [
 					{
-						'@odata.etag': '"98af0386-66bd-4524-a653-80be05e0f14d,2"',
-						id: '2',
+						name: 'Title 1',
+						value: '1',
+					},
+					{
+						name: 'Title 2',
+						value: '2',
+					},
+				],
+				paginationToken:
+					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/listItems?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+		});
+
+		it('should list search items with pagination', async () => {
+			const mockResponse = {
+				value: [
+					{
+						'@odata.etag': '"0ea4148a-f8e5-4f2f-a815-2e2be693b164,4"',
+						id: '1',
 						'fields@odata.navigationLink': 'fields',
 						fields: {
-							'@odata.etag': '"98af0386-66bd-4524-a653-80be05e0f14d,2"',
+							'@odata.etag': '"0ea4148a-f8e5-4f2f-a815-2e2be693b164,4"',
 						},
 					},
 				],
 			};
-			const mockRequestWithAuthentication = jest.fn().mockReturnValue(mockResponse);
-			const mockGetNodeParameter = jest.fn((parameterName, _fallbackValue, _options) => {
-				if (parameterName === 'site') {
-					return {
-						value: 'site1',
-					} as INodeParameterResourceLocator;
-				}
-				if (parameterName === 'list') {
-					return {
-						value: 'list1',
-					} as INodeParameterResourceLocator;
-				}
-				throw new Error('Unknown parameter');
-			});
-			const mockGetCredentials = jest.fn(async (type: string, _itemIndex?: number) => {
-				if (type === 'microsoftSharePointOAuth2Api') {
-					return FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api;
-				}
-				throw new Error('Unknown credentials');
-			});
-			const mockContext = {
-				getCredentials: mockGetCredentials,
-				getNodeParameter: mockGetNodeParameter,
-				helpers: {
-					requestWithAuthentication: mockRequestWithAuthentication,
-				},
-			} as unknown as ILoadOptionsFunctions;
-			const node = new MicrosoftSharePoint();
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('list');
 
-			const listSearchResult = await node.methods.listSearch.getItems.call(mockContext);
+			const listSearchResult = await node.methods.listSearch.getItems.call(
+				loadOptionsFunctions,
+				undefined,
+				'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/listItems?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			);
 
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				url: 'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/listItems?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
 			expect(listSearchResult).toEqual({
 				results: [
-					{ name: '2', value: '2' },
-					{ name: 'Title 1', value: '1' },
+					{
+						name: '1',
+						value: '1',
+					},
 				],
-				paginationToken:
-					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/listItems?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
 			});
 		});
 
@@ -226,48 +326,75 @@ describe('Microsoft SharePoint Node', () => {
 					{
 						'@odata.etag': '"58a279af-1f06-4392-a5ed-2b37fa1d6c1d,5"',
 						id: '58a279af-1f06-4392-a5ed-2b37fa1d6c1d',
-						name: 'List 2',
+						displayName: 'List 2',
 					},
 					{
 						'@odata.etag': '"23af565a-da89-48f0-ae5f-2d4a7244b446,0"',
 						id: '23af565a-da89-48f0-ae5f-2d4a7244b446',
-						name: 'List 1',
+						displayName: 'List 1',
 					},
 				],
 			};
-			const mockRequestWithAuthentication = jest.fn().mockReturnValue(mockResponse);
-			const mockGetNodeParameter = jest.fn((parameterName, _fallbackValue, _options) => {
-				if (parameterName === 'site') {
-					return {
-						value: 'site1',
-					} as INodeParameterResourceLocator;
-				}
-				throw new Error('Unknown parameter');
-			});
-			const mockGetCredentials = jest.fn(async (type: string, _itemIndex?: number) => {
-				if (type === 'microsoftSharePointOAuth2Api') {
-					return FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api;
-				}
-				throw new Error('Unknown credentials');
-			});
-			const mockContext = {
-				getCredentials: mockGetCredentials,
-				getNodeParameter: mockGetNodeParameter,
-				helpers: {
-					requestWithAuthentication: mockRequestWithAuthentication,
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+
+			const listSearchResult = await node.methods.listSearch.getLists.call(
+				loadOptionsFunctions,
+				'List',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				qs: {
+					$filter: "displayName eq 'List'",
 				},
-			} as unknown as ILoadOptionsFunctions;
-			const node = new MicrosoftSharePoint();
-
-			const listSearchResult = await node.methods.listSearch.getLists.call(mockContext);
-
+			});
 			expect(listSearchResult).toEqual({
 				results: [
-					{ name: 'List 1', value: '23af565a-da89-48f0-ae5f-2d4a7244b446' },
-					{ name: 'List 2', value: '58a279af-1f06-4392-a5ed-2b37fa1d6c1d' },
+					{
+						name: 'List 1',
+						value: '23af565a-da89-48f0-ae5f-2d4a7244b446',
+					},
+					{
+						name: 'List 2',
+						value: '58a279af-1f06-4392-a5ed-2b37fa1d6c1d',
+					},
 				],
 				paginationToken:
 					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/lists?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+		});
+
+		it('should list search lists with pagination', async () => {
+			const mockResponse = {
+				value: [
+					{
+						'@odata.etag': '"23af565a-da89-48f0-ae5f-2d4a7244b446,0"',
+						id: '23af565a-da89-48f0-ae5f-2d4a7244b446',
+						displayName: 'List 1',
+					},
+				],
+			};
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+			loadOptionsFunctions.getNodeParameter.mockReturnValueOnce('site');
+
+			const listSearchResult = await node.methods.listSearch.getLists.call(
+				loadOptionsFunctions,
+				undefined,
+				'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/lists?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				url: 'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/lists?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+			expect(listSearchResult).toEqual({
+				results: [
+					{
+						name: 'List 1',
+						value: '23af565a-da89-48f0-ae5f-2d4a7244b446',
+					},
+				],
 			});
 		});
 
@@ -286,27 +413,19 @@ describe('Microsoft SharePoint Node', () => {
 					},
 				],
 			};
-			const mockRequestWithAuthentication = jest.fn().mockReturnValue(mockResponse);
-			const mockGetNodeParameter = jest.fn((_parameterName, _fallbackValue, _options) => {
-				throw new Error('Unknown parameter');
-			});
-			const mockGetCredentials = jest.fn(async (type: string, _itemIndex?: number) => {
-				if (type === 'microsoftSharePointOAuth2Api') {
-					return FAKE_CREDENTIALS_DATA.microsoftSharePointOAuth2Api;
-				}
-				throw new Error('Unknown credentials');
-			});
-			const mockContext = {
-				getCredentials: mockGetCredentials,
-				getNodeParameter: mockGetNodeParameter,
-				helpers: {
-					requestWithAuthentication: mockRequestWithAuthentication,
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+
+			const listSearchResult = await node.methods.listSearch.getSites.call(
+				loadOptionsFunctions,
+				'Site',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				qs: {
+					$search: 'Site',
 				},
-			} as unknown as ILoadOptionsFunctions;
-			const node = new MicrosoftSharePoint();
-
-			const listSearchResult = await node.methods.listSearch.getSites.call(mockContext);
-
+			});
 			expect(listSearchResult).toEqual({
 				results: [
 					{
@@ -322,6 +441,38 @@ describe('Microsoft SharePoint Node', () => {
 				],
 				paginationToken:
 					'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/sites?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+		});
+
+		it('should list search sites and pagination', async () => {
+			const mockResponse = {
+				value: [
+					{
+						id: 'mydomain.sharepoint.com,3286a459-bc45-4aab-8200-b9ba7edcca96,3abe66fd-ec23-469f-abc6-332e3a95bf75',
+						title: 'Site 1',
+					},
+				],
+			};
+			mockRequestWithAuthentication.mockReturnValue(mockResponse);
+
+			const listSearchResult = await node.methods.listSearch.getSites.call(
+				loadOptionsFunctions,
+				undefined,
+				'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/sites?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			);
+
+			expect(mockRequestWithAuthentication).toHaveBeenCalledTimes(1);
+			expect(mockRequestWithAuthentication.mock.calls[0][1]).toMatchObject({
+				url: 'https://mydomain.sharepoint.com/_api/v2.0/sites(%27mydomain.sharepoint.com,site1%27)/sites?%24skiptoken=aWQ9MjFFQkEzOUMtMkU3My00NzgwLUFBQzEtMTVDNzlDMTk4QjlB',
+			});
+			expect(listSearchResult).toEqual({
+				results: [
+					{
+						name: 'Site 1',
+						value:
+							'mydomain.sharepoint.com,3286a459-bc45-4aab-8200-b9ba7edcca96,3abe66fd-ec23-469f-abc6-332e3a95bf75',
+					},
+				],
 			});
 		});
 	});
