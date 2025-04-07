a30aabda8c6cac4720b80e6d8cf0da9e3715be97
Charlie Kolb <charlie@n8n.io>
2025-04-07 11:29:44 +0200

    propagate displayOptions

--- packages/nodes-base/nodes/HttpRequest/V3/Description.ts
+++ packages/nodes-base/nodes/HttpRequest/V3/Description.ts
@@ -1171,7 +1171,7 @@ export const mainProperties: INodeProperties[] = [
 	},
 	...optimizeResponseProperties.map((x) => ({
 		...x,
-		displayOptions: { show: { ...x.displayOptions?.show, '@tool': [true] } },
+		displayOptions: { ...x.displayOptions, show: { ...x.displayOptions?.show, '@tool': [true] } },
 	})),
 	{
 		displayName:
