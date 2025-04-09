a1362d3fb4da8f55c95683527d604e280a6690eb
Elias Meire <elias@meire.dev>
2025-04-09 12:56:17 +0200

    Add tests for ToolMcpClient

--- packages/@n8n/nodes-langchain/nodes/tools/ToolMcpClient/ToolMcpClient.node.test.ts
+++ packages/@n8n/nodes-langchain/nodes/tools/ToolMcpClient/ToolMcpClient.node.test.ts
@@ -1,8 +1,15 @@
 import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 import { mock } from 'jest-mock-extended';
-import type { ILoadOptionsFunctions, INode } from 'n8n-workflow';
+import {
+	NodeOperationError,
+	type ILoadOptionsFunctions,
+	type INode,
+	type ISupplyDataFunctions,
+} from 'n8n-workflow';
 
 import { getTools } from './loadOptions';
+import { ToolMcpClient } from './ToolMcpClient.node';
+import { McpToolkit } from './utils';
 
 jest.mock('@modelcontextprotocol/sdk/client/sse.js');
 jest.mock('@modelcontextprotocol/sdk/client/index.js');
@@ -10,13 +17,168 @@ jest.mock('@modelcontextprotocol/sdk/client/index.js');
 describe('ToolMcpClient', () => {
 	describe('loadOptions: getTools', () => {
 		it('should return a list of tools', async () => {
+			jest.spyOn(Client.prototype, 'connect').mockResolvedValue();
+			jest.spyOn(Client.prototype, 'listTools').mockResolvedValue({
+				tools: [
+					{
+						name: 'MyTool',
+						description: 'MyTool does something',
+						inputSchema: { type: 'object', properties: { input: { type: 'string' } } },
+					},
+				],
+			});
+
 			const result = await getTools.call(
 				mock<ILoadOptionsFunctions>({ getNode: jest.fn(() => mock<INode>({ typeVersion: 1 })) }),
 			);
 
-			jest.spyOn(Client.prototype, 'connect').mockResolvedValue();
+			expect(result).toEqual([
+				{
+					description: 'MyTool does something',
+					name: 'MyTool',
+					value: 'MyTool',
+				},
+			]);
+		});
 
-			expect(result).toEqual([]);
+		it('should handle errors', async () => {
+			jest.spyOn(Client.prototype, 'connect').mockRejectedValue(new Error('Fail!'));
+
+			const node = mock<INode>({ typeVersion: 1 });
+			await expect(
+				getTools.call(mock<ILoadOptionsFunctions>({ getNode: jest.fn(() => node) })),
+			).rejects.toEqual(new NodeOperationError(node, 'Could not connect to your MCP server'));
+		});
+	});
+
+	describe('supplyData', () => {
+		it('should return a valid toolkit with usable tools', async () => {
+			jest.spyOn(Client.prototype, 'connect').mockResolvedValue();
+			jest
+				.spyOn(Client.prototype, 'callTool')
+				.mockResolvedValue({ content: [{ type: 'text', text: 'result from tool' }] });
+			jest.spyOn(Client.prototype, 'listTools').mockResolvedValue({
+				tools: [
+					{
+						name: 'MyTool1',
+						description: 'MyTool1 does something',
+						inputSchema: { type: 'object', properties: { input: { type: 'string' } } },
+					},
+					{
+						name: 'MyTool2',
+						description: 'MyTool2 does something',
+						inputSchema: { type: 'object', properties: { input2: { type: 'string' } } },
+					},
+				],
+			});
+
+			const supplyDataResult = await new ToolMcpClient().supplyData.call(
+				mock<ISupplyDataFunctions>({
+					getNode: jest.fn(() => mock<INode>({ typeVersion: 1 })),
+					logger: { debug: jest.fn(), error: jest.fn() },
+					addInputData: jest.fn(() => ({ index: 0 })),
+				}),
+				0,
+			);
+
+			expect(supplyDataResult.closeFunction).toBeInstanceOf(Function);
+			expect(supplyDataResult.response).toBeInstanceOf(McpToolkit);
+
+			const tools = (supplyDataResult.response as McpToolkit).getTools();
+			expect(tools).toHaveLength(2);
+
+			const toolCallResult = await tools[0].invoke({ input: 'foo' });
+			expect(toolCallResult).toEqual([{ type: 'text', text: 'result from tool' }]);
+		});
+
+		it('should support selecting tools to expose', async () => {
+			jest.spyOn(Client.prototype, 'connect').mockResolvedValue();
+			jest.spyOn(Client.prototype, 'listTools').mockResolvedValue({
+				tools: [
+					{
+						name: 'MyTool1',
+						description: 'MyTool1 does something',
+						inputSchema: { type: 'object', properties: { input: { type: 'string' } } },
+					},
+					{
+						name: 'MyTool2',
+						description: 'MyTool2 does something',
+						inputSchema: { type: 'object', properties: { input2: { type: 'string' } } },
+					},
+				],
+			});
+
+			const supplyDataResult = await new ToolMcpClient().supplyData.call(
+				mock<ISupplyDataFunctions>({
+					getNode: jest.fn(() =>
+						mock<INode>({
+							typeVersion: 1,
+						}),
+					),
+					getNodeParameter: jest.fn((key, _index) => {
+						const parameters: Record<string, any> = {
+							include: 'selected',
+							includeTools: ['MyTool2'],
+						};
+						return parameters[key];
+					}),
+					logger: { debug: jest.fn(), error: jest.fn() },
+					addInputData: jest.fn(() => ({ index: 0 })),
+				}),
+				0,
+			);
+
+			expect(supplyDataResult.closeFunction).toBeInstanceOf(Function);
+			expect(supplyDataResult.response).toBeInstanceOf(McpToolkit);
+
+			const tools = (supplyDataResult.response as McpToolkit).getTools();
+			expect(tools).toHaveLength(1);
+			expect(tools[0].name).toBe('MyTool2');
+		});
+
+		it('should support selecting tools to exclude', async () => {
+			jest.spyOn(Client.prototype, 'connect').mockResolvedValue();
+			jest.spyOn(Client.prototype, 'listTools').mockResolvedValue({
+				tools: [
+					{
+						name: 'MyTool1',
+						description: 'MyTool1 does something',
+						inputSchema: { type: 'object', properties: { input: { type: 'string' } } },
+					},
+					{
+						name: 'MyTool2',
+						description: 'MyTool2 does something',
+						inputSchema: { type: 'object', properties: { input2: { type: 'string' } } },
+					},
+				],
+			});
+
+			const supplyDataResult = await new ToolMcpClient().supplyData.call(
+				mock<ISupplyDataFunctions>({
+					getNode: jest.fn(() =>
+						mock<INode>({
+							typeVersion: 1,
+						}),
+					),
+					getNodeParameter: jest.fn((key, _index) => {
+						const parameters: Record<string, any> = {
+							include: 'except',
+							excludeTools: ['MyTool2'],
+						};
+						return parameters[key];
+					}),
+					logger: { debug: jest.fn(), error: jest.fn() },
+					addInputData: jest.fn(() => ({ index: 0 })),
+				}),
+				0,
+			);
+
+			expect(supplyDataResult.closeFunction).toBeInstanceOf(Function);
+			expect(supplyDataResult.response).toBeInstanceOf(McpToolkit);
+
+			const tools = (supplyDataResult.response as McpToolkit).getTools();
+			expect(tools).toHaveLength(1);
+			expect(tools[0].name).toBe('MyTool1');
 		});
 	});
 });
