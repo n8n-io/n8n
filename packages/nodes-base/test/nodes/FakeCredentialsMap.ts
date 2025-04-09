b52f9f0f6cbd96005f6f81a522955a47379316e1
AdinaTotorean <64439268+adina-hub@users.noreply.github.com>
2025-04-09 17:05:33 +0300

    feat(Azure Cosmos DB Node): New node (#14156)
    Co-authored-by: feelgood-interface <feelgood.interface@gmail.com>

--- packages/nodes-base/test/nodes/FakeCredentialsMap.ts
+++ packages/nodes-base/test/nodes/FakeCredentialsMap.ts
@@ -107,6 +107,12 @@ BQIDAQAB
 		},
 		baseUrl: 'https://api.gong.io',
 	},
+	microsoftAzureCosmosDbSharedKeyApi: {
+		account: 'n8n-us-east-account',
+		key: 'I3rwpzP0XoFpNzJ7hRIUXjwgpD1qaVKi71NZBbk7oOHUXrbd80WAoIAAoRaT47W9hHO3b6us1yABACDbVdilag==',
+		database: 'database_1',
+		baseUrl: 'https://n8n-us-east-account.documents.azure.com/dbs/database_1',
+	},
 	microsoftEntraOAuth2Api: {
 		grantType: 'authorizationCode',
 		authUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
