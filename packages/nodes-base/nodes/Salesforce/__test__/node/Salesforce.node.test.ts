da20fca6f94d41ce242e87e06fcd92dc9046fc11
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-04-11 15:02:05 +0300

    feat: Workflow tests for tasks

--- packages/nodes-base/nodes/Salesforce/__test__/node/Salesforce.node.test.ts
+++ packages/nodes-base/nodes/Salesforce/__test__/node/Salesforce.node.test.ts
@@ -2,6 +2,9 @@ import nock from 'nock';
 
 import { testWorkflows } from '@test/nodes/Helpers';
 
+import taskDetails from './fixtures/task-details.json';
+import taskSummary from './fixtures/task-summary.json';
+import tasks from './fixtures/tasks.json';
 import userDeltails from './fixtures/user-details.json';
 import users from './fixtures/users.json';
 
@@ -25,5 +28,40 @@ describe('Salesforce Node', () => {
 		});
 
 		testWorkflows(['nodes/Salesforce/__test__/node/users.workflow.json']);
+
+		it('should make the correct network calls', () => {
+			salesforceNock.done();
+		});
+	});
+
+	describe('tasks', () => {
+		beforeAll(() => {
+			salesforceNock
+				.post('/sobjects/task', {
+					Status: 'In Progress',
+					Subject: 'Email',
+					Description: 'Sample description',
+				})
+				.reply(200, { id: 'id1', success: true, errors: [] })
+				.get('/query')
+				.query({
+					q: 'SELECT id,subject,status,priority FROM Task ',
+				})
+				.reply(200, { records: tasks })
+				.get('/sobjects/task/id1')
+				.reply(200, taskDetails)
+				.get('/sobjects/task')
+				.reply(200, taskSummary)
+				.patch('/sobjects/task/id1', { Description: 'New description' })
+				.reply(200, { success: true, errors: [] })
+				.delete('/sobjects/task/id1')
+				.reply(200, { success: true, errors: [] });
+		});
+
+		testWorkflows(['nodes/Salesforce/__test__/node/tasks.workflow.json']);
+
+		it('should make the correct network calls', () => {
+			salesforceNock.done();
+		});
 	});
 });
