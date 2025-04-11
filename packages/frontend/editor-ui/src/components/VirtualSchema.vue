7d3d2ce590d4f857acc5b5a08b4800713ed031d3
Milorad FIlipović <milorad@n8n.io>
2025-04-11 11:17:44 +0200

    refactor(editor): Decouple `Draggable` from NDV store (#14531)

--- packages/frontend/editor-ui/src/components/VirtualSchema.vue
+++ packages/frontend/editor-ui/src/components/VirtualSchema.vue
@@ -86,6 +86,7 @@ const schemaPreviewStore = useSchemaPreviewStore();
 const environmentsStore = useEnvironmentsStore();
 const settingsStore = useSettingsStore();
 const posthogStore = usePostHog();
+
 const { getSchemaForExecutionData, getSchemaForJsonSchema, getSchema, filterSchema } =
 	useDataSchema();
 const { closedNodes, flattenSchema, flattenMultipleSchemas, toggleLeaf, toggleNode } =
@@ -97,6 +98,9 @@ const emit = defineEmits<{
 }>();
 
 const scroller = ref<RecycleScrollerInstance>();
+
+const canDraggableDrop = computed(() => ndvStore.canDraggableDrop);
+const draggableStickyPosition = computed(() => ndvStore.draggableStickyPos);
 
 const toggleNodeAndScrollTop = (id: string) => {
 	toggleNode(id);
@@ -333,11 +337,17 @@ watch(
 	{ once: true, immediate: true },
 );
 
-const onDragStart = () => {
+const onDragStart = (el: HTMLElement, data?: string) => {
+	ndvStore.draggableStartDragging({
+		type: 'mapping',
+		data: data ?? '',
+		dimensions: el?.getBoundingClientRect() ?? null,
+	});
 	ndvStore.resetMappingTelemetry();
 };
 
 const onDragEnd = (el: HTMLElement) => {
+	ndvStore.draggableStopDragging();
 	setTimeout(() => {
 		const mappingTelemetry = ndvStore.mappingTelemetry;
 		const parentNode = nodesSchemas.value.find(({ node }) => node.name === el.dataset.nodeName);
@@ -387,6 +397,8 @@ const onDragEnd = (el: HTMLElement) => {
 			type="mapping"
 			target-data-key="mappable"
 			:disabled="!mappingEnabled"
+			:can-drop="canDraggableDrop"
+			:sticky-position="draggableStickyPosition"
 			@dragstart="onDragStart"
 			@dragend="onDragEnd"
 		>
