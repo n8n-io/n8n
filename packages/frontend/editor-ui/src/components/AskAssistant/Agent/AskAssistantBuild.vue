3d0fe4a108e91dd764f2731a349272b1e2ecb9ab
Oleg Ivaniv <me@olegivaniv.com>
2025-04-16 09:17:08 +0200

    Fix-up stickies position

--- packages/frontend/editor-ui/src/components/AskAssistant/Agent/AskAssistantBuild.vue
+++ packages/frontend/editor-ui/src/components/AskAssistant/Agent/AskAssistantBuild.vue
@@ -10,6 +10,7 @@ import type { IWorkflowDataUpdate } from '@/Interface';
 import { nodeViewEventBus } from '@/event-bus';
 import { v4 as uuid } from 'uuid';
 import { useI18n } from '@/composables/useI18n';
+import { STICKY_NODE_TYPE } from '@/constants';
 
 const builderStore = useBuilderStore();
 const usersStore = useUsersStore();
@@ -54,6 +55,44 @@ function onClose() {
 	telemetry.track('User closed assistant', { source: 'top-toggle' });
 }
 
+function fixWorkflowStickiesPosition(workflowData: IWorkflowDataUpdate): IWorkflowDataUpdate {
+	const STICKY_WIDTH = 480;
+	const HEADERS_HEIGHT = 40;
+	const NEW_LINE_HEIGHT = 20;
+	const CHARACTER_WIDTH = 65;
+	const NODE_WIDTH = 100;
+	const stickyNodes = workflowData.nodes?.filter((node) => node.type === STICKY_NODE_TYPE);
+	const nonStickyNodes = workflowData.nodes?.filter((node) => node.type !== STICKY_NODE_TYPE);
+
+	const fixedStickies = stickyNodes?.map((node, index) => {
+		const content = node.parameters.content?.toString() ?? '';
+		const newLines = content.match(/\n/g) ?? [];
+		// Match any markdown heading from # to ###### at the start of a line
+		const headings = content.match(/^#{1,6} /gm) ?? [];
+		const headingHeight = headings.length * HEADERS_HEIGHT;
+		const newLinesHeight = newLines.length * NEW_LINE_HEIGHT;
+		const contentHeight = (content.length / CHARACTER_WIDTH) * NEW_LINE_HEIGHT;
+		const height = Math.ceil(headingHeight + newLinesHeight + contentHeight) + NEW_LINE_HEIGHT;
+
+		const firstNode = nonStickyNodes?.[0];
+		const xPos = (firstNode?.position[0] ?? 0) + index * (STICKY_WIDTH + NODE_WIDTH);
+		return {
+			...node,
+			parameters: {
+				...node.parameters,
+				height,
+				width: STICKY_WIDTH,
+			},
+			position: [xPos, -1 * (height + 50)] as [number, number],
+		};
+	});
+
+	return {
+		...workflowData,
+		nodes: [...(nonStickyNodes ?? []), ...(fixedStickies ?? [])],
+	};
+}
+
 function onInsertWorkflow(code: string) {
 	// TODO: Tracking
 	let workflowData: IWorkflowDataUpdate;
@@ -63,7 +102,10 @@ function onInsertWorkflow(code: string) {
 		console.error('Error parsing workflow data', error);
 		return;
 	}
-	nodeViewEventBus.emit('importWorkflowData', { data: workflowData, tidyUp: true });
+	nodeViewEventBus.emit('importWorkflowData', {
+		data: fixWorkflowStickiesPosition(workflowData),
+		tidyUp: true,
+	});
 	workflowGenerated.value = true;
 	builderStore.addAssistantMessages(
 		[
