48d57ba114fdcb74e417247e72c0670d7c912a9f
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-04-17 09:52:59 +0200

    use common function to validate and parse datetime

--- packages/nodes-base/nodes/Wait/test/Wait.node.test.ts
+++ packages/nodes-base/nodes/Wait/test/Wait.node.test.ts
@@ -1,4 +1,9 @@
+import { DateTime } from 'luxon';
+import { NodeOperationError, type IExecuteFunctions } from 'n8n-workflow';
+
 import { testWorkflows, getWorkflowFilenames } from '@test/nodes/Helpers';
+
+import { Wait } from '../Wait.node';
 const workflows = getWorkflowFilenames(__dirname);
 
 describe('Execute Wait Node', () => {
@@ -13,6 +18,31 @@ describe('Execute Wait Node', () => {
 	afterAll(() => {
 		clearInterval(timer);
 		jest.useRealTimers();
+	});
+
+	test.each<{ value: Date | DateTime | string; isValid: boolean }>([
+		{ value: 'invalid_date', isValid: false },
+		{ value: DateTime.now().plus({ days: 1 }).toISO(), isValid: true },
+		{ value: DateTime.now().plus({ days: 1 }).toJSDate(), isValid: true },
+		{ value: DateTime.now().plus({ days: 1 }), isValid: true },
+	])('Test Wait Node with specificTime $value and isValid $isValid', async ({ value, isValid }) => {
+		const waitNode = new Wait();
+		const executeFunctionsMock: IExecuteFunctions = {
+			getNodeParameter: jest.fn().mockImplementation((paramName: string) => {
+				if (paramName === 'resume') return 'specificTime';
+				if (paramName === 'dateTime') return value;
+			}),
+			getTimezone: jest.fn().mockReturnValue('UTC'),
+			putExecutionToWait: jest.fn(),
+			getInputData: jest.fn(),
+			getNode: jest.fn(),
+		} as unknown as IExecuteFunctions;
+
+		if (isValid) {
+			await expect(waitNode.execute(executeFunctionsMock)).resolves.not.toThrow();
+		} else {
+			await expect(waitNode.execute(executeFunctionsMock)).rejects.toThrow(NodeOperationError);
+		}
 	});
 
 	testWorkflows(workflows);
