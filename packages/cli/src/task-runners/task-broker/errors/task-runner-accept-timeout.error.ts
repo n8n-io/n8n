45ebe3a7a0367b3cd6170832422940ae2f52a268
Iván Ovejero <ivov.src@gmail.com>
2025-04-17 15:32:38 +0200

    Add level

--- packages/cli/src/task-runners/task-broker/errors/task-runner-accept-timeout.error.ts
+++ packages/cli/src/task-runners/task-broker/errors/task-runner-accept-timeout.error.ts
@@ -2,6 +2,8 @@ import { OperationalError } from 'n8n-workflow';
 
 export class TaskRunnerAcceptTimeoutError extends OperationalError {
 	constructor(taskId: string, runnerId: string) {
-		super(`Runner (${runnerId}) took too long to acknowledge acceptance of task (${taskId})`);
+		super(`Runner (${runnerId}) took too long to acknowledge acceptance of task (${taskId})`, {
+			level: 'warning',
+		});
 	}
 }
