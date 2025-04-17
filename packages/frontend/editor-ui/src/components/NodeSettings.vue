053565c6d500324f93576038825de3fe17bcf1bc
Benjamin Schroth <benjamin@n8n.io>
2025-04-17 16:43:00 +0200

    Fix naming errors

--- packages/frontend/editor-ui/src/components/NodeSettings.vue
+++ packages/frontend/editor-ui/src/components/NodeSettings.vue
@@ -147,7 +147,7 @@ const isExecutable = computed(() => {
 		const inputNames = NodeHelpers.getConnectionTypes(inputs);
 
 		if (
-			!inputNames.includes(NodeConnectionType.Main) &&
+			!inputNames.includes(NodeConnectionTypes.Main) &&
 			!isAiToolNode.value &&
 			!isTriggerNode.value
 		) {
@@ -649,7 +649,7 @@ const populateHiddenIssuesSet = () => {
 
 const populateSettings = () => {
 	if (isExecutable.value && !isTriggerNode.value) {
-		setting.value.push(
+		nodeSettings.value.push(
 			...([
 				{
 					displayName: i18n.baseText('nodeSettings.alwaysOutputData.displayName'),
@@ -741,7 +741,7 @@ const populateSettings = () => {
 			] as INodeProperties[]),
 		);
 	}
-	setting.value.push(
+	nodeSettings.value.push(
 		...([
 			{
 				displayName: i18n.baseText('nodeSettings.notes.displayName'),
@@ -897,7 +897,7 @@ const setNodeValues = () => {
 		}
 
 		// Set default node settings
-		for (const nodeSetting of setting.value) {
+		for (const nodeSetting of nodeSettings.value) {
 			if (!foundNodeSettings.includes(nodeSetting.name)) {
 				// Set default value
 				nodeValues.value = {
