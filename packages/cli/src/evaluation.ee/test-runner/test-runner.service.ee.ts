967eadd0a81805fdf3024663c826b539ad454c48
Eugene Molodkin <eugene@n8n.io>
2025-04-22 12:59:37 +0200

    wip: refactor EvaluationMetrics

--- packages/cli/src/evaluation.ee/test-runner/test-runner.service.ee.ts
+++ packages/cli/src/evaluation.ee/test-runner/test-runner.service.ee.ts
@@ -230,7 +230,7 @@ export class TestRunnerService {
 			});
 
 			// Initialize object to collect the results of the evaluation workflow executions
-			const metrics = new EvaluationMetrics(testMetricNames);
+			const metrics = new EvaluationMetrics();
 
 			///
 			// 2. Run over all the test cases
