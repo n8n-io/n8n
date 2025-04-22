677dd771a92981fb988526cb8bd9272866a76eca
Iván Ovejero <ivov.src@gmail.com>
2025-04-22 14:58:49 +0200

    Fix registry controller tests

--- packages/cli/src/__tests__/controller.registry.test.ts
+++ packages/cli/src/__tests__/controller.registry.test.ts
@@ -3,28 +3,30 @@ jest.mock('@/constants', () => ({
 }));
 
 import type { GlobalConfig } from '@n8n/config';
+import { MetadataState } from '@n8n/decorators';
+import { Param } from '@n8n/decorators';
+import { Get, Licensed, RestController } from '@n8n/decorators';
+import { Container } from '@n8n/di';
 import express from 'express';
 import { mock } from 'jest-mock-extended';
 import { agent as testAgent } from 'supertest';
 
 import type { AuthService } from '@/auth/auth.service';
+import { ControllerRegistry } from '@/controller.registry';
 import type { License } from '@/license';
 import type { SuperAgentTest } from '@test-integration/types';
-
-import { Param } from '../decorators/args';
-
-import { ControllerRegistry, Get, Licensed, RestController } from '@/decorators';
 
 describe('ControllerRegistry', () => {
 	const license = mock<License>();
 	const authService = mock<AuthService>();
 	const globalConfig = mock<GlobalConfig>({ endpoints: { rest: 'rest' } });
+	const metadataState = Container.get(MetadataState);
 	let agent: SuperAgentTest;
 
 	beforeEach(() => {
 		jest.resetAllMocks();
 		const app = express();
-		new ControllerRegistry(license, authService, globalConfig).activate(app);
+		new ControllerRegistry(license, authService, globalConfig, metadataState).activate(app);
 		agent = testAgent(app);
 	});
 
