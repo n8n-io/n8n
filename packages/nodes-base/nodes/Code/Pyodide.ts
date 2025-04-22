7f1204a95e72febc67c5deb77ba4ab3a235d6c54
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2025-04-22 11:55:38 +0200

    xmlhttprequest polyfil

--- packages/nodes-base/nodes/Code/Pyodide.ts
+++ packages/nodes-base/nodes/Code/Pyodide.ts
@@ -7,12 +7,17 @@ let pyodideInstance: PyodideInterface | undefined;
 export async function LoadPyodide(packageCacheDir: string): Promise<PyodideInterface> {
 	if (pyodideInstance === undefined) {
 		const { loadPyodide } = await import('pyodide');
+		const { XMLHttpRequest } = await import('xmlhttprequest-ssl');
 		const indexURL = dirname(require.resolve('pyodide'));
 		const context = createContext({
 			loadPyodide,
 			indexURL,
 			packageCacheDir,
-			jsglobals: { Object },
+			jsglobals: {
+				Object,
+				console,
+				XMLHttpRequest,
+			},
 		});
 		pyodideInstance = (await runInContext(
 			'loadPyodide({ indexURL, packageCacheDir, jsglobals })',
