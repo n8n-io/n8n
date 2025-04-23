271024ded0d55aa97daaf52cb8051abee36ad474
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2025-04-23 16:56:13 +0200

    feat(core): Add InstanceRole auth support for binary-data object- storage backend (#14800)

--- packages/@n8n/config/test/config.test.ts
+++ packages/@n8n/config/test/config.test.ts
@@ -139,20 +139,6 @@ describe('GlobalConfig', () => {
 			endpoint: 'https://api.n8n.io/api/versions/',
 			infoUrl: 'https://docs.n8n.io/hosting/installation/updating/',
 		},
-		externalStorage: {
-			s3: {
-				host: '',
-				protocol: 'https',
-				bucket: {
-					name: '',
-					region: '',
-				},
-				credentials: {
-					accessKey: '',
-					accessSecret: '',
-				},
-			},
-		},
 		workflows: {
 			defaultName: 'My workflow',
 			callerPolicyDefaultOption: 'workflowsFromSameOwner',
