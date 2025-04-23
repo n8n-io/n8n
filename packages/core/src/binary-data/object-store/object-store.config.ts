1400e1200e4deedb732413d14aae3341c5bbe3f5
कारतोफ्फेलस्क्रिप्ट™ <netroy@users.noreply.github.com>
2025-04-23 16:56:13 +0200

    feat(core): Add InstanceRole auth support for binary-data object- storage backend (#14800)

--- packages/core/src/binary-data/object-store/object-store.config.ts
+++ packages/core/src/binary-data/object-store/object-store.config.ts
@@ -1,13 +1,12 @@
+import { Config, Env, Nested } from '@n8n/config';
 import { z } from 'zod';
-
-import { Config, Env, Nested } from '../decorators';
 
 const protocolSchema = z.enum(['http', 'https']);
 
 export type Protocol = z.infer<typeof protocolSchema>;
 
 @Config
-class S3BucketConfig {
+class ObjectStoreBucketConfig {
 	/** Name of the n8n bucket in S3-compatible external storage */
 	@Env('N8N_EXTERNAL_STORAGE_S3_BUCKET_NAME')
 	name: string = '';
@@ -18,7 +17,7 @@ class S3BucketConfig {
 }
 
 @Config
-class S3CredentialsConfig {
+class ObjectStoreCredentialsConfig {
 	/** Access key in S3-compatible external storage */
 	@Env('N8N_EXTERNAL_STORAGE_S3_ACCESS_KEY')
 	accessKey: string = '';
@@ -26,11 +25,22 @@ class S3CredentialsConfig {
 	/** Access secret in S3-compatible external storage */
 	@Env('N8N_EXTERNAL_STORAGE_S3_ACCESS_SECRET')
 	accessSecret: string = '';
+
+	/**
+	 * Use automatic credential detection to authenticate S3 calls for external storage
+	 * This will ignore accessKey/accessSecret and use the default credential provider chain
+	 * https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/setting-credentials-node.html#credchain
+	 */
+	@Env('N8N_EXTERNAL_STORAGE_S3_AUTH_AUTO_DETECT')
+	authAutoDetect: boolean = false;
 }
 
 @Config
-export class S3Config {
-	/** Host of the n8n bucket in S3-compatible external storage @example "s3.us-east-1.amazonaws.com" */
+export class ObjectStoreConfig {
+	/**
+	 * Host of the object-store bucket in S3-compatible external storage
+	 * @example "s3.us-east-1.amazonaws.com"
+	 **/
 	@Env('N8N_EXTERNAL_STORAGE_S3_HOST')
 	host: string = '';
 
@@ -38,14 +48,8 @@ export class S3Config {
 	protocol: Protocol = 'https';
 
 	@Nested
-	bucket: S3BucketConfig;
+	bucket: ObjectStoreBucketConfig = {} as ObjectStoreBucketConfig;
 
 	@Nested
-	credentials: S3CredentialsConfig;
-}
-
-@Config
-export class ExternalStorageConfig {
-	@Nested
-	s3: S3Config;
+	credentials: ObjectStoreCredentialsConfig = {} as ObjectStoreCredentialsConfig;
 }
