886836211ecb404680b57bd87711dd4166f6d922
Charlie Kolb <charlie@n8n.io>
2025-04-24 15:02:50 +0200

    Finish expression parsing

--- packages/workflow/src/NodeReferenceParserUtils.ts
+++ packages/workflow/src/NodeReferenceParserUtils.ts
@@ -1,6 +1,6 @@
 import { escapeRegExp } from 'lodash';
 
-import type { INode, INodeParameters } from './Interfaces';
+import type { INode, NodeParameterValueType } from './Interfaces';
 
 export function hasDotNotationBannedChar(nodeName: string) {
 	const DOT_NOTATION_BANNED_CHARS = /^(\d)|[\\ `!@#$%^&*()_+\-=[\]{};':"\\|,.<>?~]/g;
@@ -89,6 +89,10 @@ type ReferenceReplacementResult = {
 				replacementPrefix: "$('Start').first()",
 				replacementName: "def_ghi"
 			}
+1.5 Filter for:
+	- previous nodes before input node (which go into ExecuteWorkflow)
+	- inner nodes (no changes for other inner nodes, replace for previous nodes)
+	- following nodes after output nodes -> error unless it's only output node of selection
 2. Ensure all replacementNames are unique, modify by combining with nodeName if needed
 3. Create ExecuteWorkflow and Trigger with a variable for each replacementName, setting `toBeReplaced` as their expression
 4. Replace `toBeReplaced` with `${replacementPrefix}.${replacementName}`
@@ -100,25 +104,148 @@ type ReferenceReplacementResult = {
 */
 
 type ExtractWorkflowExpressionData = {
-	replacementOfOriginalExpression: string; // "$('abc').first().def.ghi";
-	originalExpression: string; // "$('abc').first().def.ghi['mno']";
-	previousNodeName: string; // 'abc';
+	nodeNameInExpression: string; // 'abc';
+	originalExpression: string; // "$('abc').first().def.ghi";
 	replacementPrefix: string; //  "$('Start').first()";
 	replacementName: string; // "def_ghi";
 };
 
-const ITEM_ACCESSORS = ['first()', 'item'];
+const DOT_REFERENCEABLE_JS_VARIABLE = /\w[\w\d_\$]*/;
+const INVALID_JS_DOT_REFERENCE = /[^.\w\d_\$]/;
 
+/**
+ *  We want to try and extract these if possible, so that we turn
+ * 	$('myNode').ITEM_ACCESSOR.DATA_ACCESSOR.a.b['c'] => [$('myNode').ITEM_ACCESSOR.DATA_ACCESSOR.a.b, a_b['c']]
+ */
+// These allow one of the DATA_ACCESSORS
+const ITEM_TO_DATA_ACCESSORS = [
+	/^first\(\)$/,
+	/^last\(\)$/,
+	/^all$/,
+	/^item$/,
+	/^itemMatching(\d+)$/, // We only support trivial itemMatching - this could in theory hold an entirely new expression
+];
+// These we can convert to an argument
+const ITEM_ACCESSORS = ['params', 'isExecuted'];
+// These we'll just need to carry over into the workflow, pasting the whole node
+const NODE_ACCESSORS_STARTS = ['itemMatching'];
+
+// $('nodeName'). is equivalent to $ for previous node
 const DATA_ACCESSORS = ['json', 'binary'];
+
+// Grow the selection as long as we follow a simple path structure:
+// - $('myName').a.b.c => from first dot to full expression
+// - $('myName').a.b['c'] => from first dot to b
+// - $('myName').itemMatching(0) => node itself only
+//
+// Note that we start after the . after the nodeName reference
+export function parseExtractWorkflowExpressionData(
+	expression: string,
+	startIndex: number,
+	nodeNameInExpression: string,
+): null | ExtractWorkflowExpressionData {
+	const exprStart = expression.slice(0, startIndex);
+	const exprWithoutNode = expression.slice(startIndex);
+	const parts = exprWithoutNode.split('.');
+
+	if (parts.length === 0) {
+		// If a node is referenced by name without any accessor we return a proxy that stringifies as an empty object
+		// But it can still be validly passed to other functions
+		// However when passed to a sub-workflow it "collapses" into a true empty object
+		// So lets just abort porting this and don't touch it
+		return null;
+	}
+	if (ITEM_TO_DATA_ACCESSORS.some((x) => parts[0].match(x))) {
+		if (parts.length === 1) {
+			// this is a weird, but valid case
+			return {
+				nodeNameInExpression,
+				originalExpression: `${exprStart}.${parts[0]}`, // $('abc').first()
+				replacementPrefix: `$('Start').${parts[0]}`, //  $('Start').first()
+				replacementName: `${nodeNameInExpression}_${parts[0].split('(')[0]}`, // nodeName_first
+			};
+		} else {
+			if (DATA_ACCESSORS.some((x) => parts[1] === x)) {
+				let partsIdx = 2;
+				for (; partsIdx < parts.length; ++partsIdx) {
+					if (!DOT_REFERENCEABLE_JS_VARIABLE.test(parts[partsIdx])) break;
+				}
+				return {
+					nodeNameInExpression,
+					originalExpression: `${exprStart}.${parts.slice(0, partsIdx + 1).join('.')}`, // $('abc').item.json.valid.until  and not ['x'] after
+					replacementPrefix: `$('Start').${parts[0]}.${parts[1]}`, // $('Start').item.json
+					replacementName: parts.slice(2, partsIdx).join('_'), // valid_until
+				};
+			} else {
+				// this case covers any normal ObjectExtensions functions called on the ITEM_TO_DATA_ACCESSORS
+				// e.g. $('nodeName').first().toJsonObject()
+				return {
+					nodeNameInExpression,
+					originalExpression: `${exprStart}.${parts[0]}`, // $('abc').first()
+					replacementPrefix: `$('Start').${parts[0]}`, //  $('Start').first()
+					replacementName: `${nodeNameInExpression}_${parts[0].split('(')[0]}`, // nodeName_first
+				};
+			}
+		}
+	}
+
+	const itemAccessorMatch = ITEM_ACCESSORS.flatMap((x) => (x === parts[0] ? x : []))[0];
+	if (itemAccessorMatch !== undefined) {
+		return {
+			nodeNameInExpression,
+			originalExpression: `${exprStart}.${parts[0]}`, // $('abc').isExecuted
+			replacementPrefix: `$('Start').${parts[0]}`, //  $('Start').isExecuted
+			replacementName: `${nodeNameInExpression}_${parts[0]}`, // nodeName_isExecuted
+		};
+	}
+
+	// If we end up here it means that:
+	// - we have a complex `itemMatching(<expr>)` case, or
+	// - the expression should be invalid, or
+	// - a new function was added that we're not aware of.
+	//
+	// In these cases let's just not touch it and keep it as is
+	return null;
+}
+
+// const EXPRESSION_CONTINUE_REGEX = /[ ,+/*\-\(\[]/;
+
+// function growExpression(expression: string) {
+// 	for (let i = 0; i < expression.length; ++i) {
+// 		if (expression[i])
+// 	}
+// }
 
 // Map from `statementToBeReplaced` -> ExtractWorkflowExpressionData
 export function impl_buildExtractWorkflowExpressionDataMap(
 	expression: string,
-	nodeRegexps: Array<readonly [AccessPattern, LazyRegExp]>,
+	nodeRegexps: Array<readonly [RegExp, LazyRegExp]>,
 	nodeNames: string[],
-	newName: string,
-): Map<string, ExtractWorkflowExpressionData> {
-	const result = new Map<string, ExtractWorkflowExpressionData>();
+): ExtractWorkflowExpressionData[] {
+	const result: ExtractWorkflowExpressionData[] = [];
+
+	for (const [pattern, regexp] of nodeRegexps) {
+		if (pattern.test(expression)) {
+			const res = expression.matchAll(regexp.get());
+			for (const match of res) {
+				const startIndex = match.index;
+				const endIndex = startIndex + match[0].length;
+				// this works because all access patterns define match groups left and right of the name
+				const nodeNameInExpression = match[0][1];
+
+				if (!nodeNames.includes(nodeNameInExpression)) continue;
+
+				const firstImpossibleIdx = INVALID_JS_DOT_REFERENCE.exec(expression)?.index;
+				const candidate = expression.slice(endIndex + 1, firstImpossibleIdx);
+				const data = parseExtractWorkflowExpressionData(
+					candidate,
+					endIndex + 1, // skip the dot
+					nodeNameInExpression,
+				);
+				if (data !== null) result.push({ ...data, nodeNameInExpression });
+			}
+		}
+	}
 
 	return result;
 }
@@ -138,19 +265,37 @@ class LazyRegExp {
 	}
 }
 
-function isSpecialExpression(key: string, value: unknown): value is string {
-	return key === 'jsCode' && typeof value === 'string';
+type ParameterMapping<T> = undefined | T[] | { [key: string]: T[] | ParameterMapping<T> };
+
+type ParameterExtractMapping = ParameterMapping<ExtractWorkflowExpressionData>;
+
+// Note that this returns a Record<number, X> for arrays, to allow sparse mapping
+function rec_applyParameterMapping(
+	parameterValue: NodeParameterValueType,
+	mapper: (s: string) => ExtractWorkflowExpressionData[],
+): [ParameterExtractMapping, ExtractWorkflowExpressionData[]] {
+	const result: ParameterExtractMapping = {};
+
+	if (typeof parameterValue !== 'object' || parameterValue === null) {
+		if (typeof parameterValue === 'string' && parameterValue.charAt(0) === '=') {
+			const mapping = mapper(parameterValue);
+			return [mapping, mapping];
+		}
+		return [undefined, []];
+	}
+
+	const allMappings = [];
+	for (const [key, value] of Object.entries(parameterValue)) {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		const [mapping, all] = rec_applyParameterMapping(value, mapper);
+		result[key] = mapping;
+		allMappings.push(...all);
+	}
+
+	return [result, allMappings];
 }
 
-type ParameterMapping = { [key: string]: ExtractWorkflowExpressionData | ParameterMapping };
-
-function rec_applyParameterMapping(parameters: INodeParameters): ParameterMapping {}
-
-export function buildExtractWorkflowExpressionDataMap(
-	nodes: INode[],
-	nodeNames: string[],
-	newName: string = 'Start',
-) {
+export function buildExtractWorkflowExpressionDataMap(nodes: INode[], nodeNames: string[]) {
 	// Compile all candidate regexp patterns
 	//
 	// This looks scary for large workflows, but RegExp should support >1 million characters and
@@ -160,20 +305,23 @@ export function buildExtractWorkflowExpressionDataMap(
 	// That approach also risks being computationally expensive relative to expression length, rather than node count
 	const namesRegexp = nodeNames.map(escapeRegExp).join('|');
 	const nodeRegexps = ACCESS_PATTERNS.map(
-		(pattern) => [pattern, new LazyRegExp(() => pattern.replacePattern(namesRegexp), 'g')] as const,
+		(pattern) =>
+			[
+				new RegExp(pattern.checkPattern),
+				new LazyRegExp(() => pattern.replacePattern(namesRegexp), 'g'),
+			] as const,
 	);
 
-	for (const node of nodes) {
-		for (const [key, value] of Object.entries(node.parameters)) {
-			if (!isSpecialExpression(key, value) || typeof value !== 'string' || value[0] !== '=')
-				continue;
+	// This map is used to change the actual expressions once resolved
+	const recMapByNode = new Map<string, ParameterExtractMapping>();
+	// This map is used to track all candidates for change, necessary for deduplication
+	const flatMapByNode = new Map<string, ExtractWorkflowExpressionData[]>();
 
-			const map = impl_buildExtractWorkflowExpressionDataMap(
-				value,
-				nodeRegexps,
-				nodeNames,
-				newName,
-			);
-		}
+	for (const node of nodes) {
+		const [parameterMapping, allMappings] = rec_applyParameterMapping(node.parameters, (s) =>
+			impl_buildExtractWorkflowExpressionDataMap(s, nodeRegexps, nodeNames),
+		);
+		recMapByNode.set(node.name, parameterMapping);
+		flatMapByNode.set(node.name, allMappings);
 	}
 }
