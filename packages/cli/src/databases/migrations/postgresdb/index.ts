1bb3ae81fb9d1ded11aef3106123b1f10cd505b1
Marc Littlemore <MarcL@users.noreply.github.com>
2025-04-29 11:32:47 +0100

    feat(core): Add production root executions (#14845)
    Co-authored-by: Guillaume Jacquart <jacquart.guillaume@gmail.com>
    Co-authored-by: Danny Martini <danny@n8n.io>

--- packages/cli/src/databases/migrations/postgresdb/index.ts
+++ packages/cli/src/databases/migrations/postgresdb/index.ts
@@ -84,6 +84,7 @@ import { CreateFolderTable1738709609940 } from '../common/1738709609940-CreateFo
 import { CreateAnalyticsTables1739549398681 } from '../common/1739549398681-CreateAnalyticsTables';
 import { RenameAnalyticsToInsights1741167584277 } from '../common/1741167584277-RenameAnalyticsToInsights';
 import { AddScopesColumnToApiKeys1742918400000 } from '../common/1742918400000-AddScopesColumnToApiKeys';
+import { AddWorkflowStatisticsRootCount1745587087521 } from '../common/1745587087521-AddWorkflowStatisticsRootCount';
 
 export const postgresMigrations: Migration[] = [
 	InitialMigration1587669153312,
@@ -170,4 +171,5 @@ export const postgresMigrations: Migration[] = [
 	UpdateParentFolderIdColumn1740445074052,
 	RenameAnalyticsToInsights1741167584277,
 	AddScopesColumnToApiKeys1742918400000,
+	AddWorkflowStatisticsRootCount1745587087521,
 ];
