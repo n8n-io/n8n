e40e11051b67ebb38267b0705774acad61dc66a7
Danny Martini <danny@n8n.io>
2025-04-29 11:08:16 +0200

    don't disable FK checks

--- packages/cli/src/databases/migrations/sqlite/1745587087521-AddWorkflowStatisticsRootCount.ts
+++
@@ -1,5 +0,0 @@
-import { AddWorkflowStatisticsRootCount1745587087521 as BaseMigration } from '../common/1745587087521-AddWorkflowStatisticsRootCount';
-
-export class AddWorkflowStatisticsRootCount1745587087521 extends BaseMigration {
-	transaction = false as const;
-}
