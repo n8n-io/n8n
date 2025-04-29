3220cf0c2c3711067198a48d3f991b7b4d8232da
Yiorgis Gozadinos <yiorgis@n8n.io>
2025-04-29 12:27:21 +0200

    Create common status map for both test run table and test case table
    (cherry picked from commit 796be0fde3bde9b72dbe7edb75949aa07f78095d)

---
+++ packages/frontend/editor-ui/src/components/Evaluations/shared/statusDictionary.ts
@@ -0,0 +1,34 @@
+import type { TestRunRecord } from '@/api/evaluation.ee';
+import type { IconColor } from '@n8n/design-system/types/icon';
+
+export const statusDictionary: Record<TestRunRecord['status'], { icon: string; color: IconColor }> =
+	{
+		new: {
+			icon: 'status-new',
+			color: 'foreground-xdark',
+		},
+		running: {
+			icon: 'spinner',
+			color: 'secondary',
+		},
+		completed: {
+			icon: 'status-completed',
+			color: 'success',
+		},
+		error: {
+			icon: 'status-error',
+			color: 'danger',
+		},
+		cancelled: {
+			icon: 'status-canceled',
+			color: 'foreground-xdark',
+		},
+		warning: {
+			icon: 'status-warning',
+			color: 'warning',
+		},
+		success: {
+			icon: 'status-completed',
+			color: 'success',
+		},
+	};
