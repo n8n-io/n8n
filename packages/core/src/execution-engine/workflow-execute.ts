779de92d43b6c033d0eb477fd2a91277078faba6
Mutasem Aldmour <mutasem@n8n.io>
2025-04-30 12:11:17 +0200

    fix for v1

--- packages/core/src/execution-engine/workflow-execute.ts
+++ packages/core/src/execution-engine/workflow-execute.ts
@@ -195,6 +195,9 @@ export class WorkflowExecute {
 		let incomingNodeConnections: INodeConnections | undefined;
 		let connection: IConnection;
 
+		// Increment currentExecutionIndex based on previous run
+		this.additionalData.currentNodeExecutionIndex = getNextExecutionIndex(runData) ?? 0;
+
 		this.status = 'running';
 
 		const runIndex = 0;
