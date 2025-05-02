d9634cb8e65e8c6e444c5f6bf82babbb98968d5e
Shireen Missi <94372015+ShireenMissi@users.noreply.github.com>
2025-05-02 20:52:32 +0100

    chore: Fix failing unit tests (no-changelog) (#15073)

--- packages/nodes-base/nodes/Google/GSuiteAdmin/test/group/update.test.ts
+++ packages/nodes-base/nodes/Google/GSuiteAdmin/test/group/update.test.ts
@@ -1,16 +1,7 @@
+import { NodeTestHarness } from '@nodes-testing/node-test-harness';
 import nock from 'nock';
 
-import { initBinaryDataService, testWorkflows, getWorkflowFilenames } from '@test/nodes/Helpers';
-
 describe('Google GSuiteAdmin Node - Update Group', () => {
-	const workflows = getWorkflowFilenames(__dirname).filter((filename) =>
-		filename.includes('update.workflow.json'),
-	);
-
-	beforeAll(async () => {
-		await initBinaryDataService();
-	});
-
 	beforeEach(() => {
 		nock.disableNetConnect();
 		nock('https://www.googleapis.com/admin')
@@ -31,5 +22,7 @@ describe('Google GSuiteAdmin Node - Update Group', () => {
 			});
 	});
 
-	testWorkflows(workflows);
+	new NodeTestHarness().setupTests({
+		workflowFiles: ['update.workflow.json'],
+	});
 });
