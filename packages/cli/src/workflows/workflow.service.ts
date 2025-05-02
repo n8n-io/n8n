c001ef372210bbf5701a07e5f2e42faf0d456968
Jaakko Husso <jaakko@n8n.io>
2025-05-02 15:33:57 +0300

    refactor(core): Better name for skipArchived variable at archive

--- packages/cli/src/workflows/workflow.service.ts
+++ packages/cli/src/workflows/workflow.service.ts
@@ -422,7 +422,7 @@ export class WorkflowService {
 	async archive(
 		user: User,
 		workflowId: string,
-		force = false,
+		skipArchived: boolean = false,
 	): Promise<WorkflowEntity | undefined> {
 		const workflow = await this.workflowFinderService.findWorkflowForUser(workflowId, user, [
 			'workflow:delete',
@@ -432,7 +432,11 @@ export class WorkflowService {
 			return;
 		}
 
-		if (workflow.isArchived && !force) {
+		if (workflow.isArchived) {
+			if (skipArchived) {
+				return workflow;
+			}
+
 			throw new BadRequestError('Workflow is already archived.');
 		}
 
