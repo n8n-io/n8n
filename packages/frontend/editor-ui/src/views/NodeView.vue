d81ad6935a0063990037102e820f8191c83f1357
Jaakko Husso <jaakko@n8n.io>
2025-05-06 14:24:56 +0300

    feat(editor): Prevent WF save callbacks if the WF is archived

--- packages/frontend/editor-ui/src/views/NodeView.vue
+++ packages/frontend/editor-ui/src/views/NodeView.vue
@@ -760,8 +760,9 @@ function onPinNodes(ids: string[], source: PinDataSource) {
 
 async function onSaveWorkflow() {
 	const workflowIsSaved = !uiStore.stateIsDirty;
+	const workflowIsArchived = workflowsStore.workflow.isArchived;
 
-	if (workflowIsSaved) {
+	if (workflowIsSaved || workflowIsArchived) {
 		return;
 	}
 	const saved = await workflowHelpers.saveCurrentWorkflow();
