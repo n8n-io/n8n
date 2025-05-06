12836cfa264cfee79e549da67381656408faaab9
Ricardo Espinoza <ricardo@n8n.io>
2025-05-06 10:48:16 -0400

    fix merge issue

--- packages/cli/src/workflows/workflow.service.ee.ts
+++ packages/cli/src/workflows/workflow.service.ee.ts
@@ -5,7 +5,7 @@ import type {
 	WorkflowWithSharingsAndCredentials,
 	WorkflowWithSharingsMetaDataAndCredentials,
 } from '@n8n/db';
-import { Folder, Project, SharedWorkflow, CredentialsRepository } from '@n8n/db';
+import { Folder, Project, SharedWorkflow, CredentialsRepository, FolderRepository } from '@n8n/db';
 import { Service } from '@n8n/di';
 // eslint-disable-next-line n8n-local-rules/misplaced-n8n-typeorm-import
 import { In, type EntityManager } from '@n8n/typeorm';
@@ -18,7 +18,6 @@ import { ActiveWorkflowManager } from '@/active-workflow-manager';
 import { CredentialsFinderService } from '@/credentials/credentials-finder.service';
 import { CredentialsService } from '@/credentials/credentials.service';
 import { EnterpriseCredentialsService } from '@/credentials/credentials.service.ee';
-import { FolderRepository } from '@/databases/repositories/folder.repository';
 import { SharedWorkflowRepository } from '@/databases/repositories/shared-workflow.repository';
 import { WorkflowRepository } from '@/databases/repositories/workflow.repository';
 import { BadRequestError } from '@/errors/response-errors/bad-request.error';
