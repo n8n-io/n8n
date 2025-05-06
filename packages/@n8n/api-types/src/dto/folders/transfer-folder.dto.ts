4979d3e5cbb899f2f489f4b3c1744ab3029d7829
Ricardo Espinoza <ricardo@n8n.io>
2025-05-05 21:27:01 -0400

    fix path

--- packages/@n8n/api-types/src/dto/folders/transfer-folder.dto.ts
+++ packages/@n8n/api-types/src/dto/folders/transfer-folder.dto.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { Z } from 'zod-class';
 
-import { folderIdSchema } from 'schemas/folder.schema';
+import { folderIdSchema } from '../../schemas/folder.schema';
 
 export class TransferFolderBodyDto extends Z.class({
 	destinationProjectId: z.string(),
