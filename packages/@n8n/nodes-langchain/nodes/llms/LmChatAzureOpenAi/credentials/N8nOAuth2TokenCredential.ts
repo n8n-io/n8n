20f54d20d37012b9d5d65b234e813a2cddae4be5
Oleg Ivaniv <me@olegivaniv.com>
2025-05-12 09:41:08 +0200

    WIP: Refresh token

--- packages/@n8n/nodes-langchain/nodes/llms/LmChatAzureOpenAi/credentials/N8nOAuth2TokenCredential.ts
+++ packages/@n8n/nodes-langchain/nodes/llms/LmChatAzureOpenAi/credentials/N8nOAuth2TokenCredential.ts
@@ -1,6 +1,13 @@
 import type { TokenCredential, AccessToken } from '@azure/identity';
 import type { INode } from 'n8n-workflow';
-import { NodeOperationError } from 'n8n-workflow';
+import { jsonParse, NodeOperationError } from 'n8n-workflow';
+import type {
+	ClientOAuth2Options,
+	ClientOAuth2RequestObject,
+	ClientOAuth2TokenData,
+	OAuth2CredentialData,
+} from '@n8n/client-oauth2';
+import { ClientOAuth2 } from '@n8n/client-oauth2';
 
 import type { AzureEntraCognitiveServicesOAuth2ApiCredential } from '../types';
 /**
@@ -20,6 +27,39 @@ export class N8nOAuth2TokenCredential implements TokenCredential {
 			if (!this.credential?.oauthTokenData?.access_token) {
 				throw new NodeOperationError(this.node, 'Failed to retrieve access token');
 			}
+			const expirationDate = new Date(this.credential.oauthTokenData.expires_on * 1000);
+			// Refresh token if it's 5 minutes before expiration
+			const beforeExpiration = Date.now() - 5 * 60 * 1000;
+			if (expirationDate.getTime() < beforeExpiration) {
+				console.log('Refreshing token before expiration');
+				const oAuthClient = new ClientOAuth2({
+					clientId: this.credential.clientId,
+					clientSecret: this.credential.clientSecret,
+					accessTokenUri: this.credential.accessTokenUrl,
+					scopes: this.credential.scope?.split(' '),
+					authentication: this.credential.authentication,
+					authorizationUri: this.credential.authUrl,
+					additionalBodyProperties: {
+						resource: 'https://cognitiveservices.azure.com/',
+					},
+				});
+
+				const token = await oAuthClient.credentials.getToken();
+				const data = token.data as ClientOAuth2TokenData & {
+					expires_on: number;
+					ext_expires_on: number;
+				};
+				const refreshToken = oAuthClient.createToken({
+					...data,
+				});
+
+				this.credential.oauthTokenData = data;
+				return {
+					token: data.access_token,
+					expiresOnTimestamp: data.expires_on,
+				};
+			}
+			const oAuthData = this.credential.oauthTokenData;
 
 			return {
 				token: this.credential.oauthTokenData.access_token,
