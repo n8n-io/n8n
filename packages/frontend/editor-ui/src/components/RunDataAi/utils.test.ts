b197c214f98b90e3a86dc584dd488d8b9c756829
autologie <suguru@n8n.io>
2025-05-13 15:58:41 +0200

    fix erroneous merge conflict resolution

--- packages/frontend/editor-ui/src/components/RunDataAi/utils.test.ts
+++ packages/frontend/editor-ui/src/components/RunDataAi/utils.test.ts
@@ -1320,7 +1320,7 @@ describe(createLogTree, () => {
 										executionIndex: 3,
 									}),
 									createTestTaskData({
-										startTime: Date.parse('2025-04-04T00:00:03.000Z'),
+										startTime: Date.parse('2025-04-04T00:00:02.000Z'),
 										executionIndex: 2,
 									}),
 								],
