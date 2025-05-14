2758a8c4adb0a5486d11533ab3333c7e9708ebd3
Iván Ovejero <ivov.src@gmail.com>
2025-05-14 12:32:21 +0200

    Elaborate on error msg

--- packages/cli/src/modules/errors/unknown-module.error.ts
+++ packages/cli/src/modules/errors/unknown-module.error.ts
@@ -2,6 +2,8 @@ import { UnexpectedError } from 'n8n-workflow';
 
 export class UnknownModuleError extends UnexpectedError {
 	constructor(moduleName: string) {
-		super(`Unknown module "${moduleName}"`);
+		super(
+			`Found unknown internal module "${moduleName}" in \`N8N_ENABLED_MODULES\` or \`N8N_DISABLED_MODULES\`. Please review your environment variables, or ensure your internal module is in the \`MODULE_NAMES\` constant.`,
+		);
 	}
 }
