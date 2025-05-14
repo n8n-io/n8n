e4773c8384c7ebe73a85b00e303449f7fe448ae8
Iván Ovejero <ivov.src@gmail.com>
2025-05-14 12:29:31 +0200

    Better phrasing

--- packages/cli/src/modules/errors/module-loading-mismatch.error.ts
+++ packages/cli/src/modules/errors/module-loading-mismatch.error.ts
@@ -2,10 +2,10 @@ import { UnexpectedError } from 'n8n-workflow';
 
 export class ModuleLoadingMismatchError extends UnexpectedError {
 	constructor(moduleNames: string[]) {
-		const modules = moduleNames.length > 1 ? 'modules' : 'a module';
+		const sentenceStart = moduleNames.length > 1 ? 'Modules are' : 'A module is';
 
 		super(
-			`Found ${modules} listed in both \`N8N_ENABLED_MODULES\` and \`N8N_DISABLED_MODULES\`: ${moduleNames.join(', ')}. Please review these environment variables, as a module cannot be both enabled and disabled.`,
+			`${sentenceStart} listed in both \`N8N_ENABLED_MODULES\` and \`N8N_DISABLED_MODULES\`: ${moduleNames.join(', ')}. Please review these environment variables, as a module cannot be both enabled and disabled.`,
 			{ level: 'fatal' },
 		);
 	}
