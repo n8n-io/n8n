f2fa9fab68b751b770f592faa8f79ac319abba93
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-05-15 15:50:21 +0200

    load insights module for webhook instance to listen to the workflowExecuteAfter lifecycle even and save insights data

--- packages/cli/src/modules/insights/insights.pre-init.ts
+++ packages/cli/src/modules/insights/insights.pre-init.ts
@@ -1,6 +1,6 @@
 import type { ModulePreInitContext } from '../modules.config';
 
 export const shouldLoadModule = (ctx: ModulePreInitContext) =>
-	// Only main instance(s) should collect insights
-	// Because main instances are informed of all finished workflow executions, whatever the mode
-	ctx.instance.instanceType === 'main';
+	// Only main and webhook instance(s) should collect insights
+	// Because main and webhooks instances are the ones informed of all finished workflow executions, whatever the mode
+	ctx.instance.instanceType === 'main' || ctx.instance.instanceType === 'webhook';
