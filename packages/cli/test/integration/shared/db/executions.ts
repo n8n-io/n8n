b0f01e210f7fe390e2580b19ef53337671fffcb3
Danny Martini <danny@n8n.io>
2025-05-15 15:43:22 +0200

    enable test again

--- packages/cli/test/integration/shared/db/executions.ts
+++ packages/cli/test/integration/shared/db/executions.ts
@@ -32,13 +32,23 @@ export async function createExecution(
 	>,
 	workflow: IWorkflowBase,
 ) {
-	const { data, finished, mode, startedAt, stoppedAt, waitTill, status, deletedAt, metadata } =
-		attributes;
+	const {
+		data,
+		finished,
+		mode,
+		startedAt,
+		stoppedAt,
+		waitTill,
+		status,
+		deletedAt,
+		metadata,
+		createdAt,
+	} = attributes;
 
 	const execution = await Container.get(ExecutionRepository).save({
 		finished: finished ?? true,
 		mode: mode ?? 'manual',
-		createdAt: new Date(),
+		createdAt: createdAt ?? new Date(),
 		startedAt: startedAt === undefined ? new Date() : startedAt,
 		...(workflow !== undefined && { workflowId: workflow.id }),
 		stoppedAt: stoppedAt ?? new Date(),
