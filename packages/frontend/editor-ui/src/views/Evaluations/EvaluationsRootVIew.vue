10c2ab62cec959e49489ac8e6f65d994dcfc60c8
Eugene Molodkin <eugene@n8n.io>
2025-05-16 11:20:58 +0200

    fix: init workflow on opening evaluations panel

--- packages/frontend/editor-ui/src/views/Evaluations/EvaluationsRootVIew.vue
+++ packages/frontend/editor-ui/src/views/Evaluations/EvaluationsRootVIew.vue
@@ -2,15 +2,23 @@
 import { useWorkflowsStore } from '@/stores/workflows.store';
 import { useUsageStore } from '@/stores/usage.store';
 import { useAsyncState } from '@vueuse/core';
-// import { useRouter } from 'vue-router';
+import { PLACEHOLDER_EMPTY_WORKFLOW_ID } from '@/constants';
+import { useCanvasOperations } from '@/composables/useCanvasOperations';
+import { useToast } from '@/composables/useToast';
+import { useI18n } from '@/composables/useI18n';
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
 	name: string;
 }>();
 
-// const router = useRouter();
 const workflowsStore = useWorkflowsStore();
 const usageStore = useUsageStore();
+const router = useRouter();
+const toast = useToast();
+const i18n = useI18n();
+
+const { initializeWorkspace } = useCanvasOperations({ router });
 
 const { isReady } = useAsyncState(async () => {
 	await usageStore.getLicenseInfo();
@@ -19,8 +27,17 @@ const { isReady } = useAsyncState(async () => {
 
 	if (isAlreadyInitialized) return;
 
-	const workflow = await workflowsStore.fetchWorkflow(workflowId);
-	workflowsStore.workflow = workflow;
+	if (workflowId && workflowId !== 'new') {
+		// Check if we are loading the Evaluation tab directly, without having loaded the workflow
+		if (workflowsStore.workflow.id === PLACEHOLDER_EMPTY_WORKFLOW_ID) {
+			try {
+				const data = await workflowsStore.fetchWorkflow(workflowId);
+				initializeWorkspace(data);
+			} catch (error) {
+				toast.showError(error, i18n.baseText('nodeView.showError.openWorkflow.title'));
+			}
+		}
+	}
 }, undefined);
 </script>
 
