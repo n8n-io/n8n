29f5668f72b551ad6b6d66107143cc1609338454
Suguru Inoue <suguru@n8n.io>
2025-05-20 15:23:38 +0200

    fix(editor): Don't mark node as dirty when NDV is opened (#15222)

--- packages/frontend/editor-ui/src/stores/workflows.store.test.ts
+++ packages/frontend/editor-ui/src/stores/workflows.store.test.ts
@@ -31,6 +31,7 @@ import * as apiUtils from '@/utils/apiUtils';
 import { useSettingsStore } from '@/stores/settings.store';
 import { useLocalStorage } from '@vueuse/core';
 import { ref } from 'vue';
+import { createTestNode } from '@/__tests__/mocks';
 
 vi.mock('@/stores/ndv.store', () => ({
 	useNDVStore: vi.fn(() => ({
@@ -1038,6 +1039,36 @@ describe('useWorkflowsStore', () => {
 				'POST',
 				`/workflows/${workflowId}/unarchive`,
 			);
+		});
+	});
+
+	describe('setNodeParameters', () => {
+		beforeEach(() => {
+			workflowsStore.setNodes([createTestNode({ name: 'a', parameters: { p: 1, q: true } })]);
+		});
+
+		it('should set node parameters', () => {
+			expect(workflowsStore.nodesByName.a.parameters).toEqual({ p: 1, q: true });
+
+			workflowsStore.setNodeParameters({ name: 'a', value: { q: false, r: 's' } });
+
+			expect(workflowsStore.nodesByName.a.parameters).toEqual({ q: false, r: 's' });
+		});
+
+		it('should set node parameters preserving existing ones if append=true', () => {
+			expect(workflowsStore.nodesByName.a.parameters).toEqual({ p: 1, q: true });
+
+			workflowsStore.setNodeParameters({ name: 'a', value: { q: false, r: 's' } }, true);
+
+			expect(workflowsStore.nodesByName.a.parameters).toEqual({ p: 1, q: false, r: 's' });
+		});
+
+		it('should not update last parameter update time if parameters are set to the same value', () => {
+			expect(workflowsStore.getParametersLastUpdate('a')).toEqual(undefined);
+
+			workflowsStore.setNodeParameters({ name: 'a', value: { p: 1, q: true } });
+
+			expect(workflowsStore.getParametersLastUpdate('a')).toEqual(undefined);
 		});
 	});
 
