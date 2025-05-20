7582e6d82200669a3e71ffbd29064cb71edc29c4
Danny Martini <danny@n8n.io>
2025-05-20 11:24:45 +0200

    add docstring to getter

--- packages/@n8n/config/src/configs/database.config.ts
+++ packages/@n8n/config/src/configs/database.config.ts
@@ -145,6 +145,11 @@ export class DatabaseConfig {
 	@Env('DB_TYPE', dbTypeSchema)
 	type: DbType = 'sqlite';
 
+	/**
+	 * Is true if the default sqlite data source of TypeORM is used, as opposed
+	 * to any other (e.g. postgres)
+	 * This also returns false if n8n's new pooled sqlite data source is used.
+	 */
 	get isLegacySqlite() {
 		return this.type === 'sqlite' && this.sqlite.poolSize === 0;
 	}
