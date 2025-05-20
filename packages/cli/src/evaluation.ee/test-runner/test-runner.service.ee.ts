8d1159563ff8ed04d04ee33e793a7f31f76e0f3f
Eugene Molodkin <eugene@n8n.io>
2025-05-20 14:43:27 +0200

    Add runAt, completedAt on test cases in test runner service
    (cherry-picked from commit 331580513cc8442bda43c35ed9adfd352cbda765)

--- packages/cli/src/evaluation.ee/test-runner/test-runner.service.ee.ts
+++ packages/cli/src/evaluation.ee/test-runner/test-runner.service.ee.ts
@@ -345,12 +345,12 @@ export class TestRunnerService {
 				}
 
 				this.logger.debug('Running test case');
+				const runAt = new Date();
 
 				try {
 					const testCaseMetadata = {
 						...testRunMetadata,
 					};
-
 					// Run the test case and wait for it to finish
 					const testCaseResult = await this.runTestCase(
 						workflow,
@@ -383,23 +383,26 @@ export class TestRunnerService {
 						});
 						continue;
 					}
+					const completedAt = new Date();
 
 					const { addedMetrics } = metrics.addResults(
 						this.extractEvaluationResult(testCaseExecution, workflow),
 					);
 
 					this.logger.debug('Test case metrics extracted', addedMetrics);
-
 					// Create a new test case execution in DB
 					await this.testCaseExecutionRepository.createTestCaseExecution({
 						executionId: testCaseExecutionId,
 						testRun: {
 							id: testRun.id,
 						},
+						runAt,
+						completedAt,
 						status: 'success',
 						metrics: addedMetrics,
 					});
 				} catch (e) {
+					const completedAt = new Date();
 					// FIXME: this is a temporary log
 					this.logger.error('Test case execution failed', {
 						workflowId,
@@ -413,6 +416,8 @@ export class TestRunnerService {
 							testRun: {
 								id: testRun.id,
 							},
+							runAt,
+							completedAt,
 							status: 'error',
 							errorCode: e.code,
 							errorDetails: e.extra as IDataObject,
@@ -422,6 +427,8 @@ export class TestRunnerService {
 							testRun: {
 								id: testRun.id,
 							},
+							runAt,
+							completedAt,
 							status: 'error',
 							errorCode: 'UNKNOWN_ERROR',
 						});
