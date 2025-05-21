ea8a331b2806c38d0954e0bb5b9ade82b93fe909
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-05-21 14:58:16 +0200

    export datastore types schema

--- packages/@n8n/api-types/src/schemas/datastore.schema.ts
+++ packages/@n8n/api-types/src/schemas/datastore.schema.ts
@@ -13,3 +13,20 @@ export const datastoreFieldNameSchema = z
 		'Only lowercase alphanumeric characters and dashes are allowed for field names',
 	);
 export const datastoreFieldTypeSchema = z.enum(['string', 'number', 'boolean', 'date']);
+
+export const datastoreFieldSchema = z.object({
+	name: datastoreFieldNameSchema,
+	type: datastoreFieldTypeSchema,
+	createdAt: z.string().datetime(),
+	updatedAt: z.string().datetime(),
+});
+
+export const datastoreSchema = z.object({
+	id: datastoreIdSchema,
+	name: datastoreNameSchema,
+	fields: z.array(datastoreFieldSchema),
+	createdAt: z.string().datetime(),
+	updatedAt: z.string().datetime(),
+});
+export type Datastore = z.infer<typeof datastoreSchema>;
+export type DatastoreField = z.infer<typeof datastoreFieldSchema>;
