54ca0fb8dc7fba7e7659a3b7269b0288c92ac355
Rob Squires <robtf9@icloud.com>
2025-05-23 15:10:11 +0100

    node title full width

--- packages/frontend/editor-ui/src/components/NodeTitle.vue
+++ packages/frontend/editor-ui/src/components/NodeTitle.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import NodeIcon from '@/components/NodeIcon.vue';
 import { N8nInlineTextEdit } from '@n8n/design-system';
+import { useElementSize } from '@vueuse/core';
 import type { INodeTypeDescription } from 'n8n-workflow';
+import { useTemplateRef } from 'vue';
 
 type Props = {
 	modelValue: string;
@@ -24,6 +26,9 @@ function onRename(value: string) {
 		emit('update:model-value', value.trim());
 	}
 }
+
+const wrapperRef = useTemplateRef('wrapperRef');
+const { width } = useElementSize(wrapperRef);
 </script>
 
 <template>
@@ -31,12 +36,14 @@ function onRename(value: string) {
 		<span :class="$style.iconWrapper">
 			<NodeIcon :node-type="nodeType" :size="18" />
 		</span>
-		<N8nInlineTextEdit
-			:max-width="1000"
-			:model-value="modelValue"
-			:read-only="readOnly"
-			@update:model-value="onRename"
-		/>
+		<div ref="wrapperRef" :class="$style.textWrapper">
+			<N8nInlineTextEdit
+				:max-width="width"
+				:model-value="modelValue"
+				:read-only="readOnly"
+				@update:model-value="onRename"
+			/>
+		</div>
 	</span>
 </template>
 
@@ -45,8 +52,14 @@ function onRename(value: string) {
 	font-weight: var(--font-weight-medium);
 	display: flex;
 	font-size: var(--font-size-m);
-	padding-right: var(--spacing-s);
+	margin-right: var(--spacing-s);
 	color: var(--color-text-dark);
+	width: 100%;
+}
+
+.textWrapper {
+	display: flex;
+	flex-grow: 1;
 }
 
 .iconWrapper {
