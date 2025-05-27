039dad1b92cbcdc77a76ba4358fb20cf161a0609
Shireen Missi <shireen@n8n.io>
2025-05-27 16:07:20 +0100

    fix Sort node field description

--- packages/nodes-base/nodes/Transform/Sort/Sort.node.ts
+++ packages/nodes-base/nodes/Transform/Sort/Sort.node.ts
@@ -49,7 +49,7 @@ export class Sort implements INodeType {
 					},
 				],
 				default: 'simple',
-				description: 'The fields of the input items to compare to see if they are the same',
+				description: 'The type of sorting to perform',
 			},
 			{
 				displayName: 'Fields To Sort By',
