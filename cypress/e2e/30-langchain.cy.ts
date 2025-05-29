0837932a77df6a645a6318c987e609f56569a07d
Eugene Molodkin <eugene@n8n.io>
2025-05-29 14:44:25 +0200

    fix: replace chained cypress getters with single selectors

--- cypress/e2e/30-langchain.cy.ts
+++ cypress/e2e/30-langchain.cy.ts
@@ -390,9 +390,6 @@ describe('Langchain Integration', () => {
 
 		workflowPage.actions.openNode('Character Text Splitter');
 
-		// Wait for the input panel to switch to Debugging mode
-		ndv.getInputPanelItemsCount().should('not.exist');
-
 		ndv.getOutputRunSelector().should('exist');
 		ndv.getInputRunSelector().should('exist');
 		ndv.getInputRunSelector().find('input').should('include.value', '3 of 3');
