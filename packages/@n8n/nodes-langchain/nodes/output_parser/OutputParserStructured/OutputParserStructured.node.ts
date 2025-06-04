e141ebf1a2dc96b08af9239a598310933a57a74f
Eugene Molodkin <eugene@n8n.io>
2025-06-04 18:46:29 +0200

    wip: change $ref syntax hint text

--- packages/@n8n/nodes-langchain/nodes/output_parser/OutputParserStructured/OutputParserStructured.node.ts
+++ packages/@n8n/nodes-langchain/nodes/output_parser/OutputParserStructured/OutputParserStructured.node.ts
@@ -162,7 +162,8 @@ export class OutputParserStructured implements INodeType {
 		],
 		hints: [
 			{
-				message: '$ref syntax in JSON schema is currently not supported',
+				message:
+					'Fields that use $refs might have the wrong type, since this syntax is not currently supported',
 				type: 'warning',
 				location: 'outputPane',
 				whenToDisplay: 'afterExecution',
