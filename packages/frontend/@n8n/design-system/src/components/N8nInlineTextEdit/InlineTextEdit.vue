fc05f548d7e190ddf8e326b22af1d7ac680870d4
r00gm <raul00gm@gmail.com>
2025-06-05 09:47:33 +0200

    feat: split text ellipsis

--- packages/frontend/@n8n/design-system/src/components/N8nInlineTextEdit/InlineTextEdit.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nInlineTextEdit/InlineTextEdit.vue
@@ -25,7 +25,7 @@ const editableRoot = useTemplateRef('editableRoot');
 
 function forceFocus() {
 	if (!props.readonly) {
-		editableRoot.value.edit();
+		editableRoot.value?.edit();
 	}
 }
 
@@ -34,12 +34,29 @@ function forceCancel() {
 }
 
 defineExpose({ forceFocus, forceCancel });
+
+function splitStringByEndLength(str: string, endLength: number): [string, string] {
+	if (str.length <= endLength) {
+		return [str, ''];
+	}
+	return [str.slice(0, str.length - endLength), str.slice(-endLength)];
+}
 </script>
 
 <template>
 	<EditableRoot v-bind="forwarded" ref="editableRoot" class="inline-text-edit">
 		<EditableArea class="editable-area">
-			<EditablePreview class="inline-rename-preview" data-test-id="inline-edit-preview" as="div" />
+			<EditablePreview class="inline-rename-preview" data-test-id="inline-edit-preview" as="div">
+				<template v-if="props.modelValue">
+					<div
+						v-for="(segment, index) in splitStringByEndLength(props.modelValue, 4)"
+						:key="index"
+						class="text-clamp"
+					>
+						{{ segment }}
+					</div>
+				</template>
+			</EditablePreview>
 			<EditableInput class="inline-rename-input" data-test-id="inline-edit-input" />
 		</EditableArea>
 	</EditableRoot>
@@ -53,7 +70,7 @@ defineExpose({ forceFocus, forceCancel });
 	border-radius: var(--border-radius-base);
 	border-color: var(--color-foreground-base);
 	padding: 4px;
-	max-width: 200px;
+	max-width: 350px;
 	min-width: 64px;
 	&:hover:not([data-focused]) {
 		border-color: var(--color-foreground-base);
@@ -81,8 +98,19 @@ defineExpose({ forceFocus, forceCancel });
 .inline-rename-preview {
 	white-space: pre;
 	user-select: none;
+	padding: 2px 0;
+	display: grid;
+	grid-template-columns: 1fr auto;
+	max-width: 100%;
+	&[hidden] {
+		display: none;
+	}
+}
+
+.text-clamp {
+	white-space: nowrap;
+	min-width: 0;
 	overflow: hidden;
 	text-overflow: ellipsis;
-	padding: 2px 0;
 }
 </style>
