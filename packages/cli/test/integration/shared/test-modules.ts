ba17b5795966d15cb55a54ad61bf3932342845b3
Iván Ovejero <ivov.src@gmail.com>
2025-06-12 14:50:57 +0200

    Fix module-loading helper

--- packages/cli/test/integration/shared/test-modules.ts
+++ packages/cli/test/integration/shared/test-modules.ts
@@ -4,7 +4,11 @@ import { ModuleRegistry } from '@/modules/module-registry';
 
 export async function load(moduleNames: string[]) {
 	for (const moduleName of moduleNames) {
-		await import(`../../../src/modules/${moduleName}/${moduleName}.module`);
+		try {
+			await import(`../../../src/modules/${moduleName}/${moduleName}.module`);
+		} catch {
+			await import(`../../../src/modules/${moduleName}.ee/${moduleName}.module`);
+		}
 	}
 
 	Container.get(ModuleRegistry).addEntities();
