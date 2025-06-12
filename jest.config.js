a898beb463b1f9fda3fc9838112946449605efd6
Andreas Fitzek <andreas.fitzek@n8n.io>
2025-06-12 09:34:52 +0200

    jest config refactor

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,12 @@ const esmDependencies = [
 	'openid-client',
 	'oauth4webapi',
 	'jose',
-	// Add other dependencies that need to be transformed here
+	// Add other ESM dependencies that need to be transformed here
 ];
+
+const esmDependenciesPattern = esmDependencies.join('|');
+const esmDependenciesRegex = `node_modules/(${esmDependenciesPattern})/.+\\.m?js$`;
+
 /** @type {import('jest').Config} */
 const config = {
 	verbose: true,
@@ -28,7 +32,7 @@ const config = {
 	testPathIgnorePatterns: ['/dist/', '/node_modules/'],
 	transform: {
 		'^.+\\.ts$': ['ts-jest', tsJestOptions],
-		'node_modules/(pdfjs-dist|openid-client|jose|oauth4webapi)/.+\\.m?js$': [
+		[esmDependenciesRegex]: [
 			'babel-jest',
 			{
 				presets: ['@babel/preset-env'],
@@ -36,10 +40,7 @@ const config = {
 			},
 		],
 	},
-	transformIgnorePatterns: [
-		// '/dist/',
-		'/node_modules/(?!pdfjs-dist|openid-client|jose|oauth4webapi)/',
-	],
+	transformIgnorePatterns: [`/node_modules/(?!${esmDependenciesPattern})/`],
 	// This resolve the path mappings from the tsconfig relative to each jest.config.js
 	moduleNameMapper: compilerOptions?.paths
 		? pathsToModuleNameMapper(compilerOptions.paths, {
