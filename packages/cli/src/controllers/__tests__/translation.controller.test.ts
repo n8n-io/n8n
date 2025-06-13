1348d992645f6230354daec47758e718652a1d4b
Iván Ovejero <ivov.src@gmail.com>
2025-06-13 13:06:08 +0200

    Remove unneded mock fron `TranslationController`

--- packages/cli/src/controllers/__tests__/translation.controller.test.ts
+++ packages/cli/src/controllers/__tests__/translation.controller.test.ts
@@ -1,7 +1,6 @@
 import type { GlobalConfig } from '@n8n/config';
 import { mock } from 'jest-mock-extended';
 
-import config from '@/config';
 import type { TranslationRequest } from '@/controllers/translation.controller';
 import {
 	TranslationController,
@@ -11,7 +10,6 @@ import type { CredentialTypes } from '@/credential-types';
 import { BadRequestError } from '@/errors/response-errors/bad-request.error';
 
 describe('TranslationController', () => {
-	const configGetSpy = jest.spyOn(config, 'getEnv');
 	const credentialTypes = mock<CredentialTypes>();
 	const controller = new TranslationController(
 		credentialTypes,
@@ -32,7 +30,6 @@ describe('TranslationController', () => {
 		it('should return translation json on valid credential types', async () => {
 			const credentialType = 'credential-type';
 			const req = mock<TranslationRequest.Credential>({ query: { credentialType } });
-			configGetSpy.mockReturnValue('de');
 			credentialTypes.recognizes.calledWith(credentialType).mockReturnValue(true);
 			const response = { translation: 'string' };
 			jest.mock(`${CREDENTIAL_TRANSLATIONS_DIR}/de/credential-type.json`, () => response, {
