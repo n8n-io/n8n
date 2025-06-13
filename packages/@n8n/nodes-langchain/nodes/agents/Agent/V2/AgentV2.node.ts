975223f857accb84022f4b0670e3fda39bbb90c4
Jaakko Husso <jaakko@n8n.io>
2025-06-13 16:23:33 +0300

    feat(editor): Open AI agent node's callout template directly

--- packages/@n8n/nodes-langchain/nodes/agents/Agent/V2/AgentV2.node.ts
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/V2/AgentV2.node.ts
@@ -119,7 +119,7 @@ export class AgentV2 implements INodeType {
 			properties: [
 				{
 					displayName:
-						'Tip: Get a feel for agents with our quick <a href="https://docs.n8n.io/advanced-ai/intro-tutorial/" target="_blank">tutorial</a> or see an <a href="/templates/1954" target="_blank">example</a> of how this node works',
+						'Tip: Get a feel for agents with our quick <a href="https://docs.n8n.io/advanced-ai/intro-tutorial/" target="_blank">tutorial</a> or see an <a href="/workflows/templates/1954" target="_blank">example</a> of how this node works',
 					name: 'aiAgentStarterCallout',
 					type: 'callout',
 					default: '',
