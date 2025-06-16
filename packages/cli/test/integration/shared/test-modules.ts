fa752ac6b83d5cfce55bd26166b51baf68720d61
Iván Ovejero <ivov.src@gmail.com>
2025-06-16 16:28:46 +0200

    Add license state when loading test modules

--- packages/cli/test/integration/shared/test-modules.ts
+++ packages/cli/test/integration/shared/test-modules.ts
@@ -1,8 +1,12 @@
+import { LicenseState } from '@n8n/backend-common';
 import { Container } from '@n8n/di';
+import { mock } from 'jest-mock-extended';
 
 import { ModuleRegistry } from '@/modules/module-registry';
 
 export async function load(moduleNames: string[]) {
+	Container.set(LicenseState, mock<LicenseState>());
+
 	for (const moduleName of moduleNames) {
 		try {
 			await import(`../../../src/modules/${moduleName}/${moduleName}.module`);
