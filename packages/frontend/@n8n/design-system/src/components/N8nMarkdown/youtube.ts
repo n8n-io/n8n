454c923c048b4cb21d44dcc5e8a1bfa3d35578be
Jaakko Husso <jaakko@n8n.io>
2025-06-18 16:59:56 +0300

    test(editor): Add tests for youtube embeds parsing from Markdown content

--- packages/frontend/@n8n/design-system/src/components/N8nMarkdown/youtube.ts
+++ packages/frontend/@n8n/design-system/src/components/N8nMarkdown/youtube.ts
@@ -50,15 +50,6 @@ export const markdownYoutubeEmbed = (md: MarkdownIt, options: YoutubeEmbedConfig
 	md.inline.ruler.before('link', 'youtube_embed', parser);
 	md.renderer.rules.youtube_embed = (tokens: Token[], idx: number): string => {
 		const { videoId } = tokens[idx].meta as { videoId: string };
-		return `<iframe
-			width="${opts.width}"
-			height="${opts.height}"
-			src="${youtubeUrl}${videoId}"
-			title="${md.utils.escapeHtml(opts.title)}"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			referrerpolicy="strict-origin-when-cross-origin"
-			allowfullscreen>
-		</iframe>`;
+		return `<iframe width="${opts.width}" height="${opts.height}" src="${youtubeUrl}${videoId}" title="${md.utils.escapeHtml(opts.title)}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 	};
 };
