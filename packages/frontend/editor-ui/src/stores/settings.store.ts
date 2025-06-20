686624ccc9b04a9231c9b44d811595cec9536d34
Alex Grozav <alex@grozav.com>
2025-06-20 15:39:47 +0300

    refactor(editor): Detangle `versions` store from `settings` store (no-changelog) (#16508)

--- packages/frontend/editor-ui/src/stores/settings.store.ts
+++ packages/frontend/editor-ui/src/stores/settings.store.ts
@@ -20,7 +20,6 @@ import { UserManagementAuthenticationMethod } from '@/Interface';
 import type { IDataObject, WorkflowSettings } from 'n8n-workflow';
 import { defineStore } from 'pinia';
 import { useRootStore } from '@n8n/stores/useRootStore';
-import { useVersionsStore } from './versions.store';
 import { makeRestApiRequest } from '@n8n/rest-api-client';
 import { useToast } from '@/composables/useToast';
 import { useI18n } from '@n8n/i18n';
@@ -255,7 +254,6 @@ export const useSettingsStore = defineStore(STORES.SETTINGS, () => {
 		rootStore.setN8nMetadata(fetchedSettings.n8nMetadata || {});
 		rootStore.setDefaultLocale(fetchedSettings.defaultLocale);
 		rootStore.setBinaryDataMode(fetchedSettings.binaryDataMode);
-		useVersionsStore().setVersionNotificationSettings(fetchedSettings.versionNotifications);
 
 		if (fetchedSettings.telemetry.enabled) {
 			void eventsApi.sessionStarted(rootStore.restApiContext);
