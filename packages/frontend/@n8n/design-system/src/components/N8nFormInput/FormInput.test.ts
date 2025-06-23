b2aac59d883337bfdbfd3d30af57532cfe59d62c
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2025-06-23 10:28:33 +0200

    test(editor): Add n8n form input test

--- packages/frontend/@n8n/design-system/src/components/N8nFormInput/FormInput.test.ts
+++ packages/frontend/@n8n/design-system/src/components/N8nFormInput/FormInput.test.ts
@@ -21,7 +21,7 @@ describe('N8nFormInput', () => {
 	});
 
 	it('should validate required', async () => {
-		const { getByRole, emitted } = renderComponent({
+		const { getByRole, emitted, rerender } = renderComponent({
 			props: {
 				modelValue: '',
 				label: 'Test Label',
@@ -32,14 +32,53 @@ describe('N8nFormInput', () => {
 		expect(emitted('validate')).toEqual([[false]]);
 
 		const input = getByRole('textbox');
+		expect(input).toHaveValue('');
+
 		await userEvent.type(input, 'test');
 		await userEvent.tab();
+		// Re-render to comply with controlled component behavior
+		await rerender({
+			modelValue: 'test',
+			label: 'Test Label',
+			required: true,
+		});
 
+		expect(input).toHaveValue('test');
 		expect(emitted('validate')).toEqual([[false], [true]]);
 
 		await userEvent.clear(input);
 		await userEvent.tab();
+		// Re-render to comply with controlled component behavior
+		await rerender({
+			modelValue: '',
+			label: 'Test Label',
+			required: true,
+		});
 
 		expect(emitted('validate')).toEqual([[false], [true], [false]]);
+	});
+
+	it('should emit error even with missing or empty error message', async () => {
+		const { emitted } = renderComponent({
+			props: {
+				modelValue: 'test',
+				label: 'Test Label',
+				required: true,
+				showValidationWarnings: true,
+				validators: {
+					MY_VALIDATION: {
+						validate: (value: string) => {
+							if (value === 'test') {
+								return { messageKey: 'key.noTestAllowed' };
+							}
+							return false;
+						},
+					},
+				},
+				validationRules: [{ name: 'MY_VALIDATION' }],
+			},
+		});
+
+		expect(emitted('validate')).toEqual([[false]]);
 	});
 });
