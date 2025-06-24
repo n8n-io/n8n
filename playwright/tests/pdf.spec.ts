8859365b3104eee115374bb6eb9cce47473a0667
Declan Carroll <declan@n8n.io>
2025-06-24 09:34:03 +0100

    fix: adding pdf test

--- playwright/tests/pdf.spec.ts
+++ playwright/tests/pdf.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '../fixtures/base';
 
 // Example of importing a workflow from a file
 test.describe('PDF Test', () => {
-	test.skip('Can read and write PDF files and extract text', async ({ n8n }) => {
+	test('Can read and write PDF files and extract text', async ({ n8n }) => {
 		await n8n.goHome();
 		await n8n.workflows.clickAddWorklowButton();
 		await n8n.workflows.importWorkflow('test_pdf_workflow.json', 'PDF Workflow');
