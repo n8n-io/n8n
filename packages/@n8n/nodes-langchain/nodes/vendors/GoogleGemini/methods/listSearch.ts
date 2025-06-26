70887b5f90b2383643b657ebcce21aedd226c302
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-06-26 23:26:25 +0300

    feat: Model search

--- packages/@n8n/nodes-langchain/nodes/vendors/GoogleGemini/methods/listSearch.ts
+++ packages/@n8n/nodes-langchain/nodes/vendors/GoogleGemini/methods/listSearch.ts
@@ -1,7 +1,44 @@
-import type { INodeListSearchResult } from 'n8n-workflow';
+import type { ILoadOptionsFunctions, INodeListSearchResult } from 'n8n-workflow';
 
-export async function modelSearch(): Promise<INodeListSearchResult> {
+import { apiRequest } from '../transport';
+
+async function baseModelSearch(
+	this: ILoadOptionsFunctions,
+	modelFilter: (model: string) => boolean,
+	filter?: string,
+): Promise<INodeListSearchResult> {
+	const response = (await apiRequest.call(this, 'GET', '/v1beta/models', {
+		qs: {
+			pageSize: 1000,
+		},
+	})) as {
+		models: Array<{ name: string }>;
+	};
+
+	let models = response.models.filter((model) => modelFilter(model.name));
+	if (filter) {
+		models = models.filter((model) => model.name.toLowerCase().includes(filter.toLowerCase()));
+	}
+
 	return {
-		results: [],
+		results: models.map((model) => ({ name: model.name, value: model.name })),
 	};
 }
+
+export async function modelSearch(
+	this: ILoadOptionsFunctions,
+	filter?: string,
+): Promise<INodeListSearchResult> {
+	return await baseModelSearch.call(
+		this,
+		// TODO: double check this
+		(model) =>
+			!model.includes('embedding') &&
+			!model.includes('image') &&
+			!model.includes('veo') &&
+			!model.includes('audio'),
+		filter,
+	);
+}
+
+// TODO: models for audio, image, video and document processing
