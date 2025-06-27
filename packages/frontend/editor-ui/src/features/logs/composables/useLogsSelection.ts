49f19a7235d5a8abdb58521ee33a19f107207125
autologie <suguru@n8n.io>
2025-06-27 11:31:44 +0200

    reset nodeNameToSelect on selection

--- packages/frontend/editor-ui/src/features/logs/composables/useLogsSelection.ts
+++ packages/frontend/editor-ui/src/features/logs/composables/useLogsSelection.ts
@@ -118,6 +118,7 @@ export function useLogsSelection(
 				return;
 			}
 
+			nodeNameToSelect.value = undefined;
 			manualLogEntrySelection.value = { type: 'selected', entry };
 
 			let parent = entry.parent;
