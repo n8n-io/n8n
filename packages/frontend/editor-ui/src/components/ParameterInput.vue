0ece140878177ff6852227865a9a1fa72b4cf864
Mutasem Aldmour <mutasem@n8n.io>
2025-07-01 18:04:05 +0200

    fix type

--- packages/frontend/editor-ui/src/components/ParameterInput.vue
+++ packages/frontend/editor-ui/src/components/ParameterInput.vue
@@ -1091,7 +1091,7 @@ onMounted(() => {
 
 	void externalHooks.run('parameterInput.mount', {
 		parameter: props.parameter,
-		inputFieldRef: inputField.value,
+		inputFieldRef: inputField.value as InstanceType<typeof N8nInput>,
 	});
 });
 
