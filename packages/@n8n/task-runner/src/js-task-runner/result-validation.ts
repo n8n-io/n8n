6c0d9fa581a4b98f21d75eff164a7b76ba109368
Iván Ovejero <ivov.src@gmail.com>
2025-07-01 16:01:06 +0200

    Handle item index better

--- packages/@n8n/task-runner/src/js-task-runner/result-validation.ts
+++ packages/@n8n/task-runner/src/js-task-runner/result-validation.ts
@@ -46,8 +46,9 @@ function validateTopLevelKeys(item: INodeExecutionData, itemIndex: number) {
 export class ReservedKeyFoundError extends ValidationError {
 	constructor(reservedKey: string, itemIndex: number) {
 		super({
-			message: `Invalid output format [item ${itemIndex}]`,
+			message: 'Invalid output format',
 			description: `An output item contains the reserved key <code>${reservedKey}</code>. To get around this, please wrap each item in an object, under a key called <code>json</code>. <a href="https://docs.n8n.io/data/data-structure/#data-structure" target="_blank">Example</a>`,
+			itemIndex,
 		});
 	}
 }
