b10d0f5dc3a138d45d7c6d172aa982252c91c71d
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-07-03 14:55:06 +0300

    Make logic easier to read

--- packages/cli/src/controllers/oauth/abstract-oauth.controller.ts
+++ packages/cli/src/controllers/oauth/abstract-oauth.controller.ts
@@ -36,7 +36,9 @@ const MAX_CSRF_AGE = 5 * Time.minutes.toMilliseconds;
 
 // TODO: Flip this flag in v2
 // https://linear.app/n8n/issue/CAT-329
-export const skipAuthOnOAuthCallback = process.env.N8N_SKIP_AUTH_ON_OAUTH_CALLBACK !== 'false';
+export const skipAuthOnOAuthCallback =
+	!process.env.N8N_SKIP_AUTH_ON_OAUTH_CALLBACK ||
+	process.env.N8N_SKIP_AUTH_ON_OAUTH_CALLBACK === 'true';
 
 @Service()
 export abstract class AbstractOAuthController {
