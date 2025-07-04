19265be48c3de4ab9dfe4e963fe455b48c054031
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-07-04 09:07:26 +0300

    chore: Improve test container dx
    - Document `N8N_DOCKER_IMAGE` in help command
    - Add custom pull policy that never tries to pull the local image

---
+++ packages/testing/containers/n8n-image-pull-policy.ts
@@ -0,0 +1,18 @@
+import { ImagePullPolicy, PullPolicy } from 'testcontainers';
+
+/**
+ * Custom pull policy for n8n images:
+ * - Never try to pull the local image
+ * - Otherwise, use the default pull policy (pull only if not present)
+ */
+export class N8nImagePullPolicy implements ImagePullPolicy {
+	constructor(private readonly image: string) {}
+
+	public shouldPull(): boolean {
+		if (this.image === 'n8nio/n8n:local') {
+			return false;
+		}
+
+		return PullPolicy.defaultPolicy().shouldPull();
+	}
+}
