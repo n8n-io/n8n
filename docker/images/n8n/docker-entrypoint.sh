be042744f7f3f072d8381e6504fbefdd371a7963
Tristan Robert <tristan.robert.44@gmail.com>
2025-07-07 14:22:37 +0200

    refactor: :recycle: refactor http proxy env vars set in docker config

--- docker/images/n8n/docker-entrypoint.sh
+++ docker/images/n8n/docker-entrypoint.sh
@@ -6,12 +6,12 @@ if [ -d /opt/custom-certificates ]; then
   c_rehash /opt/custom-certificates
 fi
 
-if [ -z "$HTTPS_PROXY" ]; then
-	echo "HTTPS_PROXY is unset or empty, no npm config proxy set"
+if [ -z "$HTTPS_PROXY" ] && [ -z "$HTTP_PROXY" ]; then
+	echo "HTTPS_PROXY and HTTP_PROXY are unset or empty, no npm config proxy set"
 else
 	echo "proxy=$HTTP_PROXY" >> /home/node/.npmrc
 	echo "https-proxy=$HTTPS_PROXY" >> /home/node/.npmrc
-	echo "noproxy=$NO_PROXY" >> /home/node/.npmrc
+	[ -z "$NO_PROXY" ] || echo "noproxy=$NO_PROXY" >> /home/node/.npmrc
 fi
 
 if [ "$#" -gt 0 ]; then
