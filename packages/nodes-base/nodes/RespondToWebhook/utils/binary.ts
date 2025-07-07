056639f121994d6c2ca4b4a5412d9a7c17d6548c
Dana Lee <dana@n8n.io>
2025-07-07 11:56:14 +0200

    refactor shouldSandbox logic

--- packages/nodes-base/nodes/RespondToWebhook/utils/binary.ts
+++ packages/nodes-base/nodes/RespondToWebhook/utils/binary.ts
@@ -16,22 +16,22 @@ export const getBinaryResponse = (binaryData: IBinaryData, headers: IDataObject)
 	const mimeTypesToSanitize = ['text/html', 'application/xhtml+xml'];
 
 	const contentType = headers['content-type'] as string;
-	const shouldSandboxContentType = contentType ? mimeTypesToSanitize.includes(contentType) : false;
+	const shouldSandboxResponseData =
+		mimeTypesToSanitize.includes(binaryData.mimeType) ||
+		(contentType ? mimeTypesToSanitize.includes(contentType) : false);
 
 	let responseBody: IN8nHttpResponse | Readable;
 
 	if (binaryData.id) {
-		responseBody =
-			mimeTypesToSanitize.includes(binaryData.mimeType) || shouldSandboxContentType
-				? sandboxResponseData(binaryData.data)
-				: { binaryData };
+		responseBody = shouldSandboxResponseData
+			? sandboxResponseData(binaryData.data)
+			: { binaryData };
 	} else {
 		const responseBuffer = Buffer.from(binaryData.data, BINARY_ENCODING);
 
-		responseBody =
-			mimeTypesToSanitize.includes(binaryData.mimeType) || shouldSandboxContentType
-				? sandboxResponseData(responseBuffer.toString())
-				: responseBuffer;
+		responseBody = shouldSandboxResponseData
+			? sandboxResponseData(responseBuffer.toString())
+			: responseBuffer;
 
 		setContentLength(responseBody, headers);
 	}
