4c379ce4060bdceb3c983f3dca6477e6fda6af87
Dana Lee <dana@n8n.io>
2025-07-08 10:29:35 +0200

    make srcdoc => src

--- packages/nodes-base/nodes/RespondToWebhook/utils/sandbox.ts
+++ packages/nodes-base/nodes/RespondToWebhook/utils/sandbox.ts
@@ -10,7 +10,7 @@ export const sandboxResponseData = (responseBody: string) => {
 	const parsedResponseBody = replaceSingleQuotes(responseBody);
 
 	return `
-		<iframe srcdoc='${parsedResponseBody}'
+		<iframe src='data:text/html;charset=utf-8,<html>${parsedResponseBody}</html>'
 			style="position:fixed; top:0; left:0; width:100vw; height:100vh; border:none; overflow:auto;"
 			allowtransparency="true">
 		</iframe>`;
