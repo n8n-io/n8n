015d1e4b6642d887a125e44df6b4bad6397e505a
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-07-09 12:41:53 +0300

    feat: Update icon

--- packages/@n8n/nodes-langchain/nodes/vendors/GoogleGemini/actions/versionDescription.ts
+++ packages/@n8n/nodes-langchain/nodes/vendors/GoogleGemini/actions/versionDescription.ts
@@ -11,7 +11,7 @@ import * as video from './video';
 export const versionDescription: INodeTypeDescription = {
 	displayName: 'Google Gemini',
 	name: 'googleGemini',
-	icon: 'file:google.svg',
+	icon: 'file:gemini.svg',
 	group: ['transform'],
 	version: 1,
 	subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
