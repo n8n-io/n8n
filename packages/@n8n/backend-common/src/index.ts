c350171fe6e318e185c4bd7d14d1dd8b5d78e7b2
Jaakko Husso <jaakko@n8n.io>
2025-07-09 21:51:09 +0300

    fix(core): Make our file reads more robust (no-changelog) (#17162)

--- packages/@n8n/backend-common/src/index.ts
+++ packages/@n8n/backend-common/src/index.ts
@@ -4,3 +4,4 @@ export * from './types';
 export { inDevelopment, inProduction, inTest } from './environment';
 export { isObjectLiteral } from './utils/is-object-literal';
 export { Logger } from './logging/logger';
+export { isContainedWithin, safeJoinPath } from './utils/path-util';
