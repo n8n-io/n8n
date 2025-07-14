1c76305d38a43e1fbbdb65e48dbcd8a052d6261e
DHY <104208339+daihuaiyi@users.noreply.github.com>
2025-07-14 17:50:14 +0800

    feat: Share replicate api (#40)
    * feat: Share replicate api
    
    * feat: Share replicate api
    
    * feat: Share communitynodes sherpapi

--- packages/cli/src/constants/credentials.ts
+++ packages/cli/src/constants/credentials.ts
@@ -6,9 +6,11 @@ export const SHARED_CREDENTIAL_TYPES = [
 	'googlePalmApi',
 	'postgres',
 	'mongoDb',
+	'replicateApi',
 	'mcpClientApi',
 	'mcpClientSseApi',
 	'mcpClientHttpApi',
+	'serpApi',
 ] as const;
 
 export type CredentialType = (typeof SHARED_CREDENTIAL_TYPES)[number];
