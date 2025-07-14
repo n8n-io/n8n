93ecbb36ed2a57e4305e8f367f12cbada949f58a
Martin Højberg Christiansen <martin@trendhim.com>
2025-07-14 09:52:13 +0200

    fix(slack): no redefining

--- packages/nodes-base/nodes/Slack/SlackTrigger.node.ts
+++ packages/nodes-base/nodes/Slack/SlackTrigger.node.ts
@@ -343,7 +343,7 @@ export class SlackTrigger implements INodeType {
 		}
 
 		if (eventType !== 'team_join') {
-			let eventChannel = req.body.event.channel ?? req.body.event.item?.channel ?? req.body.event.file?.channel_id;
+			eventChannel = req.body.event.channel ?? req.body.event.item?.channel ?? req.body.event.file?.channel_id;
 
 			// Check for single channel
 			if (!watchWorkspace) {
