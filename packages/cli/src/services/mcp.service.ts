20dfd5ef4053c1dfb33cfc1ceb7b79334fb8f754
Milorad FIlipović <milorad@n8n.io>
2025-07-14 17:17:19 +0200

    ⚡️ Added the `get_workflow_info` tool

--- packages/cli/src/services/mcp.service.ts
+++ packages/cli/src/services/mcp.service.ts
@@ -3,13 +3,19 @@ import { User, WorkflowEntity, WorkflowRepository } from '@n8n/db';
 import { Container, Service } from '@n8n/di';
 // eslint-disable-next-line n8n-local-rules/misplaced-n8n-typeorm-import
 import { type FindOptionsWhere, In, Like } from '@n8n/typeorm';
+import { OperationalError } from 'n8n-workflow';
 import z from 'zod';
+
+import { UrlService } from './url.service';
 
 import { WorkflowFinderService } from '@/workflows/workflow-finder.service';
 
 @Service()
 export class McpService {
-	constructor(private readonly workflowFinderService: WorkflowFinderService) {}
+	constructor(
+		private readonly workflowFinderService: WorkflowFinderService,
+		private readonly urlService: UrlService,
+	) {}
 
 	getServer(user: User) {
 		console.log('Creating MCP server for user:', user.id, user.role);
@@ -27,6 +33,71 @@ export class McpService {
 
 		// TODO: Extract tools to separate files
 		server.registerTool(
+			'get_workflow_info',
+			{
+				description: 'Get information about a specific workflow',
+				inputSchema: { workflowId: z.string().describe('The ID of the workflow to retrieve') },
+			},
+			async ({ workflowId }) => {
+				/*
+				TODO: Check these cases:
+					- [X] Non-active workflows
+					- [X] Archived workflows
+					- [X] Not available in MCP workflows
+					- [X] Different HTTP methods
+					- [ ] Request payloads
+					- [ ] Webhooks with auth
+					- [ ] With respond to webhook
+					- [ ] Respond immediately
+					- [ ] Respond when last node is executed
+					- [X] Multiple webhooks
+					- [X] Disabled webhooks
+					- [ ] Other triggers
+				*/
+				const workflow = await this.workflowFinderService.findWorkflowForUser(workflowId, user, [
+					'workflow:read',
+				]);
+				if (!workflow || workflow.isArchived || !workflow.settings?.availableInMCP) {
+					throw new OperationalError('Workflow not found');
+				}
+
+				workflow.pinData = undefined; // Remove pinData to avoid sending sensitive information
+
+				const webhooks = workflow.nodes.filter(
+					(node) => node.type === 'n8n-nodes-base.webhook' && node.disabled !== true,
+				);
+
+				let triggerNotice = 'This workflow does not have a trigger node.';
+				if (webhooks.length > 0) {
+					triggerNotice = 'This workflow is triggered by the following webhook(s):\n\n';
+					triggerNotice += webhooks
+						.map(
+							(node, index) =>
+								`
+						TRIGGER ${index + 1}:
+						\t - Node name: ${node.name}
+						\t - Base URL: ${this.urlService.baseUrl}
+						\t - PATH: ${workflow.active ? '/webhook/' : '/webhook-test/'}${node.parameters.path as string}
+						\t - HTTP Method: ${(node.parameters.httpMethod as string) ?? 'GET'}
+						`,
+						)
+						.join('\n\n');
+					triggerNotice += `${
+						workflow.active
+							? '- Workflow is active and accessible.'
+							: '- Workflow is not active, it can only be triggered after clicking "Listen for test	 event" button in the n8n editor.'
+					}`;
+				}
+
+				return {
+					content: [
+						{ type: 'text', text: JSON.stringify({ workflow, triggerInfo: triggerNotice }) },
+					],
+				};
+			},
+		);
+
+		server.registerTool(
 			'search_workflows',
 			{
 				description: 'Search for workflows with optional filters',
@@ -39,6 +110,7 @@ export class McpService {
 			},
 			async ({ limit = 500, active, name, projectId }) => {
 				const where: FindOptionsWhere<WorkflowEntity> = {
+					isArchived: false,
 					...(active !== undefined && { active }),
 					...(name !== undefined && { name: Like('%' + name.trim() + '%') }),
 				};
@@ -79,6 +151,8 @@ export class McpService {
 							type: node.type,
 						})),
 						connections,
+						notice:
+							'To get more information about a workflow, use the `get_workflow_info` tool with the workflow ID.',
 					}));
 				return {
 					content: [{ type: 'text', text: JSON.stringify({ data, count: data.length }) }],
