2c97fe9953cb442c7363bf93088811b937c8ca12
Iván Ovejero <ivov.src@gmail.com>
2025-07-15 10:10:25 +0200

    Reduce diff

--- packages/@n8n/backend-common/src/modules/errors/missing-module.error.ts
+++ packages/@n8n/backend-common/src/modules/errors/missing-module.error.ts
@@ -1,7 +1,7 @@
 import { UserError } from 'n8n-workflow';
 
 export class MissingModuleError extends UserError {
-	constructor(moduleName: string, errorMsg = 'unknown error') {
+	constructor(moduleName: string, errorMsg: string) {
 		super(
 			`Failed to load module "${moduleName}": ${errorMsg}. Please review the module's entrypoint file name and the module's directory name.`,
 		);
