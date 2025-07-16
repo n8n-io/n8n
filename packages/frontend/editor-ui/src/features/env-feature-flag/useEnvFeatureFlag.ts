4d856f5a0c0f543ddf7d4ead0dd1fa970c7adb56
Csaba Tuncsik <csaba.tuncsik@gmail.com>
2025-07-16 13:09:39 +0200

    fix(editor): Simplify logic

--- packages/frontend/editor-ui/src/features/env-feature-flag/useEnvFeatureFlag.ts
+++ packages/frontend/editor-ui/src/features/env-feature-flag/useEnvFeatureFlag.ts
@@ -5,17 +5,10 @@ export const useEnvFeatureFlag = () => {
 
 	const check = (flag: Uppercase<string>): boolean => {
 		const key: `FEAT_${Uppercase<string>}` = `FEAT_${flag}`;
-
-		// Always use settings store for both dev and prod
 		const envFeatureFlags = settingsStore.settings?.envFeatureFlags;
+		const value = envFeatureFlags[key];
 
-		if (envFeatureFlags?.[key] !== undefined) {
-			const value = envFeatureFlags[key];
-			return value === 'false' ? false : !!value;
-		}
-
-		// Fallback to false if not found
-		return false;
+		return value === 'false' ? false : !!value;
 	};
 
 	return {
