14387946ff5aeb996ea6af3f3669485482d07004
Mutasem Aldmour <mutasem@n8n.io>
2025-07-17 06:56:09 +0200

    only show if successful

--- packages/frontend/editor-ui/src/views/Evaluations.ee/TestRunDetailView.vue
+++ packages/frontend/editor-ui/src/views/Evaluations.ee/TestRunDetailView.vue
@@ -223,7 +223,7 @@ onMounted(async () => {
 		</el-scrollbar>
 
 		<n8n-callout
-			v-if="!isLoading && !inputColumns.length && run?.status === 'success'"
+			v-if="!isLoading && !inputColumns.length && run?.status === 'completed'"
 			theme="secondary"
 			icon="info"
 			class="mb-s"
