26097f9d797356f7fececb6ee56be1836d337721
Milorad FIlipović <milorad@n8n.io>
2025-07-21 10:48:26 +0200

    ⚡️ Using PostHog flag

--- packages/frontend/editor-ui/src/experiments/aiTemplatesStarterCollection/stores/aiTemplatesStarterCollection.store.ts
+++ packages/frontend/editor-ui/src/experiments/aiTemplatesStarterCollection/stores/aiTemplatesStarterCollection.store.ts
@@ -3,19 +3,23 @@ import { useWorkflowsStore } from '@/stores/workflows.store';
 import { WorkflowDataCreate } from '@n8n/rest-api-client';
 import { STORES } from '@n8n/stores';
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { AGENT_WITH_MEMORY } from '../workflows/1_agent_with_memory';
 import { AGENT_WITH_TOOLS } from '../workflows/2_agent_with_tools';
 import { AGENT_WITH_KNOWLEDGE } from '../workflows/3_agent_with_knowledge';
+import { useTelemetry } from '@/composables/useTelemetry';
+import { usePostHog } from '@/stores/posthog.store';
+import { TEMPLATE_ONBOARDING_EXPERIMENT } from '@/constants';
 
 export const useAITemplatesStarterCollectionStore = defineStore(
 	STORES.AI_TEMPLATES_STARTER_COLLECTION,
 	() => {
+		const telemetry = useTelemetry();
+
 		const foldersStore = useFoldersStore();
 		const workflowsStore = useWorkflowsStore();
+		const posthogStore = usePostHog();
 
-		// TODO: Check posthog flag
-		const isFeatureEnabled = ref(true);
 		// TODO: Save this to local storage
 		const calloutDismissed = ref(false);
 
@@ -23,6 +27,13 @@ export const useAITemplatesStarterCollectionStore = defineStore(
 		const setCalloutDismissed = (dismissed: boolean) => {
 			calloutDismissed.value = dismissed;
 		};
+
+		const isFeatureEnabled = computed(() => {
+			return (
+				posthogStore.getVariant(TEMPLATE_ONBOARDING_EXPERIMENT.name) ===
+				TEMPLATE_ONBOARDING_EXPERIMENT.variantStarterPack
+			);
+		});
 
 		const createStarterWorkflows = async (projectId: string, parentFolderId?: string) => {
 			const collectionFolder = await foldersStore.createFolder(
