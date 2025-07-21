ff661a85d12981f0ec8c1a39f895893e0cdb268b
luka-mimi <luka.cat.mimi@gmail.com>
2025-07-21 16:46:49 +0800

    perf(editor): Re-optimize the code

--- packages/frontend/@n8n/chat/src/App.vue
+++ packages/frontend/@n8n/chat/src/App.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
+import hljs from 'highlight.js/lib/core';
+import hljsJavascript from 'highlight.js/lib/languages/javascript';
+import hljsXML from 'highlight.js/lib/languages/xml';
+import { computed, onMounted } from 'vue';
 
 import Chat from '@n8n/chat/components/Chat.vue';
 import ChatWindow from '@n8n/chat/components/ChatWindow.vue';
@@ -10,6 +13,11 @@ defineProps({});
 const { options } = useOptions();
 
 const isFullscreen = computed<boolean>(() => options.mode === 'fullscreen');
+
+onMounted(() => {
+	hljs.registerLanguage('xml', hljsXML);
+	hljs.registerLanguage('javascript', hljsJavascript);
+});
 </script>
 <template>
 	<Chat v-if="isFullscreen" class="n8n-chat" />
