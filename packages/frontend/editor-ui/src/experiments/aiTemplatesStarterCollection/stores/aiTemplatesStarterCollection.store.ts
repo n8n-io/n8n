9812276054e4f75f4126f468cf524de7d06947f3
Milorad FIlipović <milorad@n8n.io>
2025-07-21 09:43:14 +0200

    feat(editor): Add AI Starter pack experiment (no-changelog)

---
+++ packages/frontend/editor-ui/src/experiments/aiTemplatesStarterCollection/stores/aiTemplatesStarterCollection.store.ts
@@ -0,0 +1,59 @@
+import { useFoldersStore } from '@/stores/folders.store';
+import { useWorkflowsStore } from '@/stores/workflows.store';
+import { WorkflowDataCreate } from '@n8n/rest-api-client';
+import { STORES } from '@n8n/stores';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { AGENT_WITH_MEMORY } from '../workflows/1_agent_with_memory';
+import { AGENT_WITH_TOOLS } from '../workflows/2_agent_with_tools';
+import { AGENT_WITH_KNOWLEDGE } from '../workflows/3_agent_with_knowledge';
+
+export const useAITemplatesStarterCollectionStore = defineStore(
+	STORES.AI_TEMPLATES_STARTER_COLLECTION,
+	() => {
+		const foldersStore = useFoldersStore();
+		const workflowsStore = useWorkflowsStore();
+
+		// TODO: Check posthog flag
+		const isFeatureEnabled = ref(true);
+		// TODO: Save this to local storage
+		const calloutDismissed = ref(false);
+
+		// TODO: Save this to local storage
+		const setCalloutDismissed = (dismissed: boolean) => {
+			calloutDismissed.value = dismissed;
+		};
+
+		const createStarterWorkflows = async (projectId: string, parentFolderId?: string) => {
+			const collectionFolder = await foldersStore.createFolder(
+				'🎁 n8n basics: Learn how to build Agents in n8n',
+				projectId,
+				parentFolderId,
+			);
+			const agentWitheMemory: WorkflowDataCreate = {
+				...AGENT_WITH_MEMORY,
+				parentFolderId: collectionFolder.id,
+			};
+			const agentWithTools: WorkflowDataCreate = {
+				...AGENT_WITH_TOOLS,
+				parentFolderId: collectionFolder.id,
+			};
+			const agentWithKnowledge: WorkflowDataCreate = {
+				...AGENT_WITH_KNOWLEDGE,
+				parentFolderId: collectionFolder.id,
+			};
+			await workflowsStore.createNewWorkflow(agentWithKnowledge);
+			await workflowsStore.createNewWorkflow(agentWithTools);
+			await workflowsStore.createNewWorkflow(agentWitheMemory);
+			calloutDismissed.value = true;
+			return collectionFolder;
+		};
+
+		return {
+			isFeatureEnabled,
+			calloutDismissed,
+			setCalloutDismissed,
+			createStarterWorkflows,
+		};
+	},
+);
