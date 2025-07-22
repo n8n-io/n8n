6290be408567d51dfca4aef4cccc376d69fab31b
autologie <suguru@n8n.io>
2025-07-22 11:53:52 +0200

    perf: reduce unnecessary layout recalculation

--- packages/frontend/editor-ui/src/components/canvas/experimental/components/ExperimentalEmbeddedNodeDetails.vue
+++ packages/frontend/editor-ui/src/components/canvas/experimental/components/ExperimentalEmbeddedNodeDetails.vue
@@ -58,8 +58,8 @@ const isVisible = computed(() =>
 		{
 			x: -vf.viewport.value.x / vf.viewport.value.zoom,
 			y: -vf.viewport.value.y / vf.viewport.value.zoom,
-			width: vf.viewportRef.value?.offsetWidth ?? 0,
-			height: vf.viewportRef.value?.offsetHeight ?? 0,
+			width: vf.dimensions.value.width,
+			height: vf.dimensions.value.height,
 		},
 	),
 );
