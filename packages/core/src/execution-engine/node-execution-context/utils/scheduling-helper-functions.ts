9ffd827bd1e9c8417c2bef9c4a0cc0cfec47afea
Iván Ovejero <ivov.src@gmail.com>
2025-07-28 10:32:57 +0200

    Remove unneeded typing

--- packages/core/src/execution-engine/node-execution-context/utils/scheduling-helper-functions.ts
+++ packages/core/src/execution-engine/node-execution-context/utils/scheduling-helper-functions.ts
@@ -1,11 +1,11 @@
 import { Container } from '@n8n/di';
-import type { Cron, SchedulingFunctions, Workflow } from 'n8n-workflow';
+import type { SchedulingFunctions, Workflow } from 'n8n-workflow';
 
 import { ScheduledTaskManager } from '../../scheduled-task-manager';
 
 export const getSchedulingFunctions = (workflow: Workflow): SchedulingFunctions => {
 	const scheduledTaskManager = Container.get(ScheduledTaskManager);
 	return {
-		registerCron: (cron: Cron, onTick) => scheduledTaskManager.registerCron(workflow, cron, onTick),
+		registerCron: (cron, onTick) => scheduledTaskManager.registerCron(workflow, cron, onTick),
 	};
 };
