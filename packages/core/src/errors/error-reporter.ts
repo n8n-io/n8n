ae804e431ad56e11521c2eef5be2466bae71befc
Iván Ovejero <ivov.src@gmail.com>
2025-07-29 10:21:56 +0200

    Fix lint

--- packages/core/src/errors/error-reporter.ts
+++ packages/core/src/errors/error-reporter.ts
@@ -1,8 +1,8 @@
 import { inTest, Logger } from '@n8n/backend-common';
 import type { InstanceType } from '@n8n/constants';
 import { Service } from '@n8n/di';
-import type { ErrorEvent, EventHint } from '@sentry/core';
 import type { ReportingOptions } from '@n8n/errors';
+import type { ErrorEvent, EventHint } from '@sentry/core';
 import type { NodeOptions } from '@sentry/node';
 import { AxiosError } from 'axios';
 import { ApplicationError, ExecutionCancelledError, BaseError } from 'n8n-workflow';
