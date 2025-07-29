03c5aee180cd0e0a5e5cf167afdae717f73905a0
autologie <suguru@n8n.io>
2025-07-29 23:29:29 +0200

    fix incorrect intersect detection

--- packages/frontend/editor-ui/src/components/canvas/experimental/components/ExperimentalEmbeddedNodeDetails.vue
+++ packages/frontend/editor-ui/src/components/canvas/experimental/components/ExperimentalEmbeddedNodeDetails.vue
@@ -58,8 +58,8 @@ const isVisible = computed(() =>
 		{
 			x: -vf.viewport.value.x / vf.viewport.value.zoom,
 			y: -vf.viewport.value.y / vf.viewport.value.zoom,
-			width: vf.dimensions.value.width,
-			height: vf.dimensions.value.height,
+			width: vf.dimensions.value.width / vf.viewport.value.zoom,
+			height: vf.dimensions.value.height / vf.viewport.value.zoom,
 		},
 	),
 );
