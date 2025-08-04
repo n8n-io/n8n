6ccf0d88844e863419e1587da7fe1cca3b75869b
shortstacked <declan@n8n.io>
2025-08-04 19:59:06 +0100

    ci: Playwright project organization (#17905)

--- packages/testing/playwright/utils/path-helper.ts
+++ packages/testing/playwright/utils/path-helper.ts
@@ -20,6 +20,22 @@ function findProjectRoot(marker: string): string {
 	return dir;
 }
 
+/**
+ * Finds a folder root by searching upwards for a marker folder named 'packages'.
+ * @returns The absolute path to the folder root.
+ */
+export function findPackagesRoot(marker: string): string {
+	let dir = __dirname;
+	while (!fs.existsSync(path.join(dir, marker))) {
+		const parentDir = path.dirname(dir);
+		if (parentDir === dir) {
+			throw new TestError('Could not find packages root');
+		}
+		dir = parentDir;
+	}
+	return dir;
+}
+
 const playwrightRoot = findProjectRoot('playwright.config.ts');
 
 /**
