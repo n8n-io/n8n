82cf6361a19105421b91eeb11dca06f5a185eb4c
Mutasem Aldmour <mutasem@n8n.io>
2025-08-06 10:04:24 +0200

    remove unsued import

--- packages/testing/playwright/pages/CanvasPage.ts
+++ packages/testing/playwright/pages/CanvasPage.ts
@@ -1,4 +1,4 @@
-import type { Locator, Page } from '@playwright/test';
+import type { Locator } from '@playwright/test';
 
 import { BasePage } from './BasePage';
 import { resolveFromRoot } from '../utils/path-helper';
