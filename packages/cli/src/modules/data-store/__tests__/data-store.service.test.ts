319486797709423f1420d633bee5419ebd613b19
Daria Staferova <daria.staferova@n8n.io>
2025-08-07 18:18:54 +0300

    fix type errors

--- packages/cli/src/modules/data-store/__tests__/data-store.service.test.ts
+++ packages/cli/src/modules/data-store/__tests__/data-store.service.test.ts
@@ -1,8 +1,7 @@
 // TODO: remove once all tests pass
 /* eslint-disable n8n-local-rules/no-skipped-tests */
 import type { AddDataStoreColumnDto, CreateDataStoreColumnDto } from '@n8n/api-types';
-import type { ModuleName } from '@n8n/backend-common';
-import { createTeamProject, testDb, testModules } from '@n8n/backend-test-utils';
+import { createTeamProject, testDb } from '@n8n/backend-test-utils';
 import { GlobalConfig } from '@n8n/config';
 import { Project } from '@n8n/db';
 import { Container } from '@n8n/di';
@@ -11,12 +10,6 @@ import { DataStoreRowsRepository } from '../data-store-rows.repository';
 import { DataStoreRepository } from '../data-store.repository';
 import { DataStoreService } from '../data-store.service';
 import { toTableName } from '../utils/sql-utils';
-
-beforeAll(async () => {
-	// the module is not registered
-	await testModules.loadModules(['data-store'] as unknown as ModuleName[]);
-	await testDb.init();
-});
 
 beforeEach(async () => {
 	await testDb.truncate(['DataStoreEntity', 'DataStoreColumnEntity']);
