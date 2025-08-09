c089719428348d6d9f45c195e1903d5de275be8b
Rob Squires <robtf9@icloud.com>
2025-08-09 08:17:28 +0100

    stop click propagation

--- packages/frontend/@n8n/design-system/src/components/N8nSidebar/SidebarItem.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nSidebar/SidebarItem.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import N8nIcon from '../N8nIcon';
 import { IconName, isSupportedIconName } from '../N8nIcon/icons';
-import N8nLink from '../N8nLink';
+import N8nRoute from '../N8nRoute';
+import N8nText from '../N8nText';
 
-defineProps<{
+const props = defineProps<{
 	title: string;
 	id: string;
 	icon?: IconName | string;
@@ -13,18 +14,23 @@ defineProps<{
 	ariaLabel: string;
 	type: 'folder' | 'workflow' | 'project' | 'other';
 }>();
+
+function click(event: MouseEvent) {
+	event.stopPropagation();
+	event.preventDefault();
+
+	if (typeof props.click === 'function') {
+		props.click();
+	}
+}
 </script>
 
 <template>
-	<div class="sidebarItem">
+	<N8nRoute :to="link" class="sidebarItem" @click.stop>
 		<div v-if="type !== 'workflow'" :class="{ sidebarItemDropdown: true, other: type === 'other' }">
 			<div class="sidebarItemDropdownIcon">
 				<span v-if="icon && !isSupportedIconName(icon)" class="sidebarItemEmoji">{{ icon }}</span>
-				<N8nIcon
-					v-else
-					color="foreground-xdark"
-					:icon="isSupportedIconName(icon) ? icon : 'layers'"
-				/>
+				<N8nIcon v-else :icon="isSupportedIconName(icon) ? icon : 'layers'" />
 			</div>
 			<button
 				v-if="type !== 'other'"
@@ -32,37 +38,48 @@ defineProps<{
 				@click.stop="click"
 				:aria-label="ariaLabel"
 			>
-				<N8nIcon color="foreground-xdark" :icon="open ? 'chevron-down' : 'chevron-right'" />
+				<N8nIcon :icon="open ? 'chevron-down' : 'chevron-right'" />
 			</button>
 		</div>
-		<N8nLink class="sidebarItemLink" :to="link">{{ title }}</N8nLink>
-	</div>
+		<N8nText class="sidebarItemText">{{ title }}</N8nText>
+	</N8nRoute>
 </template>
 
 <style lang="scss" scoped>
 .sidebarItem {
 	display: flex;
 	align-items: center;
-	padding: 8px 4px;
+	padding: 6px 4px;
 	gap: 4px;
 	cursor: pointer;
 	width: 100%;
+	color: var(--color-text-base);
+	border-radius: 4px;
+	margin-top: 1px;
+	cursor: pointer;
+
+	&:hover .sidebarItemDropdownIcon {
+		color: var(--color-foreground-xdark);
+	}
 }
 
-.sidebarItemLink {
+.sidebarItem:hover {
+	background-color: var(--color-foreground-light);
+	color: var(--color-text-dark);
+}
+
+.sidebarItem:focus {
+	background-color: var(--color-foreground-light);
+}
+
+.sidebarItemText {
 	white-space: nowrap;
 	text-overflow: ellipsis;
 	overflow: hidden;
 }
 
-.sidebarItemLink * {
+.sidebarItemText * {
 	color: var(--color-text-base);
-}
-
-.sidebarItemLink:hover *,
-.sidebarItemLink:focus * {
-	color: var(--color-text-dark);
-	text-decoration: underline;
 }
 
 .sidebarItemDropdown {
@@ -79,20 +96,21 @@ defineProps<{
 		align-items: center;
 		justify-content: center;
 		pointer-events: none;
+		color: var(--color-foreground-dark);
 	}
 
-	.sidebarItemDropdownButton {
-		opacity: 0;
-	}
+	&:not(.other) {
+		.sidebarItemDropdownButton {
+			opacity: 0;
+		}
 
-	&:hover .sidebarItemDropdownButton,
-	.sidebarItemDropdownButton:focus {
-		opacity: 1;
-	}
+		&:hover .sidebarItemDropdownButton {
+			opacity: 1;
+		}
 
-	&:hover .sidebarItemDropdownIcon,
-	&:focus-within .sidebarItemDropdownIcon {
-		opacity: 0;
+		&:hover .sidebarItemDropdownIcon {
+			opacity: 0;
+		}
 	}
 }
 
