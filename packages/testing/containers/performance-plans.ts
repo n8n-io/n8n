d9223b678974a01892c08510380d490c08b6f154
Declan Carroll <declan@n8n.io>
2025-08-12 12:00:22 +0100

    fix: Adjusting limits based on feedback
    Difference between initial minimum limit and max allowed limits

--- packages/testing/containers/performance-plans.ts
+++ packages/testing/containers/performance-plans.ts
@@ -14,11 +14,11 @@ export interface BasePerformancePlan {
 }
 
 export const BASE_PERFORMANCE_PLANS: Record<string, BasePerformancePlan> = {
-	trial: { memory: 0.375, cpu: 0.2 }, // 384MB RAM, 200 millicore CPU
-	starter: { memory: 0.375, cpu: 0.2 }, // 384MB RAM, 200 millicore CPU
-	pro1: { memory: 0.625, cpu: 0.5 }, // 640MB RAM, 500 millicore CPU
-	pro2: { memory: 1.25, cpu: 0.75 }, // 1.28GB RAM, 750 millicore CPU
-	enterprise: { memory: 4.0, cpu: 1.0 }, // 4GB RAM, 1.0 CPU core
+	trial: { memory: 0.75, cpu: 1 }, // 768MB RAM, 1000 millicore CPU
+	starter: { memory: 0.75, cpu: 1 }, // 768MB RAM, 1000 millicore CPU
+	pro1: { memory: 1.25, cpu: 1 }, // 1.25GB RAM, 1000 millicore CPU
+	pro2: { memory: 2.5, cpu: 1.5 }, // 2.5GB RAM, 1500 millicore CPU
+	enterprise: { memory: 8.0, cpu: 2.0 }, // 8GB RAM, 2.0 CPU core
 } as const;
 
 export type PerformancePlanName = keyof typeof BASE_PERFORMANCE_PLANS;
