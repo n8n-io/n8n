c35b091389146242eaccdd2127ed2ecde2b0947d
Jaakko Husso <jaakko@n8n.io>
2025-08-18 09:39:15 +0300

    chore(core): Fix typo

--- packages/@n8n/api-types/src/schemas/data-store.schema.ts
+++ packages/@n8n/api-types/src/schemas/data-store.schema.ts
@@ -57,7 +57,7 @@ export const dateTimeSchema = z
 	.transform((s) => new Date(s))
 	.pipe(z.date());
 
-// Dates are received as date strings and validated before insertation
+// Dates are received as date strings and validated before insertion
 export const dataStoreColumnValueSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
 
 export type DataStoreColumnJsType = string | number | boolean | Date;
