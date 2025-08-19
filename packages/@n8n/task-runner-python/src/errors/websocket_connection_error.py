8c03f66465849c817cec2294d93468edd69c3c98
Iván Ovejero <ivov.src@gmail.com>
2025-08-19 11:47:53 +0200

    Formatting

--- packages/@n8n/task-runner-python/src/errors/websocket_connection_error.py
+++ packages/@n8n/task-runner-python/src/errors/websocket_connection_error.py
@@ -1,3 +1,5 @@
 class WebsocketConnectionError(ConnectionError):
     def __init__(self, broker_uri: str):
-        super().__init__(f"Failed to connect to broker. Please check if broker is reachable at: {broker_uri}")
+        super().__init__(
+            f"Failed to connect to broker. Please check if broker is reachable at: {broker_uri}"
+        )
