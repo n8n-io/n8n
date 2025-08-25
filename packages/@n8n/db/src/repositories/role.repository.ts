7724efcb8531a0ec47012030111cf3e5ecb3a186
Andreas Fitzek <andreas.fitzek@n8n.io>
2025-08-25 08:58:59 +0200

    Implement role management for custom roles

--- packages/@n8n/db/src/repositories/role.repository.ts
+++ packages/@n8n/db/src/repositories/role.repository.ts
@@ -1,11 +1,59 @@
+import { DatabaseConfig } from '@n8n/config';
 import { Service } from '@n8n/di';
-import { DataSource, Repository } from '@n8n/typeorm';
+import { DataSource, EntityManager, Repository } from '@n8n/typeorm';
 
 import { Role } from '../entities';
 
 @Service()
 export class RoleRepository extends Repository<Role> {
-	constructor(dataSource: DataSource) {
+	constructor(
+		dataSource: DataSource,
+		private readonly databaseConfig: DatabaseConfig,
+	) {
 		super(Role, dataSource.manager);
+	}
+
+	async findAll() {
+		return await this.find();
+	}
+
+	async findBySlug(slug: string) {
+		return await this.findOne({ where: { slug } });
+	}
+
+	async removeBySlug(slug: string) {
+		const result = await this.delete({ slug });
+		if (result.affected !== 1) {
+			throw new Error(`Failed to delete role "${slug}"`);
+		}
+	}
+
+	private async updateEntityWithManager(
+		entityManager: EntityManager,
+		slug: string,
+		newData: Partial<Pick<Role, 'description' | 'scopes' | 'displayName'>>,
+	) {
+		const role = await entityManager.findOneBy(Role, { slug });
+		if (!role) {
+			throw new Error('Role not found');
+		}
+		if (role.systemRole) {
+			throw new Error('Cannot update system roles');
+		}
+		Object.assign(role, newData);
+		return await entityManager.save<Role>(role);
+	}
+
+	async updateRole(
+		slug: string,
+		newData: Partial<Pick<Role, 'description' | 'scopes' | 'displayName'>>,
+	) {
+		// Do not use transactions for sqlite legacy
+		if (this.databaseConfig.isLegacySqlite) {
+			return await this.updateEntityWithManager(this.manager, slug, newData);
+		}
+		return await this.manager.transaction(async (transactionManager) => {
+			return await this.updateEntityWithManager(transactionManager, slug, newData);
+		});
 	}
 }
