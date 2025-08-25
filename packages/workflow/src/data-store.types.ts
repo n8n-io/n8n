9fe607a51f4c9b6f3efbb5d3e15bf8166389a11e
Charlie Kolb <charlie@n8n.io>
2025-08-25 15:59:40 +0200

    chore: Add null to DataStoreColumnJsType (no-changelog) (#18733)

--- packages/workflow/src/data-store.types.ts
+++ packages/workflow/src/data-store.types.ts
@@ -70,9 +70,9 @@ export type MoveDataStoreColumnOptions = {
 export type AddDataStoreColumnOptions = Pick<DataStoreColumn, 'name' | 'type'> &
 	Partial<Pick<DataStoreColumn, 'index'>>;
 
-export type DataStoreColumnJsType = string | number | boolean | Date;
+export type DataStoreColumnJsType = string | number | boolean | Date | null;
 
-export type DataStoreRow = Record<string, DataStoreColumnJsType | null>;
+export type DataStoreRow = Record<string, DataStoreColumnJsType>;
 export type DataStoreRows = DataStoreRow[];
 export type DataStoreRowWithId = DataStoreRow & { id: number };
 
