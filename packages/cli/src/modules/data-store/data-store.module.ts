9ca253f4418e25ca0ef8e999a1387513579eb0f0
Charlie Kolb <charlie@n8n.io>
2025-08-25 09:00:34 +0200

    add module warning

--- packages/cli/src/modules/data-store/data-store.module.ts
+++ packages/cli/src/modules/data-store/data-store.module.ts
@@ -1,11 +1,18 @@
+import { Logger } from '@n8n/backend-common';
 import type { ModuleInterface } from '@n8n/decorators';
 import { BackendModule, OnShutdown } from '@n8n/decorators';
 import { Container } from '@n8n/di';
 import { BaseEntity } from '@n8n/typeorm';
 
+const DATA_TABLE_WARNING_MESSAGE = `The data table module is experimental and subject to change.
+Any tables added before the official release may become inaccessible at any point. Use at your own risk.`;
+
 @BackendModule({ name: 'data-store' })
 export class DataStoreModule implements ModuleInterface {
 	async init() {
+		const logger = Container.get(Logger).scoped('data-table');
+		logger.warn(DATA_TABLE_WARNING_MESSAGE);
+
 		await import('./data-store.controller');
 		await import('./data-store-aggregate.controller');
 
