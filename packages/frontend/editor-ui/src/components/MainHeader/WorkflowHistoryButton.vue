e6a889e686bced61aae6d277693da1edc77ebf6e
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-08-26 16:09:06 +0300

    Fix styling of wf history button

--- packages/frontend/editor-ui/src/components/MainHeader/WorkflowHistoryButton.vue
+++ packages/frontend/editor-ui/src/components/MainHeader/WorkflowHistoryButton.vue
@@ -31,6 +31,7 @@ const workflowHistoryRoute = computed<{ name: string; params: { workflowId: stri
 				:disabled="isNewWorkflow || !isFeatureEnabled"
 				data-test-id="workflow-history-button"
 				type="tertiary"
+				text
 				icon="history"
 				size="medium"
 			/>
