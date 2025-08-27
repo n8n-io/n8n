24a444e3f5ac015d9298d2d48e51572e6fee88a4
yehorkardash <yehor.kardash@n8n.io>
2025-08-27 09:28:06 +0300

    remove `.utc(false)`

--- packages/nodes-base/nodes/Hubspot/V2/utils/parseToTimestamp.ts
+++ packages/nodes-base/nodes/Hubspot/V2/utils/parseToTimestamp.ts
@@ -4,7 +4,7 @@ export function parseToTimestamp(dateString: unknown): number {
 	if (typeof dateString !== 'string') {
 		throw new Error('Invalid date string');
 	}
-	const timestamp = moment(dateString).utc(false).valueOf();
+	const timestamp = moment(dateString).valueOf();
 	if (isNaN(timestamp)) {
 		throw new Error('Invalid date string');
 	}
