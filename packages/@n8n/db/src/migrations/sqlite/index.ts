5a2c0d96b13e9b44551e481732e067d5291d5455
Charlie Kolb <charlie@n8n.io>
2025-08-27 15:36:00 +0200

    chore(core): Rename Data Store DB entries to Data Table (no-changelog) (#18670)

--- packages/@n8n/db/src/migrations/sqlite/index.ts
+++ packages/@n8n/db/src/migrations/sqlite/index.ts
@@ -91,6 +91,7 @@ import { LinkRoleToUserTable1750252139168 } from '../common/1750252139168-LinkRo
 import { RemoveOldRoleColumn1750252139170 } from '../common/1750252139170-RemoveOldRoleColumn';
 import { AddInputsOutputsToTestCaseExecution1752669793000 } from '../common/1752669793000-AddInputsOutputsToTestCaseExecution';
 import { CreateDataStoreTables1754475614601 } from '../common/1754475614601-CreateDataStoreTables';
+import { ReplaceDataStoreTablesWithDataTables1754475614602 } from '../common/1754475614602-ReplaceDataStoreTablesWithDataTables';
 import type { Migration } from '../migration-types';
 
 const sqliteMigrations: Migration[] = [
@@ -187,6 +188,7 @@ const sqliteMigrations: Migration[] = [
 	AddInputsOutputsToTestCaseExecution1752669793000,
 	CreateDataStoreTables1754475614601,
 	RemoveOldRoleColumn1750252139170,
+	ReplaceDataStoreTablesWithDataTables1754475614602,
 ];
 
 export { sqliteMigrations };
