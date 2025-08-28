7300957985aa82d36f1a770d64ec10ef768252da
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-08-28 16:33:17 +0200

    switch to polling syntax

--- packages/nodes-base/nodes/Mindee/GenericFunctions.ts
+++ packages/nodes-base/nodes/Mindee/GenericFunctions.ts
@@ -1,3 +1,4 @@
+import type FormData from 'form-data';
 import type {
 	IExecuteFunctions,
 	ILoadOptionsFunctions,
@@ -12,45 +13,52 @@ import { setTimeout } from 'node:timers/promises';
 const INITIAL_DELAY_MS = 1500;
 const POLL_DELAY_MS = 1000;
 
+/**
+ * Makes an authenticated HTTP request to the Mindee API
+ * @param method - HTTP method.
+ * @param url - The Mindee API's (complete) URL.
+ * @param body - The request body data.
+ * @param option - Additional request options (default: empty object)
+ * @returns The API response data
+ * @throws NodeApiError when the API request fails
+ */
 export async function mindeeApiRequest(
 	this: IExecuteFunctions | ILoadOptionsFunctions,
 	method: IHttpRequestMethods,
 	url: string,
-	body: any = {},
-	qs: IDataObject = {},
+	body: IDataObject | FormData = {},
 	option = {},
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): Promise<any> {
 	const options: IHttpRequestOptions = {
 		headers: {},
 		method,
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		body,
-		qs,
 		url,
-		disableFollowRedirect: true,
-		ignoreHttpStatusErrors: true,
-		json: false,
+		body,
 	};
 	try {
+		delete options.qs;
 		if (Object.keys(body as IDataObject).length === 0) {
 			delete options.body;
-		}
-		if (Object.keys(qs).length === 0) {
-			delete options.qs;
 		}
 		if (Object.keys(option).length !== 0) {
 			Object.assign(options, option);
 		}
-
 		return await this.helpers.httpRequestWithAuthentication.call(this, 'mindeeV2Api', {
 			...options,
-			maxRedirects: 0,
 		});
 	} catch (error) {
 		throw new NodeApiError(this.getNode(), error as JsonObject);
 	}
 }
 
+/**
+ * Polls the Mindee API for the result of a job.
+ * Automatically follows the redirect on the last poll attempt.
+ * @param funcRef The execution function reference.
+ * @param initialResponse Initial POST request response from the API.
+ * @param maxDelayCounter Maximum number of attempts to poll the API.
+ */
 export async function pollMindee(
 	funcRef: IExecuteFunctions,
 	initialResponse: IDataObject,
@@ -78,30 +86,21 @@ export async function pollMindee(
 			throw new NodeApiError(funcRef.getNode(), serverResponse as JsonObject);
 		}
 
-		if (jobStatus === 'Processed') break;
-
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		serverResponse = await mindeeApiRequest.call(funcRef, 'GET', pollUrl);
+		if ('inference' in (serverResponse as JsonObject)) break;
 
-		if (serverResponse.error || !('job' in serverResponse))
+		if (!('job' in serverResponse))
 			throw new NodeApiError(funcRef.getNode(), serverResponse as JsonObject, {
-				message: 'Mindee API responded with an error.',
+				message: 'The Mindee API replied with an unexpected reply.',
 			});
 		jobStatus = (serverResponse.job as IDataObject).status as string;
 		await setTimeout(POLL_DELAY_MS);
 	}
 
-	const inferenceUrl = (serverResponse.job as IDataObject).result_url;
-	if (jobStatus !== 'Processed' || inferenceUrl === undefined || inferenceUrl === null)
+	if (!('inference' in (serverResponse as JsonObject)))
 		throw new NodeApiError(funcRef.getNode(), serverResponse as JsonObject, {
 			message: `Server polling timed out after ${maxDelayCounter} seconds. Status: ${jobStatus}.`,
-		});
-
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	serverResponse = await mindeeApiRequest.call(funcRef, 'GET', inferenceUrl as string);
-	if ((serverResponse?.job as IDataObject)?.error)
-		throw new NodeApiError(funcRef.getNode(), serverResponse as JsonObject, {
-			message: JSON.stringify(serverResponse as JsonObject),
 		});
 	result.push(serverResponse);
 	return result;
