e875a88360dc7ce100210339844ba2c06f8a3862
Charlie Kolb <charlie@n8n.io>
2025-08-28 09:19:04 +0200

    fix test

--- packages/cli/src/modules/data-table/__tests__/data-store.service.test.ts
+++ packages/cli/src/modules/data-table/__tests__/data-store.service.test.ts
@@ -1204,7 +1204,9 @@ describe('dataStore', () => {
 			]);
 
 			// ASSERT
-			await expect(result).rejects.toThrow(new DataStoreValidationError('unknown column name'));
+			await expect(result).rejects.toThrow(
+				new DataStoreValidationError("unknown column name 'cWrong'"),
+			);
 		});
 
 		it('rejects a invalid date string to date column', async () => {
