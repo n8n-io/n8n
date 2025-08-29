db1b8b03abb01abec3e6fbdd48ead2153061a2f0
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-08-29 14:03:42 +0300

    feat(editor): Display created and updated at data table columns (no-changelog) (#18948)

--- packages/frontend/editor-ui/src/features/dataStore/dataStore.api.ts
+++ packages/frontend/editor-ui/src/features/dataStore/dataStore.api.ts
@@ -151,11 +151,12 @@ export const insertDataStoreRowApi = async (
 	row: DataStoreRow,
 	projectId: string,
 ) => {
-	return await makeRestApiRequest<Array<{ id: number }>>(
+	return await makeRestApiRequest<DataStoreRow[]>(
 		context,
 		'POST',
 		`/projects/${projectId}/data-tables/${dataStoreId}/insert`,
 		{
+			returnData: true,
 			data: [row],
 		},
 	);
