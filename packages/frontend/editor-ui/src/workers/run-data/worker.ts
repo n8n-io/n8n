5e8fd2864cd831809f74f15ff4fee89f3f974fac
Alex Grozav <alex@grozav.com>
2025-08-29 12:32:16 +0100

    fix(editor): Prevent duplicate initialization in `run-data` worker (no-changelog)

--- packages/frontend/editor-ui/src/workers/run-data/worker.ts
+++ packages/frontend/editor-ui/src/workers/run-data/worker.ts
@@ -4,19 +4,24 @@ import { useDatabase } from '@/workers/database';
 import type { Promiser } from '@sqlite.org/sqlite-wasm';
 
 const state: {
+	initialized: boolean;
 	promiser: Promiser | null;
 	dbId: string | null;
 } = {
+	initialized: false,
 	promiser: null,
 	dbId: null,
 };
 
 export const actions = {
 	async initialize() {
+		if (state.initialized) return;
+
 		const { promiser, dbId } = await useDatabase(databaseConfig);
 
 		state.promiser = promiser;
 		state.dbId = dbId;
+		state.initialized = true;
 	},
 };
 
