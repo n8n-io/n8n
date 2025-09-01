fee731a6eeb9ad8bb5506a1c9fa589ca5a29ae77
autologie <suguru@n8n.io>
2025-09-01 14:52:41 +0200

    add tests

---
+++ packages/frontend/editor-ui/src/components/canvas/experimental/components/ExperimentalNodeDetailsDrawer.test.ts
@@ -0,0 +1,9 @@
+import ExperimentalNodeDetailsDrawer from './ExperimentalNodeDetailsDrawer.vue';
+
+describe(ExperimentalNodeDetailsDrawer, () => {
+	describe('when multiple nodes are selected', () => {
+		it('should show the number of selected nodes and available actions', () => {});
+
+		it('should emit contextMenuAction event when a button is pressed', () => {});
+	});
+});
