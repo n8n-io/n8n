6496aee0c9abdcb3fcc8346460eaf21db47ceae3
Shireen Missi <shireen@n8n.io>
2025-09-01 22:01:20 +0100

    fixx switch node expression mode by disallowing expressions in number of outputs

--- packages/nodes-base/nodes/Switch/Switch.node.ts
+++ packages/nodes-base/nodes/Switch/Switch.node.ts
@@ -14,7 +14,7 @@ export class Switch extends VersionedNodeType {
 			iconColor: 'light-blue',
 			group: ['transform'],
 			description: 'Route items depending on defined expression or rules',
-			defaultVersion: 3.2,
+			defaultVersion: 3.3,
 		};
 
 		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
@@ -23,6 +23,7 @@ export class Switch extends VersionedNodeType {
 			3: new SwitchV3(baseDescription),
 			3.1: new SwitchV3(baseDescription),
 			3.2: new SwitchV3(baseDescription),
+			3.3: new SwitchV3(baseDescription),
 		};
 
 		super(nodeVersions, baseDescription);
