7223073cb5c2f84832cf9640868d10b6366b3e2f
Daria Staferova <daria.staferova@n8n.io>
2025-09-04 17:20:07 +0300

    Move limit inside get

--- packages/nodes-base/nodes/DataTable/actions/row/Row.resource.ts
+++ packages/nodes-base/nodes/DataTable/actions/row/Row.resource.ts
@@ -83,19 +83,4 @@ export const description: INodeProperties[] = [
 	...get.description,
 	...update.description,
 	...upsert.description,
-	{
-		displayName: 'Limit',
-		name: 'limit',
-		type: 'number',
-		typeOptions: {
-			minValue: 1,
-		},
-		displayOptions: {
-			show: {
-				operation: [get.FIELD],
-			},
-		},
-		default: null,
-		description: 'Max number of results to return',
-	},
 ];
