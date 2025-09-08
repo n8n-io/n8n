6bc466bb26a05c7c61868a7d009cf20d6ef6e509
autologie <suguru@n8n.io>
2025-09-08 13:56:31 +0200

    fix: don't show mapper when date picker is opened

--- packages/frontend/editor-ui/src/components/ParameterInput.vue
+++ packages/frontend/editor-ui/src/components/ParameterInput.vue
@@ -81,7 +81,13 @@ import { createEventBus } from '@n8n/utils/event-bus';
 import { onClickOutside, useElementSize } from '@vueuse/core';
 import { captureMessage } from '@sentry/vue';
 import { isCredentialOnlyNodeType } from '@/utils/credentialOnlyNodes';
-import { hasFocusOnInput, isBlurrableEl, isFocusableEl, isSelectableEl } from '@/utils/typesUtils';
+import {
+	hasFocusOnInput,
+	isBlurrableEl,
+	isEmpty,
+	isFocusableEl,
+	isSelectableEl,
+} from '@/utils/typesUtils';
 import { completeExpressionSyntax, shouldConvertToExpression } from '@/utils/expressions';
 import CssEditor from './CssEditor/CssEditor.vue';
 import { useFocusPanelStore } from '@/stores/focusPanel.store';
@@ -621,6 +627,13 @@ const shouldCaptureForPosthog = computed(() => node.value?.type === AI_TRANSFORM
 
 const mapperElRef = computed(() => mapperRef.value?.contentRef);
 
+const isMapperAvailable = computed(
+	() =>
+		isModelValueExpression.value ||
+		props.forceShowExpression ||
+		(isEmpty(props.modelValue) && props.parameter.type !== 'dateTime'),
+);
+
 function isRemoteParameterOption(option: INodePropertyOptions) {
 	return remoteParameterOptionsKeys.value.includes(option.name);
 }
@@ -807,10 +820,7 @@ async function setFocus() {
 		}
 
 		isFocused.value = true;
-
-		if (isModelValueExpression.value || props.forceShowExpression || props.modelValue === '') {
-			isMapperShown.value = true;
-		}
+		isMapperShown.value = isMapperAvailable.value;
 	}
 
 	emit('focus');
