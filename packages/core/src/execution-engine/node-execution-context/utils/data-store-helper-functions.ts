48d397a3740957addf7d98d91df789434ccfc74c
Charlie Kolb <charlie@n8n.io>
2025-09-09 10:24:49 +0200

    fix build

--- packages/core/src/execution-engine/node-execution-context/utils/data-store-helper-functions.ts
+++ packages/core/src/execution-engine/node-execution-context/utils/data-store-helper-functions.ts
@@ -17,14 +17,14 @@ export function getDataStoreHelperFunctions(
 			await dataStoreProxyProvider.getDataStoreAggregateProxy(
 				workflow,
 				node,
-				additionalData.projectId,
+				additionalData.dataTableProjectId,
 			),
 		getDataStoreProxy: async (dataStoreId: string) =>
 			await dataStoreProxyProvider.getDataStoreProxy(
 				workflow,
 				node,
 				dataStoreId,
-				additionalData.projectId,
+				additionalData.dataTableProjectId,
 			),
 	};
 }
