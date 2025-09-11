34bb052edae57a2148f0e3f7ccd388e2785ceb93
Charlie Kolb <charlie@n8n.io>
2025-09-11 09:21:40 +0200

    refactor(core): Store projectId on additionalData independent of dataStore context (no-changelog) (#19093)

--- packages/cli/src/modules/data-table/data-store-proxy.service.ts
+++ packages/cli/src/modules/data-table/data-store-proxy.service.ts
@@ -49,10 +49,10 @@ export class DataStoreProxyService implements DataStoreProxyProvider {
 	async getDataStoreAggregateProxy(
 		workflow: Workflow,
 		node: INode,
-		dataStoreProjectId?: string,
+		projectId?: string,
 	): Promise<IDataStoreProjectAggregateService> {
 		this.validateRequest(node);
-		const projectId = dataStoreProjectId ?? (await this.getProjectId(workflow));
+		projectId = projectId ?? (await this.getProjectId(workflow));
 
 		return this.makeAggregateOperations(projectId);
 	}
@@ -61,10 +61,10 @@ export class DataStoreProxyService implements DataStoreProxyProvider {
 		workflow: Workflow,
 		node: INode,
 		dataStoreId: string,
-		dataStoreProjectId?: string,
+		projectId?: string,
 	): Promise<IDataStoreProjectService> {
 		this.validateRequest(node);
-		const projectId = dataStoreProjectId ?? (await this.getProjectId(workflow));
+		projectId = projectId ?? (await this.getProjectId(workflow));
 
 		return this.makeDataStoreOperations(projectId, dataStoreId);
 	}
