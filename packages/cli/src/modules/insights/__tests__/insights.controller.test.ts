9da43804d64c483fc0fca28ddaa461b8cc044e86
Irénée <irenee.ajeneza@n8n.io>
2025-09-15 16:14:11 +0100

    test: added repository query condition to integration test

--- packages/cli/src/modules/insights/__tests__/insights.controller.test.ts
+++ packages/cli/src/modules/insights/__tests__/insights.controller.test.ts
@@ -24,9 +24,14 @@ afterAll(async () => {
 
 describe('InsightsController', () => {
 	const insightsByPeriodRepository = mockInstance(InsightsByPeriodRepository);
+
 	let controller: InsightsController;
 	beforeAll(async () => {
 		controller = Container.get(InsightsController);
+	});
+
+	beforeEach(() => {
+		jest.clearAllMocks();
 	});
 
 	describe('getInsightsSummary', () => {
@@ -41,6 +46,10 @@ describe('InsightsController', () => {
 			);
 
 			// ASSERT
+			expect(
+				insightsByPeriodRepository.getPreviousAndCurrentPeriodTypeAggregates,
+			).toHaveBeenCalledWith({ periodLengthInDays: 7 });
+
 			expect(response).toEqual({
 				total: { deviation: null, unit: 'count', value: 0 },
 				failed: { deviation: null, unit: 'count', value: 0 },
@@ -66,6 +75,10 @@ describe('InsightsController', () => {
 			);
 
 			// ASSERT
+			expect(
+				insightsByPeriodRepository.getPreviousAndCurrentPeriodTypeAggregates,
+			).toHaveBeenCalledWith({ periodLengthInDays: 7 });
+
 			expect(response).toEqual({
 				total: { deviation: null, unit: 'count', value: 30 },
 				failed: { deviation: null, unit: 'count', value: 10 },
@@ -95,6 +108,46 @@ describe('InsightsController', () => {
 			);
 
 			// ASSERT
+			expect(
+				insightsByPeriodRepository.getPreviousAndCurrentPeriodTypeAggregates,
+			).toHaveBeenCalledWith({ periodLengthInDays: 7 });
+
+			expect(response).toEqual({
+				total: { deviation: 10, unit: 'count', value: 30 },
+				failed: { deviation: 6, unit: 'count', value: 10 },
+				failureRate: { deviation: 0.333 - 0.2, unit: 'ratio', value: 0.333 },
+				averageRunTime: { deviation: 300 / 30 - 40 / 20, unit: 'millisecond', value: 10 },
+				timeSaved: { deviation: 5, unit: 'minute', value: 10 },
+			});
+		});
+
+		it('should use the query filters when provided', async () => {
+			// ARRANGE
+			insightsByPeriodRepository.getPreviousAndCurrentPeriodTypeAggregates.mockResolvedValue([
+				{ period: 'previous', type: TypeToNumber.success, total_value: 16 },
+				{ period: 'previous', type: TypeToNumber.failure, total_value: 4 },
+				{ period: 'previous', type: TypeToNumber.runtime_ms, total_value: 40 },
+				{ period: 'previous', type: TypeToNumber.time_saved_min, total_value: 5 },
+				{ period: 'current', type: TypeToNumber.success, total_value: 20 },
+				{ period: 'current', type: TypeToNumber.failure, total_value: 10 },
+				{ period: 'current', type: TypeToNumber.runtime_ms, total_value: 300 },
+				{ period: 'current', type: TypeToNumber.time_saved_min, total_value: 10 },
+			]);
+
+			// ACT
+			const response = await controller.getInsightsSummary(
+				mock<AuthenticatedRequest>(),
+				mock<Response>(),
+				{ dateRange: 'month', projectId: 'test-project' },
+			);
+
+			expect(
+				insightsByPeriodRepository.getPreviousAndCurrentPeriodTypeAggregates,
+			).toHaveBeenCalledWith({
+				periodLengthInDays: 30,
+				projectId: 'test-project',
+			});
+
 			expect(response).toEqual({
 				total: { deviation: 10, unit: 'count', value: 30 },
 				failed: { deviation: 6, unit: 'count', value: 10 },
@@ -104,6 +157,8 @@ describe('InsightsController', () => {
 			});
 		});
 	});
+
+	describe('getInsightsByWorkflow', () => {});
 
 	describe('getInsightsByTime', () => {
 		it('should return insights by time with empty data', async () => {
