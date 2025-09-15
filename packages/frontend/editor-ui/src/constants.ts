48008c7fec43fd3cf5360e665ffbeca364d10954
Romeo Balta <7095569+romeobalta@users.noreply.github.com>
2025-09-15 11:21:25 +0100

    feat(editor): Implement Ready to Run Workflows V2 experiment (no-changelog) (#19468)

--- packages/frontend/editor-ui/src/constants.ts
+++ packages/frontend/editor-ui/src/constants.ts
@@ -810,6 +810,13 @@ export const TEMPLATE_RECO_V2 = {
 	variant: 'variant',
 };
 
+export const READY_TO_RUN_V2_EXPERIMENT = {
+	name: '042_ready-to-run-worfklow_v2',
+	control: 'control',
+	variant1: 'variant-1-singlebox',
+	variant2: 'variant-2-twoboxes',
+};
+
 export const EXPERIMENTS_TO_TRACK = [
 	WORKFLOW_BUILDER_EXPERIMENT.name,
 	EXTRA_TEMPLATE_LINKS_EXPERIMENT.name,
@@ -818,6 +825,7 @@ export const EXPERIMENTS_TO_TRACK = [
 	BATCH_11AUG_EXPERIMENT.name,
 	PRE_BUILT_AGENTS_EXPERIMENT.name,
 	TEMPLATE_RECO_V2.name,
+	READY_TO_RUN_V2_EXPERIMENT.name,
 ];
 
 export const MFA_FORM = {
