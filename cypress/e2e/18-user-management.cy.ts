d4b129e12a988e6b33987c203c22dbb643007f89
konstantintieber <konstantin.tieber@n8n.io>
2025-09-16 09:41:46 +0200

    Confirm current password in e2e test to change user email

--- cypress/e2e/18-user-management.cy.ts
+++ cypress/e2e/18-user-management.cy.ts
@@ -236,7 +236,7 @@ describe('User Management', { disableAutoLogin: true }, () => {
 			INSTANCE_OWNER.email,
 			updatedPersonalData.newPassword,
 		);
-		personalSettingsPage.actions.updateEmail(updatedPersonalData.newEmail);
+		personalSettingsPage.actions.updateEmail(updatedPersonalData.newEmail, INSTANCE_OWNER.password);
 		successToast().should('contain', 'Personal details updated');
 		personalSettingsPage.actions.loginWithNewData(
 			updatedPersonalData.newEmail,
