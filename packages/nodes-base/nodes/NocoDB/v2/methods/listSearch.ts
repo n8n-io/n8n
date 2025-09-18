4c4233cd6b07018035103a0ef390de64926a8508
Fendy Heryanto <fendy@nocodb.com>
2025-09-18 13:08:56 +0700

    better fields option

--- packages/nodes-base/nodes/NocoDB/v2/methods/listSearch.ts
+++ packages/nodes-base/nodes/NocoDB/v2/methods/listSearch.ts
@@ -220,6 +220,49 @@ export async function getViews(this: ILoadOptionsFunctions) {
 	}
 }
 
+export async function getFields(this: ILoadOptionsFunctions) {
+	const version = this.getNodeParameter('version', 0) as number;
+	if (version === 3) {
+		return { results: [] };
+	}
+	const baseId = this.getNodeParameter('projectId', 0, {
+		extractValue: true,
+	}) as string;
+	const tableId = this.getNodeParameter('table', 0, {
+		extractValue: true,
+	}) as string;
+
+	if (tableId) {
+		try {
+			const requestMethod = 'GET';
+			const endpoint = `/api/v3/meta/bases/${baseId}/tables/${tableId}`;
+			const responseData = await apiRequest.call(this, requestMethod, endpoint, {}, {});
+
+			return {
+				results: responseData.fields.map((i: IDataObject) => {
+					return {
+						name: i.title,
+						value: i.id,
+					};
+				}),
+			};
+		} catch (e) {
+			const message = e.messages?.[0] ?? '';
+			throw new NodeOperationError(
+				this.getNode(),
+				new Error(`Error while fetching fields: ${message}`, { cause: e }),
+				{
+					level: 'warning',
+				},
+			);
+		}
+	} else {
+		throw new NodeOperationError(this.getNode(), 'No table selected!', {
+			level: 'warning',
+		});
+	}
+}
+
 // This only supports using the table ID
 export async function getTriggerFields(this: ILoadOptionsFunctions, filter?: string) {
 	// version supported is only 4
