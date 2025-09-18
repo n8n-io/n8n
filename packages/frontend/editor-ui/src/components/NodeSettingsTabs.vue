e3f5607720e889e297b66f0b718814a81bf0573e
yehorkardash <yehor.kardash@n8n.io>
2025-09-18 12:55:40 +0200

    fix type error

--- packages/frontend/editor-ui/src/components/NodeSettingsTabs.vue
+++ packages/frontend/editor-ui/src/components/NodeSettingsTabs.vue
@@ -5,14 +5,12 @@ import { useNDVStore } from '@/stores/ndv.store';
 import { useWorkflowsStore } from '@/stores/workflows.store';
 import type { INodeTypeDescription } from 'n8n-workflow';
 import { NodeConnectionTypes } from 'n8n-workflow';
-import { computed, onMounted } from 'vue';
+import { computed } from 'vue';
 
 import { useExternalHooks } from '@/composables/useExternalHooks';
 import { useInstalledCommunityPackage } from '@/composables/useInstalledCommunityPackage';
 import { useNodeDocsUrl } from '@/composables/useNodeDocsUrl';
 import { useTelemetry } from '@/composables/useTelemetry';
-import { useCommunityNodesStore } from '@/stores/communityNodes.store';
-import { useUsersStore } from '@/stores/users.store';
 import type { NodeSettingsTab } from '@/types/nodeSettings';
 import { N8nTabs } from '@n8n/design-system';
 import { useI18n } from '@n8n/i18n';
@@ -46,7 +44,6 @@ const workflowsStore = useWorkflowsStore();
 const i18n = useI18n();
 const telemetry = useTelemetry();
 const { docsUrl } = useNodeDocsUrl({ nodeType: () => props.nodeType });
-const communityNodesStore = useCommunityNodesStore();
 
 const activeNode = computed(() => ndvStore.activeNode);
 
@@ -164,12 +161,6 @@ function onTooltipClick(tab: NodeSettingsTab, event: MouseEvent) {
 		telemetry.track('user clicked cnr docs link', { source: 'node details view' });
 	}
 }
-
-onMounted(async () => {
-	if (isCommunityNode.value && useUsersStore().isInstanceOwner) {
-		installedPackage.value = await communityNodesStore.getInstalledPackage(packageName.value);
-	}
-});
 </script>
 
 <template>
