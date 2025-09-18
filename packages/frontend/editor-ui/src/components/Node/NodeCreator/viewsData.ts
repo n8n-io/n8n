1dcb8aa28bbdcf6b6b38904218001e049fd6bee4
Liviu-Nicolae Moraru <moraruliviunicolae1@gmail.com>
2025-09-18 15:34:39 +0300

    fix(editor-ui): Update icon in NodeCreator to use new form-grey.svg path

--- packages/frontend/editor-ui/src/components/Node/NodeCreator/viewsData.ts
+++ packages/frontend/editor-ui/src/components/Node/NodeCreator/viewsData.ts
@@ -445,7 +445,7 @@ export function TriggerView() {
 					description: i18n.baseText('nodeCreator.triggerHelperPanel.formTriggerDescription'),
 					iconData: {
 						type: 'file',
-						fileBuffer: `${window.BASE_PATH}static/webhook-icon.svg`,
+						fileBuffer: `${window.BASE_PATH}static/form-grey.svg`,
 					},
 				},
 			},
