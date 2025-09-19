3133597506232a3e23fd01a3bc0ea95459c148d7
Iván Ovejero <ivov.src@gmail.com>
2025-09-19 13:37:03 +0200

    Better docline

--- packages/@n8n/task-runner-python/src/errors/task_cancelled_error.py
+++ packages/@n8n/task-runner-python/src/errors/task_cancelled_error.py
@@ -1,5 +1,5 @@
 class TaskCancelledError(Exception):
-    """Raised when a task is cancelled by the broker."""
+    """Raised when a task is cancelled by broker message, by runner shutdown, etc."""
 
     def __init__(self):
         super().__init__("Task was cancelled")
