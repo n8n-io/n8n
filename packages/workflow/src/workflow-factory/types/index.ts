dba8ef5c728c26929a1b6800f49cd77ce3b253c7
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-09-24 16:56:43 +0300

    Export ConnectOptions type

--- packages/workflow/src/workflow-factory/types/index.ts
+++ packages/workflow/src/workflow-factory/types/index.ts
@@ -1,3 +1,3 @@
 export type { SupportedNodeTypes, NodeParametersFor } from './factory';
 export type { WorkflowSettings } from './workflow';
-export type { ConnectionOptions } from './connections';
+export type { ConnectionOptions, ConnectOptions } from './connections';
