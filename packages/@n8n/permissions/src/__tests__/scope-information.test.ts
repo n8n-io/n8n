32ba3e573e3d8011ae004076ec55a8a484a6d741
Charlie Kolb <charlie@n8n.io>
2025-09-25 12:18:59 +0200

    remove all other tests

--- packages/@n8n/permissions/src/__tests__/scope-information.test.ts
+++
@@ -1,7 +0,0 @@
-import { ALL_SCOPES } from '@/scope-information';
-
-describe('Scope Information', () => {
-	it('ensure scopes are defined correctly', () => {
-		expect(ALL_SCOPES).toMatchSnapshot();
-	});
-});
