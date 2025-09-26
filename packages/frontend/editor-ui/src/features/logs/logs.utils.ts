677cddab31e882792a7053f9bc2c3d685cb9ed66
Suguru Inoue <suguru@n8n.io>
2025-09-26 10:27:48 +0200

    perf(editor): Skip calculating log tree entirely when the panel is closed (#19870)

--- packages/frontend/editor-ui/src/features/logs/logs.utils.ts
+++ packages/frontend/editor-ui/src/features/logs/logs.utils.ts
@@ -609,3 +609,24 @@ export function isSubNodeLog(logEntry: LogEntry): boolean {
 export function isPlaceholderLog(treeNode: LogEntry): boolean {
 	return treeNode.runData === undefined;
 }
+
+/**
+ * Creates a copy of execution data just deep enough to keeps logs immutable and not reactive.
+ * We deliberately avoid full deep copy here for performance reason.
+ *
+ * TODO: use shallowRef() for execution data in workflows store to make this unnecessary.
+ */
+export function copyExecutionData(executionData: IExecutionResponse): IExecutionResponse {
+	return {
+		...executionData,
+		data: {
+			...executionData.data,
+			resultData: {
+				...executionData.data?.resultData,
+				runData: Object.fromEntries(
+					Object.entries(executionData.data?.resultData.runData ?? {}).map(([k, v]) => [k, [...v]]),
+				),
+			},
+		},
+	};
+}
