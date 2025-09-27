e8d10f3749b26733f6fbfbeaf8e6e87ec5bb45ab
vishal <vishal2010073@ssn.edu.in>
2025-09-28 05:05:49 +0530

    Fix: type check

--- packages/workflow/src/node-helpers.ts
+++ packages/workflow/src/node-helpers.ts
@@ -318,9 +318,9 @@ const checkConditions = (
 					const { from, to } = targetValue as { from: number; to: number };
 					return (propertyValue as number) >= from && (propertyValue as number) <= to;
 				}
-				if (key === 'includes') {
+				if (key === 'includes' && typeof propertyValue === 'string') {
 					// return false if propertyValue is undefined or null
-					return propertyValue && (propertyValue as string).includes(targetValue);
+					return propertyValue.includes(targetValue);
 				}
 				if (key === 'startsWith') {
 					return (propertyValue as string).startsWith(targetValue);
