5a576a6a630e5c207b857bc703f41c4f1fbd3e35
Michael Siega <michael.siega@n8n.io>
2025-09-30 16:53:44 +0200

    introduce sub-errors for cancellation reasons

--- packages/workflow/src/errors/execution-cancelled.error.ts
+++ packages/workflow/src/errors/execution-cancelled.error.ts
@@ -1,10 +1,33 @@
 import { ExecutionBaseError } from './abstract/execution-base.error';
 
 export class ExecutionCancelledError extends ExecutionBaseError {
-	constructor(executionId: string) {
+	// NOTE: we make the constructor protected because we should always use a more specific sub-error.
+	// If your case doesn't fit any of the sub-errors, then create a new one.
+	protected constructor(executionId: string) {
 		super('The execution was cancelled', {
 			level: 'warning',
 			extra: { executionId },
 		});
+	}
+}
+
+export class ManualExecutionCancelledError extends ExecutionCancelledError {
+	constructor(executionId: string) {
+		super(executionId);
+		this.message = 'The execution was cancelled manually';
+	}
+}
+
+export class TimeoutExecutionCancelledError extends ExecutionCancelledError {
+	constructor(executionId: string) {
+		super(executionId);
+		this.message = 'The execution was cancelled because it timed out';
+	}
+}
+
+export class SystemShutdownExecutionCancelledError extends ExecutionCancelledError {
+	constructor(executionId: string) {
+		super(executionId);
+		this.message = 'The execution was cancelled because the system is shutting down';
 	}
 }
