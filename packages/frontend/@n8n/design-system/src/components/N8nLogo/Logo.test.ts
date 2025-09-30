c6592750f2ef93ff0eedcdcb055083cfcefe54c5
Rob <robtf9@icloud.com>
2025-09-30 09:32:48 +0100

    test prop update

--- packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.test.ts
+++ packages/frontend/@n8n/design-system/src/components/N8nLogo/Logo.test.ts
@@ -5,21 +5,21 @@ import Logo from './Logo.vue';
 describe('Logo', () => {
 	it('renders the logo for authView location', () => {
 		const wrapper = render(Logo, {
-			props: { location: 'authView', releaseChannel: 'stable' },
+			props: { size: 'large', releaseChannel: 'stable' },
 		});
 		expect(wrapper.html()).toMatchSnapshot();
 	});
 
 	it('renders the logo for sidebar location when sidebar is expanded', () => {
 		const wrapper = render(Logo, {
-			props: { location: 'sidebar', collapsed: false, releaseChannel: 'stable' },
+			props: { size: 'small', collapsed: false, releaseChannel: 'stable' },
 		});
 		expect(wrapper.html()).toMatchSnapshot();
 	});
 
 	it('renders the logo for sidebar location when sidebar is collapsed', () => {
 		const wrapper = render(Logo, {
-			props: { location: 'sidebar', collapsed: true, releaseChannel: 'stable' },
+			props: { size: 'small', collapsed: true, releaseChannel: 'stable' },
 		});
 		expect(wrapper.html()).toMatchSnapshot();
 	});
