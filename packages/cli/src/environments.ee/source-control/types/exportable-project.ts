2d69556b50c9daadb910bcedc528e9ee01a7431f
Irénée <irenee.ajeneza@n8n.io>
2025-09-30 09:24:47 +0100

    feat: export project to json file in git folder

--- packages/cli/src/environments.ee/source-control/types/exportable-project.ts
+++ packages/cli/src/environments.ee/source-control/types/exportable-project.ts
@@ -3,7 +3,7 @@ import type { RemoteResourceOwner } from './resource-owner';
 export interface ExportableProject {
 	id: string;
 	name: string;
-	icon: { type: 'emoji' | 'icon'; value: string };
-	description: string;
+	icon: { type: 'emoji' | 'icon'; value: string } | null;
+	description: string | null;
 	owner: RemoteResourceOwner;
 }
