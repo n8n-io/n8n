n8n-io/n8n

commit e860dd6d2eb6a2dac5126bc60006d53b53115a68

feat(editor): Combine 'Move to Folder' and 'Change owner' modals (#15756)
This new modal also allows transferring entire folders to other projects and users.

packages/frontend/editor-ui/src/components/WorkflowCard.vue
@@ -26,7 +26,7 @@ import { ResourceType } from '@/utils/projects.utils';
 import type { EventBus } from '@n8n/utils/event-bus';
 import type { WorkflowResource } from './layouts/ResourcesListLayout.vue';
 import type { IUser } from 'n8n-workflow';
-import { ProjectTypes } from '@/types/projects.types';
+import { type ProjectSharingData, ProjectTypes } from '@/types/projects.types';
 import type { PathItem } from '@n8n/design-system/components/N8nBreadcrumbs/Breadcrumbs.vue';
 import { useFoldersStore } from '@/stores/folders.store';
 
@@ -62,7 +62,14 @@ const emit = defineEmits<{
 	'workflow:archived': [];
 	'workflow:unarchived': [];
 	'workflow:active-toggle': [value: { id: string; active: boolean }];
-	'action:move-to-folder': [value: { id: string; name: string; parentFolderId?: string }];
+	'action:move-to-folder': [
+		value: {
+			id: string;
+			name: string;
+			parentFolderId?: string;
+			sharedWithProjects?: ProjectSharingData[];
+		},
+	];
 }>();
 
 const toast = useToast();
@@ -140,17 +147,15 @@ const actions = computed(() => {
 		});
 	}
 
-	if (workflowPermissions.value.update && showFolders.value && !props.readOnly) {
+	if (
+		((workflowPermissions.value.update && !props.readOnly) ||
+			(workflowPermissions.value.move && projectsStore.isTeamProjectFeatureEnabled)) &&
+		showFolders.value &&
+		route.name !== VIEWS.SHARED_WORKFLOWS
+	) {
 		items.push({
 			label: locale.baseText('folders.actions.moveToFolder'),
 			value: WORKFLOW_LIST_ITEM_ACTIONS.MOVE_TO_FOLDER,
-		});
-	}
-
-	if (workflowPermissions.value.move && projectsStore.isTeamProjectFeatureEnabled) {
-		items.push({
-			label: locale.baseText('workflows.item.changeOwner'),
-			value: WORKFLOW_LIST_ITEM_ACTIONS.MOVE,
 		});
 	}
 
@@ -263,6 +268,7 @@ async function onAction(action: string) {
 				id: props.data.id,
 				name: props.data.name,
 				parentFolderId: props.data.parentFolder?.id,
+				sharedWithProjects: props.data.sharedWithProjects,
 			});
 			break;
 	}

cypress/composables/folders.ts
@@ -206,6 +206,14 @@ export function getMoveToFolderInput() {
 	return getMoveToFolderDropdown().find('input');
 }
 
+export function getProjectSharingInput() {
+	return cy.getByTestId('project-sharing-select');
+}
+
+export function getProjectSharingOption(name: string) {
+	return cy.getByTestId('project-sharing-info').contains(name);
+}
+
 export function getEmptyFolderDropdownMessage(text: string) {
 	return cy.get('.el-select-dropdown__empty').contains(text);
 }
@@ -500,7 +508,12 @@ function deleteFolderAndMoveContents(folderName: string, destinationName: string
 function moveFolder(folderName: string, destinationName: string) {
 	cy.intercept('PATCH', '/rest/projects/**').as('moveFolder');
 	getMoveFolderModal().should('be.visible');
-	getMoveFolderModal().find('h1').first().contains(`Move "${folderName}" to another folder`);
+	getMoveFolderModal().find('h1').first().contains(`Move folder ${folderName}`);
+
+	// The dropdown focuses after a small delay (once modal's slide in animation is done).
+	// On the component we listen for an event, but here the wait should be very predictable.
+	cy.wait(500);
+
 	// Try to find current folder in the dropdown
 	// This tests that auto-focus worked as expected
 	cy.focused().type(folderName, { delay: 50 });
@@ -513,4 +526,28 @@ function moveFolder(folderName: string, destinationName: string) {
 	getMoveToFolderOption(destinationName).should('be.visible').click();
 	getMoveFolderConfirmButton().should('be.enabled').click();
 	cy.wait('@moveFolder');
+}
+
+export function transferWorkflow(
+	workflowName: string,
+	projectName: string,
+	destinationFolder?: string,
+) {
+	getMoveFolderModal().should('be.visible');
+	getMoveFolderModal().find('h1').first().contains(`Move workflow ${workflowName}`);
+
+	cy.wait(500);
+
+	getProjectSharingInput().should('be.visible').click();
+	cy.focused().type(projectName, { delay: 50 });
+	getProjectSharingOption(projectName).should('be.visible').click();
+
+	if (destinationFolder) {
+		getMoveToFolderInput().click();
+		// Select destination folder
+		cy.focused().type(destinationFolder, { delay: 50 });
+		getMoveToFolderOption(destinationFolder).should('be.visible').click();
+	}
+
+	getMoveFolderConfirmButton().should('be.enabled').click();
 }

cypress/composables/projects.ts
@@ -5,6 +5,7 @@ const workflowPage = new WorkflowPage();
 const credentialsModal = new CredentialsModal();
 
 export const getHomeButton = () => cy.getByTestId('project-home-menu-item');
+export const getPersonalProjectsButton = () => cy.getByTestId('project-personal-menu-item');
 export const getMenuItems = () => cy.getByTestId('project-menu-item');
 export const getAddProjectButton = () => {
 	cy.getByTestId('universal-add').should('be.visible').click();
@@ -62,6 +63,8 @@ export const addProjectMember = (email: string, role?: string) => {
 };
 export const getResourceMoveModal = () => cy.getByTestId('project-move-resource-modal');
 export const getProjectMoveSelect = () => cy.getByTestId('project-move-resource-modal-select');
+export const getProjectSharingSelect = () => cy.getByTestId('project-sharing-select');
+export const getMoveToFolderSelect = () => cy.getByTestId('move-to-folder-dropdown');
 
 export function createProject(name: string) {
 	getAddProjectButton().click();

cypress/e2e/39-projects.cy.ts
@@ -1,4 +1,5 @@
 import { createResource } from '../composables/create';
+import { transferWorkflow } from '../composables/folders';
 import { setCredentialValues } from '../composables/modals/credential-modal';
 import {
 	clickCreateNewCredential,
@@ -37,6 +38,7 @@ describe('Projects', { disableAutoLogin: true }, () => {
 		beforeEach(() => {
 			cy.resetDatabase();
 			cy.enableFeature('sharing');
+			cy.enableFeature('folders');
 			cy.enableFeature('advancedPermissions');
 			cy.enableFeature('projectRole:admin');
 			cy.enableFeature('projectRole:editor');
@@ -246,21 +248,14 @@ describe('Projects', { disableAutoLogin: true }, () => {
 			projects.getProjectSettingsSaveButton().click();
 
 			// Move the workflow from Home to Project 1
-			projects.getHomeButton().click();
+			projects.getPersonalProjectsButton().click();
 			workflowsPage.getters.workflowCards().should('have.length', 1);
-			workflowsPage.getters.workflowCards().filter(':contains("Personal")').should('exist');
 			workflowsPage.getters.workflowCardActions('My workflow').click();
 			workflowsPage.getters.workflowMoveButton().click();
 
-			projects
-				.getResourceMoveModal()
-				.should('be.visible')
-				.contains('button', 'Move workflow')
-				.should('be.disabled');
-			projects.getProjectMoveSelect().click();
-			getVisibleSelect().find('li').should('have.length', 4);
-			getVisibleSelect().find('li').filter(':contains("Project 1")').click();
-			projects.getResourceMoveModal().contains('button', 'Move workflow').click();
+			transferWorkflow('My workflow', 'Project 1', 'No folder (project root)');
+
+			projects.getMenuItems().filter(':contains("Project 1")').click();
 
 			clearNotifications();
 			cy.wait('@getResources');
@@ -396,6 +391,7 @@ describe('Projects', { disableAutoLogin: true }, () => {
 		before(() => {
 			cy.resetDatabase();
 			cy.enableFeature('sharing');
+			cy.enableFeature('folders');
 			cy.enableFeature('advancedPermissions');
 			cy.enableFeature('projectRole:admin');
 			cy.enableFeature('projectRole:editor');
@@ -453,93 +449,35 @@ describe('Projects', { disableAutoLogin: true }, () => {
 			cy.signinAsOwner();
 			cy.visit(workflowsPage.url);
 
-			projects.getHomeButton().click();
-			workflowsPage.getters.workflowCards().should('have.length', 3);
-			workflowsPage.getters.workflowCards().filter(':contains("Personal")').should('exist');
+			projects.getPersonalProjectsButton().click();
+			workflowsPage.getters.workflowCards().should('have.length', 1);
 			workflowsPage.getters.workflowCardActions('Workflow in Home project').click();
 			workflowsPage.getters.workflowMoveButton().click();
 
-			projects
-				.getResourceMoveModal()
-				.should('be.visible')
-				.contains('button', 'Move workflow')
-				.should('be.disabled');
-			projects.getProjectMoveSelect().click();
-			getVisibleSelect().find('li').should('have.length', 5);
-			getVisibleSelect().find('li').filter(':contains("Project 1")').click();
-			projects.getResourceMoveModal().contains('button', 'Move workflow').click();
+			transferWorkflow('Workflow in Home project', 'Project 2', 'No folder (project root)');
 			clearNotifications();
-			cy.wait('@getResources');
 
-			workflowsPage.getters.workflowCards().should('have.length', 3);
-			workflowsPage.getters.workflowCards().filter(':contains("Personal")').should('not.exist');
+			workflowsPage.getters.workflowCards().should('have.length', 0);
 
 			cy.log('Move the workflow from Project 1 to Project 2');
 			projects.getMenuItems().first().click();
-			workflowsPage.getters.workflowCards().should('have.length', 2);
-			workflowsPage.getters.workflowCardActions('Workflow in Home project').click();
+			workflowsPage.getters.workflowCards().should('have.length', 1);
+			workflowsPage.getters.workflowCardActions('Workflow in Project 1').click();
 			workflowsPage.getters.workflowMoveButton().click();
 
-			projects
-				.getResourceMoveModal()
-				.should('be.visible')
-				.contains('button', 'Move workflow')
-				.should('be.disabled');
-			projects.getProjectMoveSelect().click();
-			getVisibleSelect().find('li').should('have.length', 5);
-			getVisibleSelect().find('li').filter(':contains("Project 2")').click();
-			projects.getResourceMoveModal().contains('button', 'Move workflow').click();
+			transferWorkflow('Workflow in Project 1', 'Project 2', 'No folder (project root)');
 			clearNotifications();
 
 			cy.log('Move the workflow from Project 2 to a member user');
 			projects.getMenuItems().last().click();
+			workflowsPage.getters.workflowCards().should('have.length', 3);
+			workflowsPage.getters.workflowCardActions('Workflow in Home project').click();
+			workflowsPage.getters.workflowMoveButton().click();
+
+			transferWorkflow('Workflow in Home project', INSTANCE_MEMBERS[0].email);
+			clearNotifications();
+
 			workflowsPage.getters.workflowCards().should('have.length', 2);
-			workflowsPage.getters.workflowCardActions('Workflow in Home project').click();
-			workflowsPage.getters.workflowMoveButton().click();
-
-			projects
-				.getResourceMoveModal()
-				.should('be.visible')
-				.contains('button', 'Move workflow')
-				.should('be.disabled');
-			projects.getProjectMoveSelect().click();
-			getVisibleSelect().find('li').should('have.length', 5);
-			getVisibleSelect().find('li').filter(`:contains("${INSTANCE_MEMBERS[0].email}")`).click();
-
-			projects.getResourceMoveModal().contains('button', 'Move workflow').click();
-			clearNotifications();
-			cy.wait('@getResources');
-
-			workflowsPage.getters.workflowCards().should('have.length', 1);
-
-			cy.log('Move the workflow from member user back to Home');
-			projects.getHomeButton().click();
-			workflowsPage.getters.workflowCards().should('have.length', 3);
-			workflowsPage.getters
-				.workflowCards()
-				.filter(':has([data-test-id="card-badge"]:contains("Project"))')
-				.should('have.length', 2);
-			workflowsPage.getters.workflowCardActions('Workflow in Home project').click();
-			workflowsPage.getters.workflowMoveButton().click();
-
-			projects
-				.getResourceMoveModal()
-				.should('be.visible')
-				.contains('button', 'Move workflow')
-				.should('be.disabled');
-			projects.getProjectMoveSelect().click();
-			getVisibleSelect().find('li').should('have.length', 5);
-			getVisibleSelect().find('li').filter(`:contains("${INSTANCE_OWNER.email}")`).click();
-
-			projects.getResourceMoveModal().contains('button', 'Move workflow').click();
-			clearNotifications();
-			cy.wait('@getResources');
-
-			workflowsPage.getters.workflowCards().should('have.length', 3);
-			workflowsPage.getters
-				.workflowCards()
-				.filter(':contains("Personal")')
-				.should('have.length', 1);
 		});
 
 		it('should move the credential to expected projects', () => {

cypress/e2e/49-folders.cy.ts
@@ -465,7 +465,7 @@ describe('Folders', () => {
 			goToPersonalProject();
 			createFolderFromProjectHeader('Test parent');
 			createFolderInsideFolder('Move me to root', 'Test parent');
-			moveFolderFromFolderCardActions('Move me to root', 'Personal');
+			moveFolderFromFolderCardActions('Move me to root', 'No folder (project root)');
 			// Parent folder should be empty
 			getFolderEmptyState().should('exist');
 			// Child folder should be in the root

cypress/pages/workflows.ts
@@ -43,7 +43,7 @@ export class WorkflowsPage extends BasePage {
 		workflowDeleteButton: () =>
 			cy.getByTestId('action-toggle-dropdown').filter(':visible').contains('Delete'),
 		workflowMoveButton: () =>
-			cy.getByTestId('action-toggle-dropdown').filter(':visible').contains('Change owner'),
+			cy.getByTestId('action-toggle-dropdown').filter(':visible').contains('Move'),
 		workflowFilterButton: () => cy.getByTestId('resources-list-filters-trigger').filter(':visible'),
 		workflowTagsDropdown: () => cy.getByTestId('tags-dropdown'),
 		workflowTagItem: (tag: string) => cy.getByTestId('tag').contains(tag),

packages/frontend/editor-ui/src/Interface.ts
@@ -373,7 +373,6 @@ export type BaseFolderItem = BaseResource & {
 	subFolderCount: number;
 	parentFolder?: ResourceParentFolder;
 	homeProject?: ProjectSharingData;
-	sharedWithProjects?: ProjectSharingData[];
 	tags?: ITag[];
 };
 
@@ -387,11 +386,15 @@ export interface FolderListItem extends BaseFolderItem {
 	resource: 'folder';
 }
 
-export interface ChangeLocationSearchResult extends BaseFolderItem {
-	resource: 'folder' | 'project';
+export interface ChangeLocationSearchResponseItem extends BaseFolderItem {
+	path: string[];
 }
 
 export type FolderPathItem = PathItem & { parentFolder?: string };
+
+export interface ChangeLocationSearchResult extends ChangeLocationSearchResponseItem {
+	resource: 'folder' | 'project';
+}
 
 export type WorkflowListResource = WorkflowListItem | FolderListItem;
 

packages/frontend/editor-ui/src/api/workflows.ee.ts
@@ -23,3 +23,23 @@ export async function moveWorkflowToProject(
 ): Promise<void> {
 	return await makeRestApiRequest(context, 'PUT', `/workflows/${id}/transfer`, body);
 }
+
+export async function moveFolderToProject(
+	context: IRestApiContext,
+	projectId: string,
+	folderId: string,
+	destinationProjectId: string,
+	destinationParentFolderId?: string,
+	shareCredentials?: string[],
+): Promise<void> {
+	return await makeRestApiRequest(
+		context,
+		'PUT',
+		`/projects/${projectId}/folders/${folderId}/transfer`,
+		{
+			destinationProjectId,
+			destinationParentFolderId: destinationParentFolderId ?? '0',
+			shareCredentials,
+		},
+	);
+}

commit 0e9c40a3bd12af5adc641c05ecda24719ff6b221

fix: Don't allow moving a readonly workflow
In a readonly instance, no workflow should be moved to another folder.
Believed to be a regression introduced in e860dd6d2eb6a2dac5126bc60006d53b53115a68

packages/frontend/editor-ui/src/components/WorkflowCard.vue
@@ -158,8 +158,10 @@ const actions = computed(() => {
 		});
 	}
 
+	// TODO: add test to verify that moving a readonly card is not possible
 	if (
-		((workflowPermissions.value.update && !props.readOnly) ||
+		!props.readOnly &&
+		(workflowPermissions.value.update ||
 			(workflowPermissions.value.move && projectsStore.isTeamProjectFeatureEnabled)) &&
 		showFolders.value &&
 		route.name !== VIEWS.SHARED_WORKFLOWS