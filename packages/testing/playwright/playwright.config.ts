7397433b2325b7a03e967fa30130a1e6b7aa0ca6
Declan Carroll <declan@n8n.io>
2025-09-30 23:02:47 +0100

    test: Only use currents fixtures in CI

--- packages/testing/playwright/playwright.config.ts
+++ packages/testing/playwright/playwright.config.ts
@@ -60,6 +60,7 @@ export default defineConfig<CurrentsFixtures, CurrentsWorkerFixtures>({
 		viewport: MACBOOK_WINDOW_SIZE,
 		actionTimeout: 20000, // TODO: We might need to make this dynamic for container tests if we have low resource containers etc
 		navigationTimeout: 10000,
+		currentsFixturesEnabled: !!process.env.CI,
 	},
 
 	reporter: IS_CI
