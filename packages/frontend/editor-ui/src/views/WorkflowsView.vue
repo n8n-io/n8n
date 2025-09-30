29d6ef2e040ac24cdd1b243a46b77ab7210031ac
Milorad FIlipović <milorad@n8n.io>
2025-09-30 10:52:00 +0200

    fix(editor): Add success toast when moving folders via breadcrumbs (no-changelog)

--- packages/frontend/editor-ui/src/views/WorkflowsView.vue
+++ packages/frontend/editor-ui/src/views/WorkflowsView.vue
@@ -1438,6 +1438,13 @@ const moveFolder = async (payload: {
 		if (isCurrentFolder && !payload.options?.skipNavigation) {
 			// If we just moved the current folder, automatically navigate to the new folder
 			void router.push(newFolderURL);
+			toast.showMessage({
+				title: i18n.baseText('folders.move.success.title'),
+				message: i18n.baseText('folders.move.success.messageNoAccess', {
+					interpolate: { folderName: payload.folder.name },
+				}),
+				type: 'success',
+			});
 		} else {
 			// Else show success message and update the list
 			toast.showToast({
