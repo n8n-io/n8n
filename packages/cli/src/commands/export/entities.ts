6d5eb5a32b5a6188bcf1fa19ab0938a7452fae48
Stephen <sjw948@gmail.com>
2025-10-02 10:38:34 +0100

    exclude large data tables, allow inclusion of them

--- packages/cli/src/commands/export/entities.ts
+++ packages/cli/src/commands/export/entities.ts
@@ -11,19 +11,35 @@ const flagsSchema = z.object({
 		.string()
 		.describe('Output directory path')
 		.default(safeJoinPath(__dirname, './outputs')),
+	includeLargeDataTables: z.boolean().describe('Include large data tables').default(false),
 });
 
 @Command({
 	name: 'export:entities',
 	description: 'Export database entities to JSON files',
-	examples: ['', '--outputDir=./exports', '--outputDir=/path/to/backup'],
+	examples: [
+		'',
+		'--outputDir=./exports',
+		'--outputDir=/path/to/backup',
+		'--includeLargeDataTables=true',
+	],
 	flagsSchema,
 })
 export class ExportEntitiesCommand extends BaseCommand<z.infer<typeof flagsSchema>> {
 	async run() {
 		const outputDir = this.flags.outputDir;
 
-		await Container.get(ExportService).exportEntities(outputDir);
+		const excludedLargeDataTables = new Set<string>();
+
+		if (!this.flags.includeLargeDataTables) {
+			excludedLargeDataTables.add('execution_annotation_tags');
+			excludedLargeDataTables.add('execution_annotations');
+			excludedLargeDataTables.add('execution_data');
+			excludedLargeDataTables.add('execution_entity');
+			excludedLargeDataTables.add('execution_metadata');
+		}
+
+		await Container.get(ExportService).exportEntities(outputDir, excludedLargeDataTables);
 	}
 
 	catch(error: Error) {
