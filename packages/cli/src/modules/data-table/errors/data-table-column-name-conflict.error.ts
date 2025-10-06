c118369a6a277594aefd5f67c7fd2b6ef3b90892
Ricardo Espinoza <ricardo@n8n.io>
2025-10-06 15:03:34 -0400

    more renaming

--- packages/cli/src/modules/data-table/errors/data-table-column-name-conflict.error.ts
+++ packages/cli/src/modules/data-table/errors/data-table-column-name-conflict.error.ts
@@ -1,9 +1,9 @@
 import { UserError } from 'n8n-workflow';
 
 export class DataTableColumnNameConflictError extends UserError {
-	constructor(columnName: string, dataStoreName: string) {
+	constructor(columnName: string, dataTableName: string) {
 		super(
-			`Data table column with name '${columnName}' already exists in data table '${dataStoreName}'`,
+			`Data table column with name '${columnName}' already exists in data table '${dataTableName}'`,
 			{
 				level: 'warning',
 			},
