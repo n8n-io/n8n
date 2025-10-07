17d81423b6691121d8d7b3a56cd440be6d1d6e3c
Mutasem Aldmour <mutasem@n8n.io>
2025-10-07 14:42:09 +0200

    refactor, add tests

--- packages/frontend/@n8n/design-system/src/components/N8nNodeIcon/NodeIcon.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nNodeIcon/NodeIcon.vue
@@ -1,11 +1,9 @@
 <script lang="ts" setup>
 import type { Placement } from 'element-plus';
-import { computed, getCurrentInstance } from 'vue';
+import { computed } from 'vue';
 
-import N8nIcon from '../N8nIcon';
-import type { IconName } from '../N8nIcon/icons';
-import { isSupportedIconName } from '../N8nIcon/icons';
 import N8nTooltip from '../N8nTooltip';
+import IconContent from './IconContent.vue';
 
 type IconType = 'file' | 'icon' | 'unknown';
 
@@ -42,44 +40,6 @@ const iconStyleData = computed((): Record<string, string> => {
 		'line-height': `${props.size}px`,
 	};
 });
-
-const badgeSize = computed((): number => {
-	switch (props.size) {
-		case 40:
-			return 18;
-		case 24:
-			return 10;
-		case 18:
-		default:
-			return 12;
-	}
-});
-
-const fontStyleData = computed((): Record<string, string> => {
-	if (!props.size) {
-		return {};
-	}
-
-	return {
-		'max-width': `${props.size}px`,
-	};
-});
-
-const badgeStyleData = computed((): Record<string, string> => {
-	const size = badgeSize.value;
-	return {
-		padding: `${Math.floor(size / 4)}px`,
-		right: `-${Math.floor(size / 2)}px`,
-		bottom: `-${Math.floor(size / 2)}px`,
-	};
-});
-
-const supportedIconName = computed((): IconName | undefined => {
-	return isSupportedIconName(props.name) ? props.name : undefined;
-});
-
-// Get self component to avoid dependency cycle
-const N8nNodeIcon = getCurrentInstance()?.type;
 </script>
 
 <template>
@@ -97,32 +57,24 @@ const N8nNodeIcon = getCurrentInstance()?.type;
 				<template #content>
 					{{ nodeTypeName }}
 				</template>
-				<!-- TODO dedupe logic here -->
-				<div v-if="type !== 'unknown'" :class="$style.icon">
-					<img v-if="type === 'file'" :src="src" :class="$style.nodeIconImage" />
-					<N8nIcon
-						v-else-if="supportedIconName"
-						:icon="supportedIconName"
-						:style="fontStyleData"
-						:class="$style.iconFa"
-					/>
-				</div>
-				<div v-else :class="$style.nodeIconPlaceholder">
-					{{ nodeTypeName ? nodeTypeName.charAt(0) : '?' }}
-				</div>
+				<IconContent
+					:type="type"
+					:src="src"
+					:name="name"
+					:node-type-name="nodeTypeName"
+					:size="size"
+					:badge="badge"
+				/>
 			</N8nTooltip>
-			<template v-else>
-				<div v-if="type !== 'unknown'" :class="$style.icon">
-					<img v-if="type === 'file'" :src="src" :class="$style.nodeIconImage" />
-					<N8nIcon v-else-if="supportedIconName" :icon="supportedIconName" :style="fontStyleData" />
-					<div v-if="badge" :class="$style.badge" :style="badgeStyleData">
-						<N8nNodeIcon :type="badge.type" :src="badge.src" :size="badgeSize" />
-					</div>
-				</div>
-				<div v-else :class="$style.nodeIconPlaceholder">
-					{{ nodeTypeName ? nodeTypeName.charAt(0) : '?' }}
-				</div>
-			</template>
+			<IconContent
+				v-else
+				:type="type"
+				:src="src"
+				:name="name"
+				:node-type-name="nodeTypeName"
+				:size="size"
+				:badge="badge"
+			/>
 		</div>
 	</div>
 </template>
@@ -138,40 +90,6 @@ const N8nNodeIcon = getCurrentInstance()?.type;
 	text-align: center;
 	font-weight: var(--font-weight-bold);
 	font-size: 20px;
-}
-
-.icon {
-	height: 100%;
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	position: relative;
-
-	svg {
-		max-width: 100%;
-		max-height: 100%;
-	}
-
-	img,
-	svg {
-		pointer-events: none;
-	}
-}
-.nodeIconPlaceholder {
-	text-align: center;
-}
-.nodeIconImage {
-	max-width: 100%;
-	max-height: 100%;
-	width: auto;
-	height: auto;
-}
-
-.badge {
-	position: absolute;
-	background: var(--color-background-node-icon-badge, var(--color-background-base));
-	border-radius: 50%;
 }
 
 .circle {
