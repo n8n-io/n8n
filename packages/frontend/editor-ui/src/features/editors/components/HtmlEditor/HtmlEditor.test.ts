722733a819cc53896282718dd44602dc5df25b8a
Alex Grozav <alex@grozav.com>
2025-10-07 18:44:31 +0300

    fix: fix broken paths

--- packages/frontend/editor-ui/src/features/editors/components/HtmlEditor/HtmlEditor.test.ts
+++ packages/frontend/editor-ui/src/features/editors/components/HtmlEditor/HtmlEditor.test.ts
@@ -7,7 +7,7 @@ import HtmlEditor from '@/features/editors/components/HtmlEditor/HtmlEditor.vue'
 import { userEvent } from '@testing-library/user-event';
 import { waitFor } from '@testing-library/vue';
 import { setActivePinia } from 'pinia';
-import { htmlEditorEventBus } from '../event-bus';
+import { htmlEditorEventBus } from '@/event-bus';
 
 const DEFAULT_SETUP = {
 	props: {
