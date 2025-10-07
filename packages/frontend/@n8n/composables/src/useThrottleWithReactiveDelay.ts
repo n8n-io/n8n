39605a6f05c5f996da7e123fee7b37a29744c35c
autologie <suguru@n8n.io>
2025-10-07 10:39:57 +0200

    add explicit return type

--- packages/frontend/@n8n/composables/src/useThrottleWithReactiveDelay.ts
+++ packages/frontend/@n8n/composables/src/useThrottleWithReactiveDelay.ts
@@ -1,10 +1,10 @@
 import { useThrottleFn } from '@vueuse/core';
-import { shallowRef, watch, type Ref } from 'vue';
+import { shallowRef, watch, type Ref, type ShallowRef } from 'vue';
 
 /**
  * Similar to `useThrottle` from @vueuse/core, but with changeable delay
  */
-export function useThrottleWithReactiveDelay<T>(state: Ref<T>, delay: Ref<number>) {
+export function useThrottleWithReactiveDelay<T>(state: Ref<T>, delay: Ref<number>): ShallowRef<T> {
 	const throttled = shallowRef(state.value);
 
 	watch(
