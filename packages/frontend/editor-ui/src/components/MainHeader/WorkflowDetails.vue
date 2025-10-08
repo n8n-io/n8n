b22d5f93e0c8b572d28af3e6601491c4b55aaa18
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-10-08 11:44:56 +0300

    Revert unintentional change

--- packages/frontend/editor-ui/src/components/MainHeader/WorkflowDetails.vue
+++ packages/frontend/editor-ui/src/components/MainHeader/WorkflowDetails.vue
@@ -185,7 +185,7 @@ const workflowMenuItems = computed<Array<ActionDropdownItem<WORKFLOW_MENU_ACTION
 	}
 
 	if (
-		(workflowPermissions.value.create === true && !props.readOnly && !props.isArchived) ||
+		(workflowPermissions.value.delete === true && !props.readOnly && !props.isArchived) ||
 		isNewWorkflow.value
 	) {
 		actions.unshift({
