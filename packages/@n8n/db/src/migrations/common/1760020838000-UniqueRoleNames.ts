bad4f4511f12a271c5250b3a20ed3dd887e749e6
Andreas Fitzek <andreas.fitzek@n8n.io>
2025-10-09 16:49:29 +0200

    Add migration to ensure unique display name for roles

---
+++ packages/@n8n/db/src/migrations/common/1760020838000-UniqueRoleNames.ts
@@ -0,0 +1,60 @@
+import type { Role } from '../../entities';
+import type { MigrationContext, ReversibleMigration } from '../migration-types';
+
+export class UniqueRoleNames1760020838000 implements ReversibleMigration {
+	async up({ isMysql, escape, runQuery }: MigrationContext) {
+		const tableName = escape.tableName('role');
+		const displayNameColumn = escape.columnName('displayName');
+		const slugColumn = escape.columnName('slug');
+		const createdAtColumn = escape.columnName('createdAt');
+		const allRoles: Array<Pick<Role, 'slug' | 'displayName'>> = await runQuery(
+			`SELECT ${slugColumn}, ${displayNameColumn} FROM ${tableName} ORDER BY ${displayNameColumn}, ${createdAtColumn} ASC`,
+		);
+
+		// Group roles by displayName in memory
+		const groupedByName = new Map<string, Array<Pick<Role, 'slug' | 'displayName'>>>();
+
+		for (const role of allRoles) {
+			const existing = groupedByName.get(role.displayName) || [];
+			existing.push(role);
+			groupedByName.set(role.displayName, existing);
+		}
+
+		for (const [_, roles] of groupedByName.entries()) {
+			if (roles.length > 1) {
+				const duplicates = roles.slice(1);
+				await Promise.all(
+					duplicates.map(async (role, index) => {
+						if (index === 0) return;
+						return await runQuery(
+							`UPDATE ${tableName} SET ${displayNameColumn} = :displayName WHERE ${slugColumn} = :slug`,
+							{
+								displayName: `${role.displayName} ${index + 1}`,
+								slug: role.slug,
+							},
+						);
+					}),
+				);
+			}
+		}
+
+		const indexName = escape.indexName('UniqueRoleDisplayName');
+		// MySQL cannot create an index on a column with a type of TEXT or BLOB without a length limit
+		// The (100) specifies the maximum length of the index key
+		// meaning that only the first 100 characters of the displayName column will be used for indexing
+		// But since in our DTOs we limit the displayName to 100 characters, we can safely use this prefix length
+		await runQuery(
+			isMysql
+				? `CREATE UNIQUE INDEX ${indexName} ON ${tableName} (${displayNameColumn}(100))`
+				: `CREATE UNIQUE INDEX ${indexName} ON ${tableName} (${displayNameColumn})`,
+		);
+	}
+
+	async down({ isMysql, escape, runQuery }: MigrationContext) {
+		const tableName = escape.tableName('role');
+		const indexName = escape.indexName('UniqueRoleDisplayName');
+		await runQuery(
+			isMysql ? `ALTER TABLE ${tableName} DROP INDEX ${indexName}` : `DROP INDEX ${indexName}`,
+		);
+	}
+}
