aff8c0b8b8a798f78c0d6eeeaf0e38e5b1504410
Mutasem Aldmour <mutasem@n8n.io>
2025-10-13 16:26:10 +0200

    remove chat package from replace script

--- scripts/replace-css-tokens.sh
+++ scripts/replace-css-tokens.sh
@@ -8,7 +8,7 @@ FRONTEND_DIR="./packages/frontend"
 
 # Find all files in frontend folder except _tokens.scss (which is already updated)
 echo "Finding files to process..."
-files=$(find "$FRONTEND_DIR" -type f \( -name "*.css" -o -name "*.scss" -o -name "*.vue" -o -name "*.ts" -o -name "*.snap" -o -name "*.test.ts" -o -name "*.js" -o -name "*.md" \) ! -path "*/node_modules/*" ! -path "*/dist/*" ! -path "*/.vite/*" ! -path "*/_tokens.deprecated.scss" ! -path "*/_tokens.dark.deprecated.scss")
+files=$(find "$FRONTEND_DIR" -type f \( -name "*.css" -o -name "*.scss" -o -name "*.vue" -o -name "*.ts" -o -name "*.snap" -o -name "*.test.ts" -o -name "*.js" -o -name "*.md" \) ! -path "*/node_modules/*" ! -path "*/dist/*" ! -path "*/.vite/*" ! -path "*/_tokens.deprecated.scss" ! -path "*/_tokens.dark.deprecated.scss" ! -path "*/@n8n/chat/**")
 
 # _tokens file only
 #files=$(find "$FRONTEND_DIR" -type f \( -name "_tokens.scss" \))
