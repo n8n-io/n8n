f882434e4301db8b83a992d055d1b20356fd816b
Michael Siega <michael.siega@n8n.io>
2025-10-13 16:52:45 +0200

    actually include all the files

---
+++ packages/@n8n/db/src/migrations/common/1760314000000-CreateWorkflowDependencyTable.ts
@@ -0,0 +1,32 @@
+import type { MigrationContext, ReversibleMigration } from '../migration-types';
+
+export class CreateWorkflowDependencyTable1760314000000 implements ReversibleMigration {
+	async up({ schemaBuilder: { createTable, column } }: MigrationContext) {
+		await createTable('workflow_dependency')
+			.withColumns(
+				column('id').uuid.primary,
+				column('workflowId').varchar(36).notNull,
+				column('workflowVersionId').int.notNull.comment('Version of the workflow'),
+				column('dependencyType')
+					.varchar(16)
+					.notNull.comment(
+						'Type of dependency: "credential", "nodeType", "webhookPath", or "subWorkflow"',
+					),
+				column('dependencyId').varchar(255).notNull.comment('ID or name of the dependency'),
+				column('nodeId').varchar(255).comment('Node ID for nodeType dependencies, null otherwise'),
+				column('indexVersionId').int.notNull.default(1).comment('Version of the index structure'),
+			)
+			.withForeignKey('workflowId', {
+				tableName: 'workflow_entity',
+				columnName: 'id',
+				onDelete: 'CASCADE',
+			})
+			.withIndexOn(['workflowId'])
+			.withIndexOn(['dependencyType'])
+			.withIndexOn(['dependencyId']).withTimestamps;
+	}
+
+	async down({ schemaBuilder: { dropTable } }: MigrationContext) {
+		await dropTable('workflow_dependency');
+	}
+}
---
+++ packages/@n8n/db/src/migrations/mysqldb/1760314000000-CreateWorkflowDependencyTable.ts
@@ -0,0 +1,5 @@
+import { CreateWorkflowDependencyTable1760314000000 as BaseMigration } from '../common/1760314000000-CreateWorkflowDependencyTable';
+
+export class CreateWorkflowDependencyTable1760314000000 extends BaseMigration {
+	transaction = false as const;
+}
