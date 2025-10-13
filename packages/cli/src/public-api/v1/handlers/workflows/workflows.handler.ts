9058be6e627729be4c059bd31905ed6f6f1ad7f5
Milorad FIlipović <milorad@n8n.io>
2025-10-13 11:13:44 +0200

    ✨ Add workflow activation events to event service

--- packages/cli/src/public-api/v1/handlers/workflows/workflows.handler.ts
+++ packages/cli/src/public-api/v1/handlers/workflows/workflows.handler.ts
@@ -368,6 +368,14 @@ export = {
 
 				workflow.active = true;
 
+				// Emit workflow-activated event so other modules can react
+				Container.get(EventService).emit('workflow-activated', {
+					user: req.user,
+					workflowId: workflow.id,
+					workflow,
+					publicApi: true,
+				});
+
 				return res.json(workflow);
 			}
 
@@ -401,6 +409,14 @@ export = {
 				await setWorkflowAsInactive(workflow.id);
 
 				workflow.active = false;
+
+				// Emit workflow-deactivated event so other modules can react
+				Container.get(EventService).emit('workflow-deactivated', {
+					user: req.user,
+					workflowId: workflow.id,
+					workflow,
+					publicApi: true,
+				});
 
 				return res.json(workflow);
 			}
