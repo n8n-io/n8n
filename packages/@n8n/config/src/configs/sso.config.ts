a150c7f4ae1a01b19371415d36b527d39e315283
Stephen Wright <sjw948@gmail.com>
2025-10-14 10:18:32 +0100

    feat: Add new env vars for sso provisioning configuration (#20749)

--- packages/@n8n/config/src/configs/sso.config.ts
+++ packages/@n8n/config/src/configs/sso.config.ts
@@ -1,5 +1,7 @@
 import { Config, Env, Nested } from '../decorators';
 
+type ScopesProvisioningFrequency = 'never' | 'first_login' | 'every_login';
+
 @Config
 class SamlConfig {
 	/** Whether to enable SAML SSO. */
@@ -28,6 +30,33 @@ class LdapConfig {
 }
 
 @Config
+class ProvisioningConfig {
+	/** Whether to provision the instance role from an SSO auth claim */
+	@Env('N8N_SSO_SCOPES_PROVISION_INSTANCE_ROLE')
+	scopesProvisionInstanceRole: boolean = false;
+
+	/** Whether to provision the project <> role mappings from an SSO auth claim */
+	@Env('N8N_SSO_SCOPES_PROVISION_PROJECT_ROLES')
+	scopesProvisionProjectRoles: boolean = false;
+
+	/** How often to trigger provisioning, never, fist login, or every login */
+	@Env('N8N_SSO_SCOPES_PROVISIONING_FREQUENCY')
+	scopesProvisioningFrequency: ScopesProvisioningFrequency = 'never';
+
+	/** The name of scope to request on oauth flows */
+	@Env('N8N_SSO_SCOPES_NAME')
+	scopesName: string = 'n8n';
+
+	/** The name of the expected claim to be returned for provisioning instance role */
+	@Env('N8N_SSO_SCOPES_INSTANCE_ROLE_CLAIM_NAME')
+	scopesInstanceRoleClaimName: string = 'n8n_instance_role';
+
+	/** The name of the expected claim to be returned for provisioning project <> role mappings */
+	@Env('N8N_SSO_SCOPES_PROJECTS_ROLES_CLAIM_NAME')
+	scopesProjectsRolesClaimName: string = 'n8n_projects';
+}
+
+@Config
 export class SsoConfig {
 	/** Whether to create users when they log in via SSO. */
 	@Env('N8N_SSO_JUST_IN_TIME_PROVISIONING')
@@ -45,4 +74,7 @@ export class SsoConfig {
 
 	@Nested
 	ldap: LdapConfig;
+
+	@Nested
+	provisioning: ProvisioningConfig;
 }
