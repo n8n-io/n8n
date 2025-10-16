b5786e845e8c0b9ff44dd1ede198f39e11f20cae
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-10-16 18:35:55 +0300

    Add todo

--- packages/frontend/editor-ui/src/features/dataTable/composables/useAgGrid.ts
+++ packages/frontend/editor-ui/src/features/dataTable/composables/useAgGrid.ts
@@ -229,6 +229,7 @@ export const useAgGrid = <TRowData extends Record<string, unknown> = Record<stri
 
 	onClickOutside(gridContainerRef, () => {
 		resetLastFocusedCell();
+		// TODO: this doesn't really work if the grid is short and the click is within the grid min-width
 		initializedGridApi.value.stopEditing();
 		initializedGridApi.value.clearFocusedCell();
 	});
