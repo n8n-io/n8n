037eb026ef2f84a8ccd6978f01733e3f69431ce1
Mutasem Aldmour <mutasem@n8n.io>
2025-10-20 11:40:44 +0200

    make thinner

--- packages/frontend/editor-ui/src/components/VirtualSchemaHeader.vue
+++ packages/frontend/editor-ui/src/components/VirtualSchemaHeader.vue
@@ -109,7 +109,7 @@ const emit = defineEmits<{
 }
 
 .icon-past-execution {
-	border: 2px dashed var(--color--foreground--shade-1);
+	border: 1px dashed var(--color--foreground--shade-1);
 }
 
 .title {
