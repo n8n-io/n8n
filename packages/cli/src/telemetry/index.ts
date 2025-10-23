786a86e79a0eb9a122609d8a2111beb041fdb45f
Iván Ovejero <ivov.src@gmail.com>
2025-10-23 12:57:30 +0200

    Use Error reporter instead

--- packages/cli/src/telemetry/index.ts
+++ packages/cli/src/telemetry/index.ts
@@ -10,7 +10,7 @@ import { OnShutdown } from '@n8n/decorators';
 import { Container, Service } from '@n8n/di';
 import type RudderStack from '@rudderstack/rudder-sdk-node';
 import axios from 'axios';
-import { InstanceSettings } from 'n8n-core';
+import { ErrorReporter, InstanceSettings } from 'n8n-core';
 import type { ITelemetryTrackProperties } from 'n8n-workflow';
 
 import { LOWEST_SHUTDOWN_PRIORITY, N8N_VERSION } from '@/constants';
@@ -60,6 +60,7 @@ export class Telemetry {
 		private readonly instanceSettings: InstanceSettings,
 		private readonly workflowRepository: WorkflowRepository,
 		private readonly globalConfig: GlobalConfig,
+		private readonly errorReporter: ErrorReporter,
 	) {}
 
 	async init() {
@@ -86,7 +87,7 @@ export class Telemetry {
 				dataPlaneUrl,
 				gzip: false,
 				errorHandler: (error) => {
-					this.logger.error('Telemetry request failed', { error });
+					this.errorReporter.error(error);
 				},
 			});
 
