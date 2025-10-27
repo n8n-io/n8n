5d5df17b5099e44aacc9c3557bd7e3796ba00e9f
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-10-27 13:33:47 +0200

    Prep extra links

--- packages/frontend/editor-ui/src/experiments/templatesDataGathering/stores/templatesDataGathering.store.ts
+++ packages/frontend/editor-ui/src/experiments/templatesDataGathering/stores/templatesDataGathering.store.ts
@@ -4,16 +4,19 @@ import { usePostHog } from '@/stores/posthog.store';
 import { useTemplatesStore } from '@/features/workflows/templates/templates.store';
 import { defineStore } from 'pinia';
 import templateIds from '../data/templateIds.json';
+import { useSettingsStore } from '@/stores/settings.store';
 
 export const useTemplatesDataGatheringStore = defineStore('templatesDataGathering', () => {
 	const telemetry = useTelemetry();
 	const posthogStore = usePostHog();
 	const templatesStore = useTemplatesStore();
+	const settingsStore = useSettingsStore();
 
 	const isFeatureEnabled = () => {
 		return (
+			settingsStore.isTemplatesEnabled &&
 			posthogStore.getVariant(TEMPLATES_DATA_GATHERING_EXPERIMENT.name) ===
-			TEMPLATES_DATA_GATHERING_EXPERIMENT.variant
+				TEMPLATES_DATA_GATHERING_EXPERIMENT.variant
 		);
 	};
 
