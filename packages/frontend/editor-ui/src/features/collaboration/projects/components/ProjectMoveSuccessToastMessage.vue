b394e6e07cb1ed486ff29e98cd8677514a288d24
Irénée <irenee.ajeneza@n8n.io>
2025-10-29 11:37:54 +0000

    fix(editor): Add comment for navigation handling in ProjectMoveSuccessToastMessage component

--- packages/frontend/editor-ui/src/features/collaboration/projects/components/ProjectMoveSuccessToastMessage.vue
+++ packages/frontend/editor-ui/src/features/collaboration/projects/components/ProjectMoveSuccessToastMessage.vue
@@ -34,6 +34,7 @@ const targetProjectName = computed(() => {
 			<span v-else>{{ i18n.baseText('projects.move.resource.success.message.workflow') }}</span>
 		</N8nText>
 		<p v-if="isTargetProjectTeam" class="pt-s">
+			<!-- The navigation should be handled by the component showing the toast  -->
 			<a href="#">
 				{{
 					i18n.baseText('projects.move.resource.success.link', {
