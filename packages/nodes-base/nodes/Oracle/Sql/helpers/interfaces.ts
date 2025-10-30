cced0dfe53eb5c9d95380421b7b439ea75f4ba58
sudarshan12s <sudarshan12s@gmail.com>
2025-10-30 22:05:55 +0530

    - Add missing attribute

--- packages/nodes-base/nodes/Oracle/Sql/helpers/interfaces.ts
+++ packages/nodes-base/nodes/Oracle/Sql/helpers/interfaces.ts
@@ -79,6 +79,7 @@ export type OracleDBNodeCredentials = {
 	poolMin: number;
 	poolMax: number;
 	poolIncrement: number;
+	privilege?: number;
 	sslServerCertDN?: string;
 	sslServerDNMatch?: boolean;
 	sslAllowWeakDNMatch?: boolean;
