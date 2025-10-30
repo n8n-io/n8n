e5201325938cd09b4dd97845b3b156545547a5ba
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-10-31 00:48:37 +0200

    feat: Add multiple headers credential type

---
+++ packages/nodes-base/credentials/HttpMultipleHeadersAuth.credentials.ts
@@ -0,0 +1,67 @@
+/* eslint-disable n8n-nodes-base/cred-class-name-unsuffixed */
+/* eslint-disable n8n-nodes-base/cred-class-field-name-unsuffixed */
+import type { IAuthenticate, ICredentialType, INodeProperties, Icon } from 'n8n-workflow';
+
+export class HttpMultipleHeadersAuth implements ICredentialType {
+	name = 'httpMultipleHeadersAuth';
+
+	displayName = 'Multiple Headers Auth';
+
+	documentationUrl = 'httprequest';
+
+	genericAuth = true;
+
+	icon: Icon = 'node:n8n-nodes-base.httpRequest';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Headers',
+			name: 'headers',
+			type: 'fixedCollection',
+			default: { values: [{ name: '', value: '' }] },
+			typeOptions: {
+				multipleValues: true,
+			},
+			options: [
+				{
+					displayName: 'Values',
+					name: 'values',
+					values: [
+						{
+							displayName: 'Name',
+							name: 'name',
+							type: 'string',
+							default: '',
+						},
+						{
+							displayName: 'Value',
+							name: 'value',
+							type: 'string',
+							default: '',
+							typeOptions: {
+								password: true,
+							},
+						},
+					],
+				},
+			],
+		},
+	];
+
+	authenticate: IAuthenticate = async (credentials, requestOptions) => {
+		const values = (credentials.headers as { values: Array<{ name: string; value: string }> })
+			.values;
+		const headers = values.reduce(
+			(acc, cur) => {
+				acc[cur.name] = cur.value;
+				return acc;
+			},
+			{} as Record<string, string>,
+		);
+		requestOptions.headers = {
+			...requestOptions.headers,
+			...headers,
+		};
+		return await Promise.resolve(requestOptions);
+	};
+}
