d08b557f1b707b70ca4a08f58f600b2c54db3d94
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-10-31 16:59:04 +0200

    Fix incorrect import

--- packages/frontend/editor-ui/src/init.test.ts
+++ packages/frontend/editor-ui/src/init.test.ts
@@ -9,7 +9,7 @@ import { useSourceControlStore } from '@/features/integrations/sourceControl.ee/
 import { useSSOStore } from '@/features/settings/sso/sso.store';
 import { useUsersStore } from '@/features/settings/users/users.store';
 import { useVersionsStore } from '@/app/stores/versions.store';
-import { useBannersStore } from '@/app/stores/banners.store';
+import { useBannersStore } from '@/features/shared/banners/banners.store';
 import type { Cloud, CurrentUserResponse } from '@n8n/rest-api-client';
 import type { IUser } from '@n8n/rest-api-client/api/users';
 import { STORES } from '@n8n/stores';
