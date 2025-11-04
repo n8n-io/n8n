d2dfdd3c0491da267ff90d8b5b917f99031042e0
Iván Ovejero <ivov.src@gmail.com>
2025-11-04 18:05:35 +0100

    Remove from all but `unplugin-swc`

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,13 +1,13 @@
 function readPackage(pkg) {
 	/**
-	 * Remove @swc/core from ts-node's and tsup's peer dependencies to prevent them from
-	 * switching over from tsc to swc. Client packages rely on these using tsc and will
-	 * produce an incorrect bundle when using @swc/core.
+	 * Remove @swc/core from all packages' peer dependencies, except for unplugin-swc.
+	 * Client packages rely on tsc and will produce incorrect bundles when using @swc/core.
 	 */
-	if (pkg.name === 'ts-node' || pkg.name === 'tsup') {
+	if (pkg.name !== 'unplugin-swc') {
 		if (pkg.peerDependencies?.['@swc/core']) delete pkg.peerDependencies['@swc/core'];
 		if (pkg.peerDependenciesMeta?.['@swc/core']) delete pkg.peerDependenciesMeta['@swc/core'];
 	}
+
 	return pkg;
 }
 
