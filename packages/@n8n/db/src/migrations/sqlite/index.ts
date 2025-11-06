6e9407f099d798137240fb9b4617d815fc8b14cc
Ricardo Espinoza <ricardo@n8n.io>
2025-11-06 15:57:18 -0500

    simplify migration

--- packages/@n8n/db/src/migrations/sqlite/index.ts
+++ packages/@n8n/db/src/migrations/sqlite/index.ts
@@ -45,7 +45,6 @@ import { UpdateParentFolderIdColumn1740445074052 } from './1740445074052-UpdateP
 import { AddScopesColumnToApiKeys1742918400000 } from './1742918400000-AddScopesColumnToApiKeys';
 import { AddWorkflowVersionColumn1761047826451 } from './1761047826451-AddWorkflowVersionColumn';
 import { ChangeDependencyInfoToJson1761655473000 } from './1761655473000-ChangeDependencyInfoToJson';
-import { AddWorkflowDescriptionColumn1762177736257 } from './1762177736257-AddWorkflowDescriptionColumn';
 import { UniqueWorkflowNames1620821879465 } from '../common/1620821879465-UniqueWorkflowNames';
 import { UpdateWorkflowCredentials1630330987096 } from '../common/1630330987096-UpdateWorkflowCredentials';
 import { AddNodeIds1658930531669 } from '../common/1658930531669-AddNodeIds';
@@ -107,6 +106,7 @@ import { CreateOAuthEntities1760116750277 } from '../common/1760116750277-Create
 import { CreateWorkflowDependencyTable1760314000000 } from '../common/1760314000000-CreateWorkflowDependencyTable';
 import { DropUnusedChatHubColumns1760965142113 } from '../common/1760965142113-DropUnusedChatHubColumns';
 import { AddAudienceColumnToApiKeys1758731786132 } from './../common/1758731786132-AddAudienceColumnToApiKey';
+import { AddWorkflowDescriptionColumn1762177736257 } from './../common/1762177736257-AddWorkflowDescriptionColumn';
 
 const sqliteMigrations: Migration[] = [
 	InitialMigration1588102412422,
