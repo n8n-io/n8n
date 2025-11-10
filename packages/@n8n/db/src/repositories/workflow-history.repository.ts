bf6310fca6826abf71ea4d17c888bb0b2c281e6b
Daria Staferova <daria.staferova@n8n.io>
2025-11-10 16:56:14 +0200

    Remove unnecessary filter

--- packages/@n8n/db/src/repositories/workflow-history.repository.ts
+++ packages/@n8n/db/src/repositories/workflow-history.repository.ts
@@ -17,12 +17,11 @@ export class WorkflowHistoryRepository extends Repository<WorkflowHistory> {
 	 * Delete workflow history records earlier than a given date, except for current workflow versions.
 	 */
 	async deleteEarlierThanExceptCurrent(date: Date) {
-		const sub = this.manager
+		const currentVersionIdsSubquery = this.manager
 			.createQueryBuilder()
 			.subQuery()
 			.select('w.versionId')
 			.from(WorkflowEntity, 'w')
-			.where('w.versionId IS NOT NULL')
 			.getQuery();
 
 		return await this.manager
@@ -30,7 +29,7 @@ export class WorkflowHistoryRepository extends Repository<WorkflowHistory> {
 			.delete()
 			.from(WorkflowHistory)
 			.where('createdAt < :date', { date })
-			.andWhere(`versionId NOT IN (${sub})`)
+			.andWhere(`versionId NOT IN (${currentVersionIdsSubquery})`)
 			.execute();
 	}
 }
