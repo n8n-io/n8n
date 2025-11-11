7c35aa0371f22e8877f7e98c828b414f1f4cd086
Daria Staferova <daria.staferova@n8n.io>
2025-11-11 15:30:08 +0200

    Fix import order

--- packages/@n8n/db/src/migrations/mysqldb/index.ts
+++ packages/@n8n/db/src/migrations/mysqldb/index.ts
@@ -111,7 +111,7 @@ import { CreateOAuthEntities1760116750277 } from '../common/1760116750277-Create
 import { CreateWorkflowDependencyTable1760314000000 } from '../common/1760314000000-CreateWorkflowDependencyTable';
 import { AddWorkflowDescriptionColumn1762177736257 } from '../common/1762177736257-AddWorkflowDescriptionColumn';
 import { BackfillMissingWorkflowHistoryRecords1762763704614 } from '../common/1762763704614-BackfillMissingWorkflowHistoryRecords';
-import { AddActiveVersionIdColumn1762819200000 } from './../common/1762819200000-AddActiveVersionIdColumn';
+import { AddActiveVersionIdColumn1762819200000 } from '../common/1762819200000-AddActiveVersionIdColumn';
 import type { Migration } from '../migration-types';
 
 export const mysqlMigrations: Migration[] = [
