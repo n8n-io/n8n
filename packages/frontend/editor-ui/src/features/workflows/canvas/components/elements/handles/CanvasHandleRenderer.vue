febc00694ca47477a11a51c31861caf185997e1d
Tuukka Kantola <tuukka@n8n.io>
2025-11-12 11:37:34 +0100

    Replace rectangle node hadles with dot handles

--- packages/frontend/editor-ui/src/features/workflows/canvas/components/elements/handles/CanvasHandleRenderer.vue
+++ packages/frontend/editor-ui/src/features/workflows/canvas/components/elements/handles/CanvasHandleRenderer.vue
@@ -180,13 +180,6 @@ provide(CanvasNodeHandleKey, {
 	z-index: 1;
 	background: transparent;
 	border-radius: 0;
-
-	&.inputs {
-		&.main {
-			/* stylelint-disable-next-line @n8n/css-var-naming */
-			--handle--indicator--width: calc(8px * var(--canvas-zoom-compensation-factor, 1));
-		}
-	}
 }
 
 .renderType {
