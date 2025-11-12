43f49300aa371472ed5994e399a16bafbdbb6bb0
Tuukka Kantola <tuukka@n8n.io>
2025-11-12 22:29:39 +0100

    Add animation and larger target sizes to handles

--- packages/frontend/editor-ui/src/features/workflows/canvas/components/elements/handles/render-types/parts/CanvasHandleDot.vue
+++ packages/frontend/editor-ui/src/features/workflows/canvas/components/elements/handles/render-types/parts/CanvasHandleDot.vue
@@ -10,19 +10,34 @@ withDefaults(
 </script>
 
 <template>
-	<div :class="[$style.dot, handleClasses]" />
+	<div :class="$style.wrapper">
+		<div :class="[$style.dot, handleClasses]" />
+	</div>
 </template>
 
 <style lang="scss" module>
+.wrapper {
+	position: relative;
+	padding: 4px;
+	margin: -4px;
+}
+
 .dot {
 	width: var(--handle--indicator--width);
 	height: var(--handle--indicator--height);
 	border-radius: 50%;
 	background: light-dark(var(--color--neutral-white), var(--color--neutral-850));
 	border: 1px solid light-dark(var(--color--neutral-300), var(--color--neutral-500));
+	transition:
+		transform 0.2s ease,
+		background 0.2s ease,
+		border-width 0.1s ease;
 
-	&:hover {
-		background: var(--color--primary);
+	.wrapper:hover & {
+		/* background: var(--color--primary); */
+		border: 2px solid light-dark(var(--color--neutral-300), var(--color--neutral-300));
+		background: light-dark(var(--color--neutral-300), var(--color--neutral-700));
+		transform: scale(1.5);
 	}
 }
 </style>
