8cdd7b30b61f86e5c9eca6ba6e7551f87450eac1
Alex Grozav <alex@grozav.com>
2025-11-13 10:15:35 +0200

    fix: simplify example

--- packages/@n8n/workflow-json-sdk/playground/index.ts
+++ packages/@n8n/workflow-json-sdk/playground/index.ts
@@ -1,53 +1,28 @@
-/* eslint-disable id-denylist */
 import fs from 'fs';
 
 import { workflow, NodeTypes } from '../src';
 
-function createTestWorkflow() {
-	const wf = workflow({ name: 'My workflow' });
+const wf = workflow({ name: 'My workflow' });
 
-	const manualTrigger = wf.node('Manual Trigger').type(NodeTypes.ManualTrigger).parameters({});
-	const setA = wf
-		.node('Edit Fields A')
-		.type(NodeTypes.Set_v3_4)
-		.position(200, -150)
-		.parameters({ options: {} });
-	const setB = wf
-		.node('Edit Fields B')
-		.type(NodeTypes.Set_v3_4)
-		.position(200, 0)
-		.parameters({ options: {} });
-	const setC = wf
-		.node('Edit Fields C')
-		.type(NodeTypes.Set_v3_4)
-		.position(200, 150)
-		.parameters({
-			values: {
-				string: [
-					{
-						value: 'n8n',
-					},
-				],
-			},
-		});
-	const notion = wf
-		.node('Search a page on Notion')
-		.type(NodeTypes.Notion_v2_2)
-		.position(400, 0)
-		.parameters({
-			operation: 'search',
-		});
+const manualTrigger = wf.node('Manual Trigger').type(NodeTypes.ManualTrigger);
+const setA = wf.node('Edit Fields A').type(NodeTypes.Set_v3_4).position(200, -150);
+const setB = wf.node('Edit Fields B').type(NodeTypes.Set_v3_4).position(200, 0);
+const setC = wf.node('Edit Fields C').type(NodeTypes.Set_v3_4).position(200, 150);
+const notion = wf
+	.node('Search a page on Notion')
+	.type(NodeTypes.Notion_v2_2)
+	.position(400, 0)
+	.parameters({
+		operation: 'search',
+	});
 
-	wf.connection().from(manualTrigger).to([setA, setB]);
-	wf.connection()
-		.from({ node: manualTrigger, type: 'main', index: 0 })
-		.to([setA, setB])
-		.to({ node: setC, type: 'main', index: 0 });
-	wf.connection().from(setB).to(notion);
+wf.connection().from(manualTrigger).to([setA, setB]);
+wf.connection()
+	.from({ node: manualTrigger, type: 'main', index: 0 })
+	.to([setA, setB])
+	.to({ node: setC, type: 'main', index: 0 });
+wf.connection().from(setB).to(notion);
 
-	return wf.toJSON();
-}
-
-const workflowJSON = createTestWorkflow();
+const workflowJSON = wf.toJSON();
 
 fs.writeFileSync('output.json', JSON.stringify(workflowJSON, null, 4), 'utf-8');
