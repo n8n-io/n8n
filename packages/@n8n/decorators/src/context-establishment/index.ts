822cc51bebd7c42897b6e25275a360347c5d5ec2
Andreas Fitzek <andreas.fitzek@n8n.io>
2025-11-20 10:25:46 +0100

    Interface definitions

--- packages/@n8n/decorators/src/context-establishment/index.ts
+++ packages/@n8n/decorators/src/context-establishment/index.ts
@@ -1,15 +1,5 @@
-import { Container, Service } from '@n8n/di';
-
-import type { ContextEstablishmentHookClass } from './context-establishment-hook';
-import { ContextEstablishmentHookMetadata } from './context-establishment-hook-metadata';
-
-export const ContextEstablishmentHook =
-	<T extends ContextEstablishmentHookClass>(opts: { name: string }) =>
-	(target: T) => {
-		Container.get(ContextEstablishmentHookMetadata).register(opts.name, {
-			class: target,
-		});
-
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-		return Service()(target);
-	};
+export {
+	ContextEstablishmentHookMetadata,
+	ContextEstablishmentHook,
+} from './context-establishment-hook-metadata';
+export type * from './context-establishment-hook';
