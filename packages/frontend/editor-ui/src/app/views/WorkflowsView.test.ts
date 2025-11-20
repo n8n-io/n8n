ac91020bd3948a2b07c736cc197a90023628a56e
Daria <daria.staferova@n8n.io>
2025-11-20 17:47:24 +0200

    feat(core): Use active version instead of current version (no-changelog) (#21202)

--- packages/frontend/editor-ui/src/app/views/WorkflowsView.test.ts
+++ packages/frontend/editor-ui/src/app/views/WorkflowsView.test.ts
@@ -427,6 +427,7 @@ describe('Folders', () => {
 		createdAt: new Date().toISOString(),
 		updatedAt: new Date().toISOString(),
 		active: true,
+		activeVersionId: 'v1',
 		isArchived: false,
 		versionId: '1',
 		homeProject: {
