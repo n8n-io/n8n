0c1629f47f21d74b7077cfe6da651f462f6cf5f1
Iván Ovejero <ivov.src@gmail.com>
2025-11-21 12:43:35 +0100

    refactor(core): Update binary data config

--- packages/core/src/binary-data/utils.ts
+++ packages/core/src/binary-data/utils.ts
@@ -4,13 +4,7 @@ import type { Readable } from 'node:stream';
 
 import type { BinaryData } from './types';
 
-export const CONFIG_MODES = ['default', 'filesystem', 's3'] as const;
-
-const STORED_MODES = ['filesystem', 'filesystem-v2', 's3'] as const;
-
-export function areConfigModes(modes: string[]): modes is BinaryData.ConfigMode[] {
-	return modes.every((m) => CONFIG_MODES.includes(m as BinaryData.ConfigMode));
-}
+const STORED_MODES = ['filesystem', 'filesystem-v2', 's3', 'database'] as const;
 
 export function isStoredMode(mode: string): mode is BinaryData.StoredMode {
 	return STORED_MODES.includes(mode as BinaryData.StoredMode);
