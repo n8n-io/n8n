69e092a263fc5890d783247eb2759d1419f9c7f1
RomanDavydchuk <roman.davydchuk@n8n.io>
2025-11-21 10:50:32 +0200

    feat(MCP Client Node): New node (#21942)

--- packages/@n8n/nodes-langchain/nodes/mcp/McpClientTool/types.ts
+++ packages/@n8n/nodes-langchain/nodes/mcp/McpClientTool/types.ts
@@ -1,14 +1,1 @@
-import type { JSONSchema7 } from 'json-schema';
-
-export type McpTool = { name: string; description?: string; inputSchema: JSONSchema7 };
-
-export type McpServerTransport = 'sse' | 'httpStreamable';
-
 export type McpToolIncludeMode = 'all' | 'selected' | 'except';
-
-export type McpAuthenticationOption =
-	| 'none'
-	| 'headerAuth'
-	| 'bearerAuth'
-	| 'mcpOAuth2Api'
-	| 'multipleHeadersAuth';
