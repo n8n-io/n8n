f7b80940621b7d3566697522af74e5cc6cfb1520
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-11-21 18:59:27 +0200

    Add name input

--- packages/frontend/@n8n/rest-api-client/src/api/workflowHistory.ts
+++ packages/frontend/@n8n/rest-api-client/src/api/workflowHistory.ts
@@ -9,6 +9,8 @@ export type WorkflowHistory = {
 	createdAt: string;
 	updatedAt: string;
 	workflowPublishHistory: WorkflowPublishHistory[];
+	name?: string;
+	description?: string;
 };
 
 export type WorkflowPublishHistory = {
