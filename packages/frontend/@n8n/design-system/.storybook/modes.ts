eae2ded0f4a72968a0d574a286d7ae1707db4767
Milorad FIlipović <milorad@n8n.io>
2025-11-21 19:58:27 +0100

    👌 Fix background expression

--- packages/frontend/@n8n/design-system/.storybook/modes.ts
+++ packages/frontend/@n8n/design-system/.storybook/modes.ts
@@ -1,10 +1,10 @@
 export const allModes = {
 	dark: {
-		backgrounds: { value: 'var(--color--background--shade-2' },
+		backgrounds: { value: 'var(--color--background--shade-2)' },
 		theme: 'dark',
 	},
 	light: {
-		backgrounds: { value: 'var(--color--background--light-3' },
+		backgrounds: { value: 'var(--color--background--light-3)' },
 		theme: 'light',
 	},
 } as const;
