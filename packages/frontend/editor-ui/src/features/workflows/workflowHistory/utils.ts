4318923446e02fe67351f9d8db270361db54bb2d
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-11-21 19:14:43 +0200

    Generate random default name

--- packages/frontend/editor-ui/src/features/workflows/workflowHistory/utils.ts
+++ packages/frontend/editor-ui/src/features/workflows/workflowHistory/utils.ts
@@ -5,3 +5,19 @@ export const getLastPublishedByUser = (workflowPublishHistory: WorkflowPublishHi
 		(history) => history.mode === 'activate' && history.userId !== null,
 	);
 };
+
+const generateRandomSuffix = (length: number) => {
+	const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+
+	for (let i = 0; i < length; i++) {
+		const index = Math.floor(Math.random() * characters.length);
+		result += characters[index] ?? '';
+	}
+
+	return result;
+};
+
+export const generateVersionName = () => {
+	return `Version ${generateRandomSuffix(6)}`;
+};
