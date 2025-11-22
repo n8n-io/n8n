56335953f19daecbf4be1993b1b54d39bd457ba9
Milorad FIlipović <milorad@n8n.io>
2025-11-22 11:09:10 +0100

    🔥 Remove unused background from mode

--- packages/frontend/@n8n/design-system/.storybook/modes.ts
+++ packages/frontend/@n8n/design-system/.storybook/modes.ts
@@ -1,10 +1,8 @@
 export const allModes = {
 	dark: {
-		backgrounds: { value: 'var(--color--background--shade-2)' },
 		theme: 'dark',
 	},
 	light: {
-		backgrounds: { value: 'var(--color--background--light-3)' },
 		theme: 'light',
 	},
 } as const;
