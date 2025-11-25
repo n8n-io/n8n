f91f3e6a167993cebb82d6d9f52abce619bab003
Iván Ovejero <ivov.src@gmail.com>
2025-11-25 17:04:26 +0100

    Update tests

--- packages/@n8n/config/src/configs/__tests__/database.config.test.ts
+++ packages/@n8n/config/src/configs/__tests__/database.config.test.ts
@@ -17,35 +17,9 @@ describe('SqliteConfig', () => {
 			expect(config.poolSize).toBe(2);
 		});
 
-		test('should accept pool size of 3 (default)', () => {
+		test('should have default pool size of 3', () => {
 			const config = Container.get(SqliteConfig);
 			expect(config.poolSize).toBe(3);
-		});
-
-		test('should accept pool size of 1', () => {
-			const config = Container.get(SqliteConfig);
-			expect(config.poolSize).toBe(1);
-		});
-
-		test('should reject pool size of 0', () => {
-			const config = Container.get(SqliteConfig);
-			expect(() => {
-				config.poolSize = 0;
-			}).toThrow();
-		});
-
-		test('should reject negative pool size', () => {
-			const config = Container.get(SqliteConfig);
-			expect(() => {
-				config.poolSize = -1;
-			}).toThrow();
-		});
-
-		test('should reject non-integer pool size', () => {
-			const config = Container.get(SqliteConfig);
-			expect(() => {
-				config.poolSize = 2.5;
-			}).toThrow();
 		});
 	});
 });
