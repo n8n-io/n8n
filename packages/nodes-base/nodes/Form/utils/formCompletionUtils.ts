0acc6a0a38165bd60649bc51c8df7c39c5357075
Anton Engelhardt <antoncengelhardt@icloud.com>
2025-11-25 15:06:12 +0100

    fix(form): dont replace any <br> tag as sanitze will do it anyways
    Signed-off-by: Anton Engelhardt <antoncengelhardt@icloud.com>

--- packages/nodes-base/nodes/Form/utils/formCompletionUtils.ts
+++ packages/nodes-base/nodes/Form/utils/formCompletionUtils.ts
@@ -75,11 +75,9 @@ export const renderFormCompletion = async (
 		res.setHeader('Content-Security-Policy', SANDBOX_CSP);
 	}
 
-	// Process line breaks in completion message: convert \n and </br> to <br> for HTML rendering
+	// Process line breaks in completion message: convert \n to <br /> for HTML rendering
 	let processedMessage = completionMessage;
-	processedMessage = (processedMessage || '')
-		.replace(/\\n/g, '<br />')
-		.replace(/<\/br>/g, '<br />');
+	processedMessage = (processedMessage || '').replace(/\\n/g, '<br />');
 
 	res.render('form-trigger-completion', {
 		title: completionTitle,
