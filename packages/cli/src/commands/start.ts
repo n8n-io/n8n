e999b3433c725946cc42748a3ee81ade7cde9e6c
Iván Ovejero <ivov.src@gmail.com>
2025-11-26 14:48:50 +0100

    Remove unused import

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -7,7 +7,7 @@ import { Container } from '@n8n/di';
 import glob from 'fast-glob';
 import { createReadStream, createWriteStream, existsSync } from 'fs';
 import { mkdir } from 'fs/promises';
-import { jsonParse, randomString, type IWorkflowExecutionDataProcess } from 'n8n-workflow';
+import { jsonParse, type IWorkflowExecutionDataProcess } from 'n8n-workflow';
 import path from 'path';
 import replaceStream from 'replacestream';
 import { pipeline } from 'stream/promises';
