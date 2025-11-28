c5127db2f43e9f06bafd7e6dc3e5311c16bf5d31
Irénée <irenee.ajeneza@n8n.io>
2025-11-28 12:02:29 +0000

    feat: replace middleware licence checks by module level check

--- packages/cli/src/modules/source-control.ee/source-control.module.ts
+++ packages/cli/src/modules/source-control.ee/source-control.module.ts
@@ -2,7 +2,7 @@ import type { ModuleInterface } from '@n8n/decorators';
 import { BackendModule } from '@n8n/decorators';
 import { Container } from '@n8n/di';
 
-@BackendModule({ name: 'source-control' })
+@BackendModule({ name: 'source-control', licenseFlag: 'feat:sourceControl' })
 export class SourceControlModule implements ModuleInterface {
 	async init() {
 		await import('./source-control.controller.ee');
