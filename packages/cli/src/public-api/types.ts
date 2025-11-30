b6f8050dfab80647fa2ed336c23566654918f936
Daria <daria.staferova@n8n.io>
2025-12-01 00:56:37 +0200

    feat(core): Add get version public endpoint (#22407)

--- packages/cli/src/public-api/types.ts
+++ packages/cli/src/public-api/types.ts
@@ -91,6 +91,7 @@ export declare namespace WorkflowRequest {
 	type GetTags = Get;
 	type UpdateTags = AuthenticatedRequest<{ id: string }, {}, TagEntity[]>;
 	type Transfer = AuthenticatedRequest<{ id: string }, {}, { destinationProjectId: string }>;
+	type GetVersion = AuthenticatedRequest<{ id: string; versionId: string }, {}, {}, {}>;
 }
 
 export declare namespace UserRequest {
