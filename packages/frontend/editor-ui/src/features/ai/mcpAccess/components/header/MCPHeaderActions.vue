4eb498d0d6dbeddf1e27f613680287f6d4d6e37a
Milorad FIlipović <milorad@n8n.io>
2025-12-03 09:10:39 +0100

    🔨 Re-organize components

--- packages/frontend/editor-ui/src/features/ai/mcpAccess/components/header/MCPHeaderActions.vue
+++ packages/frontend/editor-ui/src/features/ai/mcpAccess/components/header/MCPHeaderActions.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { MCP_DOCS_PAGE_URL } from '@/features/ai/mcpAccess/mcp.constants';
 import ExternalLink from '@n8n/design-system/components/N8nExternalLink/ExternalLink.vue';
-import MCPAccessToggle from '@/features/ai/mcpAccess/components/MCPAccessToggle.vue';
-import McpConnectPopover from '@/features/ai/mcpAccess/components/McpConnectPopover.vue';
+import MCPAccessToggle from '@/features/ai/mcpAccess/components/header/MCPAccessToggle.vue';
+import McpConnectPopover from '@/features/ai/mcpAccess/components/header/connectionPopover/McpConnectPopover.vue';
 import { useI18n } from '@n8n/i18n';
 
 type Props = {
@@ -29,6 +29,7 @@ const i18n = useI18n();
 			:model-value="true"
 			:disabled="props.toggleDisabled"
 			:loading="props.loading"
+			:class="$style['mcp-access-toggle']"
 			@disable-mcp-access="emit('disableMcpAccess')"
 		/>
 		<McpConnectPopover />
@@ -38,11 +39,15 @@ const i18n = useI18n();
 <style lang="scss" module>
 .container {
 	display: flex;
-	gap: var(--spacing--lg);
+	gap: var(--spacing--md);
 	align-items: center;
 
+	// Link doesn't have outline when not hovered
+	// so we pull it closer to the rest of the elements
+	// to make horizontal alignment visually balanced
 	a {
 		font-size: var(--font-size--2xs);
+		margin-right: calc(-1 * var(--spacing--4xs));
 	}
 }
 </style>
