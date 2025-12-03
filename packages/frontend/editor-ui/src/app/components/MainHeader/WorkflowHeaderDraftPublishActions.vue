7a26d1aa4024708a48fc03ba5d599272441bb407
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2025-12-03 16:22:35 +0200

    Add back the collaborators pane

--- packages/frontend/editor-ui/src/app/components/MainHeader/WorkflowHeaderDraftPublishActions.vue
+++ packages/frontend/editor-ui/src/app/components/MainHeader/WorkflowHeaderDraftPublishActions.vue
@@ -16,6 +16,7 @@ import { getActivatableTriggerNodes } from '@/app/utils/nodeTypesUtils';
 import { useWorkflowSaving } from '@/app/composables/useWorkflowSaving';
 import { useRouter } from 'vue-router';
 import { getLastPublishedByUser } from '@/features/workflows/workflowHistory/utils';
+import CollaborationPane from '@/features/collaboration/collaboration/components/CollaborationPane.vue';
 
 const props = defineProps<{
 	readOnly?: boolean;
@@ -101,6 +102,7 @@ defineExpose({
 
 <template>
 	<div :class="$style.container">
+		<CollaborationPane v-if="!isNewWorkflow" />
 		<div
 			v-if="activeVersion"
 			:class="$style.activeVersionIndicator"
