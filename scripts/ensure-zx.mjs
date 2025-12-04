df7e8eb7dfb50468e32049a7c3973a9decdc2894
GRCH999 <ilia.context99@gmail.com>
2025-12-04 12:13:39 +0300

    Update ensure-zx.mjs

--- scripts/ensure-zx.mjs
+++ scripts/ensure-zx.mjs
@@ -3,6 +3,7 @@ import { execSync } from 'node:child_process';
 
 const ZX_PATH = 'node_modules/.bin/zx';
 
+
 if (!zxExists()) {
 	execSync('pnpm --frozen-lockfile --filter n8n-monorepo install', { stdio: 'inherit' });
 }
