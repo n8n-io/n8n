66c64b11c7e02ebf42655aa82c8f9c4182f3956c
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-12-04 14:54:16 +0100

    Centralize mock global config on test

--- packages/cli/src/modules/breaking-changes/rules/v2/__tests__/task-runners.rule.test.ts
+++ packages/cli/src/modules/breaking-changes/rules/v2/__tests__/task-runners.rule.test.ts
@@ -4,13 +4,21 @@ import { mock } from 'jest-mock-extended';
 import { TaskRunnersRule } from '../task-runners.rule';
 
 describe('TaskRunnersRule', () => {
+	let mockGlobalConfig: GlobalConfig;
+
+	beforeEach(() => {
+		mockGlobalConfig = mock<GlobalConfig>({
+			deployment: { type: 'default' },
+		});
+	});
+
 	describe('detect()', () => {
 		it('should not be affected on cloud deployments', async () => {
 			const mockConfig = { enabled: false } as TaskRunnersConfig;
-			const mockGlobalConfig = mock<GlobalConfig>({
+			const cloudGlobalConfig = mock<GlobalConfig>({
 				deployment: { type: 'cloud' },
 			});
-			const rule = new TaskRunnersRule(mockConfig, mockGlobalConfig);
+			const rule = new TaskRunnersRule(mockConfig, cloudGlobalConfig);
 
 			const result = await rule.detect();
 
@@ -21,9 +29,6 @@ describe('TaskRunnersRule', () => {
 
 		it('should not be affected when runners are already enabled', async () => {
 			const mockConfig = { enabled: true } as TaskRunnersConfig;
-			const mockGlobalConfig = mock<GlobalConfig>({
-				deployment: { type: 'default' },
-			});
 			const rule = new TaskRunnersRule(mockConfig, mockGlobalConfig);
 
 			const result = await rule.detect();
@@ -34,9 +39,6 @@ describe('TaskRunnersRule', () => {
 
 		it('should be affected when runners are not enabled', async () => {
 			const mockConfig = { enabled: false } as TaskRunnersConfig;
-			const mockGlobalConfig = mock<GlobalConfig>({
-				deployment: { type: 'default' },
-			});
 			const rule = new TaskRunnersRule(mockConfig, mockGlobalConfig);
 
 			const result = await rule.detect();
@@ -44,13 +46,11 @@ describe('TaskRunnersRule', () => {
 			expect(result.isAffected).toBe(true);
 			expect(result.instanceIssues).toHaveLength(1);
 			expect(result.instanceIssues[0].title).toBe('Task Runners will be enabled by default');
+			expect(result.recommendations).toHaveLength(3);
 		});
 
 		it('should be affected when runners are explicitly disabled', async () => {
 			const mockConfig = { enabled: false } as TaskRunnersConfig;
-			const mockGlobalConfig = mock<GlobalConfig>({
-				deployment: { type: 'default' },
-			});
 			const rule = new TaskRunnersRule(mockConfig, mockGlobalConfig);
 
 			const result = await rule.detect();
