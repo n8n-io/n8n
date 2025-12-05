80507de29c4aa1de73f9f2faeeadae74252d6f05
Alex Grozav <alex@grozav.com>
2025-12-05 16:02:50 +0200

    refactor: update getWorkflowId

--- packages/frontend/editor-ui/src/app/components/MainHeader/utils.ts
+++ packages/frontend/editor-ui/src/app/components/MainHeader/utils.ts
@@ -1,10 +1,3 @@
 export const getWorkflowId = (propId?: string, routeName?: string | string[]) => {
-	let id: string | undefined = undefined;
-	if (propId) {
-		id = propId;
-	} else if (routeName && routeName !== 'new') {
-		id = routeName as string;
-	}
-
-	return id;
+	return propId ?? (typeof routeName === 'string' ? routeName : undefined);
 };
