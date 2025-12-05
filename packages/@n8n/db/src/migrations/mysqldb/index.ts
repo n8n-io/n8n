e78250f94c37e9038cf728e7cec0389a68143690
Andreas Fitzek <andreas.fitzek@n8n.io>
2025-12-05 11:04:26 +0100

    chore(core): Implement generic credential storage provider (#22662)

--- packages/@n8n/db/src/migrations/mysqldb/index.ts
+++ packages/@n8n/db/src/migrations/mysqldb/index.ts
@@ -121,6 +121,7 @@ import { CreateBinaryDataTable1763716655000 } from '../common/1763716655000-Crea
 import { CreateWorkflowPublishHistoryTable1764167920585 } from '../common/1764167920585-CreateWorkflowPublishHistoryTable';
 import { AddCreatorIdToProjectTable1764276827837 } from '../common/1764276827837-AddCreatorIdToProjectTable';
 import { CreateDynamicCredentialResolverTable1764682447000 } from '../common/1764682447000-CreateCredentialResolverTable';
+import { AddDynamicCredentialEntryTable1764689388394 } from '../common/1764689388394-AddDynamicCredentialEntryTable';
 import type { Migration } from '../migration-types';
 
 export const mysqlMigrations: Migration[] = [
@@ -247,4 +248,5 @@ export const mysqlMigrations: Migration[] = [
 	CreateWorkflowPublishHistoryTable1764167920585,
 	AddCreatorIdToProjectTable1764276827837,
 	CreateDynamicCredentialResolverTable1764682447000,
+	AddDynamicCredentialEntryTable1764689388394,
 ];
