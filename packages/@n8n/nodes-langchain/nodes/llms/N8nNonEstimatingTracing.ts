e5457bd7f917de4c4de81a10f4ce62803f8eccda
Michael Kret <michael.k@radency.com>
2025-12-06 19:21:50 +0200

    redact binary data in llm tracing

--- packages/@n8n/nodes-langchain/nodes/llms/N8nNonEstimatingTracing.ts
+++ packages/@n8n/nodes-langchain/nodes/llms/N8nNonEstimatingTracing.ts
@@ -11,7 +11,7 @@ import pick from 'lodash/pick';
 import type { IDataObject, ISupplyDataFunctions, JsonObject } from 'n8n-workflow';
 import { NodeConnectionTypes, NodeError, NodeOperationError } from 'n8n-workflow';
 
-import { logAiEvent } from '@utils/helpers';
+import { logAiEvent, redactBinaryMessages } from '@utils/helpers';
 
 type RunDetail = {
 	index: number;
@@ -118,7 +118,7 @@ export class N8nNonEstimatingTracing extends BaseCallbackHandler {
 				[
 					{
 						json: {
-							messages: prompts,
+							messages: redactBinaryMessages(prompts),
 							estimatedTokens,
 							options,
 						},
