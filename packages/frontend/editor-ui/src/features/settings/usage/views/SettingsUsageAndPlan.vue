b9339f0854b680f3059f9d7ff55c81bf98a748ab
Charlie Kolb <charlie@n8n.io>
2025-12-08 11:29:45 +0100

    remove extra hint

--- packages/frontend/editor-ui/src/features/settings/usage/views/SettingsUsageAndPlan.vue
+++ packages/frontend/editor-ui/src/features/settings/usage/views/SettingsUsageAndPlan.vue
@@ -286,10 +286,6 @@ const openCommunityRegisterModal = () => {
 			</div>
 
 			<N8nInfoTip>{{ locale.baseText('settings.usageAndPlan.activeWorkflows.hint') }}</N8nInfoTip>
-			<br />
-			<N8nInfoTip v-if="usageStore.activeWorkflowTriggersLimit > 0">
-				{{ locale.baseText('settings.usageAndPlan.activeWorkflows.overcountWarning') }}
-			</N8nInfoTip>
 
 			<div :class="$style.buttons">
 				<N8nButton
