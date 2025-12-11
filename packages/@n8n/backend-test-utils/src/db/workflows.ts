9a608dc00cfd86c0588022a1d2befc7c9a28e59c
Daria <daria.staferova@n8n.io>
2025-12-11 18:14:42 +0200

    feat: Set `autosave` for history versions (no-changelog) (#23076)

--- packages/@n8n/backend-test-utils/src/db/workflows.ts
+++ packages/@n8n/backend-test-utils/src/db/workflows.ts
@@ -240,11 +240,14 @@ export const getWorkflowById = async (id: string) =>
  * Create a workflow history record for a workflow
  * @param workflow workflow to create history for
  * @param user user who created the version (optional)
+ * @param withPublishHistory publish history to create (optional)
+ * @param autosaved whether this is an autosave (optional)
  */
 export async function createWorkflowHistory(
 	workflow: IWorkflowDb,
 	userOrProject?: User | Project,
 	withPublishHistory?: Partial<WorkflowPublishHistory>,
+	autosaved = false,
 ): Promise<void> {
 	const authors =
 		userOrProject instanceof User
@@ -259,6 +262,7 @@ export async function createWorkflowHistory(
 		nodes: workflow.nodes,
 		connections: workflow.connections,
 		authors,
+		autosaved,
 	});
 
 	if (withPublishHistory) {
