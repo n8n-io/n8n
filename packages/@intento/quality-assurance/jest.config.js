4035bca64e73935efb14604e349972d28237e7d0
Anton Lysak <anton.lysak@inten.to>
2025-12-12 12:30:47 +0500

    RND-112: Fix unit tests setup for Intento packages

--- packages/@intento/quality-assurance/jest.config.js
+++ packages/@intento/quality-assurance/jest.config.js
@@ -3,8 +3,9 @@ process.env.TZ = 'UTC';
 
 /** @type {import('jest').Config} */
 module.exports = {
-	...require('../../jest.config'),
+	...require('../../../jest.config'),
 	collectCoverageFrom: ['credentials/**/*.ts', 'nodes/**/*.ts', 'utils/**/*.ts'],
 	globalSetup: '<rootDir>/test/globalSetup.ts',
 	setupFilesAfterEnv: ['jest-expect-message', '<rootDir>/test/setup.ts'],
+	passWithNoTests: true,
 };
