d61e91b0dd2e0f43aa670d0d80afdfae39cfe51e
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-12-15 16:51:26 +0200

    test: Tests for utils

--- packages/nodes-base/nodes/Postgres/test/v2/utils.test.ts
+++ packages/nodes-base/nodes/Postgres/test/v2/utils.test.ts
@@ -1,8 +1,14 @@
-import type { INode, IPairedItemData } from 'n8n-workflow';
+import type { INode, INodeExecutionData, IPairedItemData } from 'n8n-workflow';
 import { NodeOperationError } from 'n8n-workflow';
 import pgPromise from 'pg-promise';
 
-import type { ColumnInfo } from '../../v2/helpers/interfaces';
+import type {
+	ColumnInfo,
+	PostgresNodeOptions,
+	QueriesRunner,
+	QueryMode,
+	QueryWithValues,
+} from '../../v2/helpers/interfaces';
 import {
 	addSortRules,
 	addReturning,
@@ -18,6 +24,7 @@ import {
 	convertValuesToJsonWithPgp,
 	hasJsonDataTypeInSchema,
 	evaluateExpression,
+	runQueriesAndHandleErrors,
 } from '../../v2/helpers/utils';
 
 const node: INode = {
@@ -554,4 +561,84 @@ describe('Test PostgresV2, convertArraysToPostgresFormat', () => {
 		});
 		expect(item).toEqual(referenceItem);
 	});
+});
+
+describe('Test PostgresV2, runQueriesAndHandleErrors', () => {
+	it.each([['single'], ['transaction']] as QueryMode[][])(
+		'should return errors without running queries when batching is %s',
+		async (batching) => {
+			const runQueries: QueriesRunner = jest.fn().mockResolvedValue([]);
+			const queries: QueryWithValues[] = [
+				{ query: 'INSERT INTO my_table (id) VALUES (1)', values: [] },
+			];
+			const nodeOptions: PostgresNodeOptions = { queryBatching: batching };
+			const errorItemsMap: Map<number, INodeExecutionData> = new Map();
+			errorItemsMap.set(1, { json: { error: new Error('Test error') }, pairedItem: { item: 1 } });
+
+			const result = await runQueriesAndHandleErrors(
+				runQueries,
+				queries,
+				nodeOptions,
+				errorItemsMap,
+			);
+
+			expect(result).toEqual([
+				{ json: { error: new Error('Test error') }, pairedItem: { item: 1 } },
+			]);
+			expect(runQueries).not.toHaveBeenCalled();
+		},
+	);
+
+	it('should run queries and return errors when batching is independently', async () => {
+		const runQueries: QueriesRunner = jest.fn().mockResolvedValue([
+			{ json: { id: 1 }, pairedItem: { item: 0 } },
+			{ json: { id: 3 }, pairedItem: { item: 2 } },
+		]);
+		const queries: QueryWithValues[] = [
+			{ query: 'INSERT INTO my_table (id) VALUES (1)', values: [] },
+			{ query: 'INSERT INTO my_table (id) VALUES (3)', values: [] },
+		];
+		const nodeOptions: PostgresNodeOptions = { queryBatching: 'independently' };
+		const errorItemsMap: Map<number, INodeExecutionData> = new Map();
+		errorItemsMap.set(1, { json: { error: new Error('Test error') }, pairedItem: { item: 1 } });
+
+		const result = await runQueriesAndHandleErrors(runQueries, queries, nodeOptions, errorItemsMap);
+
+		expect(result).toEqual([
+			{ json: { id: 1 }, pairedItem: { item: 0 } },
+			{ json: { error: new Error('Test error') }, pairedItem: { item: 1 } },
+			{ json: { id: 3 }, pairedItem: { item: 2 } },
+		]);
+	});
+
+	it.each([['single'], ['transaction'], ['independently']] as QueryMode[][])(
+		'should run queries when batching is %s and there are no errors',
+		async (batching) => {
+			const runQueries: QueriesRunner = jest.fn().mockResolvedValue([
+				{ json: { id: 1 }, pairedItem: { item: 0 } },
+				{ json: { id: 2 }, pairedItem: { item: 1 } },
+				{ json: { id: 3 }, pairedItem: { item: 2 } },
+			]);
+			const queries: QueryWithValues[] = [
+				{ query: 'INSERT INTO my_table (id) VALUES (1)', values: [] },
+				{ query: 'INSERT INTO my_table (id) VALUES (2)', values: [] },
+				{ query: 'INSERT INTO my_table (id) VALUES (3)', values: [] },
+			];
+			const nodeOptions: PostgresNodeOptions = { queryBatching: batching };
+			const errorItemsMap: Map<number, INodeExecutionData> = new Map();
+
+			const result = await runQueriesAndHandleErrors(
+				runQueries,
+				queries,
+				nodeOptions,
+				errorItemsMap,
+			);
+
+			expect(result).toEqual([
+				{ json: { id: 1 }, pairedItem: { item: 0 } },
+				{ json: { id: 2 }, pairedItem: { item: 1 } },
+				{ json: { id: 3 }, pairedItem: { item: 2 } },
+			]);
+		},
+	);
 });
