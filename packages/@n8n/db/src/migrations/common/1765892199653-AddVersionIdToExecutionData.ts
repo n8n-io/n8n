8a8a1cc3b967bf0f048040d2e84a243f2b0c0450
Charlie Kolb <charlie@n8n.io>
2025-12-16 18:12:03 +0100

    fix column size

--- packages/@n8n/db/src/migrations/common/1765892199653-AddVersionIdToExecutionData.ts
+++ packages/@n8n/db/src/migrations/common/1765892199653-AddVersionIdToExecutionData.ts
@@ -2,7 +2,7 @@ import type { MigrationContext, ReversibleMigration } from '../migration-types';
 
 export class AddWorkflowVersionIdToExecutionData1765892199653 implements ReversibleMigration {
 	async up({ schemaBuilder: { addColumns, column } }: MigrationContext) {
-		await addColumns('execution_data', [column('workflowVersionId').varchar(32)]);
+		await addColumns('execution_data', [column('workflowVersionId').varchar(36)]);
 	}
 
 	async down({ schemaBuilder: { dropColumns } }: MigrationContext) {
