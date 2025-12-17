57e7dfb671e938e31955618dc6e50c2b2cdfb5a5
Michael Siega <michael.siega@n8n.io>
2025-12-17 16:21:08 +0100

    use redis prefix everywhere

--- packages/cli/src/commands/__tests__/webhook.test.ts
+++ packages/cli/src/commands/__tests__/webhook.test.ts
@@ -3,36 +3,26 @@ import { Container } from '@n8n/di';
 
 import { PubSubRegistry } from '@/scaling/pubsub/pubsub.registry';
 import { Subscriber } from '@/scaling/pubsub/subscriber.service';
-import { WorkerStatusService } from '@/scaling/worker-status.service.ee';
 import { RedisClientService } from '@/services/redis-client.service';
 
-import { Worker } from '../worker';
+import { Webhook } from '../webhook';
 
 mockInstance(RedisClientService);
 mockInstance(PubSubRegistry);
 const mockSubscriber = mockInstance(Subscriber);
-mockInstance(WorkerStatusService);
 
-describe('Worker', () => {
+describe('Webhook', () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
 	});
 
 	describe('initOrchestration', () => {
-		it('should instantiate WorkerStatusService during orchestration setup', async () => {
-			const containerGetSpy = jest.spyOn(Container, 'get');
-
-			await new Worker().initOrchestration();
-
-			expect(containerGetSpy).toHaveBeenCalledWith(WorkerStatusService);
-		});
-
 		it('should get command channel and subscribe to it', async () => {
 			const mockCommandChannel = 'n8n:n8n.commands';
 			mockSubscriber.getCommandChannel.mockReturnValue(mockCommandChannel);
 			mockSubscriber.subscribe.mockResolvedValue(undefined);
 
-			await new Worker().initOrchestration();
+			await new Webhook().initOrchestration();
 
 			expect(mockSubscriber.getCommandChannel).toHaveBeenCalled();
 			expect(mockSubscriber.subscribe).toHaveBeenCalledWith(mockCommandChannel);
@@ -42,7 +32,7 @@ describe('Worker', () => {
 			const pubSubRegistry = Container.get(PubSubRegistry);
 			const initSpy = jest.spyOn(pubSubRegistry, 'init');
 
-			await new Worker().initOrchestration();
+			await new Webhook().initOrchestration();
 
 			expect(initSpy).toHaveBeenCalled();
 		});
