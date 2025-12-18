c1be58c408eb1fe7b89a23b7c2bfda6261ab0876
konstantintieber <konstantin.tieber@n8n.io>
2025-12-18 14:25:11 +0100

    Remove unused migration classes

--- packages/@n8n/db/src/migrations/sqlite/1765892113000-AddWorkflowPublishScopeToProjectRoles.ts
+++
@@ -1,3 +0,0 @@
-import { AddWorkflowPublishScopeToProjectRoles1765892113000 as BaseMigration } from '../common/1765892113000-AddWorkflowPublishScopeToProjectRoles';
-
-export class AddWorkflowPublishScopeToProjectRoles1765892113000 extends BaseMigration {}
