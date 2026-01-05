b2cb138c43c11012e47392d7f3c7ecf92ccdb538
城二 <wzk374047@digital-engine.com>
2026-01-05 20:41:07 +0800

    feat: Revert mockInstance Type Definitions

--- packages/@n8n/db/src/utils/test-utils/mock-instance.ts
+++ packages/@n8n/db/src/utils/test-utils/mock-instance.ts
@@ -1,9 +1,10 @@
 import { Container, type Constructable } from '@n8n/di';
 import { mock } from 'jest-mock-extended';
+import type { DeepPartial } from 'ts-essentials';
 
 export const mockInstance = <T>(
 	serviceClass: Constructable<T>,
-	data: Parameters<typeof mock<T>>[0] = undefined,
+	data: DeepPartial<T> | undefined = undefined,
 ) => {
 	const instance = mock<T>(data);
 	Container.set(serviceClass, instance);
