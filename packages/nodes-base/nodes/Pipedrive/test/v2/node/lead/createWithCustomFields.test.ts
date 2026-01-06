4b13b5df4dda99b444fa7fbe0ea4df897989b6e0
Nico Weichbrodt <git@nico.weichbrodt.me>
2026-01-06 13:06:59 +0100

    feat(Pipedrive Node): Allow setting custom fields when creating and updating leads

--- packages/nodes-base/nodes/Pipedrive/test/v2/node/lead/createWithCustomFields.test.ts
+++ packages/nodes-base/nodes/Pipedrive/test/v2/node/lead/createWithCustomFields.test.ts
@@ -4,24 +4,29 @@ import nock from 'nock';
 import { credentials } from '../credentials';
 import { mockFieldsApi } from '../fieldsApiMock';
 
-describe('Test PipedriveV2, lead => update', () => {
+describe('Test PipedriveV2, lead => create with custom fields', () => {
 	mockFieldsApi('lead');
 
 	nock('https://api.pipedrive.com/v1')
-		.put('/leads/9ce77a10-2e16-11f1-91c1-d76c55e1594b', {
-			title: 'Updated Lead',
+		.post('/leads', {
+			title: 'Lead With Custom Fields',
+			organization_id: 7,
+			custom_fields: {
+				'48f483ac81a7b619c59322931ea839310e987725': 'option_a',
+				'7095cf9bf6bdeb457b12a76fdbb0fe06c0ad5151': 'option_b',
+			},
 		})
 		.reply(200, {
 			success: true,
 			data: {
-				id: '9ce77a10-2e16-11f1-91c1-d76c55e1594b',
-				title: 'Updated Lead',
+				id: 'aaa11111-bb22-cc33-dd44-ee5555555555',
+				title: 'Lead With Custom Fields',
 				owner_id: 25455458,
 				creator_id: 25455458,
 				label_ids: [],
 				value: null,
 				expected_close_date: null,
-				person_id: 10,
+				person_id: null,
 				organization_id: 7,
 				is_archived: false,
 				archive_time: null,
@@ -34,14 +39,18 @@ describe('Test PipedriveV2, lead => update', () => {
 				was_seen: false,
 				next_activity_id: null,
 				add_time: '2026-04-01T22:03:28.177Z',
-				update_time: '2026-04-01T22:03:32.984Z',
+				update_time: '2026-04-01T22:03:28.177Z',
 				visible_to: '3',
-				cc_email: 'added-cauliflower+14712380+lead9aeonoweeh1wbg0z8godmoua3@pipedrivemail.com',
+				cc_email: 'test+lead@pipedrivemail.com',
+				custom_fields: {
+					'48f483ac81a7b619c59322931ea839310e987725': 'option_a',
+					'7095cf9bf6bdeb457b12a76fdbb0fe06c0ad5151': 'option_b',
+				},
 			},
 		});
 
 	new NodeTestHarness().setupTests({
 		credentials,
-		workflowFiles: ['update.workflow.json'],
+		workflowFiles: ['createWithCustomFields.workflow.json'],
 	});
 });
