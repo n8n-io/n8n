def0a09d8ae64b99283fd45a47047604b9a7250d
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2026-01-07 11:17:42 +0100

    feat(core): First shot in resolving the credential resolver config expressions (#23110)

--- packages/workflow/src/index.ts
+++ packages/workflow/src/index.ts
@@ -80,6 +80,7 @@ export * as ExpressionParser from './extensions/expression-parser';
 export { NativeMethods } from './native-methods';
 export * from './node-parameters/filter-parameter';
 export * from './node-parameters/parameter-type-validation';
+export * from './node-parameters/node-parameter-value-type-guard';
 export * from './node-parameters/path-utils';
 export * from './evaluation-helpers';
 export * from './workflow-diff';
