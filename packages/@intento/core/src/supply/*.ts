dc63166cb5e2a9e8adde7e35aec7fb31deb1d876
Anton Lysak <anton.lysak@inten.to>
2026-01-08 11:18:19 +0500

    RND-97: fix exports

--- packages/@intento/core/src/supply/*.ts
+++ packages/@intento/core/src/supply/*.ts
@@ -1,5 +1,6 @@
-export * from './retriable-supplier-base';
 export * from './supply-error-base';
 export * from './supply-factory';
 export * from './supply-request-base';
 export * from './supply-response-base';
+export * from './supplier-base';
+export * from './retriable-supplier-base';
