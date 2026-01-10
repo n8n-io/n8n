ca86648a4b32de940a1bd2aba696ed138bad7889
Marcus <marcus@n8n.io>
2026-01-10 10:52:57 +0100

    fix isWindowsFilePath regex

--- packages/@n8n/utils/src/files/path.ts
+++ packages/@n8n/utils/src/files/path.ts
@@ -2,5 +2,5 @@
  * Fast check if file path starts with a windows drive letter, e.g. 'C:/' or 'C:\\'
  */
 export function isWindowsFilePath(str: string) {
-	return /^([a-zA-ZA-Z]:[/|\\])/.test(str);
+	return /^[a-zA-Z]:[\\/]/.test(str);
 }
