16166085ef421b39bd556fe47d2fa4c61f75aacd
konstantintieber <konstantin.tieber@n8n.io>
2026-01-13 18:09:18 +0100

    fix(core): Publish indicator not shown when lacking publish scope
    With the most recent changes for shipping auto-save, the published indicator
    is not longer visible on published workflows when the active user
    does not have the workflow:publish permission.
    This is not consistent, because in the workflow history and workflow list view,
    these users can see the published indicator.
    And they should be able to see it to get the right picture anyway.

--- packages/frontend/editor-ui/src/app/components/MainHeader/WorkflowHeaderDraftPublishActions.test.ts
+++ packages/frontend/editor-ui/src/app/components/MainHeader/WorkflowHeaderDraftPublishActions.test.ts
@@ -140,7 +140,7 @@ describe('WorkflowHeaderDraftPublishActions', () => {
 
 			const { queryByTestId } = renderComponent();
 
-			expect(queryByTestId('workflow-active-version-indicator')).not.toBeInTheDocument();
+			expect(queryByTestId('workflow-active-version-info')).not.toBeInTheDocument();
 		});
 
 		it('should show active version indicator when there is an active version', () => {
@@ -148,7 +148,7 @@ describe('WorkflowHeaderDraftPublishActions', () => {
 
 			const { getByTestId } = renderComponent();
 
-			expect(getByTestId('workflow-active-version-indicator')).toBeInTheDocument();
+			expect(getByTestId('workflow-active-version-info')).toBeInTheDocument();
 		});
 
 		it('should use latest activation date from workflowPublishHistory when available', () => {
@@ -199,8 +199,26 @@ describe('WorkflowHeaderDraftPublishActions', () => {
 				},
 			});
 
-			expect(getByTestId('workflow-active-version-indicator')).toBeInTheDocument();
+			expect(getByTestId('workflow-active-version-info')).toBeInTheDocument();
 			expect(getByTestId('time-ago-stub')).toHaveTextContent(latestActivationDate);
+		});
+
+		it('should show active version indicator when user does not have workflow:publish permission but workflow is currently published', () => {
+			workflowsStore.workflow.activeVersion = createMockActiveVersion('active-version-1');
+			const { getByTestId } = renderComponent({
+				props: {
+					...defaultWorkflowProps,
+					readOnly: false,
+					workflowPermissions: {
+						...defaultWorkflowProps.workflowPermissions,
+						update: true,
+						publish: false,
+					},
+				},
+			});
+
+			expect(getByTestId('workflow-active-version-indicator')).toBeInTheDocument();
+			expect(getByTestId('workflow-active-version-info')).toBeInTheDocument();
 		});
 	});
 
