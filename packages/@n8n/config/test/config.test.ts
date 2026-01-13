ab42942d29e3f7ac0b990084a8cdc12bce6e05d1
Artem Sorokin <artem@n8n.io>
2026-01-13 13:09:41 +0100

    refactor(core): Move N8N_BLOCK_ENV_ACCESS_IN_NODE to @n8n/config

--- packages/@n8n/config/test/config.test.ts
+++ packages/@n8n/config/test/config.test.ts
@@ -338,6 +338,7 @@ describe('GlobalConfig', () => {
 			awsSystemCredentialsAccess: false,
 			enableGitNodeHooks: false,
 			enableGitNodeAllConfigKeys: false,
+			blockEnvAccessInNode: true,
 		},
 		executions: {
 			mode: 'regular',
