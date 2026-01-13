5b83dc24d11a832b149b1cf52317022d2441611a
Rob <robtf9@icloud.com>
2026-01-13 13:53:43 +0000

    improvements

--- packages/@n8n/api-types/src/schemas/workflow-version.schema.ts
+++ packages/@n8n/api-types/src/schemas/workflow-version.schema.ts
@@ -1,12 +1,22 @@
 import { z } from 'zod';
+import xss from 'xss';
 
 export const WORKFLOW_VERSION_NAME_MAX_LENGTH = 128;
 export const WORKFLOW_VERSION_DESCRIPTION_MAX_LENGTH = 2048;
+
+const xssCheck = (value: string) =>
+	value ===
+	xss(value, {
+		whiteList: {}, // no tags are allowed
+	});
 
 export const workflowVersionNameSchema = z
 	.string()
 	.max(WORKFLOW_VERSION_NAME_MAX_LENGTH, {
 		message: `Version name cannot be longer than ${WORKFLOW_VERSION_NAME_MAX_LENGTH} characters`,
+	})
+	.refine(xssCheck, {
+		message: 'Potentially malicious string',
 	})
 	.optional();
 
@@ -14,5 +24,8 @@ export const workflowVersionDescriptionSchema = z
 	.string()
 	.max(WORKFLOW_VERSION_DESCRIPTION_MAX_LENGTH, {
 		message: `Version description cannot be longer than ${WORKFLOW_VERSION_DESCRIPTION_MAX_LENGTH} characters`,
+	})
+	.refine(xssCheck, {
+		message: 'Potentially malicious string',
 	})
 	.optional();
