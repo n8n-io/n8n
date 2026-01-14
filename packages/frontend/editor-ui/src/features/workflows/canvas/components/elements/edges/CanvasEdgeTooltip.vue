e14b7086f0247964c3cb18596d300f6612c0277c
yehorkardash <yehor.kardash@n8n.io>
2026-01-14 14:13:33 +0100

    feat(editor): Show tooltips for canvas edge buttons (#24311)

---
+++ packages/frontend/editor-ui/src/features/workflows/canvas/components/elements/edges/CanvasEdgeTooltip.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { useCssModule } from 'vue';
+import { N8nTooltip } from '@n8n/design-system';
+
+defineProps<{
+	content: string;
+}>();
+
+const $style = useCssModule();
+</script>
+
+<template>
+	<div :class="$style.tooltipContainer">
+		<N8nTooltip
+			:content="content"
+			placement="top"
+			:teleported="false"
+			:offset="8"
+			popper-class="canvas-edge-toolbar-popper"
+		>
+			<slot />
+		</N8nTooltip>
+	</div>
+</template>
+
+<style lang="scss" module>
+.tooltipContainer {
+	position: relative;
+}
+</style>
+
+<style lang="scss">
+.canvas-edge-toolbar-popper {
+	white-space: nowrap;
+
+	.el-popper__arrow {
+		// override default margin as it moves the arrow off the center
+		margin: 0 !important;
+	}
+}
+</style>
