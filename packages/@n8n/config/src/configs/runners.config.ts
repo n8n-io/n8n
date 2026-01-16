83aef2b70dd010f6b970a41ce0aae051d88e49d6
Iván Ovejero <ivov.src@gmail.com>
2026-01-16 10:16:20 +0100

    chore(core): Reduce runner task timeout default

--- packages/@n8n/config/src/configs/runners.config.ts
+++ packages/@n8n/config/src/configs/runners.config.ts
@@ -42,8 +42,6 @@ export class TaskRunnersConfig {
 
 	/**
 	 * How many concurrent tasks can a runner execute at a time
-	 *
-	 * Kept high for backwards compatibility - n8n v2 will reduce this to `5`
 	 */
 	@Env('N8N_RUNNERS_MAX_CONCURRENCY')
 	maxConcurrency: number = 10;
@@ -52,11 +50,9 @@ export class TaskRunnersConfig {
 	 * How long (in seconds) a task is allowed to take for completion, else the
 	 * task will be aborted. (In internal mode, the runner will also be
 	 * restarted.) Must be greater than 0.
-	 *
-	 * Kept high for backwards compatibility - n8n v2 will reduce this to `60`
 	 */
 	@Env('N8N_RUNNERS_TASK_TIMEOUT')
-	taskTimeout: number = 300; // 5 minutes
+	taskTimeout: number = 60; // 1 minute
 
 	/**
 	 * How long (in seconds) a task request can wait for a runner to become
