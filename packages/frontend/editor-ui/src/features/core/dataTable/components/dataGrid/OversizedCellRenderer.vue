adc6fbfdc1365c296fba905ff95abc12e806548e
Svetoslav Dekov <svetoslav.dekov@n8n.io>
2026-01-19 11:06:35 +0200

    Add tooltip mentioning that large data can be modified using the dt node

--- packages/frontend/editor-ui/src/features/core/dataTable/components/dataGrid/OversizedCellRenderer.vue
+++ packages/frontend/editor-ui/src/features/core/dataTable/components/dataGrid/OversizedCellRenderer.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { useI18n } from '@n8n/i18n';
+import { N8nTooltip } from '@n8n/design-system';
 
 const i18n = useI18n();
 </script>
 
 <template>
-	<span class="n8n-oversized-value">{{ i18n.baseText('dataTable.cell.oversized') }}</span>
+	<N8nTooltip placement="top">
+		<template #content>
+			{{ i18n.baseText('dataTable.cell.oversized.tooltip') }}
+		</template>
+		<span class="n8n-oversized-value">{{ i18n.baseText('dataTable.cell.oversized') }}</span>
+	</N8nTooltip>
 </template>
 
 <style lang="scss">
