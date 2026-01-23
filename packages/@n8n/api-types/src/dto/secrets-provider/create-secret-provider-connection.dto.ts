59c2baa65c89a9a202341d7ce0bf7d891dd0d0be
Irénée <irenee.ajeneza@n8n.io>
2026-01-23 08:04:33 +0000

    refactor: removed isGlobal and displayName from dtos

--- packages/@n8n/api-types/src/dto/secrets-provider/create-secret-provider-connection.dto.ts
+++ packages/@n8n/api-types/src/dto/secrets-provider/create-secret-provider-connection.dto.ts
@@ -5,10 +5,8 @@ import { Z } from 'zod-class';
 import { secretsProviderTypeSchema } from '../../schemas/secrets-provider.schema';
 
 export class CreateSecretProviderConnectionDto extends Z.class({
-	name: z.string().min(1).max(128),
+	providerKey: z.string().min(1).max(128),
 	type: secretsProviderTypeSchema,
-	displayName: z.string().min(1).max(128).optional(),
-	isGlobal: z.boolean(),
 	projectId: z.string().min(1),
 	settings: z.object({}).catchall(z.any()) satisfies z.ZodType<IDataObject>,
 }) {}
--- packages/@n8n/api-types/src/dto/secrets-provider/update-secret-provider-connection.dto.ts
+++ packages/@n8n/api-types/src/dto/secrets-provider/update-secret-provider-connection.dto.ts
@@ -6,7 +6,5 @@ import { secretsProviderTypeSchema } from '../../schemas/secrets-provider.schema
 
 export class UpdateSecretProviderConnectionDto extends Z.class({
 	type: secretsProviderTypeSchema.optional(),
-	displayName: z.string().min(1).max(128).optional(),
-	isGlobal: z.boolean().optional(),
 	settings: (z.object({}).catchall(z.any()) satisfies z.ZodType<IDataObject>).optional(),
 }) {}
