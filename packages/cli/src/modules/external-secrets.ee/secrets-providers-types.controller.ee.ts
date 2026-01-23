ff8e068db1f7aecd02c6fa339cd86e908624337c
James Gee <1285296+geemanjs@users.noreply.github.com>
2026-01-23 13:07:01 +0100

    Update packages/cli/src/modules/external-secrets.ee/secrets-providers-types.controller.ee.ts
    Co-authored-by: Irénée <irenee.ajeneza@n8n.io>

--- packages/cli/src/modules/external-secrets.ee/secrets-providers-types.controller.ee.ts
+++ packages/cli/src/modules/external-secrets.ee/secrets-providers-types.controller.ee.ts
@@ -7,7 +7,7 @@ import { BadRequestError } from '@/errors/response-errors/bad-request.error';
 import { ExternalSecretsConfig } from './external-secrets.config';
 
 @RestController('/secret-providers/types')
-export class SecretProvidersController {
+export class SecretProvidersTypesController {
 	constructor(
 		private readonly config: ExternalSecretsConfig,
 		private readonly logger: Logger,
