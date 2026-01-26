4b9831a5be56dd6be6566d4dd0d2b9931964fc51
Irénée <irenee.ajeneza@n8n.io>
2026-01-26 13:28:38 +0100

    feat: use a cach map rather than an array

--- packages/cli/src/modules/external-secrets.ee/secrets-provider-connection-store-cache.service.ts
+++ packages/cli/src/modules/external-secrets.ee/secrets-provider-connection-store-cache.service.ts
@@ -17,26 +17,30 @@ export class SecretsProviderConnectionStoreCache {
 	}
 
 	async loadAll(): Promise<SecretsProviderConnection[]> {
-		if (this.cache.length > 0) {
-			return this.cache;
+		if (this.cache.size > 0) {
+			return Array.from(this.cache.values());
 		}
 
 		return await this.reloadAll();
 	}
 
 	async reloadAll(): Promise<SecretsProviderConnection[]> {
-		this.cache = await this.connectionRepository.findAll();
-		return this.cache;
+		const connections = await this.connectionRepository.findAll();
+		this.cache.clear();
+		for (const connection of connections) {
+			this.cache.set(connection.providerKey, connection);
+		}
+		return connections;
 	}
 
 	getCachedConnections(): SecretsProviderConnection[] {
-		return this.cache;
+		return Array.from(this.cache.values());
 	}
 
 	async reloadConnection(providerKey: string): Promise<SecretsProviderConnection | null> {
 		const connection = await this.connectionRepository.findOne({ where: { providerKey } });
 		if (connection) {
-			this.cache = this.cache.map((c) => (c.providerKey === providerKey ? connection : c));
+			this.cache.set(providerKey, connection);
 		}
 
 		return connection ?? null;
@@ -44,22 +48,22 @@ export class SecretsProviderConnectionStoreCache {
 
 	async create(connection: SecretsProviderConnection): Promise<SecretsProviderConnection> {
 		// TODO: use repository to create connection in the database
-		this.cache.push(connection);
+		this.cache.set(connection.providerKey, connection);
 		return connection;
 	}
 
 	async update(connection: SecretsProviderConnection): Promise<SecretsProviderConnection> {
 		// TODO: use repository to update connection in the database
-		this.cache = this.cache.map((c) => (c.providerKey === connection.providerKey ? connection : c));
+		this.cache.set(connection.providerKey, connection);
 		return connection;
 	}
 
 	async delete(providerKey: string): Promise<void> {
 		// TODO: use repository to delete connection in the database
-		this.cache = this.cache.filter((c) => c.providerKey !== providerKey);
+		this.cache.delete(providerKey);
 	}
 
 	async get(providerKey: string): Promise<SecretsProviderConnection | undefined> {
-		return this.cache.find((c) => c.providerKey === providerKey);
+		return this.cache.get(providerKey);
 	}
 }
