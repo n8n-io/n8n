fd59a60d38d6e2e226ed73b4c8b56298ce4a9795
Mutasem Aldmour <mutasem@n8n.io>
2026-01-27 11:22:21 +0100

    add builder hint for output parser

--- packages/@n8n/nodes-langchain/nodes/agents/Agent/V3/AgentToolV3.node.ts
+++ packages/@n8n/nodes-langchain/nodes/agents/Agent/V3/AgentToolV3.node.ts
@@ -45,6 +45,7 @@ export class AgentToolV3 implements INodeType {
 					type: 'boolean',
 					default: false,
 					noDataExpression: true,
+					builderHint: 'IMPORTANT: Set to true to use outputParser subnode',
 				},
 				{
 					displayName: `Connect an <a data-action='openSelectiveNodeCreator' data-action-parameter-connectiontype='${NodeConnectionTypes.AiOutputParser}'>output parser</a> on the canvas to specify the output format you require`,
