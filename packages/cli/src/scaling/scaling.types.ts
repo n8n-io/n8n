f6b7aa9162d3cc3096774421e36c1bd4d47da6f1
Iván Ovejero <ivov.src@gmail.com>
2026-01-30 12:32:13 +0100

    perf(core): Minimize execution data fetching (#25073)

--- packages/cli/src/scaling/scaling.types.ts
+++ packages/cli/src/scaling/scaling.types.ts
@@ -2,6 +2,7 @@ import type { RunningJobSummary } from '@n8n/api-types';
 import type Bull from 'bull';
 import type {
 	ExecutionError,
+	ExecutionStatus,
 	IExecuteResponsePromiseData,
 	IRun,
 	StructuredChunk,
@@ -24,7 +25,6 @@ export type JobData = {
 
 export type JobResult = {
 	success: boolean;
-	error?: ExecutionError;
 };
 
 export type JobStatus = Bull.JobStatus;
@@ -52,13 +52,35 @@ export type RespondToWebhookMessage = {
 	workerId: string;
 };
 
-/** Message sent by worker to main to report a job has finished successfully. */
-export type JobFinishedMessage = {
+export type JobFinishedProps = {
+	success: boolean;
+	error?: ExecutionError;
+	status: ExecutionStatus;
+	lastNodeExecuted?: string;
+	usedDynamicCredentials?: boolean;
+	metadata?: Record<string, string>;
+	startedAt: Date;
+	stoppedAt: Date;
+};
+
+/** Message sent by worker to main to report a job has finished. */
+export type JobFinishedMessage = JobFinishedMessageV1 | JobFinishedMessageV2;
+
+/** @deprecated Old format without execution result details. */
+type JobFinishedMessageV1 = {
 	kind: 'job-finished';
+	version?: undefined;
 	executionId: string;
 	workerId: string;
 	success: boolean;
 };
+
+type JobFinishedMessageV2 = {
+	kind: 'job-finished';
+	version: 2;
+	executionId: string;
+	workerId: string;
+} & JobFinishedProps;
 
 export type SendChunkMessage = {
 	kind: 'send-chunk';
