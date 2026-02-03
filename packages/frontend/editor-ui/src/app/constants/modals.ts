b145e3f99dbd7b93b7383ec954c42a9299c540b0
Daria <daria.staferova@n8n.io>
2026-02-03 11:33:01 +0100

    feat(editor): Add ui store modal registration test and clean up (no-changelog) (#25209)

--- packages/frontend/editor-ui/src/app/constants/modals.ts
+++ packages/frontend/editor-ui/src/app/constants/modals.ts
@@ -11,7 +11,6 @@ export const IMPORT_WORKFLOW_URL_MODAL_KEY = 'importWorkflowUrl';
 export const VERSIONS_MODAL_KEY = 'versions';
 export const WORKFLOW_SETTINGS_MODAL_KEY = 'settings';
 export const WORKFLOW_SHARE_MODAL_KEY = 'workflowShare';
-export const NODE_PINNING_MODAL_KEY = 'nodePinning';
 export const NPS_SURVEY_MODAL_KEY = 'npsSurvey';
 export const WORKFLOW_ACTIVE_MODAL_KEY = 'activation';
 export const IMPORT_CURL_MODAL_KEY = 'importCurl';
