28d14ed6feff75bdc1cf93601c59d186a4438dc5
Andreas Fitzek <andreas.fitzek@n8n.io>
2026-02-11 20:24:05 +0100

    Fix

--- packages/frontend/editor-ui/src/Interface.ts
+++ packages/frontend/editor-ui/src/Interface.ts
@@ -949,7 +949,7 @@ export type EnterpriseEditionFeatureKey =
 	| 'EnforceMFA'
 	| 'NamedVersions'
 	| 'Provisioning'
-	| 'PersonalSpacePolicy';
+	| 'PersonalSpacePolicy'
 	| 'CustomRoles';
 
 export type EnterpriseEditionFeatureValue = keyof Omit<FrontendSettings['enterprise'], 'projects'>;
