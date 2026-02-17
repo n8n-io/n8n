b7290faefd5ca0802a20c313ed7195875acd9447
Stephen <sjw948@gmail.com>
2026-02-17 13:59:43 +0000

    lint

--- packages/@n8n/db/src/migrations/sqlite/index.ts
+++ packages/@n8n/db/src/migrations/sqlite/index.ts
@@ -135,8 +135,8 @@ import { CreateSecretsProviderConnectionTables1769433700000 } from '../common/17
 import { CreateWorkflowPublishedVersionTable1769698710000 } from '../common/1769698710000-CreateWorkflowPublishedVersionTable';
 import { ExpandSubjectIDColumnLength1769784356000 } from '../common/1769784356000-ExpandSubjectIDColumnLength';
 import { AddWorkflowUnpublishScopeToCustomRoles1769900001000 } from '../common/1769900001000-AddWorkflowUnpublishScopeToCustomRoles';
-import { ExpandProviderIdColumnLength1770000000000 } from '../common/1770000000000-ExpandProviderIdColumnLength';
 import { CreateChatHubToolsTable1770000000000 } from '../common/1770000000000-CreateChatHubToolsTable';
+import { ExpandProviderIdColumnLength1770000000000 } from '../common/1770000000000-ExpandProviderIdColumnLength';
 import type { Migration } from '../migration-types';
 
 const sqliteMigrations: Migration[] = [
