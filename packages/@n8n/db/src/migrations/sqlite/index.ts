bf3e80400d734ea0f6b0407f0861bdd76892acf6
autologie <suguru@n8n.io>
2026-02-27 11:48:36 +0100

    add back migration

--- packages/@n8n/db/src/migrations/sqlite/index.ts
+++ packages/@n8n/db/src/migrations/sqlite/index.ts
@@ -141,6 +141,7 @@ import { CreateWorkflowBuilderSessionTable1770220686000 } from '../common/177022
 import { AddScalingFieldsToTestRun1771417407753 } from '../common/1771417407753-AddScalingFieldsToTestRun';
 import { MigrateExternalSecretsToEntityStorage1771500000000 } from '../common/1771500000000-MigrateExternalSecretsToEntityStorage';
 import { AddUnshareScopeToCustomRoles1771500000001 } from '../common/1771500000001-AddUnshareScopeToCustomRoles';
+import { AddFilesColumnToChatHubAgents1771500000002 } from '../common/1771500000002-AddFilesColumnToChatHubAgents';
 import type { Migration } from '../migration-types';
 
 const sqliteMigrations: Migration[] = [
@@ -287,6 +288,7 @@ const sqliteMigrations: Migration[] = [
 	AddScalingFieldsToTestRun1771417407753,
 	MigrateExternalSecretsToEntityStorage1771500000000,
 	AddUnshareScopeToCustomRoles1771500000001,
+	AddFilesColumnToChatHubAgents1771500000002,
 ];
 
 export { sqliteMigrations };
