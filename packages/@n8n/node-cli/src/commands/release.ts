02843b2ea97c859042a11032bed05682a69b62e8
Garrit Franke <garrit.franke@n8n.io>
2026-03-05 12:09:02 +0100

    feat(n8n-node cli): Add support for publishing community nodes through GitHub Actions

--- packages/@n8n/node-cli/src/commands/release.ts
+++ packages/@n8n/node-cli/src/commands/release.ts
@@ -16,32 +16,45 @@ export default class Release extends Command {
 		intro(await getCommandHeader('n8n-node release'));
 
 		const pm = (await detectPackageManager()) ?? 'npm';
+		const isCI = Boolean(process.env.GITHUB_ACTIONS);
 
 		try {
-			await runCommand(
-				'release-it',
-				[
-					'-n',
-					'--git.requireBranch main',
-					'--git.requireCleanWorkingDir',
-					'--git.requireUpstream',
-					'--git.requireCommits',
-					'--git.commit',
-					'--git.tag',
-					'--git.push',
-					'--git.changelog="npx auto-changelog --stdout --unreleased --commit-limit false -u --hide-credit"',
-					'--github.release',
-					`--hooks.before:init="${pm} run lint && ${pm} run build"`,
-					'--hooks.after:bump="npx auto-changelog -p"',
-				],
-				{
+			if (isCI) {
+				await runCommand(pm, ['run', 'lint'], { stdio: 'inherit' });
+				await runCommand(pm, ['run', 'build'], { stdio: 'inherit' });
+				await runCommand('npm', ['publish'], {
 					stdio: 'inherit',
-					context: 'local',
 					env: {
 						RELEASE_MODE: 'true',
+						NPM_CONFIG_PROVENANCE: 'true',
 					},
-				},
-			);
+				});
+			} else {
+				await runCommand(
+					'release-it',
+					[
+						'-n',
+						'--git.requireBranch main',
+						'--git.requireCleanWorkingDir',
+						'--git.requireUpstream',
+						'--git.requireCommits',
+						'--git.commit',
+						'--git.tag',
+						'--git.push',
+						'--git.changelog="npx auto-changelog --stdout --unreleased --commit-limit false -u --hide-credit"',
+						'--github.release',
+						`--hooks.before:init="${pm} run lint && ${pm} run build"`,
+						'--hooks.after:bump="npx auto-changelog -p"',
+					],
+					{
+						stdio: 'inherit',
+						context: 'local',
+						env: {
+							RELEASE_MODE: 'true',
+						},
+					},
+				);
+			}
 		} catch (error) {
 			if (error instanceof ChildProcessError) {
 				if (error.signal) {
