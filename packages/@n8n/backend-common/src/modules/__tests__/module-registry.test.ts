ef17f6ba0c6c98eab8457fdffac1299e08eb31f2
Charlie Kolb <charlie@n8n.io>
2026-03-23 14:40:13 +0100

    fix tests

--- packages/@n8n/backend-common/src/modules/__tests__/module-registry.test.ts
+++ packages/@n8n/backend-common/src/modules/__tests__/module-registry.test.ts
@@ -38,6 +38,7 @@ describe('eligibleModules', () => {
 			'workflow-builder',
 			'redaction',
 			'instance-registry',
+			'instance-version-history',
 		]);
 	});
 
@@ -62,6 +63,7 @@ describe('eligibleModules', () => {
 			'workflow-builder',
 			'redaction',
 			'instance-registry',
+			'instance-version-history',
 		]);
 	});
 
