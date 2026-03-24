fa77c6704ef9a794c8de7f5ae9a14fbecb1a7869
Stephen <sjw948@gmail.com>
2026-03-24 08:18:20 +0000

    typechecks

--- packages/@n8n/permissions/src/utilities/__tests__/get-resource-permissions.test.ts
+++ packages/@n8n/permissions/src/utilities/__tests__/get-resource-permissions.test.ts
@@ -169,6 +169,7 @@ describe('permissions', () => {
 				manage: true,
 			},
 			credentialResolver: {},
+			roleMappingRule: {},
 		};
 
 		expect(getResourcePermissions(scopes)).toEqual(permissionRecord);
