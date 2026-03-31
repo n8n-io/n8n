2569943776d3c971c70ba74f633a8c099e16eb71
krisn0x <10799186+krisn0x@users.noreply.github.com>
2026-04-01 00:38:56 +0300

    Implement v2 Activity resource operations
    Port Activity create, delete, get, getAll, and update operations from v1
    to the v2 API pattern with cursor pagination, boolean done field, PATCH
    for updates, nested custom_fields, and deal_id filter replacing the
    removed dealActivity resource.

--- packages/nodes-base/nodes/Pipedrive/v2/actions/activity/create.operation.ts
+++ packages/nodes-base/nodes/Pipedrive/v2/actions/activity/create.operation.ts
@@ -1,5 +1,193 @@
-import type { INodeProperties } from 'n8n-workflow';
-export const description: INodeProperties[] = [];
-export async function execute(this: any): Promise<any[]> {
-	return [];
+import type {
+	IDataObject,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeProperties,
+} from 'n8n-workflow';
+
+import { updateDisplayOptions } from '../../../../../utils/utilities';
+import { pipedriveApiRequest, pipedriveGetCustomProperties } from '../../transport';
+import { encodeCustomFieldsV2, coerceToBoolean } from '../../helpers';
+import { customFieldsCollection } from '../common.description';
+
+const properties: INodeProperties[] = [
+	{
+		displayName: 'Subject',
+		name: 'subject',
+		type: 'string',
+		default: '',
+		required: true,
+		description: 'The subject of the activity to create',
+	},
+	{
+		displayName: 'Done',
+		name: 'done',
+		type: 'options',
+		options: [
+			{
+				name: 'Not Done',
+				value: '0',
+			},
+			{
+				name: 'Done',
+				value: '1',
+			},
+		],
+		default: '0',
+		description: 'Whether the activity is done or not',
+	},
+	{
+		displayName: 'Type',
+		name: 'type',
+		type: 'string',
+		default: '',
+		required: true,
+		placeholder: 'call',
+		description: 'Type of the activity like "call", "meeting", etc',
+	},
+	{
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		options: [
+			{
+				displayName: 'Deal ID',
+				name: 'deal_id',
+				type: 'number',
+				default: 0,
+				description: 'ID of the deal this activity will be associated with',
+			},
+			{
+				displayName: 'Due Date',
+				name: 'due_date',
+				type: 'dateTime',
+				default: '',
+				description: 'Due Date to activity be done YYYY-MM-DD',
+			},
+			{
+				displayName: 'Note',
+				name: 'note',
+				type: 'string',
+				typeOptions: {
+					rows: 5,
+				},
+				default: '',
+				description: 'Note of the activity (HTML format)',
+			},
+			{
+				displayName: 'Organization Name or ID',
+				name: 'org_id',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getOrganizationIds',
+				},
+				default: '',
+				description:
+					'ID of the organization this activity will be associated with. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Person ID',
+				name: 'person_id',
+				type: 'number',
+				default: 0,
+				description: 'ID of the person this activity will be associated with',
+			},
+			{
+				displayName: 'User Name or ID',
+				name: 'user_id',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getUserIds',
+				},
+				default: '',
+				description:
+					'ID of the active user whom the activity will be assigned to. If omitted, the activity will be assigned to the authorized user. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			customFieldsCollection,
+		],
+	},
+	{
+		displayName: 'Encode Custom Fields',
+		name: 'encodeCustomFields',
+		type: 'boolean',
+		default: false,
+		description:
+			'Whether to encode custom field values (e.g. convert labels to enum IDs) before sending',
+	},
+];
+
+const displayOptions = {
+	show: {
+		resource: ['activity'],
+		operation: ['create'],
+	},
+};
+
+export const description = updateDisplayOptions(displayOptions, properties);
+
+function addAdditionalFields(body: IDataObject, additionalFields: IDataObject): void {
+	for (const key of Object.keys(additionalFields)) {
+		if (
+			key === 'customFields' &&
+			(additionalFields.customFields as IDataObject)?.property !== undefined
+		) {
+			for (const customProperty of (additionalFields.customFields as IDataObject)
+				.property as Array<{ name: string; value: string }>) {
+				body[customProperty.name] = customProperty.value;
+			}
+		} else {
+			body[key] = additionalFields[key];
+		}
+	}
+}
+
+export async function execute(this: IExecuteFunctions): Promise<INodeExecutionData[]> {
+	const items = this.getInputData();
+	const returnData: INodeExecutionData[] = [];
+
+	const encodeCustom = this.getNodeParameter('encodeCustomFields', 0, false) as boolean;
+	let customProperties;
+	if (encodeCustom) {
+		customProperties = await pipedriveGetCustomProperties.call(this, 'activity');
+	}
+
+	for (let i = 0; i < items.length; i++) {
+		try {
+			const body: IDataObject = {};
+
+			body.subject = this.getNodeParameter('subject', i) as string;
+			body.done = coerceToBoolean(this.getNodeParameter('done', i));
+			body.type = this.getNodeParameter('type', i) as string;
+
+			const additionalFields = this.getNodeParameter('additionalFields', i);
+			addAdditionalFields(body, additionalFields);
+
+			if (customProperties) {
+				encodeCustomFieldsV2(customProperties, body);
+			}
+
+			const responseData = await pipedriveApiRequest.call(this, 'POST', '/activities', body);
+
+			const executionData = this.helpers.constructExecutionMetaData(
+				this.helpers.returnJsonArray(responseData.data as IDataObject),
+				{ itemData: { item: i } },
+			);
+			returnData.push(...executionData);
+		} catch (error) {
+			if (this.continueOnFail()) {
+				returnData.push(
+					...this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray({ error: (error as Error).message }),
+						{ itemData: { item: i } },
+					),
+				);
+				continue;
+			}
+			throw error;
+		}
+	}
+
+	return returnData;
 }
--- packages/nodes-base/nodes/Pipedrive/v2/actions/activity/update.operation.ts
+++ packages/nodes-base/nodes/Pipedrive/v2/actions/activity/update.operation.ts
@@ -1,5 +1,216 @@
-import type { INodeProperties } from 'n8n-workflow';
-export const description: INodeProperties[] = [];
-export async function execute(this: any): Promise<any[]> {
-	return [];
+import type {
+	IDataObject,
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeProperties,
+} from 'n8n-workflow';
+
+import { updateDisplayOptions } from '../../../../../utils/utilities';
+import { pipedriveApiRequest, pipedriveGetCustomProperties } from '../../transport';
+import { encodeCustomFieldsV2, coerceToBoolean } from '../../helpers';
+import { customFieldsCollection, encodeCustomFieldsOption } from '../common.description';
+
+const properties: INodeProperties[] = [
+	{
+		displayName: 'Activity ID',
+		name: 'activityId',
+		type: 'number',
+		default: 0,
+		required: true,
+		description: 'ID of the activity to update',
+	},
+	{
+		displayName: 'Update Fields',
+		name: 'updateFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		options: [
+			{
+				displayName: 'Busy Flag',
+				name: 'busy_flag',
+				type: 'boolean',
+				default: false,
+				description: 'Whether the user is set to busy during the activity',
+			},
+			{
+				displayName: 'Deal ID',
+				name: 'deal_id',
+				type: 'number',
+				default: 0,
+				description: 'ID of the deal this activity will be associated with',
+			},
+			{
+				displayName: 'Done',
+				name: 'done',
+				type: 'options',
+				options: [
+					{
+						name: 'Not Done',
+						value: '0',
+					},
+					{
+						name: 'Done',
+						value: '1',
+					},
+				],
+				default: '0',
+				description: 'Whether the activity is done or not',
+			},
+			{
+				displayName: 'Due Date',
+				name: 'due_date',
+				type: 'dateTime',
+				default: '',
+				description: 'Due Date to activity be done YYYY-MM-DD',
+			},
+			{
+				displayName: 'Note',
+				name: 'note',
+				type: 'string',
+				typeOptions: {
+					rows: 5,
+				},
+				default: '',
+				description: 'Note of the activity (HTML format)',
+			},
+			{
+				displayName: 'Organization Name or ID',
+				name: 'org_id',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getOrganizationIds',
+				},
+				default: '',
+				description:
+					'ID of the organization this activity will be associated with. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			{
+				displayName: 'Person ID',
+				name: 'person_id',
+				type: 'number',
+				default: 0,
+				description: 'ID of the person this activity will be associated with',
+			},
+			{
+				displayName: 'Public Description',
+				name: 'public_description',
+				type: 'string',
+				default: '',
+				description:
+					'Additional details about the activity that is synced to your external calendar',
+			},
+			{
+				displayName: 'Subject',
+				name: 'subject',
+				type: 'string',
+				default: '',
+				description: 'The subject of the activity',
+			},
+			{
+				displayName: 'Type',
+				name: 'type',
+				type: 'string',
+				default: '',
+				placeholder: 'call',
+				description: 'Type of the activity like "call", "meeting", etc',
+			},
+			{
+				displayName: 'User Name or ID',
+				name: 'user_id',
+				type: 'options',
+				typeOptions: {
+					loadOptionsMethod: 'getUserIds',
+				},
+				default: '',
+				description:
+					'ID of the active user whom the activity will be assigned to. If omitted, the activity will be assigned to the authorized user. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+			},
+			customFieldsCollection,
+		],
+	},
+	encodeCustomFieldsOption,
+];
+
+const displayOptions = {
+	show: {
+		resource: ['activity'],
+		operation: ['update'],
+	},
+};
+
+export const description = updateDisplayOptions(displayOptions, properties);
+
+function addUpdateFields(body: IDataObject, updateFields: IDataObject): void {
+	for (const key of Object.keys(updateFields)) {
+		if (
+			key === 'customFields' &&
+			(updateFields.customFields as IDataObject)?.property !== undefined
+		) {
+			for (const customProperty of (updateFields.customFields as IDataObject).property as Array<{
+				name: string;
+				value: string;
+			}>) {
+				body[customProperty.name] = customProperty.value;
+			}
+		} else {
+			body[key] = updateFields[key];
+		}
+	}
+}
+
+export async function execute(this: IExecuteFunctions): Promise<INodeExecutionData[]> {
+	const items = this.getInputData();
+	const returnData: INodeExecutionData[] = [];
+
+	const encodeCustom = this.getNodeParameter('encodeCustomFields', 0, false) as boolean;
+	let customProperties;
+	if (encodeCustom) {
+		customProperties = await pipedriveGetCustomProperties.call(this, 'activity');
+	}
+
+	for (let i = 0; i < items.length; i++) {
+		try {
+			const activityId = this.getNodeParameter('activityId', i) as number;
+			const body: IDataObject = {};
+
+			const updateFields = this.getNodeParameter('updateFields', i);
+			addUpdateFields(body, updateFields);
+
+			// Coerce done to boolean for v2 API
+			if (body.done !== undefined) {
+				body.done = coerceToBoolean(body.done);
+			}
+
+			if (customProperties) {
+				encodeCustomFieldsV2(customProperties, body);
+			}
+
+			const responseData = await pipedriveApiRequest.call(
+				this,
+				'PATCH',
+				`/activities/${activityId}`,
+				body,
+			);
+
+			const executionData = this.helpers.constructExecutionMetaData(
+				this.helpers.returnJsonArray(responseData.data as IDataObject),
+				{ itemData: { item: i } },
+			);
+			returnData.push(...executionData);
+		} catch (error) {
+			if (this.continueOnFail()) {
+				returnData.push(
+					...this.helpers.constructExecutionMetaData(
+						this.helpers.returnJsonArray({ error: (error as Error).message }),
+						{ itemData: { item: i } },
+					),
+				);
+				continue;
+			}
+			throw error;
+		}
+	}
+
+	return returnData;
 }
