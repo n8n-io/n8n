71e916d2a8a8abb6c7017904fe20151c184f091d
Jaakko Husso <jaakko@n8n.io>
2026-03-31 18:03:21 +0300

    fix: linting issue

--- packages/@n8n/db/src/migrations/sqlite/index.ts
+++ packages/@n8n/db/src/migrations/sqlite/index.ts
@@ -148,9 +148,9 @@ import { ChangeWorkflowPublishedVersionFKsToRestrict1772619247762 } from '../com
 import { AddTypeToChatHubSessions1772700000000 } from '../common/1772700000000-AddTypeToChatHubSessions';
 import { CreateRoleMappingRuleTable1772800000000 } from '../common/1772800000000-CreateRoleMappingRuleTable';
 import { CreateCredentialDependencyTable1773000000000 } from '../common/1773000000000-CreateCredentialDependencyTable';
-import { CreateInstanceAiTables1775000000000 } from '../common/1775000000000-CreateInstanceAiTables';
 import { AddRestoreFieldsToWorkflowBuilderSession1774280963551 } from '../common/1774280963551-AddRestoreFieldsToWorkflowBuilderSession';
 import { CreateInstanceVersionHistoryTable1774854660000 } from '../common/1774854660000-CreateInstanceVersionHistoryTable';
+import { CreateInstanceAiTables1775000000000 } from '../common/1775000000000-CreateInstanceAiTables';
 import type { Migration } from '../migration-types';
 
 const sqliteMigrations: Migration[] = [
