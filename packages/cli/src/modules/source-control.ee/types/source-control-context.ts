d49e534b0964a49d4c28bc5782438b3b39dbb689
James Gee <1285296+geemanjs@users.noreply.github.com>
2026-04-02 11:14:37 +0200

    chore: review comments

--- packages/cli/src/modules/source-control.ee/types/source-control-context.ts
+++ packages/cli/src/modules/source-control.ee/types/source-control-context.ts
@@ -2,13 +2,17 @@ import type { Project, User } from '@n8n/db';
 import { hasGlobalScope } from '@n8n/permissions';
 
 export class SourceControlContext {
+	private readonly _hasAccessToAllProjects: boolean;
+
 	constructor(
 		readonly user: User,
 		readonly authorizedProjects: Project[],
 		readonly accessibleWorkflowIds: string[],
-	) {}
+	) {
+		this._hasAccessToAllProjects = hasGlobalScope(this.user, 'project:update');
+	}
 
 	hasAccessToAllProjects() {
-		return hasGlobalScope(this.user, 'project:update');
+		return this._hasAccessToAllProjects;
 	}
 }
