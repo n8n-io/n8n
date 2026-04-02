e75519789b3b73a0af97cc5a061fe0ab6a594508
Bernhard Wittmann <bernhard.wittmann@n8n.io>
2026-04-02 11:58:28 +0200

    fix(Uptime Robot Node): Add credential auth and test

--- packages/nodes-base/credentials/UptimeRobotApi.credentials.ts
+++ packages/nodes-base/credentials/UptimeRobotApi.credentials.ts
@@ -1,4 +1,9 @@
-import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
 
 export class UptimeRobotApi implements ICredentialType {
 	name = 'uptimeRobotApi';
@@ -16,4 +21,24 @@ export class UptimeRobotApi implements ICredentialType {
 			default: '',
 		},
 	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			body: {
+				api_key: '={{$credentials.apiKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.uptimerobot.com',
+			url: '/v2/getAccountDetails',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+			},
+		},
+	};
 }
