34dfea7b011224e113af068ea72893d4ab2b28a7
heymynameisrob <robhough180@gmail.com>
2026-04-10 11:57:41 +0100

    added MDX for design system docs. Updated agent skill

---
+++ packages/frontend/@n8n/design-system/src/styleguide/components/PrimitivesShadowTable.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import VariableTable from '../VariableTable.vue';
+
+const shadowColorVariables = ['--shadow-color'];
+
+const shadowScaleVariables = [
+	'--shadow--outline',
+	'--shadow--2xs',
+	'--shadow--xs',
+	'--shadow--sm',
+	'--shadow--md',
+	'--shadow--xl',
+];
+</script>
+
+<template>
+	<div>
+		<VariableTable :variables="shadowColorVariables" />
+		<VariableTable :variables="shadowScaleVariables" attr="box-shadow" />
+	</div>
+</template>
