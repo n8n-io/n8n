a8bca769121b5bb9e4285fc68c2da51ee1247283
heymynameisrob <robhough180@gmail.com>
2026-04-13 09:54:00 +0100

    added type steps

--- packages/frontend/@n8n/design-system/src/types/text.ts
+++ packages/frontend/@n8n/design-system/src/types/text.ts
@@ -1,6 +1,9 @@
 const TEXT_SIZE = ['xsmall', 'small', 'mini', 'medium', 'large', 'xlarge'] as const;
 export type TextSize = (typeof TEXT_SIZE)[number];
 
+const TEXT_STEP = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
+export type TextStep = (typeof TEXT_STEP)[number];
+
 const TEXT_COLOR = [
 	'primary',
 	'secondary',
