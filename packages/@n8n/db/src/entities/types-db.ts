8ae1cba15ccfe56c25e4d0b5fd472db8904675c9
Michael Siega <michael.siega@n8n.io>
2026-04-18 08:39:49 +0200

    feat(core): add deduplicationKey column to execution entity

--- packages/@n8n/db/src/entities/types-db.ts
+++ packages/@n8n/db/src/entities/types-db.ts
@@ -60,6 +60,7 @@ export interface IExecutionBase {
 	status: ExecutionStatus;
 	waitTill?: Date | null;
 	storedAt: ExecutionDataStorageLocation;
+	deduplicationKey?: string | null;
 }
 
 // Required by PublicUser
