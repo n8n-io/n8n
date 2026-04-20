99cd970fd6bd2b8a98e4747187b07d924b046e65
Ali Elkhateeb <ali.elkhateeb@n8n.io>
2026-04-20 13:59:25 +0300

    fix lint

--- packages/cli/src/errors/credential-not-found.error.ts
+++ packages/cli/src/errors/credential-not-found.error.ts
@@ -2,8 +2,10 @@ import { UserError } from 'n8n-workflow';
 
 export class CredentialNotFoundError extends UserError {
 	constructor(credentialId: string, credentialType?: string) {
-		credentialType
-			? super(`Credential with ID "${credentialId}" does not exist for type "${credentialType}".`)
-			: super(`Credential with ID "${credentialId}" was not found.`);
+		super(
+			credentialType
+				? `Credential with ID "${credentialId}" does not exist for type "${credentialType}".`
+				: `Credential with ID "${credentialId}" was not found.`,
+		);
 	}
 }
