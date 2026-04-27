e9bd32d21397652c2e385e93b2cc4a846974125c
evan <evan@cuwengr.local>
2026-04-27 12:04:31 +0000

    fix(Cloudflare): use item index instead of hardcoded 0 for resource and operation in execute loop

--- packages/nodes-base/nodes/Cloudflare/Cloudflare.node.ts
+++ packages/nodes-base/nodes/Cloudflare/Cloudflare.node.ts
@@ -74,10 +74,10 @@ export class Cloudflare implements INodeType {
 		const length = items.length;
 		const qs: IDataObject = {};
 		let responseData;
-		const resource = this.getNodeParameter('resource', 0);
-		const operation = this.getNodeParameter('operation', 0);
-
 		for (let i = 0; i < length; i++) {
+			const resource = this.getNodeParameter('resource', i);
+			const operation = this.getNodeParameter('operation', i);
+
 			try {
 				if (resource === 'zoneCertificate') {
 					//https://api.cloudflare.com/#zone-level-authenticated-origin-pulls-delete-certificate
