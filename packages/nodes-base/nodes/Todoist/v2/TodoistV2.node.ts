58fd649f617628298ea65c42998808d8b765eb07
evan <evan@cuwengr.local>
2026-04-27 12:28:16 +0000

    fix(Todoist): use item index instead of hardcoded 0 for resource and operation in execute loop

--- packages/nodes-base/nodes/Todoist/v2/TodoistV2.node.ts
+++ packages/nodes-base/nodes/Todoist/v2/TodoistV2.node.ts
@@ -1924,10 +1924,10 @@ export class TodoistV2 implements INodeType {
 		const length = items.length;
 		const service = new TodoistService();
 		let responseData;
-		const resource = this.getNodeParameter('resource', 0);
-		const operation = this.getNodeParameter('operation', 0);
 		for (let i = 0; i < length; i++) {
 			try {
+				const resource = this.getNodeParameter('resource', i);
+				const operation = this.getNodeParameter('operation', i);
 				if (resource === 'task') {
 					if (!isTaskOperationType(operation)) {
 						throw new NodeOperationError(
