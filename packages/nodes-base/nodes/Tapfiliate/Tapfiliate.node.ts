d7a26cbc051a5464667d66bf9237b9dd364679f6
evan <evan@cuwengr.local>
2026-04-27 13:05:34 +0000

    fix(Tapfiliate): use item index instead of hardcoded 0 for resource and operation in execute loop

--- packages/nodes-base/nodes/Tapfiliate/Tapfiliate.node.ts
+++ packages/nodes-base/nodes/Tapfiliate/Tapfiliate.node.ts
@@ -93,9 +93,9 @@ export class Tapfiliate implements INodeType {
 		const qs: IDataObject = {};
 		let responseData;
 		const returnData: INodeExecutionData[] = [];
-		const resource = this.getNodeParameter('resource', 0);
-		const operation = this.getNodeParameter('operation', 0);
 		for (let i = 0; i < length; i++) {
+			const resource = this.getNodeParameter('resource', i);
+			const operation = this.getNodeParameter('operation', i);
 			try {
 				if (resource === 'affiliate') {
 					if (operation === 'create') {
