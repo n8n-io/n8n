966c3487d03449628176d7d5ff77033d6413ce09
evan <evan@cuwengr.local>
2026-04-27 12:27:56 +0000

    fix(Stripe): use item index instead of hardcoded 0 for resource and operation in execute loop

--- packages/nodes-base/nodes/Stripe/Stripe.node.ts
+++ packages/nodes-base/nodes/Stripe/Stripe.node.ts
@@ -139,14 +139,14 @@ export class Stripe implements INodeType {
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
 
-		const resource = this.getNodeParameter('resource', 0);
-		const operation = this.getNodeParameter('operation', 0);
-
 		let responseData;
 		const returnData: INodeExecutionData[] = [];
 
 		for (let i = 0; i < items.length; i++) {
 			try {
+				const resource = this.getNodeParameter('resource', i);
+				const operation = this.getNodeParameter('operation', i);
+
 				if (resource === 'balance') {
 					// *********************************************************************
 					//                             balance
