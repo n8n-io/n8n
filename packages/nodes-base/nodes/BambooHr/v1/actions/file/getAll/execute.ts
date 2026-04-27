24cfb2c8a0543d60e7c41f6258ae170d1d832593
evan <evan@cuwengr.local>
2026-04-27 12:07:39 +0000

    fix(BambooHr): use item index instead of hardcoded 0 for returnAll and limit in file getAll

--- packages/nodes-base/nodes/BambooHr/v1/actions/file/getAll/execute.ts
+++ packages/nodes-base/nodes/BambooHr/v1/actions/file/getAll/execute.ts
@@ -12,8 +12,8 @@ export async function getAll(
 
 	//limit parameters
 	const simplifyOutput: boolean = this.getNodeParameter('simplifyOutput', index) as boolean;
-	const returnAll: boolean = this.getNodeParameter('returnAll', 0, false);
-	const limit: number = this.getNodeParameter('limit', 0, 0);
+	const returnAll: boolean = this.getNodeParameter('returnAll', index, false);
+	const limit: number = this.getNodeParameter('limit', index, 0);
 
 	//response
 	const responseData = await apiRequest.call(this, requestMethod, endpoint, body);
