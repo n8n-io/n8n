b337c5e55642e46bd0bc59ae795c12ebbcdb429f
Daria Staferova <daria.staferova@n8n.io>
2026-05-11 18:31:40 +0300

    Use json

--- packages/@n8n/db/src/migrations/common/1778496335798-AddNodeGroupsColumnToWorkflowAndHistory.ts
+++ packages/@n8n/db/src/migrations/common/1778496335798-AddNodeGroupsColumnToWorkflowAndHistory.ts
@@ -2,8 +2,8 @@ import type { MigrationContext, ReversibleMigration } from '../migration-types';
 
 export class AddNodeGroupsColumnToWorkflowAndHistory1778496335798 implements ReversibleMigration {
 	async up({ schemaBuilder: { addColumns, column } }: MigrationContext) {
-		await addColumns('workflow_entity', [column('nodeGroups').text]);
-		await addColumns('workflow_history', [column('nodeGroups').text]);
+		await addColumns('workflow_entity', [column('nodeGroups').json]);
+		await addColumns('workflow_history', [column('nodeGroups').json]);
 	}
 
 	async down({ schemaBuilder: { dropColumns } }: MigrationContext) {
