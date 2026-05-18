f497c832b4f32d7d6931a19373be6eb18c55ccef
Matsuuu <huhta.matias@gmail.com>
2026-05-18 10:32:32 +0300

    chore: Migrate from SWC to pure OXC

--- packages/@n8n/di/vitest.config.ts
+++ packages/@n8n/di/vitest.config.ts
@@ -1,6 +1,4 @@
 import { mergeConfig } from 'vitest/config';
 import { createVitestConfigWithDecorators } from '@n8n/vitest-config/node-decorators';
 
-export default mergeConfig(createVitestConfigWithDecorators(), {
-	oxc: false,
-});
+export default mergeConfig(createVitestConfigWithDecorators(), {});
