78b225658dd3244de52ba835f64a75ede9664e82
Matsuuu <huhta.matias@gmail.com>
2026-05-21 12:43:14 +0300

    chore: Set isolatedModules on a tsconfig level instead of jest

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ const { resolve } = require('path');
 
 /** @type {import('ts-jest').TsJestGlobalOptions} */
 const tsJestOptions = {
-	isolatedModules: true,
 	tsconfig: {
 		...compilerOptions,
 		declaration: false,
