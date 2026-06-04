b013c707dda325199e58c936e137c97d03acc1a7
Irénée <irenee.ajeneza@n8n.io>
2026-06-04 14:22:36 +0100

    fix: project role modual show condition fixed

--- packages/frontend/editor-ui/src/features/collaboration/projects/components/ProjectCustomRolesUpgradeModal.vue
+++ packages/frontend/editor-ui/src/features/collaboration/projects/components/ProjectCustomRolesUpgradeModal.vue
@@ -36,10 +36,10 @@ const onViewPlans = async () => {
 		</div>
 		<template #footer>
 			<div :class="$style.footer">
-				<N8nButton type="secondary" @click="visible = false">
+				<N8nButton variant="subtle" @click="visible = false">
 					{{ i18n.baseText('generic.cancel') }}
 				</N8nButton>
-				<N8nButton type="primary" @click="onViewPlans">
+				<N8nButton variant="solid" @click="onViewPlans">
 					{{ i18n.baseText('projects.settings.role.upgrade.custom.viewPlans') }}
 					<template #append>
 						<span :class="$style.externalIcon">↗</span>
