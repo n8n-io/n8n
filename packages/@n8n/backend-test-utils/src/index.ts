17adff787750cea826a6ef1a3947f05aa0814a46
Matsuuu <huhta.matias@gmail.com>
2026-06-09 12:09:27 +0300

    Fix linting

--- packages/@n8n/backend-test-utils/src/index.ts
+++ packages/@n8n/backend-test-utils/src/index.ts
@@ -1,6 +1,6 @@
 import type { Logger } from '@n8n/backend-common';
-import { mock } from 'vitest-mock-extended';
 import { vi } from 'vitest';
+import { mock } from 'vitest-mock-extended';
 
 export const mockLogger = (): Logger =>
 	mock<Logger>({ scoped: vi.fn().mockReturnValue(mock<Logger>()) });
