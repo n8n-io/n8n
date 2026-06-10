6016009432ba6ed39d33db441bc11b7e66e8c51b
Irénée <irenee.ajeneza@n8n.io>
2026-06-10 14:52:12 +0100

    feat: check that source control is connected

--- packages/cli/src/modules/n8n-packages-registry.ee/n8n-packages-registry.controller.ts
+++ packages/cli/src/modules/n8n-packages-registry.ee/n8n-packages-registry.controller.ts
@@ -1,10 +1,14 @@
 import type { AuthenticatedRequest } from '@n8n/db';
 import { Get, RestController } from '@n8n/decorators';
 
+import { N8nPackagesRegistryService } from './n8n-packages-registry.service';
+
 @RestController('/n8n-packages-registry')
 export class N8nPackagesRegistryController {
+	constructor(private readonly registryService: N8nPackagesRegistryService) {}
+
 	@Get('/')
 	hello(_req: AuthenticatedRequest) {
-		return { message: 'Hello, world!' };
+		return { connected: this.registryService.isConnected() };
 	}
 }
---
+++ packages/cli/src/modules/n8n-packages-registry.ee/n8n-packages-registry.service.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@n8n/backend-common';
+import { Service } from '@n8n/di';
+import { SourceControlPreferencesService } from '../source-control.ee/source-control-preferences.service.ee';
+
+@Service()
+export class N8nPackagesRegistryService {
+	constructor(
+		private readonly logger: Logger,
+		private readonly sourceControlPreferencesService: SourceControlPreferencesService,
+	) {}
+
+	isConnected(): boolean {
+		const isConnected = this.sourceControlPreferencesService.isSourceControlConnected();
+		this.logger.info(`Source control is connected: ${isConnected}`);
+		return isConnected;
+	}
+}
