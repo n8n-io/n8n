ba7a93d7b7cc3ed0f735251b1b32a269a3e5b354
AI Contributor <gmrnlg1971@gmail.com>
2026-06-24 07:52:17 +0530

    style: apply formatting

--- packages/core/src/execution-engine/interfaces.ts
+++ packages/core/src/execution-engine/interfaces.ts
@@ -8,22 +8,18 @@ import type {
 	WorkflowExecuteMode,
 } from 'n8n-workflow';
 
-export interface IGetExecutePollFunctions {
-	(
-		workflow: Workflow,
-		node: INode,
-		additionalData: IWorkflowExecuteAdditionalData,
-		mode: WorkflowExecuteMode,
-		activation: WorkflowActivateMode,
-	): IPollFunctions;
-}
+export type IGetExecutePollFunctions = (
+	workflow: Workflow,
+	node: INode,
+	additionalData: IWorkflowExecuteAdditionalData,
+	mode: WorkflowExecuteMode,
+	activation: WorkflowActivateMode,
+) => IPollFunctions;
 
-export interface IGetExecuteTriggerFunctions {
-	(
-		workflow: Workflow,
-		node: INode,
-		additionalData: IWorkflowExecuteAdditionalData,
-		mode: WorkflowExecuteMode,
-		activation: WorkflowActivateMode,
-	): ITriggerFunctions;
-}
+export type IGetExecuteTriggerFunctions = (
+	workflow: Workflow,
+	node: INode,
+	additionalData: IWorkflowExecuteAdditionalData,
+	mode: WorkflowExecuteMode,
+	activation: WorkflowActivateMode,
+) => ITriggerFunctions;
