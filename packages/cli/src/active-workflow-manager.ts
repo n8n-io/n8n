3f65459a53771bea071e4798a5fd51bca5f09413
Elias Meire <elias@meire.dev>
2026-06-25 21:40:47 +0200

    fix(core): Fix grammar in workflow deactivation error message

--- packages/cli/src/active-workflow-manager.ts
+++ packages/cli/src/active-workflow-manager.ts
@@ -367,7 +367,7 @@ export class ActiveWorkflowManager {
 				void this.activeWorkflowTriggers.remove(failedWorkflowData.id);
 				void this.activationErrorsService.register(failedWorkflowData.id, error.message);
 				const activationError = new WorkflowActivationError(
-					`There was a problem with the trigger node "${node.name}", for that reason did the workflow had to be deactivated`,
+					`The trigger node "${node.name}" failed, so the workflow had to be deactivated`,
 					{ cause: error, node },
 				);
 				this.executeErrorWorkflow(activationError, failedWorkflowData, failureMode);
