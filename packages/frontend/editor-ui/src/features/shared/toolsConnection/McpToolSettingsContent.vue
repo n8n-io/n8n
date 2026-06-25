290154cdf07e0e5dc0c19d4293684fc357c6af21
Roman Davydchuk <roman.davydchuk@n8n.io>
2026-06-25 07:24:25 +0000

    feat: Show truncated tool descriptions

--- packages/frontend/editor-ui/src/features/shared/toolsConnection/McpToolSettingsContent.vue
+++ packages/frontend/editor-ui/src/features/shared/toolsConnection/McpToolSettingsContent.vue
@@ -43,11 +43,33 @@ const inclusionOptions: Array<{ value: McpToolInclusionMode; label: string }> = 
 ];
 
 const toolOptions = computed(() =>
-	props.item.availableTools.map((tool) => ({ value: tool.id, label: tool.name })),
+	props.item.availableTools.map((tool) => ({
+		value: tool.id,
+		label: tool.name,
+		description: tool.description,
+	})),
 );
 
-const showIncludeList = computed(() => inclusionMode.value === 'selected');
-const showExcludeList = computed(() => inclusionMode.value === 'except');
+const showToolList = computed(
+	() => inclusionMode.value === 'selected' || inclusionMode.value === 'except',
+);
+const toolListLabel = computed(() =>
+	inclusionMode.value === 'selected'
+		? i18n.baseText('tools.connection.settings.toolsToInclude')
+		: i18n.baseText('tools.connection.settings.toolsToExclude'),
+);
+const toolListTestId = computed(() =>
+	inclusionMode.value === 'selected'
+		? 'tools-connection-settings-selected'
+		: 'tools-connection-settings-excluded',
+);
+const toolListSelection = computed<string[]>({
+	get: () => (inclusionMode.value === 'except' ? excludedTools.value : selectedTools.value),
+	set: (value) => {
+		if (inclusionMode.value === 'except') excludedTools.value = value;
+		else selectedTools.value = value;
+	},
+});
 
 function toolsKey(tools: string[]): string {
 	return JSON.stringify([...tools].sort());
@@ -96,47 +118,32 @@ function handleSave() {
 				</N8nSelect>
 			</div>
 
-			<div v-if="showIncludeList" :class="$style.field">
+			<div v-if="showToolList" :class="$style.field">
 				<N8nText :class="$style.fieldLabel" tag="label" size="small">
-					{{ i18n.baseText('tools.connection.settings.toolsToInclude') }}
+					{{ toolListLabel }}
 				</N8nText>
 				<N8nSelect
-					v-model="selectedTools"
+					v-model="toolListSelection"
 					multiple
 					filterable
 					size="small"
 					:class="$style.multiSelect"
 					:placeholder="i18n.baseText('tools.connection.settings.toolsPlaceholder')"
-					data-test-id="tools-connection-settings-selected"
+					:data-test-id="toolListTestId"
 				>
 					<N8nOption
 						v-for="opt in toolOptions"
 						:key="opt.value"
 						:value="opt.value"
 						:label="opt.label"
-					/>
-				</N8nSelect>
-			</div>
-
-			<div v-if="showExcludeList" :class="$style.field">
-				<N8nText :class="$style.fieldLabel" tag="label" size="small">
-					{{ i18n.baseText('tools.connection.settings.toolsToExclude') }}
-				</N8nText>
-				<N8nSelect
-					v-model="excludedTools"
-					multiple
-					filterable
-					size="small"
-					:class="$style.multiSelect"
-					:placeholder="i18n.baseText('tools.connection.settings.toolsPlaceholder')"
-					data-test-id="tools-connection-settings-excluded"
-				>
-					<N8nOption
-						v-for="opt in toolOptions"
-						:key="opt.value"
-						:value="opt.value"
-						:label="opt.label"
-					/>
+					>
+						<div :class="$style.listOption">
+							<div :class="$style.optionHeadline">{{ opt.label }}</div>
+							<div v-if="opt.description" :class="$style.optionDescription">
+								{{ opt.description }}
+							</div>
+						</div>
+					</N8nOption>
 				</N8nSelect>
 			</div>
 		</div>
@@ -196,6 +203,31 @@ function handleSave() {
 
 .fieldLabel {
 	color: var(--color--text);
+}
+
+.listOption {
+	margin: 6px 0;
+	padding-right: 20px;
+	white-space: normal;
+}
+
+.optionHeadline {
+	font-weight: var(--font-weight--medium);
+	line-height: var(--line-height--md);
+	overflow-wrap: break-word;
+}
+
+.optionDescription {
+	margin-top: 2px;
+	color: var(--color--text--tint-1);
+	font-size: var(--font-size--2xs);
+	font-weight: var(--font-weight--regular);
+	line-height: var(--line-height--xl);
+	overflow: hidden;
+	display: -webkit-box;
+	-webkit-line-clamp: 2;
+	-webkit-box-orient: vertical;
+	text-overflow: ellipsis;
 }
 
 .footer {
