130ef550da26357ade9f9fe28f669470b2712716
Andreas Fitzek <andreas.fitzek@n8n.io>
2026-06-26 08:56:12 +0200

    Add lock/lease service

--- packages/@n8n/backend-common/src/index.ts
+++ packages/@n8n/backend-common/src/index.ts
@@ -12,3 +12,6 @@ export { assertDir, exists } from './utils/fs';
 export { parseFlatted } from './utils/parse-flatted';
 export { CliParser } from './cli-parser';
 export { TypedEmitter } from './typed-emitter';
+
+export { LockService } from './locking/lock.service';
+export { type ILockService, LockNamespace } from './locking/lock-service.interface';
