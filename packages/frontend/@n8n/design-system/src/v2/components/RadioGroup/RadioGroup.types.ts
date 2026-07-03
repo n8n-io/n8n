e24c982bcb5c055ef345d063d116f6fa2d3c25eb
Sebastien Powell <sebpowell@users.noreply.github.com>
2026-07-03 09:40:33 +0100

    Create RadioGroup.types.ts

---
+++ packages/frontend/@n8n/design-system/src/v2/components/RadioGroup/RadioGroup.types.ts
@@ -0,0 +1,9 @@
+import { RadioGroupRootEmits, RadioGroupRootProps } from 'reka-ui';
+
+export type RadioGroupProps = RadioGroupRootProps;
+
+export type RadioGroupEmits = RadioGroupRootEmits;
+
+export type RadioGroupSlots = {
+	default(): unknown;
+};
