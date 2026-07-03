87b416e512a0964047622e331d06166cf346185e
Sebastien Powell <sebpowell@users.noreply.github.com>
2026-07-03 09:34:23 +0100

    Create index.ts

---
+++ packages/frontend/@n8n/design-system/src/v2/components/RadioGroup/index.ts
@@ -0,0 +1,4 @@
+export { default as N8nRadio2 } from './Radio.vue';
+export { default as N8nRadioGroup2 } from './RadioGroup.vue';
+export type * from './Radio.types';
+export type * from './RadioGroup.types';
