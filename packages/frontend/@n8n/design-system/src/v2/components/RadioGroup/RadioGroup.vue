b2992f1b65737b79a58d057d06bc5ae0f2339544
Sebastien Powell <sebpowell@users.noreply.github.com>
2026-07-03 10:00:30 +0100

    Create RadioGroup.vue

---
+++ packages/frontend/@n8n/design-system/src/v2/components/RadioGroup/RadioGroup.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { reactivePick } from '@vueuse/core';
+import { RadioGroupRoot, useForwardPropsEmits } from './reka-ui';
+
+import type { RadioGroupEmits, RadioGroupProps, RadioGroupSlots } from './RadioGroup.types';
+
+defineOptions({ inheritAttrs: false });
+
+const props = withDefaults(defineProps<RadioGroupProps>(), {
+	orientation: 'vertical',
+	disabled: false,
+});
+
+const emit = defineEmits<RadioGroupEmits>();
+defineSlots<RadioGroupSlots>();
+
+const modelValue = defineModel<RadioGroupProps['modelValue']>();
+
+const rootProps = useForwardPropsEmits(
+	reactivePick(props, 'disabled', 'orientation', 'name', 'required', 'loop', 'dir', 'defaultValue'),
+	emit,
+);
+</script>
+
+<template>
+	<RadioGroupRoot
+		v-bind="rootProps"
+		v-model="modelValue"
+		:class="[$style.root, $style[orientation]]"
+	>
+		<slot />
+	</RadioGroupRoot>
+</template>
+
+<style module>
+.root {
+	display: flex;
+	gap: var(--spacing--4xs);
+}
+
+.vertical {
+	flex-direction: column;
+	align-items: flex-start;
+}
+
+.horizontal {
+	flex-direction: row;
+	flex-wrap: wrap;
+	align-items: center;
+}
+</style>
