9e4b4beed92e1df8ca79b9e285efe123e6d4dd31
yehorkardash <yehor.kardash@n8n.io>
2026-07-08 09:07:23 +0200

    feat(Merge Node): Add `NODES_MERGE_SQL_SANDBOX_MEMORY_LIMIT_MB` variable (#33652)
    Co-authored-by: Michael Kret <88898367+michael-radency@users.noreply.github.com>

--- packages/@n8n/config/test/config.test.ts
+++ packages/@n8n/config/test/config.test.ts
@@ -194,6 +194,7 @@ describe('GlobalConfig', () => {
 			include: [],
 			exclude: ['n8n-nodes-base.executeCommand', 'n8n-nodes-base.localFileTrigger'],
 			pythonEnabled: true,
+			mergeSqlSandboxMemoryLimitMb: 64,
 		},
 		publicApi: {
 			disabled: false,
