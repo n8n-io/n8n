3a967686424e35a66330e9ef44e956de6dd42ffc
Stephen <sjw948@gmail.com>
2026-07-20 10:10:17 +0100

    feat(Microsoft SharePoint Node): Add v2 list columns as fillable form fields (no-changelog)

--- packages/nodes-base/nodes/Microsoft/SharePoint/v2/methods/index.ts
+++ packages/nodes-base/nodes/Microsoft/SharePoint/v2/methods/index.ts
@@ -1,7 +1,12 @@
 import { getLists } from '../list';
+import { getMappingColumns } from '../list/columns';
 import { getSites } from '../site';
 
 export const listSearch = {
 	getSites,
 	getLists,
+};
+
+export const resourceMapping = {
+	getMappingColumns,
 };
