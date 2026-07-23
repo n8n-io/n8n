7a058db6a708cff64e4e123bd7edc7ab88533b6a
Sebastien Powell <sebpowell@users.noreply.github.com>
2026-07-23 12:44:18 +0100

    WIP

--- packages/frontend/@n8n/design-system/src/components/N8nInput/Input.types.ts
+++ packages/frontend/@n8n/design-system/src/components/N8nInput/Input.types.ts
@@ -20,9 +20,6 @@ export interface InputProps {
 	clearable?: boolean;
 	rows?: number;
 	maxlength?: number;
-	min?: number | string;
-	max?: number | string;
-	step?: number | string;
 	autosize?: boolean | { minRows?: number; maxRows?: number };
 	autofocus?: boolean;
 	autocomplete?: InputAutocomplete;
