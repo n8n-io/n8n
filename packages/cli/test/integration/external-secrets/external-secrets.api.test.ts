546fe34ed4a40dffbd8e3093d801b58c017121a1
Irénée <irenee.ajeneza@n8n.io>
2026-07-30 07:32:20 +0100

    fix: prevent cache gate during replacement of provider

--- packages/cli/test/integration/external-secrets/external-secrets.api.test.ts
+++ packages/cli/test/integration/external-secrets/external-secrets.api.test.ts
@@ -13,7 +13,6 @@ import { ExternalSecretsConfig } from '@/modules/external-secrets.ee/external-se
 import { ExternalSecretsProviderConnectionManager } from '@/modules/external-secrets.ee/external-secrets-provider-connection-manager.ee';
 import { ExternalSecretsProviderLifecycle } from '@/modules/external-secrets.ee/provider-lifecycle.service';
 import { ExternalSecretsProviderRegistry } from '@/modules/external-secrets.ee/provider-registry.service';
-import { ExternalSecretsRetryManager } from '@/modules/external-secrets.ee/retry-manager.service';
 import { ExternalSecretsSecretsCache } from '@/modules/external-secrets.ee/secrets-cache.service';
 import { ExternalSecretsSettingsStore } from '@/modules/external-secrets.ee/settings-store.service';
 import type {
@@ -70,7 +69,6 @@ const resetManager = async () => {
 	const settingsStore = Container.get(ExternalSecretsSettingsStore);
 	const providerRegistry = Container.get(ExternalSecretsProviderRegistry);
 	const providerLifecycle = Container.get(ExternalSecretsProviderLifecycle);
-	const retryManager = Container.get(ExternalSecretsRetryManager);
 	const providerConnectionManager = Container.get(ExternalSecretsProviderConnectionManager);
 	const secretsCache = Container.get(ExternalSecretsSecretsCache);
 	const secretsProviderConnectionRepository = Container.get(SecretsProviderConnectionRepository);
@@ -87,7 +85,6 @@ const resetManager = async () => {
 			settingsStore,
 			providerRegistry,
 			providerLifecycle,
-			retryManager,
 			providerConnectionManager,
 			secretsCache,
 			secretsProviderConnectionRepository,
@@ -143,7 +140,6 @@ beforeAll(async () => {
 	const settingsStore = Container.get(ExternalSecretsSettingsStore);
 	const providerRegistry = Container.get(ExternalSecretsProviderRegistry);
 	const providerLifecycle = Container.get(ExternalSecretsProviderLifecycle);
-	const retryManager = Container.get(ExternalSecretsRetryManager);
 	const providerConnectionManager = Container.get(ExternalSecretsProviderConnectionManager);
 	const secretsCache = Container.get(ExternalSecretsSecretsCache);
 	const secretsProviderConnectionRepository = Container.get(SecretsProviderConnectionRepository);
@@ -160,7 +156,6 @@ beforeAll(async () => {
 			settingsStore,
 			providerRegistry,
 			providerLifecycle,
-			retryManager,
 			providerConnectionManager,
 			secretsCache,
 			secretsProviderConnectionRepository,
