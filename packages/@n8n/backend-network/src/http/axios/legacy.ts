e71af1d29f0062c53d0f6db0d6feb4b87e72bebd
Matsu <matias.huhta@n8n.io>
2026-08-05 07:55:31 +0300

    chore: Bundle/2.x (#321)

--- packages/@n8n/backend-network/src/http/axios/legacy.ts
+++ packages/@n8n/backend-network/src/http/axios/legacy.ts
@@ -18,6 +18,7 @@ import {
 	getBeforeRedirectFn,
 	getHostFromRequestObject,
 	isFormDataInstance,
+	resolveLegacyRequestTarget,
 	searchForHeader,
 	setAxiosAgents,
 } from './utils';
@@ -165,12 +166,9 @@ export async function buildAxiosConfigFromLegacyRequest(
 		}
 	}
 
-	if (requestObject.uri !== undefined) {
-		axiosConfig.url = requestObject.uri?.toString();
-	}
-
-	if (requestObject.url !== undefined) {
-		axiosConfig.url = requestObject.url?.toString();
+	const target = resolveLegacyRequestTarget(requestObject);
+	if (target !== undefined) {
+		axiosConfig.url = target;
 	}
 
 	if (requestObject.baseURL !== undefined) {
