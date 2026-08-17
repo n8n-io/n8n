31c416ed5abc3db4f5472d3373fb8e2ffde368aa
Matsuuu <matias.huhta@n8n.io>
2026-08-17 10:22:12 +0300

    chore: Resolve 3.x sync conflicts

--- packages/cli/test/integration/public-api/endpoints-with-scopes-enabled.test.ts
+++ packages/cli/test/integration/public-api/endpoints-with-scopes-enabled.test.ts
@@ -1751,7 +1751,7 @@ describe('Public API endpoints with API key scopes', () => {
 								position: [240, 300],
 							},
 							{
-								id: 'uuid-1234',
+								id: 'uuid-5678',
 								parameters: utils.SCHEDULE_TRIGGER_PARAMETERS,
 								name: 'Schedule Trigger',
 								type: 'n8n-nodes-base.scheduleTrigger',
@@ -1832,7 +1832,7 @@ describe('Public API endpoints with API key scopes', () => {
 								position: [240, 300],
 							},
 							{
-								id: 'uuid-1234',
+								id: 'uuid-5678',
 								parameters: utils.SCHEDULE_TRIGGER_PARAMETERS,
 								name: 'Schedule Trigger',
 								type: 'n8n-nodes-base.scheduleTrigger',
