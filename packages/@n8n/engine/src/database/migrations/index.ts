0f6fb9238e5dc5577e5cea9ddf928f3601b040db
Iván Ovejero <ivov.src@gmail.com>
2026-08-18 16:55:33 +0200

    refactor(engine): Fold the iteration column into the create migration

--- packages/@n8n/engine/src/database/migrations/index.ts
+++ packages/@n8n/engine/src/database/migrations/index.ts
@@ -1,9 +1,7 @@
 import { CreateWorkflowExecution1778529600000 } from './1778529600000-CreateWorkflowExecution';
 import { CreateWorkflowStepExecution1784890100000 } from './1784890100000-CreateWorkflowStepExecution';
-import { AddWorkflowStepExecutionIteration1786924800000 } from './1786924800000-AddWorkflowStepExecutionIteration';
 
 export const migrations = [
 	CreateWorkflowExecution1778529600000,
 	CreateWorkflowStepExecution1784890100000,
-	AddWorkflowStepExecutionIteration1786924800000,
 ];
