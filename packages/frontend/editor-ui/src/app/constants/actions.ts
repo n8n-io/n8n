5f293d31d26e412d80c1c21d1f101e1d5c9d2e84
Pierantonio Cangianiello <10360978+pcan@users.noreply.github.com>
2026-08-26 10:31:46 +0200

    feat(editor)!: Remove workflow import from URL

--- packages/frontend/editor-ui/src/app/constants/actions.ts
+++ packages/frontend/editor-ui/src/app/constants/actions.ts
@@ -2,7 +2,6 @@ export const enum WORKFLOW_MENU_ACTIONS {
 	DUPLICATE = 'duplicate',
 	DOWNLOAD = 'download',
 	IMPORT = 'import',
-	IMPORT_FROM_URL = 'import-from-url',
 	IMPORT_FROM_FILE = 'import-from-file',
 	PUSH = 'push',
 	EDIT_DESCRIPTION = 'edit-description',
