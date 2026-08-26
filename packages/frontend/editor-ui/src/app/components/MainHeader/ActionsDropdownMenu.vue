f07add899919d62adf9835f18b70959c3e57967c
Pierantonio Cangianiello <10360978+pcan@users.noreply.github.com>
2026-08-26 10:31:46 +0200

    feat(editor)!: Remove workflow import from URL

--- packages/frontend/editor-ui/src/app/components/MainHeader/ActionsDropdownMenu.vue
+++ packages/frontend/editor-ui/src/app/components/MainHeader/ActionsDropdownMenu.vue
@@ -10,7 +10,6 @@ import {
 	WORKFLOW_MENU_ACTIONS,
 	VIEWS,
 	DUPLICATE_MODAL_KEY,
-	IMPORT_WORKFLOW_URL_MODAL_KEY,
 	WORKFLOW_SETTINGS_MODAL_KEY,
 	WORKFLOW_SHARE_MODAL_KEY,
 	EnterpriseEditionFeature,
@@ -191,11 +190,6 @@ const workflowMenuItems = computed<Array<ActionDropdownItem<WORKFLOW_MENU_ACTION
 			disabled: onExecutionsTab.value,
 			children: [
 				{
-					id: WORKFLOW_MENU_ACTIONS.IMPORT_FROM_URL,
-					label: locale.baseText('menuActions.importFromUrl'),
-					disabled: onExecutionsTab.value,
-				},
-				{
 					id: WORKFLOW_MENU_ACTIONS.IMPORT_FROM_FILE,
 					label: locale.baseText('menuActions.importFromFile'),
 					disabled: onExecutionsTab.value,
@@ -358,10 +352,6 @@ async function onWorkflowMenuSelect(action: WORKFLOW_MENU_ACTIONS): Promise<void
 
 			telemetry.track('User exported workflow', { workflow_id: workflowData.id });
 			saveAs(blob, name + '.json');
-			break;
-		}
-		case WORKFLOW_MENU_ACTIONS.IMPORT_FROM_URL: {
-			uiStore.openModal(IMPORT_WORKFLOW_URL_MODAL_KEY);
 			break;
 		}
 		case WORKFLOW_MENU_ACTIONS.IMPORT_FROM_FILE: {
