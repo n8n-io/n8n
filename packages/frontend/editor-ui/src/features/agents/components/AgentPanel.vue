044fd825529d34fda38ee76ea42c39e727aae29e
heymynameisrob <robhough180@gmail.com>
2026-09-04 10:08:30 +0100

    Add AgentPanel

---
+++ packages/frontend/editor-ui/src/features/agents/components/AgentPanel.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import AgentPanelHeader from './AgentPanelHeader.vue';
+
+interface AgentPanelProps = {
+  header:string;
+  description?:string;
+  showHeader?:boolean;
+}
+
+const props = withDefaults(defineProps<AgentPanelProps>({
+  showHeader:true;
+}))
+</script>
+
+<template>
+	<section class="$style.panelContainer">
+		<AgentPanelHeader
+			v-if="props.showHeader"
+			:title="props.header"
+			:description="props.description"
+		/>
+		<div class="$style.panel">
+			<slot />
+		</div>
+	</section>
+</template>
+
+<style module lang="scss">
+.panelContainer {
+	display: flex;
+	flex-direction: flex-column;
+}
+.panel {
+	background-color: var(--background--surface);
+	border-radius: var(--radius);
+	border: var(--border);
+	box-shadow: var(--shadow--xs);
+}
+</style>
