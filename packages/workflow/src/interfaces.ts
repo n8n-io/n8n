7f085739d3197dfe31365926d634b4bfcdefade6
adityajha77 <adityakartikey1259@gmail.com>
2026-09-08 02:13:50 +0530

    fix(core): Match regex case-insensitively when ignore case is enabled

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -4316,7 +4316,7 @@ export type FilterOptionsValue = {
 	caseSensitive: boolean;
 	leftValue: string;
 	typeValidation: 'strict' | 'loose';
-	version: 1 | 2 | 3;
+	version: 1 | 2 | 3 | 4;
 };
 
 export type FilterValue = {
