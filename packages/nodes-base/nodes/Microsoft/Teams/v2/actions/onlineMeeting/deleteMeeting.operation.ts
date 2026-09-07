02ce7fbc5d88de03317318706848bf080b6371c0
Stephen <sjw948@gmail.com>
2026-09-07 10:38:38 +0100

    fix(Microsoft Teams Node): Throw the rewritten not-found error in Online Meeting Delete

--- packages/nodes-base/nodes/Microsoft/Teams/v2/actions/onlineMeeting/deleteMeeting.operation.ts
+++ packages/nodes-base/nodes/Microsoft/Teams/v2/actions/onlineMeeting/deleteMeeting.operation.ts
@@ -32,7 +32,7 @@ export async function execute(this: IExecuteFunctions, i: number) {
 		await meetingRequest.call(this, 'DELETE', endpoint);
 		return { success: true };
 	} catch (error) {
-		rewriteNotFound.call(
+		throw rewriteNotFound.call(
 			this,
 			error,
 			"The meeting you are trying to delete doesn't exist",
