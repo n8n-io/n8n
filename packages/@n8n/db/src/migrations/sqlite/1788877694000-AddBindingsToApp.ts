1e7d6e528ae12ac3511baafceedc773ade868933
Elias Meire <elias@meire.dev>
2026-09-09 11:02:55 +0200

    fix(core): Disable foreign keys while SQLite recreates the app table for bindings

---
+++ packages/@n8n/db/src/migrations/sqlite/1788877694000-AddBindingsToApp.ts
@@ -0,0 +1,5 @@
+import { AddBindingsToApp1788877694000 as BaseMigration } from '../common/1788877694000-AddBindingsToApp';
+
+export class AddBindingsToApp1788877694000 extends BaseMigration {
+	withFKsDisabled = true as const;
+}
