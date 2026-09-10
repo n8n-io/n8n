99aeb76731e66af1be5b9495f18d6bf4d38a20a4
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2026-09-10 17:07:07 +0300

    fix(core): Allow a null annotation vote in the execution summary

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -4249,7 +4249,7 @@ export interface ExecutionSummary {
 	};
 	usedPrivateCredentials?: boolean;
 	annotation?: {
-		vote?: AnnotationVote;
+		vote?: AnnotationVote | null;
 		tags: Array<{
 			id: string;
 			name: string;
