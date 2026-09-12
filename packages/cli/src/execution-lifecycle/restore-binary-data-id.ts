d41926f23fa6cb1300d5262b0d11f46c9579fe05
Mike Repeć <mike.repec@n8n.io>
2026-09-12 23:17:21 +0200

    chore: Fix 3.x sync conflicts

--- packages/cli/src/execution-lifecycle/restore-binary-data-id.ts
+++ packages/cli/src/execution-lifecycle/restore-binary-data-id.ts
@@ -1,7 +1,7 @@
 import { Logger } from '@n8n/backend-common';
 import { Container } from '@n8n/di';
 import type { BinaryData } from 'n8n-core';
-import { BinaryDataService } from 'n8n-core';
+import { BinaryDataService, TEMP_EXECUTION_ID } from 'n8n-core';
 import type { IBinaryData, IRun, WorkflowExecuteMode } from 'n8n-workflow';
 
 /**
