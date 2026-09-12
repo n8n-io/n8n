2320a21c8f9d60cd50de3cacd08d631da85d2d98
Mike Repeć <mike.repec@n8n.io>
2026-09-12 23:17:21 +0200

    chore: Fix 3.x sync conflicts

--- packages/cli/src/execution-lifecycle/restore-binary-data-id.ts
+++ packages/cli/src/execution-lifecycle/restore-binary-data-id.ts
@@ -1,11 +1,7 @@
 import { Logger } from '@n8n/backend-common';
 import { Container } from '@n8n/di';
 import type { BinaryData } from 'n8n-core';
-<<<<<<< HEAD
-import { BinaryDataService } from 'n8n-core';
-=======
-import { BinaryDataConfig, BinaryDataService, TEMP_EXECUTION_ID } from 'n8n-core';
->>>>>>> 072f4960f0b6cf3ab5b55b402a9799bb0f84c971
+import { BinaryDataService, TEMP_EXECUTION_ID } from 'n8n-core';
 import type { IBinaryData, IRun, WorkflowExecuteMode } from 'n8n-workflow';
 
 /**
--- packages/core/src/binary-data/index.ts
+++ packages/core/src/binary-data/index.ts
@@ -1,12 +1,5 @@
 export * from './binary-data.service';
-<<<<<<< HEAD
-export { BinaryDataBlobManager, getExecutionIdFromFileId } from './blob.manager';
+export { BinaryDataBlobManager, parseExecutionFileId } from './blob.manager';
 export { BinaryDataConfig, isInMemoryModeConfigured } from './binary-data.config';
 export type * from './types';
-export { isStoredMode, FileLocation } from './utils';
-=======
-export { BinaryDataBlobManager, parseExecutionFileId } from './blob.manager';
-export { BinaryDataConfig } from './binary-data.config';
-export type * from './types';
-export { isStoredMode as isValidNonDefaultMode, FileLocation, TEMP_EXECUTION_ID } from './utils';
->>>>>>> 072f4960f0b6cf3ab5b55b402a9799bb0f84c971
+export { isStoredMode, FileLocation, TEMP_EXECUTION_ID } from './utils';
