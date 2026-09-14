8f9faa7b5813f3403d75fc50fd13f2fbcb83c132
Yen Su <49794855+yens1@users.noreply.github.com>
2026-09-14 13:05:31 +0000

    feat(Microsoft Teams Node): Add the chat member remove operation (#37747)

--- packages/nodes-base/nodes/Microsoft/Teams/v2/actions/node.type.ts
+++ packages/nodes-base/nodes/Microsoft/Teams/v2/actions/node.type.ts
@@ -3,7 +3,7 @@ import type { AllEntities } from 'n8n-workflow';
 type NodeMap = {
 	channel: 'create' | 'deleteChannel' | 'get' | 'getAll' | 'update';
 	channelMessage: 'create' | 'get' | 'getAll' | 'getAllReplies' | 'reply' | 'softDeleteMessage';
-	chatMember: 'add' | 'getAll';
+	chatMember: 'add' | 'getAll' | 'remove';
 	chatMessage:
 		| 'create'
 		| 'get'
