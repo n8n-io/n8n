88c84b506ae626387cdab824a48a86e5ed54307f
Matsuuu <matias.huhta@n8n.io>
2026-09-15 09:49:27 +0300

    chore: Resolve 3.x sync conflicts

--- packages/@n8n/permissions/src/roles/scopes/project-scopes.ee.ts
+++ packages/@n8n/permissions/src/roles/scopes/project-scopes.ee.ts
@@ -79,6 +79,11 @@ export const REGULAR_PROJECT_ADMIN_SCOPES: Scope[] = [
 	'externalSecretsProvider:list',
 	'externalSecretsProvider:sync',
 	'externalSecret:list',
+	'projectAiPreference:list',
+	'projectAiPreference:read',
+	'projectAiPreference:create',
+	'projectAiPreference:update',
+	'projectAiPreference:delete',
 ];
 
 export const PERSONAL_PROJECT_OWNER_SCOPES: Scope[] = [
@@ -195,6 +200,11 @@ export const PROJECT_EDITOR_SCOPES: Scope[] = [
 	'externalSecretsProvider:read',
 	'externalSecretsProvider:list',
 	'externalSecret:list',
+	'projectAiPreference:list',
+	'projectAiPreference:read',
+	'projectAiPreference:create',
+	'projectAiPreference:update',
+	'projectAiPreference:delete',
 ];
 
 export const PROJECT_VIEWER_SCOPES: Scope[] = [
