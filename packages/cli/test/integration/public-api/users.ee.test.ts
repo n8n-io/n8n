df8090a2bcc2420e2a49e97cebb017a1abed05cb
Sam Wooler <swooler592@gmail.com>
2026-09-16 14:52:13 +0200

    test(API): Revert users integration test

--- packages/cli/test/integration/public-api/users.ee.test.ts
+++ packages/cli/test/integration/public-api/users.ee.test.ts
@@ -1,4 +1,9 @@
-import { createTeamProject, linkUserToProject, testDb } from '@n8n/backend-test-utils';
+import {
+	createTeamProject,
+	linkUserToProject,
+	testDb,
+	mockInstance,
+} from '@n8n/backend-test-utils';
 import { GLOBAL_MEMBER_ROLE, type User } from '@n8n/db';
 import { v4 as uuid } from 'uuid';
 import validator from 'validator';
@@ -14,6 +19,10 @@ import {
 } from '../shared/db/users';
 import type { SuperAgentTest } from '../shared/types';
 import * as utils from '../shared/utils/';
+
+mockInstance(License, {
+	getUsersLimit: vi.fn().mockReturnValue(-1),
+});
 
 const testServer = utils.setupTestServer({ endpointGroups: ['publicApi'] });
 
@@ -286,7 +295,7 @@ describe('With license without quota:users', () => {
 	let authOwnerAgent: SuperAgentTest;
 
 	beforeEach(async () => {
-		testServer.license.setQuota('quota:users', 0);
+		mockInstance(License, { getUsersLimit: vi.fn().mockReturnValue(null) });
 
 		const owner = await createOwnerWithApiKey();
 		authOwnerAgent = testServer.publicApiAgentFor(owner);
