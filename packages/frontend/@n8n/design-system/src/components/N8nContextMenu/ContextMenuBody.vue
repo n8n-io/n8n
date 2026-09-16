2b813b01b9deec13f87025193c4fdf397fd717b9
Sebastien Powell <sebpowell@users.noreply.github.com>
2026-09-16 15:07:55 +0100

    Update ContextMenuBody.vue

--- packages/frontend/@n8n/design-system/src/components/N8nContextMenu/ContextMenuBody.vue
+++ packages/frontend/@n8n/design-system/src/components/N8nContextMenu/ContextMenuBody.vue
@@ -8,7 +8,7 @@ import N8nLoading from '../N8nLoading';
 
 defineOptions({ name: 'N8nContextMenuBody' });
 
-withDefaults(
+const props = withDefaults(
 	defineProps<{
 		nodes: Array<ContextMenuNode<T>>;
 		loading?: boolean;
