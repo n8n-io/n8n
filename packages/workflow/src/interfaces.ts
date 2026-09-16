9b66a2bbfa9fb901af56e61e2e0aa49c5b2fc331
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2026-09-16 10:45:58 +0000

    feat(engine): Read executions from the data plane and merge with the DB (#38110)

--- packages/workflow/src/interfaces.ts
+++ packages/workflow/src/interfaces.ts
@@ -4255,7 +4255,7 @@ export interface ExecutionSummary {
 	};
 	usedPrivateCredentials?: boolean;
 	annotation?: {
-		vote: AnnotationVote;
+		vote?: AnnotationVote | null;
 		tags: Array<{
 			id: string;
 			name: string;
