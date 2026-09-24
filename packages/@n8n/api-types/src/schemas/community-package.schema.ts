a2276db96e6b3295d4da8ab3c5023f4a1d532400
Ali Elkhateeb <ali.elkhateeb@n8n.io>
2026-09-24 10:38:10 +0300

    remove unnecessary schema

--- packages/@n8n/api-types/src/schemas/community-package.schema.ts
+++
@@ -1,4 +0,0 @@
-export {
-	communityPackagePublicSchema as communityPackageResponseSchema,
-	type CommunityPackagePublic as CommunityPackageResponse,
-} from '../dto/community-packages/community-package-public.dto';
